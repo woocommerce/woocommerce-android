596fb660096234471ef836e4202d68e9efbd57ad
Alex Forcier <alex.forcier@automattic.com>
2017-10-26 12:20:35 -0400

    Set up debug app with separate DI configuration

---
+++ WooCommerce/src/debug/kotlin/com.woocommerce.android/WooCommerceDebug.kt
@@ -0,0 +1,12 @@
+package com.woocommerce.android
+
+import com.woocommerce.android.di.AppComponent
+import com.woocommerce.android.di.DaggerAppComponentDebug
+
+open class WooCommerceDebug : WooCommerce() {
+    override val component: AppComponent by lazy {
+        DaggerAppComponentDebug.builder()
+                .application(this)
+                .build()
+    }
+}
