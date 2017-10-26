291aca6c18bd8e2fd05fd7c13e82e5800da83f24
Alex Forcier <alex.forcier@automattic.com>
2017-10-26 12:22:20 -0400

    Configure Stetho network interceptor

--- WooCommerce/src/debug/kotlin/com.woocommerce.android/di/AppComponentDebug.kt
+++ WooCommerce/src/debug/kotlin/com.woocommerce.android/di/AppComponentDebug.kt
@@ -6,7 +6,6 @@ import dagger.Component
 import dagger.android.support.AndroidSupportInjectionModule
 import org.wordpress.android.fluxc.module.ReleaseBaseModule
 import org.wordpress.android.fluxc.module.ReleaseNetworkModule
-import org.wordpress.android.fluxc.module.ReleaseOkHttpClientModule
 import javax.inject.Singleton
 
 @Singleton
@@ -16,7 +15,7 @@ import javax.inject.Singleton
         AppSecretsModule::class,
         ReleaseBaseModule::class,
         ReleaseNetworkModule::class,
-        ReleaseOkHttpClientModule::class))
+        DebugOkHttpClientModule::class))
 interface AppComponentDebug : AppComponent {
     @Component.Builder
     interface Builder : AppComponent.Builder {
