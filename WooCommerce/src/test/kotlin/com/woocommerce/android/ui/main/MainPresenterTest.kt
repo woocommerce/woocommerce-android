c3d1f7c57bc50fbff465160011464aa4df87eab4
Alex Forcier <alex.forcier@automattic.com>
2017-12-13 12:58:33 +1100

    Replace @Mock annotation with mockito-kotlin mock()

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/main/MainPresenterTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/main/MainPresenterTest.kt
@@ -1,29 +1,22 @@
 package com.woocommerce.android.ui.main
 
 import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.spy
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.store.AccountStore
 
 class MainPresenterTest {
-    @Mock private lateinit var mainContractView: MainContract.View
+    private val mainContractView: MainContract.View = mock()
 
-    @Mock private lateinit var dispatcher: Dispatcher
-    @Mock private lateinit var accountStore: AccountStore
+    private val dispatcher: Dispatcher = mock()
+    private val accountStore: AccountStore = mock()
 
     private lateinit var mainPresenter: MainPresenter
-
-    @Before
-    fun setup() {
-        // Inject the annotated mocks
-        MockitoAnnotations.initMocks(this)
-    }
 
     @Test
     fun `Reports AccountStore token status correctly`() {
