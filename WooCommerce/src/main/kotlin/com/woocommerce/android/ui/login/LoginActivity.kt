26818e3959d258a43ae1b096f472d64cbcd8e2cf
Alex Forcier <alex.forcier@automattic.com>
2018-01-29 19:19:38 +1100

    Add temporary toast about Google login being unsupported

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginActivity.kt
@@ -255,6 +255,8 @@ class LoginActivity : AppCompatActivity(), LoginListener, GoogleLoginListener, H
     }
 
     override fun addGoogleLoginFragment(parent: Fragment) {
+        // TODO: Remove this toast when social signin with Google is configured
+        ToastUtils.showToast(this, "Login with Google is not yet implemented")
         val fragmentManager = parent.childFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         var loginGoogleFragment = fragmentManager.findFragmentByTag(LoginGoogleFragment.TAG) as LoginGoogleFragment?
