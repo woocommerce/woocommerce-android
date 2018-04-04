c290adec0df8a000b54c48d671bcbee2ef819346
Aaron Douglas <astralbodies@gmail.com>
2018-04-04 07:21:25 -0500

    Removed integration into the Application lifecycle for now

--- WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
@@ -3,10 +3,6 @@ package com.woocommerce.android
 import android.app.Activity
 import android.app.Application
 import android.app.Service
-import android.content.ComponentCallbacks2
-import android.content.res.Configuration
-import android.os.Bundle
-import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.di.AppComponent
 import com.woocommerce.android.di.DaggerAppComponent
 import com.woocommerce.android.di.WooCommerceGlideModule
@@ -36,50 +32,10 @@ open class WooCommerce : Application(), HasActivityInjector, HasServiceInjector 
 
         component.inject(this)
         WellSql.init(WellSqlConfig(applicationContext))
-
-        val lifecycleMonitor = ApplicationLifecycleMonitor()
-        registerComponentCallbacks(lifecycleMonitor)
-        registerActivityLifecycleCallbacks(lifecycleMonitor)
-
-        AnalyticsTracker.init(applicationContext)
     }
 
     override fun activityInjector(): AndroidInjector<Activity> = activityInjector
 
     override fun serviceInjector(): AndroidInjector<Service> = serviceInjector
 
-    private class ApplicationLifecycleMonitor : Application.ActivityLifecycleCallbacks, ComponentCallbacks2 {
-        override fun onActivityPaused(activity: Activity?) {
-        }
-
-        override fun onActivityStarted(activity: Activity?) {
-        }
-
-        override fun onActivityDestroyed(activity: Activity?) {
-        }
-
-        override fun onActivitySaveInstanceState(activity: Activity?, bundle: Bundle?) {
-        }
-
-        override fun onActivityStopped(activity: Activity?) {
-        }
-
-        override fun onActivityCreated(activity: Activity?, bundle: Bundle?) {
-        }
-
-        override fun onActivityResumed(activity: Activity?) {
-            AnalyticsTracker.track(AnalyticsTracker.Stat.APPLICATION_OPENED)
-        }
-
-        override fun onConfigurationChanged(config: Configuration?) {
-        }
-
-        override fun onLowMemory() {
-        }
-
-        override fun onTrimMemory(level: Int) {
-            AnalyticsTracker.track(AnalyticsTracker.Stat.APPLICATION_CLOSED)
-            AnalyticsTracker.flush()
-        }
-    }
 }
