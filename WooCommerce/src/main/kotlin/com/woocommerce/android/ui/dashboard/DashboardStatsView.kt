02bb06c5428c5d6ae0f53c1697f6c33323db0783
Alex Forcier <alex.forcier@automattic.com>
2018-05-18 11:47:24 -0400

    Add and populate totals in order stats view

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardStatsView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardStatsView.kt
@@ -5,11 +5,12 @@ import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
-import com.woocommerce.android.R
-import kotlinx.android.synthetic.main.dashboard_stats.view.*
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.woocommerce.android.R
+import com.woocommerce.android.util.CurrencyUtils
+import kotlinx.android.synthetic.main.dashboard_stats.view.*
 
 class DashboardStatsView @JvmOverloads constructor(ctx: Context, attrs: AttributeSet? = null)
     : RelativeLayout(ctx, attrs) {
@@ -31,6 +32,9 @@ class DashboardStatsView @JvmOverloads constructor(ctx: Context, attrs: Attribut
         timeframe: StatsTimeframe = getActiveTimeframe()
     ) {
         barchart_progress.visibility = View.GONE
+
+        revenue_value.text = formatCurrencyAmountForDisplay(revenueStats.values.sum(), currencyCode)
+        orders_value.text = orderStats.values.sum().toString()
 
         if (revenueStats.isEmpty()) {
             // TODO Replace with custom empty view
@@ -64,4 +68,8 @@ class DashboardStatsView @JvmOverloads constructor(ctx: Context, attrs: Attribut
         // TODO: Return state of timeframe selector
         return StatsTimeframe.THIS_MONTH
     }
+
+    // TODO For certain currencies/locales, replace the thousands mark with k
+    private fun formatCurrencyAmountForDisplay(amount: Double, currencyCode: String?) =
+            CurrencyUtils.currencyStringRounded(context, amount, currencyCode ?: "")
 }
