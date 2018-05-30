3cfe17563e9851f835430f89542ed90ab09a56d1
AmandaRiu <amanda.riu@automattic.com>
2018-05-30 15:51:52 -0600

    Fragment navigation and refresh state logic

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/base/TopLevelFragmentView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/base/TopLevelFragmentView.kt
@@ -16,8 +16,11 @@ interface TopLevelFragmentView : FragmentManager.OnBackStackChangedListener {
     /**
      * Load the provided fragment into the current view and disable the main
      * underlying fragment view.
+     *
+     * @param fragment The child fragment to load
+     * @param tag The fragment tag for recovering fragment from back stack
      */
-    fun loadChildFragment(fragment: Fragment)
+    fun loadChildFragment(fragment: Fragment, tag: String)
 
     /**
      * Inflate the fragment view and return to be added to the parent
@@ -39,4 +42,20 @@ interface TopLevelFragmentView : FragmentManager.OnBackStackChangedListener {
      * Refresh this top-level fragment data and reset its state.
      */
     fun refreshFragmentState()
+
+    /**
+     * Pop fragments on back stack until the one labeled with this state tag
+     * is reached.
+     *
+     * @return True if the state was found and all fragments above that state
+     * in the back stack were popped. False if state not found - no action taken.
+     */
+    fun popToState(tag: String): Boolean
+
+    /**
+     * Locate a fragment on the back stack using the back stack tag provided.
+     *
+     * @return The fragment matching the provided tag, or null if not found.
+     */
+    fun getFragmentFromBackStack(tag: String): Fragment?
 }
