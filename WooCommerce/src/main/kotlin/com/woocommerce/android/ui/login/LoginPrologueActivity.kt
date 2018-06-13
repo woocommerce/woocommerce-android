692c49e16c003ebb4a9077c409fc6995f4dadff6
Nick Bradbury <nick.bradbury@gmail.com>
2018-06-13 18:05:10 -0400

    Changed status bar color to darker purple app-wide

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueActivity.kt
@@ -2,12 +2,8 @@ package com.woocommerce.android.ui.login
 
 import android.app.Activity
 import android.graphics.Paint
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import com.woocommerce.android.R
@@ -17,7 +13,6 @@ class LoginPrologueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_prologue)
-        setStatusBarColor()
 
         val btnLogin = findViewById<Button>(R.id.button_login_jetpack)
         btnLogin.setOnClickListener({
@@ -30,15 +25,6 @@ class LoginPrologueActivity : AppCompatActivity() {
         txtConfigLink.setOnClickListener {
             ActivityUtils.openUrlExternal(this,
                     "https://jetpack.com/support/getting-started-with-jetpack/")
-        }
-    }
-
-    private fun setStatusBarColor() {
-        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            val window = window
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = ContextCompat.getColor(this, R.color.wc_purple_status)
         }
     }
 }
