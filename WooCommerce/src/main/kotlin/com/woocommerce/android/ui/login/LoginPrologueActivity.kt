d573eb14abf52598ac5353c2ca35ed97a5c3f6bf
Nick Bradbury <nick.bradbury@gmail.com>
2018-06-13 15:39:07 -0400

    Added configuration link to prologue

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueActivity.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.login
 
 import android.app.Activity
+import android.graphics.Paint
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Bundle
@@ -8,7 +9,9 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
 import com.woocommerce.android.R
+import com.woocommerce.android.util.ActivityUtils
 
 class LoginPrologueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +24,13 @@ class LoginPrologueActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK)
             finish()
         })
+
+        val txtConfigLink = findViewById<TextView>(R.id.text_config_link)
+        txtConfigLink.paintFlags = txtConfigLink.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        txtConfigLink.setOnClickListener {
+            ActivityUtils.openUrlExternal(this,
+                    "https://jetpack.com/support/getting-started-with-jetpack/")
+        }
     }
 
     private fun setStatusBarColor() {
