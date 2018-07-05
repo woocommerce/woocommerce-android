caee778800eee5c80762f40ac97765585402b511
Nick Bradbury <nick.bradbury@gmail.com>
2018-07-05 15:13:42 -0400

    Added privacy sections to fragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.prefs
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.util.ActivityUtils
 import kotlinx.android.synthetic.main.fragment_privacy_settings.*
 
 class PrivacySettingsFragment : Fragment() {
     companion object {
-        const val TAG = "app-settings"
+        const val TAG = "privacy-settings"
+        private const val URL_PRIVACY_POLICY = "https://www.automattic.com/privacy"
+        private const val URL_COOKIE_POLICY = "https://www.automattic.com/cookies"
 
         fun newInstance(): PrivacySettingsFragment {
             return PrivacySettingsFragment()
@@ -29,10 +33,30 @@ class PrivacySettingsFragment : Fragment() {
         switchSendStats.setOnClickListener {
             AnalyticsTracker.sendUsageStats = switchSendStats.isChecked
         }
+
+        buttonLearnMore.setOnClickListener {
+            showCookiePolicy()
+        }
+
+        buttonPrivacyPolicy.setOnClickListener {
+            showPrivacyPolicy()
+        }
+
+        buttonTracking.setOnClickListener {
+            showCookiePolicy()
+        }
     }
 
     override fun onResume() {
         super.onResume()
         activity?.setTitle(R.string.privacy_settings)
+    }
+
+    private fun showCookiePolicy() {
+        ActivityUtils.openUrlExternal(activity as Context, URL_COOKIE_POLICY)
+    }
+
+    private fun showPrivacyPolicy() {
+        ActivityUtils.openUrlExternal(activity as Context, URL_PRIVACY_POLICY)
     }
 }
