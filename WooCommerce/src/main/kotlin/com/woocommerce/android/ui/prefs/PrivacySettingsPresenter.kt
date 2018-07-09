efc2a489067f2ba76850dae119129543bbbaf70e
Nick Bradbury <nick.bradbury@gmail.com>
2018-07-09 17:49:11 -0400

    Sync usage tracking with wp.com

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsPresenter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsPresenter.kt
@@ -4,11 +4,15 @@ import com.woocommerce.android.analytics.AnalyticsTracker
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.AccountActionBuilder
+import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged
+import org.wordpress.android.fluxc.store.AccountStore.PushAccountSettingsPayload
 import javax.inject.Inject
 
 class PrivacySettingsPresenter @Inject constructor(
-    private val dispatcher: Dispatcher
+    private val dispatcher: Dispatcher,
+        private val accountStore: AccountStore
 ) : PrivacySettingsContract.Presenter {
     private var privacySettingsFragmentView: PrivacySettingsContract.View? = null
 
@@ -24,12 +28,22 @@ class PrivacySettingsPresenter @Inject constructor(
 
     override fun updateUsagePref(allowUsageTracking: Boolean) {
         AnalyticsTracker.sendUsageStats = allowUsageTracking
-        // TODO: update in wp account
+
+        // sync with wpcom if a token is available
+        if (accountStore.hasAccessToken()) {
+            accountStore.getAccount().setTracksOptOut(!allowUsageTracking)
+            val payload = PushAccountSettingsPayload()
+            payload.params = HashMap<String, Any>()
+            payload.params.put("tracks_opt_out", !allowUsageTracking)
+            dispatcher.dispatch(AccountActionBuilder.newPushSettingsAction(payload))
+        }
     }
 
     @Suppress("unused")
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun onAccountChanged(event: OnAccountChanged) {
-        // TODO ?
+        if (event.isError) {
+            // TODO ?
+        }
     }
 }
