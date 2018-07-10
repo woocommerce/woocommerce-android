35ae40e1632044a25434437297bba28ed3554637
Alex Forcier <alex.forcier@automattic.com>
2018-07-10 16:16:31 -0400

    Add unit tests for short month date utils

--- WooCommerce/src/test/kotlin/com/woocommerce/android/util/DateUtilsTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/util/DateUtilsTest.kt
@@ -23,4 +23,25 @@ class DateUtilsTest {
     fun `getNumberOfDaysInMonth() throws exception on invalid string`() {
         assertEquals(29, DateUtils.getNumberOfDaysInMonth("invalid"))
     }
+
+    @Test
+    fun `getShortMonthDayString() returns correct values`() {
+        assertEquals("Jul 3", DateUtils.getShortMonthDayString("2018-07-03"))
+        assertEquals("Jul 28", DateUtils.getShortMonthDayString("2018-07-28"))
+    }
+
+    @Test
+    fun `getShortMonthDayStringForWeek() returns correct values`() {
+        assertEquals("Mar 12", DateUtils.getShortMonthDayStringForWeek("2018-W11"))
+        // Jan 1 2018 happened to be a Monday, so the first day of the first week happens to also be
+        // the first day of the year
+        assertEquals("Jan 1", DateUtils.getShortMonthDayStringForWeek("2018-W1"))
+        assertEquals("Jan 2", DateUtils.getShortMonthDayStringForWeek("2017-W1"))
+    }
+
+    @Test
+    fun `getShortMonthString() returns correct values`() {
+        assertEquals("Jul", DateUtils.getShortMonthString("2018-07"))
+        assertEquals("Jan", DateUtils.getShortMonthString("2017-01"))
+    }
 }
