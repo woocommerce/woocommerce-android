0cacf63837227bcd7b89a08c95f949c440652f11
Nick Bradbury <nick.bradbury@gmail.com>
2018-07-11 16:40:31 -0400

    Show "X" in toolbar

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderDetailAddNoteActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderDetailAddNoteActivity.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.orders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.woocommerce.android.R
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,7 +20,10 @@ class OrderDetailAddNoteActivity : AppCompatActivity(), OrderDetailAddNoteContra
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_detail_add_note)
+
         setSupportActionBar(toolbar as Toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_gridicons_cross_white_24dp)
 
         presenter.takeView(this)
     }
@@ -27,5 +31,15 @@ class OrderDetailAddNoteActivity : AppCompatActivity(), OrderDetailAddNoteContra
     override fun onDestroy() {
         presenter.dropView()
         super.onDestroy()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
