956e74529d81108e77ed70efccf7338a0b90e429
Alex Forcier <alex.forcier@automattic.com>
2018-07-30 22:36:18 -0400

    Call FCMRegistrationIntentService when InstanceID token is updated

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/push/InstanceIDService.kt
@@ -0,0 +1,18 @@
+package com.woocommerce.android.push
+
+import android.content.Intent
+
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class InstanceIDService : FirebaseInstanceIdService() {
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is also called
+     * when the InstanceID token is initially generated, so this is where
+     * you retrieve the token.
+     */
+    override fun onTokenRefresh() {
+        // Register for Cloud messaging
+        FCMRegistrationIntentService.enqueueWork(this, Intent(this, FCMRegistrationIntentService::class.java))
+    }
+}
