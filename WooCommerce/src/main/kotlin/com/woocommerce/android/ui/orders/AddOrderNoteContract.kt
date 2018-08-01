966f9cdd270160edda203971da69aa5ace5f7040
Nick Bradbury <nick.bradbury@gmail.com>
2018-08-01 13:22:26 -0400

    Removed unused import

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/AddOrderNoteContract.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/AddOrderNoteContract.kt
@@ -2,7 +2,6 @@ package com.woocommerce.android.ui.orders
 
 import com.woocommerce.android.ui.base.BasePresenter
 import com.woocommerce.android.ui.base.BaseView
-import org.wordpress.android.fluxc.model.WCOrderModel
 import org.wordpress.android.fluxc.model.order.OrderIdentifier
 
 interface AddOrderNoteContract {
