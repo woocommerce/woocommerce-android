d2151c9c84464df3a1b2800c63e27129720e7086
Nick Bradbury <nick.bradbury@gmail.com>
2018-08-14 14:33:47 -0400

    Use ObjectAnimator

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/WooAniUtils.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/WooAniUtils.kt
@@ -1,9 +1,13 @@
 package com.woocommerce.android.util
 
 import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Context
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
 
 object WooAniUtils {
     enum class Duration {
@@ -23,43 +27,36 @@ object WooAniUtils {
     private val DEFAULT_DURATION = Duration.MEDIUM
 
     fun scaleIn(target: View, duration: Duration = DEFAULT_DURATION) {
-        with (target) {
-            scaleX = 0f
-            scaleY = 0f
-            visibility = View.VISIBLE
-            animate()
-                    .scaleX(1f)
-                    .scaleY(1f)
-                    .setInterpolator(AccelerateDecelerateInterpolator())
-                    .setDuration(duration.toMillis(target.context))
-        }
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 0f, 1f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 0f, 1f)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(target, scaleX, scaleY)
+        animator.duration = duration.toMillis(target.context)
+        animator.interpolator = AccelerateDecelerateInterpolator()
+
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator) {
+                target.visibility = View.VISIBLE
+            }
+        })
+
+        animator.start()
     }
 
     fun scaleOut(target: View, duration: Duration = DEFAULT_DURATION) {
-        with(target) {
-            scaleX = 1f
-            scaleY = 1f
-            animate()
-                    .scaleX(0f)
-                    .scaleY(0f)
-                    .setInterpolator(AccelerateDecelerateInterpolator())
-                    .setDuration(duration.toMillis(target.context))
-                    .setListener(object : Animator.AnimatorListener {
-                        override fun onAnimationStart(animation: Animator?) {
-                            // noop
-                        }
-                        override fun onAnimationRepeat(animation: Animator?) {
-                            // noop
-                        }
-                        override fun onAnimationEnd(animation: Animator?) {
-                            target.visibility = View.GONE
-                            target.scaleX = 1f
-                            target.scaleY = 1f
-                        }
-                        override fun onAnimationCancel(animation: Animator?) {
-                            // noop
-                        }
-                    })
-        }
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, 0f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, 0f)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(target, scaleX, scaleY)
+        animator.duration = duration.toMillis(target.context)
+        animator.interpolator = AccelerateInterpolator()
+
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                target.visibility = View.GONE
+            }
+        })
+
+        animator.start()
     }
 }
