2a98d0c96fc34f1be1a5873d88aac8a2c480dd4e
Nick Bradbury <nick.bradbury@gmail.com>
2018-08-15 10:49:54 -0400

    Removed unnecessary code involving refresh

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardFragment.kt
@@ -88,7 +88,6 @@ class DashboardFragment : TopLevelFragment(), DashboardContract.View, DashboardS
         // If this fragment is now visible and we've deferred loading data due to it not
         // being visible - go ahead and load the data.
         if (isActive && loadDataPending) {
-            loadDataPending = false
             hideUnfilledOrdersCard()
             refreshDashboard()
         }
@@ -129,12 +128,7 @@ class DashboardFragment : TopLevelFragment(), DashboardContract.View, DashboardS
     }
 
     override fun refreshFragmentState() {
-        if (isActive) {
-            setLoadingIndicator(true)
-            refreshDashboard()
-        } else {
-            loadDataPending = true
-        }
+        refreshDashboard()
     }
 
     override fun refreshDashboard() {
