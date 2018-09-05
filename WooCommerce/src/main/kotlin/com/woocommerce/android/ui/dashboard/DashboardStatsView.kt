e5fbdc500b35dca6c16a3353d3b14a67a49e1a18
Nick Bradbury <nick.bradbury@gmail.com>
2018-09-05 09:33:58 -0400

    Changed grid color to wc_border_color

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardStatsView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardStatsView.kt
@@ -154,7 +154,7 @@ class DashboardStatsView @JvmOverloads constructor(ctx: Context, attrs: Attribut
                 setDrawZeroLine(false)
                 setDrawAxisLine(false)
                 setDrawGridLines(true)
-                gridColor = ContextCompat.getColor(context, R.color.gray)
+                gridColor = ContextCompat.getColor(context, R.color.wc_border_color)
                 setLabelCount(3, true)
 
                 axisMinimum = 0F
