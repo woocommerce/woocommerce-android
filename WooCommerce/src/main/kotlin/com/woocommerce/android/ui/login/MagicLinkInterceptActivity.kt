b62296fd8cae3aa18960bbf2f4f24ff810adb33f
AmandaRiu <amanda.riu@automattic.com>
2018-09-17 09:55:32 -0400

    Remove "back pressed" tracking logic from activity.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/MagicLinkInterceptActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/MagicLinkInterceptActivity.kt
@@ -3,9 +3,6 @@ package com.woocommerce.android.ui.login
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
-import com.woocommerce.android.analytics.AnalyticsTracker
-import com.woocommerce.android.analytics.AnalyticsTracker.Stat
 import com.woocommerce.android.ui.main.MainActivity
 import dagger.android.AndroidInjection
 import org.wordpress.android.login.LoginAnalyticsListener
@@ -13,8 +10,6 @@ import javax.inject.Inject
 
 class MagicLinkInterceptActivity : Activity() {
     @Inject internal lateinit var loginAnalyticsListener: LoginAnalyticsListener
-
-    private var isUpEvent = false // Tracks if the user clicked the up menu or device back button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -29,22 +24,5 @@ class MagicLinkInterceptActivity : Activity() {
                 Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         finish()
-    }
-
-    override fun onBackPressed() {
-        if (!isUpEvent) {
-            isUpEvent = false
-            AnalyticsTracker.track(Stat.DEVICE_BACK_BUTTON_TAPPED)
-        }
-        super.onBackPressed()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return item?.itemId?.let {
-            AnalyticsTracker.track(Stat.MAIN_MENU_UP_TAPPED)
-            isUpEvent = true
-            onBackPressed()
-            true
-        } ?: super.onOptionsItemSelected(item)
     }
 }
