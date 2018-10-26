e4f8f8a7d4efff63fe71eacc366934b312d6a287
Nick Bradbury <nick.bradbury@gmail.com>
2018-10-26 11:02:50 -0400

    Hide marker when empty bar tapped

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardStatsMarkerView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardStatsMarkerView.kt
@@ -1,12 +1,14 @@
 package com.woocommerce.android.ui.dashboard
 
 import android.content.Context
+import android.view.View
 import android.widget.TextView
 import com.github.mikephil.charting.components.MarkerView
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.utils.MPPointF
 import com.woocommerce.android.R
+import kotlinx.android.synthetic.main.dashboard_stats_marker_view.view.*
 
 /**
  * Custom MarkerView which appears on the stats chart when the user taps a bar
@@ -20,7 +22,12 @@ class DashboardStatsMarkerView(context: Context, layoutResource: Int) : MarkerVi
     var captionListener: RequestMarkerCaptionListener? = null
 
     override fun refreshContent(entry: Entry, highlight: Highlight) {
-        tvContent.text = captionListener?.onRequestMarkerCaption(entry)
+        if (entry.y <= 0) {
+            marker_container.visibility = View.GONE
+        } else {
+            tvContent.text = captionListener?.onRequestMarkerCaption(entry)
+            marker_container.visibility = View.VISIBLE
+        }
         super.refreshContent(entry, highlight)
     }
 
