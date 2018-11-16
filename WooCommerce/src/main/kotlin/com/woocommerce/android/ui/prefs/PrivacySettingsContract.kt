88c56c88e4444c03cc50510854c4fe1d603d40e9
Nick Bradbury <nick.bradbury@gmail.com>
2018-11-16 09:26:00 -0500

    Call `initCrashlytics` when user enables crash reporting

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsContract.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsContract.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.prefs
 
+import android.content.Context
 import com.woocommerce.android.ui.base.BasePresenter
 import com.woocommerce.android.ui.base.BaseView
 
@@ -8,7 +9,7 @@ interface PrivacySettingsContract {
         fun getSendUsageStats(): Boolean
         fun setSendUsageStats(sendUsageStats: Boolean)
         fun getCrashReportingEnabled(): Boolean
-        fun setCrashReportingEnabled(enabled: Boolean)
+        fun setCrashReportingEnabled(context: Context, enabled: Boolean)
     }
 
     interface View : BaseView<Presenter> {
