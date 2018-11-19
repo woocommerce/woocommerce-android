543437958711a3d40510383bff3753642ab46425
Alex Forcier <alex.forcier@automattic.com>
2018-11-19 16:10:40 -0500

    Use translatable labels for OrderStatusFilterDialog

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderStatusFilterDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderStatusFilterDialog.kt
@@ -27,7 +27,7 @@ class OrderStatusFilterDialog : DialogFragment() {
         ): OrderStatusFilterDialog {
             val fragment = OrderStatusFilterDialog()
             fragment.listener = listener
-            fragment.selectedFilter = currentFilter
+            fragment.selectedFilter = currentFilter?.value ?: "all"
             return fragment
         }
     }
@@ -36,15 +36,37 @@ class OrderStatusFilterDialog : DialogFragment() {
         fun onFilterSelected(orderStatus: String?)
     }
 
-    val filterOptions: Array<String> by lazy {
-        arrayOf(resources.getString(R.string.all)).plus(CoreOrderStatus.values().map { it.label }.toTypedArray())
+    private val filterOptions: Array<String> by lazy {
+        arrayOf(resources.getString(R.string.all)).plus(CoreOrderStatus.values().map { statusToLabelMap[it] }.toTypedArray())
+    }
+
+    private val stringIds: Array<String> by lazy {
+        arrayOf("all").plus(CoreOrderStatus.values().map { it.value }.toTypedArray())
+    }
+
+    private val statusToLabelMap: Map<CoreOrderStatus, String> by lazy {
+        mapOf(
+                CoreOrderStatus.PENDING to getString(R.string.orderstatus_pending),
+                CoreOrderStatus.PROCESSING to getString(R.string.orderstatus_processing),
+                CoreOrderStatus.ON_HOLD to getString(R.string.orderstatus_hold),
+                CoreOrderStatus.COMPLETED to getString(R.string.orderstatus_completed),
+                CoreOrderStatus.CANCELLED to getString(R.string.orderstatus_cancelled),
+                CoreOrderStatus.REFUNDED to getString(R.string.orderstatus_refunded),
+                CoreOrderStatus.FAILED to getString(R.string.orderstatus_failed)
+        )
     }
 
     var listener: OrderListFilterListener? = null
-    var selectedFilter: CoreOrderStatus? = null
+    var selectedFilter: String? = null
+
+    private fun getOrderStatusAtIndex(index: Int) = stringIds[index]
+
+    private fun getCurrentOrderStatusIndex(): Int {
+        return stringIds.indexOfFirst { it == selectedFilter }
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val selectedIndex = selectedFilter?.ordinal?.inc() ?: 0
+        val selectedIndex = getCurrentOrderStatusIndex()
 
         return AlertDialog.Builder(context)
                 .setTitle(resources.getString(R.string.orderlist_filter_by))
@@ -55,14 +77,15 @@ class OrderStatusFilterDialog : DialogFragment() {
                     AnalyticsTracker.track(Stat.FILTER_ORDERS_BY_STATUS_DIALOG_OPTION_SELECTED,
                             mapOf("status" to selectedLabel))
 
-                    selectedFilter = CoreOrderStatus.fromLabel(selectedLabel)
+                    selectedFilter = getOrderStatusAtIndex(which)
                 }
                 .setPositiveButton(R.string.orderlist_filter_apply) { dialog, _ ->
                     AnalyticsTracker.track(Stat.FILTER_ORDERS_BY_STATUS_DIALOG_APPLY_FILTER_BUTTON_TAPPED)
 
-                    val newSelectedIndex = selectedFilter?.ordinal?.inc() ?: 0
+                    val newSelectedIndex = getCurrentOrderStatusIndex()
                     if (newSelectedIndex != selectedIndex) {
-                        listener?.onFilterSelected(selectedFilter?.value)
+                        // If 'All' is selected filter, pass null to signal a filterless refresh
+                        listener?.onFilterSelected(if (newSelectedIndex > 0) selectedFilter else null)
                     }
                     dialog.cancel()
                 }.create()
