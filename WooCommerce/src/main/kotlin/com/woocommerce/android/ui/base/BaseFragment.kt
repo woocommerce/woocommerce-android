0a25838cf915dd075c46befb7a8f2084d04af8ef
Nick Bradbury <nick.bradbury@gmail.com>
2018-11-20 14:22:02 -0500

    Updated BaseFragment comment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/base/BaseFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/base/BaseFragment.kt
@@ -3,10 +3,12 @@ package com.woocommerce.android.ui.base
 import android.support.v4.app.Fragment
 import com.woocommerce.android.ui.base.TopLevelFragment.FragmentScrollListener
 
+/**
+ * All top-level fragments and their child fragments should extend this class, and if they contain
+ * a scrolling view they should implement the two functions below. We use these to tell the main
+ * activity to show/hide the bottom navbar when the user scrolls the fragment
+ */
 abstract class BaseFragment : Fragment() {
-    /**
-     * We use these to tell the main activity to show/hide the bottom navbar when the user scrolls the fragment
-     */
     fun onScrollUp() {
         if (activity is FragmentScrollListener) {
             (activity as FragmentScrollListener).onFragmentScrollUp()
