a5e73e1835696173c6cbf0acdb0c3b61e867f753
Nick Bradbury <nick.bradbury@gmail.com>
2018-12-06 15:27:19 -0500

    Added analytics for notifs settings changes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsFragment.kt
@@ -13,6 +13,7 @@ import com.woocommerce.android.analytics.AnalyticsTracker.Stat.SETTINGS_ABOUT_OP
 import com.woocommerce.android.analytics.AnalyticsTracker.Stat.SETTINGS_ABOUT_WOOCOMMERCE_LINK_TAPPED
 import com.woocommerce.android.analytics.AnalyticsTracker.Stat.SETTINGS_LOGOUT_BUTTON_TAPPED
 import com.woocommerce.android.analytics.AnalyticsTracker.Stat.SETTINGS_PRIVACY_SETTINGS_BUTTON_TAPPED
+import com.woocommerce.android.analytics.AnalyticsTracker.Stat.SETTING_CHANGE
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_settings_main.*
 import javax.inject.Inject
@@ -20,6 +21,9 @@ import javax.inject.Inject
 class MainSettingsFragment : Fragment(), MainSettingsContract.View {
     companion object {
         const val TAG = "main-settings"
+        private const val SETTING_NOTIFS_ORDERS = "notifications_orders"
+        private const val SETTING_NOTIFS_REVIEWS = "notifications_reviews"
+        private const val SETTING_NOTIFS_TONE = "notifications_tone"
 
         fun newInstance(): MainSettingsFragment {
             return MainSettingsFragment()
@@ -65,17 +69,20 @@ class MainSettingsFragment : Fragment(), MainSettingsContract.View {
 
         switchNotifsOrders.isChecked = AppPrefs.isOrderNotificationsEnabled()
         switchNotifsOrders.setOnCheckedChangeListener { _, isChecked ->
+            doSettingToggled(SETTING_NOTIFS_ORDERS, isChecked)
             AppPrefs.setOrderNotificationsEnabled(isChecked)
             updateNotificationSettings()
         }
 
         switchNotifsReviews.isChecked = AppPrefs.isReviewNotificationsEnabled()
         switchNotifsReviews.setOnCheckedChangeListener { _, isChecked ->
+            doSettingToggled(SETTING_NOTIFS_REVIEWS, isChecked)
             AppPrefs.setReviewNotificationsEnabled(isChecked)
         }
 
         switchNotifsTone.isChecked = AppPrefs.isOrderNotificationsChaChingEnabled()
         switchNotifsTone.setOnCheckedChangeListener { _, isChecked ->
+            doSettingToggled(SETTING_NOTIFS_TONE, isChecked)
             AppPrefs.setOrderNotificationsChaChingEnabled(isChecked)
         }
 
@@ -97,6 +104,13 @@ class MainSettingsFragment : Fragment(), MainSettingsContract.View {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        AnalyticsTracker.trackViewShown(this)
+
+        activity?.setTitle(R.string.settings)
+    }
+
     /**
      * The review and tone notification switches are only enabled when order notifications are enabled
      */
@@ -106,10 +120,11 @@ class MainSettingsFragment : Fragment(), MainSettingsContract.View {
         switchNotifsTone.isEnabled = enable
     }
 
-    override fun onResume() {
-        super.onResume()
-        AnalyticsTracker.trackViewShown(this)
-
-        activity?.setTitle(R.string.settings)
+    private fun doSettingToggled(keyName: String, newValue: Boolean) {
+        AnalyticsTracker.track(
+                SETTING_CHANGE, mapOf(
+                AnalyticsTracker.KEY_NAME to keyName,
+                AnalyticsTracker.KEY_FROM to !newValue,
+                AnalyticsTracker.KEY_TO to newValue))
     }
 }
