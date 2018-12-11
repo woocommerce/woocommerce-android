1e2d22ed9f3aa6a08b252f07215d6fd91a9aebca
AmandaRiu <amanda.riu@automattic.com>
2018-12-10 19:38:27 -0500

    Add logic for enabling buttons, listeners and state

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/notifications/ReviewDetailFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/notifications/ReviewDetailFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton.OnCheckedChangeListener
 import com.woocommerce.android.R
 import com.woocommerce.android.di.GlideApp
 import com.woocommerce.android.extensions.canMarkAsSpam
@@ -18,15 +19,14 @@ import com.woocommerce.android.extensions.getRating
 import com.woocommerce.android.extensions.getReviewDetail
 import com.woocommerce.android.ui.base.UIMessageResolver
 import com.woocommerce.android.util.ActivityUtils
+import com.woocommerce.android.util.WooLog
+import com.woocommerce.android.util.WooLog.T.NOTIFICATIONS
 import com.woocommerce.android.widgets.SkeletonView
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_review_detail.*
 import org.wordpress.android.fluxc.model.CommentModel
 import org.wordpress.android.fluxc.model.CommentStatus
 import org.wordpress.android.fluxc.model.CommentStatus.APPROVED
-import org.wordpress.android.fluxc.model.CommentStatus.DELETED
-import org.wordpress.android.fluxc.model.CommentStatus.SPAM
-import org.wordpress.android.fluxc.model.CommentStatus.TRASH
 import org.wordpress.android.fluxc.model.CommentStatus.UNAPPROVED
 import org.wordpress.android.fluxc.model.notification.NotificationModel
 import org.wordpress.android.util.DateTimeUtils
@@ -44,7 +44,7 @@ class ReviewDetailFragment : Fragment(), ReviewDetailContract.View {
         fun newInstance(notification: NotificationModel): ReviewDetailFragment {
             val args = Bundle()
             args.putLong(FIELD_REMOTE_NOTIF_ID, notification.remoteNoteId)
-            args.putLong(FIELD_REMOTE_COMMENT_ID, notification.getCommentId() ?: 0L)
+            args.putLong(FIELD_REMOTE_COMMENT_ID, notification.getCommentId())
             args.putParcelable(FIELD_REVIEW_DETAIL, notification.getReviewDetail())
 
             val fragment = ReviewDetailFragment()
@@ -60,6 +60,12 @@ class ReviewDetailFragment : Fragment(), ReviewDetailContract.View {
     private var remoteNoteId: Long = 0L
     private var remoteCommentId: Long = 0L
     private var productUrl: String? = null
+    private val moderateListener = OnCheckedChangeListener { _, isChecked ->
+        when (isChecked) {
+            true -> approveReview()
+            false -> disapproveReview()
+        }
+    }
 
     override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
@@ -127,55 +133,45 @@ class ReviewDetailFragment : Fragment(), ReviewDetailContract.View {
     }
 
     private fun configureModerationButtons(note: NotificationModel) {
-        if (note.canModerate()) {
-            review_approve.visibility = View.VISIBLE
-            review_approve.setOnCheckedChangeListener { _, isChecked ->
-                when (isChecked) {
-                    true -> disapproveReview()
-                    false -> approveReview()
-                }
+        // Configure the moderate button
+        with (review_approve) {
+            if (note.canModerate()) {
+                visibility = View.VISIBLE
+                setOnCheckedChangeListener(moderateListener)
+            } else {
+                visibility = View.GONE
             }
-        } else {
-            review_approve.visibility = View.GONE
         }
 
-        if (note.canMarkAsSpam()) {
-            review_spam.visibility = View.VISIBLE
-            review_spam.setOnCheckedChangeListener { _, isChecked ->
-                when (isChecked) {
-                    // User has marked this review as spam
-                    true -> spamReview()
-                    // User has marked this review as not spam, set as approved automatically
-                    false -> approveReview()
-                }
+        // Configure the spam button
+        with (review_spam) {
+            if (note.canMarkAsSpam()) {
+                visibility = View.VISIBLE
+                setOnClickListener { spamReview() }
+            } else {
+                visibility = View.GONE
             }
-        } else {
-            review_spam.visibility = View.GONE
         }
 
-        if (note.canTrash()) {
-            review_trash.visibility = View.VISIBLE
-            review_trash.setOnCheckedChangeListener { _, isChecked ->
-                when (isChecked) {
-                    true -> trashReview()
-                    // If un-trashed, mark it as approved automatically
-                    false -> approveReview()
-                }
+        // Configure the trash button
+        with (review_trash) {
+            if (note.canTrash()) {
+                visibility = View.VISIBLE
+                setOnClickListener { trashReview() }
+            } else {
+                visibility = View.GONE
             }
-        } else {
-            review_trash.visibility = View.GONE
         }
     }
 
     override fun updateStatus(status: CommentStatus) {
+        review_approve.setOnCheckedChangeListener(null)
         when (status) {
             APPROVED -> review_approve.isChecked = true
             UNAPPROVED -> review_approve.isChecked = false
-            SPAM -> {}
-            TRASH -> {}
-            DELETED -> {}
-            else -> {}
+            else -> WooLog.w(NOTIFICATIONS, "Unable to process Notification with a status of $status")
         }
+        review_approve.setOnCheckedChangeListener(moderateListener)
     }
 
     override fun showLoadReviewError() {
