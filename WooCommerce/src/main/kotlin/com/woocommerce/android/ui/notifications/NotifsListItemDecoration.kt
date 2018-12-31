874555ad246ac8a551b3e42b3761c099061dd3a6
Nick Bradbury <nick.bradbury@gmail.com>
2018-12-31 10:07:29 -0500

    Simplified by using drawRect()

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/notifications/NotifsListItemDecoration.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/notifications/NotifsListItemDecoration.kt
@@ -2,37 +2,36 @@ package com.woocommerce.android.ui.notifications
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Rect
-import android.graphics.drawable.Drawable
+import android.graphics.Paint
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import com.woocommerce.android.R
 import org.wordpress.android.util.DisplayUtils
 
 class NotifsListItemDecoration(context: Context) : DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL) {
-    private val dividerWidth: Int = DisplayUtils.dpToPx(context, 10)
-    private val divider: Drawable? = ContextCompat.getDrawable(context, R.drawable.notifs_unread_indicator)
+    private val dividerWidth = DisplayUtils.dpToPx(context, 10).toFloat()
+    private val dividerPaint = Paint()
 
-    override fun getItemOffsets(
-        outRect: Rect, view: View, parent: RecyclerView,
-        state: RecyclerView.State
-    ) {
-        outRect.right = outRect.left + dividerWidth
-        outRect.bottom = view.layoutParams.height
+    init {
+        dividerPaint.color = ContextCompat.getColor(context, R.color.wc_green)
     }
 
-    override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        val childCount = parent.childCount
-        for (i in 0 until childCount) {
+    override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state:  RecyclerView.State) {
+        for (i in 0 until parent.childCount) {
             val child = parent.getChildAt(i)
-            val params = child.layoutParams as RecyclerView.LayoutParams
-            val top = child.bottom + params.bottomMargin
-            val bottom = top + params.height
+            val position = parent.getChildAdapterPosition(child)
+            val viewType = parent.adapter.getItemViewType(position)
 
-            divider?.setBounds(0, 0, dividerWidth, bottom)
-            divider?.draw(canvas)
+            // a viewType of 0 is a header
+            if (viewType > 0) {
+                val left = child.left.toFloat()
+                val right = left + dividerWidth
+                val top = child.top.toFloat()
+                val bottom = top + child.bottom.toFloat()
+
+                canvas.drawRect(left, top, right, bottom, dividerPaint)
+            }
         }
     }
 }
