3a61a9ceadfccc3c9a3d386ad1d197380e85c75a
Alex Forcier <alex.forcier@automattic.com>
2019-01-10 14:29:57 -0500

    Move API path regex to helper file

--- WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
@@ -21,6 +21,8 @@ import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.ApplicationLifecycleMonitor
 import com.woocommerce.android.util.ApplicationLifecycleMonitor.ApplicationLifecycleListener
 import com.woocommerce.android.util.CrashlyticsUtils
+import com.woocommerce.android.util.REGEX_API_JETPACK_TUNNEL_METHOD
+import com.woocommerce.android.util.REGEX_API_NUMERIC_PARAM
 import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.WooLog.T
 import com.yarolegovich.wellsql.WellSql
@@ -202,8 +204,8 @@ open class WooCommerce : MultiDexApplication(), HasActivityInjector, HasServiceI
     fun onJetpackTimeoutError(event: OnJetpackTimeoutError) {
         with(event) {
             // Replace numeric IDs with a placeholder so events can be aggregated
-            val genericPath = apiPath.replace(Regex("/[0-9]*/"), "/ID/")
-            val protocol = Regex("_method=([a-z]*)").find(apiPath)?.groups?.get(1)?.value ?: ""
+            val genericPath = apiPath.replace(REGEX_API_NUMERIC_PARAM, "/ID/")
+            val protocol = REGEX_API_JETPACK_TUNNEL_METHOD.find(apiPath)?.groups?.get(1)?.value ?: ""
 
             val properties = mapOf(
                     "path" to genericPath,
