100bc8c954ec8a8bb35da6e644af0541d026b50b
Alex Forcier <alex.forcier@automattic.com>
2019-01-28 16:33:53 -0500

    Add currency formatter builder helper function

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/CurrencyFormatter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/CurrencyFormatter.kt
@@ -7,4 +7,17 @@ class CurrencyFormatter(private val wcStore: WooCommerceStore, private val selec
     fun formatCurrency(rawValue: String, currencyCode: String, applyDecimalFormatting: Boolean): String {
         return wcStore.formatCurrencyForDisplay(rawValue, selectedSite.get(), currencyCode, applyDecimalFormatting)
     }
+
+    /**
+     * Utility function that returns a reduced function for formatting currencies for orders.
+     *
+     * For order objects, we generally want to show exact values, and the currency used can be set once at a global
+     * level - then the same function can be used for all the various currency fields of an order.
+     *
+     * @param currencyCode the ISO 4217 currency code to use for formatting
+     * @return a function which, given a raw amount as a String, returns the String formatted for display as a currency
+     */
+    fun buildFormatter(currencyCode: String) = { rawValue: String? ->
+        formatCurrency(rawValue ?: "0.0", currencyCode, true)
+    }
 }
