b78b5b716e14b077c173aafb8b3c9d3eefc740b0
Nick Bradbury <nick.bradbury@gmail.com>
2019-02-13 08:39:33 -0500

    Use builder pattern

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/ChromeCustomTabUtils.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/ChromeCustomTabUtils.kt
@@ -8,12 +8,12 @@ import com.woocommerce.android.R
 
 object ChromeCustomTabUtils {
     fun viewUrl(context: Context, url: String) {
-        val builder = CustomTabsIntent.Builder()
-        builder.setToolbarColor(ContextCompat.getColor(context, R.color.wc_purple))
-        builder.setStartAnimations(context, R.anim.activity_slide_in_from_right, 0)
-        builder.setExitAnimations(context, 0, R.anim.activity_slide_out_to_right)
-
-        val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(context, Uri.parse(url))
+        CustomTabsIntent.Builder()
+                .setToolbarColor(ContextCompat.getColor(context, R.color.wc_purple))
+                .setStartAnimations(context, R.anim.activity_slide_in_from_right, 0)
+                .setExitAnimations(context, 0, R.anim.activity_slide_out_to_right)
+                .setShowTitle(true)
+                .build()
+                .launchUrl(context, Uri.parse(url))
     }
 }
