a28626df53977e44a9da577b9ec901bc0dd8043e
AmandaRiu <amanda.riu@automattic.com>
2019-04-01 18:34:58 -0500

    Add order shipment custom layouts and view logic

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderDetailShipmentTrackingItemView.kt
@@ -0,0 +1,31 @@
+package com.woocommerce.android.ui.orders
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.view.View
+import com.woocommerce.android.R
+import com.woocommerce.android.util.ChromeCustomTabUtils
+import kotlinx.android.synthetic.main.order_detail_shipment_tracking_item.view.*
+import org.wordpress.android.fluxc.model.WCOrderShipmentTrackingModel
+
+class OrderDetailShipmentTrackingItemView @JvmOverloads constructor(
+    ctx: Context,
+    attrs: AttributeSet? = null
+) : ConstraintLayout(ctx, attrs) {
+    init {
+        View.inflate(context, R.layout.order_detail_shipment_tracking_item, this)
+    }
+
+    fun initView(item: WCOrderShipmentTrackingModel) {
+        tracking_type.text = item.trackingProvider
+        tracking_number.text = item.trackingNumber
+
+        if (item.trackingLink.isNotEmpty()) {
+            tracking_btnTrack.setOnClickListener {
+                // TODO tracks analytics
+                ChromeCustomTabUtils.launchUrl(context, item.trackingLink)
+            }
+        }
+    }
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderDetailShipmentTrackingListView.kt
@@ -0,0 +1,57 @@
+package com.woocommerce.android.ui.orders
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woocommerce.android.R
+import kotlinx.android.synthetic.main.order_detail_shipment_tracking_list.view.*
+import org.wordpress.android.fluxc.model.WCOrderShipmentTrackingModel
+
+class OrderDetailShipmentTrackingListView @JvmOverloads constructor(
+    ctx: Context,
+    attrs: AttributeSet? = null
+) : ConstraintLayout(ctx, attrs) {
+    init {
+        View.inflate(context, R.layout.order_detail_shipment_tracking_list, this)
+    }
+
+    fun initView(trackings: List<WCOrderShipmentTrackingModel>) {
+        val viewManager = LinearLayoutManager(context)
+        val viewAdapter = ShipmentTrackingListAdapter(trackings)
+
+        shipmentTrack_items.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+
+    class ShipmentTrackingListAdapter(
+        private val trackings: List<WCOrderShipmentTrackingModel>
+    ) : RecyclerView.Adapter<ShipmentTrackingListAdapter.ViewHolder>() {
+        class ViewHolder(val view: OrderDetailShipmentTrackingItemView) : RecyclerView.ViewHolder(view)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view: OrderDetailShipmentTrackingItemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.order_detail_product_list_item, parent, false)
+                    as OrderDetailShipmentTrackingItemView
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+            // TODO why is this required?
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+            holder.view.initView(trackings[position])
+        }
+
+        override fun getItemCount() = trackings.size
+    }
+}
