6048dba203e6421790bbd7c4c57383971af57adf
anitaa <murthyanitaa@gmail.com>
2019-04-26 13:54:37 +0530

    Added logic to store/retrieve currently selected stats/top earners tab when activity is resumed

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardTopEarnersView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/DashboardTopEarnersView.kt
@@ -29,18 +29,20 @@ import org.wordpress.android.fluxc.model.WCTopEarnerModel
 import org.wordpress.android.fluxc.store.WCStatsStore.StatsGranularity
 import org.wordpress.android.util.FormatUtils
 import org.wordpress.android.util.PhotonUtils
+import java.io.Serializable
 
 class DashboardTopEarnersView @JvmOverloads constructor(ctx: Context, attrs: AttributeSet? = null)
     : LinearLayout(ctx, attrs) {
     init {
         View.inflate(context, R.layout.dashboard_top_earners, this)
     }
+    var tabStateStats: Serializable? = null // Save the current position of top earners tab view
 
     var activeGranularity: StatsGranularity = DEFAULT_STATS_GRANULARITY
         get() {
             return topEarners_tab_layout.getTabAt(topEarners_tab_layout.selectedTabPosition)?.let {
                 it.tag as StatsGranularity
-            } ?: DEFAULT_STATS_GRANULARITY
+            } ?: tabStateStats?.let { it as StatsGranularity } ?: DEFAULT_STATS_GRANULARITY
         }
 
     private lateinit var selectedSite: SelectedSite
