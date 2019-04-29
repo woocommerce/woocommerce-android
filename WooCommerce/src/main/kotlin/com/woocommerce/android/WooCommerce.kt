78c8dc684eb69bc5ad2fdff9b66bc10735489039
Nick Bradbury <nick.bradbury@gmail.com>
2019-04-29 06:49:28 -0400

    Initialize Crashlytics before WellSql

--- WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
@@ -106,6 +106,11 @@ open class WooCommerce : MultiDexApplication(), HasActivityInjector, HasServiceI
             VolleyLog.DEBUG = false
         }
 
+        // we init Crashlytics further down using the selected site and account, but we also want to init it here
+        // to catch crashes that may occur before we can access the site and account (most notably crashes with
+        // initializing WellSql)
+        CrashlyticsUtils.initCrashlytics(this, null, null)
+
         val wellSqlConfig = WooWellSqlConfig(applicationContext)
         WellSql.init(wellSqlConfig)
 
