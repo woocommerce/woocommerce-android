85e1b563fbc7d5b1de0e4091f25441bb2cea79bf
anitaa <murthyanitaa@gmail.com>
2019-04-30 12:10:54 +0530

    Added ui tests for navigating to settings & help page

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/main/MainNavigationTest.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/main/MainNavigationTest.kt
@@ -1,14 +1,22 @@
 package com.woocommerce.android.ui.main
 
 import android.support.design.widget.BottomNavigationView
+import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import android.support.test.espresso.NoActivityResumedException
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.doesNotExist
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withContentDescription
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.filters.LargeTest
 import android.support.test.runner.AndroidJUnit4
+import android.widget.ListView
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.TestBase
 import org.junit.Assert.fail
@@ -106,6 +114,66 @@ class MainNavigationTest : TestBase() {
         // Verify the toolbar title has changed to 'My Store'
         onView(withId(R.id.toolbar)).check(matches(
                 WCMatchers.withToolbarTitle(equalToIgnoringCase(appContext.getString(R.string.my_store)))))
+    }
+
+    @Test
+    fun verifyOverFlowMenuAndSettingsIsDisplayed() {
+        // Open the overflow menu OR open the options menu,
+        // depending on if the device has a hardware or software overflow menu button.
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext())
+
+        // verify that there are 2 items in the menu list
+        onView(isAssignableFrom(ListView::class.java))
+                .check(matches(WCMatchers.correctNumberOfItems(2)))
+
+        // verify that the menu is displayed & the first item in the list is Settings
+        onView(withText(appContext.getString(R.string.settings))).check(matches(isDisplayed()))
+
+        // verify that when first item is clicked, it opens settings
+        // by checking if the toolbar title is changed to Settings
+        // by checking if the UP indicator is visible
+        onView(withText(appContext.getString(R.string.settings))).perform(click())
+        onView(withId(R.id.toolbar)).check(matches(
+                WCMatchers.withToolbarTitle(equalToIgnoringCase(appContext.getString(R.string.settings)))))
+        onView(withContentDescription(R.string.abc_action_bar_up_description)).check(matches(isDisplayed()))
+
+
+        // click back button and verify that the toolbar title is changed
+        // up button is no longer visible
+        onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click())
+        onView(withId(R.id.toolbar)).check(matches(
+                WCMatchers.withToolbarTitle(equalToIgnoringCase(appContext.getString(R.string.my_store)))))
+        onView(withContentDescription(appContext.getString(R.string.abc_action_bar_up_description))).check(doesNotExist())
+    }
+
+    @Test
+    fun verifyOverFlowMenuAndHelpIsDisplayed() {
+        // Open the overflow menu OR open the options menu,
+        // depending on if the device has a hardware or software overflow menu button.
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext())
+
+        // verify that there are 2 items in the menu list
+        onView(isAssignableFrom(ListView::class.java))
+                .check(matches(WCMatchers.correctNumberOfItems(2)))
+
+        // verify that the menu is displayed & the second item in the list is Help
+        onView(withText(appContext.getString(R.string.support_help))).check(matches(isDisplayed()))
+
+        // verify that when first item is clicked, it opens Help & Support
+        // by checking if the toolbar title is changed to Help & Support
+        // by checking if the UP indicator is visible
+        onView(withText(appContext.getString(R.string.support_help))).perform(click())
+        onView(withId(R.id.toolbar)).check(matches(
+                WCMatchers.withToolbarTitle(equalToIgnoringCase(appContext.getString(R.string.support_help)))))
+        onView(withContentDescription(R.string.abc_action_bar_up_description)).check(matches(isDisplayed()))
+
+
+        // click back button and verify that the toolbar title is changed
+        // up button is no longer visible
+        onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click())
+        onView(withId(R.id.toolbar)).check(matches(
+                WCMatchers.withToolbarTitle(equalToIgnoringCase(appContext.getString(R.string.my_store)))))
+        onView(withContentDescription(appContext.getString(R.string.abc_action_bar_up_description))).check(doesNotExist())
     }
 
     private fun assertPressingBackExitsApp() {
