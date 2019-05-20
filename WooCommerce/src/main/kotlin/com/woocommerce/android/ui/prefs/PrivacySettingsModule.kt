3d9d285f67f6ebf94d387a96ce16bbb2771cb54c
Nick Bradbury <nick.bradbury@gmail.com>
2019-05-20 11:15:34 -0400

    Removed FragmentScope

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsModule.kt
@@ -1,19 +1,15 @@
 package com.woocommerce.android.ui.prefs
 
-import com.woocommerce.android.di.FragmentScope
-
 import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 internal abstract class PrivacySettingsModule {
-    @FragmentScope
     @Binds
     abstract fun providePrivacySettingsPresenter(privacySettingsPresenter: PrivacySettingsPresenter):
             PrivacySettingsContract.Presenter
 
-    @FragmentScope
     @ContributesAndroidInjector
     abstract fun privacySettingsFragment(): PrivacySettingsFragment
 }
