3e93b87e1bd669960eb2275b8cf5c27e71cf3eb2
AmandaRiu <amanda.riu@automattic.com>
2019-05-28 21:31:06 -0600

    Remove all mention of Jetpack
    Also converts the layouts to ConstraintLayout for optimal rendering and smoother management.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueFragment.kt
@@ -1,23 +1,18 @@
 package com.woocommerce.android.ui.login
 
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.analytics.AnalyticsTracker.Stat
-import com.woocommerce.android.util.ChromeCustomTabUtils
 import kotlinx.android.synthetic.main.fragment_login_prologue.*
-import org.wordpress.android.util.DisplayUtils
 
 class LoginPrologueFragment : Fragment() {
     companion object {
-        private const val JETPACK_HELP_URL = "https://jetpack.com/support/getting-started-with-jetpack/"
         const val TAG = "login-prologue-fragment"
 
         fun newInstance(): LoginPrologueFragment {
@@ -55,23 +50,9 @@ class LoginPrologueFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        button_login_jetpack.setOnClickListener {
+        button_login.setOnClickListener {
             prologueFinishedListener?.onPrologueFinished()
             AnalyticsTracker.track(Stat.LOGIN_PROLOGUE_JETPACK_LOGIN_BUTTON_TAPPED)
-        }
-
-        val separator = if (DisplayUtils.isLandscape(activity)) " " else "<br>"
-        val html = getString(R.string.login_jetpack_required) + separator +
-                getString(R.string.login_configure_link, "<a href='$JETPACK_HELP_URL'>", "</a>")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            text_jetpack.text = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY)
-        } else {
-            text_jetpack.text = Html.fromHtml(html)
-        }
-
-        text_jetpack.setOnClickListener {
-            AnalyticsTracker.track(Stat.LOGIN_PROLOGUE_JETPACK_CONFIGURATION_INSTRUCTIONS_LINK_TAPPED)
-            ChromeCustomTabUtils.launchUrl(activity as Context, JETPACK_HELP_URL)
         }
     }
 }
