e164f0a6274b3cf0f6d8fce52267a1f1b42cb7b4
anitaa <murthyanitaa@gmail.com>
2019-06-03 12:18:04 +0530

    Fix timezone issue when running UI tests for Order list

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/helpers/WcDateTimeTestUtils.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/helpers/WcDateTimeTestUtils.kt
@@ -3,23 +3,29 @@ package com.woocommerce.android.helpers
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.TimeZone
 
 object WcDateTimeTestUtils {
     private const val DATE_FORMAT = "yyyy-MM-dd'T'hh:mm:ss'Z'"
+    private val timeZone = TimeZone.getTimeZone("UTC")
 
-    fun getCurrentDateTime(): Date = Calendar.getInstance().time
+    fun getCurrentDateTime(): Date = Calendar.getInstance(timeZone).time
 
     fun getCurrentDateTimeMinusDays(days: Int): Date {
-        val calendar = Calendar.getInstance()
+        val calendar = Calendar.getInstance(timeZone)
         calendar.add(Calendar.DATE, -days)
         return calendar.time
     }
 
     fun getCurrentDateTimeMinusMonths(month: Int): Date {
-        val calendar = Calendar.getInstance()
+        val calendar = Calendar.getInstance(timeZone)
         calendar.add(Calendar.MONTH, -month)
         return calendar.time
     }
 
-    fun formatDate(date: Date): String = SimpleDateFormat(DATE_FORMAT).format(date)
+    fun formatDate(date: Date): String {
+        val simpleDateFormat = SimpleDateFormat(DATE_FORMAT)
+        simpleDateFormat.timeZone = timeZone
+        return simpleDateFormat.format(date)
+    }
 }
