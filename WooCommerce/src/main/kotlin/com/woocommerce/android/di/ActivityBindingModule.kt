2aeabc41043c6c2a29e93c5d52f2a081b0e835dc
Nick Bradbury <nick.bradbury@gmail.com>
2019-06-04 13:04:59 -0400

    First pass at resolving conflicts

--- WooCommerce/src/main/kotlin/com/woocommerce/android/di/ActivityBindingModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/di/ActivityBindingModule.kt
@@ -10,7 +10,6 @@ import com.woocommerce.android.ui.main.MainModule
 import com.woocommerce.android.ui.notifications.NotifsListModule
 import com.woocommerce.android.ui.notifications.ReviewDetailModule
 import com.woocommerce.android.ui.orders.AddOrderNoteModule
-import com.woocommerce.android.ui.orders.AddOrderShipmentTrackingActivity
 import com.woocommerce.android.ui.orders.AddOrderShipmentTrackingModule
 import com.woocommerce.android.ui.orders.AddOrderTrackingProviderListModule
 import com.woocommerce.android.ui.orders.OrderDetailModule
@@ -42,7 +41,9 @@ abstract class ActivityBindingModule {
             NotifsListModule::class,
             ProductDetailModule::class,
             ReviewDetailModule::class,
-            SitePickerModule::class))
+            SitePickerModule::class,
+            AddOrderShipmentTrackingModule::class,
+            AddOrderTrackingProviderListModule::class))
     abstract fun provideMainActivityInjector(): MainActivity
 
     @ActivityScope
@@ -67,11 +68,4 @@ abstract class ActivityBindingModule {
     @ActivityScope
     @ContributesAndroidInjector(modules = arrayOf(SitePickerModule::class))
     abstract fun provideSitePickerActivityInjector(): SitePickerActivity
-
-    @ActivityScope
-    @ContributesAndroidInjector(modules = [
-        AddOrderShipmentTrackingModule::class,
-        AddOrderTrackingProviderListModule::class
-    ])
-    abstract fun provideAddOrderShipmentTrackingActivity(): AddOrderShipmentTrackingActivity
 }
