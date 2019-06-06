387b4c96500cc641aadb21bf493ff7e566a944fb
anitaa <murthyanitaa@gmail.com>
2019-06-06 13:30:16 +0530

    Modified logic to format date to fix timezone issue when running UI tests

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/helpers/WCDateTimeTestUtils.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/helpers/WCDateTimeTestUtils.kt
@@ -1,33 +1,37 @@
 package com.woocommerce.android.helpers
 
-import org.wordpress.android.util.DateTimeUtils
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 import java.util.TimeZone
 
 object WCDateTimeTestUtils {
     private const val DATE_FORMAT = "yyyy-MM-dd'T'hh:mm:ss'Z'"
 
-    fun getCurrentDateTime(): Date = DateTimeUtils.nowUTC()
+    fun getCurrentDateTime(): Date = getCalendarInstance().time
 
     fun getCurrentDateTimeMinusDays(days: Int): Date {
-        val calendar = Calendar.getInstance()
-        calendar.time = DateTimeUtils.nowUTC()
-        calendar.add(Calendar.DATE, -days)
-        return calendar.time
+        return getCalendarInstance().apply {
+            add(Calendar.DATE, -days)
+        }.time
     }
 
     fun getCurrentDateTimeMinusMonths(month: Int): Date {
-        val calendar = Calendar.getInstance()
-        calendar.time = DateTimeUtils.nowUTC()
-        calendar.add(Calendar.MONTH, -month)
-        return calendar.time
+        return getCalendarInstance().apply {
+            add(Calendar.MONTH, -month)
+        }.time
     }
 
     fun formatDate(date: Date): String {
-        val simpleDateFormat = SimpleDateFormat(DATE_FORMAT)
-        simpleDateFormat.timeZone = TimeZone.getTimeZone("UTC")
+        val simpleDateFormat = SimpleDateFormat(DATE_FORMAT, Locale.US)
+                .apply { timeZone = TimeZone.getTimeZone("UTC") }
         return simpleDateFormat.format(date)
+    }
+
+    private fun getCalendarInstance(): Calendar {
+        return Calendar.getInstance().apply {
+            time = Date()
+        }
     }
 }
