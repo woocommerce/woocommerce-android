e6d64b30232a75874bc8a342d14911f427373db5
Nick Bradbury <nick.bradbury@gmail.com>
2019-06-13 13:50:56 -0400

    Added empty fragment

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/EmptyFragment.kt
@@ -0,0 +1,18 @@
+package com.woocommerce.android.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.woocommerce.android.R.layout
+
+class EmptyFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layout.fragment_empty, container, false)
+    }
+}
+
