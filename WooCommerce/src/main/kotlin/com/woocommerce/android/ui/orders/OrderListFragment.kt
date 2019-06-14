dc2696b74f92845e816d64a4265cd91076d5cc7d
Nick Bradbury <nick.bradbury@gmail.com>
2019-06-14 14:57:00 -0400

    Attempt to fix wrong options menu items in order detail

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderListFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderListFragment.kt
@@ -95,12 +95,14 @@ class OrderListFragment : TopLevelFragment(), OrderListContract.View,
 
     // region options menu
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater?.inflate(R.menu.menu_order_list_fragment, menu)
+        if (isActive) {
+            inflater?.inflate(R.menu.menu_order_list_fragment, menu)
 
-        filterMenuItem = menu?.findItem(R.id.menu_filter)
+            filterMenuItem = menu?.findItem(R.id.menu_filter)
 
-        searchMenuItem = menu?.findItem(R.id.menu_search)
-        searchView = searchMenuItem?.actionView as SearchView?
+            searchMenuItem = menu?.findItem(R.id.menu_search)
+            searchView = searchMenuItem?.actionView as SearchView?
+        }
 
         super.onCreateOptionsMenu(menu, inflater)
     }
