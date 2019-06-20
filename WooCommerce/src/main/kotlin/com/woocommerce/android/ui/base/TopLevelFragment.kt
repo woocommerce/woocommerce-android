9564f53ae506e3e95d5b14e28e5651a7c381accc
Nick Bradbury <nick.bradbury@gmail.com>
2019-06-20 10:37:07 -0400

    Removed unnecessary TopLevelFragment code that hide the container when a child is showing

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/base/TopLevelFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/base/TopLevelFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.main.MainNavigationRouter
 import kotlinx.android.synthetic.main.fragment_parent.*
@@ -14,13 +13,6 @@ import kotlinx.android.synthetic.main.fragment_parent.*
  * The main fragments hosted by the bottom bar should extend this class
  */
 abstract class TopLevelFragment : androidx.fragment.app.Fragment(), TopLevelFragmentView {
-    companion object {
-        // Bundle label to store the state of this top-level fragment.
-        // If the value associated with this label is true, then this
-        // fragment is currently hosting a child fragment (drilled in).
-        const val CHILD_FRAGMENT_ACTIVE = "child-fragment-active"
-    }
-
     /**
      * The extending class may use this variable to defer a part of its
      * normal initialization until manually requested.
@@ -60,41 +52,6 @@ abstract class TopLevelFragment : androidx.fragment.app.Fragment(), TopLevelFrag
     override fun onDestroyView() {
         container.removeAllViews()
         super.onDestroyView()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        // Save the current view state of this top-level fragment.
-        (activity as? MainNavigationRouter)?. let { router ->
-            outState.putBoolean(CHILD_FRAGMENT_ACTIVE, router.isChildFragmentShowing())
-        }
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        savedInstanceState?.let { bundle ->
-            val childViewActive = bundle.getBoolean(CHILD_FRAGMENT_ACTIVE, false)
-            updateParentViewState(childViewActive)
-        } ?: updateActivityTitle()
-    }
-
-    /**
-     * Update the parent view state to match hierarchy status. If [childActive]
-     * is true, then set the title bar to appropriately show the "up" option,
-     * and set the title to the active child fragments title.
-     */
-    private fun updateParentViewState(childActive: Boolean) {
-        val mainActivity: AppCompatActivity? = activity as? AppCompatActivity
-        if (childActive) {
-            container?.getChildAt(0)?.visibility = View.GONE
-            mainActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-            mainActivity?.supportActionBar?.setDisplayShowHomeEnabled(true)
-        } else {
-            container?.getChildAt(0)?.visibility = View.VISIBLE
-            mainActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(false)
-            mainActivity?.supportActionBar?.setDisplayShowHomeEnabled(false)
-            updateActivityTitle()
-        }
     }
 
     fun updateActivityTitle() {
