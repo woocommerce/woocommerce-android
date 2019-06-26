a08b544c8ad7fe1227432e44369fd56c7e05d73f
anitaa <murthyanitaa@gmail.com>
2019-06-26 14:01:52 +0530

    Revert "Added logic to send logout tracking event only if it is not sending already"
    This reverts commit 61cf098325488146828566d4e09860a80181944f.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
@@ -72,7 +72,6 @@ open class WooCommerce : MultiDexApplication(), HasActivityInjector, HasServiceI
     // Listens for changes in device connectivity
     @Inject lateinit var connectionReceiver: ConnectionChangeReceiver
     private var connectionReceiverRegistered = false
-    private var accountLogoutEventRegistered = false
 
     protected open val component: AppComponent by lazy {
         DaggerAppComponent.builder()
@@ -231,22 +230,17 @@ open class WooCommerce : MultiDexApplication(), HasActivityInjector, HasServiceI
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun onAccountChanged(event: OnAccountChanged) {
         if (!accountStore.hasAccessToken()) {
-            if (!accountLogoutEventRegistered) {
-                // Logged out
-                AnalyticsTracker.track(Stat.ACCOUNT_LOGOUT)
+            // Logged out
+            AnalyticsTracker.track(Stat.ACCOUNT_LOGOUT)
 
-                // Reset analytics
-                AnalyticsTracker.flush()
-                AnalyticsTracker.clearAllData()
-                CrashUtils.resetAccountAndSite()
-                zendeskHelper.reset()
+            // Reset analytics
+            AnalyticsTracker.flush()
+            AnalyticsTracker.clearAllData()
+            CrashUtils.resetAccountAndSite()
+            zendeskHelper.reset()
 
-                // Wipe user-specific preferences
-                AppPrefs.reset()
-
-                // set the variable as true to avoid sending duplicate events for the same action
-                accountLogoutEventRegistered = true
-            }
+            // Wipe user-specific preferences
+            AppPrefs.reset()
         } else if (event.causeOfChange == AccountAction.FETCH_SETTINGS) {
             // make sure local usage tracking matches the account setting
             val hasUserOptedOut = !AnalyticsTracker.sendUsageStats
