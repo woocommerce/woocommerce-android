5f543ca6735496450c8963ad7a2015c8d6178572
Ondrej Ruttkay <ondrej@automattic.com>
2019-07-30 21:45:18 +0200

    Trasnform the LiveData value returned by the ViewModel with hardcoded parameters for testing purposes

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/products/MockedViewModelModule.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/products/MockedViewModelModule.kt
@@ -1,20 +1,26 @@
 package com.woocommerce.android.ui.products
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModelProvider
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.whenever
+import com.woocommerce.android.model.Product
 import com.woocommerce.android.model.toAppModel
 import com.woocommerce.android.tools.NetworkStatus
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.CurrencyFormatter
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import org.wordpress.android.fluxc.model.WCProductModel
 import org.wordpress.android.fluxc.store.WooCommerceStore
+import java.math.BigDecimal
+import kotlin.math.roundToInt
 
 @Module
 internal abstract class MockedViewModelModule {
@@ -37,7 +43,7 @@ internal abstract class MockedViewModelModule {
             val mockProductRepository = mock<ProductRepository>()
 
             val mockedProductDetailViewModel = spy(
-                    ProductDetailViewModel(
+                    MockedProductDetailViewModel(
                             Dispatchers.Main,
                             wcStore,
                             site,
@@ -55,6 +61,59 @@ internal abstract class MockedViewModelModule {
             doReturn(mockedProductDetailViewModel).whenever(mockFactory).create<ProductDetailViewModel>(any())
 
             return mockFactory
+        }
+    }
+
+    // necessary, since we can't mock WooCommerceStore (╯°Д°)╯︵ ┻━┻
+    class MockedProductDetailViewModel(
+        mainDispatcher: CoroutineDispatcher,
+        wooCommerceStore: WooCommerceStore,
+        selectedSite: SelectedSite,
+        productRepository: ProductRepository,
+        networkStatus: NetworkStatus,
+        private val currencyFormatter: CurrencyFormatter
+    ) : ProductDetailViewModel(
+            mainDispatcher,
+            wooCommerceStore,
+            selectedSite,
+            productRepository,
+            networkStatus,
+            currencyFormatter
+    ) {
+        override val productData: LiveData<ProductWithParameters>
+            get() = Transformations.map(super.productData) {
+                combineData(it.product, Parameters("$", "oz", "in"))
+            }
+
+        private fun combineData(product: Product, parameters: Parameters): ProductWithParameters {
+            val weight = if (product.weight > 0) "${product.weight.roundToInt()}${parameters.weightUnit ?: ""}" else ""
+
+            val hasLength = product.length > 0
+            val hasWidth = product.width > 0
+            val hasHeight = product.height > 0
+            val unit = parameters.dimensionUnit ?: ""
+            val size = if (hasLength && hasWidth && hasHeight) {
+                "${product.length.roundToInt()} x ${product.width.roundToInt()} x ${product.height.roundToInt()} $unit"
+            } else if (hasWidth && hasHeight) {
+                "${product.width.roundToInt()} x ${product.height.roundToInt()} $unit"
+            } else {
+                ""
+            }.trim()
+
+            return ProductWithParameters(
+                    product,
+                    weight,
+                    size,
+                    formatCurrency(product.price, parameters.currencyCode),
+                    formatCurrency(product.salePrice, parameters.currencyCode),
+                    formatCurrency(product.regularPrice, parameters.currencyCode)
+            )
+        }
+
+        private fun formatCurrency(amount: BigDecimal?, currencyCode: String?): String {
+            return currencyCode?.let {
+                currencyFormatter.formatCurrency(amount ?: BigDecimal.ZERO, it)
+            } ?: amount.toString()
         }
     }
 }
