3babf8c959d06b331e0b888278411dc4af689679
Nick Bradbury <nick.bradbury@gmail.com>
2019-08-08 10:32:28 -0400

    Corrected comment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/WooWellSqlConfig.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/WooWellSqlConfig.kt
@@ -16,14 +16,15 @@ import java.util.Locale
 
 class WooWellSqlConfig(context: Context?) : WellSqlConfig(context, WellSqlConfig.ADDON_WOOCOMMERCE) {
     /**
-     * Detect when the database is downgraded in debug builds so we can recreate all the tables and show a
-     * toast alerting to the downgrade. The initial purpose of this was to avoid the hassle of devs switching
-     * branches and having to clear storage and login again due to a version downgrade, but we've had a
-     * couple cases where a beta build with a DB downgrade was released, resulting in a lot of crashes.
+     * Detect when the database is downgraded in debug and beta builds so we can recreate all the tables and
+     * show a toast about the downgrade. The initial purpose of this was to avoid the hassle of devs switching
+     * branches and having to clear storage and login again due to a version downgrade, but we've had a couple
+     * cases where a beta build with a DB downgrade was released, resulting in a lot of crashes.
      */
     override fun onDowngrade(db: SQLiteDatabase?, helper: WellTableManager?, oldVersion: Int, newVersion: Int) {
         val versionName = PackageUtils.getVersionName(context).toLowerCase(Locale.ROOT)
         val isBetaOrDebug = BuildConfig.DEBUG || versionName.contains("beta") || versionName.contains("rc")
+
         if (isBetaOrDebug) {
             // note: don't call super() here because it throws an exception
             AppLog.w(T.DB, "Resetting database due to downgrade from version $oldVersion to $newVersion")
