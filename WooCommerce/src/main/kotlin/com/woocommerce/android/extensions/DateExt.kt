4cb8a072ae91108f171b23422262a568b3f9b188
anitaa <murthyanitaa@gmail.com>
2019-08-12 11:45:38 +0530

    Added explicit return type to methods in `DateExt` classes to address inspection warnings

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/DateExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/DateExt.kt
@@ -5,13 +5,13 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun Date.formatToYYYYmm() = SimpleDateFormat("yyyy-MM", Locale.getDefault()).format(this)
+fun Date.formatToYYYYmm(): String = SimpleDateFormat("yyyy-MM", Locale.getDefault()).format(this)
 
-fun Date.formatToYYYY() = SimpleDateFormat("yyyy", Locale.getDefault()).format(this)
+fun Date.formatToYYYY(): String = SimpleDateFormat("yyyy", Locale.getDefault()).format(this)
 
-fun Date.formatToYYYYWmm() = SimpleDateFormat("yyyy-'W'ww", Locale.getDefault()).format(this)
+fun Date.formatToYYYYWmm(): String = SimpleDateFormat("yyyy-'W'ww", Locale.getDefault()).format(this)
 
-fun Date.formatToMMMMdd() = SimpleDateFormat("MMMM dd", Locale.getDefault()).format(this)
+fun Date.formatToMMMMdd(): String = SimpleDateFormat("MMMM dd", Locale.getDefault()).format(this)
 
 fun Date.formatToEEEEMMMddhha(): String {
     val symbols = DateFormatSymbols(Locale.getDefault())
