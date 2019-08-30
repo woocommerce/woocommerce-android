4ed5bca4fa06a56d37514691e30a11a60b4196b6
anitaa <murthyanitaa@gmail.com>
2019-08-30 17:34:40 +0530

    Added new attribute to the WCToggleSingleOptionView to display a separate text color for the subtitle

--- WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/WCToggleSingleOptionView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/WCToggleSingleOptionView.kt
@@ -9,6 +9,7 @@ import android.widget.Checkable
 import android.widget.CompoundButton
 import android.widget.CompoundButton.OnCheckedChangeListener
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.woocommerce.android.R
 import kotlinx.android.synthetic.main.view_toggle_single_option.view.*
 
@@ -30,6 +31,16 @@ class WCToggleSingleOptionView @JvmOverloads constructor(ctx: Context, attrs: At
             try {
                 // Set the view title
                 switchSetting_title.text = a.getString(R.styleable.WCToggleSingleOptionView_switchTitle).orEmpty()
+
+                // set the summary text color
+                var textColor = a.getColor(R.styleable.WCToggleSingleOptionView_switchSummaryTextColor, 0)
+                if (textColor == 0) {
+                    val textColorResId = a.getResourceId(
+                            R.styleable.WCToggleSingleOptionView_switchSummaryTextColor, R.color.wc_grey_medium
+                    )
+                    textColor = ContextCompat.getColor(context, textColorResId)
+                }
+                switchSetting_switch.setTextColor(textColor)
 
                 // Set the summary and switch state
                 switchSetting_switch.isChecked =
