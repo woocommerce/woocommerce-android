ea0864158a2b5407dad4257415194da7041eeca7
AmandaRiu <amanda.riu@automattic.com>
2019-09-24 17:18:34 -0700

    Add class comments for DelayedUndoRequest

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/DelayedUndoRequest.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/DelayedUndoRequest.kt
@@ -1,5 +1,19 @@
 package com.woocommerce.android.model
 
+/**
+ * Base class for communicating the status of a user initiated request event that
+ * uses the "optimistic" design by delaying the submission of a user request to the API
+ * and displaying an UNDO snack message giving the user the opportunity to back out of
+ * the requested action. The request will then be submitted to the API once the snackbar
+ * times out, the user navigates away from the screen or app, or if the user swipes to
+ * dismiss the snackbar.
+ *
+ * @see [com.woocommerce.android.ui.reviews.ProductReviewModerationAction] for an example
+ * of how this class is implemented.
+ * @see [com.woocommerce.android.ui.reviews.OnRequestModerateReviewEvent] for an example
+ * of how the various status options are used to communicate the lifecycle of a request to
+ * moderate a [ProductReview].
+ */
 abstract class DelayedUndoRequest(var requestStatus: RequestStatus) {
     enum class RequestStatus {
         PENDING {
