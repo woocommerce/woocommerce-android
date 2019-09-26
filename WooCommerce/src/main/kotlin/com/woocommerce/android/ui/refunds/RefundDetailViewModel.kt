faf3bbc74e4ff0c8894443752eba56bba8224b2d
Ondrej Ruttkay <ondrej@automattic.com>
2019-09-26 23:17:23 +0200

    Add the refund loading logic

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundDetailViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundDetailViewModel.kt
@@ -3,44 +3,29 @@ package com.woocommerce.android.ui.refunds
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.woocommerce.android.R
-import com.woocommerce.android.analytics.AnalyticsTracker
-import com.woocommerce.android.analytics.AnalyticsTracker.Stat
-import com.woocommerce.android.analytics.AnalyticsTracker.Stat.ADD_ORDER_REFUND_AMOUNT_NEXT_BUTTON_TAPPED
-import com.woocommerce.android.analytics.AnalyticsTracker.Stat.ADD_ORDER_REFUND_SUMMARY_REFUND_BUTTON_TAPPED
-import com.woocommerce.android.analytics.AnalyticsTracker.Stat.ADD_ORDER_REFUND_SUMMARY_UNDO_BUTTON_TAPPED
 import com.woocommerce.android.annotations.OpenClassOnDebug
-import com.woocommerce.android.di.BG_THREAD
 import com.woocommerce.android.di.UI_THREAD
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.toAppModel
-import com.woocommerce.android.tools.NetworkStatus
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.CurrencyFormatter
-import com.woocommerce.android.extensions.isEqualTo
-import com.woocommerce.android.ui.refunds.IssueRefundViewModel.InputValidationState.TOO_HIGH
-import com.woocommerce.android.ui.refunds.IssueRefundViewModel.InputValidationState.TOO_LOW
-import com.woocommerce.android.ui.refunds.IssueRefundViewModel.InputValidationState.VALID
 import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
-import com.woocommerce.android.viewmodel.SingleLiveEvent
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.model.order.OrderIdentifier
+import org.wordpress.android.fluxc.model.refunds.RefundModel
 import org.wordpress.android.fluxc.store.RefundsStore
 import org.wordpress.android.fluxc.store.WCOrderStore
-import org.wordpress.android.fluxc.store.WooCommerceStore
 import java.math.BigDecimal
 import javax.inject.Inject
 import javax.inject.Named
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 @OpenClassOnDebug
 class RefundDetailViewModel @Inject constructor(
     @Named(UI_THREAD) private val mainDispatcher: CoroutineDispatcher,
     private val refundStore: RefundsStore,
+    private val orderStore: WCOrderStore,
     private val selectedSite: SelectedSite,
     private val currencyFormatter: CurrencyFormatter,
     private val resourceProvider: ResourceProvider
@@ -51,8 +36,40 @@ class RefundDetailViewModel @Inject constructor(
     private val _formattedRefundAmount = MutableLiveData<String>()
     val formattedRefundAmount: LiveData<String> = _formattedRefundAmount
 
+    private val _refundMethod = MutableLiveData<String>()
+    val refundMethod: LiveData<String> = _refundMethod
+
+    private val _refundReason = MutableLiveData<String>()
+    val refundReason: LiveData<String> = _refundReason
+
     private lateinit var formatCurrency: (BigDecimal) -> String
 
     fun start(orderId: Long, refundId: Long) {
+        _screenTitle.value = "${resourceProvider.getString(R.string.order_refunds_refund)} #$refundId"
+
+        orderStore.getOrderByIdentifier(OrderIdentifier(selectedSite.get().id, orderId))
+                ?.toAppModel()?.let { order ->
+            formatCurrency = currencyFormatter.buildBigDecimalFormatter(order.currency)
+
+            val localRefund = refundStore.getRefund(selectedSite.get(), orderId, refundId)
+            if (localRefund == null) {
+                launch {
+                    val refund = refundStore.fetchRefund(selectedSite.get(), orderId, refundId).model
+                    if (refund != null) {
+                        displayRefundDetails(refund, order)
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                displayRefundDetails(localRefund, order)
+            }
+        }
+    }
+
+    private fun displayRefundDetails(refund: RefundModel, order: Order) {
+        _formattedRefundAmount.value = formatCurrency(refund.amount)
+        _refundMethod.value = order.paymentMethodTitle
+        _refundReason.value = refund.reason
     }
 }
