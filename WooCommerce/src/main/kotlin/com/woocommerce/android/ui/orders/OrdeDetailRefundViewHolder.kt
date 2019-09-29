9521a324294eb6fc476928cf4b2c30c613e072cf
Ondrej Ruttkay <ondrej@automattic.com>
2019-09-29 11:38:54 +0200

    Remove empty file

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrdeDetailRefundViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.woocommerce.android.ui.orders
-
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.woocommerce.android.R
-import org.wordpress.android.fluxc.model.refunds.RefundModel
-
