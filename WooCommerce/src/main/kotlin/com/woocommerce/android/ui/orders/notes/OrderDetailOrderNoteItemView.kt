9b19640c254d1aff8f27c748986e6dbe35c6e755
Ondrej Ruttkay <ondrej@automattic.com>
2019-10-11 10:30:56 +0200

    Add a note type to the note date header

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/notes/OrderDetailOrderNoteItemView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/notes/OrderDetailOrderNoteItemView.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.orders.notes
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
 import android.text.Html
@@ -13,14 +14,22 @@ import com.woocommerce.android.R
 import com.woocommerce.android.model.OrderNote
 import kotlinx.android.synthetic.main.order_detail_note_item.view.*
 
-class OrderDetailOrderNoteItemView @JvmOverloads constructor(ctx: Context, attrs: AttributeSet? = null)
+class OrderDetailOrderNoteItemView @JvmOverloads constructor(val ctx: Context, attrs: AttributeSet? = null)
     : ConstraintLayout(ctx, attrs) {
     init {
         View.inflate(context, R.layout.order_detail_note_item, this)
     }
 
+    @SuppressLint("SetTextI18n")
     fun initView(note: OrderNote) {
-        orderNote_created.text = DateFormat.getTimeFormat(context).format(note.dateCreated)
+        val date = DateFormat.getTimeFormat(context).format(note.dateCreated)
+        val type = when {
+            note.isCustomerNote -> ctx.getString(R.string.orderdetail_note_public)
+            note.isSystemNote -> ctx.getString(R.string.orderdetail_note_system)
+            else -> ctx.getString(R.string.orderdetail_note_private)
+        }
+
+        orderNote_created.text = "$date ($type)"
         orderNote_note.text = getHtmlText(note.note)
 
         when {
