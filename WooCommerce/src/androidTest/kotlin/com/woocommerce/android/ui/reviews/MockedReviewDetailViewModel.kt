1ca5ab9ef55242293e075fc7fbd9244f994768bc
Ondrej Ruttkay <ondrej@automattic.com>
2019-10-29 20:33:06 +0100

    Add all the missing fragment modules and mock view models

---
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/reviews/MockedReviewDetailViewModel.kt
@@ -0,0 +1,23 @@
+package com.woocommerce.android.ui.reviews
+
+import androidx.lifecycle.SavedStateHandle
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import com.woocommerce.android.di.ViewModelAssistedFactory
+import com.woocommerce.android.tools.NetworkStatus
+import com.woocommerce.android.util.CoroutineDispatchers
+
+class MockedReviewDetailViewModel @AssistedInject constructor(
+    dispatchers: CoroutineDispatchers,
+    reviewRepository: ReviewDetailRepository,
+    networkStatus: NetworkStatus,
+    @Assisted arg0: SavedStateHandle
+) : ReviewDetailViewModel(
+        dispatchers,
+        reviewRepository,
+        networkStatus,
+        arg0
+) {
+    @AssistedInject.Factory
+    interface Factory : ViewModelAssistedFactory<MockedReviewDetailViewModel>
+}
