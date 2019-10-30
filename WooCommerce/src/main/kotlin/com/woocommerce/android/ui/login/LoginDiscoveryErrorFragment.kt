00bf42aa058751290dfd4f1eee0fbf564d47ce4b
anitaa <murthyanitaa@gmail.com>
2019-10-30 18:22:34 +0530

    Added xml + fragment for discovery error screen

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginDiscoveryErrorFragment.kt
@@ -0,0 +1,56 @@
+package com.woocommerce.android.ui.login
+
+import android.os.Bundle
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.woocommerce.android.R
+import com.woocommerce.android.R.layout
+import kotlinx.android.synthetic.main.fragment_login_discovery_error.*
+
+class LoginDiscoveryErrorFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(layout.fragment_login_discovery_error, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById(R.id.toolbar) as Toolbar
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowTitleEnabled(false)
+        }
+
+        discovery_error_message.text = Html.fromHtml(getString(R.string.login_discovery_error_xmlrpc))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_login, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.help) {
+            // TODO: add event here
+            // TODO: redirect to help screen
+            return true
+        }
+
+        return false
+    }
+}
