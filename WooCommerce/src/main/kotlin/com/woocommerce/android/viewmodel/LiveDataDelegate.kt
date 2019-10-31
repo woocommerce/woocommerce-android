7c78eedce5b49342fc51aa69d2770c184fc53f7c
Ondrej Ruttkay <ondrej@automattic.com>
2019-10-31 11:55:29 +0100

    Require for LiveDataDelegate data to be parcelable and supply previous value to observers

--- WooCommerce/src/main/kotlin/com/woocommerce/android/viewmodel/LiveDataDelegate.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/viewmodel/LiveDataDelegate.kt
@@ -1,22 +1,34 @@
 package com.woocommerce.android.viewmodel
 
+import android.os.Parcelable
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateHandle
 import kotlin.reflect.KProperty
 
-class LiveDataDelegate<T: Any>(private val liveData: MutableLiveData<T>) {
+class LiveDataDelegate<T: Parcelable>(private val liveData: MutableLiveData<T>) {
     constructor(savedState: SavedStateHandle, initialValue: T) :
             this(savedState.getLiveData(initialValue.javaClass.name, initialValue))
 
+    var previousValue : T? = null
+        private set
     val value
         get() = liveData.value
 
-    fun observe(owner: LifecycleOwner, observer: (T) -> Unit): Unit =
-            liveData.observe(owner, Observer { observer(it) })
+    fun observe(owner: LifecycleOwner, observer: (T?, T) -> Unit) {
+        liveData.observe(owner, Observer {
+            observer(previousValue, it)
+            previousValue = it
+        })
+    }
 
-    fun observeForever(observer: (T) -> Unit): Unit = liveData.observeForever(observer)
+    fun observeForever(observer: (T?, T) -> Unit) {
+        liveData.observeForever {
+            observer(previousValue, it)
+            previousValue = it
+        }
+    }
 
     operator fun setValue(ref: Any, p: KProperty<*>, value: T) {
         liveData.value = value
