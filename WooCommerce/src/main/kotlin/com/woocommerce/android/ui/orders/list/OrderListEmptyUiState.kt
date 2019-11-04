cb47c4ef8037e18927ef35bc6395bd3c4393adb0
AmandaRiu <amanda.riu@automattic.com>
2019-11-04 14:15:09 -0800

    Add new empty states to the empty view builder

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListEmptyUiState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListEmptyUiState.kt
@@ -5,6 +5,7 @@ import com.woocommerce.android.R
 import com.woocommerce.android.model.UiString
 import com.woocommerce.android.model.UiString.UiStringRes
 import com.woocommerce.android.ui.orders.list.OrderListType.ALL
+import com.woocommerce.android.ui.orders.list.OrderListType.PROCESSING
 import com.woocommerce.android.ui.orders.list.OrderListType.SEARCH
 
 sealed class OrderListEmptyUiState(
@@ -14,22 +15,28 @@ sealed class OrderListEmptyUiState(
     val onButtonClick: (() -> Unit)? = null,
     val emptyViewVisible: Boolean = true
 ) {
-    class ShareStore(
-        onButtonClick: (() -> Unit)?
-    ) : OrderListEmptyUiState(
-            title = UiStringRes(R.string.waiting_for_customers),
-            imgResId = R.drawable.ic_woo_waiting_customers,
-            buttonText = UiStringRes(R.string.share_store_button),
-            onButtonClick = onButtonClick
-    )
+    /**
+     * Base class for displaying "empty" list results.
+     */
+    class EmptyList(title: UiString, imgResId: Int?) : OrderListEmptyUiState(title, imgResId)
 
-    class EmptyList(title: UiString) : OrderListEmptyUiState(title)
-
+    /**
+     * Use this to hide the empty view when there is data available to view.
+     */
     object DataShown : OrderListEmptyUiState(emptyViewVisible = false)
 
+    /**
+     * The view to display while data is loading. This is the view that should be visible
+     * while orders are being fetched. The next step from here would either be to handle no orders,
+     * or to display the orders fetched.
+     */
     object Loading : OrderListEmptyUiState(title = UiStringRes(R.string.orderlist_fetching))
 
-    class RefreshError(
+    /**
+     * There was an error fetching orders. Display an error message along with a button
+     * to "Retry".
+     */
+    class ErrorWithRetry(
         title: UiString,
         buttonText: UiString? = null,
         onButtonClick: (() -> Unit)? = null
@@ -40,15 +47,18 @@ sealed class OrderListEmptyUiState(
             onButtonClick = onButtonClick)
 }
 
+/**
+ * Use this method as a builder for creating "empty" UI views.
+ */
 fun createEmptyUiState(
     orderListType: OrderListType,
     isNetworkAvailable: Boolean,
     isLoadingData: Boolean,
     isListEmpty: Boolean,
+    hasOrders: Boolean,
     isSearchPromptRequired: Boolean,
     isError: Boolean = false,
-    fetchFirstPage: () -> Unit,
-    shareStoreFunc: (() -> Unit)? = null
+    fetchFirstPage: () -> Unit
 ): OrderListEmptyUiState {
     return if (isListEmpty) {
         when {
@@ -63,7 +73,7 @@ fun createEmptyUiState(
             }
             else -> {
                 if (isNetworkAvailable) {
-                    createEmptyListUiState(orderListType, isSearchPromptRequired, shareStoreFunc)
+                    createEmptyListUiState(orderListType, isSearchPromptRequired, hasOrders)
                 } else {
                     createErrorListUiState(isNetworkAvailable, fetchFirstPage)
                 }
@@ -74,6 +84,9 @@ fun createEmptyUiState(
     }
 }
 
+/**
+ * Takes care of creating empty list error views.
+ */
 private fun createErrorListUiState(
     isNetworkAvailable: Boolean,
     fetchFirstPage: () -> Unit
@@ -83,22 +96,42 @@ private fun createErrorListUiState(
     } else {
         UiStringRes(R.string.error_generic_network)
     }
-    return OrderListEmptyUiState.RefreshError(errorText, UiStringRes(R.string.retry), fetchFirstPage)
+    return OrderListEmptyUiState.ErrorWithRetry(errorText, UiStringRes(R.string.retry), fetchFirstPage)
 }
 
+/**
+ * Takes care of creating empty list views.
+ */
 private fun createEmptyListUiState(
     orderListType: OrderListType,
     isSearchPromptRequired: Boolean,
-    shareStoreFunc: (() -> Unit)? = null
+    hasOrders: Boolean
 ): OrderListEmptyUiState {
     return when (orderListType) {
         SEARCH -> {
             if (isSearchPromptRequired) {
-                OrderListEmptyUiState.EmptyList(UiStringRes(R.string.orderlist_search_prompt))
+                OrderListEmptyUiState.EmptyList(UiStringRes(R.string.orderlist_search_prompt), null)
             } else {
-                OrderListEmptyUiState.EmptyList(UiStringRes(R.string.orders_empty_message_with_search))
+                OrderListEmptyUiState.EmptyList(UiStringRes(R.string.orders_empty_message_with_search), null)
             }
         }
-        ALL -> OrderListEmptyUiState.ShareStore(shareStoreFunc)
+        PROCESSING -> {
+            if (hasOrders) {
+                // User has processed all orders
+                OrderListEmptyUiState.EmptyList(
+                        UiStringRes(R.string.orders_processed_empty_message),
+                        R.drawable.ic_gridicons_checkmark)
+            } else {
+                // Waiting for orders to process
+                OrderListEmptyUiState.EmptyList(
+                        UiStringRes(R.string.orders_empty_message_with_processing),
+                        R.drawable.ic_hourglass_empty)
+            }
+        }
+        ALL -> {
+            OrderListEmptyUiState.EmptyList(
+                    UiStringRes(R.string.orders_empty_message_with_filter),
+                    R.drawable.ic_hourglass_empty)
+        }
     }
 }
