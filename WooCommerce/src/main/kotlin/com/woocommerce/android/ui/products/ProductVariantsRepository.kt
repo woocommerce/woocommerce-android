cab5bedec8a2b66aeca22d67f43c57c11e19bdad
anitaa <murthyanitaa@gmail.com>
2019-11-07 14:30:12 +0530

    First pass at adding a product variant repository

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductVariantsRepository.kt
@@ -0,0 +1,87 @@
+package com.woocommerce.android.ui.products
+
+import com.woocommerce.android.model.ProductVariant
+import com.woocommerce.android.model.toAppModel
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.util.WooLog
+import com.woocommerce.android.util.suspendCoroutineWithTimeout
+import kotlinx.coroutines.CancellationException
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.action.WCProductAction.FETCHED_PRODUCT_VARIATIONS
+import org.wordpress.android.fluxc.generated.WCProductActionBuilder
+import org.wordpress.android.fluxc.store.WCProductStore
+import org.wordpress.android.fluxc.store.WCProductStore.OnProductChanged
+import javax.inject.Inject
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+
+class ProductVariantsRepository @Inject constructor(
+    private val dispatcher: Dispatcher,
+    private val productStore: WCProductStore,
+    private val selectedSite: SelectedSite
+) {
+    companion object {
+        private const val ACTION_TIMEOUT = 10L * 1000
+    }
+
+    private var loadContinuation: Continuation<Boolean>? = null
+    private var isLoadingProductVariants = false
+
+    init {
+        dispatcher.register(this)
+    }
+
+    fun onCleanup() {
+        dispatcher.unregister(this)
+    }
+
+    /**
+     * Submits a fetch request to get a list of products variants for the current site and productId
+     * and returns the full list of product variants from the database
+     */
+    suspend fun fetchProductVariants(remoteProductId: Long): List<ProductVariant> {
+        if (!isLoadingProductVariants) {
+            try {
+                suspendCoroutineWithTimeout<Boolean>(ACTION_TIMEOUT) {
+                    loadContinuation = it
+                    isLoadingProductVariants = true
+                    val payload = WCProductStore.FetchProductVariationsPayload(
+                            selectedSite.get(),
+                            remoteProductId
+                    )
+                    dispatcher.dispatch(WCProductActionBuilder.newFetchProductVariationsAction(payload))
+                }
+            } catch (e: CancellationException) {
+                WooLog.e(WooLog.T.PRODUCTS, "CancellationException while fetching product variants", e)
+            }
+        }
+
+        return getProductVariantList(remoteProductId)
+    }
+
+    /**
+     * Returns all product variants for a product and current site that are in the database
+     */
+    fun getProductVariantList(remoteProductId: Long): List<ProductVariant> {
+        val wcProducts = productStore.getVariationsForProduct(selectedSite.get(), remoteProductId)
+        return wcProducts.map { it.toAppModel() }
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onProductChanged(event: OnProductChanged) {
+        if (event.causeOfChange == FETCHED_PRODUCT_VARIATIONS) {
+            isLoadingProductVariants = false
+            if (event.isError) {
+                loadContinuation?.resume(false)
+                // TODO: add analytics here in a different commit
+            } else {
+                // TODO: add analytics here in a different commit
+                loadContinuation?.resume(true)
+            }
+            loadContinuation = null
+        }
+    }
+}
