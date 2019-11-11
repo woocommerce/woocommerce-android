08ede4c5fe67d86bc9b25bd4c7fb5cb9df159426
Ondrej Ruttkay <ondrej@automattic.com>
2019-11-11 10:57:50 +0100

    Restore saved arguments if they're missing

--- WooCommerce/src/main/kotlin/com/woocommerce/android/viewmodel/SavedState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/viewmodel/SavedState.kt
@@ -6,8 +6,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavArgs
 import androidx.navigation.NavArgsLazy
+import java.io.Serializable
 
-class SavedState(private val savedState: SavedStateHandle, val defaultArgs: Bundle?) {
+/**
+ *  A wrapper for the [SavedStateHandle], which takes the arguments [Bundle] and provides a delegate for type-safe
+ *  navigation arguments object. The arguments are supplied by the DI, usually coming from Fragment.arguments.
+ */
+class SavedState(private val savedState: SavedStateHandle, val arguments: Bundle?) {
+    init {
+        // there's a specific case, when the app is destroyed and the original arguments are lost;
+        // the SaveStateHandle would contain the the preserved values, which must be restored
+        if (arguments != null) {
+            savedState.keys().forEach {
+                val value = savedState.get<Any>(it)
+                if (!arguments.containsKey(it) && value is Serializable) {
+                    arguments.putSerializable(it, savedState.get(it))
+                }
+            }
+        }
+    }
+
     fun <T> getLiveData(key: String, initialValue: T? = null): MutableLiveData<T>
             = savedState.getLiveData(key, initialValue)
 
@@ -19,6 +37,6 @@ class SavedState(private val savedState: SavedStateHandle, val defaultArgs: Bund
 
     @MainThread
     inline fun <reified Args : NavArgs> navArgs() = NavArgsLazy(Args::class) {
-        defaultArgs ?: throw IllegalStateException("$this has null arguments")
+        arguments ?: throw IllegalStateException("$this has null arguments")
     }
 }
