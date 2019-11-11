335e28b47418367ef1d130fda47e74a0ca4b6b42
Ondrej Ruttkay <ondrej@automattic.com>
2019-11-11 20:28:28 +0100

    Add a refunds by items module

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundByItemsModule.kt
@@ -0,0 +1,32 @@
+package com.woocommerce.android.ui.refunds
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+import com.woocommerce.android.di.ViewModelAssistedFactory
+import com.woocommerce.android.ui.main.MainActivity
+import com.woocommerce.android.viewmodel.ViewModelKey
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class RefundByItemsModule {
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        fun provideDefaultArgs(fragment: RefundByItemsFragment): Bundle? {
+            return fragment.parentFragment?.arguments
+        }
+    }
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(IssueRefundViewModel::class)
+    abstract fun bindFactory(factory: IssueRefundViewModel.Factory): ViewModelAssistedFactory<out ViewModel>
+
+    @Binds
+    abstract fun bindSavedStateRegistryOwner(activity: MainActivity): SavedStateRegistryOwner
+}
