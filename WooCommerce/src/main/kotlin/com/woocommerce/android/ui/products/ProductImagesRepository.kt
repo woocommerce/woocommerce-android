3793d8baf47e1f4d73850ed905fc2ff24dd74ada
Nick Bradbury <nick.bradbury@gmail.com>
2019-11-15 12:08:32 -0500

    3rd pass at moving image removal to the image viewer

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductImagesRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductImagesRepository.kt
@@ -4,24 +4,14 @@ import com.woocommerce.android.annotations.OpenClassOnDebug
 import com.woocommerce.android.model.Product
 import com.woocommerce.android.model.toAppModel
 import com.woocommerce.android.tools.SelectedSite
-import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Inject
 
 @OpenClassOnDebug
 class ProductImagesRepository @Inject constructor(
-    private val dispatcher: Dispatcher,
     private val productStore: WCProductStore,
     private val selectedSite: SelectedSite
 ) {
-    init {
-        dispatcher.register(this)
-    }
-
-    fun onCleanup() {
-        dispatcher.unregister(this)
-    }
-
     fun getProduct(remoteProductId: Long): Product? =
             productStore.getProductByRemoteId(selectedSite.get(), remoteProductId)?.toAppModel()
 }
