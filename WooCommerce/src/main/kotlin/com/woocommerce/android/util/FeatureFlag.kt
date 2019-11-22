c001ae1c8a136e8485a4f53c818c65813e1dc696
anitaa <murthyanitaa@gmail.com>
2019-11-22 10:09:35 +0530

    disable products tab for debug builds

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -13,7 +13,7 @@ enum class FeatureFlag {
     REFUNDS;
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
-            PRODUCT_RELEASE_TEASER -> BuildConfig.DEBUG || AppPrefs.isProductsFeatureEnabled()
+            PRODUCT_RELEASE_TEASER -> AppPrefs.isProductsFeatureEnabled()
             REFUNDS -> BuildConfig.DEBUG
             DB_DOWNGRADE -> {
                 BuildConfig.DEBUG || context != null && PackageUtils.isBetaBuild(context)
