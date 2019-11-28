0af56c44f88bf7155836b55b67839f89c9e8bcd5
Ondrej Ruttkay <ondrej@automattic.com>
2019-11-29 00:22:54 +0100

    Create explicity Refund model

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/Refund.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/Refund.kt
@@ -1,7 +1,58 @@
 package com.woocommerce.android.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import org.wordpress.android.fluxc.model.refunds.WCRefundModel
 import org.wordpress.android.fluxc.model.refunds.WCRefundModel.WCRefundItem
+import java.math.BigDecimal
+import java.util.Date
 
-typealias Refund = WCRefundModel
-typealias Item = WCRefundItem
+@Parcelize
+data class Refund(
+    val id: Long,
+    val dateCreated: Date,
+    val amount: BigDecimal,
+    val reason: String?,
+    val automaticGatewayRefund: Boolean,
+    val items: List<Item>
+) : Parcelable {
+    @Parcelize
+    data class Item(
+        val productId: Long,
+        val quantity: Int,
+        val itemId: Long = 0,
+        val name: String = "",
+        val variationId: Long = 0,
+        val subtotal: BigDecimal = BigDecimal.ZERO,
+        val total: BigDecimal = BigDecimal.ZERO,
+        val totalTax: BigDecimal = BigDecimal.ZERO,
+        val sku: String = "",
+        val price: BigDecimal = BigDecimal.ZERO
+    ) : Parcelable
+}
+
+fun WCRefundModel.toAppModel(): Refund {
+    return Refund(
+            id,
+            dateCreated,
+            amount,
+            reason,
+            automaticGatewayRefund,
+            items.map { it.toAppModel() }
+    )
+}
+
+fun WCRefundItem.toAppModel(): Refund.Item {
+    return Refund.Item(
+            productId,
+            -quantity.toInt(), // WCRefundItem.quantity is NEGATIVE
+            itemId,
+            name,
+            variationId,
+            -subtotal, // WCRefundItem.subtotal is NEGATIVE
+            -total, // WCRefundItem.total is NEGATIVE
+            -totalTax, // WCRefundItem.totalTax is NEGATIVE
+            sku,
+            price
+    )
+}
