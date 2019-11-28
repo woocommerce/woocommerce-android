1c28a1f2e7f5044de2c03ccf1ca7adccd5af7c40
anitaa <murthyanitaa@gmail.com>
2019-11-28 11:46:06 +0530

    Added logic to display default text if no description is available

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailFragment.kt
@@ -240,10 +240,15 @@ class ProductDetailFragment : BaseFragment(), OnGalleryImageClickListener, Navig
         addPropertyView(DetailCard.Primary, R.string.product_name, productTitle, LinearLayout.VERTICAL)
 
         if (FeatureFlag.ADD_EDIT_PRODUCT_RELEASE_1.isEnabled()) {
+            val description = if (product.description.isEmpty()) {
+                getString(R.string.product_description_empty)
+            } else {
+                product.description
+            }
             addPropertyView(
                     DetailCard.Primary,
                     R.string.product_description,
-                    product.description,
+                    description,
                     LinearLayout.VERTICAL
             )?.also {
                 it.setMaxLines(2)
