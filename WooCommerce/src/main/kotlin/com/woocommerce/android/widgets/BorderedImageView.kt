d200680a049e1d9d4aec624ccf6567b72b105a9d
Nick Bradbury <nick.bradbury@gmail.com>
2019-12-02 16:30:20 -0500

    Enable setting border attributes in XML

--- WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/BorderedImageView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/BorderedImageView.kt
@@ -17,28 +17,42 @@ class BorderedImageView @JvmOverloads constructor(ctx: Context, attrs: Attribute
         attrs
 ) {
     private val borderPaint: Paint
-    private val borderSize = context.resources.getDimensionPixelSize(R.dimen.image_border_size).toFloat()
-    private val cornerSize = DisplayUtils.dpToPx(context, 2).toFloat()
+
+    private var borderSize = context.resources.getDimensionPixelSize(R.dimen.image_border_size).toFloat()
+    private var borderRadius = DisplayUtils.dpToPx(context, 2).toFloat()
+    private var borderColor = ContextCompat.getColor(context, R.color.list_divider)
 
     init {
+        attrs?.let {
+            val attrArray = context.obtainStyledAttributes(it, R.styleable.BorderedImageView)
+            try {
+                borderSize = attrArray.getDimension(R.styleable.BorderedImageView_borderSize, borderSize)
+                borderRadius = attrArray.getFloat(R.styleable.BorderedImageView_borderRadius, borderRadius)
+                borderColor = attrArray.getColor(R.styleable.BorderedImageView_borderColor, borderColor)
+            } finally {
+                attrArray.recycle()
+            }
+        }
+
+        
+
         borderPaint = Paint().also { paint ->
             paint.isAntiAlias = true
             paint.style = Paint.Style.STROKE
-            paint.color = ContextCompat.getColor(context, R.color.list_divider)
+            paint.color = borderColor
             paint.strokeWidth = borderSize
         }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        // canvas.drawRect(borderSize, borderSize, width - borderSize, height - borderSize, borderPaint)
         canvas.drawRoundRect(
                 0f, // left
                 0f, // top
                 width.toFloat(), // right
                 height.toFloat(), // bottom
-                cornerSize,
-                cornerSize,
+                borderRadius,
+                borderRadius,
                 borderPaint
         )
     }
