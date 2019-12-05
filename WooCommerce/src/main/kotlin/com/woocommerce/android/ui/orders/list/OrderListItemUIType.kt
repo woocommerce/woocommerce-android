7fd3d56eee5971e20db6dcf99aa44575a6cb3064
AmandaRiu <amanda.riu@automattic.com>
2019-12-04 20:05:27 -0700

    Convert to data classes so equals() will auto-generate

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListItemUIType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListItemUIType.kt
@@ -17,13 +17,13 @@ sealed class OrderListItemUIType {
     /**
      * Data required to populate a section header in the order list view.
      */
-    class SectionHeader(val title: TimeGroup) : OrderListItemUIType()
+    data class SectionHeader(val title: TimeGroup) : OrderListItemUIType()
 
     /**
      * Flag that the data or a single order item is not yet available for
      * display. Signals a loading view for that item should be displayed.
      */
-    class LoadingItem(val remoteId: RemoteId) : OrderListItemUIType()
+    data class LoadingItem(val remoteId: RemoteId) : OrderListItemUIType()
 
     /**
      * Data required to populate a single order item in the order list view.
@@ -37,35 +37,4 @@ sealed class OrderListItemUIType {
         val dateCreated: String,
         val currencyCode: String
     ) : OrderListItemUIType()
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-
-        if (javaClass != other?.javaClass) return false
-
-        if (this is SectionHeader && other is SectionHeader) {
-            return this.title == other.title
-        }
-
-        if (this is LoadingItem && other is LoadingItem) {
-            return this.remoteId == other.remoteId
-        }
-
-        if (this is OrderListItemUI && other is OrderListItemUI) {
-            if (remoteOrderId != other.remoteOrderId ||
-                    orderNumber != other.orderNumber ||
-                    orderName != other.orderName ||
-                    orderTotal != other.orderTotal ||
-                    status != other.status ||
-                    dateCreated != other.dateCreated ||
-                    currencyCode != other.currencyCode) {
-                return false
-            }
-        }
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return javaClass.hashCode()
-    }
 }
