50200c85b2c59e8254d5f85bd4edc941c6ad1a84
AmandaRiu <amanda.riu@automattic.com>
2020-01-09 15:00:06 -0700

    Clean up lint warnings

--- WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/UnreadItemDecoration.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/UnreadItemDecoration.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.woocommerce.android.R
 import com.woocommerce.android.widgets.sectionedrecyclerview.SectionedRecyclerViewAdapter
 import org.wordpress.android.util.DisplayUtils
+import kotlin.math.roundToInt
 
 /**
  * Item decoration for recycler views which simply shows a vertical indicator bar to the left to
@@ -37,12 +38,11 @@ class UnreadItemDecoration(context: Context, private val decorListener: ItemDeco
             val position = child?.let { parent.getChildAdapterPosition(it) }
                     ?: SectionedRecyclerViewAdapter.INVALID_POSITION
             if (position != SectionedRecyclerViewAdapter.INVALID_POSITION) {
-                val itemType = decorListener.getItemTypeAtPosition(position)
                 /*
                  * note that we have to draw the indicator for all items rather than just unread ones
                  * in order to paint over recycled cells that have a previously-drawn indicator
                  */
-                val colorId = when (itemType) {
+                val colorId = when (decorListener.getItemTypeAtPosition(position)) {
                     ItemType.HEADER -> android.R.color.transparent
                     ItemType.UNREAD -> R.color.unread_indicator_color
                     else -> android.R.color.transparent
@@ -53,7 +53,7 @@ class UnreadItemDecoration(context: Context, private val decorListener: ItemDeco
 
                 parent.getDecoratedBoundsWithMargins(child, bounds)
                 val top = bounds.top.toFloat()
-                val bottom = (bounds.bottom + Math.round(child.translationY)).toFloat()
+                val bottom = (bounds.bottom + child.translationY.roundToInt()).toFloat()
                 val left = bounds.left.toFloat()
                 val right = left + dividerWidth
 
