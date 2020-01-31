f4cdec0f91a2de2a81111a49348906536d290548
Nick Bradbury <nick.bradbury@gmail.com>
2020-01-31 16:18:08 -0500

    Fixed ktlint issue

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductShippingClassAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductShippingClassAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckedTextView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.products.ProductShippingClassAdapter.ViewHolder
