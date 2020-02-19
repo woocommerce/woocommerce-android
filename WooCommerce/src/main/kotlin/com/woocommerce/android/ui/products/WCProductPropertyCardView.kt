bcbcc15a23aa00830f5a5434bd36512b797d42dd
Ondrej Ruttkay <ondrej@automattic.com>
2020-02-19 20:15:03 +0100

    Fix ktlint & checkstyle issues

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/WCProductPropertyCardView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/WCProductPropertyCardView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.woocommerce.android.R
 
