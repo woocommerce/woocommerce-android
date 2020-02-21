8ffe0ca506d8c396da9d0a2465a316a95d2264c2
anitaa <murthyanitaa@gmail.com>
2020-02-21 13:22:54 +0530

    Added extension function to fast strip html from product title

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/StringExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/StringExt.kt
@@ -1,6 +1,6 @@
 package com.woocommerce.android.extensions
 
-import org.wordpress.android.util.HtmlUtils
+import org.apache.commons.text.StringEscapeUtils
 
 /**
  * If the provided [line] is not null and not empty, then add the string to this instance. Will prepend the
@@ -15,4 +15,36 @@ fun StringBuilder.appendWithIfNotEmpty(line: String?, separator: String = ", "):
     } ?: this
 }
 
-fun String.fastStripHtml(): String = HtmlUtils.fastStripHtml(this)
+/**
+ * This is much faster than Html.fromHtml but should only be used when we know the html is valid
+ * since the regex will be unpredictable with invalid html
+ * String param containing only valid html
+ * @return String without HTML
+ *
+ * Replicated from HtmlUtils.fastStripHtml and HtmlUtils.trimStart
+ */
+fun String.fastStripHtml(): String {
+    // insert a line break before P tags unless the only one is at the start
+    var str = this
+    if (str.isEmpty()) return str
+
+    if (str.lastIndexOf("<p") > 0) {
+        str = str.replace("<p(.|\n)*?>", "\n<p>")
+    }
+
+    // convert BR tags to line breaks
+    if (str.contains("<br")) {
+        str = str.replace("<br(.|\n)*?>", "\n")
+    }
+
+    val htmlString = StringEscapeUtils.unescapeHtml4(str.replace("<(.|\n)*?>", ""))
+    if (htmlString.isEmpty()) return str
+
+    var start = 0
+    while (start != 0 && (Character.isWhitespace(htmlString[start]) || htmlString[start].toInt() == 160)) {
+        start++
+    }
+
+    // use regex to strip tags, then convert entities in the result
+    return htmlString.substring(start)
+}
