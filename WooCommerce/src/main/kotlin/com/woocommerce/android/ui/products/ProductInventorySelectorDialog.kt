c8f9ab897acb1e7fca2e56f381c4798f7679f65c
AmandaRiu <amanda.riu@automattic.com>
2020-02-24 21:21:24 -0700

    Implement light/dark dialog themes on inventory selector and WCPromo dialogs

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductInventorySelectorDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductInventorySelectorDialog.kt
@@ -1,10 +1,10 @@
 package com.woocommerce.android.ui.products
 
-import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.woocommerce.android.RequestCodes
 import com.woocommerce.android.analytics.AnalyticsTracker
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductBackOrders
@@ -65,7 +65,7 @@ class ProductInventorySelectorDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val selectedIndex = getCurrentProductInventoryListIndex()
 
-        val builder = AlertDialog.Builder(requireContext())
+        val builder = MaterialAlertDialogBuilder(requireContext())
         builder.setTitle(dialogTitle)
                 .setSingleChoiceItems(listItemMap?.values?.toTypedArray(), selectedIndex) { dialog, which ->
                     listener?.onProductInventoryItemSelected(resultCode, listItemMap?.keys?.toTypedArray()?.get(which))
