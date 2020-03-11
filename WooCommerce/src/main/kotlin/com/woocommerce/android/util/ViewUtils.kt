9c22c44aebd6d4a72cc27716cc427fb6c687679d
AmandaRiu <amanda.riu@automattic.com>
2020-03-10 19:21:47 -0700

    Add helper for determining if device is a tablet

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/ViewUtils.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/ViewUtils.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.util
 
 import android.content.Context
+import com.woocommerce.android.R
 
 /**
  * Converts a pixel to a density pixel to match the density of
@@ -10,3 +11,5 @@ fun getDensityPixel(context: Context, dps: Int): Int {
     val scale = context.resources.displayMetrics.density
     return (dps * scale + 0.5f).toInt()
 }
+
+fun isTabletMode(context: Context) = context.resources.getBoolean(R.bool.is_tablet)
