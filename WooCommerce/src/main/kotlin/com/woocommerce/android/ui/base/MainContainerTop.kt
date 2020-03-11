d2686d92ef0c7ef1faa6c43c6eb8f24f95cdfc2a
AmandaRiu <amanda.riu@automattic.com>
2020-03-10 19:23:29 -0700

    Break out top and child fragment containers for reuse; add tablet layout

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/base/MainContainerTop.kt
@@ -0,0 +1,17 @@
+package com.woocommerce.android.ui.base
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.woocommerce.android.R
+
+class MainContainerTop @JvmOverloads constructor(
+    ctx: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : CoordinatorLayout(ctx, attrs, defStyleAttr) {
+    init {
+        View.inflate(context, R.layout.container_main_top, this)
+    }
+}
