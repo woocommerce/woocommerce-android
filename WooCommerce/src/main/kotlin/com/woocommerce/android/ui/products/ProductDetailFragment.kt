dfd293cee3add5b9025c550507675549b8006512
Nick Bradbury <nick.bradbury@gmail.com>
2020-03-14 08:52:04 -0400

    Don't show the product type in the activity title

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailFragment.kt
@@ -39,8 +39,6 @@ import com.woocommerce.android.ui.products.ProductNavigationTarget.ViewProductIn
 import com.woocommerce.android.ui.products.ProductNavigationTarget.ViewProductPricing
 import com.woocommerce.android.ui.products.ProductNavigationTarget.ViewProductShipping
 import com.woocommerce.android.ui.products.ProductNavigationTarget.ViewProductVariations
-import com.woocommerce.android.ui.products.ProductType.EXTERNAL
-import com.woocommerce.android.ui.products.ProductType.GROUPED
 import com.woocommerce.android.ui.products.ProductType.VARIABLE
 import com.woocommerce.android.util.DateUtils
 import com.woocommerce.android.util.FeatureFlag
@@ -64,7 +62,6 @@ class ProductDetailFragment : BaseProductFragment(), OnGalleryImageClickListener
         PurchaseDetails
     }
 
-    private var productTitle = ""
     private var productName = ""
     private var isVariation = false
     private val skeletonView = SkeletonView()
@@ -162,26 +159,13 @@ class ProductDetailFragment : BaseProductFragment(), OnGalleryImageClickListener
         progressDialog = null
     }
 
-    override fun getFragmentTitle() = productTitle
+    override fun getFragmentTitle() = productName
 
     private fun showProduct(productData: ProductDetailViewState) {
         if (!isAdded) return
 
         val product = requireNotNull(productData.product)
         productName = product.name.fastStripHtml()
-        productTitle = when (product.type) {
-            EXTERNAL -> getString(R.string.product_name_external, productName)
-            GROUPED -> getString(R.string.product_name_grouped, productName)
-            VARIABLE -> getString(R.string.product_name_variable, productName)
-            else -> {
-                if (product.isVirtual) {
-                    getString(R.string.product_name_virtual, productName)
-                } else {
-                    productName
-                }
-            }
-        }
-
         updateActivityTitle()
 
         if (product.images.isEmpty() && !viewModel.isUploading()) {
@@ -230,7 +214,7 @@ class ProductDetailFragment : BaseProductFragment(), OnGalleryImageClickListener
                 view.setOnTextChangedListener { viewModel.updateProductDraft(title = it.toString()) }
             }
         } else {
-            addPropertyView(DetailCard.Primary, R.string.product_name, productTitle, LinearLayout.VERTICAL)
+            addPropertyView(DetailCard.Primary, R.string.product_name, productName, LinearLayout.VERTICAL)
         }
 
         if (isAddEditProductRelease1Enabled(product.type)) {
