79368c698140b5b5dff92817c9b9ea3468c87622
Jeremy Massel <jeremy.massel@automattic.com>
2020-03-20 22:07:17 -0600

    Fix fastlane issues

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/products/ProductListScreen.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/products/ProductListScreen.kt
@@ -7,6 +7,7 @@ import com.woocommerce.android.screenshots.util.Screen
 class ProductListScreen : Screen {
     companion object {
         const val LIST_VIEW = R.id.productsRecycler
+        const val LIST_ITEM = R.id.linearLayout
     }
 
     val tabBar = TabNavComponent()
@@ -14,7 +15,7 @@ class ProductListScreen : Screen {
     constructor(): super(LIST_VIEW)
 
     fun selectProduct(index: Int): SingleProductScreen {
-        selectItemAtIndexInRecyclerView(index, LIST_VIEW)
+        selectItemAtIndexInRecyclerView(index, LIST_VIEW, LIST_ITEM)
         return SingleProductScreen()
     }
 }
