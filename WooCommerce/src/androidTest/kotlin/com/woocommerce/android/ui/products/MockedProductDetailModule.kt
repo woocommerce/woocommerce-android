2955091938a93d5fb0d14f32c058ee710779cfc0
Ondrej Ruttkay <ondrej@automattic.com>
2020-04-03 13:39:03 +0200

    Remove code that can never be called

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/products/MockedProductDetailModule.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/products/MockedProductDetailModule.kt
@@ -1,28 +1,15 @@
 package com.woocommerce.android.ui.products
 
 import android.os.Bundle
-import com.woocommerce.android.viewmodel.SavedStateWithArgs
 import androidx.lifecycle.ViewModel
 import androidx.savedstate.SavedStateRegistryOwner
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.spy
-import com.nhaarman.mockitokotlin2.whenever
 import com.woocommerce.android.di.ViewModelAssistedFactory
-import com.woocommerce.android.model.toAppModel
-import com.woocommerce.android.tools.NetworkStatus
-import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.util.CoroutineDispatchers
-import com.woocommerce.android.util.CurrencyFormatter
 import com.woocommerce.android.viewmodel.ViewModelKey
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
-import kotlinx.coroutines.Dispatchers.Unconfined
 import org.wordpress.android.fluxc.model.WCProductModel
-import org.wordpress.android.fluxc.store.WooCommerceStore
 
 @Module
 internal abstract class MockedProductDetailModule {
@@ -36,36 +23,6 @@ internal abstract class MockedProductDetailModule {
 
         @JvmStatic
         @Provides
-        fun provideProductDetailViewModel(
-            currencyFormatter: CurrencyFormatter,
-            networkStatus: NetworkStatus,
-            wcStore: WooCommerceStore,
-            site: SelectedSite
-        ): MockedProductDetailViewModel {
-            val mockProductRepository = mock<ProductDetailRepository>()
-            val coroutineDispatchers = CoroutineDispatchers(Unconfined, Unconfined, Unconfined)
-            val savedState: SavedStateWithArgs = mock()
-
-            val mockedProductDetailViewModel = spy(
-                    MockedProductDetailViewModel(
-                            coroutineDispatchers,
-                            wcStore,
-                            site,
-                            mockProductRepository,
-                            networkStatus,
-                            currencyFormatter,
-                            savedState
-                    )
-            )
-
-            doReturn(product?.toAppModel()).whenever(mockProductRepository).getProduct(any())
-            doReturn(true).whenever(networkStatus).isConnected()
-
-            return mockedProductDetailViewModel
-        }
-
-        @JvmStatic
-        @Provides
         fun provideDefaultArgs(): Bundle? {
             return null
         }
@@ -73,7 +30,7 @@ internal abstract class MockedProductDetailModule {
 
     @Binds
     @IntoMap
-    @ViewModelKey(MockedProductDetailViewModel::class)
+    @ViewModelKey(ProductDetailViewModel::class)
     abstract fun bindFactory(factory: MockedProductDetailViewModel.Factory): ViewModelAssistedFactory<out ViewModel>
 
     @Binds
