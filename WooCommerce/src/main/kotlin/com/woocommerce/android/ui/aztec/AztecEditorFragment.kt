97d03c8855437754b913a592c12bcf6c0130b8ae
Nick Bradbury <nick.bradbury@gmail.com>
2020-04-07 07:13:38 -0400

    Added separate request codes for description & short description

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/aztec/AztecEditorFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/aztec/AztecEditorFragment.kt
@@ -28,9 +28,9 @@ import org.wordpress.aztec.toolbar.IAztecToolbarClickListener
 class AztecEditorFragment : BaseFragment(), IAztecToolbarClickListener, BackPressListener {
     companion object {
         const val TAG: String = "AztecEditorFragment"
-        const val AZTEC_EDITOR_REQUEST_CODE = 3001
         const val ARG_AZTEC_EDITOR_TEXT = "editor-text"
         const val ARG_AZTEC_HAS_CHANGES = "editor-has-changes"
+
         private const val FIELD_IS_CONFIRMING_DISCARD = "is_confirming_discard"
         private const val FIELD_IS_HTML_EDITOR_ENABLED = "is_html_editor_enabled"
     }
