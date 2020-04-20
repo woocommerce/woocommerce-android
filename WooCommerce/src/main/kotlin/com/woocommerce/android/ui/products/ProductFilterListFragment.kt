a522f306897ad133cd33391028795967646aa1bf
anitaa <murthyanitaa@gmail.com>
2020-04-20 19:00:53 +0530

    Added viewModel + module for filter list fragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductFilterListFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductFilterListFragment.kt
@@ -1,7 +1,30 @@
 package com.woocommerce.android.ui.products
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.ui.base.BaseFragment
 
 class ProductFilterListFragment : BaseFragment() {
-    // TODO:
+    companion object {
+        const val TAG = "ProductFilterListFragment"
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_product_filter_list, container, false)
+    }
+
+    override fun getFragmentTitle() = getString(R.string.product_list_filters)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        AnalyticsTracker.trackViewShown(this)
+    }
 }
