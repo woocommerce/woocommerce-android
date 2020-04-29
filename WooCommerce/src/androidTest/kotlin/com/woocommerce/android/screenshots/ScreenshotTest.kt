348ee04d495b8a3a9843e9e51617c134bb184459
Gio Lodi <giovanni.lodi42@gmail.com>
2020-04-29 13:31:44 +1000

    Add instrumented test to record screenshots
    This comes with a whole mini-framework for UI testing by @jkmassel and
    integrates Fastlane's screengrab tool to eventually perform the actual
    capturing of the screenshots.

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/ScreenshotTest.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/ScreenshotTest.kt
@@ -5,12 +5,20 @@ import androidx.test.rule.ActivityTestRule
 import com.woocommerce.android.BuildConfig
 import com.woocommerce.android.screenshots.login.WelcomeScreen
 import com.woocommerce.android.screenshots.mystore.MyStoreScreen
+import com.woocommerce.android.screenshots.orders.OrderListScreen
+import com.woocommerce.android.screenshots.orders.SingleOrderScreen
+import com.woocommerce.android.screenshots.products.ProductListScreen
+import com.woocommerce.android.screenshots.reviews.ReviewsListScreen
 import com.woocommerce.android.ui.main.MainActivity
+import org.junit.AfterClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import tools.fastlane.screengrab.Screengrab
 import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+import tools.fastlane.screengrab.cleanstatusbar.BarsMode.TRANSPARENT
+import tools.fastlane.screengrab.cleanstatusbar.CleanStatusBar
+import tools.fastlane.screengrab.cleanstatusbar.IconVisibility.SHOW
 import tools.fastlane.screengrab.locale.LocaleTestRule
 
 @RunWith(AndroidJUnit4::class)
@@ -24,6 +32,14 @@ class ScreenshotTest {
     @Test
     fun screenshots() {
         Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+
+        // Configure the status bar so it looks the same in all screenshots
+        CleanStatusBar()
+                .setBatteryLevel(100)
+                .setClock("1231")
+                .setWifiVisibility(SHOW)
+                .setBarsMode(TRANSPARENT)
+                .enable()
 
         WelcomeScreen
             .logoutIfNeeded()
@@ -41,5 +57,32 @@ class ScreenshotTest {
         MyStoreScreen()
             .dismissTopBannerIfNeeded()
             .then<MyStoreScreen> { it.stats.switchToStatsDashboardYearsTab() }
+                .thenTakeScreenshot<MyStoreScreen>("order-dashboard")
+
+                // Orders
+                .tabBar.gotoOrdersScreen()
+                // This is a workaround to make sure the data has loaded
+                .selectOrder(0)
+                .goBackToOrdersScreen()
+                .thenTakeScreenshot<OrderListScreen>("order-list")
+                .selectOrder(0)
+                .thenTakeScreenshot<SingleOrderScreen>("order-detail")
+                .goBackToOrdersScreen()
+
+                // Reviews
+                .tabBar.gotoReviewsScreen()
+                .thenTakeScreenshot<ReviewsListScreen>("review-list")
+
+                // Products
+                .tabBar.gotoProductsScreen()
+                .thenTakeScreenshot<ProductListScreen>("product-list")
+    }
+
+    companion object {
+        // The matching `.enable()` call for CleanStatusBar is done inline in the test, to keep the configuration closer
+        // to where it's used.
+        @AfterClass @JvmStatic fun afterAll() {
+            CleanStatusBar.disable()
+        }
     }
 }
