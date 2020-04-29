70f3754c46cb19c221185dc33883abb7354ad3ea
Mohammad Haseeb <mhaqs@live.com>
2020-04-29 20:05:43 +1000

    Step 1 of showing categories on the product page. No tests have been updated because they mostly seem to be related to the view models.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailFragment.kt
@@ -31,6 +31,7 @@ import com.woocommerce.android.model.Product
 import com.woocommerce.android.ui.aztec.AztecEditorFragment
 import com.woocommerce.android.ui.aztec.AztecEditorFragment.Companion.ARG_AZTEC_EDITOR_TEXT
 import com.woocommerce.android.ui.main.MainActivity.NavigationResult
+import com.woocommerce.android.ui.products.ProductDetailFragment.DetailCard.Secondary
 import com.woocommerce.android.ui.products.ProductDetailViewModel.ProductDetailViewState
 import com.woocommerce.android.ui.products.ProductDetailViewModel.ProductExitEvent.ExitProductDetail
 import com.woocommerce.android.ui.products.ProductNavigationTarget.ViewProductDescriptionEditor
@@ -404,6 +405,22 @@ class ProductDetailFragment : BaseProductFragment(), OnGalleryImageClickListener
             it.setClickListener {
                 AnalyticsTracker.track(Stat.PRODUCT_DETAIL_VIEW_INVENTORY_SETTINGS_TAPPED)
                 viewModel.onEditProductCardClicked(ViewProductInventory(product.remoteId))
+            }
+        }
+
+        if (FeatureFlag.PRODUCT_RELEASE_M3.isEnabled()) {
+            addPropertyView(
+                    Secondary,
+                    getString(R.string.product_categories),
+                    if (product.categories.isEmpty()) {
+                        getString(R.string.product_category_empty)
+                    } else {
+                        product.categories.joinToString(transform = { it.name })
+                    },
+                    LinearLayout.VERTICAL,
+                    R.drawable.ic_gridicons_folder
+            )?.also {
+                it.setMaxLines(5)
             }
         }
 
