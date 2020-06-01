82de063430b7674cca89517f98fbdb6c71f2ee4e
anitaa <murthyanitaa@gmail.com>
2020-06-01 16:46:47 +0530

    Added modules for product categories

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/categories/ProductCategoriesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/categories/ProductCategoriesFragment.kt
@@ -1,7 +1,20 @@
 package com.woocommerce.android.ui.products.categories
 
-import androidx.fragment.app.Fragment
+import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.ui.products.BaseProductFragment
 
-class ProductCategoriesFragment : Fragment() {
-// TODO:
+class ProductCategoriesFragment : BaseProductFragment() {
+    override fun getFragmentTitle() = getString(R.string.product_categories)
+
+    override fun onResume() {
+        super.onResume()
+        AnalyticsTracker.trackViewShown(this)
+    }
+
+    override fun onRequestAllowBackPress(): Boolean {
+        // TODO:
+        return true
+    }
 }
+
