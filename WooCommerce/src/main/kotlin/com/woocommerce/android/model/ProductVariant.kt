4bdd80b4443978237ddadbdac4626a44e1f286e3
Ondrej Ruttkay <ondrej@automattic.com>
2020-06-17 13:05:45 +0200

    Extract the common dimension properties from products and variants to consolidate utility methods

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/ProductVariant.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/ProductVariant.kt
@@ -34,7 +34,11 @@ data class ProductVariant(
     val isDownloadable: Boolean,
     val description: String,
     val status: ProductStatus?
-) : Parcelable {
+    override val length: Float,
+    override val width: Float,
+    override val height: Float,
+    override val weight: Float
+) : Parcelable, IProduct {
     fun isSameVariant(variant: ProductVariant): Boolean {
         return remoteVariationId == variant.remoteVariationId &&
             remoteProductId == variant.remoteProductId &&
@@ -54,6 +58,10 @@ data class ProductVariant(
             isDownloadable == variant.isDownloadable &&
             description == variant.description &&
             status == variant.status
+            weight == variant.weight &&
+            length == variant.length &&
+            height == variant.height &&
+            width == variant.width
     }
 }
 
@@ -83,6 +91,10 @@ fun WCProductVariationModel.toAppModel(): ProductVariant {
         isVirtual = this.virtual,
         description = this.description,
         status = ProductStatus.fromString(this.status)
+        length = this.length.toFloatOrNull() ?: 0f,
+        width = this.width.toFloatOrNull() ?: 0f,
+        height = this.height.toFloatOrNull() ?: 0f,
+        weight = this.weight.toFloatOrNull() ?: 0f
     )
 }
 
