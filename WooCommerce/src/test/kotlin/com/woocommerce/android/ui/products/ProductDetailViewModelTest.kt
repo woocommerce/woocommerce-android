417127dccbb11d3df15d69d1855ca7c0732e96ef
Ondrej Ruttkay <ondrej@automattic.com>
2020-06-25 13:53:48 +0200

    Add a new coroutine test rule and update the way the VM is initialize

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/ProductDetailViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/ProductDetailViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.products
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
@@ -8,7 +9,6 @@ import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
-import com.woocommerce.android.BuildConfig
 import com.woocommerce.android.R
 import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.media.ProductImagesServiceWrapper
@@ -19,7 +19,6 @@ import com.woocommerce.android.ui.products.ProductDetailViewModel.ProductImagesV
 import com.woocommerce.android.ui.products.categories.ProductCategoriesRepository
 import com.woocommerce.android.ui.products.models.ProductProperty.ComplexProperty
 import com.woocommerce.android.ui.products.models.ProductPropertyCard
-import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.util.CurrencyFormatter
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowSnackbar
@@ -38,9 +37,10 @@ import com.woocommerce.android.ui.products.models.ProductProperty.PropertyGroup
 import com.woocommerce.android.ui.products.models.ProductProperty.RatingBar
 import com.woocommerce.android.ui.products.models.ProductPropertyCard.Type.PRIMARY
 import com.woocommerce.android.ui.products.models.ProductPropertyCard.Type.SECONDARY
+import com.woocommerce.android.util.CoroutineTestRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
 import java.math.BigDecimal
 
 @ExperimentalCoroutinesApi
@@ -61,12 +61,19 @@ class ProductDetailViewModelTest : BaseUnitTest() {
     private val currencyFormatter: CurrencyFormatter = mock {
         on(it.formatCurrency(any<BigDecimal>(), any(), any())).thenAnswer { i -> "${i.arguments[1]}${i.arguments[0]}" }
     }
-    private val savedState: SavedStateWithArgs = mock()
 
-    @ExperimentalCoroutinesApi
-    private val coroutineDispatchers = CoroutineDispatchers(
-        TestCoroutineDispatcher(), TestCoroutineDispatcher(), TestCoroutineDispatcher()
+    private val savedState: SavedStateWithArgs = spy(
+        SavedStateWithArgs(
+            SavedStateHandle(),
+            null,
+            ProductDetailFragmentArgs(remoteProductId = PRODUCT_REMOTE_ID)
+        )
     )
+
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineTestRule()
+
     private val product = ProductTestUtils.generateProduct(PRODUCT_REMOTE_ID)
     private val offlineProduct = ProductTestUtils.generateProduct(OFFLINE_PRODUCT_REMOTE_ID)
     private val productCategories = ProductTestUtils.generateProductCategories()
@@ -129,24 +136,8 @@ class ProductDetailViewModelTest : BaseUnitTest() {
             .whenever(savedState).getLiveData<ProductImagesViewState>(any(), any())
 
         // Avoids the unnecessary stubbing exception
-        if (BuildConfig.DEBUG) {
-            doReturn(TEST_STRING).whenever(resources).getString(any())
-        }
+        doReturn(TEST_STRING).whenever(resources).getString(any())
 
-        viewModel = spy(
-            ProductDetailViewModel(
-                savedState,
-                coroutineDispatchers,
-                selectedSite,
-                productRepository,
-                networkStatus,
-                currencyFormatter,
-                wooCommerceStore,
-                productImagesServiceWrapper,
-                resources,
-                productCategoriesRepository
-            )
-        )
         val prodSettings = WCProductSettingsModel(0).apply {
             dimensionUnit = "cm"
             weightUnit = "kg"
@@ -161,23 +152,38 @@ class ProductDetailViewModelTest : BaseUnitTest() {
         doReturn(siteSettings).whenever(wooCommerceStore).getSiteSettings(any())
     }
 
+    private fun initViewModel() {
+        viewModel = spy(
+                ProductDetailViewModel(
+                    savedState,
+                    coroutinesTestRule.testDispatchers,
+                    selectedSite,
+                    productRepository,
+                    networkStatus,
+                    currencyFormatter,
+                    wooCommerceStore,
+                    productImagesServiceWrapper,
+                    resources,
+                    productCategoriesRepository
+                )
+        )
+    }
+
     @Test
-    fun `Displays the product detail properties correctly`() = runBlockingTest {
-        // This feature is only enabled in debug mode and would thus fail in release mode
-        if (BuildConfig.DEBUG) {
-            doReturn(product).whenever(productRepository).getProduct(any())
+    fun `Displays the product detail properties correctly`() = coroutinesTestRule.testDispatcher.runBlockingTest {
+        doReturn(true).whenever(networkStatus).isConnected()
+        doReturn(product).whenever(productRepository).getProduct(any())
 
-            viewModel.productDetailViewStateData.observeForever { _, _ -> }
+        initViewModel()
 
-            var cards: List<ProductPropertyCard>? = null
-            viewModel.productDetailCards.observeForever {
-                cards = it.map { card -> stripCallbacks(card) }
-            }
+        viewModel.productDetailViewStateData.observeForever { _, _ -> }
 
-            viewModel.start(PRODUCT_REMOTE_ID)
-
-            assertThat(expectedCards).isEqualTo(cards)
+        var cards: List<ProductPropertyCard>? = null
+        viewModel.productDetailCards.observeForever {
+            cards = it.map { card -> stripCallbacks(card) }
         }
+
+        assertThat(cards).isEqualTo(expectedCards)
     }
 
     private fun stripCallbacks(card: ProductPropertyCard): ProductPropertyCard {
@@ -196,27 +202,25 @@ class ProductDetailViewModelTest : BaseUnitTest() {
     fun `Displays the product detail view correctly`() {
         doReturn(product).whenever(productRepository).getProduct(any())
 
+        initViewModel()
+
         var productData: ProductDetailViewState? = null
         viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
-
-        assertThat(productData).isEqualTo(ProductDetailViewState())
-
-        viewModel.start(PRODUCT_REMOTE_ID)
 
         assertThat(productData).isEqualTo(productWithParameters)
     }
 
     @Test
-    fun `Display error message on fetch product error`() = runBlockingTest {
+    fun `Display error message on fetch product error`() = coroutinesTestRule.testDispatcher.runBlockingTest {
         whenever(productRepository.fetchProduct(PRODUCT_REMOTE_ID)).thenReturn(null)
         whenever(productRepository.getProduct(PRODUCT_REMOTE_ID)).thenReturn(null)
+
+        initViewModel()
 
         var snackbar: ShowSnackbar? = null
         viewModel.event.observeForever {
             if (it is ShowSnackbar) snackbar = it
         }
-
-        viewModel.start(PRODUCT_REMOTE_ID)
 
         verify(productRepository, times(1)).fetchProduct(PRODUCT_REMOTE_ID)
 
@@ -224,16 +228,16 @@ class ProductDetailViewModelTest : BaseUnitTest() {
     }
 
     @Test
-    fun `Do not fetch product from api when not connected`() = runBlockingTest {
+    fun `Do not fetch product from api when not connected`() = coroutinesTestRule.testDispatcher.runBlockingTest {
         doReturn(offlineProduct).whenever(productRepository).getProduct(any())
         doReturn(false).whenever(networkStatus).isConnected()
+
+        initViewModel()
 
         var snackbar: ShowSnackbar? = null
         viewModel.event.observeForever {
             if (it is ShowSnackbar) snackbar = it
         }
-
-        viewModel.start(PRODUCT_REMOTE_ID)
 
         verify(productRepository, times(1)).getProduct(PRODUCT_REMOTE_ID)
         verify(productRepository, times(0)).fetchProduct(any())
@@ -242,15 +246,15 @@ class ProductDetailViewModelTest : BaseUnitTest() {
     }
 
     @Test
-    fun `Shows and hides product detail skeleton correctly`() = runBlockingTest {
+    fun `Shows and hides product detail skeleton correctly`() = coroutinesTestRule.testDispatcher.runBlockingTest {
         doReturn(null).whenever(productRepository).getProduct(any())
+
+        initViewModel()
 
         val isSkeletonShown = ArrayList<Boolean>()
         viewModel.productDetailViewStateData.observeForever { old, new ->
             new.isSkeletonShown?.takeIfNotEqualTo(old?.isSkeletonShown) { isSkeletonShown.add(it) }
         }
-
-        viewModel.start(PRODUCT_REMOTE_ID)
 
         assertThat(isSkeletonShown).containsExactly(true, false)
     }
@@ -259,18 +263,17 @@ class ProductDetailViewModelTest : BaseUnitTest() {
     fun `Displays the updated product detail view correctly`() {
         doReturn(product).whenever(productRepository).getProduct(any())
 
+        initViewModel()
+
         var productData: ProductDetailViewState? = null
         viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
 
         assertThat(productData).isEqualTo(ProductDetailViewState())
-
-        viewModel.start(PRODUCT_REMOTE_ID)
         assertThat(productData).isEqualTo(productWithParameters)
 
         val updatedDescription = "Updated product description"
         viewModel.updateProductDraft(updatedDescription)
 
-        viewModel.start(PRODUCT_REMOTE_ID)
         assertThat(productData?.productDraft?.description).isEqualTo(updatedDescription)
     }
 
@@ -278,120 +281,112 @@ class ProductDetailViewModelTest : BaseUnitTest() {
     fun `Displays update menu action if product is edited`() {
         doReturn(product).whenever(productRepository).getProduct(any())
 
+        initViewModel()
+
         var productData: ProductDetailViewState? = null
         viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
 
-        viewModel.start(PRODUCT_REMOTE_ID)
         assertThat(productData?.isProductUpdated).isNull()
 
         val updatedDescription = "Updated product description"
         viewModel.updateProductDraft(updatedDescription)
 
-        viewModel.start(PRODUCT_REMOTE_ID)
         assertThat(productData?.isProductUpdated).isTrue()
     }
 
     @Test
-    fun `Displays progress dialog when product is edited`() = runBlockingTest {
-        // This feature is only enabled in debug mode and would thus fail in release mode
-        if (BuildConfig.DEBUG) {
-            doReturn(product).whenever(productRepository).getProduct(any())
-            doReturn(false).whenever(productRepository).updateProduct(any())
+    fun `Displays progress dialog when product is edited`() = coroutinesTestRule.testDispatcher.runBlockingTest {
+        doReturn(product).whenever(productRepository).getProduct(any())
+        doReturn(false).whenever(productRepository).updateProduct(any())
 
-            val isProgressDialogShown = ArrayList<Boolean>()
-            viewModel.productDetailViewStateData.observeForever { old, new ->
-                new.isProgressDialogShown?.takeIfNotEqualTo(old?.isProgressDialogShown) {
-                    isProgressDialogShown.add(it)
-                }
+        initViewModel()
+
+        val isProgressDialogShown = ArrayList<Boolean>()
+        viewModel.productDetailViewStateData.observeForever { old, new ->
+            new.isProgressDialogShown?.takeIfNotEqualTo(old?.isProgressDialogShown) {
+                isProgressDialogShown.add(it)
             }
-
-            viewModel.start(PRODUCT_REMOTE_ID)
-            viewModel.onUpdateButtonClicked()
-
-            assertThat(isProgressDialogShown).containsExactly(true, false)
         }
+
+        viewModel.onUpdateButtonClicked()
+
+        assertThat(isProgressDialogShown).containsExactly(true, false)
     }
 
     @Test
-    fun `Do not update product when not connected`() = runBlockingTest {
-        // This feature is only enabled in debug mode and would thus fail in release mode
-        if (BuildConfig.DEBUG) {
-            doReturn(product).whenever(productRepository).getProduct(any())
-            doReturn(false).whenever(networkStatus).isConnected()
+    fun `Do not update product when not connected`() = coroutinesTestRule.testDispatcher.runBlockingTest {
+        doReturn(product).whenever(productRepository).getProduct(any())
+        doReturn(false).whenever(networkStatus).isConnected()
 
-            var snackbar: ShowSnackbar? = null
-            viewModel.event.observeForever {
-                if (it is ShowSnackbar) snackbar = it
-            }
+        initViewModel()
 
-            var productData: ProductDetailViewState? = null
-            viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
-
-            viewModel.start(PRODUCT_REMOTE_ID)
-            viewModel.onUpdateButtonClicked()
-
-            verify(productRepository, times(0)).updateProduct(any())
-            assertThat(snackbar).isEqualTo(ShowSnackbar(R.string.offline_error))
-            assertThat(productData?.isProgressDialogShown).isFalse()
+        var snackbar: ShowSnackbar? = null
+        viewModel.event.observeForever {
+            if (it is ShowSnackbar) snackbar = it
         }
+
+        var productData: ProductDetailViewState? = null
+        viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
+
+        viewModel.onUpdateButtonClicked()
+
+        verify(productRepository, times(0)).updateProduct(any())
+        assertThat(snackbar).isEqualTo(ShowSnackbar(R.string.offline_error))
+        assertThat(productData?.isProgressDialogShown).isFalse()
     }
 
     @Test
-    fun `Display error message on update product error`() = runBlockingTest {
-        // This feature is only enabled in debug mode and would thus fail in release mode
-        if (BuildConfig.DEBUG) {
-            doReturn(product).whenever(productRepository).getProduct(any())
-            doReturn(false).whenever(productRepository).updateProduct(any())
+    fun `Display error message on update product error`() = coroutinesTestRule.testDispatcher.runBlockingTest {
+        doReturn(product).whenever(productRepository).getProduct(any())
+        doReturn(false).whenever(productRepository).updateProduct(any())
 
-            var snackbar: ShowSnackbar? = null
-            viewModel.event.observeForever {
-                if (it is ShowSnackbar) snackbar = it
-            }
+        initViewModel()
 
-            var productData: ProductDetailViewState? = null
-            viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
-
-            viewModel.start(PRODUCT_REMOTE_ID)
-            viewModel.onUpdateButtonClicked()
-
-            verify(productRepository, times(1)).updateProduct(any())
-            assertThat(snackbar).isEqualTo(ShowSnackbar(R.string.product_detail_update_product_error))
-            assertThat(productData?.isProgressDialogShown).isFalse()
+        var snackbar: ShowSnackbar? = null
+        viewModel.event.observeForever {
+            if (it is ShowSnackbar) snackbar = it
         }
+
+        var productData: ProductDetailViewState? = null
+        viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
+
+        viewModel.onUpdateButtonClicked()
+
+        verify(productRepository, times(1)).updateProduct(any())
+        assertThat(snackbar).isEqualTo(ShowSnackbar(R.string.product_detail_update_product_error))
+        assertThat(productData?.isProgressDialogShown).isFalse()
     }
 
     @Test
-    fun `Display success message on update product success`() = runBlockingTest {
-        // This feature is only enabled in debug mode and would thus fail in release mode
-        if (BuildConfig.DEBUG) {
-            doReturn(product).whenever(productRepository).getProduct(any())
-            doReturn(true).whenever(productRepository).updateProduct(any())
+    fun `Display success message on update product success`() = coroutinesTestRule.testDispatcher.runBlockingTest {
+        doReturn(product).whenever(productRepository).getProduct(any())
+        doReturn(true).whenever(productRepository).updateProduct(any())
 
-            var successSnackbarShown = false
-            viewModel.event.observeForever {
-                if (it is ShowSnackbar && it.message == R.string.product_detail_update_product_success) {
-                    successSnackbarShown = true
-                }
+        initViewModel()
+
+        var successSnackbarShown = false
+        viewModel.event.observeForever {
+            if (it is ShowSnackbar && it.message == R.string.product_detail_update_product_success) {
+                successSnackbarShown = true
             }
-
-            var productData: ProductDetailViewState? = null
-            viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
-
-            viewModel.start(PRODUCT_REMOTE_ID)
-            viewModel.onUpdateButtonClicked()
-
-            verify(productRepository, times(1)).updateProduct(any())
-            verify(productRepository, times(2)).getProduct(PRODUCT_REMOTE_ID)
-
-            assertThat(successSnackbarShown).isTrue()
-            assertThat(productData?.isProgressDialogShown).isFalse()
-            assertThat(productData?.isProductUpdated).isFalse()
-            assertThat(productData?.productDraft).isEqualTo(product)
         }
+
+        var productData: ProductDetailViewState? = null
+        viewModel.productDetailViewStateData.observeForever { _, new -> productData = new }
+
+        viewModel.onUpdateButtonClicked()
+
+        verify(productRepository, times(1)).updateProduct(any())
+        verify(productRepository, times(2)).getProduct(PRODUCT_REMOTE_ID)
+
+        assertThat(successSnackbarShown).isTrue()
+        assertThat(productData?.isProgressDialogShown).isFalse()
+        assertThat(productData?.isProductUpdated).isFalse()
+        assertThat(productData?.productDraft).isEqualTo(product)
     }
 
     @Test
-    fun `Correctly sorts the Product Categories By their Parent Ids and by name`() = runBlockingTest {
+    fun `Correctly sorts the Product Categories By their Parent Ids and by name`() = coroutinesTestRule.testDispatcher.runBlockingTest {
         val sortedByNameAndParent = viewModel.sortAndStyleProductCategories(
             product, productCategories
         ).toList()
@@ -409,7 +404,7 @@ class ProductDetailViewModelTest : BaseUnitTest() {
     }
 
     @Test
-    fun `Correctly computes the cascading margin for the product Category by their Parent Ids`() = runBlockingTest {
+    fun `Correctly computes the cascading margin for the product Category by their Parent Ids`() = coroutinesTestRule.testDispatcher.runBlockingTest {
         val sortedAndStyledList = viewModel.sortAndStyleProductCategories(product, productCategories)
 
         assertThat(sortedAndStyledList[0].category).isEqualTo(productCategories[0])
