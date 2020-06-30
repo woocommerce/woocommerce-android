499e92d5c2bf221711be369df2785dea29735950
Volchansky <ivan.volchansky@outlook.com>
2020-06-30 15:33:45 -0700

    Add TestBase

---
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/tests/TestBase.kt
@@ -0,0 +1,36 @@
+package com.woocommerce.android.screenshots.tests
+
+import androidx.test.rule.ActivityTestRule
+import com.woocommerce.android.BuildConfig
+import com.woocommerce.android.screenshots.login.WelcomeScreen
+import com.woocommerce.android.ui.main.MainActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import tools.fastlane.screengrab.locale.LocaleTestRule
+
+open class TestBase() {
+    @Rule @JvmField
+    val localeTestRule = LocaleTestRule()
+
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun setup() {
+        WelcomeScreen
+            .logoutIfNeeded()
+            .selectLogin()
+            // Connect a WooCommerce store by URL
+            .proceedWith(BuildConfig.SCREENSHOTS_URL)
+            // Enter email address
+            .proceedWith(BuildConfig.SCREENSHOTS_USERNAME)
+            // No magic link, this is an automated test, enter password manually
+            .proceedWithPassword()
+            .proceedWith(BuildConfig.SCREENSHOTS_PASSWORD)
+    }
+
+    @After
+    open fun tearDown() {
+    }
+}
