003d5ee74035fd8da0068f0b293732f3a4ab1255
anitaa <murthyanitaa@gmail.com>
2020-07-19 19:36:00 +0530

    Added modules + viewModel + UI for refunding shipping label

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/ShippingLabelRefundFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/ShippingLabelRefundFragment.kt
@@ -1,5 +1,17 @@
 package com.woocommerce.android.ui.orders.shippinglabels
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.base.BaseFragment
 
-class ShippingLabelRefundFragment : BaseFragment()
+class ShippingLabelRefundFragment : BaseFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_shipping_label_refund, container, false)
+    }
+
+    override fun getFragmentTitle() = getString(R.string.orderdetail_shipping_label_request_refund)
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/ShippingLabelRefundViewModel.kt
@@ -0,0 +1,16 @@
+package com.woocommerce.android.ui.orders.shippinglabels
+
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import com.woocommerce.android.di.ViewModelAssistedFactory
+import com.woocommerce.android.util.CoroutineDispatchers
+import com.woocommerce.android.viewmodel.SavedStateWithArgs
+import com.woocommerce.android.viewmodel.ScopedViewModel
+
+class ShippingLabelRefundViewModel @AssistedInject constructor(
+    @Assisted savedState: SavedStateWithArgs,
+    dispatchers: CoroutineDispatchers
+) : ScopedViewModel(savedState, dispatchers) {
+    @AssistedInject.Factory
+    interface Factory : ViewModelAssistedFactory<ShippingLabelRefundViewModel>
+}
