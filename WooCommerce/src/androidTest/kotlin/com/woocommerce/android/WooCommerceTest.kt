876c0a495053c192ac3df882a5c0b1f376b8878d
rachelmcr <rachel@automattic.com>
2020-07-24 13:34:32 +0100

    Remove unused UI instrumentation tests and mocking

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooCommerceTest.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooCommerceTest.kt
@@ -7,18 +7,8 @@ import com.woocommerce.android.di.DaggerAppComponentTest
 
 open class WooCommerceTest : WooCommerce() {
     override val component: AppComponent by lazy {
-        // The mocks interfere with the screenshots recording, which right
-        // now uses a live account. Use this parameter to disable them.
-        val shouldDisableMocks = InstrumentationRegistry.getArguments().get("disableMocks") == "true"
-
-        if (shouldDisableMocks) {
-            DaggerAppComponent.builder()
-                    .application(this)
-                    .build()
-        } else {
-            DaggerAppComponentTest.builder()
-                    .application(this)
-                    .build()
-        }
+        DaggerAppComponentTest.builder()
+                .application(this)
+                .build()
     }
 }
