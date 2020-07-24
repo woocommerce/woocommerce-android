b37ee5c18d2a754fc3fd1f8c31f73f787a1ed136
rachelmcr <rachel@automattic.com>
2020-07-24 14:04:19 +0100

    Remove unused imports

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooCommerceTest.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooCommerceTest.kt
@@ -1,8 +1,6 @@
 package com.woocommerce.android
 
-import androidx.test.platform.app.InstrumentationRegistry
 import com.woocommerce.android.di.AppComponent
-import com.woocommerce.android.di.DaggerAppComponent
 import com.woocommerce.android.di.DaggerAppComponentTest
 
 open class WooCommerceTest : WooCommerce() {
