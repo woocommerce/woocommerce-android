5ec6833ba3f764db1d87769da43387da0494134e
Amanda Riu <amanda.riu@automattic.com>
2020-07-28 16:45:45 -0600

    Add feedback card to the My Store page

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackRequestCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackRequestCard.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.View
 import com.woocommerce.android.R
 import com.woocommerce.android.widgets.WCElevatedConstraintLayout
+import kotlinx.android.synthetic.main.feedback_request_card.view.*
 
 class FeedbackRequestCard @JvmOverloads constructor(
     ctx: Context, attrs:
@@ -13,5 +14,16 @@ class FeedbackRequestCard @JvmOverloads constructor(
 ) : WCElevatedConstraintLayout(ctx, attrs, defStyleAttr) {
     init {
         View.inflate(context, R.layout.feedback_request_card, this)
+    }
+
+    /**
+     * Sets the click listeners for the buttons on this card
+     *
+     * @param negativeButtonAction The action to perform when the user clicks "Could be better"
+     * @param positiveButtonAction The action to perform when the user clicks "I like it"
+     */
+    fun initView(negativeButtonAction: (() -> Unit), positiveButtonAction: (() -> Unit)) {
+        btn_feedbackReq_negative.setOnClickListener { negativeButtonAction() }
+        btn_feedbackReq_positive.setOnClickListener { positiveButtonAction() }
     }
 }
