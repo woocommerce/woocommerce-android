becbbadcdfd3730dec4a3d6008d9bdb151c7547f
Ondrej Ruttkay <ondrej@automattic.com>
2020-08-07 18:49:35 +0200

    Add the warning view to variation list

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/viewholders/WarningViewHolder.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/viewholders/WarningViewHolder.kt
@@ -5,7 +5,9 @@ import android.widget.TextView
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.products.models.ProductProperty.Warning
 
-class WarningViewHolder(parent: ViewGroup) : ProductPropertyViewHolder(parent, R.layout.product_property_warning_layout) {
+class WarningViewHolder(
+    parent: ViewGroup
+) : ProductPropertyViewHolder(parent, R.layout.product_property_warning_layout) {
     fun bind(item: Warning) {
         val content = itemView.findViewById<TextView>(R.id.warningBody)
         content.text = item.content
