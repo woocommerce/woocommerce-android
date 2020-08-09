30cdffd30eaea770db26bba5558ef25ddcfeed41
anitaa <murthyanitaa@gmail.com>
2020-08-09 20:00:41 +0530

    Added logic to display bottom sheet for changing product type

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductTypesBottomSheetFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductTypesBottomSheetFragment.kt
@@ -1,11 +1,23 @@
 package com.woocommerce.android.ui.products
 
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.products.ProductTypesBottomSheetViewModel.ProductTypesBottomSheetUiItem
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ViewModelFactory
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.dialog_product_detail_bottom_sheet_list.*
 import javax.inject.Inject
 
 class ProductTypesBottomSheetFragment : BottomSheetDialogFragment(), HasAndroidInjector {
@@ -14,7 +26,57 @@ class ProductTypesBottomSheetFragment : BottomSheetDialogFragment(), HasAndroidI
     @Inject lateinit var viewModelFactory: ViewModelFactory
     val viewModel: ProductTypesBottomSheetViewModel by viewModels { viewModelFactory }
 
+    private lateinit var productTypesBottomSheetAdapter: ProductTypesBottomSheetAdapter
+
     override fun androidInjector(): AndroidInjector<Any> {
         return childInjector
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.dialog_product_detail_bottom_sheet_list, container, false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        AndroidSupportInjection.inject(this)
+        return super.onCreateDialog(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObservers()
+        viewModel.loadProductTypes()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        productTypesBottomSheetAdapter = ProductTypesBottomSheetAdapter(
+            viewModel::onProductTypeSelected
+        )
+        with(productDetailInfo_optionsList) {
+            adapter = productTypesBottomSheetAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.productTypesBottomSheetList.observe(viewLifecycleOwner, Observer {
+            showProductTypeOptions(it)
+        })
+
+        viewModel.event.observe(viewLifecycleOwner, Observer { event ->
+            when (event) {
+                is Exit -> {
+                    dismiss()
+                }
+                else -> event.isHandled = false
+            }
+        })
+    }
+
+    private fun showProductTypeOptions(
+        productTypeOptions: List<ProductTypesBottomSheetUiItem>
+    ) {
+        productTypesBottomSheetAdapter.setProductTypeOptions(productTypeOptions)
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductTypesBottomSheetViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductTypesBottomSheetViewModel.kt
@@ -1,9 +1,16 @@
 package com.woocommerce.android.ui.products
 
+import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import com.woocommerce.android.R
 import com.woocommerce.android.annotations.OpenClassOnDebug
 import com.woocommerce.android.di.ViewModelAssistedFactory
+import com.woocommerce.android.ui.products.ProductType.EXTERNAL
+import com.woocommerce.android.ui.products.ProductType.GROUPED
+import com.woocommerce.android.ui.products.ProductType.SIMPLE
 import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.SavedStateWithArgs
 import com.woocommerce.android.viewmodel.ScopedViewModel
@@ -13,6 +20,50 @@ class ProductTypesBottomSheetViewModel @AssistedInject constructor(
     @Assisted savedState: SavedStateWithArgs,
     dispatchers: CoroutineDispatchers
 ) : ScopedViewModel(savedState, dispatchers) {
+    private val _productTypesBottomSheetList = MutableLiveData<List<ProductTypesBottomSheetUiItem>>()
+    val productTypesBottomSheetList: LiveData<List<ProductTypesBottomSheetUiItem>> = _productTypesBottomSheetList
+
+    fun loadProductTypes() {
+        _productTypesBottomSheetList.value = buildProductTypeList()
+    }
+
+    fun onProductTypeSelected(type: ProductType) {
+        // TODO: display confirmation dialog
+    }
+
+    private fun buildProductTypeList(): List<ProductTypesBottomSheetUiItem> {
+        return listOf(
+            ProductTypesBottomSheetUiItem(
+                type = SIMPLE,
+                titleResource = R.string.product_type_physical,
+                descResource = R.string.product_type_physical_desc
+            ),
+            ProductTypesBottomSheetUiItem(
+                type = SIMPLE,
+                isVirtual = true,
+                titleResource = R.string.product_type_virtual,
+                descResource = R.string.product_type_virtual_desc
+            ),
+            ProductTypesBottomSheetUiItem(
+                type = GROUPED,
+                titleResource = R.string.product_type_grouped,
+                descResource = R.string.product_type_grouped_desc
+            ),
+            ProductTypesBottomSheetUiItem(
+                type = EXTERNAL,
+                titleResource = R.string.product_type_external,
+                descResource = R.string.product_type_external_desc
+            )
+        )
+    }
+
+    data class ProductTypesBottomSheetUiItem(
+        val type: ProductType,
+        val isVirtual: Boolean = false,
+        @StringRes val titleResource: Int,
+        @StringRes val descResource: Int
+    )
+
     @AssistedInject.Factory
     interface Factory : ViewModelAssistedFactory<ProductTypesBottomSheetViewModel>
 }
