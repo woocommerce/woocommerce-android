e08773639d83e2425390b3b889ec247b3909386f
Thomaz Cortez <thomazfcortez@gmail.com>
2020-08-13 06:44:36 -0300

    Add Progress Dialog and WebViewClient into FeedbackSurveyFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackSurveyFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackSurveyFragment.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.woocommerce.android.BuildConfig
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.widgets.CustomProgressDialog
 import kotlinx.android.synthetic.main.fragment_licenses.*
 
 class FeedbackSurveyFragment : androidx.fragment.app.Fragment() {
     companion object {
         const val TAG = "feedback_survey"
     }
+
+    private var progressDialog: CustomProgressDialog? = null
+    private val surveyWebViewClient = SurveyWebViewClient()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_feedback_survey, container, false)
@@ -22,6 +28,8 @@ class FeedbackSurveyFragment : androidx.fragment.app.Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         context?.let {
+            showProgressDialog()
+            webView.webViewClient = surveyWebViewClient
             webView.loadUrl(BuildConfig.CROWDSIGNAL_URL)
         }
     }
@@ -33,6 +41,27 @@ class FeedbackSurveyFragment : androidx.fragment.app.Fragment() {
         activity?.let {
             it.title = getString(R.string.feedback_survey_request_title)
             (it as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_gridicons_cross_white_24dp)
+        }
+    }
+
+    private fun showProgressDialog() {
+        hideProgressDialog()
+        progressDialog = CustomProgressDialog.show(
+            "Loading",
+            "Please wait"
+        ).also { it.show(parentFragmentManager, CustomProgressDialog.TAG) }
+        progressDialog?.isCancelable = false
+    }
+
+    private fun hideProgressDialog() {
+        progressDialog?.dismiss()
+        progressDialog = null
+    }
+
+    private inner class SurveyWebViewClient : WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            hideProgressDialog()
+            super.onPageFinished(view, url)
         }
     }
 }
