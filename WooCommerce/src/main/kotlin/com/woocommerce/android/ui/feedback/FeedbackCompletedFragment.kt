aabc8a80263fc9f09f4efb60a1eaa412205f0379
Thomaz Cortez <thomazfcortez@gmail.com>
2020-08-18 02:40:42 -0300

    Make FeedbackCompletedFragment content text clickable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackCompletedFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackCompletedFragment.kt
@@ -1,12 +1,19 @@
 package com.woocommerce.android.ui.feedback
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.ui.main.MainActivity
+import com.woocommerce.android.widgets.WooClickableSpan
+import kotlinx.android.synthetic.main.fragment_feedback_completed.*
 
 class FeedbackCompletedFragment : androidx.fragment.app.Fragment() {
     companion object {
@@ -22,8 +29,48 @@ class FeedbackCompletedFragment : androidx.fragment.app.Fragment() {
         AnalyticsTracker.trackViewShown(this)
 
         activity?.let {
+            it.invalidateOptionsMenu()
             it.title = getString(R.string.feedback_completed_title)
             (it as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_gridicons_cross_white_24dp)
         }
+
+        val contactUsText = getString(R.string.feedback_completed_contact_us)
+        getString(R.string.feedback_completed_description, contactUsText)
+            .configureStringClick(
+                clickableContent = contactUsText,
+                clickAction = WooClickableSpan {
+                    context?.let { (activity as? MainActivity)?.showHelpAndSupport() }
+                })
+
+        btn_back_to_store.setOnClickListener { activity?.onBackPressed() }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        activity?.invalidateOptionsMenu()
+    }
+
+    private fun String.configureStringClick(clickableContent: String, clickAction: WooClickableSpan) {
+        SpannableString(this)
+            .buildClickableUrlSpan(clickableContent, this, clickAction)
+            .let {
+                completion_help_guide.apply {
+                    setText(it, TextView.BufferType.SPANNABLE)
+                    movementMethod = LinkMovementMethod.getInstance()
+                }
+            }
+    }
+
+    private fun SpannableString.buildClickableUrlSpan(
+        clickableContent: String,
+        fullContent: String,
+        clickAction: WooClickableSpan
+    ) = apply {
+        setSpan(
+            clickAction,
+            (fullContent.length - clickableContent.length),
+            fullContent.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 }
