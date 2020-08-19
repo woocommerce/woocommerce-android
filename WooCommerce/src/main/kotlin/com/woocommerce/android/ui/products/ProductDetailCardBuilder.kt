e7aa0ec2cb8ca9e8d5053d0333f650839ed0f5b3
Ondrej Ruttkay <ondrej@automattic.com>
2020-08-19 17:36:45 +0200

    Fix the reviews property display value

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailCardBuilder.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailCardBuilder.kt
@@ -458,7 +458,7 @@ class ProductDetailCardBuilder(
             RatingBar(
                 R.string.product_reviews,
                 resources.getString(R.string.product_reviews_count, ratingCount),
-                ratingCount.toFloat(),
+                this.averageRating,
                 R.drawable.ic_reviews
             ) {
                 viewModel.onEditProductCardClicked(
