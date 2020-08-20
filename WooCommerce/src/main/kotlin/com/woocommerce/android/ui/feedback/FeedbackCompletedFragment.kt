5de36b017f217bb62560586389e60a3f60951ffc
Thomaz Cortez <thomazfcortez@gmail.com>
2020-08-20 18:11:31 -0300

    Remove unused import from FeedbackCompletedFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackCompletedFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/FeedbackCompletedFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.extensions.configureToolbarWithCloseButton
