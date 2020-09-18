738c03574ee8e69dcad16a9f2603c893484dcd22
Thomaz Cortez <thomazfcortez@gmail.com>
2020-09-18 11:36:11 -0300

    Delete APP_FEEDBACK Feature flag

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -10,14 +10,13 @@ import com.woocommerce.android.BuildConfig
 enum class FeatureFlag {
     PRODUCT_RELEASE_M3,
     SHIPPING_LABELS_M1,
-    APP_FEEDBACK,
     DB_DOWNGRADE;
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
             // This feature will live switched on from the
             // setting screen. i.e. check AppPrefs.isProductsFeatureEnabled() method
             // Also, turn on the feature during testing
-            APP_FEEDBACK, SHIPPING_LABELS_M1 -> BuildConfig.DEBUG || isTesting()
+            SHIPPING_LABELS_M1 -> BuildConfig.DEBUG || isTesting()
             PRODUCT_RELEASE_M3 -> isTesting() || AppPrefs.isProductsFeatureEnabled()
             DB_DOWNGRADE -> {
                 BuildConfig.DEBUG || context != null && PackageUtils.isBetaBuild(context)
