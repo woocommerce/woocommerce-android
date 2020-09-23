5ed7e10c7eb7aeb069944e24cdf3df1bb474345d
anitaa <murthyanitaa@gmail.com>
2020-09-23 12:01:36 +0530

    Added UI to print shipping label

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelFragment.kt
@@ -1,5 +1,20 @@
 package com.woocommerce.android.ui.orders.shippinglabels
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.base.BaseFragment
 
-class PrintShippingLabelFragment : BaseFragment()
+class PrintShippingLabelFragment : BaseFragment() {
+    override fun getFragmentTitle() = getString(R.string.orderdetail_shipping_label_reprint)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_print_shipping_label, container, false)
+    }
+}
