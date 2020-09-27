a2e9f05791130d75403ee4d55eb0b547cea5a99f
anitaa <murthyanitaa@gmail.com>
2020-09-27 19:48:46 +0530

    Added UI to print shipping labels info screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelInfoFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelInfoFragment.kt
@@ -1,5 +1,19 @@
 package com.woocommerce.android.ui.orders.shippinglabels
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.woocommerce.android.R
 
-class PrintShippingLabelInfoFragment : Fragment()
+class PrintShippingLabelInfoFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_print_shipping_label_info, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.title = getString(R.string.print_shipping_label_info_title)
+    }
+}
