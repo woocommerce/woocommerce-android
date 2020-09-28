a0a382e4b220b6fd9ebd72af3f87db0f3dd3e451
Ondrej Ruttkay <ondrej@automattic.com>
2020-09-28 18:37:05 +0200

    Prevent navigation result LiveData from calling the handler multiple times

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
@@ -3,17 +3,21 @@ package com.woocommerce.android.extensions
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import java.util.concurrent.atomic.AtomicBoolean
 
 fun <T> Fragment.handleResult(key: String, handler: (T) -> Unit) {
-    findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<T>(key)?.observe(
+    findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Pair<T, AtomicBoolean>>(key)?.observe(
         this.viewLifecycleOwner,
         Observer {
-            handler(it)
+            val isFresh = it.second.getAndSet(false)
+            if (isFresh) {
+                handler(it.first)
+            }
         }
     )
 }
 
 fun <T> Fragment.navigateBackWithResult(key: String, result: T) {
-    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, result)
+    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, Pair(result, AtomicBoolean(true)))
     findNavController().navigateUp()
 }
