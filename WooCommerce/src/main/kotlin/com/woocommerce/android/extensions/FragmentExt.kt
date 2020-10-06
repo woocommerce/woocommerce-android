d29964e4eba83f83c119aaa4210b897821d32a83
Ondrej Ruttkay <ondrej@automattic.com>
2020-10-06 12:28:41 +0200

    Update the navigation result comments to better explain the usage

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import java.util.concurrent.atomic.AtomicBoolean
 
-/*
+/**
  * A helper function that sets the submitted key-value pair in the Fragment's SavedStateHandle. The value can be
- * observed as a LiveData using the same key - see the Fragment.handleResult() extension function.
+ * observed as a LiveData using the same key - see the Fragment.handleResult() extension function. This mechanism is
+ * used to facilitate the request-result communication between 2 separate fragments.
  *
- * This mechanism is used to facilitate the request-result communication between 2 separate fragments.
+ * @param [key] A unique string that is the same as the one used in [handleResult]
+ * @param [result] A result value to be returned
  *
  * Note: The value is stored along with a boolean value (true), which signifies that the data is fresh and has not been
  * observed yet. AtomicBoolean type is used to store the boolean so that the value can be updated once once the data is
@@ -20,9 +22,14 @@ fun <T> Fragment.navigateBackWithResult(key: String, result: T) {
     findNavController().navigateUp()
 }
 
-/*
+/**
  * A helper function that subscribes a supplied handler function to the Fragment's SavedStateHandle LiveData associated
  * with the supplied key.
+ *
+ * @param [key] A unique string that is the same as the one used in [navigateBackWithResult]
+ * @param [entryId] An optional ID to identify the correct back stack entry. It's required when calling [handleResult]
+ *  from TopLevelFragment or Dialog (otherwise the result will get lost upon configuration change)
+ * @param [handler] A result handler
  *
  * Note: The handler is called only if the value wasn't handled before (i.e. the data is fresh). Once the observer is
  * called, the boolean value is updated. This puts a limit on the number of observers for a particular key-result pair
