6d9f053c3a52956daf6af31e4f5f116f12520e26
anitaa <murthyanitaa@gmail.com>
2020-10-16 10:23:51 +0530

    Renamed products WIP notice card to FeatureWIPNoticeCard

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/FeatureWIPNoticeCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/FeatureWIPNoticeCard.kt
@@ -12,15 +12,15 @@ import com.woocommerce.android.analytics.AnalyticsTracker.Companion.VALUE_PRODUC
 import com.woocommerce.android.analytics.AnalyticsTracker.Stat.FEATURE_FEEDBACK_BANNER
 import com.woocommerce.android.model.FeatureFeedbackSettings.Feature.PRODUCTS_M3
 import com.woocommerce.android.util.WooAnimUtils
-import kotlinx.android.synthetic.main.products_wip_notice.view.*
+import kotlinx.android.synthetic.main.feature_wip_notice.view.*
 
-class ProductsWIPNoticeCard @JvmOverloads constructor(
+class FeatureWIPNoticeCard @JvmOverloads constructor(
     ctx: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : MaterialCardView(ctx, attrs, defStyleAttr) {
     init {
-        View.inflate(context, R.layout.products_wip_notice, this)
+        View.inflate(context, R.layout.feature_wip_notice, this)
     }
 
     val wipFeatureType
