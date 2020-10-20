dbebbf8d36eae5e0f375a248eba6d5126a9f9692
Thomaz Cortez <thomazfcortez@gmail.com>
2020-10-20 09:19:47 -0300

    Add milestone tracking as a custom tag for the CrowdSignal Survey

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/SurveyType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/SurveyType.kt
@@ -3,11 +3,16 @@ package com.woocommerce.android.ui.feedback
 import com.woocommerce.android.AppUrls
 import com.woocommerce.android.AppUrls.CROWDSIGNAL_PLATFORM_TAG
 
-enum class SurveyType(private val untagedUrl: String) {
-    PRODUCT(AppUrls.CROWDSIGNAL_PRODUCT_SURVEY),
-    SHIPPING_LABELS(AppUrls.CROWDSIGNAL_SHIPPING_LABELS_SURVEY),
+enum class SurveyType(private val untagedUrl: String, private val milestone: Int? = null) {
+    PRODUCT(AppUrls.CROWDSIGNAL_PRODUCT_SURVEY, 4),
+    SHIPPING_LABELS(AppUrls.CROWDSIGNAL_SHIPPING_LABELS_SURVEY, 1),
     MAIN(AppUrls.CROWDSIGNAL_MAIN_SURVEY);
 
     val url
-        get() = "$untagedUrl?$CROWDSIGNAL_PLATFORM_TAG"
+        get() = "$untagedUrl?$CROWDSIGNAL_PLATFORM_TAG$milestoneTag"
+
+    private val milestoneTag
+        get() = milestone?.let {
+            "&milestone=$it"
+        } ?: ""
 }
--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/feedback/SurveyTypeTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/feedback/SurveyTypeTest.kt
@@ -1,5 +1,8 @@
 package com.woocommerce.android.ui.feedback
 
+import com.woocommerce.android.ui.feedback.SurveyType.MAIN
+import com.woocommerce.android.ui.feedback.SurveyType.PRODUCT
+import com.woocommerce.android.ui.feedback.SurveyType.SHIPPING_LABELS
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -9,5 +12,20 @@ class SurveyTypeTest {
         SurveyType.values().forEach {
             assertThat(it.url.contains("woo-mobile-platform=android")).isTrue()
         }
+    }
+
+    @Test
+    fun `Product SurveyType url should include a milestone tag`() {
+        assertThat(PRODUCT.url.contains(Regex("milestone=\\d(?!\\S)"))).isTrue()
+    }
+
+    @Test
+    fun `ShippingLabels SurveyType url should include a milestone tag`() {
+        assertThat(SHIPPING_LABELS.url.contains(Regex("milestone=\\d(?!\\S)"))).isTrue()
+    }
+
+    @Test
+    fun `Main SurveyType url should NOT include a milestone tag`() {
+        assertThat(MAIN.url.contains(Regex("milestone=\\d(?!\\S)"))).isFalse()
     }
 }
