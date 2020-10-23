8ad912768af0851f6045e4bcb73a78835675d228
Nick Bradbury <nick.bradbury@gmail.com>
2020-10-23 07:53:03 -0400

    Resolved conflicts

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/LinkedProductsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/LinkedProductsFragment.kt
@@ -11,8 +11,12 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.extensions.handleResult
 import com.woocommerce.android.extensions.hide
+import com.woocommerce.android.extensions.navigateSafely
 import com.woocommerce.android.extensions.show
+import com.woocommerce.android.ui.products.GroupedProductListType.CROSS_SELLS
+import com.woocommerce.android.ui.products.GroupedProductListType.UPSELLS
 import com.woocommerce.android.ui.products.ProductDetailViewModel.ProductExitEvent.ExitLinkedProducts
 import kotlinx.android.synthetic.main.fragment_linked_products.*
 import org.wordpress.android.util.ActivityUtils
@@ -54,6 +58,7 @@ class LinkedProductsFragment : BaseProductFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupObservers()
+        updateProductView()
     }
 
     private fun setupObservers() {
@@ -63,7 +68,18 @@ class LinkedProductsFragment : BaseProductFragment() {
                 else -> event.isHandled = false
             }
         })
-        updateProductView()
+
+        handleResult<List<Long>>(GroupedProductListFragment.KEY_UPSELL_PRODUCT_IDS_RESULT) {
+            viewModel.updateProductDraft(upsellProductIds = it)
+            changesMade()
+            updateProductView()
+        }
+
+        handleResult<List<Long>>(GroupedProductListFragment.KEY_CROSS_SELL_PRODUCT_IDS_RESULT) {
+            viewModel.updateProductDraft(crossSellProductIds = it)
+            changesMade()
+            updateProductView()
+        }
     }
 
     private fun updateProductView() {
@@ -82,13 +98,35 @@ class LinkedProductsFragment : BaseProductFragment() {
         val numCrossSells = viewModel.getProduct().productDraft?.crossSellProductIds?.size ?: 0
         if (numCrossSells > 0) {
             cross_sells_count.text = resources.getQuantityString(R.plurals.product_count, numCrossSells, numCrossSells)
-            upsells_count.show()
+            cross_sells_count.show()
             add_cross_sell_products.text = getString(R.string.edit_products_button)
         } else {
             cross_sells_count.hide()
             add_cross_sell_products.text = getString(R.string.add_products_button)
         }
+
+        add_upsell_products.setOnClickListener {
+            showGroupedProductFragment(UPSELLS)
+        }
+
+        add_cross_sell_products.setOnClickListener {
+            showGroupedProductFragment(CROSS_SELLS)
+        }
     }
 
     override fun onRequestAllowBackPress() = viewModel.onBackButtonClicked(ExitLinkedProducts())
+
+    private fun showGroupedProductFragment(groupedProductType: GroupedProductListType) {
+        val productIds = when (groupedProductType) {
+            UPSELLS -> viewModel.getProduct().productDraft?.upsellProductIds
+            else -> viewModel.getProduct().productDraft?.crossSellProductIds
+        }
+
+        val action = GroupedProductListFragmentDirections.actionGlobalGroupedProductListFragment(
+            viewModel.getRemoteProductId(),
+            productIds?.joinToString(",") ?: "",
+            groupedProductType.ordinal
+        )
+        findNavController().navigateSafely(action)
+    }
 }
