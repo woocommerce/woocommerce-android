a1f9124618c14323086b74aa98fc2138faf559a4
Nick Bradbury <nick.bradbury@gmail.com>
2020-11-05 13:14:44 -0500

    Prefixed the resultKey with "key_"

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/GroupedProductListType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/GroupedProductListType.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StringRes
 import com.woocommerce.android.R
 
 enum class GroupedProductListType(@StringRes val titleId: Int, val resultKey: String) {
-    GROUPED(R.string.grouped_products, "grouped"),
-    UPSELLS(R.string.upsells_label, "upsells"),
-    CROSS_SELLS(R.string.cross_sells_label, "cross-sells");
+    GROUPED(R.string.grouped_products, "key_grouped"),
+    UPSELLS(R.string.upsells_label, "key_upsells"),
+    CROSS_SELLS(R.string.cross_sells_label, "key_cross-sells");
 }
