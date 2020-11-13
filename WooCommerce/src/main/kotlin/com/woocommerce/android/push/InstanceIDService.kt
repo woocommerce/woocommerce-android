7f030aa4bb73636dd4766974ba13e4103122cb22
Ondrej Ruttkay <ondrej@automattic.com>
2020-11-13 16:14:48 +0100

    Fix the token Firebase refresh after library upgrade

--- WooCommerce/src/main/kotlin/com/woocommerce/android/push/InstanceIDService.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/push/InstanceIDService.kt
@@ -1,16 +1,17 @@
 package com.woocommerce.android.push
 
-import com.google.firebase.iid.FirebaseInstanceIdService
+import com.google.firebase.messaging.FirebaseMessagingService
 
-class InstanceIDService : FirebaseInstanceIdService() {
+class InstanceIDService : FirebaseMessagingService() {
     /**
      * Called if InstanceID token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is also called
      * when the InstanceID token is initially generated, so this is where
      * you retrieve the token.
      */
-    override fun onTokenRefresh() {
-        // Register for Cloud messaging
+    // [START refresh_token]
+    override fun onNewToken(s: String) {
+        super.onNewToken(s)
         FCMRegistrationIntentService.enqueueWork(this)
     }
 }
