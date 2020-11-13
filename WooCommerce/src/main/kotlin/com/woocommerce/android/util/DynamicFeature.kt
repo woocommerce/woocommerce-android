df6c65495c8aab7ff163679ecd6e23435b4e4734
Ondrej Ruttkay <ondrej@automattic.com>
2020-11-13 16:13:39 +0100

    Add dynamic module installation listener

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/DynamicFeature.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/DynamicFeature.kt
@@ -4,19 +4,37 @@ import android.content.Context
 import android.util.Log
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 import com.google.android.play.core.splitinstall.SplitInstallRequest
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 import javax.inject.Inject
 
 class DynamicFeature @Inject constructor(private val appContext: Context) {
     fun installModule(moduleName: String) {
         appContext.applicationContext?.let { context ->
-            val splitInstallManager = SplitInstallManagerFactory.create(context)
+            val splitInstallManager = SplitInstallManagerFactory.create(
+                context
+//                ,
+//                context.getExternalFilesDir("local_testing")
+            )
             val request = SplitInstallRequest.newBuilder()
                 .addModule(moduleName)
                 .build()
 
+            var sessionId: Int = 0
+            val listener = SplitInstallStateUpdatedListener { splitInstallSessionState ->
+                if (splitInstallSessionState.sessionId() == sessionId) {
+                    when (splitInstallSessionState.status()) {
+                        SplitInstallSessionStatus.INSTALLED -> {
+                            Log.d("WooCommerceApp", "Dynamic Module downloaded")
+                        }
+                    }
+                }
+            }
+
             splitInstallManager.startInstall(request)
                 .addOnSuccessListener {
                     Log.d("WooCommerceApp", it.toString())
+                    sessionId = it
                 }
                 .addOnFailureListener {
                     Log.e("WooCommerceApp", it.toString())
