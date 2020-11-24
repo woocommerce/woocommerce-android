ff621c5448e1d99e4237b0527b6f0e4083a8fff5
Nick Bradbury <nick.bradbury@gmail.com>
2020-11-24 14:45:26 -0500

    Removed unused constructors

--- WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/DisabledAppBarLayoutBehavior.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/DisabledAppBarLayoutBehavior.kt
@@ -1,7 +1,5 @@
 package com.woocommerce.android.widgets
 
-import android.content.Context
-import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.appbar.AppBarLayout
@@ -9,19 +7,21 @@ import com.google.android.material.appbar.AppBarLayout
 /**
  * Custom layout behavior which prevents collapsing toolbar from being expanded
  */
-class DisabledAppBarLayoutBehavior : AppBarLayout.Behavior {
+class DisabledAppBarLayoutBehavior() : AppBarLayout.Behavior() {
+    /**
+     * Prevent the toolbar from being dragged in child fragments
+     */
     init {
-        // prevent the toolbar from being dragged in child fragments
         setDragCallback(object : AppBarLayout.Behavior.DragCallback() {
             override fun canDrag(appBarLayout: AppBarLayout): Boolean {
                 return false
             }
         })
     }
-    constructor() : super() {}
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
 
-    // prevent nested scroll
+    /**
+     * Prevent nested scroll
+     */
     override fun onStartNestedScroll(
         parent: CoordinatorLayout,
         child: AppBarLayout,
