a5c55bf4bde16fedd0b631b347fca23b8528dbc5
Ondrej Ruttkay <ondrej@automattic.com>
2020-11-26 20:50:14 +0100

    Add observers to the ViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/CreateShippingLabelFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/CreateShippingLabelFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
+import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.base.UIMessageResolver
+import com.woocommerce.android.ui.orders.shippinglabels.creation.CreateShippingLabelViewModel.Step
 import com.woocommerce.android.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.fragment_create_shipping_label.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
@@ -27,5 +30,35 @@ class CreateShippingLabelFragment  : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_create_shipping_label, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.viewStateData.observe(viewLifecycleOwner) { old, new ->
+            new.originAddressStep?.takeIfNotEqualTo(old?.originAddressStep) {
+                originStep.update(it)
+            }
+            new.shippingAddressStep?.takeIfNotEqualTo(old?.shippingAddressStep) {
+                shippingStep.update(it)
+            }
+            new.packagingDetailsStep?.takeIfNotEqualTo(old?.packagingDetailsStep) {
+                packagingStep.update(it)
+            }
+        }
+
+        originStep.continueButtonClickListener = viewModel::onValidateOriginButtonTapped
+        originStep.editButtonClickListener = viewModel::onEditOriginAddressButtonTapped
+        shippingStep.continueButtonClickListener = viewModel::onValidateShippingButtonTapped
+        shippingStep.editButtonClickListener = viewModel::onEditShippingAddressButtonTapped
+        packagingStep.continueButtonClickListener = viewModel::onContinueToCarrierButtonTapped
+        packagingStep.editButtonClickListener = viewModel::onEditPackagingButtonTapped
+    }
+
+    private fun ShippingLabelCreationStepView.update(data: Step) {
+        details = data.details
+        isViewEnabled = data.isEnabled
+        isContinueButtonVisible = data.isContinueButtonVisible
+        isEditButtonVisible = data.isEditButtonVisible
     }
 }
