286cdac19b2b47a5fa5f704f8b8b41978e9f081a
Nick Bradbury <nick.bradbury@gmail.com>
2020-11-30 16:53:48 -0500

    Fixed incomplete conversion of WCSettingsToggleOptionView

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/WCSettingsToggleOptionView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/WCSettingsToggleOptionView.kt
@@ -17,7 +17,6 @@ import com.woocommerce.android.R
 import com.woocommerce.android.databinding.ViewSettingsToggleOptionBinding
 import com.woocommerce.android.util.StyleAttrUtils
 import com.woocommerce.android.util.UiHelpers
-import kotlinx.android.synthetic.main.view_settings_toggle_option.view.*
 
 class WCSettingsToggleOptionView @JvmOverloads constructor(
     ctx: Context,
@@ -46,7 +45,7 @@ class WCSettingsToggleOptionView @JvmOverloads constructor(
                         isInEditMode,
                         R.styleable.WCSettingsToggleOptionView_toggleOptionTitle,
                         R.styleable.WCSettingsToggleOptionView_tools_toggleOptionTitle
-                ).let { UiHelpers.setTextOrHide(toggleSetting_title, it) }
+                ).let { UiHelpers.setTextOrHide(binding.toggleSettingTitle, it) }
 
                 // Set the view description if it exists, or hide
                 StyleAttrUtils.getString(
@@ -54,7 +53,7 @@ class WCSettingsToggleOptionView @JvmOverloads constructor(
                         isInEditMode,
                         R.styleable.WCSettingsToggleOptionView_toggleOptionDesc,
                         R.styleable.WCSettingsToggleOptionView_tools_toggleOptionDesc
-                ).let { UiHelpers.setTextOrHide(toggleSetting_desc, it) }
+                ).let { UiHelpers.setTextOrHide(binding.toggleSettingDesc, it) }
 
                 // Set the view icon if exists, or hide
                 StyleAttrUtils.getResourceId(
@@ -62,10 +61,10 @@ class WCSettingsToggleOptionView @JvmOverloads constructor(
                         isInEditMode,
                         R.styleable.WCSettingsToggleOptionView_toggleOptionIcon,
                         R.styleable.WCSettingsToggleOptionView_tools_toggleOptionIcon
-                ).let { UiHelpers.setImageOrHide(toggleSetting_icon, it, setInvisible = true) }
+                ).let { UiHelpers.setImageOrHide(binding.toggleSettingIcon, it, setInvisible = true) }
 
                 // Set the view checked state
-                toggleSetting_switch.isChecked = StyleAttrUtils.getBoolean(
+                binding.toggleSettingSwitch.isChecked = StyleAttrUtils.getBoolean(
                         a,
                         isInEditMode,
                         R.styleable.WCSettingsToggleOptionView_toggleOptionChecked,
@@ -85,20 +84,20 @@ class WCSettingsToggleOptionView @JvmOverloads constructor(
     }
 
     var title: String?
-        get() = toggleSetting_title.text.toString()
-        set(value) { UiHelpers.setTextOrHide(toggleSetting_title, value) }
+        get() = binding.toggleSettingTitle.text.toString()
+        set(value) { UiHelpers.setTextOrHide(binding.toggleSettingTitle, value) }
 
     var description: String?
-        get() = toggleSetting_desc.text.toString()
-        set(value) { UiHelpers.setTextOrHide(toggleSetting_desc, value) }
+        get() = binding.toggleSettingDesc.text.toString()
+        set(value) { UiHelpers.setTextOrHide(binding.toggleSettingDesc, value) }
 
     var iconImageResource: Int? = null
-        set(value) { UiHelpers.setImageOrHide(toggleSetting_icon, value) }
+        set(value) { UiHelpers.setImageOrHide(binding.toggleSettingIcon, value) }
 
     var iconDrawable: Drawable? = null
-        set(value) { UiHelpers.setDrawableOrHide(toggleSetting_icon, value) }
+        set(value) { UiHelpers.setDrawableOrHide(binding.toggleSettingIcon, value) }
 
-    private val checkable: CompoundButton by lazy { toggleSetting_switch }
+    private val checkable: CompoundButton by lazy { binding.toggleSettingSwitch }
     var listener: OnCheckedChangeListener? = null
 
     override fun isChecked() = checkable.isChecked
