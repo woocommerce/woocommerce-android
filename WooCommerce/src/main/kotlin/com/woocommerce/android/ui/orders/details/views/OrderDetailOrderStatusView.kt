f54598055325f13f777a055edfbc10353b646c20
Nick Bradbury <nick.bradbury@gmail.com>
2020-12-08 15:10:52 -0500

    Converted OrderDetailOrderStatusView to ViewBinding

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailOrderStatusView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailOrderStatusView.kt
@@ -3,9 +3,11 @@ package com.woocommerce.android.ui.orders.details.views
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import com.google.android.material.card.MaterialCardView
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.OrderDetailOrderStatusBinding
 import com.woocommerce.android.extensions.getMediumDate
 import com.woocommerce.android.extensions.getTimeString
 import com.woocommerce.android.extensions.isToday
@@ -13,21 +15,18 @@ import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.Order.OrderStatus
 import com.woocommerce.android.ui.orders.OrderStatusTag
 import com.woocommerce.android.widgets.tags.TagView
-import kotlinx.android.synthetic.main.order_detail_order_status.view.*
 
 class OrderDetailOrderStatusView @JvmOverloads constructor(
     ctx: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : MaterialCardView(ctx, attrs, defStyleAttr) {
-    init {
-        View.inflate(context, R.layout.order_detail_order_status, this)
-    }
+    private val binding = OrderDetailOrderStatusBinding.inflate(LayoutInflater.from(ctx), this)
 
     fun updateStatus(orderStatus: OrderStatus, onTap: ((view: View) -> Unit)) {
-        orderStatus_orderTags.removeAllViews()
-        orderStatus_orderTags.addView(getTagView(orderStatus))
-        orderStatus_edit.setOnClickListener(onTap)
+        binding.orderStatusOrderTags.removeAllViews()
+        binding.orderStatusOrderTags.addView(getTagView(orderStatus))
+        binding.orderStatusEdit.setOnClickListener(onTap)
     }
 
     fun updateOrder(order: Order) {
@@ -36,13 +35,13 @@ class OrderDetailOrderStatusView @JvmOverloads constructor(
         } else {
             order.dateCreated.getMediumDate(context)
         }
-        orderStatus_dateAndOrderNum.text = context.getString(
+        binding.orderStatusDateAndOrderNum.text = context.getString(
             R.string.orderdetail_orderstatus_date_and_ordernum,
             dateStr,
             order.number
         )
 
-        orderStatus_name.text = order.getBillingName(context.getString(R.string.orderdetail_customer_name_default))
+        binding.orderStatusName.text = order.getBillingName(context.getString(R.string.orderdetail_customer_name_default))
     }
 
     private fun getTagView(orderStatus: OrderStatus): TagView {
