7b0c4d47334d319832f5f213557a05178179df16
Hicham Boushaba <hicham.boushaba@gmail.com>
2020-12-08 15:36:48 +0100

    Complete the fragment/viewmodel implementation keeping the prev behavior

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/tracking/AddOrderShipmentTrackingFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/tracking/AddOrderShipmentTrackingFragment.kt
@@ -1,7 +1,6 @@
 package com.woocommerce.android.ui.orders.tracking
 
 import android.app.DatePickerDialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -11,42 +10,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.woocommerce.android.AppPrefs
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
-import com.woocommerce.android.analytics.AnalyticsTracker.Stat.ORDER_SHIPMENT_TRACKING_ADD_BUTTON_TAPPED
 import com.woocommerce.android.databinding.FragmentAddShipmentTrackingBinding
 import com.woocommerce.android.extensions.navigateBackWithResult
 import com.woocommerce.android.extensions.takeIfNotEqualTo
-import com.woocommerce.android.model.OrderShipmentTracking
 import com.woocommerce.android.tools.NetworkStatus
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.base.UIMessageResolver
 import com.woocommerce.android.ui.dialog.WooDialog
 import com.woocommerce.android.ui.main.MainActivity.Companion.BackPressListener
 import com.woocommerce.android.util.DateUtils
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ExitWithResult
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowDialog
 import com.woocommerce.android.viewmodel.ViewModelFactory
-import com.woocommerce.android.widgets.AppRatingDialog
-import kotlinx.android.synthetic.main.fragment_add_shipment_tracking.*
-import org.wordpress.android.fluxc.model.order.OrderIdentifier
 import org.wordpress.android.util.ActivityUtils
 import java.util.Calendar
 import javax.inject.Inject
 import org.wordpress.android.fluxc.utils.DateUtils as FluxCDateUtils
 
 class AddOrderShipmentTrackingFragment : BaseFragment(R.layout.fragment_add_shipment_tracking),
-    AddOrderShipmentTrackingContract.View,
     AddOrderTrackingProviderActionListener,
     BackPressListener {
     companion object {
-        const val FIELD_ORDER_TRACKING_NUMBER = "order-tracking-number"
-        const val FIELD_ORDER_TRACKING_DATE_SHIPPED = "order-tracking-date-shipped"
-        const val FIELD_ORDER_TRACKING_PROVIDER = "order-tracking-provider"
-        const val FIELD_IS_CONFIRMING_DISCARD = "is-confirming-discard"
-        const val FIELD_IS_CUSTOM_PROVIDER = "is-custom-provider"
-        const val FIELD_ORDER_TRACKING_CUSTOM_PROVIDER_NAME = "order-tracking-custom-provider-name"
-        const val FIELD_ORDER_TRACKING_CUSTOM_PROVIDER_URL = "order-tracking-custom-provider-url"
         const val KEY_ADD_SHIPMENT_TRACKING_RESULT = "key_add_shipment_tracking_result"
     }
 
@@ -56,10 +46,7 @@ class AddOrderShipmentTrackingFragment : BaseFragment(R.layout.fragment_add_ship
 
     private val viewModel: AddOrderShipmentTrackingViewModel by viewModels { viewModelFactory }
 
-    private var isConfirmingDiscard = false
-    private var shouldShowDiscardDialog = true
     private var isSelectedProviderCustom = false
-    private lateinit var orderId: OrderIdentifier
     private var dateShippedPickerDialog: DatePickerDialog? = null
     private var providerListPickerDialog: AddOrderTrackingProviderListFragment? = null
 
@@ -77,45 +64,6 @@ class AddOrderShipmentTrackingFragment : BaseFragment(R.layout.fragment_add_ship
 
     override fun getFragmentTitle() = getString(R.string.order_shipment_tracking_toolbar_title)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        orderId = navArgs.orderId
-
-        if (savedInstanceState != null) {
-            isSelectedProviderCustom = savedInstanceState.getBoolean(FIELD_IS_CUSTOM_PROVIDER, false)
-            addTracking_number.setText(savedInstanceState.getString(FIELD_ORDER_TRACKING_NUMBER, ""))
-            addTracking_date.setText(savedInstanceState.getString(FIELD_ORDER_TRACKING_DATE_SHIPPED, ""))
-            addTracking_custom_provider_name.setText(
-                savedInstanceState.getString(FIELD_ORDER_TRACKING_CUSTOM_PROVIDER_NAME, "")
-            )
-            addTracking_custom_provider_url.setText(
-                savedInstanceState.getString(FIELD_ORDER_TRACKING_CUSTOM_PROVIDER_URL, "")
-            )
-            if (savedInstanceState.getBoolean(FIELD_IS_CONFIRMING_DISCARD)) {
-                confirmDiscard()
-            }
-        } else {
-            isSelectedProviderCustom = navArgs.isCustomProvider
-            val dateShipped = FluxCDateUtils.getCurrentDateString()
-            displayFormatDateShippedText(dateShipped)
-        }
-
-        val selectedCarrierName = savedInstanceState?.let {
-            savedInstanceState.getString(FIELD_ORDER_TRACKING_PROVIDER)
-        } ?: navArgs.orderTrackingProvider
-        if (isCustomProvider()) {
-            addTracking_custom_provider_name.setText(selectedCarrierName)
-            addTracking_editCarrier.setText(getString(R.string.order_shipment_tracking_custom_provider_section_name))
-            showCustomProviderFields()
-        } else {
-            addTracking_editCarrier.setText(selectedCarrierName)
-            hideCustomProviderFields()
-        }
-
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -126,46 +74,69 @@ class AddOrderShipmentTrackingFragment : BaseFragment(R.layout.fragment_add_ship
 
     private fun setupObservers(binding: FragmentAddShipmentTrackingBinding) {
         viewModel.addOrderShipmentTrackingViewStateData.observe(viewLifecycleOwner) { old, new ->
+            //Carrier data
             new.carrier.takeIfNotEqualTo(old?.carrier) {
                 if (it.isCustom) {
-                    showCustomProviderFields()
-                    binding.addTrackingEditCarrier.setText(getString(R.string.order_shipment_tracking_custom_provider_section_name))
-                    if (binding.addTrackingCustomProviderName.text.toString() != it.name) {
-                        binding.addTrackingCustomProviderName.setText(it.name)
+                    showCustomProviderFields(binding)
+                    binding.carrier.setText(getString(R.string.order_shipment_tracking_custom_provider_section_name))
+                    if (binding.customProviderName.text.toString() != it.name) {
+                        binding.customProviderName.setText(it.name)
                     }
                 } else {
-                    hideCustomProviderFields()
-                    binding.addTrackingEditCarrier.setText(it.name)
+                    hideCustomProviderFields(binding)
+                    binding.carrier.setText(it.name)
                 }
             }
+            new.carrierError.takeIfNotEqualTo(old?.carrierError) { error ->
+                binding.carrierLayout.error = error?.let { getString(it) }
+            }
+            new.customCarrierNameError.takeIfNotEqualTo(old?.customCarrierNameError) { error ->
+                binding.customProviderNameLayout.error = error?.let { getString(it) }
+            }
+
+            //tracking number
             new.trackingNumber.takeIfNotEqualTo(old?.trackingNumber) {
-                if (binding.addTrackingNumber.text.toString() != it) {
-                    binding.addTrackingNumber.setText(it)
+                if (binding.trackingNumber.text.toString() != it) {
+                    binding.trackingNumber.setText(it)
                 }
             }
+            new.trackingNumberError.takeIfNotEqualTo(old?.trackingNumberError) { error ->
+                binding.trackingNumberLayout.error = error?.let { getString(it) }
+            }
+
+            //custom URL
             new.trackingLink.takeIfNotEqualTo(old?.trackingLink) {
-                if (binding.addTrackingCustomProviderUrl.text.toString() != it) {
-                    binding.addTrackingCustomProviderUrl.setText(it)
+                if (binding.customProviderUrl.text.toString() != it) {
+                    binding.customProviderUrl.setText(it)
                 }
             }
+
             new.date.takeIfNotEqualTo(old?.date) {
-                binding.addTrackingDate.setText(DateUtils().getLocalizedLongDateString(requireActivity(), it))
+                binding.date.setText(DateUtils().getLocalizedLongDateString(requireActivity(), it))
+            }
+        }
+        viewModel.event.observe(viewLifecycleOwner) {event ->
+            when(event){
+                is ShowDialog -> event.showDialog()
+                is Exit -> findNavController().navigateUp()
+                is ExitWithResult<*> -> navigateBackWithResult(KEY_ADD_SHIPMENT_TRACKING_RESULT, event.data)
+                else -> event.isHandled = false
             }
         }
     }
 
     private fun initUi(binding: FragmentAddShipmentTrackingBinding) {
-        binding.addTrackingEditCarrier.setOnClickListener {
+        binding.carrier.setOnClickListener {
             providerListPickerDialog = AddOrderTrackingProviderListFragment
                 .newInstance(
                     listener = this,
-                    selectedProviderText = binding.addTrackingEditCarrier.text.toString(),
-                    orderIdentifier = orderId
+                    selectedProviderText = binding.carrier.text.toString(),
+                    orderIdentifier = viewModel.orderId
                 )
                 .also { it.show(parentFragmentManager, AddOrderTrackingProviderListFragment.TAG) }
         }
 
-        binding.addTrackingDate.setOnClickListener {
+        binding.date.setOnClickListener {
             val calendar = FluxCDateUtils.getCalendarInstance(viewModel.currentSelectedDate)
             dateShippedPickerDialog = DatePickerDialog(
                 requireActivity(),
@@ -177,13 +148,13 @@ class AddOrderShipmentTrackingFragment : BaseFragment(R.layout.fragment_add_ship
             dateShippedPickerDialog?.show()
         }
 
-        binding.addTrackingCustomProviderName.doOnTextChanged { text, _, _, _ ->
+        binding.customProviderName.doOnTextChanged { text, _, _, _ ->
             viewModel.onCustomCarrierNameEntered(text.toString())
         }
-        binding.addTrackingNumber.doOnTextChanged { text, _, _, _ ->
+        binding.trackingNumber.doOnTextChanged { text, _, _, _ ->
             viewModel.onTrackingNumberEntered(text.toString())
         }
-        binding.addTrackingCustomProviderUrl.doOnTextChanged { text, _, _, _ ->
+        binding.customProviderUrl.doOnTextChanged { text, _, _, _ ->
             viewModel.onTrackingLinkEntered(text.toString())
         }
     }
@@ -218,54 +189,7 @@ class AddOrderShipmentTrackingFragment : BaseFragment(R.layout.fragment_add_ship
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_add -> {
-                if (addTracking_editCarrier.text.toString().isEmpty()) {
-                    addTracking_carrierLayout.error = getString(R.string.order_shipment_tracking_empty_provider)
-                    addTracking_numberLayout.error = null
-                    addTracking_customNameLayout.error = null
-                    return true
-                }
-
-                if (isCustomProvider() && addTracking_custom_provider_name.text.toString().isEmpty()) {
-                    addTracking_number.error = null
-                    addTracking_carrierLayout.error = null
-                    addTracking_custom_provider_name.requestFocus()
-                    addTracking_customNameLayout.error = getString(
-                        R.string.order_shipment_tracking_empty_custom_provider_name
-                    )
-                    return true
-                }
-
-                if (addTracking_number.text.isNullOrEmpty()) {
-                    addTracking_carrierLayout.error = null
-                    addTracking_customNameLayout.error = null
-                    addTracking_number.requestFocus()
-                    addTracking_numberLayout.error = getString(R.string.order_shipment_tracking_empty_tracking_num)
-                    return true
-                }
-
-                AnalyticsTracker.track(ORDER_SHIPMENT_TRACKING_ADD_BUTTON_TAPPED)
-                AppRatingDialog.incrementInteractions()
-
-                val providerText = getProviderText()
-                val trackingNumText = addTracking_number.text.toString()
-                val isCustomProvider = isCustomProvider()
-                val customProviderTrackingUrl = addTracking_custom_provider_url.text.toString()
-
-                AppPrefs.setSelectedShipmentTrackingProviderName(providerText)
-                AppPrefs.setIsSelectedShipmentTrackingProviderNameCustom(isCustomProvider)
-
-                val shipmentTracking = OrderShipmentTracking(
-                    trackingNumber = trackingNumText,
-                    dateShipped = getDateShippedText(),
-                    trackingProvider = providerText,
-                    isCustomProvider = isCustomProvider,
-                    trackingLink = if (isCustomProvider) {
-                        customProviderTrackingUrl
-                    } else ""
-                )
-
-                shouldShowDiscardDialog = false
-                navigateBackWithResult(KEY_ADD_SHIPMENT_TRACKING_RESULT, shipmentTracking)
+                viewModel.onAddButtonTapped()
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -276,127 +200,27 @@ class AddOrderShipmentTrackingFragment : BaseFragment(R.layout.fragment_add_ship
      * Prevent back press in the main activity if the user entered a note so we can confirm the discard
      */
     override fun onRequestAllowBackPress(): Boolean {
-        val providerText = addTracking_editCarrier.text.toString()
-        val trackingNumText = addTracking_number.text.toString()
-        val customProviderText = addTracking_custom_provider_name.text.toString()
-        val customTrackingUrlText = addTracking_custom_provider_url.text.toString()
-        val displayConfirmDialog = providerText.isNotEmpty() || trackingNumText.isNotEmpty() ||
-            (isCustomProvider() && (customProviderText.isNotEmpty() || customTrackingUrlText.isNotEmpty()))
-
-        return if (displayConfirmDialog && shouldShowDiscardDialog) {
-            confirmDiscard()
-            false
-        } else {
-            true
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putBoolean(FIELD_IS_CONFIRMING_DISCARD, isConfirmingDiscard)
-        outState.putBoolean(FIELD_IS_CUSTOM_PROVIDER, isCustomProvider())
-
-        addTracking_number?.let {
-            outState.putString(FIELD_ORDER_TRACKING_NUMBER, it.text.toString())
-        }
-        addTracking_date?.let {
-            outState.putString(FIELD_ORDER_TRACKING_DATE_SHIPPED, it.text.toString())
-        }
-        addTracking_editCarrier?.let {
-            outState.putString(FIELD_ORDER_TRACKING_PROVIDER, it.text.toString())
-        }
-        addTracking_custom_provider_name?.let {
-            outState.putString(FIELD_ORDER_TRACKING_CUSTOM_PROVIDER_NAME, it.text.toString())
-        }
-        addTracking_custom_provider_url?.let {
-            outState.putString(FIELD_ORDER_TRACKING_CUSTOM_PROVIDER_URL, it.text.toString())
-        }
-    }
-
-    override fun confirmDiscard() {
-        isConfirmingDiscard = true
-        WooDialog.showDialog(
-            requireActivity(),
-            messageId = R.string.discard_message,
-            positiveButtonId = R.string.discard,
-            posBtnAction = DialogInterface.OnClickListener { _, _ ->
-                shouldShowDiscardDialog = false
-                activity?.onBackPressed()
-            },
-            negativeButtonId = R.string.keep_editing,
-            negBtnAction = DialogInterface.OnClickListener { _, _ ->
-                isConfirmingDiscard = false
-            })
-    }
-
-    override fun getProviderText(): String {
-        return if (isCustomProvider()) {
-            addTracking_custom_provider_name.text.toString()
-        } else {
-            addTracking_editCarrier.text.toString().trim()
-        }
-    }
-
-    /**
-     * Formats the localized date string and returns it in the format of yyyy-MM-dd
-     * for use with the API.
-     *
-     * example: May 9, 2019 -> 2019-05-09
-     */
-    override fun getDateShippedText(): String {
-        val dateSelected = DateUtils().getDateFromLocalizedLongDateString(
-            requireActivity(),
-            addTracking_date.text.toString()
-        )
-        return DateUtils().getYearMonthDayStringFromDate(dateSelected)
+        return viewModel.onBackButtonPressed()
     }
 
     override fun onTrackingProviderSelected(selectedCarrierName: String) {
-        addTracking_editCarrier.setText(selectedCarrierName)
-        addTracking_editCarrier.error = null
-        addTracking_editCarrier.isFocusableInTouchMode = false
-        addTracking_editCarrier.isFocusable = false
-        isSelectedProviderCustom = addTracking_editCarrier.text.toString() ==
+        isSelectedProviderCustom = selectedCarrierName ==
             getString(R.string.order_shipment_tracking_custom_provider_section_name)
-        // Display custom provider fields only if selectedCarrierName = custom provider
-        if (isCustomProvider()) {
-            showCustomProviderFields()
-        } else {
-            hideCustomProviderFields()
-        }
+
+        val carrier = Carrier(
+            name = if (isSelectedProviderCustom) "" else selectedCarrierName,
+            isCustom = isSelectedProviderCustom
+        )
+        viewModel.onCarrierSelected(carrier)
     }
 
-    override fun isCustomProvider() = isSelectedProviderCustom
-
-    override fun showOfflineSnack() {
-        uiMessageResolver.showOfflineSnack()
+    private fun showCustomProviderFields(binding: FragmentAddShipmentTrackingBinding) {
+        binding.customProviderNameLayout.visibility = View.VISIBLE
+        binding.customProviderUrlLayout.visibility = View.VISIBLE
     }
 
-    override fun showAddAddShipmentTrackingErrorSnack() {
-        uiMessageResolver.getSnack(R.string.order_shipment_tracking_error).show()
-    }
-
-    override fun showAddShipmentTrackingSnack() {
-        uiMessageResolver.getSnack(R.string.order_shipment_tracking_added).show()
-    }
-
-    private fun displayFormatDateShippedText(dateString: String) {
-        context?.let {
-            addTracking_date.setText(
-                DateUtils().getLocalizedLongDateString(
-                    it,
-                    dateString
-                )
-            )
-        }
-    }
-
-    private fun showCustomProviderFields() {
-        addTracking_customNameLayout.visibility = View.VISIBLE
-        addTracking_customUrlLayout.visibility = View.VISIBLE
-    }
-
-    private fun hideCustomProviderFields() {
-        addTracking_customNameLayout.visibility = View.GONE
-        addTracking_customUrlLayout.visibility = View.GONE
+    private fun hideCustomProviderFields(binding: FragmentAddShipmentTrackingBinding) {
+        binding.customProviderNameLayout.visibility = View.GONE
+        binding.customProviderUrlLayout.visibility = View.GONE
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/tracking/AddOrderShipmentTrackingViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/tracking/AddOrderShipmentTrackingViewModel.kt
@@ -1,18 +1,27 @@
 package com.woocommerce.android.ui.orders.tracking
 
+import android.content.DialogInterface
 import android.os.Parcelable
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import com.woocommerce.android.AppPrefs
 import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.analytics.AnalyticsTracker.Stat.ORDER_SHIPMENT_TRACKING_ADD_BUTTON_TAPPED
 import com.woocommerce.android.di.ViewModelAssistedFactory
+import com.woocommerce.android.model.OrderShipmentTracking
 import com.woocommerce.android.tools.NetworkStatus
-import com.woocommerce.android.ui.orders.details.OrderDetailViewModel
 import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.LiveDataDelegate
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ExitWithResult
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowDialog
 import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.SavedStateWithArgs
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.widgets.AppRatingDialog
 import kotlinx.android.parcel.Parcelize
+import org.wordpress.android.fluxc.model.order.OrderIdentifier
 import org.wordpress.android.fluxc.utils.DateUtils as FluxCDateUtils
 
 class AddOrderShipmentTrackingViewModel @AssistedInject constructor(
@@ -36,17 +45,30 @@ class AddOrderShipmentTrackingViewModel @AssistedInject constructor(
     val currentSelectedDate: String
         get() = addOrderShipmentTrackingViewState.date
 
+    val orderId: OrderIdentifier
+        get() = navArgs.orderId
+
     fun onCarrierSelected(carrier: Carrier) {
-        addOrderShipmentTrackingViewState = addOrderShipmentTrackingViewState.copy(carrier = carrier)
+        addOrderShipmentTrackingViewState = addOrderShipmentTrackingViewState.copy(
+            carrier = carrier,
+            trackingLink = if (!carrier.isCustom) "" else addOrderShipmentTrackingViewState.trackingLink,
+            carrierError = null
+        )
     }
 
     fun onCustomCarrierNameEntered(name: String) {
         val carrier = addOrderShipmentTrackingViewState.carrier.copy(name = name)
-        onCarrierSelected(carrier)
+        addOrderShipmentTrackingViewState = addOrderShipmentTrackingViewState.copy(
+            carrier = carrier,
+            customCarrierNameError = null
+        )
     }
 
     fun onTrackingNumberEntered(trackingNumber: String) {
-        addOrderShipmentTrackingViewState = addOrderShipmentTrackingViewState.copy(trackingNumber = trackingNumber)
+        addOrderShipmentTrackingViewState = addOrderShipmentTrackingViewState.copy(
+            trackingNumber = trackingNumber,
+            trackingNumberError = null
+        )
     }
 
     fun onTrackingLinkEntered(trackingLink: String) {
@@ -57,16 +79,70 @@ class AddOrderShipmentTrackingViewModel @AssistedInject constructor(
         addOrderShipmentTrackingViewState = addOrderShipmentTrackingViewState.copy(date = date)
     }
 
+    fun onAddButtonTapped() {
+        if (addOrderShipmentTrackingViewState.carrier.name.isEmpty()) {
+            addOrderShipmentTrackingViewState = if (!addOrderShipmentTrackingViewState.carrier.isCustom) {
+                addOrderShipmentTrackingViewState.copy(
+                    carrierError = R.string.order_shipment_tracking_empty_provider
+                )
+            } else {
+                addOrderShipmentTrackingViewState.copy(
+                    customCarrierNameError = R.string.order_shipment_tracking_empty_custom_provider_name
+                )
+            }
+            return
+        }
+        if (addOrderShipmentTrackingViewState.trackingNumber.isEmpty()) {
+            addOrderShipmentTrackingViewState = addOrderShipmentTrackingViewState.copy(
+                trackingNumberError = R.string.order_shipment_tracking_empty_tracking_num
+            )
+            return
+        }
+
+        AnalyticsTracker.track(ORDER_SHIPMENT_TRACKING_ADD_BUTTON_TAPPED)
+        AppRatingDialog.incrementInteractions()
+
+        AppPrefs.setSelectedShipmentTrackingProviderName(addOrderShipmentTrackingViewState.carrier.name)
+        AppPrefs.setIsSelectedShipmentTrackingProviderNameCustom(addOrderShipmentTrackingViewState.carrier.isCustom)
+
+        val shipmentTracking = OrderShipmentTracking(
+            trackingNumber = addOrderShipmentTrackingViewState.trackingNumber,
+            dateShipped = addOrderShipmentTrackingViewState.date,
+            trackingProvider = addOrderShipmentTrackingViewState.carrier.name,
+            isCustomProvider = addOrderShipmentTrackingViewState.carrier.isCustom,
+            trackingLink = addOrderShipmentTrackingViewState.trackingLink
+        )
+        triggerEvent(ExitWithResult(shipmentTracking))
+    }
+
+    fun onBackButtonPressed(): Boolean {
+        return if (addOrderShipmentTrackingViewState.carrier.name.isNotEmpty()
+            || addOrderShipmentTrackingViewState.trackingNumber.isNotEmpty()
+            || (addOrderShipmentTrackingViewState.carrier.isCustom && addOrderShipmentTrackingViewState.trackingLink.isNotEmpty())) {
+            triggerEvent(ShowDialog.buildDiscardDialogEvent(
+                positiveBtnAction = DialogInterface.OnClickListener { _, _ ->
+                    triggerEvent(Exit)
+                }
+            ))
+            false
+        } else {
+            true
+        }
+    }
+
     @Parcelize
     data class ViewState(
         val isSelectedProviderCustom: Boolean,
         val carrier: Carrier,
         val trackingNumber: String = "",
-        val trackingLink: String? = null,
+        val trackingLink: String = "",
         val date: String = FluxCDateUtils.getCurrentDateString(),
-        val showLoadingProgress: Boolean = false
+        val showLoadingProgress: Boolean = false,
+        val carrierError: Int? = null,
+        val customCarrierNameError: Int? = null,
+        val trackingNumberError: Int? = null
     ) : Parcelable
 
     @AssistedInject.Factory
-    interface Factory : ViewModelAssistedFactory<OrderDetailViewModel>
+    interface Factory : ViewModelAssistedFactory<AddOrderShipmentTrackingViewModel>
 }
