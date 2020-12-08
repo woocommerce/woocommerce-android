8f92c1312793cab54f441200deb57273b5836f75
Nick Bradbury <nick.bradbury@gmail.com>
2020-12-08 15:17:16 -0500

    Converted OrderDetailPaymentInfoView to ViewBinding

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailPaymentInfoView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailPaymentInfoView.kt
@@ -2,11 +2,13 @@ package com.woocommerce.android.ui.orders.details.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.card.MaterialCardView
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.OrderDetailPaymentInfoBinding
 import com.woocommerce.android.extensions.getMediumDate
 import com.woocommerce.android.extensions.hide
 import com.woocommerce.android.extensions.isEqualTo
@@ -14,7 +16,6 @@ import com.woocommerce.android.extensions.show
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.Refund
 import com.woocommerce.android.ui.orders.details.adapter.OrderDetailRefundsAdapter
-import kotlinx.android.synthetic.main.order_detail_payment_info.view.*
 import java.math.BigDecimal
 
 class OrderDetailPaymentInfoView @JvmOverloads constructor(
@@ -22,42 +23,40 @@ class OrderDetailPaymentInfoView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : MaterialCardView(ctx, attrs, defStyleAttr) {
-    init {
-        View.inflate(context, R.layout.order_detail_payment_info, this)
-    }
+    private val binding = OrderDetailPaymentInfoBinding.inflate(LayoutInflater.from(ctx), this)
 
     fun updatePaymentInfo(
         order: Order,
         formatCurrencyForDisplay: (BigDecimal) -> String,
         onIssueRefundClickListener: ((view: View) -> Unit)
     ) {
-        paymentInfo_productsTotal.text = formatCurrencyForDisplay(order.productsTotal)
-        paymentInfo_shippingTotal.text = formatCurrencyForDisplay(order.shippingTotal)
-        paymentInfo_taxesTotal.text = formatCurrencyForDisplay(order.totalTax)
-        paymentInfo_total.text = formatCurrencyForDisplay(order.total)
-        paymentInfo_lblTitle.text = context.getString(R.string.payment)
+        binding.paymentInfoProductsTotal.text = formatCurrencyForDisplay(order.productsTotal)
+        binding.paymentInfoShippingTotal.text = formatCurrencyForDisplay(order.shippingTotal)
+        binding.paymentInfoTaxesTotal.text = formatCurrencyForDisplay(order.totalTax)
+        binding.paymentInfoTotal.text = formatCurrencyForDisplay(order.total)
+        binding.paymentInfoLblTitle.text = context.getString(R.string.payment)
 
-        with(paymentInfo_refunds) {
+        with(binding.paymentInfoRefunds) {
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
         }
 
         if (order.isOrderPaid) {
-            paymentInfo_paymentMsg.hide()
-            paymentInfo_paidSection.hide()
+            binding.paymentInfoPaymentMsg.hide()
+            binding.paymentInfoPaidSection.hide()
         } else {
-            paymentInfo_paymentMsg.show()
+            binding.paymentInfoPaymentMsg.show()
 
             if (order.isAwaitingPayment) {
-                paymentInfo_paid.text = formatCurrencyForDisplay(BigDecimal.ZERO)
-                paymentInfo_paymentMsg.text = context.getString(
+                binding.paymentInfoPaid.text = formatCurrencyForDisplay(BigDecimal.ZERO)
+                binding.paymentInfoPaymentMsg.text = context.getString(
                     R.string.orderdetail_payment_summary_onhold, order.paymentMethodTitle
                 )
             } else {
-                paymentInfo_paid.text = formatCurrencyForDisplay(order.total)
+                binding.paymentInfoPaid.text = formatCurrencyForDisplay(order.total)
 
                 val dateStr = order.datePaid?.getMediumDate(context)
-                paymentInfo_paymentMsg.text = if (order.paymentMethodTitle.isNotEmpty()) {
+                binding.paymentInfoPaymentMsg.text = if (order.paymentMethodTitle.isNotEmpty()) {
                     context.getString(
                         R.string.orderdetail_payment_summary_completed,
                         dateStr,
@@ -69,14 +68,14 @@ class OrderDetailPaymentInfoView @JvmOverloads constructor(
 
         // Populate or hide discounts section
         if (order.discountTotal isEqualTo BigDecimal.ZERO) {
-            paymentInfo_discountSection.hide()
+            binding.paymentInfoDiscountSection.hide()
         } else {
-            paymentInfo_discountSection.show()
-            paymentInfo_discountTotal.text = context.getString(
+            binding.paymentInfoDiscountSection.show()
+            binding.paymentInfoDiscountTotal.text = context.getString(
                 R.string.orderdetail_customer_note,
                 formatCurrencyForDisplay(order.discountTotal)
             )
-            paymentInfo_discountItems.text = context.getString(
+            binding.paymentInfoDiscountItems.text = context.getString(
                 R.string.orderdetail_discount_items,
                 order.discountCodes
             )
@@ -84,22 +83,22 @@ class OrderDetailPaymentInfoView @JvmOverloads constructor(
 
         // Populate or hide the fees section
         if (order.feesTotal isEqualTo BigDecimal.ZERO) {
-            paymentInfo_feesSection.hide()
+            binding.paymentInfoFeesSection.hide()
         } else {
-            paymentInfo_feesSection.show()
-            paymentInfo_Fees.text = formatCurrencyForDisplay(order.feesTotal)
+            binding.paymentInfoFeesSection.show()
+            binding.paymentInfoFees.text = formatCurrencyForDisplay(order.feesTotal)
         }
 
         // Populate or hide refund section
         if (order.refundTotal > BigDecimal.ZERO) {
-            paymentInfo_refundSection.show()
+            binding.paymentInfoRefundSection.show()
             val newTotal = order.total - order.refundTotal
-            paymentInfo_newTotal.text = formatCurrencyForDisplay(newTotal)
+            binding.paymentInfoNewTotal.text = formatCurrencyForDisplay(newTotal)
         } else {
-            paymentInfo_refundSection.hide()
+            binding.paymentInfoRefundSection.hide()
         }
 
-        paymentInfo_issueRefundButton.setOnClickListener(onIssueRefundClickListener)
+        binding.paymentInfoIssueRefundButton.setOnClickListener(onIssueRefundClickListener)
     }
 
     fun showRefunds(
@@ -107,13 +106,13 @@ class OrderDetailPaymentInfoView @JvmOverloads constructor(
         refunds: List<Refund>,
         formatCurrencyForDisplay: (BigDecimal) -> String
     ) {
-        val adapter = paymentInfo_refunds.adapter as? OrderDetailRefundsAdapter
+        val adapter = binding.paymentInfoRefunds.adapter as? OrderDetailRefundsAdapter
             ?: OrderDetailRefundsAdapter(order.isCashPayment, order.paymentMethodTitle, formatCurrencyForDisplay)
-        paymentInfo_refunds.adapter = adapter
+        binding.paymentInfoRefunds.adapter = adapter
         adapter.refundList = refunds
 
-        paymentInfo_refunds.show()
-        paymentInfo_refundTotalSection.hide()
+        binding.paymentInfoRefunds.show()
+        binding.paymentInfoRefundTotalSection.hide()
 
         var availableRefundQuantity = order.availableRefundQuantity
         refunds.flatMap { it.items }.groupBy { it.uniqueId }.forEach { productRefunds ->
@@ -122,7 +121,7 @@ class OrderDetailPaymentInfoView @JvmOverloads constructor(
         }
 
         // TODO: Once the refund by amount is supported again, this condition will need to be updated
-        paymentInfo_issueRefundButtonSection.isVisible = availableRefundQuantity > 0 && order.isRefundAvailable
+        binding.paymentInfoIssueRefundButtonSection.isVisible = availableRefundQuantity > 0 && order.isRefundAvailable
     }
 
     fun showRefundTotal(
@@ -130,9 +129,9 @@ class OrderDetailPaymentInfoView @JvmOverloads constructor(
         refundTotal: BigDecimal,
         formatCurrencyForDisplay: (BigDecimal) -> String
     ) {
-        paymentInfo_refundTotal.text = formatCurrencyForDisplay(refundTotal)
-        paymentInfo_refunds.hide()
-        paymentInfo_refundTotalSection.show()
-        paymentInfo_issueRefundButtonSection.isVisible = show
+        binding.paymentInfoRefundTotal.text = formatCurrencyForDisplay(refundTotal)
+        binding.paymentInfoRefunds.hide()
+        binding.paymentInfoRefundTotalSection.show()
+        binding.paymentInfoIssueRefundButtonSection.isVisible = show
     }
 }
