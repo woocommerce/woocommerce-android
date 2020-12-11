f508049db5b56b91ffc58c91826da8220593d2a0
Ondrej Ruttkay <ondrej@automattic.com>
2020-12-11 16:49:52 +0100

    Add custom extension functions for dialogs and TopLevelFragments

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
@@ -3,6 +3,9 @@ package com.woocommerce.android.extensions
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.TopLevelFragment
+import com.woocommerce.android.ui.products.ProductDetailFragment
 
 /**
  * A helper function that sets the submitted key-value pair in the Fragment's SavedStateHandle. The value can be
@@ -49,4 +52,36 @@ fun <T> Fragment.handleResult(key: String, entryId: Int? = null, handler: (T) ->
             }
         )
     }
+}
+
+/**
+ * A helper function that subscribes a supplied handler function to the Fragment's SavedStateHandle LiveData associated
+ * with the supplied key. This method *must* be used for handling results from dialogs because the entry ID is required.
+ *
+ * @param [key] A unique string that is the same as the one used in [navigateBackWithResult]
+ * @param [entryId] A mandatory ID to identify the correct back stack entry. It's required when calling [handleResult]
+ *  from TopLevelFragment or Dialog (otherwise the result will get lost upon configuration change)
+ * @param [handler] A result handler
+ *
+ * Note: The handler is called only if the value wasn't handled before (i.e. the data is fresh). Once the observer is
+ * called, the boolean value is updated. This puts a limit on the number of observers for a particular key-result pair
+ * to 1.
+ */
+fun <T> Fragment.handleDialogResult(key: String, entryId: Int, handler: (T) -> Unit) {
+    handleResult(key, entryId, handler)
+}
+
+/**
+ * A helper function that subscribes a supplied handler function to the [TopLevelFragment]'s SavedStateHandle LiveData
+ * associated with the supplied key. The `rootFragment` entry ID must be used to deliver the result.
+ *
+ * @param [key] A unique string that is the same as the one used in [navigateBackWithResult]
+ * @param [handler] A result handler
+ *
+ * Note: The handler is called only if the value wasn't handled before (i.e. the data is fresh). Once the observer is
+ * called, the boolean value is updated. This puts a limit on the number of observers for a particular key-result pair
+ * to 1.
+ */
+fun <T> TopLevelFragment.handleResult(key: String, handler: (T) -> Unit) {
+    (this as Fragment).handleResult(key, entryId = R.id.rootFragment, handler = handler)
 }
