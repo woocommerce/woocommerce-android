67e2fb6aa317867556e195eec54254718607701d
Amanda Riu <amanda.riu@automattic.com>
2020-12-16 18:14:03 -0500

    Add back the orderRefreshLayout PTR handling.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListFragment.kt
@@ -161,6 +161,14 @@ class OrderListFragment : TopLevelFragment(R.layout.fragment_order_list),
         _binding = FragmentOrderListBinding.bind(view)
         binding.orderListView.init(currencyFormatter = currencyFormatter, orderListListener = this)
         binding.orderStatusListView.init(listener = this)
+        binding.orderRefreshLayout?.apply {
+            // Set the scrolling view in the custom refresh SwipeRefreshLayout
+            scrollUpChild = binding.orderListView.ordersList
+            setOnRefreshListener {
+                AnalyticsTracker.track(Stat.ORDERS_LIST_PULLED_TO_REFRESH)
+                refreshOrders()
+            }
+        }
 
         initializeViewModel()
         initializeTabs()
