f163f13da50b36e8a37c95e48e2910e1c92674e1
Nick Bradbury <nick.bradbury@gmail.com>
2021-01-06 12:19:02 -0500

    Converted ProductStatusFragment to view binding

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/settings/ProductSlugFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/settings/ProductSlugFragment.kt
@@ -1,9 +1,7 @@
 package com.woocommerce.android.ui.products.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.woocommerce.android.R
 import com.woocommerce.android.RequestCodes
