817094e6dff0558f9a4d9a9b86d210e0989b7b35
Nick Bradbury <nick.bradbury@gmail.com>
2021-01-06 12:16:00 -0500

    Converted ProductSlugFragment to view binding

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/settings/ProductSlugFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/settings/ProductSlugFragment.kt
@@ -8,12 +8,12 @@ import androidx.navigation.fragment.navArgs
 import com.woocommerce.android.R
 import com.woocommerce.android.RequestCodes
 import com.woocommerce.android.analytics.AnalyticsTracker
-import kotlinx.android.synthetic.main.fragment_product_slug.*
+import com.woocommerce.android.databinding.FragmentProductSlugBinding
 
 /**
  * Settings screen which enables editing a product's slug
  */
-class ProductSlugFragment : BaseProductSettingsFragment() {
+class ProductSlugFragment : BaseProductSettingsFragment(R.layout.fragment_product_slug) {
     companion object {
         const val ARG_SLUG = "slug"
     }
@@ -21,16 +21,23 @@ class ProductSlugFragment : BaseProductSettingsFragment() {
     override val requestCode = RequestCodes.PRODUCT_SETTINGS_SLUG
     private val navArgs: ProductSlugFragmentArgs by navArgs()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_product_slug, container, false)
-    }
+    private var _binding: FragmentProductSlugBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        editSlug.setText(navArgs.slug)
-        editSlug.setOnTextChangedListener {
+
+        _binding = FragmentProductSlugBinding.bind(view)
+
+        binding.editSlug.setText(navArgs.slug)
+        binding.editSlug.setOnTextChangedListener {
             changesMade()
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun hasChanges() = getSlug() != navArgs.slug
@@ -46,7 +53,7 @@ class ProductSlugFragment : BaseProductSettingsFragment() {
     /**
      * As with the web, we trim the string and replace any spaces with hyphens
      */
-    private fun getSlug() = editSlug.getText().trim().replace(" ", "-")
+    private fun getSlug() = binding.editSlug.getText().trim().replace(" ", "-")
 
     override fun onResume() {
         super.onResume()
