2c95f9b146891233af1133dda7e0365a70504b33
Ondrej Ruttkay <ondrej@automattic.com>
2021-01-06 11:38:26 +0100

    Add notice extension functions for dialogs and TopLevelFragments

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/FragmentExt.kt
@@ -85,6 +85,24 @@ fun <T> Fragment.handleDialogResult(key: String, entryId: Int, handler: (T) -> U
 }
 
 /**
+ * A helper function that subscribes a supplied notice handler function to the dialog Fragment's SavedStateHandle
+ * LiveData associated with the supplied key. This method *must* be used for handling notices from dialogs because the
+ * entry ID is required.
+ *
+ * @param [key] A unique string that is the same as the one used in [navigateBackWithNotice]
+ * @param [entryId] A mandatory ID to identify the correct back stack entry. It's required when calling [handleNotice]
+ *  from TopLevelFragment or Dialog (otherwise the result will get lost upon configuration change)
+ * @param [handler] A result handler
+ *
+ * Note: The handler is called only if the value wasn't handled before (i.e. the data is fresh). Once the observer is
+ * called, the value is nulled and the handler won't be called. This puts a limit on the number of observers for
+ * a particular key-result pair to 1.
+ */
+fun <T> Fragment.handleDialogNotice(key: String, entryId: Int, handler: (T) -> Unit) {
+    handleResult(key, entryId, handler)
+}
+
+/**
  * A helper function that subscribes a supplied handler function to the [TopLevelFragment]'s SavedStateHandle LiveData
  * associated with the supplied key. The `rootFragment` entry ID must be used to deliver the result.
  *
@@ -92,13 +110,41 @@ fun <T> Fragment.handleDialogResult(key: String, entryId: Int, handler: (T) -> U
  * @param [handler] A result handler
  *
  * Note: The handler is called only if the value wasn't handled before (i.e. the data is fresh). Once the observer is
- * called, the boolean value is updated. This puts a limit on the number of observers for a particular key-result pair
- * to 1.
+ * called, the value is nulled and the handler won't be called. This puts a limit on the number of observers for
+ * a particular key-result pair to 1.
  */
 fun <T> TopLevelFragment.handleResult(key: String, handler: (T) -> Unit) {
     (this as Fragment).handleResult(key, entryId = R.id.rootFragment, handler = handler)
 }
 
+/**
+ * A helper function that subscribes a supplied noice handler function to the [TopLevelFragment]'s SavedStateHandle
+ * LiveData associated with the supplied key. The `rootFragment` entry ID must be used to deliver the result.
+ *
+ * @param [key] A unique string that is the same as the one used in [navigateBackWithNotice]
+ * @param [handler] A result handler
+ *
+ * Note: The handler is called only if the value wasn't handled before (i.e. the data is fresh). Once the observer is
+ * called, the value is nulled and the handler won't be called. This puts a limit on the number of observers for
+ * a particular key-result pair to 1.
+ */
+fun TopLevelFragment.handleNotice(key: String, handler: () -> Unit) {
+    (this as Fragment).handleNotice(key, entryId = R.id.rootFragment, handler = handler)
+}
+
+/**
+ * A helper function that subscribes a supplied handler function to the Fragment's SavedStateHandle LiveData associated
+ * with the supplied key.
+ *
+ * @param [key] A unique string that is the same as the one used in [navigateBackWithNotice]
+ * @param [entryId] A mandatory ID to identify the correct back stack entry. It's required when calling [handleNotice]
+ *  from TopLevelFragment or Dialog (otherwise the result will get lost upon configuration change)
+ * @param [handler] A result handler
+ *
+ * Note: The handler is called only if the value wasn't handled before (i.e. the data is fresh). Once the observer is
+ * called, the value is nulled and the handler won't be called. This puts a limit on the number of observers for
+ * a particular key-result pair to 1.
+ */
 fun Fragment.handleNotice(key: String, entryId: Int? = null, handler: () -> Unit) {
     val entry = if (entryId != null) {
         findNavController().getBackStackEntry(entryId)
