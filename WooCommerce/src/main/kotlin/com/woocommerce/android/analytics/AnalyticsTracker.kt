4a09fc25633934dd455a2ed4b8708ba40bb5c09d
Shiki <jayson@basanes.net>
2021-01-07 15:10:45 -0700

    Delete unused event

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsTracker.kt
@@ -329,7 +329,6 @@ class AnalyticsTracker private constructor(private val context: Context) {
 
         // -- Connected Products (Grouped products, Upsells, Cross-sells)
         CONNECTED_PRODUCTS_LIST,
-        GROUPED_PRODUCT_LINKED_PRODUCTS_DONE_BUTTON_TAPPED,
 
         // -- Product external link
         PRODUCT_DETAIL_VIEW_EXTERNAL_PRODUCT_LINK_TAPPED,
