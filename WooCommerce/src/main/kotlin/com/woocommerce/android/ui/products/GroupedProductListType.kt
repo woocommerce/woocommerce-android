7763a97ae293a314548e73ee62fe9390d153bcfa
Shiki <jayson@basanes.net>
2021-01-07 14:50:45 -0700

    Add statAction to GroupedProductListType

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/GroupedProductListType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/GroupedProductListType.kt
@@ -2,9 +2,14 @@ package com.woocommerce.android.ui.products
 
 import androidx.annotation.StringRes
 import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsTracker.Companion.ConnectedProductsListContext
 
-enum class GroupedProductListType(@StringRes val titleId: Int, val resultKey: String) {
-    GROUPED(R.string.grouped_products, "key_grouped"),
-    UPSELLS(R.string.upsells_label, "key_upsells"),
-    CROSS_SELLS(R.string.cross_sells_label, "key_cross-sells");
+enum class GroupedProductListType(
+    @StringRes val titleId: Int,
+    val resultKey: String,
+    val statContext: ConnectedProductsListContext
+) {
+    GROUPED(R.string.grouped_products, "key_grouped", ConnectedProductsListContext.GROUPED_PRODUCTS),
+    UPSELLS(R.string.upsells_label, "key_upsells", ConnectedProductsListContext.UPSELLS),
+    CROSS_SELLS(R.string.cross_sells_label, "key_cross-sells", ConnectedProductsListContext.CROSS_SELLS);
 }
