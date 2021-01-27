67d82e7842cd61067417160a5173465a3f60a78e
Nick Bradbury <nick.bradbury@gmail.com>
2021-01-27 12:50:45 -0500

    Converted RefundByAmountFragment to view binding

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundByAmountFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundByAmountFragment.kt
@@ -1,13 +1,12 @@
 package com.woocommerce.android.ui.refunds
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.navGraphViewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.databinding.FragmentRefundByAmountBinding
 import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.refunds.IssueRefundViewModel.IssueRefundEvent.HideValidationError
@@ -15,22 +14,19 @@ import com.woocommerce.android.ui.refunds.IssueRefundViewModel.IssueRefundEvent.
 import com.woocommerce.android.util.CurrencyFormatter
 import com.woocommerce.android.viewmodel.ViewModelFactory
 import dagger.Lazy
-import kotlinx.android.synthetic.main.fragment_refund_by_amount.*
 import javax.inject.Inject
 
-class RefundByAmountFragment : BaseFragment() {
+class RefundByAmountFragment : BaseFragment(R.layout.fragment_refund_by_amount) {
     @Inject lateinit var viewModelFactory: Lazy<ViewModelFactory>
     @Inject lateinit var currencyFormatter: CurrencyFormatter
+
+    private var _binding: FragmentRefundByAmountBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: IssueRefundViewModel by navGraphViewModels(R.id.nav_graph_refunds) {
         viewModelFactory.get()
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreate(savedInstanceState)
-        return inflater.inflate(R.layout.fragment_refund_by_amount, container, false)
-    }
-
+    
     override fun onResume() {
         super.onResume()
         AnalyticsTracker.trackViewShown(this)
@@ -39,12 +35,19 @@ class RefundByAmountFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        _binding = FragmentRefundByAmountBinding.bind(view)
+
         initializeViews()
         setupObservers()
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     private fun initializeViews() {
-        issueRefund_btnNextFromAmount.setOnClickListener {
+        binding.issueRefundBtnNextFromAmount.setOnClickListener {
             viewModel.onNextButtonTappedFromAmounts()
         }
     }
@@ -52,28 +55,28 @@ class RefundByAmountFragment : BaseFragment() {
     private fun setupObservers() {
         viewModel.refundByAmountStateLiveData.observe(viewLifecycleOwner) { old, new ->
             new.availableForRefund?.takeIfNotEqualTo(old?.availableForRefund) {
-                issueRefund_txtAvailableForRefund.text = it
+                binding.issueRefundTxtAvailableForRefund.text = it
             }
             new.currency?.takeIfNotEqualTo(old?.currency) {
-                issueRefund_refundAmount.initView(new.currency, new.decimals, currencyFormatter)
+                binding.issueRefundRefundAmount.initView(new.currency, new.decimals, currencyFormatter)
             }
             new.enteredAmount.takeIfNotEqualTo(old?.enteredAmount) {
-                issueRefund_refundAmount.setValue(new.enteredAmount)
+                binding.issueRefundRefundAmount.setValue(new.enteredAmount)
             }
             new.isNextButtonEnabled?.takeIfNotEqualTo(old?.isNextButtonEnabled) {
-                issueRefund_btnNextFromAmount.isEnabled = it
+                binding.issueRefundBtnNextFromAmount.isEnabled = it
             }
         }
 
         viewModel.event.observe(viewLifecycleOwner, Observer { event ->
             when (event) {
-                is ShowValidationError -> issueRefund_refundAmount.error = event.message
-                is HideValidationError -> issueRefund_refundAmount.error = null
+                is ShowValidationError -> binding.issueRefundRefundAmount.error = event.message
+                is HideValidationError -> binding.issueRefundRefundAmount.error = null
                 else -> event.isHandled = false
             }
         })
 
-        issueRefund_refundAmount.value.observe(viewLifecycleOwner, Observer {
+        binding.issueRefundRefundAmount.value.observe(viewLifecycleOwner, Observer {
             viewModel.onManualRefundAmountChanged(it)
         })
     }
