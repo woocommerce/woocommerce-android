fa7ba5b2a1036ad7e5145a8f6d1045ab049fbbb6
Ondrej Ruttkay <ondrej@automattic.com>
2021-02-26 21:54:41 +0100

    Update ShippingRate model

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/ShippingRate.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/ShippingRate.kt
@@ -2,19 +2,36 @@ package com.woocommerce.android.model
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import java.math.BigDecimal
+import java.util.Date
 
 @Parcelize
 data class ShippingRate(
     val id: String,
     val title: String,
     val deliveryEstimate: Int,
-    val price: BigDecimal,
-    val carrier: ShippingCarrier
+    val deliveryDate: Date?,
+    val price: String,
+    val carrier: ShippingCarrier,
+    val isTrackingAvailable: Boolean,
+    val isFreePickupAvailable: Boolean,
+    val isInsuranceAvailable: Boolean,
+    val insuranceCoverage: String?,
+    val isSignatureRequired: Boolean,
+    val isSignatureAvailable: Boolean,
+    val signaturePrice: String?,
+    val isAdultSignatureAvailable: Boolean,
+    val adultSignaturePrice: String?,
+    val extraOptionSelected: ExtraOption,
+    val isSelected: Boolean
 ) : Parcelable {
-    enum class ShippingCarrier {
-        FEDEX,
-        USPS,
-        UPS
+    enum class ShippingCarrier(val title: String) {
+        FEDEX("Fedex"),
+        USPS("USPS"),
+        UPS("UPS")
+    }
+    enum class ExtraOption {
+        SIGNATURE,
+        ADULT_SIGNATURE,
+        NONE
     }
 }
