8c9f67043b28ac297ae6377fb8beccc4627ee711
Thomaz Cortez <thomazfcortez@gmail.com>
2021-03-04 18:51:46 -0300

    Create EditVariationAttributesFragment and respective entities

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/EditVariationAttributesFragment.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.products.variations.attributes
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.products.BaseProductEditorFragment
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
+
+class EditVariationAttributesFragment :
+    BaseProductEditorFragment(R.layout.fragment_edit_variation_attributes) {
+    override val lastEvent: Event?
+        get() = TODO("Not yet implemented")
+
+    override fun onExit() {
+        TODO("Not yet implemented")
+    }
+}
