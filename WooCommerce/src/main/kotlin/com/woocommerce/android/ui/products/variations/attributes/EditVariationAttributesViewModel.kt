61e7233b694ba38e44d3b25e16667971ba0fedbc
Thomaz Cortez <thomazfcortez@gmail.com>
2021-03-15 21:19:39 -0300

    Setup EditVariationAttributesViewModel injection

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/EditVariationAttributesViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/EditVariationAttributesViewModel.kt
@@ -1,4 +1,18 @@
 package com.woocommerce.android.ui.products.variations.attributes
 
-class EditVariationAttributesViewModel {
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import com.woocommerce.android.di.ViewModelAssistedFactory
+import com.woocommerce.android.ui.products.ProductDetailRepository
+import com.woocommerce.android.util.CoroutineDispatchers
+import com.woocommerce.android.viewmodel.SavedStateWithArgs
+import com.woocommerce.android.viewmodel.ScopedViewModel
+
+class EditVariationAttributesViewModel @AssistedInject constructor(
+    @Assisted savedState: SavedStateWithArgs,
+    dispatchers: CoroutineDispatchers,
+    private val productRepository: ProductDetailRepository
+): ScopedViewModel(savedState, dispatchers) {
+    @AssistedInject.Factory
+    interface Factory : ViewModelAssistedFactory<EditVariationAttributesViewModel>
 }
