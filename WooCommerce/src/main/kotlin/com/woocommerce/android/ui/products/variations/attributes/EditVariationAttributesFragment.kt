e562860102da43db76400e8e1ba20f7ba80e28ac
Thomaz Cortez <thomazfcortez@gmail.com>
2021-03-16 22:34:12 -0300

    Connect EditVariationAttributesFragment with ViewModel observers

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/EditVariationAttributesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/EditVariationAttributesFragment.kt
@@ -1,8 +1,14 @@
 package com.woocommerce.android.ui.products.variations.attributes
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.woocommerce.android.R
+import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.products.variations.attributes.EditVariationAttributesViewModel.VariationAttributeSelectionGroup
+import com.woocommerce.android.ui.products.variations.attributes.EditVariationAttributesViewModel.ViewState
 import com.woocommerce.android.viewmodel.ViewModelFactory
 import javax.inject.Inject
 
@@ -15,4 +21,30 @@ class EditVariationAttributesFragment :
     @Inject lateinit var viewModelFactory: ViewModelFactory
 
     private val viewModel: EditVariationAttributesViewModel by viewModels { viewModelFactory }
+
+    private fun setupObservers() = viewModel.apply {
+        viewStateLiveData.observe(viewLifecycleOwner, ::handleViewStateChanges)
+        editableVariationAttributeList.observe(viewLifecycleOwner, Observer(::handleVariationAttributeListChanges))
+    }
+
+    private fun handleViewStateChanges(old: ViewState?, new: ViewState?) {
+        new?.isSkeletonShown?.takeIfNotEqualTo(old?.isSkeletonShown) {
+            // change skeleton visibility
+        }
+
+        new?.isRefreshing?.takeIfNotEqualTo(old?.isRefreshing) {
+            // update refresh layout
+        }
+
+        new?.isEmptyViewVisible?.takeIfNotEqualTo(old?.isEmptyViewVisible) {
+            // update empty view visibility
+        }
+    }
+
+    private fun handleVariationAttributeListChanges(selectableOptions: List<VariationAttributeSelectionGroup>) {
+        showAttributeSelectableOptions(selectableOptions)
+        requireActivity().invalidateOptionsMenu()
+    }
+
+    private fun showAttributeSelectableOptions(selectableOptions: List<VariationAttributeSelectionGroup>) {}
 }
