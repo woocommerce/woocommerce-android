e3d117d4c02c929b9ea59a8ce06ca55bddaf0247
Thomaz Cortez <thomazfcortez@gmail.com>
2021-03-18 21:18:22 -0300

    Define setup method to configure EditVariationAttributesFragment views

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/edit/EditVariationAttributesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/edit/EditVariationAttributesFragment.kt
@@ -5,6 +5,9 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentEditVariationAttributesBinding
 import com.woocommerce.android.extensions.takeIfNotEqualTo
@@ -12,6 +15,7 @@ import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.products.variations.attributes.edit.EditVariationAttributesViewModel.VariationAttributeSelectionGroup
 import com.woocommerce.android.ui.products.variations.attributes.edit.EditVariationAttributesViewModel.ViewState
 import com.woocommerce.android.viewmodel.ViewModelFactory
+import com.woocommerce.android.widgets.AlignedDividerDecoration
 import javax.inject.Inject
 
 class EditVariationAttributesFragment :
@@ -32,12 +36,25 @@ class EditVariationAttributesFragment :
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentEditVariationAttributesBinding.bind(view)
         setupObservers()
+        setupViews()
         viewModel.start(navArgs.remoteProductId, navArgs.remoteVariationId)
     }
 
     private fun setupObservers() = viewModel.apply {
         viewStateLiveData.observe(viewLifecycleOwner, ::handleViewStateChanges)
         editableVariationAttributeList.observe(viewLifecycleOwner, Observer(::handleVariationAttributeListChanges))
+    }
+
+    private fun setupViews() = binding.apply {
+        variationList.apply {
+            layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+            itemAnimator = null
+            addItemDecoration(
+                AlignedDividerDecoration(
+                    requireContext(), DividerItemDecoration.VERTICAL, R.id.variationOptionName, clipToMargin = false
+                )
+            )
+        }
     }
 
     private fun handleViewStateChanges(old: ViewState?, new: ViewState?) {
