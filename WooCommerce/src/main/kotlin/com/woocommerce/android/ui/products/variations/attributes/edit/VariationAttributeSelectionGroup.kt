6e6f6080d4931a48e3d66ba810891ada949939d9
Thomaz Cortez <thomazfcortez@gmail.com>
2021-03-24 02:03:27 -0300

    Make VariationAttributeSelectionGroup parcelable for ViewState

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/edit/VariationAttributeSelectionGroup.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/edit/VariationAttributeSelectionGroup.kt
@@ -1,12 +1,15 @@
 package com.woocommerce.android.ui.products.variations.attributes.edit
 
+import android.os.Parcelable
 import com.woocommerce.android.model.ProductVariation.Option
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class VariationAttributeSelectionGroup(
     val attributeName: String,
     val options: List<String>,
     var selectedOptionIndex: Int
-) {
+): Parcelable {
     val selectedOption
         get() = options.getOrNull(selectedOptionIndex) ?: ""
 
