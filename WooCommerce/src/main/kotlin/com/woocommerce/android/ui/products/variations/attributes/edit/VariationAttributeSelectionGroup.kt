4904670f3b10f6121ef707e556c5d2b3672d1110
Thomaz Cortez <thomazfcortez@gmail.com>
2021-03-24 18:30:01 -0300

    Adapt VariationAttributeSelectionGroup to support Any as selectable option

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/edit/VariationAttributeSelectionGroup.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/edit/VariationAttributeSelectionGroup.kt
@@ -2,19 +2,42 @@ package com.woocommerce.android.ui.products.variations.attributes.edit
 
 import android.os.Parcelable
 import com.woocommerce.android.model.ProductVariation.Option
+import com.woocommerce.android.model.VariantOption
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class VariationAttributeSelectionGroup(
+    private val id: Long,
     val attributeName: String,
-    val options: List<String>,
-    var selectedOptionIndex: Int
+    private var options: List<String>,
+    var selectedOptionIndex: Int,
+    private var anySelectionEnabled: Boolean = false
 ) : Parcelable {
     val selectedOption
         get() = options.getOrNull(selectedOptionIndex) ?: ""
 
-    fun toVariantOption() = Option(
-        attributeName = attributeName,
-        optionChoice = selectedOption
-    )
+    val attributeOptions
+        get() = options
+
+    private val anySelectionOption = "Any $attributeName"
+
+    private val isAnyOptionSelected
+        get() = options.getOrNull(selectedOptionIndex) == anySelectionOption
+
+    init {
+        if (anySelectionEnabled) options.toMutableList()
+            .apply {
+                add(anySelectionOption)
+                selectedOptionIndex = indexOf(anySelectionOption)
+            }.also { options = it }
+    }
+
+    fun toVariantOption() =
+        takeIf { isAnyOptionSelected.not() }?.let {
+            VariantOption(
+                id = id,
+                name = attributeName,
+                option = selectedOption
+            )
+        }
 }
