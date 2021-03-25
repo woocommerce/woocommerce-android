5345d8882f22f79665f4b4be7c647867e443c6f0
malinajirka <malinajirka@gmail.com>
2021-03-25 16:46:33 -0600

    Introduce TokenProvider

---
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/TokenProvider.kt
@@ -0,0 +1,13 @@
+package com.woocommerce.android.cardreader.internal
+
+import com.stripe.stripeterminal.callable.ConnectionTokenCallback
+import com.stripe.stripeterminal.callable.ConnectionTokenProvider
+import com.woocommerce.android.cardreader.WCPayStoreWrapper
+import kotlinx.coroutines.runBlocking
+
+class TokenProvider(private val storeWrapper: WCPayStoreWrapper) : ConnectionTokenProvider {
+    override fun fetchConnectionToken(callback: ConnectionTokenCallback) {
+        // this method is invoked from the 3rd party Stripe SDK on a bg thread
+        runBlocking { storeWrapper.getConnectionToken() }
+    }
+}
