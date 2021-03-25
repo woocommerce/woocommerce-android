5a0bb0ef1bc6f6f27e0b0399118d9c868e8f57ac
malinajirka <malinajirka@gmail.com>
2021-03-25 16:50:43 -0600

    Init StripeTerminal in CardReaderManager.initialize()

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/wrappers/TerminalWrapper.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/wrappers/TerminalWrapper.kt
@@ -1,9 +1,19 @@
 package com.woocommerce.android.cardreader.internal.wrappers
 
+import android.app.Application
 import com.stripe.stripeterminal.Terminal
 import com.stripe.stripeterminal.TerminalLifecycleObserver
+import com.stripe.stripeterminal.callable.TerminalListener
+import com.stripe.stripeterminal.log.LogLevel
+import com.woocommerce.android.cardreader.internal.TokenProvider
 
 internal class TerminalWrapper {
     fun isInitialized() = Terminal.isInitialized()
     fun getLifecycleObserver() = TerminalLifecycleObserver.getInstance()
+    fun initTerminal(
+        application: Application,
+        logLevel: LogLevel,
+        tokenProvider: TokenProvider,
+        listener: TerminalListener
+    ) = Terminal.initTerminal(application, logLevel, tokenProvider, listener)
 }
