818ea873e38d334f54847767f2fdd973ff3f8286
malinajirka <malinajirka@gmail.com>
2021-03-26 10:26:38 -0600

    Request location permissions before using card reader

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/CardReaderSettingsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/CardReaderSettingsFragment.kt
@@ -1,9 +1,16 @@
 package com.woocommerce.android.ui.prefs
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.woocommerce.android.R
+import com.woocommerce.android.WooCommerceDebug
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.databinding.FragmentSettingsCardReaderBinding
 
@@ -12,12 +19,37 @@ class CardReaderSettingsFragment : Fragment(R.layout.fragment_settings_card_read
         const val TAG = "card-reader-settings"
     }
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // TODO cardreader move this into a VM
+                connectToReader()
+            } else {
+                // TODO cardreader Move this into a VM and use string resource
+                Snackbar.make(requireView(), "Missing required permissions", BaseTransientBottomBar.LENGTH_SHORT)
+            }
+        }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentSettingsCardReaderBinding.bind(view)
         binding.connectReaderButton.setOnClickListener {
+            // TODO cardreader move this into a vm
             // TODO card reader implement connect reader button
+            val permissionType = Manifest.permission.ACCESS_COARSE_LOCATION
+            val locationPermissionResult = ContextCompat.checkSelfPermission(
+                requireContext(),
+                permissionType
+            )
+
+            if (locationPermissionResult == PackageManager.PERMISSION_GRANTED) {
+                connectToReader()
+            } else {
+                requestPermissionLauncher.launch(permissionType)
+            }
         }
     }
 
@@ -26,5 +58,8 @@ class CardReaderSettingsFragment : Fragment(R.layout.fragment_settings_card_read
         AnalyticsTracker.trackViewShown(this)
 
         activity?.setTitle(R.string.settings_card_reader)
+    }
+
+    private fun connectToReader() {
     }
 }
