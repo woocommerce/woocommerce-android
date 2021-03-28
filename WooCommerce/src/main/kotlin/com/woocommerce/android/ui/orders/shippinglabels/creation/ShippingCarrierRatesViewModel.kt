f2565cbed02e0f5b1bfe878daadf77f30189f561
Ondrej Ruttkay <ondrej@automattic.com>
2021-03-28 23:57:11 +0200

    Add shipping carriers view events

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingCarrierRatesViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingCarrierRatesViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.analytics.AnalyticsTracker.Stat
 import com.woocommerce.android.di.ViewModelAssistedFactory
 import com.woocommerce.android.extensions.isEqualTo
 import com.woocommerce.android.model.Order
@@ -261,6 +263,8 @@ class ShippingCarrierRatesViewModel @AssistedInject constructor(
     }
 
     fun onDoneButtonClicked() {
+        AnalyticsTracker.track(Stat.SHIPPING_LABEL_SHIPPING_CARRIER_DONE_BUTTON_TAPPED)
+
         val selectedRates = shippingRates.value?.let { rates ->
             rates.map { it.selectedRate }
         }
