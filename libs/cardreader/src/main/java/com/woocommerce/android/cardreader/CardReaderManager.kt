ce905783b1b2f364ce54f937dca860aa892c543c
malinajirka <malinajirka@gmail.com>
2021-03-30 11:47:38 -0600

    Remove unused imports

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/CardReaderManager.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/CardReaderManager.kt
@@ -1,11 +1,6 @@
 package com.woocommerce.android.cardreader
 
 import android.app.Application
-import com.woocommerce.android.cardreader.internal.CardReaderManagerImpl
-import com.woocommerce.android.cardreader.internal.TokenProvider
-import com.woocommerce.android.cardreader.internal.temporary.CardReaderStoreImpl
-import com.woocommerce.android.cardreader.internal.wrappers.LogWrapper
-import com.woocommerce.android.cardreader.internal.wrappers.TerminalWrapper
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 
