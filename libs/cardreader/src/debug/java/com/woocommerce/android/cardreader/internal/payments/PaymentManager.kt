fcb372e88ab298f44549e41d45c9c703a3c4b068
malinajirka <malinajirka@gmail.com>
2021-04-01 12:27:04 -0600

    Introduce PaymentManager.kt

---
+++ libs/cardreader/src/debug/java/com/woocommerce/android/cardreader/internal/payments/PaymentManager.kt
@@ -0,0 +1,115 @@
+package com.woocommerce.android.cardreader.internal.payments
+
+import com.stripe.stripeterminal.model.external.PaymentIntent
+import com.stripe.stripeterminal.model.external.PaymentIntentStatus
+import com.woocommerce.android.cardreader.CardPaymentStatus
+import com.woocommerce.android.cardreader.CardPaymentStatus.CapturingPaymentFailed
+import com.woocommerce.android.cardreader.CardPaymentStatus.CollectingPayment
+import com.woocommerce.android.cardreader.CardPaymentStatus.CollectingPaymentFailed
+import com.woocommerce.android.cardreader.CardPaymentStatus.InitializingPayment
+import com.woocommerce.android.cardreader.CardPaymentStatus.InitializingPaymentFailed
+import com.woocommerce.android.cardreader.CardPaymentStatus.PaymentCompleted
+import com.woocommerce.android.cardreader.CardPaymentStatus.ProcessingPayment
+import com.woocommerce.android.cardreader.CardPaymentStatus.ProcessingPaymentFailed
+import com.woocommerce.android.cardreader.CardPaymentStatus.ShowAdditionalInfo
+import com.woocommerce.android.cardreader.CardPaymentStatus.WaitingForInput
+import com.woocommerce.android.cardreader.internal.payments.actions.CollectPaymentAction
+import com.woocommerce.android.cardreader.internal.payments.actions.CollectPaymentAction.CollectPaymentStatus
+import com.woocommerce.android.cardreader.internal.payments.actions.CollectPaymentAction.CollectPaymentStatus.DisplayMessageRequested
+import com.woocommerce.android.cardreader.internal.payments.actions.CollectPaymentAction.CollectPaymentStatus.ReaderInputRequested
+import com.woocommerce.android.cardreader.internal.payments.actions.CreatePaymentAction
+import com.woocommerce.android.cardreader.internal.payments.actions.CreatePaymentAction.CreatePaymentStatus.Failure
+import com.woocommerce.android.cardreader.internal.payments.actions.CreatePaymentAction.CreatePaymentStatus.Success
+import com.woocommerce.android.cardreader.internal.payments.actions.ProcessPaymentAction
+import com.woocommerce.android.cardreader.internal.payments.actions.ProcessPaymentAction.ProcessPaymentStatus
+import com.woocommerce.android.cardreader.internal.temporary.CardReaderStoreImpl
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+
+@ExperimentalCoroutinesApi
+internal class PaymentManager constructor(
+    private val createPaymentAction: CreatePaymentAction,
+    private val collectPaymentAction: CollectPaymentAction,
+    private val processPaymentAction: ProcessPaymentAction
+) {
+    suspend fun acceptPayment(amount: Int, currency: String) = flow {
+        var paymentIntent = createPaymentIntent(amount, currency)
+        if (paymentIntent?.status != PaymentIntentStatus.REQUIRES_PAYMENT_METHOD) {
+            return@flow
+        }
+
+        paymentIntent = collectPayment(paymentIntent)
+        if (paymentIntent.status != PaymentIntentStatus.REQUIRES_CONFIRMATION) {
+            return@flow
+        }
+
+        paymentIntent = processPayment(paymentIntent)
+        if (paymentIntent.status != PaymentIntentStatus.REQUIRES_CAPTURE) {
+            return@flow
+        }
+
+        capturePayment(paymentIntent)
+    }
+
+    private suspend fun FlowCollector<CardPaymentStatus>.createPaymentIntent(
+        amount: Int,
+        currency: String
+    ): PaymentIntent? {
+        var paymentIntent: PaymentIntent? = null
+        emit(InitializingPayment)
+        createPaymentAction.createPaymentIntent(amount, currency).collect {
+            when (it) {
+                is Failure -> emit(InitializingPaymentFailed)
+                is Success -> paymentIntent = it.paymentIntent
+            }
+        }
+        return paymentIntent
+    }
+
+    private suspend fun FlowCollector<CardPaymentStatus>.collectPayment(
+        paymentIntent: PaymentIntent
+    ): PaymentIntent {
+        var result = paymentIntent
+        emit(CollectingPayment)
+        collectPaymentAction.collectPayment(paymentIntent).collect {
+            when (it) {
+                is DisplayMessageRequested -> emit(ShowAdditionalInfo)
+                is ReaderInputRequested -> emit(WaitingForInput)
+                is CollectPaymentStatus.Failure -> emit(CollectingPaymentFailed)
+                is CollectPaymentStatus.Success -> result = it.paymentIntent
+            }
+        }
+        return result
+    }
+
+    private suspend fun FlowCollector<CardPaymentStatus>.processPayment(
+        paymentIntent: PaymentIntent
+    ): PaymentIntent {
+        var result = paymentIntent
+        emit(ProcessingPayment)
+        processPaymentAction.processPayment(paymentIntent).collect {
+            when (it) {
+                is ProcessPaymentStatus.Failure -> emit(ProcessingPaymentFailed)
+                is ProcessPaymentStatus.Success -> result = it.paymentIntent
+            }
+        }
+        return result
+    }
+
+    private suspend fun FlowCollector<CardPaymentStatus>.capturePayment(
+        paymentIntent: PaymentIntent
+    ) {
+        val success = withContext(Dispatchers.IO) {
+            CardReaderStoreImpl().capturePaymentIntent(paymentIntent.id)
+        }
+        if (success) {
+            emit(PaymentCompleted)
+        } else {
+            emit(CapturingPaymentFailed)
+        }
+    }
+}
