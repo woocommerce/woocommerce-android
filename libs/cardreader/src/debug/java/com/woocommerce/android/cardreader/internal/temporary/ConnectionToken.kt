5b237b224528a0f9b3a6bf969b8f9f6c6a6304fb
malinajirka <malinajirka@gmail.com>
2021-04-01 13:02:45 -0600

    Remove unused classes

--- libs/cardreader/src/debug/java/com/woocommerce/android/cardreader/internal/temporary/ConnectionToken.kt
+++
@@ -1,6 +0,0 @@
-package com.woocommerce.android.cardreader.internal.temporary
-
-/**
- * This is a temporary class which will be removed when we implement the endpoints in FluxC.
- */
-data class ConnectionToken(val secret: String)
