195f158cba4944350687bb82cc39fa68b704cc88
malinajirka <malinajirka@gmail.com>
2021-04-01 14:56:38 -0600

    Add tests for CreatePaymentAction

---
+++ libs/cardreader/src/test/java/com/woocommerce/android/cardreader/internal/payments/actions/CreatePaymentActionTest.kt
@@ -0,0 +1,94 @@
+package com.woocommerce.android.cardreader.internal.payments.actions
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.stripe.stripeterminal.callable.PaymentIntentCallback
+import com.stripe.stripeterminal.model.external.PaymentIntent
+import com.stripe.stripeterminal.model.external.PaymentIntentParameters
+import com.woocommerce.android.cardreader.internal.payments.actions.CreatePaymentAction.CreatePaymentStatus
+import com.woocommerce.android.cardreader.internal.wrappers.PaymentIntentParametersFactory
+import com.woocommerce.android.cardreader.internal.wrappers.TerminalWrapper
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.toList
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import test
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+internal class CreatePaymentActionTest {
+    private lateinit var action: CreatePaymentAction
+    private val paymentIntentParametersFactory = mock<PaymentIntentParametersFactory>()
+    private val terminal: TerminalWrapper = mock()
+    private val intentParametersBuilder = mock<PaymentIntentParameters.Builder>()
+
+    @Before
+    fun setUp() {
+        action = CreatePaymentAction(paymentIntentParametersFactory, terminal, mock())
+        whenever(paymentIntentParametersFactory.createBuilder()).thenReturn(intentParametersBuilder)
+        whenever(intentParametersBuilder.setAmount(any())).thenReturn(intentParametersBuilder)
+        whenever(intentParametersBuilder.setCurrency(any())).thenReturn(intentParametersBuilder)
+        whenever(intentParametersBuilder.build()).thenReturn(mock())
+    }
+
+    @Test
+    fun `when creating paymentIntent succeeds, then Success is emitted`() = test {
+        whenever(terminal.createPaymentIntent(any(), any())).thenAnswer {
+            (it.arguments[1] as PaymentIntentCallback).onSuccess(mock())
+        }
+
+        val result = action.createPaymentIntent(0, "").first()
+
+        assertThat(result).isExactlyInstanceOf(CreatePaymentStatus.Success::class.java)
+    }
+
+    @Test
+    fun `when creating paymentIntent fails, then Failure is emitted`() = test {
+        whenever(terminal.createPaymentIntent(any(), any())).thenAnswer {
+            (it.arguments[1] as PaymentIntentCallback).onFailure(mock())
+        }
+
+        val result = action.createPaymentIntent(0, "").first()
+
+        assertThat(result).isExactlyInstanceOf(CreatePaymentStatus.Failure::class.java)
+    }
+
+    @Test
+    fun `when creating paymentIntent succeeds, then updated paymentIntent is returned`() = test {
+        val updatedPaymentIntent = mock<PaymentIntent>()
+        whenever(terminal.createPaymentIntent(any(), any())).thenAnswer {
+            (it.arguments[1] as PaymentIntentCallback).onSuccess(updatedPaymentIntent)
+        }
+
+        val result = action.createPaymentIntent(0, "").first()
+
+        assertThat((result as CreatePaymentStatus.Success).paymentIntent).isEqualTo(updatedPaymentIntent)
+    }
+
+    @Test
+    fun `when creating paymentIntent succeeds, then flow is terminated`() = test {
+        whenever(terminal.createPaymentIntent(any(), any())).thenAnswer {
+            (it.arguments[1] as PaymentIntentCallback).onSuccess(mock())
+        }
+
+        val result = action.createPaymentIntent(0, "").toList()
+
+        assertThat(result.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `when creating paymentIntent fails, then flow is terminated`() = test {
+        whenever(terminal.createPaymentIntent(any(), any())).thenAnswer {
+            (it.arguments[1] as PaymentIntentCallback).onFailure(mock())
+        }
+
+        val result = action.createPaymentIntent(0, "").toList()
+
+        assertThat(result.size).isEqualTo(1)
+    }
+}
