a2ad2ba01643dcb90b99d982cb0d75059705a76e
malinajirka <malinajirka@gmail.com>
2021-04-01 12:27:30 -0600

    Fix failing capturePayment method

--- libs/cardreader/src/debug/java/com/woocommerce/android/cardreader/internal/temporary/ApiEndpoints.kt
+++ libs/cardreader/src/debug/java/com/woocommerce/android/cardreader/internal/temporary/ApiEndpoints.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.cardreader.internal.temporary
 
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 /**
@@ -11,6 +12,7 @@ interface ApiEndpoints {
     @POST("connection_token")
     fun getConnectionToken(): Call<ConnectionToken>
 
+    @FormUrlEncoded
     @POST("capture_payment_intent")
     fun capturePaymentIntent(@Field("payment_intent_id") id: String): Call<Void>
 }
