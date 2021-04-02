0e169c757b2e1844c906f56234faf0781afbb51a
Thomaz Cortez <thomazfcortez@gmail.com>
2021-04-02 04:59:59 -0300

    Adapt AddAttributeFragment to handle Variation creation scenario

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AddAttributeFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AddAttributeFragment.kt
@@ -2,9 +2,13 @@ package com.woocommerce.android.ui.products.variations.attributes
 
 import android.os.Bundle
 import android.os.Parcelable
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.databinding.FragmentAddAttributeBinding
+import com.woocommerce.android.extensions.navigateSafely
 import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.model.ProductAttribute
 import com.woocommerce.android.model.ProductGlobalAttribute
@@ -25,10 +30,13 @@ class AddAttributeFragment : BaseProductFragment(R.layout.fragment_add_attribute
     companion object {
         const val TAG: String = "AddAttributeFragment"
         private const val LIST_STATE_KEY = "list_state"
+        private const val ID_ADD_ATTRIBUTES = 1
     }
 
     private var layoutManager: LayoutManager? = null
     private val skeletonView = SkeletonView()
+
+    private val navArgs: AddAttributeFragmentArgs by navArgs()
 
     private var _binding: FragmentAddAttributeBinding? = null
     private val binding get() = _binding!!
@@ -61,6 +69,29 @@ class AddAttributeFragment : BaseProductFragment(R.layout.fragment_add_attribute
     override fun onSaveInstanceState(outState: Bundle) {
         layoutManager?.let {
             outState.putParcelable(LIST_STATE_KEY, it.onSaveInstanceState())
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        if(navArgs.isVariationCreation) {
+            menu.add(Menu.FIRST, ID_ADD_ATTRIBUTES, Menu.FIRST, R.string.done).apply {
+                setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+                isVisible = true
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            ID_ADD_ATTRIBUTES -> {
+                AddAttributeFragmentDirections
+                    .actionAddAttributeFragmentToAttributeListFragment(isVariationCreation = true)
+                    .run { findNavController().navigateSafely(this) }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
