972d7696e1246f87b977824ea9fa90b5748e931c
malinajirka <malinajirka@gmail.com>
2021-04-19 15:36:11 -0500

    Init DI for CardReaderScan

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/CardReaderModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/CardReaderModule.kt
@@ -2,12 +2,14 @@ package com.woocommerce.android.ui.prefs.cardreader
 
 import com.woocommerce.android.di.FragmentScope
 import com.woocommerce.android.ui.prefs.cardreader.CardReaderModule.CardReaderConnectFragmentModule
+import com.woocommerce.android.ui.prefs.cardreader.CardReaderModule.CardReaderScanFragmentModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module(
     includes = [
-        CardReaderConnectFragmentModule::class
+        CardReaderConnectFragmentModule::class,
+        CardReaderScanFragmentModule::class
     ]
 )
 object CardReaderModule {
@@ -16,5 +18,12 @@ object CardReaderModule {
         @FragmentScope
         @ContributesAndroidInjector(modules = [CardReaderConnectModule::class])
         abstract fun cardReaderConnectFragment(): CardReaderConnectFragment
+    }
+
+    @Module
+    abstract class CardReaderScanFragmentModule {
+        @FragmentScope
+        @ContributesAndroidInjector(modules = [CardReaderScanModule::class])
+        abstract fun cardReaderScanFragment(): CardReaderScanFragment
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/CardReaderScanViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/CardReaderScanViewModel.kt
@@ -1,9 +1,7 @@
 package com.woocommerce.android.ui.prefs.cardreader
 
 import com.woocommerce.android.di.ViewModelAssistedFactory
-import com.woocommerce.android.ui.prefs.cardreader.CardReaderConnectViewModel.NavigationTarget.InitiateCardReaderScan
 import com.woocommerce.android.util.CoroutineDispatchers
-import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.SavedStateWithArgs
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.assisted.Assisted
@@ -12,19 +10,16 @@ import dagger.assisted.AssistedInject
 import org.wordpress.android.fluxc.utils.AppLogWrapper
 import org.wordpress.android.util.AppLog.T
 
-class CardReaderConnectViewModel @AssistedInject constructor(
+class CardReaderScanViewModel @AssistedInject constructor(
     @Assisted savedState: SavedStateWithArgs,
-    dispatchers: CoroutineDispatchers
+    dispatchers: CoroutineDispatchers,
+    private val appLogWrapper: AppLogWrapper
 ) : ScopedViewModel(savedState, dispatchers) {
 
-    fun onInitiateScanBtnClicked() {
-        triggerEvent(InitiateCardReaderScan)
-    }
-
-    sealed class NavigationTarget : Event() {
-        object InitiateCardReaderScan : NavigationTarget()
+    fun foo() {
+        appLogWrapper.d(T.MAIN, "Scan foo() invoked")
     }
 
     @AssistedFactory
-    interface Factory : ViewModelAssistedFactory<CardReaderConnectViewModel>
+    interface Factory : ViewModelAssistedFactory<CardReaderScanViewModel>
 }
