dd722b2d0d59da5ff4b610173d7dc7e07d175a32
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-04-20 18:20:09 +0000

    Update modules to have depenedencies aware of the release type

---
+++ WooCommerce/src/debug/kotlin/com.woocommerce.android/di/FluxCModule.kt
@@ -0,0 +1,21 @@
+package com.woocommerce.android.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.wordpress.android.fluxc.module.DebugOkHttpClientModule
+import org.wordpress.android.fluxc.module.ReleaseBaseModule
+import org.wordpress.android.fluxc.module.ReleaseNetworkModule
+import org.wordpress.android.fluxc.module.ReleaseOkHttpClientModule
+import org.wordpress.android.fluxc.module.ReleaseWCNetworkModule
+
+@InstallIn(SingletonComponent::class)
+@Module(
+    includes = [
+        ReleaseBaseModule::class,
+        ReleaseNetworkModule::class,
+        ReleaseWCNetworkModule::class,
+        DebugOkHttpClientModule::class
+    ]
+)
+abstract class FluxCModule
