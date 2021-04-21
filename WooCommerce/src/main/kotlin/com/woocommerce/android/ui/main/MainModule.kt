aa65379fc7a30ea1d5776ecb7bf08c7240659a3b
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-04-21 15:32:56 +0000

    Migrate MainActivity to Hilt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainModule.kt
@@ -1,17 +1,30 @@
 package com.woocommerce.android.ui.main
 
-import com.woocommerce.android.di.ActivityScope
+import android.app.Activity
 import com.woocommerce.android.ui.base.UIMessageResolver
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
+@InstallIn(ActivityComponent::class)
 @Module
 internal abstract class MainModule {
-    @ActivityScope
+    companion object {
+        // Hilt provides the current activity typed as Activity, this casts it to MainActivity
+        @Provides
+        fun provideMainActivity(activity: Activity): MainActivity {
+            return activity as MainActivity
+        }
+    }
+
+    @ActivityScoped
     @Binds
     abstract fun provideMainPresenter(mainActivityPresenter: MainPresenter): MainContract.Presenter
 
-    @ActivityScope
+    @ActivityScoped
     @Binds
     abstract fun provideUiMessageResolver(mainUIMessageResolver: MainUIMessageResolver): UIMessageResolver
 }
