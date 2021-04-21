5c66de18513706793382f71bcf458e95feef808e
malinajirka <malinajirka@gmail.com>
2021-04-21 15:24:36 -0500

    Fix merge conflict

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewModel.kt
@@ -62,7 +62,7 @@ class CardReaderPaymentViewModel @AssistedInject constructor(
                     order.total.toBigDecimalOrNull()?.let { amount ->
                         // TODO cardreader don't hardcode currency symbol ($)
                         collectPaymentFlow(cardReaderManager, amount, order.currency, "$$amount")
-                    } ?: IllegalStateException("Converting order.total to BigDecimal failed")
+                    } ?: throw IllegalStateException("Converting order.total to BigDecimal failed")
                 } ?: throw IllegalStateException("Null order is not expected at this point")
             } catch (e: IllegalStateException) {
                 logger.e(T.MAIN, e.stackTraceToString())
