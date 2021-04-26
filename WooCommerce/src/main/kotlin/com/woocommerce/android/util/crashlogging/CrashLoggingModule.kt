b52c9d12be71111ac343939d48eba8cfc8898c61
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-04-26 23:04:40 +0200

    Make CrashLogging a Singleton

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/CrashLoggingModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/CrashLoggingModule.kt
@@ -9,11 +9,13 @@ import com.woocommerce.android.util.locale.LocaleProvider
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 abstract class CrashLoggingModule {
     companion object {
         @Provides
+        @Singleton
         fun provideCrashLogging(context: Context, crashLoggingDataProvider: CrashLoggingDataProvider): CrashLogging {
             return CrashLoggingProvider.createInstance(context, crashLoggingDataProvider)
         }
