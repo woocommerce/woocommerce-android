3291cc158a85682383024c4a35d71232a333458d
malinajirka <malinajirka@gmail.com>
2021-05-03 16:27:37 -0500

    Introduce SoftwareUpdateManager

---
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/SoftwareUpdateStatus.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.cardreader
+
+sealed class SoftwareUpdateStatus {
+    object Initializing : SoftwareUpdateStatus()
+    object UpToDate: SoftwareUpdateStatus()
+    object CheckForUpdatesFailed: SoftwareUpdateStatus()
+    data class Installing(val progress: Float) : SoftwareUpdateStatus()
+    object Success : SoftwareUpdateStatus()
+    data class Failed(val message: String) : SoftwareUpdateStatus()
+}
