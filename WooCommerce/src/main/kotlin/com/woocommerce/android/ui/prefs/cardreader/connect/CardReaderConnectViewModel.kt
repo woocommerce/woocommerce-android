89551a72272bab159499ec5017c9ffcc9ea6bda7
malinajirka <malinajirka@gmail.com>
2021-05-05 13:23:57 -0500

    Initialize CardReaderManager and start scanning on vm start

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModel.kt
@@ -2,25 +2,53 @@ package com.woocommerce.android.ui.prefs.cardreader.connect
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.woocommerce.android.R
-import com.woocommerce.android.R.string
+import com.woocommerce.android.cardreader.CardReaderManager
 import com.woocommerce.android.di.ViewModelAssistedFactory
+import com.woocommerce.android.ui.prefs.cardreader.connect.CardReaderConnectViewModel.CardReaderConnectEvent.InitializeCardReaderManager
+import com.woocommerce.android.ui.prefs.cardreader.connect.CardReaderConnectViewModel.ViewState.ScanningState
 import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.DaggerScopedViewModel
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.SavedStateWithArgs
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import org.wordpress.android.fluxc.utils.AppLogWrapper
-import org.wordpress.android.util.AppLog.T
 
 class CardReaderConnectViewModel @AssistedInject constructor(
     @Assisted savedState: SavedStateWithArgs,
     dispatchers: CoroutineDispatchers,
     private val appLogWrapper: AppLogWrapper
 ) : DaggerScopedViewModel(savedState, dispatchers) {
-    fun foo() {
-        appLogWrapper.d(T.MAIN, "Connect foo() invoked")
+    private lateinit var cardReaderManager: CardReaderManager
+
+    // The app shouldn't store the state as connection flow gets canceled when the vm dies
+    private val viewState = MutableLiveData<ViewState>(ScanningState(::onCancelScanningClicked))
+    val viewStateData: LiveData<ViewState> = viewState
+
+    // TODO cardreader replace start with init when DI for CardReaderManager is supported
+    fun start(cardReaderManager: CardReaderManager) {
+        triggerEvent(InitializeCardReaderManager(cardReaderManager))
+    }
+
+    fun onCardReaderManagerInitialized() {
+        // TODO cardreader check location permissions
+        startScanning()
+    }
+
+    private fun onCancelScanningClicked() {
+        // TODO cardreader implement
+    }
+
+    private fun startScanning() {
+
+    }
+
+    sealed class CardReaderConnectEvent : Event() {
+        data class InitializeCardReaderManager(val cardReaderManager: CardReaderManager) : CardReaderConnectEvent()
     }
 
     sealed class ViewState(
@@ -58,6 +86,7 @@ class CardReaderConnectViewModel @AssistedInject constructor(
             hintLabel = R.string.card_reader_connect_connecting_hint,
             secondaryActionLabel = R.string.cancel
         )
+        // TODO cardreader add error state
     }
 
     @AssistedFactory
