0fbb0b5b96c17a82edd4907616072e8c77a1002f
malinajirka <malinajirka@gmail.com>
2021-05-05 13:00:43 -0500

    Introduce card reader scanning state

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModel.kt
@@ -1,5 +1,9 @@
 package com.woocommerce.android.ui.prefs.cardreader.connect
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.woocommerce.android.R
+import com.woocommerce.android.R.string
 import com.woocommerce.android.di.ViewModelAssistedFactory
 import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.DaggerScopedViewModel
@@ -17,6 +21,24 @@ class CardReaderConnectViewModel @AssistedInject constructor(
 ) : DaggerScopedViewModel(savedState, dispatchers) {
     fun foo() {
         appLogWrapper.d(T.MAIN, "Connect foo() invoked")
+    }
+
+    sealed class ViewState(
+        @StringRes val headerLabel: Int? = null,
+        @DrawableRes val illustration: Int? = null,
+        @StringRes val hintLabel: Int? = null,
+        val primaryActionLabel: Int? = null,
+        val secondaryActionLabel: Int? = null
+    ) {
+        open val onPrimaryActionClicked: (() -> Unit)? = null
+        open val onSecondaryActionClicked: (() -> Unit)? = null
+
+        data class ScanningState(override val onSecondaryActionClicked: (() -> Unit)) : ViewState(
+            headerLabel = R.string.card_reader_connect_scanning_header,
+            illustration = R.drawable.img_card_reader_scanning,
+            hintLabel = R.string.card_reader_connect_scanning_hint,
+            secondaryActionLabel = R.string.cancel
+        )
     }
 
     @AssistedFactory
