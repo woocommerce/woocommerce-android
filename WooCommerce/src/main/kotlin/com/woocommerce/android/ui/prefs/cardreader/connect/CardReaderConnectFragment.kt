4666cf295197c0be9d9bf62c8653aa85db7096e6
malinajirka <malinajirka@gmail.com>
2021-05-05 14:53:08 -0500

    Make CardReaderConnectFragment a DialogFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectFragment.kt
@@ -2,11 +2,21 @@ package com.woocommerce.android.ui.prefs.cardreader.connect
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.R
+import com.woocommerce.android.WooCommerce
 import com.woocommerce.android.databinding.CardReaderConnectBinding
-import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.prefs.cardreader.connect.CardReaderConnectViewModel.CardReaderConnectEvent.InitializeCardReaderManager
+import com.woocommerce.android.util.UiHelpers
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import dagger.hilt.android.AndroidEntryPoint
 
-class CardReaderConnectFragment : BaseFragment(R.layout.card_reader_connect) {
+@AndroidEntryPoint
+class CardReaderConnectFragment : DialogFragment(R.layout.card_reader_connect) {
+    val viewModel: CardReaderConnectViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
