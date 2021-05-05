0210db04935030007e46e12d9df136920c3c6120
malinajirka <malinajirka@gmail.com>
2021-05-05 14:53:25 -0500

    Connect vm to view layer

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectFragment.kt
@@ -21,6 +21,35 @@ class CardReaderConnectFragment : DialogFragment(R.layout.card_reader_connect) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = CardReaderConnectBinding.bind(view)
-        binding.testingText.setText("Hardcoded: Card Reader Connect Fragment")
+        initObservers(binding)
+        (requireActivity().application as? WooCommerce)?.cardReaderManager?.let {
+            viewModel.start(it)
+        } ?: throw IllegalStateException("CardReaderManager is null.")
+    }
+
+    private fun initObservers(binding: CardReaderConnectBinding) {
+        viewModel.event.observe(viewLifecycleOwner) {
+            when (it) {
+                is InitializeCardReaderManager -> {
+                    it.cardReaderManager.initialize(requireActivity().application)
+                    viewModel.onCardReaderManagerInitialized()
+                }
+                is Exit -> findNavController().navigateUp()
+                else -> it.isHandled = false
+            }
+        }
+        viewModel.viewStateData.observe(viewLifecycleOwner) { viewState ->
+            UiHelpers.setTextOrHide(binding.headerLabel, viewState.headerLabel)
+            UiHelpers.setImageOrHide(binding.illustration, viewState.illustration)
+            UiHelpers.setTextOrHide(binding.hintLabel, viewState.hintLabel)
+            UiHelpers.setTextOrHide(binding.primaryActionBtn, viewState.primaryActionLabel)
+            UiHelpers.setTextOrHide(binding.secondaryActionBtn, viewState.secondaryActionLabel)
+            binding.primaryActionBtn.setOnClickListener {
+                viewState.onPrimaryActionClicked?.invoke()
+            }
+            binding.secondaryActionBtn.setOnClickListener {
+                viewState.onSecondaryActionClicked?.invoke()
+            }
+        }
     }
 }
