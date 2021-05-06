e88f9dca98b3b0e724fbb91f613519a5a9f6fd66
malinajirka <malinajirka@gmail.com>
2021-05-06 13:42:53 -0500

    Remove empty line

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewModel.kt
@@ -57,7 +57,6 @@ class CardReaderPaymentViewModel @AssistedInject constructor(
     private val viewState = MutableLiveData<ViewState>(LoadingDataState)
     val viewStateData: LiveData<ViewState> = viewState
 
-
     private lateinit var cardReaderManager: CardReaderManager
 
     private var paymentFlowJob: Job? = null
