6c2cfc35ab1aa9f8210cb2c95796b083f59fb0a4
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-05-11 17:12:44 +0000

    Inject CardReaderManager

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentDialog.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.woocommerce.android.R
-import com.woocommerce.android.WooCommerce
 import com.woocommerce.android.databinding.FragmentCardReaderPaymentBinding
 import com.woocommerce.android.util.UiHelpers
 import com.woocommerce.android.viewmodel.ViewModelFactory
@@ -38,9 +37,7 @@ class CardReaderPaymentDialog : DialogFragment(R.layout.fragment_card_reader_pay
     }
 
     private fun initViewModel() {
-        val manager = (requireActivity().application as WooCommerce).cardReaderManager
-        // TODO card reader: remove !! when cardReaderManager is changed to a nonnullable type in WooCommerce
-        viewModel.start(manager!!)
+        viewModel.start()
     }
 
     private fun initObservers(binding: FragmentCardReaderPaymentBinding) {
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewModel.kt
@@ -48,6 +48,7 @@ private const val ARTIFICIAL_RETRY_DELAY = 500L
 class CardReaderPaymentViewModel @AssistedInject constructor(
     @Assisted savedState: SavedStateWithArgs,
     dispatchers: CoroutineDispatchers,
+    cardReaderManager: CardReaderManager?,
     private val logger: AppLogWrapper,
     private val orderStore: WCOrderStore
 ) : DaggerScopedViewModel(savedState, dispatchers) {
@@ -57,12 +58,13 @@ class CardReaderPaymentViewModel @AssistedInject constructor(
     private val viewState = MutableLiveData<ViewState>(LoadingDataState)
     val viewStateData: LiveData<ViewState> = viewState
 
-    private lateinit var cardReaderManager: CardReaderManager
+    // TODO remove this, and make the constructor parameter as a non nullable property when
+    //  the actual implementation is injected in release builds
+    private val cardReaderManager = cardReaderManager!!
 
     private var paymentFlowJob: Job? = null
 
-    final fun start(cardReaderManager: CardReaderManager) {
-        this.cardReaderManager = cardReaderManager
+    final fun start() {
         // TODO cardreader Check if the payment was already processed and cancel this flow
         // TODO cardreader Make sure a reader is connected
         if (paymentFlowJob == null) {
