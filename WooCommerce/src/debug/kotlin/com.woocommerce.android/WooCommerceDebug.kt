ba1ba33362bc405699cede8159c1f290d7d0c57b
malinajirka <malinajirka@gmail.com>
2021-05-12 14:54:38 -0500

    Map errors from capture payment endpoint

--- WooCommerce/src/debug/kotlin/com.woocommerce.android/WooCommerceDebug.kt
+++ WooCommerce/src/debug/kotlin/com.woocommerce.android/WooCommerceDebug.kt
@@ -13,10 +13,17 @@ import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPl
 import com.facebook.soloader.SoLoader
 import com.woocommerce.android.cardreader.CardReaderManagerFactory
 import com.woocommerce.android.cardreader.CardReaderStore
+import com.woocommerce.android.cardreader.CardReaderStore.CapturePaymentResponse
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.WooLog.T
 import dagger.hilt.android.HiltAndroidApp
+import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.CAPTURE_ERROR
+import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.GENERIC_ERROR
+import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.MISSING_ORDER
+import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.NETWORK_ERROR
+import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.PAYMENT_ALREADY_CAPTURED
+import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.SERVER_ERROR
 import org.wordpress.android.fluxc.store.WCPayStore
 import javax.inject.Inject
 
@@ -31,8 +38,17 @@ class WooCommerceDebug : WooCommerce() {
             return result.model?.token.orEmpty()
         }
 
-        override suspend fun capturePaymentIntent(orderId: Long, paymentId: String): Boolean {
-            return !payStore.capturePayment(selectedSite.get(), paymentId, orderId).isError
+        override suspend fun capturePaymentIntent(orderId: Long, paymentId: String): CapturePaymentResponse {
+            val response = payStore.capturePayment(selectedSite.get(), paymentId, orderId)
+            return when (response.error?.type) {
+                null -> CapturePaymentResponse.SUCCESS
+                GENERIC_ERROR -> CapturePaymentResponse.GENERIC_ERROR
+                PAYMENT_ALREADY_CAPTURED -> CapturePaymentResponse.PAYMENT_ALREADY_CAPTURED
+                MISSING_ORDER -> CapturePaymentResponse.MISSING_ORDER
+                CAPTURE_ERROR -> CapturePaymentResponse.CAPTURE_ERROR
+                SERVER_ERROR -> CapturePaymentResponse.SERVER_ERROR
+                NETWORK_ERROR -> CapturePaymentResponse.NETWORK_ERROR
+            }
         }
     })
 
--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/CardReaderStore.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/CardReaderStore.kt
@@ -3,5 +3,15 @@ package com.woocommerce.android.cardreader
 interface CardReaderStore {
     suspend fun getConnectionToken(): String
 
-    suspend fun capturePaymentIntent(orderId: Long, paymentId: String): Boolean
+    suspend fun capturePaymentIntent(orderId: Long, paymentId: String): CapturePaymentResponse
+
+    enum class CapturePaymentResponse {
+        SUCCESS,
+        GENERIC_ERROR,
+        PAYMENT_ALREADY_CAPTURED,
+        MISSING_ORDER,
+        CAPTURE_ERROR,
+        SERVER_ERROR,
+        NETWORK_ERROR;
+    }
 }
