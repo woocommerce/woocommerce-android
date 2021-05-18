9082e6098a3a294fe5f2b1329990981b247896be
malinajirka <malinajirka@gmail.com>
2021-05-18 12:38:54 -0500

    Map FluxC response to CardReaderModule response

--- WooCommerce/src/debug/kotlin/com.woocommerce.android/di/CardReaderModule.kt
+++ WooCommerce/src/debug/kotlin/com.woocommerce.android/di/CardReaderModule.kt
@@ -5,16 +5,11 @@ import com.woocommerce.android.cardreader.CardReaderManagerFactory
 import com.woocommerce.android.cardreader.CardReaderStore
 import com.woocommerce.android.cardreader.CardReaderStore.CapturePaymentResponse
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.util.CapturePaymentResponseMapper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.CAPTURE_ERROR
-import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.GENERIC_ERROR
-import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.MISSING_ORDER
-import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.NETWORK_ERROR
-import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.PAYMENT_ALREADY_CAPTURED
-import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.SERVER_ERROR
 import org.wordpress.android.fluxc.store.WCPayStore
 import javax.inject.Singleton
 
@@ -23,7 +18,11 @@ import javax.inject.Singleton
 class CardReaderModule {
     @Provides
     @Singleton
-    fun provideCardReaderManager(selectedSite: SelectedSite, payStore: WCPayStore): CardReaderManager {
+    fun provideCardReaderManager(
+        selectedSite: SelectedSite,
+        payStore: WCPayStore,
+        responseMapper: CapturePaymentResponseMapper
+    ): CardReaderManager {
         return CardReaderManagerFactory.createCardReaderManager(object : CardReaderStore {
             override suspend fun getConnectionToken(): String {
                 val result = payStore.fetchConnectionToken(selectedSite.get())
@@ -32,15 +31,7 @@ class CardReaderModule {
 
             override suspend fun capturePaymentIntent(orderId: Long, paymentId: String): CapturePaymentResponse {
                 val response = payStore.capturePayment(selectedSite.get(), paymentId, orderId)
-                return when (response.error?.type) {
-                    null -> CapturePaymentResponse.SUCCESS
-                    GENERIC_ERROR -> CapturePaymentResponse.GENERIC_ERROR
-                    PAYMENT_ALREADY_CAPTURED -> CapturePaymentResponse.PAYMENT_ALREADY_CAPTURED
-                    MISSING_ORDER -> CapturePaymentResponse.MISSING_ORDER
-                    CAPTURE_ERROR -> CapturePaymentResponse.CAPTURE_ERROR
-                    SERVER_ERROR -> CapturePaymentResponse.SERVER_ERROR
-                    NETWORK_ERROR -> CapturePaymentResponse.NETWORK_ERROR
-                }
+                return responseMapper.mapResponse(response)
             }
         })
     }
