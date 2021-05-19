bdb0617a6a42fc07f56f2d32c90967a3e33681ae
malinajirka <malinajirka@gmail.com>
2021-05-19 16:10:42 -0500

    Extract data classes to separate files

---
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/receipts/ReceiptData.kt
@@ -0,0 +1,23 @@
+package com.woocommerce.android.cardreader.receipts
+
+data class ReceiptData(
+    val purchasedProducts: List<ReceiptLineItem>,
+    val amount: Int,
+    val currency: String,
+    val receiptDate: String,
+    val storeName: String?,
+    val applicationPreferredName: String?,
+    val dedicatedFileName: String?, //  Also known as “AID”.
+    val cardInfo: CardInfo
+)
+
+data class CardInfo(
+    val last4CardDigits: String,
+    val brand: PaymentCardBrand
+)
+
+data class ReceiptLineItem(
+    val title: String,
+    val quantity: String,
+    val amount: String
+)
