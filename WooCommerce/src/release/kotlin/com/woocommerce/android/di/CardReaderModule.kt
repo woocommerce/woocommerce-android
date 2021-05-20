b18e63e24d6b4994b619b4df2dab9128b273e26d
Andrey <andrey.kidinov@a8c.com>
2021-05-20 17:32:55 +0300

    Revert "Workaround for release that CardReaderManager is nullable"
    This reverts commit 895fbf36

--- WooCommerce/src/release/kotlin/com/woocommerce/android/di/CardReaderModule.kt
+++ WooCommerce/src/release/kotlin/com/woocommerce/android/di/CardReaderModule.kt
@@ -1,48 +1,18 @@
 package com.woocommerce.android.di
 
-import android.app.Application
-import com.woocommerce.android.cardreader.CardPaymentStatus
-import com.woocommerce.android.cardreader.CardReader
-import com.woocommerce.android.cardreader.CardReaderDiscoveryEvents
 import com.woocommerce.android.cardreader.CardReaderManager
-import com.woocommerce.android.cardreader.CardReaderStatus
-import com.woocommerce.android.cardreader.PaymentData
-import com.woocommerce.android.cardreader.SoftwareUpdateStatus
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import java.math.BigDecimal
+import javax.annotation.Nullable
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class CardReaderModule {
     @Provides
+    @Nullable
     @Singleton
-    fun provideCardReaderManager(): CardReaderManager = object : CardReaderManager {
-        override val isInitialized: Boolean = false
-        override val readerStatus: StateFlow<CardReaderStatus> = MutableStateFlow(CardReaderStatus.NOT_CONNECTED)
-
-        override fun initialize(app: Application) {}
-
-        override fun discoverReaders(isSimulated: Boolean): Flow<CardReaderDiscoveryEvents> = flow {}
-
-        override suspend fun connectToReader(cardReader: CardReader): Boolean = false
-
-        override suspend fun collectPayment(
-            orderId: Long,
-            amount: BigDecimal,
-            currency: String
-        ): Flow<CardPaymentStatus> = flow {}
-
-        override suspend fun retryCollectPayment(orderId: Long, paymentData: PaymentData): Flow<CardPaymentStatus> =
-            flow {}
-
-        override suspend fun updateSoftware(): Flow<SoftwareUpdateStatus> = flow {}
-    }
+    fun provideCardReaderManager(): CardReaderManager? = null
 }
