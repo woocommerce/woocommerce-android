175f6c9a9712d0a1130f1d6dcdf7c8b91693ef58
Andrey <andrey.kidinov@a8c.com>
2021-05-21 11:25:07 +0300

    Fixed imports

--- WooCommerce/src/release/kotlin/com/woocommerce/android/di/CardReaderModule.kt
+++ WooCommerce/src/release/kotlin/com/woocommerce/android/di/CardReaderModule.kt
@@ -1,22 +1,33 @@
 package com.woocommerce.android.di
 
+import android.app.Application
+import com.woocommerce.android.cardreader.CardPaymentStatus
+import com.woocommerce.android.cardreader.CardReader
+import com.woocommerce.android.cardreader.CardReaderDiscoveryEvents
 import com.woocommerce.android.cardreader.CardReaderManager
+import com.woocommerce.android.cardreader.CardReaderStatus
+import com.woocommerce.android.cardreader.CardReaderStatus.NotConnected
+import com.woocommerce.android.cardreader.PaymentData
+import com.woocommerce.android.cardreader.SoftwareUpdateStatus
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.annotation.Nullable
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import java.math.BigDecimal
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class CardReaderModule {
     @Provides
-    @Nullable
     @Singleton
     fun provideCardReaderManager(): CardReaderManager = object : CardReaderManager {
         override val isInitialized: Boolean = false
-        override val readerStatus: StateFlow<CardReaderStatus> = MutableStateFlow(CardReaderStatus.NotConnected)
+        override val readerStatus: StateFlow<CardReaderStatus> = MutableStateFlow(NotConnected)
 
         override fun initialize(app: Application) {}
 
