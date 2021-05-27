991674217e2b71a746788e1d86e786416c97890a
malinajirka <malinajirka@gmail.com>
2021-05-27 13:01:51 -0500

    Remove unused imports

--- WooCommerce/src/test/kotlin/com/woocommerce/android/util/receipts/ReceiptDataMapperTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/util/receipts/ReceiptDataMapperTest.kt
@@ -1,14 +1,11 @@
 package com.woocommerce.android.util.receipts
 
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.ResourceProvider
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers.anyInt
-import org.wordpress.android.fluxc.model.SiteModel
 
 class ReceiptDataMapperTest : BaseUnitTest() {
     private lateinit var receiptDataMapper: ReceiptDataMapper
