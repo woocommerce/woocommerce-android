5119e4954212436fc7866bd07752ce19461cc91b
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-03 18:07:48 +0100

    Wire the view with the fragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelCustomsFormFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelCustomsFormFragment.kt
@@ -1,11 +1,43 @@
 package com.woocommerce.android.ui.orders.shippinglabels
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentPrintLabelCustomsFormBinding
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PrintShippingLabelCustomsFormFragment : BaseFragment(R.layout.fragment_print_label_customs_form) {
     private val viewModel: PrintShippingLabelCustomsFormViewModel by viewModels()
+
+    override fun getFragmentTitle(): String = getString(R.string.shipping_label_print_customs_form_screen_title)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentPrintLabelCustomsFormBinding.bind(view)
+        setupObservers()
+        setupView(binding)
+    }
+
+    private fun setupObservers() {
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is Exit -> findNavController().navigateUp()
+                else -> event.isHandled = false
+            }
+        }
+    }
+
+    private fun setupView(binding: FragmentPrintLabelCustomsFormBinding) {
+        binding.printButton.setOnClickListener {
+            viewModel.onPrintButtonClicked()
+        }
+        binding.saveForLaterButton.setOnClickListener {
+            viewModel.onSaveForLaterClicked()
+        }
+    }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelCustomsFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/PrintShippingLabelCustomsFormViewModel.kt
@@ -1,12 +1,33 @@
 package com.woocommerce.android.ui.orders.shippinglabels
 
+import android.os.Parcelable
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
 class PrintShippingLabelCustomsFormViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ScopedViewModel(savedStateHandle) {
+    private val navArgs: PrintShippingLabelCustomsFormFragmentArgs by savedState.navArgs()
+
+    private val _viewState = MutableLiveData(ViewState())
+    val viewState = _viewState
+
+    fun onPrintButtonClicked() {
+    }
+
+    fun onSaveForLaterClicked() {
+        triggerEvent(Exit)
+    }
+
+    @Parcelize
+    data class ViewState(
+        val isProgressDialogShown: Boolean = false
+    ) : Parcelable
 }
