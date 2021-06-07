3065aafc626aa251bef140a92780ed07120343e9
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-07 14:56:32 +0100

    Migrate AddOrderTrackingProviderListFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/di/MainActivityAggregatorModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/di/MainActivityAggregatorModule.kt
@@ -1,7 +1,6 @@
 package com.woocommerce.android.di
 
 import com.woocommerce.android.ui.aztec.AztecModule
-import com.woocommerce.android.ui.orders.OrdersModule
 import com.woocommerce.android.ui.orders.shippinglabels.ShippingLabelsModule
 import com.woocommerce.android.ui.products.ProductsModule
 import com.woocommerce.android.ui.refunds.RefundsModule
@@ -18,7 +17,6 @@ import dagger.hilt.android.components.ActivityComponent
 @InstallIn(ActivityComponent::class)
 @Module(
     includes = [
-        OrdersModule::class,
         RefundsModule::class,
         ProductsModule::class,
         ReviewsModule::class,
