0619ff9777a11f0bebb95dca8464f7597f8cb044
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-07 14:50:36 +0100

    Migrate AddOrderShipmentTrackingFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrdersModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrdersModule.kt
@@ -1,38 +1,16 @@
 package com.woocommerce.android.ui.orders
 
 import com.woocommerce.android.di.FragmentScope
-import com.woocommerce.android.ui.orders.OrdersModule.AddOrderNoteFragmentModule
-import com.woocommerce.android.ui.orders.OrdersModule.AddOrderShipmentTrackingFragmentModule
 import com.woocommerce.android.ui.orders.OrdersModule.AddOrderTrackingProviderListFragmentModule
-import com.woocommerce.android.ui.orders.notes.AddOrderNoteFragment
-import com.woocommerce.android.ui.orders.notes.AddOrderNoteModule
-import com.woocommerce.android.ui.orders.tracking.AddOrderShipmentTrackingFragment
-import com.woocommerce.android.ui.orders.tracking.AddOrderShipmentTrackingModule
 import com.woocommerce.android.ui.orders.tracking.AddOrderTrackingProviderListFragment
 import com.woocommerce.android.ui.orders.tracking.AddOrderTrackingProviderListModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module(includes = [
-    AddOrderNoteFragmentModule::class,
-    AddOrderShipmentTrackingFragmentModule::class,
     AddOrderTrackingProviderListFragmentModule::class
 ])
 object OrdersModule {
-    @Module
-    abstract class AddOrderNoteFragmentModule {
-        @FragmentScope
-        @ContributesAndroidInjector(modules = [AddOrderNoteModule::class])
-        abstract fun addOrderNoteFragment(): AddOrderNoteFragment
-    }
-
-    @Module
-    abstract class AddOrderShipmentTrackingFragmentModule {
-        @FragmentScope
-        @ContributesAndroidInjector(modules = [AddOrderShipmentTrackingModule::class])
-        abstract fun addOrderShipmentTrackingFragment(): AddOrderShipmentTrackingFragment
-    }
-
     @Module
     abstract class AddOrderTrackingProviderListFragmentModule {
         @FragmentScope
