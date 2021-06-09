5d3011aeb7d8ac08e202b109f6d31625565f8d22
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-09 16:50:52 +0100

    Migrate WPMediaPickerFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductsModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductsModule.kt
@@ -1,21 +1,7 @@
 package com.woocommerce.android.ui.products
 
-import com.woocommerce.android.di.FragmentScope
-import com.woocommerce.android.ui.products.ProductsModule.WPMediaPickerFragmentModule
-import com.woocommerce.android.ui.wpmediapicker.WPMediaPickerFragment
-import com.woocommerce.android.ui.wpmediapicker.WPMediaPickerModule
 import dagger.Module
-import dagger.android.ContributesAndroidInjector
 
-@Module(includes = [
-    WPMediaPickerFragmentModule::class
-])
-
+@Module
 object ProductsModule {
-    @Module
-    internal abstract class WPMediaPickerFragmentModule {
-        @FragmentScope
-        @ContributesAndroidInjector(modules = [WPMediaPickerModule::class])
-        abstract fun wpMediaPickerFragment(): WPMediaPickerFragment
-    }
 }
