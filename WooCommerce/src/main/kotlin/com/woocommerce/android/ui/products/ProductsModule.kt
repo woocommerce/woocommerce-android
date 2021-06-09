3db5becf6e14273af9785f3c5a0c1c93d047426e
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-09 16:46:20 +0100

    Migrate EditVariationAttributesFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductsModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductsModule.kt
@@ -1,17 +1,13 @@
 package com.woocommerce.android.ui.products
 
 import com.woocommerce.android.di.FragmentScope
-import com.woocommerce.android.ui.products.ProductsModule.EditVariationAttributesFragmentModule
 import com.woocommerce.android.ui.products.ProductsModule.WPMediaPickerFragmentModule
-import com.woocommerce.android.ui.products.variations.attributes.edit.EditVariationAttributesFragment
-import com.woocommerce.android.ui.products.variations.attributes.edit.EditVariationAttributesModule
 import com.woocommerce.android.ui.wpmediapicker.WPMediaPickerFragment
 import com.woocommerce.android.ui.wpmediapicker.WPMediaPickerModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module(includes = [
-    EditVariationAttributesFragmentModule::class,
     WPMediaPickerFragmentModule::class
 ])
 
@@ -21,12 +17,5 @@ object ProductsModule {
         @FragmentScope
         @ContributesAndroidInjector(modules = [WPMediaPickerModule::class])
         abstract fun wpMediaPickerFragment(): WPMediaPickerFragment
-    }
-
-    @Module
-    internal abstract class EditVariationAttributesFragmentModule {
-        @FragmentScope
-        @ContributesAndroidInjector(modules = [EditVariationAttributesModule::class])
-        abstract fun editVariationAttributesFragment(): EditVariationAttributesFragment
     }
 }
