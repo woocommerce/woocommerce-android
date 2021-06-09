8893c210b4bff73568a09e752b6c2b87912d637a
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-09 14:52:08 +0100

    Remove the unused property childInjector from the dialogs

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundConfirmationDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundConfirmationDialog.kt
@@ -3,14 +3,10 @@ package com.woocommerce.android.ui.refunds
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.widgets.ConfirmationDialog
-import dagger.android.DispatchingAndroidInjector
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class RefundConfirmationDialog : ConfirmationDialog() {
-    @Inject internal lateinit var childInjector: DispatchingAndroidInjector<Any>
-
     private val viewModel: IssueRefundViewModel by hiltNavGraphViewModels(R.id.nav_graph_refunds)
 
     override fun returnResult(result: Boolean) {
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundItemsPickerDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/refunds/RefundItemsPickerDialog.kt
@@ -4,14 +4,10 @@ import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import androidx.navigation.fragment.navArgs
 import com.woocommerce.android.R
 import com.woocommerce.android.widgets.NumberPickerDialog
-import dagger.android.DispatchingAndroidInjector
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class RefundItemsPickerDialog : NumberPickerDialog() {
-    @Inject internal lateinit var childInjector: DispatchingAndroidInjector<Any>
-
     private val viewModel: IssueRefundViewModel by hiltNavGraphViewModels(R.id.nav_graph_refunds)
 
     private val navArgs: RefundItemsPickerDialogArgs by navArgs()
