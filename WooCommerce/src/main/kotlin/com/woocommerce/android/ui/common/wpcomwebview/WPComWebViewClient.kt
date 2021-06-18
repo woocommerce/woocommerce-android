551595b30de9bdd487ca357e4d52b21bf7b68306
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-18 18:53:41 +0100

    Update WebView fragment to pass a url that triggers exit

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/common/wpcomwebview/WPComWebViewClient.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/common/wpcomwebview/WPComWebViewClient.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.common.wpcomwebview
 
+import android.graphics.Bitmap
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
@@ -7,5 +8,9 @@ class WPComWebViewClient(private val urlIntercepter: UrlIntercepter) : WebViewCl
     override fun onLoadResource(view: WebView?, url: String?) {
         super.onLoadResource(view, url)
         url?.let { urlIntercepter.onLoadUrl(url) }
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/common/wpcomwebview/WPComWebViewFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/common/wpcomwebview/WPComWebViewFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentWpcomWebviewBinding
@@ -46,17 +47,20 @@ class WPComWebViewFragment : BaseFragment(R.layout.fragment_wpcom_webview), UrlI
                 }
             }
             this.settings.javaScriptEnabled = true
-            settings.setUserAgentString(userAgent.getUserAgent())
+            // TODO check if it's OK to use wp-android in the user agent or not
+            // Using it will make Calypso to hide WordPress.com top navigation bar, which improves the UX
+            val userAgent = userAgent.getUserAgent().replace("wc-android", "wp-android")
+            settings.setUserAgentString(userAgent)
         }
 
-        loadAuthenticatedUrl(binding.webView, navArgs.url)
+        loadAuthenticatedUrl(binding.webView, navArgs.urlToLoad)
     }
 
     private fun loadAuthenticatedUrl(webView: WebView, urlToLoad: String) {
         val postData = getAuthenticationPostData(
-            urlToLoad = urlToLoad,
-            username = accountStore.account.userName,
-            token = accountStore.accessToken
+                urlToLoad = urlToLoad,
+                username = accountStore.account.userName,
+                token = accountStore.accessToken
         )
 
         webView.postUrl(WPCOM_LOGIN_URL, postData.toByteArray())
@@ -64,15 +68,20 @@ class WPComWebViewFragment : BaseFragment(R.layout.fragment_wpcom_webview), UrlI
 
     override fun onLoadUrl(url: String) {
         println(url)
+        navArgs.urlToTriggerExit?.let {
+            if (url.contains(it)) {
+                findNavController().navigateUp()
+            }
+        }
     }
 
     fun getAuthenticationPostData(urlToLoad: String, username: String, token: String): String {
         val utf8 = StandardCharsets.UTF_8.name()
         try {
             var postData = String.format(
-                "log=%s&redirect_to=%s",
-                URLEncoder.encode(StringUtils.notNullStr(username), utf8),
-                URLEncoder.encode(StringUtils.notNullStr(urlToLoad), utf8)
+                    "log=%s&redirect_to=%s",
+                    URLEncoder.encode(StringUtils.notNullStr(username), utf8),
+                    URLEncoder.encode(StringUtils.notNullStr(urlToLoad), utf8)
             )
 
             // Add token authorization
