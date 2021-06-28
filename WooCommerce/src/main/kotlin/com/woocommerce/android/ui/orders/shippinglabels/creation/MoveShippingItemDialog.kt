521a43d0cf4d23552120500791b89e0d31efc64b
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-28 18:16:38 +0100

    Create files for the new dialog

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/MoveShippingItemDialog.kt
@@ -0,0 +1,9 @@
+package com.woocommerce.android.ui.orders.shippinglabels.creation
+
+import androidx.fragment.app.DialogFragment
+import com.woocommerce.android.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MoveShippingItemDialog : DialogFragment(R.layout.dialog_move_shipping_item) {
+}
