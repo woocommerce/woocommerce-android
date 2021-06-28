6d38c21238e97407ef05e23c8548b2bfd9d918fd
Hafiz Rahman <hfzrhmn@gmail.com>
2021-06-28 15:31:12 +0700

    Add menu item "Done" for create custom package form.
    The item doesn't process the data yet.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingLabelCreateCustomPackageFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingLabelCreateCustomPackageFragment.kt
@@ -1,6 +1,9 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
@@ -8,6 +11,7 @@ import com.woocommerce.android.databinding.FragmentShippingLabelCreateCustomPack
 import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.model.CustomPackageType
 import com.woocommerce.android.ui.base.BaseFragment
+import org.wordpress.android.util.ActivityUtils
 
 class ShippingLabelCreateCustomPackageFragment : BaseFragment(R.layout.fragment_shipping_label_create_custom_package) {
     private var _binding: FragmentShippingLabelCreateCustomPackageBinding? = null
@@ -22,6 +26,18 @@ class ShippingLabelCreateCustomPackageFragment : BaseFragment(R.layout.fragment_
         initializeInputFields()
         setupObservers()
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_done, menu)
+    }
+
 
     private fun initializeInputFields() {
         // Initialize spinner
@@ -39,6 +55,16 @@ class ShippingLabelCreateCustomPackageFragment : BaseFragment(R.layout.fragment_
             new.customPackageType.takeIfNotEqualTo(old?.customPackageType) {
                 binding.customPackageFormType.setText(getString(it.stringResource))
             }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_done -> {
+                ActivityUtils.hideKeyboard(activity)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
