68454001060ad0839c22e8451e0994fe557c495f
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-29 19:23:20 +0100

    Prepare UI

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/MoveShippingItemDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/MoveShippingItemDialog.kt
@@ -1,9 +1,67 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation
 
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import androidx.core.view.children
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
+import com.woocommerce.android.R.string
+import com.woocommerce.android.databinding.DialogMoveShippingItemBinding
+import com.woocommerce.android.extensions.takeIfNotEqualTo
+import com.woocommerce.android.model.getTitle
+import com.woocommerce.android.ui.orders.shippinglabels.creation.MoveShippingItemViewModel.DestinationItem
+import com.woocommerce.android.ui.orders.shippinglabels.creation.MoveShippingItemViewModel.DestinationItem.ExistingPackage
+import com.woocommerce.android.ui.orders.shippinglabels.creation.MoveShippingItemViewModel.DestinationItem.NewPackage
+import com.woocommerce.android.ui.orders.shippinglabels.creation.MoveShippingItemViewModel.DestinationItem.OriginalPackage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MoveShippingItemDialog : DialogFragment(R.layout.dialog_move_shipping_item) {
+    private val viewModel: MoveShippingItemViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = DialogMoveShippingItemBinding.bind(view)
+        initUi(binding)
+        setupObservers(binding)
+    }
+
+    private fun initUi(binding: DialogMoveShippingItemBinding) {
+        val packageDescription = viewModel.currentPackage.getTitle(requireContext()) +
+            viewModel.currentPackage.selectedPackage?.let { ": ${it.title}" }
+        binding.dialogDescription.text =
+            getString(string.shipping_label_move_item_dialog_description, packageDescription)
+
+        with(viewModel.availableDestinations) {
+            forEach {
+                binding.optionsGroup.addView(it.generateRadioButton())
+            }
+        }
+    }
+
+    private fun setupObservers(binding: DialogMoveShippingItemBinding) {
+        viewModel.viewStateData.observe(viewLifecycleOwner, { old, new ->
+            new.selectedDestination?.takeIfNotEqualTo(old?.selectedDestination) { selectedItem ->
+                binding.optionsGroup.children.firstOrNull { it.tag == selectedItem }?.isSelected = true
+            }
+            new.isMoveButtonEnabled.takeIfNotEqualTo(old?.isMoveButtonEnabled) {
+                binding.moveButton.isEnabled = it
+            }
+        })
+    }
+
+    private fun DestinationItem.generateRadioButton(): RadioButton {
+        return RadioButton(requireContext()).apply {
+            setText(
+                when (this@generateRadioButton) {
+                    is ExistingPackage -> TODO()
+                    NewPackage -> R.string.shipping_label_move_item_dialog_new_package_option
+                    OriginalPackage -> R.string.shipping_label_move_item_dialog_original_packaging_option
+                }
+            )
+            tag = this@generateRadioButton
+        }
+    }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/MoveShippingItemViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/MoveShippingItemViewModel.kt
@@ -1,12 +1,52 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation
 
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.model.ShippingLabelPackage
+import com.woocommerce.android.ui.orders.shippinglabels.creation.MoveShippingItemViewModel.DestinationItem.NewPackage
+import com.woocommerce.android.ui.orders.shippinglabels.creation.MoveShippingItemViewModel.DestinationItem.OriginalPackage
+import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
 class MoveShippingItemViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
+    private val navArgs: MoveShippingItemDialogArgs by savedState.navArgs()
+
+    val viewStateData = LiveDataDelegate(savedState, ViewState())
+    private var viewState by viewStateData
+
+    val availableDestinations: List<DestinationItem>
+    val currentPackage = navArgs.currentPackage
+
+    init {
+        // TODO
+        availableDestinations = listOf(NewPackage, OriginalPackage)
+    }
+
+    @Parcelize
+    data class ViewState(
+        val selectedDestination: DestinationItem? = null
+    ) : Parcelable {
+        @IgnoredOnParcel
+        val isMoveButtonEnabled
+            get() = selectedDestination != null
+    }
+
+    sealed class DestinationItem : Parcelable {
+        @Parcelize
+        object NewPackage : DestinationItem()
+
+        @Parcelize
+        class ExistingPackage(val destinationPackage: ShippingLabelPackage) : DestinationItem()
+
+        @Parcelize
+        object OriginalPackage : DestinationItem()
+    }
 }
