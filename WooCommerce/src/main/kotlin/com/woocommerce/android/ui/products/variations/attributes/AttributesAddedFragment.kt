ffc58ae81be6b5bb7372aa124dd525aaeac27374
ThomazFB <thomazfcortez@gmail.com>
2021-06-29 22:03:24 -0300

    Define base Fragment structure for Attributes Added

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributesAddedFragment.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.products.variations.attributes
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+
+class AttributesAddedFragment : BaseFragment(R.layout.fragment_attributes_added) {
+
+}
