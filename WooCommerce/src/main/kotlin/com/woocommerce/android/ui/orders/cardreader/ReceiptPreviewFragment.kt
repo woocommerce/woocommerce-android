2334e46dd15f8cfb81b85c630779cd0a13d6d13a
malinajirka <malinajirka@gmail.com>
2021-06-30 15:02:30 -0500

    Add toolbar menu items

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/ReceiptPreviewFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/ReceiptPreviewFragment.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.orders.cardreader
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
@@ -9,12 +10,26 @@ import com.woocommerce.android.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ReceiptPreviewFragment: BaseFragment(R.layout.fragment_receipt_preview) {
+class ReceiptPreviewFragment : BaseFragment(R.layout.fragment_receipt_preview) {
     val viewModel: ReceiptPreviewViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentReceiptPreviewBinding.bind(view)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_print -> {
+                viewModel.onPrintClicked()
+                true
+            }
+            R.id.menu_send -> {
+                viewModel.onSendEmailClicked()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
