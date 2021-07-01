149bf1bca946c98462fb5fc7bc68c8e94dbefdc8
ThomazFB <thomazfcortez@gmail.com>
2021-07-01 20:55:40 -0300

    Define AttributesAddedFragment basic view binding

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributesAddedFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributesAddedFragment.kt
@@ -1,8 +1,21 @@
 package com.woocommerce.android.ui.products.variations.attributes
 
+import android.os.Bundle
+import android.view.View
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentAttributesAddedBinding
 import com.woocommerce.android.ui.base.BaseFragment
 
 class AttributesAddedFragment : BaseFragment(R.layout.fragment_attributes_added) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        FragmentAttributesAddedBinding.bind(view).apply {
+            generateVariationButton.setOnClickListener(::onGenerateVariationClicked)
+        }
+    }
+
+    private fun onGenerateVariationClicked(view: View) {
+
+    }
 }
