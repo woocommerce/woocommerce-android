553fafb9bbd145e97ae8f26d793036a8601304e5
ThomazFB <thomazfcortez@gmail.com>
2021-07-01 21:08:34 -0300

    Define structure to allow AttributesAddedFragment to generate a variation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributesAddedFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributesAddedFragment.kt
@@ -4,18 +4,58 @@ import android.os.Bundle
 import android.view.View
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentAttributesAddedBinding
-import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.extensions.takeIfNotEqualTo
+import com.woocommerce.android.ui.products.BaseProductFragment
+import com.woocommerce.android.widgets.CustomProgressDialog
 
-class AttributesAddedFragment : BaseFragment(R.layout.fragment_attributes_added) {
+class AttributesAddedFragment : BaseProductFragment(R.layout.fragment_attribute_list) {
+    companion object {
+        const val TAG: String = "AttributesAddedFragment"
+    }
+
+    private var progressDialog: CustomProgressDialog? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         FragmentAttributesAddedBinding.bind(view).apply {
             generateVariationButton.setOnClickListener(::onGenerateVariationClicked)
         }
+
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.attributeListViewStateData.observe(viewLifecycleOwner) { old, new ->
+            new.isCreatingVariationDialogShown?.takeIfNotEqualTo(old?.isCreatingVariationDialogShown) {
+                showProgressDialog(it)
+            }
+        }
     }
 
     private fun onGenerateVariationClicked(view: View) {
+        viewModel.onGenerateVariationClicked()
+    }
 
+    override fun onRequestAllowBackPress(): Boolean {
+        return false
+    }
+
+    private fun showProgressDialog(show: Boolean) {
+        if (show) {
+            hideProgressDialog()
+            progressDialog = CustomProgressDialog.show(
+                getString(R.string.variation_create_dialog_title),
+                getString(R.string.product_update_dialog_message)
+            ).also { it.show(parentFragmentManager, CustomProgressDialog.TAG) }
+            progressDialog?.isCancelable = false
+        } else {
+            hideProgressDialog()
+        }
+    }
+
+    private fun hideProgressDialog() {
+        progressDialog?.dismiss()
+        progressDialog = null
     }
 }
