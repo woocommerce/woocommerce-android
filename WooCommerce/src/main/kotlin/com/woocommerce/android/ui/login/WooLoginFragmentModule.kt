727830caab4d99ee786716794cdf985b04aba61c
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-07-06 17:33:48 +0100

    Migrate MagicLinkInterceptFragment to Hilt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/WooLoginFragmentModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/WooLoginFragmentModule.kt
@@ -1,16 +1,10 @@
 package com.woocommerce.android.ui.login
 
-import com.woocommerce.android.di.FragmentScope
 import dagger.Module
-import dagger.android.ContributesAndroidInjector
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import org.wordpress.android.login.di.LoginFragmentModule
 
 @Module(includes = [LoginFragmentModule::class])
 @InstallIn(ActivityComponent::class)
-internal abstract class WooLoginFragmentModule {
-    @FragmentScope
-    @ContributesAndroidInjector(modules = [MagicLinkInterceptModule::class])
-    internal abstract fun magicLinkInterceptFragment(): MagicLinkInterceptFragment
-}
+internal abstract class WooLoginFragmentModule
