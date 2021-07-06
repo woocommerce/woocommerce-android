895cd851d9342d291f93f0f6811f1cc93075e05f
Nick Bradbury <nick.bradbury@gmail.com>
2021-07-06 08:57:58 -0400

    Added pageCount attribute to custom indicator

--- WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/WCViewPagerIndicator.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/WCViewPagerIndicator.kt
@@ -4,25 +4,38 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.content.res.use
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.login.LoginPrologueViewPager
 
 /**
  * Simple list of oval images used below a view pager to indicate which page is selected
  */
-class WCViewPagerIndicator : LinearLayout {
+class WCViewPagerIndicator @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
     interface OnIndicatorClickedListener {
         fun onIndicatorClicked(index: Int)
     }
 
     private val indicators = ArrayList<ImageView>()
     private var listener: OnIndicatorClickedListener? = null
+    private var pageCount: Int = 0
 
     init {
+        if (attrs != null) {
+            context.obtainStyledAttributes(attrs, R.styleable.WCViewPagerIndicator).use {
+                pageCount = it.getInt(R.styleable.WCViewPagerIndicator_pageCount, 0)
+            }
+        }
+
         orientation = HORIZONTAL
         val padding = context.resources.getDimensionPixelSize(R.dimen.margin_small)
 
-        for (i in 0 until LoginPrologueViewPager.NUM_PAGES) {
+        for (i in 0 until pageCount) {
             ImageView(context).also { imageView ->
                 imageView.setImageResource(R.drawable.ic_tab_indicator)
                 imageView.isSelected = i == 0
@@ -36,10 +49,6 @@ class WCViewPagerIndicator : LinearLayout {
             }
         }
     }
-
-    constructor(context: Context) : super(context)
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
     fun setSelectedIndicator(index: Int) {
         for (i in 0 until LoginPrologueViewPager.NUM_PAGES) {
