840db4329c68fcccac6bd97915da203cb658f845
anitaa1990 <murthyanitaa@gmail.com>
2021-07-07 17:53:32 +0530

    Added logic to display errors in the new screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/MediaUploadErrorsFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.woocommerce.android.ui.products
-
-import com.woocommerce.android.R
-import com.woocommerce.android.ui.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class MediaUploadErrorsFragment : BaseFragment(R.layout.fragment_media_upload_errors)
