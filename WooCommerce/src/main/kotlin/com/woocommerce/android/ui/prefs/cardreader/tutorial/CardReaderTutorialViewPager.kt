78bec845a09f4a3be6eb298bada525232c718d6c
Nick Bradbury <nick.bradbury@gmail.com>
2021-07-07 09:12:29 -0400

    Comment out the reconnection step since it's not supported yet

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/tutorial/CardReaderTutorialViewPager.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/tutorial/CardReaderTutorialViewPager.kt
@@ -19,25 +19,25 @@ class CardReaderTutorialViewPager : WCViewPager {
 
     private inner class ViewPagerAdapter(fm: FragmentManager) :
         FragmentPagerAdapter(fm) {
-        // TODO dark vectors
+        // note that the "sleep" step is skipped for now, since we don't support reconnection yet
         private val drawableIds = arrayOf(
             R.drawable.img_wc_ship_payment,
             R.drawable.img_wc_ship_card_insert,
-            R.drawable.img_wc_ship_card_sleep,
+            // R.drawable.img_wc_ship_card_sleep,
             R.drawable.img_wc_ship_card_charge
         )
 
         private val labelIds = arrayOf(
             R.string.card_reader_tutorial_connected_label,
             R.string.card_reader_tutorial_collect_label,
-            R.string.card_reader_tutorial_reconnect_label,
+            // R.string.card_reader_tutorial_reconnect_label,
             R.string.card_reader_tutorial_charged_label
         )
 
         private val detailIds = arrayOf(
             R.string.card_reader_tutorial_connected_detail,
             R.string.card_reader_tutorial_collect_detail,
-            R.string.card_reader_tutorial_reconnect_detail,
+            // R.string.card_reader_tutorial_reconnect_detail,
             R.string.card_reader_tutorial_charged_detail
         )
 
