a10a8397360cff99a7e601c7a0ee9030a1e6bf44
malinajirka <malinajirka@gmail.com>
2021-07-09 15:45:48 +0200

    Hide collect payment button on refunded orders

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentCollectibilityChecker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentCollectibilityChecker.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.orders.cardreader
 import com.woocommerce.android.extensions.CASH_ON_DELIVERY_PAYMENT_TYPE
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
+import java.math.BigDecimal
 import javax.inject.Inject
 
 class CardReaderPaymentCollectibilityChecker @Inject constructor(
@@ -13,6 +14,9 @@ class CardReaderPaymentCollectibilityChecker @Inject constructor(
             currency.equals("USD", ignoreCase = true) &&
                 (listOf(Order.Status.Pending, Order.Status.Processing, Order.Status.OnHold)).any { it == status } &&
                 !isOrderPaid &&
+                // TODO cardreader remove the following check when the backend issue is fixed
+                // https://github.com/Automattic/woocommerce-payments/issues/2390
+                order.refundTotal == BigDecimal.ZERO &&
                 // Empty payment method explanation:
                 // https://github.com/woocommerce/woocommerce/issues/29471
                 (paymentMethod == CASH_ON_DELIVERY_PAYMENT_TYPE || paymentMethod.isEmpty()) &&
