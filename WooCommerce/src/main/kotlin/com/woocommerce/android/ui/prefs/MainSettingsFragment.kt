892b211398d7d9b35abe0d2d6abb55df62dda2b5
malinajirka <malinajirka@gmail.com>
2021-07-13 14:26:48 +0200

    Revert import removal

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsFragment.kt
@@ -38,6 +38,7 @@ import com.woocommerce.android.ui.sitepicker.SitePickerActivity
 import com.woocommerce.android.util.AnalyticsUtils
 import com.woocommerce.android.util.AppThemeUtils
 import com.woocommerce.android.util.ChromeCustomTabUtils
+import com.woocommerce.android.util.ThemeOption
 import com.woocommerce.android.util.FeatureFlag.CARD_READER
 import com.woocommerce.android.util.FeatureFlag.CARD_READER_ONBOARDING
 import com.woocommerce.android.widgets.WCPromoTooltip
