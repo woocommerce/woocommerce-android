75de067f47fe7c0e82333f0d6319698d3185bd0b
anitaa1990 <murthyanitaa@gmail.com>
2021-07-13 15:54:48 +0530

    Added logic to redirect to media upload error screen when the snackbar is clicked

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/media/MediaUploadErrorListFragment.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.media
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MediaUploadErrorListFragment : BaseFragment(R.layout.fragment_media_upload_error_list)
