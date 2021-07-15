27f2febeda14e6ae6818b65c445444cac9da8222
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-07-15 10:57:57 +0100

    Update the condition to check if packages are empty

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/ShippingLabelRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/ShippingLabelRepository.kt
@@ -121,7 +121,7 @@ class ShippingLabelRepository @Inject constructor(
             carrierRates.model == null || carrierRates.model!!.packageRates.isEmpty() -> {
                 WooResult(WooError(INVALID_RESPONSE, GenericErrorType.PARSE_ERROR, "Empty response"))
             }
-            carrierRates.model!!.packageRates.all { pack ->
+            carrierRates.model!!.packageRates.any { pack ->
                 pack.shippingOptions.isEmpty() || pack.shippingOptions.all { option ->
                     option.rates.isEmpty()
                 }
