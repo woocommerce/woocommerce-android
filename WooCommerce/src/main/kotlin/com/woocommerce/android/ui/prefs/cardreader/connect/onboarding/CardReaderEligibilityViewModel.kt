8c67159a9c701e5dfc4fe79120464b0e147e3672
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-07-16 10:25:45 -0400

    Add support for ViewStates. Add the first one: CountryNotSupportedState

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/onboarding/CardReaderEligibilityViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/onboarding/CardReaderEligibilityViewModel.kt
@@ -1,8 +1,12 @@
 package com.woocommerce.android.ui.prefs.cardreader.connect.onboarding
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
-import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
-import com.woocommerce.android.cardreader.CardReaderManager
+import com.woocommerce.android.R
+import com.woocommerce.android.model.UiString
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -11,5 +15,35 @@ import javax.inject.Inject
 class CardReaderEligibilityViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
+    private val viewState = MutableLiveData<ViewState>(ViewState.CountryNotSupportedState("US"))
+    val viewStateData: LiveData<ViewState> = viewState
 
+    sealed class ViewState(
+        val headerLabel: UiString? = null,
+        @DrawableRes val illustration: Int? = null,
+        val hintLabel: UiString? = null,
+        val contactSupportLabel: UiString? = null,
+        val learnMoreLabel: UiString? = null
+    ) {
+        data class CountryNotSupportedState(
+            val countryCode: String,
+        ) : ViewState(
+            headerLabel = UiString.UiStringRes(
+                stringRes = R.string.card_reader_eligibility_country_not_supported_header,
+                params = listOf(UiString.UiStringText(" HC: United States"))
+            ),
+            illustration = R.drawable.img_products_error,
+            hintLabel = UiString.UiStringRes(
+               stringRes = R.string.card_reader_eligibility_country_not_supported_hint
+            ),
+            contactSupportLabel = UiString.UiStringRes(
+                stringRes = R.string.card_reader_eligibility_country_not_supported_contact_support,
+                containsHtml = true
+            ),
+            learnMoreLabel = UiString.UiStringRes(
+                stringRes = R.string.card_reader_eligibility_country_not_supported_learn_more,
+                containsHtml = true
+            )
+        )
+    }
 }
