64c42f7480d5e64106d8a50b0b0d915cc7f7d00c
ThomazFB <thomazfcortez@gmail.com>
2021-07-15 23:31:44 -0300

    Adjust method cyclomatic complexity

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -18,14 +18,11 @@ enum class FeatureFlag {
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
             SHIPPING_LABELS_M4 -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
-            DB_DOWNGRADE -> {
-                PackageUtils.isDebugBuild() || context != null && PackageUtils.isBetaBuild(context)
-            }
-            ORDER_CREATION -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
+            DB_DOWNGRADE -> { PackageUtils.isDebugBuild() || context != null && PackageUtils.isBetaBuild(context) }
+            ORDER_CREATION, PRODUCT_ADD_ONS -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
             CARD_READER -> CardPresentEligibleFeatureChecker.isCardPresentEligible.get()
             CARD_READER_RECONNECTION -> CARD_READER.isEnabled() && PackageUtils.isDebugBuild()
             CARD_READER_ONBOARDING -> CARD_READER.isEnabled() && PackageUtils.isDebugBuild()
-            PRODUCT_ADD_ONS -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
         }
     }
 
