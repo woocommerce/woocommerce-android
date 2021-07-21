e3b691e3e6c521bd2082fb5647db672f82d09e02
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-07-21 17:16:18 -0400

    Declare action lambdas

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/onboarding/CardReaderEligibilityViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/onboarding/CardReaderEligibilityViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.R
 import com.woocommerce.android.model.UiString
+import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -15,8 +16,18 @@ import javax.inject.Inject
 class CardReaderEligibilityViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
-    private val viewState = MutableLiveData<ViewState>(ViewState.CountryNotSupportedState("US"))
+    private val viewState = MutableLiveData<ViewState>(ViewState.CountryNotSupportedState("US",
+        ::onContactSupportClicked,
+        ::onLearnMoreClicked))
     val viewStateData: LiveData<ViewState> = viewState
+
+    private fun onContactSupportClicked() {
+
+    }
+
+    private fun onLearnMoreClicked() {
+
+    }
 
     sealed class ViewState(
         val headerLabel: UiString? = null,
@@ -25,8 +36,13 @@ class CardReaderEligibilityViewModel @Inject constructor(
         val contactSupportLabel: UiString? = null,
         val learnMoreLabel: UiString? = null
     ) {
+        open val onContactSupportActionClicked: (() -> Unit)? = null
+        open val onLearnMoreActionClicked: (() -> Unit)? = null
+
         data class CountryNotSupportedState(
             val countryCode: String,
+            override val onContactSupportActionClicked: (() -> Unit),
+            override val onLearnMoreActionClicked: (() -> Unit),
         ) : ViewState(
             headerLabel = UiString.UiStringRes(
                 stringRes = R.string.card_reader_eligibility_country_not_supported_header,
