cf1d9863cc8db772b92b5831c2cce94176ad727f
Nick Bradbury <nick.bradbury@gmail.com>
2021-07-23 05:51:13 -0400

    Use `animateLayoutChanges` rather than explicit fade animation, simplified layout

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/onboarding/CardReaderOnboardingFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/onboarding/CardReaderOnboardingFragment.kt
@@ -1,7 +1,5 @@
 package com.woocommerce.android.ui.prefs.cardreader.onboarding
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +9,6 @@ import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentCardReaderOnboardingBinding
 import com.woocommerce.android.extensions.navigateBackWithNotice
 import com.woocommerce.android.ui.base.BaseFragment
-import com.woocommerce.android.util.WooAnimUtils
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +20,6 @@ class CardReaderOnboardingFragment : BaseFragment(R.layout.fragment_card_reader_
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentCardReaderOnboardingBinding.bind(view)
         initObservers(binding)
-        // showOnboardingLayout(binding, R.layout.fragment_card_reader_onboarding_loading)
     }
 
     private fun initObservers(binding: FragmentCardReaderOnboardingBinding) {
@@ -46,30 +42,8 @@ class CardReaderOnboardingFragment : BaseFragment(R.layout.fragment_card_reader_
     }
 
     private fun showOnboardingLayout(binding: FragmentCardReaderOnboardingBinding, @LayoutRes layoutRes: Int) {
-        val duration = WooAnimUtils.Duration.LONG
-
-        // create a fade-in animation which inflates the desired layout when started
-        val fadeInAnim = WooAnimUtils.getFadeInAnim(binding.container, duration)
-        fadeInAnim.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationStart(animation: Animator?) {
-                LayoutInflater.from(requireActivity()).inflate(layoutRes, binding.container, true)
-            }
-        })
-
-        // if there are existing views we need to fade them out then start the fade-in animation,
-        // otherwise we just start the fade-in animation which will inflate the layout
-        if (binding.container.childCount > 0) {
-            val fadeOutAnim = WooAnimUtils.getFadeOutAnim(binding.container, duration)
-            fadeOutAnim.addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    binding.container.removeAllViews()
-                    fadeInAnim.start()
-                }
-            })
-            fadeOutAnim.start()
-        } else {
-            fadeInAnim.start()
-        }
+        binding.container.removeAllViews()
+        LayoutInflater.from(requireActivity()).inflate(layoutRes, binding.container, true)
     }
 
     override fun getFragmentTitle() = resources.getString(R.string.payment_onboarding_title)
