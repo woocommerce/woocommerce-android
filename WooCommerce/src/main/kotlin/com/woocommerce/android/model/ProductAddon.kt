7a2d3cfbb26bf866fea1f5769d8aa8e4e6751f7c
ThomazFB <thomazfcortez@gmail.com>
2021-07-23 19:41:42 -0300

    Complete addons data models info parsing

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/ProductAddon.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/ProductAddon.kt
@@ -3,6 +3,11 @@ package com.woocommerce.android.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import org.wordpress.android.fluxc.model.addons.WCProductAddonModel
+import org.wordpress.android.fluxc.model.addons.WCProductAddonModel.AddOnPriceType
+import org.wordpress.android.fluxc.model.addons.WCProductAddonModel.AddOnType
+import org.wordpress.android.fluxc.model.addons.WCProductAddonModel.AddOnDisplay
+import org.wordpress.android.fluxc.model.addons.WCProductAddonModel.AddOnRestrictionsType
+import org.wordpress.android.fluxc.model.addons.WCProductAddonModel.AddOnTitleFormat
 
 @Parcelize
 data class ProductAddon(
@@ -12,7 +17,24 @@ data class ProductAddon(
     val max: String,
     val min: String,
     val position: String,
-    val price: String
+    val price: String,
+    val adjustPrice: String,
+    val required: String,
+    val restrictions: String,
+    val titleFormat: AddOnTitleFormat?,
+    val restrictionsType: AddOnRestrictionsType?,
+    val priceType: AddOnPriceType?,
+    val type: AddOnType?,
+    val display: AddOnDisplay?,
+    val options: List<ProductAddonOption>
+) : Parcelable
+
+@Parcelize
+data class ProductAddonOption(
+    val priceType: AddOnPriceType?,
+    val label: String,
+    val price: String,
+    val image: String
 ) : Parcelable
 
 fun WCProductAddonModel.toAppModel() =
@@ -23,5 +45,22 @@ fun WCProductAddonModel.toAppModel() =
         max = this.max ?: "",
         min = this.min ?: "",
         position = this.position ?: "",
-        price = this.price ?: ""
+        price = this.price ?: "",
+        adjustPrice = this.adjustPrice ?: "",
+        required = this.required ?: "",
+        restrictions = this.restrictions ?: "",
+        titleFormat = this.titleFormat,
+        restrictionsType = this.restrictionsType,
+        priceType = this.priceType,
+        type = this.type,
+        display = this.display,
+        options = this.options?.map { it.toAppModel() } ?: emptyList()
+    )
+
+fun WCProductAddonModel.ProductAddonOption.toAppModel() =
+    ProductAddonOption(
+        priceType = this.priceType,
+        label = this.label ?: "",
+        price = this.price ?: "",
+        image = this.image ?: ""
     )
