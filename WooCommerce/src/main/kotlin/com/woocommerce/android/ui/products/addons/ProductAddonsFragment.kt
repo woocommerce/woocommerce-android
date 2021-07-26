120b9ed3c92ebfa956e96af9d0addbac1934b82a
ThomazFB <thomazfcortez@gmail.com>
2021-07-26 20:56:50 -0300

    Configure Addon Adapter to Addon RecyclerView

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/ProductAddonsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/ProductAddonsFragment.kt
@@ -1,9 +1,30 @@
 package com.woocommerce.android.ui.products.addons
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentProductAddonsBinding
 import com.woocommerce.android.ui.products.BaseProductFragment
 
 class ProductAddonsFragment : BaseProductFragment(R.layout.fragment_product_addons) {
+    companion object {
+        const val TAG = "ProductAddonsFragment"
+    }
+
+    private var layoutManager: LayoutManager? = null
+
+    private var _binding: FragmentProductAddonsBinding? = null
+    private val binding get() = _binding!!
+
+    private val storedAddons
+        get() = viewModel.getProduct().storedProduct?.addons
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        storedAddons?.let { binding.addonsList.adapter = AddonListAdapter(it) }
+    }
+
     override fun onRequestAllowBackPress(): Boolean {
         TODO("Not yet implemented")
     }
