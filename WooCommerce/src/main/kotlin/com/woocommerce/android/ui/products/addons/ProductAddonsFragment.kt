d2b78c8f1d67196934761a860a3a3f38f97a5a2c
ThomazFB <thomazfcortez@gmail.com>
2021-07-26 20:36:47 -0300

    Create base scaffold for Product Addons fragment

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/ProductAddonsFragment.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.ui.products.addons
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.products.BaseProductFragment
+
+class ProductAddonsFragment : BaseProductFragment(R.layout.fragment_product_addons) {
+    override fun onRequestAllowBackPress(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
