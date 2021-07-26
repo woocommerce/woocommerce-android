a70b97238f737d158a5821b6bc239cc264e5fba0
malinajirka <malinajirka@gmail.com>
2021-07-26 10:54:59 +0200

    Merge CardReaderEligibilityFragment and CardReaderOnboardingFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/onboarding/CardReaderOnboardingFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/onboarding/CardReaderOnboardingFragment.kt
@@ -3,12 +3,14 @@ package com.woocommerce.android.ui.prefs.cardreader.onboarding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import androidx.annotation.LayoutRes
 import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentCardReaderOnboardingBinding
+import com.woocommerce.android.databinding.FragmentCardReaderOnboardingLoadingBinding
+import com.woocommerce.android.databinding.FragmentCardReaderOnboardingUnsupportedCountryBinding
 import com.woocommerce.android.extensions.navigateBackWithNotice
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.util.UiHelpers
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -36,14 +38,60 @@ class CardReaderOnboardingFragment : BaseFragment(R.layout.fragment_card_reader_
         viewModel.viewStateData.observe(
             viewLifecycleOwner,
             { state ->
-                showOnboardingLayout(binding, state.layoutRes)
+                showOnboardingLayout(binding, state)
             }
         )
     }
 
-    private fun showOnboardingLayout(binding: FragmentCardReaderOnboardingBinding, @LayoutRes layoutRes: Int) {
+    private fun showOnboardingLayout(
+        binding: FragmentCardReaderOnboardingBinding,
+        state: CardReaderOnboardingViewModel.OnboardingViewState
+    ) {
         binding.container.removeAllViews()
-        LayoutInflater.from(requireActivity()).inflate(layoutRes, binding.container, true)
+        val layout = LayoutInflater.from(requireActivity()).inflate(state.layoutRes, binding.container, false)
+        binding.container.addView(layout)
+        when (state) {
+            is CardReaderOnboardingViewModel.OnboardingViewState.GenericErrorState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.LoadingState ->
+                showLoadingState(layout, state)
+            is CardReaderOnboardingViewModel.OnboardingViewState.NoConnectionErrorState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.UnsupportedCountryState ->
+                showCountryNotSupportedState(layout, state)
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayAccountOverdueRequirementsState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayAccountPendingRequirementsState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayAccountRejectedState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayAccountUnderReviewState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayInTestModeWithLiveAccountState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayNotActivatedState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayNotInstalledState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayNotSetupState -> TODO()
+            is CardReaderOnboardingViewModel.OnboardingViewState.WCPayUnsupportedVersionState -> TODO()
+        }
+    }
+
+    private fun showLoadingState(
+        view: View,
+        state: CardReaderOnboardingViewModel.OnboardingViewState.LoadingState
+    ) {
+        val binding = FragmentCardReaderOnboardingLoadingBinding.bind(view)
+    }
+
+    private fun showCountryNotSupportedState(
+        view: View,
+        state: CardReaderOnboardingViewModel.OnboardingViewState.UnsupportedCountryState
+    ) {
+        val binding = FragmentCardReaderOnboardingUnsupportedCountryBinding.bind(view)
+        UiHelpers.setTextOrHide(binding.eligibilityHeader, state.headerLabel)
+        UiHelpers.setImageOrHide(binding.eligibilityIllustration, state.illustration)
+        UiHelpers.setTextOrHide(binding.eligibilityHint, state.hintLabel)
+        UiHelpers.setTextOrHide(binding.eligibilityHelp, state.contactSupportLabel)
+        UiHelpers.setTextOrHide(binding.eligibilityLearnMore, state.learnMoreLabel)
+        binding.eligibilityHelp.setOnClickListener {
+            state.onContactSupportActionClicked?.invoke()
+        }
+        binding.eligibilityLearnMore.setOnClickListener {
+            state.onLearnMoreActionClicked?.invoke()
+        }
     }
 
     override fun getFragmentTitle() = resources.getString(R.string.card_reader_onboarding_title)
