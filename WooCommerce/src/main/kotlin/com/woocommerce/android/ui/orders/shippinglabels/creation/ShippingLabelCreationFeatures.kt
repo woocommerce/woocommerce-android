6a59a28e66330ceb1b8659617eccd63eaabb87b3
hicham <hicham.boushaba@outlook.com>
2021-07-30 11:35:05 +0100

    Enable adding payment methods in shipping labels form

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingLabelCreationFeatures.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingLabelCreationFeatures.kt
@@ -1,9 +1,7 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation
 
-import com.woocommerce.android.util.FeatureFlag
-
 object ShippingLabelCreationFeatures {
-    val CAN_CREATE_PAYMENT_METHOD = FeatureFlag.SHIPPING_LABELS_M4.isEnabled()
+    const val CAN_CREATE_PAYMENT_METHOD = true
     const val CAN_CREATE_PACKAGE = true
     const val CAN_CREATE_CUSTOMS_FORM = true
 }
