7965183b4a98e0ef5d4d9c8279f1b7ded2dc01ef
hicham <hicham.boushaba@outlook.com>
2021-07-30 12:30:02 +0100

    Remove the shipping label features class

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingLabelCreationFeatures.kt
+++
@@ -1,7 +0,0 @@
-package com.woocommerce.android.ui.orders.shippinglabels.creation
-
-object ShippingLabelCreationFeatures {
-    const val CAN_CREATE_PAYMENT_METHOD = true
-    const val CAN_CREATE_PACKAGE = true
-    const val CAN_CREATE_CUSTOMS_FORM = true
-}
