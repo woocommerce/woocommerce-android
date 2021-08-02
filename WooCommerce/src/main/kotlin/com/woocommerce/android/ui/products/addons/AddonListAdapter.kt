46ac952e424862f2fd9339404e3b57c74b7b1fc8
ThomazFB <thomazfcortez@gmail.com>
2021-08-02 16:20:25 -0300

    Refactor VH import for AddonListAdapter

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/AddonListAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/AddonListAdapter.kt
@@ -3,10 +3,11 @@ package com.woocommerce.android.ui.products.addons
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.woocommerce.android.model.ProductAddon
+import com.woocommerce.android.ui.products.addons.AddonListAdapter.AddonsViewHolder
 
 class AddonListAdapter(
     val addons: List<ProductAddon>
-) : RecyclerView.Adapter<AddonListAdapter.AddonsViewHolder>() {
+) : RecyclerView.Adapter<AddonsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         AddonsViewHolder(ProductAddonCard(parent.context))
 
