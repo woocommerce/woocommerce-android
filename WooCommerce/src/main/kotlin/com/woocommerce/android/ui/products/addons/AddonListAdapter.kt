6a172e76c1acd9a2f1a91bf1a9ad67bef871fa9c
ThomazFB <thomazfcortez@gmail.com>
2021-08-02 15:54:35 -0300

    Adjust AddonListAdapter to connect with AddonCard

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/AddonListAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/AddonListAdapter.kt
@@ -1,34 +1,22 @@
 package com.woocommerce.android.ui.products.addons
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.woocommerce.android.databinding.AddonItemBinding
 import com.woocommerce.android.model.ProductAddon
 
 class AddonListAdapter(
     val addons: List<ProductAddon>
 ) : RecyclerView.Adapter<AddonListAdapter.AddonsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        AddonsViewHolder(
-            AddonItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+        AddonsViewHolder(AddonCard(parent.context))
 
     override fun onBindViewHolder(holder: AddonsViewHolder, position: Int) {
-        holder.bind(addons[position])
+        holder.addonCard.bind(addons[position])
     }
 
     override fun getItemCount() = addons.size
 
     inner class AddonsViewHolder(
-        val viewBinding: AddonItemBinding
-    ) : RecyclerView.ViewHolder(viewBinding.root) {
-        fun bind(addon: ProductAddon) {
-            viewBinding.addonName.text = addon.name
-        }
-    }
+        val addonCard: AddonCard
+    ) : RecyclerView.ViewHolder(addonCard)
 }
