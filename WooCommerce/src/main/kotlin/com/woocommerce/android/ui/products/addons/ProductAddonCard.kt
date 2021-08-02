1a741e580d7650fabad9ac107447baa58df63381
ThomazFB <thomazfcortez@gmail.com>
2021-08-02 16:30:59 -0300

    Initialize options list inside ProductAddonCard

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/ProductAddonCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/ProductAddonCard.kt
@@ -4,23 +4,36 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.woocommerce.android.databinding.ProductAddonCardBinding
 import com.woocommerce.android.model.ProductAddon
+import com.woocommerce.android.ui.products.addons.options.AddonOptionListAdapter
 
 class ProductAddonCard @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
-    private val binding =
+    private val binding: ProductAddonCardBinding =
         ProductAddonCardBinding.inflate(
             LayoutInflater.from(context),
             this,
             true
         )
 
-    fun bind(addon: ProductAddon) {
-        binding.name.text = addon.name
-        binding.description.text = addon.description
+    init {
+        binding.optionsList.layoutManager =
+            LinearLayoutManager(
+                context,
+                RecyclerView.VERTICAL,
+                false
+            )
+    }
+
+    fun bind(addon: ProductAddon) = with(binding) {
+        name.text = addon.name
+        description.text = addon.description
+        optionsList.adapter = AddonOptionListAdapter(addon.options)
     }
 }
