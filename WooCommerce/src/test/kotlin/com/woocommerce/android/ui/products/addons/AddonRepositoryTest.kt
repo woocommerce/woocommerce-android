2caba60540d5f716791c6be87aa437656cc847dc
ThomazFB <thomazfcortez@gmail.com>
2021-08-12 20:38:52 -0300

    Create full code test coverage for AddonRepository

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/addons/AddonRepositoryTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/addons/AddonRepositoryTest.kt
@@ -4,10 +4,8 @@ import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import com.woocommerce.android.extensions.unwrap
-import com.woocommerce.android.model.Order.Item.Attribute
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.products.addons.AddonTestFixtures.defaultProduct
-import com.woocommerce.android.ui.products.addons.AddonTestFixtures.defaultProductAddon
 import com.woocommerce.android.ui.products.addons.AddonTestFixtures.defaultWCOrderItemList
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
@@ -49,29 +47,54 @@ class AddonRepositoryTest {
             productStoreMock,
             selectedSiteMock
         )
-
-        configureOrderResponse()
-        configureProductResponse()
     }
 
     @Test
-    fun `test`() {
-        val expectedAttributeList = listOf(
-            Attribute("test-key", "test-value")
-        )
+    fun `fetchOrderAddonsData should return both order addons and product addons data`() {
+        configureSuccessfulOrderResponse()
+        configureSuccessfulProductResponse()
 
-        val expectedAddonList = listOf(
-            defaultProductAddon.copy(name = "test-addon-name")
-        )
-
-        repositoryUnderTest.fetchOrderAddonsData(remoteOrderID, remoteProductID)
-            ?.unwrap { addons, attributes ->
-                assertThat(addons).isNotEmpty
-                assertThat(attributes).isNotEmpty
-            } ?: fail()
+        repositoryUnderTest.fetchOrderAddonsData(123, 333)
+            ?.unwrap { productAddons, orderAddons ->
+                assertThat(productAddons).isNotEmpty
+                assertThat(orderAddons).isNotEmpty
+            } ?: fail("non-null Pair with valid data was expected")
     }
 
-    private fun configureOrderResponse() {
+    @Test
+    fun `fetchOrderAddonsData should map and filter orderAddons keys as List correctly`() {
+        configureSuccessfulOrderResponse()
+        configureSuccessfulProductResponse()
+
+        val expectedStrings = listOf(
+            "Topping ($3,00)",
+            "Topping ($4,00)",
+            "Topping ($3,00)",
+            "Soda ($8,00)",
+            "Delivery ($5,00)"
+        )
+
+        repositoryUnderTest.fetchOrderAddonsData(123, 333)
+            ?.unwrap { _, orderAddons ->
+                assertThat(orderAddons).isEqualTo(expectedStrings)
+            } ?: fail("non-null Pair with valid data was expected")
+    }
+
+    @Test
+    fun `fetchOrderAddonsData should return null if product addon data fails`() {
+        configureSuccessfulOrderResponse()
+        val response = repositoryUnderTest.fetchOrderAddonsData(123, 333)
+        assertThat(response).isNull()
+    }
+
+    @Test
+    fun `fetchOrderAddonsData should return null if order addon data fails`() {
+        configureSuccessfulProductResponse()
+        val response = repositoryUnderTest.fetchOrderAddonsData(123, 333)
+        assertThat(response).isNull()
+    }
+
+    private fun configureSuccessfulOrderResponse() {
         mock<WCOrderModel>().apply {
             whenever(getLineItemList()).thenReturn(defaultWCOrderItemList)
         }.let {
@@ -83,7 +106,7 @@ class AddonRepositoryTest {
         }
     }
 
-    private fun configureProductResponse() {
+    private fun configureSuccessfulProductResponse() {
         whenever(
             productStoreMock.getProductByRemoteId(
                 siteModelMock, remoteProductID
