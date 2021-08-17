6c49b733ec2fa51d868fc2d4c0a7467201fa2195
ThomazFB <thomazfcortez@gmail.com>
2021-08-17 20:50:25 -0300

    Connect View add-ons tap event

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderDetailProductItemView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/OrderDetailProductItemView.kt
@@ -15,6 +15,8 @@ import com.woocommerce.android.util.StringUtils
 import org.wordpress.android.util.PhotonUtils
 import java.math.BigDecimal
 
+typealias ViewAddonClickListener = (Order.Item) -> Unit
+
 class OrderDetailProductItemView @JvmOverloads constructor(
     ctx: Context,
     attrs: AttributeSet? = null,
@@ -25,7 +27,8 @@ class OrderDetailProductItemView @JvmOverloads constructor(
     fun initView(
         item: Order.Item,
         productImage: String?,
-        formatCurrencyForDisplay: (BigDecimal) -> String
+        formatCurrencyForDisplay: (BigDecimal) -> String,
+        onViewAddonsClick: ViewAddonClickListener?
     ) {
         binding.productInfoName.text = item.name
 
