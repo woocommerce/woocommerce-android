f7b7867cfa9c7609bc6a62ecbaf9c4ae4f216a8c
ThomazFB <thomazfcortez@gmail.com>
2021-08-18 17:05:36 -0300

    Create OrderedAddonFragment scaffold

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/order/OrderedAddonFragment.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.ui.products.addons.order
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OrderedAddonFragment : BaseFragment(R.layout.fragment_ordered_addon) {
+
+}
