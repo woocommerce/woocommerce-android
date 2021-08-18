0b98508ff31c7348611760cf1718f46230ccabde
ThomazFB <thomazfcortez@gmail.com>
2021-08-18 17:52:58 -0300

    Serve OrderedAddonFragment navigation arguments to viewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/order/OrderedAddonFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/order/OrderedAddonFragment.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.products.addons.order
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentOrderedAddonBinding
 import com.woocommerce.android.ui.base.BaseFragment
@@ -16,11 +17,18 @@ class OrderedAddonFragment : BaseFragment(R.layout.fragment_ordered_addon) {
 
     private val viewModel: OrderedAddonViewModel by viewModels()
 
+    private val navArgs: OrderedAddonFragmentArgs by navArgs()
+
     private var _binding: FragmentOrderedAddonBinding? = null
     private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentOrderedAddonBinding.bind(view)
+        viewModel.start(
+            navArgs.orderId,
+            navArgs.orderItemId,
+            navArgs.addonsProductId
+        )
     }
 }
