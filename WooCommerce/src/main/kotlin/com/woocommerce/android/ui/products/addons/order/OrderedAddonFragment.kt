11e6ba31183f4e031b9a09a48b50a3d05a10cb45
ThomazFB <thomazfcortez@gmail.com>
2021-08-18 17:28:35 -0300

    Configure OrderedAddonFragment bindings

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/order/OrderedAddonFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/order/OrderedAddonFragment.kt
@@ -1,10 +1,26 @@
 package com.woocommerce.android.ui.products.addons.order
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentOrderedAddonBinding
 import com.woocommerce.android.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OrderedAddonFragment : BaseFragment(R.layout.fragment_ordered_addon) {
+    companion object {
+        val TAG: String = OrderedAddonFragment::class.java.simpleName
+    }
 
+    private val viewModel: OrderedAddonViewModel by viewModels()
+
+    private var _binding: FragmentOrderedAddonBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentOrderedAddonBinding.bind(view)
+    }
 }
