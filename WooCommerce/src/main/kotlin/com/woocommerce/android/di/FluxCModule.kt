1406cdbed4d5b81918454560019e314c2edcf76c
Thomaz de Freitas Barbosa Cortez <thomazfcortez@gmail.com>
2021-08-26 14:14:05 -0300

    Revert "Fix FluxC room injection module"

--- WooCommerce/src/main/kotlin/com/woocommerce/android/di/FluxCModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/di/FluxCModule.kt
@@ -3,7 +3,6 @@ package com.woocommerce.android.di
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.wordpress.android.fluxc.di.WCDatabaseModule
 import org.wordpress.android.fluxc.module.OkHttpClientModule
 import org.wordpress.android.fluxc.module.ReleaseNetworkModule
 
@@ -11,8 +10,7 @@ import org.wordpress.android.fluxc.module.ReleaseNetworkModule
 @Module(
     includes = [
         ReleaseNetworkModule::class,
-        OkHttpClientModule::class,
-        WCDatabaseModule::class
+        OkHttpClientModule::class
     ]
 )
 abstract class FluxCModule
