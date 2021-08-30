01ddc6c87ce91c967ee2aa15616b21d783f33967
ThomazFB <thomazfcortez@gmail.com>
2021-08-30 16:51:39 -0300

    Refactor BetaFeaturesFragment to work with Product Add-ons feature

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/BetaFeaturesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/BetaFeaturesFragment.kt
@@ -6,39 +6,27 @@ import androidx.fragment.app.Fragment
 import com.woocommerce.android.AppPrefs
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
-import com.woocommerce.android.analytics.AnalyticsTracker.Stat.SETTINGS_BETA_FEATURES_PRODUCTS_TOGGLED
 import com.woocommerce.android.databinding.FragmentSettingsBetaBinding
 import com.woocommerce.android.ui.prefs.MainSettingsFragment.AppSettingsListener
-import com.woocommerce.android.util.AnalyticsUtils
 
 class BetaFeaturesFragment : Fragment(R.layout.fragment_settings_beta) {
     companion object {
         const val TAG = "beta-features"
     }
 
-    private lateinit var settingsListener: AppSettingsListener
+    private val settingsListener by lazy {
+        activity as? AppSettingsListener
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentSettingsBetaBinding.bind(view)
 
-        if (activity is AppSettingsListener) {
-            settingsListener = activity as AppSettingsListener
-        } else {
-            throw ClassCastException(context.toString() + " must implement AppSettingsListener")
-        }
-
-        binding.switchProductsUI.isChecked = AppPrefs.isProductsFeatureEnabled()
-        binding.switchProductsUI.setOnCheckedChangeListener { _, isChecked ->
-            AnalyticsTracker.track(
-                SETTINGS_BETA_FEATURES_PRODUCTS_TOGGLED,
-                mapOf(
-                    AnalyticsTracker.KEY_STATE to
-                        AnalyticsUtils.getToggleStateLabel(binding.switchProductsUI.isChecked)
-                )
-            )
-            settingsListener.onProductsFeatureOptionChanged(isChecked)
+        binding.switchAddonsToggle.isChecked = AppPrefs.isProductAddonsEnabled
+        binding.switchAddonsToggle.setOnCheckedChangeListener { _, isChecked ->
+            // trigger track event
+            settingsListener?.onProductsFeatureOptionChanged(isChecked)
         }
     }
 
