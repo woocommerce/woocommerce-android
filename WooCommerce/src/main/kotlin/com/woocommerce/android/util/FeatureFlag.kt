925fd9f85b36269ae9f21e1e4baf97ec6910f411
Hafiz Rahman <hfzrhmn@gmail.com>
2021-09-02 13:51:45 +0700

    Start feature flag for "What's New".

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -11,14 +11,15 @@ enum class FeatureFlag {
     DB_DOWNGRADE,
     ORDER_CREATION,
     CARD_READER,
-    PRODUCT_ADD_ONS;
+    PRODUCT_ADD_ONS,
+    WHATS_NEW;
 
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
             DB_DOWNGRADE -> {
                 PackageUtils.isDebugBuild() || context != null && PackageUtils.isBetaBuild(context)
             }
-            ORDER_CREATION -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
+            ORDER_CREATION, WHATS_NEW -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
             CARD_READER -> CardPresentEligibleFeatureChecker.isCardPresentEligible
             PRODUCT_ADD_ONS ->
                 (PackageUtils.isDebugBuild() || PackageUtils.isTesting()) &&
