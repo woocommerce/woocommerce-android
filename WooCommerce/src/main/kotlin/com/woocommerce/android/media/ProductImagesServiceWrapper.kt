d14d6a90a0603738ab3c69348e6e8068c4e7c126
hicham <hicham.boushaba@outlook.com>
2021-09-03 12:43:46 +0100

    Remove unused methods

--- WooCommerce/src/main/kotlin/com/woocommerce/android/media/ProductImagesServiceWrapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/media/ProductImagesServiceWrapper.kt
@@ -2,23 +2,18 @@ package com.woocommerce.android.media
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import androidx.core.app.JobIntentService
 import androidx.core.content.ContextCompat
-import com.woocommerce.android.JobServiceIds
-import com.woocommerce.android.model.Product
 import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
- * Wrapper for the product images service - it's sole purpose is to have an injected context so viewModels
- * can upload media to the service without requiring a context
+ * Wrapper for the product images service - this allows clients to control the service without having a reference
+ * to the [Context]
  */
 @Singleton
 class ProductImagesServiceWrapper
 @Inject constructor(
-    private val context: Context,
-    private val productImagesNotificationHandler: ProductImagesNotificationHandler
+    private val context: Context
 ) {
     fun startService() {
         ContextCompat.startForegroundService(context, Intent(context, ProductImagesService::class.java))
@@ -26,13 +21,5 @@ class ProductImagesServiceWrapper
 
     fun stopService() {
         context.stopService(Intent(context, ProductImagesService::class.java))
-    }
-
-    fun showUploadFailureNotification(productId: Long, failuresCount: Int) {
-        productImagesNotificationHandler.postUploadFailureNotification(productId, failuresCount)
-    }
-
-    fun removeUploadFailureNotification(productId: Long) {
-        productImagesNotificationHandler.removeUploadFailureNotification(productId)
     }
 }
