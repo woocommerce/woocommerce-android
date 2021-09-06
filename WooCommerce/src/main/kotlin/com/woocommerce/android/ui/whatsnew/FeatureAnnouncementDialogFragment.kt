9f7b880ef351190f9d8aafd62613ae27d63591af
Hafiz Rahman <hfzrhmn@gmail.com>
2021-09-06 17:53:03 +0700

    Start a basic, feature announcement dialog fragment.
    The content and design are not yet accurate, this is just to ensure we have starting files we can show.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/whatsnew/FeatureAnnouncementDialogFragment.kt
@@ -0,0 +1,26 @@
+package com.woocommerce.android.ui.whatsnew
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FeatureAnnouncementDialogFragmentBinding
+
+class FeatureAnnouncementDialogFragment : DialogFragment() {
+    companion object {
+        const val TAG: String = "FeatureAnnouncementDialog"
+    }
+
+    override fun getTheme(): Int {
+        return R.style.Woo_Dialog_Fullscreen
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.feature_announcement_dialog_fragment, container, false)
+        val binding = FeatureAnnouncementDialogFragmentBinding.bind(view)
+
+        return view
+    }
+}
