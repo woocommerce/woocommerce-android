c9cee546bda480d5be97bb9b1b07da9635df94b7
Hafiz Rahman <hfzrhmn@gmail.com>
2021-09-07 16:35:40 +0700

    Add FeatureAnnouncementViewModel with example data.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/whatsnew/FeatureAnnouncementViewModel.kt
@@ -0,0 +1,40 @@
+package com.woocommerce.android.ui.whatsnew
+
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.model.FeatureAnnouncement
+import com.woocommerce.android.model.FeatureAnnouncementItem
+import com.woocommerce.android.viewmodel.ScopedViewModel
+
+class FeatureAnnouncementViewModel(
+    savedState: SavedStateHandle
+) : ScopedViewModel(savedState) {
+
+    private val exampleAnnouncement = FeatureAnnouncement(
+        appVersionName = "14.2",
+        announcementVersion = 1337,
+        minimumAppVersion = "14.2",
+        maximumAppVersion = "14.3",
+        appVersionTargets = listOf("alpha-centauri-1", "alpha-centauri-2"),
+        detailsUrl = "https://woocommerce.com/",
+        features = listOf(
+            FeatureAnnouncementItem(
+                title = "Super Publishing",
+                subtitle = "Super Publishing is here! Publish using the power of your mind.",
+                iconBase64 = "",
+                iconUrl = "https://s0.wordpress.com/i/store/mobile/plans-personal.png"
+            ),
+            FeatureAnnouncementItem(
+                title = "Amazing Feature",
+                subtitle = "That's right! They are right in the app! They require pets right now.",
+                iconBase64 = "",
+                iconUrl = "https://s0.wordpress.com/i/store/mobile/plans-premium.png"
+            ),
+            FeatureAnnouncementItem(
+                title = "We like long feature announcements that why this one is going to be extra long",
+                subtitle = "Super Publishing is here! Publish using the power of your mind.",
+                iconBase64 = "",
+                iconUrl = "https://s0.wordpress.com/i/store/mobile/plans-business.png"
+            )
+        )
+    )
+}
