f72fa0c12bc41686bc68427045da0aa6fc85f993
Hafiz Rahman <hfzrhmn@gmail.com>
2021-09-08 18:15:13 +0700

    Create ImageUtils for handling url and base64 img loading.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/ImageUtils.kt
@@ -0,0 +1,55 @@
+package com.woocommerce.android.util
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Base64
+import android.widget.ImageView
+import com.woocommerce.android.di.GlideApp
+import org.wordpress.android.util.AppLog
+
+object ImageUtils {
+    /**
+     * Loads an image from the "imgUrl" into the ImageView and applies circle transformation.
+     */
+    fun loadUrlIntoCircle(
+        context: Context,
+        imageView: ImageView,
+        imgUrl: String,
+        placeholder: Drawable?
+    ) {
+        GlideApp.with(context)
+            .load(imgUrl)
+            .placeholder(placeholder)
+            .circleCrop()
+            .into(imageView)
+            .clearOnDetach()
+    }
+
+    /**
+     * Loads a base64 string without prefix (data:image/png;base64,) into the ImageView and applies circle
+     * transformation.
+     */
+    fun loadBase64IntoCircle(
+        context: Context,
+        imageView: ImageView,
+        base64ImageData: String,
+        placeholder: Drawable?
+    ) {
+
+        val imageData: ByteArray
+        try {
+            val sanitizedBase64String = base64ImageData.replace("data:image/png;base64,", "")
+            imageData = Base64.decode(sanitizedBase64String, Base64.DEFAULT)
+        } catch (ex: IllegalArgumentException) {
+            AppLog.e(AppLog.T.UTILS, String.format("Cant parse base64 image data:" + ex.message))
+            return
+        }
+
+        GlideApp.with(context)
+            .load(imageData)
+            .placeholder(placeholder)
+            .circleCrop()
+            .into(imageView)
+            .clearOnDetach()
+    }
+}
