49ce89f286ef5acf6e3a2998476f0d7d700f599e
Hafiz Rahman <hfzrhmn@gmail.com>
2021-09-24 13:51:59 +0700

    Add API call for use on Settings page.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/whatsnew/FeatureAnnouncementViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/whatsnew/FeatureAnnouncementViewModel.kt
@@ -6,20 +6,33 @@ import com.woocommerce.android.model.FeatureAnnouncement
 import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
 class FeatureAnnouncementViewModel @Inject constructor(
-    savedState: SavedStateHandle
+    savedState: SavedStateHandle,
+    featureAnnouncementRepository: FeatureAnnouncementRepository
 ) : ScopedViewModel(savedState) {
     val viewStateData = LiveDataDelegate(savedState, FeatureAnnouncementViewState())
     private var viewState by viewStateData
 
     init {
-        viewState = viewState.copy(
-            announcement = FeatureAnnouncementRepository.exampleAnnouncement
-        )
+        launch {
+            val latestAnnouncement = featureAnnouncementRepository.getLatestFeatureAnnouncement(true)
+                ?: featureAnnouncementRepository.getLatestFeatureAnnouncement(false)
+
+            latestAnnouncement?.let {
+                viewState = viewState.copy(
+                    announcement = it
+                )
+
+                // TODO for display logic:
+                // 1. save latest announcement's announcement version in prefs
+                // 2. save current app version in prefs
+            }
+        }
     }
 
     @Parcelize
