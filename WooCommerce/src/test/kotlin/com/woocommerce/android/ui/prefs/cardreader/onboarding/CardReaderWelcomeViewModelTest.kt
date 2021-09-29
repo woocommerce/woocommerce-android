2c01546874bee6cbea9948fdf61e61b51b5b86e3
malinajirka <malinajirka@gmail.com>
2021-09-29 16:29:39 +0200

    Add tests for card reader welcome screen

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/prefs/cardreader/onboarding/CardReaderWelcomeViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/prefs/cardreader/onboarding/CardReaderWelcomeViewModelTest.kt
@@ -1,8 +1,11 @@
 package com.woocommerce.android.ui.prefs.cardreader.onboarding
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.ui.prefs.cardreader.onboarding.CardReaderWelcomeViewModel.CardReaderWelcomeDialogEvent.NavigateToOnboardingFlow
 import com.woocommerce.android.viewmodel.BaseUnitTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
@@ -13,5 +16,17 @@ class CardReaderWelcomeViewModelTest : BaseUnitTest() {
     @Before
     fun setUp() {
         viewModel = CardReaderWelcomeViewModel(SavedStateHandle())
+    }
+
+    @Test
+    fun `when screen shown, then view state initialized`() {
+        assertThat(viewModel.viewState).isNotNull
+    }
+
+    @Test
+    fun `when user clicks on continue, then the app navigates to onboarding flow`() {
+        viewModel.viewState.value!!.buttonAction.invoke()
+
+        assertThat(viewModel.event).isInstanceOf(NavigateToOnboardingFlow::class.java)
     }
 }
