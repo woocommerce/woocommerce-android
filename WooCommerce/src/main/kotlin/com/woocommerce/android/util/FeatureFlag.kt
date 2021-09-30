c1ebbd05730f722db7acd7db97bb2996609b9f2d
hicham <hicham.boushaba@gmail.com>
2021-09-30 12:00:50 +0100

    Add a feature flag for Jetpack CP project

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -10,14 +10,18 @@ enum class FeatureFlag {
     ORDER_CREATION,
     ORDER_EDITING,
     CARD_READER,
-    WHATS_NEW;
+    WHATS_NEW,
+    JETPACK_CP;
 
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
             DB_DOWNGRADE -> {
                 PackageUtils.isDebugBuild() || context != null && PackageUtils.isBetaBuild(context)
             }
-            ORDER_CREATION, ORDER_EDITING, WHATS_NEW -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
+            ORDER_CREATION,
+            ORDER_EDITING,
+            JETPACK_CP,
+            WHATS_NEW -> PackageUtils.isDebugBuild() || PackageUtils.isTesting()
             CARD_READER -> true // Keeping the flag for a few sprints so we can quickly disable the feature if needed
         }
     }
