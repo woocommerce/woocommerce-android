7842c775f3fa4c307e4d6172fb7050540b9b0c18
Nick Bradbury <nick.bradbury@gmail.com>
2021-10-05 14:11:30 -0400

    Fleshed out BaseOrderEditFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/BaseOrderEditFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/BaseOrderEditFragment.kt
@@ -1,9 +1,31 @@
 package com.woocommerce.android.ui.orders.details.editing
 
 import androidx.annotation.LayoutRes
+import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.main.MainActivity.Companion.BackPressListener
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 
-open class BaseOrderEditFragment : BaseFragment {
+abstract class BaseOrderEditFragment : BaseFragment, BackPressListener {
     constructor() : super()
     constructor(@LayoutRes layoutId: Int) : super(layoutId)
+
+    abstract fun hasChanges(): Boolean
+
+    override fun onRequestAllowBackPress(): Boolean {
+        return if (hasChanges()) {
+            confirmDisard()
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun confirmDisard() {
+        MultiLiveEvent.Event.ShowDialog.buildDiscardDialogEvent(
+            positiveBtnAction = { _, _ ->
+                findNavController().navigateUp()
+            }
+        )
+    }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/EditCustomerOrderNoteFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/EditCustomerOrderNoteFragment.kt
@@ -6,34 +6,51 @@ import androidx.navigation.fragment.navArgs
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.databinding.FragmentEditCustomerOrderNoteBinding
-import com.woocommerce.android.ui.main.MainActivity.Companion.BackPressListener
+import com.woocommerce.android.databinding.FragmentOrderDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 import org.wordpress.android.util.ActivityUtils
 
 @AndroidEntryPoint
 class EditCustomerOrderNoteFragment :
-    BaseOrderEditFragment(R.layout.fragment_edit_customer_order_note), BackPressListener {
+    BaseOrderEditFragment(R.layout.fragment_edit_customer_order_note) {
     companion object {
         const val TAG = "EditCustomerOrderNoteFragment"
         const val KEY_EDIT_NOTE_RESULT = "key_edit_note_result"
+        private const val KEY_ORIGINAL_NOTE = "key_original_note"
     }
 
+    private var _binding: FragmentEditCustomerOrderNoteBinding? = null
+    private val binding get() = _binding!!
+
     private val navArgs: EditCustomerOrderNoteFragmentArgs by navArgs()
+    private var originalNote: String = ""
 
     /** TODO
      * DONE button
-     * discard dialog
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding = FragmentEditCustomerOrderNoteBinding.bind(view)
+        _binding = FragmentEditCustomerOrderNoteBinding.bind(view)
 
         if (savedInstanceState == null) {
+            originalNote = navArgs.customerOrderNote
             binding.customerOrderNoteEditor.setText(navArgs.customerOrderNote)
             binding.customerOrderNoteEditor.requestFocus()
             ActivityUtils.showKeyboard(binding.customerOrderNoteEditor)
+        } else {
+            originalNote = savedInstanceState.getString(KEY_ORIGINAL_NOTE, "")
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_ORIGINAL_NOTE, originalNote)
     }
 
     override fun getFragmentTitle() = requireActivity().getString(R.string.orderdetail_customer_provided_note)
@@ -50,5 +67,7 @@ class EditCustomerOrderNoteFragment :
         }
     }
 
-    override fun onRequestAllowBackPress() = true
+    override fun hasChanges() = originalNote != getCustomerNote()
+
+    private fun getCustomerNote() = binding.customerOrderNoteEditor.text.toString()
 }
