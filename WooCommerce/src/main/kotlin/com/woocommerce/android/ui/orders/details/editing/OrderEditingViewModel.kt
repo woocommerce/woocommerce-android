f13eb3b12fa500a04282070480702d6f5c928449
Nick Bradbury <nick.bradbury@gmail.com>
2021-10-07 10:51:02 -0400

    Use dispatchers.io

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/OrderEditingViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/OrderEditingViewModel.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.orders.details.editing
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.ui.orders.details.OrderDetailFragmentArgs
+import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,6 +13,7 @@ import javax.inject.Inject
 @HiltViewModel
 class OrderEditingViewModel @Inject constructor(
     savedState: SavedStateHandle,
+    private val dispatchers: CoroutineDispatchers,
     private val orderEditingRepository: OrderEditingRepository
 ) : ScopedViewModel(savedState) {
     private val navArgs by savedState.navArgs<OrderDetailFragmentArgs>()
@@ -28,7 +30,7 @@ class OrderEditingViewModel @Inject constructor(
     }
 
     fun updateCustomerOrderNote(updatedCustomerOrderNote: String) {
-        launch {
+        launch(dispatchers.io) {
             orderEditingRepository.updateCustomerOrderNote(order, updatedCustomerOrderNote)
         }
     }
