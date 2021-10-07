bb3eca952cab02ea69cffcea5c11c78aa23784ea
Andrey <andrey.kidinov@a8c.com>
2021-10-07 14:36:27 +0300

    Fixed tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModelTest.kt
@@ -430,8 +430,11 @@ class CardReaderConnectViewModelTest : BaseUnitTest() {
     fun `given last connected reader is matching, when reader found, then reader connecting state shown`() =
         coroutinesTestRule.testDispatcher.runBlockingTest {
             whenever(appPrefs.getLastConnectedCardReaderId()).thenReturn("Dummy1")
+            val readerStatusStateFlow = MutableStateFlow<CardReaderStatus>(CardReaderStatus.Connecting)
+            whenever(cardReaderManager.readerStatus).thenReturn(readerStatusStateFlow)
 
             init(scanState = READER_FOUND)
+            readerStatusStateFlow.emit(CardReaderStatus.Connected(mock()))
 
             assertThat(viewModel.viewStateData.value).isInstanceOf(ConnectingState::class.java)
         }
@@ -998,12 +1001,19 @@ class CardReaderConnectViewModelTest : BaseUnitTest() {
             }
         }
         whenever(locationRepository.getDefaultLocationId()).thenReturn(locationId)
-        whenever(cardReaderManager.startConnectionToReader(reader, locationId)).thenReturn(connectingSucceeds)
-        whenever(cardReaderManager.startConnectionToReader(reader2, locationId)).thenReturn(connectingSucceeds)
         (viewModel.event.value as CheckLocationPermissions).onPermissionsCheckResult(true)
         (viewModel.event.value as CheckLocationEnabled).onLocationEnabledCheckResult(true)
         (viewModel.event.value as CheckBluetoothEnabled).onBluetoothCheckResult(true)
         (viewModel.event.value as InitializeCardReaderManager).onCardManagerInitialized(cardReaderManager)
+        whenever(cardReaderManager.readerStatus).thenReturn(
+            MutableStateFlow(
+                if (connectingSucceeds) {
+                    CardReaderStatus.Connected(reader)
+                } else {
+                    CardReaderStatus.NotConnected
+                }
+            )
+        )
     }
 
     private enum class ScanResult {
