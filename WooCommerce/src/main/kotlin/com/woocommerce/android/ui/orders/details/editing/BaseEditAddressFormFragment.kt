0ee71f1d9c7f2e35f33fe697f0951ca8d9a28c40
ThomazFB <thomazfcortez@gmail.com>
2021-10-08 18:35:27 -0300

    Define BaseEditAddressFormFragment

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/BaseEditAddressFormFragment.kt
@@ -0,0 +1,7 @@
+package com.woocommerce.android.ui.orders.details.editing
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+
+class BaseEditAddressFormFragment : BaseFragment(R.layout.fragment_edit_order_address) {
+}
