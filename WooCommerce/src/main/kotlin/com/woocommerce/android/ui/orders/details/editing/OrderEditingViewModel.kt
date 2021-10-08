6c474976bbe8cef690ed200622c38043245a5bbc
Nick Bradbury <nick.bradbury@gmail.com>
2021-10-08 13:57:33 -0400

    Moved collecting result to its own function for reusability

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/OrderEditingViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/OrderEditingViewModel.kt
@@ -13,6 +13,7 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -57,25 +58,29 @@ class OrderEditingViewModel @Inject constructor(
         }
 
         launch(dispatchers.io) {
-            orderEditingRepository.updateCustomerOrderNote(order, updatedNote).collect { result ->
-                when (result) {
-                    is WCOrderStore.UpdateOrderResult.OptimisticUpdateResult -> {
-                        withContext(Dispatchers.Main) {
-                            viewState = viewState.copy(orderEdited = true)
-                        }
+            collectUpdateFlow(orderEditingRepository.updateCustomerOrderNote(order, updatedNote))
+        }
+
+        return true
+    }
+
+    private suspend fun collectUpdateFlow(flow: Flow<WCOrderStore.UpdateOrderResult>) {
+        flow.collect { result ->
+            when (result) {
+                is WCOrderStore.UpdateOrderResult.OptimisticUpdateResult -> {
+                    withContext(Dispatchers.Main) {
+                        viewState = viewState.copy(orderEdited = true)
                     }
-                    is WCOrderStore.UpdateOrderResult.RemoteUpdateResult -> {
-                        if (result.event.isError) {
-                            withContext(Dispatchers.Main) {
-                                viewState = viewState.copy(orderEditingFailed = true)
-                            }
+                }
+                is WCOrderStore.UpdateOrderResult.RemoteUpdateResult -> {
+                    if (result.event.isError) {
+                        withContext(Dispatchers.Main) {
+                            viewState = viewState.copy(orderEditingFailed = true)
                         }
                     }
                 }
             }
         }
-
-        return true
     }
 
     @Parcelize
