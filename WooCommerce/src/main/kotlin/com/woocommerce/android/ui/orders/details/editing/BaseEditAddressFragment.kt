0aef11664d4cf77648442fe71a2b3cfd2339eb23
ThomazFB <thomazfcortez@gmail.com>
2021-10-11 09:25:00 -0300

    Rename BaseEditAddressFormFragment to BaseEditAddressFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/BaseEditAddressFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/BaseEditAddressFragment.kt
@@ -3,5 +3,7 @@ package com.woocommerce.android.ui.orders.details.editing
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.base.BaseFragment
 
-class BaseEditAddressFormFragment : BaseFragment(R.layout.fragment_edit_order_address) {
+class BaseEditAddressFragment :
+    BaseFragment(R.layout.fragment_edit_order_address) {
+
 }
