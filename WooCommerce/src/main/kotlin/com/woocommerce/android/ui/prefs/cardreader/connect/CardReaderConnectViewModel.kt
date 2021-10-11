2191e3c0b2f71738a091f92649023a319fbe90d9
Andrey <andrey.kidinov@a8c.com>
2021-10-11 15:26:16 +0300

    updateStatusJob cancelation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/connect/CardReaderConnectViewModel.kt
@@ -206,9 +206,12 @@ class CardReaderConnectViewModel @Inject constructor(
 
     private fun onCardReaderManagerInitialized(cardReaderManager: CardReaderManager) {
         this.cardReaderManager = cardReaderManager
+
+        updateStatusJob?.cancel()
         updateStatusJob = launch {
             listenToSoftwareUpdateStatus()
         }
+
         launch {
             if (cardReaderManager.readerStatus.value is CardReaderStatus.Connecting) {
                 listenToConnectionStatus(cardReaderManager)
