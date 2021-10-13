db0bbfd3f96db724e39d9a81a05466f9514c104c
jorgemucientesfayos <jorge.mucientes@automattic.com>
2021-10-13 16:17:10 +0200

    Load orders filter list and display it

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/filters/OrderFilterListViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/filters/OrderFilterListViewModel.kt
@@ -1,10 +1,15 @@
 package com.woocommerce.android.ui.orders.filters
 
+import android.os.Parcelable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.R
 import com.woocommerce.android.tools.NetworkStatus
 import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +18,33 @@ class OrderFilterListViewModel @Inject constructor(
     private val resourceProvider: ResourceProvider,
     private val networkStatus: NetworkStatus,
 ) : ScopedViewModel(savedState) {
+
+    private val _filterListItems = MutableLiveData<List<FilterListItemUiModel>>()
+    val filterListItems: LiveData<List<FilterListItemUiModel>> = _filterListItems
+
+    init {
+        _filterListItems.value = buildFilterListUiModel()
+    }
+
+    private fun buildFilterListUiModel(): List<FilterListItemUiModel> =
+        listOf(
+            FilterListItemUiModel(
+                displayName = resourceProvider.getString(R.string.orderfilters_order_status_filter),
+                selectedValue = resourceProvider.getString(R.string.orderfilters_default_filter_value)
+            ),
+            FilterListItemUiModel(
+                displayName = resourceProvider.getString(R.string.orderfilters_date_range_filter),
+                selectedValue = resourceProvider.getString(R.string.orderfilters_default_filter_value)
+            )
+        )
+
+    fun onShowOrdersClicked() {
+        //TODO
+    }
+
+    @Parcelize
+    data class FilterListItemUiModel(
+        val displayName: String,
+        val selectedValue: String
+    ) : Parcelable
 }
