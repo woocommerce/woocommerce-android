eb0fb3ca4f5b24764f092a3fd4f93b1184e1a643
hicham <hicham.boushaba@gmail.com>
2021-10-15 21:02:06 +0100

    Adapt the jetpack benefits dialog to tablets in landscape

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/JetpackBenefitsDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/JetpackBenefitsDialog.kt
@@ -6,13 +6,19 @@ import androidx.fragment.app.DialogFragment
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.DialogJetpackBenefitsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import org.wordpress.android.util.DisplayUtils
 
 @AndroidEntryPoint
 class JetpackBenefitsDialog : DialogFragment(R.layout.dialog_jetpack_benefits) {
+    companion object {
+        private const val TABLET_LANDSCAPE_WIDTH_RATIO = 0.35f
+        private const val TABLET_LANDSCAPE_HEIGHT_RATIO = 0.8f
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Use fullscreen style
-        setStyle(STYLE_NO_TITLE, R.style.Theme_Woo)
+        // Use fullscreen style for all cases except tablet in landscape mode
+        setStyle(STYLE_NO_TITLE, if (isTabletLandscape()) R.style.Theme_Woo_Dialog else R.style.Theme_Woo)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,4 +35,17 @@ class JetpackBenefitsDialog : DialogFragment(R.layout.dialog_jetpack_benefits) {
             // TODO navigate to the jetpack installation screen
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        if (isTabletLandscape()) {
+            requireDialog().window!!.setLayout(
+                (DisplayUtils.getDisplayPixelWidth() * TABLET_LANDSCAPE_WIDTH_RATIO).toInt(),
+                (DisplayUtils.getDisplayPixelHeight(context) * TABLET_LANDSCAPE_HEIGHT_RATIO).toInt()
+            )
+        }
+    }
+
+    private fun isTabletLandscape() = (DisplayUtils.isTablet(context) || DisplayUtils.isXLargeTablet(context)) &&
+        DisplayUtils.isLandscape(context)
 }
