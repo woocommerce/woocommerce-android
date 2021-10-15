52ea8b2fbb0ee6641fdccca306d485f66d845d8a
ThomazFB <thomazfcortez@gmail.com>
2021-10-15 08:20:16 -0300

    Handle use as another address switch view state

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/address/ShippingAddressEditingFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/editing/address/ShippingAddressEditingFragment.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.orders.details.editing.address
 
 import android.view.View
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentBaseEditAddressBinding
 import com.woocommerce.android.model.Address
@@ -12,9 +13,17 @@ class ShippingAddressEditingFragment : BaseAddressEditingFragment() {
     override fun saveChanges() = sharedViewModel.updateShippingAddress(addressDraft)
 
     override fun onViewBound(binding: FragmentBaseEditAddressBinding) {
+        handleUseAsAnotherAddressSwitch(binding.useAsAnotherAddressSwitch)
         binding.email.visibility = View.GONE
         binding.addressSectionHeader.text = getString(R.string.order_detail_shipping_address_section)
     }
 
     override fun getFragmentTitle() = getString(R.string.order_detail_shipping_address_section)
+
+    private fun handleUseAsAnotherAddressSwitch(switch: SwitchMaterial) {
+        switch.visibility = View.VISIBLE
+        sharedViewModel.order.billingAddress.copy(email = "")
+            .let { it == storedAddress }
+            .let { switch.isChecked = it }
+    }
 }
