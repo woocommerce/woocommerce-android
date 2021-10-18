8be81eece3122b5db5c7f466f7e032c089158d50
jorgemucientesfayos <jorge.mucientes@automattic.com>
2021-10-18 17:15:55 +0200

    Properly handle back navigation from order filters flow

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainActivity.kt
@@ -409,7 +409,8 @@ class MainActivity :
                 R.id.printingInstructionsFragment,
                 R.id.editCustomerOrderNoteFragment,
                 R.id.shippingAddressEditingFragment,
-                R.id.billingAddressEditingFragment -> {
+                R.id.billingAddressEditingFragment,
+                R.id.orderFilterCategoryListFragment -> {
                     true
                 }
                 R.id.productDetailFragment -> {
