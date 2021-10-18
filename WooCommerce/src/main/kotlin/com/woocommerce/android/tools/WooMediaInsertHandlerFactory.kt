620a91412abc930adc2ebbc0a21ba9382606e548
Ondrej Ruttkay <ondrej@automattic.com>
2021-10-18 17:15:28 +0200

    Remove unused insert handler

--- WooCommerce/src/main/kotlin/com/woocommerce/android/tools/WooMediaInsertHandlerFactory.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/tools/WooMediaInsertHandlerFactory.kt
@@ -4,7 +4,6 @@ import org.wordpress.android.mediapicker.api.MediaInsertHandler
 import org.wordpress.android.mediapicker.api.MediaInsertHandlerFactory
 import org.wordpress.android.mediapicker.api.MediaInsertUseCase
 import org.wordpress.android.mediapicker.api.MediaPickerSetup
-import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.*
 import org.wordpress.android.mediapicker.source.device.DeviceMediaInsertUseCase.DeviceMediaInsertUseCaseFactory
 import javax.inject.Inject
 
@@ -12,13 +11,8 @@ class WooMediaInsertHandlerFactory @Inject constructor(
     private val deviceMediaInsertUseCaseFactory: DeviceMediaInsertUseCaseFactory,
 ) : MediaInsertHandlerFactory {
     override fun build(mediaPickerSetup: MediaPickerSetup): MediaInsertHandler {
-        return when (mediaPickerSetup.primaryDataSource) {
-            DEVICE, CAMERA, SYSTEM_PICKER -> deviceMediaInsertUseCaseFactory.build(mediaPickerSetup.areResultsQueued)
-            GIF_LIBRARY -> DefaultMediaInsertUseCase
-        }.toMediaInsertHandler()
+        return deviceMediaInsertUseCaseFactory.build(mediaPickerSetup.areResultsQueued).toMediaInsertHandler()
     }
 
     private fun MediaInsertUseCase.toMediaInsertHandler() = MediaInsertHandler(this)
-
-    private object DefaultMediaInsertUseCase : MediaInsertUseCase
 }
