123a5a4033744d9a3a3c2c1018c85a2d22d08e5a
hicham <hicham.boushaba@gmail.com>
2021-10-21 11:26:25 +0100

    Move mockTests to its own class

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/MocksTest.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/MocksTest.kt
@@ -5,18 +5,15 @@ import com.woocommerce.android.BuildConfig
 import com.woocommerce.android.helpers.InitializationRule
 import com.woocommerce.android.helpers.TestBase
 import com.woocommerce.android.screenshots.login.WelcomeScreen
-import com.woocommerce.android.screenshots.mystore.MyStoreScreen
 import com.woocommerce.android.ui.main.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
-import tools.fastlane.screengrab.Screengrab
-import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
 import tools.fastlane.screengrab.locale.LocaleTestRule
 
 @HiltAndroidTest
-class ScreenshotTest : TestBase() {
+class MocksTest: TestBase() {
     @get:Rule(order = 0)
     val rule = HiltAndroidRule(this)
 
@@ -30,9 +27,7 @@ class ScreenshotTest : TestBase() {
     var activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun screenshots() {
-        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
-
+    fun mocksDemo() {
         WelcomeScreen
             .logoutIfNeeded()
             .selectLogin()
@@ -41,9 +36,6 @@ class ScreenshotTest : TestBase() {
             .proceedWith(BuildConfig.SCREENSHOTS_USERNAME)
             .proceedWith(BuildConfig.SCREENSHOTS_PASSWORD)
 
-        // My Store
-        // When debugging these tests, you might want to save time and avoid the logout - login flow above.
-        MyStoreScreen()
-            .then<MyStoreScreen> { it.stats.switchToStatsDashboardYearsTab() }
+        Thread.sleep(1000000)
     }
 }
