508efbd35985cec4262feaaea4c1196212dbd525
Nick Bradbury <nick.bradbury@gmail.com>
2021-11-04 11:07:04 -0400

    Resize dialog after view created

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/quickorder/QuickOrderDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/quickorder/QuickOrderDialog.kt
@@ -10,30 +10,28 @@ import org.wordpress.android.util.DisplayUtils
 
 @AndroidEntryPoint
 class QuickOrderDialog : DialogFragment(R.layout.dialog_quick_order) {
-    companion object {
-        private const val WIDTH_RATIO = 0.35f
-        private const val HEIGHT_RATIO = 0.8f
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NO_TITLE, R.style.Theme_Woo_Dialog)
+        setStyle(STYLE_NORMAL, R.style.Theme_Woo_Dialog)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Specify transition animations
-        dialog?.window?.attributes?.windowAnimations = R.style.Woo_Animations_Dialog
+        requireDialog().setTitle(R.string.quickorder_dialog_title)
+        requireDialog().window?.let { window ->
+            window.attributes?.windowAnimations = R.style.Woo_Animations_Dialog
+            window.setLayout(
+                (DisplayUtils.getDisplayPixelWidth() * RATIO).toInt(),
+                (DisplayUtils.getDisplayPixelHeight(context) * RATIO).toInt()
+            )
+        }
+        requireDialog().setTitle(R.string.quickorder_dialog_title)
 
         val binding = DialogQuickOrderBinding.bind(view)
     }
 
-    override fun onStart() {
-        super.onStart()
-        requireDialog().window!!.setLayout(
-            (DisplayUtils.getDisplayPixelWidth() * WIDTH_RATIO).toInt(),
-            (DisplayUtils.getDisplayPixelHeight(context) * HEIGHT_RATIO).toInt()
-        )
+    companion object {
+        const val RATIO = 0.9
     }
 }
