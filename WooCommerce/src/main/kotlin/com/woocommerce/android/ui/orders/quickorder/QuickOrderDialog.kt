9d78bc79a29b258767b75b34080eb17f3e54142a
Nick Bradbury <nick.bradbury@gmail.com>
2021-11-04 11:33:04 -0400

    Added toolbar

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/quickorder/QuickOrderDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/quickorder/QuickOrderDialog.kt
@@ -2,17 +2,20 @@ package com.woocommerce.android.ui.orders.quickorder
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.DialogQuickOrderBinding
 import dagger.hilt.android.AndroidEntryPoint
+import org.wordpress.android.util.ActivityUtils
 import org.wordpress.android.util.DisplayUtils
 
 @AndroidEntryPoint
 class QuickOrderDialog : DialogFragment(R.layout.dialog_quick_order) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NORMAL, R.style.Theme_Woo_Dialog)
+        setStyle(STYLE_NO_TITLE, R.style.Theme_Woo_Dialog)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,9 +29,16 @@ class QuickOrderDialog : DialogFragment(R.layout.dialog_quick_order) {
                 (DisplayUtils.getDisplayPixelHeight(context) * RATIO).toInt()
             )
         }
-        requireDialog().setTitle(R.string.quickorder_dialog_title)
 
         val binding = DialogQuickOrderBinding.bind(view)
+        ActivityUtils.showKeyboard(binding.editPrice)
+
+        val toolbar = binding.toolbar.toolbar
+        toolbar.setTitle(R.string.quickorder_dialog_title)
+        toolbar.navigationIcon = ContextCompat.getDrawable(requireActivity(), R.drawable.ic_gridicons_cross_24dp)
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
     companion object {
