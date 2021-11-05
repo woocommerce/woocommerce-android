5a7a026773006d9139ae655b2bdc910ab951de04
pachlava <pachlava@gmail.com>
2021-11-05 14:11:48 +0200

    A change to solve Unexpected indentation warning

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/util/JSONArray+iterator.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/util/JSONArray+iterator.kt
@@ -4,5 +4,5 @@ import org.json.JSONArray
 import org.json.JSONObject
 
 // Found at https://www.baeldung.com/kotlin/iterate-over-jsonarray
-operator fun JSONArray.iterator(): Iterator<JSONObject>
-    = (0 until length()).asSequence().map { get(it) as JSONObject }.iterator()
+operator fun JSONArray.iterator(): Iterator<JSONObject> =
+    (0 until length()).asSequence().map { get(it) as JSONObject }.iterator()
