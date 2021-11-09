c8841ad860997885adda450ceca39c19118b57ce
Hafiz Rahman <hfzrhmn@gmail.com>
2021-11-09 16:44:44 +0700

    Add: activate plugin in repository, as well as some smaller tweaks.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/JetpackInstallRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/JetpackInstallRepository.kt
@@ -10,9 +10,6 @@ import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.PluginActionBuilder
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.store.PluginStore
-import org.wordpress.android.fluxc.store.PluginStore.OnSitePluginInstalled
-import org.wordpress.android.fluxc.store.PluginStore.OnSitePluginConfigured
 import javax.inject.Inject
 import com.woocommerce.android.ui.mystore.JetpackInstallRepository.PluginStatus.PluginInstalled
 import com.woocommerce.android.ui.mystore.JetpackInstallRepository.PluginStatus.PluginInstallFailed
@@ -20,19 +17,20 @@ import com.woocommerce.android.ui.mystore.JetpackInstallRepository.PluginStatus.
 import com.woocommerce.android.ui.mystore.JetpackInstallRepository.PluginStatus.PluginActivationFailed
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.parcelize.Parcelize
+import org.wordpress.android.fluxc.store.PluginStore.*
 
 class JetpackInstallRepository @Inject constructor(
     private val dispatcher: Dispatcher,
     private val selectedSite: SelectedSite
 ) {
     companion object {
-        const val JETPACK_SLUG = "wp-category-meta" // example plugin, should be jetpack
+        const val JETPACK_SLUG = "jetpack"
         const val JETPACK_NAME = "jetpack/jetpack"
         const val GENERIC_ERROR = "Unknown issue."
     }
 
     fun installJetpackPlugin() = callbackFlow<PluginStatus> {
-        val payload = PluginStore.InstallSitePluginPayload(selectedSite.get(), JETPACK_SLUG)
+        val payload = InstallSitePluginPayload(selectedSite.get(), JETPACK_SLUG)
         dispatcher.dispatch(PluginActionBuilder.newInstallSitePluginAction(payload))
 
         val listener = PluginActionListener(this)
@@ -44,8 +42,19 @@ class JetpackInstallRepository @Inject constructor(
     }
 
     fun fetchJetpackPlugin() {
-        val payload = PluginStore.FetchJetpackSitePluginPayload(selectedSite.get(), JETPACK_NAME)
+        val payload = FetchJetpackSitePluginPayload(selectedSite.get(), JETPACK_NAME)
         dispatcher.dispatch(PluginActionBuilder.newFetchJetpackSitePluginAction(payload))
+    }
+
+    fun activateJetpackPlugin() {
+        val payload = ConfigureSitePluginPayload(
+            selectedSite.get(),
+            JETPACK_NAME,
+            JETPACK_SLUG,
+            true,
+            true
+        )
+        dispatcher.dispatch(PluginActionBuilder.newConfigureSitePluginAction(payload))
     }
 
     private inner class PluginActionListener(private val producerScope: ProducerScope<PluginStatus>) {
@@ -81,14 +90,21 @@ class JetpackInstallRepository @Inject constructor(
 
     sealed class PluginStatus : Parcelable {
         @Parcelize
+        object PluginInstallStarted : PluginStatus()
+
+        @Parcelize
         data class PluginInstalled(val slug: String, val site: SiteModel) : PluginStatus()
+
         @Parcelize
         data class PluginInstallFailed(val error: String) : PluginStatus()
+
         @Parcelize
         data class PluginActivated(val name: String, val site: SiteModel) : PluginStatus()
+
         @Parcelize
         data class PluginActivationFailed(val error: String) : PluginStatus()
+
         @Parcelize
-        object PluginInstallStopped: PluginStatus()
+        object PluginInstallStopped : PluginStatus()
     }
 }
