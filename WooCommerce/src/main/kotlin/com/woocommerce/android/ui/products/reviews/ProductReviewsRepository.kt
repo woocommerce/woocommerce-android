0444922e7d3845299328467f792c8de07b13b006
malinajirka <malinajirka@gmail.com>
2021-11-10 18:38:06 +0100

    Remove isLoadingMore which is always false

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/reviews/ProductReviewsRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/reviews/ProductReviewsRepository.kt
@@ -27,7 +27,6 @@ class ProductReviewsRepository @Inject constructor(
 
     private var offset = 0
     private var isFetchingProductReviews = false
-    private var isLoadingMore = false
 
     var canLoadMore: Boolean = false
         private set
@@ -49,8 +48,6 @@ class ProductReviewsRepository @Inject constructor(
         )
         val result = productStore.fetchProductReviews(payload)
         if (result.isError) {
-
-
             WooLog.e(
                 REVIEWS,
                 "Error fetching product review: " +
@@ -58,7 +55,6 @@ class ProductReviewsRepository @Inject constructor(
             )
             continuationReviews.continueWith(false)
         } else {
-            isLoadingMore = false
             canLoadMore = result.canLoadMore
             continuationReviews.continueWith(true)
         }
