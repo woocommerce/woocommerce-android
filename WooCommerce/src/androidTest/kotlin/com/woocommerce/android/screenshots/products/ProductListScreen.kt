48cf15f10f10bf179d3c35e6e18637e860332e0e
Sheri Bigelow <sheri@designsimply.com>
2021-11-12 13:07:16 -0800

    have prdouct list screen select based on id productsRecycler and adddata checks

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/products/ProductListScreen.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/products/ProductListScreen.kt
@@ -1,21 +1,82 @@
 package com.woocommerce.android.screenshots.products
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import com.woocommerce.android.R
-import com.woocommerce.android.screenshots.TabNavComponent
+import com.woocommerce.android.screenshots.reviews.SingleProductScreen
+import com.woocommerce.android.screenshots.util.ProductData
 import com.woocommerce.android.screenshots.util.Screen
+import org.hamcrest.Matchers
 
 class ProductListScreen : Screen {
-    companion object {
-        const val LIST_VIEW = R.id.productsRecycler
-        const val LIST_ITEM = R.id.linearLayout
-    }
 
-    val tabBar = TabNavComponent()
+    companion object {
+        // TODO: refactor to LAYOUT_ID from LIST_VIEW
+        const val LIST_VIEW = R.id.productsRecycler
+    }
 
     constructor() : super(LIST_VIEW)
 
-    fun selectProduct(index: Int): SingleProductScreen {
-        selectItemAtIndexInRecyclerView(index, LIST_VIEW, LIST_ITEM)
+    fun scrollToProduct(productTitle: String): ProductListScreen {
+        // this just checks scrolling
+        Espresso.onView(ViewMatchers.withId(LIST_VIEW)).perform(
+            RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                ViewMatchers.hasDescendant(
+                    ViewMatchers.withText(
+                        productTitle,
+                    )
+                ),
+                ViewActions.scrollTo()
+            )
+        )
+        return this
+    }
+
+    fun assertProductCardMatchesMockData(product: ProductData): ProductListScreen {
+        // check the card information against the json data
+
+        // matcher for the card which has the correct product name
+        val matcherForProductCard = Matchers.allOf(
+            // must have element id
+            ViewMatchers.withId(R.id.productInfoContainer),
+            // must have a child
+            ViewMatchers.withChild(
+                Matchers.allOf(
+                    // child must have id productName
+                    ViewMatchers.withId(R.id.productName),
+                    // child text must match the product name mock data
+                    ViewMatchers.withText(product.name),
+                )
+            )
+        )
+        val productCardMatch = Espresso.onView(matcherForProductCard)
+        productCardMatch.check(
+            // check if it exists!
+            ViewAssertions.matches(ViewMatchers.withId(R.id.productInfoContainer))
+        )
+        // TODO: add more checks. productCardMatch.check(...)
+
+        return this
+    }
+
+    fun selectSingleProductByName(productName: String): SingleProductScreen {
+        Espresso.onView(ViewMatchers.withId(LIST_VIEW)).perform(
+            RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                ViewMatchers.hasDescendant(
+                    ViewMatchers.withText(
+                        productName
+                    )
+                ),
+                ViewActions.click()
+            )
+        )
+
         return SingleProductScreen()
     }
+
+
 }
