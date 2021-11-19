b88f6e3c4390c7d31e700fd2d61d5f1cb01ce834
colymore <rfermontero@gmail.com>
2021-11-19 15:57:10 +0100

    Creates an empty analytics fragment into the application to be shown later.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/AnalyticsFragment.kt
@@ -0,0 +1,18 @@
+package com.woocommerce.android.ui.analytics
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.TopLevelFragment
+
+/**
+ * This would be an empty fragment until issue
+ * https://github.com/woocommerce/woocommerce-android/issues/5237
+ * gets finished
+ */
+class AnalyticsFragment : TopLevelFragment(R.layout.fragment_analytics) {
+
+    override fun getFragmentTitle() = getString(R.string.analytics)
+
+    override fun shouldExpandToolbar(): Boolean = false
+
+    override fun scrollToTop() {}
+}
