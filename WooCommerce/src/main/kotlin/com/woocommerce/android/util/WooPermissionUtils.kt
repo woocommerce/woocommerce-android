4487874bc8e67c6b719bac6508a6a022e645bf6e
hicham <hicham.boushaba@gmail.com>
2021-11-20 18:27:18 +0100

    Update permissions handling to align with Android 12

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/WooPermissionUtils.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/WooPermissionUtils.kt
@@ -24,13 +24,32 @@ object WooPermissionUtils {
         context.startActivity(intent)
     }
 
-    fun hasFineLocationPermission(context: Context): Boolean {
-        return ContextCompat.checkSelfPermission(
-            context, permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
+    fun hasBluetoothRequiredPermissions(context: Context): Boolean {
+        return if (SystemVersionUtils.isAtLeastS()) {
+            context.isGranted(permission.ACCESS_FINE_LOCATION) &&
+                context.isGranted(permission.BLUETOOTH_CONNECT) &&
+                context.isGranted(permission.BLUETOOTH_SCAN)
+        } else {
+            context.isGranted(permission.ACCESS_FINE_LOCATION)
+        }
     }
 
-    fun requestFineLocationPermission(requestPermissionLauncher: ActivityResultLauncher<String>) {
-        requestPermissionLauncher.launch(permission.ACCESS_FINE_LOCATION)
+    fun requestBluetoothPermissions(context: Context, requestPermissionLauncher: ActivityResultLauncher<Array<String>>) {
+        val deniedPermissions = mutableListOf<String>()
+        if (!context.isGranted(permission.ACCESS_FINE_LOCATION)) {
+            deniedPermissions.add(permission.ACCESS_FINE_LOCATION)
+        }
+        if (SystemVersionUtils.isAtLeastS()) {
+            if (!context.isGranted(permission.BLUETOOTH_CONNECT)) {
+                deniedPermissions.add(permission.BLUETOOTH_CONNECT)
+            }
+            if (!context.isGranted(permission.BLUETOOTH_SCAN)) {
+                deniedPermissions.add(permission.BLUETOOTH_SCAN)
+            }
+        }
+        requestPermissionLauncher.launch(deniedPermissions.toTypedArray())
     }
+
+    private fun Context.isGranted(permission:String) =
+        ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
 }
