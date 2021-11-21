48f6c839dae09dac4f7fa9b751385362952e082c
colymore <rfermontero@gmail.com>
2021-11-21 22:45:57 +0100

    Create AnalyticsDateRangeCardView

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/daterangeselector/AnalyticsDateRangeCardView.kt
@@ -0,0 +1,24 @@
+package com.woocommerce.android.ui.analytics.daterangeselector
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.google.android.material.card.MaterialCardView
+import com.woocommerce.android.databinding.AnalyticsDateRangeCardViewBinding
+import com.woocommerce.android.ui.analytics.daterangeselector.AnalyticsDateRangeSelectorContract.DateRangeEvent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AnalyticsDateRangeCardView @JvmOverloads constructor(
+    ctx: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : MaterialCardView(ctx, attrs, defStyleAttr) {
+
+    val binding = AnalyticsDateRangeCardViewBinding.inflate(LayoutInflater.from(ctx), this)
+
+    internal fun initView(dateRangeEvent: DateRangeEvent) {
+        binding.btnDateRangeSelector.setOnClickListener { dateRangeEvent.onDateRangeCalendarClickEvent() }
+    }
+
+}
