a7c328fa0df5725282ca36c2e38a04082710e081
hicham <hicham.boushaba@gmail.com>
2021-11-22 15:59:59 +0100

    Revert to using `fromBundle` to fix a crash when restoring instance

--- WooCommerce/src/main/kotlin/com/woocommerce/android/viewmodel/SavedStateHandleExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/viewmodel/SavedStateHandleExt.kt
@@ -1,48 +1,28 @@
 package com.woocommerce.android.viewmodel
 
-import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Parcelable
 import androidx.annotation.MainThread
-import androidx.collection.ArrayMap
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavArgs
-import java.lang.reflect.Method
-import kotlin.reflect.KClass
-
-@MainThread
-inline fun <reified Args : NavArgs> SavedStateHandle.navArgs() = NavArgsLazy(Args::class, this)
-
-/** cache the methods for [NavArgsLazy] to avoid depending on reflection for all invocations **/
-private val methodMap = ArrayMap<KClass<out NavArgs>, Method>()
+import androidx.navigation.NavArgsLazy
+import java.io.Serializable
 
 /**
- * An implementation of [Lazy] to retrieve [NavArgs] from the provided [savedStateHandle]
- *
- * The implementation is copied from [androidx.navigation.NavArgsLazy]
+ * The current implementation of `fromSavedStateHandle` can't restore Parcelable[] properly
+ * So we'll keep using the old implementation temporarily.
+ * TODO go back to using `fromSavedStateHandle` when the issue is fixed https://issuetracker.google.com/issues/207315994
  */
-class NavArgsLazy<Args : NavArgs>(
-    private val navArgsClass: KClass<Args>,
-    private val savedStateHandle: SavedStateHandle
-) : Lazy<Args> {
-    private var cached: Args? = null
-
-    override val value: Args
-        get() {
-            var args = cached
-            if (args == null) {
-                val method: Method = methodMap[navArgsClass]
-                    ?: navArgsClass.java.getMethod("fromSavedStateHandle", SavedStateHandle::class.java)
-                        .also { method ->
-                            // Save a reference to the method
-                            methodMap[navArgsClass] = method
-                        }
-
-                @SuppressLint("BanUncheckedReflection") // needed for method.invoke
-                @Suppress("UNCHECKED_CAST")
-                args = method.invoke(null, savedStateHandle) as Args
-                cached = args
-            }
-            return args
+@MainThread
+inline fun <reified Args : NavArgs> SavedStateHandle.navArgs() = NavArgsLazy(Args::class) {
+    val bundle = Bundle()
+    keys().forEach {
+        val value = get<Any>(it)
+        if (value is Serializable?) {
+            bundle.putSerializable(it, value)
+        } else if (value is Parcelable?) {
+            bundle.putParcelable(it, value)
         }
-
-    override fun isInitialized(): Boolean = cached != null
+    }
+    bundle
 }
