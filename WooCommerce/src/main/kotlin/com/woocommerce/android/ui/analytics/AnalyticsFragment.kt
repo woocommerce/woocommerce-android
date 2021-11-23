71f6838418a4efd3b78cf5ffc68fe15f5e0a3fe7
colymore <rfermontero@gmail.com>
2021-11-23 10:41:52 +0100

    Reformat comment for AnalyticsFragment so fixing lint issue

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/AnalyticsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/AnalyticsFragment.kt
@@ -5,8 +5,7 @@ import com.woocommerce.android.ui.base.TopLevelFragment
 
 /**
  * This would be an empty fragment until issue
- * https://github.com/woocommerce/woocommerce-android/issues/5237
- * gets finished
+ * https://github.com/woocommerce/woocommerce-android/issues/5237  gets finished
  */
 class AnalyticsFragment : TopLevelFragment(R.layout.fragment_analytics) {
     override fun getFragmentTitle() = getString(R.string.analytics)
