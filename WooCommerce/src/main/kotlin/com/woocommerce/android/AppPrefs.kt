c1a8870eeee331981a3fdd6b21b9afcda77e612a
ThomazFB <thomazfcortez@gmail.com>
2021-11-22 21:23:12 -0300

    Define isOrderCreationEnabled property at AppPrefs

--- WooCommerce/src/main/kotlin/com/woocommerce/android/AppPrefs.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/AppPrefs.kt
@@ -43,6 +43,7 @@ object AppPrefs {
         IS_PRODUCTS_FEATURE_ENABLED,
         IS_PRODUCT_ADDONS_ENABLED,
         IS_QUICK_ORDER_ENABLED,
+        IS_ORDER_CREATION_ENABLED,
         LOGIN_USER_BYPASSED_JETPACK_REQUIRED,
         SELECTED_ORDER_LIST_TAB_POSITION,
         IMAGE_OPTIMIZE_ENABLED,
@@ -152,6 +153,10 @@ object AppPrefs {
     var isQuickOrderEnabled: Boolean
         get() = getBoolean(DeletablePrefKey.IS_QUICK_ORDER_ENABLED, false)
         set(value) = setBoolean(DeletablePrefKey.IS_QUICK_ORDER_ENABLED, value)
+
+    var isOrderCreationEnabled: Boolean
+        get() = getBoolean(DeletablePrefKey.IS_ORDER_CREATION_ENABLED, false)
+        set(value) = setBoolean(DeletablePrefKey.IS_ORDER_CREATION_ENABLED, value)
 
     fun getLastAppVersionCode(): Int {
         return getDeletableInt(UndeletablePrefKey.LAST_APP_VERSION_CODE)
