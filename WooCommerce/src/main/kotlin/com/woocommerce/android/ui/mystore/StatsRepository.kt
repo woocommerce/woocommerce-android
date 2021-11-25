f596f2654af9c96819b536ce5f3130f244925a54
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-11-25 10:07:40 -0500

    Update call to fetch revenue stats

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/StatsRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/StatsRepository.kt
@@ -3,16 +3,9 @@ package com.woocommerce.android.ui.mystore
 import com.woocommerce.android.AppConstants
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.ContinuationWrapper
-import com.woocommerce.android.util.ContinuationWrapper.ContinuationResult.Cancellation
-import com.woocommerce.android.util.ContinuationWrapper.ContinuationResult.Success
 import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.WooLog.T.DASHBOARD
 import kotlinx.coroutines.withTimeoutOrNull
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
-import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.action.WCStatsAction.FETCH_REVENUE_STATS
-import org.wordpress.android.fluxc.generated.WCStatsActionBuilder
 import org.wordpress.android.fluxc.model.WCRevenueStatsModel
 import org.wordpress.android.fluxc.model.leaderboards.WCTopPerformerProductModel
 import org.wordpress.android.fluxc.store.WCLeaderboardsStore
@@ -23,7 +16,6 @@ import javax.inject.Inject
 
 class StatsRepository @Inject constructor(
     private val selectedSite: SelectedSite,
-    private val dispatcher: Dispatcher,
     private val wcStatsStore: WCStatsStore,
     @Suppress("UnusedPrivateMember", "Required to ensure the WCOrderStore is initialized!")
     private val wcOrderStore: WCOrderStore,
@@ -34,26 +26,26 @@ class StatsRepository @Inject constructor(
     }
 
     private val continuationRevenueStats = ContinuationWrapper<Result<WCRevenueStatsModel?>>(DASHBOARD)
-    private lateinit var lastRevenueStatsGranularity: StatsGranularity
-
-    fun init() {
-        dispatcher.register(this)
-    }
-
-    fun onCleanup() {
-        dispatcher.unregister(this)
-    }
 
     suspend fun fetchRevenueStats(granularity: StatsGranularity, forced: Boolean): Result<WCRevenueStatsModel?> {
-        lastRevenueStatsGranularity = granularity
-        val result = continuationRevenueStats.callAndWait {
-            val statsPayload = FetchRevenueStatsPayload(selectedSite.get(), granularity, forced = forced)
-            dispatcher.dispatch(WCStatsActionBuilder.newFetchRevenueStatsAction(statsPayload))
-        }
+        val statsPayload = FetchRevenueStatsPayload(selectedSite.get(), granularity, forced = forced)
+        val result = wcStatsStore.fetchRevenueStats(statsPayload)
 
-        return when (result) {
-            is Cancellation -> Result.failure(result.exception)
-            is Success -> result.value
+        return if(!result.isError) {
+            val revenueStatsModel = wcStatsStore.getRawRevenueStats(
+                selectedSite.get(), result.granularity, result.startDate!!, result.endDate!!
+            )
+            Result.success(revenueStatsModel)
+        } else {
+            val errorMessage = result?.error?.message ?: "Timeout"
+            WooLog.e(
+                DASHBOARD,
+                "$TAG - Error fetching revenue stats: $errorMessage"
+            )
+            var exception = StatsException(
+                error = result.error
+            )
+            Result.failure(exception)
         }
     }
 
@@ -113,27 +105,6 @@ class StatsRepository @Inject constructor(
             )
 
             Result.failure(Exception(errorMessage))
-        }
-    }
-
-    @Suppress("unused")
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onWCRevenueStatsChanged(event: OnWCRevenueStatsChanged) {
-        if (event.causeOfChange == FETCH_REVENUE_STATS && event.granularity == lastRevenueStatsGranularity) {
-            if (event.isError) {
-                WooLog.e(DASHBOARD, "$TAG - Error fetching stats: ${event.error.message}")
-                // display a different error snackbar if the error type is not "plugin not active", since
-                // this error is already being handled by the activity class
-                val exception = StatsException(
-                    error = event.error
-                )
-                continuationRevenueStats.continueWith(Result.failure(exception))
-            } else {
-                val revenueStatsModel = wcStatsStore.getRawRevenueStats(
-                    selectedSite.get(), event.granularity, event.startDate!!, event.endDate!!
-                )
-                continuationRevenueStats.continueWith(Result.success(revenueStatsModel))
-            }
         }
     }
 
