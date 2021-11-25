a17933a20ca6c80eb5db9beedf1dd920ae5533fb
colymore <rfermontero@gmail.com>
2021-11-25 20:48:43 +0100

    Create ViewState for AnalyticsInformationSectionView

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/informationcard/AnalyticsInformationSectionContract.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/informationcard/AnalyticsInformationSectionContract.kt
@@ -1,11 +1,10 @@
 package com.woocommerce.android.ui.analytics.informationcard
 
 class AnalyticsInformationSectionContract {
-    data class ViewState(
-        val title: String,
-        val value: String,
-        val delta: Int,
-    ) {
-        fun getSign(): String = if (delta > 0) "+" else "-"
+    sealed class SectionViewState {
+        object SectionHiddenViewState : SectionViewState()
+        data class SectionDataViewState(val title: String, val value: String, val delta: Int) : SectionViewState() {
+            fun getSign(): String = if (delta > 0) "+" else "-"
+        }
     }
 }
