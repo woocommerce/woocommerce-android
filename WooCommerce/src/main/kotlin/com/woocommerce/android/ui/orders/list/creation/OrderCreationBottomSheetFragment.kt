cdebb15815e7dfd297e7b946b87951eabb809682
ThomazFB <thomazfcortez@gmail.com>
2021-11-25 23:59:57 -0300

    Handle Bottom sheet click events and return the selection

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/creation/OrderCreationBottomSheetFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/creation/OrderCreationBottomSheetFragment.kt
@@ -6,15 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.woocommerce.android.databinding.DialogOrderCreationBottomSheetBinding
-import dagger.hilt.android.AndroidEntryPoint
+import com.woocommerce.android.extensions.navigateBackWithResult
+import com.woocommerce.android.ui.orders.list.creation.OrderCreationBottomSheetFragment.CreationAction.CREATE_ORDER
+import com.woocommerce.android.ui.orders.list.creation.OrderCreationBottomSheetFragment.CreationAction.SIMPLE_PAYMENT
 
-@AndroidEntryPoint
 class OrderCreationBottomSheetFragment : BottomSheetDialogFragment() {
+    companion object {
+        const val KEY_ORDER_CREATION_ACTION_RESULT = "key_order_creation_action_result"
+    }
+
     private var _binding: DialogOrderCreationBottomSheetBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = DialogOrderCreationBottomSheetBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.orderCreationButton.setOnClickListener {
+            navigateBackWithResult(KEY_ORDER_CREATION_ACTION_RESULT, CREATE_ORDER)
+        }
+
+        binding.simplePaymentButton.setOnClickListener {
+            navigateBackWithResult(KEY_ORDER_CREATION_ACTION_RESULT, SIMPLE_PAYMENT)
+        }
+    }
+
+    enum class CreationAction {
+        CREATE_ORDER,
+        SIMPLE_PAYMENT
     }
 }
