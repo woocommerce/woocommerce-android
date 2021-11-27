ffa1d58db4bab8389043e28ea8e947e92022d811
ThomazFB <thomazfcortez@gmail.com>
2021-11-27 13:18:39 -0300

    Create OrderDraft basic structure at OrderCreationViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
@@ -1,13 +1,30 @@
 package com.woocommerce.android.ui.orders.creation
 
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.model.Order
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class OrderCreationViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
+    lateinit var orderDraft: OrderDraft
 
+    fun start() {
+        orderDraft = OrderDraft(
+            status = Order.Status.Pending,
+            dateCreated = Date()
+        )
+    }
+
+    @Parcelize
+    data class OrderDraft(
+        val status: Order.Status,
+        val dateCreated: Date
+    ) : Parcelable
 }
