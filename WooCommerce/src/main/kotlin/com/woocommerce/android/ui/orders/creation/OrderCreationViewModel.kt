72d12465426351dbcf05a43ab15941429602e139
ThomazFB <thomazfcortez@gmail.com>
2021-11-28 14:37:11 -0300

    Define OrderCreationViewModel ViewState structure

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.orders.creation
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.model.Order
+import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.parcelize.Parcelize
@@ -13,12 +14,15 @@ import javax.inject.Inject
 class OrderCreationViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
-    lateinit var orderDraft: OrderDraft
+    val viewStateData = LiveDataDelegate(savedState, ViewState())
+    private var viewState by viewStateData
 
     fun start() {
-        orderDraft = OrderDraft(
-            status = Order.Status.Pending,
-            dateCreated = Date()
+        viewState = viewState.copy(
+            orderDraft = OrderDraft(
+                status = Order.Status.Pending,
+                dateCreated = Date()
+            )
         )
     }
 
@@ -26,5 +30,10 @@ class OrderCreationViewModel @Inject constructor(
     data class OrderDraft(
         val status: Order.Status,
         val dateCreated: Date
+    ) : Parcelable
+
+    @Parcelize
+    data class ViewState(
+        val orderDraft: OrderDraft? = null
     ) : Parcelable
 }
