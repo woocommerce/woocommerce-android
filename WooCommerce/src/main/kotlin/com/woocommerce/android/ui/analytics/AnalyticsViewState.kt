f5c6efeab0aab76a95d302d409fbe4633554801e
colymore <rfermontero@gmail.com>
2021-11-28 20:47:15 +0100

    Add revenueCardState in analytics view state

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/AnalyticsViewState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/AnalyticsViewState.kt
@@ -1,7 +1,7 @@
 package com.woocommerce.android.ui.analytics
 
 import com.woocommerce.android.ui.analytics.daterangeselector.AnalyticsDateRangeSelectorViewState
-import com.woocommerce.android.ui.analytics.informationcard.AnalyticsCardInformationContract.AnalyticsCardInformationViewState.CardDataViewState
+import com.woocommerce.android.ui.analytics.informationcard.AnalyticsCardInformationViewState
 
 data class AnalyticsViewState(val analyticsDateRangeSelectorState: AnalyticsDateRangeSelectorViewState,
-                              val revenueCardState: CardDataViewState)
+                              val revenueCardState: AnalyticsCardInformationViewState.DataViewState)
