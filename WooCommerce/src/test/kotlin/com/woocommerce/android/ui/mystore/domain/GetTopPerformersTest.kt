6c17e99a1a6fdf45d5cc25b225f10a8270549010
Mucientes <jorge.mucientes@automattic.com>
2021-11-29 09:44:24 +0100

    Add unit tests for GetTopPerformers

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/mystore/domain/GetTopPerformersTest.kt
@@ -0,0 +1,85 @@
+package com.woocommerce.android.ui.mystore.domain
+
+import com.woocommerce.android.ui.mystore.data.StatsRepository
+import com.woocommerce.android.ui.mystore.domain.GetTopPerformers.LoadTopPerformersResult.TopPerformersLoadedError
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyBoolean
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import org.wordpress.android.fluxc.model.leaderboards.WCTopPerformerProductModel
+import org.wordpress.android.fluxc.store.WCStatsStore
+
+@ExperimentalCoroutinesApi
+class GetTopPerformersTest : BaseUnitTest() {
+    private val statsRepository: StatsRepository = mock()
+    private val dispatcher: CoroutineDispatcher = TestCoroutineDispatcher()
+
+    private val getTopPerformers = GetTopPerformers(
+        statsRepository,
+        dispatcher
+    )
+
+    @Test
+    fun `Given fetch product leader boards success, when result available, then emits sorted top performers`() =
+        testBlocking {
+            givenFetchProductLeaderboardsResult(Result.success(UNORDERED_PERFORMERS_PRODUCT_LIST))
+
+            val result = getTopPerformers(
+                false, WCStatsStore.StatsGranularity.DAYS, ANY_TOP_PERFORMERS_NUMBER
+            ).first()
+
+            assertThat(result).isEqualTo(
+                GetTopPerformers.LoadTopPerformersResult.TopPerformersLoadedSuccess(SORTED_PERFORMERS_PRODUCT_LIST)
+            )
+        }
+
+    @Test
+    fun `Given error loading top performers, when result is available, then emits top performers error`() =
+        testBlocking {
+            givenFetchProductLeaderboardsResult(REPOSITORY_ERROR_RESULT)
+
+            val result = getTopPerformers(
+                false, WCStatsStore.StatsGranularity.DAYS, ANY_TOP_PERFORMERS_NUMBER
+            ).first()
+
+            assertThat(result).isEqualTo(TopPerformersLoadedError)
+        }
+
+    private suspend fun givenFetchProductLeaderboardsResult(result: Result<List<WCTopPerformerProductModel>>) {
+        whenever(
+            statsRepository.fetchProductLeaderboards(
+                anyBoolean(),
+                any(),
+                anyInt()
+            )
+        ).thenReturn(flow {
+            emit(result)
+        })
+    }
+
+    private companion object {
+        const val ANY_TOP_PERFORMERS_NUMBER = 3
+        const val ANY_ERROR_MESSAGE = "Error message"
+        val REPOSITORY_ERROR_RESULT: Result<List<WCTopPerformerProductModel>> =
+            Result.failure(Exception(ANY_ERROR_MESSAGE))
+        val UNORDERED_PERFORMERS_PRODUCT_LIST = listOf(
+            WCTopPerformerProductModel(quantity = 3, total = 3 * 10.toDouble()),
+            WCTopPerformerProductModel(quantity = 1, total = 15.toDouble()),
+            WCTopPerformerProductModel(quantity = 5, total = 5 * 5.toDouble())
+        )
+        val SORTED_PERFORMERS_PRODUCT_LIST = listOf(
+            WCTopPerformerProductModel(quantity = 5, total = 5 * 5.toDouble()),
+            WCTopPerformerProductModel(quantity = 3, total = 3 * 10.toDouble()),
+            WCTopPerformerProductModel(quantity = 1, total = 15.toDouble())
+        )
+    }
+}
