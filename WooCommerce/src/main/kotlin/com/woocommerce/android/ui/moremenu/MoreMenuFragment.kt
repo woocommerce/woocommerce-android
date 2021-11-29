4ac49f695577fdc78c0ca2ce883c06aaa24af93e
Ondrej Ruttkay <ondrej@automattic.com>
2021-11-29 17:56:10 +0100

    Remove irrelevant comment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuFragment.kt
@@ -3,10 +3,6 @@ package com.woocommerce.android.ui.moremenu
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.base.TopLevelFragment
 
-/**
- * This would be an empty fragment until issue
- * https://github.com/woocommerce/woocommerce-android/issues/5237  gets finished
- */
 class MoreMenuFragment : TopLevelFragment(R.layout.fragment_analytics) {
     override fun getFragmentTitle() = getString(R.string.more_menu)
 
@@ -17,3 +13,4 @@ class MoreMenuFragment : TopLevelFragment(R.layout.fragment_analytics) {
         return
     }
 }
+    
