05810ff88092840e64f6a3dce1f24ac4953f7acb
ThomazFB <thomazfcortez@gmail.com>
2021-11-30 23:07:46 -0300

    Update viewModel injection to use hilt nav graph delegation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFragment.kt
@@ -2,13 +2,13 @@ package com.woocommerce.android.ui.orders.creation
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentOrderCreationBinding
 import com.woocommerce.android.ui.base.BaseFragment
 
 class OrderCreationFragment : BaseFragment(R.layout.fragment_order_creation) {
-    private val viewModel: OrderCreationViewModel by viewModels()
+    private val viewModel by hiltNavGraphViewModels<OrderCreationViewModel>(R.id.nav_graph_order_creations)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
