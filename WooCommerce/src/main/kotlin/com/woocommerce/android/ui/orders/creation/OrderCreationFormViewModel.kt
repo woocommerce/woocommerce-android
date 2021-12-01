a41122c5f56fef393e23e8677c0b9b601e7c39b9
ThomazFB <thomazfcortez@gmail.com>
2021-12-01 20:33:55 -0300

    Set OrderCreationFormViewModel viewState configuration

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormViewModel.kt
@@ -1,9 +1,13 @@
 package com.woocommerce.android.ui.orders.creation
 
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.model.Order
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
+import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +15,17 @@ class OrderCreationFormViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val orderDetailRepository: OrderDetailRepository
 ) : ScopedViewModel(savedState) {
+    val viewStateData = LiveDataDelegate(savedState, ViewState())
+    private var viewState by viewStateData
 
+    fun onOrderStatusSelected(status: Order.Status) {
+        orderDetailRepository.getOrderStatus(status.value).let {
+            viewState = viewState.copy(orderStatus = it)
+        }
+    }
+
+    @Parcelize
+    data class ViewState(
+        val orderStatus: Order.OrderStatus? = null
+    ) : Parcelable
 }
