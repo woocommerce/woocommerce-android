e21b55a6112468850ac0b96f0c1f9998f971f313
ThomazFB <thomazfcortez@gmail.com>
2021-12-02 02:57:29 -0300

    Trigger ViewOrderStatusSelector event when requested

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormViewModel.kt
@@ -3,6 +3,8 @@ package com.woocommerce.android.ui.orders.creation
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.model.Order
+import com.woocommerce.android.model.Order.OrderStatus
+import com.woocommerce.android.ui.orders.OrderNavigationTarget.ViewOrderStatusSelector
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
 import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.LiveDataDelegate
@@ -16,21 +18,39 @@ import javax.inject.Inject
 @HiltViewModel
 class OrderCreationFormViewModel @Inject constructor(
     savedState: SavedStateHandle,
-    private val coroutineDispatchers: CoroutineDispatchers,
+    private val dispatchers: CoroutineDispatchers,
     private val orderDetailRepository: OrderDetailRepository
 ) : ScopedViewModel(savedState) {
     val viewStateData = LiveDataDelegate(savedState, ViewState())
     private var viewState by viewStateData
 
-    fun onOrderStatusSelected(status: Order.Status) = launch(coroutineDispatchers.io) {
-        val orderStatus = orderDetailRepository.getOrderStatus(status.value)
-        withContext(coroutineDispatchers.main) {
-            viewState = viewState.copy(orderStatus = orderStatus)
+    fun onOrderStatusSelected(status: Order.Status) {
+        launch(dispatchers.io) {
+            val orderStatus = orderDetailRepository.getOrderStatus(status.value)
+            withContext(dispatchers.main) {
+                viewState = viewState.copy(orderStatus = orderStatus)
+            }
+        }
+    }
+
+    fun onEditOrderStatusSelected() {
+        launch(dispatchers.io) {
+            val statusList = orderDetailRepository.getOrderStatusOptions().toTypedArray()
+            withContext(dispatchers.main) { displayOrderStatusSelector(statusList) }
+        }
+    }
+
+    private fun displayOrderStatusSelector(statusList: Array<OrderStatus>) {
+        viewState.orderStatus?.run {
+            ViewOrderStatusSelector(
+                currentStatus = statusKey,
+                orderStatusList = statusList
+            ).let { triggerEvent(it) }
         }
     }
 
     @Parcelize
     data class ViewState(
-        val orderStatus: Order.OrderStatus? = null
+        val orderStatus: OrderStatus? = null
     ) : Parcelable
 }
