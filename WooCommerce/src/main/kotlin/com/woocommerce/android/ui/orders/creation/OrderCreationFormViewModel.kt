fb46a17cff14095c3995eee3a95e9113b6dd0bf4
ThomazFB <thomazfcortez@gmail.com>
2021-12-02 02:46:54 -0300

    Make OrderStatus database get an async call

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormViewModel.kt
@@ -4,23 +4,28 @@ import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
+import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
 class OrderCreationFormViewModel @Inject constructor(
     savedState: SavedStateHandle,
+    private val coroutineDispatchers: CoroutineDispatchers,
     private val orderDetailRepository: OrderDetailRepository
 ) : ScopedViewModel(savedState) {
     val viewStateData = LiveDataDelegate(savedState, ViewState())
     private var viewState by viewStateData
 
-    fun onOrderStatusSelected(status: Order.Status) {
-        orderDetailRepository.getOrderStatus(status.value).let {
-            viewState = viewState.copy(orderStatus = it)
+    fun onOrderStatusSelected(status: Order.Status) = launch(coroutineDispatchers.io) {
+        val orderStatus = orderDetailRepository.getOrderStatus(status.value)
+        withContext(coroutineDispatchers.main) {
+            viewState = viewState.copy(orderStatus = orderStatus)
         }
     }
 
