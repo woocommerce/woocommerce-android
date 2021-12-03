0ff22c2adcb4a12a39ce0d5c548c56a7e4d4774a
Andrey <andrey.kidinov@a8c.com>
2021-12-03 11:25:07 +0300

    Replaced reflection usage with simple when

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/PaymentErrorMapper.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/PaymentErrorMapper.kt
@@ -12,6 +12,7 @@ import com.woocommerce.android.cardreader.payments.CardPaymentStatus.CardPayment
 import com.woocommerce.android.cardreader.payments.CardPaymentStatus.CardPaymentStatusErrorType.Generic
 import com.woocommerce.android.cardreader.payments.CardPaymentStatus.CardPaymentStatusErrorType.NoNetwork
 import com.woocommerce.android.cardreader.payments.CardPaymentStatus.PaymentFailed
+import com.woocommerce.android.cardreader.payments.CardPaymentStatus.CardPaymentStatusErrorType.DeclinedByStripeApiError.CardDeclined.*
 
 internal class PaymentErrorMapper {
     fun mapTerminalError(
@@ -31,19 +32,68 @@ internal class PaymentErrorMapper {
         return PaymentFailed(type, paymentData, exception.errorMessage)
     }
 
+    @Suppress("ComplexMethod")
     private fun mapDeclinedByStripeApiError(exception: TerminalException): DeclinedByStripeApiError =
-        // https://stripe.com/docs/error-codes#card-declined
-        // Apparently the implementation is contradict with the doc
-        // Some decline codes come without "card_declined" in "exception.apiError?.code"
-        DeclinedByStripeApiError.CardDeclined::class.sealedSubclasses
-            .mapNotNull { it.objectInstance }
-            .firstOrNull {
-                it.declineErrorCodes.contains(exception.apiError?.declineCode)
-            } ?: DeclinedByStripeApiError::class.sealedSubclasses
-            .mapNotNull { it.objectInstance }
-            .firstOrNull {
-                it.errorCodes.contains(exception.apiError?.code)
-            } ?: DeclinedByStripeApiError.Unknown
+        when (exception.apiError?.declineCode) {
+            "approve_with_id",
+            "issuer_not_available",
+            "processing_error",
+            "reenter_transaction",
+            "try_again_later" -> Temporary
+
+            "call_issuer",
+            "card_velocity_exceeded",
+            "do_not_honor",
+            "do_not_try_again",
+            "fraudulent",
+            "lost_card",
+            "merchant_blacklist",
+            "pickup_card",
+            "restricted_card",
+            "revocation_of_all_authorizations",
+            "revocation_of_authorization",
+            "security_violation",
+            "stolen_card",
+            "stop_payment_order" -> Fraud
+
+            "generic_decline",
+            "no_action_taken",
+            "not_permitted",
+            "service_not_allowed",
+            "transaction_not_allowed" -> DeclinedByStripeApiError.CardDeclined.Generic
+
+            "invalid_account",
+            "new_account_information_available" -> InvalidAccount
+
+            "card_not_supported" -> CardNotSupported
+
+            "currency_not_supported" -> CurrencyNotSupported
+
+            "duplicate_transaction" -> DuplicateTransaction
+
+            "expired_card" -> ExpiredCard
+
+            "incorrect_zip" -> IncorrectPostalCode
+
+            "insufficient_funds",
+            "withdrawal_count_limit_exceeded" -> InsufficientFunds
+
+            "invalid_amount" -> InvalidAmount
+
+            "invalid_pin",
+            "offline_pin_required",
+            "online_or_offline_pin_required" -> PinRequired
+
+            "pin_try_exceeded" -> TooManyPinTries
+
+            "testmode_decline" -> TestCard
+
+            "test_mode_live_card" -> TestModeLiveCard
+            else -> when (exception.apiError?.code) {
+                "amount_too_small" -> DeclinedByStripeApiError.AmountTooSmall
+                else -> DeclinedByStripeApiError.Unknown
+            }
+        }
 
     fun mapCapturePaymentError(
         originalPaymentIntent: PaymentIntent,
--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/payments/CardPaymentStatus.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/payments/CardPaymentStatus.kt
@@ -20,204 +20,111 @@ sealed class CardPaymentStatus {
         object Server : CardPaymentStatusErrorType()
         object Generic : CardPaymentStatusErrorType()
 
-        sealed class DeclinedByStripeApiError(val errorCodes: Array<String>) : CardPaymentStatusErrorType() {
+        sealed class DeclinedByStripeApiError : CardPaymentStatusErrorType() {
             /**
              * The specified amount is less than the minimum amount allowed (50 cents at the moment)
              */
-            object AmountTooSmall : DeclinedByStripeApiError(
-                arrayOf(
-                    "amount_too_small"
-                )
-            )
+            object AmountTooSmall : DeclinedByStripeApiError()
 
             /**
              * Declined by stripe api with unknown reason
              */
-            object Unknown : DeclinedByStripeApiError(emptyArray())
+            object Unknown : DeclinedByStripeApiError()
 
-            sealed class CardDeclined(val declineErrorCodes: Array<String>) : DeclinedByStripeApiError(emptyArray()) {
-                companion object {
-                    const val ERROR_CODE = "card_declined"
-                }
-
+            sealed class CardDeclined : DeclinedByStripeApiError() {
                 /**
                  * A possibly temporary error caused the decline (e.g. the issuing
                  * bank's servers could not be contacted.) Tell the user this and prompt
                  * them to try again with the same (or another) payment method.
                  */
-                object Temporary : CardDeclined(
-                    arrayOf(
-                        "approve_with_id",
-                        "issuer_not_available",
-                        "processing_error",
-                        "reenter_transaction",
-                        "try_again_later",
-                    )
-                )
+                object Temporary : CardDeclined()
 
                 /**
                  * The card has been reported lost or stolen. Don't reveal this
                  * to the user. Treat it like you would a generic decline.
                  */
-                object Fraud : CardDeclined(
-                    arrayOf(
-                        "call_issuer",
-                        "card_velocity_exceeded",
-                        "do_not_honor",
-                        "do_not_try_again",
-                        "fraudulent",
-                        "lost_card",
-                        "merchant_blacklist",
-                        "pickup_card",
-                        "restricted_card",
-                        "revocation_of_all_authorizations",
-                        "revocation_of_authorization",
-                        "security_violation",
-                        "stolen_card",
-                        "stop_payment_order",
-                    )
-                )
+                object Fraud : CardDeclined()
 
                 /**
                  * The payment was declined for an unspecified reason. Ask
                  * the user to try another payment method.
                  */
-                object Generic : CardDeclined(
-                    arrayOf(
-                        "generic_decline",
-                        "no_action_taken",
-                        "not_permitted",
-                        "service_not_allowed",
-                        "transaction_not_allowed",
-                    )
-                )
+                object Generic : CardDeclined()
 
                 /**
                  * The card, or the account it connects to, is not valid. Ask
                  * the user to try another payment method.
                  */
-                object InvalidAccount : CardDeclined(
-                    arrayOf(
-                        "invalid_account",
-                        "new_account_information_available",
-                    )
-                )
+                object InvalidAccount : CardDeclined()
 
                 /**
                  * The card presented is not supported. Tell the user this and
                  * ask them to try another payment method.
                  */
-                object CardNotSupported : CardDeclined(
-                    arrayOf(
-                        "card_not_supported",
-                    )
-                )
+                object CardNotSupported : CardDeclined()
 
                 /**
                  * The currency is not supported by the card presented. Tell the
                  * user this and ask them to try another payment method.
                  */
-                object CurrencyNotSupported : CardDeclined(
-                    arrayOf(
-                        "currency_not_supported",
-                    )
-                )
+                object CurrencyNotSupported : CardDeclined()
 
                 /**
                  * An identical transaction was just completed for the card presented.
                  * Tell the user this and ask them to try another payment method if they
                  * really want to do this.
                  */
-                object DuplicateTransaction : CardDeclined(
-                    arrayOf(
-                        "duplicate_transaction",
-                    )
-                )
+                object DuplicateTransaction : CardDeclined()
 
                 /**
                  * The card presented has expired. Tell the user this and ask them
                  * to try another payment method.
                  */
-                object ExpiredCard : CardDeclined(
-                    arrayOf(
-                        "expired_card",
-                    )
-                )
+                object ExpiredCard : CardDeclined()
 
                 /**
                  * The card presented has a different ZIP/postal code than was
                  * used to place the order. Tell the user this and ask them
                  * to try another payment method (or correct the order.)
                  */
-                object IncorrectPostalCode : CardDeclined(
-                    arrayOf(
-                        "incorrect_zip",
-                    )
-                )
+                object IncorrectPostalCode : CardDeclined()
 
                 /**
                  * The card presented has insufficient funds for the purchase.
                  * Tell the user this and ask them to try another payment method.
                  */
-                object InsufficientFunds : CardDeclined(
-                    arrayOf(
-                        "insufficient_funds",
-                        "withdrawal_count_limit_exceeded",
-                    )
-                )
+                object InsufficientFunds : CardDeclined()
 
                 /**
                  * The card presented does not allow purchases of the amount
                  * given. Tell the user this and ask them to try another payment method.
                  */
-                object InvalidAmount : CardDeclined(
-                    arrayOf(
-                        "invalid_amount",
-                    )
-                )
+                object InvalidAmount : CardDeclined()
 
                 /**
                  * The card presented requires a PIN and the device doesn't support
                  * PIN entry. Tell the user this and ask them to try another payment method.
                  */
-                object PinRequired : CardDeclined(
-                    arrayOf(
-                        "invalid_pin",
-                        "offline_pin_required",
-                        "online_or_offline_pin_required",
-                    )
-                )
+                object PinRequired : CardDeclined()
 
                 /**
                  * The card presented has had an incorrect PIN entered too many times.
                  * Tell the user this and ask them to try another payment method.
                  */
-                object TooManyPinTries : CardDeclined(
-                    arrayOf(
-                        "pin_try_exceeded",
-                    )
-                )
+                object TooManyPinTries : CardDeclined()
 
                 /**
                  * The card presented is a system test card and cannot be used to
                  * process a payment. Tell the user this and ask them to try another
                  * payment method.
                  */
-                object TestCard : CardDeclined(
-                    arrayOf(
-                        "testmode_decline",
-                    )
-                )
+                object TestCard : CardDeclined()
 
                 /**
                  * The card presented is a live card, but the store/account is in test
                  * mode. Tell the user this and ask them to use a system test card instead.
                  */
-                object TestModeLiveCard : CardDeclined(
-                    arrayOf(
-                        "test_mode_live_card"
-                    )
-                )
+                object TestModeLiveCard : CardDeclined()
             }
         }
     }
