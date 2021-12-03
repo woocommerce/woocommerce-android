d1836f1b4b9d57b748f6de9dc9c7331e8d5d646c
hicham <hicham.boushaba@gmail.com>
2021-12-03 17:50:50 +0100

    Update the navigation target

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationNavigationTarget.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationNavigationTarget.kt
@@ -3,5 +3,5 @@ package com.woocommerce.android.ui.orders.creation
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 
 sealed class OrderCreationNavigationTarget : Event() {
-    data class EditCustomerNote(val currentNote: String): OrderCreationNavigationTarget()
+    object EditCustomerNote: OrderCreationNavigationTarget()
 }
