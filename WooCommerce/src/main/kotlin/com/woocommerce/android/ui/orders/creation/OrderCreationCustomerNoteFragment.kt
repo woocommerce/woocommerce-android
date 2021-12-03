13fde314e1c3eb97ffa49a62aa04e324675b8393
hicham <hicham.boushaba@gmail.com>
2021-12-03 17:41:33 +0100

    Add a new fragment for adding customer notes

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationCustomerNoteFragment.kt
@@ -0,0 +1,46 @@
+package com.woocommerce.android.ui.orders.creation
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentEditCustomerOrderNoteBinding
+import com.woocommerce.android.ui.base.BaseFragment
+
+class OrderCreationCustomerNoteFragment : BaseFragment(R.layout.fragment_edit_customer_order_note) {
+    private val sharedViewModel by hiltNavGraphViewModels<OrderCreationViewModel>(R.id.nav_graph_order_creations)
+
+    private var _binding: FragmentEditCustomerOrderNoteBinding? = null
+    val binding
+        get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentEditCustomerOrderNoteBinding.bind(view)
+        binding.customerOrderNoteEditor.setText(sharedViewModel.currentDraft.customerNote)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.menu_done, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_done -> {
+                sharedViewModel.onCustomerNoteEdited(binding.customerOrderNoteEditor.text.toString())
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
