335545e7c9d1c69515642be5f1eacd03644935c4
ThomazFB <thomazfcortez@gmail.com>
2021-12-03 17:27:52 -0300

    Modify OrderCreationViewModel to keep OrderStatus synced by itself

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
@@ -1,23 +1,39 @@
 package com.woocommerce.android.ui.orders.creation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.model.Order
+import com.woocommerce.android.model.Order.OrderStatus
+import com.woocommerce.android.ui.orders.details.OrderDetailRepository
 import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class OrderCreationViewModel @Inject constructor(
-    savedState: SavedStateHandle
+    savedState: SavedStateHandle,
+    private val orderDetailRepository: OrderDetailRepository
 ) : ScopedViewModel(savedState) {
-    val orderDraftData = LiveDataDelegate(savedState, Order.EMPTY)
+    val orderDraftData = LiveDataDelegate(savedState, Order.EMPTY) { old, new ->
+        if (old?.status != new.status) {
+            updateOrderStatus(new.status)
+        }
+    }
     private var orderDraft by orderDraftData
 
-    val currentStatus
-        get() = orderDraft.status
+    private var _orderStatus = MutableLiveData<OrderStatus>()
+    val orderStatus: LiveData<OrderStatus> = _orderStatus
 
     fun onOrderStatusChanged(status: Order.Status) {
         orderDraft = orderDraft.copy(status = status)
+    }
+
+    private fun updateOrderStatus(status: Order.Status) {
+        launch {
+            _orderStatus.value = orderDetailRepository.getOrderStatus(status.value)
+        }
     }
 }
