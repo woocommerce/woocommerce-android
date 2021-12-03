bb2745f52c20b266bc51cf800b275f8f1c829de3
hicham <hicham.boushaba@gmail.com>
2021-12-03 17:49:57 +0100

    Add a class to handle navigation

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationNavigator.kt
@@ -0,0 +1,19 @@
+package com.woocommerce.android.ui.orders.creation
+
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.ui.orders.creation.OrderCreationNavigationTarget.EditCustomerNote
+import javax.inject.Inject
+
+class OrderCreationNavigator @Inject constructor() {
+    fun navigate(fragment: Fragment, target: OrderCreationNavigationTarget) {
+        val navController = fragment.findNavController()
+
+        val action = when (target) {
+            is EditCustomerNote -> OrderCreationFormFragmentDirections
+                .actionOrderCreationFragmentToOrderCreationCustomerNoteFragment()
+        }
+
+        navController.navigate(action)
+    }
+}
