104b13a77607d7a1a388b963bc783cf4feb59ad3
colymore <rfermontero@gmail.com>
2021-12-07 18:36:44 +0100

    Refactor ViewModel tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/AnalyticsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/AnalyticsViewModelTest.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.analytics
 
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.model.RevenueStat
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.analytics.AnalyticsRepository.RevenueResult.RevenueData
 import com.woocommerce.android.ui.analytics.daterangeselector.AnalyticsDateRangeCalculator
 import com.woocommerce.android.ui.analytics.daterangeselector.AnalyticsDateRanges.LAST_YEAR
@@ -16,8 +17,10 @@ import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.ResourceProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.asFlow
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.kotlin.*
+import org.wordpress.android.fluxc.model.SiteModel
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.test.assertEquals
@@ -40,8 +43,19 @@ class AnalyticsViewModelTest : BaseUnitTest() {
         )
     }
 
-    private val currencyFormatter: CurrencyFormatter = mock()
-    private val analyticsRepository: AnalyticsRepository = mock()
+    private val currencyFormatter: CurrencyFormatter = mock {
+        on { formatCurrency(TOTAL_VALUE.toString(), CURRENCY_CODE) } doReturn TOTAL_CURRENCY_VALUE
+        on { formatCurrency(NET_VALUE.toString(), CURRENCY_CODE) } doReturn NET_CURRENCY_VALUE
+    }
+
+    private val analyticsRepository: AnalyticsRepository = mock {
+        on { getRevenueAdminPanelUrl() } doReturn ANY_URL
+    }
+
+    private val siteModel: SiteModel = mock()
+    private val selectedSite: SelectedSite = mock {
+        on { getIfExists() } doReturn siteModel
+    }
     private val savedState = SavedStateHandle()
 
     private lateinit var sut: AnalyticsViewModel
@@ -52,7 +66,7 @@ class AnalyticsViewModelTest : BaseUnitTest() {
             val resourceProvider: ResourceProvider = mock {
                 on { getString(any()) } doReturn ANY_VALUE
                 on { getString(any(), anyVararg()) } doReturn ANY_DATE_RANGE_EXPECTED_DATE_MESSAGE
-                on { getStringArray(any()) } doAnswer { dateRangeSelectors.toTypedArray() }
+                on { getStringArray(any()) } doAnswer { DATE_RANGE_SELECTORS.toTypedArray() }
             }
 
             sut = givenAViewModel(resourceProvider)
@@ -62,7 +76,7 @@ class AnalyticsViewModelTest : BaseUnitTest() {
                 assertEquals(ANY_VALUE, selectedPeriod)
                 assertEquals(ANY_DATE_RANGE_EXPECTED_DATE_MESSAGE, fromDatePeriod)
                 assertEquals(ANY_DATE_RANGE_EXPECTED_DATE_MESSAGE, toDatePeriod)
-                assertEquals(dateRangeSelectors, availableRangeDates)
+                assertEquals(DATE_RANGE_SELECTORS, availableRangeDates)
             }
 
             with(sut.state.value.revenueState) {
@@ -78,11 +92,10 @@ class AnalyticsViewModelTest : BaseUnitTest() {
                     listOf(ANY_VALUE, LAST_YEAR.description)
                 on { getString(any(), anyVararg()) } doReturnConsecutively
                     listOf(ANY_DATE_RANGE_EXPECTED_DATE_MESSAGE, ANY_OTHER_RANGE_EXPECTED_DATE_MESSAGE)
-                on { getStringArray(any()) } doAnswer { dateRangeSelectors.toTypedArray() }
+                on { getStringArray(any()) } doAnswer { DATE_RANGE_SELECTORS.toTypedArray() }
             }
 
             sut = givenAViewModel(resourceProvider)
-
             sut.onSelectedDateRangeChanged(LAST_YEAR.description)
 
             with(sut.state.value.analyticsDateRangeSelectorState) {
@@ -90,26 +103,17 @@ class AnalyticsViewModelTest : BaseUnitTest() {
                 assertEquals(LAST_YEAR.description, selectedPeriod)
                 assertEquals(ANY_OTHER_RANGE_EXPECTED_DATE_MESSAGE, fromDatePeriod)
                 assertEquals(ANY_OTHER_RANGE_EXPECTED_DATE_MESSAGE, toDatePeriod)
-                assertEquals(dateRangeSelectors, availableRangeDates)
+                assertEquals(DATE_RANGE_SELECTORS, availableRangeDates)
             }
         }
 
     @Test
     fun `given a view model, when selected date range changes, then has expected revenue values`() =
         testBlocking {
-            val resourceProvider: ResourceProvider = mock {
-                on { getString(any()) } doAnswer { invocationOnMock -> invocationOnMock.arguments[0].toString() }
-                on { getString(any(), any()) } doAnswer { invMock -> invMock.arguments[0].toString() }
-                on { getStringArray(any()) } doAnswer { dateRangeSelectors.toTypedArray() }
-            }
-
-            sut = givenAViewModel(resourceProvider)
-            whenever(currencyFormatter.formatCurrency(TOTAL_VALUE.toString(), CURRENCY_CODE))
-                .thenReturn(TOTAL_CURRENCY_VALUE)
             whenever(analyticsRepository.fetchRevenueData(any(), any()))
                 .thenReturn(listOf(getRevenueStats(), getRevenueStats()).asFlow())
-            whenever(currencyFormatter.formatCurrency(NET_VALUE.toString(), CURRENCY_CODE))
-                .thenReturn(NET_CURRENCY_VALUE)
+
+            sut = givenAViewModel()
 
             sut.onSelectedDateRangeChanged(LAST_YEAR.description)
 
@@ -126,8 +130,50 @@ class AnalyticsViewModelTest : BaseUnitTest() {
             }
         }
 
-    private fun givenAViewModel(resourceProvider: ResourceProvider) =
-        AnalyticsViewModel(resourceProvider, dateUtil, calculator, currencyFormatter, analyticsRepository, savedState)
+    @Test
+    fun `given a WPCom site, when see report is clicked, then OpenWPComWebView event is triggered`() =
+        testBlocking {
+            whenever(siteModel.isWPCom).thenReturn(true)
+
+            sut = givenAViewModel()
+            sut.onRevenueSeeReportClick()
+
+            assertThat(sut.event.value).isInstanceOf(AnalyticsViewEvent.OpenWPComWebView::class.java)
+        }
+
+    @Test
+    fun `given a WPComAtomic site, when see report is clicked, then OpenWPComWebView event is triggered`() =
+        testBlocking {
+            whenever(siteModel.isWPComAtomic).thenReturn(true)
+
+            sut = givenAViewModel()
+            sut.onRevenueSeeReportClick()
+
+            assertThat(sut.event.value).isInstanceOf(AnalyticsViewEvent.OpenWPComWebView::class.java)
+        }
+
+    @Test
+    fun `given a no WPComAtomic and no WPCom site, when see report is clicked, then OpenUrl event is triggered`() =
+        testBlocking {
+            whenever(siteModel.isWPComAtomic).thenReturn(false)
+            whenever(siteModel.isWPCom).thenReturn(false)
+
+            sut = givenAViewModel()
+            sut.onRevenueSeeReportClick()
+
+            assertThat(sut.event.value).isInstanceOf(AnalyticsViewEvent.OpenUrl::class.java)
+        }
+
+    private fun givenAResourceProvider(): ResourceProvider = mock {
+        on { getString(any()) } doAnswer { invocationOnMock -> invocationOnMock.arguments[0].toString() }
+        on { getString(any(), any()) } doAnswer { invMock -> invMock.arguments[0].toString() }
+        on { getStringArray(any()) } doAnswer { DATE_RANGE_SELECTORS.toTypedArray() }
+    }
+
+    private fun givenAViewModel(resourceProvider: ResourceProvider = givenAResourceProvider()) =
+        AnalyticsViewModel(resourceProvider, dateUtil, calculator,
+            currencyFormatter, analyticsRepository,
+            selectedSite, savedState)
 
     private fun getRevenueStats() =
         RevenueData(RevenueStat(TOTAL_VALUE, TOTAL_DELTA, NET_VALUE, NET_DELTA, CURRENCY_CODE))
@@ -148,7 +194,7 @@ class AnalyticsViewModelTest : BaseUnitTest() {
         private val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         private val ANY_DATE: Date = sdf.parse(ANY_DATE_TIME_VALUE)!!
         private val ANY_OTHER_DATE: Date = sdf.parse(ANY_OTHER_DATE_TIME_VALUE)!!
-        private val dateRangeSelectors = listOf(ANY_VALUE, ANY_OTHER_VALUE)
+        private val DATE_RANGE_SELECTORS = listOf(ANY_VALUE, ANY_OTHER_VALUE)
 
         const val TOTAL_VALUE = 10.0
         const val TOTAL_DELTA = 5
@@ -158,5 +204,6 @@ class AnalyticsViewModelTest : BaseUnitTest() {
         const val TOTAL_CURRENCY_VALUE = "10 E"
         const val NET_CURRENCY_VALUE = "10 E"
 
+        const val ANY_URL = "https://a8c.com"
     }
 }
