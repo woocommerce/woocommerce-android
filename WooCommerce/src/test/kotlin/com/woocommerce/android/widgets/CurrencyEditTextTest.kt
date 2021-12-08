2ffece4142eef790732ee60d803f866588a1b01e
Shiki <jayson@basanes.net>
2021-12-08 15:09:52 -0700

    Change tests to indicate char deletion for symbols on the right

--- WooCommerce/src/test/kotlin/com/woocommerce/android/widgets/CurrencyEditTextTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/widgets/CurrencyEditTextTest.kt
@@ -29,18 +29,41 @@ class CurrencyEditTextTest {
      *
      * Example scenario:
      *
-     * 1. If the current text is "123.79$" (French Canada places the dollar symbol on the right)
+     * 1. If the current text is "123.79CA$" (French Canada places the dollar symbol on the right)
      * 2. The user presses backspace
-     * 3. The [CurrencyEditText.onTextChanged] will be called with [text] equal to "123.79"
+     * 3. The [CurrencyEditText.onTextChanged] will be called with [text] equal to "123.79CA"
      *
-     * If keep this as is and reformat again, we'll end up with same text, "123.79$". So it'll look like the backspace
+     * If keep this as is and reformat again, we'll end up with same text, "123.79CA$". So it'll look like the backspace
      * was ignored.
      */
     @Test
     fun `given a currency symbol on the right, when pressing backspace, clean() removes the last digit`() {
-        val text = "123.79$"
+        val text = "123.79CA"
 
         // We're only emulating a backspace by specifying that the [lengthBefore] is larger than the [lengthAfter].
+        val cleaned = CurrencyEditText.clean(text = text, decimals = 2, lengthBefore = text.length + 1, lengthAfter = text.length)
+
+        assertEquals(12.37.toBigDecimal(), cleaned)
+    }
+
+    @Test
+    fun `given a currency symbol with space on the right, when pressing backspace, clean() removes the last digit`() {
+        val text = "123.79 $"
+
+        // We're only emulating a backspace by specifying that the [lengthBefore] is larger than the [lengthAfter].
+        val cleaned = CurrencyEditText.clean(text = text, decimals = 2, lengthBefore = text.length + 1, lengthAfter = text.length)
+
+        assertEquals(12.37.toBigDecimal(), cleaned)
+    }
+
+    /**
+     * In this case, we assume that there was previously a symbol on the right but the backspace deleted it. To emulate
+     * proper backspace behavior, the last digit should be removed.
+     */
+    @Test
+    fun `given no currency symbol, when pressing backspace, clean() removes the last digit`() {
+        val text = "123.79"
+
         val cleaned = CurrencyEditText.clean(text = text, decimals = 2, lengthBefore = text.length + 1, lengthAfter = text.length)
 
         assertEquals(12.37.toBigDecimal(), cleaned)
@@ -59,23 +82,21 @@ class CurrencyEditTextTest {
      * If we manually remove the last digit, we'd end up with "123", which means we've incorrectly deleted 2 characters!
      */
     @Test
-    fun `given no currency symbol, when pressing backspace, clean() removes the last digit`() {
-        val text = "123.79"
-
-        // We're only emulating a backspace by specifying that the [lengthBefore] is larger than the [lengthAfter].
-        val cleaned = CurrencyEditText.clean(text = text, decimals = 2, lengthBefore = text.length + 1, lengthAfter = text.length)
-
-        assertEquals(123.79.toBigDecimal(), cleaned)
-    }
-
-    @Test
     fun `given a currency symbol on the left, when pressing backspace, clean() does not remove the last digit`() {
         val text = "$1,237,87.39"
 
-        // We're only emulating a backspace by specifying that the [lengthBefore] is larger than the [lengthAfter].
         val cleaned = CurrencyEditText.clean(text = text, decimals = 2, lengthBefore = text.length + 1, lengthAfter = text.length)
 
         assertEquals(1_237_87.39.toBigDecimal(), cleaned)
+    }
+
+    @Test
+    fun `given a currency symbol with space on the left, when pressing backspace, clean() does not remove the last digit`() {
+        val text = "$ 123.89"
+
+        val cleaned = CurrencyEditText.clean(text = text, decimals = 2, lengthBefore = text.length + 1, lengthAfter = text.length)
+
+        assertEquals(123.89.toBigDecimal(), cleaned)
     }
 
     @Test
