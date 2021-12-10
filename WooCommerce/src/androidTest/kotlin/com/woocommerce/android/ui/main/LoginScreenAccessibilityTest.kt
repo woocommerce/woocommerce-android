e42fe6fc1b3e8c913ec19e5fafdf042bd960055a
Anirudh Uppunda <abhat38@gmail.com>
2021-12-10 11:46:53 +0530

    add accessibility for login screen

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/main/LoginScreenAccessibilityTest.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/ui/main/LoginScreenAccessibilityTest.kt
@@ -2,11 +2,13 @@ package com.woocommerce.android.ui.main
 
 import androidx.test.espresso.accessibility.AccessibilityChecks
 import androidx.test.rule.ActivityTestRule
+import com.woocommerce.android.BuildConfig
 import com.woocommerce.android.helpers.InitializationRule
 import com.woocommerce.android.helpers.TestBase
 import com.woocommerce.android.screenshots.login.WelcomeScreen
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,12 +23,42 @@ class LoginScreenAccessibilityTest : TestBase() {
     @get:Rule(order = 2)
     var activityRule = ActivityTestRule(MainActivity::class.java)
 
+    @Before
+    fun setUp() {
+        AccessibilityChecks.enable().setRunChecksFromRootView(true)
+    }
 
     @Test
-    fun loginScreenAccessibilityCheck() {
-        AccessibilityChecks.enable().setRunChecksFromRootView(true)
+    fun welcomeScreenAccessibilityCheck() {
         WelcomeScreen
             .logoutIfNeeded()
             .selectLogin()
+    }
+
+    @Test
+    fun storeAddressScreenAccessibilityCheck() {
+        WelcomeScreen
+            .logoutIfNeeded()
+            .selectLogin()
+            .proceedWith(BuildConfig.SCREENSHOTS_URL)
+    }
+
+    @Test
+    fun emailAddressScreenAccessibilityCheck() {
+        WelcomeScreen
+            .logoutIfNeeded()
+            .selectLogin()
+            .proceedWith(BuildConfig.SCREENSHOTS_URL)
+            .proceedWith(BuildConfig.SCREENSHOTS_USERNAME)
+    }
+
+    @Test
+    fun passwordScreenAccessibilityCheck() {
+        WelcomeScreen
+            .logoutIfNeeded()
+            .selectLogin()
+            .proceedWith(BuildConfig.SCREENSHOTS_URL)
+            .proceedWith(BuildConfig.SCREENSHOTS_USERNAME)
+            .proceedWith(BuildConfig.SCREENSHOTS_PASSWORD)
     }
 }
