1360f03b0a631df3b33911dcd904f505f21675fd
Mucientes <jorge.mucientes@automattic.com>
2021-12-16 17:08:45 +0100

    Test jUnit 5 test in CI environment

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/filters/OrderFilterCategoriesViewModelTestjUnit5.kt
@@ -0,0 +1,133 @@
+package com.woocommerce.android.ui.orders.filters
+
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.ui.orders.OrderTestUtils
+import com.woocommerce.android.ui.orders.filters.data.*
+import com.woocommerce.android.ui.orders.filters.domain.GetDateRangeFilterOptions
+import com.woocommerce.android.ui.orders.filters.domain.GetOrderStatusFilterOptions
+import com.woocommerce.android.ui.orders.filters.domain.GetTrackingForFilterSelection
+import com.woocommerce.android.ui.orders.filters.model.OrderFilterCategoryUiModel
+import com.woocommerce.android.ui.orders.filters.model.OrderFilterEvent
+import com.woocommerce.android.ui.orders.filters.model.OrderFilterOptionUiModel
+import com.woocommerce.android.util.CoroutinesTestExtension
+import com.woocommerce.android.util.DateUtils
+import com.woocommerce.android.util.InstantExecutorExtension
+import com.woocommerce.android.viewmodel.ResourceProvider
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@ExperimentalCoroutinesApi
+@ExtendWith(InstantExecutorExtension::class)
+class OrderFilterCategoriesViewModelTestjUnit5 {
+    private val savedStateHandle: SavedStateHandle = SavedStateHandle()
+    private val resourceProvider: ResourceProvider = mock()
+    private val getOrderStatusFilterOptions: GetOrderStatusFilterOptions = mock()
+    private val getDateRangeFilterOptions: GetDateRangeFilterOptions = mock()
+    private val orderFilterRepository: OrderFiltersRepository = mock()
+    private val getTrackingForFilterSelection: GetTrackingForFilterSelection = mock()
+    private val dateUtils: DateUtils = mock()
+
+    private lateinit var viewModel: OrderFilterCategoriesViewModel
+
+    private val currentCategoryList
+        get() = viewModel.categories.liveData.value!!.list
+
+    @ExperimentalCoroutinesApi
+    @RegisterExtension @JvmField
+    val coroutinesTestExtension = CoroutinesTestExtension()
+
+    @BeforeEach
+    fun setup() = runBlockingTest {
+        givenResourceProviderReturnsNonEmptyStrings()
+        givenOrderStatusOptionsAvailable()
+        initViewModel()
+    }
+
+    @Nested
+    inner class FilterCategorySelected {
+        @Test
+        @DisplayName("When filter category is selected, then update screen title")
+        fun selectCategoryUpdatesScreen() {
+            initViewModel()
+            viewModel.onFilterCategorySelected(AN_ORDER_STATUS_FILTER_CATEGORY_WITH_SELECTED_FILTER)
+
+            Assertions.assertThat(viewModel.event.value).isEqualTo(
+                OrderFilterEvent.ShowFilterOptionsForCategory(
+                    AN_ORDER_STATUS_FILTER_CATEGORY_WITH_SELECTED_FILTER
+                )
+            )
+        }
+    }
+
+    private fun initViewModel() {
+        viewModel = OrderFilterCategoriesViewModel(
+            savedStateHandle,
+            resourceProvider,
+            getOrderStatusFilterOptions,
+            getDateRangeFilterOptions,
+            orderFilterRepository,
+            getTrackingForFilterSelection,
+            dateUtils
+        )
+    }
+
+    private suspend fun givenOrderStatusOptionsAvailable() {
+        whenever(getOrderStatusFilterOptions.invoke()).thenReturn(
+            OrderTestUtils.generateOrderStatusOptions()
+                .map {
+                    OrderStatusOption(
+                        key = it.statusKey,
+                        label = it.label,
+                        statusCount = it.statusCount,
+                        isSelected = false
+                    )
+                }
+        )
+    }
+
+    private fun givenDateRangeFiltersAvailable() {
+        whenever(getDateRangeFilterOptions.invoke()).thenReturn(
+            listOf(DateRange.TODAY, DateRange.LAST_2_DAYS, DateRange.LAST_7_DAYS, DateRange.LAST_30_DAYS)
+                .map {
+                    DateRangeFilterOption(
+                        dateRange = it,
+                        isSelected = false,
+                        startDate = 0,
+                        endDate = 0
+                    )
+                }
+        )
+    }
+
+    private fun givenResourceProviderReturnsNonEmptyStrings() {
+        whenever(resourceProvider.getString(any())).thenReturn("AnyString")
+        whenever(resourceProvider.getString(any(), any(), any())).thenReturn("AnyString")
+    }
+
+    private companion object {
+        const val DEFAULT_FILTER_TITLE = "Title"
+        const val ANY_ORDER_STATUS_KEY = "OrderStatusOptionKey"
+        val SELECTED_ORDER_STATUS_FILTER_OPTION = OrderFilterOptionUiModel(
+            key = ANY_ORDER_STATUS_KEY,
+            displayName = "OrderStatus",
+            isSelected = true
+        )
+        val A_LIST_OF_ORDER_STATUS_FILTER_OPTIONS = listOf(SELECTED_ORDER_STATUS_FILTER_OPTION)
+        val AN_ORDER_STATUS_FILTER_CATEGORY_WITH_SELECTED_FILTER = OrderFilterCategoryUiModel(
+            categoryKey = OrderListFilterCategory.ORDER_STATUS,
+            displayName = "",
+            displayValue = "",
+            orderFilterOptions = A_LIST_OF_ORDER_STATUS_FILTER_OPTIONS
+        )
+    }
+}
---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/util/Junit5Extensions.kt
@@ -0,0 +1,55 @@
+package com.woocommerce.android.util
+
+import androidx.arch.core.executor.ArchTaskExecutor
+import androidx.arch.core.executor.TaskExecutor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+/**
+ * Add this JUnit 5 extension to your test class using
+ * @JvmField
+ * @RegisterExtension
+ * val coroutinesTestExtension = CoroutinesTestExtension()
+ */
+@ExperimentalCoroutinesApi
+class CoroutinesTestExtension(
+    private val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+) : BeforeEachCallback, AfterEachCallback, TestCoroutineScope by TestCoroutineScope(testDispatcher) {
+
+    override fun beforeEach(context: ExtensionContext?) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+}
+
+class InstantExecutorExtension : BeforeEachCallback, AfterEachCallback {
+
+    override fun beforeEach(context: ExtensionContext?) {
+        ArchTaskExecutor.getInstance()
+            .setDelegate(object : TaskExecutor() {
+                override fun executeOnDiskIO(runnable: Runnable) = runnable.run()
+
+                override fun postToMainThread(runnable: Runnable) = runnable.run()
+
+                override fun isMainThread(): Boolean = true
+            })
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        ArchTaskExecutor.getInstance().setDelegate(null)
+    }
+
+}
+
+
