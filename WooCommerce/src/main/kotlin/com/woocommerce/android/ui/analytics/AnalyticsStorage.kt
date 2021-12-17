876a9a2721ac7ef1f1f9f54ccbcc0f144393a73d
colymore <rfermontero@gmail.com>
2021-12-17 19:11:37 +0100

    Create Analytics storage

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/AnalyticsStorage.kt
@@ -0,0 +1,40 @@
+package com.woocommerce.android.ui.analytics
+
+import org.wordpress.android.fluxc.model.WCRevenueStatsModel
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import javax.inject.Inject
+
+class AnalyticsStorage @Inject constructor() {
+
+    private val statsConsumptions = mutableMapOf<Int, AtomicInteger>()
+    private val statsCache = mutableMapOf<Int, WCRevenueStatsModel>()
+
+    /**
+     * This in memory cache allows only MAX_STATS_CACHE_CONSUMPTIONS hits.
+     * After reach that number is restarted and next gets will miss until is populated again
+     */
+    fun getStats(startDate: String, endDate: String): WCRevenueStatsModel? {
+        val cacheKey = getStatsCacheKey(startDate, endDate)
+        val cacheConsumptionTimes = statsConsumptions.getOrElse(cacheKey) { AtomicInteger(0) }
+        if (cacheConsumptionTimes.get() < MAX_STATS_CACHE_CONSUMPTIONS) {
+            statsCache[cacheKey]?.let {
+                statsConsumptions[cacheKey]?.incrementAndGet() ?: statsConsumptions.put(cacheKey, AtomicInteger(1))
+                return it
+            } ?: return null
+        }
+        return null
+    }
+
+    fun saveStats(startDate: String, endDate: String, it: WCRevenueStatsModel) {
+        val cacheKey = getStatsCacheKey(startDate, endDate)
+        statsConsumptions[cacheKey] = AtomicInteger(0)
+        statsCache[cacheKey] = it
+    }
+
+    companion object {
+        const val MAX_STATS_CACHE_CONSUMPTIONS = 2
+    }
+
+    private fun getStatsCacheKey(startDate: String, endDate: String) = Objects.hash(startDate + endDate)
+}
---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/AnalyticsStorageTest.kt
@@ -0,0 +1,65 @@
+package com.woocommerce.android.ui.analytics
+
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.wordpress.android.fluxc.model.WCRevenueStatsModel
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class AnalyticsStorageTest : BaseUnitTest() {
+
+    private val sut = AnalyticsStorage()
+
+    @Test
+    fun `given an empty storage, when getStats first time, then result is null`() {
+        val result = sut.getStats(START_DATE, END_DATE)
+
+        assertNull(result)
+    }
+
+
+    @Test
+    fun `given a data in storage, when getStats first time, then result is expected`() {
+        val stats: WCRevenueStatsModel = mock()
+        sut.saveStats(START_DATE, END_DATE, stats)
+
+        val result = sut.getStats(START_DATE, END_DATE)
+
+        assertEquals(stats, result)
+    }
+
+    @Test
+    fun `given a data in storage with 3 hits and save stats, when get stats, then result is expected`() {
+        val stats: WCRevenueStatsModel = mock()
+        sut.saveStats(START_DATE, END_DATE, stats)
+        sut.getStats(START_DATE, END_DATE)
+        sut.getStats(START_DATE, END_DATE)
+        sut.getStats(START_DATE, END_DATE)
+
+        sut.saveStats(START_DATE, END_DATE, stats)
+        val result = sut.getStats(START_DATE, END_DATE)
+
+        assertEquals(stats, result)
+    }
+
+    @Test
+    fun `given a data in storage with 3 hits, when save stats, then result is null`() {
+        val stats: WCRevenueStatsModel = mock()
+        sut.saveStats(START_DATE, END_DATE, stats)
+        sut.getStats(START_DATE, END_DATE)
+        sut.getStats(START_DATE, END_DATE)
+        sut.getStats(START_DATE, END_DATE)
+
+        val result = sut.getStats(START_DATE, END_DATE)
+
+        assertNull(result)
+    }
+
+
+    companion object {
+        const val START_DATE = "10-10-2020"
+        const val END_DATE = "12-10-2020"
+
+    }
+}
