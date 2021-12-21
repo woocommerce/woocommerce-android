271d0d0be95316171c6155c3bf57342728fb5c75
ThomazFB <thomazfcortez@gmail.com>
2021-12-21 19:09:23 -0300

    Define basic scaffold for sharedViewModel and ViewBinding at OrderCreationProductSelectionFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationProductSelectionFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationProductSelectionFragment.kt
@@ -1,7 +1,20 @@
 package com.woocommerce.android.ui.orders.creation
 
+import android.os.Bundle
+import android.view.View
+import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentOrderCreationProductSelectionBinding
 import com.woocommerce.android.ui.base.BaseFragment
 
 class OrderCreationProductSelectionFragment :
-    BaseFragment(R.layout.fragment_order_creation_product_selection)
+    BaseFragment(R.layout.fragment_order_creation_product_selection) {
+    private val sharedViewModel by hiltNavGraphViewModels<OrderCreationViewModel>(R.id.nav_graph_order_creations)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(FragmentOrderCreationProductSelectionBinding.bind(view)) {
+
+        }
+    }
+}
