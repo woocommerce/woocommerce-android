030472fff4026dd53365b06900253296aa2fe223
ThomazFB <thomazfcortez@gmail.com>
2021-12-20 21:27:00 -0300

    Connect viewModel AddSimpleProduct event with nav graph action

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationNavigator.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationNavigator.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.orders.creation
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.ui.orders.creation.OrderCreationNavigationTarget.AddSimpleProduct
 import com.woocommerce.android.ui.orders.creation.OrderCreationNavigationTarget.EditCustomerNote
 
 object OrderCreationNavigator {
@@ -13,7 +14,9 @@ object OrderCreationNavigator {
                 TODO()
             EditCustomerNote ->
                 OrderCreationFormFragmentDirections.actionOrderCreationFragmentToOrderCreationCustomerNoteFragment()
-        }
+            AddSimpleProduct ->
+                OrderCreationFormFragmentDirections.actionOrderCreationFragmentToOrderCreationProductSelectionFragment()
+         }
 
         navController.navigate(action)
     }
