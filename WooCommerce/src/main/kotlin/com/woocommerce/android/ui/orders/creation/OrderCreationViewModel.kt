8cfd5db4e381b7eb04b4ba0be8e15051161df662
hicham <hicham.boushaba@gmail.com>
2021-12-22 17:13:45 +0100

    Create a data model for representing products and map it from the order draft

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
@@ -3,10 +3,13 @@ package com.woocommerce.android.ui.orders.creation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.extensions.mapAsync
 import com.woocommerce.android.extensions.runWithContext
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.Order.OrderStatus
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
+import com.woocommerce.android.ui.products.ProductDetailRepository
+import com.woocommerce.android.ui.products.variations.VariationDetailRepository
 import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
@@ -18,13 +21,29 @@ import javax.inject.Inject
 class OrderCreationViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val dispatchers: CoroutineDispatchers,
-    private val orderDetailRepository: OrderDetailRepository
+    private val orderDetailRepository: OrderDetailRepository,
+    private val productDetailRepository: ProductDetailRepository,
+    private val variationDetailRepository: VariationDetailRepository
 ) : ScopedViewModel(savedState) {
     val orderDraftData = LiveDataDelegate(savedState, Order.EMPTY, onChange = ::onOrderDraftChange)
     private var orderDraft by orderDraftData
 
     private val orderStatus = MutableLiveData<OrderStatus>()
     val orderStatusData: LiveData<OrderStatus> = orderStatus
+
+    val products: LiveData<List<ProductUIModel>> = orderDraftData.liveData.mapAsync { order ->
+        order.items.map { item ->
+            val stockQuantity = if (item.isVariation) {
+                variationDetailRepository.getVariation(item.productId, item.variationId)?.stockQuantity
+            } else {
+                productDetailRepository.getProduct(item.productId)?.stockQuantity
+            }
+            ProductUIModel(
+                item = item,
+                stockQuantity = stockQuantity ?: 0.0
+            )
+        }
+    }
 
     val currentDraft
         get() = orderDraft
@@ -56,3 +75,8 @@ class OrderCreationViewModel @Inject constructor(
         orderDraft = orderDraft.copy(customerNote = newNote)
     }
 }
+
+data class ProductUIModel(
+    val item: Order.Item,
+    val stockQuantity: Double
+)
