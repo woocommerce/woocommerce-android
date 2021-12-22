605290ae8908991b43d8fdd3497e774c9ae7f9cf
ThomazFB <thomazfcortez@gmail.com>
2021-12-21 23:45:26 -0300

    Initialize product list layoutManager and adapter

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationProductSelectionFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationProductSelectionFragment.kt
@@ -3,18 +3,34 @@ package com.woocommerce.android.ui.orders.creation
 import android.os.Bundle
 import android.view.View
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentOrderCreationProductSelectionBinding
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.products.OnLoadMoreListener
+import com.woocommerce.android.ui.products.ProductListAdapter
 
 class OrderCreationProductSelectionFragment :
-    BaseFragment(R.layout.fragment_order_creation_product_selection) {
+    BaseFragment(R.layout.fragment_order_creation_product_selection),
+    OnLoadMoreListener {
     private val sharedViewModel by hiltNavGraphViewModels<OrderCreationViewModel>(R.id.nav_graph_order_creations)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(FragmentOrderCreationProductSelectionBinding.bind(view)) {
-
+            productsList.layoutManager = LinearLayoutManager(requireActivity())
+            productsList.adapter = ProductListAdapter(
+                clickListener = ::onProductClick,
+                loadMoreListener = this@OrderCreationProductSelectionFragment
+            )
         }
+    }
+
+    private fun onProductClick(remoteProductId: Long, sharedView: View?) {
+
+    }
+
+    override fun onRequestLoadMore() {
+
     }
 }
