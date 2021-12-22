e3bdf36de535f964bb693b40f820390969e8f20b
ThomazFB <thomazfcortez@gmail.com>
2021-12-22 00:22:14 -0300

    Configure productList LiveData and request basic product loading

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/products/OrderCreationProductSelectionViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/products/OrderCreationProductSelectionViewModel.kt
@@ -1,10 +1,14 @@
 package com.woocommerce.android.ui.orders.creation.products
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.model.Product
 import com.woocommerce.android.ui.products.ProductListRepository
 import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +16,19 @@ class OrderCreationProductSelectionViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val dispatchers: CoroutineDispatchers,
     private val productListRepository: ProductListRepository
-) : ScopedViewModel(savedState)
+) : ScopedViewModel(savedState) {
+    private val productList = MutableLiveData<List<Product>>()
+    val productListData: LiveData<List<Product>> = productList
+
+    init {
+        fetchProductList()
+    }
+
+    fun fetchProductList(
+        loadMore: Boolean = false
+    ) {
+        launch {
+            productList.value = productListRepository.fetchProductList(loadMore)
+        }
+    }
+}
