0871162f4e3bfe8793c6db677fdf14bd7489d85e
hicham <hicham.boushaba@gmail.com>
2021-12-22 14:29:12 +0100

    Create a stepper view

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/StepperView.kt
@@ -0,0 +1,50 @@
+package com.woocommerce.android.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.ContextCompat
+import com.woocommerce.android.R
+import com.woocommerce.android.databinding.ViewStepperBinding
+
+class StepperView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayoutCompat(context, attrs, defStyleAttr) {
+    private val binding = ViewStepperBinding.inflate(LayoutInflater.from(context), this)
+
+    var value: Int
+        get() = binding.valueText.text.toString().toIntOrNull() ?: 0
+        set(value) {
+            binding.valueText.text = value.toString()
+        }
+
+    var isPlusButtonEnabled: Boolean
+        get() = binding.plusButton.isEnabled
+        set(value) {
+            binding.plusButton.isEnabled = value
+        }
+
+    var isMinusButtonEnabled: Boolean
+        get() = binding.minusButton.isEnabled
+        set(value) {
+            binding.minusButton.isEnabled = value
+        }
+
+    init {
+        background = ContextCompat.getDrawable(context, R.drawable.bg_stepper_view)
+        orientation = HORIZONTAL
+    }
+
+    fun init(
+        currentValue: Int = 0,
+        onPlusButtonClick: () -> Unit,
+        onMinusButtonClick: () -> Unit
+    ) {
+        value = currentValue
+        binding.plusButton.setOnClickListener { onPlusButtonClick() }
+        binding.minusButton.setOnClickListener { onMinusButtonClick() }
+    }
+}
