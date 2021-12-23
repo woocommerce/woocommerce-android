485a01abb653457bed527c47f2abd8f3416d6504
hicham <hicham.boushaba@gmail.com>
2021-12-23 11:16:37 +0100

    Simplify handling of order status changes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
@@ -1,10 +1,10 @@
 package com.woocommerce.android.ui.orders.creation
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.distinctUntilChanged
+import androidx.lifecycle.map
 import com.woocommerce.android.extensions.mapAsync
-import com.woocommerce.android.extensions.runWithContext
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.Order.OrderStatus
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
@@ -15,10 +15,7 @@ import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.wordpress.android.util.PhotonUtils
-import java.math.BigDecimal
 import javax.inject.Inject
 
 @HiltViewModel
@@ -34,21 +31,29 @@ class OrderCreationViewModel @Inject constructor(
         private const val PARAMETERS_KEY = "parameters_key"
     }
 
-    val orderDraftData = LiveDataDelegate(savedState, Order.EMPTY, onChange = ::onOrderDraftChange)
+    val orderDraftData = LiveDataDelegate(savedState, Order.EMPTY)
     private var orderDraft by orderDraftData
 
-    private val orderStatus = MutableLiveData<OrderStatus>()
-    val orderStatusData: LiveData<OrderStatus> = orderStatus
+    val orderStatusData: LiveData<OrderStatus> = orderDraftData.liveData
+        .map { it.status }
+        .distinctUntilChanged()
+        .mapAsync { status ->
+            withContext(dispatchers.io) {
+                orderDetailRepository.getOrderStatus(status.value)
+            }
+        }
 
-    val products: LiveData<List<ProductUIModel>> = orderDraftData.liveData.mapAsync { order ->
-        order.items.map { item -> item.toProductUIModel() }
-    }
+    val products: LiveData<List<ProductUIModel>> = orderDraftData.liveData
+        .map { it.items }
+        .distinctUntilChanged()
+        .mapAsync { items ->
+            items.map { item -> item.toProductUIModel() }
+        }
 
     val currentDraft
         get() = orderDraft
 
     init {
-        updateOrderStatus(orderDraft.status)
         orderDraft = orderDraft.copy(
             currency = parameterRepository.getParameters(PARAMETERS_KEY, savedState).currencySymbol.orEmpty()
         )
@@ -56,21 +61,6 @@ class OrderCreationViewModel @Inject constructor(
 
     fun onOrderStatusChanged(status: Order.Status) {
         orderDraft = orderDraft.copy(status = status)
-    }
-
-    private fun onOrderDraftChange(old: Order?, new: Order) {
-        if (old?.status != new.status) {
-            updateOrderStatus(new.status)
-        }
-    }
-
-    private fun updateOrderStatus(status: Order.Status) {
-        launch(dispatchers.io) {
-            orderDetailRepository.getOrderStatus(status.value)
-                .runWithContext(dispatchers.main) {
-                    orderStatus.value = it
-                }
-        }
     }
 
     fun onCustomerNoteEdited(newNote: String) {
