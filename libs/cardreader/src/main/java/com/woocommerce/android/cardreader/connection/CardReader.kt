90be524aa8636835cdd79589e3e8118ceb6385ac
Andrey <andrey.kidinov@a8c.com>
2021-12-27 10:01:38 +0300

    Adds a reader model to the metadata of the payment intent

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/connection/CardReader.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/connection/CardReader.kt
@@ -2,7 +2,7 @@ package com.woocommerce.android.cardreader.connection
 
 interface CardReader {
     val id: String?
-    val type: String?
+    val type: String
     val currentBatteryLevel: Float?
     val firmwareVersion: String
     val locationId: String?
