1d245d508b2a5b6b98315fb3c9b7094aa30e5d88
hicham <hicham.boushaba@gmail.com>
2021-12-27 17:31:45 +0100

    Handle navigation to the product details screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationProductDetailsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationProductDetailsFragment.kt
@@ -2,21 +2,63 @@ package com.woocommerce.android.ui.orders.creation
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import androidx.navigation.navGraphViewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentOrderCreationProductDetailsBinding
+import com.woocommerce.android.di.GlideApp
+import com.woocommerce.android.model.Order
+import com.woocommerce.android.tools.ProductImageMap
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.util.CurrencyFormatter
 import dagger.hilt.android.AndroidEntryPoint
+import org.wordpress.android.util.PhotonUtils
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class OrderCreationProductDetailsFragment : BaseFragment(R.layout.fragment_order_creation_product_details) {
-    private val sharedViewModel: OrderCreationViewModel by navGraphViewModels(R.navigation.nav_graph_order_creations)
-    private val viewModel: OrderCreationProductDetailsViewModel by viewModels()
+    private val sharedViewModel: OrderCreationViewModel by navGraphViewModels(R.id.nav_graph_order_creations)
+    private val navArgs: OrderCreationProductDetailsFragmentArgs by navArgs()
+
+    @Inject lateinit var productImageMap: ProductImageMap
+    @Inject lateinit var currencyFormatter: CurrencyFormatter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentOrderCreationProductDetailsBinding.bind(view)
-        TODO()
+        val item = navArgs.item
+        val priceFormatter = currencyFormatter.buildBigDecimalFormatter(
+            currencyCode = sharedViewModel.currentDraft.currency
+        )
+        with(binding) {
+            productName.text = item.name
+
+            productAttributes.text = getString(
+                R.string.orderdetail_product_lineitem_qty_and_price,
+                item.quantity,
+                priceFormatter(item.price)
+            )
+            loadImage(item)
+
+            addDiscountButton.setOnClickListener {
+                // TODO
+            }
+
+            removeProductButton.setOnClickListener {
+                // TODO
+            }
+        }
+    }
+
+    private fun FragmentOrderCreationProductDetailsBinding.loadImage(item: Order.Item) {
+        val imageSize = resources.getDimensionPixelSize(R.dimen.image_major_50)
+        PhotonUtils.getPhotonImageUrl(
+            productImageMap.get(item.uniqueId), imageSize, imageSize
+        )?.let { imageUrl ->
+            GlideApp.with(requireContext())
+                .load(imageUrl)
+                .placeholder(R.drawable.ic_product)
+                .into(productIcon)
+        }
     }
 }
