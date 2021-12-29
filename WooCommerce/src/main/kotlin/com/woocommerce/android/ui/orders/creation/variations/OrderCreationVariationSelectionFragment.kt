342d172a063a379959499676cce43764d76a98a5
hicham <hicham.boushaba@gmail.com>
2021-12-29 12:31:02 +0100

    Handle variations loading

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/variations/OrderCreationVariationSelectionFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/variations/OrderCreationVariationSelectionFragment.kt
@@ -5,9 +5,13 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentOrderCreationProductSelectionBinding
+import com.woocommerce.android.di.GlideApp
 import com.woocommerce.android.ui.orders.creation.OrderCreationViewModel
+import com.woocommerce.android.ui.products.OnLoadMoreListener
+import com.woocommerce.android.ui.products.variations.VariationListAdapter
 
 class OrderCreationVariationSelectionFragment : Fragment(R.layout.fragment_order_creation_product_selection) {
     private val sharedViewModel by hiltNavGraphViewModels<OrderCreationViewModel>(R.id.nav_graph_order_creations)
@@ -16,5 +20,30 @@ class OrderCreationVariationSelectionFragment : Fragment(R.layout.fragment_order
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentOrderCreationProductSelectionBinding.bind(view)
+        with(binding) {
+            productsList.layoutManager = LinearLayoutManager(requireContext())
+        }
+        setupObservers(binding)
+    }
+
+    private fun setupObservers(binding: FragmentOrderCreationProductSelectionBinding) {
+        viewModel.viewState.observe(viewLifecycleOwner) {
+            if (binding.productsList.adapter == null) {
+                binding.productsList.adapter = VariationListAdapter(
+                    requireContext(),
+                    GlideApp.with(requireContext()),
+                    parentProduct = it.parentProduct,
+                    loadMoreListener = object : OnLoadMoreListener {
+                        override fun onRequestLoadMore() {
+                            viewModel.onLoadMore()
+                        }
+                    },
+                    onItemClick = {
+                        // TODO
+                    }
+                )
+            }
+            (binding.productsList.adapter as VariationListAdapter).setVariationList(it.variationsList)
+        }
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/variations/OrderCreationVariationSelectionViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/variations/OrderCreationVariationSelectionViewModel.kt
@@ -1,14 +1,61 @@
 package com.woocommerce.android.ui.orders.creation.variations
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
+import com.woocommerce.android.model.Product
+import com.woocommerce.android.model.ProductVariation
+import com.woocommerce.android.ui.products.ProductDetailRepository
+import com.woocommerce.android.ui.products.ProductListRepository
+import com.woocommerce.android.ui.products.variations.VariationRepository
+import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class OrderCreationVariationSelectionViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val variationRepository: VariationRepository,
+    private val productRepository: ProductDetailRepository,
+    private val dispatchers: CoroutineDispatchers
 ) : ScopedViewModel(savedStateHandle) {
     private val navArgs: OrderCreationVariationSelectionFragmentArgs by savedStateHandle.navArgs()
+
+    private val loadMoreTrigger = MutableSharedFlow<Unit>(extraBufferCapacity = 1)
+
+    private val parentProductLiveData = flow {
+        withContext(dispatchers.io) {
+            emit(productRepository.getProduct(navArgs.productId))
+        }
+    }
+
+    private val variationsList = flow {
+        // Let's start with the cached variations
+        emit(variationRepository.getProductVariationList(navArgs.productId))
+        // Then fetch from network
+        emit(variationRepository.fetchProductVariations(navArgs.productId))
+
+        // Monitor loadMore requests
+        loadMoreTrigger.collect {
+            emit(variationRepository.fetchProductVariations(navArgs.productId, loadMore = true))
+        }
+    }
+
+    val viewState = parentProductLiveData.combine(variationsList) { parentProduct, variationList ->
+        ViewState(parentProduct, variationList)
+    }.asLiveData()
+
+    fun onLoadMore() {
+        if (!variationRepository.canLoadMoreProductVariations) return
+        loadMoreTrigger.tryEmit(Unit)
+    }
+
+    data class ViewState(
+        val parentProduct: Product?,
+        val variationsList: List<ProductVariation>
+    )
 }
