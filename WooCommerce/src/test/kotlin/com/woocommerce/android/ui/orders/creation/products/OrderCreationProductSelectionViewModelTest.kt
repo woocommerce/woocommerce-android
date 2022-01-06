5dbb085da447a6c763fb1538ba873112caa3c272
ThomazFB <thomazfcortez@gmail.com>
2022-01-05 23:55:05 -0300

    Create scaffold for OrderCreationProductSelectionViewModelTests

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/products/OrderCreationProductSelectionViewModelTest.kt
@@ -0,0 +1,18 @@
+package com.woocommerce.android.ui.orders.creation.products
+
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class OrderCreationProductSelectionViewModelTest : BaseUnitTest() {
+    @Before
+    fun setup() {
+        Unit
+    }
+
+    @Test
+    fun `initial test`() {
+        assertThat(true).isTrue
+    }
+}
