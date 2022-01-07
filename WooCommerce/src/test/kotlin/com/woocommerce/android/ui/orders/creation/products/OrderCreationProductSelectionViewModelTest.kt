c31f6b7a99e4d107263631f83080f68caf623e1e
ThomazFB <thomazfcortez@gmail.com>
2022-01-07 00:11:19 -0300

    Improve OrderCreationProductSelectionViewModelTest scenarios

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/products/OrderCreationProductSelectionViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/products/OrderCreationProductSelectionViewModelTest.kt
@@ -2,41 +2,61 @@ package com.woocommerce.android.ui.orders.creation.products
 
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.ui.products.ProductListRepository
+import com.woocommerce.android.ui.products.ProductTestUtils
 import com.woocommerce.android.viewmodel.BaseUnitTest
-import org.junit.Before
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.inOrder
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
+import org.mockito.kotlin.*
 
+@ExperimentalCoroutinesApi
 class OrderCreationProductSelectionViewModelTest : BaseUnitTest() {
     private lateinit var sut: OrderCreationProductSelectionViewModel
 
     private val productListRepository: ProductListRepository = mock {
-        on { getProductList() } doReturn emptyList()
-        onBlocking { fetchProductList() } doReturn emptyList()
-    }
-
-    @Before
-    fun setup() {
-        sut = OrderCreationProductSelectionViewModel(
-            SavedStateHandle(),
-            productListRepository
-        )
+        on { getProductList() } doReturn ProductTestUtils.generateProductList()
+        onBlocking { fetchProductList() } doReturn ProductTestUtils.generateProductList()
     }
 
     @Test
     fun `when loading products, then get cached products before fetching from remote`() = testBlocking {
+        startSut()
         inOrder(productListRepository).run {
             this.verify(productListRepository).getProductList()
             this.verify(productListRepository).fetchProductList()
         }
     }
 
-    //should hide skeleton twice if products are cached
+    @Test
+    fun `when loading products, then pass loadMore to fetch products from store`() = testBlocking {
+        startSut()
+        verify(productListRepository).fetchProductList(false)
+        sut.loadProductList(true)
+        verify(productListRepository).fetchProductList(true)
+    }
+
+    @Test
+    fun `when loading empty cached product list, then ignore result`() = testBlocking {
+        var productListUpdateCalls = 0
+        whenever(productListRepository.getProductList()).thenReturn(emptyList())
+        startSut()
+        sut.productListData.observeForever { productListUpdateCalls++ }
+        assertThat(productListUpdateCalls).isEqualTo(1)
+    }
+
+    @Test
+    fun `when loading products, then hide skeleton if there are cached products`() {
+
+    }
 
     //onProductSelected trigger AddProduct when numVariations == 0
 
     //onProductSelected trigger ShowProductVariations when numVariations > 0
+
+    private fun startSut() {
+        sut = OrderCreationProductSelectionViewModel(
+            SavedStateHandle(),
+            productListRepository
+        )
+    }
 }
