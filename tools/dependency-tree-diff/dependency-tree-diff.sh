749724dbb394e767174dcf1a197d22668968f989
Andrey <andrey.kidinov@a8c.com>
2022-01-24 16:27:36 +0300

    Proper formatting

--- tools/dependency-tree-diff/dependency-tree-diff.sh
+++ tools/dependency-tree-diff/dependency-tree-diff.sh
@@ -16,7 +16,7 @@ if [ "${CIRCLE_PULL_REQUEST##*/}" != "" ]; then
   githubResponse="$(curl "$githubUrl" -H "Authorization: token $GITHUB_API_TOKEN")"
   targetBranch=$(echo "$githubResponse" | tr '\r\n' ' ' | jq '.base.ref' | tr -d '"')
 
-git merge "origin/$targetBranch" --no-edit
+  git merge "origin/$targetBranch" --no-edit
 
   if [[ $(git diff --name-status "$targetBranch" | grep ".gradle") ]]; then
       echo ".gradle files have been changed. Looking for caused dependency changes"
