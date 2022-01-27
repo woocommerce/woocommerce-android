888108c335d499df9b3bf3df194e4f15760516f5
Hafiz Rahman <hfzrhmn@gmail.com>
2022-01-27 13:42:28 +0700

    Add: ViewModel to  handle More Screen states.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuViewModel.kt
@@ -0,0 +1,67 @@
+package com.woocommerce.android.ui.moremenu
+
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.viewmodel.LiveDataDelegate
+import com.woocommerce.android.viewmodel.MultiLiveEvent
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
+
+@HiltViewModel
+class MoreMenuViewModel @Inject constructor(
+    savedState: SavedStateHandle,
+    val selectedSite: SelectedSite
+) : ScopedViewModel(savedState) {
+    val viewStateLiveData = LiveDataDelegate(savedState, MoreMenuViewState())
+    private var viewState by viewStateLiveData
+
+    init {
+        viewState = viewState.copy(
+            adminUrl = selectedSite.get().adminUrl,
+            storeUrl = selectedSite.get().url
+        )
+    }
+
+    fun handleSiteSwitch() {
+        // Update URLs to use the newly selected site's information.
+        viewState = viewState.copy(
+            adminUrl = selectedSite.get().adminUrl,
+            storeUrl = selectedSite.get().url
+        )
+    }
+
+    fun onSettingsClick() {
+        triggerEvent(NavigateToSettingsEvent)
+    }
+
+    fun onSwitchSiteClick() {
+        triggerEvent(StartSitePickerEvent)
+    }
+
+    fun onViewAdminButtonClick(url: String) {
+        triggerEvent(ViewAdminEvent(url))
+    }
+
+    fun onViewStoreButtonClick(url: String) {
+        triggerEvent(ViewStoreEvent(url))
+    }
+
+    fun onReviewsButtonClick() {
+        triggerEvent(ViewReviewsEvent)
+    }
+
+    @Parcelize
+    data class MoreMenuViewState(
+        val adminUrl: String = "",
+        val storeUrl: String = ""
+    ) : Parcelable
+
+    object NavigateToSettingsEvent : MultiLiveEvent.Event()
+    object StartSitePickerEvent : MultiLiveEvent.Event()
+    data class ViewAdminEvent(val url: String) : MultiLiveEvent.Event()
+    data class ViewStoreEvent(val url: String) : MultiLiveEvent.Event()
+    object ViewReviewsEvent : MultiLiveEvent.Event()
+}
