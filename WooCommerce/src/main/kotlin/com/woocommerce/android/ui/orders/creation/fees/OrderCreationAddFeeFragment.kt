7e1a35ead1172efc19364437c7e4d63adcfddeb5
ThomazFB <thomazfcortez@gmail.com>
2022-02-03 17:29:32 -0300

    Create base scaffold for Order Creation Add Fee view

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeFragment.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.orders.creation.fees
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+
+class OrderCreationAddFeeFragment :
+    BaseFragment(R.layout.fragment_order_creation_add_fee) {
+}
