049ca109c672c758b761fcff3979e45089e68f33
ThomazFB <thomazfcortez@gmail.com>
2022-02-03 17:37:00 -0300

    Create viewModel scaffold for OrderCreationAddFeeFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeFragment.kt
@@ -1,8 +1,15 @@
 package com.woocommerce.android.ui.orders.creation.fees
 
+import androidx.fragment.app.viewModels
+import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.orders.creation.OrderCreationViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OrderCreationAddFeeFragment :
     BaseFragment(R.layout.fragment_order_creation_add_fee) {
+    private val sharedViewModel by hiltNavGraphViewModels<OrderCreationViewModel>(R.id.nav_graph_order_creations)
+    private val addFeeViewModel by viewModels<OrderCreationAddFeeViewModel>()
 }
