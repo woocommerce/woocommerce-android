61060df04f79e590de1679c96774cef508ead34a
malinajirka <malinajirka@gmail.com>
2022-02-03 15:24:39 +0100

    Introduce CardReaderTracker.kt

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/CardReaderTracker.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.ui.prefs.cardreader
+
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
+import javax.inject.Inject
+
+class CardReaderTracker @Inject constructor(
+    private val trackerWrapper: AnalyticsTrackerWrapper,
+) {
+
+}
