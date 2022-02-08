08f86e5ca7a2edc640fcaf0e08999283760032ab
Mucientes <jorge.mucientes@automattic.com>
2022-02-08 16:13:52 +0100

    Use existing method getTranslatedString to match element on the UI using string value

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/moremenu/MoreMenuScreen.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/moremenu/MoreMenuScreen.kt
@@ -1,11 +1,9 @@
 package com.woocommerce.android.screenshots.moremenu
 
-import android.content.res.Resources
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.test.platform.app.InstrumentationRegistry
 import com.woocommerce.android.R
 import com.woocommerce.android.screenshots.mystore.settings.SettingsScreen
 import com.woocommerce.android.screenshots.reviews.ReviewsListScreen
@@ -16,18 +14,16 @@ class MoreMenuScreen : Screen(MORE_MENU_VIEW) {
         const val MORE_MENU_VIEW = R.id.menu
     }
 
-    private val resources: Resources = InstrumentationRegistry.getInstrumentation().targetContext.resources
-
     fun openReviewsListScreen(composeTestRule: ComposeTestRule): ReviewsListScreen {
         composeTestRule.onNodeWithText(
-            resources.getString(R.string.more_menu_button_reviews)
+            getTranslatedString(R.string.more_menu_button_reviews)
         ).performClick()
         return ReviewsListScreen()
     }
 
     fun openSettings(composeTestRule: ComposeTestRule): SettingsScreen {
         composeTestRule.onNodeWithContentDescription(
-            resources.getString(R.string.settings)
+            getTranslatedString(R.string.settings)
         ).performClick()
         return SettingsScreen()
     }
