80ac6b76a219ac118b316e05887b491c1e60abd8
hicham <hicham.boushaba@gmail.com>
2022-02-10 13:41:21 +0100

    Create shipping fees fragment and layout

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderCreationShippingFeesFragment.kt
@@ -0,0 +1,7 @@
+package com.woocommerce.android.ui.orders.creation.shipping
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+
+class OrderCreationShippingFeesFragment : BaseFragment(R.layout.fragment_order_creation_shipping_fees) {
+}
