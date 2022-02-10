87d6b97d98e546b57bc6a6532af0732887917395
ThomazFB <thomazfcortez@gmail.com>
2022-02-10 00:29:46 -0300

    Configure done options menu for OrderCreationAddFeeFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeFragment.kt
@@ -1,5 +1,8 @@
 package com.woocommerce.android.ui.orders.creation.fees
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import com.woocommerce.android.R
@@ -13,6 +16,19 @@ class OrderCreationAddFeeFragment :
     BaseFragment(R.layout.fragment_order_creation_add_fee) {
     private val sharedViewModel by hiltNavGraphViewModels<OrderCreationViewModel>(R.id.nav_graph_order_creations)
     private val addFeeViewModel by viewModels<OrderCreationAddFeeViewModel>()
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.menu_done, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_done -> true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
 
     override fun getFragmentTitle() = getString(R.string.order_creation_add_fee)
 }
