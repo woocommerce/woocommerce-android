1d6c2f2070f3ed43587c09cdbbd27ab281481be2
ThomazFB <thomazfcortez@gmail.com>
2022-02-10 00:47:16 -0300

    Define base structure to trigger the Fee creation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeViewModel.kt
@@ -1,6 +1,8 @@
 package com.woocommerce.android.ui.orders.creation.fees
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.model.Order
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,4 +10,17 @@ import javax.inject.Inject
 @HiltViewModel
 class OrderCreationAddFeeViewModel @Inject constructor(
     savedState: SavedStateHandle
-) : ScopedViewModel(savedState)
+) : ScopedViewModel(savedState) {
+    fun onDoneSelected(input: String, isPercentageSelected: Boolean) {
+
+    }
+
+    enum class FeeType {
+        AMOUNT, PERCENTAGE
+    }
+
+    data class FeeCreationData(
+        val inputValue: String,
+        val feeType: FeeType
+    ) : Event()
+}
