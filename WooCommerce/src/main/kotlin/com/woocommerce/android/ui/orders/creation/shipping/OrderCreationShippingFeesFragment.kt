a30b13b39fae417cf0574f0783e1b942451db11b
hicham <hicham.boushaba@gmail.com>
2022-02-10 14:30:47 +0100

    Add a viewmodel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderCreationShippingFeesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderCreationShippingFeesFragment.kt
@@ -1,7 +1,31 @@
 package com.woocommerce.android.ui.orders.creation.shipping
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.navGraphViewModels
 import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentOrderCreationShippingFeesBinding
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.orders.creation.OrderCreationViewModel
+import com.woocommerce.android.util.CurrencyFormatter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class OrderCreationShippingFeesFragment : BaseFragment(R.layout.fragment_order_creation_shipping_fees) {
+    private val viewModel: OrderCreationShippingFeesViewModel by viewModels()
+    private val sharedViewModel: OrderCreationViewModel by navGraphViewModels(R.id.nav_graph_order_creations)
+
+    @Inject lateinit var currencyFormatter: CurrencyFormatter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentOrderCreationShippingFeesBinding.bind(view)
+        binding.amountEditText.initView(
+            currency = sharedViewModel.currentDraft.currency,
+            decimals = viewModel.currencyDecimals,
+            currencyFormatter = currencyFormatter
+        )
+    }
 }
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderCreationShippingFeesViewModel.kt
@@ -0,0 +1,34 @@
+package com.woocommerce.android.ui.orders.creation.shipping
+
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.orders.simplepayments.SimplePaymentsSharedViewModel
+import com.woocommerce.android.ui.orders.simplepayments.SimplePaymentsSharedViewModel.Companion
+import com.woocommerce.android.viewmodel.LiveDataDelegate
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
+import org.wordpress.android.fluxc.store.WooCommerceStore
+import javax.inject.Inject
+
+private const val DEFAULT_DECIMAL_PRECISION = 2
+
+@HiltViewModel
+class OrderCreationShippingFeesViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val selectedSite: SelectedSite,
+    private val wooCommerceStore: WooCommerceStore
+) : ScopedViewModel(savedStateHandle) {
+    val viewStateData = LiveDataDelegate(savedStateHandle, ViewState())
+    private var viewState by viewStateData
+
+    val currencyDecimals: Int
+        get() = wooCommerceStore.getSiteSettings(selectedSite.get())?.currencyDecimalNumber ?: DEFAULT_DECIMAL_PRECISION
+
+    @Parcelize
+    data class ViewState(
+        val amount: Double? = null,
+        val name: String? = null
+    ) : Parcelable
+}
