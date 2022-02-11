3acdfd380ca3893656405c8d8feeced559e554ac
ThomazFB <thomazfcortez@gmail.com>
2022-02-11 20:22:28 -0300

    Configure currency decimals values inside OrderCreationAddFeeViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeViewModel.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.orders.creation.fees
 
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.orders.creation.fees.OrderCreationAddFeeViewModel.FeeType.AMOUNT
 import com.woocommerce.android.ui.orders.creation.fees.OrderCreationAddFeeViewModel.FeeType.PERCENTAGE
 import com.woocommerce.android.viewmodel.LiveDataDelegate
@@ -9,15 +10,25 @@ import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.parcelize.Parcelize
+import org.wordpress.android.fluxc.store.WooCommerceStore
 import java.math.BigDecimal
 import javax.inject.Inject
 
+private const val DEFAULT_DECIMAL_PRECISION = 2
+
 @HiltViewModel
 class OrderCreationAddFeeViewModel @Inject constructor(
-    savedState: SavedStateHandle
+    savedState: SavedStateHandle,
+    private val selectedSite: SelectedSite,
+    private val wooCommerceStore: WooCommerceStore
 ) : ScopedViewModel(savedState) {
     val viewStateData = LiveDataDelegate(savedState, ViewState())
     private var viewState by viewStateData
+
+    val currencyDecimals: Int
+        get() = wooCommerceStore.getSiteSettings(selectedSite.get())
+            ?.currencyDecimalNumber
+            ?: DEFAULT_DECIMAL_PRECISION
 
     private val currentFeeType
         get() = when (viewState.isPercentageSelected) {
