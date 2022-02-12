fa91ee6536f57764be995a5acb2470dc675934d8
ThomazFB <thomazfcortez@gmail.com>
2022-02-12 02:37:51 -0300

    Remove unnecessary mocks

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationAddFeeViewModelTest.kt
@@ -1,7 +1,6 @@
 package com.woocommerce.android.ui.orders.creation.fees
 
 import androidx.lifecycle.SavedStateHandle
-import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.orders.creation.fees.OrderCreationAddFeeViewModel.*
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
@@ -9,11 +8,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
 import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.model.WCSettingsModel
-import org.wordpress.android.fluxc.store.WooCommerceStore
 import java.math.BigDecimal
 
 class OrderCreationAddFeeViewModelTest : BaseUnitTest() {
@@ -21,21 +16,10 @@ class OrderCreationAddFeeViewModelTest : BaseUnitTest() {
 
     @Before
     fun setUp() {
-        val siteModelMock: SiteModel = mock()
-        val siteMock: SelectedSite = mock {
-            on { get() } doReturn siteModelMock
-        }
-        val settingsMock: WCSettingsModel = mock {
-            on { currencyDecimalNumber } doReturn 123
-        }
-        val storeMock: WooCommerceStore = mock {
-            on { getSiteSettings(siteModelMock) } doReturn settingsMock
-        }
-
         sut = OrderCreationAddFeeViewModel(
             SavedStateHandle(),
-            siteMock,
-            storeMock
+            mock(),
+            mock()
         )
     }
 
