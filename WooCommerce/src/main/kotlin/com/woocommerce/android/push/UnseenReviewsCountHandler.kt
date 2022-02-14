e45db9da5b4ba926921e2d6ddd3eac97a2b47312
Mucientes <jorge.mucientes@automattic.com>
2022-02-14 19:57:48 +0100

    Use stateflow with initial value to properly load unread reviews notifications

--- WooCommerce/src/main/kotlin/com/woocommerce/android/push/UnseenReviewsCountHandler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/push/UnseenReviewsCountHandler.kt
@@ -1,11 +1,13 @@
 package com.woocommerce.android.push
 
+import com.woocommerce.android.di.AppCoroutineScope
 import com.woocommerce.android.tools.SelectedSite
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import org.wordpress.android.fluxc.action.NotificationAction
-import org.wordpress.android.fluxc.model.notification.NotificationModel
+import org.wordpress.android.fluxc.model.notification.NotificationModel.Subkind.STORE_REVIEW
 import org.wordpress.android.fluxc.store.NotificationStore
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -13,21 +15,34 @@ import javax.inject.Singleton
 @ExperimentalCoroutinesApi
 @Singleton
 class UnseenReviewsCountHandler @Inject constructor(
+    @AppCoroutineScope private val appCoroutineScope: CoroutineScope,
     private val notificationStore: NotificationStore,
     private val selectedSite: SelectedSite
 ) {
 
-    private val unseenReviewsCount: Flow<Int> =
+    private val unseenReviewsCount: StateFlow<Int> =
         notificationStore.observeNotificationChanges()
-            .filter { it.causeOfChange == NotificationAction.UPDATE_NOTIFICATION }
+            .filter { onlyNotificationUpdates(it) }
             .mapLatest { getUnreadReviewsNotificationCount() }
             .flowOn(Dispatchers.IO)
+            .stateIn(
+                appCoroutineScope,
+                SharingStarted.WhileSubscribed(),
+                getUnreadReviewsNotificationCount()
+            )
 
     fun observeUnseenCount(): Flow<Int> = unseenReviewsCount
+
+    private fun onlyNotificationUpdates(it: NotificationStore.OnNotificationChanged) =
+        when (it.causeOfChange) {
+            NotificationAction.UPDATE_NOTIFICATION,
+            NotificationAction.MARK_NOTIFICATIONS_SEEN -> true
+            else -> false
+        }
 
     private fun getUnreadReviewsNotificationCount() =
         notificationStore.getNotificationsForSite(
             site = selectedSite.get(),
-            filterBySubtype = listOf(NotificationModel.Subkind.STORE_REVIEW.toString())
+            filterBySubtype = listOf(STORE_REVIEW.toString())
         ).filter { !it.read }.count()
 }
