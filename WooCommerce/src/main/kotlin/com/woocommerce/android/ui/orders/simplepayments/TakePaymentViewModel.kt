6bd9bd5f11852e9adaac34ba243f6e210625c341
Nick Bradbury <nick.bradbury@gmail.com>
2022-02-16 09:49:17 -0500

    Check status of order following payment flow

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/simplepayments/TakePaymentViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/simplepayments/TakePaymentViewModel.kt
@@ -128,14 +128,27 @@ class TakePaymentViewModel @Inject constructor(
     }
 
     fun onCardReaderPaymentCompleted() {
-        AnalyticsTracker.track(
-            AnalyticsTracker.Stat.SIMPLE_PAYMENTS_FLOW_COMPLETED,
-            mapOf(
-                AnalyticsTracker.KEY_AMOUNT to orderTotal.toString(),
-                AnalyticsTracker.KEY_PAYMENT_METHOD to VALUE_SIMPLE_PAYMENTS_COLLECT_CARD
-            )
-        )
         launch {
+            // this fun is called even when the payment fails, so we check the status of the order to
+            // determine whether payment succeeded
+            val status = orderStore.getOrderByIdAndSite(navArgs.order.id, selectedSite.get())?.status
+            if (status == CoreOrderStatus.COMPLETED.value) {
+                AnalyticsTracker.track(
+                    AnalyticsTracker.Stat.SIMPLE_PAYMENTS_FLOW_COMPLETED,
+                    mapOf(
+                        AnalyticsTracker.KEY_AMOUNT to orderTotal.toString(),
+                        AnalyticsTracker.KEY_PAYMENT_METHOD to VALUE_SIMPLE_PAYMENTS_COLLECT_CARD
+                    )
+                )
+            } else {
+                AnalyticsTracker.track(
+                    AnalyticsTracker.Stat.SIMPLE_PAYMENTS_FLOW_FAILED,
+                    mapOf(
+                        AnalyticsTracker.KEY_SOURCE to
+                            AnalyticsTracker.VALUE_SIMPLE_PAYMENTS_SOURCE_PAYMENT_METHOD
+                    )
+                )
+            }
             delay(DELAY_MS)
             triggerEvent(MultiLiveEvent.Event.Exit)
         }
