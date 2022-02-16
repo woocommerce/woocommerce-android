4939983f3daa6f4a9ba904324567e26fd1ebb7c3
Mucientes <jorge.mucientes@automattic.com>
2022-02-16 15:24:01 +0100

    Fix imports Detekt error

--- WooCommerce/src/main/kotlin/com/woocommerce/android/push/UnseenReviewsCountHandler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/push/UnseenReviewsCountHandler.kt
@@ -2,14 +2,14 @@ package com.woocommerce.android.push
 
 import com.woocommerce.android.di.AppCoroutineScope
 import com.woocommerce.android.tools.SelectedSite
-import javax.inject.Inject
-import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import org.wordpress.android.fluxc.model.notification.NotificationModel.Subkind.STORE_REVIEW
 import org.wordpress.android.fluxc.store.NotificationStore
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @ExperimentalCoroutinesApi
 @Singleton
