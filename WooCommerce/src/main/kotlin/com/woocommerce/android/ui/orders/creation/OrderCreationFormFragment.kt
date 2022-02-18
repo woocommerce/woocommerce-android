1986bbecba8aec826321d8b2d436896a24fd9aba
hicham <hicham.boushaba@gmail.com>
2022-02-18 12:52:43 +0100

    Update logic of binding the current shipping value to the payment section

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationFormFragment.kt
@@ -196,17 +196,19 @@ class OrderCreationFormFragment : BaseFragment(R.layout.fragment_order_creation_
         paymentSection.taxLayout.isVisible = FeatureFlag.ORDER_CREATION_M2.isEnabled()
         paymentSection.taxCalculationHint.isVisible = !FeatureFlag.ORDER_CREATION_M2.isEnabled()
 
-        val hasShipping = newOrderData.shippingLines.any { it.methodId.isNotEmpty() }
+        val currentShipping = newOrderData.shippingLines.firstOrNull { it.methodId.isNotEmpty() }
         paymentSection.shippingButton.setText(
-            if (hasShipping) R.string.order_creation_edit_shipping
+            if (currentShipping != null) R.string.order_creation_edit_shipping
             else R.string.order_creation_add_shipping
         )
         paymentSection.shippingButton.setIconResource(
-            if (hasShipping) 0
+            if (currentShipping != null) 0
             else R.drawable.ic_add
         )
-        paymentSection.shippingValue.isVisible = hasShipping
-        paymentSection.shippingValue.text = bigDecimalFormatter(newOrderData.shippingLines.sumOf { it.total })
+        paymentSection.shippingValue.isVisible = currentShipping != null
+        currentShipping?.let {
+            paymentSection.shippingValue.text = bigDecimalFormatter(it.total)
+        }
 
         paymentSection.productsTotalValue.text = bigDecimalFormatter(newOrderData.productsTotal)
         paymentSection.taxValue.text = bigDecimalFormatter(newOrderData.totalTax)
