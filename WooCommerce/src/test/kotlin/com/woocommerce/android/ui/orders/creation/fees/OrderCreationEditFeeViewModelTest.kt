599303816f2b7f1c2bd2ebbabf00f19b4faff042
ThomazFB <thomazfcortez@gmail.com>
2022-02-21 20:17:08 -0300

    Fix lint issues

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationEditFeeViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/fees/OrderCreationEditFeeViewModelTest.kt
@@ -2,7 +2,7 @@ package com.woocommerce.android.ui.orders.creation.fees
 
 import com.woocommerce.android.extensions.isEqualTo
 import com.woocommerce.android.initSavedStateHandle
-import com.woocommerce.android.ui.orders.creation.fees.OrderCreationEditFeeViewModel.*
+import com.woocommerce.android.ui.orders.creation.fees.OrderCreationEditFeeViewModel.UpdateFee
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import org.assertj.core.api.Assertions.assertThat
@@ -26,9 +26,9 @@ class OrderCreationEditFeeViewModelTest : BaseUnitTest() {
 
     private fun initSut() {
         sut = OrderCreationEditFeeViewModel(
-                savedState,
-                mock(),
-                mock()
+            savedState,
+            mock(),
+            mock()
         )
     }
 
