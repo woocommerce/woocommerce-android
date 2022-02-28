264c5caf1ba748d58f1b3f2740bfa0054162cf6d
jorgemucientesfayos <jorge.mucientes@automattic.com>
2022-02-28 10:42:04 -0700

    Clear card reader data when site changes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainPresenter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainPresenter.kt
@@ -10,6 +10,8 @@ import com.woocommerce.android.push.NotificationChannelType.NEW_ORDER
 import com.woocommerce.android.tools.ProductImageMap
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.tools.SelectedSite.SelectedSiteChangedEvent
+import com.woocommerce.android.ui.orders.cardreader.ClearCardReaderData
+import com.woocommerce.android.util.FeatureFlag
 import com.woocommerce.android.util.WooLog
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -43,6 +45,7 @@ class MainPresenter @Inject constructor(
     private val productImageMap: ProductImageMap,
     private val appPrefsWrapper: AppPrefsWrapper,
     private val wcOrderStore: WCOrderStore,
+    private val clearCardReaderData: ClearCardReaderData
 ) : MainContract.Presenter {
     private var mainView: MainContract.View? = null
 
@@ -100,6 +103,9 @@ class MainPresenter @Inject constructor(
             WCOrderActionBuilder
                 .newFetchOrderStatusOptionsAction(FetchOrderStatusOptionsPayload(site))
         )
+        if (FeatureFlag.CARD_READER.isEnabled()) {
+            coroutineScope.launch { clearCardReaderData() }
+        }
     }
 
     override fun fetchUnfilledOrderCount() {
