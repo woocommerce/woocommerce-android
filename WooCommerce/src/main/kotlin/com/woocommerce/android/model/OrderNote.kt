c0f9963ed6ac9938df637d228232577a32f38a84
hicham <hicham.boushaba@gmail.com>
2022-03-02 15:32:10 +0100

    Update OrderNote model

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/OrderNote.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/OrderNote.kt
@@ -9,29 +9,22 @@ import java.util.Date
 @Parcelize
 data class OrderNote(
     val remoteNoteId: Long = 0L,
+    val orderId: Long = 0L,
     val author: String = "",
     val dateCreated: Date = Date(),
     val isCustomerNote: Boolean,
     val isSystemNote: Boolean = false,
-    val localOrderId: Int = 0,
-    val localSiteId: Int = 0,
     val note: String
-) : Parcelable {
-    fun toDataModel() = WCOrderNoteModel().also { orderNoteModel ->
-        orderNoteModel.isCustomerNote = this.isCustomerNote
-        orderNoteModel.note = this.note
-    }
-}
+) : Parcelable
 
 fun WCOrderNoteModel.toAppModel(): OrderNote {
     return OrderNote(
-        remoteNoteId,
-        author,
-        DateTimeUtils.dateUTCFromIso8601(this.dateCreated) ?: Date(),
-        isCustomerNote,
-        isSystemNote,
-        localOrderId,
-        localSiteId,
-        note
+        remoteNoteId = noteId.value,
+        orderId = orderId.value,
+        author = author,
+        dateCreated = DateTimeUtils.dateUTCFromIso8601(this.dateCreated) ?: Date(),
+        isCustomerNote = isCustomerNote,
+        isSystemNote = isSystemNote,
+        note = note
     )
 }
