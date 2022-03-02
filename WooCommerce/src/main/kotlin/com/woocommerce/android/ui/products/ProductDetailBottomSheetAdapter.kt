3d962636aa3ecfac4956c1403069da95f4574409
Nick Bradbury <nick.bradbury@gmail.com>
2022-03-02 10:51:11 -0500

    Rely entirely on DiffUtils when populating the list

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailBottomSheetAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailBottomSheetAdapter.kt
@@ -36,15 +36,13 @@ class ProductDetailBottomSheetAdapter(
     override fun getItemCount() = options.size
 
     fun setProductDetailBottomSheetOptions(optionList: List<ProductDetailBottomSheetUiItem>) {
-        if (optionList != options) {
-            val diffResult =
-                DiffUtil.calculateDiff(
-                    ProductDetailBottomSheetItemDiffUtil(options, optionList)
-                )
-            options.clear()
-            options.addAll(optionList)
-            diffResult.dispatchUpdatesTo(this)
-        }
+        val diffResult =
+            DiffUtil.calculateDiff(
+                ProductDetailBottomSheetItemDiffUtil(options, optionList)
+            )
+        options.clear()
+        options.addAll(optionList)
+        diffResult.dispatchUpdatesTo(this)
     }
 
     class ProductDetailBottomSheetViewHolder(private val viewBinding: ProductDetailBottomSheetListItemBinding) :
