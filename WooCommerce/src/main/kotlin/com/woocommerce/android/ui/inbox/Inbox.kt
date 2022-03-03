da641169cb848969a66aaa714da37827e099de8b
jorgemucientesfayos <jorge.mucientes@automattic.com>
2022-03-02 17:43:55 -0700

    Add skeletons for loading inbox notes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/inbox/Inbox.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/inbox/Inbox.kt
@@ -1,6 +1,8 @@
 package com.woocommerce.android.ui.inbox
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -13,6 +15,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -33,7 +37,7 @@ fun Inbox(state: InboxState) {
     when {
         state.isLoading -> InboxSkeleton()
         state.notes.isEmpty() -> InboxEmptyCase()
-        state.notes.isNotEmpty() -> InboxEmptyCase()
+        state.notes.isNotEmpty() -> InboxSkeleton()
     }
 }
 
@@ -67,7 +71,23 @@ fun InboxEmptyCase() {
 
 @Composable
 fun InboxSkeleton() {
+    LazyColumn {
 
+        /*
+          Lay down the Shimmer Animated item 5 time
+          [repeat] is like a loop which executes the body
+          according to the number specified
+        */
+        repeat(4) {
+            item {
+                ShimmerAnimation()
+                Divider(
+                    color = colorResource(id = R.color.divider_color),
+                    thickness = 1.dp
+                )
+            }
+        }
+    }
 }
 
 @Composable
@@ -128,4 +148,110 @@ fun InboxNoteRow(note: InboxNoteUi) {
             }
         }
     }
+}
+
+
+@Composable
+fun InboxNoteSkeletonItem(
+    brush: Brush
+) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Spacer(
+            modifier = Modifier
+                .width(96.dp)
+                .height(16.dp)
+                .background(brush = brush)
+        )
+        Spacer(modifier = Modifier.padding(top = 20.dp))
+        Spacer(
+            modifier = Modifier
+                .width(190.dp)
+                .height(16.dp)
+                .background(brush = brush)
+        )
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+                .background(brush = brush)
+        )
+        Spacer(modifier = Modifier.padding(top = 6.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+                .background(brush = brush)
+        )
+        Spacer(modifier = Modifier.padding(top = 6.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+                .background(brush = brush)
+        )
+        Spacer(modifier = Modifier.padding(top = 14.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(16.dp)
+                    .background(brush = brush)
+            )
+            Spacer(
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(16.dp)
+                    .background(brush = brush)
+            )
+        }
+    }
+}
+
+@Composable
+fun ShimmerAnimation() {
+
+    /*
+     Create InfiniteTransition
+     which holds child animation like [Transition]
+     animations start running as soon as they enter
+     the composition and do not stop unless they are removed
+    */
+    val transition = rememberInfiniteTransition()
+    val translateAnim by transition.animateFloat(
+        /*
+         Specify animation positions,
+         initial Values 0F means it
+         starts from 0 position
+        */
+        initialValue = 0f,
+        targetValue = 1600f,
+        animationSpec = infiniteRepeatable(
+            // Tween Animates between values over specified [durationMillis]
+            tween(durationMillis = 1200, easing = FastOutSlowInEasing),
+            RepeatMode.Restart
+        )
+    )
+
+    val ShimmerColorShades = listOf(
+        colorResource(id = R.color.skeleton_color),
+        colorResource(id = R.color.skeleton_color).copy(0.15f),
+        colorResource(id = R.color.skeleton_color)
+    )
+    /*
+      Create a gradient using the list of colors
+      Use Linear Gradient for animating in any direction according to requirement
+      start=specifies the position to start with in cartesian like system Offset(10f,10f) means x(10,0) , y(0,10)
+      end = Animate the end position to give the shimmer effect using the transition created above
+    */
+    val brush = Brush.linearGradient(
+        colors = ShimmerColorShades,
+        start = Offset(10f, 10f),
+        end = Offset(translateAnim, translateAnim)
+    )
+
+    InboxNoteSkeletonItem(brush = brush)
 }
