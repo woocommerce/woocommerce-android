cab800a142d84800ffeacce0b3fd66b0e1d1d605
Anirudh Uppunda <abhat38@gmail.com>
2022-03-14 11:18:57 +0530

    add awaitClose() for CollectRefundAction.kt

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/actions/CollectRefundAction.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/actions/CollectRefundAction.kt
@@ -4,6 +4,7 @@ import com.stripe.stripeterminal.external.callable.Callback
 import com.stripe.stripeterminal.external.models.RefundParameters
 import com.stripe.stripeterminal.external.models.TerminalException
 import com.woocommerce.android.cardreader.internal.wrappers.TerminalWrapper
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -15,18 +16,33 @@ internal class CollectRefundAction(private val terminal: TerminalWrapper) {
 
     fun collectRefund(refundParameters: RefundParameters): Flow<CollectRefundStatus> {
         return callbackFlow {
-            terminal.refundPayment(
+            val cancelable = terminal.refundPayment(
                 refundParameters,
                 object : Callback {
                     override fun onSuccess() {
                         trySend(CollectRefundStatus.Success)
+                        close()
                     }
 
                     override fun onFailure(e: TerminalException) {
                         trySend(CollectRefundStatus.Failure(e))
+                        close()
                     }
                 }
             )
+            awaitClose {
+                if (!cancelable.isCompleted) cancelable.cancel(noop)
+            }
         }
+    }
+}
+
+private val noop = object : Callback {
+    override fun onFailure(e: TerminalException) {
+        // noop
+    }
+
+    override fun onSuccess() {
+        // noop
     }
 }
