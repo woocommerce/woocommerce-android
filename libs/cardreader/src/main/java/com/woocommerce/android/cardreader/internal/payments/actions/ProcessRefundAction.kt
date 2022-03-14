c0ea1a9dbf5b207e59386df697d1b5c71f499b73
Anirudh Uppunda <abhat38@gmail.com>
2022-03-14 11:19:13 +0530

    add awaitClose() for ProcessRefundAction.kt

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/actions/ProcessRefundAction.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/actions/ProcessRefundAction.kt
@@ -4,6 +4,7 @@ import com.stripe.stripeterminal.external.callable.RefundCallback
 import com.stripe.stripeterminal.external.models.Refund
 import com.stripe.stripeterminal.external.models.TerminalException
 import com.woocommerce.android.cardreader.internal.wrappers.TerminalWrapper
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -19,12 +20,15 @@ internal class ProcessRefundAction(private val terminal: TerminalWrapper) {
             terminal.processRefund(object : RefundCallback {
                 override fun onSuccess(refund: Refund) {
                     trySend(ProcessRefundStatus.Success(refund))
+                    close()
                 }
 
                 override fun onFailure(e: TerminalException) {
                     trySend(ProcessRefundStatus.Failure(e))
+                    close()
                 }
             })
+            awaitClose {  }
         }
     }
 }
