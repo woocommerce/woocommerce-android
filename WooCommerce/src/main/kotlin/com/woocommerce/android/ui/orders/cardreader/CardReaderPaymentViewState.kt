2a72816909ed51887dac85c23142833a2aa3ebfa
Anirudh Uppunda <abhat38@gmail.com>
2022-03-14 11:26:30 +0530

    add different Interac refund UI state and their corresponding strings

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderPaymentViewState.kt
@@ -35,6 +35,18 @@ sealed class ViewState(
         isProgressVisible = true
     )
 
+    data class FailedRefundState(
+        override val amountWithCurrencyLabel: String?,
+        private val primaryLabel: Int? = R.string.try_again,
+        override val onPrimaryActionClicked: (() -> Unit)
+    ) : ViewState(
+        headerLabel = R.string.card_reader_payment_refund_failed_header,
+        paymentStateLabel = R.string.order_error_fetch_generic,
+        paymentStateLabelTopMargin = R.dimen.major_100,
+        primaryActionLabel = primaryLabel,
+        illustration = R.drawable.img_products_error
+    )
+
     data class FailedPaymentState(
         private val errorType: PaymentFlowError,
         override val amountWithCurrencyLabel: String?,
@@ -48,6 +60,15 @@ sealed class ViewState(
         illustration = R.drawable.img_products_error
     )
 
+    data class CollectRefundState(
+        override val amountWithCurrencyLabel: String,
+        override val hintLabel: Int = R.string.card_reader_payment_collect_payment_hint,
+        override val headerLabel: Int = R.string.card_reader_payment_refund_payment,
+    ) : ViewState(
+        paymentStateLabel = R.string.card_reader_payment_collect_payment_state,
+        illustration = R.drawable.img_card_reader_available
+    )
+
     data class CollectPaymentState(
         override val amountWithCurrencyLabel: String,
         override val hintLabel: Int = R.string.card_reader_payment_collect_payment_hint,
@@ -56,6 +77,14 @@ sealed class ViewState(
         paymentStateLabel = R.string.card_reader_payment_collect_payment_state,
         illustration = R.drawable.img_card_reader_available
     )
+
+    data class ProcessingRefundState(override val amountWithCurrencyLabel: String) :
+        ViewState(
+            hintLabel = R.string.card_reader_payment_processing_payment_hint,
+            headerLabel = R.string.card_reader_payment_refund_payment,
+            paymentStateLabel = R.string.card_reader_payment_processing_refund_state,
+            illustration = R.drawable.img_card_reader_available
+        )
 
     data class ProcessingPaymentState(override val amountWithCurrencyLabel: String) :
         ViewState(
@@ -72,6 +101,13 @@ sealed class ViewState(
             paymentStateLabel = R.string.card_reader_payment_capturing_payment_state,
             illustration = R.drawable.img_card_reader_available
         )
+
+    data class RefundSuccessfulState(
+        override val amountWithCurrencyLabel: String
+    ) : ViewState(
+        headerLabel = R.string.card_reader_payment_completed_refund_header,
+        illustration = R.drawable.img_celebration,
+    )
 
     data class PaymentSuccessfulState(
         override val amountWithCurrencyLabel: String,
