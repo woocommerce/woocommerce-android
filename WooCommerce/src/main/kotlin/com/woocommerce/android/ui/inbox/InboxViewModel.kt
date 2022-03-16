618493644391371370b7e774ae36374832e3afa0
jorgemucientesfayos <jorge.mucientes@automattic.com>
2022-03-15 19:08:44 -0600

    Add formatting to date for Inbox notes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/inbox/InboxViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/inbox/InboxViewModel.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.inbox
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
+import com.woocommerce.android.R
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.DateUtils
 import com.woocommerce.android.viewmodel.ResourceProvider
@@ -11,6 +12,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.inbox.InboxNoteDto
 import org.wordpress.android.fluxc.store.WCInboxStore
+import org.wordpress.android.util.DateTimeUtils
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
@@ -39,9 +42,35 @@ class InboxViewModel @Inject constructor(
             id = id,
             title = title!!,
             description = content!!,
-            updatedTime = getReladateCreated!!,
+            updatedTime = getRelativeDateToCurrentDate(dateCreated!!),
             actions = emptyList(),
         )
+
+    @SuppressWarnings("MagicNumber", "ReturnCount")
+    private fun getRelativeDateToCurrentDate(createdDate: String): String {
+        val creationDate = dateutils.getDateFromIso8601String(createdDate)
+        val now = Date()
+
+        val minutes = DateTimeUtils.minutesBetween(now, creationDate)
+        when {
+            minutes < 1 -> return resourceProvider.getString(R.string.inbox_note_recency_now)
+            minutes < 60 -> return resourceProvider.getString(R.string.inbox_note_recency_minutes, minutes)
+        }
+        val hours = DateTimeUtils.hoursBetween(now, creationDate)
+        when {
+            hours == 1 -> return resourceProvider.getString(R.string.inbox_note_recency_one_hour)
+            hours < 24 -> return resourceProvider.getString(R.string.inbox_note_recency_hours, hours)
+        }
+        val days = DateTimeUtils.daysBetween(now, creationDate)
+        when {
+            days == 1 -> return resourceProvider.getString(R.string.inbox_note_recency_one_day)
+            days < 30 -> return resourceProvider.getString(R.string.inbox_note_recency_days, days)
+        }
+        return resourceProvider.getString(
+            R.string.inbox_note_recency_date_time,
+            dateutils.toDisplayMMMddYYYYDate(creationDate?.time ?: 0) ?: ""
+        )
+    }
 
     data class InboxState(
         val isLoading: Boolean = false,
