84b8cf3852bf006fb478d2197e4f700c05de5571
ThomazFB <thomazfcortez@gmail.com>
2022-03-17 20:54:43 -0300

    Create OrderCreationViewModelTest

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.woocommerce.android.ui.orders.creation
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class OrderCreationViewModelTest {
+    private lateinit var sut: OrderCreationViewModel
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun `first test`() {
+
+    }
+}
