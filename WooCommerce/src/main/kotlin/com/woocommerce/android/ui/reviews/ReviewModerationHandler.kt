8aa415b6333742316f2444670b73ba27ee0f3060
hicham <hicham.boushaba@gmail.com>
2022-03-18 12:30:52 +0100

    Update the handler logic to allow for better handling of queue

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/reviews/ReviewModerationHandler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/reviews/ReviewModerationHandler.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.reviews
 
+import androidx.annotation.VisibleForTesting
 import com.woocommerce.android.analytics.AnalyticsEvent
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.di.AppCoroutineScope
@@ -9,10 +10,7 @@ import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.WooLog.T
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -24,82 +22,118 @@ class ReviewModerationHandler @Inject constructor(
     @AppCoroutineScope private val appCoroutineScope: CoroutineScope
 ) {
     companion object {
+        @VisibleForTesting
         const val UNDO_DELAY = 2750L
+        @VisibleForTesting
+        const val ERROR_SNACKBAR_DELAY = 1000L
+        @VisibleForTesting
+        const val DELAY_FOR_REFRESHING = 100L
     }
 
     private val _queue = MutableSharedFlow<ReviewModerationRequest>(extraBufferCapacity = Int.MAX_VALUE)
 
-    private val _pendingModerationStatus = MutableSharedFlow<ReviewModerationStatus>(replay = 1)
-    val pendingModerationStatus: Flow<ReviewModerationStatus> = _pendingModerationStatus.asSharedFlow()
+    private val _pendingModerationStatus = MutableStateFlow<List<ReviewModerationStatus>>(emptyList())
+    val pendingModerationStatus = _pendingModerationStatus.map { it.sorted() }
 
-    private var moderationRequestJob: Job? = null
+    private val _skipDelayTrigger = MutableSharedFlow<Unit>()
+
+    private var pendingJobs = mutableMapOf<Long, Job>()
 
     init {
         appCoroutineScope.launch {
-            _queue.collect {
+            // Queue requests
+            _queue
+                .onEach { request ->
+                    WooLog.d(
+                        T.REVIEWS,
+                        "Queue moderation request for review " +
+                            "${request.review.remoteId} to status: ${request.newStatus}"
+                    )
+
+                    if (_skipDelayTrigger.subscriptionCount.value > 0) {
+                        _skipDelayTrigger.emit(Unit)
+                    }
+                    _pendingModerationStatus.update { it + ReviewModerationStatus(request) }
+                }
+                .launchIn(appCoroutineScope)
+
+            // Process requests
+            _queue.collect { request ->
                 coroutineScope {
-                    moderationRequestJob = handleModerationRequest(it)
+                    pendingJobs[request.review.remoteId] = processModerationRequest(request)
                 }
             }
         }
     }
 
     fun postModerationRequest(review: ProductReview, newStatus: ProductReviewStatus) {
-        _queue.tryEmit(
-            ReviewModerationRequest(
-                review = review,
-                newStatus = newStatus
+        appCoroutineScope.launch {
+            _queue.emit(
+                ReviewModerationRequest(
+                    review = review,
+                    newStatus = newStatus
+                )
             )
-        )
+        }
     }
 
-    fun undoLastOperation() {
-        AnalyticsTracker.track(AnalyticsEvent.REVIEW_ACTION_UNDO)
-        moderationRequestJob?.cancel()
+    fun undoOperation(review: ProductReview) {
+        pendingJobs[review.remoteId]?.cancel()
     }
 
-    private fun CoroutineScope.handleModerationRequest(request: ReviewModerationRequest): Job = launch {
+    private fun CoroutineScope.processModerationRequest(request: ReviewModerationRequest): Job = launch {
         try {
-            WooLog.d(
-                T.REVIEWS,
-                "Handle moderation request for review ${request.review.remoteId} to status: ${request.newStatus}"
-            )
-            val status = ReviewModerationStatus(request)
-            _pendingModerationStatus.emit(status)
-            delay(UNDO_DELAY)
+            // A delay to allow the user to undo the action
+            wait()
 
             AnalyticsTracker.track(
                 AnalyticsEvent.REVIEW_ACTION,
                 mapOf(AnalyticsTracker.KEY_TYPE to request.review.toString())
             )
 
-            _pendingModerationStatus.emit(status.copy(actionStatus = SUBMITTED))
+            updateStatus(ReviewModerationStatus(request = request, actionStatus = SUBMITTED))
             WooLog.d(
                 T.REVIEWS,
                 "Submit API call to moderate review ${request.review.remoteId} to status: ${request.newStatus}"
             )
             submitReviewStatusToTheApi(request)
-        } catch (@Suppress("SwallowedException") e: CancellationException) {
+        } catch (e: CancellationException) {
             WooLog.d(
                 T.REVIEWS,
                 "Moderation request is cancelled, revert to previous status"
             )
-            _pendingModerationStatus.tryEmit(
+            updateStatus(
                 ReviewModerationStatus(
-                    review = request.review,
-                    newStatus = ProductReviewStatus.fromString(request.review.status),
+                    request = ReviewModerationRequest(
+                        review = request.review,
+                        newStatus = ProductReviewStatus.fromString(request.review.status)
+                    ),
                     actionStatus = SUCCESS
                 )
             )
         } finally {
             // Clear the status cache to avoid resubmitting completed operations
-            _pendingModerationStatus.resetReplayCache()
+            _pendingModerationStatus.update { statuses -> statuses.filterNot { it.review == request.review } }
         }
+    }
+
+    private suspend fun wait() {
+        merge(
+            _skipDelayTrigger,
+            flow {
+                delay(UNDO_DELAY)
+                emit(Unit)
+            }
+        ).first()
+    }
+
+    private fun updateStatus(status: ReviewModerationStatus) {
+        _pendingModerationStatus.update { statuses -> statuses.filterNot { it.review == status.review } + status }
     }
 
     private suspend fun submitReviewStatusToTheApi(request: ReviewModerationRequest) {
         val remoteOperationResult = productStore.updateProductReviewStatus(
-            site = selectedSite.get(),
+            selectedSite.get(),
             reviewId = request.review.remoteId,
             newStatus = request.newStatus.toString()
         )
@@ -118,7 +152,8 @@ class ReviewModerationHandler @Inject constructor(
                     AnalyticsTracker.KEY_ERROR_DESC to remoteOperationResult.error?.message
                 )
             )
-            _pendingModerationStatus.emit(ReviewModerationStatus(request, actionStatus = ERROR))
+            updateStatus(ReviewModerationStatus(request, actionStatus = ERROR))
+            delay(ERROR_SNACKBAR_DELAY)
         } else {
             val updatedReview = remoteOperationResult.model!!
             WooLog.w(
@@ -126,7 +161,10 @@ class ReviewModerationHandler @Inject constructor(
                 "Review status updated, id: ${updatedReview.remoteProductReviewId}, status:${updatedReview.status}"
             )
             AnalyticsTracker.track(AnalyticsEvent.REVIEW_ACTION_SUCCESS)
-            _pendingModerationStatus.emit(ReviewModerationStatus(request, actionStatus = SUCCESS))
+            updateStatus(ReviewModerationStatus(request, actionStatus = SUCCESS))
+
+            // Wait a bit to give clients a chance to reload reviews from DB
+            delay(DELAY_FOR_REFRESHING)
         }
     }
 }
