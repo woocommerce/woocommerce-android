cd0a04d9a94d10feaa1f4ff9f8003dfd655769df
Anirudh Uppunda <abhat38@gmail.com>
2022-03-21 13:16:14 +0530

    add UI interac refund error states

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderInteracRefundViewState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderInteracRefundViewState.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.orders.cardreader
 
+import androidx.annotation.StringRes
 import com.woocommerce.android.R
 
 sealed class InteracRefund {
@@ -45,4 +46,38 @@ sealed class InteracRefund {
         headerLabel = R.string.card_reader_payment_completed_refund_header,
         illustration = R.drawable.img_celebration,
     )
+}
+
+sealed class InteracRefundFlowError(@StringRes val message: Int) {
+    object FetchingOrderFailed : InteracRefundFlowError(R.string.order_error_fetch_generic)
+    object NoNetwork : InteracRefundFlowError(R.string.card_reader_payment_failed_no_network_state)
+    object Server : InteracRefundFlowError(R.string.card_reader_payment_failed_server_error_state)
+    object Generic : InteracRefundFlowError(R.string.card_reader_refund_failed_unexpected_error_state)
+    object AmountTooSmall : Declined(R.string.card_reader_payment_failed_amount_too_small), NonRetryableError
+    object Unknown : Declined(R.string.card_reader_payment_refund_failed_header)
+    sealed class Declined(message: Int) : InteracRefundFlowError(message) {
+        object Temporary : Declined(R.string.card_reader_payment_failed_temporary)
+        object Fraud : Declined(R.string.card_reader_refund_failed_fraud), NonRetryableError
+        object Generic : Declined(R.string.card_reader_refund_failed_generic)
+        object InvalidAccount : Declined(R.string.card_reader_payment_failed_invalid_account), NonRetryableError
+        object CardNotSupported : Declined(R.string.card_reader_refund_failed_card_not_supported)
+        object CurrencyNotSupported :
+            Declined(R.string.card_reader_payment_failed_currency_not_supported), NonRetryableError
+
+        object DuplicateTransaction :
+            Declined(R.string.card_reader_refund_failed_duplicate_transaction), NonRetryableError
+
+        object ExpiredCard : Declined(R.string.card_reader_payment_failed_expired_card)
+        object IncorrectPostalCode :
+            Declined(R.string.card_reader_payment_failed_incorrect_postal_code), NonRetryableError
+
+        object InsufficientFunds : Declined(R.string.card_reader_refund_failed_insufficient_funds)
+        object InvalidAmount : Declined(R.string.card_reader_refund_failed_invalid_amount), NonRetryableError
+        object PinRequired : Declined(R.string.card_reader_payment_failed_pin_required)
+        object TooManyPinTries : Declined(R.string.card_reader_payment_failed_too_many_pin_tries)
+        object TestCard : Declined(R.string.card_reader_refund_failed_test_card)
+        object TestModeLiveCard : Declined(R.string.card_reader_payment_failed_test_mode_live_card)
+    }
+
+    interface NonRetryableError
 }
