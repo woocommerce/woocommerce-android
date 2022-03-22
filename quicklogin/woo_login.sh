8a0a61d502be1a78d1567b62c55683b584858532
Andrey <andrey.kidinov@a8c.com>
2022-03-22 18:29:25 +0300

    Optionally take ADB_PATH

--- quicklogin/woo_login.sh
+++ quicklogin/woo_login.sh
@@ -1,14 +1,21 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 # PASS WORDPRESS_EMAIL and WORDPRESS_PASSWORD as env variable to the script
-# Example: WORDPRESS_EMAIL=my_wp_email@gmail.com  WORDPRESS_PASSWORD=my_pasword_1_# ./quicklogin/woo_login.sh
+# Optionally pass path to your adb: ADB_PATH
+# Example: ADB_PATH=~/Library/Android/sdk/platform-tools/adb WORDPRESS_EMAIL=my_wp_email@gmail.com  WORDPRESS_PASSWORD=my_pasword_1_# ./quicklogin/woo_login.sh
 
-~/Library/Android/sdk/platform-tools/adb shell pm clear com.woocommerce.android.prealpha
+if [[ "$ADB_PATH" ]]; then
+  adbPath="$ADB_PATH"
+else
+  adbPath='~/Library/Android/sdk/platform-tools/adb'
+fi
+
+eval $adbPath shell pm clear com.woocommerce.android.prealpha
 
 ./gradlew installJalapenoDebug
 ./gradlew :quicklogin:installDebug \
 -PquickLoginWpEmail="$WORDPRESS_EMAIL" \
 -PquickLoginWpPassword="$WORDPRESS_PASSWORD"
 
-~/Library/Android/sdk/platform-tools/adb shell am instrument -w -r -e debug false -e class 'com.woocommerce.android.quicklogin.QuickLoginWordpress' com.woocommerce.android.prealpha.quicklogin/androidx.test.runner.AndroidJUnitRunner
-~/Library/Android/sdk/platform-tools/adb shell am start -n com.woocommerce.android.prealpha/com.woocommerce.android.ui.main.MainActivity
+eval $adbPath shell am instrument -w -r -e debug false -e class 'com.woocommerce.android.quicklogin.QuickLoginWordpress' com.woocommerce.android.prealpha.quicklogin/androidx.test.runner.AndroidJUnitRunner
+eval $adbPath shell am start -n com.woocommerce.android.prealpha/com.woocommerce.android.ui.main.MainActivity
