95660aeb75961d6bfa86b339f06116d95ae1bbd8
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-03-23 11:40:01 -0400

    Removing comments as per reviewer's suggestion

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/OrderListAdapter.kt
@@ -191,10 +191,6 @@ class OrderListAdapter(
         fun onBind(header: SectionHeader) {
             viewBinding.orderListHeader.setText(TimeGroup.valueOf(header.title.name).labelRes)
 
-            /*
-            Set the announceForAccessibility function to the heading container to allow the use of the
-            explore by touch option.
-            */
             (viewBinding.headingContainer as View).announceForAccessibility(
                 viewBinding.headingContainer.resources
                     .getString(TimeGroup.valueOf(header.title.name).labelRes)
