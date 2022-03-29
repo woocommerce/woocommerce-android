cf280acdc25386dd70aa9fcedec50a26267085e0
ThomazFB <thomazfcortez@gmail.com>
2022-03-29 14:24:02 -0300

    Make FeatureFeedbackSettingsTest go green

--- WooCommerce/src/test/kotlin/com/woocommerce/android/model/FeatureFeedbackSettingsTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/model/FeatureFeedbackSettingsTest.kt
@@ -18,11 +18,13 @@ class FeatureFeedbackSettingsTest {
 
     @Test
     fun `when settings is created, then set tag value as requesting view with Feature class simple name`() {
-        sut = FeatureFeedbackSettings(
-            feature = Feature.ProductVariations("test-view-name")
-        )
+        val requestingViewName = "testViewName"
+        val feature = Feature.ProductVariations(requestingViewName)
 
-        assertThat(sut.feature.tag).isEqualTo("")
+        sut = FeatureFeedbackSettings(feature = feature)
+
+        val expectedTag = requestingViewName + "_" + feature::class.java.simpleName
+        assertThat(sut.feature.tag).isEqualTo(expectedTag)
     }
 
 
