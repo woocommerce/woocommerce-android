4827ce4084277bca341356b658aa85a37cd6d737
ThomazFB <thomazfcortez@gmail.com>
2022-03-29 13:54:55 -0300

    Create FeatureFeedbackSettings unit test structure

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/model/FeatureFeedbackSettingsTest.kt
@@ -0,0 +1,23 @@
+package com.woocommerce.android.model
+
+import org.junit.Before
+import org.junit.Test
+
+class FeatureFeedbackSettingsTest {
+    private lateinit var sut: FeatureFeedbackSettingsTest
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun `when settings is created with no define state, then set it as UNANSWERED by default`() {
+
+    }
+
+    @Test
+    fun `when settings is created, then set tag value as requesting view with Feature class simple name`() {
+
+    }
+}
