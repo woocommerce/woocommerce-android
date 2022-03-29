bdfcf55d2903b1b5339ac13319a6883915fd1962
ThomazFB <thomazfcortez@gmail.com>
2022-03-29 14:08:04 -0300

    Create basic unit test coverage for FeatureFeedbackSettings

--- WooCommerce/src/test/kotlin/com/woocommerce/android/model/FeatureFeedbackSettingsTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/model/FeatureFeedbackSettingsTest.kt
@@ -1,23 +1,29 @@
 package com.woocommerce.android.model
 
-import org.junit.Before
+import com.woocommerce.android.model.FeatureFeedbackSettings.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class FeatureFeedbackSettingsTest {
-    private lateinit var sut: FeatureFeedbackSettingsTest
-
-    @Before
-    fun setUp() {
-
-    }
+    private lateinit var sut: FeatureFeedbackSettings
 
     @Test
     fun `when settings is created with no define state, then set it as UNANSWERED by default`() {
+        sut = FeatureFeedbackSettings(
+            feature = Feature.SimplePayments("test-view-name")
+        )
 
+        assertThat(sut.state).isEqualTo(FeedbackState.UNANSWERED)
     }
 
     @Test
     fun `when settings is created, then set tag value as requesting view with Feature class simple name`() {
+        sut = FeatureFeedbackSettings(
+            feature = Feature.ProductVariations("test-view-name")
+        )
 
+        assertThat(sut.feature.tag).isEqualTo("")
     }
+
+
 }
