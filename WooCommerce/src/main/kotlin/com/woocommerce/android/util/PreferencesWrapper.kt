f253e257ea1b26012ed4020a883deb2ff49cd970
Ovi Trif <octrif@gmail.com>
2022-04-01 18:08:08 +0200

    Read device token via AppPrefs in ZendeskHelper to remove dependency on PreferencesWrapper

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/PreferencesWrapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/PreferencesWrapper.kt
@@ -5,10 +5,6 @@ import androidx.preference.PreferenceManager
 import javax.inject.Inject
 
 class PreferencesWrapper @Inject constructor(val context: Context) {
-    companion object {
-        const val WPCOM_PUSH_DEVICE_TOKEN = "WC_PREF_NOTIFICATIONS_TOKEN"
-    }
-
     val sharedPreferences
         get() = PreferenceManager.getDefaultSharedPreferences(context)
 }
