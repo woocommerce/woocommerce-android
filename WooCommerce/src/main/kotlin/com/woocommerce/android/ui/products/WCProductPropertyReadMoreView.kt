25af850dc28474d19844600332e5b45b1aaf68c3
Nick Bradbury <nick.bradbury@gmail.com>
2022-04-01 16:32:34 -0400

    Removed unused import

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/WCProductPropertyReadMoreView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/WCProductPropertyReadMoreView.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.isVisible
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textview.MaterialTextView
