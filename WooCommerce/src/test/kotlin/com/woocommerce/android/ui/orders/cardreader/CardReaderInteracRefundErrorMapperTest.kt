8235033396a01dcd09f08c281521fb35315111f1
Anirudh Uppunda <abhat38@gmail.com>
2022-04-04 13:18:13 +0530

    fix detekt errors

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderInteracRefundErrorMapperTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/cardreader/CardReaderInteracRefundErrorMapperTest.kt
@@ -8,7 +8,6 @@ import org.junit.Before
 import org.junit.Test
 
 class CardReaderInteracRefundErrorMapperTest : BaseUnitTest() {
-
     private lateinit var cardReaderInteracRefundErrorMapper: CardReaderInteracRefundErrorMapper
 
     @Before
