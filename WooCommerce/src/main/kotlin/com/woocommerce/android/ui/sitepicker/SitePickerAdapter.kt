c45c6a420a1bfd3729d08f3e9444891676d66944
Nick Bradbury <nick.bradbury@gmail.com>
2022-04-05 15:07:54 -0400

    First pass at converting SitePickerAdapter to a ListAdapter

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/sitepicker/SitePickerAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/sitepicker/SitePickerAdapter.kt
@@ -5,6 +5,8 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.SitePickerItemBinding
@@ -13,14 +15,7 @@ import com.woocommerce.android.util.StringUtils
 import org.wordpress.android.fluxc.model.SiteModel
 
 class SitePickerAdapter(private val context: Context, private val listener: OnSiteClickListener) :
-    RecyclerView.Adapter<SiteViewHolder>() {
-    var siteList: List<SiteModel> = ArrayList()
-        set(value) {
-            if (!isSameSiteList(value)) {
-                field = value
-                notifyDataSetChanged()
-            }
-        }
+    ListAdapter<SiteModel, SiteViewHolder>(SiteModelDiffCallBack) {
     var selectedSiteId: Long = 0
         set(value) {
             if (field != value) {
@@ -37,13 +32,7 @@ class SitePickerAdapter(private val context: Context, private val listener: OnSi
         setHasStableIds(true)
     }
 
-    override fun getItemId(position: Int): Long {
-        return siteList[position].siteId
-    }
-
-    override fun getItemCount(): Int {
-        return siteList.size
-    }
+    override fun getItemId(position: Int): Long = getItem(position).siteId
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SiteViewHolder {
         return SiteViewHolder(
@@ -56,37 +45,8 @@ class SitePickerAdapter(private val context: Context, private val listener: OnSi
     }
 
     override fun onBindViewHolder(holder: SiteViewHolder, position: Int) {
-        val site = siteList[position]
+        val site = getItem(position)
         holder.bind(site)
-    }
-
-    /**
-     * returns true if the passed list of sites is the same as the current list
-     */
-    private fun isSameSiteList(sites: List<SiteModel>): Boolean {
-        if (sites.size != siteList.size) {
-            return false
-        }
-
-        sites.forEach {
-            if (!containsSite(it)) {
-                return false
-            }
-        }
-
-        return true
-    }
-
-    /**
-     * Returns true if the passed order is in the current list of orders
-     */
-    private fun containsSite(site: SiteModel): Boolean {
-        siteList.forEach {
-            if (it.siteId == site.siteId) {
-                return true
-            }
-        }
-        return false
     }
 
     inner class SiteViewHolder(val viewBinding: SitePickerItemBinding) : RecyclerView.ViewHolder(viewBinding.root) {
@@ -95,7 +55,7 @@ class SitePickerAdapter(private val context: Context, private val listener: OnSi
         }
 
         fun bind(site: SiteModel) {
-            viewBinding.radio.isVisible = siteList.size > 1
+            viewBinding.radio.isVisible = itemCount > 1
             viewBinding.radio.isChecked = site.siteId == selectedSiteId
             viewBinding.textSiteName.text =
                 if (!TextUtils.isEmpty(site.name)) site.name else context.getString(R.string.untitled)
@@ -111,5 +71,17 @@ class SitePickerAdapter(private val context: Context, private val listener: OnSi
                 viewBinding.root.setOnClickListener(null)
             }
         }
+    }
+
+    object SiteModelDiffCallBack : DiffUtil.ItemCallback<SiteModel>() {
+        override fun areItemsTheSame(
+            oldItem: SiteModel,
+            newItem: SiteModel
+        ): Boolean = oldItem.siteId == newItem.siteId
+
+        override fun areContentsTheSame(
+            oldItem: SiteModel,
+            newItem: SiteModel
+        ): Boolean = oldItem == newItem
     }
 }
