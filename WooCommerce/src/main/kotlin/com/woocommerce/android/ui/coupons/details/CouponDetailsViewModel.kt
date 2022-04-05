5900488d3dafff69be4597aee19086c7418d786f
hicham <hicham.boushaba@gmail.com>
2022-04-05 15:31:11 +0000

    Use CouponDetailsRepository for retrieving the demo coupon

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/details/CouponDetailsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/details/CouponDetailsViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import org.wordpress.android.fluxc.store.WooCommerceStore
 import java.math.BigDecimal
 import javax.inject.Inject
@@ -15,8 +16,10 @@ import javax.inject.Inject
 class CouponDetailsViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val wooCommerceStore: WooCommerceStore,
-    private val selectedSite: SelectedSite
+    private val selectedSite: SelectedSite,
+    private val couponDetailsRepository: CouponDetailsRepository
 ) : ScopedViewModel(savedState) {
+    private val navArgs by savedState.navArgs<CouponDetailsFragmentArgs>()
     private val currencyCode by lazy {
         wooCommerceStore.getSiteSettings(selectedSite.get())?.currencyCode
     }
@@ -24,21 +27,8 @@ class CouponDetailsViewModel @Inject constructor(
     val couponState = loadCoupon().asLiveData()
 
     @Suppress("MagicNumber")
-    private fun loadCoupon(): Flow<CouponDetailsState> = flow {
-        emit(
-            CouponDetailsState(
-                coupon = CouponUi(
-                    id = 1,
-                    code = "ABCDE",
-                    amount = BigDecimal(25),
-                    formattedDiscount = "25%",
-                    affectedArticles = "Everything excl. 5 products",
-                    formattedSpendingInfo = "Minimum spend of $20 \n\nMaximum spend of $200 \n",
-                    isActive = true
-                ),
-            )
-        )
-    }
+    private fun loadCoupon(): Flow<CouponDetailsState> = couponDetailsRepository.loadCoupon(navArgs.couponId)
+        .map { CouponDetailsState(coupon = it) }
 
     data class CouponDetailsState(
         val isLoading: Boolean = false,
