cb072af8ec8dfa29ebb7d6b1316f026b03230801
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-04-11 10:55:21 -0400

    Adding the CardReaderManualsFragment.kt file

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/manuals/CardReaderManualsFragment.kt
@@ -0,0 +1,40 @@
+package com.woocommerce.android.ui.prefs.cardreader.manuals
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
+import com.woocommerce.android.R
+import com.woocommerce.android.databinding.FragmentCardReaderManualsBinding
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CardReaderManualsFragment : BaseFragment(R.layout.fragment_card_reader_manuals) {
+    private var _binding: FragmentCardReaderManualsBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: CardReaderManualsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCardReaderManualsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.cardReaderManualsComposeView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WooThemeWithBackground {
+                    CardReaderManualsScreen()
+                }
+            }
+        }
+        return view
+    }
+}
