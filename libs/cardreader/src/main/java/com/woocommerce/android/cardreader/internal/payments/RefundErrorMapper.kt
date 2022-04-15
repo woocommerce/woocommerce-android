5b3ed4853ee08023da66b064e66c5c921b8cb8f4
Anirudh Uppunda <abhat38@gmail.com>
2022-04-15 11:32:52 +0530

    Make RefundErrorMapper.kt internal

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/RefundErrorMapper.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/internal/payments/RefundErrorMapper.kt
@@ -8,7 +8,7 @@ import com.woocommerce.android.cardreader.payments.CardInteracRefundStatus.Refun
 import com.woocommerce.android.cardreader.payments.CardInteracRefundStatus.RefundStatusErrorType.NoNetwork
 import com.woocommerce.android.cardreader.payments.RefundParams
 
-class RefundErrorMapper {
+internal class RefundErrorMapper {
     fun mapTerminalError(
         refundParams: RefundParams,
         exception: TerminalException
