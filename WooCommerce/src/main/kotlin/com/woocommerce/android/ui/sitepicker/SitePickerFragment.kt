ce702c7d8360de9b2f5220bba15a22b42aeff8d7
anitaamurthy <murthyanitaa@gmail.com>
2022-04-17 21:17:26 +0530

    Added new site picker fragment + xml

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/sitepicker/SitePickerFragment.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.sitepicker
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SitePickerFragment : BaseFragment(R.layout.fragment_site_picker)
