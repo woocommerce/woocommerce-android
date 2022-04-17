e18e29b462ee9c48b4c46ee5ade22e96c6af6d9e
Alejo <alejandro.torres.veiga@automattic.com>
2022-04-17 15:34:53 -0300

    select simple product

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/orders/OrderCreationScreen.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/screenshots/orders/OrderCreationScreen.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.screenshots.orders
 
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.woocommerce.android.R
@@ -19,6 +20,12 @@ class OrderCreationScreen : Screen {
         return SingleOrderScreen()
     }
 
+    fun addProduct(): OrderSelectProductScreen {
+        waitForElementToBeDisplayed(R.id.products_section)
+        Espresso.onView(withText(R.string.order_creation_add_products)).perform(click())
+        return OrderSelectProductScreen()
+    }
+
     fun assertNewOrderScreen(): OrderCreationScreen {
         Espresso.onView(withId(R.id.collapsing_toolbar))
             .check(matches(hasDescendant(withText(R.string.order_creation_fragment_title))))
@@ -26,5 +33,10 @@ class OrderCreationScreen : Screen {
         Espresso.onView(withId(ORDER_CREATION)).check(matches(isDisplayed()))
         Espresso.onView(withId(CREATE_BUTTON)).check(matches(isDisplayed()))
         return this
+    }
+
+    fun assertNewOrderScreenWithProduct(productName:String): OrderCreationScreen{
+        Espresso.onView(withText(productName)).check(matches(isDisplayed()))
+        return assertNewOrderScreen()
     }
 }
