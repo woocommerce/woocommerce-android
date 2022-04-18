7b1df0e031250487c90e6530a6162b38a0f0ea65
JorgeMucientes <jorge.mucientes@automattic.com>
2022-04-18 12:39:01 +0200

    Get local WC version to check if Inbox should be available

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/domain/IsInboxEnabled.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/domain/IsInboxEnabled.kt
@@ -1,14 +1,25 @@
 package com.woocommerce.android.ui.moremenu.domain
 
+import com.woocommerce.android.extensions.semverCompareTo
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.FeatureFlag
+import org.wordpress.android.fluxc.store.WooCommerceStore
+import org.wordpress.android.fluxc.store.WooCommerceStore.WooPlugin.WOO_CORE
 import javax.inject.Inject
 
 class IsInboxEnabled @Inject constructor(
     private val selectedSite: SelectedSite,
+    private val wooCommerceStore: WooCommerceStore
+) {
+    companion object {
+        private const val INBOX_MINIMUM_SUPPORTED_VERSION = "5.0.0"
+    }
 
-    ) {
     operator fun invoke(): Boolean {
-        return FeatureFlag.MORE_MENU_INBOX.isEnabled()
+        if (!FeatureFlag.MORE_MENU_INBOX.isEnabled()) return false
+
+        val currentWooCoreVersion =
+            wooCommerceStore.getSitePlugin(selectedSite.get(), WOO_CORE)?.version ?: "0.0"
+        return currentWooCoreVersion.semverCompareTo(INBOX_MINIMUM_SUPPORTED_VERSION) >= 0
     }
 }
