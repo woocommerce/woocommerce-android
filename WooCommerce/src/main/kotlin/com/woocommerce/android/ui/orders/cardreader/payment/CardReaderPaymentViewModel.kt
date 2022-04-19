2ce773e04ccb13c3b3152ba8a7551042647cc22b
Anirudh Uppunda <abhat38@gmail.com>
2022-04-19 11:07:39 +0530

    Make "When" as expression by adding .exhaustive extension function

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/payment/CardReaderPaymentViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/cardreader/payment/CardReaderPaymentViewModel.kt
@@ -311,7 +311,7 @@ class CardReaderPaymentViewModel
                 amountLabel,
                 refundStatus
             )
-        }
+        }.exhaustive
     }
 
     private fun onPaymentCompleted(
