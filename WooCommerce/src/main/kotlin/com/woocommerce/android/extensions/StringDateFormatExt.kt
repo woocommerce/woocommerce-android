c4ad9a18076156ef70d4ac002d57b540e8a7b793
hicham <hicham.boushaba@gmail.com>
2022-04-21 17:43:59 +0000

    Fix parsing of GMT dates.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/StringDateFormatExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/StringDateFormatExt.kt
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.GregorianCalendar
 import java.util.Locale
+import java.util.TimeZone
 
 /**
  * Method to convert date string from yyyy-MM-dd format to yyyy-MM format
@@ -131,6 +132,24 @@ fun String?.parseFromIso8601DateFormat(locale: Locale = Locale.getDefault()): Da
     return try {
         if (!this.isNullOrEmpty()) {
             val originalFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", locale)
+            return originalFormat.parse(this)
+        }
+        null
+    } catch (e: Exception) {
+        throw IllegalArgumentException("Date string argument is not of format yyyy-MM-dd'T'HH:mm:ss: $this")
+    }
+}
+
+/**
+ * Method to convert month string from yyyy-MM-dd'T'hh:mm:ss format to Date object
+ */
+@Throws(IllegalArgumentException::class)
+fun String?.parseGMTDateFromIso8601DateFormat(locale: Locale = Locale.getDefault()): Date? {
+    return try {
+        if (!this.isNullOrEmpty()) {
+            val originalFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", locale).apply {
+                timeZone = TimeZone.getTimeZone("UTC")
+            }
             return originalFormat.parse(this)
         }
         null
