025d6bf06b64d2abda447e13b287850c6119e800
Nick Bradbury <nick.bradbury@gmail.com>
2022-04-21 10:57:24 -0400

    Fixed failing test

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/OrderDetailViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/OrderDetailViewModelTest.kt
@@ -91,7 +91,8 @@ class OrderDetailViewModelTest : BaseUnitTest() {
         isCreateShippingLabelButtonVisible = false,
         isProductListVisible = true,
         areShippingLabelsVisible = false,
-        isProductListMenuVisible = false
+        isProductListMenuVisible = false,
+        isSharePaymentLinkVisible = false
     )
 
     @Before
