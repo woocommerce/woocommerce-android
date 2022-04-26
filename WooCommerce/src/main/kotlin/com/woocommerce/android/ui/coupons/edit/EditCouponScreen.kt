bf1cfd28adc2a3dda2d4a0ee11ade943eb1ed1dd
hicham <hicham.boushaba@gmail.com>
2022-04-26 18:27:57 +0000

    Create edit coupon screen

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponScreen.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.coupons.edit
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EditCouponScreen(viewModel: EditCouponViewModel) {
+
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponViewModel.kt
@@ -0,0 +1,9 @@
+package com.woocommerce.android.ui.coupons.edit
+
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class EditCouponViewModel(savedStateHandle: SavedStateHandle) : ScopedViewModel(savedStateHandle) {
+}
