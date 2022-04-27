fd1f4d76180096d7c2cf2e931caa958045bf969a
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-04-27 19:49:52 -0300

    Update ExperimentationModule with new experimentation dependency version

--- WooCommerce/src/main/kotlin/com/woocommerce/android/di/ExperimentationModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/di/ExperimentationModule.kt
@@ -1,9 +1,7 @@
 package com.woocommerce.android.di
 
 import com.automattic.android.experimentation.ExPlat
-import com.automattic.android.experimentation.Experiment
 import com.woocommerce.android.BuildConfig
-import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,22 +14,16 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 class ExperimentationModule {
-    // TODO Suppress can be removed after the first Experiment is added
-    @Suppress("UnusedPrivateMember")
-    @Provides
-    @Singleton
-    fun provideExperiments(exPlat: ExPlat): Set<Experiment> = setOf()
 
     @Provides
     @Singleton
     fun provideExPlat(
-        experiments: Lazy<Set<Experiment>>,
         experimentStore: ExperimentStore,
         appLogWrapper: AppLogWrapper,
         @AppCoroutineScope appCoroutineScope: CoroutineScope,
     ) = ExPlat(
         platform = ExperimentStore.Platform.WOOCOMMERCE_ANDROID,
-        experiments = experiments,
+        experiments = setOf(),
         experimentStore = experimentStore,
         appLogWrapper = appLogWrapper,
         coroutineScope = appCoroutineScope,
