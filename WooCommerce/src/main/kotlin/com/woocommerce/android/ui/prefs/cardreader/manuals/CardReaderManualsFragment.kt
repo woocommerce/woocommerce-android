1c37e30e9c9b1d677a95263026d9d3271d004bdd
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-04-28 11:45:41 -0400

    Removing xml file and adding Composeview directly to the Fragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/manuals/CardReaderManualsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/manuals/CardReaderManualsFragment.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.woocommerce.android.R
-import com.woocommerce.android.databinding.FragmentCardReaderManualsBinding
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CardReaderManualsFragment : BaseFragment(R.layout.fragment_card_reader_manuals) {
-    private var _binding: FragmentCardReaderManualsBinding? = null
-    private val binding get() = _binding!!
+class CardReaderManualsFragment : BaseFragment() {
 
 //    private val viewModel: CardReaderManualsViewModel by viewModels()
 
@@ -23,10 +21,7 @@ class CardReaderManualsFragment : BaseFragment(R.layout.fragment_card_reader_man
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentCardReaderManualsBinding.inflate(inflater, container, false)
-        val view = binding.root
-        binding.cardReaderManualsComposeView.apply {
-            // Dispose of the Composition when the view's LifecycleOwner is destroyed
+        return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 WooThemeWithBackground {
@@ -34,11 +29,7 @@ class CardReaderManualsFragment : BaseFragment(R.layout.fragment_card_reader_man
                 }
             }
         }
-        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+    override fun getFragmentTitle() = resources.getString(R.string.card_reader_manuals)
 }
