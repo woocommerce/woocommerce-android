6a6c352e54ab581bf725d2b3265257498d2857f9
hicham <hicham.boushaba@gmail.com>
2022-04-28 13:28:26 +0000

    Update the repository API to use a suspendable function instead of Flow

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponRepository.kt
@@ -5,6 +5,7 @@ import com.woocommerce.android.model.toAppModel
 import com.woocommerce.android.tools.SelectedSite
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import org.wordpress.android.fluxc.store.CouponStore
 import javax.inject.Inject
@@ -13,7 +14,9 @@ class EditCouponRepository @Inject constructor(
     private val selectedSite: SelectedSite,
     private val couponStore: CouponStore
 ) {
-    fun observeCoupon(couponId: Long): Flow<Coupon> = couponStore.observeCoupon(selectedSite.get(), couponId)
+    // TODO replace this with the added [getCoupon] function when the last FluxC PR is merged
+    suspend fun getCoupon(couponId: Long): Coupon = couponStore.observeCoupon(selectedSite.get(), couponId)
         .filterNotNull()
         .map { it.toAppModel() }
+        .first()
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponViewModel.kt
@@ -9,12 +9,10 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getNullableStateFlow
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,8 +23,9 @@ class EditCouponViewModel @Inject constructor(
     private val couponUtils: CouponUtils
 ) : ScopedViewModel(savedStateHandle) {
     private val navArgs: EditCouponFragmentArgs by savedStateHandle.navArgs()
-    private val storedCoupon: Flow<Coupon> = editCouponRepository.observeCoupon(navArgs.couponId)
-        .shareIn(viewModelScope, started = SharingStarted.WhileSubscribed())
+    private val storedCoupon: Deferred<Coupon> = async {
+        editCouponRepository.getCoupon(navArgs.couponId)
+    }
 
     private val couponDraft = savedStateHandle.getNullableStateFlow(viewModelScope, null, Coupon::class.java)
 
@@ -43,7 +42,7 @@ class EditCouponViewModel @Inject constructor(
     init {
         if (couponDraft.value == null) {
             launch {
-                couponDraft.value = storedCoupon.first()
+                couponDraft.value = storedCoupon.await()
             }
         }
     }
