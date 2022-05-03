ad3786bb10a3c5dc82d1fa726a452827c1dedc24
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-05-03 13:59:28 -0400

    Adding data class and list function

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModel.kt
@@ -1,6 +1,8 @@
 package com.woocommerce.android.ui.cardreader.manuals
 
+import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.R
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,5 +11,27 @@ import javax.inject.Inject
 class CardReaderManualsViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
-    // TODO add manuals screen logic here
+
+    private val _manualState = getManualItems().toMutableStateList()
+    val manualState: List<ManualItem>
+        get() = _manualState
+
+    private fun getManualItems(): List<ManualItem> = listOf(
+        ManualItem(
+            icon = R.drawable.ic_card_reader_manual,
+            label = R.string.card_reader_bbpos_manual_card_reader,
+            onManualClicked = { }
+        ),
+        ManualItem(
+            icon = R.drawable.ic_card_reader_manual,
+            label = R.string.card_reader_m2_manual_card_reader,
+            onManualClicked = { }
+        )
+    )
+
+    data class ManualItem(
+        val icon: Int,
+        val label: Int,
+        val onManualClicked: () -> Unit
+    )
 }
