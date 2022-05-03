151b06d8ade6d8d32986f634f21481519c82794e
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-05-03 18:41:21 -0400

    Add click events to ViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModel.kt
@@ -2,7 +2,9 @@ package com.woocommerce.android.ui.cardreader.manuals
 
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.AppUrls
 import com.woocommerce.android.R
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -19,14 +21,26 @@ class CardReaderManualsViewModel @Inject constructor(
         ManualItem(
             icon = R.drawable.ic_card_reader_manual,
             label = R.string.card_reader_bbpos_manual_card_reader,
-            onManualClicked = { }
+            onManualClicked = ::onBbposManualClicked
         ),
         ManualItem(
             icon = R.drawable.ic_card_reader_manual,
             label = R.string.card_reader_m2_manual_card_reader,
-            onManualClicked = { }
+            onManualClicked = ::onM2ManualClicked
         )
     )
+
+    private fun onBbposManualClicked() {
+        triggerEvent(ManualEvents.NavigateToCardReaderManualLink(AppUrls.BBPOS_MANUAL_CARD_READER))
+    }
+
+    private fun onM2ManualClicked() {
+        triggerEvent(ManualEvents.NavigateToCardReaderManualLink(AppUrls.M2_MANUAL_CARD_READER))
+    }
+
+    sealed class ManualEvents : MultiLiveEvent.Event() {
+        data class NavigateToCardReaderManualLink(val url: String) : ManualEvents()
+    }
 
     data class ManualItem(
         val icon: Int,
