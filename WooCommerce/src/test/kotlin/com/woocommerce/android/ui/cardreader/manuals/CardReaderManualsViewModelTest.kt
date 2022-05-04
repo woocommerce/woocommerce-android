0106e6cceda5de6f0e2412fb6aaf0f1873a22f99
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-05-04 12:42:17 -0400

    Add tests to CardReaderManualsViewModelTest.kt

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModelTest.kt
@@ -1,10 +1,12 @@
 package com.woocommerce.android.ui.cardreader.manuals
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.AppUrls
+import com.woocommerce.android.R
 import com.woocommerce.android.viewmodel.BaseUnitTest
-import org.junit.Assert.*
-
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
+import org.junit.Test
 
 class CardReaderManualsViewModelTest : BaseUnitTest() {
     private lateinit var viewModel: CardReaderManualsViewModel
@@ -13,5 +15,73 @@ class CardReaderManualsViewModelTest : BaseUnitTest() {
     @Before
     fun setUp() {
         viewModel = CardReaderManualsViewModel(savedStateHandle)
+    }
+
+    @Test
+    fun `when screen shown, then BBPOS label is displayed`() {
+        val bbposRow = viewModel.manualState.find {
+            it.label == R.string.card_reader_bbpos_manual_card_reader
+        }
+
+        assertThat(bbposRow).isNotNull
+    }
+
+    @Test
+    fun `when user clicks BBPOS reader, then app navigates to BBPOS manual link`() {
+        val bbposRow = viewModel.manualState.find {
+            it.label == R.string.card_reader_bbpos_manual_card_reader
+        }
+
+        bbposRow!!.onManualClicked.invoke()
+
+        assertThat(viewModel.event.value)
+            .isEqualTo(
+                CardReaderManualsViewModel.ManualEvents.NavigateToCardReaderManualLink(
+                    AppUrls.BBPOS_MANUAL_CARD_READER
+                )
+            )
+    }
+
+    @Test
+    fun `when screen shown, then BBPOS icon is displayed`() {
+        val bbposRow = viewModel.manualState.find {
+            it.icon == R.drawable.ic_card_reader_manual
+        }
+
+        assertThat(bbposRow).isNotNull
+    }
+
+    @Test
+    fun `when screen shown, then M2 label is displayed`() {
+        val m2Row = viewModel.manualState.find {
+            it.label == R.string.card_reader_m2_manual_card_reader
+        }
+
+        assertThat(m2Row).isNotNull
+    }
+
+    @Test
+    fun `when user clicks M2 reader, then app navigates to M2 manual link`() {
+        val m2Row = viewModel.manualState.find {
+            it.label == R.string.card_reader_m2_manual_card_reader
+        }
+
+        m2Row!!.onManualClicked.invoke()
+
+        assertThat(viewModel.event.value)
+            .isEqualTo(
+                CardReaderManualsViewModel.ManualEvents.NavigateToCardReaderManualLink(
+                    AppUrls.M2_MANUAL_CARD_READER
+                )
+            )
+    }
+
+    @Test
+    fun `when screen is shown, then M2 icon is displayed`() {
+        val m2Row = viewModel.manualState.find {
+            it.icon == R.drawable.ic_card_reader_manual
+        }
+
+        assertThat(m2Row).isNotNull
     }
 }
