576cbeaa17081377000459b40e3970443e6a33e0
Nick Bradbury <nick.bradbury@gmail.com>
2022-05-05 08:55:25 -0500

    Only adjust bottom sheet size on large tablets

--- WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/WCBottomSheetDialogFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/widgets/WCBottomSheetDialogFragment.kt
@@ -9,16 +9,15 @@ import org.wordpress.android.util.DisplayUtils
 
 /**
  * The default behavior of a bottom sheet in landscape causes it to be cut off after the first item,
- * which makes sense for phones but not tablets since there's usually plenty of room to show more
- * items. This simple BottomSheetDialogFragment wrapper resolves this by showing the entire sheet on
- * landscape tablets.
+ * which makes sense for phones but not large tablets since there's usually plenty of room to show
+ * more items. This simple BottomSheetDialogFragment wrapper resolves this by showing the entire
+ * sheet on large landscape tablets.
  */
 open class WCBottomSheetDialogFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val isTablet = DisplayUtils.isTablet(requireContext()) || DisplayUtils.isXLargeTablet(requireContext())
-        if (isTablet && DisplayUtils.isLandscape(requireContext())) {
+        if (DisplayUtils.isXLargeTablet(requireContext()) && DisplayUtils.isLandscape(requireContext())) {
             dialog?.setOnShowListener {
                 val dialog = it as BottomSheetDialog
                 dialog.findViewById<View>(R.id.design_bottom_sheet)?.let { sheet ->
