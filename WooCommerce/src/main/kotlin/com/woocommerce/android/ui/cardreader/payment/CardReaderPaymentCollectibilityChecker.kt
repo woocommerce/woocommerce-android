02b1f3b7736c4ddbe95166f5533e4544cd8fd8fb
Andrey <andrey.kidinov@a8c.com>
2022-05-11 12:02:59 +0400

    Added stripe as supported payment method for a payment collectability

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/payment/CardReaderPaymentCollectibilityChecker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/payment/CardReaderPaymentCollectibilityChecker.kt
@@ -1,9 +1,13 @@
 package com.woocommerce.android.ui.cardreader.payment
 
 import com.woocommerce.android.extensions.CASH_ON_DELIVERY_PAYMENT_TYPE
+import com.woocommerce.android.extensions.STRIPE_PAYMENTS_PAYMENT_TYPE
 import com.woocommerce.android.extensions.WOOCOMMERCE_BOOKINGS_PAYMENT_TYPE
 import com.woocommerce.android.extensions.WOOCOMMERCE_PAYMENTS_PAYMENT_TYPE
 import com.woocommerce.android.model.Order
+import com.woocommerce.android.model.Order.Status.OnHold
+import com.woocommerce.android.model.Order.Status.Pending
+import com.woocommerce.android.model.Order.Status.Processing
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
 import java.math.BigDecimal
 import javax.inject.Inject
@@ -15,19 +19,25 @@ class CardReaderPaymentCollectibilityChecker @Inject constructor(
     suspend fun isCollectable(order: Order): Boolean {
         return with(order) {
             cardReaderPaymentCurrencySupportedChecker.isCurrencySupported(currency) &&
-                (listOf(Order.Status.Pending, Order.Status.Processing, Order.Status.OnHold)).any { it == status } &&
+                isStatusCollectable() &&
                 !isOrderPaid &&
                 order.total.compareTo(BigDecimal.ZERO) == 1 &&
                 BigDecimal.ZERO.compareTo(order.refundTotal) == 0 &&
-                // Empty payment method explanation:
-                // https://github.com/woocommerce/woocommerce/issues/29471
-                (
-                    paymentMethod == CASH_ON_DELIVERY_PAYMENT_TYPE ||
-                        paymentMethod.isEmpty() ||
-                        paymentMethod == WOOCOMMERCE_PAYMENTS_PAYMENT_TYPE ||
-                        paymentMethod == WOOCOMMERCE_BOOKINGS_PAYMENT_TYPE
-                    ) &&
+                isPaymentMethodCollectable() &&
                 !orderDetailRepository.hasSubscriptionProducts(order.getProductIds())
         }
     }
+
+    private fun Order.isPaymentMethodCollectable() =
+        paymentMethod in arrayOf(
+            // Empty payment method explanation:
+            // https://github.com/woocommerce/woocommerce/issues/29471
+            "",
+            CASH_ON_DELIVERY_PAYMENT_TYPE,
+            WOOCOMMERCE_PAYMENTS_PAYMENT_TYPE,
+            STRIPE_PAYMENTS_PAYMENT_TYPE,
+            WOOCOMMERCE_BOOKINGS_PAYMENT_TYPE,
+        )
+
+    private fun Order.isStatusCollectable() = (listOf(Pending, Processing, OnHold)).any { it == status }
 }
