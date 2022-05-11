522c088148e0f94b569920a1634cac6320bb7167
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-05-11 08:32:53 -0400

    Add wisepad manual Unit tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/cardreader/manuals/CardReaderManualsViewModelTest.kt
@@ -3,18 +3,22 @@ package com.woocommerce.android.ui.cardreader.manuals
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.AppUrls
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.cardreader.InPersonPaymentsCanadaFeatureFlag
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 class CardReaderManualsViewModelTest : BaseUnitTest() {
     private lateinit var viewModel: CardReaderManualsViewModel
     private val savedStateHandle: SavedStateHandle = SavedStateHandle()
+    private val inPersonPaymentsCanadaFeatureFlag: InPersonPaymentsCanadaFeatureFlag = mock()
 
     @Before
     fun setUp() {
-        viewModel = CardReaderManualsViewModel(savedStateHandle)
+        viewModel = CardReaderManualsViewModel(savedStateHandle, InPersonPaymentsCanadaFeatureFlag())
     }
 
     @Test
@@ -83,5 +87,58 @@ class CardReaderManualsViewModelTest : BaseUnitTest() {
         }
 
         assertThat(m2Row).isNotNull
+    }
+
+    @Test
+    fun `given IPP Canada enabled, when screen shown, then wisepad3 label is displayed`() {
+        whenever(inPersonPaymentsCanadaFeatureFlag.isEnabled()).thenReturn(true)
+
+        initViewModel()
+
+        val wisePad3 = viewModel.manualState.find {
+            it.label == R.string.card_reader_wisepad_3_manual_card_reader
+        }
+
+        assertThat(wisePad3).isNotNull
+    }
+
+    @Test
+    fun `given IPP Canada enabled, when user clicks wisepad3 reader, then app navigates to wisepad3 manual link`() {
+        whenever(inPersonPaymentsCanadaFeatureFlag.isEnabled()).thenReturn(true)
+
+        initViewModel()
+
+        val wisePad3 = viewModel.manualState.find {
+            it.label == R.string.card_reader_wisepad_3_manual_card_reader
+        }
+
+        wisePad3!!.onManualClicked.invoke()
+
+        assertThat(viewModel.event.value)
+            .isEqualTo(
+                CardReaderManualsViewModel.ManualEvents.NavigateToCardReaderManualLink(
+                    AppUrls.WISEPAD_3_MANUAL_CARD_READER
+                )
+            )
+    }
+
+    @Test
+    fun `given IPP Canada enabled, when screen is shown, then wisepad3 icon is displayed`() {
+        whenever(inPersonPaymentsCanadaFeatureFlag.isEnabled()).thenReturn(true)
+
+        initViewModel()
+
+        val wisePad3 = viewModel.manualState.find {
+            it.icon == R.drawable.ic_wisepad3_reader
+        }
+
+        assertThat(wisePad3).isNotNull
+    }
+
+    private fun initViewModel() {
+        viewModel = CardReaderManualsViewModel(
+            savedStateHandle,
+            inPersonPaymentsCanadaFeatureFlag
+        )
     }
 }
