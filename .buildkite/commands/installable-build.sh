1009f680f864b928851888b615962351e5ca660b
Oguz Kocer <oguz.kocer@automattic.com>
2022-05-11 14:18:52 -0400

    Update bash-cache plugin to restore Gradle dependency cache

--- .buildkite/commands/installable-build.sh
+++ .buildkite/commands/installable-build.sh
@@ -6,8 +6,5 @@ install_gems
 echo "--- :closed_lock_with_key: Installing Secrets"
 bundle exec fastlane run configure_apply
 
-echo "--- Restoring Gradle Cache"
-restore_cache "$GRADLE_DEPENDENCY_CACHE_KEY"
-
 echo "--- :hammer_and_wrench: Building"
 bundle exec fastlane build_and_upload_installable_build
