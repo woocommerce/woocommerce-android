55130c0161a838e3844fc771b99f28e9df45d84b
Andrey <andrey.kidinov@a8c.com>
2022-05-12 13:14:09 +0400

    Adjusted a comment changes to which I removed by mistake

--- quicklogin/woo_login.sh
+++ quicklogin/woo_login.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-# PASS WORDPRESS_EMAIL and WORDPRESS_PASSWORD as env variable to the script
+# PASS WORDPRESS_EMAIL, WORDPRESS_SITE and WORDPRESS_PASSWORD as env variable to the script
 # Optionally pass path to your adb: ADB_PATH
-# Example: ADB_PATH=~/Library/Android/sdk/platform-tools/adb WORDPRESS_EMAIL=my_wp_email@gmail.com  WORDPRESS_PASSWORD=my_pasword_1_# ./quicklogin/woo_login.sh
+# Example: ADB_PATH=~/Library/Android/sdk/platform-tools/adb WORDPRESS_EMAIL=my_wp_email@gmail.com  WORDPRESS_PASSWORD=my_pasword_1_# WORDPRESS_SITE=my_wordpress_site ./quicklogin/woo_login.sh
 
 if [[ "$ADB_PATH" ]]; then
   adbPath="$ADB_PATH"
