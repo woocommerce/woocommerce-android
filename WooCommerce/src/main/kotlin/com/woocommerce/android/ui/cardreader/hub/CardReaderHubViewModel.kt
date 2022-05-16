a5cc95c9c918b5effe583891fb7186861a12bad0
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-05-16 09:24:13 -0400

    Remove CARD_READER_MANUALS FeatureFlag param and import

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/hub/CardReaderHubViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/hub/CardReaderHubViewModel.kt
@@ -10,7 +10,6 @@ import com.woocommerce.android.R
 import com.woocommerce.android.model.UiString
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.cardreader.InPersonPaymentsCanadaFeatureFlag
-import com.woocommerce.android.ui.cardreader.manuals.CardReaderManualsFeatureFlag
 import com.woocommerce.android.ui.cardreader.onboarding.CardReaderFlowParam
 import com.woocommerce.android.ui.cardreader.onboarding.PluginType.STRIPE_EXTENSION_GATEWAY
 import com.woocommerce.android.ui.cardreader.onboarding.PluginType.WOOCOMMERCE_PAYMENTS
@@ -18,14 +17,12 @@ import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
-import okhttp3.internal.toImmutableList
 import javax.inject.Inject
 
 @HiltViewModel
 class CardReaderHubViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val inPersonPaymentsCanadaFeatureFlag: InPersonPaymentsCanadaFeatureFlag,
-    private val cardReaderManualsFeatureFlag: CardReaderManualsFeatureFlag,
     private val appPrefsWrapper: AppPrefsWrapper,
     private val selectedSite: SelectedSite,
 ) : ScopedViewModel(savedState) {
