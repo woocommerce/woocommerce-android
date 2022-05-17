67872308c58bde5748f68f54987b119f873733c8
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-05-17 18:40:18 +0200

    Register FCM device when new token arrives

--- WooCommerce/src/main/kotlin/com/woocommerce/android/push/FCMMessageService.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/push/FCMMessageService.kt
@@ -2,24 +2,27 @@ package com.woocommerce.android.push
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.woocommerce.android.AppPrefsWrapper
+import com.woocommerce.android.di.AppCoroutineScope
 import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.WooLog.T
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FCMMessageService : FirebaseMessagingService() {
     @Inject lateinit var notificationMessageHandler: NotificationMessageHandler
-    @Inject lateinit var notificationRegistrationHandler: NotificationRegistrationHandler
+    @Inject lateinit var appPrefsWrapper: AppPrefsWrapper
+    @Inject lateinit var registerDevice: RegisterDevice
+    @AppCoroutineScope lateinit var appCoroutineScope: CoroutineScope
 
-    /**
-     * Called if the FCM registration token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the
-     * FCM registration token is initially generated so this is where you would retrieve the token.
-     */
     override fun onNewToken(newToken: String) {
-        WooLog.d(T.NOTIFS, "Sending FCM token to our remote services: $newToken")
-        notificationRegistrationHandler.onNewFCMTokenReceived(newToken)
+        appPrefsWrapper.setFCMToken(newToken)
+        appCoroutineScope.launch {
+            registerDevice()
+        }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
