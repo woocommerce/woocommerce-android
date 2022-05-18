bd6cfe274233d2c0e6dfcb342cd3de11f2b33110
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-05-18 13:03:38 +0200

    Add tests for NotificationRepository

--- WooCommerce/src/main/kotlin/com/woocommerce/android/push/NotificationRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/push/NotificationRepository.kt
@@ -1,18 +1,14 @@
 package com.woocommerce.android.push
 
 import com.woocommerce.android.util.WooLog
-import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.fluxc.store.NotificationStore
 import javax.inject.Inject
 
 class NotificationRepository @Inject constructor(
-    private val accountStore: AccountStore,
     private val notificationStore: NotificationStore,
 ) {
     suspend fun registerDevice(token: String) {
-        if (accountStore.hasAccessToken()) {
-            WooLog.d(WooLog.T.NOTIFS, "Sending FCM token to our remote services: $token")
-            notificationStore.registerDevice(token, NotificationStore.NotificationAppKey.WOOCOMMERCE)
-        }
+        WooLog.d(WooLog.T.NOTIFS, "Sending FCM token to our remote services: $token")
+        notificationStore.registerDevice(token, NotificationStore.NotificationAppKey.WOOCOMMERCE)
     }
 }
