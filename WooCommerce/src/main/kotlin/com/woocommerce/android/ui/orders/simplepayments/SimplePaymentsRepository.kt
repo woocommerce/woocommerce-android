1760e29a1f75ccd8ce08f1774999d651d314e02b
Nick Bradbury <nick.bradbury@gmail.com>
2022-05-18 16:30:52 -0400

    Removed unused parameters and imports

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/simplepayments/SimplePaymentsRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/simplepayments/SimplePaymentsRepository.kt
@@ -1,23 +1,16 @@
 package com.woocommerce.android.ui.orders.simplepayments
 
-import com.woocommerce.android.extensions.semverCompareTo
 import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.ui.orders.creation.OrderCreationRepository
-import com.woocommerce.android.util.CoroutineDispatchers
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.store.OrderUpdateStore
 import org.wordpress.android.fluxc.store.WCOrderStore
-import org.wordpress.android.fluxc.store.WooCommerceStore
 import javax.inject.Inject
 
 @ViewModelScoped
 class SimplePaymentsRepository @Inject constructor(
     private val orderUpdateStore: OrderUpdateStore,
-    private val selectedSite: SelectedSite,
-    private val wooCommerceStore: WooCommerceStore,
-    private val dispatchers: CoroutineDispatchers
+    private val selectedSite: SelectedSite
 ) {
     suspend fun updateSimplePayment(
         orderId: Long,
@@ -34,13 +27,5 @@ class SimplePaymentsRepository @Inject constructor(
             billingEmail,
             isTaxable
         )
-    }
-
-    private suspend fun isAutoDraftSupported(): Boolean {
-        val version = withContext(dispatchers.io) {
-            wooCommerceStore.getSitePlugin(selectedSite.get(), WooCommerceStore.WooPlugin.WOO_CORE)?.version
-                ?: "0.0"
-        }
-        return version.semverCompareTo(OrderCreationRepository.AUTO_DRAFT_SUPPORTED_VERSION) >= 0
     }
 }
