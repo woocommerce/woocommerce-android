fdd2607a1f9c329ee5774099b4be7e3992bcc472
ThomazFB <thomazfcortez@gmail.com>
2022-05-18 11:04:53 -0300

    Adjust back button click to account for isEmpty method usage

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreationViewModel.kt
@@ -240,7 +240,7 @@ class OrderCreationViewModel @Inject constructor(
     }
 
     fun onBackButtonClicked() {
-        if (_orderDraft.value.copy(currency = "") == Order.EMPTY) {
+        if (_orderDraft.value.copy(currency = "").isEmpty()) {
             triggerEvent(Exit)
             return
         }
