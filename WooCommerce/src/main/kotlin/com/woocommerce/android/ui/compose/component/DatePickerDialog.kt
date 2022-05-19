0a770686856c8ec50aa44ac6be432f954de1f692
hicham <hicham.boushaba@gmail.com>
2022-05-19 10:56:34 +0100

    Remove unused import

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/component/DatePickerDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/component/DatePickerDialog.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
