2f0bac165c3fed2fca9739409032d87a1109abc0
hicham <hicham.boushaba@gmail.com>
2022-05-20 15:48:32 +0100

    Add restriction switches

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/CouponRestrictionsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/CouponRestrictionsScreen.kt
@@ -4,18 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.component.BigDecimalTextFieldValueMapper
 import com.woocommerce.android.ui.compose.component.WCOutlinedTypedTextField
+import com.woocommerce.android.ui.compose.component.WCSwitch
 import java.math.BigDecimal
 
 @Composable
@@ -33,11 +38,7 @@ fun CouponRestrictionsScreen(viewState: CouponRestrictionsViewModel.ViewState) {
         modifier = Modifier
             .background(color = MaterialTheme.colors.surface)
             .verticalScroll(scrollState)
-            .padding(
-                start = dimensionResource(id = R.dimen.major_100),
-                top = dimensionResource(id = R.dimen.major_100),
-                bottom = dimensionResource(id = R.dimen.major_100)
-            )
+            .padding(vertical = dimensionResource(id = R.dimen.major_100))
             .fillMaxSize()
     ) {
         WCOutlinedTypedTextField(
@@ -45,7 +46,7 @@ fun CouponRestrictionsScreen(viewState: CouponRestrictionsViewModel.ViewState) {
             onValueChange = { },
             label = stringResource(id = R.string.coupon_restrictions_minimum_spend_hint, viewState.currencyCode),
             valueMapper = BigDecimalTextFieldValueMapper(supportsNegativeValue = false),
-            modifier = Modifier.padding(end = dimensionResource(id = R.dimen.major_100))
+            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.major_100))
         )
 
         WCOutlinedTypedTextField(
@@ -53,7 +54,63 @@ fun CouponRestrictionsScreen(viewState: CouponRestrictionsViewModel.ViewState) {
             onValueChange = { },
             label = stringResource(id = R.string.coupon_restrictions_maximum_spend_hint, viewState.currencyCode),
             valueMapper = BigDecimalTextFieldValueMapper(supportsNegativeValue = false),
-            modifier = Modifier.padding(end = dimensionResource(id = R.dimen.major_100))
+            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.major_100))
+        )
+
+        IndividualUseSwitch(viewState.restrictions.isForIndividualUse ?: false)
+        SaleItemsSwitch(viewState.restrictions.areSaleItemsExcluded ?: false)
+    }
+}
+
+@Composable
+private fun IndividualUseSwitch(isForIndividualUse: Boolean) {
+    Column(Modifier.fillMaxWidth()) {
+        WCSwitch(
+            text = stringResource(id = R.string.coupon_restrictions_individual_use),
+            checked = isForIndividualUse,
+            onCheckedChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(id = R.dimen.major_100))
+        )
+        Divider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(id = R.dimen.major_100),
+                start = dimensionResource(id = R.dimen.major_100)
+            )
+        )
+        Text(
+            text = stringResource(id = R.string.coupon_restrictions_individual_use_hint),
+            style = MaterialTheme.typography.caption,
+            color = colorResource(id = R.color.color_on_surface_medium),
+            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.major_100))
         )
     }
 }
+
+@Composable
+private fun SaleItemsSwitch(areSaleItemsExcluded: Boolean) {
+    Column(Modifier.fillMaxWidth()) {
+        WCSwitch(
+            text = stringResource(id = R.string.coupon_restrictions_exclude_sale_items),
+            checked = areSaleItemsExcluded,
+            onCheckedChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(id = R.dimen.major_100))
+        )
+        Divider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(id = R.dimen.major_100),
+                start = dimensionResource(id = R.dimen.major_100)
+            )
+        )
+        Text(
+            text = stringResource(id = R.string.coupon_restrictions_exclude_sale_items_hint),
+            style = MaterialTheme.typography.caption,
+            color = colorResource(id = R.color.color_on_surface_medium),
+            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.major_100))
+        )
+    }
+}
+
