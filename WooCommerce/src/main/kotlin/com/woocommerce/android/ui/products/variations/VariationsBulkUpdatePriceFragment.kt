bf6ff517acff5bb94ac4f7e73910d9ad5d97d874
samiuelson <samiuelson@gmail.com>
2022-05-21 10:52:37 +0200

    Calculate and render view state of Price Update

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceFragment.kt
@@ -8,8 +8,17 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentVariationsBulkUpdatePriceBinding
+import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.products.variations.ValuesGroupType.Common
+import com.woocommerce.android.ui.products.variations.ValuesGroupType.Mixed
+import com.woocommerce.android.ui.products.variations.ValuesGroupType.None
+import com.woocommerce.android.ui.products.variations.VariationsBulkUpdatePriceViewModel.PriceType.Regular
+import com.woocommerce.android.ui.products.variations.VariationsBulkUpdatePriceViewModel.PriceType.Sale
+import dagger.hilt.android.AndroidEntryPoint
+import java.math.BigDecimal
 
+@AndroidEntryPoint
 class VariationsBulkUpdatePriceFragment : BaseFragment(R.layout.fragment_variations_bulk_update_price) {
     private val viewModel: VariationsBulkUpdatePriceViewModel by viewModels()
 
@@ -24,10 +33,60 @@ class VariationsBulkUpdatePriceFragment : BaseFragment(R.layout.fragment_variati
             viewModel.onPriceEntered(price)
         }
         binding.price.showKeyboard()
+        observeViewStateChanges()
     }
 
-    override fun getFragmentTitle(): String {
-        return getString(R.string.variations_bulk_update_regular_price)
+    private fun observeViewStateChanges() {
+        viewModel.viewStateData.observe(viewLifecycleOwner) { old, new ->
+            new.priceType?.takeIfNotEqualTo(old?.priceType) {
+                requireActivity().title = when (new.priceType) {
+                    Regular -> getString(R.string.variations_bulk_update_regular_price)
+                    Sale -> getString(R.string.variations_bulk_update_sale_price)
+                }
+            }
+            new.currency?.takeIfNotEqualTo(old?.currency) {
+                setupPriceInputField(new.currency, new.isCurrencyPrefix)
+            }
+            new.variationsToUpdateCount?.takeIfNotEqualTo(old?.variationsToUpdateCount) {
+                binding.priceUpdateInfo.text =
+                    getString(R.string.variations_bulk_update_price_info).format(new.variationsToUpdateCount)
+            }
+            new.pricesGroupType?.takeIfNotEqualTo(old?.pricesGroupType) {
+                updateCurrentPricesLabel(new.pricesGroupType, new)
+            }
+        }
+    }
+
+    private fun updateCurrentPricesLabel(
+        pricesGroupType: ValuesGroupType,
+        viewState: VariationsBulkUpdatePriceViewModel.ViewState
+    ) {
+        binding.currentPriceInfo.text = when (pricesGroupType) {
+            Mixed -> getString(R.string.variations_bulk_update_current_prices_mixed)
+            None -> ""
+            is Common -> {
+                with(viewState) {
+                    val price = pricesGroupType.data as? BigDecimal?
+                    if (currency != null && price != null) {
+                        getString(R.string.variations_bulk_update_current_price)
+                            .format(formatPrice(price, currency, isCurrencyPrefix))
+                    } else {
+                        ""
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun setupPriceInputField(currency: String, isCurrencyPrefix: Boolean) {
+        with(binding.price) {
+            if (isCurrencyPrefix) {
+                prefixText = currency
+            } else {
+                suffixText = currency
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceViewModel.kt
@@ -3,24 +3,79 @@ package com.woocommerce.android.ui.products.variations
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.model.ProductVariation
+import com.woocommerce.android.ui.products.ParameterRepository
+import com.woocommerce.android.ui.products.models.SiteParameters
+import com.woocommerce.android.viewmodel.LiveDataDelegate
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.navArgs
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.parcelize.Parcelize
+import org.wordpress.android.fluxc.model.WCSettingsModel
+import org.wordpress.android.fluxc.model.WCSettingsModel.CurrencyPosition.LEFT
+import org.wordpress.android.fluxc.model.WCSettingsModel.CurrencyPosition.LEFT_SPACE
 import java.math.BigDecimal
+import javax.inject.Inject
 
-class VariationsBulkUpdatePriceViewModel(savedStateHandle: SavedStateHandle): ScopedViewModel(savedStateHandle) {
+@HiltViewModel
+class VariationsBulkUpdatePriceViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    parameterRepository: ParameterRepository,
+) : ScopedViewModel(savedStateHandle) {
+
+    private val args: VariationsBulkUpdatePriceFragmentArgs by savedStateHandle.navArgs()
+    private val variationsToUpdate: List<ProductVariation> = args.priceUpdateData.variationsToUpdate
+
+    private val parameters: SiteParameters by lazy {
+        parameterRepository.getParameters("key_product_parameters", savedState)
+    }
+
+    val viewStateData = LiveDataDelegate(savedState, ViewState())
+    private var viewState by viewStateData
+
+    init {
+        val data : PriceUpdateData = args.priceUpdateData
+        viewState = viewState.copy(
+            currencyPosition = parameters.currencyFormattingParameters?.currencyPosition,
+            currency = parameters.currencySymbol,
+            pricesGroupType = data.getPriceCollection().groupType(),
+            priceType = data.priceType,
+            variationsToUpdateCount = data.variationsToUpdate.size,
+        )
+    }
+
     fun onDoneClicked() {
-
+        // TODO perform bulk update 🚀
     }
 
     fun onPriceEntered(price: BigDecimal?) {
+        viewState = viewState.copy(price = price)
+    }
 
+    @Parcelize
+    data class ViewState(
+        val currency: String? = null,
+        val price: BigDecimal? = null,
+        val priceType: PriceType? = null,
+        val pricesGroupType: ValuesGroupType? = null,
+        val variationsToUpdateCount: Int? = null,
+        private val currencyPosition: WCSettingsModel.CurrencyPosition? = null,
+    ) : Parcelable {
+        val isCurrencyPrefix: Boolean
+            get() = currencyPosition == LEFT || currencyPosition == LEFT_SPACE
     }
 
     @Parcelize
     data class PriceUpdateData(
         val variationsToUpdate: List<ProductVariation>,
         val priceType: PriceType,
-    ): Parcelable
+    ) : Parcelable
+
+    private fun PriceUpdateData.getPriceCollection() : Collection<BigDecimal?> {
+        return when(priceType) {
+            PriceType.Sale -> variationsToUpdate.map { it.salePrice }
+            PriceType.Regular -> variationsToUpdate.map { it.regularPrice }
+        }
+    }
 
     enum class PriceType {
         Regular, Sale
