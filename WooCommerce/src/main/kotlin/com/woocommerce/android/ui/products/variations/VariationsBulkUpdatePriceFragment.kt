40d38b7c17670806a0631e490b33e02099fc28da
samiuelson <samiuelson@gmail.com>
2022-05-21 22:55:00 +0200

    Perform bulk update when "done" clicked

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceFragment.kt
@@ -10,16 +10,22 @@ import com.woocommerce.android.R
 import com.woocommerce.android.databinding.FragmentVariationsBulkUpdatePriceBinding
 import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.base.UIMessageResolver
 import com.woocommerce.android.ui.products.variations.ValuesGroupType.Common
 import com.woocommerce.android.ui.products.variations.ValuesGroupType.Mixed
 import com.woocommerce.android.ui.products.variations.ValuesGroupType.None
 import com.woocommerce.android.ui.products.variations.VariationsBulkUpdatePriceViewModel.PriceType.Regular
 import com.woocommerce.android.ui.products.variations.VariationsBulkUpdatePriceViewModel.PriceType.Sale
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowSnackbar
 import dagger.hilt.android.AndroidEntryPoint
 import java.math.BigDecimal
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class VariationsBulkUpdatePriceFragment : BaseFragment(R.layout.fragment_variations_bulk_update_price) {
+    @Inject lateinit var uiMessageResolver: UIMessageResolver
+
     private val viewModel: VariationsBulkUpdatePriceViewModel by viewModels()
 
     private var _binding: FragmentVariationsBulkUpdatePriceBinding? = null
@@ -34,11 +40,21 @@ class VariationsBulkUpdatePriceFragment : BaseFragment(R.layout.fragment_variati
         }
         binding.price.showKeyboard()
         observeViewStateChanges()
+        observeEvents()
+    }
+
+    private fun observeEvents() {
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is ShowSnackbar -> uiMessageResolver.showSnack(event.message)
+                is Exit -> requireActivity().onBackPressed()
+            }
+        }
     }
 
     private fun observeViewStateChanges() {
         viewModel.viewStateData.observe(viewLifecycleOwner) { old, new ->
-            new.priceType?.takeIfNotEqualTo(old?.priceType) {
+            new.priceType.takeIfNotEqualTo(old?.priceType) {
                 requireActivity().title = when (new.priceType) {
                     Regular -> getString(R.string.variations_bulk_update_regular_price)
                     Sale -> getString(R.string.variations_bulk_update_sale_price)
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdatePriceViewModel.kt
@@ -2,13 +2,18 @@ package com.woocommerce.android.ui.products.variations
 
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.R
 import com.woocommerce.android.model.ProductVariation
 import com.woocommerce.android.ui.products.ParameterRepository
 import com.woocommerce.android.ui.products.models.SiteParameters
+import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.LiveDataDelegate
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
 import org.wordpress.android.fluxc.model.WCSettingsModel
 import org.wordpress.android.fluxc.model.WCSettingsModel.CurrencyPosition.LEFT
@@ -20,20 +25,21 @@ import javax.inject.Inject
 class VariationsBulkUpdatePriceViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     parameterRepository: ParameterRepository,
+    private val variationRepository: VariationRepository,
+    private val dispatchers: CoroutineDispatchers,
 ) : ScopedViewModel(savedStateHandle) {
-
     private val args: VariationsBulkUpdatePriceFragmentArgs by savedStateHandle.navArgs()
+    private val data: PriceUpdateData = args.priceUpdateData
     private val variationsToUpdate: List<ProductVariation> = args.priceUpdateData.variationsToUpdate
 
     private val parameters: SiteParameters by lazy {
         parameterRepository.getParameters("key_product_parameters", savedState)
     }
 
-    val viewStateData = LiveDataDelegate(savedState, ViewState())
+    val viewStateData = LiveDataDelegate(savedState, ViewState(priceType = data.priceType))
     private var viewState by viewStateData
 
     init {
-        val data : PriceUpdateData = args.priceUpdateData
         viewState = viewState.copy(
             currencyPosition = parameters.currencyFormattingParameters?.currencyPosition,
             currency = parameters.currencySymbol,
@@ -44,7 +50,26 @@ class VariationsBulkUpdatePriceViewModel @Inject constructor(
     }
 
     fun onDoneClicked() {
-        // TODO perform bulk update 🚀
+        launch(dispatchers.io) {
+            val productId = variationsToUpdate.first().remoteProductId
+            val variationsIds = variationsToUpdate.map { it.remoteVariationId }
+            // TODO: show progress bar
+            val result = when (viewState.priceType) {
+                PriceType.Regular -> variationRepository.bulkUpdateVariations(productId, variationsIds, viewState.price)
+                PriceType.Sale -> variationRepository.bulkUpdateVariations(productId, variationsIds, viewState.price)
+            }
+            val snackText = if (result) {
+                // TODO: use different text for sale price
+                R.string.variations_bulk_update_regular_prices_success
+            } else {
+                R.string.variations_bulk_update_error
+            }
+
+            withContext(dispatchers.main) {
+                triggerEvent(MultiLiveEvent.Event.ShowSnackbar(snackText))
+                if (result) triggerEvent(MultiLiveEvent.Event.Exit)
+            }
+        }
     }
 
     fun onPriceEntered(price: BigDecimal?) {
@@ -55,7 +80,7 @@ class VariationsBulkUpdatePriceViewModel @Inject constructor(
     data class ViewState(
         val currency: String? = null,
         val price: BigDecimal? = null,
-        val priceType: PriceType? = null,
+        val priceType: PriceType,
         val pricesGroupType: ValuesGroupType? = null,
         val variationsToUpdateCount: Int? = null,
         private val currencyPosition: WCSettingsModel.CurrencyPosition? = null,
@@ -70,8 +95,8 @@ class VariationsBulkUpdatePriceViewModel @Inject constructor(
         val priceType: PriceType,
     ) : Parcelable
 
-    private fun PriceUpdateData.getPriceCollection() : Collection<BigDecimal?> {
-        return when(priceType) {
+    private fun PriceUpdateData.getPriceCollection(): Collection<BigDecimal?> {
+        return when (priceType) {
             PriceType.Sale -> variationsToUpdate.map { it.salePrice }
             PriceType.Regular -> variationsToUpdate.map { it.regularPrice }
         }
