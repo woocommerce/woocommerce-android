293f300a2d3d908cc850e02054205f6995445f39
hicham <hicham.boushaba@gmail.com>
2022-05-24 18:14:37 +0100

    Add unit tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponViewModelTests.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponViewModelTests.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.coupons.edit
 
+import com.woocommerce.android.R
 import com.woocommerce.android.initSavedStateHandle
 import com.woocommerce.android.model.Coupon
 import com.woocommerce.android.ui.coupons.CouponRepository
@@ -11,15 +12,19 @@ import com.woocommerce.android.util.CurrencyFormatter
 import com.woocommerce.android.util.captureValues
 import com.woocommerce.android.util.runAndCaptureValues
 import com.woocommerce.android.viewmodel.BaseUnitTest
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowSnackbar
 import com.woocommerce.android.viewmodel.ResourceProvider
 import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.Mockito.spy
 import org.mockito.kotlin.any
+import org.mockito.kotlin.argThat
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import java.math.BigDecimal
 import java.util.Date
@@ -206,5 +211,41 @@ class EditCouponViewModelTests : BaseUnitTest() {
 
         val state = viewModel.viewState.captureValues().last()
         assertThat(state.couponDraft.isShippingFree).isTrue()
+    }
+
+    @Test
+    fun `when save button is clicked, then update coupon`() = testBlocking {
+        setup()
+
+        viewModel.onCouponCodeChanged("New code")
+        viewModel.onSaveClick()
+
+        verify(couponRepository).updateCoupon(argThat { code == "New code" })
+    }
+
+    @Test
+    fun `when coupon is updated, then show a snackbar and navigate up`() = testBlocking {
+        setup {
+            whenever(couponRepository.updateCoupon(any())).thenReturn(Result.success(Unit))
+        }
+
+        val events = viewModel.event.runAndCaptureValues {
+            viewModel.onSaveClick()
+        }.takeLast(2)
+
+        assertThat(events[0]).isEqualTo(ShowSnackbar(R.string.coupon_edit_coupon_updated))
+        assertThat(events[1]).isEqualTo(Exit)
+    }
+
+    @Test
+    fun `when coupon is fails, then show an error snackbar`() = testBlocking {
+        setup {
+            whenever(couponRepository.updateCoupon(any())).thenReturn(Result.failure(Exception()))
+        }
+
+        viewModel.onSaveClick()
+
+        val event = viewModel.event.captureValues().last()
+        assertThat(event).isEqualTo(ShowSnackbar(R.string.coupon_edit_coupon_update_failed))
     }
 }
