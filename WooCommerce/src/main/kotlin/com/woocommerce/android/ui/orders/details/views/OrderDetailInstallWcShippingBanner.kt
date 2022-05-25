edddd44a3ffb4cd0e560e5370ac02ca6687ce12f
JorgeMucientes <jorge.mucientes@automattic.com>
2022-05-25 17:09:19 +0200

    Add minor UI adjustments over banner elements

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailInstallWcShippingBanner.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailInstallWcShippingBanner.kt
@@ -2,14 +2,10 @@ package com.woocommerce.android.ui.orders.details.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
 import com.google.android.material.card.MaterialCardView
-import com.woocommerce.android.databinding.OrderDetailInstallWcShippingBannerBinding
 
 class OrderDetailInstallWcShippingBanner @JvmOverloads constructor(
     ctx: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : MaterialCardView(ctx, attrs, defStyleAttr) {
-    private val binding = OrderDetailInstallWcShippingBannerBinding.inflate(LayoutInflater.from(ctx), this)
-}
+) : MaterialCardView(ctx, attrs, defStyleAttr)
