1b5e29cf6edc41ee96f00036da35253ee3d90b23
samiuelson <samiuelson@gmail.com>
2022-05-25 15:59:15 +0200

    Remove redundant code

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdateAttrPickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationsBulkUpdateAttrPickerViewModel.kt
@@ -15,9 +15,6 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import org.wordpress.android.fluxc.model.WCSettingsModel
-import org.wordpress.android.fluxc.model.WCSettingsModel.CurrencyPosition.LEFT
-import org.wordpress.android.fluxc.model.WCSettingsModel.CurrencyPosition.LEFT_SPACE
 import javax.inject.Inject
 
 /**
@@ -37,7 +34,6 @@ class VariationsBulkUpdateAttrPickerViewModel @Inject constructor(
 
     private val _viewState = MutableStateFlow(
         ViewState(
-            currencyPosition = parameters.currencyFormattingParameters?.currencyPosition,
             currency = parameters.currencySymbol
         )
     )
@@ -56,13 +52,9 @@ class VariationsBulkUpdateAttrPickerViewModel @Inject constructor(
     }
 
     data class ViewState(
-        private val currencyPosition: WCSettingsModel.CurrencyPosition? = null,
         val currency: String? = null,
         val regularPriceGroupType: ValuesGroupType = ValuesGroupType.None,
-    ) {
-        val isCurrencyPrefix: Boolean
-            get() = currencyPosition == LEFT || currencyPosition == LEFT_SPACE
-    }
+    )
 
     data class OpenVariationsBulkUpdatePrice(
         val data: PriceUpdateData
