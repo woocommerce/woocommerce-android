1b48f5ed3b13f779eb9e76800900b79d15b2b3be
ThomazFB <thomazfcortez@gmail.com>
2022-05-25 23:04:00 -0300

    Improve ProductDetailCardBuilderTest

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/ProductDetailCardBuilderTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/ProductDetailCardBuilderTest.kt
@@ -3,7 +3,6 @@ package com.woocommerce.android.ui.products
 import com.woocommerce.android.model.Product
 import com.woocommerce.android.ui.products.addons.AddonRepository
 import com.woocommerce.android.ui.products.models.ProductProperty
-import com.woocommerce.android.ui.products.models.ProductProperty.Type.PROPERTY_GROUP
 import com.woocommerce.android.ui.products.models.ProductPropertyCard.Type.SECONDARY
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.ResourceProvider
@@ -58,13 +57,39 @@ class ProductDetailCardBuilderTest: BaseUnitTest() {
         val cards = sut.buildPropertyCards(productStub, "")
         assertThat(cards).isNotEmpty
 
-        cards.filter { it.type == SECONDARY }
-            .takeIf { it.isNotEmpty() and (it.size == 1) }
-            ?.first()?.properties?.filter { it.type == PROPERTY_GROUP }
-            ?.takeIf { it.isNotEmpty() and (it.size == 1) }
-            ?.first()?.run { this as? ProductProperty.PropertyGroup }
-            ?.properties?.toList()
-            ?.let { assertThat(it.first()).isEqualTo(Pair("Color", "3")) }
-            ?: fail("Expected a Product with a single Attribute named Color with value 3 selected")
+        cards.find { it.type == SECONDARY }
+            ?.properties?.mapNotNull { it as? ProductProperty.PropertyGroup }
+            ?.find { propertyGroup ->
+                propertyGroup.properties.toList()
+                    .find { it.first == "Color" } != null
+            }?.properties?.toList()?.let {
+                assertThat(it.first()).isEqualTo(Pair("Color", "3"))
+            } ?: fail("Expected a Product card with a single Attribute named Color with value 3 selected")
+    }
+
+    @Test
+    fun `given a product with no attribute, then ignore Attributes card`() = testBlocking {
+        productStub = ProductTestUtils.generateProduct()
+            .copy(
+                reviewsAllowed = false,
+                type = ProductType.VARIABLE.value,
+                weight = 0F,
+                length = 0F,
+                width = 0F,
+                height = 0F,
+                attributes = emptyList()
+            )
+
+        val cards = sut.buildPropertyCards(productStub, "")
+        assertThat(cards).isNotEmpty
+
+        cards.find { it.type == SECONDARY }
+            ?.properties?.mapNotNull { it as? ProductProperty.PropertyGroup }
+            ?.find { propertyGroup ->
+                propertyGroup.properties.toList()
+                    .find { it.first == "Color" } != null
+            }?.properties?.toList()?.let {
+                fail("Expected no Product card with Attributes configured")
+            }
     }
 }
