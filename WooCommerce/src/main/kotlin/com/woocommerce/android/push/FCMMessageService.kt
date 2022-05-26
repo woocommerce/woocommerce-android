e5805c627b3978d1be75058d0bf92150de83f0e8
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-05-26 20:03:22 +0200

    Invalidate device registration status when new token is generated

--- WooCommerce/src/main/kotlin/com/woocommerce/android/push/FCMMessageService.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/push/FCMMessageService.kt
@@ -9,7 +9,9 @@ import com.woocommerce.android.util.WooLog.T
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import org.wordpress.android.fluxc.store.InvalidateDeviceRegistration
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -17,10 +19,15 @@ class FCMMessageService : FirebaseMessagingService() {
     @Inject lateinit var notificationMessageHandler: NotificationMessageHandler
     @Inject lateinit var appPrefsWrapper: AppPrefsWrapper
     @Inject lateinit var registerDevice: RegisterDevice
+    @Inject lateinit var invalidateDeviceRegistration: InvalidateDeviceRegistration
+
+    private val job = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.IO + job)
 
     override fun onNewToken(newToken: String) {
         appPrefsWrapper.setFCMToken(newToken)
-        CoroutineScope(Dispatchers.IO).launch {
+        serviceScope.launch {
+            invalidateDeviceRegistration()
             registerDevice(FORCEFULLY)
         }
     }
@@ -28,5 +35,10 @@ class FCMMessageService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         WooLog.v(T.NOTIFS, "Received message from Firebase")
         notificationMessageHandler.onNewMessageReceived(message.data, applicationContext)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
