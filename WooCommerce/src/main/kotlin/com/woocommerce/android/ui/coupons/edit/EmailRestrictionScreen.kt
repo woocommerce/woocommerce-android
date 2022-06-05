66df546567dbe0822f12be02bd69c1ca1fbbd77a
Hafiz Rahman <hfzrhmn@gmail.com>
2022-06-05 17:30:25 +0700

    Add: Temporary suppress for unused viewmodel in parameter.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EmailRestrictionScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EmailRestrictionScreen.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.coupons.edit
 import androidx.compose.runtime.Composable
 
 @Composable
+@Suppress("UnusedPrivateMember")
 fun EmailRestrictionScreen(viewModel: EmailRestrictionViewModel) {
     /* TODO */
 }
