e9e366003d01ac48708a9060de13c593b2202c03
Hafiz Rahman <hfzrhmn@gmail.com>
2022-06-05 17:07:24 +0700

    Add: Back navigation from Email Restriction to Coupon Restrictions.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EmailRestrictionFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EmailRestrictionFragment.kt
@@ -9,17 +9,19 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.R
+import com.woocommerce.android.extensions.navigateBackWithResult
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.main.AppBarStatus
 import com.woocommerce.android.ui.main.MainActivity.Companion.BackPressListener
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ExitWithResult
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EmailRestrictionFragment : BaseFragment(), BackPressListener {
     companion object {
-        const val RESTRICTIONS_RESULT = "restrictions-result"
+        const val ALLOWED_EMAILS = "allowed-emails"
     }
 
     private val viewModel: EmailRestrictionViewModel by viewModels()
@@ -50,11 +52,14 @@ class EmailRestrictionFragment : BaseFragment(), BackPressListener {
         viewModel.event.observe(viewLifecycleOwner) { event ->
             when (event) {
                 is Exit -> findNavController().navigateUp()
+                is ExitWithResult<*> -> {
+                    navigateBackWithResult(ALLOWED_EMAILS, event.data as List<*>)
+                }
             }
         }
     }
 
-    override fun getFragmentTitle() = getString(R.string.coupon_restrictions_allowed_emails_placeholder)
+    override fun getFragmentTitle() = getString(R.string.coupon_restrictions_allowed_emails)
 
     override fun onRequestAllowBackPress(): Boolean {
         viewModel.onBackPressed()
