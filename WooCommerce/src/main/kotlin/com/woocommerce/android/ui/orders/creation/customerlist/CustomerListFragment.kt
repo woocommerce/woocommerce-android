5348d00c1451814e4c53478e8ae05f718546cf8c
Nick Bradbury <nick.bradbury@gmail.com>
2022-06-08 17:05:36 -0400

    Added search view

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/customerlist/CustomerListFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/customerlist/CustomerListFragment.kt
@@ -2,8 +2,12 @@ package com.woocommerce.android.ui.orders.creation.customerlist
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import com.woocommerce.android.R
@@ -13,15 +17,18 @@ import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CustomerListFragment :
-    BaseFragment(R.layout.fragment_customer_list) {
+class CustomerListFragment : BaseFragment(R.layout.fragment_customer_list) {
     private val viewModel by viewModels<CustomerListViewModel>()
+
+    private var searchMenuItem: MenuItem? = null
+    private var searchView: SearchView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
         val binding = FragmentCustomerListBinding.inflate(inflater, container, false)
 
         binding.customerComposeView.apply {
@@ -39,4 +46,15 @@ class CustomerListFragment :
     }
 
     override fun getFragmentTitle() = getString(R.string.order_creation_customer_search_title)
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_search, menu)
+
+        searchMenuItem = menu.findItem(R.id.menu_search)
+        searchView = searchMenuItem?.actionView as SearchView?
+        searchView?.queryHint = getString(R.string.order_creation_customer_search_hint)
+        searchView?.setIconifiedByDefault(false)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 }
