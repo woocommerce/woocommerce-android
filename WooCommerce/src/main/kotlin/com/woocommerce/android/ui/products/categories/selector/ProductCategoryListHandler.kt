f76abd00dbccb79503978f7580f024338df86bb7
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-06-08 13:02:37 +0100

    Add implementation for search categories

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/categories/selector/ProductCategoryListHandler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/categories/selector/ProductCategoryListHandler.kt
@@ -1,7 +1,10 @@
 package com.woocommerce.android.ui.products.categories.selector
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
@@ -17,31 +20,65 @@ class ProductCategoryListHandler @Inject constructor(
     private var offset = 0
     private var canLoadMore = true
 
-    val categories: Flow<List<ProductCategoryTreeItem>> = repository.observeCategories()
-        .map { it.convertToTree() }
+    private val searchQuery = MutableStateFlow("")
+    private val searchResults = MutableStateFlow(emptyList<ProductCategoryTreeItem>())
+
+    val categories: Flow<List<ProductCategoryTreeItem>> = searchQuery.flatMapLatest {
+        if (it.isEmpty()) {
+            repository.observeCategories()
+                .map { it.convertToTree() }
+        } else {
+            searchResults
+        }
+    }
 
     suspend fun fetchCategories(
-        forceRefresh: Boolean = false
+        forceRefresh: Boolean = false,
+        searchQuery: String = ""
     ): Result<Unit> = mutex.withLock {
         // Reset pagination attributes
         offset = 0
         canLoadMore = true
 
-        return if (forceRefresh) {
-            loadCategories()
+        this.searchQuery.value = searchQuery
+        return@withLock if (searchQuery.isEmpty()) {
+            if (forceRefresh) {
+                loadCategories()
+            } else {
+                Result.success(Unit)
+            }
         } else {
-            Result.success(Unit)
+            searchResults.value = emptyList()
+            searchCategories()
         }
     }
 
     suspend fun loadMore(): Result<Unit> = mutex.withLock {
         if (!canLoadMore) return@withLock Result.success(Unit)
-        return loadCategories()
+        return if (searchQuery.value.isEmpty()) {
+            loadCategories()
+        } else {
+            searchCategories()
+        }
     }
 
     private suspend fun loadCategories(): Result<Unit> {
         return repository.fetchCategories(offset, PAGE_SIZE).onSuccess {
             canLoadMore = it
+            offset += PAGE_SIZE
+        }.map { }
+    }
+
+    private suspend fun searchCategories(): Result<Unit> {
+        return repository.searchCategories(
+            searchQuery = searchQuery.value,
+            offset = offset,
+            pageSize = PAGE_SIZE
+        ).onSuccess { result ->
+            // Return results as flat tree
+            val mappedResults = result.productCategories.map { ProductCategoryTreeItem(it, emptyList()) }
+            searchResults.update { it + mappedResults }
+            canLoadMore = result.canLoadMore
             offset += PAGE_SIZE
         }.map { }
     }
