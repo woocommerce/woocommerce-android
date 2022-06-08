a45808273d9d1691167139b56248484b23c32fdc
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-06-08 10:15:06 -0400

    Change formatting to trigger another build

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/hub/CardReaderHubFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/cardreader/hub/CardReaderHubFragment.kt
@@ -18,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class CardReaderHubFragment : BaseFragment(R.layout.fragment_card_reader_hub) {
     override fun getFragmentTitle() = resources.getString(R.string.card_reader_onboarding_title)
     val viewModel: CardReaderHubViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
