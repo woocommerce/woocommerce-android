a5d3c726cbfa5dcca6774bbdc447ee7d4de79bc7
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-06-08 12:18:05 +0100

    Add a new SearchView component

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/component/TextFields.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/component/TextFields.kt
@@ -1,20 +1,32 @@
 package com.woocommerce.android.ui.compose.component
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
@@ -160,6 +172,54 @@ private fun WCOutlinedTextFieldLayout(
     }
 }
 
+@Composable
+fun WCSearchView(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    hint: String = ""
+) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .defaultMinSize(minHeight = dimensionResource(id = R.dimen.major_250))
+            .background(
+                TextFieldDefaults
+                    .textFieldColors()
+                    .backgroundColor(enabled = true).value,
+                RoundedCornerShape(dimensionResource(id = R.dimen.minor_100))
+            ),
+        decorationBox = { innerTextField ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(horizontal = dimensionResource(id = R.dimen.minor_100))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = colorResource(id = R.color.color_on_surface_medium)
+                )
+
+                Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.minor_100)))
+
+                Box {
+                    if (value.isEmpty()) {
+                        Text(
+                            text = hint,
+                            style = MaterialTheme.typography.subtitle1,
+                            color = colorResource(id = R.color.color_on_surface_medium)
+                        )
+                    }
+
+                    innerTextField()
+                }
+            }
+        }
+    )
+}
+
 @Preview(name = "Light mode")
 @Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
@@ -176,6 +236,17 @@ private fun WCOutlinedTextFieldPreview() {
                 onValueChange = {},
                 helperText = "Helper Text",
                 isError = true
+            )
+            WCSearchView(
+                value = "",
+                onValueChange = {},
+                hint = "Search",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = dimensionResource(id = R.dimen.major_100),
+                        vertical = dimensionResource(id = R.dimen.minor_100)
+                    )
             )
         }
     }
