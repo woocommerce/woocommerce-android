3f5f957105d555bfb3c44334ea958874eef5aeef
ThomazFB <thomazfcortez@gmail.com>
2022-06-20 15:20:03 -0300

    Improve OrderedAddonScreen column creation to a static one

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/order/OrderedAddonScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/addons/order/OrderedAddonScreen.kt
@@ -2,9 +2,6 @@ package com.woocommerce.android.ui.products.addons.order
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -36,7 +33,9 @@ fun OrderedAddonScreen(
     state: ViewState
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.surface)
     ) {
         OrderedAddonList(orderedAddons = orderedAddons)
     }
@@ -46,24 +45,17 @@ fun OrderedAddonScreen(
 private fun OrderedAddonList(
     orderedAddons: List<Addon>
 ) {
-    val listState = rememberLazyListState()
-    LazyColumn(
-        state = listState,
-        modifier = Modifier
-            .background(color = MaterialTheme.colors.surface)
-    ) {
-        itemsIndexed(orderedAddons) { _, addon ->
-            Column(
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_50)),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        horizontal = dimensionResource(id = R.dimen.major_100),
-                        vertical = dimensionResource(id = R.dimen.minor_100)
-                    )
-            ) {
-                OrderedAddonItem(orderedAddon = addon)
-            }
+    orderedAddons.forEach { addon ->
+        Column(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_50)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = dimensionResource(id = R.dimen.major_100),
+                    vertical = dimensionResource(id = R.dimen.minor_100)
+                )
+        ) {
+            OrderedAddonItem(orderedAddon = addon)
         }
     }
 }
