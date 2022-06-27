2b3618ba07697109a7a91ce50d11b8ff63be0244
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-06-27 17:20:29 +0100

    Add animation for the loading indicator

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/shipping/InstallWCShippingFlow.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/shipping/InstallWCShippingFlow.kt
@@ -10,11 +10,16 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.ExperimentalTransitionApi
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -28,7 +33,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -38,9 +42,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
@@ -103,16 +117,13 @@ private fun AnimatedVisibilityScope.PreInstallationContent(viewState: Installati
                 )
         ) {
             SpacerWithMinHeight(1f, dimensionResource(id = R.dimen.major_100))
-            Box(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .border(
-                        width = dimensionResource(id = R.dimen.major_75),
-                        color = colorResource(id = R.color.woo_purple_20),
-                        shape = CircleShape
-                    )
-                    .size(dimensionResource(id = R.dimen.image_major_120))
-            ) {
+            Box {
+                InstallationLoadingIndicator(
+                    showLoadingIndicator = false,
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_major_120))
+                )
+
                 Icon(
                     painter = painterResource(id = R.drawable.ic_arrow_forward_rounded),
                     contentDescription = null,
@@ -178,25 +189,52 @@ private fun AnimatedVisibilityScope.InstallationContent(viewState: InstallationO
         // fill equivalent space as the cross-icon
         Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_300)))
         SpacerWithMinHeight(1f, dimensionResource(id = R.dimen.major_100))
-        Box(
-            modifier = Modifier
-                .clip(CircleShape)
-                .border(
-                    width = dimensionResource(id = R.dimen.major_75),
-                    color = colorResource(id = R.color.woo_purple_20),
-                    shape = CircleShape
-                )
-                .size(dimensionResource(id = R.dimen.image_major_120))
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_arrow_forward_rounded),
-                contentDescription = null,
-                tint = colorResource(id = R.color.woo_purple_50),
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .size(dimensionResource(id = R.dimen.image_major_64))
+
+        Box(modifier = Modifier.size(dimensionResource(id = R.dimen.image_major_120))) {
+            var isCursorVisible by remember { mutableStateOf(true) }
+            var isShowingLoadingIndicator by remember { mutableStateOf(false) }
+
+            LaunchedEffect(Unit) {
+                isCursorVisible = false
+            }
+            val alpha by animateFloatAsState(
+                targetValue = if (isCursorVisible) 1f else 0f,
+                animationSpec = tween(1000, delayMillis = 1000),
+                finishedListener = {
+                    isShowingLoadingIndicator = true
+                }
             )
+            val rotation by animateFloatAsState(
+                targetValue = if (isCursorVisible) 0f else 180f,
+                animationSpec = keyframes {
+                    if (isCursorVisible) return@keyframes
+                    durationMillis = 2000
+                    0f at 1000
+                    -20f at 1100
+                    180f at 2000
+                }
+            )
+
+            Box {
+                InstallationLoadingIndicator(
+                    showLoadingIndicator = isShowingLoadingIndicator,
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_major_120))
+                )
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_forward_rounded),
+                    contentDescription = null,
+                    tint = colorResource(id = R.color.woo_purple_50),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .alpha(alpha)
+                        .rotate(rotation)
+                        .size(dimensionResource(id = R.dimen.image_major_64))
+                )
+            }
         }
+
         Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_150)))
         MainContent(viewState)
         Box(modifier = Modifier.weight(1.5f)) {
@@ -282,6 +320,51 @@ private fun InstallationInfoLink(onClick: () -> Unit, modifier: Modifier = Modif
 private fun ColumnScope.SpacerWithMinHeight(weight: Float, minHeight: Dp) {
     Spacer(modifier = Modifier.height(minHeight))
     Spacer(modifier = Modifier.weight(weight))
+}
+
+@Composable
+private fun InstallationLoadingIndicator(showLoadingIndicator: Boolean, modifier: Modifier = Modifier) {
+    val stroke = with(LocalDensity.current) {
+        Stroke(width = dimensionResource(id = R.dimen.major_75).toPx(), cap = StrokeCap.Round)
+    }
+
+    val circleColor = colorResource(id = R.color.woo_purple_20)
+    val progressColor = colorResource(id = R.color.woo_purple_50)
+
+    val startAngle by if (showLoadingIndicator) {
+        val transition = rememberInfiniteTransition()
+
+        transition.animateFloat(
+            -90f,
+            270f,
+            infiniteRepeatable(
+                animation = tween(1332, easing = LinearEasing)
+            )
+        )
+    } else {
+        remember { mutableStateOf(-90f) }
+    }
+
+    Canvas(modifier) {
+        val size = size.width - stroke.width
+
+        drawCircle(
+            color = circleColor,
+            radius = (size) / 2,
+            style = stroke
+        )
+        if (showLoadingIndicator) {
+            drawArc(
+                color = progressColor,
+                startAngle = startAngle,
+                sweepAngle = 30f,
+                useCenter = false,
+                size = Size(size, size),
+                topLeft = Offset(stroke.width / 2, stroke.width / 2),
+                style = stroke
+            )
+        }
+    }
 }
 
 @Preview
