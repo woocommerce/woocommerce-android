11939168c2a946058d6dba6d7a8870f3ac02e5ff
Ondrej Ruttkay <ondrej@automattic.com>
2022-06-28 16:35:35 +0200

    Enable the M2 feature flag

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -22,11 +22,11 @@ enum class FeatureFlag {
             DB_DOWNGRADE -> {
                 PackageUtils.isDebugBuild() || context != null && PackageUtils.isBetaBuild(context)
             }
+            COUPONS_M2,
             JETPACK_CP,
             IN_PERSON_PAYMENTS_CANADA -> true
             ANALYTICS_HUB,
             MORE_MENU_INBOX,
-            COUPONS_M2,
             WC_SHIPPING_BANNER,
             IPP_SELECT_PAYMENT_GATEWAY,
             UNIFIED_ORDER_EDITING -> PackageUtils.isDebugBuild()
