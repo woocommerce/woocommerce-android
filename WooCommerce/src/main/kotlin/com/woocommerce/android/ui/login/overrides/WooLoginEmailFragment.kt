99348eb17df985128ed0452d1e08f8aec1da8e7e
JorgeMucientes <jorge.mucientes@automattic.com>
2022-07-06 17:32:43 +0200

    Handle clicks over new link "What is WordPress.com"

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/overrides/WooLoginEmailFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/overrides/WooLoginEmailFragment.kt
@@ -1,8 +1,10 @@
 package com.woocommerce.android.ui.login.overrides
 
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.annotation.LayoutRes
 import com.woocommerce.android.R
+import com.woocommerce.android.util.ChromeCustomTabUtils
 import org.wordpress.android.login.LoginEmailFragment
 
 class WooLoginEmailFragment : LoginEmailFragment() {
@@ -12,5 +14,8 @@ class WooLoginEmailFragment : LoginEmailFragment() {
 
     override fun setupContent(rootView: ViewGroup) {
         super.setupContent(rootView)
+        rootView.findViewById<Button>(R.id.login_what_is_wordpress).setOnClickListener {
+            ChromeCustomTabUtils.launchUrl(requireContext(), "https://woocommerce.com/")
+        }
     }
 }
