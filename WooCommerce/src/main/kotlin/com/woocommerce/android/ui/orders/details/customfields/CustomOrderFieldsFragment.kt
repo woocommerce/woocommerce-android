7edc80728229ffb16e2bafe59383b93d4dacfa33
Nick Bradbury <nick.bradbury@gmail.com>
2022-07-12 12:37:22 -0400

    Added order fields fragment

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/customfields/CustomOrderFieldsFragment.kt
@@ -0,0 +1,33 @@
+package com.woocommerce.android.ui.orders.details.customfields
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.orders.details.OrderDetailViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CustomOrderFieldsFragment : BaseFragment() {
+    private val viewModel by viewModels<OrderDetailViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WooThemeWithBackground {
+                    CustomOrderFieldsScreen(viewModel)
+                }
+            }
+        }
+    }
+}
