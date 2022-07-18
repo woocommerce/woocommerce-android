963d7c4a811fce23a2f62d2b6879fceef518b41f
Anirudh Uppunda <abhat38@gmail.com>
2022-07-18 09:26:25 +0530

    Add contract methods required for banner in MainSettingsContract.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsContract.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsContract.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.prefs
 
+import androidx.lifecycle.MutableLiveData
 import com.woocommerce.android.model.FeatureAnnouncement
 import com.woocommerce.android.ui.base.BasePresenter
 import com.woocommerce.android.ui.base.BaseView
@@ -11,11 +12,21 @@ interface MainSettingsContract {
         fun hasMultipleStores(): Boolean
         fun setupAnnouncementOption()
         fun setupJetpackInstallOption()
+        fun onCtaClicked()
+        fun onDismissClicked()
+        fun onRemindLaterClicked(currentTimeInMillis: Long)
+        fun onDontShowAgainClicked()
+        fun canShowCardReaderUpsellBanner(currentTimeInMillis: Long): Boolean
+        val shouldShowUpsellCardReaderDismissDialog: MutableLiveData<Boolean>
     }
 
     interface View : BaseView<Presenter> {
         fun showDeviceAppNotificationSettings()
         fun showLatestAnnouncementOption(announcement: FeatureAnnouncement)
         fun handleJetpackInstallOption(isJetpackCPSite: Boolean)
+        fun dismissUpsellCardReaderBanner()
+        fun dismissUpsellCardReaderBannerViaRemindLater()
+        fun dismissUpsellCardReaderBannerViaDontShowAgain()
+        fun openPurchaseCardReaderLink(url: String)
     }
 }
