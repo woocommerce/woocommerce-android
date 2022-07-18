a8162189fed0914cf9859417e659a4ca775d5f3c
Anirudh Uppunda <abhat38@gmail.com>
2022-07-18 09:55:34 +0530

    refactor MainSettingsPresenter.kt to make use of BannerDisplayEligibilityChecker class

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsPresenter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/MainSettingsPresenter.kt
@@ -1,22 +1,17 @@
 package com.woocommerce.android.ui.prefs
 
 import androidx.lifecycle.MutableLiveData
-import com.woocommerce.android.AppPrefsWrapper
-import com.woocommerce.android.AppUrls
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.compose.component.banner.BannerDisplayEligibilityChecker
 import com.woocommerce.android.ui.whatsnew.FeatureAnnouncementRepository
 import com.woocommerce.android.util.BuildConfigWrapper
-import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.util.StringUtils
-import com.woocommerce.android.util.WooLog
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.fluxc.store.WooCommerceStore
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class MainSettingsPresenter @Inject constructor(
@@ -25,8 +20,7 @@ class MainSettingsPresenter @Inject constructor(
     private val wooCommerceStore: WooCommerceStore,
     private val featureAnnouncementRepository: FeatureAnnouncementRepository,
     private val buildConfigWrapper: BuildConfigWrapper,
-    private val appPrefsWrapper: AppPrefsWrapper,
-    private val dispatchers: CoroutineDispatchers,
+    private val bannerDisplayEligibilityChecker: BannerDisplayEligibilityChecker,
 ) : MainSettingsContract.Presenter {
     private var appSettingsFragmentView: MainSettingsContract.View? = null
 
@@ -79,12 +73,7 @@ class MainSettingsPresenter @Inject constructor(
 
     override fun onCtaClicked() {
         coroutineScope.launch {
-            val countryCode = getStoreCountryCode()
-            withContext(dispatchers.main) {
-                appSettingsFragmentView?.openPurchaseCardReaderLink(
-                    "${AppUrls.WOOCOMMERCE_PURCHASE_CARD_READER_IN_COUNTRY}$countryCode"
-                )
-            }
+            bannerDisplayEligibilityChecker.getPurchaseCardReaderUrl()
         }
     }
 
@@ -95,80 +84,24 @@ class MainSettingsPresenter @Inject constructor(
 
     override fun onRemindLaterClicked(currentTimeInMillis: Long) {
         shouldShowUpsellCardReaderDismissDialog.value = false
-        storeRemindLaterTimeStamp(currentTimeInMillis)
+        bannerDisplayEligibilityChecker.onRemindLaterClicked(currentTimeInMillis)
         appSettingsFragmentView?.dismissUpsellCardReaderBannerViaRemindLater()
     }
 
     override fun onDontShowAgainClicked() {
         shouldShowUpsellCardReaderDismissDialog.value = false
-        storeDismissBannerForever()
+        bannerDisplayEligibilityChecker.onDontShowAgainClicked()
         appSettingsFragmentView?.dismissUpsellCardReaderBannerViaDontShowAgain()
     }
 
-    private suspend fun getStoreCountryCode(): String? {
-        return withContext(dispatchers.io) {
-            wooCommerceStore.getStoreCountryCode(selectedSite.get()) ?: null.also {
-                WooLog.e(WooLog.T.CARD_READER, "Store's country code not found.")
-            }
-        }
-    }
-
-    private fun storeRemindLaterTimeStamp(currentTimeInMillis: Long) {
-        val site = selectedSite.get()
-        appPrefsWrapper.setCardReaderUpsellBannerRemindMeLater(
-            lastDialogDismissedInMillis = currentTimeInMillis,
-            localSiteId = site.id,
-            remoteSiteId = site.siteId,
-            selfHostedSiteId = site.selfHostedSiteId
-        )
-    }
-
-    private fun storeDismissBannerForever() {
-        val site = selectedSite.get()
-        appPrefsWrapper.setCardReaderUpsellBannerDismissed(
-            isDismissed = true,
-            localSiteId = site.id,
-            remoteSiteId = site.siteId,
-            selfHostedSiteId = site.selfHostedSiteId
-        )
-    }
-
-    private fun isCardReaderUpsellBannerDismissedForever(): Boolean {
-        val site = selectedSite.get()
-        return appPrefsWrapper.isCardReaderUpsellBannerDismissedForever(
-            localSiteId = site.id,
-            remoteSiteId = site.siteId,
-            selfHostedSiteId = site.selfHostedSiteId
-        )
-    }
-
-    private fun getCardReaderUpsellBannerLastDismissed(): Long {
-        val site = selectedSite.get()
-        return appPrefsWrapper.getCardReaderUpsellBannerLastDismissed(
-            localSiteId = site.id,
-            remoteSiteId = site.siteId,
-            selfHostedSiteId = site.selfHostedSiteId
-        )
-    }
-
-    fun isLastDialogDismissedMoreThan14DaysAgo(currentTimeInMillis: Long): Boolean {
-        val timeDifference = currentTimeInMillis - getCardReaderUpsellBannerLastDismissed()
-        return TimeUnit.MILLISECONDS.toDays(timeDifference) > CARD_READER_UPSELL_BANNER_REMIND_THRESHOLD_IN_DAYS
-    }
-
-    private fun hasTheMerchantDismissedBannerViaRemindMeLater(): Boolean {
-        return getCardReaderUpsellBannerLastDismissed() != 0L
-    }
-
     override fun canShowCardReaderUpsellBanner(currentTimeInMillis: Long): Boolean {
-        return !isCardReaderUpsellBannerDismissedForever() &&
-            (
-                !hasTheMerchantDismissedBannerViaRemindMeLater() || hasTheMerchantDismissedBannerViaRemindMeLater() &&
-                    isLastDialogDismissedMoreThan14DaysAgo(currentTimeInMillis)
-                )
-    }
-
-    companion object {
-        private const val CARD_READER_UPSELL_BANNER_REMIND_THRESHOLD_IN_DAYS = 14
+        with(bannerDisplayEligibilityChecker) {
+            return !isCardReaderUpsellBannerDismissedForever() &&
+                (
+                    !hasTheMerchantDismissedBannerViaRemindMeLater() ||
+                        hasTheMerchantDismissedBannerViaRemindMeLater() &&
+                            isLastDialogDismissedMoreThan14DaysAgo(currentTimeInMillis)
+                    )
+        }
     }
 }
