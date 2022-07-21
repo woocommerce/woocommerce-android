ba357cadf96cb5469a92f49fa217225c86f20906
Nick Bradbury <nick.bradbury@gmail.com>
2022-07-21 13:53:46 -0400

    Fixed FeatureFlag

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -14,7 +14,8 @@ enum class FeatureFlag {
     COUPONS_M2,
     WC_SHIPPING_BANNER,
     UNIFIED_ORDER_EDITING,
-    ORDER_CREATION_CUSTOMER_SEARCH;
+    ORDER_CREATION_CUSTOMER_SEARCH,
+    PRE_LOGIN_NOTIFICATIONS;
 
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
@@ -24,11 +25,12 @@ enum class FeatureFlag {
             COUPONS_M2,
             JETPACK_CP,
             IN_PERSON_PAYMENTS_CANADA,
+            ORDER_CREATION_CUSTOMER_SEARCH,
             UNIFIED_ORDER_EDITING -> true
             ANALYTICS_HUB,
             MORE_MENU_INBOX,
             WC_SHIPPING_BANNER,
-            ORDER_CREATION_CUSTOMER_SEARCH -> PackageUtils.isDebugBuild()
+            PRE_LOGIN_NOTIFICATIONS -> PackageUtils.isDebugBuild()
         }
     }
 }
