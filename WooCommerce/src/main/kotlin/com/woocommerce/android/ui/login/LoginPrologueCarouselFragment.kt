d669728fc8ea6b48b6b15d6897e928b736081732
Ondrej Ruttkay <ondrej@automattic.com>
2022-07-21 15:47:29 +0200

    Add carousel event tracking

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueCarouselFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueCarouselFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsEvent.LOGIN_ONBOARDING_NEXT_BUTTON_TAPPED
+import com.woocommerce.android.analytics.AnalyticsEvent.LOGIN_ONBOARDING_SHOWN
+import com.woocommerce.android.analytics.AnalyticsEvent.LOGIN_ONBOARDING_SKIP_BUTTON_TAPPED
 import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.databinding.FragmentLoginPrologueCarouselBinding
 import com.woocommerce.android.ui.login.UnifiedLoginTracker.Flow
 import com.woocommerce.android.ui.login.UnifiedLoginTracker.Step
@@ -27,6 +31,7 @@ class LoginPrologueCarouselFragment : Fragment(R.layout.fragment_login_prologue_
     }
 
     @Inject lateinit var unifiedLoginTracker: UnifiedLoginTracker
+    @Inject lateinit var analyticsTrackerWrapper: AnalyticsTrackerWrapper
     private var prologueCarouselListener: PrologueCarouselListener? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,13 +41,22 @@ class LoginPrologueCarouselFragment : Fragment(R.layout.fragment_login_prologue_
         binding.buttonNext.setOnClickListener {
             if (binding.viewPager.currentItem == adapter.itemCount - 1) {
                 prologueCarouselListener?.onCarouselFinished()
+                analyticsTrackerWrapper.track(
+                    LOGIN_ONBOARDING_NEXT_BUTTON_TAPPED,
+                    mapOf(Pair(AnalyticsTracker.VALUE_LOGIN_ONBOARDING_IS_FINAL_PAGE, true))
+                )
             } else {
                 binding.viewPager.setCurrentItem(binding.viewPager.currentItem + 1, true)
+                analyticsTrackerWrapper.track(
+                    LOGIN_ONBOARDING_NEXT_BUTTON_TAPPED,
+                    mapOf(Pair(AnalyticsTracker.VALUE_LOGIN_ONBOARDING_IS_FINAL_PAGE, false))
+                )
             }
         }
 
         binding.buttonSkip.setOnClickListener {
             prologueCarouselListener?.onCarouselFinished()
+            analyticsTrackerWrapper.track(LOGIN_ONBOARDING_SKIP_BUTTON_TAPPED)
         }
 
         binding.viewPager.adapter = adapter
@@ -50,6 +64,7 @@ class LoginPrologueCarouselFragment : Fragment(R.layout.fragment_login_prologue_
 
         if (savedInstanceState == null) {
             unifiedLoginTracker.track(Flow.PROLOGUE, Step.PROLOGUE_CAROUSEL)
+            analyticsTrackerWrapper.track(LOGIN_ONBOARDING_SHOWN)
         }
     }
 
