dee1d32e63068c3d46f4668e95df4e20562e2956
Ondrej Ruttkay <ruttkay@gmail.com>
2022-07-25 12:00:50 +0200

    Add site credentials experiment remote config value management

--- WooCommerce/src/main/kotlin/com/woocommerce/android/config/DataStoreRemoteConfigRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/config/DataStoreRemoteConfigRepository.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.config
 
+import androidx.compose.ui.text.toUpperCase
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -7,6 +8,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import com.woocommerce.android.datastore.DataStoreQualifier
 import com.woocommerce.android.datastore.DataStoreType
 import com.woocommerce.android.experiment.PrologueVariant
+import com.woocommerce.android.experiment.SiteLoginExperiment.SiteLoginVariant
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -16,6 +18,7 @@ class DataStoreRemoteConfigRepository @Inject constructor(
 ) : RemoteConfigRepository {
     companion object {
         const val PROLOGUE_VARIANT_KEY = "prologue_variant_key"
+        const val SITE_CREDENTIALS_EXPERIMENT_VARIANT_KEY = "site_credentials_experiment_variant_key"
     }
 
     override fun observePrologueVariant(): Flow<PrologueVariant> {
@@ -28,7 +31,22 @@ class DataStoreRemoteConfigRepository @Inject constructor(
 
     override suspend fun updatePrologueVariantValue(variantValue: String) {
         dataStore.edit { trackerPreferences ->
-            trackerPreferences[stringPreferencesKey(PROLOGUE_VARIANT_KEY)] = variantValue
+            trackerPreferences[stringPreferencesKey(PROLOGUE_VARIANT_KEY)] = variantValue.uppercase()
+        }
+    }
+
+    override fun observeSiteLoginVariant(): Flow<SiteLoginVariant> {
+        return dataStore.data.map { preferences ->
+            SiteLoginVariant.valueOf(
+                preferences[stringPreferencesKey(SITE_CREDENTIALS_EXPERIMENT_VARIANT_KEY)]
+                    ?: SiteLoginVariant.EMAIL_LOGIN.toString()
+            )
+        }
+    }
+
+    override suspend fun updateSiteLoginVariantValue(variantValue: String) {
+        dataStore.edit { trackerPreferences ->
+            trackerPreferences[stringPreferencesKey(SITE_CREDENTIALS_EXPERIMENT_VARIANT_KEY)] = variantValue.uppercase()
         }
     }
 }
