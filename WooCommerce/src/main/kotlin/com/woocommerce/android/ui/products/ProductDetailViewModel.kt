9362c4ad31e4886f640237bb6df0433ebc26e6e8
Nick Bradbury <nick.bradbury@gmail.com>
2022-07-28 08:24:41 -0400

    Fixed wildcard import

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailViewModel.kt
@@ -65,7 +65,6 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collectLatest
@@ -290,13 +289,6 @@ class ProductDetailViewModel @Inject constructor(
             initializeStoredProductAfterRestoration()
         }
         observeImageUploadEvents()
-        // TODO remove
-        launch {
-            delay(1000L)
-            withContext(dispatchers.main) {
-                checkLinkedProductPromo()
-            }
-        }
     }
 
     private fun initializeViewState() {
@@ -1628,6 +1620,7 @@ class ProductDetailViewModel @Inject constructor(
     }
 
     fun onLinkedProductPromoClicked() {
+        // TODO analytics
         triggerEvent(ProductNavigationTarget.ViewLinkedProducts(getRemoteProductId()))
     }
 
