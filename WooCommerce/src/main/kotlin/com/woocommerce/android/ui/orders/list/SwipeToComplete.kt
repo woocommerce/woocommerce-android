c4b70afa217c09ed8028b1034e7f7d4cfd50801f
Alejo <alejandro.torres.veiga@automattic.com>
2022-07-29 10:17:06 -0300

    disable right swiping

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/SwipeToComplete.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/SwipeToComplete.kt
@@ -21,7 +21,7 @@ class SwipeToComplete(
     private val listener: OnSwipeListener
 ) : ItemTouchHelper.SimpleCallback(
     0,
-    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ItemTouchHelper.LEFT
 ) {
     companion object {
         private const val NO_SWIPE_ABLE_SCREEN_PERCENT = 0.10
