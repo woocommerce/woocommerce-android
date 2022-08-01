6e90eeab070c18c547948a3d7aaa7dfa24f2180b
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-08-01 17:25:09 +0300

    Switch to a coroutine for delaying the full-screen change
    This makes sure that when the view is not attached anymore, the coroutine will be cancelled, and the app won't crash.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductImageViewerFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductImageViewerFragment.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -31,6 +32,8 @@ import com.woocommerce.android.ui.main.MainActivity.Companion.BackPressListener
 import com.woocommerce.android.ui.products.ImageViewerFragment.Companion.ImageViewerListener
 import com.woocommerce.android.util.WooAnimUtils
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -103,10 +106,11 @@ class ProductImageViewerFragment :
         // which would prevent the previous fragment's view from getting destroyed, this causes an issue where the
         // Toolbar doesn't get restored when navigating back.
         // This seems like a bug in the fragment library.
-        view.postDelayed({
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(500)
             @Suppress("DEPRECATION")
             requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        }, 500)
+        }
     }
 
     override fun onDestroyView() {
