8f79e09be98419b98364d9d4c5067fe7b9993030
Ondrej Ruttkay <ruttkay@gmail.com>
2022-08-01 16:39:25 +0200

    Add tracking events

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/ExperimentTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/ExperimentTracker.kt
@@ -6,6 +6,7 @@ interface ExperimentTracker {
     companion object {
         const val PROLOGUE_EXPERIMENT_ELIGIBLE_EVENT = "prologue_carousel_displayed"
         const val SITE_CREDENTIALS_EXPERIMENT_ELIGIBLE_EVENT = "site_credentials_experiment_eligible"
+        const val MAGIC_LINK_SENT_EXPERIMENT_ELIGIBLE_EVENT = "magic_link_sent_experiment_eligible"
         const val LOGIN_SUCCESSFUL_EVENT = "login_successful"
     }
 
