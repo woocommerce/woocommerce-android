495dbe7b8b3b4e047cb203ea1d469f953233037c
ThomazFB <thomazfcortez@gmail.com>
2022-08-02 21:49:22 -0300

    Introduce WaitingTimeTracker States

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/WaitingTimeTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/WaitingTimeTracker.kt
@@ -1,4 +1,10 @@
 package com.woocommerce.android.analytics
 
 class WaitingTimeTracker {
+
+    sealed class State {
+        object Idle : State()
+        object Waiting : State()
+        object Done : State()
+    }
 }
