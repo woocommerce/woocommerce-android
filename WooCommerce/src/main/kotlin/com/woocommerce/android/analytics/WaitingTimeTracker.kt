ef8422d9bcb0fac497d3fef04631a99239fb0351
ThomazFB <thomazfcortez@gmail.com>
2022-08-02 21:51:02 -0300

    Introduce operation interface for WaitingTimeTracker

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/WaitingTimeTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/WaitingTimeTracker.kt
@@ -1,6 +1,15 @@
 package com.woocommerce.android.analytics
 
 class WaitingTimeTracker {
+    private var state: State = State.Idle
+
+    fun onWaitingStarted() {
+
+    }
+
+    fun onWaitingEnded() {
+
+    }
 
     sealed class State {
         object Idle : State()
