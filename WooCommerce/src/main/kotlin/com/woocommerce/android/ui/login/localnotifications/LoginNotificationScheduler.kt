a7ddea193390a8d8d8544695b3bc3e1ab9a9998c
JorgeMucientes <jorge.mucientes@automattic.com>
2022-08-05 09:00:09 +0200

    Switch back to 24h notification delay after testing has been done

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/localnotifications/LoginNotificationScheduler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/localnotifications/LoginNotificationScheduler.kt
@@ -20,7 +20,7 @@ class LoginNotificationScheduler @Inject constructor(
 ) {
     companion object {
         const val LOGIN_NOTIFICATION_TYPE_KEY = "Notification-type"
-        const val NOTIFICATION_TEST_DELAY_IN_HOURS = 5L
+        const val NOTIFICATION_TEST_DELAY_IN_HOURS = 24L
         const val LOGIN_HELP_NOTIFICATION_ID = 987612345
         const val LOGIN_HELP_NOTIFICATION_TAG = "login-help-notification"
     }
@@ -44,7 +44,7 @@ class LoginNotificationScheduler @Inject constructor(
             val workRequest: WorkRequest =
                 OneTimeWorkRequestBuilder<LoginHelpNotificationWorker>()
                     .setInputData(notificationData)
-                    .setInitialDelay(NOTIFICATION_TEST_DELAY_IN_HOURS, TimeUnit.SECONDS)
+                    .setInitialDelay(NOTIFICATION_TEST_DELAY_IN_HOURS, TimeUnit.HOURS)
                     .build()
 
             prefsWrapper.setPreLoginNotificationWorkRequestId(workRequest.id.toString())
