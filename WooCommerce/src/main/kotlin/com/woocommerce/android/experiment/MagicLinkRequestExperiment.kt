22b2c37d425a13de4140bbd087b66bfaf6259cd0
Ondrej Ruttkay <ruttkay@gmail.com>
2022-08-08 13:04:20 +0200

    Add another variant to the magic link experiment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/experiment/MagicLinkRequestExperiment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/experiment/MagicLinkRequestExperiment.kt
@@ -5,38 +5,42 @@ import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.analytics.ExperimentTracker
 import com.woocommerce.android.config.RemoteConfigRepository
-import com.woocommerce.android.experiment.AutomaticMagicLinkRequestExperiment.AutomaticMagicLinkRequestVariant.AUTOMATIC
-import com.woocommerce.android.experiment.AutomaticMagicLinkRequestExperiment.AutomaticMagicLinkRequestVariant.CONTROL
+import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.AUTOMATIC
+import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.CONTROL
+import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.ENHANCED
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
-class AutomaticMagicLinkRequestExperiment @Inject constructor(
+class MagicLinkRequestExperiment @Inject constructor(
     private val experimentTracker: ExperimentTracker,
     private val remoteConfigRepository: RemoteConfigRepository,
     private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
 ) {
     suspend fun run(
         openOriginalLoginScreen: () -> Unit,
-        openAutomaticMagicLinkScreen: () -> Unit
+        openAutomaticMagicLinkScreen: () -> Unit,
+        openEnhancedMagicLinkScreen: () -> Unit
     ) {
-        experimentTracker.log(ExperimentTracker.AUTOMATIC_MAGIC_LINK_EXPERIMENT_ELIGIBLE_EVENT)
+        experimentTracker.log(ExperimentTracker.MAGIC_LINK_EXPERIMENT_ELIGIBLE_EVENT)
 
-        val magicLinkRequestVariant = remoteConfigRepository.observeAutomaticMagicLinkRequestVariant().first()
+        val magicLinkRequestVariant = remoteConfigRepository.observeMagicLinkRequestVariant().first()
 
         // track the variant used
         analyticsTrackerWrapper.track(
-            AnalyticsEvent.AUTOMATIC_MAGIC_LINK_REQUEST_EXPERIMENT,
+            AnalyticsEvent.MAGIC_LINK_REQUEST_EXPERIMENT,
             mapOf(Pair(AnalyticsTracker.KEY_EXPERIMENT_VARIANT, magicLinkRequestVariant.name))
         )
 
         when (magicLinkRequestVariant) {
             CONTROL -> openOriginalLoginScreen()
             AUTOMATIC -> openAutomaticMagicLinkScreen()
+            ENHANCED -> openEnhancedMagicLinkScreen()
         }
     }
 
-    enum class AutomaticMagicLinkRequestVariant {
+    enum class MagicLinkRequestVariant {
         CONTROL,
+        ENHANCED,
         AUTOMATIC
     }
 }
