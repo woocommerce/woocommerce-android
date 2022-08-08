6b318d44f3fa283e2dd5595eee23710459f14d70
Ondrej Ruttkay <ruttkay@gmail.com>
2022-08-08 13:22:16 +0200

    Optimize imports

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginActivity.kt
@@ -6,10 +6,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.BaseTransientBottomBar
