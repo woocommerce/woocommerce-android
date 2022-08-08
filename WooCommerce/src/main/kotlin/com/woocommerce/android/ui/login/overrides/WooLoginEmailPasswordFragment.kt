72475fd7c089e6c985f1799a7f046d7ea4625f60
JorgeMucientes <jorge.mucientes@automattic.com>
2022-08-08 18:51:41 +0200

    Fix issues on WooLoginEmailPasswordFragment after merge with trunk

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/overrides/WooLoginEmailPasswordFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/overrides/WooLoginEmailPasswordFragment.kt
@@ -13,8 +13,11 @@ import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRe
 import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.AUTOMATIC
 import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.CONTROL
 import dagger.android.support.AndroidSupportInjection
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.login.LoginEmailPasswordFragment
 import org.wordpress.android.login.LoginListener
+import org.wordpress.android.login.LoginWpcomService
 
 class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
     companion object {
@@ -43,6 +46,12 @@ class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
         }
     }
 
+    interface Listener {
+        fun onPasswordError()
+    }
+
+    private lateinit var onPassWordErrorListener: Listener
+
     private var loginListener: LoginListener? = null
     private var variant: MagicLinkRequestVariant = CONTROL
     private var email: String? = null
@@ -64,6 +73,9 @@ class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
             context
         } else {
             throw MissingComponentException("$context must implement LoginListener")
+        }
+        if (activity is Listener) {
+            onPassWordErrorListener = activity as Listener
         }
     }
 
@@ -92,6 +104,14 @@ class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
                     loginListener?.useMagicLinkInstead(email, false)
                 }
             }
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    override fun onLoginStateUpdated(loginState: LoginWpcomService.LoginState) {
+        super.onLoginStateUpdated(loginState)
+        if (loginState.step == LoginWpcomService.LoginStep.FAILURE_EMAIL_WRONG_PASSWORD) {
+            onPassWordErrorListener.onPasswordError()
         }
     }
 }
