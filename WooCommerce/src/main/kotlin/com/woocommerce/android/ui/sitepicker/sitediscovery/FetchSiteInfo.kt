19d332bcf403df3db4ae8e230de714a76da59e8e
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-08-18 14:48:34 +0300

    Remove unnecessary inner class

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/sitepicker/sitediscovery/FetchSiteInfo.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/sitepicker/sitediscovery/FetchSiteInfo.kt
@@ -1,6 +1,5 @@
 package com.woocommerce.android.ui.sitepicker.sitediscovery
 
-import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.suspendCancellableCoroutine
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode.MAIN
@@ -17,7 +16,22 @@ import kotlin.coroutines.resume
 class FetchSiteInfo @Inject constructor(private val dispatcher: Dispatcher) {
     suspend operator fun invoke(siteAddress: String) =
         suspendCancellableCoroutine<Result<ConnectSiteInfoPayload>> { continuation ->
-            val listener = ConnectSiteInfoListener(continuation)
+            val listener = object : Any() {
+                @Subscribe(threadMode = MAIN)
+                fun onFetchedConnectSiteInfo(event: OnConnectSiteInfoChecked) {
+                    dispatcher.unregister(this)
+                    if (!continuation.isActive) return
+
+                    if (event.isError) {
+                        AppLog.e(API, "onFetchedConnectSiteInfo has error: " + event.error.message)
+                        continuation.resume(
+                            Result.failure(FetchSiteInfoException(event.error.type, event.error.message))
+                        )
+                    } else {
+                        continuation.resume(Result.success(event.info))
+                    }
+                }
+            }
             dispatcher.register(listener)
             dispatcher.dispatch(SiteActionBuilder.newFetchConnectSiteInfoAction(siteAddress))
 
@@ -25,23 +39,6 @@ class FetchSiteInfo @Inject constructor(private val dispatcher: Dispatcher) {
                 dispatcher.unregister(listener)
             }
         }
-
-    private inner class ConnectSiteInfoListener(
-        private val continuation: CancellableContinuation<Result<ConnectSiteInfoPayload>>
-    ) {
-        @Subscribe(threadMode = MAIN)
-        fun onFetchedConnectSiteInfo(event: OnConnectSiteInfoChecked) {
-            dispatcher.unregister(this@ConnectSiteInfoListener)
-
-            if (!continuation.isActive) return
-            if (event.isError) {
-                AppLog.e(API, "onFetchedConnectSiteInfo has error: " + event.error.message)
-                continuation.resume(Result.failure(FetchSiteInfoException(event.error.type, event.error.message)))
-            } else {
-                continuation.resume(Result.success(event.info))
-            }
-        }
-    }
 
     class FetchSiteInfoException(val type: SiteErrorType, message: String?) : Exception(message)
 }
