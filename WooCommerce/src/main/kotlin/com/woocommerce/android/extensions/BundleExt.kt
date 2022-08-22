aa4f7b4e8b5741e137537ac2b397e086fa0b6f06
Andrey <andrey.kidinov@a8c.com>
2022-08-22 11:25:26 +0800

    Replaced getParcelable depricated usage

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/BundleExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/BundleExt.kt
@@ -2,9 +2,15 @@ package com.woocommerce.android.extensions
 
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.TIRAMISU
+import android.os.Bundle
 import android.os.Parcel
 
 inline fun <reified T> Parcel.parcelable(loader: ClassLoader?): T? = when {
     SDK_INT >= TIRAMISU -> readParcelable(loader, T::class.java)
     else -> @Suppress("DEPRECATION") readParcelable(loader) as? T
+}
+
+inline fun <reified T> Bundle.parcelable(key: String): T? = when {
+    SDK_INT >= TIRAMISU -> getParcelable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationListFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/VariationListFragment.kt
@@ -26,6 +26,7 @@ import com.woocommerce.android.di.GlideApp
 import com.woocommerce.android.extensions.handleResult
 import com.woocommerce.android.extensions.navigateBackWithResult
 import com.woocommerce.android.extensions.navigateSafely
+import com.woocommerce.android.extensions.parcelable
 import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.model.Product
 import com.woocommerce.android.model.ProductVariation
@@ -109,7 +110,7 @@ class VariationListFragment :
         val layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         this.layoutManager = layoutManager
 
-        savedInstanceState?.getParcelable<Parcelable>(LIST_STATE_KEY)?.let {
+        savedInstanceState?.parcelable<Parcelable>(LIST_STATE_KEY)?.let {
             layoutManager.onRestoreInstanceState(it)
         }
 
