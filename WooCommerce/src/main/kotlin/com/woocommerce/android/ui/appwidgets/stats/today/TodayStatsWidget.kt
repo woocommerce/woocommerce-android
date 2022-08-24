f27c5419c633425eda93ff7cd5d2e191981450c0
Nick Bradbury <nick.bradbury@gmail.com>
2022-08-24 09:02:09 -0400

    Updated manifest

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/stats/today/TodayStatsWidget.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/stats/today/TodayStatsWidget.kt
@@ -1,6 +1,5 @@
 package com.woocommerce.android.ui.widgets.stats.today
 
-import com.woocommerce.android.di.AppComponent
 import com.woocommerce.android.ui.widgets.WidgetUpdater
 import com.woocommerce.android.ui.widgets.stats.StatsWidget
 import javax.inject.Inject
@@ -9,8 +8,4 @@ class TodayStatsWidget : StatsWidget() {
     @Inject lateinit var todayWidgetUpdater: TodayWidgetUpdater
     override val widgetUpdater: WidgetUpdater
         get() = todayWidgetUpdater
-
-    override fun inject(appComponent: AppComponent) {
-        appComponent.inject(this)
-    }
 }
