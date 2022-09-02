5892dc4e2a7ae86dc2caa5f82941209275d5d3fe
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-09-02 18:28:50 +0100

    Add a feature flag for Jetpack connection

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -15,7 +15,8 @@ enum class FeatureFlag {
     WC_SHIPPING_BANNER,
     UNIFIED_ORDER_EDITING,
     ORDER_CREATION_CUSTOMER_SEARCH,
-    PRE_LOGIN_NOTIFICATIONS;
+    PRE_LOGIN_NOTIFICATIONS,
+    LOGIN_JETPACK_CONNECTION;
 
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
@@ -30,7 +31,8 @@ enum class FeatureFlag {
             ANALYTICS_HUB,
             MORE_MENU_INBOX,
             WC_SHIPPING_BANNER,
-            PRE_LOGIN_NOTIFICATIONS -> PackageUtils.isDebugBuild()
+            PRE_LOGIN_NOTIFICATIONS,
+            LOGIN_JETPACK_CONNECTION-> PackageUtils.isDebugBuild()
         }
     }
 }
