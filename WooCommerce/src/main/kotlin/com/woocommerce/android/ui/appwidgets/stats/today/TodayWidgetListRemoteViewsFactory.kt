f43170669ecbff8e1e84ef15defa27e63bcdc248
Nick Bradbury <nick.bradbury@gmail.com>
2022-09-07 08:55:42 -0400

    Removed comment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/stats/today/TodayWidgetListRemoteViewsFactory.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/stats/today/TodayWidgetListRemoteViewsFactory.kt
@@ -16,7 +16,6 @@ class TodayWidgetListRemoteViewsFactory @Inject constructor(
     val context: Context,
     intent: Intent
 ) : RemoteViewsFactory {
-    // TODO this injection doesn't work
     @Inject lateinit var viewModel: TodayWidgetListViewModel
     @Inject lateinit var widgetUpdater: TodayWidgetUpdater
 
