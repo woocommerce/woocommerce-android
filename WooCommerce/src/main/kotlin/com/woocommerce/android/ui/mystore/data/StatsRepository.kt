f5e4d9c6c65e9193e2992ed6c02cf5776288ebdd
Alejo <alejandro.torres.veiga@automattic.com>
2022-09-12 10:04:17 -0300

    fetch stats data on suspend functions

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/data/StatsRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/data/StatsRepository.kt
@@ -5,11 +5,18 @@ import com.woocommerce.android.extensions.semverCompareTo
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.WooLog.T.DASHBOARD
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withTimeoutOrNull
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.WCRevenueStatsModel
 import org.wordpress.android.fluxc.model.leaderboards.WCTopPerformerProductModel
+import org.wordpress.android.fluxc.network.BaseRequest
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
 import org.wordpress.android.fluxc.store.WCLeaderboardsStore
 import org.wordpress.android.fluxc.store.WCOrderStore
 import org.wordpress.android.fluxc.store.WCStatsStore
@@ -137,4 +144,129 @@ class StatsRepository @Inject constructor(
     }
 
     data class StatsException(val error: OrderStatsError?) : Exception()
+    data class SiteStats(val revenue: WCRevenueStatsModel?, val visitors: Map<String, Int>?)
+
+    private suspend fun fetchRevenueStats(
+        granularity: StatsGranularity,
+        forced: Boolean,
+        site: SiteModel = selectedSite.get()
+    ): WooResult<WCRevenueStatsModel?> {
+        val statsPayload = FetchRevenueStatsPayload(
+            site = site,
+            granularity = granularity,
+            forced = forced
+        )
+
+        val result = wcStatsStore.fetchRevenueStats(statsPayload)
+
+        return if (result.isError) {
+            val error = result.error.toWooError()
+            WooLog.e(
+                DASHBOARD,
+                "$TAG - Error fetching revenue stats: ${error.message}"
+            )
+            WooResult(error)
+        } else {
+            val revenueStatsModel = wcStatsStore.getRawRevenueStats(
+                site = site,
+                granularity = result.granularity,
+                startDate = result.startDate!!,
+                endDate = result.endDate!!
+            )
+            WooResult(revenueStatsModel)
+        }
+    }
+
+    private suspend fun fetchVisitorStats(
+        granularity: StatsGranularity,
+        forced: Boolean,
+        site: SiteModel = selectedSite.get()
+    ): WooResult<Map<String, Int>> {
+        val visitsPayload = FetchNewVisitorStatsPayload(
+            site = site,
+            granularity = granularity,
+            forced = forced
+        )
+
+        val result = wcStatsStore.fetchNewVisitorStats(visitsPayload)
+
+        return if (result.isError) {
+            val error = result.error.toWooError()
+            WooLog.e(
+                DASHBOARD,
+                "$TAG - Error fetching visitor stats: ${error.message}"
+            )
+            WooResult(error)
+        } else {
+            val visitorStats = wcStatsStore.getNewVisitorStats(
+                selectedSite.get(), result.granularity, result.quantity, result.date, result.isCustomField
+            )
+            WooResult(visitorStats)
+        }
+    }
+
+    suspend fun fetchStats(
+        granularity: StatsGranularity,
+        forced: Boolean,
+        site: SiteModel = selectedSite.get()
+    ): WooResult<SiteStats> = coroutineScope {
+        val fetchVisitorStats = async {
+            fetchVisitorStats(
+                granularity = granularity,
+                forced = forced,
+                site = site
+            )
+        }
+        val fetchRevenueStats = async {
+            fetchRevenueStats(
+                granularity = granularity,
+                forced = forced,
+                site = site
+            )
+        }
+        val visitorStats = fetchVisitorStats.await()
+        val revenueStats = fetchRevenueStats.await()
+
+        return@coroutineScope if (visitorStats.isError || revenueStats.isError) {
+            val error = WooError(
+                type = WooErrorType.GENERIC_ERROR,
+                original = BaseRequest.GenericErrorType.UNKNOWN,
+                message = "Error fetching site stats for site ${site.siteId}"
+            )
+            WooResult(error)
+        } else {
+            WooResult(SiteStats(revenue = revenueStats.model, visitors = visitorStats.model))
+        }
+    }
+
+
+    private fun OrderStatsError?.toWooError(): WooError {
+        if (this == null) {
+            return WooError(
+                type = WooErrorType.TIMEOUT,
+                original = BaseRequest.GenericErrorType.TIMEOUT,
+                message = "Timeout"
+            )
+        }
+
+        val type = when (this.type) {
+            WCStatsStore.OrderStatsErrorType.RESPONSE_NULL -> WooErrorType.INVALID_RESPONSE
+            WCStatsStore.OrderStatsErrorType.INVALID_PARAM -> WooErrorType.INVALID_PARAM
+            WCStatsStore.OrderStatsErrorType.PLUGIN_NOT_ACTIVE -> WooErrorType.PLUGIN_NOT_ACTIVE
+            WCStatsStore.OrderStatsErrorType.GENERIC_ERROR -> WooErrorType.GENERIC_ERROR
+        }
+
+        val original = when (this.type) {
+            WCStatsStore.OrderStatsErrorType.RESPONSE_NULL -> BaseRequest.GenericErrorType.INVALID_RESPONSE
+            WCStatsStore.OrderStatsErrorType.INVALID_PARAM -> BaseRequest.GenericErrorType.INVALID_RESPONSE
+            WCStatsStore.OrderStatsErrorType.PLUGIN_NOT_ACTIVE -> BaseRequest.GenericErrorType.NOT_FOUND
+            WCStatsStore.OrderStatsErrorType.GENERIC_ERROR -> BaseRequest.GenericErrorType.UNKNOWN
+        }
+
+        return WooError(
+            type = type,
+            original = original,
+            message = this.message
+        )
+    }
 }
