856d935fb08471a30437d2c9bd8fbdeec7da9b0c
Alejo <alejandro.torres.veiga@automattic.com>
2022-09-12 10:05:45 -0300

    refactor widget list view model

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/stats/today/TodayWidgetListViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/stats/today/TodayWidgetListViewModel.kt
@@ -1,23 +1,23 @@
 package com.woocommerce.android.ui.appwidgets.stats.today
 
 import androidx.annotation.LayoutRes
-import com.woocommerce.android.AppPrefsWrapper
 import com.woocommerce.android.R
 import com.woocommerce.android.R.string
 import com.woocommerce.android.util.CurrencyFormatter
 import com.woocommerce.android.viewmodel.ResourceProvider
 import kotlinx.coroutines.runBlocking
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.model.WCRevenueStatsModel
 import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.fluxc.store.WCStatsStore
+import org.wordpress.android.fluxc.store.WooCommerceStore
 import javax.inject.Inject
 
 class TodayWidgetListViewModel @Inject constructor(
     private val siteStore: SiteStore,
     private val resourceProvider: ResourceProvider,
     private val currencyFormatter: CurrencyFormatter,
-    private val repository: TodayWidgetListViewRepository,
-    private val appPrefsWrapper: AppPrefsWrapper
+    private val wooCommerceStore: WooCommerceStore,
+    private val getWidgetStats: GetWidgetStats,
 ) {
     private var siteId: Int? = null
     private var appWidgetId: Int? = null
@@ -30,15 +30,7 @@ class TodayWidgetListViewModel @Inject constructor(
         this.appWidgetId = appWidgetId
     }
 
-    @Suppress("ReturnCount")
     fun onDataSetChanged(onError: (appWidgetId: Int) -> Unit) {
-        // currently the stats widget data can only be fetched if the user is logged in. If user is not
-        // logged in, display an error message in the widget
-        if (!repository.userIsLoggedIn()) {
-            appWidgetId?.let { onError(it) }
-            return
-        }
-
         if (siteId == null) {
             appWidgetId?.let { onError(it) }
             return
@@ -53,66 +45,49 @@ class TodayWidgetListViewModel @Inject constructor(
         // fetch current day stats from API. The data is fetched synchronously. The widget will remain
         // in its current state while work is being done here, and a loading view will show up in lieu
         // of the actual contents in the interim
-        runBlocking {
-            repository.fetchTodayStats(site)
-        }
+        val todayStatsResult = runBlocking { getWidgetStats(WCStatsStore.StatsGranularity.DAYS, site) }
 
-        // The v4 stats API is only available if user has the WC-Admin plugin or uses a WooCommerce version >= 4.0.
-        // If v4 stats is not available, an error message is displayed in the widget
-        if (!appPrefsWrapper.isV4StatsSupported()) {
+        if (todayStatsResult.isError) {
             appWidgetId?.let { onError(it) }
-            return
-        }
-
-        // get the current day stats from the local db and build a UI model for the widget now that
-        // the v4 stats is available
-        val revenueStats = repository.getTodayRevenueStats(site)
-        val visitorStats = repository.getTodayVisitorStats(site)
-        val currencyCode = repository.getStatsCurrency(site)
-        val uiModels = buildListItemUiModel(revenueStats, visitorStats, currencyCode, site)
-        if (uiModels != data) {
-            mutableData.clear()
-            mutableData.addAll(uiModels)
+        } else {
+            val uiModels = buildListItemUiModel(
+                site = site,
+                stats = todayStatsResult.model!!
+            )
+            if (uiModels != data) {
+                mutableData.clear()
+                mutableData.addAll(uiModels)
+            }
         }
     }
 
     private fun buildListItemUiModel(
-        revenueStats: WCRevenueStatsModel?,
-        visitorCount: String,
-        currencyCode: String?,
-        site: SiteModel
+        site: SiteModel,
+        stats: GetWidgetStats.WidgetStats
     ): List<TodayWidgetListItem> {
         val layout = R.layout.stats_widget_list_item
-
-        var grossRevenue = 0.0
-        var orderCount = 0
-        revenueStats?.parseTotal()?.let { total ->
-            grossRevenue = total.totalSales ?: 0.0
-            orderCount = total.ordersCount ?: 0
-        }
-
         val localSiteId = site.siteId.toInt()
-
         val formatCurrencyForDisplay = currencyFormatter::formatCurrencyRounded
+        val currencyCode = wooCommerceStore.getSiteSettings(site)?.currencyCode.orEmpty()
 
         return listOf(
             TodayWidgetListItem(
                 layout,
                 localSiteId,
                 resourceProvider.getString(string.dashboard_stats_revenue),
-                formatCurrencyForDisplay(grossRevenue, currencyCode.orEmpty())
+                formatCurrencyForDisplay(stats.revenueGross, currencyCode)
             ),
             TodayWidgetListItem(
                 layout,
                 localSiteId,
                 resourceProvider.getString(string.dashboard_stats_orders),
-                orderCount.toString()
+                stats.ordersTotal.toString()
             ),
             TodayWidgetListItem(
                 layout,
                 localSiteId,
                 resourceProvider.getString(string.dashboard_stats_visitors),
-                visitorCount
+                stats.visitorsTotal.toString()
             )
         )
     }
