3b505e45776a15d3144eebffb3187beb1df6a874
Ondrej Ruttkay <ruttkay@gmail.com>
2022-10-05 15:12:53 +0200

    Remove the magic link request experiment and roll out the "enhanced" variant

--- WooCommerce/src/main/kotlin/com/woocommerce/android/config/RemoteConfigRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/config/RemoteConfigRepository.kt
@@ -1,7 +1,6 @@
 package com.woocommerce.android.config
 
 import com.woocommerce.android.experiment.JetpackTimeoutExperiment.JetpackTimeoutPolicyVariant
-import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant
 import com.woocommerce.android.experiment.MagicLinkSentScreenExperiment.MagicLinkSentScreenVariant
 import com.woocommerce.android.experiment.PrologueExperiment.PrologueVariant
 import kotlinx.coroutines.flow.Flow
@@ -10,7 +9,6 @@ interface RemoteConfigRepository {
     fun fetchRemoteConfig()
     fun observePrologueVariant(): Flow<PrologueVariant>
     fun observeMagicLinkSentScreenVariant(): Flow<MagicLinkSentScreenVariant>
-    fun observeMagicLinkRequestVariant(): Flow<MagicLinkRequestVariant>
     fun getPerformanceMonitoringSampleRate(): Double
     fun observeJetpackTimeoutPolicyVariantVariant(): Flow<JetpackTimeoutPolicyVariant>
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/overrides/WooLoginEmailPasswordFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/overrides/WooLoginEmailPasswordFragment.kt
@@ -2,31 +2,21 @@ package com.woocommerce.android.ui.login.overrides
 
 import android.content.Context
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.annotation.LayoutRes
 import androidx.core.view.isVisible
 import com.bumptech.glide.Registry.MissingComponentException
 import com.woocommerce.android.R
-import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant
-import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.AUTOMATIC
-import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.CONTROL
-import com.woocommerce.android.experiment.MagicLinkRequestExperiment.MagicLinkRequestVariant.ENHANCED
-import com.woocommerce.android.extensions.requestPasswordAutoFillWithDelay
-import com.woocommerce.android.extensions.showKeyboardWithDelay
 import dagger.android.support.AndroidSupportInjection
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.login.LoginEmailPasswordFragment
 import org.wordpress.android.login.LoginListener
 import org.wordpress.android.login.LoginWpcomService
-import org.wordpress.android.login.widgets.WPLoginInputRow
 
 class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
     companion object {
-        private const val ARG_VARIANT = "ARG_VARIANT"
-
         @Suppress("LongParameterList")
         fun newInstance(
             emailAddress: String?,
@@ -34,9 +24,8 @@ class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
             idToken: String? = null,
             service: String? = null,
             isSocialLogin: Boolean = false,
-            allowMagicLink: Boolean = false,
-            verifyMagicLinkEmail: Boolean = false,
-            variant: MagicLinkRequestVariant = CONTROL
+            allowMagicLink: Boolean = true,
+            verifyMagicLinkEmail: Boolean = false
         ): WooLoginEmailPasswordFragment {
             val fragment = WooLoginEmailPasswordFragment()
             val args = Bundle()
@@ -47,7 +36,6 @@ class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
             args.putBoolean(ARG_SOCIAL_LOGIN, isSocialLogin)
             args.putBoolean(ARG_ALLOW_MAGIC_LINK, allowMagicLink)
             args.putBoolean(ARG_VERIFY_MAGIC_LINK_EMAIL, verifyMagicLinkEmail)
-            args.putString(ARG_VARIANT, variant.name)
             fragment.arguments = args
             return fragment
         }
@@ -59,15 +47,10 @@ class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
 
     private lateinit var onPassWordErrorListener: Listener
     private var loginListener: LoginListener? = null
-    private var variant: MagicLinkRequestVariant = CONTROL
     private var email: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        arguments?.getString(ARG_VARIANT)?.let {
-            variant = MagicLinkRequestVariant.valueOf(it)
-        }
 
         email = arguments?.getString(ARG_EMAIL_ADDRESS)
     }
@@ -92,40 +75,16 @@ class WooLoginEmailPasswordFragment : LoginEmailPasswordFragment() {
     }
 
     @LayoutRes
-    override fun getContentLayout(): Int =
-        when (variant) {
-            CONTROL -> super.getContentLayout()
-            ENHANCED,
-            AUTOMATIC -> R.layout.fragment_login_email_password
-        }
+    override fun getContentLayout(): Int = R.layout.fragment_login_email_password
 
     override fun setupContent(rootView: ViewGroup) {
         super.setupContent(rootView)
-        when (variant) {
-            ENHANCED -> addRequestMagicLinkButton(rootView)
-            AUTOMATIC -> addOpenEmailClientButton(rootView)
-            CONTROL -> {
-                with(rootView.findViewById<WPLoginInputRow>(R.id.login_password_row).editText) {
-                    requestPasswordAutoFillWithDelay()
-                    showKeyboardWithDelay()
-                }
-            }
-        }
-    }
 
-    private fun addRequestMagicLinkButton(rootView: ViewGroup?) {
-        rootView?.findViewById<View>(R.id.magic_link_message)?.isVisible = false
-        rootView?.findViewById<Button>(R.id.bottom_button_magic_link)?.apply {
-            isVisible = true
+        rootView.findViewById<Button>(R.id.bottom_button_magic_link)?.apply {
             setOnClickListener {
+                it.isVisible = arguments?.getBoolean(ARG_ALLOW_MAGIC_LINK) ?: true
                 loginListener?.useMagicLinkInstead(email, false)
             }
-        }
-    }
-
-    private fun addOpenEmailClientButton(rootView: ViewGroup?) {
-        rootView?.findViewById<Button>(R.id.button_login_open_email_client)?.setOnClickListener {
-            loginListener?.openEmailClient(true)
         }
     }
 
