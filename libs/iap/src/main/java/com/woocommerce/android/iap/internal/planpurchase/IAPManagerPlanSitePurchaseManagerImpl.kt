b44b876c7da9fbf53ea3cab60834e7d8747b523d
Andrey <andrey.kidinov@a8c.com>
2022-10-07 11:52:22 +0400

    Changed package order

--- libs/iap/src/main/java/com/woocommerce/android/iap/internal/planpurchase/IAPManagerPlanSitePurchaseManagerImpl.kt
+++ libs/iap/src/main/java/com/woocommerce/android/iap/internal/planpurchase/IAPManagerPlanSitePurchaseManagerImpl.kt
@@ -1,10 +1,10 @@
 package com.woocommerce.android.iap.internal.planpurchase
 
 import androidx.appcompat.app.AppCompatActivity
+import com.woocommerce.android.iap.internal.core.IAPManagerFactory
 import com.woocommerce.android.iap.public.IAPSitePurchasePlanManager
 import com.woocommerce.android.iap.public.IAPStore
 import com.woocommerce.android.iap.public.LogWrapper
-import com.woocommerce.android.iap.internal.core.IAPManagerFactory
 import com.woocommerce.android.iap.public.model.IAPProduct
 import com.woocommerce.android.iap.public.model.IAPPurchaseResponse
 import com.woocommerce.android.iap.public.model.IAPPurchaseResponse.Error
