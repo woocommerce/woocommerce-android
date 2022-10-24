20d69c831712bfed37aaf0b4a75e10e84fa22a46
samiuelson <samiuelson@gmail.com>
2022-10-24 10:07:31 +0200

    Add documentation

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/payments/StatementDescriptor.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/payments/StatementDescriptor.kt
@@ -1,5 +1,16 @@
 package com.woocommerce.android.cardreader.payments
 
+/**
+ * Class which acts as a wrapper to raw statement descriptor text and applies set of sanitation operations to prepare it
+ * for use with Stripe API. There are the following requirements applied:
+ * 1. Text must be 22 chars long.
+ * 2. It must not contain the following characters: <, >, ', ".
+ *
+ * [StatementDescriptor] exposes the [StatementDescriptor.value] property which is ready to use with Stripe API.
+ *
+ * @param rawValue Raw statement descriptor value used while instantiating [StatementDescriptor] object.
+ * @property value Statement descriptor value after sanitation.
+ */
 class StatementDescriptor(rawValue: String?) {
 
     val value: String? = rawValue?.take(MAX_LENGTH)?.split("<", ">", "'", "\"")?.joinToString(REPLACEMENT_CHAR)
