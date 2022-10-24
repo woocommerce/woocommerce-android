0abc9cbf0056feb4be62a28b643ee037ca165d95
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-10-24 14:04:31 +0100

    Make getCurrentVariant suspendable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/experiment/SimplifiedLoginExperiment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/experiment/SimplifiedLoginExperiment.kt
@@ -3,7 +3,7 @@ package com.woocommerce.android.experiment
 import javax.inject.Inject
 
 class SimplifiedLoginExperiment @Inject constructor() {
-    fun getCurrentVariant() = LoginVariant.SIMPLIFIED
+    suspend fun getCurrentVariant() = LoginVariant.SIMPLIFIED
 
     enum class LoginVariant {
         STANDARD,
