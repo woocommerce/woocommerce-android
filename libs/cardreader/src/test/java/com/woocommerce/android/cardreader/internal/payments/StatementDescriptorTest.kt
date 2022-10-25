de84c5f95e0bcf14b0981c5325c1382fa6567b74
samiuelson <samiuelson@gmail.com>
2022-10-25 19:37:45 +0200

    Update test method names

--- libs/cardreader/src/test/java/com/woocommerce/android/cardreader/internal/payments/StatementDescriptorTest.kt
+++ libs/cardreader/src/test/java/com/woocommerce/android/cardreader/internal/payments/StatementDescriptorTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertTrue
 class StatementDescriptorTest {
 
     @Test
-    fun `given statement descriptor longer than 22 chars first 22 chars should be returned`() {
+    fun `given statement descriptor longer than 22 chars, when wrapped with StatementDescriptor, then it's shortened to the first 22 chars`() {
         // given
         val originalStatementDescriptor = "khfdsjklfhjklfhdsalkjflk;asdjflk;fjlk;sadfjlsa;fjl;fjlads;"
         assertTrue(originalStatementDescriptor.length > 22)
@@ -22,7 +22,7 @@ class StatementDescriptorTest {
     }
 
     @Test
-    fun `given statement descriptor contains illegal chars then replace them with -`() {
+    fun `given statement descriptor contains illegal chars, when wrapped with StatementDescriptor, then illegal chars are replaced with -`() {
         // given
         val originalStatementDescriptor = "aaa<aaa>aaa'aaa\"aaa*"
 
@@ -34,19 +34,7 @@ class StatementDescriptorTest {
     }
 
     @Test
-    fun `given statement descriptor contains illegal chars only then replace them with -`() {
-        // given
-        val originalStatementDescriptor = "<>a'\"*"
-
-        // when
-        val statementDescriptor = StatementDescriptor(originalStatementDescriptor)
-
-        // then
-        assertEquals("--a---", statementDescriptor.value)
-    }
-
-    @Test
-    fun `given statement descriptor size smaller than 5 chars it should be appended to at least 5 chars`() {
+    fun `given statement descriptor length smaller then 5 chars, when wrapped with StatementDescriptor, then it is appended to 5 chars`() {
         // given
         val originalStatementDescriptor = "khf"
         assertTrue(originalStatementDescriptor.length < 5)
@@ -60,7 +48,7 @@ class StatementDescriptorTest {
     }
 
     @Test
-    fun `given statement descriptor null it should be set to null`() {
+    fun `given null statement descriptor, when wrapped with StatementDescriptor, then it is set to null`() {
         // given
         val originalStatementDescriptor = null
 
@@ -72,7 +60,7 @@ class StatementDescriptorTest {
     }
 
     @Test
-    fun `given statement descriptor doesn't contain any letters it should be set to null`() {
+    fun `given statement descriptor doesn't contain any letters, when wrapped with StatementDescriptor, then it is set to null`() {
         // given
         val originalStatementDescriptor = "./`;'+_)(*&^%$#@!="
         originalStatementDescriptor.filter { char ->
