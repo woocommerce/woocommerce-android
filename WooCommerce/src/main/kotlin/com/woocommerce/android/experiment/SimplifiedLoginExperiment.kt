c50548f9ddfdf306f3e9c7cfe3edd715ac9fdf7c
Hafiz Rahman <hfzrhmn@gmail.com>
2022-10-25 17:55:15 +0700

    Update: getCurrentVariant() to return actual value from Firebase.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/experiment/SimplifiedLoginExperiment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/experiment/SimplifiedLoginExperiment.kt
@@ -1,9 +1,24 @@
 package com.woocommerce.android.experiment
 
+import com.woocommerce.android.analytics.ExperimentTracker
+import com.woocommerce.android.config.RemoteConfigRepository
 import javax.inject.Inject
 
-class SimplifiedLoginExperiment @Inject constructor() {
-    fun getCurrentVariant() = LoginVariant.SIMPLIFIED
+class SimplifiedLoginExperiment @Inject constructor(
+    private val remoteConfigRepository: RemoteConfigRepository,
+) {
+    companion object {
+        private const val VARIANT_CONTROL = "control"
+        private const val VARIANT_SIMPLIFIED = "simplified_login_i1"
+    }
+
+    fun getCurrentVariant(): LoginVariant {
+        return when (remoteConfigRepository.getSimplifiedLoginVariant()) {
+            VARIANT_CONTROL -> LoginVariant.STANDARD
+            VARIANT_SIMPLIFIED -> LoginVariant.SIMPLIFIED
+            else ->  LoginVariant.STANDARD
+        }
+    }
 
     enum class LoginVariant {
         STANDARD,
