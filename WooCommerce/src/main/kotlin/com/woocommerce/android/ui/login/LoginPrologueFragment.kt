b3c2f9e97025e58a5fb9aa0668c04e9fcb97d08b
Hafiz Rahman <hfzrhmn@gmail.com>
2022-10-27 12:01:07 +0700

    Show Create A Store button in Simplified Login variant.
    This also hides the "Login With Site Address" and "Get Started" buttons.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginPrologueFragment.kt
@@ -14,6 +14,7 @@ import com.woocommerce.android.databinding.FragmentLoginPrologueBinding
 import com.woocommerce.android.experiment.SimplifiedLoginExperiment
 import com.woocommerce.android.experiment.SimplifiedLoginExperiment.LoginVariant.SIMPLIFIED
 import com.woocommerce.android.extensions.hide
+import com.woocommerce.android.extensions.show
 import com.woocommerce.android.ui.login.UnifiedLoginTracker.Flow
 import com.woocommerce.android.ui.login.UnifiedLoginTracker.Step
 import com.woocommerce.android.util.FeatureFlag
@@ -69,8 +70,11 @@ open class LoginPrologueFragment(@LayoutRes layout: Int) : Fragment(layout) {
 
         // Various updates related to Simplified Login A/B testing.
         if (isSimplifiedLoginVariant) {
-            binding.buttonLoginStore.hide()
             binding.newToWooButton.text = getString(R.string.login_prologue_learn_more_about_woo)
+
+            binding.buttonCreateStore.show()
+            binding.buttonLoginStore.hide()
+            binding.buttonGetStarted.hide()
         }
     }
 
