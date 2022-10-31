4db10c52b54e1aebdae7635f895f1375f28e24e4
JorgeMucientes <jorge.mucientes@automattic.com>
2022-10-31 18:36:32 +0100

    Add fake dispatcher

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/signup/SignUpRepositoryTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/signup/SignUpRepositoryTest.kt
@@ -1,20 +1,18 @@
 package com.woocommerce.android.ui.login.signup
 
 import com.woocommerce.android.AppPrefsWrapper
-import com.woocommerce.android.util.dispatchAndAwait
+import com.woocommerce.android.FakeDispatcher
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Before
+import kotlinx.coroutines.async
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyString
-import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged
 import org.wordpress.android.fluxc.store.AccountStore.OnAuthenticationChanged
-import org.wordpress.android.fluxc.store.AccountStore.UpdateTokenPayload
 import org.wordpress.android.fluxc.store.signup.SignUpStore
 import org.wordpress.android.fluxc.store.signup.SignUpStore.CreateWpAccountResult
 
@@ -32,12 +30,10 @@ class SignUpRepositoryTest : BaseUnitTest() {
             "usernameThree"
         )
         val DEFAULT_SUCCESSFUL_ACCOUNT_CREATION_RESULT = CreateWpAccountResult(success = true, ANY_AUTH_TOKEN)
-        val DEFAULT_ON_ACCOUNT_CHANGED_EVENT = OnAccountChanged()
-        val DEFAULT_ON_AUTH_TOKEN_CHANGED_EVENT = OnAuthenticationChanged()
     }
 
     private val signUpStore: SignUpStore = mock()
-    private val dispatcher: Dispatcher = mock()
+    private val dispatcher: Dispatcher = FakeDispatcher()
     private val prefsWrapper: AppPrefsWrapper = mock()
     private val signUpCredentialsValidator: SignUpCredentialsValidator = mock()
 
@@ -48,16 +44,8 @@ class SignUpRepositoryTest : BaseUnitTest() {
         signUpCredentialsValidator
     )
 
-    @Before
-    fun setUp() {
-        testBlocking {
-            givenDispatcherReturnsForTokenUpdates(DEFAULT_ON_AUTH_TOKEN_CHANGED_EVENT)
-            givenDispatcherReturnsForAccountChanges(DEFAULT_ON_ACCOUNT_CHANGED_EVENT)
-        }
-    }
-
     @Test
-    fun `given valid email, when creating account , then fetch username suggestions`() =
+    fun `given valid email, when creating account, then fetch username suggestions`() =
         testBlocking {
             givenValidateCredentialsReturns(null)
             givenAccountCreationResult(DEFAULT_SUCCESSFUL_ACCOUNT_CREATION_RESULT)
@@ -67,17 +55,22 @@ class SignUpRepositoryTest : BaseUnitTest() {
                 )
             )
 
-            sut.createAccount(ANY_VALID_USER_EMAIL, ANY_PASSWORD)
+            val task = async { sut.createAccount(ANY_VALID_USER_EMAIL, ANY_PASSWORD) }
+            // Handle token update
+            dispatcher.emitChange(OnAuthenticationChanged())
+            // Handle account fetching
+            dispatcher.emitChange(OnAccountChanged())
+            task.await()
 
             verify(signUpStore).fetchUserNameSuggestions(ANY_VALID_USER_EMAIL)
             verify(signUpStore).createWpAccount(ANY_VALID_USER_EMAIL, ANY_PASSWORD, DEFAULT_USERNAME_SUGGESTION)
         }
 
-//    @Test
-//    fun `given username error, when fetching username suggestions , then use email address to create account `() =
-//        testBlocking {
-//
-//        }
+    @Test
+    fun `given username error, when fetching username suggestions , then use email address to create account`() =
+        testBlocking {
+
+        }
 //
 //    @Test
 //    fun `given an email with wordpress wording in it, when create new account , then strip wordpress from email`() =
@@ -95,15 +88,5 @@ class SignUpRepositoryTest : BaseUnitTest() {
 
     private suspend fun givenAccountCreationResult(result: CreateWpAccountResult) {
         whenever(signUpStore.createWpAccount(anyString(), anyString(), anyString())).thenReturn(result)
-    }
-
-    private suspend fun givenDispatcherReturnsForTokenUpdates(result: OnAuthenticationChanged) {
-        whenever(dispatcher.dispatchAndAwait<UpdateTokenPayload, OnAuthenticationChanged>(any()))
-            .thenReturn(result)
-    }
-
-    private suspend fun givenDispatcherReturnsForAccountChanges(result: OnAccountChanged) {
-        whenever(dispatcher.dispatchAndAwait<Void, OnAccountChanged>(any()))
-            .thenReturn(result)
     }
 }
