7542ed103d06e57273d0c31e1083fb113440a8e7
Anirudh Uppunda <abhat38@gmail.com>
2022-11-01 13:45:55 +0530

    Add JitmTracker.kt class to hold all analytics related to JITM

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/JitmTracker.kt
@@ -0,0 +1,42 @@
+package com.woocommerce.android.ui
+
+import androidx.annotation.VisibleForTesting
+import com.woocommerce.android.analytics.AnalyticsEvent
+import com.woocommerce.android.analytics.AnalyticsTracker.Companion.KEY_SOURCE
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType
+import javax.inject.Inject
+
+class JitmTracker @Inject constructor(
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
+) {
+    @VisibleForTesting
+    fun track(
+        stat: AnalyticsEvent,
+        properties: Map<String, Any> = mapOf(),
+        errorType: String? = null,
+        errorDescription: String? = null,
+    ) {
+        val isError = !errorType.isNullOrBlank() || !errorDescription.isNullOrEmpty()
+        if (isError) {
+            analyticsTrackerWrapper.track(
+                stat,
+                properties,
+                this@JitmTracker.javaClass.simpleName,
+                errorType,
+                errorDescription
+            )
+        } else {
+            analyticsTrackerWrapper.track(stat, properties)
+        }
+    }
+
+    fun trackJitmFetchFailure(source: String, type: WooErrorType, message: String?) {
+        track(
+            stat = AnalyticsEvent.JITM_FETCH_FAILURE,
+            properties = mapOf(KEY_SOURCE to source),
+            errorType = type.name,
+            errorDescription = message
+        )
+    }
+}
