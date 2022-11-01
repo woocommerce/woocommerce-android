c9e0569f23ecf871891209f40c58ae54c79276b8
Andrey <andrey.kidinov@a8c.com>
2022-11-01 10:43:50 +0000

    Removed todos that have created issues already

--- libs/iap/src/main/java/com/woocommerce/android/iap/internal/core/IAPExt.kt
+++ libs/iap/src/main/java/com/woocommerce/android/iap/internal/core/IAPExt.kt
@@ -4,14 +4,12 @@ import com.android.billingclient.api.BillingClient.BillingResponseCode
 import com.android.billingclient.api.BillingResult
 import com.android.billingclient.api.ProductDetails
 
-// TODO support for multiple plans?
 val ProductDetails.priceOfTheFirstPurchasedOfferInMicros
     get() = subscriptionOfferDetails?.get(0)!!.pricingPhases.pricingPhaseList[0]!!.priceAmountMicros
 
 val ProductDetails.currencyOfTheFirstPurchasedOffer
     get() = subscriptionOfferDetails?.get(0)!!.pricingPhases.pricingPhaseList[0]!!.priceCurrencyCode
 
-// TODO support for multiple offers?
 val ProductDetails.firstOfferToken
     get() = subscriptionOfferDetails!!.first().offerToken
 
