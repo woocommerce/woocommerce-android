b722c521ded2dadc512800137f16e3cac8a29a35
Anirudh Uppunda <abhat38@gmail.com>
2022-11-02 12:38:04 +0530

    Tests to verify if proper default prefixes are added for UTM params

--- WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooCommerceComUTMProviderTest.kt
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooCommerceComUTMProviderTest.kt
@@ -38,11 +38,13 @@ class WooCommerceComUTMProviderTest {
     @Test
     fun `testUtmQueryParamsAreAddedToTheUrlProperly`() {
         val utmCampaign = "feature_announcement_card"
+        val jitmPrefixUtmCampaign = "jitm_group_feature_announcement_card"
         val utmSource = "orders_list"
         val utmContent = "upsell_card_readers"
+        val jitmPrefixUtmContent = "jitm_upsell_card_readers"
         val url = "https://www.woocommerce.com?utm_campaign=$utmCampaign&utm_source=$utmSource"
-        val expectedUrl = "https://www.woocommerce.com?utm_campaign=$utmCampaign&utm_source=$utmSource" +
-            "&utm_content=$utmContent&utm_term=1234&utm_medium=woo_android"
+        val expectedUrl = "https://www.woocommerce.com?utm_campaign=$jitmPrefixUtmCampaign&utm_source=$utmSource" +
+            "&utm_content=$jitmPrefixUtmContent&utm_term=1234&utm_medium=woo_android"
         val defaultUTMMedium = "woo_android"
 
         val urlWithUTM = provideUTMProvider(
@@ -53,18 +55,19 @@ class WooCommerceComUTMProviderTest {
         ).getUrlWithUtmParams(url)
 
         assertThat(urlWithUTM.toUri().getQueryParameter("utm_medium")).isEqualTo(defaultUTMMedium)
-        assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(utmCampaign)
+        assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(jitmPrefixUtmCampaign)
         assertThat(urlWithUTM.toUri().getQueryParameter("utm_source")).isEqualTo(utmSource)
-        assertThat(urlWithUTM.toUri().getQueryParameter("utm_content")).isEqualTo(utmContent)
+        assertThat(urlWithUTM.toUri().getQueryParameter("utm_content")).isEqualTo(jitmPrefixUtmContent)
         assertThat(urlWithUTM).isEqualTo(expectedUrl)
     }
 
     @Test
     fun `testUtmQueriesAreExcludedInTheUrlIfTheyAreNullOrEmpty`() {
         val utmCampaign = "feature_announcement_card"
+        val jitmPrefixUtmCampaign = "jitm_group_feature_announcement_card"
         val defaultUTMMedium = "woo_android"
         val url = "https://www.woocommerce.com/us/hw"
-        val expectedUrl = "https://www.woocommerce.com/us/hw?utm_campaign=$utmCampaign" +
+        val expectedUrl = "https://www.woocommerce.com/us/hw?utm_campaign=$jitmPrefixUtmCampaign" +
             "&utm_term=1234&utm_medium=$defaultUTMMedium"
 
         val urlWithUTM = provideUTMProvider(
@@ -75,7 +78,7 @@ class WooCommerceComUTMProviderTest {
         ).getUrlWithUtmParams(url)
 
         assertThat(urlWithUTM.toUri().getQueryParameter("utm_medium")).isEqualTo(defaultUTMMedium)
-        assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(utmCampaign)
+        assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(jitmPrefixUtmCampaign)
         assertFalse(urlWithUTM.contains("utm_source"))
         assertFalse(urlWithUTM.contains("utm_content"))
         assertThat(urlWithUTM).isEqualTo(expectedUrl)
@@ -86,13 +89,15 @@ class WooCommerceComUTMProviderTest {
         val existingUtmCampaign = "payments_menu_item"
         val existingUtmSource = "payments_menu"
         val utmCampaign = "feature_announcement_card"
+        val jitmPrefixUtmCampaign = "jitm_group_feature_announcement_card"
         val utmSource = "orders_list"
         val utmContent = "upsell_card_readers"
+        val jitmPrefixUtmContent = "jitm_upsell_card_readers"
         val defaultUTMMedium = "woo_android"
         val url = "https://www.woocommerce.com/us/hw?utm_campaign=$existingUtmCampaign" +
             "&utm_source=$existingUtmSource"
-        val expectedUrl = "https://www.woocommerce.com/us/hw?utm_campaign=$utmCampaign&utm_source=$utmSource" +
-            "&utm_content=$utmContent&utm_term=1234&utm_medium=$defaultUTMMedium"
+        val expectedUrl = "https://www.woocommerce.com/us/hw?utm_campaign=$jitmPrefixUtmCampaign&utm_source=$utmSource" +
+            "&utm_content=$jitmPrefixUtmContent&utm_term=1234&utm_medium=$defaultUTMMedium"
 
         val urlWithUTM = provideUTMProvider(
             campaign = utmCampaign,
@@ -102,9 +107,9 @@ class WooCommerceComUTMProviderTest {
         ).getUrlWithUtmParams(url)
 
         assertThat(urlWithUTM.toUri().getQueryParameter("utm_medium")).isEqualTo(defaultUTMMedium)
-        assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(utmCampaign)
+        assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(jitmPrefixUtmCampaign)
         assertThat(urlWithUTM.toUri().getQueryParameter("utm_source")).isEqualTo(utmSource)
-        assertThat(urlWithUTM.toUri().getQueryParameter("utm_content")).isEqualTo(utmContent)
+        assertThat(urlWithUTM.toUri().getQueryParameter("utm_content")).isEqualTo(jitmPrefixUtmContent)
         assertThat(urlWithUTM).isEqualTo(expectedUrl)
     }
 
@@ -171,6 +176,46 @@ class WooCommerceComUTMProviderTest {
         ).getUrlWithUtmParams(url)
 
         assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(utmCampaign)
+        assertThat(urlWithUTM).isEqualTo(expectedUrl)
+    }
+
+    @Test
+    fun `testCampaignPrefixIsAddedWhileAddingUtmParams`() {
+        val utmCampaign = "feature_announcement_card"
+        val jitmPrefixUtmCampaign = "jitm_group_$utmCampaign"
+        val defaultUTMMedium = "woo_android"
+        val url = "https://www.woocommerce.com/us/hw"
+        val expectedUrl = "https://www.woocommerce.com/us/hw?utm_campaign=$jitmPrefixUtmCampaign" +
+            "&utm_term=1234&utm_medium=$defaultUTMMedium"
+
+        val urlWithUTM = provideUTMProvider(
+            campaign = utmCampaign,
+            source = "",
+            content = null,
+            siteId = 1234L
+        ).getUrlWithUtmParams(url)
+
+        assertThat(urlWithUTM.toUri().getQueryParameter("utm_campaign")).isEqualTo(jitmPrefixUtmCampaign)
+        assertThat(urlWithUTM).isEqualTo(expectedUrl)
+    }
+
+    @Test
+    fun `testContentPrefixIsAddedWhileAddingUtmParams`() {
+        val utmContent = "test_content"
+        val jitmPrefixedUtmContent = "jitm_$utmContent"
+        val defaultUTMMedium = "woo_android"
+        val url = "https://www.woocommerce.com/us/hw"
+        val expectedUrl = "https://www.woocommerce.com/us/hw?" +
+            "utm_content=$jitmPrefixedUtmContent&utm_term=1234&utm_medium=$defaultUTMMedium"
+
+        val urlWithUTM = provideUTMProvider(
+            campaign = "",
+            source = "",
+            content = utmContent,
+            siteId = 1234L
+        ).getUrlWithUtmParams(url)
+
+        assertThat(urlWithUTM.toUri().getQueryParameter("utm_content")).isEqualTo(jitmPrefixedUtmContent)
         assertThat(urlWithUTM).isEqualTo(expectedUrl)
     }
 }
