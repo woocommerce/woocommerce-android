af839d3cac042b0374d35302097d3a09d9b0e2cd
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-02 20:03:19 +0100

    Add basic layout for domain picker

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/domainpicker/SiteDomainPickerScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/domainpicker/SiteDomainPickerScreen.kt
@@ -1,10 +1,143 @@
 package com.woocommerce.android.ui.login.storecreation.domainpicker
 
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.component.WCSearchField
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.login.storecreation.domainpicker.SiteDomainPickerViewModel.DomainSuggestion
+import com.woocommerce.android.ui.login.storecreation.domainpicker.SiteDomainPickerViewModel.SiteDomainPickerState
 
 @Composable
 fun SiteDomainPickerScreen(viewModel: SiteDomainPickerViewModel) {
+    viewModel.viewState.observeAsState(SiteDomainPickerState()).value.let { viewState ->
+        Scaffold(topBar = {
+            Toolbar(onArrowBackPressed = viewModel::onBackPressed)
+        }) {
+            SiteDomainSearchForm(
+                state = viewState,
+                onDomainQueryChanged = viewModel::onDomainChanged,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+private fun Toolbar(
+    onArrowBackPressed: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.surface,
+        title = {},
+        navigationIcon = {
+            IconButton(onClick = onArrowBackPressed) {
+                Icon(
+                    Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(id = R.string.back)
+                )
+            }
+        },
+        elevation = 0.dp,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun SiteDomainSearchForm(
+    state: SiteDomainPickerState,
+    onDomainQueryChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .background(MaterialTheme.colors.surface)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(dimensionResource(id = R.dimen.major_125)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.major_100)),
+    ) {
+        Text(
+            text = stringResource(id = R.string.store_creation_domain_picker_title),
+            style = MaterialTheme.typography.h4,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.store_creation_domain_picker_subtitle),
+            style = MaterialTheme.typography.body1,
+        )
+        WCSearchField(
+            value = state.domain,
+            onValueChange = onDomainQueryChanged,
+            hint = stringResource(id = R.string.store_creation_domain_picker_hint),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp)
+                .border(
+                    BorderStroke(
+                        width = dimensionResource(id = R.dimen.minor_10),
+                        color = colorResource(id = R.color.gray_5)
+                    ),
+                    RoundedCornerShape(dimensionResource(id = R.dimen.minor_100))
+                ),
+            backgroundColor = TextFieldDefaults.outlinedTextFieldColors().backgroundColor(enabled = true).value
+        )
+        DomainSuggestionList(
+            suggestions = state.domainSuggestions, isLoading = state.isLoading
+        )
+    }
+}
+
+@Composable
+private fun DomainSuggestionList(suggestions: List<DomainSuggestion>, isLoading: Boolean) {
 
 }
 
+@ExperimentalFoundationApi
+@Preview(name = "dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "small screen", device = Devices.PIXEL)
+@Preview(name = "mid screen", device = Devices.PIXEL_4)
+@Preview(name = "large screen", device = Devices.NEXUS_10)
+@Composable
+fun SiteDomainPickerPreview() {
+    WooThemeWithBackground {
+        SiteDomainSearchForm(
+            state = SiteDomainPickerState(),
+            onDomainQueryChanged = {}
+        )
+    }
+}
 
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/domainpicker/SiteDomainPickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/domainpicker/SiteDomainPickerViewModel.kt
@@ -1,20 +1,47 @@
 package com.woocommerce.android.ui.login.storecreation.domainpicker
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 @HiltViewModel
 class SiteDomainPickerViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
 ) : ScopedViewModel(savedStateHandle) {
-    private val _viewState = MutableLiveData<SiteDomainPickerState>()
-    val viewState: LiveData<SiteDomainPickerState> = _viewState
+    private val domainQuery = savedState.getStateFlow(this, "")
+    private val domainSuggestions = savedState.getStateFlow(this, emptyList<DomainSuggestion>())
+
+    val viewState = combine(
+        domainQuery,
+        domainSuggestions
+    ) { domainQuery, domainSuggestions ->
+        SiteDomainPickerState(
+            isLoading = false,
+            domain = domainQuery,
+            domainSuggestions = domainSuggestions
+        )
+    }.asLiveData()
+
+    fun onBackPressed() {
+
+    }
+
+    fun onDomainChanged(query: String) {
+        domainQuery.value = query
+    }
 
     data class SiteDomainPickerState(
-        val isLoading: Boolean = false
+        val isLoading: Boolean = false,
+        val domain: String = "",
+        val domainSuggestions: List<DomainSuggestion> = emptyList()
+    )
+
+    data class DomainSuggestion(
+        val isSelected: Boolean = false,
+        val domain: String = ""
     )
 }
