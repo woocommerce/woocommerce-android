93918149dc59de6fe3fc4d1b1f163298b1b72e66
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-03 20:51:27 +0100

    Fix site picker view model unit tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/sitepicker/SitePickerViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/sitepicker/SitePickerViewModelTest.kt
@@ -521,10 +521,11 @@ class SitePickerViewModelTest : BaseUnitTest() {
 
             viewModel.onAddStoreClick()
 
-            verify(analyticsTrackerWrapper, times(1)).track(
-                AnalyticsEvent.SITE_PICKER_ADD_A_STORE_TAPPED
+            verify(analyticsTrackerWrapper).track(
+                AnalyticsEvent.SITE_PICKER_CREATE_SITE_TAPPED,
+                mapOf(AnalyticsTracker.KEY_SOURCE to "login")
             )
-            assertThat(view).isEqualTo(NavigateToAddStoreEvent)
+            assertThat(view).isEqualTo(NavigateToAddStoreEvent(source = "login"))
         }
 
     @Test
