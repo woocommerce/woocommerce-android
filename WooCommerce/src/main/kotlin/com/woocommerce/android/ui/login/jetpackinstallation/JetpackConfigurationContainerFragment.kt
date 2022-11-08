3376ed49e83bdb08e67cf1cc25ff26f9c811ad69
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-08 18:28:33 +0100

    Hide the toolbar from all of Jetpack fragments

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpackinstallation/JetpackConfigurationContainerFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpackinstallation/JetpackConfigurationContainerFragment.kt
@@ -2,9 +2,15 @@ package com.woocommerce.android.ui.login.jetpackinstallation
 
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.main.AppBarStatus
 
 class JetpackConfigurationContainerFragment: BaseFragment(R.layout.fragment_jetpack_configuration_container) {
     companion object {
         const val TAG = "JetpackConfigurationContainerFragment"
     }
+
+    // Hide the Toolbar, this fragment is to be reused between MainActivity and LoginActivity,
+    // so we'll leave it to each child fragment to handle its toolbar
+    override val activityAppBarStatus: AppBarStatus
+        get() = AppBarStatus.Hidden
 }
