a413b17835c45aff84243d0987730fca794acd23
Andrey <andrey.kidinov@a8c.com>
2022-11-08 17:15:31 +0400

    Removed enabling of the pending purchases

--- libs/iap/src/main/java/com/woocommerce/android/iap/internal/core/IAPBillingClientProvider.kt
+++ libs/iap/src/main/java/com/woocommerce/android/iap/internal/core/IAPBillingClientProvider.kt
@@ -12,7 +12,6 @@ internal class IAPBillingClientProvider(
         BillingClient
             .newBuilder(context)
             .setListener(listener)
-            .enablePendingPurchases()
             .build()
     )
 }
