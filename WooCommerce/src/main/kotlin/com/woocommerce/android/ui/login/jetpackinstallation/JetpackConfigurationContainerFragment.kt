929b84d6f3d460ede19cfa3a7d66813e0a274edf
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-08 18:24:34 +0100

    Add a container fragment to host the nav graph for jetpack installation

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpackinstallation/JetpackConfigurationContainerFragment.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.ui.login.jetpackinstallation
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+
+class JetpackConfigurationContainerFragment: BaseFragment(R.layout.fragment_jetpack_configuration_container) {
+    companion object {
+        const val TAG = "JetpackConfigurationContainerFragment"
+    }
+}
