2bcc675c48d0662b407a06e49380c1d07751d1a2
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-09 16:49:34 +0100

    Revert "Remove unused constant"
    This reverts commit 09371895f0adacddb37784b892c3d30017184ebe.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/ExperimentTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/ExperimentTracker.kt
@@ -5,6 +5,7 @@ import com.google.firebase.analytics.ktx.ParametersBuilder
 interface ExperimentTracker {
     companion object {
         const val PROLOGUE_EXPERIMENT_ELIGIBLE_EVENT = "prologue_carousel_displayed"
+        const val LOGIN_SUCCESSFUL_EVENT = "login_successful"
         const val SITE_VERIFICATION_SUCCESSFUL_EVENT = "site_verification_successful"
         const val SIMPLIFIED_LOGIN_ELIGIBLE_EVENT = "simplified_login_experiment_eligible"
         const val SIMPLIFIED_LOGIN_SUCCESSFUL_EVENT = "my_store_displayed"
