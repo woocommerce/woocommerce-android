8db97e2821f41647c15965a4be566f1df93c8714
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-10 17:58:36 +0100

    Update the logic of the installation events to use the new extension

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/common/PluginRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/common/PluginRepository.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.common
 
 import android.os.Parcelable
 import com.woocommerce.android.AppConstants
+import com.woocommerce.android.OnChangedException
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.common.PluginRepository.PluginStatus.PluginActivated
 import com.woocommerce.android.ui.common.PluginRepository.PluginStatus.PluginActivationFailed
@@ -9,11 +10,13 @@ import com.woocommerce.android.ui.common.PluginRepository.PluginStatus.PluginIns
 import com.woocommerce.android.ui.common.PluginRepository.PluginStatus.PluginInstalled
 import com.woocommerce.android.util.ContinuationWrapper
 import com.woocommerce.android.util.WooLog
-import kotlinx.coroutines.channels.ProducerScope
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.trySendBlocking
-import kotlinx.coroutines.flow.callbackFlow
+import com.woocommerce.android.util.observeEvents
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.retryWhen
 import kotlinx.parcelize.Parcelize
 import org.greenrobot.eventbus.Subscribe
@@ -31,7 +34,6 @@ import org.wordpress.android.fluxc.store.PluginStore.InstallSitePluginPayload
 import org.wordpress.android.fluxc.store.PluginStore.OnJetpackSitePluginFetched
 import org.wordpress.android.fluxc.store.PluginStore.OnSitePluginConfigured
 import org.wordpress.android.fluxc.store.PluginStore.OnSitePluginInstalled
-import org.wordpress.android.fluxc.store.Store
 import javax.inject.Inject
 
 class PluginRepository @Inject constructor(
@@ -54,45 +56,69 @@ class PluginRepository @Inject constructor(
 
     private var continuationFetchJetpackSitePlugin = ContinuationWrapper<SitePluginModel?>(WooLog.T.WP)
 
-    // Note that the `newInstallSitePluginAction` action automatically tries to activate the plugin after
-    // installation is successful, so when using this function, there's no need to call `activatePlugin()`
-    // separately.
-    fun installPlugin(slug: String, name: String) = callbackFlow<PluginStatus> {
-        val listener = PluginActionListener(this)
-        dispatcher.register(listener)
-
-        // Check whether plugin exists first, in which case we just need to activate it.
+    fun installPlugin(slug: String, name: String): Flow<PluginStatus> = flow {
         val plugin = fetchJetpackSitePlugin(name)
+
         if (plugin != null) {
-            trySend(PluginInstalled(slug, selectedSite.get()))
-            activatePlugin(name, slug, true)
+            // Plugin is already installed, proceed to activation
+            emit(PluginInstalled(slug, selectedSite.get()))
+            val payload = ConfigureSitePluginPayload(
+                selectedSite.get(),
+                name,
+                slug,
+                true,
+                true
+            )
+            dispatcher.dispatch(PluginActionBuilder.newConfigureSitePluginAction(payload))
         } else {
+            // This request will automatically proceed to activating the plugin after the installation
             val payload = InstallSitePluginPayload(selectedSite.get(), slug)
             dispatcher.dispatch(PluginActionBuilder.newInstallSitePluginAction(payload))
         }
 
-        awaitClose {
-            dispatcher.unregister(listener)
-        }
-    }.retryWhen { cause, attempt ->
-        cause is PluginException && attempt < ATTEMPT_LIMIT
-    }.catch { cause ->
-        if (cause is PluginException) {
-            if (cause.errorType is InstallSitePluginError) {
-                emit(
-                    PluginInstallFailed(
-                        errorDescription = cause.errorMessage,
-                        errorType = cause.errorType.type.name
+        val installationEvents = dispatcher.observeEvents<OnSitePluginInstalled>()
+            .map { event ->
+                if (!event.isError) {
+                    PluginInstalled(event.slug, event.site)
+                } else {
+                    throw OnChangedException(
+                        event.error,
+                        event.error.message
                     )
-                )
-            } else if (cause.errorType is ConfigureSitePluginError) {
-                emit(
-                    PluginActivationFailed(
-                        errorDescription = cause.errorMessage,
-                        errorType = cause.errorType.type.name
-                    )
-                )
+                }
             }
+        val activationEvents = dispatcher.observeEvents<OnSitePluginConfigured>()
+            .map { event ->
+                if (!event.isError) {
+                    // If there is no error, we can assume that the configuration (activating in our case) is successful.
+                    PluginActivated(event.pluginName, event.site)
+                } else {
+                    throw OnChangedException(
+                        event.error,
+                        event.error.message
+                    )
+                }
+            }
+
+        emitAll(merge(installationEvents, activationEvents))
+    }.retryWhen { cause, attempt ->
+        cause is OnChangedException && attempt < ATTEMPT_LIMIT
+    }.catch { cause ->
+        if (cause !is OnChangedException) throw cause
+        if (cause.error is InstallSitePluginError) {
+            emit(
+                PluginInstallFailed(
+                    errorDescription = cause.message ?: GENERIC_ERROR,
+                    errorType = cause.error.type.name
+                )
+            )
+        } else if (cause.error is ConfigureSitePluginError) {
+            emit(
+                PluginActivationFailed(
+                    errorDescription = cause.message ?: GENERIC_ERROR,
+                    errorType = cause.error.type.name
+                )
+            )
         }
     }
 
@@ -107,54 +133,6 @@ class PluginRepository @Inject constructor(
                 is ContinuationWrapper.ContinuationResult.Success -> result.value
             }
         } else null
-    }
-
-    fun activatePlugin(name: String, slug: String, enableAutoUpdate: Boolean = true) {
-        val payload = ConfigureSitePluginPayload(
-            selectedSite.get(),
-            name,
-            slug,
-            true,
-            enableAutoUpdate
-        )
-        dispatcher.dispatch(PluginActionBuilder.newConfigureSitePluginAction(payload))
-    }
-
-    private inner class PluginActionListener(private val producerScope: ProducerScope<PluginStatus>) {
-        @Suppress("unused")
-        @Subscribe(threadMode = ThreadMode.MAIN)
-        fun onSitePluginInstalled(event: OnSitePluginInstalled) {
-            if (!event.isError) {
-                producerScope.trySendBlocking(
-                    PluginInstalled(event.slug, event.site)
-                )
-            } else {
-                producerScope.close(
-                    PluginException(
-                        event.error,
-                        event.error.message ?: GENERIC_ERROR
-                    )
-                )
-            }
-        }
-
-        @Suppress("unused")
-        @Subscribe(threadMode = ThreadMode.MAIN)
-        fun onSitePluginConfigured(event: OnSitePluginConfigured) {
-            if (!event.isError) {
-                // If there is no error, we can assume that the configuration (activating in our case) is successful.
-                producerScope.trySendBlocking(
-                    PluginActivated(event.pluginName, event.site)
-                )
-            } else {
-                producerScope.close(
-                    PluginException(
-                        event.error,
-                        event.error.message ?: GENERIC_ERROR
-                    )
-                )
-            }
-        }
     }
 
     @Suppress("unused")
@@ -180,6 +158,4 @@ class PluginRepository @Inject constructor(
         @Parcelize
         data class PluginActivationFailed(val errorDescription: String, val errorType: String) : PluginStatus()
     }
-
-    private class PluginException(val errorType: Store.OnChangedError, val errorMessage: String) : Exception()
 }
