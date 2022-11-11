1260640e2fc892fa80569e5a2b3b4c53f75585e9
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-11 11:32:30 +0100

    Fix logic of navigation after account creation
    We don't want to start the store creation flow unless the signup was started from the "Get Started" button of the prologue

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/signup/SignUpFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/signup/SignUpFragment.kt
@@ -66,6 +66,7 @@ class SignUpFragment : BaseFragment() {
         super.onCreate(savedInstanceState)
 
         nextStep = requireArguments().serializable(NEXT_STEP_KEY) ?: error("Screen requires passing a NextStep")
+        viewModel.nextStep = nextStep
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
