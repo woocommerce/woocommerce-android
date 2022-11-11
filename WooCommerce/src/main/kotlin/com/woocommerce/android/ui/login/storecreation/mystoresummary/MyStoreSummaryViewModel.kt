d0cbfffa57354a6003d4cbad150f0f76d5bc7c1d
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-11 16:17:56 +0100

    Add a class data holder for new store input data

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/mystoresummary/MyStoreSummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/mystoresummary/MyStoreSummaryViewModel.kt
@@ -1,29 +1,30 @@
 package com.woocommerce.android.ui.login.storecreation.mystoresummary
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import com.woocommerce.android.ui.login.storecreation.NewStore
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class MyStoreSummaryViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val newStore: NewStore
 ) : ScopedViewModel(savedStateHandle) {
 
-    private val _viewState = MutableLiveData<MyStoreSummaryState>()
-    val viewState: LiveData<MyStoreSummaryState> = _viewState
-
-    init {
-        _viewState.value = MyStoreSummaryState(
-            name = "White Christmas Trees",
-            domain = "whitechristmastrees.mywc.mysite",
-            category = "Arts and Crafts",
-            country = "Canada"
-        )
-    }
+    val viewState = newStore.store
+        .map {
+            MyStoreSummaryState(
+                name = it.name,
+                domain = it.domain ?: "",
+                category = it.category,
+                country = it.country ?: "TODO default value US?"
+            )
+        }
+        .asLiveData()
 
     fun onBackPressed() {
         triggerEvent(MultiLiveEvent.Event.Exit)
@@ -37,6 +38,6 @@ class MyStoreSummaryViewModel @Inject constructor(
         val name: String? = null,
         val domain: String,
         val category: String? = null,
-        val country: String
+        val country: String,
     )
 }
