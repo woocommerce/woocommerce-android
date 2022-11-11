28505adc44d13157bfa595cf130d6819089fd0b4
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-11 16:19:01 +0100

    Add missing @AndroidEntryPoint

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/mystoresummary/MyStoreSummaryFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/mystoresummary/MyStoreSummaryFragment.kt
@@ -12,7 +12,9 @@ import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.main.AppBarStatus
 import com.woocommerce.android.viewmodel.MultiLiveEvent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyStoreSummaryFragment : BaseFragment() {
     private val viewModel: MyStoreSummaryViewModel by viewModels()
 
