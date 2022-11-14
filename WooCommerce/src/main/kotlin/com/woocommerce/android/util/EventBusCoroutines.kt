f48ce87cf5311cc479e44f6f0d98bc4dad49da85
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-14 11:24:10 +0100

    Add a base listener class to reuse for both extensions

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/EventBusCoroutines.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/EventBusCoroutines.kt
@@ -12,6 +12,7 @@ import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.annotations.action.Action
 import kotlin.coroutines.resume
+import kotlin.reflect.KClass
 
 suspend inline fun <PAYLOAD, reified EVENT : Any> Dispatcher.dispatchAndAwait(
     action: Action<PAYLOAD>
@@ -23,14 +24,8 @@ suspend inline fun <PAYLOAD, reified EVENT : Any> Dispatcher.dispatchAndAwait(
 }
 
 suspend inline fun <reified EVENT : Any> Dispatcher.awaitEvent(): EVENT = suspendCancellableCoroutine { continuation ->
-    val listener = object {
-        @Subscribe(threadMode = ThreadMode.MAIN)
-        @Suppress("unused")
-        fun onEvent(event: EVENT) {
-            // Since generic types are suppressed at runtime, this listener will be registered using the type Object
-            // But using the reified EVENT type, we can compare the class and ignore unwanted events
-            if (event::class != EVENT::class) return
-
+    val listener = object : GenericEventBusListener<EVENT>(EVENT::class) {
+        override fun handleEvent(event: EVENT) {
             unregister(this)
             if (!continuation.isActive) {
                 WooLog.w(WooLog.T.UTILS, "Listener for ${EVENT::class} invoked after cancellation")
@@ -47,14 +42,8 @@ suspend inline fun <reified EVENT : Any> Dispatcher.awaitEvent(): EVENT = suspen
 }
 
 inline fun <reified EVENT : Any> Dispatcher.observeEvents(): Flow<EVENT> = callbackFlow {
-    val listener = object {
-        @Subscribe(threadMode = ThreadMode.MAIN)
-        @Suppress("unused")
-        fun onEvent(event: EVENT) {
-            // Since generic types are suppressed at runtime, this listener will be registered using the type Object
-            // But using the reified EVENT type, we can compare the class and ignore unwanted events
-            if (event::class != EVENT::class) return
-
+    val listener = object : GenericEventBusListener<EVENT>(EVENT::class) {
+        override fun handleEvent(event: EVENT) {
             trySend(event)
                 .onFailure {
                     WooLog.w(WooLog.T.UTILS, "Failure to emit EventBus's event $event as Flow, ${it?.toString()}")
@@ -68,3 +57,16 @@ inline fun <reified EVENT : Any> Dispatcher.observeEvents(): Flow<EVENT> = callb
     }
 }
 
+abstract class GenericEventBusListener<EVENT : Any>(private val kClass: KClass<EVENT>) {
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    @Suppress("unused")
+    fun onEvent(event: EVENT) {
+        // Since generic types are suppressed at runtime, this listener will be registered using the type Object
+        // But using the reified EVENT type, we can compare the class and ignore unwanted events
+        if (event::class != kClass) return
+
+        handleEvent(event)
+    }
+
+    abstract fun handleEvent(event: EVENT)
+}
