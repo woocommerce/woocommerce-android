bea8904d957c84446f2cccfcd047c16cadd73ac4
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-15 14:18:54 +0100

    Create site credentials screen boilerplate

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/sitecredentials/JetpackActivationSiteCredentialsScreen.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.login.jetpack.sitecredentials
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JetpackActivationSiteCredentialsScreen(viewModel: JetpackActivationSiteCredentialsViewModel) {
+    TODO("Not yet implemented")
+}
