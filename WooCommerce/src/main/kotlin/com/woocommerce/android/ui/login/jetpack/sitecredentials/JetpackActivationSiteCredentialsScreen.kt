18eead32cd10a1f3f1b421b37a1f3f63974ab47d
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-15 18:37:02 +0100

    Create initial layout for site credentials

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/sitecredentials/JetpackActivationSiteCredentialsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/sitecredentials/JetpackActivationSiteCredentialsScreen.kt
@@ -1,8 +1,134 @@
 package com.woocommerce.android.ui.login.jetpack.sitecredentials
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.component.WCColoredButton
+import com.woocommerce.android.ui.compose.component.WCOutlinedTextField
+import com.woocommerce.android.ui.compose.component.WCPasswordField
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.login.jetpack.sitecredentials.JetpackActivationSiteCredentialsViewModel.JetpackActivationSiteCredentialsViewState
 
 @Composable
 fun JetpackActivationSiteCredentialsScreen(viewModel: JetpackActivationSiteCredentialsViewModel) {
-    TODO("Not yet implemented")
+    viewModel.viewState.observeAsState().value?.let {
+        JetpackActivationSiteCredentialsScreen(it)
+    }
+}
+
+@Composable
+fun JetpackActivationSiteCredentialsScreen(
+    viewState: JetpackActivationSiteCredentialsViewState,
+    onUsernameChanged: (String) -> Unit = {},
+    onPasswordChanged: (String) -> Unit = {},
+    onConnectClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.surface)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.major_100)),
+    ) {
+        Toolbar(onCloseClick = {})
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.major_100)),
+        ) {
+            Text(text = stringResource(id = R.string.enter_credentials_for_site, viewState.siteUrl))
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_100)))
+            WCOutlinedTextField(
+                value = viewState.username,
+                onValueChange = onUsernameChanged,
+                label = stringResource(id = R.string.username),
+                isError = viewState.errorMessage != null,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+            )
+            WCPasswordField(
+                value = viewState.password,
+                onValueChange = onPasswordChanged,
+                label = stringResource(id = R.string.password),
+                isError = viewState.errorMessage != null,
+                helperText = viewState.errorMessage?.let { stringResource(id = it) },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { onConnectClick() }
+                )
+            )
+        }
+
+        WCColoredButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(id = R.dimen.major_100))
+        ) {
+            Text(
+                text = stringResource(
+                    id = if (viewState.isJetpackInstalled) R.string.login_jetpack_connect
+                    else R.string.login_jetpack_install
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun Toolbar(
+    onCloseClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.surface,
+        title = { Text(stringResource(id = R.string.login_jetpack_installation_screen_title)) },
+        navigationIcon = {
+            IconButton(onClick = onCloseClick) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = stringResource(id = R.string.back)
+                )
+            }
+        },
+        elevation = 0.dp,
+        modifier = modifier
+    )
+}
+
+
+@Preview
+@Composable
+private fun JetpackActivationSiteCredentialsScreenPreview() {
+    WooThemeWithBackground {
+        JetpackActivationSiteCredentialsScreen(
+            viewState = JetpackActivationSiteCredentialsViewState(
+                isJetpackInstalled = false,
+                siteUrl = "reallyniceshirts.com"
+            )
+        )
+    }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/sitecredentials/JetpackActivationSiteCredentialsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/sitecredentials/JetpackActivationSiteCredentialsViewModel.kt
@@ -1,13 +1,59 @@
 package com.woocommerce.android.ui.login.jetpack.sitecredentials
 
+import android.os.Parcelable
+import androidx.annotation.StringRes
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
+import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
 class JetpackActivationSiteCredentialsViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val wpApiSiteRepository: WPApiSiteRepository
 ) : ScopedViewModel(savedStateHandle) {
+    private val navArgs: JetpackActivationSiteCredentialsFragmentArgs by savedStateHandle.navArgs()
 
+    private val _viewState = savedStateHandle.getStateFlow(
+        viewModelScope, JetpackActivationSiteCredentialsViewState(
+            isJetpackInstalled = TODO(),
+            siteUrl = navArgs.siteUrl
+        )
+    )
+    val viewState = _viewState.asLiveData()
+
+    fun onUsernameChanged(username: String) {
+        _viewState.update { it.copy(username = username) }
+    }
+
+    fun onPasswordChanged(password: String) {
+        _viewState.update { it.copy(password = password) }
+    }
+
+    fun onCloseClick() {
+        TODO()
+    }
+
+    fun onContinueClick() {
+        TODO()
+    }
+
+    @Parcelize
+    data class JetpackActivationSiteCredentialsViewState(
+        val isJetpackInstalled: Boolean,
+        val siteUrl: String,
+        val username: String = "",
+        val password: String = "",
+        @StringRes val errorMessage: Int? = null
+    ) : Parcelable {
+        @IgnoredOnParcel
+        val isValid = username.isNotBlank() && password.isNotBlank()
+    }
 }
