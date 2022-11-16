544ffab2f599094a4de78d6f50801ba4dc58254e
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-16 14:18:15 +0100

    Add boilerplate for the main installation fragment

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/main/JetpackActivationMainScreen.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.login.jetpack.main
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JetpackActivationMainScreen(viewModel: JetpackActivationMainViewModel) {
+    TODO("Not yet implemented")
+}
