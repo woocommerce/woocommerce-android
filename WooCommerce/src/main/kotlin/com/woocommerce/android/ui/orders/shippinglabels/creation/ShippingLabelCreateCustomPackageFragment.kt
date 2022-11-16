fbf97982727cc024c362e50bdea0ab67abfa5944
Andrey <andrey.kidinov@a8c.com>
2022-11-16 11:30:38 +0400

    Removed forgotten super call from ShippingLabelCreateCustomPackageFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingLabelCreateCustomPackageFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/ShippingLabelCreateCustomPackageFragment.kt
@@ -165,7 +165,7 @@ class ShippingLabelCreateCustomPackageFragment :
                 viewModel.onCustomFormDoneMenuClicked()
                 true
             }
-            else -> super.onOptionsItemSelected(item)
+            else -> false
         }
     }
 }
