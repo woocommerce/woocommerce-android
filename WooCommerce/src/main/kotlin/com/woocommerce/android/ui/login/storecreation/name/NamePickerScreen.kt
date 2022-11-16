8ffd2b3957b829b9cb15464ba802ec41336d1416
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-16 09:13:33 +0100

    Add basic layout for name picker screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/NamePickerScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/NamePickerScreen.kt
@@ -1,8 +1,44 @@
 package com.woocommerce.android.ui.login.storecreation.name
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
 
 @Composable
-fun NamePicker(namePickerViewModel: NamePickerViewModel) {
+fun NamePickerScreen(viewModel: NamePickerViewModel) {
+    viewModel.storeName.observeAsState().value.let { storeName ->
+        Scaffold(topBar = {
+            Toolbar(
+                onCancelPressed = viewModel::onCancelPressed
+            )
+        }) {
 
+        }
+    }
+}
+
+@Composable
+private fun Toolbar(
+    onCancelPressed: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.surface,
+        title = {},
+        navigationIcon = {
+            TextButton(onClick = onCancelPressed) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+        },
+        elevation = 0.dp,
+        modifier = modifier
+    )
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/NamePickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/NamePickerViewModel.kt
@@ -1,5 +1,7 @@
 package com.woocommerce.android.ui.login.storecreation.name
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,4 +11,8 @@ import javax.inject.Inject
 class NamePickerViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
 ) : ScopedViewModel(savedStateHandle) {
+    private val _storeName = MutableLiveData<String>()
+    val storeName: LiveData<String> = _storeName
+
+    fun onCancelPressed() {}
 }
