055ec029528ee2cf078259b4bb95dc0d123c1603
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-16 17:11:07 +0100

    Use savedState to avoid loosing store name

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/StoreNamePickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/StoreNamePickerViewModel.kt
@@ -1,11 +1,12 @@
 package com.woocommerce.android.ui.login.storecreation.name
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ui.login.storecreation.NewStore
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
@@ -15,8 +16,8 @@ class StoreNamePickerViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val newStore: NewStore
 ) : ScopedViewModel(savedStateHandle) {
-    private val _storeName = MutableLiveData("")
-    val storeName: LiveData<String> = _storeName
+    private val _storeName = savedState.getStateFlow(scope = this, initialValue = "")
+    val storeName: LiveData<String> = _storeName.asLiveData()
 
     fun onCancelPressed() {
         triggerEvent(MultiLiveEvent.Event.Exit)
