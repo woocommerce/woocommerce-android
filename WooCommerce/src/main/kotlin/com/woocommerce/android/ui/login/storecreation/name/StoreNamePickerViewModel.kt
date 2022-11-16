0d5644fc8806945d3666b4ca86f16b1b0b90ec12
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-16 16:55:40 +0100

    Handle click on continue button

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/StoreNamePickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/StoreNamePickerViewModel.kt
@@ -3,13 +3,17 @@ package com.woocommerce.android.ui.login.storecreation.name
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.ui.login.storecreation.NewStore
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class StoreNamePickerViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val newStore: NewStore
 ) : ScopedViewModel(savedStateHandle) {
     private val _storeName = MutableLiveData("")
     val storeName: LiveData<String> = _storeName
@@ -21,6 +25,11 @@ class StoreNamePickerViewModel @Inject constructor(
     }
 
     fun onContinueClicked() {
-
+        newStore.store.update {
+            it.copy(name = storeName.value)
+        }
+        triggerEvent(NavigateToNextStep)
     }
+
+    object NavigateToNextStep : MultiLiveEvent.Event()
 }
