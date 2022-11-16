0a4e754270929e031445d50bd85dee4e0371fb6b
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-11-16 14:31:24 +0100

    Create models for the installation steps

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/main/JetpackActivationMainViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/main/JetpackActivationMainViewModel.kt
@@ -1,8 +1,11 @@
 package com.woocommerce.android.ui.login.jetpack.main
 
+import android.os.Parcelable
+import androidx.annotation.StringRes
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +13,29 @@ class JetpackActivationMainViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ScopedViewModel(savedStateHandle) {
 
+    @Parcelize
+    data class ViewState(
+        val steps: List<Step>
+    ) : Parcelable
+
+    @Parcelize
+    data class Step(
+        @StringRes val title: Int,
+        val state: StepState,
+        @StringRes val additionalInfo: Int? = null
+    ) : Parcelable
+
+    sealed interface StepState : Parcelable {
+        @Parcelize
+        object Idle : StepState
+
+        @Parcelize
+        object Ongoing : StepState
+
+        @Parcelize
+        object Success : StepState
+
+        @Parcelize
+        data class Error(val code: Int) : StepState
+    }
 }
