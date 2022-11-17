13e4dd57ec4988174dc372c15aab0e037d5c902b
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-17 16:08:28 +0100

    Fix import issue

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/domainpicker/DomainPickerFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/domainpicker/DomainPickerFragment.kt
@@ -14,6 +14,7 @@ import com.woocommerce.android.ui.base.UIMessageResolver
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.login.storecreation.domainpicker.DomainPickerViewModel.NavigateToNextStep
 import com.woocommerce.android.ui.main.AppBarStatus
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -47,7 +48,7 @@ class DomainPickerFragment : BaseFragment() {
             when (event) {
                 is Exit -> findNavController().popBackStack()
                 is NavigateToNextStep -> navigateToStoreSummaryFragment()
-                is ShowSnackbar -> uiMessageResolver.showSnack(event.message)
+                is MultiLiveEvent.Event.ShowSnackbar -> uiMessageResolver.showSnack(event.message)
             }
         }
     }
