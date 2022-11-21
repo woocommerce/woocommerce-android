99439afdf141b3dfe7090641c9c6dd7972a6cffc
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-11-21 16:14:51 +0100

    Add fetchSSR() method to retrieve ssr info

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/help/HelpViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/help/HelpViewModel.kt
@@ -1,8 +1,11 @@
 package com.woocommerce.android.support.help
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.R
 import com.woocommerce.android.support.TicketType
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.util.WooLog
+import com.woocommerce.android.util.WooLogWrapper
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,7 +20,26 @@ class HelpViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val wooStore: WooCommerceStore,
     private val selectedSite: SelectedSite,
+    private val wooLogWrapper: WooLogWrapper
 ) : ScopedViewModel(savedState) {
+    var ssr: String? = null
+
+    init {
+        fetchSSR()
+    }
+
+    private fun fetchSSR() {
+        launch {
+            wooLogWrapper.i(WooLog.T.SUPPORT, "Fetching SSR")
+            val result = wooStore.fetchSSR(selectedSite.get())
+            if (result.isError) {
+                wooLogWrapper.e(WooLog.T.SUPPORT, "Error fetching SSR")
+            } else {
+                wooLogWrapper.i(WooLog.T.SUPPORT, "SSR fetched successfully")
+            }
+            ssr = result.model?.toString()
+        }
+    }
 
     fun contactSupport(ticketType: TicketType) {
         if (!selectedSite.exists()) {
