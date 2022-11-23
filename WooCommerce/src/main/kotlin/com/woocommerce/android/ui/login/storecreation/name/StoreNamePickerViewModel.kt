4216590cc7c43bceb5607831e15a3e6d164b8878
JorgeMucientes <jorge.mucientes@automattic.com>
2022-11-23 11:33:34 +0100

    Add dismiss tracking event for native flow

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/StoreNamePickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/name/StoreNamePickerViewModel.kt
@@ -3,9 +3,9 @@ package com.woocommerce.android.ui.login.storecreation.name
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
+import com.woocommerce.android.AppPrefsWrapper
 import com.woocommerce.android.analytics.AnalyticsEvent
-import com.woocommerce.android.analytics.AnalyticsTracker.Companion.KEY_STEP
-import com.woocommerce.android.analytics.AnalyticsTracker.Companion.VALUE_STEP_STORE_NAME
+import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.ui.login.storecreation.NewStore
 import com.woocommerce.android.viewmodel.MultiLiveEvent
@@ -19,7 +19,8 @@ import javax.inject.Inject
 class StoreNamePickerViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val newStore: NewStore,
-    val analyticsTrackerWrapper: AnalyticsTrackerWrapper
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper,
+    private val prefsWrapper: AppPrefsWrapper
 ) : ScopedViewModel(savedStateHandle) {
     private val _storeName = savedState.getStateFlow(scope = this, initialValue = "")
     val storeName: LiveData<String> = _storeName.asLiveData()
@@ -28,12 +29,20 @@ class StoreNamePickerViewModel @Inject constructor(
         analyticsTrackerWrapper.track(
             AnalyticsEvent.SITE_CREATION_STEP,
             mapOf(
-                KEY_STEP to VALUE_STEP_STORE_NAME
+                AnalyticsTracker.KEY_STEP to AnalyticsTracker.VALUE_STEP_STORE_NAME
             )
         )
     }
 
     fun onCancelPressed() {
+        analyticsTrackerWrapper.track(
+            AnalyticsEvent.SITE_CREATION_DISMISSED,
+            mapOf(
+                AnalyticsTracker.KEY_STEP to AnalyticsTracker.VALUE_STEP_STORE_CREATED,
+                AnalyticsTracker.KEY_FLOW to AnalyticsTracker.VALUE_NATIVE,
+                AnalyticsTracker.KEY_SOURCE to prefsWrapper.getStoreCreationSource()
+            )
+        )
         triggerEvent(MultiLiveEvent.Event.Exit)
     }
 
