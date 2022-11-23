8a0504bfe411a76217edc10002f840c2c6daea59
Alejo <alejandro.torres.veiga@automattic.com>
2022-11-23 14:08:19 -0300

    add generate variations picker

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/GenerateVariationPickerDialog.kt
@@ -0,0 +1,42 @@
+package com.woocommerce.android.ui.products.variations
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.woocommerce.android.R
+import com.woocommerce.android.databinding.DialogGenerateVariationsBinding
+
+class GenerateVariationPickerDialog(context: Context) : BottomSheetDialog(context) {
+    private var _binding: DialogGenerateVariationsBinding? = null
+    private val binding get() = _binding!!
+
+    init {
+        _binding = DialogGenerateVariationsBinding.inflate(LayoutInflater.from(context))
+        setContentView(binding.root)
+        binding.allVariation.setOnClickListener {
+            listener?.onGenerateAllVariations()
+            dismiss()
+        }
+        binding.newVariation.setOnClickListener {
+            listener?.onGenerateNewVariation()
+            dismiss()
+        }
+    }
+
+    var totalVariations: Int = 0
+        set(value) {
+            field = value
+            binding.allVariation.visibility = if(value > 0) View.VISIBLE else View.GONE
+            if(value > 0) {
+                binding.allVariationTitle.text = context.getString(R.string.variation_add_all, value)
+            }
+        }
+
+    var listener: GenerateVariationPickerDialogListener? = null
+
+    interface GenerateVariationPickerDialogListener {
+        fun onGenerateAllVariations()
+        fun onGenerateNewVariation()
+    }
+}
