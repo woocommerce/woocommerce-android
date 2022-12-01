e98d14c746774b9cf373f655393aafa222ddc527
Ondrej Ruttkay <ruttkay@gmail.com>
2022-12-01 17:48:28 +0100

    Fix the error preview

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/StoreCreationErrorScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/StoreCreationErrorScreen.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.login.storecreation
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -21,11 +22,14 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R.dimen
 import com.woocommerce.android.R.drawable
 import com.woocommerce.android.R.string
 import com.woocommerce.android.ui.compose.component.WCColoredButton
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
 fun StoreCreationErrorScreen(
@@ -96,6 +100,7 @@ fun StoreCreationErrorScreen(
             }
         }
     }
+}
 
 @Preview(name = "dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview(name = "light", uiMode = Configuration.UI_MODE_NIGHT_NO)
@@ -105,7 +110,7 @@ fun StoreCreationErrorScreen(
 @Composable
 fun StoreCreationErrorPreview() {
     WooThemeWithBackground {
-        StoreCreationError(
+        StoreCreationErrorScreen(
             errorType = StoreCreationErrorType.SITE_CREATION_FAILED,
             onArrowBackPressed = {},
             message = "Error creating store",
@@ -113,4 +118,3 @@ fun StoreCreationErrorPreview() {
         )
     }
 }
-
