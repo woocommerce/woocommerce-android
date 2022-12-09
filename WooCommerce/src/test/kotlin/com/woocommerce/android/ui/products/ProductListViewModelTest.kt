6ad0d35a0b023e3fb1320849219fef2f13efac64
Alejo <alejandro.torres.veiga@automattic.com>
2022-12-09 10:01:44 -0600

    unit test bulk update price

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/ProductListViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/ProductListViewModelTest.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.R
 import com.woocommerce.android.extensions.takeIfNotEqualTo
 import com.woocommerce.android.model.Product
+import com.woocommerce.android.model.RequestResult
 import com.woocommerce.android.tools.NetworkStatus
 import com.woocommerce.android.ui.media.MediaFileUploadHandler
 import com.woocommerce.android.ui.products.ProductListViewModel.ProductListEvent.ShowProductFilterScreen
@@ -14,6 +15,7 @@ import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowSnackbar
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.advanceUntilIdle
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -185,7 +187,7 @@ class ProductListViewModelTest : BaseUnitTest() {
         }
 
     @Test
-    /* We show the Add Product FAB after searching is completed. */
+        /* We show the Add Product FAB after searching is completed. */
     fun `Shows add product button after opening and closing search`() =
         testBlocking {
             // when
@@ -209,7 +211,7 @@ class ProductListViewModelTest : BaseUnitTest() {
         }
 
     @Test
-    /* We hide the filters when searching. */
+        /* We hide the filters when searching. */
     fun `Hides filters buttons when searching`() =
         testBlocking {
             // when
@@ -346,5 +348,45 @@ class ProductListViewModelTest : BaseUnitTest() {
         createViewModel()
 
         verify(productRepository, AtLeast(2)).getProductList()
+    }
+
+    @Test
+    fun `Shows success message when bulk update product's price completes`() = testBlocking {
+        val productIds = listOf(1L, 2L, 3L)
+        val price = "24.45"
+
+        doReturn(RequestResult.SUCCESS).whenever(productRepository).bulkUpdateProductsPrice(productIds, price)
+
+        createViewModel()
+
+        var snackbar: ShowSnackbar? = null
+        viewModel.event.observeForever {
+            if (it is ShowSnackbar) snackbar = it
+        }
+
+        viewModel.onUpdatePriceConfirmed(productIds, price)
+        // We delayed the message waiting the resume animation to complete
+        advanceUntilIdle()
+        assertThat(snackbar).isEqualTo(ShowSnackbar(R.string.product_bulk_update_price_updated))
+    }
+
+    @Test
+    fun `Shows error message when bulk update product's price fails`() = testBlocking {
+        val productIds = listOf(1L, 2L, 3L)
+        val price = "24.45"
+
+        doReturn(RequestResult.ERROR).whenever(productRepository).bulkUpdateProductsPrice(productIds, price)
+
+        createViewModel()
+
+        var snackbar: ShowSnackbar? = null
+        viewModel.event.observeForever {
+            if (it is ShowSnackbar) snackbar = it
+        }
+
+        viewModel.onUpdatePriceConfirmed(productIds, price)
+        // We delayed the message waiting the resume animation to complete
+        advanceUntilIdle()
+        assertThat(snackbar).isEqualTo(ShowSnackbar(R.string.error_generic))
     }
 }
