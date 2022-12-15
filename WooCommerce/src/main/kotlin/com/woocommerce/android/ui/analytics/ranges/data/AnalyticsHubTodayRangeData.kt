9fe197a3bbc46b09c1914f64e134f66a720adeaa
ThomazFB <thomazfcortez@gmail.com>
2022-12-15 15:41:07 -0300

    Introduce basic AnalyticsHubTodayRangeData

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubTodayRangeData.kt
@@ -0,0 +1,12 @@
+package com.woocommerce.android.ui.analytics.ranges.data
+
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
+import java.util.*
+
+class AnalyticsHubTodayRangeData(private val referenceDate: Date): AnalyticsHubTimeRangeData {
+    override val currentRange: AnalyticsHubTimeRange
+        get() = TODO("Not yet implemented")
+    override val previousRange: AnalyticsHubTimeRange
+        get() = TODO("Not yet implemented")
+}
