b2f8d365a2b87628821e552b63531f3a29c19601
ThomazFB <thomazfcortez@gmail.com>
2022-12-15 15:42:50 -0300

    Add initial time range data generation to AnalyticsHubRangeSelectionType

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubRangeSelectionType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubRangeSelectionType.kt
@@ -1,5 +1,8 @@
 package com.woocommerce.android.ui.analytics.ranges
 
+import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubTodayRangeData
+import java.util.*
+
 enum class AnalyticsHubRangeSelectionType {
     TODAY,
     YESTERDAY,
@@ -11,5 +14,22 @@ enum class AnalyticsHubRangeSelectionType {
     MONTH_TO_DATE,
     QUARTER_TO_DATE,
     YEAR_TO_DATE,
-    CUSTOM
+    CUSTOM;
+
+    fun generateTimeRangeData(referenceDate: Date): AnalyticsHubTimeRangeData {
+        return AnalyticsHubTodayRangeData(referenceDate)
+//        return when (this) {
+//            TODAY -> AnalyticsHubTodayTimeRangeData()
+//            YESTERDAY -> AnalyticsHubYesterdayTimeRangeData()
+//            LAST_WEEK -> AnalyticsHubLastWeekTimeRangeData()
+//            LAST_MONTH -> AnalyticsHubLastMonthTimeRangeData()
+//            LAST_QUARTER -> AnalyticsHubLastQuarterTimeRangeData()
+//            LAST_YEAR -> AnalyticsHubLastYearTimeRangeData()
+//            WEEK_TO_DATE -> AnalyticsHubWeekToDateRangeData()
+//            MONTH_TO_DATE -> AnalyticsHubMonthToDateRangeData()
+//            QUARTER_TO_DATE -> AnalyticsHubQuarterToDateRangeData()
+//            YEAR_TO_DATE -> AnalyticsHubYearToDateRangeData()
+//            CUSTOM -> AnalyticsHubCustomTimeRangeData()
+//        }
+    }
 }
