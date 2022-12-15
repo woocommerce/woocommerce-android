1c2a1b48361e0af71db68001628406c740674d73
ThomazFB <thomazfcortez@gmail.com>
2022-12-15 15:00:43 -0300

    Define AnalyticsHubRangeSelectionType class

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
@@ -1,7 +1,7 @@
 package com.woocommerce.android.ui.analytics.ranges
 
 class AnalyticsHubDateRangeSelection(
-    private val selectionType: SelectionType
+    private val selectionType: AnalyticsHubRangeSelectionType
 ) {
     private val currentRange: AnalyticsHubTimeRange?
     private val previousRange: AnalyticsHubTimeRange?
@@ -9,19 +9,5 @@ class AnalyticsHubDateRangeSelection(
     init {
         this.currentRange = null
         this.previousRange = null
-    }
-
-    enum class SelectionType {
-        TODAY,
-        YESTERDAY,
-        LAST_WEEK,
-        LAST_MONTH,
-        LAST_QUARTER,
-        LAST_YEAR,
-        WEEK_TO_DATE,
-        MONTH_TO_DATE,
-        QUARTER_TO_DATE,
-        YEAR_TO_DATE,
-        CUSTOM
     }
 }
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubRangeSelectionType.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.analytics.ranges
+
+enum class AnalyticsHubRangeSelectionType {
+    TODAY,
+    YESTERDAY,
+    LAST_WEEK,
+    LAST_MONTH,
+    LAST_QUARTER,
+    LAST_YEAR,
+    WEEK_TO_DATE,
+    MONTH_TO_DATE,
+    QUARTER_TO_DATE,
+    YEAR_TO_DATE,
+    CUSTOM
+}
