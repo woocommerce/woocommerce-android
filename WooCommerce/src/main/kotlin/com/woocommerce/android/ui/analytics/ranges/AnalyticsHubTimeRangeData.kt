5eadb0154031981b73531ddb3d06529ae1e079e3
ThomazFB <thomazfcortez@gmail.com>
2022-12-15 15:40:43 -0300

    Adjust AnalyticsHubTimeRangeData to become an interface

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRangeData.kt
@@ -1,16 +1,6 @@
 package com.woocommerce.android.ui.analytics.ranges
 
-import java.util.*
-
-abstract class AnalyticsHubTimeRangeData {
-    abstract val currentRangeStart: Date
-    abstract val currentRangeEnd: Date
-    abstract val previousRangeStart: Date
-    abstract val previousRangeEnd: Date
-
+interface AnalyticsHubTimeRangeData {
     val currentRange: AnalyticsHubTimeRange
-        get() = AnalyticsHubTimeRange(currentRangeStart, currentRangeEnd)
-
     val previousRange: AnalyticsHubTimeRange
-        get() = AnalyticsHubTimeRange(previousRangeStart, previousRangeEnd)
 }
