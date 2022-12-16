36c88414090cadb8bfa8e98f58797649d703ebe1
ThomazFB <thomazfcortez@gmail.com>
2022-12-16 12:26:20 -0300

    Define first test covering yesterday scenario

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelectionTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelectionTest.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.analytics.ranges
 import com.woocommerce.android.extensions.endOfToday
 import com.woocommerce.android.extensions.startOfToday
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubRangeSelectionType.TODAY
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubRangeSelectionType.YESTERDAY
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -34,7 +35,34 @@ internal class AnalyticsHubDateRangeSelectionTest {
         )
 
         // When
-        val sut = AnalyticsHubDateRangeSelection(selectionType = TODAY)
+        val sut = AnalyticsHubDateRangeSelection(
+            selectionType = TODAY,
+            currentDate = today
+        )
+
+        // Then
+        assertThat(sut.currentRange).isEqualTo(expectedCurrentRange)
+        assertThat(sut.previousRange).isEqualTo(expectedPreviousRange)
+    }
+
+    @Test
+    fun `when selection type is yesterday, then generate expected date information`() {
+        // Given
+        val today = midDayFrom("2022-07-01")
+        val expectedCurrentRange = AnalyticsHubTimeRange(
+            start = dayStartFrom("2022-06-30"),
+            end = midDayFrom("2022-06-30")
+        )
+        val expectedPreviousRange = AnalyticsHubTimeRange(
+            start = dayStartFrom("2022-06-29"),
+            end = midDayFrom("2022-06-29")
+        )
+
+        // When
+        val sut = AnalyticsHubDateRangeSelection(
+            selectionType = YESTERDAY,
+            currentDate = today
+        )
 
         // Then
         assertThat(sut.currentRange).isEqualTo(expectedCurrentRange)
