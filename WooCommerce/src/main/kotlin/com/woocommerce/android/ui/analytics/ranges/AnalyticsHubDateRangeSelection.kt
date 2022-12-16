a250688d78e5de6decb99a0f0188004a570ef75a
ThomazFB <thomazfcortez@gmail.com>
2022-12-16 11:37:16 -0300

    Initialize AnalyticsHubDateRangeSelection with selection data

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
@@ -1,13 +1,17 @@
 package com.woocommerce.android.ui.analytics.ranges
 
+import java.util.*
+
 class AnalyticsHubDateRangeSelection(
-    private val selectionType: AnalyticsHubRangeSelectionType
+    private val selectionType: AnalyticsHubRangeSelectionType,
+    private val currentDate: Date = Date()
 ) {
     private val currentRange: AnalyticsHubTimeRange?
     private val previousRange: AnalyticsHubTimeRange?
 
     init {
-        this.currentRange = null
-        this.previousRange = null
+        val rangeData = selectionType.generateTimeRangeData(currentDate)
+        this.currentRange = rangeData.currentRange
+        this.previousRange = rangeData.previousRange
     }
 }
