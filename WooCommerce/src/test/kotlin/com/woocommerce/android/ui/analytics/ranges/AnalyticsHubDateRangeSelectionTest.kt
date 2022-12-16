d216b01b2f127354c5b0b5abe94057efcdb47383
ThomazFB <thomazfcortez@gmail.com>
2022-12-16 12:14:18 -0300

    Define AnalyticsHubDateRangeSelectionTest file

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelectionTest.kt
@@ -0,0 +1,11 @@
+package com.woocommerce.android.ui.analytics.ranges
+
+import com.woocommerce.android.extensions.endOfToday
+import com.woocommerce.android.extensions.startOfToday
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubRangeSelectionType.TODAY
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.*
+
+internal class AnalyticsHubDateRangeSelectionTest {
+}
