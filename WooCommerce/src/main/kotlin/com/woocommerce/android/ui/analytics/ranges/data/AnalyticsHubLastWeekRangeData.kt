977cd58a2ad72b4f9432a7ee7be87fe4726ddf93
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 19:49:17 -0300

    Add documentation to week and month ranges

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubLastWeekRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubLastWeekRangeData.kt
@@ -8,6 +8,14 @@ import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
 import java.util.Calendar
 import java.util.Date
 
+// Responsible for defining two ranges of data, one starting from the first day of the last week
+// until the final day of that week, and the previous one as two weeks ago, also starting
+// from the first day until the final day of that week. E. g.
+//
+// Today: 29 Jul 2022
+// Current range: Jul 18 until Jul 24, 2022
+// Previous range: Jul 11 until Jul 17, 2022
+//
 class AnalyticsHubLastWeekRangeData(
     referenceDate: Date,
     calendar: Calendar
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubMonthToDateRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubMonthToDateRangeData.kt
@@ -7,6 +7,14 @@ import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
 import java.util.Calendar
 import java.util.Date
 
+// Responsible for defining two ranges of data, one starting from the first day of the current month
+// until the current date and the previous one, starting from the first day of the previous month
+// until the same day of the previous month. E. g.
+//
+// Today: 31 Jul 2022
+// Current range: Jul 1 until Jul 31, 2022
+// Previous range: Jun 1 until Jun 30, 2022
+//
 class AnalyticsHubMonthToDateRangeData(
     referenceDate: Date,
     calendar: Calendar
