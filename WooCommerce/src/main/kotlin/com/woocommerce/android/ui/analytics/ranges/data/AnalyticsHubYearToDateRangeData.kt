77308201c52f2a3807b1a2b12919b117ec555bc9
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 09:14:57 -0300

    Introduce AnalyticsHubYearToDateRangeData

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubYearToDateRangeData.kt
@@ -0,0 +1,31 @@
+package com.woocommerce.android.ui.analytics.ranges.data
+
+import com.woocommerce.android.extensions.oneYearAgo
+import com.woocommerce.android.extensions.startOfCurrentYear
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
+import java.util.Calendar
+import java.util.Date
+
+class AnalyticsHubYearToDateRangeData(
+    referenceDate: Date,
+    calendar: Calendar
+) : AnalyticsHubTimeRangeData {
+    override val currentRange: AnalyticsHubTimeRange
+    override val previousRange: AnalyticsHubTimeRange
+
+    init {
+        calendar.time = referenceDate
+        currentRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentYear(),
+            end = referenceDate
+        )
+
+        val oneYearAgo = referenceDate.oneYearAgo(calendar)
+        calendar.time = oneYearAgo
+        previousRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentYear(),
+            end = oneYearAgo
+        )
+    }
+}
