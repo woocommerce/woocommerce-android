0ed567b0e781070e33037fffd91d8f3c13cfdfd1
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 19:05:01 -0300

    Add documentation to Today and Yesterday range generation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubTodayRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubTodayRangeData.kt
@@ -4,8 +4,17 @@ import com.woocommerce.android.extensions.startOfCurrentDay
 import com.woocommerce.android.extensions.theDayBeforeIt
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
-import java.util.*
+import java.util.Calendar
+import java.util.Date
 
+// Responsible for defining two ranges of data, one starting from the the first second of the current day
+// until the same day in the current time and the previous one, starting from the first second of
+// yesterday until the same time of that day. E. g.
+//
+// Today: 29 Jul 2022
+// Current range: Jul 29 until Jul 29, 2022
+// Previous range: Jul 28 until Jul 28, 2022
+//
 class AnalyticsHubTodayRangeData(
     referenceDate: Date,
     calendar: Calendar
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubYesterdayRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubYesterdayRangeData.kt
@@ -6,6 +6,14 @@ import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
 import java.util.*
 
+// Responsible for defining two ranges of data, one starting from the the first second of yesterday
+// until the last minute of  the same day and the previous one, starting from the first second of
+// the day before yesterday until the end of that day. E. g.
+//
+// Today: 29 Jul 2022
+// Current range: Jul 28 until Jul 28, 2022
+// Previous range: Jul 27 until Jul 27, 2022
+//
 class AnalyticsHubYesterdayRangeData(
     referenceDate: Date,
     calendar: Calendar
