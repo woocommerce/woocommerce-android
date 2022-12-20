825729d6e9ce32a74424ca077afd78c1e2c0a451
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 19:43:26 -0300

    Adjust new SelectionType placements

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
@@ -1,8 +1,16 @@
 package com.woocommerce.android.ui.analytics.ranges
 
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.AnalyticsHubRangeSelectionType.LAST_MONTH
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.AnalyticsHubRangeSelectionType.LAST_WEEK
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.AnalyticsHubRangeSelectionType.MONTH_TO_DATE
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.AnalyticsHubRangeSelectionType.TODAY
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.AnalyticsHubRangeSelectionType.WEEK_TO_DATE
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.AnalyticsHubRangeSelectionType.YESTERDAY
+import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubLastMonthRangeData
+import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubLastWeekRangeData
+import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubMonthToDateRangeData
 import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubTodayRangeData
+import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubWeekToDateRangeData
 import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubYesterdayRangeData
 import java.util.Calendar
 import java.util.Date
@@ -29,11 +37,19 @@ class AnalyticsHubDateRangeSelection(
         return when (selectedType) {
             TODAY -> AnalyticsHubTodayRangeData(referenceDate, calendar)
             YESTERDAY -> AnalyticsHubYesterdayRangeData(referenceDate, calendar)
+            WEEK_TO_DATE -> AnalyticsHubWeekToDateRangeData(referenceDate, calendar)
+            LAST_WEEK -> AnalyticsHubLastWeekRangeData(referenceDate, calendar)
+            MONTH_TO_DATE -> AnalyticsHubMonthToDateRangeData(referenceDate, calendar)
+            LAST_MONTH -> AnalyticsHubLastMonthRangeData(referenceDate, calendar)
         }
     }
 
     enum class AnalyticsHubRangeSelectionType {
         TODAY,
-        YESTERDAY;
+        YESTERDAY,
+        WEEK_TO_DATE,
+        LAST_WEEK,
+        MONTH_TO_DATE,
+        LAST_MONTH;
     }
 }
