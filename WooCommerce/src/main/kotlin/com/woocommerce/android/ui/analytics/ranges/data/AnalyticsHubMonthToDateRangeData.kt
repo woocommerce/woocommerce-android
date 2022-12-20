bd8cb295272a961a57422317aedbebcb1bd64e63
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 08:05:19 -0300

    Introduce month to date range data

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubMonthToDateRangeData.kt
@@ -0,0 +1,32 @@
+package com.woocommerce.android.ui.analytics.ranges.data
+
+import com.woocommerce.android.extensions.endOfCurrentMonth
+import com.woocommerce.android.extensions.oneMonthAgo
+import com.woocommerce.android.extensions.startOfCurrentMonth
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
+import java.util.Calendar
+import java.util.Date
+
+class AnalyticsHubMonthToDateRangeData(
+    referenceDate: Date,
+    calendar: Calendar
+) : AnalyticsHubTimeRangeData {
+    override val currentRange: AnalyticsHubTimeRange
+    override val previousRange: AnalyticsHubTimeRange
+
+    init {
+        calendar.time = referenceDate
+        currentRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentMonth(),
+            end = referenceDate
+        )
+
+        val oneMonthAgo = referenceDate.oneMonthAgo(calendar)
+        calendar.time = oneMonthAgo
+        previousRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentMonth(),
+            end = oneMonthAgo
+        )
+    }
+}
