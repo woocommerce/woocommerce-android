17b50916a747f1eb4eb9765636509927ec6a87f1
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 11:32:41 -0300

    Introduce AnalyticsHubQuarterToDateRangeData

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubLastQuarterRangeData.kt
@@ -0,0 +1,33 @@
+package com.woocommerce.android.ui.analytics.ranges.data
+
+import com.woocommerce.android.extensions.endOfCurrentQuarter
+import com.woocommerce.android.extensions.oneQuarterAgo
+import com.woocommerce.android.extensions.startOfCurrentQuarter
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
+import java.util.Calendar
+import java.util.Date
+
+class AnalyticsHubLastQuarterRangeData(
+    referenceDate: Date,
+    calendar: Calendar
+) : AnalyticsHubTimeRangeData {
+    override val currentRange: AnalyticsHubTimeRange
+    override val previousRange: AnalyticsHubTimeRange
+
+    init {
+        val oneQuarterAgo = referenceDate.oneQuarterAgo(calendar)
+        calendar.time = oneQuarterAgo
+        currentRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentQuarter(),
+            end = calendar.endOfCurrentQuarter()
+        )
+
+        val twoQuartersAgo = oneQuarterAgo.oneQuarterAgo(calendar)
+        calendar.time = twoQuartersAgo
+        previousRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentQuarter(),
+            end = calendar.endOfCurrentQuarter()
+        )
+    }
+}
