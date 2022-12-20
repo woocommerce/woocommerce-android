3a1229a85c678acb68d34e05f2b9bca48545d52c
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 11:58:14 -0300

    Refactor AnalyticsHubDateRangeSelection to support description text

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubDateRangeSelection.kt
@@ -1,5 +1,7 @@
 package com.woocommerce.android.ui.analytics.ranges
 
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.SelectionType.CUSTOM
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.SelectionType.LAST_MONTH
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.SelectionType.LAST_QUARTER
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.SelectionType.LAST_WEEK
@@ -20,24 +22,28 @@ import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubTodayRangeDa
 import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubWeekToDateRangeData
 import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubYearToDateRangeData
 import com.woocommerce.android.ui.analytics.ranges.data.AnalyticsHubYesterdayRangeData
+import com.woocommerce.android.viewmodel.ResourceProvider
 import java.util.Calendar
 import java.util.Date
 
 class AnalyticsHubDateRangeSelection(
     selectionType: SelectionType,
+    resourceProvider: ResourceProvider,
     currentDate: Date = Date(),
     calendar: Calendar = Calendar.getInstance()
 ) {
     val currentRange: AnalyticsHubTimeRange?
     val previousRange: AnalyticsHubTimeRange?
+    val description: String
 
     init {
         val rangeData = generateTimeRangeData(selectionType, currentDate, calendar)
-        this.currentRange = rangeData.currentRange
-        this.previousRange = rangeData.previousRange
+        currentRange = rangeData.currentRange
+        previousRange = rangeData.previousRange
+        description = generateRangeDescription(selectionType, resourceProvider)
     }
 
-    fun generateTimeRangeData(
+    private fun generateTimeRangeData(
         selectionType: SelectionType,
         referenceDate: Date,
         calendar: Calendar
@@ -53,8 +59,26 @@ class AnalyticsHubDateRangeSelection(
             LAST_QUARTER -> AnalyticsHubLastQuarterRangeData(referenceDate, calendar)
             YEAR_TO_DATE -> AnalyticsHubYearToDateRangeData(referenceDate, calendar)
             LAST_YEAR -> AnalyticsHubLastYearRangeData(referenceDate, calendar)
+            // TODO: support custom range
             else -> AnalyticsHubTodayRangeData(referenceDate, calendar)
         }
+    }
+
+    private fun generateRangeDescription(
+        selectionType: SelectionType,
+        resourceProvider: ResourceProvider
+    ) = when (selectionType) {
+        TODAY -> resourceProvider.getString(R.string.date_timeframe_today)
+        YESTERDAY -> resourceProvider.getString(R.string.date_timeframe_yesterday)
+        LAST_WEEK -> resourceProvider.getString(R.string.date_timeframe_last_week)
+        LAST_MONTH -> resourceProvider.getString(R.string.date_timeframe_last_month)
+        LAST_QUARTER -> resourceProvider.getString(R.string.date_timeframe_last_quarter)
+        LAST_YEAR -> resourceProvider.getString(R.string.date_timeframe_last_year)
+        WEEK_TO_DATE -> resourceProvider.getString(R.string.date_timeframe_week_to_date)
+        MONTH_TO_DATE -> resourceProvider.getString(R.string.date_timeframe_month_to_date)
+        QUARTER_TO_DATE -> resourceProvider.getString(R.string.date_timeframe_quarter_to_date)
+        YEAR_TO_DATE -> resourceProvider.getString(R.string.date_timeframe_year_to_date)
+        CUSTOM -> resourceProvider.getString(R.string.date_timeframe_custom)
     }
 
     enum class SelectionType {
