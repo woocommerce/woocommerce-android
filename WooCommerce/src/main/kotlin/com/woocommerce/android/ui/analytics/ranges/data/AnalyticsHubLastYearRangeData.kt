d4c20cfcc5010cea004f4bf7f4a314ce6328d018
ThomazFB <thomazfcortez@gmail.com>
2022-12-20 09:15:08 -0300

    Introduce AnalyticsHubLastYearRangeData

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubLastYearRangeData.kt
@@ -0,0 +1,33 @@
+package com.woocommerce.android.ui.analytics.ranges.data
+
+import com.woocommerce.android.extensions.endOfCurrentYear
+import com.woocommerce.android.extensions.oneYearAgo
+import com.woocommerce.android.extensions.startOfCurrentYear
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
+import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
+import java.util.Calendar
+import java.util.Date
+
+class AnalyticsHubLastYearRangeData(
+    referenceDate: Date,
+    calendar: Calendar
+) : AnalyticsHubTimeRangeData {
+    override val currentRange: AnalyticsHubTimeRange
+    override val previousRange: AnalyticsHubTimeRange
+
+    init {
+        val oneYearAgo = referenceDate.oneYearAgo(calendar)
+        calendar.time = oneYearAgo
+        currentRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentYear(),
+            end = calendar.endOfCurrentYear()
+        )
+
+        val twoYearsAgo = oneYearAgo.oneYearAgo(calendar)
+        calendar.time = twoYearsAgo
+        previousRange = AnalyticsHubTimeRange(
+            start = calendar.startOfCurrentYear(),
+            end = calendar.endOfCurrentYear()
+        )
+    }
+}
