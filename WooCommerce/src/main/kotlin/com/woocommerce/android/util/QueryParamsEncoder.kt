7e3c2dbf88aa624be4d4dddedb4878dab66ac794
Anirudh Uppunda <abhat38@gmail.com>
2022-12-21 12:00:36 +0530

    inject buildConfigWrapper into QueryParamsEncoder.kt class to determine the build type

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/QueryParamsEncoder.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/QueryParamsEncoder.kt
@@ -4,9 +4,11 @@ import com.woocommerce.android.BuildConfig
 import java.net.URLEncoder
 import javax.inject.Inject
 
-class QueryParamsEncoder @Inject constructor() {
+class QueryParamsEncoder @Inject constructor(
+    private val buildConfigWrapper: BuildConfigWrapper
+) {
     fun getEncodedQueryParams(): String {
-        val query = if (BuildConfig.DEBUG) {
+        val query = if (buildConfigWrapper.debug) {
             "build_type=developer&platform=android&version=${BuildConfig.VERSION_NAME}"
         } else {
             "platform=android&version=${BuildConfig.VERSION_NAME}"
--- WooCommerce/src/test/kotlin/com/woocommerce/android/util/QueryParamsEncoderTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/util/QueryParamsEncoderTest.kt
@@ -3,30 +3,45 @@ package com.woocommerce.android.util
 import com.woocommerce.android.BuildConfig
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import java.net.URLEncoder
 
 class QueryParamsEncoderTest {
-    private val queryParramsEncoder = QueryParamsEncoder()
+    private val buildConfigWrapper: BuildConfigWrapper = mock()
+    private val queryParramsEncoder = QueryParamsEncoder(buildConfigWrapper)
+
     @Test
-    fun `when getEncodedQueryParams called, then proper encoded query params returned`() {
+    fun `given debug build, when getEncodedQueryParams called, then proper encoded query params returned`() {
+        // GIVEN
+        whenever(buildConfigWrapper.debug).thenReturn(BuildConfig.DEBUG)
+
         // WHEN
         val encoderQueryParams = queryParramsEncoder.getEncodedQueryParams()
 
         // THEN
-        if (BuildConfig.DEBUG) {
-            assertThat(encoderQueryParams).isEqualTo(
-                URLEncoder.encode(
-                    "build_type=developer&platform=android&version=${BuildConfig.VERSION_NAME}",
-                    Charsets.UTF_8.name()
-                )
+        assertThat(encoderQueryParams).isEqualTo(
+            URLEncoder.encode(
+                "build_type=developer&platform=android&version=${BuildConfig.VERSION_NAME}",
+                Charsets.UTF_8.name()
             )
-        } else {
-            assertThat(encoderQueryParams).isEqualTo(
-                URLEncoder.encode(
-                    "platform=android&version=${BuildConfig.VERSION_NAME}",
-                    Charsets.UTF_8.name()
-                )
+        )
+    }
+
+    @Test
+    fun `given release build, when getEncodedQueryParams called, then proper encoded query params returned`() {
+        // GIVEN
+        whenever(buildConfigWrapper.debug).thenReturn(false)
+
+        // WHEN
+        val encoderQueryParams = queryParramsEncoder.getEncodedQueryParams()
+
+        // THEN
+        assertThat(encoderQueryParams).isEqualTo(
+            URLEncoder.encode(
+                "platform=android&version=${BuildConfig.VERSION_NAME}",
+                Charsets.UTF_8.name()
             )
-        }
+        )
     }
 }
