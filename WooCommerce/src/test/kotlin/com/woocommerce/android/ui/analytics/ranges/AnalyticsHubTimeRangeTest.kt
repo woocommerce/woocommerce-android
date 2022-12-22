d490cd8a11d32588e643a57e82af5eb0475e3562
ThomazFB <thomazfcortez@gmail.com>
2022-12-22 13:01:56 -0300

    Configure AnalyticsHubTimeRangeTest full test scenario

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRangeTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRangeTest.kt
@@ -22,19 +22,6 @@ internal class AnalyticsHubTimeRangeTest {
         testCalendar.firstDayOfWeek = Calendar.MONDAY
     }
 
-//    func test_when_time_range_format_is_simplified_then_describes_only_first_date() {
-//        // Given
-//        let startDate = startDate(from: "2022-07-01")!
-//        let endDate = endDate(from: "2022-07-02")!
-//        let timeRange = AnalyticsHubTimeRange(start: startDate, end: endDate)
-//
-//        // When
-//        let description = timeRange.formatToString(simplified: true, timezone: testTimezone, calendar: testCalendar)
-//
-//        //Then
-//        XCTAssertEqual(description, "Jul 1, 2022")
-//    }
-
     fun `when time range format is simplified then describes only first date`() {
         // Given
         val sut = AnalyticsHubTimeRange(
@@ -43,50 +30,53 @@ internal class AnalyticsHubTimeRangeTest {
         )
 
         // When
-        val description = sut.generateDescription(simplified = true)
+        val description = sut.generateDescription(simplified = true, testCalendar)
 
         //Then
         assertThat(description).isEqualTo("Jul 1, 2022")
     }
-//
-//    func test_when_time_range_format_is_in_different_months_then_describes_both_dates() {
-//        // Given
-//        let startDate = startDate(from: "2022-07-01")!
-//        let endDate = endDate(from: "2022-08-02")!
-//        let timeRange = AnalyticsHubTimeRange(start: startDate, end: endDate)
-//
-//        // When
-//        let description = timeRange.formatToString(simplified: false, timezone: testTimezone, calendar: testCalendar)
-//
-//        //Then
-//        XCTAssertEqual(description, "Jul 1 - Aug 2, 2022")
-//    }
-//
-//    func test_when_time_range_is_in_the_same_month_then_describe_month_only_in_the_start_date() {
-//        // Given
-//        let startDate = startDate(from: "2022-07-01")!
-//        let endDate = endDate(from: "2022-07-05")!
-//        let timeRange = AnalyticsHubTimeRange(start: startDate, end: endDate)
-//
-//        // When
-//        let description = timeRange.formatToString(simplified: false, timezone: testTimezone, calendar: testCalendar)
-//
-//        //Then
-//        XCTAssertEqual(description, "Jul 1 - 5, 2022")
-//    }
-//
-//    func test_when_time_range_is_in_different_years_then_fully_describe_both_dates() {
-//        // Given
-//        let startDate = startDate(from: "2021-04-15")!
-//        let endDate = endDate(from: "2022-04-15")!
-//        let timeRange = AnalyticsHubTimeRange(start: startDate, end: endDate)
-//
-//        // When
-//        let description = timeRange.formatToString(simplified: false, timezone: testTimezone, calendar: testCalendar)
-//
-//        //Then
-//        XCTAssertEqual(description, "Apr 15, 2021 - Apr 15, 2022")
-//    }
+
+    fun `when time range format is in different months then describes both dates`() {
+        // Given
+        val sut = AnalyticsHubTimeRange(
+            start = dayStartFrom("2022-07-01"),
+            end = dayEndFrom("2022-08-02")
+        )
+
+        // When
+        val description = sut.generateDescription(simplified = false, testCalendar)
+
+        //Then
+        assertThat(description).isEqualTo("Jul 1 - Aug 2, 2022")
+    }
+
+    fun `when time range is in the same month then describe month only in the start date`() {
+        // Given
+        val sut = AnalyticsHubTimeRange(
+            start = dayStartFrom("2022-07-01"),
+            end = dayEndFrom("2022-07-05")
+        )
+
+        // When
+        val description = sut.generateDescription(simplified = false, testCalendar)
+
+        //Then
+        assertThat(description).isEqualTo("Jul 1 - 5, 2022")
+    }
+
+    fun `when time range is in different years then fully describe both dates`() {
+        // Given
+        val sut = AnalyticsHubTimeRange(
+            start = dayStartFrom("2021-04-15"),
+            end = dayEndFrom("2022-04-15")
+        )
+
+        // When
+        val description = sut.generateDescription(simplified = false, testCalendar)
+
+        //Then
+        assertThat(description).isEqualTo("Apr 15, 2021 - Apr 15, 2022")
+    }
 
     private fun midDayFrom(date: String): Date {
         val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
