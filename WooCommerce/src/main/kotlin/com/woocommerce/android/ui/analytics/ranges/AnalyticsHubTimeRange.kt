fa632f82d0a221bb1473eb4393dfc97e4b5f22a3
ThomazFB <thomazfcortez@gmail.com>
2022-12-22 21:48:05 -0300

    Implement simplified description case

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRange.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRange.kt
@@ -1,8 +1,9 @@
 package com.woocommerce.android.ui.analytics.ranges
 
+import com.woocommerce.android.extensions.formatToDayMonthYear
 import java.io.Serializable
-import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
 data class AnalyticsHubTimeRange(
     val start: Date,
@@ -11,12 +12,13 @@ data class AnalyticsHubTimeRange(
     val description: String
         get() = "$start - $end"
 
-    fun generateDescription(simplified: Boolean, calendar: Calendar): String {
-        calendar.time
+    fun generateDescription(simplified: Boolean, locale: Locale): String {
         if (simplified) {
-            return "simplified"
-        } else {
-            return description
+            return start.formatToDayMonthYear(locale)
         }
+
+
+
+        return description
     }
 }
