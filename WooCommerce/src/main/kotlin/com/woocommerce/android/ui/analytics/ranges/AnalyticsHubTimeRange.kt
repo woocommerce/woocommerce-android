380ff1a0d2858714a9686d12872f8d8157a7e1df
ThomazFB <thomazfcortez@gmail.com>
2022-12-22 21:58:34 -0300

    Implement functional date formatting

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRange.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/AnalyticsHubTimeRange.kt
@@ -1,7 +1,12 @@
 package com.woocommerce.android.ui.analytics.ranges
 
+import com.woocommerce.android.extensions.formatToDayMonth
 import com.woocommerce.android.extensions.formatToDayMonthYear
+import com.woocommerce.android.extensions.formatToDayYear
+import com.woocommerce.android.extensions.isInSameMonthAs
+import com.woocommerce.android.extensions.isInSameYearAs
 import java.io.Serializable
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
@@ -12,13 +17,23 @@ data class AnalyticsHubTimeRange(
     val description: String
         get() = "$start - $end"
 
-    fun generateDescription(simplified: Boolean, locale: Locale): String {
+    fun generateDescription(simplified: Boolean, locale: Locale, calendar: Calendar): String {
         if (simplified) {
             return start.formatToDayMonthYear(locale)
         }
 
+        val formattedStartDate = if (start.isInSameYearAs(end, calendar)) {
+            start.formatToDayMonth(locale)
+        } else {
+            start.formatToDayMonthYear(locale)
+        }
 
+        val formattedEndDate = if (start.isInSameMonthAs(end, calendar)) {
+            end.formatToDayYear(locale)
+        } else {
+            end.formatToDayMonthYear(locale)
+        }
 
-        return description
+        return "$formattedStartDate - $formattedEndDate"
     }
 }
