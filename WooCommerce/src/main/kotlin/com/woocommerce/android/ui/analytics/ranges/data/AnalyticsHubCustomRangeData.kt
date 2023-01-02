2f1601a5d8e959a6c71314c5ac63746c3fe59af0
ThomazFB <thomazfcortez@gmail.com>
2023-01-02 16:39:15 -0300

    Introduce expected range logic to AnalyticsHubCustomRangeData

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubCustomRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/AnalyticsHubCustomRangeData.kt
@@ -1,16 +1,32 @@
 package com.woocommerce.android.ui.analytics.ranges.data
 
+import com.woocommerce.android.extensions.oneDayAgo
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRange
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
+import java.util.Calendar
 import java.util.Date
 
 class AnalyticsHubCustomRangeData(
-    val startDate: Date,
-    val endDate: Date
+    startDate: Date,
+    endDate: Date,
+    calendar: Calendar
 ) : AnalyticsHubTimeRangeData {
     override val currentRange: AnalyticsHubTimeRange
-        get() = AnalyticsHubTimeRange(startDate, endDate)
-
     override val previousRange: AnalyticsHubTimeRange
-        get() = AnalyticsHubTimeRange(startDate, endDate)
+
+    init {
+        val dayDifference = Date(endDate.time - startDate.time)
+        val previousEnd = startDate.oneDayAgo(calendar)
+        val previousStart = Date(previousEnd.time - dayDifference.time)
+
+        currentRange = AnalyticsHubTimeRange(
+            start = startDate,
+            end = endDate
+        )
+
+        previousRange = AnalyticsHubTimeRange(
+            start = previousStart,
+            end = previousEnd
+        )
+    }
 }
