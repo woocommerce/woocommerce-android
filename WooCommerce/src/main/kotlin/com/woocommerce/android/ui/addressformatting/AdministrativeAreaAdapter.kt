47afd99f9f6f11639196563f5c922ccd37cd81a4
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-01-03 17:51:26 +0100

    refactor: improve naming

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/addressformatting/AdministrativeAreaAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/addressformatting/AdministrativeAreaAdapter.kt
@@ -1,14 +1,13 @@
 package com.woocommerce.android.ui.addressformatting
 
-import com.woocommerce.android.model.Location
+import com.woocommerce.android.model.AmbiguousLocation
 import com.woocommerce.android.ui.orders.details.editing.address.LocationCode
 
-object AdministrativeAreaAdapter {
-
-    fun determineAreaForLibAddressInputLibrary(countryCode: LocationCode, adminArea: Location): String =
+fun AmbiguousLocation.presentationName(countryCode: LocationCode): String =
+    this.asLocation().run {
         if (countryCode == "JP" || countryCode == "TR") {
-            adminArea.name
+            name
         } else {
-            adminArea.code
+            code
         }
-}
+    }
