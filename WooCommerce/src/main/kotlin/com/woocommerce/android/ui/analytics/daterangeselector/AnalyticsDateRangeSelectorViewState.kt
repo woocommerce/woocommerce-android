c85e675ecb4573e39747cec572a7297859b3e7b8
ThomazFB <thomazfcortez@gmail.com>
2023-01-03 13:26:23 -0300

    Remove no longer used `availableRangeDates` value from AnalyticsDateRangeSelectorViewState.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/daterangeselector/AnalyticsDateRangeSelectorViewState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/daterangeselector/AnalyticsDateRangeSelectorViewState.kt
@@ -3,14 +3,12 @@ package com.woocommerce.android.ui.analytics.daterangeselector
 data class AnalyticsDateRangeSelectorViewState(
     val toDatePeriod: String,
     val fromDatePeriod: String,
-    val availableRangeDates: List<String>,
     val selectedPeriod: String
 ) {
     companion object {
         val EMPTY = AnalyticsDateRangeSelectorViewState(
             toDatePeriod = "",
             fromDatePeriod = "",
-            availableRangeDates = emptyList(),
             selectedPeriod = ""
         )
     }
