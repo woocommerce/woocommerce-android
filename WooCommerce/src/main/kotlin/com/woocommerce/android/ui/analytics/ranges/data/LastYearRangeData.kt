95ca0700e14c86e3894d0082eb5847a17243d6b8
ThomazFB <thomazfcortez@gmail.com>
2023-01-04 14:06:48 -0300

    Add documentation to the year and quarter date range classes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/LastYearRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/LastYearRangeData.kt
@@ -8,6 +8,15 @@ import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
 import java.util.Calendar
 import java.util.Date
 
+// Responsible for defining two ranges of data, the current one, starting from January 1st of the last year
+// until December 31th of that same year, and the previous one as two years ago, also ranging
+// all days of that year. E. g.
+//
+// Today: 29 Jul 2022
+// When user requests report at 05:49 PM
+// Current range: Jan 1, 00:00 until Dec 31, 23:59:59, 2021
+// Previous range: Jan 1, 00:00 until Dec 31, 23:59:59, 2020
+//
 class LastYearRangeData(
     referenceDate: Date,
     referenceCalendar: Calendar
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/QuarterToDateRangeData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/ranges/data/QuarterToDateRangeData.kt
@@ -7,6 +7,15 @@ import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubTimeRangeData
 import java.util.Calendar
 import java.util.Date
 
+// Responsible for defining two ranges of data, the current one, starting from the first day of the current quarter
+// until the current date and the previous one, starting from the first day of the previous quarter
+// until the same relative day of the previous quarter. E. g.
+//
+// Today: 15 Feb 2022
+// When user requests report at 05:49 PM
+// Current range: Jan 1, 00:00 until Feb 15, 05:49 PM, 2022
+// Previous range: Oct 1, 00:00 until Nov 15, 05:49 PM, 2021
+//
 class QuarterToDateRangeData(
     referenceDate: Date,
     referenceCalendar: Calendar
