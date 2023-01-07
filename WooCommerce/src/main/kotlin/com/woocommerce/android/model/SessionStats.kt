bf9d35688a0521fb129510a63852c5a4471dff12
ThomazFB <thomazfcortez@gmail.com>
2023-01-07 13:43:58 -0300

    Adjust SessionStats to have an EMPTY static field

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/SessionStats.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/SessionStats.kt
@@ -1,6 +1,13 @@
 package com.woocommerce.android.model
 
 data class SessionStats(
-    val conversionRate: Int,
+    val visitorsCount: Int,
     val viewsCount: Int
-)
+) {
+    companion object {
+        val EMPTY = SessionStats(
+            visitorsCount = 0,
+            viewsCount = 0
+        )
+    }
+}
