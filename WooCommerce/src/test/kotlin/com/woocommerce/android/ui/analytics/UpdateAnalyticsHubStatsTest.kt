cfba9ef5c88f81902613448182397abfaaf731b6
ThomazFB <thomazfcortez@gmail.com>
2023-01-10 11:42:49 -0300

    Define UpdateAnalyticsHubStatsTest.kt

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/UpdateAnalyticsHubStatsTest.kt
@@ -0,0 +1,25 @@
+package com.woocommerce.android.ui.analytics
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+internal class UpdateAnalyticsHubStatsTest {
+    private lateinit var analyticsRepository: AnalyticsRepository
+
+    private lateinit var sut: UpdateAnalyticsHubStats
+
+    @Before
+    fun setUp() {
+        analyticsRepository = mock()
+        sut = UpdateAnalyticsHubStats(
+            analyticsRepository = analyticsRepository
+        )
+    }
+
+    @Test
+    fun `initial test`() {
+        assertThat(sut).isNotNull
+    }
+}
