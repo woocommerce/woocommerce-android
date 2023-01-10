b7329f5205268783b15ee4d23af75801f6b34b6c
ThomazFB <thomazfcortez@gmail.com>
2023-01-09 22:49:43 -0300

    Improve SessionStat.kt to make conversionRate a computed property

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/SessionStat.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/SessionStat.kt
@@ -1,13 +1,21 @@
 package com.woocommerce.android.model
 
+import java.text.DecimalFormat
+
 data class SessionStat(
-    val conversionRate: String,
-    val viewsCount: Int
+    val ordersCount: Int,
+    val visitorsCount: Int
 ) {
+    val conversionRate: String
+        get() = when {
+            visitorsCount > 0 -> (ordersCount / visitorsCount.toFloat()) * 100
+            else -> 0F
+        }.let { DecimalFormat("##.#").format(conversionRate) + "%" }
+
     companion object {
         val EMPTY = SessionStat(
-            conversionRate = "",
-            viewsCount = 0
+            ordersCount = 0,
+            visitorsCount = 0
         )
     }
 }
