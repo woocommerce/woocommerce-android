6b27aa41e2236beb9888468a7cf8bafc48eae817
ThomazFB <thomazfcortez@gmail.com>
2023-01-11 15:34:43 -0300

    Fix lint issues

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/daterangeselector/AnalyticsDateRangeSelectorViewState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/daterangeselector/AnalyticsDateRangeSelectorViewState.kt
@@ -1,17 +1,12 @@
 package com.woocommerce.android.ui.analytics.daterangeselector
 
-import android.content.res.Resources
 import com.woocommerce.android.ui.analytics.ranges.AnalyticsHubDateRangeSelection.SelectionType
-import com.woocommerce.android.viewmodel.ResourceProvider
 
 data class AnalyticsDateRangeSelectorViewState(
     val currentRange: String,
     val previousRange: String,
     val selectionType: SelectionType
 ) {
-    fun generateSelectionTitle(resources: Resources) =
-        resources.getString(selectionType.localizedResourceId)
-
     companion object {
         val EMPTY = AnalyticsDateRangeSelectorViewState(
             currentRange = "",
