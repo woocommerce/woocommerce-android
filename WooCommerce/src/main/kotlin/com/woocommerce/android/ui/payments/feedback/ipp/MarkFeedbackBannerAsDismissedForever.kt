47384b7ac1ad2024aa9762b65160975cffcf3603
samiuelson <samiuelson@gmail.com>
2023-01-13 15:01:04 +0100

    Add use cases for storing banner actions in prefs

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/feedback/ipp/MarkFeedbackBannerAsDismissedForever.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.payments.feedback.ipp
+
+import com.woocommerce.android.AppPrefsWrapper
+import javax.inject.Inject
+
+/**
+ * This class is used to mark the IPP feedback banner as dismissed forever.
+ */
+class MarkFeedbackBannerAsDismissedForever @Inject constructor(
+    private val prefs: AppPrefsWrapper,
+) {
+    operator fun invoke() {
+        prefs.setIPPFeedbackBannerDismissedForever(true)
+    }
+}
