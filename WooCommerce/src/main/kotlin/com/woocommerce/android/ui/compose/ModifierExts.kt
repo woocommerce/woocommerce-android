a25f6f5cf895fcc3eb7635b23b8a3e6ad1398475
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-01-26 15:58:12 +0100

    Add a new modifier to handle auto mirroring on RTL languages

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/ModifierExts.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/ModifierExts.kt
@@ -1,12 +1,17 @@
 package com.woocommerce.android.ui.compose
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
 fun Modifier.drawShadow(
@@ -39,4 +44,12 @@ fun Modifier.drawShadow(
             paint
         )
     }
+}
+
+@Stable
+fun Modifier.autoMirror(): Modifier = composed {
+    if (LocalLayoutDirection.current == LayoutDirection.Rtl)
+        this.scale(scaleX = -1f, scaleY = 1f)
+    else
+        this
 }
