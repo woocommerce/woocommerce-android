b05a01e9c7ab2fae50dac1b179ecfecac41d44bf
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-02-15 14:06:47 +0100

    Remove unused constant

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/ExperimentTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/ExperimentTracker.kt
@@ -4,7 +4,6 @@ import com.google.firebase.analytics.ktx.ParametersBuilder
 
 interface ExperimentTracker {
     companion object {
-        const val PROLOGUE_EXPERIMENT_ELIGIBLE_EVENT = "prologue_carousel_displayed"
         const val LOGIN_SUCCESSFUL_EVENT = "login_successful"
         const val SITE_VERIFICATION_SUCCESSFUL_EVENT = "site_verification_successful"
     }
