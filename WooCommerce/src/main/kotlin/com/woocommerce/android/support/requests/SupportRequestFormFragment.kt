03d31055d23609674eb81aa769d8aab3fff09b7f
ThomazFB <thomazfcortez@gmail.com>
2023-02-17 18:15:22 -0300

    Configure basic injectors and constructors on both SupportRequestForm classes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormFragment.kt
@@ -1,4 +1,11 @@
 package com.woocommerce.android.support.requests
 
-class SupportRequestFormFragment {
+import androidx.fragment.app.viewModels
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SupportRequestFormFragment : BaseFragment(R.layout.fragment_support_request_form) {
+    private val viewModel: SupportRequestFormViewModel by viewModels()
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormViewModel.kt
@@ -1,4 +1,14 @@
 package com.woocommerce.android.support.requests
 
-class SupportRequestFormViewModel {
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.support.ZendeskHelper
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SupportRequestFormViewModel @Inject constructor(
+    private val zendeskHelper: ZendeskHelper,
+    savedState: SavedStateHandle
+) : ScopedViewModel(savedState) {
 }
