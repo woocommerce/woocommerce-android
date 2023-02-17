81966620fdeb356df673d6da4bcedfa862c3483d
malinajirka <malinajirka@gmail.com>
2023-02-17 08:08:05 +0100

    Extra method and use AnalyticsTrackerWrapper

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditViewModel.kt
@@ -327,15 +327,7 @@ class OrderCreateEditViewModel @Inject constructor(
                 viewState = viewState.copy(isProgressDialogShown = true)
                 orderCreateEditRepository.placeOrder(order).fold(
                     onSuccess = {
-                        AnalyticsTracker.track(
-                            ORDER_CREATION_SUCCESS,
-                            mutableMapOf<String, String>().also { mutableMap ->
-                                OrderDurationRecorder.millisecondsSinceOrderAddNew().getOrNull()?.let { timeElapsed ->
-                                    mutableMap[AnalyticsTracker.KEY_TIME_ELAPSED_SINCE_ADD_NEW_ORDER_IN_MILLIS] =
-                                        timeElapsed.toString()
-                                }
-                            }
-                        )
+                        trackOrderCreationSuccess()
                         triggerEvent(ShowSnackbar(string.order_creation_success_snackbar))
                         triggerEvent(ShowCreatedOrder(it.id))
                     },
@@ -350,6 +342,18 @@ class OrderCreateEditViewModel @Inject constructor(
                 triggerEvent(Exit)
             }
         }
+    }
+
+    private fun trackOrderCreationSuccess() {
+        tracker.track(
+            ORDER_CREATION_SUCCESS,
+            mutableMapOf<String, String>().also { mutableMap ->
+                OrderDurationRecorder.millisecondsSinceOrderAddNew().getOrNull()?.let { timeElapsed ->
+                    mutableMap[AnalyticsTracker.KEY_TIME_ELAPSED_SINCE_ADD_NEW_ORDER_IN_MILLIS] =
+                        timeElapsed.toString()
+                }
+            }
+        )
     }
 
     fun onBackButtonClicked() {
