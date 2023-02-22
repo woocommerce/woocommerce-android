422aa8d89373be2e89e5317924e2e92e9ab3ed9a
ThomazFB <thomazfcortez@gmail.com>
2023-02-22 18:48:35 -0300

    Introduce viewState stateFlow

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormViewModel.kt
@@ -2,13 +2,16 @@ package com.woocommerce.android.support.requests
 
 import android.content.Context
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.support.TicketType
 import com.woocommerce.android.support.ZendeskHelper
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlinx.coroutines.flow.map
 
 @HiltViewModel
 class SupportRequestFormViewModel @Inject constructor(
@@ -16,6 +19,11 @@ class SupportRequestFormViewModel @Inject constructor(
     private val selectedSite: SelectedSite,
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
+    val viewState = savedState.getStateFlow(
+        scope = viewModelScope,
+        initialValue = ViewState.EMPTY
+    )
+
     fun onSubmitRequestButtonClicked(
         context: Context,
         ticketType: TicketType,
@@ -29,13 +37,18 @@ class SupportRequestFormViewModel @Inject constructor(
         val helpOption: HelpOption,
         val subject: String,
         val message: String
-    )
+    ) {
+        companion object {
+            val EMPTY = ViewState(HelpOption.None, "", "")
+        }
+    }
 
-    sealed class HelpOption(val ticketType: TicketType) {
+    sealed class HelpOption(val ticketType: TicketType?) {
         object MobileApp: HelpOption(TicketType.General)
         object InPersonPayments: HelpOption(TicketType.Payments)
         object Payments: HelpOption(TicketType.Payments)
         object WooPlugin: HelpOption(TicketType.General)
         object OtherPlugins: HelpOption(TicketType.General)
+        object None: HelpOption(null)
     }
 }
