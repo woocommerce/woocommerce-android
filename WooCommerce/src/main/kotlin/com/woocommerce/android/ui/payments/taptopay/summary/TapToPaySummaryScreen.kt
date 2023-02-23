84548ce8aae5790fc3d724a422bf6e8839ff8ffa
Andrey <andrey.kidinov@a8c.com>
2023-02-23 11:53:27 +0400

    State and UI in progress

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryScreen.kt
@@ -1,8 +1,87 @@
 package com.woocommerce.android.ui.payments.taptopay.summary
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.woocommerce.android.R.dimen
+import com.woocommerce.android.ui.compose.component.Toolbar
+import com.woocommerce.android.ui.compose.component.WCColoredButton
+import com.woocommerce.android.ui.payments.taptopay.summary.TapToPaySummaryViewModel.UiState
 
 @Composable
 fun TapToPaySummaryScreen(viewModel: TapToPaySummaryViewModel) {
-    val state by viewModel.uiState.collectAsState()
+    viewModel.uiState.observeAsState().value?.let { state ->
+        TapToPaySummaryScreen(
+            state,
+            onTryPaymentClicked = viewModel::onTryPaymentClicked,
+            onBackClick = viewModel::onBackClicked
+        )
+    }
+}
+
+@Composable
+fun TapToPaySummaryScreen(
+    state: UiState,
+    onTryPaymentClicked: () -> Unit,
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            Toolbar(
+                title = stringResource(id = state.screenTitleText),
+                onNavigationButtonClick = onBackClick,
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colors.surface)
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(dimensionResource(id = dimen.major_200)))
+            Text(
+                text = stringResource(id = state.titleText),
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(id = dimen.major_200)))
+            Image(
+                painter = painterResource(id = state.illustration),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(id = dimen.major_200)))
+
+            WCColoredButton(onClick = onTryPaymentClicked) {
+
+            }
+        }
+    }
+}
+
+@Preview(name = "Light mode")
+@Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun TapToPaySummaryScreenPreview() {
+    TapToPaySummaryScreen(
+        state = UiState,
+        onTryPaymentClicked = {},
+        onBackClick = {}
+    )
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryViewModel.kt
@@ -1,16 +1,37 @@
 package com.woocommerce.android.ui.payments.taptopay.summary
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import com.woocommerce.android.R
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class TapToPaySummaryViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
 ) : ScopedViewModel(savedStateHandle) {
-    private val _uiState = MutableStateFlow(GameUiState())
-    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(UiState)
+    val uiState: LiveData<UiState> = _uiState.asLiveData()
+
+    fun onTryPaymentClicked() {
+
+    }
+
+    fun onBackClicked() {
+
+    }
+
+    object UiState {
+        const val screenTitleText = R.string.card_reader_tap_to_pay_explanation_screen_title
+        const val titleText = R.string.card_reader_tap_to_pay_explanation_title
+        const val illustration = R.drawable.img_card_reader_available
+        const val explanationOneText = R.string.card_reader_tap_to_pay_explanation_one
+        const val explanationTwoText = R.string.card_reader_tap_to_pay_explanation_two
+        const val explanationThreeText = R.string.card_reader_tap_to_pay_explanation_three
+        const val buttonText = R.string.card_reader_tap_to_pay_explanation_try_payment
+        const val learnMoreText = R.string.card_reader_tap_to_pay_explanation_learn_more
+    }
 }
