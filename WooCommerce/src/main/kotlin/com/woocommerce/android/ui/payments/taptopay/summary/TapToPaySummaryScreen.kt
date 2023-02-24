29574309256d22cff3f16a36dcd4c45397db1a02
Andrey <andrey.kidinov@a8c.com>
2023-02-24 15:36:04 +0400

    UI changes as per feedback from the design team

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryScreen.kt
@@ -1,66 +1,50 @@
 package com.woocommerce.android.ui.payments.taptopay.summary
 
 import android.content.res.Configuration
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
-import com.woocommerce.android.ui.compose.annotatedStringRes
 import com.woocommerce.android.ui.compose.component.Toolbar
 import com.woocommerce.android.ui.compose.component.WCColoredButton
-import com.woocommerce.android.ui.payments.taptopay.summary.TapToPaySummaryViewModel.UiState
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
 fun TapToPaySummaryScreen(viewModel: TapToPaySummaryViewModel) {
-    viewModel.uiState.observeAsState().value?.let { state ->
-        TapToPaySummaryScreen(
-            state,
-            onTryPaymentClicked = viewModel::onTryPaymentClicked,
-            onLearnMoreClicked = viewModel::onLearnMoreClicked,
-            onBackClick = viewModel::onBackClicked
-        )
-    }
+    TapToPaySummaryScreen(
+        onTryPaymentClicked = viewModel::onTryPaymentClicked,
+        onBackClick = viewModel::onBackClicked
+    )
 }
 
 @Composable
 fun TapToPaySummaryScreen(
-    state: UiState,
     onTryPaymentClicked: () -> Unit,
-    onLearnMoreClicked: () -> Unit,
     onBackClick: () -> Unit,
 ) {
     Scaffold(
         topBar = {
             Toolbar(
-                title = stringResource(id = state.screenTitleText),
+                title = stringResource(id = R.string.card_reader_tap_to_pay_explanation_screen_title),
                 onNavigationButtonClick = onBackClick,
             )
         }
@@ -69,113 +53,78 @@ fun TapToPaySummaryScreen(
             modifier = Modifier
                 .background(MaterialTheme.colors.surface)
                 .padding(paddingValues)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = CenterHorizontally,
+            verticalArrangement = SpaceBetween
         ) {
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_200)))
             Text(
-                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.major_100)),
-                text = stringResource(id = state.titleText),
-                style = MaterialTheme.typography.h6,
-                fontWeight = FontWeight.Bold
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.major_200)),
+                text = stringResource(id = R.string.card_reader_tap_to_pay_explanation_title),
+                style = MaterialTheme.typography.h5,
+                textAlign = Center,
             )
-            Spacer(modifier = Modifier.weight(1.0f))
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_100)))
             Image(
-                painter = painterResource(id = state.illustration),
+                painter = painterResource(id = R.drawable.img_tap_to_pay_summary),
                 contentDescription = null,
             )
-            Spacer(modifier = Modifier.weight(1.0f))
-
-            ExplanationRaw("1", stringResource(id = state.explanationOneText))
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.minor_100)))
-            ExplanationRaw("2", stringResource(id = state.explanationTwoText))
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.minor_100)))
-            ExplanationRaw("3", stringResource(id = state.explanationThreeText))
-
-            Spacer(modifier = Modifier.weight(1.0f))
-
-            WCColoredButton(
-                modifier = Modifier
-                    .padding(horizontal = dimensionResource(id = R.dimen.major_100))
-                    .fillMaxWidth(),
-                onClick = onTryPaymentClicked
-            ) {
-                Text(stringResource(id = state.buttonText))
-            }
 
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_100)))
-            LearnMore(state.learnMoreText, onLearnMoreClicked)
+
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = dimensionResource(id = R.dimen.major_250)),
+                horizontalAlignment = CenterHorizontally,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.card_reader_tap_to_pay_explanation_one),
+                    style = MaterialTheme.typography.subtitle1,
+                    textAlign = Center,
+                )
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_150)))
+                Text(
+                    text = stringResource(id = R.string.card_reader_tap_to_pay_explanation_two),
+                    style = MaterialTheme.typography.subtitle1,
+                    textAlign = Center,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_200)))
+
+            Column(horizontalAlignment = CenterHorizontally) {
+                Text(
+                    modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.major_300)),
+                    text = stringResource(id = R.string.card_reader_tap_to_pay_explanation_three),
+                    style = MaterialTheme.typography.caption,
+                    textAlign = Center,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.4f)
+                )
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_100)))
+                WCColoredButton(
+                    modifier = Modifier
+                        .padding(horizontal = dimensionResource(id = R.dimen.major_100))
+                        .fillMaxWidth(),
+                    onClick = onTryPaymentClicked
+                ) {
+                    Text(stringResource(id = R.string.card_reader_tap_to_pay_explanation_try_payment))
+                }
+            }
+
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_200)))
         }
     }
 }
 
-@Composable
-private fun ExplanationRaw(number: String, text: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = dimensionResource(id = R.dimen.major_100)),
-        verticalAlignment = CenterVertically,
-    ) {
-        val numberBackgroundColor = colorResource(id = R.color.disconnected_state_number_background_color)
-        val radius = dimensionResource(id = R.dimen.major_100)
-        Text(
-            modifier = Modifier
-                .drawBehind {
-                    drawCircle(color = numberBackgroundColor, radius = radius.toPx())
-                }
-                .padding(16.dp),
-            text = number,
-            style = MaterialTheme.typography.body2,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.major_100)))
-        Text(
-            text = text,
-            style = MaterialTheme.typography.body1,
-        )
-    }
-}
-
-@Composable
-private fun LearnMore(@StringRes textId: Int, onLearnMoreClicked: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple(bounded = true)
-            ) { onLearnMoreClicked() }
-            .padding(
-                vertical = dimensionResource(id = R.dimen.minor_100),
-                horizontal = dimensionResource(id = R.dimen.major_125)
-            ),
-        verticalAlignment = CenterVertically,
-    ) {
-        Image(painter = painterResource(id = R.drawable.ic_info_outline_20dp), contentDescription = null)
-
-        Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.minor_100)))
-
-        Text(
-            modifier = Modifier.padding(start = dimensionResource(id = R.dimen.minor_100)),
-            text = annotatedStringRes(stringResId = textId),
-            style = MaterialTheme.typography.caption.copy(
-                color = colorResource(id = R.color.color_on_surface_medium)
-            ),
-        )
-    }
-}
-
 @Preview(name = "Light mode")
 @Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Preview(name = "Small device", heightDp = 600)
 @Composable
 fun TapToPaySummaryScreenPreview() {
-    TapToPaySummaryScreen(
-        state = UiState,
-        onTryPaymentClicked = {},
-        onLearnMoreClicked = {},
-        onBackClick = {}
-    )
+    WooThemeWithBackground {
+        TapToPaySummaryScreen(
+            onTryPaymentClicked = {},
+            onBackClick = {}
+        )
+    }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/taptopay/summary/TapToPaySummaryViewModel.kt
@@ -1,59 +1,20 @@
 package com.woocommerce.android.ui.payments.taptopay.summary
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.asLiveData
-import com.woocommerce.android.R
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class TapToPaySummaryViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
 ) : ScopedViewModel(savedStateHandle) {
-    private val _uiState = MutableStateFlow(UiState)
-    val uiState: LiveData<UiState> = _uiState.asLiveData()
-
     fun onTryPaymentClicked() {
         // TODO
     }
 
     fun onBackClicked() {
         triggerEvent(Event.Exit)
-    }
-
-    fun onLearnMoreClicked() {
-        // TODO
-    }
-
-    object UiState {
-        @StringRes
-        const val screenTitleText = R.string.card_reader_tap_to_pay_explanation_screen_title
-
-        @StringRes
-        const val titleText = R.string.card_reader_tap_to_pay_explanation_title
-
-        @DrawableRes
-        const val illustration = R.drawable.img_card_reader
-
-        @StringRes
-        const val explanationOneText = R.string.card_reader_tap_to_pay_explanation_one
-
-        @StringRes
-        const val explanationTwoText = R.string.card_reader_tap_to_pay_explanation_two
-
-        @StringRes
-        const val explanationThreeText = R.string.card_reader_tap_to_pay_explanation_three
-
-        @StringRes
-        const val buttonText = R.string.card_reader_tap_to_pay_explanation_try_payment
-
-        @StringRes
-        const val learnMoreText = R.string.card_reader_tap_to_pay_explanation_learn_more
     }
 }
