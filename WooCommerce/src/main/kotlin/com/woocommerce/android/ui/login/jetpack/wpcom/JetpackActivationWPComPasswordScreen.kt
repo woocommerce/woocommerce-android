70d9bf71a9035310c3e142f77182bf0d66acc1d8
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-02-27 18:10:20 +0100

    Update wording of the label of the password screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/wpcom/JetpackActivationWPComPasswordScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/wpcom/JetpackActivationWPComPasswordScreen.kt
@@ -116,7 +116,10 @@ fun JetpackActivationWPComPasswordScreen(
                 )
                 Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_100)))
                 Text(
-                    text = stringResource(id = R.string.enter_wpcom_password)
+                    text = stringResource(
+                        id = if (viewState.isJetpackInstalled) R.string.login_jetpack_connection_enter_wpcom_password
+                        else R.string.login_jetpack_installation_enter_wpcom_password
+                    )
                 )
                 Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_100)))
                 WCPasswordField(
