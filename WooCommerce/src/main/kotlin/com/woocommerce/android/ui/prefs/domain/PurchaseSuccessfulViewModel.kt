734a2ec5cd7fee7d43e0da613bf4c0a9c7c59d50
Ondrej Ruttkay <ruttkay@gmail.com>
2023-03-01 16:12:12 +0100

    Add success screen event tracking

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/domain/PurchaseSuccessfulViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/domain/PurchaseSuccessfulViewModel.kt
@@ -3,6 +3,10 @@ package com.woocommerce.android.ui.prefs.domain
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
+import com.woocommerce.android.AppPrefsWrapper
+import com.woocommerce.android.analytics.AnalyticsEvent
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
@@ -12,7 +16,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PurchaseSuccessfulViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    analyticsTrackerWrapper: AnalyticsTrackerWrapper,
+    appPrefsWrapper: AppPrefsWrapper,
 ) : ScopedViewModel(savedStateHandle) {
     companion object {
         private const val KEY_DOMAIN = "domain"
@@ -20,6 +26,16 @@ class PurchaseSuccessfulViewModel @Inject constructor(
 
     private val _viewState = savedStateHandle.getStateFlow(this, ViewState(savedStateHandle[KEY_DOMAIN] ?: ""))
     val viewState = _viewState.asLiveData()
+
+    init {
+        analyticsTrackerWrapper.track(
+            AnalyticsEvent.CUSTOM_DOMAINS_STEP,
+            mapOf(
+                AnalyticsTracker.KEY_SOURCE to appPrefsWrapper.getCustomDomainsSource(),
+                AnalyticsTracker.KEY_STEP to AnalyticsTracker.VALUE_STEP_PURCHASE_SUCCESS
+            )
+        )
+    }
 
     fun onDoneButtonClicked() {
         triggerEvent(NavigateToDashboard)
