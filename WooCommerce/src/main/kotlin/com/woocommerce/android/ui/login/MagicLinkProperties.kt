5f1e11c2db8f97fd2ce4b6f4f2b57c84eff9c220
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-03-01 16:24:48 +0100

    Add a function to request magic links

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/MagicLinkProperties.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/MagicLinkProperties.kt
@@ -1,11 +1,18 @@
 package com.woocommerce.android.ui.login
 
-enum class MagicLinkSource(val value: String) {
+import org.wordpress.android.fluxc.store.AccountStore.AuthEmailFlow
+import org.wordpress.android.fluxc.store.AccountStore.AuthEmailSource
+
+enum class MagicLinkSource(private val value: String): AuthEmailSource {
     JetpackInstallation("jetpack-installation"),
     JetpackConnection("jetpack-connection"),
-    WPComAuthentication("wpcom-authentication\n")
+    WPComAuthentication("wpcom-authentication\n");
+
+    override fun getName(): String = value
 }
 
-enum class MagicLinkFlow(val value: String) {
-    SiteCredentialsToWPCom("sitecredentials-to-wpcom")
+enum class MagicLinkFlow(private val value: String): AuthEmailFlow {
+    SiteCredentialsToWPCom("sitecredentials-to-wpcom");
+
+    override fun getName(): String = value
 }
