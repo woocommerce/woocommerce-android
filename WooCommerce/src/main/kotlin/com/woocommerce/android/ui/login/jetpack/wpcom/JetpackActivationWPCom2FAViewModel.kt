ee5e2fec1ae4e9de13e03ae80c6e2c1aea3aaa77
Hafiz Rahman <hfzrhmn@gmail.com>
2023-03-03 19:01:37 +0700

    Add: Basic handling for "Continue" button click.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/wpcom/JetpackActivationWPCom2FAViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/jetpack/wpcom/JetpackActivationWPCom2FAViewModel.kt
@@ -3,7 +3,10 @@ package com.woocommerce.android.ui.login.jetpack.wpcom
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.R
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.login.AccountRepository
+import com.woocommerce.android.ui.login.WPComLoginRepository
 import com.woocommerce.android.ui.login.jetpack.JetpackActivationRepository
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowSnackbar
@@ -12,7 +15,7 @@ import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOf
-import org.wordpress.android.login.R
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,6 +23,8 @@ class JetpackActivationWPCom2FAViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     selectedSite: SelectedSite,
     jetpackAccountRepository: JetpackActivationRepository,
+    private val wpComLoginRepository: WPComLoginRepository,
+    private val accountRepository: AccountRepository
 ) : JetpackActivationWPComPostLoginViewModel(savedStateHandle, selectedSite, jetpackAccountRepository) {
 
     private val navArgs: JetpackActivationWPCom2FAFragmentArgs by savedStateHandle.navArgs()
@@ -46,8 +51,26 @@ class JetpackActivationWPCom2FAViewModel @Inject constructor(
         triggerEvent(ShowSnackbar(R.string.requesting_otp))
     }
 
-    fun onContinueClick() {
-        TODO()
+    fun onContinueClick() = launch {
+        wpComLoginRepository.submitTwoStepCode(
+            emailOrUsername = navArgs.emailOrUsername,
+            password = navArgs.password,
+            twoStepCode = otp.value
+        ).fold(
+            onSuccess = { fetchAccount() },
+            onFailure = { triggerEvent(ShowSnackbar(R.string.error_generic)) }
+        )
+    }
+
+    private suspend fun fetchAccount() {
+        accountRepository.fetchUserAccount().fold(
+            onSuccess = {
+                onLoginSuccess(navArgs.jetpackStatus)
+            },
+            onFailure = {
+                triggerEvent(ShowSnackbar(R.string.error_fetch_my_profile))
+            }
+        )
     }
 
     fun onOTPChanged(enteredOTP: String) {
