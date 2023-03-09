10c97025265be07d521692a2804415baef1540e2
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-03-09 12:52:43 +0100

    Persist the ApiFaker status

--- libs/apifaker/src/main/java/com/woocommerce/android/apifaker/ApiFakerConfig.kt
+++ libs/apifaker/src/main/java/com/woocommerce/android/apifaker/ApiFakerConfig.kt
@@ -1,20 +1,28 @@
 package com.woocommerce.android.apifaker
 
+import android.content.Context
 import com.woocommerce.android.apifaker.db.EndpointDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@Singleton
-internal class ApiFakerConfig @Inject constructor() {
-    @Inject
-    internal lateinit var endpointDao: EndpointDao
+private const val PREF_FILE_NAME = "api_faker"
+private const val PREFERENCE_KEY = "api_faker_enabled"
 
-    private val _enabled: MutableStateFlow<Boolean> = MutableStateFlow(false)
+@Singleton
+internal class ApiFakerConfig @Inject constructor(
+    context: Context,
+    private val endpointDao: EndpointDao
+) {
+    private val preferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+
+    private val _enabled: MutableStateFlow<Boolean> = MutableStateFlow(preferences.getBoolean(PREFERENCE_KEY, false))
     val enabled = _enabled.asStateFlow()
 
-    internal suspend fun setStatus(enabled: Boolean) {
-        _enabled.value = enabled && !endpointDao.isEmpty()
+    suspend fun setStatus(enabled: Boolean) {
+        val status = enabled && !endpointDao.isEmpty()
+        _enabled.value = status
+        preferences.edit().putBoolean(PREFERENCE_KEY, status).apply()
     }
 }
