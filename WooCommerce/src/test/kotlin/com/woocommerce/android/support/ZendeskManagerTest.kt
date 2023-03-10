c602b49ae75dcbf9e6cd0a68f533efcb7021b876
ThomazFB <thomazfcortez@gmail.com>
2023-03-10 17:32:41 -0300

    Define ZendeskManager first test scenario

--- WooCommerce/src/test/kotlin/com/woocommerce/android/support/ZendeskManagerTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/support/ZendeskManagerTest.kt
@@ -1,9 +1,16 @@
 package com.woocommerce.android.support
 
+import com.woocommerce.android.support.ZendeskException.IdentityNotSetException
+import com.woocommerce.android.support.help.HelpOrigin
 import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.single
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
+import org.junit.Test
 import org.mockito.kotlin.mock
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class ZendeskManagerTest : BaseUnitTest() {
     private lateinit var sut: ZendeskManager
 
@@ -14,5 +21,27 @@ internal class ZendeskManagerTest : BaseUnitTest() {
             siteStore = mock(),
             dispatchers = mock()
         )
+    }
+
+    @Test
+    fun `when createRequest is called with no identity set, then an result with IdentityNotSetException is emitted`()
+    = testBlocking {
+        // Given
+
+        // When
+        val result = sut.createRequest(
+            context = mock(),
+            origin = HelpOrigin.LOGIN_HELP_NOTIFICATION,
+            ticketType = TicketType.MobileApp,
+            selectedSite = mock(),
+            subject = "subject",
+            description = "description",
+            extraTags = emptyList()
+        ).single()
+
+        // Then
+        assertThat(result).isNotNull
+        assertThat(result.isFailure).isTrue
+        assertThat(result.exceptionOrNull()).isEqualTo(IdentityNotSetException)
     }
 }
