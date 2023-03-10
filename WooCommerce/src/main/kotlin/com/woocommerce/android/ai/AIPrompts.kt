9d0a8c14521d9bd839a7d495e4d425749e5abbff
Hafiz Rahman <hfzrhmn@gmail.com>
2023-03-10 19:26:57 +0700

    Add: AI prompt for generating promo tweet for product.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ai/AIPrompts.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ai/AIPrompts.kt
@@ -3,4 +3,7 @@ package com.woocommerce.android.ai
 object AIPrompts {
     const val GENERATE_PRODUCT_DESCRIPTION_FROM_TITLE =
         "Create a product description for the following product title: "
+
+    const val GENERATE_PROMO_TWEET_FROM_PRODUCT_TITLE =
+        "Create a promotional tweet, that should not exceed 280 characters, for the following product: "
 }
