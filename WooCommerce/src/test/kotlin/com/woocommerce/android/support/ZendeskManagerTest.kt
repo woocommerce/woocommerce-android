2fcba859b554c2120b8f1c71659b9011a03bc516
ThomazFB <thomazfcortez@gmail.com>
2023-03-10 19:09:36 -0300

    Improve ZendeskManagerTest with new test scenario

--- WooCommerce/src/test/kotlin/com/woocommerce/android/support/ZendeskManagerTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/support/ZendeskManagerTest.kt
@@ -1,22 +1,34 @@
 package com.woocommerce.android.support
 
-import com.woocommerce.android.support.ZendeskException.IdentityNotSetException
+import com.woocommerce.android.support.zendesk.ZendeskException.IdentityNotSetException
 import com.woocommerce.android.support.help.HelpOrigin
+import com.woocommerce.android.support.zendesk.TicketType
+import com.woocommerce.android.support.zendesk.ZendeskManager
+import com.woocommerce.android.support.zendesk.ZendeskSettings
 import com.woocommerce.android.viewmodel.BaseUnitTest
+import com.zendesk.service.ZendeskCallback
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.wordpress.android.fluxc.store.SiteStore
+import zendesk.support.Request
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class ZendeskManagerTest : BaseUnitTest() {
     private lateinit var sut: ZendeskManager
     private lateinit var zendeskSettings: ZendeskSettings
     private lateinit var siteStore: SiteStore
+    private val captor = argumentCaptor<ZendeskCallback<Request>>()
 
     @Before
     fun setup() {
@@ -51,6 +63,35 @@ internal class ZendeskManagerTest : BaseUnitTest() {
         assertThat(result).isNotNull
         assertThat(result.isFailure).isTrue
         assertThat(result.exceptionOrNull()).isEqualTo(IdentityNotSetException)
+    }
+
+    @Test
+    fun `when createRequest is called correctly, then an result with the Request is emitted` () = testBlocking {
+        // Given
+        var result: Result<Request?>? = null
+
+
+        // When
+        val job = launch {
+             result = sut.createRequest(
+                context = mock(),
+                origin = HelpOrigin.LOGIN_HELP_NOTIFICATION,
+                ticketType = TicketType.MobileApp,
+                selectedSite = null,
+                subject = "subject",
+                description = "description",
+                extraTags = emptyList()
+            ).first()
+        }
+
+        verify(zendeskSettings).requestProvider?.createRequest(any(), captor.capture())
+        captor.firstValue.onSuccess(Request())
+        advanceUntilIdle()
+        job.cancel()
+
+        // Then
+        assertThat(result).isNotNull
+        assertThat(result?.isSuccess).isTrue
     }
 
     private fun createSUT() {
