7c6df677b62e7c4edccf3545f2c383c6591f05be
ThomazFB <thomazfcortez@gmail.com>
2023-03-10 19:22:58 -0300

    Add site operations to ZendeskEnvironmentDataSource

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/zendesk/ZendeskEnvironmentDataSource.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/zendesk/ZendeskEnvironmentDataSource.kt
@@ -3,13 +3,19 @@ package com.woocommerce.android.support.zendesk
 import android.content.Context
 import android.net.ConnectivityManager
 import android.telephony.TelephonyManager
+import android.text.TextUtils
+import com.woocommerce.android.extensions.logInformation
+import com.woocommerce.android.extensions.stateLogInformation
 import com.woocommerce.android.util.PackageUtils
 import com.woocommerce.android.util.WooLog
 import java.util.Locale
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.util.DeviceUtils
 import org.wordpress.android.util.NetworkUtils
+import org.wordpress.android.util.StringUtils
+import org.wordpress.android.util.UrlUtils
 
-class ZendeskDeviceDataSource {
+class ZendeskEnvironmentDataSource {
     val totalAvailableMemorySize: String get() = DeviceUtils.getTotalAvailableMemorySize()
     val deviceLanguage: String get() = Locale.getDefault().language
     val deviceLogs get() = WooLog.toString().takeLast(maxLogfileLength)
@@ -33,13 +39,42 @@ class ZendeskDeviceDataSource {
         ).joinToString(separator = "\n")
     }
 
+    /**
+     * This is a small helper function which just joins the `logInformation` of all the sites passed in with a separator.
+     */
+    fun generateCombinedLogInformationOfSites(allSites: List<SiteModel>?): String {
+        return allSites?.let { sites ->
+            sites.joinToString(separator = blogSeparator) { it.logInformation }
+        } ?: noneValue
+    }
+
+    fun generateHostData(selectedSite: SiteModel?) =
+        selectedSite?.let {
+            "${selectedSite.hostURL} (${selectedSite.stateLogInformation})"
+        } ?: "not_selected"
+
+    private val SiteModel.hostURL: String
+        get() = UrlUtils.removeScheme(url)
+            .let { StringUtils.removeTrailingSlash(it) }
+            .takeUnless { TextUtils.isEmpty(it) }
+            ?: UrlUtils.getHost(xmlRpcUrl)
+
     companion object Constants {
+        // Platform
+        const val platformTag = "Android"
+        const val sourcePlatform = "Mobile_-_Woo_Android"
+
+        // Network
         const val networkWifi = "WiFi"
         const val networkWWAN = "Mobile"
         const val networkTypeLabel = "Network Type:"
         const val networkCarrierLabel = "Carrier:"
         const val networkCountryCodeLabel = "Country Code:"
         const val unknownValue = "unknown"
+
+        //URL
+        const val blogSeparator = "\n----------\n"
+        const val noneValue = "none"
 
         const val maxLogfileLength: Int = 63000 // Max characters allowed in the system status report field
     }
