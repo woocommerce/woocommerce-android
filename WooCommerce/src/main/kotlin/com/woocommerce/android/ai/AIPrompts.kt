39c414f2238cb1ff7e918fe8244114ef6be103d5
Hafiz Rahman <hfzrhmn@gmail.com>
2023-03-10 18:47:43 +0700

    Add: AIPrompts object to store all the prompts to be used in the app.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ai/AIPrompts.kt
@@ -0,0 +1,6 @@
+package com.woocommerce.android.ai
+
+object AIPrompts {
+    const val CHATGPT_GENERATE_PRODUCT_FROM_TITLE =
+        "Create a product description for the following product title: "
+}
