e62c4c5f095e707fc882cb6321edef4c377c555f
Hafiz Rahman <hfzrhmn@gmail.com>
2023-03-11 20:07:50 +0700

    Add: Starter files for creating separate product details AI tools screen.
    Does nothing yet.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductAIToolsFragment.kt
@@ -0,0 +1,43 @@
+package com.woocommerce.android.ui.products
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+
+class ProductAIToolsFragment: BaseFragment() {
+    private val viewModel: ProductAIToolsViewModel by viewModels()
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+            setContent {
+                WooThemeWithBackground {
+                    ProductsAIToolsScreen()
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is Exit -> findNavController().navigateUp()
+                else -> { /* do nothing */ }
+            }
+        }
+    }
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductsAIToolsScreen.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.products
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProductsAIToolsScreen() {
+
+}
+
+@Preview
+@Composable
+private fun ProductsAIToolsScreenPreview() {
+    ProductsAIToolsScreen()
+}
