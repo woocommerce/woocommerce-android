b752ba98f3ec5d05aba657667f4dfef0cacb56b5
Hafiz Rahman <hfzrhmn@gmail.com>
2023-03-12 11:56:15 +0700

    Add:Better styling for ProductsAIToolsScreen.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductsAIToolsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductsAIToolsScreen.kt
@@ -1,20 +1,25 @@
 package com.woocommerce.android.ui.products
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
 fun ProductAIToolsScreen(viewModel: ProductAIToolsViewModel) {
@@ -32,7 +37,10 @@ fun ProductAIToolsScreen(
     onGenerateTweetClicked: () -> Unit = {}
 ) {
     LazyColumn(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.surface)
+            .padding(vertical = dimensionResource(id = R.dimen.major_100)),
     ) {
         items(viewState.options.size) { index ->
             val option = viewState.options[index]
@@ -62,15 +70,25 @@ fun ProductAIToolOption(
             )
     ) {
         Text(
+            modifier = Modifier
+                .padding(horizontal = dimensionResource(id = R.dimen.major_100)),
             text = title,
             style = MaterialTheme.typography.h6,
             color = MaterialTheme.colors.onSurface
         )
         Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_extra_small)))
         Text(
+            modifier = Modifier
+                .padding(horizontal = dimensionResource(id = R.dimen.major_100)),
             text = description,
             style = MaterialTheme.typography.body1,
             color = MaterialTheme.colors.onSurface
+        )
+        Divider(
+            modifier = Modifier
+                .padding(vertical = dimensionResource(id = R.dimen.major_100)),
+            color = colorResource(id = R.color.divider_color),
+            thickness = dimensionResource(id = R.dimen.minor_10)
         )
     }
 
@@ -79,14 +97,24 @@ fun ProductAIToolOption(
 @Preview
 @Composable
 private fun ProductsAIToolsScreenPreview() {
-    ProductAIToolsScreen(
-        viewState = ProductAIToolsViewModel.ViewState(
-            options = listOf(
-                ProductAIToolsViewModel.AIToolOption(
-                    title = stringResource(R.string.ai_product_tools_generate_tweet),
-                    description = stringResource(R.string.ai_product_tools_generate_tweet_description),
+    WooThemeWithBackground {
+        ProductAIToolsScreen(
+            viewState = ProductAIToolsViewModel.ViewState(
+                options = listOf(
+                    ProductAIToolsViewModel.AIToolOption(
+                        title = stringResource(R.string.ai_product_tools_generate_tweet),
+                        description = stringResource(R.string.ai_product_tools_generate_tweet_description),
+                    ),
+                    ProductAIToolsViewModel.AIToolOption(
+                        title = stringResource(R.string.ai_product_tools_generate_tweet),
+                        description = stringResource(R.string.ai_product_tools_generate_tweet_description),
+                    ),
+                    ProductAIToolsViewModel.AIToolOption(
+                        title = stringResource(R.string.ai_product_tools_generate_tweet),
+                        description = stringResource(R.string.ai_product_tools_generate_tweet_description),
+                    ),
                 )
             )
         )
-    )
+    }
 }
