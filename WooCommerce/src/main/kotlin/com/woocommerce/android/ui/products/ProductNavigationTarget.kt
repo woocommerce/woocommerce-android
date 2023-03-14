1bfc21898d717f1dec5c6653600b51992839a600
Alejo <alejandro.torres.veiga@automattic.com>
2023-03-14 12:16:36 -0600

    Show subscription details screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductNavigationTarget.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductNavigationTarget.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.products
 
 import com.woocommerce.android.model.Product.Image
 import com.woocommerce.android.model.ProductFile
+import com.woocommerce.android.model.SubscriptionDetails
 import com.woocommerce.android.ui.products.ProductInventoryViewModel.InventoryData
 import com.woocommerce.android.ui.products.ProductPricingViewModel.PricingData
 import com.woocommerce.android.ui.products.ProductShippingViewModel.ShippingData
@@ -121,4 +122,6 @@ sealed class ProductNavigationTarget : Event() {
         val productCategory: String?,
         val productCategoryName: String?
     ) : ProductNavigationTarget()
+
+    data class ViewProductSubscription(val subscription: SubscriptionDetails) : ProductNavigationTarget()
 }
