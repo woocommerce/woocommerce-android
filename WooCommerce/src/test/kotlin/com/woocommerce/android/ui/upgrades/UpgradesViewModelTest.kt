0db928849e89a492c063a5d00513944dcca8114e
ThomazFB <thomazfcortez@gmail.com>
2023-03-15 01:05:45 -0300

    Create UpgradesViewModel test covering the request form event parameters

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/upgrades/UpgradesViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/upgrades/UpgradesViewModelTest.kt
@@ -1,19 +1,35 @@
 package com.woocommerce.android.ui.upgrades
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.support.ZendeskTags
+import com.woocommerce.android.support.help.HelpOrigin
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.plans.repository.SitePlanRepository
+import com.woocommerce.android.ui.upgrades.UpgradesViewModel.UpgradesEvent
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import com.woocommerce.android.viewmodel.MultiLiveEvent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.wordpress.android.fluxc.model.SiteModel
 
-internal class UpgradesViewModelTest {
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class UpgradesViewModelTest : BaseUnitTest() {
     lateinit var sut: UpgradesViewModel
     lateinit var selectedSite: SelectedSite
 
     @Before
     fun setup() {
-        selectedSite = mock()
+        val siteModel = mock<SiteModel> {
+            on { planId } doReturn SitePlanRepository.FREE_TRIAL_PLAN_ID
+        }
+        selectedSite = mock {
+            on { observe() } doReturn flowOf(siteModel)
+        }
         sut = UpgradesViewModel(
             savedState = SavedStateHandle(),
             selectedSite = selectedSite
@@ -21,7 +37,23 @@ internal class UpgradesViewModelTest {
     }
 
     @Test
-    fun `initial empty test`() {
-        assertThat(true).isTrue
-    }
+    fun `when onReportSubscriptionIssueClicked is called with a free trial site, then trigger OpenSupportRequestForm with the expected values`() =
+        testBlocking {
+            // Given
+            val events = mutableListOf<MultiLiveEvent.Event>()
+            sut.event.observeForever {
+                events.add(it)
+            }
+
+            // When
+            sut.onReportSubscriptionIssueClicked()
+
+            // Then
+            assertThat(events).containsExactly(
+                UpgradesEvent.OpenSupportRequestForm(
+                    HelpOrigin.UPGRADES,
+                    listOf(ZendeskTags.freeTrialTag)
+                )
+            )
+        }
 }
