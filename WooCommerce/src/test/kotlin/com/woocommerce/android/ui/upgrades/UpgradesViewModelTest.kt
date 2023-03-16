8be1f5c5d078793020919498bd9353edb43c5030
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-03-16 19:29:17 +0100

    fix: adjust unit tests to new requirements

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/upgrades/UpgradesViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/upgrades/UpgradesViewModelTest.kt
@@ -4,14 +4,11 @@ import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.support.ZendeskTags
 import com.woocommerce.android.support.help.HelpOrigin
 import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.ui.plans.repository.SitePlanRepository
+import com.woocommerce.android.ui.plans.domain.FREE_TRIAL_PLAN_ID
 import com.woocommerce.android.ui.upgrades.UpgradesViewModel.UpgradesEvent
-import com.woocommerce.android.ui.upgrades.UpgradesViewModel.UpgradesViewState.CurrentPlanInfo.NonUpgradeable
-import com.woocommerce.android.ui.upgrades.UpgradesViewModel.UpgradesViewState.CurrentPlanInfo.Upgradeable
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -20,51 +17,17 @@ import org.mockito.kotlin.mock
 import org.wordpress.android.fluxc.model.SiteModel
 
 @OptIn(ExperimentalCoroutinesApi::class)
-internal class UpgradesViewModelTest : BaseUnitTest() {
+class UpgradesViewModelTest : BaseUnitTest() {
     lateinit var sut: UpgradesViewModel
     lateinit var selectedSite: SelectedSite
 
     @Before
     fun setup() {
-        val siteModel = mock<SiteModel> {
-            on { planId } doReturn SitePlanRepository.FREE_TRIAL_PLAN_ID
-            on { planShortName } doReturn "Free Trial site"
+        val siteModel = SiteModel().apply {
+           planId = FREE_TRIAL_PLAN_ID
         }
         createSut(siteModel)
     }
-
-    @Test
-    fun `when selectedSite is in free trial, then currentPlan is set to Upgradeable`() =
-        testBlocking {
-            // Given
-            var viewModelState: UpgradesViewModel.UpgradesViewState? = null
-            sut.upgradesState.observeForever {
-                viewModelState = it
-            }
-
-            // Then
-            assertThat(viewModelState).isNotNull
-            assertThat(viewModelState?.currentPlan).isEqualTo(Upgradeable(name = "Free Trial site"))
-        }
-
-    @Test
-    fun `when selectedSite is NOT in free trial, then currentPlan is set to NonUpgradeable`() =
-        testBlocking {
-            // Given
-            val siteModel = mock<SiteModel> {
-                on { planId } doReturn 0
-                on { planShortName } doReturn "Not in Free Trial site"
-            }
-            createSut(siteModel)
-            var viewModelState: UpgradesViewModel.UpgradesViewState? = null
-            sut.upgradesState.observeForever {
-                viewModelState = it
-            }
-
-            // Then
-            assertThat(viewModelState).isNotNull
-            assertThat(viewModelState?.currentPlan).isEqualTo(NonUpgradeable(name = "Not in Free Trial site"))
-        }
 
     @Test
     fun `when onReportSubscriptionIssueClicked is called with a free trial site, then trigger OpenSupportRequestForm with the expected values`() =
@@ -115,13 +78,15 @@ internal class UpgradesViewModelTest : BaseUnitTest() {
 
     private fun createSut(siteModel: SiteModel) {
         selectedSite = mock {
-            on { observe() } doReturn flowOf(siteModel)
             on { getIfExists() } doReturn siteModel
         }
 
         sut = UpgradesViewModel(
             savedState = SavedStateHandle(),
-            selectedSite = selectedSite
+            selectedSite = selectedSite,
+            mock(),
+            mock(),
+            mock()
         )
     }
 }
