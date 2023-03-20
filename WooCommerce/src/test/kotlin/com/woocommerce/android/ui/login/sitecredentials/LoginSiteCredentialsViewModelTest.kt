5db59f8e21b4b4c8bea8923e47068b9796fbb405
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-03-20 17:00:00 +0000

    Fix unit tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/sitecredentials/LoginSiteCredentialsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/sitecredentials/LoginSiteCredentialsViewModelTest.kt
@@ -2,7 +2,6 @@ package com.woocommerce.android.ui.login.sitecredentials
 
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.AppPrefsWrapper
-import com.woocommerce.android.OnChangedException
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsEvent
 import com.woocommerce.android.analytics.AnalyticsTracker
@@ -12,6 +11,7 @@ import com.woocommerce.android.applicationpasswords.ApplicationPasswordsNotifier
 import com.woocommerce.android.model.UiString.UiStringText
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.login.WPApiSiteRepository
+import com.woocommerce.android.ui.login.WPApiSiteRepository.CookieNonceAuthenticationException
 import com.woocommerce.android.ui.login.sitecredentials.LoginSiteCredentialsViewModel.LoggedIn
 import com.woocommerce.android.ui.login.sitecredentials.LoginSiteCredentialsViewModel.ShowApplicationPasswordsUnavailableScreen
 import com.woocommerce.android.ui.login.sitecredentials.LoginSiteCredentialsViewModel.ShowNonWooErrorScreen
@@ -34,9 +34,8 @@ import org.mockito.kotlin.whenever
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError
 import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType
+import org.wordpress.android.fluxc.network.rest.wpapi.Nonce
 import org.wordpress.android.fluxc.network.rest.wpapi.WPAPINetworkError
-import org.wordpress.android.fluxc.store.SiteStore.SiteError
-import org.wordpress.android.fluxc.store.SiteStore.SiteErrorType
 import org.wordpress.android.login.LoginAnalyticsListener
 
 @ExperimentalCoroutinesApi
@@ -145,17 +144,14 @@ class LoginSiteCredentialsViewModelTest : BaseUnitTest() {
 
     @Test
     fun `given incorrect credentials, when submitting, then show error`() = testBlocking {
-        val returnedErrorMessage = "Username or password incorrect"
+        val expectedError = CookieNonceAuthenticationException(
+            errorMessage = UiStringText("Username or password incorrect"),
+            errorType = Nonce.CookieNonceErrorType.NOT_AUTHENTICATED.name,
+            networkStatusCode = null
+        )
         setup {
             whenever(wpApiSiteRepository.loginAndFetchSite(siteAddress, testUsername, testPassword)).thenReturn(
-                Result.failure(
-                    OnChangedException(
-                        SiteError(
-                            SiteErrorType.NOT_AUTHENTICATED,
-                            message = returnedErrorMessage
-                        )
-                    )
-                )
+                Result.failure(expectedError)
             )
         }
 
@@ -165,15 +161,15 @@ class LoginSiteCredentialsViewModelTest : BaseUnitTest() {
             viewModel.onContinueClick()
         }.last()
 
-        assertThat(state.errorDialogMessage).isEqualTo(UiStringText(returnedErrorMessage))
+        assertThat(state.errorDialogMessage).isEqualTo(expectedError.errorMessage)
         verify(analyticsTracker).track(
             stat = eq(AnalyticsEvent.LOGIN_SITE_CREDENTIALS_LOGIN_FAILED),
             properties = argThat {
                 get(AnalyticsTracker.KEY_STEP) == LoginSiteCredentialsViewModel.Step.AUTHENTICATION.name.lowercase()
             },
             errorContext = anyOrNull(),
-            errorType = anyOrNull(),
-            errorDescription = anyOrNull()
+            errorType = eq(expectedError.errorType),
+            errorDescription = eq((expectedError.errorMessage as UiStringText).text)
         )
         verify(loginAnalyticsListener).trackFailure(anyOrNull())
     }
