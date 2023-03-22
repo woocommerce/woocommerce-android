c808104fede2300b0b20fde7523367042f0278ed
Ondrej Ruttkay <ruttkay@gmail.com>
2023-03-22 21:48:14 +0100

    Fix unit tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/storecreation/PlansViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/storecreation/PlansViewModelTest.kt
@@ -154,8 +154,12 @@ class PlansViewModelTest : BaseUnitTest() {
     @Test
     fun `when a site is created, a plan is added to site's checkout cart and a WebView is launched`() =
         testBlocking {
-            whenever(repository.createNewSite(siteData, PlansViewModel.NEW_SITE_LANGUAGE_ID, TimeZone.getDefault().id))
-                .thenReturn(StoreCreationResult.Success(SITE_ID))
+            whenever(repository.createNewSite(
+                siteData = siteData,
+                languageWordPressId = PlansViewModel.NEW_SITE_LANGUAGE_ID,
+                timeZoneId = TimeZone.getDefault().id,
+                siteCreationFlow = newStore.data.planPathSlug
+            )).thenReturn(StoreCreationResult.Success(SITE_ID))
 
             whenever(repository.addPlanToCart(newStore.data.planProductId, newStore.data.planPathSlug, SITE_ID))
                 .thenReturn(StoreCreationResult.Success(Unit))
@@ -178,8 +182,12 @@ class PlansViewModelTest : BaseUnitTest() {
         testBlocking {
             givenIsIAPEnabled()
             whenever(repository.fetchPlan(ECOMMERCE_MONTHLY)).thenReturn(plan)
-            whenever(repository.createNewSite(siteData, PlansViewModel.NEW_SITE_LANGUAGE_ID, TimeZone.getDefault().id))
-                .thenReturn(StoreCreationResult.Success(SITE_ID))
+            whenever(repository.createNewSite(
+                siteData = siteData,
+                languageWordPressId = PlansViewModel.NEW_SITE_LANGUAGE_ID,
+                timeZoneId = TimeZone.getDefault().id,
+                siteCreationFlow = newStore.data.planPathSlug
+            )).thenReturn(StoreCreationResult.Success(SITE_ID))
 
             whenViewModelIsCreated()
             viewModel.onConfirmClicked(iapActivityWrapper)
@@ -196,8 +204,12 @@ class PlansViewModelTest : BaseUnitTest() {
     @Test
     fun `when a site already exists and belongs to the user, the flow is successfully recovered`() =
         testBlocking {
-            whenever(repository.createNewSite(siteData, PlansViewModel.NEW_SITE_LANGUAGE_ID, TimeZone.getDefault().id))
-                .thenReturn(StoreCreationResult.Failure(SITE_ADDRESS_ALREADY_EXISTS))
+            whenever(repository.createNewSite(
+                siteData = siteData,
+                languageWordPressId = PlansViewModel.NEW_SITE_LANGUAGE_ID,
+                timeZoneId = TimeZone.getDefault().id,
+                siteCreationFlow = newStore.data.planPathSlug
+            )).thenReturn(StoreCreationResult.Failure(SITE_ADDRESS_ALREADY_EXISTS))
 
             whenever(repository.getSiteByUrl(newStore.data.domain!!))
                 .thenReturn(SiteModel().apply { siteId = SITE_ID })
@@ -221,8 +233,12 @@ class PlansViewModelTest : BaseUnitTest() {
     @Test
     fun `when a site already exists and and it doesn't belong to the user, the flow fails`() =
         testBlocking {
-            whenever(repository.createNewSite(siteData, PlansViewModel.NEW_SITE_LANGUAGE_ID, TimeZone.getDefault().id))
-                .thenReturn(StoreCreationResult.Failure(SITE_ADDRESS_ALREADY_EXISTS))
+            whenever(repository.createNewSite(
+                siteData = siteData,
+                languageWordPressId = PlansViewModel.NEW_SITE_LANGUAGE_ID,
+                timeZoneId = TimeZone.getDefault().id,
+                siteCreationFlow = newStore.data.planPathSlug
+            )).thenReturn(StoreCreationResult.Failure(SITE_ADDRESS_ALREADY_EXISTS))
 
             whenever(repository.getSiteByUrl(newStore.data.domain!!))
                 .thenReturn(null)
