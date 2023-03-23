b04370d028658b21b5523d6fa5ca59fcd0dab9d9
Ondrej Ruttkay <ruttkay@gmail.com>
2023-03-23 18:13:02 +0100

    Update the screen based on the VM changes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/payments/GetPaidScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/payments/GetPaidScreen.kt
@@ -2,22 +2,23 @@ package com.woocommerce.android.ui.login.storecreation.onboarding.payments
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.woocommerce.android.R.string
 import com.woocommerce.android.ui.common.wpcomwebview.WPComWebViewAuthenticator
 import com.woocommerce.android.ui.compose.component.Toolbar
 import com.woocommerce.android.ui.compose.component.WCWebView
-import com.woocommerce.android.ui.compose.component.WebViewProgressIndicator.Circular
-import com.woocommerce.android.ui.compose.component.WebViewProgressIndicator.Linear
-import com.woocommerce.android.ui.login.storecreation.onboarding.payments.GetPaidViewModel.ViewState
-import com.woocommerce.android.util.WooLog
+import com.woocommerce.android.ui.login.storecreation.onboarding.payments.GetPaidViewModel.ViewState.LoadingState
+import com.woocommerce.android.ui.login.storecreation.onboarding.payments.GetPaidViewModel.ViewState.WebViewState
 import org.wordpress.android.fluxc.network.UserAgent
 
 @Composable
@@ -26,42 +27,59 @@ fun GetPaidScreen(
     userAgent: UserAgent,
     authenticator: WPComWebViewAuthenticator
 ) {
-    viewModel.viewState.observeAsState(ViewState("", false)).value.let { state ->
+    viewModel.viewState.observeAsState(LoadingState).value.let { state ->
         Scaffold(topBar = {
             Toolbar(
                 title = stringResource(id = string.store_onboarding_task_payments_setup_title),
                 onNavigationButtonClick = viewModel::onBackPressed
             )
         }) { padding ->
-            WpAdmin(
-                state,
-                userAgent,
-                authenticator,
-                viewModel::onUrlLoaded,
-                modifier = Modifier.background(MaterialTheme.colors.surface)
-                    .fillMaxSize()
-                    .padding(padding)
-            )
+            when (state) {
+                is LoadingState -> {
+                    ProgressIndicator(modifier = Modifier
+                        .padding(padding)
+                    )
+                }
+                is WebViewState -> {
+                    WpAdmin(
+                        state,
+                        userAgent,
+                        if (state.shouldAuthenticate) authenticator else null,
+                        modifier = Modifier
+                            .background(MaterialTheme.colors.surface)
+                            .fillMaxSize()
+                            .padding(padding)
+                    )
+                }
+            }
         }
+    }
+}
+
+@Composable
+private fun ProgressIndicator(modifier: Modifier) {
+    Box(modifier = Modifier
+        .background(MaterialTheme.colors.surface)
+        .fillMaxSize()) {
+        CircularProgressIndicator(
+            modifier = modifier
+                .align(Alignment.Center)
+        )
     }
 }
 
 @Composable
 @SuppressLint("SetJavaScriptEnabled", "ClickableViewAccessibility")
 private fun WpAdmin(
-    viewState: ViewState,
+    viewState: WebViewState,
     userAgent: UserAgent,
-    authenticator: WPComWebViewAuthenticator,
-    onUrlLoaded: (String) -> Unit,
+    authenticator: WPComWebViewAuthenticator?,
     modifier: Modifier
 ) {
-    WooLog.d(WooLog.T.UTILS, "GetPaidScreen: url = $viewState.url")
     WCWebView(
         url = viewState.url,
         userAgent = userAgent,
         wpComAuthenticator = authenticator,
-        onUrlLoaded = onUrlLoaded,
-        progressIndicator = if (viewState.showSpinner) Circular() else Linear(),
         modifier = modifier
     )
 }
