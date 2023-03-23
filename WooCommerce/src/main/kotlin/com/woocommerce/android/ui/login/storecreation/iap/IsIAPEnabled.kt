71372fb72ac3feca83d1c88d573fced879067dbf
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-03-23 16:45:48 +0100

    Change approach to disable IAP everywhere while fixing detekt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/iap/IsIAPEnabled.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/iap/IsIAPEnabled.kt
@@ -1,7 +1,8 @@
 package com.woocommerce.android.ui.login.storecreation.iap
 
+import com.woocommerce.android.util.FeatureFlag
 import javax.inject.Inject
 
 class IsIAPEnabled @Inject constructor() {
-    operator fun invoke(): Boolean = false
+    operator fun invoke(): Boolean = FeatureFlag.IAP_FOR_STORE_CREATION.isEnabled()
 }
