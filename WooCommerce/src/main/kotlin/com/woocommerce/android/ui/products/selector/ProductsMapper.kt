b3ad830c37cfe69f44b8771b450fa6ca8d122142
Anirudh Uppunda <abhat38@gmail.com>
2023-03-24 12:53:10 +0530

    Mapper to map product ids to Product model.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/selector/ProductsMapper.kt
@@ -0,0 +1,28 @@
+package com.woocommerce.android.ui.products.selector
+
+import com.woocommerce.android.model.Product
+import com.woocommerce.android.model.toAppModel
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.WCProductModel
+import org.wordpress.android.fluxc.persistence.ProductSqlUtils
+import javax.inject.Inject
+
+class ProductsMapper @Inject constructor(private val site: SiteModel) {
+    fun mapProductIdsToProduct(productIds: List<Long>): List<Product> {
+        return productIds.asProductList(site).map { product ->
+            product.toAppModel()
+        }
+    }
+
+    /**
+     * This method gets all Products from the IDs described by the
+     * List<Long>, but it only gets the product that are already available in the database.
+     */
+    private fun List<Long>.asProductList(
+        site: SiteModel,
+    ): List<WCProductModel> {
+        return this
+            .filter { ProductSqlUtils.geProductExistsByRemoteId(site, it) }
+            .mapNotNull { ProductSqlUtils.getProductByRemoteId(site, it) }
+    }
+}
