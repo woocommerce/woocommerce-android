5323ad93cb8adb88eebd864ab89faf40f93af46a
ThomazFB <thomazfcortez@gmail.com>
2023-03-24 11:29:02 -0300

    Add new Free Trial Analytic events

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsEvent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsEvent.kt
@@ -818,6 +818,9 @@ enum class AnalyticsEvent(val siteless: Boolean = false) {
     PLAN_UPGRADE_SUCCESS,
     PLAN_UPGRADE_ABANDONED,
     UPGRADES_REPORT_SUBSCRIPTION_ISSUE_TAPPED,
+    START_FREE_TRIAL_TAPPED,
+    FREE_TRIAL_STORE_CREATED,
+
 
     // Store onboarding
     STORE_ONBOARDING_SHOWN,
