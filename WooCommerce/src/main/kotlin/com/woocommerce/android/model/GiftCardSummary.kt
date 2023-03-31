e024525d4bc31eb5916c42c5b337ec03ea5e65a0
Alejo <alejandro.torres.veiga@automattic.com>
2023-03-31 17:18:19 -0600

    fetch gift card summary

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.model
 
+import com.woocommerce.android.network.giftcard.GiftCardRestClient
 import java.math.BigDecimal
 
 data class GiftCardSummary(
@@ -7,3 +8,11 @@ data class GiftCardSummary(
     val code: String,
     val used: BigDecimal
 )
+
+fun GiftCardRestClient.GiftCardSummaryDto.toAppModel(): GiftCardSummary {
+    return GiftCardSummary(
+        id = this.id ?: 0,
+        code = this.code.orEmpty(),
+        used = this.amount?.toBigDecimalOrNull() ?: BigDecimal.ZERO
+    )
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/network/giftcard/GiftCardRestClient.kt
@@ -0,0 +1,38 @@
+package com.woocommerce.android.network.giftcard
+
+import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.Response
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooNetwork
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
+import org.wordpress.android.fluxc.utils.toWooPayload
+import javax.inject.Inject
+
+class GiftCardRestClient @Inject constructor(private val wooNetwork: WooNetwork) {
+    suspend fun fetchGiftCardSummaryByOrderId(site: SiteModel, orderId: Long): WooPayload<List<GiftCardSummaryDto>> {
+        val url = WOOCOMMERCE.orders.id(orderId).pathV3
+        val params = mapOf("_fields" to "gift_cards")
+
+        return wooNetwork.executeGetGsonRequest(
+            site = site,
+            path = url,
+            clazz = GiftCardSummaryResponse::class.java,
+            params = params
+        ).toWooPayload { giftCardSummaryResponse ->
+            giftCardSummaryResponse.gift_cards ?: emptyList()
+        }
+    }
+
+    @Suppress("PropertyName", "VariableNaming")
+    class GiftCardSummaryResponse : Response {
+        val gift_cards: List<GiftCardSummaryDto>? = null
+    }
+
+    data class GiftCardSummaryDto(
+        val id: Long? = null,
+        val code: String? = null,
+        val amount: String? = null,
+    )
+}
+
+
