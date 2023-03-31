5deee6c40895d95bc52f5b9a9ae326301f48a466
Alejo <alejandro.torres.veiga@automattic.com>
2023-03-31 12:28:17 -0600

    fix summary model

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
@@ -1,6 +1,5 @@
 package com.woocommerce.android.model
 
-import com.woocommerce.android.network.giftcard.GiftCardRestClient
 import java.math.BigDecimal
 
 data class GiftCardSummary(
@@ -8,11 +7,3 @@ data class GiftCardSummary(
     val code: String,
     val used: BigDecimal
 )
-
-fun GiftCardRestClient.GiftCardSummaryDto.toAppModel(): GiftCardSummary {
-    return GiftCardSummary(
-        id = this.id ?: 0,
-        code = this.code.orEmpty(),
-        used = this.amount?.toBigDecimalOrNull() ?: BigDecimal.ZERO
-    )
-}
