93963c48ab09b28d24e5f9dca9c6cd568eddb05f
ThomazFB <thomazfcortez@gmail.com>
2023-04-03 16:10:33 -0300

    Move free trial creation functions to CreateFreeTrialStore

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/CreateFreeTrialStore.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/CreateFreeTrialStore.kt
@@ -9,4 +9,41 @@ class CreateFreeTrialStore @Inject constructor(
     private val repository: StoreCreationRepository
 ) {
     private val error = MutableStateFlow<StoreCreationErrorType?>(null)
+
+    suspend fun createFreeTrialSite(
+        storeDomain: String,
+        storeName: String
+    ): StoreCreationResult<Long> {
+        suspend fun StoreCreationResult<Long>.recoverIfSiteExists(): StoreCreationResult<Long> {
+            return if ((this as? StoreCreationResult.Failure<Long>)?.type == StoreCreationErrorType.SITE_ADDRESS_ALREADY_EXISTS) {
+                repository.getSiteByUrl(storeDomain)?.let { site ->
+                    StoreCreationResult.Success(site.siteId)
+                } ?: this
+            } else {
+                this
+            }
+        }
+
+        return repository.createNewFreeTrialSite(
+            StoreCreationRepository.SiteCreationData(
+                siteDesign = PlansViewModel.NEW_SITE_THEME,
+                domain = storeDomain,
+                title = storeName,
+                segmentId = null
+            ),
+            PlansViewModel.NEW_SITE_LANGUAGE_ID,
+            TimeZone.getDefault().id
+        ).recoverIfSiteExists()
+    }
+
+    private suspend fun <T : Any?> StoreCreationResult<T>.ifSuccessfulThen(
+        successAction: suspend (T) -> Unit
+    ) {
+        when (this) {
+            is StoreCreationResult.Success -> successAction(this.data)
+            is StoreCreationResult.Failure -> {
+                error.emit(this.type)
+            }
+        }
+    }
 }
