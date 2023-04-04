51e6794c71c83215e66b14d28c579b16b3705266
ThomazFB <thomazfcortez@gmail.com>
2023-04-04 12:58:11 -0300

    Configure bulk UI structure in StoreCreationSummaryScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryScreen.kt
@@ -1,14 +1,22 @@
 package com.woocommerce.android.ui.login.storecreation.summary
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.component.ToolbarWithHelpButton
+import com.woocommerce.android.ui.compose.component.WCColoredButton
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
@@ -31,8 +39,35 @@ private fun StoreCreationSummaryScreen(
             onHelpButtonClick = onHelpPressed,
         )
     }) {
-        Column(modifier = Modifier.padding(it)) {
-            Text("Hello World!")
+        Column(modifier = Modifier
+            .background(MaterialTheme.colors.surface)
+            .fillMaxSize()
+            .padding(it)
+            .padding(dimensionResource(id = R.dimen.major_125))
+        ) {
+            Text(
+                text = "Launch in days, grow for years",
+                style = MaterialTheme.typography.h4
+            )
+            Text(
+                "We offer everything you need to build and grow an online store, powered by WooCommerce and hosted on WordPress.com.",
+                style = MaterialTheme.typography.body1
+            )
+            Text(
+                "Try it for 14 days.",
+                style = MaterialTheme.typography.h5
+            )
+            Divider()
+            WCColoredButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Try for free")
+            }
+            Text(
+                "No credit card required.",
+                style = MaterialTheme.typography.caption
+            )
         }
     }
 }
