8b50f02f55a1a8e2f68b11f03eef46ce972723bc
ThomazFB <thomazfcortez@gmail.com>
2023-04-04 12:11:24 -0300

    Define basic scaffold for StoreCreationSummaryScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryScreen.kt
@@ -1,2 +1,23 @@
 package com.woocommerce.android.ui.login.storecreation.summary
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.woocommerce.android.ui.compose.component.ToolbarWithHelpButton
+
+@Composable
+fun StoreCreationSummaryScreen(viewModel: StoreCreationSummaryViewModel) {
+    Scaffold(topBar = {
+        ToolbarWithHelpButton(
+            onNavigationButtonClick = {},
+            onHelpButtonClick = { },
+        )
+    }) {
+        Column(modifier = Modifier.padding(it)) {
+            Text("Hello World!")
+        }
+    }
+}
