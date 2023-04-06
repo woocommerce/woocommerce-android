9ae4a26b0ca5643aefbdaedfa143bacdd4cf883f
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 13:16:27 -0300

    Configure StoreCreationSummaryFragment to navigate to installation view

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryFragment.kt
@@ -2,10 +2,13 @@ package com.woocommerce.android.ui.login.storecreation.summary
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.extensions.navigateSafely
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +26,22 @@ class StoreCreationSummaryFragment : BaseFragment() {
         setContent {
             WooThemeWithBackground {
                 StoreCreationSummaryScreen(viewModel)
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupEventObservers()
+    }
+
+    private fun setupEventObservers() {
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is StoreCreationSummaryViewModel.OnCancelPressed -> findNavController().popBackStack()
+                is StoreCreationSummaryViewModel.OnStoreCreationSuccess -> findNavController().navigateSafely(
+                    StoreCreationSummaryFragmentDirections.actionSummaryFragmentToInstallationFragment()
+                )
             }
         }
     }
