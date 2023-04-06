78b9ea2fda571b096a3263e42de0ee4aa282d535
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 12:23:51 -0300

    Define StoreCreationSummaryViewModel viewState

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
@@ -1,10 +1,12 @@
 package com.woocommerce.android.ui.login.storecreation.summary
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore
 import com.woocommerce.android.ui.login.storecreation.NewStore
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -14,11 +16,17 @@ class StoreCreationSummaryViewModel @Inject constructor(
     private val newStore: NewStore,
     private val createStore: CreateFreeTrialStore
 ) : ScopedViewModel(savedStateHandle) {
+    val viewState = savedState.getStateFlow(
+        scope = this,
+        initialValue = ViewState(isLoading = false)
+    ).asLiveData()
+
     fun onCancelPressed() { triggerEvent(OnCancelPressed) }
     fun onTryForFreeButtonPressed() { triggerEvent(OnStoreCreationSuccess) }
 
+    data class ViewState(val isLoading: Boolean)
+
     object OnCancelPressed : MultiLiveEvent.Event()
     object OnStoreCreationSuccess : MultiLiveEvent.Event()
-
     object OnStoreCreationFailure: MultiLiveEvent.Event()
 }
