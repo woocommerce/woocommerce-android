071b846f8cbc4d012e4da0ae784cb3e01fa9fe7a
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 12:27:27 -0300

    Make ViewState parcelable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.login.storecreation.summary
 
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore
@@ -9,6 +10,7 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.parcelize.Parcelize
 
 @HiltViewModel
 class StoreCreationSummaryViewModel @Inject constructor(
@@ -24,7 +26,9 @@ class StoreCreationSummaryViewModel @Inject constructor(
     fun onCancelPressed() { triggerEvent(OnCancelPressed) }
     fun onTryForFreeButtonPressed() { triggerEvent(OnStoreCreationSuccess) }
 
-    data class ViewState(val isLoading: Boolean)
+
+    @Parcelize
+    data class ViewState(val isLoading: Boolean) : Parcelable
 
     object OnCancelPressed : MultiLiveEvent.Event()
     object OnStoreCreationSuccess : MultiLiveEvent.Event()
