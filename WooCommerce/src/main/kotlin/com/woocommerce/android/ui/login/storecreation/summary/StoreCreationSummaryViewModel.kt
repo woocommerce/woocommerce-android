552c7e9e47996fc919eec69bb6999b96f7c7cead
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 12:07:17 -0300

    Set newStore dependency to StoreCreationSummaryViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.login.storecreation.summary
 
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore
+import com.woocommerce.android.ui.login.storecreation.NewStore
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,6 +11,7 @@ import javax.inject.Inject
 @HiltViewModel
 class StoreCreationSummaryViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val newStore: NewStore,
     private val createStore: CreateFreeTrialStore
 ) : ScopedViewModel(savedStateHandle) {
     fun onCancelPressed() { triggerEvent(OnCancelPressed) }
