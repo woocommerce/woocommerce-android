982b00ed26b577ea7e8aeaedaa6a0fd28fd650d5
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 12:29:44 -0300

    Set store creation behavior in StoreCreationSummaryViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
@@ -10,6 +10,7 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 
 @HiltViewModel
@@ -24,8 +25,17 @@ class StoreCreationSummaryViewModel @Inject constructor(
     ).asLiveData()
 
     fun onCancelPressed() { triggerEvent(OnCancelPressed) }
-    fun onTryForFreeButtonPressed() { triggerEvent(OnStoreCreationSuccess) }
-
+    fun onTryForFreeButtonPressed() {
+        launch {
+            createStore(
+                storeDomain = newStore.data.domain,
+                storeName = newStore.data.name,
+            ).collect {
+                newStore.update(siteId = it)
+                triggerEvent(OnStoreCreationSuccess)
+            }
+        }
+    }
 
     @Parcelize
     data class ViewState(val isLoading: Boolean) : Parcelable
