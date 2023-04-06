450d6989581fe4a68fabc59fabacd69fabeb1aa2
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 14:38:21 -0300

    Refactor StoreCreationSummaryViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
@@ -3,13 +3,11 @@ package com.woocommerce.android.ui.login.storecreation.summary
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore
-import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore.StoreCreationState.Failed
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore.StoreCreationState.Loading
 import com.woocommerce.android.ui.login.storecreation.NewStore
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -24,23 +22,17 @@ class StoreCreationSummaryViewModel @Inject constructor(
         .map { it is Loading }
         .asLiveData()
 
-    init {
-        launch {
-            createStore.state
-                .filter { it is Failed }
-                .collect { triggerEvent(OnStoreCreationFailure) }
-        }
-    }
-
     fun onCancelPressed() { triggerEvent(OnCancelPressed) }
     fun onTryForFreeButtonPressed() {
         launch {
             createStore(
                 storeDomain = newStore.data.domain,
                 storeName = newStore.data.name,
-            ).collect {
-                newStore.update(siteId = it)
-                triggerEvent(OnStoreCreationSuccess)
+            ).collect {siteId ->
+                siteId?.let {
+                    newStore.update(siteId = it)
+                    triggerEvent(OnStoreCreationSuccess)
+                } ?: triggerEvent(OnStoreCreationFailure)
             }
         }
     }
