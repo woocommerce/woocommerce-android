8bf0a645707402651935a132c1ceb5bc6b0dc682
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 11:59:54 -0300

    Set initial test scenario for StoreCreationSummaryViewModelTest

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModelTest.kt
@@ -4,16 +4,32 @@ import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore.StoreCreationState
 import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class StoreCreationSummaryViewModelTest: BaseUnitTest() {
     lateinit var sut: StoreCreationSummaryViewModel
     lateinit var createStore: CreateFreeTrialStore
     private val savedState = SavedStateHandle()
+
+    @Test
+    fun `when onTryForFreeButtonPressed is called, then start the store creation`() = testBlocking {
+        val expectedDomain = "test domain"
+        val expectedTitle = "test title"
+        createSut(expectedDomain, expectedTitle)
+
+        sut.onTryForFreeButtonPressed()
+
+        verify(createStore).invoke(expectedDomain, expectedTitle)
+    }
 
     private fun createSut(
         expectedDomain: String,
