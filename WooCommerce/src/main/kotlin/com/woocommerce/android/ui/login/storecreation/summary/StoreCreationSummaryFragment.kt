c7fa7af364d096d09eda00ed50f60f2cd7b2f56a
ThomazFB <thomazfcortez@gmail.com>
2023-04-06 14:27:49 -0300

    Make creation summary app bar hidden

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryFragment.kt
@@ -11,11 +11,15 @@ import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.extensions.navigateSafely
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.main.AppBarStatus
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StoreCreationSummaryFragment : BaseFragment() {
     private val viewModel: StoreCreationSummaryViewModel by viewModels()
+
+    override val activityAppBarStatus: AppBarStatus
+        get() = AppBarStatus.Hidden
 
     override fun onCreateView(
         inflater: LayoutInflater,
