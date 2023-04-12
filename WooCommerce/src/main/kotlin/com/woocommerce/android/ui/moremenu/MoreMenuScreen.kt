3c7dd6a6c2aeae801c4de438ec6f12935197ca57
ThomazFB <thomazfcortez@gmail.com>
2023-04-12 15:11:35 -0300

    Adapt app menu buttons to be set as list instead of grid

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuScreen.kt
@@ -30,9 +30,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -98,13 +97,11 @@ fun MoreMenuScreen(
 @ExperimentalFoundationApi
 @Composable
 private fun MoreMenuItems(state: MoreMenuViewState) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
+    LazyColumn(
         contentPadding = PaddingValues(
             horizontal = dimensionResource(id = R.dimen.major_100),
             vertical = dimensionResource(id = R.dimen.major_100)
         ),
-        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.major_75)),
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.major_75))
     ) {
         itemsIndexed(
