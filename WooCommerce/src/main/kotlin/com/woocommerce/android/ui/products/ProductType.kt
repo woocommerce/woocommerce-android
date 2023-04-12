cce94feeda516d3d7e6189ca990708337752a455
Alejo <alejandro.torres.veiga@automattic.com>
2023-04-12 17:05:23 -0600

    add composite product type

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductType.kt
@@ -12,6 +12,7 @@ enum class ProductType(@StringRes val stringResource: Int = 0, val value: String
     VARIABLE(R.string.product_type_variable, CoreProductType.VARIABLE.value),
     SUBSCRIPTION(R.string.product_type_subscription, "subscription"),
     VARIABLE_SUBSCRIPTION(R.string.product_type_subscription, "variable-subscription"),
+    COMPOSITE(R.string.product_type_composite, "composite"),
     OTHER;
 
     companion object {
@@ -23,6 +24,7 @@ enum class ProductType(@StringRes val stringResource: Int = 0, val value: String
                 "simple" -> SIMPLE
                 "subscription" -> SUBSCRIPTION
                 "variable-subscription" -> VARIABLE_SUBSCRIPTION
+                "composite" -> COMPOSITE
                 else -> OTHER
             }
         }
