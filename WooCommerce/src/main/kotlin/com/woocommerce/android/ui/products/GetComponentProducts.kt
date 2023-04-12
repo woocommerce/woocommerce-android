c1566daa2198ce5f6d4916f9b869a7aea01a2759
Alejo <alejandro.torres.veiga@automattic.com>
2023-04-12 17:04:37 -0600

    add mock get components use case

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/GetComponentProducts.kt
@@ -0,0 +1,30 @@
+package com.woocommerce.android.ui.products
+
+import com.woocommerce.android.model.ComponentProduct
+import com.woocommerce.android.model.QueryType
+import com.woocommerce.android.util.CoroutineDispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetComponentProducts @Inject constructor(
+    private val dispatchers: CoroutineDispatchers
+) {
+    suspend operator fun invoke(productId: Long): List<ComponentProduct> {
+        println(productId)
+        return withContext(dispatchers.io) {
+            delay(500)
+            List(6) { n ->
+                ComponentProduct(
+                    id = n.toLong(),
+                    title = "Component $n",
+                    description = "This component $n is very helpful",
+                    queryType = if (n % 2 == 0) QueryType.PRODUCT else QueryType.CATEGORY,
+                    queryIds = (0..n + 1).toList().map { it * 1L },
+                    defaultOption = n.toLong(),
+                    thumbnailUrl = null
+                )
+            }
+        }
+    }
+}
