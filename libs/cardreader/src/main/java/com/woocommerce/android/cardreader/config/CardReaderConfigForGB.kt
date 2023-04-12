45391e8617a36439c7d4dd236a313de4253a752d
samiuelson <samiuelson@gmail.com>
2023-04-12 14:54:29 +0200

    Update supported plugins for the UK

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/config/CardReaderConfigForGB.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/config/CardReaderConfigForGB.kt
@@ -12,12 +12,8 @@ object CardReaderConfigForGB : CardReaderConfigForSupportedCountry(
     paymentMethodTypes = listOf(PaymentMethodType.CARD_PRESENT),
     supportedExtensions = listOf(
         SupportedExtension(
-            type = SupportedExtensionType.STRIPE,
-            supportedSince = "6.2.0"
-        ),
-        SupportedExtension(
             type = SupportedExtensionType.WC_PAY,
-            supportedSince = "3.2.1"
+            supportedSince = "4.4.0"
         ),
     ),
 )
