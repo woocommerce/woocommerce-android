86ae0c87c8af79b2815388df42b4521ce26eabe7
Hafiz Rahman <hfzrhmn@gmail.com>
2023-04-17 08:15:33 +0700

    Add: include profiler data into API call to add ecommerce plan free trial.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/StoreCreationRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/StoreCreationRepository.kt
@@ -181,7 +181,7 @@ class StoreCreationRepository @Inject constructor(
                 val site = siteStore.getSiteBySiteId(createSiteResult.data)
                 val domain = Uri.parse(site?.url.orEmpty()).host.orEmpty()
                 newStore.update(domain = domain)
-                sitePlanRestClient.addEcommercePlanTrial(createSiteResult.data)
+                sitePlanRestClient.addEcommercePlanTrial(createSiteResult.data, siteData)
                     .let { addTrialResult ->
                         return when (addTrialResult) {
                             is WPComGsonRequestBuilder.Response.Success -> {
