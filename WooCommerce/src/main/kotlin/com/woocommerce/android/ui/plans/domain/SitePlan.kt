d1ae78826a65a801418e1c0e8dbea5f11b579248
ThomazFB <thomazfcortez@gmail.com>
2023-04-20 01:44:00 -0300

    Move plan name formatting logic to SitePlan class

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/plans/domain/SitePlan.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/plans/domain/SitePlan.kt
@@ -7,6 +7,17 @@ data class SitePlan(
     val expirationDate: ZonedDateTime,
     val type: Type,
 ) {
+    val formattedPlanName: String
+        get() {
+            return if (type == Type.FREE_TRIAL) {
+                "Free Trial"
+            } else {
+                name.removePrefix("WordPress.com")
+                    .removePrefix("Woo Express:")
+                    .trimIndent()
+            }
+        }
+
     enum class Type {
         FREE_TRIAL, OTHER
     }
