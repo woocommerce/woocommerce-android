2d8cc92219072d1f213872c0c16bc9a94f23270d
Hafiz Rahman <hfzrhmn@gmail.com>
2023-04-20 17:01:52 +0700

    Enable STORE_CREATION_PROFILER for all users now that all features are complete.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -47,12 +47,12 @@ enum class FeatureFlag {
             QUANTITY_RULES_READ_ONLY_SUPPORT,
             BUNDLED_PRODUCTS_READ_ONLY_SUPPORT,
             IPP_UK,
-            ANALYTICS_HUB_FEEDBACK_BANNER -> true
+            ANALYTICS_HUB_FEEDBACK_BANNER,
+            STORE_CREATION_PROFILER -> true
 
             MORE_MENU_INBOX,
             WC_SHIPPING_BANNER,
             IPP_TAP_TO_PAY,
-            STORE_CREATION_PROFILER,
             COMPOSITE_PRODUCTS_READ_ONLY_SUPPORT -> PackageUtils.isDebugBuild()
 
             IAP_FOR_STORE_CREATION -> false
