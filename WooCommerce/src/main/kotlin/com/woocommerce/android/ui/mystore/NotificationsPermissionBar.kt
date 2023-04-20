0dec1ccebb43920dd5d2647cd61de48f5668b8e5
Ondrej Ruttkay <ruttkay@gmail.com>
2023-04-20 13:43:01 +0200

    Add the icon back

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/NotificationsPermissionBar.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/NotificationsPermissionBar.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,6 +20,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.woocommerce.android.R
 import com.woocommerce.android.R.color
 import com.woocommerce.android.R.dimen
+import com.woocommerce.android.R.drawable
 import com.woocommerce.android.ui.compose.component.WCTextButton
 import com.woocommerce.android.ui.main.MainActivityViewModel
 
@@ -35,19 +38,25 @@ fun NotificationsPermissionCard(viewModel: MainActivityViewModel = viewModel()) 
                 .fillMaxWidth()
                 .padding(dimensionResource(id = dimen.major_100))
         ) {
-            Text(
-                text = stringResource(id = R.string.notifications_permission_title),
-                style = MaterialTheme.typography.subtitle1,
-                fontWeight = FontWeight.Bold,
-                color = colorResource(id = R.color.color_on_surface),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = dimensionResource(id = R.dimen.minor_100))
-            )
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = dimensionResource(id = dimen.minor_100))) {
+                Image(
+                    modifier = Modifier.padding(end = dimensionResource(id = dimen.minor_100)),
+                    painter = painterResource(drawable.ic_megaphone),
+                    contentDescription = "",
+                )
+                Text(
+                    text = stringResource(id = R.string.notifications_permission_title),
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(id = color.color_on_surface),
+                )
+            }
             Text(
                 text = stringResource(id = R.string.notifications_permission_description),
                 style = MaterialTheme.typography.body1,
-                color = colorResource(id = R.color.color_on_surface),
+                color = colorResource(id = color.color_on_surface),
                 modifier = Modifier.padding(bottom = dimensionResource(id = dimen.major_100))
             )
             Row(
