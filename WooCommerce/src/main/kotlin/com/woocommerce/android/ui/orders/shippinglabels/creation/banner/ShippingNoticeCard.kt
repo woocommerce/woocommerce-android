7b9e619e0e1e3ed86824b75c890521428a8c4c26
ThomazFB <thomazfcortez@gmail.com>
2023-04-26 08:00:11 -0300

    Integrate ShippingNoticeCard with bindings

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/ShippingNoticeCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/ShippingNoticeCard.kt
@@ -1,4 +1,26 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation.banner
 
-class ShippingNoticeCard {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.card.MaterialCardView
+import com.woocommerce.android.databinding.ShippingNoticeBannerBinding
+
+class ShippingNoticeCard @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : MaterialCardView(context, attrs, defStyleAttr) {
+    private val binding = ShippingNoticeBannerBinding.inflate(
+        LayoutInflater.from(context), this, true
+    )
+
+    var onLearnMoreClicked: (View) -> Unit = {}
+    var onDismissClicked: (View) -> Unit = {}
+
+    init {
+        binding.dismissButton.setOnClickListener(onDismissClicked)
+        binding.ctaButton.setOnClickListener(onLearnMoreClicked)
+    }
 }
