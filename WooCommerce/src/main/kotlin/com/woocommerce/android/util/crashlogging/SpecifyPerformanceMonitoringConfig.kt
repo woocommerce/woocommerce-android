c02d02f03be9bd43da4b133b666b72741a341ed9
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-04-27 19:11:04 +0200

    feat: adjust monitoring config logic

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/SpecifyPerformanceMonitoringConfig.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/SpecifyPerformanceMonitoringConfig.kt
@@ -2,20 +2,31 @@ package com.woocommerce.android.util.crashlogging
 
 import com.automattic.android.tracks.crashlogging.PerformanceMonitoringConfig
 import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
-import com.woocommerce.android.config.RemoteConfigRepository
+import com.woocommerce.android.util.BuildConfigWrapper
 import javax.inject.Inject
 
 class SpecifyPerformanceMonitoringConfig @Inject constructor(
-    private val remoteConfigRepository: RemoteConfigRepository,
+    private val buildConfig: BuildConfigWrapper,
     private val analyticsWrapper: AnalyticsTrackerWrapper
 ) {
+
+    private companion object {
+        const val RELEASE_PERFORMANCE_MONITORING_SAMPLE_RATE = 0.1
+        const val DEBUG_PERFORMANCE_MONITORING_SAMPLE_RATE = 1.0
+    }
+
     operator fun invoke(): PerformanceMonitoringConfig {
-        val sampleRate = remoteConfigRepository.getPerformanceMonitoringSampleRate()
+        val sampleRate = if (buildConfig.debug) {
+            DEBUG_PERFORMANCE_MONITORING_SAMPLE_RATE
+        } else {
+            RELEASE_PERFORMANCE_MONITORING_SAMPLE_RATE
+        }
         val userEnabled = analyticsWrapper.sendUsageStats
 
-        return when {
-            !userEnabled || sampleRate <= 0.0 || sampleRate > 1.0 -> PerformanceMonitoringConfig.Disabled
-            else -> PerformanceMonitoringConfig.Enabled(sampleRate)
+        return if (!userEnabled) {
+            PerformanceMonitoringConfig.Disabled
+        } else {
+            PerformanceMonitoringConfig.Enabled(sampleRate)
         }
     }
 }
