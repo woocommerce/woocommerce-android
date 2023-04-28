0c1ae89b87a189c26da545ab42a66fe97c73f628
ThomazFB <thomazfcortez@gmail.com>
2023-04-28 14:23:24 -0300

    Handle dismiss button click directly inside ShippingNoticeCard

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/ShippingNoticeCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/ShippingNoticeCard.kt
@@ -5,8 +5,10 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import com.google.android.material.card.MaterialCardView
+import com.woocommerce.android.AppPrefs
 import com.woocommerce.android.AppUrls
 import com.woocommerce.android.databinding.ShippingNoticeBannerBinding
+import com.woocommerce.android.extensions.collapse
 
 class ShippingNoticeCard @JvmOverloads constructor(
     context: Context,
@@ -16,6 +18,13 @@ class ShippingNoticeCard @JvmOverloads constructor(
     private val binding = ShippingNoticeBannerBinding.inflate(
         LayoutInflater.from(context), this, true
     )
+
+    init {
+        binding.dismissButton.setOnClickListener {
+            AppPrefs.isEUShippingNoticeDismissed = true
+            collapse()
+        }
+    }
 
     fun setLearnMoreClickListener(action: (url: String) -> Unit) {
         binding.learnMoreButton.setOnClickListener {
