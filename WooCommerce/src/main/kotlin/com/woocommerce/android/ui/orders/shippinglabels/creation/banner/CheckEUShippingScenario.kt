6cfaf419ffc85d7a0153024d14e334b2d97e526c
ThomazFB <thomazfcortez@gmail.com>
2023-05-01 14:23:13 -0300

    Place basic FeatureFlag check into CheckEUShippingScenario

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
@@ -1,7 +1,9 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation.banner
 
+import com.woocommerce.android.util.FeatureFlag
+
 class CheckEUShippingScenario {
     operator fun invoke(): Boolean {
-        return false
+        return FeatureFlag.EU_SHIPPING_NOTIFICATION.isEnabled()
     }
 }
