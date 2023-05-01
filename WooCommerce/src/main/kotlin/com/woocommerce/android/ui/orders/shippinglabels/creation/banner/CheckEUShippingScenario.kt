cccf25ecda822c993d1307734f6182d4b68db3d3
ThomazFB <thomazfcortez@gmail.com>
2023-05-01 15:51:55 -0300

    POrt CheckEUShippingScenario to use flow for better compatibility with Shipping StateMachine

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
@@ -1,9 +1,14 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation.banner
 
+import com.woocommerce.android.ui.orders.shippinglabels.creation.ShippingLabelsStateMachine
 import com.woocommerce.android.util.FeatureFlag
+import javax.inject.Inject
+import kotlinx.coroutines.flow.flow
 
-class CheckEUShippingScenario {
-    operator fun invoke(): Boolean {
-        return FeatureFlag.EU_SHIPPING_NOTIFICATION.isEnabled()
+class CheckEUShippingScenario @Inject constructor(
+    private val stateMachine: ShippingLabelsStateMachine
+) {
+    operator fun invoke() = flow {
+        emit(FeatureFlag.EU_SHIPPING_NOTIFICATION.isEnabled())
     }
 }
