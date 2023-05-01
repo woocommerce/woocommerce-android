134e64d9272bd5121ca35c3693314fbd5b933e11
ThomazFB <thomazfcortez@gmail.com>
2023-05-01 16:14:52 -0300

    Implement basic country check in CheckEUShippingScenario

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creation.banner
 
 import com.woocommerce.android.ui.orders.shippinglabels.creation.ShippingLabelsStateMachine
+import com.woocommerce.android.ui.orders.shippinglabels.creation.ShippingLabelsStateMachine.State.WaitingForInput
 import com.woocommerce.android.util.FeatureFlag
 import javax.inject.Inject
 import kotlinx.coroutines.flow.flow
@@ -9,6 +10,21 @@ class CheckEUShippingScenario @Inject constructor(
     private val stateMachine: ShippingLabelsStateMachine
 ) {
     operator fun invoke() = flow {
-        emit(FeatureFlag.EU_SHIPPING_NOTIFICATION.isEnabled())
+        if (FeatureFlag.EU_SHIPPING_NOTIFICATION.isEnabled().not()) emit(false)
+
+        stateMachine.transitions.collect {
+            when (it.state) {
+                is WaitingForInput -> emit(it.state.isEUShippingConditionMet())
+                else -> emit(false)
+
+            }
+        }
+    }
+
+    private fun WaitingForInput.isEUShippingConditionMet(): Boolean {
+        val originCountry = data.stepsState.originAddressStep.data.country
+        val destinationCountry = data.stepsState.shippingAddressStep.data.country
+
+        return originCountry.code == "US" && destinationCountry.code == "UK"
     }
 }
