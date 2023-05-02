64613d00db4ae6c284a06f2f74bc2b77c9c4dc54
Ondrej Ruttkay <ruttkay@gmail.com>
2023-05-02 18:17:16 +0200

    Remove the extra test executor rule

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/GenerateVariationBottomSheetViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/GenerateVariationBottomSheetViewModelTest.kt
@@ -1,13 +1,11 @@
 package com.woocommerce.android.ui.products.variations
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
@@ -17,9 +15,6 @@ class GenerateVariationBottomSheetViewModelTest : BaseUnitTest() {
     private lateinit var viewModel: GenerateVariationBottomSheetViewModel
     private val savedStateHandle: SavedStateHandle = mock()
     private val eventObserver: Observer<Event> = mock()
-
-    @get:Rule
-    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setUp() {
