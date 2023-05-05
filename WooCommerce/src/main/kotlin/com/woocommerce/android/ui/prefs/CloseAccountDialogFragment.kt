8eda352e765ae58f40f3167b937d4c1743b74481
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-05-05 16:06:16 +0200

    Add layout for close account dialog

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/CloseAccountDialogFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/CloseAccountDialogFragment.kt
@@ -4,14 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
-import com.woocommerce.android.R.style
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.R
+import com.woocommerce.android.R.color
+import com.woocommerce.android.R.dimen
+import com.woocommerce.android.R.string
+import com.woocommerce.android.ui.compose.component.WCOutlinedTextField
+import com.woocommerce.android.ui.compose.component.WCTextButton
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.prefs.CloseAccountViewModel.CloseAccountState
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,7 +47,7 @@ class CloseAccountDialogFragment : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Specify transition animations
-        dialog?.window?.attributes?.windowAnimations = style.Woo_Animations_Dialog
+        dialog?.window?.attributes?.windowAnimations = R.style.Woo_Animations_Dialog
 
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
@@ -29,10 +55,91 @@ class CloseAccountDialogFragment : DialogFragment() {
             setContent {
                 WooThemeWithBackground {
                     viewModel.viewState.observeAsState().value?.let { state ->
-                        Text(text = "Closing account for user ${state.userName}")
+                        CloseAccountDialog(state)
                     }
                 }
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is Event.Exit -> {
+                    findNavController().popBackStack()
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun CloseAccountDialog(state: CloseAccountState) {
+        val focusRequester = remember { FocusRequester() }
+        Column {
+            Column(
+                modifier = Modifier.padding(dimensionResource(id = dimen.major_100)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = dimen.major_75)),
+
+                ) {
+                Text(
+                    text = stringResource(id = string.settings_close_account_dialog_title),
+                    style = MaterialTheme.typography.h6,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(id = string.settings_close_account_dialog_description),
+                    style = MaterialTheme.typography.subtitle1,
+                )
+                Text(
+                    text = state.userName,
+                    style = MaterialTheme.typography.subtitle1,
+                )
+                WCOutlinedTextField(
+                    modifier = Modifier
+                        .focusRequester(focusRequester)
+                        .padding(top = dimensionResource(id = dimen.minor_100)),
+                    value = state.enteredUserName,
+                    onValueChange = { viewModel.onUserNameInputChanged(it) },
+                    label = stringResource(id = string.username),
+                    singleLine = true,
+                )
+            }
+            Divider(
+                color = colorResource(id = color.divider_color),
+                thickness = dimensionResource(id = dimen.minor_10)
+            )
+            Row(
+                modifier = Modifier.padding(
+                    start = dimensionResource(id = dimen.major_100),
+                    end = dimensionResource(id = dimen.major_100),
+                    bottom = dimensionResource(id = dimen.minor_100),
+                ),
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                WCTextButton(
+                    onClick = viewModel::onCloseAccountDismissed
+                ) {
+                    Text(
+                        text = stringResource(id = string.cancel),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                }
+                WCTextButton(
+                    onClick = viewModel::onConfirmCloseAccount,
+                    enabled = state.enteredUserName == state.userName
+                ) {
+                    Text(
+                        text = stringResource(id = string.settings_close_account_dialog_confirm_button),
+                        style = MaterialTheme.typography.subtitle1,
+                        textAlign = TextAlign.Center,
+                        color =
+                        if (state.enteredUserName == state.userName) colorResource(id = color.color_error)
+                        else colorResource(id = color.color_on_surface_disabled)
+                    )
+                }
+            }
+        }
+        LaunchedEffect(Unit) { focusRequester.requestFocus() }
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/CloseAccountViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/CloseAccountViewModel.kt
@@ -2,9 +2,8 @@ package com.woocommerce.android.ui.prefs
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
-import com.woocommerce.android.R
 import com.woocommerce.android.ui.login.AccountRepository
-import com.woocommerce.android.viewmodel.MultiLiveEvent
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -18,29 +17,28 @@ class CloseAccountViewModel @Inject constructor(
     private val _viewState = MutableLiveData(
         CloseAccountState(
             userName = accountRepository.getUserAccount()?.userName
-                ?: error("Account deletion setting requires user to log in with WP.com account")
+                ?: error("Account deletion setting requires user to log in with WP.com account"),
+            enteredUserNameError = null
         )
     )
     val viewState = _viewState
 
-    fun onCloseAccountClicked() {
-        triggerEvent(
-            MultiLiveEvent.Event.ShowDialog(
-                titleId = R.string.store_onboarding_dialog_title,
-                messageId = R.string.store_onboarding_dialog_description,
-                positiveButtonId = R.string.remove,
-                positiveBtnAction = { dialog, _ ->
+    fun onConfirmCloseAccount() {
 
-                    dialog.dismiss()
-                },
-                negativeBtnAction = { dialog, _ -> dialog.dismiss() },
-                negativeButtonId = R.string.cancel,
-            )
-        )
+    }
+
+    fun onCloseAccountDismissed() {
+        triggerEvent(Exit)
+    }
+
+    fun onUserNameInputChanged(input: String) {
+        _viewState.value = _viewState.value?.copy(enteredUserName = input)
     }
 
     data class CloseAccountState(
         val userName: String,
+        val enteredUserName: String = "",
+        val enteredUserNameError: String?,
         val isLoading: Boolean = false
     )
 }
