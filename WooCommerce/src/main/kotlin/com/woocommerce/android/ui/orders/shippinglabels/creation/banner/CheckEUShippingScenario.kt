555ab07b1ecb97b2deece2e26a919f3841fe8984
Thomaz de Freitas Barbosa Cortez <thomazfcortez@gmail.com>
2023-05-05 13:21:14 -0300

    Update WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
    Co-authored-by: Alejandro Torres Veiga <atorresveiga@gmail.com>

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/banner/CheckEUShippingScenario.kt
@@ -29,7 +29,41 @@ class CheckEUShippingScenario @Inject constructor() {
             destinationCountry.isEUCountryFollowingNewCustomRules()
     }
 
-    private fun Location.isEUCountryFollowingNewCustomRules() =
+    private fun Location.isEUCountryFollowingNewCustomRules() = code in EU_COUNTRIES_FOLLOWING_CUSTOMS_RULES
+
+    companion object {
+        const val US_COUNTRY_CODE = "US"
+        val EU_COUNTRIES_FOLLOWING_CUSTOMS_RULES = setOf(
+            "AT", "AUT",
+            "BE", "BEL",
+            "BG", "BGR",
+            "HR", "HRV",
+            "CY", "CYP",
+            "CZ", "CZE",
+            "DK", "DNK",
+            "EE", "EST",
+            "FI", "FIN",
+            "FR", "FRA",
+            "DE", "DEU",
+            "GR", "GRC",
+            "HU", "HUN",
+            "IE", "IRL",
+            "IT", "ITA",
+            "LV", "LVA",
+            "LT", "LTU",
+            "LU", "LUX",
+            "MT", "MLT",
+            "NL", "NLD",
+            "NO", "NOR",
+            "PL", "POL",
+            "PT", "PRT",
+            "RO", "ROU",
+            "SK", "SVK",
+            "SI", "SVN",
+            "ES", "ESP",
+            "SE", "SWE",
+            "CH", "CHE"
+        )
         EU_COUNTRIES_FOLLOWING_CUSTOMS_RULES.any { countryIsoCode ->
             countryIsoCode.first == code || countryIsoCode.second == code
         }
