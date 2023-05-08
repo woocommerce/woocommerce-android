05e37fc8a0b39dea56d7926013d598addcbe772e
Anirudh Uppunda <abhat38@gmail.com>
2023-05-08 15:04:36 +0530

    Implement CodeScanner interface and implementation.
    Codescanner is responsible for scanning the barcode and returning the either success or failure.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/CodeScanner.kt
@@ -0,0 +1,33 @@
+package com.woocommerce.android.ui.orders.creation
+
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+interface CodeScanner {
+    fun startScan() : Flow<CodeScannerStatus>
+}
+
+class GoogleCodeScanner @Inject constructor(private val scanner: GmsBarcodeScanner) : CodeScanner {
+    override fun startScan(): Flow<CodeScannerStatus> {
+        return callbackFlow {
+            scanner.startScan()
+                .addOnSuccessListener { code ->
+                    this@callbackFlow.trySend(CodeScannerStatus.Success(code.rawValue))
+                    this@callbackFlow.close()
+                }
+                .addOnFailureListener { throwable ->
+                    this@callbackFlow.trySend(CodeScannerStatus.Failure(throwable.cause))
+                    this@callbackFlow.close()
+                }
+            awaitClose {  }
+        }
+    }
+}
+
+sealed class CodeScannerStatus {
+    data class Success(val code: String?) : CodeScannerStatus()
+    data class Failure(val error: Throwable?) : CodeScannerStatus()
+}
