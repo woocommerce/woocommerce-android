91602830c00c783df4cc304091cc89deaf82c24c
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-05-08 20:46:10 +0200

    feat: remove tracking from Analytics switch action

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsViewModel.kt
@@ -5,21 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.AppPrefs
 import com.woocommerce.android.analytics.AnalyticsEvent
-import com.woocommerce.android.analytics.AnalyticsEvent.SETTING_CHANGE
-import com.woocommerce.android.analytics.AnalyticsEvent.SETTING_CHANGE_FAILED
-import com.woocommerce.android.analytics.AnalyticsEvent.SETTING_CHANGE_SUCCESS
 import com.woocommerce.android.analytics.AnalyticsTracker
-import com.woocommerce.android.util.AnalyticsUtils
 import com.woocommerce.android.util.dispatchAndAwait
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.action.AccountAction
 import org.wordpress.android.fluxc.generated.AccountActionBuilder
 import org.wordpress.android.fluxc.store.AccountStore
-import org.wordpress.android.fluxc.store.AccountStore.AccountErrorType
 import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged
 import org.wordpress.android.fluxc.store.AccountStore.PushAccountSettingsPayload
 import javax.inject.Inject
@@ -45,62 +39,24 @@ class PrivacySettingsViewModel @Inject constructor(
     private fun getSendUsageStats() = !accountStore.account.tracksOptOut
 
     private fun setSendUsageStats(sendUsageStats: Boolean) {
-        // note that we don't init/disable Crashlytics here because that requires the app to be restarted
         AnalyticsTracker.sendUsageStats = sendUsageStats
 
         launch {
-            // sync with wpcom if a token is available
             if (accountStore.hasAccessToken()) {
-                AnalyticsTracker.track(
-                    SETTING_CHANGE,
-                    mapOf(
-                        AnalyticsTracker.KEY_NAME to SETTING_TRACKS_OPT_OUT,
-                        AnalyticsTracker.KEY_FROM to !sendUsageStats,
-                        AnalyticsTracker.KEY_TO to sendUsageStats
-                    )
-                )
-
                 val payload = PushAccountSettingsPayload().apply {
                     params = mapOf(SETTING_TRACKS_OPT_OUT to !sendUsageStats)
                 }
 
                 val action = AccountActionBuilder.newPushSettingsAction(payload)
-                val event: OnAccountChanged = dispatcher.dispatchAndAwait(action)
-
-                handleOnAccountChanged(event)
+                dispatcher.dispatchAndAwait<PushAccountSettingsPayload?, OnAccountChanged>(action)
             }
         }
     }
 
-    fun getCrashReportingEnabled() = AppPrefs.isCrashReportingEnabled()
+    private fun getCrashReportingEnabled() = AppPrefs.isCrashReportingEnabled()
 
     private fun setCrashReportingEnabled(enabled: Boolean) {
         AppPrefs.setCrashReportingEnabled(enabled)
-    }
-
-    private fun handleOnAccountChanged(event: OnAccountChanged) {
-        if (event.isError) {
-            when (event.error.type) {
-                AccountErrorType.SETTINGS_POST_ERROR -> {
-                    AnalyticsTracker.track(
-                        SETTING_CHANGE_FAILED,
-                        this::class.java.simpleName,
-                        event.error.type.toString(),
-                        event.error.message
-                    )
-                }
-
-                else -> {}
-            }
-        } else {
-            when (event.causeOfChange) {
-                AccountAction.PUSH_SETTINGS -> {
-                    AnalyticsTracker.track(SETTING_CHANGE_SUCCESS)
-                }
-
-                else -> Unit // Do nothing
-            }
-        }
     }
 
     fun onPrivacyPolicyClicked() {
@@ -118,24 +74,11 @@ class PrivacySettingsViewModel @Inject constructor(
     }
 
     fun onCrashReportingSettingChanged(checked: Boolean) {
-        AnalyticsTracker.track(
-            AnalyticsEvent.PRIVACY_SETTINGS_CRASH_REPORTING_TOGGLED,
-            mapOf(
-                AnalyticsTracker.KEY_STATE to AnalyticsUtils.getToggleStateLabel(checked)
-            )
-        )
         _state.value = _state.value?.copy(crashReportingEnabled = checked)
         setCrashReportingEnabled(checked)
     }
 
     fun onSendStatsSettingChanged(checked: Boolean) {
-        AnalyticsTracker.track(
-            AnalyticsEvent.PRIVACY_SETTINGS_COLLECT_INFO_TOGGLED,
-            mapOf(
-                AnalyticsTracker.KEY_STATE to
-                    AnalyticsUtils.getToggleStateLabel(checked)
-            )
-        )
         _state.value = _state.value?.copy(sendUsageStats = checked)
         setSendUsageStats(checked)
     }
