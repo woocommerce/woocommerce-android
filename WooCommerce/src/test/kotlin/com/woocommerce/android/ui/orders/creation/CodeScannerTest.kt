eda399bf08a0bcc9e595acfad0e7c2ec4d2d4b35
Anirudh Uppunda <abhat38@gmail.com>
2023-05-09 13:26:30 +0530

    Add test to verify proper failure message is emitted when the code scanning fails in CodeScannerTest.kt

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/CodeScannerTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/CodeScannerTest.kt
@@ -110,4 +110,31 @@ class CodeScannerTest : BaseUnitTest() {
             assertThat(result).isExactlyInstanceOf(CodeScannerStatus.Failure::class.java)
         }
     }
+
+    @Test
+    fun `when scanning code fails, then proper failure message is emitted`() {
+        testBlocking {
+            val errorMessage = "Invalid Barcode"
+            val mockBarcode = mock<Task<Barcode>>()
+            whenever(scanner.startScan()).thenAnswer {
+                mockBarcode
+            }
+            whenever(mockBarcode.addOnSuccessListener(any())).thenReturn(mockBarcode)
+            whenever(mockBarcode.addOnFailureListener(any())).thenAnswer {
+                @Suppress("UNCHECKED_CAST")
+                (it.arguments[0] as OnFailureListener).onFailure(
+                    mock {
+                        on {
+                            cause
+                        }.thenReturn(Throwable(errorMessage))
+                    }
+                )
+                mock<Task<Barcode>>()
+            }
+
+            val result = codeScanner.startScan().first()
+
+            assertThat((result as CodeScannerStatus.Failure).error?.message).isEqualTo(errorMessage)
+        }
+    }
 }
