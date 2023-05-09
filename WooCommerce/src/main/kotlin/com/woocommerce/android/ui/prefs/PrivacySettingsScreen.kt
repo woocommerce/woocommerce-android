b92f37f92b9729bddc9ee4d7f30a9718bdf5f563
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-05-09 20:40:51 +0200

    feat: redesign screen to not use Card

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/PrivacySettingsScreen.kt
@@ -1,9 +1,11 @@
 package com.woocommerce.android.ui.prefs
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -24,8 +25,10 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
@@ -60,11 +63,9 @@ fun PrivacySettingsScreen(
                 text = stringResource(R.string.settings_privacy_statement),
                 modifier = Modifier.padding(16.dp)
             )
-            Card(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp),
-                elevation = 8.dp,
+                    .fillMaxWidth(),
                 content = {
                     Column {
                         OptionRow(
@@ -74,6 +75,7 @@ fun PrivacySettingsScreen(
                             sectionTitle = stringResource(R.string.settings_tracking_analytics),
                             sectionDescription = stringResource(R.string.settings_tracking_analytics_description),
                         )
+                        Spacer(modifier = Modifier.height(16.dp))
                         OptionRow(
                             switchChecked = state.crashReportingEnabled,
                             onReportCrashesChanged,
@@ -97,29 +99,33 @@ private fun OptionRow(
     sectionDescription: String,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier.padding(16.dp)) {
+    Column(modifier = modifier) {
         Text(
             text = sectionHeader,
-            style = MaterialTheme.typography.caption,
-            modifier = Modifier.padding(bottom = 16.dp),
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
             color = MaterialTheme.colors.primary,
         )
         Row(
             modifier = Modifier
                 .height(IntrinsicSize.Min)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .clickable {
+                    onSwitchChanged(!switchChecked)
+                },
             verticalAlignment = Alignment.CenterVertically
         ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(end = 16.dp)
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
             ) {
                 Text(
                     text = sectionTitle,
+                    style = MaterialTheme.typography.subtitle1,
                 )
                 Text(
-                    style = MaterialTheme.typography.caption,
+                    style = textAppearanceWooBody2(),
                     text = sectionDescription,
                 )
             }
@@ -141,9 +147,19 @@ private fun OptionRow(
     }
 }
 
+@Composable
+// Style of TextAppearance.Woo.Body2
+private fun textAppearanceWooBody2() = TextStyle(
+    lineHeight = 20.sp,
+    color = MaterialTheme.colors.onSurface.copy(
+        alpha = 0.60f
+    ),
+    fontSize = 14.sp,
+)
+
 @Preview(name = "Light mode")
-@Preview(name = "RTL mode", locale = "ar")
 @Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "RTL mode", locale = "ar")
 @Composable
 private fun Default() {
     WooThemeWithBackground {
