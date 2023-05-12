2a71edb8c83fd6c01ab31f241a691022848b6971
Hafiz Rahman <hfzrhmn@gmail.com>
2023-05-12 10:31:55 +0700

    Add: Result and error handling for fetching feature flags.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/config/WPComRemoteFeatureFlagRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/config/WPComRemoteFeatureFlagRepository.kt
@@ -1,6 +1,8 @@
 package com.woocommerce.android.config
 
+import com.woocommerce.android.OnChangedException
 import org.wordpress.android.fluxc.store.mobile.FeatureFlagsStore
+import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,13 +13,35 @@ class WPComRemoteFeatureFlagRepository @Inject constructor(
     companion object {
         private const val PLATFORM_NAME = "android"
     }
-    // Fetch all WPCOM feature flags for Android, and optionally only for certain app version
-    suspend fun fetchFeatureFlags(appVersion: String = "") =
-        featureFlagsStore.fetchFeatureFlags(
-        buildNumber = "",
-        deviceId = "",
-        identifier = "",
-        marketingVersion = appVersion,
-        platform = PLATFORM_NAME
+
+    suspend fun fetchFeatureFlags(appVersion: String = ""): Result<List<RemoteFeatureFlag>> {
+        // Empty string are parameters not used by this app.
+        val result = featureFlagsStore.fetchFeatureFlags(
+            buildNumber = "",
+            deviceId = "",
+            identifier = "",
+            marketingVersion = appVersion,
+            platform = PLATFORM_NAME
+        )
+        return if (result.isError) {
+            AppLog.e(AppLog.T.API, "Error fetching WPCom remote feature flags: ${result.error}")
+
+            Result.failure(OnChangedException(result.error, result.error.message))
+        } else {
+            AppLog.i(AppLog.T.API, "Successfully fetched WPCom remote feature flags")
+
+            val remoteFeatureFlags = result.featureFlags?.map { flag ->
+                RemoteFeatureFlag(
+                    key = flag.key,
+                    value = flag.value
+                )
+            }.orEmpty()
+            Result.success(remoteFeatureFlags)
+        }
+    }
+
+    class RemoteFeatureFlag(
+        val key: String,
+        val value: Boolean
     )
 }
