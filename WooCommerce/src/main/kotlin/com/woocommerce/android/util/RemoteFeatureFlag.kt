6620582394a305a617f735d5c4fd90e8d3767571
Hafiz Rahman <hfzrhmn@gmail.com>
2023-05-12 19:17:26 +0700

    Add: RemoteFeatureFlag class for setting up remote feature flags.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/RemoteFeatureFlag.kt
@@ -0,0 +1,30 @@
+package com.woocommerce.android.util
+
+import com.woocommerce.android.config.WPComRemoteFeatureFlagRepository
+
+enum class RemoteFeatureFlag(private val remoteKey: String) {
+    LOCAL_NOTIFICATION_STORE_CREATION_READY("woo_notification_store_creation_ready"),
+    LOCAL_NOTIFICATION_NUDGE_FREE_TRIAL_AFTER_1D("woo_notification_nudge_free_trial_after_1d"),
+    LOCAL_NOTIFICATION_1D_BEFORE_FREE_TRIAL_EXPIRES("woo_notification_1d_before_free_trial_expires"),
+    LOCAL_NOTIFICATION_1D_AFTER_FREE_TRIAL_EXPIRES("woo_notification_1d_after_free_trial_expires");
+
+    companion object {
+        lateinit var wpComRemoteFeatureFlagRepository: WPComRemoteFeatureFlagRepository
+    }
+
+    // Similar to how it works in `FeatureFlag`, add `PackageUtils.isDebugBuild()` to force-enable
+    // the feature flag in debug builds.
+    fun isEnabled() : Boolean {
+        return when (this) {
+            LOCAL_NOTIFICATION_STORE_CREATION_READY,
+            LOCAL_NOTIFICATION_NUDGE_FREE_TRIAL_AFTER_1D,
+            LOCAL_NOTIFICATION_1D_BEFORE_FREE_TRIAL_EXPIRES,
+            LOCAL_NOTIFICATION_1D_AFTER_FREE_TRIAL_EXPIRES ->
+                PackageUtils.isDebugBuild() || getRemoteFlagValue(remoteKey)
+        }
+    }
+
+    private fun getRemoteFlagValue(key: String): Boolean {
+        return wpComRemoteFeatureFlagRepository.getFeatureFlag(key)?.value ?: false
+    }
+}
