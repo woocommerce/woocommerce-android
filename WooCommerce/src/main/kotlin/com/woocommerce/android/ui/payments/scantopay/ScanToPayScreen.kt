702779b8ce8e444cb9577ea16b2165f3e86ad29c
kidinov <andrey.kidinov@a8c.com>
2023-05-15 15:15:34 +0500

    Rounded corners for the QR dialog

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/scantopay/ScanToPayScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/scantopay/ScanToPayScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -28,6 +30,7 @@ fun ScanToPayScreen(
 ) {
     Column(
         modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
             .background(color = colorResource(id = R.color.color_surface_elevated))
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
