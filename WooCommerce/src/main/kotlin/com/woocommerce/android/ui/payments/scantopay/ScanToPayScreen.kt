a56c8af3a487bfb9a6215e6bda4902dac297692c
kidinov <andrey.kidinov@a8c.com>
2023-05-16 17:37:17 +0500

    Fixed import ordering

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/scantopay/ScanToPayScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/scantopay/ScanToPayScreen.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
