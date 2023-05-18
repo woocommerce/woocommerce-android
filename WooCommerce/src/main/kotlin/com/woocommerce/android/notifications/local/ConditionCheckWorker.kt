940e5d1119d9445c36ec3cb716472d1192630c89
Ondrej Ruttkay <ruttkay@gmail.com>
2023-05-18 23:31:56 +0200

    Add a condition check worker, which will allow conditional display of notifications

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/local/ConditionCheckWorker.kt
@@ -0,0 +1,51 @@
+package com.woocommerce.android.notifications.local
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.woocommerce.android.notifications.local.LocalNotification.Companion.NOTIFICATION_TYPE_AFTER_FREE_TRIAL
+import com.woocommerce.android.notifications.local.LocalNotification.Companion.NOTIFICATION_TYPE_BEFORE_FREE_TRIAL
+import com.woocommerce.android.notifications.local.LocalNotification.Companion.NOTIFICATION_TYPE_STORE_CREATION_COMPLETE
+import com.woocommerce.android.notifications.local.LocalNotification.Companion.NOTIFICATION_TYPE_WITHOUT_FREE_TRIAL
+import com.woocommerce.android.notifications.local.LocalNotificationScheduler.Companion.LOCAL_NOTIFICATION_CONDITION
+import com.woocommerce.android.notifications.local.LocalNotificationScheduler.Companion.LOCAL_NOTIFICATION_TYPE
+import com.woocommerce.android.util.WooLog.T
+import com.woocommerce.android.util.WooLog.T.NOTIFICATIONS
+import com.woocommerce.android.util.WooLogWrapper
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+@HiltWorker
+@Suppress("ComplexCondition", "ForbiddenComment")
+class ConditionCheckWorker @AssistedInject constructor(
+    @Assisted private val appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val wooLogWrapper: WooLogWrapper,
+) : Worker(appContext, workerParams) {
+    override fun doWork(): Result {
+        val tag = inputData.getString(LOCAL_NOTIFICATION_TYPE)
+        return if (tag != null) {
+            when (tag) {
+                "test", // TODO: Remove
+                NOTIFICATION_TYPE_STORE_CREATION_COMPLETE,
+                NOTIFICATION_TYPE_WITHOUT_FREE_TRIAL,
+                NOTIFICATION_TYPE_BEFORE_FREE_TRIAL,
+                NOTIFICATION_TYPE_AFTER_FREE_TRIAL -> {
+                    wooLogWrapper.i(NOTIFICATIONS, "Condition satisfied for $tag")
+                    return Result.success()
+                }
+                else -> {
+                    wooLogWrapper.i(NOTIFICATIONS, "Condition NOT satisfied for $tag. Cancelling work.")
+                    WorkManager.getInstance(appContext).cancelWorkById(id)
+                    Result.failure()
+                }
+            }
+        } else {
+            wooLogWrapper.e(NOTIFICATIONS, "Notification check data is invalid")
+            Result.failure()
+        }
+    }
+}
--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/local/LocalNotificationScheduler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/local/LocalNotificationScheduler.kt
@@ -1,14 +1,13 @@
 package com.woocommerce.android.notifications.local
 
 import android.content.Context
+import androidx.work.ExistingWorkPolicy.REPLACE
+import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.WorkRequest
 import androidx.work.workDataOf
 import com.woocommerce.android.analytics.AnalyticsEvent
 import com.woocommerce.android.analytics.AnalyticsTracker
-import com.woocommerce.android.util.WooLog
-import com.woocommerce.android.util.WooLog.T
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -16,37 +15,48 @@ class LocalNotificationScheduler @Inject constructor(
     @ApplicationContext private val appContext: Context
 ) {
     companion object {
-        const val LOCAL_NOTIFICATION_TAG = "local-notification-tag"
-        const val LOCAL_NOTIFICATION_ID = "local-notification-id"
-        const val LOCAL_NOTIFICATION_TITLE = "local-notification-title"
-        const val LOCAL_NOTIFICATION_DESC = "local-notification-description"
+        const val LOCAL_NOTIFICATION_WORK_NAME = "Scheduled notification work"
+        const val LOCAL_NOTIFICATION_TYPE = "local_notification_type"
+        const val LOCAL_NOTIFICATION_ID = "local_notification_id"
+        const val LOCAL_NOTIFICATION_TITLE = "local_notification_title"
+        const val LOCAL_NOTIFICATION_DESC = "local_notification_description"
     }
 
     private val workManager = WorkManager.getInstance(appContext)
 
     fun scheduleNotification(notification: LocalNotification) {
         cancelScheduledNotification(notification)
-        val notificationData = workDataOf(
-            LOCAL_NOTIFICATION_TAG to notification.type,
-            LOCAL_NOTIFICATION_ID to notification.id,
-            LOCAL_NOTIFICATION_TITLE to notification.title,
-            LOCAL_NOTIFICATION_DESC to notification.description
-        )
-        val workRequest: WorkRequest =
-            OneTimeWorkRequestBuilder<LocalNotificationWorker>()
-                .setInputData(notificationData)
-                .addTag(notification.type)
-                .setInitialDelay(notification.delay, notification.delayUnit)
-                .build()
+
+        workManager
+            .beginUniqueWork(LOCAL_NOTIFICATION_WORK_NAME, REPLACE, buildConditionWorkRequest(notification))
+            .then(buildNotificationWorkRequest(notification))
+            .enqueue()
 
         AnalyticsTracker.track(
             AnalyticsEvent.LOCAL_NOTIFICATION_SCHEDULED,
             mapOf(AnalyticsTracker.KEY_TYPE to notification.type)
         )
+    }
 
-        WooLog.d(T.NOTIFICATIONS, "Local notification SCHEDULED: $notification")
+    private fun buildConditionWorkRequest(notification: LocalNotification): OneTimeWorkRequest {
+        val conditionData = workDataOf(LOCAL_NOTIFICATION_TYPE to notification.type)
+        return OneTimeWorkRequestBuilder<ConditionCheckWorker>()
+                .setInputData(conditionData)
+                .addTag(notification.type)
+                .setInitialDelay(notification.delay, notification.delayUnit)
+                .build()
+    }
 
-        workManager.enqueue(workRequest)
+    private fun buildNotificationWorkRequest(notification: LocalNotification): OneTimeWorkRequest {
+        val notificationData = workDataOf(
+            LOCAL_NOTIFICATION_TYPE to notification.type,
+            LOCAL_NOTIFICATION_ID to notification.id,
+            LOCAL_NOTIFICATION_TITLE to notification.title,
+            LOCAL_NOTIFICATION_DESC to notification.description
+        )
+        return OneTimeWorkRequestBuilder<LocalNotificationWorker>()
+                .setInputData(notificationData)
+                .build()
     }
 
     private fun cancelScheduledNotification(notification: LocalNotification) {
