3f595c9f9fcbf22895827fd1eeb2d4b521389cd5
Anirudh Uppunda <abhat38@gmail.com>
2023-05-18 13:46:52 +0530

    Move the fetchProductBySKU call when barcode navargs is present into the creation mode logic

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditViewModel.kt
@@ -168,6 +168,11 @@ class OrderCreateEditViewModel @Inject constructor(
                     )
                 }
                 monitorOrderChanges()
+                // Presence of barcode indicates that this screen was called from the
+                // Order listing screen after scanning the barcode.
+                args.barcode?.let { code ->
+                    fetchProductBySKU(code)
+                }
             }
             is Mode.Edit -> {
                 viewModelScope.launch {
@@ -180,11 +185,6 @@ class OrderCreateEditViewModel @Inject constructor(
                             multipleLinesContext = determineMultipleLinesContext(order)
                         )
                         monitorOrderChanges()
-                        // Presence of barcode indicates that this screen was called from the
-                        // Order listing screen after scanning the barcode.
-                        args.barcode?.let { code ->
-                            fetchProductBySKU(code)
-                        }
                     }
                 }
             }
