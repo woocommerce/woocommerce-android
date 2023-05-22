9c0e6bdbd78a91d3bd09fd8edf2958f848124911
Alejo <alejandro.torres.veiga@automattic.com>
2023-05-22 10:43:14 -0300

    move toOrderProducts to a mapper class

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/OrderProduct.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/OrderProduct.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.orders.details
 import android.os.Parcelable
 import com.woocommerce.android.model.Order
 import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
 
 @Parcelize
 sealed class OrderProduct : Parcelable {
@@ -17,34 +18,45 @@ sealed class OrderProduct : Parcelable {
     ) : OrderProduct()
 }
 
-fun List<Order.Item>.toOrderProducts(): List<OrderProduct> {
-    if (isEmpty()) return emptyList()
+class OrderProductMapper @Inject constructor() {
+    fun toOrderProducts(currentProducts: List<OrderProduct>, newProducts: List<Order.Item>): List<OrderProduct> {
+        if (newProducts.isEmpty()) return emptyList()
 
-    val itemsMap = associateBy { item -> item.itemId }
-    val childrenMap = mutableMapOf<Long, MutableList<OrderProduct.ProductItem>>()
+        val isExpanded = currentProducts
+            .filterIsInstance<OrderProduct.GroupedProductItem>()
+            .associate { Pair(it.product.itemId, it.isExpanded) }
 
-    val result = mapNotNull { item ->
-        if (item.parent == null) {
-            item
-        } else {
-            val children = childrenMap.getOrPut(
-                item.parent
-            ) { mutableListOf() }
+        val itemsMap = newProducts.associateBy { item -> item.itemId }
+        val childrenMap = mutableMapOf<Long, MutableList<OrderProduct.ProductItem>>()
 
-            children.add(OrderProduct.ProductItem(item))
-            null
+        val result = newProducts.mapNotNull { item ->
+            if (item.parent == null) {
+                item
+            } else {
+                val children = childrenMap.getOrPut(
+                    item.parent
+                ) { mutableListOf() }
+
+                children.add(OrderProduct.ProductItem(item))
+                null
+            }
+        }.filter { item ->
+            (item.itemId in childrenMap.keys).not()
+        }.map<Order.Item, OrderProduct> { OrderProduct.ProductItem(it) }
+            .toMutableList()
+
+        for (parentId in childrenMap.keys) {
+            val parent = itemsMap[parentId] ?: continue
+            val children = childrenMap[parentId] ?: emptyList()
+
+            val groupedProduct =
+                OrderProduct.GroupedProductItem(
+                    parent,
+                    children,
+                    isExpanded.getOrDefault(parent.itemId, false)
+                )
+            result.add(groupedProduct)
         }
-    }.filter { item ->
-        (item.itemId in childrenMap.keys).not()
-    }.map<Order.Item, OrderProduct> { OrderProduct.ProductItem(it) }
-        .toMutableList()
-
-    for (parentId in childrenMap.keys) {
-        val parent = itemsMap[parentId] ?: continue
-        val children = childrenMap[parentId] ?: emptyList()
-
-        val groupedProduct = OrderProduct.GroupedProductItem(parent, children)
-        result.add(groupedProduct)
+        return result
     }
-    return result
 }
