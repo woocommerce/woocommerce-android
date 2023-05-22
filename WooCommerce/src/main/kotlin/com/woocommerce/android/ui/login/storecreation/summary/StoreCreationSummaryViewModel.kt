c222ce4aeeada388970bf6a32398bf7364c0c94e
Ondrej Ruttkay <ruttkay@gmail.com>
2023-05-22 14:36:58 +0200

    Schedule a local notification job when site creation begins if feature flag enabled

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/summary/StoreCreationSummaryViewModel.kt
@@ -5,17 +5,24 @@ import androidx.lifecycle.asLiveData
 import com.woocommerce.android.analytics.AnalyticsEvent
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
+import com.woocommerce.android.extensions.isNotNullOrEmpty
+import com.woocommerce.android.notifications.local.LocalNotification.StoreCreationCompleteNotification
+import com.woocommerce.android.notifications.local.LocalNotificationScheduler
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore.StoreCreationState.Failed
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore.StoreCreationState.Finished
 import com.woocommerce.android.ui.login.storecreation.CreateFreeTrialStore.StoreCreationState.Loading
 import com.woocommerce.android.ui.login.storecreation.NewStore
+import com.woocommerce.android.util.IsRemoteFeatureFlagEnabled
+import com.woocommerce.android.util.RemoteFeatureFlag.LOCAL_NOTIFICATION_STORE_CREATION_READY
 import com.woocommerce.android.viewmodel.MultiLiveEvent
+import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.wordpress.android.fluxc.store.AccountStore
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,7 +30,11 @@ class StoreCreationSummaryViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val newStore: NewStore,
     private val createStore: CreateFreeTrialStore,
-    private val tracker: AnalyticsTrackerWrapper
+    private val tracker: AnalyticsTrackerWrapper,
+    private val resourceProvider: ResourceProvider,
+    private val localNotificationScheduler: LocalNotificationScheduler,
+    private val isRemoteFeatureFlagEnabled: IsRemoteFeatureFlagEnabled,
+    private val accountStore: AccountStore
 ) : ScopedViewModel(savedStateHandle) {
     private val _isLoading = savedStateHandle.getStateFlow(scope = this, initialValue = false)
     val isLoading = _isLoading.asLiveData()
@@ -52,6 +63,15 @@ class StoreCreationSummaryViewModel @Inject constructor(
 
     fun onTryForFreeButtonPressed() {
         tracker.track(AnalyticsEvent.SITE_CREATION_TRY_FOR_FREE_TAPPED)
+
+        if (isRemoteFeatureFlagEnabled(LOCAL_NOTIFICATION_STORE_CREATION_READY)) {
+            val name = if (accountStore.account.firstName.isNotNullOrEmpty())
+                accountStore.account.firstName
+            else
+                accountStore.account.userName
+            localNotificationScheduler.scheduleNotification(StoreCreationCompleteNotification(name, resourceProvider))
+        }
+
         launch {
             createStore(
                 storeDomain = newStore.data.domain,
