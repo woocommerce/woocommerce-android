f2003ee885b871cd740135d0d981f7011fb8a725
Ondrej Ruttkay <ruttkay@gmail.com>
2023-05-22 14:49:17 +0200

    Refactor the new store notification data class

--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/local/LocalNotification.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/local/LocalNotification.kt
@@ -1,76 +1,44 @@
 package com.woocommerce.android.notifications.local
 
+import androidx.annotation.StringRes
+import com.woocommerce.android.R
+import com.woocommerce.android.viewmodel.ResourceProvider
 import java.util.concurrent.TimeUnit
 
-@Suppress("ForbiddenComment")
 sealed class LocalNotification(
-    open val title: String,
-    open val description: String,
+    @StringRes val title: Int,
+    @StringRes val description: Int,
     val type: String,
     val delay: Long,
     val delayUnit: TimeUnit
 ) {
     companion object {
-        const val NOTIFICATION_TYPE_STORE_CREATION_COMPLETE = "store_creation_complete"
-        const val NOTIFICATION_TYPE_WITHOUT_FREE_TRIAL = "one_day_after_store_creation_name_without_free_trial"
-        const val NOTIFICATION_TYPE_BEFORE_FREE_TRIAL = "one_day_before_free_trial_expires"
-        const val NOTIFICATION_TYPE_AFTER_FREE_TRIAL = "one_day_after_free_trial_expires"
+        const val STORE_CREATION_COMPLETE_NOTICE = "store_creation_complete"
+        const val FREE_TRIAL_REMINDER = "one_day_after_store_creation_name_without_free_trial"
+        const val FREE_TRIAL_EXPIRING_NOTICE = "one_day_before_free_trial_expires"
+        const val FREE_TRIAL_EXPIRED_NOTICE = "one_day_after_free_trial_expires"
     }
+
     val id = type.hashCode()
 
-    // TODO: Remove this test notification
-    data class Test(
-        override val title: String,
-        override val description: String
-    ) : LocalNotification(
-        title = title,
-        description = description,
-        type = "test",
-        delay = 10,
-        delayUnit = TimeUnit.SECONDS
-    )
+    abstract fun getTitleString(resourceProvider: ResourceProvider): String
+    abstract fun getDescriptionString(resourceProvider: ResourceProvider): String
 
-    data class NewStoreReadyNotice(
-        override val title: String,
-        override val description: String
+    data class StoreCreationCompleteNotification(
+        val name: String
     ) : LocalNotification(
-        title = title,
-        description = description,
-        type = NOTIFICATION_TYPE_STORE_CREATION_COMPLETE,
+        title = R.string.local_notification_store_creation_complete_title,
+        description = R.string.local_notification_store_creation_complete_description,
+        type = STORE_CREATION_COMPLETE_NOTICE,
         delay = 5,
         delayUnit = TimeUnit.MINUTES
-    )
+    ) {
+        override fun getTitleString(resourceProvider: ResourceProvider): String {
+            return resourceProvider.getString(title)
+        }
 
-    data class FreeTrialReminder(
-        override val title: String,
-        override val description: String
-    ) : LocalNotification(
-        title = title,
-        description = description,
-        type = NOTIFICATION_TYPE_WITHOUT_FREE_TRIAL,
-        delay = 24,
-        delayUnit = TimeUnit.HOURS
-    )
-
-    data class FreeTrialExpiringNotice(
-        override val title: String,
-        override val description: String
-    ) : LocalNotification(
-        title = title,
-        description = description,
-        type = NOTIFICATION_TYPE_BEFORE_FREE_TRIAL,
-        delay = 13,
-        delayUnit = TimeUnit.DAYS
-    )
-
-    data class FreeTrialExpiredNotice(
-        override val title: String,
-        override val description: String
-    ) : LocalNotification(
-        title = title,
-        description = description,
-        type = NOTIFICATION_TYPE_AFTER_FREE_TRIAL,
-        delay = 15,
-        delayUnit = TimeUnit.DAYS
-    )
+        override fun getDescriptionString(resourceProvider: ResourceProvider): String {
+            return resourceProvider.getString(description, name)
+        }
+    }
 }
