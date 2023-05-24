f72e8443e3b10b4482f14cdc24f9fc780fe82309
Ondrej Ruttkay <ruttkay@gmail.com>
2023-05-24 21:43:10 +0200

    Fix the notification delay unit to days

--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/local/LocalNotification.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/local/LocalNotification.kt
@@ -54,7 +54,7 @@ sealed class LocalNotification(
         description = R.string.local_notification_one_day_before_free_trial_expires_description,
         type = LocalNotificationType.FREE_TRIAL_EXPIRING,
         delay = 13,
-        delayUnit = TimeUnit.SECONDS
+        delayUnit = TimeUnit.DAYS
     ) {
         override val data: String = siteId.toString()
 
