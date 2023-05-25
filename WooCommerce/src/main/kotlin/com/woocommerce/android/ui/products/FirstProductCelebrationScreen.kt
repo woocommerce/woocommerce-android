869271b633c48800301e328f017819247f8f4e54
Hafiz Rahman <hfzrhmn@gmail.com>
2023-05-25 18:47:11 +0700

    Add: Share button to the celebration dialog.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/FirstProductCelebrationScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/FirstProductCelebrationScreen.kt
@@ -3,16 +3,20 @@ package com.woocommerce.android.ui.products
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.component.WCColoredButton
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
@@ -39,7 +43,9 @@ fun FirstProductCelebrationScreen(
                 .padding(top = dimensionResource(id = R.dimen.major_350))
                 .verticalScroll(rememberScrollState())
         ) {
-            // TODO
+            WCColoredButton(onClick = onShareClick, modifier = Modifier.fillMaxWidth()) {
+                Text(text = stringResource(R.string.share))
+            }
         }
     }
 }
