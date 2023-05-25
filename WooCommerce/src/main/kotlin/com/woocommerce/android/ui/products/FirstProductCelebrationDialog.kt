7d0b49865116af5ab5a749cafe16d23e10009d8d
Hafiz Rahman <hfzrhmn@gmail.com>
2023-05-25 17:41:11 +0700

    Add: Starter dialog fragment for the first product celebration view.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/FirstProductCelebrationDialog.kt
@@ -0,0 +1,38 @@
+package com.woocommerce.android.ui.products
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+import org.wordpress.android.util.DisplayUtils
+
+@AndroidEntryPoint
+class FirstProductCelebrationDialog: DialogFragment() {
+    companion object {
+        private const val TABLET_LANDSCAPE_WIDTH_RATIO = 0.35f
+        private const val TABLET_LANDSCAPE_HEIGHT_RATIO = 0.8f
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (isTabletLandscape()) {
+            requireDialog().window!!.setLayout(
+                (DisplayUtils.getWindowPixelWidth(requireContext()) * TABLET_LANDSCAPE_WIDTH_RATIO).toInt(),
+                (DisplayUtils.getWindowPixelHeight(requireContext()) * TABLET_LANDSCAPE_HEIGHT_RATIO).toInt()
+            )
+        }
+    }
+
+    private fun isTabletLandscape() = (DisplayUtils.isTablet(context) || DisplayUtils.isXLargeTablet(context)) &&
+        DisplayUtils.isLandscape(context)
+}
