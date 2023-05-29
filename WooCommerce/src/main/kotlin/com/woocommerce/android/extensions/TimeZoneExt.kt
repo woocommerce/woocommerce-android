65a2e64b9cd2ba8a5c0093ea6e2e9a5c89a8508a
ThomazFB <thomazfcortez@gmail.com>
2023-05-29 19:42:24 -0300

    Implement offsetInHours property in TimeZoneExt.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/TimeZoneExt.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/TimeZoneExt.kt
@@ -1,2 +1,9 @@
 package com.woocommerce.android.extensions
 
+import java.util.TimeZone
+import java.util.concurrent.TimeUnit
+
+val TimeZone.offsetInHours: Int
+    get() = rawOffset.toLong().let {
+        TimeUnit.HOURS.convert(it, TimeUnit.MILLISECONDS)
+    }.toInt()
