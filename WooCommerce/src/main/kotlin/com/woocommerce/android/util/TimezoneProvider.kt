90dc7f4416e2743418d4e9434045f484585b8e53
ThomazFB <thomazfcortez@gmail.com>
2023-05-31 23:47:57 -0300

    Make TimezoneProvider injectable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/TimezoneProvider.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/TimezoneProvider.kt
@@ -1,8 +1,9 @@
 package com.woocommerce.android.util
 
 import java.util.TimeZone
+import javax.inject.Inject
 
-class TimezoneProvider {
+class TimezoneProvider @Inject constructor() {
     val deviceTimezone: TimeZone
         get() = TimeZone.getDefault()
 }
