90422ba2101c3425ccf044c7cbfca949ef805011
Alejo <alejandro.torres.veiga@automattic.com>
2023-06-01 17:34:47 -0300

    move logic from WCOrderFetcher -> FetchOrdersRepository

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/FetchOrdersRepository.kt
@@ -0,0 +1,63 @@
+package com.woocommerce.android.ui.orders.list
+
+import com.woocommerce.android.analytics.AnalyticsEvent
+import com.woocommerce.android.analytics.AnalyticsTracker
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.WCOrderActionBuilder
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.store.WCOrderStore
+import org.wordpress.android.util.AppLog
+import java.util.Collections
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FetchOrdersRepository @Inject constructor(private val dispatcher: Dispatcher) {
+    /**
+    * The [RemoteId] of the OrderEntity in the process of being fetched from
+    * the remote API.
+    */
+    private val ongoingRequests = Collections.synchronizedSet(mutableSetOf<Long>())
+
+    init {
+        dispatcher.register(this)
+    }
+
+    /**
+     * Fetch a list of orders matching the provided list of [RemoteId]'s from the
+     * API. Will first remove any [RemoteId]'s already in the process of being
+     * fetched.
+     *
+     * @param [site] The [SiteModel] to fetch the orders from
+     * @param [orderIds] A list containing the ids of the orders to fetch
+     */
+    fun fetchOrders(site: SiteModel, orderIds: List<Long>) {
+        val idsToFetch = orderIds.filter {
+            // ignore duplicate requests
+            !ongoingRequests.contains(it)
+        }
+        if (idsToFetch.isNotEmpty()) {
+            ongoingRequests.addAll(idsToFetch)
+            val payload = WCOrderStore.FetchOrdersByIdsPayload(site = site, orderIds = idsToFetch)
+            dispatcher.dispatch(WCOrderActionBuilder.newFetchOrdersByIdsAction(payload))
+        }
+    }
+
+    @Suppress("unused", "ForbiddenComment")
+    @Subscribe(threadMode = ThreadMode.BACKGROUND)
+    fun onOrdersFetchedById(event: WCOrderStore.OnOrdersFetchedByIds) {
+        if (event.isError) {
+            AppLog.e(AppLog.T.API, "Error fetching orders by remoteOrderId: ${event.error.message}")
+            AnalyticsTracker.track(
+                AnalyticsEvent.ORDER_LIST_LOAD_ERROR,
+                this.javaClass.simpleName,
+                event.error.type.toString(),
+                event.error.message
+            )
+        }
+        ongoingRequests.removeAll(event.orderIds.toSet())
+    }
+}
