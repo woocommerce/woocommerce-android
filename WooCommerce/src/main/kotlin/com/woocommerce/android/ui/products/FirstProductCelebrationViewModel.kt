7fcdb39d30ac637e44f6d721c53734d1ecd78b5a
Hafiz Rahman <hfzrhmn@gmail.com>
2023-06-07 11:48:20 +0700

    Add: Tracking for when dialog is shown and share is tapped.
    This is to match analytics added in iOS:
    
    https://github.com/woocommerce/woocommerce-ios/pull/9790/

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/FirstProductCelebrationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/FirstProductCelebrationViewModel.kt
@@ -1,6 +1,8 @@
 package com.woocommerce.android.ui.products
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.analytics.AnalyticsEvent
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
@@ -9,11 +11,16 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FirstProductCelebrationViewModel @Inject constructor(
+    private val tracker: AnalyticsTrackerWrapper,
     savedStateHandle: SavedStateHandle
 ) : ScopedViewModel(savedStateHandle) {
     private val navArgs: FirstProductCelebrationDialogArgs by savedStateHandle.navArgs()
 
+    init {
+        tracker.track(AnalyticsEvent.FIRST_CREATED_PRODUCT_SHOWN)
+    }
     fun onShareButtonClicked() {
+        tracker.track(AnalyticsEvent.FIRST_CREATED_PRODUCT_SHARE_TAPPED)
         triggerEvent(ProductNavigationTarget.ShareProduct(navArgs.permalink, navArgs.productName))
     }
 
