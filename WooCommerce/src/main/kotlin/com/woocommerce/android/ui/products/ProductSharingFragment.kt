a22734cc65ff07fc221fb2f065ee22cc1cd078ae
Hafiz Rahman <hfzrhmn@gmail.com>
2023-06-13 19:03:27 +0700

    Detekt fix.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductSharingFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductSharingFragment.kt
@@ -10,7 +10,7 @@ import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
-class ProductSharingFragment: BottomSheetDialogFragment() {
+class ProductSharingFragment : BottomSheetDialogFragment() {
 
     private val viewModel: ProductSharingViewModel by viewModels()
 
@@ -20,7 +20,7 @@ class ProductSharingFragment: BottomSheetDialogFragment() {
 
             setContent {
                 WooThemeWithBackground {
-                     ProductSharingBottomSheet(viewModel = viewModel)
+                    ProductSharingBottomSheet(viewModel = viewModel)
                 }
             }
         }
