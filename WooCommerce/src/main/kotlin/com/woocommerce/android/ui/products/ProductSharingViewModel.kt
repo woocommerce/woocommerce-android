f6d69e3341f14b790ef6559fdc8dc1479041a757
Hafiz Rahman <hfzrhmn@gmail.com>
2023-06-15 19:48:49 +0700

    Feat: fetch sharing suggestion from AI and display it.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductSharingViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductSharingViewModel.kt
@@ -3,6 +3,9 @@ package com.woocommerce.android.ui.products
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.R
+import com.woocommerce.android.ai.AIPrompts
+import com.woocommerce.android.ai.AIRepository
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.products.ProductSharingViewModel.ViewState.LoadingState
 import com.woocommerce.android.ui.products.ProductSharingViewModel.ViewState.ProductSharingViewState
 import com.woocommerce.android.viewmodel.ResourceProvider
@@ -11,11 +14,14 @@ import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductSharingViewModel @Inject constructor(
+    private val aiRepository: AIRepository,
     private val resourceProvider: ResourceProvider,
+    private val selectedSite: SelectedSite,
     savedStateHandle: SavedStateHandle
 ) : ScopedViewModel(savedStateHandle) {
     private val navArgs: ProductSharingDialogArgs by savedStateHandle.navArgs()
@@ -43,7 +49,36 @@ class ProductSharingViewModel @Inject constructor(
                 isGenerating = true
             )
         }
-        // todo generate message here.
+
+        launch {
+            val result = aiRepository.fetchJetpackAICompletionsForSite(
+                site = selectedSite.get(),
+                prompt = AIPrompts.generateProductSharingPrompt(
+                    navArgs.productName,
+                    navArgs.permalink,
+                    navArgs.productDescription.orEmpty()
+                )
+            )
+            if (result.isFailure) {
+                // error handling
+            } else {
+                val completions = result.getOrNull()
+                completions?.let {
+                    _viewState.update {
+                        ProductSharingViewState(
+                            productTitle = navArgs.productName,
+                            buttonState = AIButtonState.Regenerate(
+                                resourceProvider.getString(R.string.product_sharing_regenerate)
+                            ),
+                            shareMessage = completions,
+                            isGenerating = false
+                        )
+                    }
+                }
+            }
+        }
+
+
     }
 
     sealed class ViewState {
