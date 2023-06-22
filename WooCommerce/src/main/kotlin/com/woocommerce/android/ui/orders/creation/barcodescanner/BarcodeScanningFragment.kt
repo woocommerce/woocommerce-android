1c4e36e438d962ab71d8b762550d67bbb1c2f45e
Anirudh Uppunda <abhat38@gmail.com>
2023-06-22 16:12:52 +0530

    Inflate compose layout in BarcodeScanningFragment.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/BarcodeScanningFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/BarcodeScanningFragment.kt
@@ -1,4 +1,18 @@
 package com.woocommerce.android.ui.orders.creation.barcodescanner
 
-class BarcodeScanningFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.base.BaseFragment
+
+class BarcodeScanningFragment : BaseFragment(R.layout.fragment_barcode_scanning) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        ComposeView(requireContext())
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
