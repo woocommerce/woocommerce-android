4f8a70d01a02077ba4b6aa6ed016993547834cdc
Anirudh Uppunda <abhat38@gmail.com>
2023-06-23 19:38:06 +0530

    Integrate Refactored barcode scanning screen UI in BarcodeScanningFragment.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/BarcodeScanningFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/BarcodeScanningFragment.kt
@@ -8,29 +8,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.woocommerce.android.R
 import com.woocommerce.android.extensions.navigateBackWithResult
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.orders.creation.CodeScannerStatus
+import com.woocommerce.android.ui.orders.creation.GoogleMLKitCodeScanner
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.wordpress.android.util.ToastUtils
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class BarcodeScanningFragment : BaseFragment(R.layout.fragment_barcode_scanning) {
 
-    private val viewModel: BarcodeScanningViewModel by viewModels()
+    @Inject lateinit var codeScanner: GoogleMLKitCodeScanner
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 WooThemeWithBackground {
-                    BarcodeScannerScreen {
-                        viewModel.startScan(it)
+                    BarcodeScannerScreen(codeScanner) { codeScannerStatus ->
+                        lifecycleScope.launch {
+                            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                                codeScannerStatus.collect { status ->
+                                    navigateBackWithResult("barcode", status)
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -39,28 +49,5 @@ class BarcodeScanningFragment : BaseFragment(R.layout.fragment_barcode_scanning)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupObservers()
-    }
-
-    private fun setupObservers() {
-        viewModel.event.observe(viewLifecycleOwner) { event ->
-            when (event) {
-               is MultiLiveEvent.Event.ExitWithResult<*> -> {
-                   val data = when (event.data) {
-                       is CodeScannerStatus.Success -> {
-                           ToastUtils.showToast(context, event.data.code).show()
-                           event.data.code
-                       }
-                       is CodeScannerStatus.Failure -> {
-                           event.data.error
-                       }
-                       else -> {
-                           null
-                       }
-                   }
-                   navigateBackWithResult("barcode", data)
-               }
-            }
-        }
     }
 }
