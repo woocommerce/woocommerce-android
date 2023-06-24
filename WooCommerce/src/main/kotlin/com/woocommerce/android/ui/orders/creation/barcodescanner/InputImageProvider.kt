eda61eb6eab3ee0fe5c2d0994b41223e786adde9
Anirudh Uppunda <abhat38@gmail.com>
2023-06-24 17:21:43 +0530

    Add a new class InputImageProvider.kt so that GoogleMLKitCodeScanner.kt can be unit tested.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/InputImageProvider.kt
@@ -0,0 +1,13 @@
+package com.woocommerce.android.ui.orders.creation.barcodescanner
+
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import javax.inject.Inject
+
+@ExperimentalGetImage
+class InputImageProvider @Inject constructor() {
+    fun provideImage(imageProxy: ImageProxy) : InputImage {
+        return InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+    }
+}
