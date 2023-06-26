10964b8605749adbf1b9207f1a86a7a7a18e8a66
ThomazFB <thomazfcortez@gmail.com>
2023-06-26 19:44:01 -0300

    Define ShouldFetchNewStatsDataTest

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/hub/sync/ShouldFetchNewStatsDataTest.kt
@@ -0,0 +1,11 @@
+package com.woocommerce.android.ui.analytics.hub.sync
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class ShouldFetchNewStatsDataTest {
+    @Test
+    fun `first basic test`() {
+        assertThat(false).isFalse
+    }
+}
