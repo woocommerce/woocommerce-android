795c9c5dfbaebae9c6b20e85e1ba262823597789
Anirudh Uppunda <abhat38@gmail.com>
2023-06-26 15:57:54 +0530

    Add test to verify proper event is triggered on scan failure

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/OrderListViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/OrderListViewModelTest.kt
@@ -981,48 +981,34 @@ class OrderListViewModelTest : BaseUnitTest() {
         assertThat(viewModel.event.value).isInstanceOf(OrderListEvent.OnBarcodeScanned::class.java)
     }
 
-//    @Test
-//    fun `when code scanner fails, then trigger proper event`() {
-//        whenever(codeScanner.startScan()).thenAnswer {
-//            flow<CodeScannerStatus> {
-//                emit(
-//                    CodeScannerStatus.Failure(
-//                        error = "Failed to recognize the barcode",
-//                        type = CodeScanningErrorType.NotFound
-//                    )
-//                )
-//            }
-//        }
-//
-//        viewModel = createViewModel()
-//        viewModel.onScanClicked()
-//
-//        assertThat(viewModel.event.value).isInstanceOf(
-//            OnAddingProductViaScanningFailed::class.java
-//        )
-//    }
-//
+    @Test
+    fun `when code scanner fails, then trigger proper event`() {
+        val scannedStatus = CodeScannerStatus.Failure(
+            error = "Failed to recognize the barcode",
+            type = CodeScanningErrorType.NotFound
+        )
+        viewModel = createViewModel()
+        viewModel.handleBarcodeScannedStatus(scannedStatus)
+
+        assertThat(viewModel.event.value).isInstanceOf(
+            OnAddingProductViaScanningFailed::class.java
+        )
+    }
+
 //    @Test
 //    fun `when code scanner fails, then trigger event proper message`() {
-//        whenever(codeScanner.startScan()).thenAnswer {
-//            flow<CodeScannerStatus> {
-//                emit(
-//                    CodeScannerStatus.Failure(
-//                        error = "Failed to recognize the barcode",
-//                        type = CodeScanningErrorType.NotFound
-//                    )
-//                )
-//            }
-//        }
-//
+//        val scannedStatus = CodeScannerStatus.Failure(
+//            error = "Failed to recognize the barcode",
+//            type = CodeScanningErrorType.NotFound
+//        )
 //        viewModel = createViewModel()
-//        viewModel.onScanClicked()
+//        viewModel.handleBarcodeScannedStatus(scannedStatus)
 //
 //        assertThat(
 //            (viewModel.event.value as OnAddingProductViaScanningFailed).message
 //        ).isEqualTo(R.string.order_list_barcode_scanning_scanning_failed)
 //    }
-//
+
 //    @Test
 //    fun `given code scanner failure, when retry clicked, then scan restarted`() {
 //        whenever(codeScanner.startScan()).thenAnswer {
