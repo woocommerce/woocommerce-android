1ba54b05a02506af803953d19239059c263c8cb4
ThomazFB <thomazfcortez@gmail.com>
2023-06-26 14:05:50 -0300

    Stub ShouldFetchNewStatsData behavior

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/sync/ShouldFetchNewStatsData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/sync/ShouldFetchNewStatsData.kt
@@ -3,4 +3,7 @@ package com.woocommerce.android.ui.analytics.hub.sync
 import javax.inject.Inject
 
 class ShouldFetchNewStatsData @Inject constructor() {
+    operator fun invoke(): Boolean {
+        return true
+    }
 }
