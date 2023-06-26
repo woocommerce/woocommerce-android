bb24b22347f8dfba395d1c8bd4c5002ae5acb79b
Anirudh Uppunda <abhat38@gmail.com>
2023-06-26 15:42:46 +0530

    Add BarcodeScanningTracker.kt to track everything related to barcode scanning.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/BarcodeScanningTracker.kt
@@ -0,0 +1,32 @@
+package com.woocommerce.android.ui.orders.creation.barcodescanner
+
+import com.woocommerce.android.analytics.AnalyticsEvent
+import com.woocommerce.android.analytics.AnalyticsTracker.Companion.KEY_SCANNING_FAILURE_REASON
+import com.woocommerce.android.analytics.AnalyticsTracker.Companion.KEY_SCANNING_SOURCE
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
+import com.woocommerce.android.ui.orders.creation.CodeScanningErrorType
+import com.woocommerce.android.ui.orders.creation.ScanningSource
+import javax.inject.Inject
+
+class BarcodeScanningTracker @Inject constructor(
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
+) {
+    fun trackScanFailure(source: ScanningSource, type: CodeScanningErrorType) {
+        analyticsTrackerWrapper.track(
+            AnalyticsEvent.BARCODE_SCANNING_FAILURE,
+            mapOf(
+                KEY_SCANNING_SOURCE to source.source,
+                KEY_SCANNING_FAILURE_REASON to type.toString(),
+            )
+        )
+    }
+
+    fun trackSuccess(source: ScanningSource) {
+        analyticsTrackerWrapper.track(
+            AnalyticsEvent.BARCODE_SCANNING_SUCCESS,
+            mapOf(
+                KEY_SCANNING_SOURCE to source.source
+            )
+        )
+    }
+}
