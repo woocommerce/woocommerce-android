9c0cba65f41f4c643bc9fad476403b0efa2db223
ThomazFB <thomazfcortez@gmail.com>
2023-06-27 17:55:42 -0300

    Reorganize ShouldFetchNewStatsData to properly use the new Analytics data store

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/sync/ShouldFetchNewStatsData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/sync/ShouldFetchNewStatsData.kt
@@ -1,10 +1,34 @@
 package com.woocommerce.android.ui.analytics.hub.sync
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
+import com.woocommerce.android.datastore.DataStoreQualifier
+import com.woocommerce.android.datastore.DataStoreType
 import com.woocommerce.android.ui.analytics.ranges.StatsTimeRangeSelection
 import javax.inject.Inject
+import kotlinx.coroutines.flow.map
 
-class ShouldFetchNewStatsData @Inject constructor() {
+class ShouldFetchNewStatsData @Inject constructor(
+    @DataStoreQualifier(DataStoreType.ANALYTICS) private val dataStore: DataStore<Preferences>
+) {
     operator fun invoke(rangeSelection: StatsTimeRangeSelection): Boolean {
+        dataStore.data.map { preferences ->
+            val timestampKey = rangeSelection.selectionType.identifier
+            preferences[longPreferencesKey(timestampKey)]
+        }
         return true
+    }
+
+    suspend fun storeLastAnalyticsUpdate(rangeSelection: StatsTimeRangeSelection) {
+        dataStore.edit { preferences ->
+            val timestampKey = rangeSelection.selectionType.identifier
+            preferences[longPreferencesKey(timestampKey)] = System.currentTimeMillis()
+        }
+    }
+
+    companion object {
+        const val maxOutdatedTime = 1000 * 30 // 30 seconds
     }
 }
