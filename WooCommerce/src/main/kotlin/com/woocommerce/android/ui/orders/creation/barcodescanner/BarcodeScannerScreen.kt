e080a72bed5c9569919f27c549d9c96938da75b1
Anirudh Uppunda <abhat38@gmail.com>
2023-06-28 17:18:44 +0530

    Show camera permission required text when the permission is denied.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/BarcodeScannerScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/BarcodeScannerScreen.kt
@@ -11,19 +11,28 @@ import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.orders.creation.CodeScanner
 import com.woocommerce.android.ui.orders.creation.CodeScannerStatus
 import com.woocommerce.android.util.WooLog
@@ -95,6 +104,21 @@ fun BarcodeScannerScreen(codeScanner: CodeScanner, onScannedResult: (Flow<CodeSc
                 },
                 modifier = Modifier.fillMaxSize()
             )
+        } else {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.barcode_scanning_camera_permission_denied),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.major_150),
+                        end = dimensionResource(id = R.dimen.major_150)
+                    )
+                )
+            }
         }
     }
 }
