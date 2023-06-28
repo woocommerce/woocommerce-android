e30f690fc1e283d871491e6895d6bd2a8b87e7ae
Anirudh Uppunda <abhat38@gmail.com>
2023-06-28 16:57:46 +0530

    Replace media image to bitmap image.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/InputImageProvider.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/barcodescanner/InputImageProvider.kt
@@ -1,13 +1,15 @@
 package com.woocommerce.android.ui.orders.creation.barcodescanner
 
-import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.common.InputImage
 import javax.inject.Inject
 
-@ExperimentalGetImage
-class InputImageProvider @Inject constructor() {
-    fun provideImage(imageProxy: ImageProxy): InputImage {
-        return InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+
+interface InputImageProvider {
+    fun provideImage(imageProxy: ImageProxy): InputImage
+}
+class BitmapImageProvider @Inject constructor() : InputImageProvider {
+    override fun provideImage(imageProxy: ImageProxy): InputImage {
+        return InputImage.fromBitmap(imageProxy.toBitmap(), imageProxy.imageInfo.rotationDegrees)
     }
 }
