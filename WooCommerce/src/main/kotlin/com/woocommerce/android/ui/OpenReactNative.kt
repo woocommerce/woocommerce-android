1c0772edbd8cf367287a9e84ed310e8848a75d6e
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-07-03 18:15:08 +0200

    feat: support application passwords auth

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/OpenReactNative.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/OpenReactNative.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.shared.library.ReactActivity
 import dagger.hilt.android.scopes.ActivityScoped
+import org.wordpress.android.fluxc.network.rest.wpapi.applicationpasswords.ApplicationPasswordsStore
 import javax.inject.Inject
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
 
@@ -13,12 +14,19 @@ class OpenReactNative @Inject constructor(
     private val activity: Activity,
     private val accessToken: AccessToken,
     private val selectedSite: SelectedSite,
+    private val applicationPasswordsStore: ApplicationPasswordsStore,
 ) {
 
     operator fun invoke() {
+        val site = selectedSite.get()
         val intent = Intent(activity, ReactActivity::class.java).apply {
             putExtra(ReactActivity.PROPERTY_TOKEN, accessToken.get())
-            putExtra(ReactActivity.PROPERTY_BLOG_ID, selectedSite.get().siteId.toString())
+            putExtra(ReactActivity.PROPERTY_BLOG_ID, site.siteId.toString())
+            putExtra(ReactActivity.PROPERTY_SITE_URL, "enormous-humboldt.jurassic.ninja")
+            putExtra(
+                ReactActivity.PROPERTY_APP_PASSWORD,
+                applicationPasswordsStore.getApplicationPasswordAuthOption(site)
+            )
         }
         activity.startActivity(intent)
     }
