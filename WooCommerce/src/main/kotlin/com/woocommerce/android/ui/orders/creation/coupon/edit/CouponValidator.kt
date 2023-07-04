d06fdde273ec675a50c5ff67d064139731b89520
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-07-04 13:34:52 -0400

    refactor CouponValidator method to return enum class instead od Boolean

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/coupon/edit/CouponValidator.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/coupon/edit/CouponValidator.kt
@@ -1,15 +1,31 @@
 package com.woocommerce.android.ui.orders.creation.coupon.edit
 
+import android.content.Context
 import com.woocommerce.android.tools.SelectedSite
 import org.wordpress.android.fluxc.store.CouponStore
+import org.wordpress.android.util.NetworkUtils
 import javax.inject.Inject
 
 class CouponValidator @Inject constructor(
     private val selectedSite: SelectedSite,
     private val store: CouponStore,
+    private val context: Context,
 ) {
-    suspend fun isCouponValid(code: String): Boolean {
-        val result = store.searchCoupons(selectedSite.get(), code).model ?: return false
-        return result.coupons.find { code.lowercase() == it.coupon.code?.lowercase() } != null
+    suspend fun isCouponValid(code: String): CouponValidationResult {
+        val result = store.searchCoupons(selectedSite.get(), code).model
+
+        return if (!NetworkUtils.isNetworkAvailable(context)) {
+            CouponValidationResult.NETWORK_ERROR
+        } else if (result?.coupons?.find { code.lowercase() == it.coupon.code?.lowercase() } != null) {
+            CouponValidationResult.VALID
+        } else {
+            CouponValidationResult.INVALID
+        }
+    }
+
+    enum class CouponValidationResult {
+        VALID,
+        INVALID,
+        NETWORK_ERROR,
     }
 }
