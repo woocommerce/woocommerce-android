b253cddd0373ba3de180c667394196d8df3c696e
samiuelson <samiuelson@gmail.com>
2023-07-04 19:26:51 +0200

    Fix detekt issue

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/product/discount/OrderCreateEditProductDiscountFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/product/discount/OrderCreateEditProductDiscountFragment.kt
@@ -1,5 +1,7 @@
 package com.woocommerce.android.ui.orders.creation.product.discount
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.woocommerce.android.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -7,4 +9,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class OrderCreateEditProductDiscountFragment : BaseFragment() {
     private val viewModel: OrderCreateEditProductDiscountViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.event.observe(viewLifecycleOwner) {}
+    }
 }
