5bbbc6a072ff53060c7fe35eff9b5570db855b7d
Anirudh Uppunda <abhat38@gmail.com>
2023-07-05 17:49:18 +0530

    Add UI permission states in BarcodeScanningViewModel.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModel.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.barcodescanner
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.ScopedViewModel
@@ -12,5 +13,25 @@ class BarcodeScanningViewModel @Inject constructor(
     sealed class ScanningEvents : Event() {
         object LaunchCameraPermission : ScanningEvents()
         object OpenAppSettings : ScanningEvents()
+    }
+
+    sealed class PermissionState {
+        object Granted : PermissionState()
+
+        data class ShouldShowRationale(
+            @StringRes val title: Int,
+            @StringRes val message: Int,
+            @StringRes val ctaLabel: Int,
+            val ctaAction: () -> Unit
+        ) : PermissionState()
+
+        data class PermanentlyDenied(
+            @StringRes val title: Int,
+            @StringRes val message: Int,
+            @StringRes val ctaLabel: Int,
+            val ctaAction: () -> Unit
+        ) : PermissionState()
+
+        object Unknown : PermissionState()
     }
 }
