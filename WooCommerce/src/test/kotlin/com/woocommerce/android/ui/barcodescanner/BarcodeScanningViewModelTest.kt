0bf05ae94a8c498c1155fefe01f1ec549cad77aa
Anirudh Uppunda <abhat38@gmail.com>
2023-07-05 18:13:32 +0530

    Add test to verify that the camera permission is asked again when the user navigates back from the app settings.

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.woocommerce.android.ui.barcodescanner
+
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.ui.barcodescanner.BarcodeScanningViewModel.PermissionState.PermanentlyDenied
+import com.woocommerce.android.ui.barcodescanner.BarcodeScanningViewModel.ScanningEvents.LaunchCameraPermission
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+@ExperimentalCoroutinesApi
+class BarcodeScanningViewModelTest : BaseUnitTest() {
+    private lateinit var barcodeScanningViewModel: BarcodeScanningViewModel
+    private lateinit var savedStateHandle: SavedStateHandle
+
+    @Before
+    fun setup() {
+        savedStateHandle = mock()
+        barcodeScanningViewModel = BarcodeScanningViewModel(savedStateHandle)
+    }
+
+    @Test
+    fun `when user navigates back from the app settings, then launch the camera permission again`() {
+        barcodeScanningViewModel.updatePermissionState(
+            isPermissionGranted = false,
+            shouldShowRequestPermissionRationale = false
+        )
+        (barcodeScanningViewModel.permissionState.value as PermanentlyDenied).ctaAction.invoke()
+
+        barcodeScanningViewModel.onResume()
+
+        assertThat(barcodeScanningViewModel.event.value).isInstanceOf(LaunchCameraPermission::class.java)
+    }
+}
