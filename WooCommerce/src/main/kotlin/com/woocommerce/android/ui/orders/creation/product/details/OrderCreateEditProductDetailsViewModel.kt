59fd8d0fc4f70fb2b38726fe39d2aad731b5d7a7
samiuelson <samiuelson@gmail.com>
2023-07-05 09:23:44 +0200

    Fix lint

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/product/details/OrderCreateEditProductDetailsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/product/details/OrderCreateEditProductDetailsViewModel.kt
@@ -133,7 +133,9 @@ class OrderCreateEditProductDetailsViewModel @Inject constructor(
 
     @Parcelize
     sealed class ProductDetailsEditResult : Parcelable {
+        @Parcelize
         data class ProductDetailsEdited(val changes: Order.Item) : Parcelable, ProductDetailsEditResult()
+        @Parcelize
         data class ProductRemoved(val item: Order.Item) : Parcelable, ProductDetailsEditResult()
     }
 
