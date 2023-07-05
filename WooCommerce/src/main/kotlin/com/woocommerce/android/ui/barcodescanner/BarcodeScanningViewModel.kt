975f7a834578d1de4f3166fd37a26e3a4c169ceb
Anirudh Uppunda <abhat38@gmail.com>
2023-07-05 17:48:42 +0530

    Add UI events in BarcodeScanningViewModel.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModel.kt
@@ -1,10 +1,16 @@
 package com.woocommerce.android.ui.barcodescanner
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
 
 class BarcodeScanningViewModel @Inject constructor(
     savedState: SavedStateHandle,
 ) : ScopedViewModel(savedState) {
+
+    sealed class ScanningEvents : Event() {
+        object LaunchCameraPermission : ScanningEvents()
+        object OpenAppSettings : ScanningEvents()
+    }
 }
