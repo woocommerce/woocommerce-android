bdcf62ff4cbb0bb2a1632dd703ccbff1ae9da54d
Anirudh Uppunda <abhat38@gmail.com>
2023-07-05 17:51:28 +0530

    Add logic to handle granted, denied, permanently denied states in BarcodeScanningViewModel.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModel.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.R
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
@@ -13,6 +14,46 @@ class BarcodeScanningViewModel @Inject constructor(
 ) : ScopedViewModel(savedState) {
     private val _permissionState = MutableLiveData<PermissionState>()
     val permissionState: LiveData<PermissionState> = _permissionState
+
+    fun updatePermissionState(
+        isPermissionGranted: Boolean,
+        shouldShowRequestPermissionRationale: Boolean,
+    ) {
+        when {
+            isPermissionGranted -> {
+                // display scanning screen
+                _permissionState.value = PermissionState.Granted
+            }
+
+            !isPermissionGranted && shouldShowRequestPermissionRationale -> {
+                // Denied once, ask for camera permission again
+                _permissionState.value = PermissionState.ShouldShowRationale(
+                    title = R.string.barcode_scanning_alert_dialog_title,
+                    message = R.string.barcode_scanning_alert_dialog_rationale_message,
+                    ctaLabel = R.string.barcode_scanning_alert_dialog_rationale_cta_label,
+                    ctaAction = { triggerEvent(ScanningEvents.LaunchCameraPermission) }
+                )
+            }
+
+            !isPermissionGranted && !shouldShowRequestPermissionRationale -> {
+                // Permanently denied, ask to enable permission from the app settings
+                _permissionState.value = PermissionState.PermanentlyDenied(
+                    title = R.string.barcode_scanning_alert_dialog_title,
+                    message = R.string.barcode_scanning_alert_dialog_permanently_denied_message,
+                    ctaLabel = R.string.barcode_scanning_alert_dialog_permanently_denied_cta_label,
+                    ctaAction = {
+                        // Open settings
+                        triggerEvent(ScanningEvents.OpenAppSettings)
+                    }
+                )
+            }
+
+            else -> {
+                _permissionState.value = PermissionState.Unknown
+            }
+        }
+    }
+
 
     sealed class ScanningEvents : Event() {
         object LaunchCameraPermission : ScanningEvents()
