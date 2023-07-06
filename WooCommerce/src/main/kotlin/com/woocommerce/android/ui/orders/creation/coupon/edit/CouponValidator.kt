f6eca0442af41f6f0aa8bc4305682982c3771c46
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-07-06 09:23:50 -0400

    Refactor CouponValidator error logic to WooResult

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/coupon/edit/CouponValidator.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/coupon/edit/CouponValidator.kt
@@ -1,23 +1,25 @@
 package com.woocommerce.android.ui.orders.creation.coupon.edit
 
-import android.content.Context
 import com.woocommerce.android.tools.SelectedSite
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
+import org.wordpress.android.fluxc.persistence.entity.CouponWithEmails
 import org.wordpress.android.fluxc.store.CouponStore
-import org.wordpress.android.util.NetworkUtils
 import javax.inject.Inject
 
 class CouponValidator @Inject constructor(
     private val selectedSite: SelectedSite,
     private val store: CouponStore,
-    private val context: Context,
 ) {
     suspend fun isCouponValid(code: String): CouponValidationResult {
-        val networkAvailable = NetworkUtils.isNetworkAvailable(context)
-        val coupons = store.searchCoupons(selectedSite.get(), code).model?.coupons
-        val validCoupon = coupons?.find { it.coupon.code?.equals(code, ignoreCase = true) == true }
+        val couponSearchResult: WooResult<CouponStore.CouponSearchResult> =
+            store.searchCoupons(selectedSite.get(), code)
+        if (couponSearchResult.isError) {
+            return CouponValidationResult.NETWORK_ERROR
+        }
+        val couponList: List<CouponWithEmails>? = couponSearchResult.model?.coupons
+        val validCoupon = couponList?.find { it.coupon.code?.equals(code, ignoreCase = true) == true }
 
         return when {
-            !networkAvailable -> CouponValidationResult.NETWORK_ERROR
             validCoupon != null -> CouponValidationResult.VALID
             else -> CouponValidationResult.INVALID
         }
