b3160121837225a526cc18fc4dcf1f5bca7c6887
samiuelson <samiuelson@gmail.com>
2023-07-06 21:46:48 +0200

    Add discount amount validation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/product/discount/OrderCreateEditProductDiscountViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/product/discount/OrderCreateEditProductDiscountViewModel.kt
@@ -1,8 +1,10 @@
 package com.woocommerce.android.ui.orders.creation.product.discount
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.R
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.viewmodel.MultiLiveEvent
+import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,13 +19,14 @@ import javax.inject.Inject
 @HiltViewModel
 class OrderCreateEditProductDiscountViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val resourceProvider: ResourceProvider,
 ) : ScopedViewModel(savedStateHandle) {
-    private val args = OrderCreateEditProductDiscountFragmentArgs.fromSavedStateHandle(savedStateHandle)
-    private val orderItem = savedStateHandle.getStateFlow(scope = this, initialValue = args.item, key = "key_item")
+    private val args =
+        OrderCreateEditProductDiscountFragmentArgs.fromSavedStateHandle(savedStateHandle)
+    private val orderItem =
+        savedStateHandle.getStateFlow(scope = this, initialValue = args.item, key = "key_item")
     private val discount = savedStateHandle.getStateFlow(
-        scope = this,
-        initialValue = getInitialDiscountString(),
-        key = "key_discount"
+        scope = this, initialValue = getInitialDiscountString(), key = "key_discount"
     )
 
     private val currency = Currency.getInstance(args.currency).symbol
@@ -36,9 +39,28 @@ class OrderCreateEditProductDiscountViewModel @Inject constructor(
     val viewState: StateFlow<ViewState> = discount.map {
         ViewState(
             currency = currency,
-            discountAmount = it
+            discountAmount = it,
+            discountValidationState = checkDiscountValidationState(it)
         )
     }.toStateFlow(ViewState("", ""))
+
+    private fun checkDiscountValidationState(discount: String): DiscountAmountValidationState {
+        if (discount.isEmpty()) return DiscountAmountValidationState.Valid
+
+        val discountAmount: BigDecimal = try {
+            discount.toBigDecimal()
+        } catch (e: NumberFormatException) {
+            return DiscountAmountValidationState.Invalid(
+                resourceProvider.getString(R.string.order_creation_discount_invalid_number_error)
+            )
+        }
+        if (discountAmount > orderItem.value.pricePreDiscount) {
+            return DiscountAmountValidationState.Invalid(
+                resourceProvider.getString(R.string.order_creation_discount_too_big_error)
+            )
+        }
+        return DiscountAmountValidationState.Valid
+    }
 
     fun onNavigateBack() {
         triggerEvent(MultiLiveEvent.Event.Exit)
@@ -71,7 +93,14 @@ class OrderCreateEditProductDiscountViewModel @Inject constructor(
     data class ViewState(
         val currency: String,
         val discountAmount: String,
+        val discountValidationState: DiscountAmountValidationState = DiscountAmountValidationState.Valid,
+        val isDoneButtonEnabled: Boolean = discountValidationState is DiscountAmountValidationState.Valid,
     )
+
+    sealed class DiscountAmountValidationState {
+        object Valid : DiscountAmountValidationState()
+        data class Invalid(val errorMessage: String) : DiscountAmountValidationState()
+    }
 
     data class ReturnDiscountResult(val item: Order.Item) : MultiLiveEvent.Event()
 }
