59014aed244db899fbaeae1b751f6c9ef20918fb
Anirudh Uppunda <abhat38@gmail.com>
2023-07-06 09:52:52 +0530

    Add test to verify that when the camera permission is not granted and should not show rationale, then message is correct.

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanningViewModelTest.kt
@@ -150,4 +150,16 @@ class BarcodeScanningViewModelTest : BaseUnitTest() {
             (barcodeScanningViewModel.permissionState.value as PermanentlyDenied).title
         ).isEqualTo(R.string.barcode_scanning_alert_dialog_title)
     }
+
+    @Test
+    fun `given camera permission not granted and should not show rationale, then dialog message is correct`() {
+        barcodeScanningViewModel.updatePermissionState(
+            isPermissionGranted = false,
+            shouldShowRequestPermissionRationale = false
+        )
+
+        assertThat(
+            (barcodeScanningViewModel.permissionState.value as PermanentlyDenied).message
+        ).isEqualTo(R.string.barcode_scanning_alert_dialog_permanently_denied_message)
+    }
 }
