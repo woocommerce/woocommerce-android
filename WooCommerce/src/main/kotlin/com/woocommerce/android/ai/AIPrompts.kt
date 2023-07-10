dcde91141e83688cea28deccf041fd5ff983e229
Hafiz Rahman <hfzrhmn@gmail.com>
2023-07-10 21:13:17 +0700

    Add: language ISO code for product description prompt.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ai/AIPrompts.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ai/AIPrompts.kt
@@ -2,15 +2,19 @@ package com.woocommerce.android.ai
 
 object AIPrompts {
     private const val PRODUCT_DESCRIPTION_PROMPT = "Write a description for a product with title \"%1\$s\"%2\$s.\n" +
-        "Identify the language used in the product title and use the same language in your response.\n" +
+        "Your response should be in the ISO language code \"%3\$s\". \n" +
         "Make the description 50-60 words or less.\n" +
         "Use a 9th grade reading level.\n" +
         "Perform in-depth keyword research relating to the product in the same language of the product title, " +
         "and use them in your sentences without listing them out."
 
-    fun generateProductDescriptionPrompt(name: String, features: String = ""): String {
+    fun generateProductDescriptionPrompt(
+        name: String,
+        features: String = "",
+        languageISOCode: String = "en"
+    ): String {
         val featuresPart = if (features.isNotEmpty()) " and features: \"$features\"" else ""
-        return String.format(PRODUCT_DESCRIPTION_PROMPT, name, featuresPart)
+        return String.format(PRODUCT_DESCRIPTION_PROMPT, name, featuresPart, languageISOCode)
     }
 
     private const val PRODUCT_SHARING_PROMPT = "Your task is to help a merchant create a message to share with " +
