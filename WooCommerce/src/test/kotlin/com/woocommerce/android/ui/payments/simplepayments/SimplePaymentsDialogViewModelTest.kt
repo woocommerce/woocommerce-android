4007dd4e913e328b75eace08a750ed5bd19b3a43
samiuelson <samiuelson@gmail.com>
2023-07-17 18:16:16 +0200

    Add unit test

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/simplepayments/SimplePaymentsDialogViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/simplepayments/SimplePaymentsDialogViewModelTest.kt
@@ -4,13 +4,19 @@ import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.analytics.AnalyticsEvent
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
+import com.woocommerce.android.model.Order
 import com.woocommerce.android.tools.NetworkStatus
 import com.woocommerce.android.ui.orders.creation.OrderCreateEditRepository
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
-class SimplePaymentsDialogViewModelTest {
+@OptIn(ExperimentalCoroutinesApi::class)
+class SimplePaymentsDialogViewModelTest: BaseUnitTest() {
     private val networkStatus: NetworkStatus = mock()
     private val orderCreateEditRepository: OrderCreateEditRepository = mock()
     private val analyticsTracker: AnalyticsTrackerWrapper = mock()
@@ -32,5 +38,22 @@ class SimplePaymentsDialogViewModelTest {
             AnalyticsEvent.PAYMENTS_FLOW_CANCELED,
             mapOf(AnalyticsTracker.KEY_FLOW to AnalyticsTracker.VALUE_SIMPLE_PAYMENTS_FLOW)
         )
+    }
+
+    @Test
+    fun `given network available, when done clicked, then should create order`() = testBlocking {
+        // given
+        viewModel.viewState = viewModel.viewState.copy(currentPrice = 23.toBigDecimal())
+        whenever(networkStatus.isConnected()).thenReturn(true)
+        whenever(orderCreateEditRepository.createSimplePaymentOrder(viewModel.viewState.currentPrice))
+            .thenReturn(Result.success(Order.EMPTY.copy(total = 23.toBigDecimal())))
+
+        // when
+        viewModel.onDoneButtonClicked()
+
+        val latestViewState = viewModel.viewStateLiveData.liveData.value
+
+        // then
+        assertThat(latestViewState).isNotNull()
     }
 }
