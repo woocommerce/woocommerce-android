666525ea4027c314dd94b546cb8db1215cab50fb
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-07-25 10:57:56 +0200

    Update survey value options

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/freetrial/FreeTrialSurveyViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/feedback/freetrial/FreeTrialSurveyViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.asLiveData
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.feedback.freetrial.FreeTrialSurveyViewModel.SurveyOption.COLLECTIVE_DECISION
 import com.woocommerce.android.ui.feedback.freetrial.FreeTrialSurveyViewModel.SurveyOption.COMPARING_WITH_OTHER_PLATFORMS
-import com.woocommerce.android.ui.feedback.freetrial.FreeTrialSurveyViewModel.SurveyOption.EVALUATING_HOW_TO_INTEGRATE_SERVICE
 import com.woocommerce.android.ui.feedback.freetrial.FreeTrialSurveyViewModel.SurveyOption.OTHER_REASONS
+import com.woocommerce.android.ui.feedback.freetrial.FreeTrialSurveyViewModel.SurveyOption.PRICE_IS_SIGNIFICANT_FACTOR
 import com.woocommerce.android.ui.feedback.freetrial.FreeTrialSurveyViewModel.SurveyOption.STILL_EXPLORING
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ScopedViewModel
@@ -36,12 +36,12 @@ class FreeTrialSurveyViewModel @Inject constructor(
             SurveyOptionUi(
                 isSelected = false,
                 textId = R.string.free_trial_survey_option3,
-                optionType = COLLECTIVE_DECISION
+                optionType = PRICE_IS_SIGNIFICANT_FACTOR
             ),
             SurveyOptionUi(
                 isSelected = false,
                 textId = R.string.free_trial_survey_option4,
-                optionType = EVALUATING_HOW_TO_INTEGRATE_SERVICE
+                optionType = COLLECTIVE_DECISION
             ),
             SurveyOptionUi(
                 isSelected = false,
@@ -89,8 +89,8 @@ class FreeTrialSurveyViewModel @Inject constructor(
     enum class SurveyOption {
         STILL_EXPLORING,
         COMPARING_WITH_OTHER_PLATFORMS,
+        PRICE_IS_SIGNIFICANT_FACTOR,
         COLLECTIVE_DECISION,
-        EVALUATING_HOW_TO_INTEGRATE_SERVICE,
         OTHER_REASONS,
     }
 }
