0b5c034c31f0edf1ad4a19b78288c00c9a71eaee
ThomazFB <thomazfcortez@gmail.com>
2023-07-26 18:16:49 -0300

    Define ConvertGranularityToStatsTimeRange use case

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/domain/ConvertGranularityToStatsTimeRange.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.ui.mystore.domain
+
+import org.wordpress.android.fluxc.store.WCStatsStore
+
+class ConvertGranularityToStatsTimeRange {
+
+    operator fun invoke(granularity: WCStatsStore.StatsGranularity) {
+
+    }
+}
