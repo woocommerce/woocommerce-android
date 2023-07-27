0cfa97561e901e5c622635d5dec7e473c1ef93dd
ThomazFB <thomazfcortez@gmail.com>
2023-07-27 05:16:50 -0300

    Update GetTopPerformers to directly use StatsTimeRangeSelection

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/domain/GetTopPerformers.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/domain/GetTopPerformers.kt
@@ -4,27 +4,24 @@ import com.woocommerce.android.ui.analytics.hub.sync.AnalyticsUpdateDataStore
 import com.woocommerce.android.ui.analytics.ranges.StatsTimeRangeSelection
 import com.woocommerce.android.ui.mystore.data.StatsRepository
 import com.woocommerce.android.util.CoroutineDispatchers
-import com.woocommerce.android.util.locale.LocaleProvider
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import org.wordpress.android.fluxc.persistence.entity.TopPerformerProductEntity
-import org.wordpress.android.fluxc.store.WCStatsStore
 import javax.inject.Inject
 
 class GetTopPerformers @Inject constructor(
     private val statsRepository: StatsRepository,
     private val coroutineDispatchers: CoroutineDispatchers,
-    private val analyticsUpdateDataStore: AnalyticsUpdateDataStore,
-    private val localeProvider: LocaleProvider
+    private val analyticsUpdateDataStore: AnalyticsUpdateDataStore
 ) {
     private companion object {
         const val NUM_TOP_PERFORMERS = 5
     }
 
-    fun observeTopPerformers(granularity: WCStatsStore.StatsGranularity): Flow<List<TopPerformerProduct>> =
-        statsRepository.observeTopPerformers(granularity)
+    fun observeTopPerformers(selectionRange: StatsTimeRangeSelection): Flow<List<TopPerformerProduct>> =
+        statsRepository.observeTopPerformers(selectionRange.selectionType.toStatsGranularity())
             .map { topPerformersProductEntities ->
                 topPerformersProductEntities
                     .map { it.toTopPerformerProduct() }
@@ -32,22 +29,24 @@ class GetTopPerformers @Inject constructor(
             }.flowOn(coroutineDispatchers.computation)
 
     suspend fun fetchTopPerformers(
-        granularity: WCStatsStore.StatsGranularity,
+        selectionRange: StatsTimeRangeSelection,
         refresh: Boolean = false,
         topPerformersCount: Int = NUM_TOP_PERFORMERS,
     ): Result<Unit> {
-        val selectionRange = granularity.asRangeSelection(localeProvider.provideLocale())
         val isForcedRefresh = shouldUpdateStats(selectionRange, refresh)
-        return statsRepository.fetchTopPerformerProducts(isForcedRefresh, granularity, topPerformersCount)
-            .let { result ->
-                if (result.isSuccess && isForcedRefresh) {
-                    analyticsUpdateDataStore.storeLastAnalyticsUpdate(
-                        rangeSelection = selectionRange,
-                        analyticData = AnalyticsUpdateDataStore.AnalyticData.TOP_PERFORMERS
-                    )
-                }
-                result
+        return statsRepository.fetchTopPerformerProducts(
+            isForcedRefresh,
+            selectionRange.selectionType.toStatsGranularity(),
+            topPerformersCount
+        ).let { result ->
+            if (result.isSuccess && isForcedRefresh) {
+                analyticsUpdateDataStore.storeLastAnalyticsUpdate(
+                    rangeSelection = selectionRange,
+                    analyticData = AnalyticsUpdateDataStore.AnalyticData.TOP_PERFORMERS
+                )
             }
+            result
+        }
     }
 
     private suspend fun shouldUpdateStats(
