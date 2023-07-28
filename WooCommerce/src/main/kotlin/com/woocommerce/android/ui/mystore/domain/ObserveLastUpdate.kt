052ccd910a7dc574b3c73527f3ca0345012e013f
ThomazFB <thomazfcortez@gmail.com>
2023-07-28 09:04:51 -0300

    Introduce direct timeRangeSelection overload for ObserveLastUpdate

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/domain/ObserveLastUpdate.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/domain/ObserveLastUpdate.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.mystore.domain
 
 import com.woocommerce.android.ui.analytics.hub.sync.AnalyticsUpdateDataStore
+import com.woocommerce.android.ui.analytics.ranges.StatsTimeRangeSelection
 import com.woocommerce.android.util.locale.LocaleProvider
 import kotlinx.coroutines.flow.Flow
 import org.wordpress.android.fluxc.store.WCStatsStore
@@ -29,6 +30,15 @@ class ObserveLastUpdate @Inject constructor(
         return analyticsUpdateDataStore.observeLastUpdate(
             rangeSelection = rangeSelection,
             analyticData = analyticData
+        )
+    }
+
+    operator fun invoke(
+        timeRangeSelection: StatsTimeRangeSelection
+    ) : Flow<Long?> {
+        return analyticsUpdateDataStore.observeLastUpdate(
+            rangeSelection = timeRangeSelection,
+            analyticData = AnalyticsUpdateDataStore.AnalyticData.ALL
         )
     }
 }
