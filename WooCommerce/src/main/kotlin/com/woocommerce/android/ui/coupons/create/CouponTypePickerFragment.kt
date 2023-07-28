3f363eaea762b887777009eafdfe66e41e5fa232
samiuelson <samiuelson@gmail.com>
2023-07-28 09:14:51 +0200

    Clean up code

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/create/CouponTypePickerFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/create/CouponTypePickerFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
@@ -26,13 +25,11 @@ class CouponTypePickerFragment : WCBottomSheetDialogFragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 WooThemeWithBackground {
-                    Column {
-                        CouponTypePickerScreen(
-                            viewModel::onPercentageDiscountClicked,
-                            viewModel::onFixedCartDiscountClicked,
-                            viewModel::onFixedProductDiscountClicked
-                        )
-                    }
+                    CouponTypePickerScreen(
+                        viewModel::onPercentageDiscountClicked,
+                        viewModel::onFixedCartDiscountClicked,
+                        viewModel::onFixedProductDiscountClicked
+                    )
                 }
             }
         }
