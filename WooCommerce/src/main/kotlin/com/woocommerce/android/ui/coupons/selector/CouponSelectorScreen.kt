f4daab903c47246c3ead1f4cbd5f658b42620d62
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-08-01 11:22:35 -0400

    CouponSelectorScreen add empty screen and skeleton composables

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/selector/CouponSelectorScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/selector/CouponSelectorScreen.kt
@@ -1,47 +1,171 @@
 package com.woocommerce.android.ui.coupons.selector
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.SwipeRefreshIndicator
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.animations.SkeletonView
+import com.woocommerce.android.ui.compose.component.InfiniteListHandler
+import com.woocommerce.android.ui.compose.component.WCColoredButton
 import com.woocommerce.android.ui.coupons.components.CouponExpirationLabel
 
 @Composable
 fun CouponSelectorScreen(viewModel: CouponSelectorViewModel) {
+    val couponListState by viewModel.couponSelectorState.observeAsState(CouponSelectorState())
 
+    CouponSelectorScreen(
+        state = couponListState,
+        onCouponClicked = viewModel::onCouponClicked,
+        onRefresh = viewModel::onRefresh,
+         onLoadMore = viewModel::onLoadMore,
+    )
+
+}
+
+@Composable
+fun CouponSelectorScreen(
+    state: CouponSelectorState,
+    onCouponClicked: (Long) -> Unit,
+    onRefresh: () -> Unit,
+    onLoadMore: () -> Unit,
+) {
+    when {
+        state.coupons.isNotEmpty() -> CouponSelectorList(
+            coupons = state.coupons,
+            loadingState = state.loadingState,
+            onCouponClicked = onCouponClicked,
+            onRefresh = onRefresh,
+            onLoadMore = onLoadMore,
+        )
+        state.loadingState == LoadingState.Loading -> CouponSelectorListSkeleton()
+        state.isSearchOpen -> CouponSelectorEmptySearch(searchQuery = state.searchQuery.orEmpty())
+        else -> EmptyCouponSelectorList()
+    }
+}
+
+@Composable
+fun EmptyCouponSelectorList() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = dimensionResource(id = R.dimen.major_200)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.order_creation_coupons_empty_list_title),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier.padding(
+                start = dimensionResource(id = R.dimen.major_150),
+                end = dimensionResource(id = R.dimen.major_150)
+            )
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.major_325)))
+        Image(
+            painter = painterResource(id = R.drawable.img_empty_coupon_list),
+            contentDescription = null,
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.major_200)))
+        Text(
+            text = stringResource(id = R.string.order_creation_coupons_empty_list_message),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.caption,
+            modifier = Modifier.padding(
+                start = dimensionResource(id = R.dimen.major_150),
+                end = dimensionResource(id = R.dimen.major_150)
+            )
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.major_325)))
+        WCColoredButton(
+            onClick = {},
+            text = stringResource(id = R.string.order_creation_coupons_empty_list_button),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.major_100)),
+        )
+    }
 }
 
 
 @Composable
 fun CouponSelectorList(
     coupons: List<CouponSelectorItem>,
-    onCouponSelected: (Long) -> Unit
+    loadingState: LoadingState,
+    onCouponClicked: (Long) -> Unit,
+    onRefresh: () -> Unit,
+    onLoadMore: () -> Unit,
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_50)),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(id = R.dimen.major_100))
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(isRefreshing = loadingState == LoadingState.Refreshing),
+        onRefresh = onRefresh,
+        indicator = { state, refreshTrigger ->
+            SwipeRefreshIndicator(
+                state = state,
+                refreshTriggerDistance = refreshTrigger,
+                contentColor = MaterialTheme.colors.primary,
+            )
+        }
     ) {
-        coupons.forEach { coupon ->
-            CouponSelectorListItem(coupon, onCouponSelected)
+        val listState = rememberLazyListState()
+        LazyColumn(
+            state = listState,
+            modifier = Modifier
+                .background(color = MaterialTheme.colors.surface)
+        ) {
+            itemsIndexed(coupons) {_, coupons ->
+                CouponSelectorListItem(coupons, onCouponClicked)
+                Divider(
+                    modifier = Modifier.offset(x = dimensionResource(id = R.dimen.major_100)),
+                    color = colorResource(id = R.color.divider_color),
+                    thickness = dimensionResource(id = R.dimen.minor_10)
+                )
+            }
+            if(loadingState == LoadingState.Appending) {
+                item {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth()
+                            .padding(vertical = dimensionResource(id = R.dimen.minor_100))
+                    )
+                }
+            }
+        }
+
+        InfiniteListHandler(listState = listState) {
+            onLoadMore()
         }
     }
 }
@@ -83,7 +207,76 @@ fun CouponSelectorListItemInfo(summary: String) {
     )
 }
 
-@Preview
+@Composable
+fun CouponSelectorListSkeleton() {
+    val numberOfInboxSkeletonRows = 10
+    LazyColumn(Modifier.background(color = MaterialTheme.colors.surface)) {
+        repeat(numberOfInboxSkeletonRows) {
+            item {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_50)),
+                    modifier = Modifier.padding(
+                        horizontal = dimensionResource(id = R.dimen.major_100),
+                        vertical = dimensionResource(id = R.dimen.minor_100)
+                    )
+                ) {
+                    SkeletonView(
+                        dimensionResource(id = R.dimen.skeleton_text_medium_width),
+                        dimensionResource(id = R.dimen.major_125)
+                    )
+                    SkeletonView(
+                        dimensionResource(id = R.dimen.skeleton_text_large_width),
+                        dimensionResource(id = R.dimen.major_100)
+                    )
+                    SkeletonView(
+                        dimensionResource(id = R.dimen.skeleton_text_small_width),
+                        dimensionResource(id = R.dimen.major_125)
+                    )
+                }
+                Divider(
+                    modifier = Modifier
+                        .offset(x = dimensionResource(id = R.dimen.major_100)),
+                    color = colorResource(id = R.color.divider_color),
+                    thickness = dimensionResource(id = R.dimen.minor_10)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CouponSelectorEmptySearch(searchQuery: String) {
+    if(searchQuery.isEmpty()) return
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = dimensionResource(id = R.dimen.major_200)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.empty_message_with_search, searchQuery),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier.padding(
+                start = dimensionResource(id = R.dimen.major_150),
+                end = dimensionResource(id = R.dimen.major_150)
+            )
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.major_325)))
+        Image(
+            painter = painterResource(id = R.drawable.img_empty_search),
+            contentDescription = null,
+        )
+    }
+}
+
+
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF
+)
 @Composable
 fun CouponSelectorListItemPreview() {
     CouponSelectorListItem(
@@ -97,7 +290,10 @@ fun CouponSelectorListItemPreview() {
     )
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF
+)
 @Composable
 fun CouponSelectorListPreview() {
     val coupons = listOf(
@@ -120,5 +316,25 @@ fun CouponSelectorListPreview() {
             isActive = true
         ),
     )
-    CouponSelectorList(coupons = coupons, onCouponSelected = {})
+    CouponSelectorList(
+        coupons = coupons,
+        onCouponClicked = {},
+        loadingState = LoadingState.Idle,
+        onRefresh = {},
+        onLoadMore = {}
+    )
+}
+
+@Preview()
+@Composable
+fun CouponSelectorListSkeletonPreview() {
+    CouponSelectorListSkeleton()
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF)
+@Composable
+fun CouponSelectorEmptyListPreview() {
+    EmptyCouponSelectorList()
 }
