ea44d2be47e11052559b646e4c39e1aeb6f5bdef
Hafiz Rahman <hfzrhmn@gmail.com>
2023-08-02 19:18:59 +0700

    UI: Make the "Try a test order" dialog scrollable, especially useful in landscape mode.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/CreateTestOrderDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/list/CreateTestOrderDialog.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -43,8 +45,10 @@ fun CreateTestOrderDialog(
     ) {
 
         Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
                 modifier = Modifier.padding(top = dimensionResource(id = R.dimen.major_100)),
