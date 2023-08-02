a8d870b5b376c55721d3d4646a7540f5baff7cf5
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-08-02 16:46:50 -0400

    optimize imports

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/selector/CouponSelectorScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/selector/CouponSelectorScreen.kt
@@ -18,15 +18,10 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
