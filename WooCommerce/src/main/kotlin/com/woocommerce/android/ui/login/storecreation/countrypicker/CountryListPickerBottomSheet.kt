50cd2f895e9ec6df613ca72011805b0278e74a1f
Hafiz Rahman <hfzrhmn@gmail.com>
2023-08-15 18:24:59 +0700

    Add: build list of countries in CountryListPickerViewModel.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/countrypicker/CountryListPickerBottomSheet.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/countrypicker/CountryListPickerBottomSheet.kt
@@ -2,8 +2,11 @@ package com.woocommerce.android.ui.login.storecreation.countrypicker
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 
 @Composable
 fun CountryPickerSelectorBottomSheet(viewModel: CountryListPickerViewModel) {
-    Text(text = viewModel.toString())
+    viewModel.countryListPickerState.observeAsState().value?.let { viewState ->
+        Text(text = viewState.toString())
+    }
 }
