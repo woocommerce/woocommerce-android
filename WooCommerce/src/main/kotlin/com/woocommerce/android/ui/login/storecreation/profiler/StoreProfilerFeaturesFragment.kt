651a42b1809cdfdc369bb8af04593628068b02ea
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-08-23 17:16:07 +0200

    Reorder store creation steps

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/profiler/StoreProfilerFeaturesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/profiler/StoreProfilerFeaturesFragment.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.extensions.navigateSafely
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.main.AppBarStatus
@@ -41,10 +42,15 @@ class StoreProfilerFeaturesFragment : BaseFragment() {
         viewModel.event.observe(viewLifecycleOwner) { event ->
             when (event) {
                 is MultiLiveEvent.Event.Exit -> findNavController().popBackStack()
-                is BaseStoreProfilerViewModel.NavigateToNextStep -> {
-                    TODO()
-                }
+                is BaseStoreProfilerViewModel.NavigateToNextStep -> navigateToStoreInstallationStep()
             }
         }
+    }
+
+    private fun navigateToStoreInstallationStep() {
+        findNavController().navigateSafely(
+            StoreProfilerFeaturesFragmentDirections
+                .actionStoreProfilerFeaturesFragmentToStoreCreationInstallationFragment()
+        )
     }
 }
