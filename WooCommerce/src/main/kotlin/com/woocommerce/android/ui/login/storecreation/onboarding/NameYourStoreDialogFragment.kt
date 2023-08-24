429df12e150ffae4bc10a78612b3a272e69b1064
Hafiz Rahman <hfzrhmn@gmail.com>
2023-08-24 21:48:52 +0700

    Add: site title saving in the dialog fragment.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/NameYourStoreDialogFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/NameYourStoreDialogFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.FrameLayout
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
 
 class NameYourStoreDialogFragment : DialogFragment() {
+    private val viewModel: StoreOnboardingViewModel by viewModels()
     companion object {
         const val TAG: String = "NameYourStoreDialogFragment"
     }
@@ -28,7 +30,7 @@ class NameYourStoreDialogFragment : DialogFragment() {
         builder.setTitle(resources.getString(R.string.store_onboarding_name_your_store_dialog_title))
             .setView(frameLayout)
             .setPositiveButton(resources.getString(R.string.dialog_ok)) { dialog, _ ->
-                // todo handle the inputText
+                viewModel.saveSiteTitle(inputField.text.toString())
                 dialog.dismiss()
             }
             .setNegativeButton(resources.getString(R.string.cancel)) { dialog, _ ->
