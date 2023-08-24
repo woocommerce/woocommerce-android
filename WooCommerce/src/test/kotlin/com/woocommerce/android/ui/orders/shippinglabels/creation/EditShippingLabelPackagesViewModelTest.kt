ade63d4d24773484dc82b9fd5271b626ec4fea62
ThomazFB <thomazfcortez@gmail.com>
2023-08-23 21:09:49 -0300

    Introduce hazmat dialog event test scenario

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesViewModelTest.kt
@@ -5,6 +5,7 @@ import com.woocommerce.android.model.ShippingLabelPackage
 import com.woocommerce.android.ui.orders.OrderTestUtils
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
 import com.woocommerce.android.ui.orders.shippinglabels.ShippingLabelRepository
+import com.woocommerce.android.ui.orders.shippinglabels.creation.EditShippingLabelPackagesViewModel.OpenHazmatCategorySelector
 import com.woocommerce.android.ui.orders.shippinglabels.creation.EditShippingLabelPackagesViewModel.ViewState
 import com.woocommerce.android.ui.products.ParameterRepository
 import com.woocommerce.android.ui.products.ProductDetailRepository
@@ -303,5 +304,27 @@ class EditShippingLabelPackagesViewModelTest : BaseUnitTest() {
             assertThat(length).isEqualTo(testProduct.length)
             assertThat(height).isEqualTo(testProduct.height)
         }
+    }
+
+    @Test
+    fun `when select hazmat category is clicked, then trigger hazmat dialog event`() = testBlocking {
+        setup(emptyArray())
+        var event: MultiLiveEvent.Event? = null
+        val onHazmatCategorySelected: OnHazmatCategorySelected = { _ -> }
+        viewModel.event.observeForever { event = it }
+
+        viewModel.onHazmatCategoryClicked(
+            currentSelection = ShippingLabelHazmatCategory.AIR_ELIGIBLE_ETHANOL,
+            packagePosition = 0,
+            onHazmatCategorySelected = onHazmatCategorySelected
+        )
+
+        assertThat(event).isEqualTo(
+            OpenHazmatCategorySelector(
+                packagePosition = 0,
+                currentSelection = ShippingLabelHazmatCategory.AIR_ELIGIBLE_ETHANOL,
+                onHazmatCategorySelected = onHazmatCategorySelected
+            )
+        )
     }
 }
