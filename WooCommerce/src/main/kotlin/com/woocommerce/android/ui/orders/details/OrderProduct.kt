fcc337e235eb6cddfaed0c3528a6e2de87d07a44
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-08-24 17:15:32 +0200

    feat: make OrderProduct not Parcelable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/OrderProduct.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/OrderProduct.kt
@@ -1,16 +1,11 @@
 package com.woocommerce.android.ui.orders.details
 
-import android.os.Parcelable
 import com.woocommerce.android.model.Order
-import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
-@Parcelize
-sealed class OrderProduct : Parcelable {
-    @Parcelize
+sealed class OrderProduct {
     data class ProductItem(val product: Order.Item) : OrderProduct()
 
-    @Parcelize
     data class GroupedProductItem(
         val product: Order.Item,
         val children: List<ProductItem>,
