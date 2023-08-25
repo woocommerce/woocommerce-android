211206cf9625ec71af3c7c5421650172a5dd05c4
ThomazFB <thomazfcortez@gmail.com>
2023-08-25 19:53:56 -0300

    Add URL event handling inside EditShippingLabelPackagesViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesViewModel.kt
@@ -186,6 +186,10 @@ class EditShippingLabelPackagesViewModel @Inject constructor(
         triggerEvent(OpenHazmatCategorySelector(currentSelection, onHazmatCategorySelected))
     }
 
+    fun onURLClicked(url: String) {
+        triggerEvent(OpenURL(url))
+    }
+
     // all the logic is inside local functions, so it should be OK, but detekt complains still
     @Suppress("ComplexMethod")
     fun handleMoveItemResult(result: MoveItemResult) {
@@ -381,4 +385,6 @@ class EditShippingLabelPackagesViewModel @Inject constructor(
         val currentSelection: ShippingLabelHazmatCategory?,
         val onHazmatCategorySelected: OnHazmatCategorySelected
     ) : MultiLiveEvent.Event()
+
+    data class OpenURL(val url: String) : MultiLiveEvent.Event()
 }
