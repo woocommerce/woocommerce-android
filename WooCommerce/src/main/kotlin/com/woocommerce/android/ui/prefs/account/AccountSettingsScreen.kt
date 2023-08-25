7dd92234581d05bc6bf925541d4bb42fea1278ed
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-08-25 10:21:36 +0100

    Update design of the AccountSettings screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/account/AccountSettingsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/account/AccountSettingsScreen.kt
@@ -1,42 +1,86 @@
 package com.woocommerce.android.ui.prefs.account
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Divider
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.woocommerce.android.R
-import com.woocommerce.android.ui.compose.component.WCColoredButton
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
 fun AccountSettingsScreen(
     onCloseAccountClick: () -> Unit
 ) {
-    Box(
+    Column(
         modifier = Modifier
+            .background(MaterialTheme.colors.surface)
             .fillMaxSize()
-            .padding(dimensionResource(id = R.dimen.major_100))
     ) {
-        WCColoredButton(
+        Divider()
+        AccountSettingsItem(
+            title = stringResource(id = R.string.settings_close_account).uppercase(),
             onClick = onCloseAccountClick,
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = MaterialTheme.colors.surface,
-                contentColor = MaterialTheme.colors.error
-            ),
-            // The elevation is needed to give the button some contrast on dark mode
-            elevation = if (MaterialTheme.colors.isLight) null else ButtonDefaults.elevation(),
+            textColor = colorResource(id = R.color.woo_pink_50),
+            boldText = true,
             modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = stringResource(id = R.string.settings_close_account))
+        )
+        Divider()
+    }
+}
+
+@Composable
+private fun AccountSettingsItem(
+    title: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    subtitle: String? = null,
+    enabled: Boolean = true,
+    boldText: Boolean = false,
+    textColor: Color = LocalContentColor.current
+) {
+    Column(
+        modifier = modifier
+            .clickable(enabled = enabled, onClick = onClick)
+            .padding(
+                start = dimensionResource(id = R.dimen.major_375),
+                top = dimensionResource(id = R.dimen.major_100),
+                bottom = dimensionResource(id = R.dimen.major_100),
+                end = dimensionResource(id = R.dimen.major_100)
+            )
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = if (boldText) FontWeight.Medium else null,
+            color = textColor.let {
+                if (!enabled) it.copy(alpha = ContentAlpha.disabled) else it
+            }
+        )
+        subtitle?.let {
+            Text(
+                text = subtitle,
+                style = MaterialTheme.typography.body2,
+                fontWeight = if (boldText) FontWeight.Medium else null,
+                color = textColor.let {
+                    if (!enabled) it.copy(alpha = ContentAlpha.disabled) else it
+                }
+            )
         }
     }
 }
