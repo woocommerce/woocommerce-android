9dfa4ddfbdaf692697eac862acf6ae8064ea4d1e
samiuelson <samiuelson@gmail.com>
2023-08-30 14:16:48 +0200

    Implement mocked TaxRateRepository

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/taxes/rates/TaxRateRepository.kt
@@ -0,0 +1,62 @@
+package com.woocommerce.android.ui.orders.creation.taxes.rates
+
+import com.woocommerce.android.tools.SelectedSite
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class TaxRateRepository @Inject constructor(private val selectedSite: SelectedSite) {
+    suspend fun fetchTaxRates(): List<TaxRate> {
+        // TODO: Remove mock data and implement endpoint call
+        delay(1000)
+        return listOf(
+            TaxRate(
+                id = 1,
+                countryCode = "US",
+                stateCode = "CA",
+                postcode = "94016",
+                city = "San Francisco",
+                postCodes = null,
+                cities = null,
+                rate = "10%",
+                name = "Government Sales Tax",
+                priority = 1,
+                compound = false,
+                shipping = false,
+                order = 1,
+                taxClass = "standard",
+            ),
+            TaxRate(
+                id = 2,
+                countryCode = "US",
+                stateCode = "CA",
+                postcode = "",
+                city = "",
+                postCodes = null,
+                cities = null,
+                rate = "10%",
+                name = "",
+                priority = 1,
+                compound = false,
+                shipping = false,
+                order = 1,
+                taxClass = "standard",
+            ),
+            TaxRate(
+                id = 2,
+                countryCode = "",
+                stateCode = "AU",
+                postcode = "",
+                city = "",
+                postCodes = null,
+                cities = null,
+                rate = "10%",
+                name = "",
+                priority = 1,
+                compound = false,
+                shipping = false,
+                order = 1,
+                taxClass = "standard",
+            )
+        )
+    }
+}
