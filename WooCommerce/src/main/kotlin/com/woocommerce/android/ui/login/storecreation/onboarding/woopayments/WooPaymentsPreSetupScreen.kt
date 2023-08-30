1a4ba534075305993cbf0e8e221b6a6b769cf437
Hafiz Rahman <hfzrhmn@gmail.com>
2023-08-30 21:24:09 +0700

    UI: Add preview.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/woopayments/WooPaymentsPreSetupScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/woopayments/WooPaymentsPreSetupScreen.kt
@@ -18,10 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.component.Toolbar
 import com.woocommerce.android.ui.compose.component.WCColoredButton
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
 fun WooPaymentsPreSetupScreen() {
@@ -89,5 +91,13 @@ fun WooPaymentsPreSetupFooter() {
                 style = MaterialTheme.typography.subtitle1,
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun WooPaymentsPreSetupScreenPreview() {
+    WooThemeWithBackground {
+        WooPaymentsPreSetupScreen()
     }
 }
