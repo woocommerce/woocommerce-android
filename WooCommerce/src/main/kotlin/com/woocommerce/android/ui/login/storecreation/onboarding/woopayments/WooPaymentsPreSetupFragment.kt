75912746ec403e1d5ed425075255dd6082c9ccf5
Hafiz Rahman <hfzrhmn@gmail.com>
2023-08-30 20:51:00 +0700

    Add: Starter file for WooPaymentsPreSetup screen.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/woopayments/WooPaymentsPreSetupFragment.kt
@@ -0,0 +1,53 @@
+package com.woocommerce.android.ui.login.storecreation.onboarding.woopayments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.DialogFragment
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import org.wordpress.android.util.DisplayUtils
+
+class WooPaymentsPreSetupFragment : DialogFragment() {
+    companion object {
+        private const val TABLET_LANDSCAPE_WIDTH_RATIO = 0.35f
+        private const val TABLET_LANDSCAPE_HEIGHT_RATIO = 0.8f
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Use fullscreen style for all cases except tablet in landscape mode
+        setStyle(STYLE_NO_TITLE, if (isTabletLandscape()) R.style.Theme_Woo_Dialog else R.style.Theme_Woo)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // Specify transition animations
+        dialog?.window?.attributes?.windowAnimations = R.style.Woo_Animations_Dialog
+
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+            setContent {
+                WooThemeWithBackground {
+                    WooPaymentsPreSetupScreen()
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (isTabletLandscape()) {
+            requireDialog().window!!.setLayout(
+                (DisplayUtils.getWindowPixelWidth(requireContext()) * TABLET_LANDSCAPE_WIDTH_RATIO).toInt(),
+                (DisplayUtils.getWindowPixelHeight(requireContext()) * TABLET_LANDSCAPE_HEIGHT_RATIO).toInt()
+            )
+        }
+    }
+
+    private fun isTabletLandscape() = (DisplayUtils.isTablet(context) || DisplayUtils.isXLargeTablet(context)) &&
+        DisplayUtils.isLandscape(context)
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/woopayments/WooPaymentsPreSetupScreen.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.login.storecreation.onboarding.woopayments
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WooPaymentsPreSetupScreen() {
+
+}
