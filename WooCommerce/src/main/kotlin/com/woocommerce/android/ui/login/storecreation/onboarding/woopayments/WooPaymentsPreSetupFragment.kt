ab1283da7aef160570295f15aa45747fee71e0c3
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-08-31 16:20:36 +0200

    Remove duplicated toolbar for Woo payments setup fragments
    Also stop inheriting from DialogFragment which simplifies the code and allow us to hide the parent fragment Toolbar

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/woopayments/WooPaymentsPreSetupFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/onboarding/woopayments/WooPaymentsPreSetupFragment.kt
@@ -6,32 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.fragment.app.DialogFragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.AppUrls
-import com.woocommerce.android.R
 import com.woocommerce.android.extensions.navigateSafely
+import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.main.AppBarStatus
 import com.woocommerce.android.util.ChromeCustomTabUtils
-import org.wordpress.android.util.DisplayUtils
 
-class WooPaymentsPreSetupFragment : DialogFragment() {
-    companion object {
-        private const val TABLET_LANDSCAPE_WIDTH_RATIO = 0.35f
-        private const val TABLET_LANDSCAPE_HEIGHT_RATIO = 0.8f
-    }
+class WooPaymentsPreSetupFragment : BaseFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // Use fullscreen style for all cases except tablet in landscape mode
-        setStyle(STYLE_NO_TITLE, if (isTabletLandscape()) R.style.Theme_Woo_Dialog else R.style.Theme_Woo)
-    }
+    override val activityAppBarStatus: AppBarStatus
+        get() = AppBarStatus.Hidden
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        // Specify transition animations
-        dialog?.window?.attributes?.windowAnimations = R.style.Woo_Animations_Dialog
-
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 
@@ -62,17 +51,4 @@ class WooPaymentsPreSetupFragment : DialogFragment() {
                 .actionWooPaymentsPreSetupFragmentToWooPaymentsSetupFragment()
         )
     }
-
-    override fun onStart() {
-        super.onStart()
-        if (isTabletLandscape()) {
-            requireDialog().window!!.setLayout(
-                (DisplayUtils.getWindowPixelWidth(requireContext()) * TABLET_LANDSCAPE_WIDTH_RATIO).toInt(),
-                (DisplayUtils.getWindowPixelHeight(requireContext()) * TABLET_LANDSCAPE_HEIGHT_RATIO).toInt()
-            )
-        }
-    }
-
-    private fun isTabletLandscape() = (DisplayUtils.isTablet(context) || DisplayUtils.isXLargeTablet(context)) &&
-        DisplayUtils.isLandscape(context)
 }
