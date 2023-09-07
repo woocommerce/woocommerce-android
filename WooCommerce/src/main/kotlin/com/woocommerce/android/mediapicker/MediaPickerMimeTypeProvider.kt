255487ce41696b64d5a78ed491f8bf497f1bfe57
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-09-07 08:51:12 +0100

    Update logic to align with new media picker changes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/mediapicker/MediaPickerMimeTypeProvider.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/mediapicker/MediaPickerMimeTypeProvider.kt
@@ -7,6 +7,7 @@ class MediaPickerMimeTypeProvider @Inject constructor() : MimeTypeProvider {
     override val imageTypes: List<String> = listOf("JPEG", "PNG")
     override val videoTypes: List<String> = listOf("MP4")
     override val audioTypes: List<String> = emptyList()
+    override val documentTypes: List<String> = emptyList()
 
     override fun isApplicationTypeSupported(applicationType: String): Boolean = true
     override fun isMimeTypeSupported(mimeType: String): Boolean = true
--- WooCommerce/src/main/kotlin/com/woocommerce/android/mediapicker/MediaPickerSetupFactory.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/mediapicker/MediaPickerSetupFactory.kt
@@ -15,26 +15,33 @@ import java.security.InvalidParameterException
 import javax.inject.Inject
 
 class MediaPickerSetupFactory @Inject constructor() : MediaPickerSetup.Factory {
-    override fun build(source: DataSource, isMultiSelectAllowed: Boolean): MediaPickerSetup {
+    override fun build(
+        source: DataSource,
+        mediaTypes: MediaTypes,
+        isMultiSelectAllowed: Boolean
+    ): MediaPickerSetup {
         return when (source) {
             CAMERA -> DeviceMediaPickerSetup.buildCameraPicker()
             WP_MEDIA_LIBRARY -> MediaLibraryPickerSetup.build(
-                mediaTypes = MediaTypes.IMAGES,
+                mediaTypes = mediaTypes,
                 canMultiSelect = isMultiSelectAllowed
             )
+
             DEVICE -> MediaPickerSetup(
                 primaryDataSource = DEVICE,
                 isMultiSelectEnabled = isMultiSelectAllowed,
                 areResultsQueued = false,
                 searchMode = VISIBLE_UNTOGGLED,
                 availableDataSources = setOf(SYSTEM_PICKER),
-                allowedTypes = MediaTypes.IMAGES.allowedTypes,
+                allowedTypes = mediaTypes.allowedTypes,
                 title = R.string.photo_picker_title
             )
+
             SYSTEM_PICKER -> DeviceMediaPickerSetup.buildSystemPicker(
-                mediaTypes = MediaTypes.IMAGES,
+                mediaTypes = mediaTypes,
                 canMultiSelect = false
             )
+
             else -> throw InvalidParameterException("${source.name} source is not supported")
         }
     }
