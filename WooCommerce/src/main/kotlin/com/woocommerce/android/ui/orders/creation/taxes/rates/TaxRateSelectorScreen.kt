d2792c5f543c736ababa248fb7e20868fcb852c7
samiuelson <samiuelson@gmail.com>
2023-09-07 15:48:15 +0200

    Fix detekt issues

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/taxes/rates/TaxRateSelectorScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/taxes/rates/TaxRateSelectorScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -57,7 +56,13 @@ fun TaxRateSelectorScreen(
         backgroundColor = MaterialTheme.colors.surface,
         topBar = { Toolbar(onDismiss, onInfoIconClicked) }
     ) {
-        TaxRates(Modifier.padding(it), viewState.collectAsState().value, onInfoIconClicked, onTaxRateClick, onEditTaxRatesInAdminClicked)
+        TaxRates(
+            Modifier.padding(it),
+            viewState.collectAsState().value,
+            onInfoIconClicked,
+            onTaxRateClick,
+            onEditTaxRatesInAdminClicked
+        )
     }
 }
 
