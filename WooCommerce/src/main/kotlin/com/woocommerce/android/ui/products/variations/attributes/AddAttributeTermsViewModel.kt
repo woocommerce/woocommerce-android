8b4425c6d6d613fe8521028b9c13320911bbeca7
ThomazFB <thomazfcortez@gmail.com>
2023-09-07 17:19:06 -0300

    Define AddAttributeTermsViewModel state flows

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AddAttributeTermsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/attributes/AddAttributeTermsViewModel.kt
@@ -1,9 +1,14 @@
 package com.woocommerce.android.ui.products.variations.attributes
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import com.woocommerce.android.model.ProductAttributeTerm
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @HiltViewModel
@@ -11,11 +16,31 @@ class AddAttributeTermsViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val termsListHandler: AttributeTermsListHandler
 ): ScopedViewModel(savedState) {
-    init {
+    private val termsFlow = savedState.getStateFlow(
+        this,
+        emptyList<ProductAttributeTerm>()
+    )
+    val termsListState = termsFlow.asLiveData()
 
+    private val loadingStateFlow = MutableStateFlow(LoadingState.Idle)
+    val loadingState = loadingStateFlow.asLiveData()
+
+    fun onLoadMore(remoteAttributeId: Long) = launch {
+        loadingStateFlow.update { LoadingState.Appending }
+        publishChangesFrom(termsListHandler.loadMore(remoteAttributeId))
     }
 
-    private fun fetchAttributeTerms() = launch {
+    fun onFetchAttributeTerms(remoteAttributeId: Long) = launch {
+        loadingStateFlow.update { LoadingState.Loading }
+        publishChangesFrom(termsListHandler.fetchAttributeTerms(remoteAttributeId))
+    }
 
+    private fun publishChangesFrom(newTerms: List<ProductAttributeTerm>) {
+        termsFlow.update { it.toMutableList().apply { addAll(newTerms) } }
+        loadingStateFlow.update { LoadingState.Idle }
+    }
+
+    enum class LoadingState {
+        Idle, Loading, Appending
     }
 }
