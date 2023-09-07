2fd4be3f361c775a1d4027888c677398e9c305fe
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-09-07 10:01:21 +0100

    Enable all mime types supported by WordPress

--- WooCommerce/src/main/kotlin/com/woocommerce/android/mediapicker/MediaPickerMimeTypeProvider.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/mediapicker/MediaPickerMimeTypeProvider.kt
@@ -1,13 +1,16 @@
 package com.woocommerce.android.mediapicker
 
+import org.wordpress.android.fluxc.utils.MimeTypes
 import org.wordpress.android.mediapicker.api.MimeTypeProvider
 import javax.inject.Inject
 
 class MediaPickerMimeTypeProvider @Inject constructor() : MimeTypeProvider {
-    override val imageTypes: List<String> = listOf("JPEG", "PNG")
-    override val videoTypes: List<String> = listOf("MP4")
-    override val audioTypes: List<String> = emptyList()
-    override val documentTypes: List<String> = emptyList()
+    private val supportedMimes = MimeTypes()
+
+    override val imageTypes: List<String> = supportedMimes.getImageTypesOnly().asList()
+    override val videoTypes: List<String> = supportedMimes.getVideoTypesOnly().asList()
+    override val audioTypes: List<String> = supportedMimes.getAudioTypesOnly().asList()
+    override val documentTypes: List<String> = supportedMimes.getDocumentTypesOnly().asList()
 
     override fun isApplicationTypeSupported(applicationType: String): Boolean = true
     override fun isMimeTypeSupported(mimeType: String): Boolean = true
