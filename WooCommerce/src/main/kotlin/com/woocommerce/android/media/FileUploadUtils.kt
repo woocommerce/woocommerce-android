a7a5c7aaccfa2861108903ecf4ff09357d805d19
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-09-08 11:45:32 +0100

    Some fixes to the way we infer the file extension
    Previously, we were simply using the Uri path to try to infer it, but this doesn't work well for all cases (for example when picking a file using Google Drive of OneDrive), the fix attempts to retrieve the mimeType using the ContentResolver, and then uses the mime type to infer the extension if it's missing.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/media/FileUploadUtils.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/media/FileUploadUtils.kt
@@ -20,6 +20,7 @@ object FileUploadUtils {
     private const val OPTIMIZE_IMAGE_MAX_SIZE = 3000
     private const val OPTIMIZE_IMAGE_QUALITY = 85
 
+    @Suppress("ReturnCount")
     fun mediaModelFromLocalUri(
         context: Context,
         localSiteId: Int,
@@ -35,9 +36,11 @@ object FileUploadUtils {
                     getOptimizedImagePath(context, filePath)
                 } else filePath
 
+                val mimeType = getMimeType(context, localUri, fetchedUri) ?: return null
+
                 val file = File(path)
                 if (file.exists()) {
-                    return createMediaModel(mediaStore, fetchedUri, path, localSiteId)
+                    return createMediaModel(mediaStore, fetchedUri, path, localSiteId, mimeType)
                 } else {
                     WooLog.w(T.MEDIA, "mediaModelFromLocalUri > file does not exist, $path")
                 }
@@ -47,31 +50,39 @@ object FileUploadUtils {
         return null
     }
 
+    private fun getMimeType(context: Context, originalUri: Uri, fetchedUri: Uri): String? {
+        return originalUri.takeIf { it.scheme == "content" }
+            ?.let { context.contentResolver.getType(originalUri) }
+            ?: UrlUtils.getUrlMimeType(fetchedUri.toString())
+            ?: FluxCMediaUtils.getExtension(fetchedUri.path).let { extension ->
+                MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+            }
+    }
+
     @Suppress("MagicNumber")
     private fun createMediaModel(
         mediaStore: MediaStore,
         fetchedUri: Uri,
         path: String,
-        localSiteId: Int
+        localSiteId: Int,
+        mimeType: String
     ): MediaModel? {
         val media = mediaStore.instantiateMediaModel()
         var filename = FluxCMediaUtils.getFileName(fetchedUri.path)
-        var fileExtension: String? = FluxCMediaUtils.getExtension(fetchedUri.path)
 
-        var mimeType = UrlUtils.getUrlMimeType(fetchedUri.toString())
-        if (mimeType == null) {
-            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension)
-            if (mimeType == null) {
-                // Default to image jpeg
-                mimeType = "image/jpeg"
+        val fileExtension: String = filename
+            .substringAfterLast(delimiter = ".", missingDelimiterValue = "")
+            .ifBlank {
+                MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType)?.also {
+                    // Filename doesn't have an extension in this case
+                    filename += ".$it"
+                }
             }
-        }
-
-        // If file extension is null, upload won't work on wordpress.com
-        if (fileExtension.isNullOrBlank()) {
-            fileExtension = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType)
-            filename += "." + fileExtension!!
-        }
+            ?: run {
+                // If file extension is null, upload won't work on wordpress.com
+                WooLog.w(T.MEDIA, "We couldn't identify the file's extension")
+                return null
+            }
 
         media.fileName = filename
         media.title = filename
