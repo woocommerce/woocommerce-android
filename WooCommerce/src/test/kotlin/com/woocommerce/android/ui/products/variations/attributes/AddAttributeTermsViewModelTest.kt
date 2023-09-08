e1fb412a3b14ef2cb98169390bd136afa7dcfc30
ThomazFB <thomazfcortez@gmail.com>
2023-09-08 13:25:52 -0300

    Create AddAttributeTermsViewModelTest scaffold

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/attributes/AddAttributeTermsViewModelTest.kt
@@ -0,0 +1,32 @@
+package com.woocommerce.android.ui.products.variations.attributes
+
+import org.junit.Before
+import org.junit.Test
+
+class AddAttributeTermsViewModelTest {
+    private lateinit var sut: AddAttributeTermsViewModel
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun `when onFetchAttributeTerms is called, then termsListState should be updated`() {
+    }
+
+    @Test
+    fun `when onLoadMore is called, then termsListState should be updated by appending new items`() {
+    }
+
+    @Test
+    fun `when resetGlobalAttributeTerms is called, then termsListState should be empty`() {
+    }
+
+    @Test
+    fun `when onFetchAttributeTerms is called, then loadingState should be updated as expected`() {
+    }
+
+    @Test
+    fun `when onLoadMore is called, then loadingState should be updated as expected`() {
+    }
+}
