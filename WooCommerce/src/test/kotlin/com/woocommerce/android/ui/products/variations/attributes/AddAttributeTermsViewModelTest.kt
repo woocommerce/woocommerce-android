93e469d43bf4cfd81976f0126764c4d22fc07277
ThomazFB <thomazfcortez@gmail.com>
2023-09-08 15:28:58 -0300

    Create AddAttributeTermsViewModelTest fixtures

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/attributes/AddAttributeTermsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/attributes/AddAttributeTermsViewModelTest.kt
@@ -1,13 +1,23 @@
 package com.woocommerce.android.ui.products.variations.attributes
 
+import com.woocommerce.android.model.ProductAttributeTerm
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 
 class AddAttributeTermsViewModelTest {
     private lateinit var sut: AddAttributeTermsViewModel
+    private lateinit var termsListHandler: AttributeTermsListHandler
+
+    private val defaultAttributeId = 123L
 
     @Before
     fun setUp() {
+        termsListHandler = mock {
+            onBlocking { fetchAttributeTerms(defaultAttributeId) } doReturn defaultAttributeList
+            onBlocking { loadMore(defaultAttributeId) } doReturn defaultLoadMoreList
+        }
     }
 
     @Test
@@ -28,5 +38,21 @@ class AddAttributeTermsViewModelTest {
 
     @Test
     fun `when onLoadMore is called, then loadingState should be updated as expected`() {
+    }
+
+    private val defaultAttributeList = generateAttributeList(from = 1, to = 10)
+
+    private val defaultLoadMoreList = generateAttributeList(from = 10, to = 16)
+
+    private fun generateAttributeList(from: Int, to: Int): List<ProductAttributeTerm> {
+        return (from..to).map { index ->
+            ProductAttributeTerm(
+                id = index,
+                remoteId = index,
+                name = "Term $index",
+                slug = "term-$index",
+                description = "Term $index description"
+            )
+        }
     }
 }
