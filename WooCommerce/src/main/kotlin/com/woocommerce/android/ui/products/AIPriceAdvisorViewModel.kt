3103261baf86b884f6c366bee9d1e18b1b2b071d
Hafiz Rahman <hfzrhmn@gmail.com>
2023-09-10 13:08:14 +0700

    Add: viewmodel states.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/AIPriceAdvisorViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/AIPriceAdvisorViewModel.kt
@@ -1,9 +1,11 @@
 package com.woocommerce.android.ui.products
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ai.AIRepository
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,8 +13,24 @@ class AIPriceAdvisorViewModel @Inject constructor(
     private val aiRepository: AIRepository,
     savedStateHandle: SavedStateHandle
 ) : ScopedViewModel(savedStateHandle) {
+
+    private val _viewState = MutableStateFlow(ViewState())
+    val viewState = _viewState.asLiveData()
+
     enum class AdviceType {
         REGULAR_PRICE,
         SALE_PRICE
+    }
+
+    data class ViewState(
+        val generatedAdvice: String = "",
+        val generationState: GenerationState = GenerationState.Generating
+    )
+
+    sealed class GenerationState {
+        object Generating : GenerationState()
+        data class Generated(val showError: Boolean = false) : GenerationState()
+        object Regenerating : GenerationState()
+        object Failed : GenerationState()
     }
 }
