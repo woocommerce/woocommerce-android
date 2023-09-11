0eedb9f34e12adf32430a544a89fd090b542e7cf
ThomazFB <thomazfcortez@gmail.com>
2023-09-11 14:32:11 -0300

    Create basic AttributeTermsListHandlerTest scenarios

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributeTermsListHandlerTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributeTermsListHandlerTest.kt
@@ -3,7 +3,9 @@ package com.woocommerce.android.ui.products.variations.attributes
 import com.woocommerce.android.ui.products.ProductDetailRepository
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
+import org.junit.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
@@ -29,5 +31,24 @@ class AttributeTermsListHandlerTest: BaseUnitTest() {
         sut = AttributeTermsListHandler(
             repository = repository
         )
+    }
+
+    @Test
+    fun `when fetchAttributeTerms is called, then it should return the first page of attribute terms`() = testBlocking {
+        // When
+        val result = sut.fetchAttributeTerms(defaultAttributeId)
+
+        // Then
+        assertThat(result).isEqualTo(defaultAttributeList)
+    }
+
+    @Test
+    fun `when loadMore is called, then it should return the second page of attribute terms`() = testBlocking {
+        // When
+        sut.fetchAttributeTerms(defaultAttributeId)
+        val result = sut.loadMore(defaultAttributeId)
+
+        // Then
+        assertThat(result).isEqualTo(defaultLoadMoreList)
     }
 }
