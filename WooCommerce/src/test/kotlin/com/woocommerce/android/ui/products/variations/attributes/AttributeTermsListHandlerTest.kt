b06ac61b4d2402c2b55a64920db60f9218f53797
ThomazFB <thomazfcortez@gmail.com>
2023-09-11 13:56:07 -0300

    Create AttributeTermsListHandlerTest basic configuration

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributeTermsListHandlerTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/products/variations/attributes/AttributeTermsListHandlerTest.kt
@@ -5,10 +5,46 @@ import com.woocommerce.android.ui.products.ProductDetailRepository
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class AttributeTermsListHandlerTest: BaseUnitTest() {
     private lateinit var sut: AttributeTermsListHandler
     private lateinit var repository: ProductDetailRepository
+
+    private val defaultPageSize = 10
+    private val defaultAttributeId = 123L
+
+    @Before
+    fun setUp() {
+        repository = mock {
+            onBlocking {
+                fetchGlobalAttributeTerms(defaultAttributeId, 1, defaultPageSize)
+            } doReturn defaultAttributeList
+
+            onBlocking {
+                fetchGlobalAttributeTerms(defaultAttributeId, 2, defaultPageSize)
+            } doReturn defaultLoadMoreList
+        }
+        sut = AttributeTermsListHandler(
+            repository = repository
+        )
+    }
+
+    private val defaultAttributeList = generateAttributeList(from = 1, to = 10)
+
+    private val defaultLoadMoreList = generateAttributeList(from = 11, to = 16)
+
+    private fun generateAttributeList(from: Int, to: Int): List<ProductAttributeTerm> {
+        return (from..to).map { index ->
+            ProductAttributeTerm(
+                id = index,
+                remoteId = index,
+                name = "Term $index",
+                slug = "term-$index",
+                description = "Term $index description"
+            )
+        }
+    }
 }
