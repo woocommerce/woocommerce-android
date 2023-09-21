0f1c60ed698a503dc1ddc54761a8ea8260c54491
Hafiz Rahman <hfzrhmn@gmail.com>
2023-09-21 17:15:32 +0700

    UI: Add custom component of a textfield with embedded button.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/ProductNameSubScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/ProductNameSubScreen.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.products.ai
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,23 +11,35 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.component.WCColoredButton
-import com.woocommerce.android.ui.compose.component.WCOutlinedButton
 import com.woocommerce.android.ui.compose.component.WCOutlinedTextField
+import com.woocommerce.android.ui.compose.component.WCTextButton
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
@@ -76,25 +89,10 @@ fun ProductNameForm(
 
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_250)))
 
-            WCOutlinedTextField(
-                value = enteredName,
-                onValueChange = onProductNameChanged,
-                label = stringResource(id = R.string.ai_product_creation_add_name_keywords_label),
-                placeholderText = stringResource(id = R.string.ai_product_creation_add_name_keywords_placeholder),
-                textFieldModifier = Modifier.height(dimensionResource(id = R.dimen.multiline_textfield_height))
-            )
-
-            WCOutlinedButton(
-                onClick = onSuggestNameClicked,
-                text = stringResource(id = R.string.ai_product_creation_add_name_suggest_name_button),
-                leadingIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_ai_share_button),
-                        contentDescription = null
-                    )
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
+            ProductKeywordsTextFieldWithEmbeddedButton(
+                textFieldContent = enteredName,
+                onTextFieldContentChanged = onProductNameChanged,
+                onButtonClicked = onSuggestNameClicked
             )
 
             Spacer(modifier = Modifier.weight(1f))
@@ -123,6 +121,59 @@ fun ProductNameForm(
             ) {
                 Text(text = stringResource(id = R.string.continue_button))
             }
+        }
+    }
+}
+
+@Composable
+fun ProductKeywordsTextFieldWithEmbeddedButton(
+    textFieldContent: String,
+    onTextFieldContentChanged: (String) -> Unit,
+    onButtonClicked: () -> Unit
+) {
+    var isFocused by remember { mutableStateOf(false) }
+    Column {
+        Text(
+            text = stringResource(id = R.string.ai_product_creation_add_name_keywords_label),
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.minor_100))
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = if (isFocused) colorResource(id = R.color.color_primary)
+                    else colorResource(id = R.color.divider_color),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .clip(RoundedCornerShape(10.dp))
+        ) {
+            WCOutlinedTextField(
+                value = textFieldContent,
+                onValueChange = onTextFieldContentChanged,
+                label = "", // Can't use label here as it breaks the visual design.
+                placeholderText = stringResource(id = R.string.ai_product_creation_add_name_keywords_placeholder),
+                textFieldModifier = Modifier.height(dimensionResource(id = R.dimen.multiline_textfield_height)),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Transparent, // Remove outline and use Column's border instead.
+                    unfocusedBorderColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .onFocusChanged { focusState -> isFocused = focusState.isFocused }
+            )
+
+            Divider()
+
+            WCTextButton(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = dimensionResource(id = R.dimen.minor_50)),
+                onClick = onButtonClicked,
+                icon = ImageVector.vectorResource(id = R.drawable.ic_ai_share_button),
+                allCaps = false,
+                text = stringResource(id = R.string.ai_product_creation_add_name_suggest_name_button),
+            )
         }
     }
 }
