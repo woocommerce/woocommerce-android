3a4182e0c82c4eafdff197d8e6bf51b2fff4130c
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-09-21 10:58:35 +0200

    Trigger event to navigate to tone bottom sheet

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/AboutProductSubViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/AboutProductSubViewModel.kt
@@ -2,17 +2,23 @@ package com.woocommerce.android.ui.products.ai
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.getStateFlow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.map
 
 class AboutProductSubViewModel(
     savedStateHandle: SavedStateHandle,
     override val onDone: (String) -> Unit
 ) : AddProductWithAISubViewModel<String> {
+    private val _events = MutableSharedFlow<Event>(extraBufferCapacity = 1)
+    override val events: Flow<Event> get() = _events
+
     private val viewModelScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
     private val productFeatures = savedStateHandle.getStateFlow(viewModelScope, "")
@@ -30,7 +36,7 @@ class AboutProductSubViewModel(
     }
 
     fun onChangeToneClicked() {
-        // TODO
+        _events.tryEmit(NavigateToSetToneBottomSheet)
     }
 
     override fun close() {
@@ -40,4 +46,6 @@ class AboutProductSubViewModel(
     data class UiState(
         val productFeatures: String
     )
+
+    object NavigateToSetToneBottomSheet : Event()
 }
