c7141eee53ed756ddb98c75d2f5810b412ff73ba
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-09-22 13:08:30 +0100

    Handle the AI generation in the subviewmodel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/ProductPreviewSubViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/ProductPreviewSubViewModel.kt
@@ -5,27 +5,46 @@ import androidx.annotation.StringRes
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ai.AIRepository
 import com.woocommerce.android.model.Product
+import com.woocommerce.android.ui.products.ParameterRepository
+import com.woocommerce.android.ui.products.categories.ProductCategoriesRepository
+import com.woocommerce.android.ui.products.tags.ProductTagsRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 class ProductPreviewSubViewModel(
     private val aiRepository: AIRepository,
     private val buildProductPreviewProperties: BuildProductPreviewProperties,
+    private val categoriesRepository: ProductCategoriesRepository,
+    private val tagsRepository: ProductTagsRepository,
+    private val parametersRepository: ParameterRepository,
     override val onDone: (Product) -> Unit,
 ) : AddProductWithAISubViewModel<Product> {
     private val viewModelScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+
+    private val siteParameters by lazy { parametersRepository.getParameters() }
 
     private val _state = MutableStateFlow<State>(State.Loading)
     val state = _state.asLiveData()
 
     fun startGeneratingProduct(name: String, keywords: String) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             _state.value = State.Loading
-            aiRepository.generateProduct(name, keywords).fold(
+            aiRepository.generateProduct(
+                productName = name,
+                productKeyWords = keywords,
+                tone = "neutral",  // TODO,
+                weightUnit = siteParameters.weightUnit ?: "kg",
+                dimensionUnit = siteParameters.dimensionUnit ?: "cm",
+                currency = siteParameters.currencyCode ?: "USD",
+                existingCategories = categoriesRepository.getProductCategoriesList().map { it.name },
+                existingTags = tagsRepository.getProductTags().map { it.name },
+                languageISOCode = Locale.getDefault().language
+            ).fold(
                 onSuccess = { product ->
                     _state.value = State.Success(
                         product = product,
@@ -34,7 +53,8 @@ class ProductPreviewSubViewModel(
                     onDone(product)
                 },
                 onFailure = {
-                    TODO()
+                    // TODO
+                    it.printStackTrace()
                 }
             )
         }
