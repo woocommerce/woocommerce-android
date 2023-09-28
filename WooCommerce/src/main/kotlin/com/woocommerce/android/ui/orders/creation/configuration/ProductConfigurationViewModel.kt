80195b271ea4e510125022d40679c776584ebbca
Alejo <alejandro.torres.veiga@automattic.com>
2023-09-27 23:25:13 -0600

    refactor view model

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/configuration/ProductConfigurationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/configuration/ProductConfigurationViewModel.kt
@@ -1,12 +1,17 @@
 package com.woocommerce.android.ui.orders.creation.configuration
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.ui.orders.creation.GetProductRules
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,20 +22,36 @@ class ProductConfigurationViewModel @Inject constructor(
 ) : ScopedViewModel(savedState) {
 
     private val navArgs: ProductConfigurationFragmentArgs by savedState.navArgs()
-    val viewState: MutableStateFlow<ViewState> = MutableStateFlow(ViewState.Loading)
+
+    private val rules = MutableStateFlow<ProductRules?>(null)
+
+    private val configuration = MutableStateFlow<ProductConfiguration?>(null)
+
+    val viewState = combine(
+        flow = rules.drop(1),
+        flow2 = configuration.drop(1)
+    ) { rules, configuration ->
+        if (rules == null || configuration == null) {
+            ViewState.Error("rules not found")
+        } else {
+            ViewState.DisplayConfiguration(rules, configuration)
+        }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, ViewState.Loading)
 
     init {
-        getProductConfiguration()
+        launch {
+            getProductRules.getRules(navArgs.productId)?.let {
+                rules.value = it
+                configuration.value = ProductConfiguration.getConfiguration(it)
+            }
+        }
     }
 
-    private fun getProductConfiguration() {
-        launch {
-            val rules = getProductRules.getRules(navArgs.productId) ?: run {
-                viewState.value = ViewState.Error("rules not found")
-                return@launch
-            }
-            val configuration = ProductConfiguration.getConfiguration(rules)
-            viewState.value = ViewState.DisplayConfiguration(rules, configuration)
+    fun onUpdateChildrenConfiguration(itemId: Long, ruleKey: String, value: String) {
+        configuration.value?.let { currentConfiguration ->
+            currentConfiguration.updateChildrenConfiguration(itemId, ruleKey, value)
+            configuration.value =
+                ProductConfiguration(currentConfiguration.configuration, currentConfiguration.childrenConfiguration)
         }
     }
 
