e4525aa4dcbf874e95705b78581eca93a258f0a0
Hafiz Rahman <hfzrhmn@gmail.com>
2023-09-29 13:31:19 +0700

    Add: Tracking for when "Create a product with AI" button is tapped.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/AddProductWithAIBottomSheet.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/AddProductWithAIBottomSheet.kt
@@ -73,6 +73,7 @@ class AddProductWithAIBottomSheet : WCBottomSheetDialogFragment() {
     }
 
     private fun showAICreationFlow() {
+        AnalyticsTracker.track(AnalyticsEvent.PRODUCT_CREATION_AI_ENTRY_POINT_TAPPED)
         findNavController().navigateSafely(
             AddProductWithAIBottomSheetDirections.actionAddProductWithAIBottomSheetToAddProductWithAIFragment()
         )
