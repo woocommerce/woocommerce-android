a06112038cbcbf6af55a4a5ec7083295e5832804
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-10-03 18:33:52 +0200

    chore: remove shipping zones feature flag

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -21,7 +21,6 @@ enum class FeatureFlag {
     PRIVACY_CHOICES,
     BLAZE,
     ORDER_CREATION_PRODUCT_DISCOUNTS,
-    SHIPPING_ZONES,
     BETTER_CUSTOMER_SEARCH_M2,
     ORDER_CREATION_TAX_RATE_SELECTOR,
     ORDER_CREATION_AUTO_TAX_RATE,
@@ -52,7 +51,6 @@ enum class FeatureFlag {
             BETTER_CUSTOMER_SEARCH_M2,
             ORDER_CREATION_AUTO_TAX_RATE -> PackageUtils.isDebugBuild()
 
-            SHIPPING_ZONES,
             IAP_FOR_STORE_CREATION -> false
         }
     }
