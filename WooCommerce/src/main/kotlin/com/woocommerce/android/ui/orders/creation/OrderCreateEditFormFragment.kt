45f781df7aceed457c1703ae014d39dc8db16a55
Anirudh Uppunda <abhat38@gmail.com>
2023-10-12 11:03:09 +0530

    Fix detekt error

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditFormFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditFormFragment.kt
@@ -261,7 +261,7 @@ class OrderCreateEditFormFragment :
                 addCustomAmountsButton = AddButton(
                     text = getString(R.string.order_creation_add_custom_amounts),
                     onClickListener = {
-                        // TODO: Implement custom amounts click listener
+                        // Implement custom amounts click listener
                     }
                 )
             )
