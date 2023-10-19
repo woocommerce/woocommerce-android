6b4dbd5edba41dbe023de0ae5dabce497c4e3724
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-10-19 11:42:45 +0100

    Extract reusable logic to its own function

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/BlazeUrlsHelper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/BlazeUrlsHelper.kt
@@ -8,33 +8,27 @@ class BlazeUrlsHelper @Inject constructor(
 ) {
     companion object {
         private const val HTTP_PATTERN = "(https?://)"
-        private const val BASE_URL = "https://wordpress.com/advertising/"
-        private const val BLAZE_CREATION_FLOW_PRODUCT = "$BASE_URL%s?blazepress-widget=post-%d&_source=%s"
-        private const val BLAZE_CREATION_FLOW_SITE = "$BASE_URL%s?_source=%s"
+        private const val BASE_URL = "https://wordpress.com/advertising"
+        private const val BLAZE_CREATION_FLOW_PRODUCT = "$BASE_URL/%s?blazepress-widget=post-%d&_source=%s"
+        private const val BLAZE_CREATION_FLOW_SITE = "$BASE_URL/%s?_source=%s"
 
         // Analytics
         const val BLAZEPRESS_WIDGET = "blazepress-widget"
     }
 
     fun buildUrlForSite(source: BlazeFlowSource): String {
-        val siteUrlWithoutProtocol = selectedSite.get().url.replace(Regex(HTTP_PATTERN), "")
-        return BLAZE_CREATION_FLOW_SITE.format(siteUrlWithoutProtocol, source.trackingName)
+        return BLAZE_CREATION_FLOW_SITE.format(getSiteUrl(), source.trackingName)
     }
 
     fun buildUrlForProduct(productId: Long, source: BlazeFlowSource): String {
-        val siteUrlWithoutProtocol = selectedSite.get().url.replace(Regex(HTTP_PATTERN), "")
-        return BLAZE_CREATION_FLOW_PRODUCT.format(siteUrlWithoutProtocol, productId, source.trackingName)
+        return BLAZE_CREATION_FLOW_PRODUCT.format(getSiteUrl(), productId, source.trackingName)
     }
 
-    fun buildCampaignsListUrl(): String {
-        val siteUrlWithoutProtocol = selectedSite.get().url.replace(Regex(HTTP_PATTERN), "")
-        return "${BASE_URL}campaigns/$siteUrlWithoutProtocol"
-    }
+    fun buildCampaignsListUrl(): String = "${BASE_URL}/campaigns/${getSiteUrl()}"
 
-    fun buildCampaignDetailsUrl(campaignId: Int): String {
-        val siteUrlWithoutProtocol = selectedSite.get().url.replace(Regex(HTTP_PATTERN), "")
-        return "${BASE_URL}campaigns/$campaignId/$siteUrlWithoutProtocol"
-    }
+    fun buildCampaignDetailsUrl(campaignId: Int): String = "${BASE_URL}/campaigns/$campaignId/${getSiteUrl()}"
+
+    private fun getSiteUrl() = selectedSite.get().url.replace(Regex(HTTP_PATTERN), "")
 
     enum class BlazeFlowSource(val trackingName: String) {
         PRODUCT_DETAIL_OVERFLOW_MENU("product_more_menu"),
