ea03f89e0a9f2df49117149fe2336e5c3a930470
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-10-23 12:53:04 +0100

    Add UI for the blaze campaign intro screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/BlazeCampaignCreationScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/BlazeCampaignCreationScreen.kt
@@ -1,21 +1,42 @@
 package com.woocommerce.android.ui.blaze
 
+import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
 import androidx.activity.result.ActivityResultRegistry
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.with
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.blaze.BlazeCampaignCreationViewModel.BlazeCreationViewState
 import com.woocommerce.android.ui.common.wpcomwebview.WPComWebViewAuthenticator
 import com.woocommerce.android.ui.compose.component.Toolbar
 import com.woocommerce.android.ui.compose.component.WCWebView
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import org.wordpress.android.fluxc.network.UserAgent
 
 @Composable
@@ -53,7 +74,8 @@ fun BlazeCampaignCreationScreen(
                 onNavigationButtonClick = onClose,
                 navigationIcon = Filled.ArrowBack
             )
-        }
+        },
+        backgroundColor = MaterialTheme.colors.surface
     ) { paddingValues ->
         when (viewState) {
             is BlazeCreationViewState.Intro -> TODO()
@@ -68,5 +90,77 @@ fun BlazeCampaignCreationScreen(
                     .fillMaxSize()
             )
         }
+    }
+}
+
+@Composable
+private fun BlazeCreationIntroScreen(
+    onCreateCampaignClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        Column(
+            Modifier
+                .padding(dimensionResource(id = R.dimen.major_100))
+                .weight(1f)
+        ) {
+            Spacer(modifier = Modifier.weight(0.8f))
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_blaze),
+                contentDescription = null,
+                modifier = Modifier
+                    .background(
+                        color = colorResource(id = R.color.woo_orange_5).copy(alpha = 0.5f),
+                        shape = CircleShape
+                    )
+                    .size(dimensionResource(id = R.dimen.image_major_120))
+                    .padding(dimensionResource(id = R.dimen.major_250))
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_150)))
+            Text(
+                text = stringResource(id = R.string.blaze_campaign_creation_intro_title),
+                style = MaterialTheme.typography.h4
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.major_150)))
+            Text(
+                text = "• ${stringResource(id = R.string.blaze_campaign_creation_intro_benefit_1)}",
+                style = MaterialTheme.typography.subtitle1
+            )
+            Text(
+                text = "• ${stringResource(id = R.string.blaze_campaign_creation_intro_benefit_2)}",
+                style = MaterialTheme.typography.subtitle1
+            )
+            Text(
+                text = "• ${stringResource(id = R.string.blaze_campaign_creation_intro_benefit_3)}",
+                style = MaterialTheme.typography.subtitle1
+            )
+            Text(
+                text = "• ${stringResource(id = R.string.blaze_campaign_creation_intro_benefit_4)}",
+                style = MaterialTheme.typography.subtitle1
+            )
+
+            Spacer(modifier = Modifier.weight(1.3f))
+        }
+
+        Divider()
+
+        Button(
+            onClick = onCreateCampaignClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.major_100))
+        ) {
+            Text(text = stringResource(id = R.string.blaze_campaign_creation_intro_button))
+        }
+    }
+}
+
+@Composable
+@Preview(name = "light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+private fun BlazeCreationIntroPreview() {
+    WooThemeWithBackground {
+        BlazeCreationIntroScreen(onCreateCampaignClick = {})
     }
 }
