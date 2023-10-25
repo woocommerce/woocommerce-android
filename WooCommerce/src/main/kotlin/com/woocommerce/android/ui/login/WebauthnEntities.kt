b2c3c142f84aa19908ffd61cf3b027aac16026d1
ThomazFB <thomazfcortez@gmail.com>
2023-10-24 23:36:49 -0300

    Correctly parse the user credential data

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/WebauthnEntities.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/WebauthnEntities.kt
@@ -1,26 +1,37 @@
 package com.woocommerce.android.ui.login
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.google.gson.annotations.SerializedName
+import java.util.Base64
 
 data class CredentialManagerData(
     @SerializedName("two_step_nonce") val twoStepNonce: String,
-    @SerializedName("user.name") val username: String,
-    @SerializedName("user.id") val userId: Long,
     val challenge: String,
     val rpId: String,
     val allowCredentials: List<WebauthnCredential>,
     val timeout: Int,
+    val user: WebauthnUser
 ) {
+    @RequiresApi(Build.VERSION_CODES.O)
     constructor(challengeInfo: WebauthnChallengeInfo, userId: Long, username: String) : this(
-        username = username,
-        userId = userId,
-        challenge = challengeInfo.challenge,
+        challenge = String(Base64.getUrlDecoder().decode(challengeInfo.challenge)),
         rpId = challengeInfo.rpId,
         twoStepNonce = challengeInfo.twoStepNonce,
         allowCredentials = challengeInfo.allowCredentials,
-        timeout = challengeInfo.timeout
+        timeout = challengeInfo.timeout,
+        user = WebauthnUser(
+            name = username,
+            id = userId
+        )
     )
 }
+
+data class WebauthnUser(
+    val name: String,
+    val id: Long
+)
+
 data class WebauthnChallengeInfo(
     val challenge: String,
     val rpId: String,
