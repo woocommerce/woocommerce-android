570602ade6efaa0d0073a092e7de1a04d0e03f24
ThomazFB <thomazfcortez@gmail.com>
2023-10-25 19:00:20 -0300

    Extend the WebauthnEntities.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/WebauthnEntities.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/WebauthnEntities.kt
@@ -4,32 +4,35 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import com.google.gson.annotations.SerializedName
 import java.util.Base64
+import kotlin.math.ceil
 
 data class CredentialManagerData(
     @SerializedName("two_step_nonce") val twoStepNonce: String,
     val challenge: String,
-    val rpId: String,
     val allowCredentials: List<WebauthnCredential>,
     val timeout: Int,
-    val user: WebauthnUser
+    val rpId: String
 ) {
-    @RequiresApi(Build.VERSION_CODES.O)
-    constructor(challengeInfo: WebauthnChallengeInfo, userId: Long, username: String) : this(
-        challenge = String(Base64.getUrlDecoder().decode(challengeInfo.challenge)),
-        rpId = challengeInfo.rpId,
+    constructor(challengeInfo: WebauthnChallengeInfo) : this(
+        challenge = Base64.getUrlDecoder().decode(challengeInfo.challenge).toString(),
         twoStepNonce = challengeInfo.twoStepNonce,
-        allowCredentials = challengeInfo.allowCredentials,
+        allowCredentials = challengeInfo.allowCredentials.map {
+            it.copy(id = Base64.getUrlDecoder().decode(it.id).toString())
+        },
         timeout = challengeInfo.timeout,
-        user = WebauthnUser(
-            name = username,
-            id = userId
-        )
+        rpId = challengeInfo.rpId
     )
 }
 
 data class WebauthnUser(
     val name: String,
-    val id: Long
+    val displayName: String,
+    val id: String
+)
+
+data class WebauthnRp(
+    val name: String,
+    val id: String
 )
 
 data class WebauthnChallengeInfo(
@@ -45,3 +48,20 @@ data class WebauthnCredential(
     val id: String,
     val transports: List<String>
 )
+
+data class WebauthnKeyCredParams(
+    val type: String,
+    val alg: Int
+)
+
+fun String.base64UrlDecode(): ByteArray? {
+    var base64 = this.replace("-", "+").replace("_", "/")
+    val length = base64.toByteArray().size.toDouble()
+    val requiredLength = 4 * ceil(length / 4.0)
+    val paddingLength = requiredLength - length
+    if (paddingLength > 0) {
+        val padding = "=".repeat(paddingLength.toInt())
+        base64 += padding
+    }
+    return Base64.getDecoder().decode(base64)
+}
