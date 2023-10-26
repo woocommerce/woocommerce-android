579ceaba4a6ad4d52adbe552ab16280a38e7e79d
Anirudh Uppunda <abhat38@gmail.com>
2023-10-26 10:39:52 +0530

    Add mapper class to map from fee line to CustomAmountUIModel

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/MapItemToCustomAmountUiModel.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.orders.creation
+
+import com.woocommerce.android.model.Order
+import com.woocommerce.android.ui.payments.customamounts.CustomAmountsDialog.Companion.CUSTOM_AMOUNT
+import javax.inject.Inject
+
+class MapFeeLineToCustomAmountUiModel @Inject constructor() {
+
+    operator fun invoke(feeLine: Order.FeeLine): CustomAmountUIModel {
+        return CustomAmountUIModel(
+            amount = feeLine.total,
+            name = feeLine.name ?: CUSTOM_AMOUNT
+        )
+    }
+}
