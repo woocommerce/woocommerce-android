58a2c20e48db975ad6f24a291df36940191fd69d
Ondrej Ruttkay <ruttkay@gmail.com>
2023-10-31 15:47:27 +0100

    Add the 404 code case handler and refactor the response

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/jetpack/benefits/FetchJetpackStatus.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/jetpack/benefits/FetchJetpackStatus.kt
@@ -4,9 +4,6 @@ import com.woocommerce.android.OnChangedException
 import com.woocommerce.android.extensions.orNullIfEmpty
 import com.woocommerce.android.model.JetpackStatus
 import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.ui.jetpack.benefits.FetchJetpackStatus.JetpackStatusFetchResponse.FORBIDDEN
-import com.woocommerce.android.ui.jetpack.benefits.FetchJetpackStatus.JetpackStatusFetchResponse.NOT_FOUND
-import com.woocommerce.android.ui.jetpack.benefits.FetchJetpackStatus.JetpackStatusFetchResponse.SUCCESS
 import org.wordpress.android.fluxc.store.JetpackStore
 import org.wordpress.android.fluxc.store.WooCommerceStore
 import javax.inject.Inject
@@ -33,15 +30,17 @@ class FetchJetpackStatus @Inject constructor(
 ) {
     companion object {
         private const val FORBIDDEN_CODE = 403
+        private const val NOT_FOUND_CODE = 404
         private const val JETPACK_SLUG = "jetpack"
     }
 
-    enum class JetpackStatusFetchResponse {
-        SUCCESS, NOT_FOUND, FORBIDDEN
+    sealed interface JetpackStatusFetchResponse {
+        data class Success(val status: JetpackStatus) : JetpackStatusFetchResponse
+        object ConnectionForbidden : JetpackStatusFetchResponse
     }
 
     @Suppress("ReturnCount")
-    suspend operator fun invoke(): Result<Pair<JetpackStatus, JetpackStatusFetchResponse>> {
+    suspend operator fun invoke(): Result<JetpackStatusFetchResponse> {
         val isJetpackInstalled = wooCommerceStore.fetchSitePlugins(selectedSite.get()).let { result ->
             when {
                 result.isError -> {
@@ -56,14 +55,17 @@ class FetchJetpackStatus @Inject constructor(
         return jetpackStore.fetchJetpackUser(selectedSite.get(), useApplicationPasswords = true).let { result ->
             when {
                 result.error?.errorCode == FORBIDDEN_CODE -> {
+                    Result.success(JetpackStatusFetchResponse.ConnectionForbidden)
+                }
+
+                result.error?.errorCode == NOT_FOUND_CODE -> {
                     Result.success(
-                        Pair(
+                        JetpackStatusFetchResponse.Success(
                             JetpackStatus(
-                                isJetpackInstalled = isJetpackInstalled,
+                                isJetpackInstalled = false,
                                 isJetpackConnected = false,
                                 wpComEmail = null
-                            ),
-                            if (isJetpackInstalled) FORBIDDEN else NOT_FOUND
+                            )
                         )
                     )
                 }
@@ -74,13 +76,12 @@ class FetchJetpackStatus @Inject constructor(
 
                 else -> {
                     Result.success(
-                        Pair(
+                        JetpackStatusFetchResponse.Success(
                             JetpackStatus(
                                 isJetpackInstalled = isJetpackInstalled,
                                 isJetpackConnected = result.user!!.isConnected,
                                 wpComEmail = result.user!!.wpcomEmail.orNullIfEmpty()
-                            ),
-                            if (isJetpackInstalled) SUCCESS else NOT_FOUND
+                            )
                         )
                     )
                 }
