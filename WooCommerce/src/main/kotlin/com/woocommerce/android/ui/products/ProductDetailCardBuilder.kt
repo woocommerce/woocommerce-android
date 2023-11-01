50fd9d01f96edca2445a5b52ad2228c0ff5b5930
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-11-01 17:53:55 +0100

    Check if a product is already being promoted with Blaze
    If the product is already promoted we hide the Blaze cta inside product detail

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailCardBuilder.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductDetailCardBuilder.kt
@@ -22,6 +22,7 @@ import com.woocommerce.android.model.Product
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.blaze.BlazeUrlsHelper.BlazeFlowSource
 import com.woocommerce.android.ui.blaze.IsBlazeEnabled
+import com.woocommerce.android.ui.blaze.IsProductCurrentlyPromoted
 import com.woocommerce.android.ui.products.ProductInventoryViewModel.InventoryData
 import com.woocommerce.android.ui.products.ProductNavigationTarget.ViewGroupedProducts
 import com.woocommerce.android.ui.products.ProductNavigationTarget.ViewLinkedProducts
@@ -84,6 +85,7 @@ class ProductDetailCardBuilder(
     private val variationRepository: VariationRepository,
     private val appPrefsWrapper: AppPrefsWrapper,
     private val isBlazeEnabled: IsBlazeEnabled,
+    private val isProductCurrentlyPromoted: IsProductCurrentlyPromoted,
     private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
 ) {
     private var blazeCtaShownTracked = false
@@ -142,7 +144,8 @@ class ProductDetailCardBuilder(
 
         if (!isBlazeEnabled() ||
             !isProductPublic ||
-            viewModel.isProductUnderCreation
+            viewModel.isProductUnderCreation ||
+            isProductCurrentlyPromoted(product.remoteId.toString())
         ) return null
 
         if (!blazeCtaShownTracked) {
