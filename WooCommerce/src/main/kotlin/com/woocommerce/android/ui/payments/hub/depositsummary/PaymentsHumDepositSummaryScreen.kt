7ac3d6dc77e705dc540d8164ec61b04ba2fb1932
Andrey <akidinov@gmail.com>
2023-11-06 16:24:10 +0100

    Base for the deposit view

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/hub/depositsummary/PaymentsHumDepositSummaryScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/hub/depositsummary/PaymentsHumDepositSummaryScreen.kt
@@ -1,12 +1,52 @@
 package com.woocommerce.android.ui.payments.hub.depositsummary
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+private const val ANIM_DURATION_MILLIS = 128
+
 @Composable
-fun PaymentsHubDepositSummaryScreen(
+fun PaymentsHubDepositSummaryView(
     viewModel: PaymentsHumDepositSummaryViewModel = viewModel()
 ) {
-    viewModel.viewState.observeAsState().let { }
+    viewModel.viewState.observeAsState().let {
+        val value = it.value
+        when (value) {
+            is PaymentsHubDepositSummaryState.Success -> PaymentsHubDepositSummaryView(value.overview)
+            null,
+            PaymentsHubDepositSummaryState.Loading,
+            is PaymentsHubDepositSummaryState.Error -> {
+                // show nothing
+            }
+        }
+    }
+}
+
+@Composable
+fun PaymentsHubDepositSummaryView(
+    overview: PaymentsHubDepositSummaryState.Overview
+) {
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+    Card(
+        elevation = 8.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable {
+                isExpanded = !isExpanded
+            }
+    ) {
+
+    }
 }
