74113ca127b2d639ad300b31bb09b2d4548f6f97
ThomazFB <thomazfcortez@gmail.com>
2023-11-10 14:39:26 -0300

    Adjust Login2faFragment to correctly start the 2FA scenario

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/LoginActivity.kt
@@ -496,8 +496,18 @@ class LoginActivity :
         changeFragment(login2FaFragment, true, Login2FaFragment.TAG)
     }
 
-    override fun needs2faSecurityKey(email: String?, password: String?, userId: String?, webauthnNonce: String?) {
-        val login2FaFragment = Login2FaFragment.newInstanceSecurityKey(email, password, userId, webauthnNonce)
+    override fun needs2fa(
+        email: String?,
+        password: String?,
+        userId: String?,
+        webauthnNonce: String?,
+        nonceAuthenticator: String?,
+        nonceBackup: String?,
+        noncePush: String?,
+        supportedAuthTypes: MutableList<String>?
+    ) {
+        val login2FaFragment = Login2FaFragment.newInstance(email, password, userId,
+            webauthnNonce, nonceAuthenticator, nonceBackup, noncePush, supportedAuthTypes)
         changeFragment(login2FaFragment, true, Login2FaFragment.TAG)
     }
 
