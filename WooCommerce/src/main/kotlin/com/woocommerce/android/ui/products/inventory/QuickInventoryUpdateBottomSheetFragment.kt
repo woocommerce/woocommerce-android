638c1300ad6ccd32eb68ce99c5b409c5f7194fa2
samiuelson <samiuelson@gmail.com>
2023-11-13 18:40:40 +0100

    Add QuickInventoryUpdateBottomSheetFragment class skeleton

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/QuickInventoryUpdateBottomSheetFragment.kt
@@ -0,0 +1,31 @@
+package com.woocommerce.android.ui.products.inventory
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+
+class QuickInventoryUpdateBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WooThemeWithBackground {
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
