4c3ddd8eec9d135f0163426e2a0ad63da8b2aa1d
Anirudh Uppunda <abhat38@gmail.com>
2023-11-14 20:07:24 +0530

    Move class CustomAmountUIModel.kt under order package

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/MapFeeLineToCustomAmountUiModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/MapFeeLineToCustomAmountUiModel.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.orders.creation
 
 import com.woocommerce.android.model.Order
+import com.woocommerce.android.ui.orders.CustomAmountUIModel
 import com.woocommerce.android.ui.payments.customamounts.CustomAmountsDialog.Companion.CUSTOM_AMOUNT
 import javax.inject.Inject
 
