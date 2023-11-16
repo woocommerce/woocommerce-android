dc0f2684e085345eaef67738d4b39cae829227d3
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-11-16 11:47:51 -0500

    Test commit

--- libs/cardreader/src/test/java/com/woocommerce/android/cardreader/internal/connection/ConnectionManagerTest.kt
+++ libs/cardreader/src/test/java/com/woocommerce/android/cardreader/internal/connection/ConnectionManagerTest.kt
@@ -36,20 +36,6 @@ import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
 class ConnectionManagerTest : CardReaderBaseUnitTest() {
-    init {
-        // This is a workaround for the change in kotlinx.coroutines 1.7.0 that causes tests that
-        // throw exceptions to fail. Previously test methods that threw exceptions would not prevent
-        // tests from passing, which was a bug in kotlinx.coroutines that has now been fixed. However,
-        // significant number of our tests are currently failing because of this change.
-        //
-        // See the following issue for more details: https://github.com/Kotlin/kotlinx.coroutines/issues/1205.
-        // The workaround below is taken from the related PR: https://github.com/Kotlin/kotlinx.coroutines/pull/3736
-        // and is a solution suggested by JetBrains to disable the new behavior using non-public API
-        // until we fix our tests. This should not be considered a long-term solution, rather a temporary hack.
-        Class.forName("kotlinx.coroutines.test.TestScopeKt")
-            .getDeclaredMethod("setCatchNonTestRelatedExceptions", Boolean::class.java)
-            .invoke(null, false)
-    }
 
     private val terminalWrapper: TerminalWrapper = mock()
     private val bluetoothReaderListener: BluetoothReaderListenerImpl = mock()
