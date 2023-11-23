f895158b01f9b44b00be6e2dc9b74e8ec361e50e
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-11-23 23:57:52 +0100

    Adds an overflow menu to Blaze view in MyStore

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/MyStoreBlazeView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/MyStoreBlazeView.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -17,10 +20,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons.Outlined
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,8 +43,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
+import com.woocommerce.android.R.dimen
+import com.woocommerce.android.R.string
 import com.woocommerce.android.ui.blaze.MyStoreBlazeViewModel.MyStoreBlazeCampaignState
 import com.woocommerce.android.ui.blaze.campaigs.BlazeCampaignItem
 import com.woocommerce.android.ui.compose.component.ProductThumbnail
@@ -48,54 +63,60 @@ fun MyStoreBlazeView(
             .wrapContentHeight(),
         shape = RoundedCornerShape(0.dp)
     ) {
-        Column {
-            Column(
-                modifier = Modifier.padding(
-                    top = dimensionResource(id = R.dimen.major_100),
-                    start = dimensionResource(id = R.dimen.major_100),
-                    end = dimensionResource(id = R.dimen.major_100),
-                )
-            ) {
-                BlazeCampaignHeader()
-                when (state) {
-                    is MyStoreBlazeCampaignState.Campaign -> BlazeCampaignItem(
-                        campaign = state.campaign,
-                        onCampaignClicked = state.onCampaignClicked,
-                        modifier = Modifier.padding(top = dimensionResource(id = R.dimen.major_100))
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Column {
+                Column(
+                    modifier = Modifier.padding(
+                        top = dimensionResource(id = R.dimen.major_100),
+                        start = dimensionResource(id = R.dimen.major_100),
+                        end = dimensionResource(id = R.dimen.major_100),
                     )
-
-                    is MyStoreBlazeCampaignState.NoCampaign -> {
-                        Text(
-                            modifier = Modifier.padding(
-                                top = dimensionResource(id = R.dimen.major_100),
-                                end = dimensionResource(id = R.dimen.major_300)
-                            ),
-                            text = stringResource(id = R.string.blaze_campaign_subtitle),
-                            style = MaterialTheme.typography.body1,
-                        )
-                        BlazeProductItem(
-                            product = state.product,
-                            onProductSelected = state.onProductClicked,
+                ) {
+                    BlazeCampaignHeader()
+                    when (state) {
+                        is MyStoreBlazeCampaignState.Campaign -> BlazeCampaignItem(
+                            campaign = state.campaign,
+                            onCampaignClicked = state.onCampaignClicked,
                             modifier = Modifier.padding(top = dimensionResource(id = R.dimen.major_100))
                         )
+
+                        is MyStoreBlazeCampaignState.NoCampaign -> {
+                            Text(
+                                modifier = Modifier.padding(
+                                    top = dimensionResource(id = R.dimen.major_100),
+                                    end = dimensionResource(id = R.dimen.major_300)
+                                ),
+                                text = stringResource(id = R.string.blaze_campaign_subtitle),
+                                style = MaterialTheme.typography.body1,
+                            )
+                            BlazeProductItem(
+                                product = state.product,
+                                onProductSelected = state.onProductClicked,
+                                modifier = Modifier.padding(top = dimensionResource(id = R.dimen.major_100))
+                            )
+                        }
+
+                        else -> error("Invalid state")
                     }
+                }
+                when (state) {
+                    is MyStoreBlazeCampaignState.Campaign -> ShowAllOrCreateCampaignFooter(
+                        onShowAllClicked = state.onViewAllCampaignsClicked,
+                        onCreateCampaignClicked = state.onCreateCampaignClicked
+                    )
+
+                    is MyStoreBlazeCampaignState.NoCampaign -> CreateCampaignFooter(
+                        onCreateCampaignClicked = state.onCreateCampaignClicked,
+                        modifier = Modifier.padding(top = dimensionResource(id = R.dimen.major_100))
+                    )
 
                     else -> error("Invalid state")
                 }
             }
-            when (state) {
-                is MyStoreBlazeCampaignState.Campaign -> ShowAllOrCreateCampaignFooter(
-                    onShowAllClicked = state.onViewAllCampaignsClicked,
-                    onCreateCampaignClicked = state.onCreateCampaignClicked
-                )
-
-                is MyStoreBlazeCampaignState.NoCampaign -> CreateCampaignFooter(
-                    onCreateCampaignClicked = state.onCreateCampaignClicked,
-                    modifier = Modifier.padding(top = dimensionResource(id = R.dimen.major_100))
-                )
-
-                else -> error("Invalid state")
-            }
+            WcOverflowMenu(
+                items = arrayOf("Dismiss"),
+                onSelected = { /*TODO*/ },
+            )
         }
     }
 }
@@ -191,6 +212,50 @@ fun BlazeProductItem(
                 painter = painterResource(id = R.drawable.ic_arrow_right),
                 contentDescription = null
             )
+        }
+    }
+}
+
+@Composable
+private fun <T> BoxScope.WcOverflowMenu(
+    items: Array<T>,
+    onSelected: (T) -> Unit,
+    mapper: (T) -> String = { it.toString() },
+) {
+    var showMenu by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .align(Alignment.TopEnd)
+            .padding(top = dimensionResource(id = dimen.minor_100))
+    ) {
+        IconButton(onClick = { showMenu = !showMenu }) {
+            Icon(
+                imageVector = Outlined.MoreVert,
+                contentDescription = stringResource(string.more_menu),
+            )
+        }
+        DropdownMenu(
+            offset = DpOffset(
+                x = dimensionResource(id = dimen.major_100),
+                y = 0.dp
+            ),
+            expanded = showMenu,
+            onDismissRequest = { showMenu = false }
+        ) {
+            items.forEachIndexed { index, item ->
+                DropdownMenuItem(
+                    modifier = Modifier.height(dimensionResource(id = dimen.major_175)),
+                    onClick = {
+                        showMenu = false
+                        onSelected(item)
+                    }
+                ) {
+                    Text(mapper(item))
+                }
+                if (index < items.size - 1) {
+                    Spacer(modifier = Modifier.height(dimensionResource(id = dimen.minor_100)))
+                }
+            }
         }
     }
 }
