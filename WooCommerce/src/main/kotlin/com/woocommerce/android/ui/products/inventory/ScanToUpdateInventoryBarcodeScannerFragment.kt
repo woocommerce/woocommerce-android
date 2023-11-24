06529034114be75bc48db6d15dc301111f660877
samiuelson <samiuelson@gmail.com>
2023-11-24 14:59:10 +0100

    Implement business logic handling scanner's state machine

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/ScanToUpdateInventoryBarcodeScannerFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/ScanToUpdateInventoryBarcodeScannerFragment.kt
@@ -1,9 +1,43 @@
 package com.woocommerce.android.ui.products.inventory
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.barcodescanner.BarcodeScanningFragment
+import com.woocommerce.android.ui.base.UIMessageResolver
+import com.woocommerce.android.ui.orders.creation.CodeScannerStatus
+import com.woocommerce.android.ui.products.inventory.ScanToUpdateInventoryBarcodeScannerFragmentDirections.Companion.actionScanToUpdateInventoryBarcodeScannerFragmentToQuickInventoryUpdateBottomSheet
+import com.woocommerce.android.viewmodel.fixedHiltNavGraphViewModels
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ScanToUpdateInventoryBarcodeScannerFragment : BarcodeScanningFragment() {
+    private val viewModel: ScanToUpdateInventoryViewModel by fixedHiltNavGraphViewModels(R.id.nav_graph_main)
+    @Inject
+    lateinit var uiMessageResolver: UIMessageResolver
+
     override val isContinuousScanningEnabled = true
+
+    override fun onScannedResult(status: CodeScannerStatus) {
+        viewModel.onBarcodeScanningResult(status)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            viewModel.viewState.collect {
+                if (it is ScanToUpdateInventoryViewModel.ViewState.ProductLoaded) {
+                    actionScanToUpdateInventoryBarcodeScannerFragmentToQuickInventoryUpdateBottomSheet(
+                        it.product
+                    ).let {
+                        findNavController().navigate(it)
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/ScanToUpdateInventoryViewModel.kt
@@ -0,0 +1,103 @@
+package com.woocommerce.android.ui.products.inventory
+
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.R
+import com.woocommerce.android.model.Product
+import com.woocommerce.android.model.UiString
+import com.woocommerce.android.ui.orders.creation.CodeScannerStatus
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowUiStringSnackbar
+import com.woocommerce.android.viewmodel.ResourceProvider
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
+
+@HiltViewModel
+class ScanToUpdateInventoryViewModel @Inject constructor(
+    savedState: SavedStateHandle,
+    private val fetchProductBySKU: FetchProductBySKU,
+    private val resourceProvider: ResourceProvider,
+) : ScopedViewModel(savedState) {
+    private val _viewState: MutableStateFlow<ViewState> =
+        savedState.getStateFlow(this, ViewState.BarcodeScanning, "viewState")
+    val viewState: StateFlow<ViewState> = _viewState
+
+    fun onBarcodeScanningResult(status: CodeScannerStatus) {
+        if (viewState.value !is ViewState.BarcodeScanning) return
+
+        if (status is CodeScannerStatus.Success) {
+            _viewState.value = ViewState.ProductLoading
+            handleBarcodeScanningSuccess(status)
+        }
+    }
+
+    fun onBottomSheetDismissed() {
+        if (viewState.value is ViewState.ProductUpdating) return
+        _viewState.value = ViewState.BarcodeScanning
+    }
+
+    private fun handleBarcodeScanningSuccess(status: CodeScannerStatus.Success) = launch {
+        val productResult: Result<Product> = fetchProductBySKU(status.code, status.format)
+        if (productResult.isSuccess) {
+            val product = productResult.getOrNull()
+            if (product != null) {
+                val productInfo = ProductInfo(
+                    id = product.remoteId,
+                    name = product.name,
+                    imageUrl = product.firstImageUrl.orEmpty(),
+                    sku = product.sku,
+                    quantity = product.stockQuantity.toInt(),
+                )
+                _viewState.value = ViewState.ProductLoaded(productInfo)
+            } else {
+                handleProductNotFound(status.code)
+            }
+        } else {
+            handleProductNotFound(status.code)
+        }
+    }
+
+    private suspend fun handleProductNotFound(barcode: String) {
+        triggerProductNotFoundSnackBar(barcode)
+        delay(SCANNER_RESTART_DEBOUNCE_MS)
+        _viewState.value = ViewState.BarcodeScanning
+    }
+
+    private fun triggerProductNotFoundSnackBar(barcode: String) {
+        val message = resourceProvider.getString(R.string.scan_to_update_inventory_unable_to_find_product, barcode)
+        triggerEvent(ShowUiStringSnackbar(UiString.UiStringText(message)))
+    }
+
+    @Suppress("ForbiddenComment")
+    fun onIncrementQuantityClicked() {
+        // TODO: Implement actual logic
+        _viewState.value = ViewState.BarcodeScanning
+    }
+
+    @Parcelize
+    data class ProductInfo(
+        val id: Long,
+        val name: String,
+        val imageUrl: String,
+        val sku: String,
+        val quantity: Int,
+    ) : Parcelable
+
+    @Parcelize
+    sealed class ViewState : Parcelable {
+        object BarcodeScanning : ViewState()
+        object ProductLoading : ViewState()
+        data class ProductLoaded(val product: ProductInfo) : ViewState()
+        object ProductUpdating : ViewState()
+    }
+
+    companion object {
+        private const val SCANNER_RESTART_DEBOUNCE_MS = 1000L
+    }
+}
