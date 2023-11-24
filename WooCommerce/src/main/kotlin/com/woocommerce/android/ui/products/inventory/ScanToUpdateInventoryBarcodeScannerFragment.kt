d18b6dd33a4245b0cba23c13fde5dcb4baec12b4
samiuelson <samiuelson@gmail.com>
2023-11-24 15:51:59 +0100

    Implement product update logic and edge case handling

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/ScanToUpdateInventoryBarcodeScannerFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/ScanToUpdateInventoryBarcodeScannerFragment.kt
@@ -9,6 +9,7 @@ import com.woocommerce.android.ui.barcodescanner.BarcodeScanningFragment
 import com.woocommerce.android.ui.base.UIMessageResolver
 import com.woocommerce.android.ui.orders.creation.CodeScannerStatus
 import com.woocommerce.android.ui.products.inventory.ScanToUpdateInventoryBarcodeScannerFragmentDirections.Companion.actionScanToUpdateInventoryBarcodeScannerFragmentToQuickInventoryUpdateBottomSheet
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.fixedHiltNavGraphViewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -17,6 +18,7 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class ScanToUpdateInventoryBarcodeScannerFragment : BarcodeScanningFragment() {
     private val viewModel: ScanToUpdateInventoryViewModel by fixedHiltNavGraphViewModels(R.id.nav_graph_main)
+
     @Inject
     lateinit var uiMessageResolver: UIMessageResolver
 
@@ -36,6 +38,13 @@ class ScanToUpdateInventoryBarcodeScannerFragment : BarcodeScanningFragment() {
                     ).let {
                         findNavController().navigate(it)
                     }
+                }
+            }
+        }
+        viewModel.event.observe(viewLifecycleOwner) {
+            when (it) {
+                is MultiLiveEvent.Event.ShowUiStringSnackbar -> {
+                    uiMessageResolver.showSnack(it.message)
                 }
             }
         }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/ScanToUpdateInventoryViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/ScanToUpdateInventoryViewModel.kt
@@ -6,6 +6,7 @@ import com.woocommerce.android.R
 import com.woocommerce.android.model.Product
 import com.woocommerce.android.model.UiString
 import com.woocommerce.android.ui.orders.creation.CodeScannerStatus
+import com.woocommerce.android.ui.products.ProductDetailRepository
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowUiStringSnackbar
 import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
@@ -23,6 +24,7 @@ class ScanToUpdateInventoryViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val fetchProductBySKU: FetchProductBySKU,
     private val resourceProvider: ResourceProvider,
+    private val productRepository: ProductDetailRepository,
 ) : ScopedViewModel(savedState) {
     private val _viewState: MutableStateFlow<ViewState> =
         savedState.getStateFlow(this, ViewState.BarcodeScanning, "viewState")
@@ -39,10 +41,12 @@ class ScanToUpdateInventoryViewModel @Inject constructor(
 
     fun onBottomSheetDismissed() {
         if (viewState.value is ViewState.ProductUpdating) return
+
         _viewState.value = ViewState.BarcodeScanning
     }
 
     private fun handleBarcodeScanningSuccess(status: CodeScannerStatus.Success) = launch {
+        triggerEvent(ShowUiStringSnackbar(UiString.UiStringRes(R.string.scan_to_update_inventory_loading_product)))
         val productResult: Result<Product> = fetchProductBySKU(status.code, status.format)
         if (productResult.isSuccess) {
             val product = productResult.getOrNull()
@@ -54,13 +58,31 @@ class ScanToUpdateInventoryViewModel @Inject constructor(
                     sku = product.sku,
                     quantity = product.stockQuantity.toInt(),
                 )
-                _viewState.value = ViewState.ProductLoaded(productInfo)
+                if (product.isStockManaged) {
+                    _viewState.value = ViewState.ProductLoaded(productInfo)
+                } else {
+                    handleProductIsNotStockManaged(product)
+                }
             } else {
                 handleProductNotFound(status.code)
             }
         } else {
             handleProductNotFound(status.code)
         }
+    }
+
+    private suspend fun handleProductIsNotStockManaged(product: Product) {
+        triggerProductNotStockManagedSnackBar(product)
+        delay(SCANNER_RESTART_DEBOUNCE_MS)
+        _viewState.value = ViewState.BarcodeScanning
+    }
+
+    private fun triggerProductNotStockManagedSnackBar(product: Product) {
+        val message = resourceProvider.getString(
+            R.string.scan_to_update_inventory_product_not_stock_managed,
+            product.sku
+        )
+        triggerEvent(ShowUiStringSnackbar(UiString.UiStringText(message)))
     }
 
     private suspend fun handleProductNotFound(barcode: String) {
@@ -74,9 +96,41 @@ class ScanToUpdateInventoryViewModel @Inject constructor(
         triggerEvent(ShowUiStringSnackbar(UiString.UiStringText(message)))
     }
 
-    @Suppress("ForbiddenComment")
     fun onIncrementQuantityClicked() {
-        // TODO: Implement actual logic
+        val state = viewState.value
+        if (state !is ViewState.ProductLoaded) return
+        val product = productRepository.getProduct(state.product.id)
+        _viewState.value = ViewState.ProductUpdating
+        if (product == null) {
+            handleQuantityUpdateError()
+        } else {
+            launch {
+                val updatedProduct = product.copy(stockQuantity = product.stockQuantity + 1)
+                val result = productRepository.updateProduct(updatedProduct)
+                if (result) {
+                    handleQuantityUpdateSuccess(product, updatedProduct)
+                } else {
+                    handleQuantityUpdateError()
+                }
+            }
+        }
+    }
+
+    private suspend fun handleQuantityUpdateSuccess(oldProduct: Product, updatedProduct: Product) {
+        val oldQuantity = oldProduct.stockQuantity.toInt()
+        val newQuantity = updatedProduct.stockQuantity.toInt()
+        val quantityChangeString = "$oldQuantity ➡ $newQuantity"
+        val message = resourceProvider.getString(
+            R.string.scan_to_update_inventory_success_snackbar,
+            quantityChangeString
+        )
+        triggerEvent(ShowUiStringSnackbar(UiString.UiStringText(message, true)))
+        delay(SCANNER_RESTART_DEBOUNCE_MS)
+        _viewState.value = ViewState.BarcodeScanning
+    }
+
+    private fun handleQuantityUpdateError() {
+        triggerEvent(ShowUiStringSnackbar(UiString.UiStringRes(R.string.scan_to_update_inventory_failure_snackbar)))
         _viewState.value = ViewState.BarcodeScanning
     }
 
