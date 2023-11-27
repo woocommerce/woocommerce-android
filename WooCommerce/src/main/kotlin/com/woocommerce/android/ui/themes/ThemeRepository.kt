c7938af7b1839b7266d6232b1cb83de3236cb4c0
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-11-27 11:01:24 +0100

    Add logic for installing and activating theme

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/themes/ThemeRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/themes/ThemeRepository.kt
@@ -3,20 +3,26 @@ package com.woocommerce.android.ui.themes
 import com.woocommerce.android.OnChangedException
 import com.woocommerce.android.model.Theme
 import com.woocommerce.android.model.toAppModel
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.dispatchAndAwait
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.ThemeActionBuilder
+import org.wordpress.android.fluxc.model.ThemeModel
 import org.wordpress.android.fluxc.store.ThemeStore
 import org.wordpress.android.fluxc.store.ThemeStore.FetchWPComThemesPayload
+import org.wordpress.android.fluxc.store.ThemeStore.OnThemeInstalled
 import org.wordpress.android.fluxc.store.ThemeStore.OnWpComThemesChanged
+import org.wordpress.android.fluxc.store.ThemeStore.SiteThemePayload
+import org.wordpress.android.fluxc.store.ThemeStore.ThemeErrorType
 import javax.inject.Inject
 
 class ThemeRepository @Inject constructor(
     private val themeStore: ThemeStore,
-    private val dispatcher: Dispatcher
+    private val dispatcher: Dispatcher,
+    private val selectedSite: SelectedSite
 ) {
     companion object {
         private const val STORE_THEMES_FILTER = "subject:store"
@@ -53,5 +59,64 @@ class ThemeRepository @Inject constructor(
 
     suspend fun getTheme(themeId: String): Theme? = withContext(Dispatchers.IO) {
         themeStore.getWpComThemeByThemeId(themeId)?.toAppModel()
+    }
+
+    suspend fun activateTheme(theme: Theme): Result<Unit> {
+        installThemeIfNeeded(theme).onFailure { return Result.failure(it) }
+
+        val activationResult: OnThemeInstalled = dispatcher.dispatchAndAwait(
+            ThemeActionBuilder.newActivateThemeAction(
+                SiteThemePayload(selectedSite.get(), theme.toThemeModel())
+            )
+        )
+
+        return when {
+            !activationResult.isError -> {
+                WooLog.d(WooLog.T.THEMES, "Theme activated successfully: ${theme.id}")
+                Result.success(Unit)
+            }
+
+            else -> {
+                with(activationResult.error) {
+                    WooLog.w(WooLog.T.THEMES, "Error activating theme: ${type}, $message")
+                }
+                Result.failure(OnChangedException(activationResult.error))
+            }
+        }
+    }
+
+    private suspend fun installThemeIfNeeded(theme: Theme): Result<Unit> {
+        val installationResult: OnThemeInstalled = dispatcher.dispatchAndAwait(
+            ThemeActionBuilder.newInstallThemeAction(
+                SiteThemePayload(selectedSite.get(), theme.toThemeModel())
+            )
+        )
+
+        return when {
+            !installationResult.isError || installationResult.error.type == ThemeErrorType.THEME_ALREADY_INSTALLED -> {
+                if (installationResult.isError) {
+                    WooLog.w(WooLog.T.THEMES, "Theme already installed: ${theme.id}")
+                } else {
+                    WooLog.d(WooLog.T.THEMES, "Theme installed successfully: ${theme.id}")
+                }
+
+                Result.success(Unit)
+            }
+
+            else -> {
+                with(installationResult.error) {
+                    WooLog.w(WooLog.T.THEMES, "Error installing theme: ${type}, $message")
+                }
+                Result.failure(OnChangedException(installationResult.error))
+            }
+        }
+    }
+
+    private fun Theme.toThemeModel(): ThemeModel {
+        return ThemeModel().apply {
+            themeId = this@toThemeModel.id
+            name = this@toThemeModel.name
+            demoUrl = this@toThemeModel.demoUrl
+        }
     }
 }
