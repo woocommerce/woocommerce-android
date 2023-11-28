e82798677e62518801e3783c366860dd94a7ff84
ThomazFB <thomazfcortez@gmail.com>
2023-11-27 22:50:20 -0300

    Set gift card initial value as the one provided by the navigation args

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
+import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -12,9 +13,11 @@ import javax.inject.Inject
 class OrderCreateEditGiftCardViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
+    private val navArgs: OrderCreateEditGiftCardFragmentArgs by savedState.navArgs()
+
     private val _giftCard = savedState.getStateFlow(
         scope = viewModelScope,
-        initialValue = ""
+        initialValue = navArgs.giftCard.orEmpty()
     )
     val giftCard = _giftCard.asLiveData()
 
