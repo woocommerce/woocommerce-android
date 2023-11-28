51e20ec197c717f07bd10b7dcbb186c0dc9fcfa7
ThomazFB <thomazfcortez@gmail.com>
2023-11-27 22:06:30 -0300

    Map expected database entity gift card field

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/OrderMapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/OrderMapper.kt
@@ -59,7 +59,7 @@ class OrderMapper @Inject constructor(private val getLocations: GetLocations) {
             shippingPhone = metaDataList.getOrEmpty(SHIPPING_PHONE_KEY),
             paymentUrl = databaseEntity.paymentUrl,
             isEditable = databaseEntity.isEditable,
-            giftCard = "" //TODO: use actual value from databaseEntity
+            giftCard = databaseEntity.giftCard
         )
     }
 
