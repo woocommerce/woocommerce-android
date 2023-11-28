92ecc98729cd12056784ff516dfb451f4b69f439
ThomazFB <thomazfcortez@gmail.com>
2023-11-27 22:53:33 -0300

    Define OrderCreateEditGiftCardViewModelTest initial test structure

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.woocommerce.android.ui.orders.creation.giftcards
+
+import androidx.lifecycle.SavedStateHandle
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+class OrderCreateEditGiftCardViewModelTest {
+    private lateinit var sut: OrderCreateEditGiftCardViewModel
+
+    @Before
+    fun setUp() {
+        val savedState: SavedStateHandle = mock()
+        sut = OrderCreateEditGiftCardViewModel(savedState)
+    }
+
+    @Test
+    fun `onGiftCardChanged should update giftCard`() {
+        // Given
+        val expectedGiftCardCode = "test gift card code"
+        var lastGiftCardUpdate: String? = null
+        sut.giftCard.observeForever { lastGiftCardUpdate = it }
+
+        // When
+        sut.onGiftCardChanged("test gift card code")
+
+        // Then
+        assertThat(lastGiftCardUpdate).isEqualTo(expectedGiftCardCode)
+    }
+}
