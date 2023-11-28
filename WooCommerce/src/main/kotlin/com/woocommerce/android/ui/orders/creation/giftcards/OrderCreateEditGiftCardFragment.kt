76a2f95f254c5aed3d698db43208fdcd97b572da
ThomazFB <thomazfcortez@gmail.com>
2023-11-27 22:48:36 -0300

    Initialize viewModel inside OrderCreateEditGiftCardFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 class OrderCreateEditGiftCardFragment : BaseFragment() {
+    val viewModel: OrderCreateEditGiftCardViewModel by viewModels()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
