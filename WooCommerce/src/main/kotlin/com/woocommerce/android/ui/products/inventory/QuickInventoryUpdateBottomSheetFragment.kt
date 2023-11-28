ba4030202f05e3259b515e431d41d19b7fd796c5
samiuelson <samiuelson@gmail.com>
2023-11-28 08:59:05 +0100

    Fix bottom sheet behavior with keyboard

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/QuickInventoryUpdateBottomSheetFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/QuickInventoryUpdateBottomSheetFragment.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.products.inventory
 
+import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.theme.WooTheme
 import com.woocommerce.android.viewmodel.fixedHiltNavGraphViewModels
@@ -16,6 +20,20 @@ import com.woocommerce.android.widgets.WCBottomSheetDialogFragment
 class QuickInventoryUpdateBottomSheetFragment : WCBottomSheetDialogFragment() {
     private val viewModel: ScanToUpdateInventoryViewModel by fixedHiltNavGraphViewModels(R.id.nav_graph_main)
     private val args: QuickInventoryUpdateBottomSheetFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.Woo_Theme_BottomSheetDialog_WithKeyboard)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            if (this is BottomSheetDialog) {
+                behavior.skipCollapsed = true
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
