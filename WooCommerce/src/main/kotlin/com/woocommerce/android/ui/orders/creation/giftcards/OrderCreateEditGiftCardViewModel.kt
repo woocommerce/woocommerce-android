d29b9d5df54165e4bff63160a184122e317e99d4
ThomazFB <thomazfcortez@gmail.com>
2023-11-27 22:18:03 -0300

    Define OrderCreateEditGiftCardViewModel

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardViewModel.kt
@@ -0,0 +1,20 @@
+package com.woocommerce.android.ui.orders.creation.giftcards
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderCreateEditGiftCardViewModel @Inject constructor(
+    savedState: SavedStateHandle
+) : ScopedViewModel(savedState) {
+    private val _giftCard = savedState.getStateFlow(
+        scope = viewModelScope,
+        initialValue = ""
+    )
+    val giftCard = _giftCard.asLiveData()
+}
