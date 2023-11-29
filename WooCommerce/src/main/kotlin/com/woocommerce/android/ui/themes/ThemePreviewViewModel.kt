8e682d49097cceb6c5f07ec1c66f51d87db1a9fb
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-11-30 00:20:18 +0100

    Fetch demo pages from server

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/themes/ThemePreviewViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/themes/ThemePreviewViewModel.kt
@@ -10,8 +10,10 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import org.wordpress.android.fluxc.network.UserAgent
+import org.wordpress.android.fluxc.store.ThemeCoroutineStore
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,13 +21,31 @@ class ThemePreviewViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     val wpComWebViewAuthenticator: WPComWebViewAuthenticator,
     val userAgent: UserAgent,
+    val themeCoroutineStore: ThemeCoroutineStore,
 ) : ScopedViewModel(savedStateHandle) {
     private val navArgs: ThemePreviewFragmentArgs by savedStateHandle.navArgs()
     private val _viewState = savedStateHandle.getStateFlow(
         viewModelScope,
-        ViewState(demoUri = navArgs.themeDemoUri)
+        ViewState(
+            demoUri = navArgs.themeDemoUri,
+            themePages = emptyList()
+        )
     )
     val viewState = _viewState.asLiveData()
+
+    init {
+        launch {
+            val themePages = themeCoroutineStore.fetchDemoThemePages(navArgs.themeDemoUri)
+            _viewState.value = _viewState.value.copy(
+                themePages = themePages.map {
+                    ThemeDemoPage(
+                        uri = it.link,
+                        title = it.title
+                    )
+                }
+            )
+        }
+    }
 
     fun onPageSelected(updatedDemoUri: String) {
         _viewState.value = _viewState.value.copy(demoUri = updatedDemoUri)
@@ -38,5 +58,12 @@ class ThemePreviewViewModel @Inject constructor(
     @Parcelize
     data class ViewState(
         val demoUri: String,
+        val themePages: List<ThemeDemoPage>
+    ) : Parcelable
+
+    @Parcelize
+    data class ThemeDemoPage(
+        val uri: String,
+        val title: String
     ) : Parcelable
 }
