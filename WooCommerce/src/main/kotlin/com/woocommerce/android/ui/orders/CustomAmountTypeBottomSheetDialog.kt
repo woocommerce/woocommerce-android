b72b57f28bb4d368a670f1355b5a3658de06711b
AnirudhBhat <abhat38@gmail.com>
2023-11-30 08:16:53 +0530

    Bottom sheet dialog fragment which houses the compose view

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/CustomAmountTypeBottomSheetDialog.kt
@@ -0,0 +1,29 @@
+package com.woocommerce.android.ui.orders
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.widgets.WCBottomSheetDialogFragment
+import javax.annotation.Nullable
+
+
+class CustomAmountTypeBottomSheetDialog : WCBottomSheetDialogFragment() {
+//    private val sharedViewModel: OrderCreateEditViewModel by fixedHiltNavGraphViewModels(R.id.nav_graph_order_creations)
+    override fun onCreateView(
+        inflater: LayoutInflater, @Nullable container: ViewGroup?,
+        @Nullable savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WooThemeWithBackground {
+                    CustomAmountTypeBottomSheet()
+                }
+            }
+        }
+    }
+}
