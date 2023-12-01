b1a6722cd1f3f6fce45c58eef38438fc8cb448e0
ThomazFB <thomazfcortez@gmail.com>
2023-12-01 12:30:57 -0300

    Bind viewModel state to Gift card screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/EditGiftCardScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/EditGiftCardScreen.kt
@@ -6,22 +6,25 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.woocommerce.android.R
 
 @Composable
 fun EditGiftCardScreen(viewModel: OrderCreateEditGiftCardViewModel) {
-    EditGiftCardScreen(viewModel::onGiftCardChanged)
+    val giftCardValue by viewModel.giftCard.observeAsState()
+    EditGiftCardScreen(giftCardValue.orEmpty(), viewModel::onGiftCardChanged)
 }
 
 @Composable
-fun EditGiftCardScreen(onTextChanged: (String) -> Unit) {
+fun EditGiftCardScreen(giftCardValue: String, onTextChanged: (String) -> Unit) {
     TextField(
-        value = stringResource(id = R.string.gift_card_default_value),
+        value = giftCardValue,
         onValueChange = onTextChanged,
         placeholder = {
-            Text(stringResource(id = R.string.gift_card_hint_value))
+            Text(stringResource(id = R.string.order_creation_gift_card_text_field_hint))
         },
         colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
         modifier = Modifier.fillMaxSize()
