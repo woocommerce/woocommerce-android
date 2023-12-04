1f5bedae80d394d4c72bb633d612a3bd0650051f
Andrey <akidinov@gmail.com>
2023-12-04 12:28:45 +0100

    Select all on focus

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/views/ExpandableProductCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/views/ExpandableProductCard.kt
@@ -15,14 +15,19 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -34,6 +39,7 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -50,6 +56,10 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -332,8 +342,7 @@ fun ExtendedProductCardContent(
                 onIncreaseClicked = onIncreaseItemAmountClicked,
                 onDecreaseClicked = onDecreaseItemAmountClicked,
                 product = product,
-                isDecreaseButtonEnabled = areAmountButtonsEnabled,
-                isIncreaseButtonEnabled = areAmountButtonsEnabled
+                isAmountChangeable = areAmountButtonsEnabled,
             )
         }
         Row(
@@ -506,22 +515,36 @@ private fun AmountPicker(
     onIncreaseClicked: () -> Unit,
     onDecreaseClicked: () -> Unit,
     product: OrderCreationProduct,
-    isDecreaseButtonEnabled: Boolean = true,
-    isIncreaseButtonEnabled: Boolean = true
+    isAmountChangeable: Boolean = true,
 ) {
+    var textFieldValue by remember { mutableStateOf(TextFieldValue(product.item.quantity.toInt().toString())) }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    LaunchedEffect(isFocused) {
+        val endRange = if (isFocused) textFieldValue.text.length else 0
+        textFieldValue = textFieldValue.copy(
+            selection = TextRange(
+                start = 0,
+                end = endRange
+            )
+        )
+    }
+
     Row(
         modifier = modifier
             .border(
                 1.dp,
                 colorResource(id = R.color.divider_color),
                 shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_large))
-            ),
+            )
+            .wrapContentWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_100))
     ) {
-        val decreaseButtonTint = if (isDecreaseButtonEnabled) MaterialTheme.colors.primary else Color.Gray
-        val increaseButtonTint = if (isIncreaseButtonEnabled) MaterialTheme.colors.primary else Color.Gray
-        IconButton(onClick = onDecreaseClicked, enabled = isDecreaseButtonEnabled) {
+        val decreaseButtonTint = if (isAmountChangeable) MaterialTheme.colors.primary else Color.Gray
+        val increaseButtonTint = if (isAmountChangeable) MaterialTheme.colors.primary else Color.Gray
+        IconButton(onClick = onDecreaseClicked, enabled = isAmountChangeable) {
             Icon(
                 imageVector = Icons.Filled.Remove,
                 contentDescription =
@@ -529,8 +552,22 @@ private fun AmountPicker(
                 tint = decreaseButtonTint
             )
         }
-        Text(text = product.item.quantity.toInt().toString(), color = MaterialTheme.colors.onSurface)
-        IconButton(onClick = onIncreaseClicked, enabled = isIncreaseButtonEnabled) {
+        BasicTextField(
+            value = textFieldValue,
+            onValueChange = {},
+            readOnly = !isAmountChangeable,
+            singleLine = true,
+            textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onSurface),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            interactionSource = interactionSource,
+            modifier = Modifier
+                .padding(horizontal = dimensionResource(id = R.dimen.minor_25))
+                .width(IntrinsicSize.Min)
+        )
+        IconButton(onClick = onIncreaseClicked, enabled = isAmountChangeable) {
             Icon(
                 imageVector = Icons.Filled.Add,
                 contentDescription =
