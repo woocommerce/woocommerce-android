3b65a046d80957d43b556249b3527976c765ba6a
Andrey <akidinov@gmail.com>
2023-12-04 10:43:26 +0100

    Fixed issue when recognition stopped after onResume

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanner.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/barcodescanner/BarcodeScanner.kt
@@ -8,9 +8,9 @@ import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
 import androidx.camera.core.ImageProxy
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -18,6 +18,8 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import androidx.camera.core.Preview as CameraPreview
 
@@ -29,35 +31,54 @@ fun BarcodeScanner(
 ) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
+
     val cameraProviderFuture = remember {
         ProcessCameraProvider.getInstance(context)
     }
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        AndroidView(
-            factory = { context ->
-                val previewView = PreviewView(context)
-                val preview = CameraPreview.Builder().build()
-                preview.setSurfaceProvider(previewView.surfaceProvider)
-                val selector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-                val imageAnalysis = ImageAnalysis.Builder().setTargetResolution(
-                    Size(
-                        previewView.width,
-                        previewView.height
-                    )
-                ).setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST).build()
-                imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context), onNewFrame)
+    val previewView = remember { PreviewView(context) }
+    val cameraPreview = remember {
+        CameraPreview.Builder().build().apply {
+            setSurfaceProvider(previewView.surfaceProvider)
+        }
+    }
+    val selector = remember {
+        CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+    }
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME) {
                 try {
-                    cameraProviderFuture.get().bindToLifecycle(lifecycleOwner, selector, preview, imageAnalysis)
+                    val cameraProvider = cameraProviderFuture.get()
+                    val imageAnalysisUseCase = ImageAnalysis.Builder()
+                        .setTargetResolution(
+                            Size(
+                                previewView.width,
+                                previewView.height
+                            )
+                        ).setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+                        .build()
+                        .apply {
+                            setAnalyzer(ContextCompat.getMainExecutor(context), onNewFrame)
+                        }
+
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(lifecycleOwner, selector, cameraPreview, imageAnalysisUseCase)
                 } catch (e: Exception) {
                     onBindingException(e)
                 }
-                previewView
-            },
-            modifier = Modifier.fillMaxSize()
-        )
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
     }
+
+    AndroidView(
+        factory = { previewView },
+        modifier = Modifier.fillMaxSize()
+    )
 }
 
 @Preview(name = "Light mode")
