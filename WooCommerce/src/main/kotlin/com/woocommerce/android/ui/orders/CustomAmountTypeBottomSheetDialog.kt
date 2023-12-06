918b3fcbe1953872f3c9e277b41ce64ed6c49c34
AnirudhBhat <abhat38@gmail.com>
2023-12-06 10:56:20 +0530

    Remove unnecessary @Nullable annotation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/CustomAmountTypeBottomSheetDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/CustomAmountTypeBottomSheetDialog.kt
@@ -12,14 +12,13 @@ import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.orders.creation.OrderCreateEditViewModel
 import com.woocommerce.android.viewmodel.fixedHiltNavGraphViewModels
 import com.woocommerce.android.widgets.WCBottomSheetDialogFragment
-import javax.annotation.Nullable
 
 class CustomAmountTypeBottomSheetDialog : WCBottomSheetDialogFragment() {
     private val sharedViewModel: OrderCreateEditViewModel by fixedHiltNavGraphViewModels(R.id.nav_graph_order_creations)
     override fun onCreateView(
         inflater: LayoutInflater,
-        @Nullable container: ViewGroup?,
-        @Nullable savedInstanceState: Bundle?
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
