8128218488e75538ab3de621fda96a58f81cdf3d
samiuelson <samiuelson@gmail.com>
2023-12-06 17:14:44 +0100

    Clean up code

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/QuickInventoryUpdateBottomSheet.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/QuickInventoryUpdateBottomSheet.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -118,7 +117,6 @@ fun QuickInventoryUpdateBottomSheet(
                             horizontal = dimensionResource(id = R.dimen.minor_100),
                             vertical = dimensionResource(id = R.dimen.minor_50),
                         )
-                        .widthIn(min = 0.dp, max = 128.dp)
                         .width(IntrinsicSize.Min),
                     value = state.newQuantity,
                     onValueChange = { value ->
