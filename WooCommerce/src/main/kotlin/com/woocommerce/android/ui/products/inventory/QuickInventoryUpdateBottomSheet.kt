5c7b7f4f5051583972362b58caeee622f61e4c5d
samiuelson <samiuelson@gmail.com>
2023-12-07 12:21:28 +0100

    Remove unused import

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/QuickInventoryUpdateBottomSheet.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/QuickInventoryUpdateBottomSheet.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
