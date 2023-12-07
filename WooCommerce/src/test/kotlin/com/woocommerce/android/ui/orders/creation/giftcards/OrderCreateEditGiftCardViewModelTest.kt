45b2779570a480aad97e86f624cd5d90cef740f4
ThomazFB <thomazfcortez@gmail.com>
2023-12-07 13:28:34 -0300

    Add test coverage for ExitWithResult event

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/creation/giftcards/OrderCreateEditGiftCardViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.woocommerce.android.ui.orders.creation.giftcards
 
 import com.woocommerce.android.initSavedStateHandle
+import com.woocommerce.android.ui.orders.creation.giftcards.OrderCreateEditGiftCardViewModel.GiftCardResult
 import com.woocommerce.android.viewmodel.BaseUnitTest
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
@@ -51,6 +53,25 @@ class OrderCreateEditGiftCardViewModelTest : BaseUnitTest() {
 
         // Then
         assertThat(lastGiftCardUpdate).isEqualTo("")
+    }
+
+    @Test
+    fun `when onDoneButtonClicked is triggered, then expected ExitWithResult event should be triggered`() {
+        // Given
+        var lastEvent: MultiLiveEvent.Event.ExitWithResult<*>? = null
+        sut.event.observeForever {
+            lastEvent = it as? MultiLiveEvent.Event.ExitWithResult<*>
+        }
+
+        // When
+        sut.onGiftCardChanged("test-gift-card-code")
+        sut.onDoneButtonClicked()
+
+        // Then
+        assertThat(lastEvent).isNotNull
+        assertThat(lastEvent?.data).isInstanceOf(GiftCardResult::class.java)
+        val giftCardResult = lastEvent?.data as GiftCardResult
+        assertThat(giftCardResult.selectedGiftCard).isEqualTo("test-gift-card-code")
     }
 
     private fun createSutWith(initialGiftCardValue: String?) {
