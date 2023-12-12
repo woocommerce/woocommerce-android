6f094b6aa5ed51e7e5e37a35d8b0121e612bdbe2
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-12-12 08:16:49 +0100

    Add preview for survey bottomsheet

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/AIProductCreationSurveyBottomSheet.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/AIProductCreationSurveyBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.products.ai
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,62 +12,90 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.fragment.findNavController
-import com.woocommerce.android.R
+import com.woocommerce.android.R.color
+import com.woocommerce.android.R.dimen
+import com.woocommerce.android.R.drawable
+import com.woocommerce.android.R.string
 import com.woocommerce.android.ui.compose.component.WCColoredButton
 import com.woocommerce.android.ui.compose.component.WCOutlinedButton
 import com.woocommerce.android.ui.compose.composeView
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.widgets.WCBottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
-import java.lang.reflect.Modifier
 
 @AndroidEntryPoint
 class AIProductCreationSurveyBottomSheet : WCBottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return composeView {
             WooThemeWithBackground {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(dimensionResource(id = R.dimen.major_100)),
-                    verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.major_100)),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ai_product_creation_survey_icon),
-                        contentDescription = "", // No relevant content desc
-                    )
-                    Text(
-                        text = stringResource(id = R.string.blaze_campaign_creation_intro_title),
-                        style = MaterialTheme.typography.subtitle1
-                    )
-                    Text(
-                        text = stringResource(id = R.string.blaze_campaign_creation_intro_title),
-                        style = MaterialTheme.typography.body2,
-                        color = colorResource(id = R.color.color_on_surface_medium_selector),
-                    )
-                    WCColoredButton(
-                        onClick = { /*TODO*/ },
-                        text = stringResource(id = R.string.product_creation_survey_button_open)
-                    )
-                    WCOutlinedButton(
-                        onClick = { findNavController().popBackStack() },
-                        text = stringResource(id = R.string.product_creation_survey_button_skip)
-                    )
-                }
+                SurveyBottomSheetContent(
+                    onStartSurveyClick = {},
+                    onSkipPressed = { findNavController().popBackStack() },
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
+}
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        observeEvents()
-//    }
+@Composable
+private fun SurveyBottomSheetContent(
+    onStartSurveyClick: () -> Unit,
+    onSkipPressed: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(
+            start = dimensionResource(id = dimen.major_100),
+            end = dimensionResource(id = dimen.major_100),
+            top = dimensionResource(id = dimen.major_250),
+            bottom = dimensionResource(id = dimen.major_250),
+        ),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = dimen.major_100)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = drawable.ai_product_creation_survey_icon),
+            contentDescription = "", // No relevant content desc
+        )
+        Text(
+            text = stringResource(id = string.blaze_campaign_creation_intro_title),
+            style = MaterialTheme.typography.subtitle1
+        )
+        Text(
+            text = stringResource(id = string.blaze_campaign_creation_intro_title),
+            style = MaterialTheme.typography.body2,
+            color = colorResource(id = color.color_on_surface_medium_selector),
+        )
+        WCColoredButton(
+            onClick = onStartSurveyClick,
+            text = stringResource(id = string.product_creation_survey_button_open)
+        )
+        WCOutlinedButton(
+            onClick = onSkipPressed,
+            text = stringResource(id = string.product_creation_survey_button_skip)
+        )
+    }
+}
+
+@Composable
+@Preview(name = "light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+private fun BlazeCreationIntroPreview() {
+    WooThemeWithBackground {
+        SurveyBottomSheetContent(
+            onStartSurveyClick = {},
+            onSkipPressed = {},
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
