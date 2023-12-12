b811ac57bc2fb2fc44dac150f3894a09b01a4bcf
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-12-12 07:52:29 -0500

    Add StockManagementBottomSheet file and layout

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/StockManagementBottomSheet.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/StockManagementBottomSheet.kt
@@ -1,7 +1,11 @@
 package com.woocommerce.android.ui.products.inventory
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -18,18 +22,24 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.component.ProductThumbnail
+import com.woocommerce.android.ui.compose.component.WCOutlinedButton
+import com.woocommerce.android.ui.compose.component.WCTextButton
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
 fun StockManagementBottomSheet(
-    state: ScanToUpdateInventoryViewModel.ViewState.QuickInventoryBottomSheetVisible
+    state: ScanToUpdateInventoryViewModel.ViewState.QuickInventoryBottomSheetVisible,
+    onManageStockClicked: () -> Unit = {},
+    onViewProductDetailsClicked: () -> Unit = {},
 ) {
     Column(
         modifier = Modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
-    ){
+    ) {
         Text(
             modifier = Modifier.padding(dimensionResource(id = R.dimen.major_100)),
             text = stringResource(id = R.string.scan_to_update_inventory_product_label),
@@ -60,6 +70,50 @@ fun StockManagementBottomSheet(
             text = state.product.sku
         )
         Divider()
+        Row(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.major_100)),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.scan_to_update_inventory_stock_not_managed)
+            )
+        }
+        Divider()
+        WCTextButton(onClick = onManageStockClicked) {
+            Text(text = stringResource(R.string.scan_to_update_inventory_manage_stock))
+        }
+        WCOutlinedButton(
+            onClick = onViewProductDetailsClicked,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = dimensionResource(id = R.dimen.major_100),
+                    end = dimensionResource(id = R.dimen.major_100),
+                    top = dimensionResource(id = R.dimen.major_350),
+                    bottom = dimensionResource(id = R.dimen.major_200),
+                ),
+        ) {
+            Text(
+                text = stringResource(R.string.scan_to_update_inventory_product_details_button)
+            )
+        }
+    }
+}
 
+@Preview(name = "Light mode")
+@Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun StockManagementBottomSheetPreview() {
+    val product = ScanToUpdateInventoryViewModel.ProductInfo(
+        id = 12,
+        name = "Product Name",
+        imageUrl = "https://woocommerce.com/wp-content/uploads/2017/03/woocommerce-logo.png",
+        sku = "123-SKU-456",
+        quantity = 10,
+    )
+    val state = ScanToUpdateInventoryViewModel.ViewState.QuickInventoryBottomSheetVisible(product)
+    WooThemeWithBackground {
+        StockManagementBottomSheet(state)
     }
 }
