ff2e1c6d4319cd4cb4b8e12fb9ba91e065061c94
Andrey <akidinov@gmail.com>
2023-12-18 17:21:30 +0100

    Ui adjustements

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/totals/OrderCreateEditTotalsView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/totals/OrderCreateEditTotalsView.kt
@@ -1,21 +1,21 @@
 package com.woocommerce.android.ui.orders.creation.totals
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,29 +27,34 @@ import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 @Composable
 fun OrderCreateEditTotalsView(state: TotalsSectionsState.Shown) {
     WooThemeWithBackground {
-        Column(
-            modifier = Modifier
-                .background(color = MaterialTheme.colors.surface)
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .shadow(1.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Card(
+            shape = RoundedCornerShape(0.dp),
+            elevation = 0.dp
         ) {
-            Icon(
-                imageVector = Icons.Default.KeyboardArrowUp,
-                contentDescription = null,
-                tint = colorResource(id = R.color.color_primary)
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            WCColoredButton(
-                onClick = { state.button.onClick },
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = dimensionResource(id = R.dimen.major_100)),
+                    .wrapContentHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = state.button.text)
+                Divider(modifier = Modifier.fillMaxWidth())
+
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = null,
+                    tint = colorResource(id = R.color.color_primary)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                WCColoredButton(
+                    onClick = { state.button.onClick },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = dimensionResource(id = R.dimen.major_100)),
+                ) {
+                    Text(text = state.button.text)
+                }
             }
         }
     }
