776eaffad07860eca16072fcb1959d72cbf06530
Ondrej Ruttkay <ruttkay@gmail.com>
2023-12-19 23:45:22 +0100

    Add the reload logic

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/themes/ThemePickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/themes/ThemePickerViewModel.kt
@@ -18,12 +18,10 @@ import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -38,12 +36,9 @@ class ThemePickerViewModel @Inject constructor(
     private val navArgs: ThemePickerFragmentArgs by savedStateHandle.navArgs()
 
     private val currentTheme = MutableStateFlow<CurrentThemeState>(CurrentThemeState.Hidden)
+    private val carouselState = MutableStateFlow<CarouselState>(CarouselState.Loading)
     val viewState = combine(
-        loadThemes().stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Lazily,
-            initialValue = CarouselState.Loading
-        ),
+        carouselState,
         currentTheme
     ) { carouselState, currentThemeState ->
         val updatedCarouseState = removeCurrentThemeFromCarouselItems(carouselState, currentThemeState)
@@ -55,7 +50,7 @@ class ThemePickerViewModel @Inject constructor(
     }.asLiveData()
 
     init {
-        loadCurrentTheme()
+        loadData()
         analyticsTrackerWrapper.track(
             stat = AnalyticsEvent.THEME_PICKER_SCREEN_DISPLAYED,
             properties = mapOf(
@@ -67,39 +62,41 @@ class ThemePickerViewModel @Inject constructor(
         )
     }
 
-    private fun loadThemes(): Flow<CarouselState> = flow {
-        emit(CarouselState.Loading)
-        val result = themeRepository.fetchThemes().fold(
-            onSuccess = { result ->
-                CarouselState.Success(
-                    carouselItems = result
-                        .filter { theme -> theme.demoUrl != null }
-                        .map { theme ->
-                            Theme(
-                                themeId = theme.id,
-                                name = theme.name,
-                                screenshotUrl = AppUrls.getScreenshotUrl(theme.demoUrl!!)
-                            )
-                        }
-                        .plus(
-                            Message(
-                                title = resourceProvider.getString(
-                                    R.string.theme_picker_carousel_info_item_title
-                                ),
-                                description = resourceProvider.getString(
-                                    resourceId = if (navArgs.isFromStoreCreation) {
-                                        R.string.theme_picker_carousel_info_item_description
-                                    } else {
-                                        R.string.theme_picker_carousel_info_item_description_settings
-                                    }
+    private fun loadThemes() {
+        viewModelScope.launch {
+            carouselState.update { CarouselState.Loading }
+            val result = themeRepository.fetchThemes().fold(
+                onSuccess = { result ->
+                    CarouselState.Success(
+                        carouselItems = result
+                            .filter { theme -> theme.demoUrl != null }
+                            .map { theme ->
+                                Theme(
+                                    themeId = theme.id,
+                                    name = theme.name,
+                                    screenshotUrl = AppUrls.getScreenshotUrl(theme.demoUrl!!)
+                                )
+                            }
+                            .plus(
+                                Message(
+                                    title = resourceProvider.getString(
+                                        R.string.theme_picker_carousel_info_item_title
+                                    ),
+                                    description = resourceProvider.getString(
+                                        resourceId = if (navArgs.isFromStoreCreation) {
+                                            R.string.theme_picker_carousel_info_item_description
+                                        } else {
+                                            R.string.theme_picker_carousel_info_item_description_settings
+                                        }
+                                    )
                                 )
                             )
-                        )
-                )
-            },
-            onFailure = { CarouselState.Error }
-        )
-        emit(result)
+                    )
+                },
+                onFailure = { CarouselState.Error }
+            )
+            carouselState.update { result }
+        }
     }
 
     private fun loadCurrentTheme() {
@@ -107,8 +104,8 @@ class ThemePickerViewModel @Inject constructor(
             currentTheme.value = CurrentThemeState.Hidden
             return
         }
-        currentTheme.value = CurrentThemeState.Loading
-        launch {
+        viewModelScope.launch {
+            currentTheme.value = CurrentThemeState.Loading
             val result = themeRepository.fetchCurrentTheme().fold(
                 onSuccess = { theme ->
                     CurrentThemeState.Success(theme.name, theme.id)
@@ -160,6 +157,15 @@ class ThemePickerViewModel @Inject constructor(
             val message = "Screenshot for theme $themeName is unavailable"
             crashLogger.sendReport(Exception(message))
         }
+    }
+
+    fun onRetryTapped() {
+        loadData()
+    }
+
+    private fun loadData() {
+        loadCurrentTheme()
+        loadThemes()
     }
 
     data class ViewState(
