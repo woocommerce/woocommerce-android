9a0fb7690b47434aba3ba96f1ef64ca3f37892b5
Andrey <akidinov@gmail.com>
2023-12-20 10:18:46 +0100

    Icon animation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/totals/OrderCreateEditTotalsView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/totals/OrderCreateEditTotalsView.kt
@@ -2,11 +2,15 @@ package com.woocommerce.android.ui.orders.creation.totals
 
 import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,12 +19,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
@@ -32,6 +44,7 @@ import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.compose.component.WCColoredButton
 import com.woocommerce.android.ui.compose.theme.WooTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun OrderCreateEditTotalsView(
@@ -80,6 +93,10 @@ fun OrderCreateEditTotalsView(
 
 @Composable
 private fun TotalsView(state: TotalsSectionsState) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val totalsIs = remember { MutableInteractionSource() }
+    val topRowCoroutineScope = rememberCoroutineScope()
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -88,12 +105,31 @@ private fun TotalsView(state: TotalsSectionsState) {
             .background(
                 color = colorResource(id = R.color.color_surface)
             )
+            .clickable(
+                interactionSource = totalsIs,
+                indication = null
+            ) {
+                val press = PressInteraction.Press(Offset.Zero)
+                topRowCoroutineScope.launch {
+                    totalsIs.emit(press)
+                    totalsIs.emit(PressInteraction.Release(press))
+                }
+                isExpanded = !isExpanded
+            }
     ) {
-        Icon(
-            imageVector = Icons.Default.KeyboardArrowUp,
-            contentDescription = null,
-            tint = colorResource(id = R.color.color_primary)
-        )
+        Crossfade(
+            targetState = isExpanded,
+            label = "totals_icon"
+        ) { expanded ->
+            IconButton(interactionSource = totalsIs, onClick = {}) {
+                Icon(
+                    imageVector = if (expanded) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
+                    contentDescription = null,
+                    tint = colorResource(id = R.color.color_primary),
+                    modifier = Modifier.padding(all = dimensionResource(id = R.dimen.minor_100))
+                )
+            }
+        }
 
         Spacer(modifier = Modifier.height(24.dp))
 
