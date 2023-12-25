443beb64a746e8f30ab1cc548eccee1116d7d7a9
Andrey <akidinov@gmail.com>
2023-12-25 14:46:36 +0100

    Save expanded state

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/totals/OrderCreateEditTotalsFullView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/totals/OrderCreateEditTotalsFullView.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -121,7 +122,7 @@ private fun TotalsView(
     state: TotalsSectionsState.Full,
     isPreview: Boolean = LocalInspectionMode.current,
 ) {
-    var isExpanded by remember { mutableStateOf(isPreview) }
+    var isExpanded by rememberSaveable { mutableStateOf(isPreview) }
 
     val totalsIs = remember { MutableInteractionSource() }
 
