04f083c55f199f11589ce06e9d0fcdd0d981d155
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-01-17 17:49:21 +0100

    Map budget into display value

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/preview/BlazeCampaignCreationPreviewViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/preview/BlazeCampaignCreationPreviewViewModel.kt
@@ -3,9 +3,11 @@ package ui.blaze.creation.preview
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.R
+import com.woocommerce.android.extensions.formatToMMMdd
 import com.woocommerce.android.ui.blaze.BlazeRepository
 import com.woocommerce.android.ui.blaze.BlazeRepository.CampaignPreview
 import com.woocommerce.android.ui.blaze.creation.preview.BlazeCampaignCreationPreviewFragmentArgs
+import com.woocommerce.android.util.CurrencyFormatter
 import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
@@ -18,7 +20,8 @@ import javax.inject.Inject
 class BlazeCampaignCreationPreviewViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     blazeRepository: BlazeRepository,
-    private val resourceProvider: ResourceProvider
+    private val resourceProvider: ResourceProvider,
+    private val currencyFormatter: CurrencyFormatter
 ) : ScopedViewModel(savedStateHandle) {
 
     private val _viewState = MutableLiveData<CampaignPreviewUiState>(CampaignPreviewUiState.Loading)
@@ -69,7 +72,18 @@ class BlazeCampaignCreationPreviewViewModel @Inject constructor(
             campaignImageUrl = campaignImageUrl ?: "",
         )
 
-    private fun BlazeRepository.Budget.toDisplayValue() = "TODO"
+    private fun BlazeRepository.Budget.toDisplayValue(): String {
+        val totalBudgetWithCurrency = currencyFormatter.formatCurrency(
+            totalBudget.toBigDecimal(),
+            currencyCode
+        )
+        val duration = resourceProvider.getString(
+            R.string.blaze_campaign_preview_days_duration,
+            durationInDays,
+            startDate.formatToMMMdd()
+        )
+        return "$totalBudgetWithCurrency,  $duration"
+    }
 
     sealed interface CampaignPreviewUiState {
         object Loading : CampaignPreviewUiState
