d49e129d2ede925a055dac191e6efefdeb7409ce
Ondrej Ruttkay <ruttkay@gmail.com>
2024-01-19 12:36:52 +0100

    Fix the character limit and add the missing tap handlers

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdViewModel.kt
@@ -3,7 +3,6 @@ package com.woocommerce.android.ui.blaze.creation.ad
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ai.AIRepository
-import com.woocommerce.android.ui.blaze.creation.preview.BlazeCampaignCreationPreviewFragmentArgs
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -29,6 +28,8 @@ class BlazeCampaignCreationEditAdViewModel @Inject constructor(
         val tagLine: String,
         val description: String,
         val campaignImageUrl: String?,
+        val isPreviousSuggestionButtonEnabled: Boolean = false,
+        val isNextSuggestionButtonEnabled: Boolean = true,
     ) {
         val taglineCharactersRemaining: Int
             get() = TAGLINE_MAX_LENGTH - tagLine.length
@@ -36,19 +37,27 @@ class BlazeCampaignCreationEditAdViewModel @Inject constructor(
             get() = DESCRIPTION_MAX_LENGTH - description.length
     }
 
-    fun onNextAISuggestionTapped() {
+    fun onNextSuggestionTapped() {
 
     }
 
-    fun onPreviousAISuggestionTapped() {
+    fun onPreviousSuggestionTapped() {
+
+    }
+
+    fun onChangeImageTapped() {
+
+    }
+
+    fun onBackButtonTapped() {
 
     }
 
     fun onTagLineChanged(tagLine: String) {
-        _viewState.value = _viewState.value.copy(tagLine = tagLine.substring(0, TAGLINE_MAX_LENGTH))
+        _viewState.value = _viewState.value.copy(tagLine = tagLine.take(TAGLINE_MAX_LENGTH))
     }
 
     fun onDescriptionChanged(description: String) {
-        _viewState.value = _viewState.value.copy(description = description.substring(0, DESCRIPTION_MAX_LENGTH))
+        _viewState.value = _viewState.value.copy(description = description.take(DESCRIPTION_MAX_LENGTH))
     }
 }
