4eb86ba2ea829da1e8e71b9f00cbf3e536b332af
Ondrej Ruttkay <ruttkay@gmail.com>
2024-01-19 14:39:53 +0100

    Add media picker for image selection

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdFragment.kt
@@ -1,25 +1,70 @@
 package com.woocommerce.android.ui.blaze.creation.ad
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.extensions.navigateBackWithResult
+import com.woocommerce.android.mediapicker.MediaPickerHelper
+import com.woocommerce.android.mediapicker.MediaPickerHelper.MediaPickerResultHandler
+import com.woocommerce.android.model.Product.Image
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.blaze.creation.ad.BlazeCampaignCreationEditAdViewModel.EditAdResult
+import com.woocommerce.android.ui.blaze.creation.ad.BlazeCampaignCreationEditAdViewModel.ShowMediaLibrary
 import com.woocommerce.android.ui.compose.composeView
 import com.woocommerce.android.ui.main.AppBarStatus
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ExitWithResult
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class BlazeCampaignCreationEditAdFragment : BaseFragment() {
+class BlazeCampaignCreationEditAdFragment : BaseFragment(), MediaPickerResultHandler {
     override val activityAppBarStatus: AppBarStatus
         get() = AppBarStatus.Hidden
 
     val viewModel: BlazeCampaignCreationEditAdViewModel by viewModels()
 
+    @Inject
+    lateinit var mediaPickerHelper: MediaPickerHelper
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return composeView {
             BlazeCampaignCreationPreviewScreen(viewModel = viewModel)
         }
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is Exit -> findNavController().popBackStack()
+                is ExitWithResult<*> -> {
+                    navigateBackWithResult(EDIT_AD_RESULT, event.data as EditAdResult)
+                }
+                is ShowMediaLibrary -> mediaPickerHelper.showMediaPicker(event.source)
+            }
+        }
+    }
+
+    override fun onDeviceMediaSelected(imageUris: List<Uri>, source: String) {
+        if (imageUris.isNotEmpty()) {
+            onImageSelected(imageUris.first().toString())
+        }
+    }
+
+    override fun onWPMediaSelected(images: List<Image>) {
+        if (images.isNotEmpty()) {
+            onImageSelected(images.first().source)
+        }
+    }
+
+    private fun onImageSelected(mediaUri: String) {
+        viewModel.onImageChanged(mediaUri)
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdViewModel.kt
@@ -7,6 +7,7 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import org.wordpress.android.mediapicker.api.MediaPickerSetup
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,9 +28,10 @@ class BlazeCampaignCreationEditAdViewModel @Inject constructor(
     data class ViewState(
         val tagLine: String,
         val description: String,
-        val campaignImageUrl: String?,
+        val adImageUrl: String?,
         val isPreviousSuggestionButtonEnabled: Boolean = false,
         val isNextSuggestionButtonEnabled: Boolean = true,
+        val isMediaPickerDialogVisible: Boolean = false
     ) {
         val taglineCharactersRemaining: Int
             get() = TAGLINE_MAX_LENGTH - tagLine.length
@@ -46,11 +48,20 @@ class BlazeCampaignCreationEditAdViewModel @Inject constructor(
     }
 
     fun onChangeImageTapped() {
+        setMediaPickerDialogVisibility(true)
+    }
 
+    fun onMediaPickerDialogDismissed() {
+        setMediaPickerDialogVisibility(false)
+    }
+
+    fun onMediaLibraryRequested(source: MediaPickerSetup.DataSource) {
+        triggerEvent(ShowMediaLibrary(source))
+        setMediaPickerDialogVisibility(false)
     }
 
     fun onBackButtonTapped() {
-
+        triggerEvent(Exit)
     }
 
     fun onTagLineChanged(tagLine: String) {
@@ -60,4 +71,18 @@ class BlazeCampaignCreationEditAdViewModel @Inject constructor(
     fun onDescriptionChanged(description: String) {
         _viewState.value = _viewState.value.copy(description = description.take(DESCRIPTION_MAX_LENGTH))
     }
+
+    fun onImageChanged(url: String) {
+        _viewState.update {
+            _viewState.value.copy(adImageUrl = url)
+        }
+    }
+
+    private fun setMediaPickerDialogVisibility(isVisible: Boolean) {
+        _viewState.update {
+            _viewState.value.copy(isMediaPickerDialogVisible = isVisible)
+        }
+    }
+
+    data class ShowMediaLibrary(val source: MediaPickerSetup.DataSource) : MultiLiveEvent.Event()
 }
