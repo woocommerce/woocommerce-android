37046c8139948a9c169c313da89faff99d1fb047
Ondrej Ruttkay <ruttkay@gmail.com>
2024-01-19 12:36:27 +0100

    Update the Edit ad screen layout and suggestion buttons

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/ad/BlazeCampaignCreationEditAdScreen.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.blaze.creation.ad
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,12 +19,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.runtime.Composable
@@ -32,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -54,7 +56,15 @@ import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 @Composable
 fun BlazeCampaignCreationPreviewScreen(viewModel: BlazeCampaignCreationEditAdViewModel) {
     viewModel.viewState.observeAsState().value?.let { viewState ->
-        BlazeCampaignCreationEditAdScreen(viewState, viewModel::onTagLineChanged, viewModel::onDescriptionChanged)
+        BlazeCampaignCreationEditAdScreen(
+            viewState = viewState,
+            onTagLineChanged = viewModel::onTagLineChanged,
+            onDescriptionChanged = viewModel::onDescriptionChanged,
+            onChangeImageTapped = viewModel::onChangeImageTapped,
+            onPreviousSuggestionTapped = viewModel::onPreviousSuggestionTapped,
+            onNextSuggestionTapped = viewModel::onNextSuggestionTapped,
+            onBackButtonTapped = viewModel::onBackButtonTapped
+        )
     }
 }
 
@@ -63,13 +73,19 @@ private fun BlazeCampaignCreationEditAdScreen(
     viewState: BlazeCampaignCreationEditAdViewModel.ViewState,
     onTagLineChanged: (String) -> Unit,
     onDescriptionChanged: (String) -> Unit,
+    onChangeImageTapped: () -> Unit,
+    onPreviousSuggestionTapped: () -> Unit,
+    onNextSuggestionTapped: () -> Unit,
+    onBackButtonTapped: () -> Unit
 ) {
     Scaffold(
         topBar = {
             Toolbar(
-                title = stringResource(id = R.string.blaze_campaign_screen_fragment_title),
-                onNavigationButtonClick = { /*TODO*/ },
-                navigationIcon = Filled.ArrowBack
+                title = stringResource(id = string.blaze_campaign_preview_edit_ad),
+                onNavigationButtonClick = onBackButtonTapped,
+                navigationIcon = Filled.ArrowBack,
+                onActionButtonClick = { },
+                actionButtonText = stringResource(id = string.save).uppercase()
             )
         },
         modifier = Modifier.background(MaterialTheme.colors.surface)
@@ -77,11 +93,18 @@ private fun BlazeCampaignCreationEditAdScreen(
         Column(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
+                .background(MaterialTheme.colors.surface)
                 .fillMaxSize()
                 .padding(paddingValues)
-                .background(color = MaterialTheme.colors.surface)
         ) {
-            CampaignEditAdContent(viewState, onTagLineChanged, onDescriptionChanged)
+            CampaignEditAdContent(
+                viewState,
+                onTagLineChanged,
+                onDescriptionChanged,
+                onChangeImageTapped,
+                onPreviousSuggestionTapped,
+                onNextSuggestionTapped
+            )
         }
     }
 }
@@ -91,136 +114,160 @@ fun CampaignEditAdContent(
     viewState: BlazeCampaignCreationEditAdViewModel.ViewState,
     onTagLineChanged: (String) -> Unit,
     onDescriptionChanged: (String) -> Unit,
+    onChangeImageTapped: () -> Unit,
+    onPreviousSuggestionTapped: () -> Unit,
+    onNextSuggestionTapped: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(viewState.campaignImageUrl)
-                .crossfade(true)
-                .fallback(R.drawable.blaze_campaign_product_placeholder)
-                .placeholder(R.drawable.blaze_campaign_product_placeholder)
-                .error(R.drawable.blaze_campaign_product_placeholder)
-                .build(),
-            contentScale = ContentScale.Crop,
-            contentDescription = null,
-            modifier = Modifier
-                .size(dimensionResource(id = R.dimen.image_major_120))
-                .clip(shape = RoundedCornerShape(size = 8.dp))
-        )
-
-        WCTextButton(
-            modifier = Modifier.padding(top = 8.dp),
-            onClick = { /*TODO*/ },
+        Column(
+            modifier = modifier
+                .padding(dimensionResource(id = R.dimen.major_100))
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(stringResource(id = string.blaze_campaign_edit_ad_change_image_button))
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(viewState.campaignImageUrl)
+                    .crossfade(true)
+                    .fallback(R.drawable.blaze_campaign_product_placeholder)
+                    .placeholder(R.drawable.blaze_campaign_product_placeholder)
+                    .error(R.drawable.blaze_campaign_product_placeholder)
+                    .build(),
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(dimensionResource(id = R.dimen.image_major_120))
+                    .clip(shape = RoundedCornerShape(size = 8.dp))
+            )
+
+            WCTextButton(
+                modifier = Modifier.padding(top = 8.dp),
+                onClick = onChangeImageTapped,
+            ) {
+                Text(stringResource(id = string.blaze_campaign_edit_ad_change_image_button))
+            }
         }
 
         Divider(
             color = colorResource(id = R.color.divider_color),
             thickness = 1.dp,
-            modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.major_100))
+            modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.minor_100))
         )
-
-        WCOutlinedTextField(
-            value = viewState.tagLine,
-            onValueChange = onTagLineChanged,
-            label = stringResource(id = string.blaze_campaign_edit_ad_change_tagline_title),
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
-        )
-
-        Text(
-            text = stringResource(
-                id = string.blaze_campaign_edit_ad_characters_remaining,
-                viewState.taglineCharactersRemaining
-            ),
-            style = MaterialTheme.typography.caption,
-            color = colorResource(id = R.color.color_on_surface_disabled),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        WCOutlinedTextField(
-            value = viewState.description,
-            onValueChange = onDescriptionChanged,
-            label = stringResource(id = string.blaze_campaign_edit_ad_change_description_title),
-            maxLines = 4,
-            minLines = 4,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-            modifier = Modifier
-                .height(120.dp)
-                .padding(top = dimensionResource(id = R.dimen.major_100))
-        )
-
-        Text(
-            text = stringResource(
-                id = string.blaze_campaign_edit_ad_characters_remaining,
-                viewState.descriptionCharactersRemaining
-            ),
-            style = MaterialTheme.typography.caption,
-            color = colorResource(id = R.color.color_on_surface_disabled),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Row(
-            modifier = Modifier
-                .padding(top = dimensionResource(id = R.dimen.major_100))
+        Column(
+            modifier = modifier
+                .padding(dimensionResource(id = R.dimen.major_100))
                 .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_ai),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(colorResource(id = R.color.color_on_surface)),
-                modifier = Modifier
-                    .size(24.dp)
-                    .padding(end = dimensionResource(id = R.dimen.minor_100))
+            WCOutlinedTextField(
+                value = viewState.tagLine,
+                onValueChange = onTagLineChanged,
+                label = stringResource(id = string.blaze_campaign_edit_ad_change_tagline_title),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
             )
+
             Text(
-                text = stringResource(id = string.blaze_campaign_edit_ad_suggested_by_ai),
-                style = MaterialTheme.typography.h6,
+                text = stringResource(
+                    id = string.blaze_campaign_edit_ad_characters_remaining,
+                    viewState.taglineCharactersRemaining
+                ),
+                style = MaterialTheme.typography.caption,
+                color = colorResource(id = R.color.color_on_surface_disabled),
+                modifier = Modifier.fillMaxWidth()
             )
-            Spacer(modifier = Modifier.weight(1f))
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.major_100))
+
+            WCOutlinedTextField(
+                value = viewState.description,
+                onValueChange = onDescriptionChanged,
+                label = stringResource(id = string.blaze_campaign_edit_ad_change_description_title),
+                maxLines = 4,
+                minLines = 4,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                modifier = Modifier
+                    .height(120.dp)
+                    .padding(top = dimensionResource(id = R.dimen.major_100))
+            )
+
+            Text(
+                text = stringResource(
+                    id = string.blaze_campaign_edit_ad_characters_remaining,
+                    viewState.descriptionCharactersRemaining
+                ),
+                style = MaterialTheme.typography.caption,
+                color = colorResource(id = R.color.color_on_surface_disabled),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(top = dimensionResource(id = R.dimen.major_100))
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Box(
+                Image(
+                    painter = painterResource(id = R.drawable.ic_ai),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(colorResource(id = R.color.color_on_surface)),
                     modifier = Modifier
-                        .border(
-                            width = 1.dp,
-                            color = colorResource(id = R.color.image_border_color),
-                            shape = RoundedCornerShape(dimensionResource(id = R.dimen.minor_100))
-                        )
-                        .padding(dimensionResource(id = R.dimen.minor_100))
-                ) {
-                    Icon(
-                        Filled.ArrowBackIosNew, null, tint = MaterialTheme.colors.primary
-                    )
-                }
-            }
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.major_100))
-            ) {
-                Box(
-                    modifier = Modifier
-                        .border(
-                            width = 1.dp,
-                            color = colorResource(id = R.color.image_border_color),
-                            shape = RoundedCornerShape(dimensionResource(id = R.dimen.minor_100))
-                        )
-                        .padding(dimensionResource(id = R.dimen.minor_100))
-                ) {
-                    Icon(
-                        Filled.ArrowForwardIos, null, tint = MaterialTheme.colors.primary
-                    )
-                }
+                        .size(24.dp)
+                        .padding(end = dimensionResource(id = R.dimen.minor_100))
+                )
+                Text(
+                    text = stringResource(id = string.blaze_campaign_edit_ad_suggested_by_ai),
+                    style = MaterialTheme.typography.h6,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+
+                SuggestionButton(
+                    onClick = onPreviousSuggestionTapped,
+                    isEnabled = viewState.isPreviousSuggestionButtonEnabled,
+                    icon = Filled.ArrowBackIosNew
+                )
+                SuggestionButton(
+                    onClick = onNextSuggestionTapped,
+                    isEnabled = viewState.isNextSuggestionButtonEnabled,
+                    icon = Filled.ArrowForwardIos,
+                    modifier.padding(start = dimensionResource(id = R.dimen.minor_100))
+                )
             }
         }
+    }
+}
+
+@Composable
+private fun SuggestionButton(
+    onClick: () -> Unit,
+    isEnabled: Boolean,
+    icon: ImageVector,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = colorResource(id = R.color.image_border_color),
+                shape = RoundedCornerShape(dimensionResource(id = R.dimen.minor_100))
+            )
+            .clip(RoundedCornerShape(dimensionResource(id = R.dimen.minor_100)))
+            .clickable(onClick = onClick, enabled = isEnabled)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = if (isEnabled)
+                MaterialTheme.colors.primary
+            else
+                colorResource(id = R.color.color_on_surface_disabled),
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(dimensionResource(id = R.dimen.minor_100))
+        )
     }
 }
 
@@ -236,7 +283,10 @@ fun PreviewCampaignEditAdContent() {
                 campaignImageUrl = "https://rb.gy/gmjuwb"
             ),
             onTagLineChanged = { },
-            onDescriptionChanged = { }
+            onDescriptionChanged = { },
+            onChangeImageTapped = { },
+            onPreviousSuggestionTapped = { },
+            onNextSuggestionTapped = { }
         )
     }
 }
