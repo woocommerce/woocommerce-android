09b47c56659393aca5b00fe7f4d362ba173094da
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-01-24 12:06:55 +0100

    build: upgrade kotlin, dagger, hilt, compose
    To apply KSP plugin

--- WooCommerce/src/main/kotlin/com/woocommerce/android/di/ProductImageMapModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/di/ProductImageMapModule.kt
@@ -5,11 +5,13 @@ import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.CoroutineDispatchers
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.migration.DisableInstallInCheck
 import kotlinx.coroutines.CoroutineScope
 import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Singleton
 
 @Module
+@DisableInstallInCheck
 class ProductImageMapModule {
     @Provides
     @Singleton
