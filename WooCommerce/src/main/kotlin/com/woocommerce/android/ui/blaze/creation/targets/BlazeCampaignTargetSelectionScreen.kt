e5b7400aa821a7ed732c2614f3a9ec3387898e4e
Ondrej Ruttkay <ruttkay@gmail.com>
2024-02-03 14:44:55 +0100

    Include search UI in the target selection screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/targets/BlazeCampaignTargetSelectionScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/targets/BlazeCampaignTargetSelectionScreen.kt
@@ -1,12 +1,24 @@
 package com.woocommerce.android.ui.blaze.creation.targets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -17,17 +29,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.woocommerce.android.R
+import com.woocommerce.android.R.color
 import com.woocommerce.android.R.dimen
 import com.woocommerce.android.R.string
-import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.TargetItem
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.SearchState.Hidden
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.SearchState.Inactive
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.SearchState.NoResults
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.SearchState.Ready
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.SearchState.Results
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.SearchState.Results.SearchItem
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.SearchState.Searching
 import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.ViewState
+import com.woocommerce.android.ui.blaze.creation.targets.BlazeCampaignTargetSelectionViewModel.ViewState.SelectionItem
 import com.woocommerce.android.ui.compose.component.MultiSelectAllItemsButton
 import com.woocommerce.android.ui.compose.component.MultiSelectList
 import com.woocommerce.android.ui.compose.component.Toolbar
@@ -41,9 +68,11 @@ fun BlazeCampaignTargetSelectionScreen(viewModel: BlazeCampaignTargetSelectionVi
             state = state,
             onBackPressed = viewModel::onBackPressed,
             onSaveTapped = viewModel::onSaveTapped,
-            onItemTapped = viewModel::onItemTapped,
+            onItemTapped = viewModel::onItemToggled,
+            onSearchItemTapped = viewModel::onSearchItemTapped,
             onAllButtonTapped = viewModel::onAllButtonTapped,
-            onSearchQueryChanged = viewModel::onSearchQueryChanged
+            onSearchQueryChanged = viewModel::onSearchQueryChanged,
+            onSearchActiveStateChanged = viewModel::onSearchActiveStateChanged
         )
     }
 }
@@ -53,10 +82,16 @@ private fun TargetSelectionScreen(
     state: ViewState,
     onBackPressed: () -> Unit,
     onSaveTapped: () -> Unit,
-    onItemTapped: (TargetItem) -> Unit,
+    onItemTapped: (SelectionItem) -> Unit,
+    onSearchItemTapped: (SearchItem) -> Unit,
     onAllButtonTapped: () -> Unit,
-    onSearchQueryChanged: (String) -> Unit
+    onSearchQueryChanged: (String) -> Unit,
+    onSearchActiveStateChanged: (Boolean) -> Unit
 ) {
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+    val interactionSource = remember { MutableInteractionSource() }
+
     Scaffold(
         topBar = {
             Toolbar(
@@ -64,20 +99,24 @@ private fun TargetSelectionScreen(
                 onNavigationButtonClick = onBackPressed,
                 navigationIcon = Filled.ArrowBack,
                 actions = {
-                    TextButton(onClick = onSaveTapped, enabled = state.selectedItems.isNotEmpty()) {
-                        Text(stringResource(id = string.save).uppercase())
+                    if (state.searchState is Hidden || state.searchState is Inactive) {
+                        TextButton(onClick = onSaveTapped, enabled = state.isSaveButtonEnabled) {
+                            Text(stringResource(id = string.save).uppercase())
+                        }
                     }
                 }
             )
         },
-        modifier = Modifier.background(MaterialTheme.colors.surface)
+        modifier = Modifier
+            .background(MaterialTheme.colors.surface)
+            .clickable(indication = null, interactionSource = interactionSource) { focusManager.clearFocus() } // Clear focus when clicked outside
     ) { paddingValues ->
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colors.surface)
                 .padding(paddingValues)
         ) {
-            if (state.isSearchVisible) {
+            if (state.searchState != Hidden) {
                 val searchQuery = remember { mutableStateOf(state.searchQuery) }
                 val newLineRegex = Regex("[\n\r]")
 
@@ -90,29 +129,158 @@ private fun TargetSelectionScreen(
                             onSearchQueryChanged(newValue)
                         }
                     },
-                    hint = state.searchHint,
+                    hint = stringResource(string.blaze_campaign_preview_target_location_search_hint),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(
                             horizontal = dimensionResource(id = dimen.major_100),
                             vertical = dimensionResource(id = dimen.minor_100)
-                        ),
+                        )
+                        .onFocusChanged { state ->
+                            onSearchActiveStateChanged(state.isFocused)
+                        }
+                        .focusRequester(focusRequester), // Request focus
                     keyboardOptions = KeyboardOptions(autoCorrect = false),
                 )
             }
 
-            MultiSelectList(
-                items = state.items,
-                selectedItems = state.selectedItems,
-                itemFormatter = { value },
-                onItemToggled = onItemTapped,
-                allItemsButton = MultiSelectAllItemsButton(
-                    text = stringResource(id = string.blaze_campaign_preview_target_default_value),
-                    onClicked = onAllButtonTapped
-                ),
-                modifier = Modifier
-                    .weight(1f)
-                    .verticalScroll(rememberScrollState())
+            when(state.searchState) {
+                is Searching, is Results, is NoResults, is Ready -> {
+                    SearchList(
+                        state = state,
+                        focusManager = focusManager,
+                        onSearchItemTapped = onSearchItemTapped,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+                else -> {
+                    MultiSelectList(
+                        items = state.items,
+                        selectedItems = state.selectedItems,
+                        titleFormatter = { title },
+                        onItemToggled = onItemTapped,
+                        isAllButtonToggled = state.isAllButtonToggled,
+                        allItemsButton = MultiSelectAllItemsButton(
+                            text = stringResource(id = string.blaze_campaign_preview_target_default_value),
+                            onClicked = onAllButtonTapped
+                        ),
+                        modifier = Modifier
+                            .weight(1f)
+                            .verticalScroll(rememberScrollState())
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SearchList(
+    state: ViewState,
+    focusManager: FocusManager,
+    onSearchItemTapped: (SearchItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    when (state.searchState) {
+        is Searching -> {
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(dimensionResource(id = dimen.progress_bar_small))
+
+                )
+            }
+        }
+        is Results -> {
+            LazyColumn(
+                modifier = modifier
+            ) {
+                items(state.searchState.resultItems) { item ->
+                    SearchListItem(
+                        item = item,
+                        onItemTapped = {
+                            focusManager.clearFocus()
+                            onSearchItemTapped(it)
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+        is NoResults -> {
+            val image = painterResource(id = R.drawable.search_failed_illustration)
+            val message = stringResource(id = string.blaze_campaign_creation_location_search_failed_message)
+            ImageWithMessage(modifier, image, message)
+        }
+        else -> {
+            val image = painterResource(id = R.drawable.search_illustration)
+            val message = stringResource(id = string.blaze_campaign_creation_location_search_message)
+            ImageWithMessage(modifier, image, message)
+        }
+    }
+}
+
+@Composable
+private fun ImageWithMessage(modifier: Modifier, image: Painter, message: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(dimensionResource(id = dimen.major_100))
+                .fillMaxWidth()
+        )
+        Text(
+            text = message,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = dimensionResource(id = dimen.major_100))
+        )
+    }
+}
+
+@Composable
+private fun SearchListItem(
+    item: SearchItem,
+    onItemTapped: (SearchItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = dimen.major_100)),
+        modifier = modifier
+            .clickable { onItemTapped(item) }
+            .padding(horizontal = dimensionResource(id = dimen.major_100))
+            .heightIn(min = dimensionResource(id = dimen.major_300))
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = item.title
+            )
+            if (item.subtitle != null) {
+                Text(
+                    text = item.subtitle,
+                    style = MaterialTheme.typography.caption,
+                    color = colorResource(id = color.color_on_surface_medium)
+                )
+            }
+        }
+
+        if (item.type != null) {
+            Text(
+                text = item.type,
+                style = MaterialTheme.typography.subtitle1,
+                color = colorResource(id = color.color_on_surface_medium)
             )
         }
     }
@@ -124,31 +292,32 @@ fun PreviewTargetSelectionScreen() {
     TargetSelectionScreen(
         state = ViewState(
             items = listOf(
-                TargetItem("1", "Item 1"),
-                TargetItem("2", "Item 2"),
-                TargetItem("3", "Item 3"),
-                TargetItem("4", "Item 4"),
-                TargetItem("5", "Item 5"),
-                TargetItem("6", "Item 6"),
-                TargetItem("7", "Item 7"),
-                TargetItem("8", "Item 8"),
-                TargetItem("9", "Item 9")
+                SelectionItem("1", "Item 1"),
+                SelectionItem("2", "Item 2"),
+                SelectionItem("3", "Item 3"),
+                SelectionItem("4", "Item 4"),
+                SelectionItem("5", "Item 5"),
+                SelectionItem("6", "Item 6"),
+                SelectionItem("7", "Item 7"),
+                SelectionItem("8", "Item 8"),
+                SelectionItem("9", "Item 9")
             ),
             selectedItems = listOf(
-                TargetItem("4", "Item 4"),
-                TargetItem("5", "Item 5"),
-                TargetItem("8", "Item 8"),
-                TargetItem("9", "Item 9")
+                SelectionItem("4", "Item 4"),
+                SelectionItem("5", "Item 5"),
+                SelectionItem("8", "Item 8"),
+                SelectionItem("9", "Item 9")
             ),
             title = "Title",
-            isSearchVisible = true,
-            searchQuery = "Query",
-            searchHint = "Search hint"
+            searchQuery = "",
+            searchState = Searching
         ),
         onBackPressed = { /*TODO*/ },
         onSaveTapped = { /*TODO*/ },
         onItemTapped = {},
+        onSearchItemTapped = {},
         onAllButtonTapped = {},
-        onSearchQueryChanged = {}
+        onSearchQueryChanged = {},
+        onSearchActiveStateChanged = {}
     )
 }
