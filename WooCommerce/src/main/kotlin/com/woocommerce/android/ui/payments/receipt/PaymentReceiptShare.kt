ecd83ebda786f91b6455bc5856290e6bbfa09d5e
Andrey <akidinov@gmail.com>
2024-02-05 10:34:04 +0100

    Added text along with shared receipt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/receipt/PaymentReceiptShare.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/receipt/PaymentReceiptShare.kt
@@ -4,7 +4,9 @@ import android.app.Application
 import android.content.Intent
 import android.os.Environment
 import androidx.core.content.FileProvider
+import com.woocommerce.android.R
 import com.woocommerce.android.media.FileUtils
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.FileDownloader
 import java.io.File
 import javax.inject.Inject
@@ -13,6 +15,7 @@ class PaymentReceiptShare @Inject constructor(
     private val fileUtils: FileUtils,
     private val fileDownloader: FileDownloader,
     private val context: Application,
+    private val selectedSite: SelectedSite,
 ) {
     @Suppress("TooGenericExceptionCaught")
     suspend operator fun invoke(receiptUrl: String, orderNumber: Long): ReceiptShareResult {
@@ -35,9 +38,15 @@ class PaymentReceiptShare @Inject constructor(
             context.packageName + ".provider",
             receiptFile
         )
+
+        val text = context.getString(
+            R.string.card_reader_payment_receipt_email_subject,
+            selectedSite.get().name.orEmpty()
+        )
         val intent = Intent(Intent.ACTION_SEND).apply {
             type = "application/*"
             putExtra(Intent.EXTRA_STREAM, uri)
+            putExtra(Intent.EXTRA_TEXT, text)
         }
         return try {
             context.startActivity(
