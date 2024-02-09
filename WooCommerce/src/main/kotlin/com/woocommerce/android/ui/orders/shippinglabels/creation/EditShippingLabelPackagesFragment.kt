17ac4b627305808222aec64d99691cfece1fe532
AnirudhBhat <abhat38@gmail.com>
2024-02-09 18:45:27 +0530

    Revert "Add fragment specific toolbar for EditShippingLabelPackagesFragment.kt"
    This reverts commit 1210e19a805c0b908c1ecb7a8d00605f61a1ec66.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesFragment.kt
@@ -5,7 +5,6 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -41,7 +40,8 @@ typealias OnHazmatCategorySelected = (ShippingLabelHazmatCategory) -> Unit
 @AndroidEntryPoint
 class EditShippingLabelPackagesFragment :
     BaseFragment(R.layout.fragment_edit_shipping_label_packages),
-    BackPressListener {
+    BackPressListener,
+    MenuProvider {
     companion object {
         const val EDIT_PACKAGES_CLOSED = "edit_packages_closed"
         const val EDIT_PACKAGES_RESULT = "edit_packages_result"
@@ -70,7 +70,13 @@ class EditShippingLabelPackagesFragment :
 
     override fun getFragmentTitle() = getString(R.string.orderdetail_shipping_label_item_package_info)
 
-    fun onMenuItemSelected(item: MenuItem): Boolean {
+    override fun onCreateMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_done, menu)
+        doneMenuItem = menu.findItem(R.id.menu_done)
+        doneMenuItem.isVisible = viewModel.viewStateData.liveData.value?.isDataValid ?: false
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_done -> {
                 viewModel.onDoneButtonClicked()
@@ -85,8 +91,9 @@ class EditShippingLabelPackagesFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requireActivity().addMenuProvider(this, viewLifecycleOwner)
+
         val binding = FragmentEditShippingLabelPackagesBinding.bind(view)
-        setupToolbar(binding)
         with(binding.packagesList) {
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             adapter = packagesAdapter
@@ -98,23 +105,6 @@ class EditShippingLabelPackagesFragment :
 
         setupObservers(binding)
         setupResultHandlers()
-    }
-
-    private fun setupToolbar(binding: FragmentEditShippingLabelPackagesBinding) {
-        binding.toolbar.title = getString(R.string.orderdetail_shipping_label_item_package_info)
-        binding.toolbar.setOnMenuItemClickListener { menuItem ->
-            onMenuItemSelected(menuItem)
-        }
-        binding.toolbar.navigationIcon = AppCompatResources.getDrawable(
-            requireActivity(),
-            R.drawable.ic_back_24dp
-        )
-        binding.toolbar.setNavigationOnClickListener {
-            onRequestAllowBackPress()
-        }
-        binding.toolbar.inflateMenu(R.menu.menu_done)
-        doneMenuItem = binding.toolbar.menu.findItem(R.id.menu_done)
-        doneMenuItem.isVisible = viewModel.viewStateData.liveData.value?.isDataValid ?: false
     }
 
     private fun setupResultHandlers() {
