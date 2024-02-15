14f26a0d994dace53b2d8ea723aa431012baa23a
Alejo <alejandro.torres.veiga@automattic.com>
2024-02-15 15:07:13 -0600

    data store to persist cards configuration in preferences

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/sync/AnalyticsSettingsDataStore.kt
@@ -0,0 +1,39 @@
+package com.woocommerce.android.ui.analytics.hub.sync
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.woocommerce.android.datastore.DataStoreQualifier
+import com.woocommerce.android.datastore.DataStoreType
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.analytics.hub.settings.AnalyticCardConfiguration
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class AnalyticsSettingsDataStore @Inject constructor(
+    @DataStoreQualifier(DataStoreType.ANALYTICS) private val dataStore: DataStore<Preferences>,
+    private val gson: Gson,
+    private val selectedSite: SelectedSite
+) {
+    private fun getCardsConfigurationKey() = "${selectedSite.getOrNull() ?: ""}AnalyticsCardsConfiguration"
+    private val configurationType = object : TypeToken<List<AnalyticCardConfiguration>>() {}.type
+
+    fun observeCardsConfiguration(): Flow<List<AnalyticCardConfiguration>?> {
+        return dataStore.data.map { prefs ->
+            val rawCards = prefs[stringPreferencesKey(getCardsConfigurationKey())]
+            runCatching {
+                gson.fromJson<List<AnalyticCardConfiguration>>(rawCards, configurationType)
+            }.getOrNull()
+        }
+    }
+
+    suspend fun saveAnalyticsCardsConfiguration(cards: List<AnalyticCardConfiguration>) {
+        dataStore.edit { preferences ->
+            preferences[stringPreferencesKey(getCardsConfigurationKey())] = gson.toJson(cards)
+        }
+    }
+}
