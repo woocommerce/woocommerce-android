b3d5016bd0f0ee1e09a6639c8477b2a719a3245b
Alejo <alejandro.torres.veiga@automattic.com>
2024-02-19 12:19:19 -0600

    refactor add AnalyticsCards enum

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/sync/AnalyticsSettingsResourcesRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/sync/AnalyticsSettingsResourcesRepository.kt
@@ -1,21 +1,15 @@
 package com.woocommerce.android.ui.analytics.hub.sync
 
-import com.woocommerce.android.R
 import com.woocommerce.android.model.AnalyticCardConfiguration
+import com.woocommerce.android.model.AnalyticsCards
 import com.woocommerce.android.viewmodel.ResourceProvider
 import javax.inject.Inject
 
 class AnalyticsSettingsResourcesRepository @Inject constructor(private val resourceProvider: ResourceProvider) {
-    private val cardsTitleResourcesIds = listOf(
-        R.string.analytics_revenue_card_title,
-        R.string.analytics_orders_card_title,
-        R.string.analytics_products_card_title,
-        R.string.analytics_session_card_title
-    )
-    fun getDefaultAnalyticsCardsConfiguration() = cardsTitleResourcesIds.mapIndexed { i, res ->
+    fun getDefaultAnalyticsCardsConfiguration() = AnalyticsCards.entries.map { card ->
         AnalyticCardConfiguration(
-            id = i,
-            title = resourceProvider.getString(res),
+            id = card.ordinal,
+            title = resourceProvider.getString(card.resId),
             isVisible = true
         )
     }
