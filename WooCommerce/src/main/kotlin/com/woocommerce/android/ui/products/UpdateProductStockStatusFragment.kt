a1a04e9aa681ad99277da46d0813611fd92e87c4
Joel Dean <joel.dean@automattic.com>
2024-02-26 16:42:17 -0500

    Adding the ability to navigate back after an update.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/UpdateProductStockStatusFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/UpdateProductStockStatusFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.extensions.navigateBackWithResult
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.composeView
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UpdateProductStockStatusFragment : BaseFragment() {
     private val viewModel: UpdateProductStockStatusViewModel by viewModels()
-    private val args: UpdateProductStockStatusFragmentArgs by navArgs()
+
+    companion object {
+        const val UPDATE_STOCK_STATUS_DONE = "update_stock_status_done"
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return composeView {
@@ -23,6 +28,20 @@ class UpdateProductStockStatusFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.loadProductStockStatuses(args.selectedProductIds.toList())
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is MultiLiveEvent.Event.Exit -> findNavController().popBackStack()
+                is MultiLiveEvent.Event.ExitWithResult<*> -> {
+                    navigateBackWithResult(
+                        UPDATE_STOCK_STATUS_DONE,
+                        true
+                    )
+                }
+            }
+        }
     }
 }
