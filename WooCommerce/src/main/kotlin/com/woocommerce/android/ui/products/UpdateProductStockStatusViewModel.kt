a6761ae012d75d310bb8d552b4e867621607806d
Joel Dean <joel.dean@automattic.com>
2024-02-27 16:46:38 -0500

    Added parcelize to the ui state.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/UpdateProductStockStatusViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/UpdateProductStockStatusViewModel.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.products
 
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
@@ -10,6 +11,7 @@ import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
@@ -67,22 +69,27 @@ class UpdateProductStockStatusViewModel @Inject constructor(
         }
     }
 
+    @Parcelize
     data class UpdateStockStatusUiState(
         val productsToUpdateCount: Int = 0,
         val ignoredProductsCount: Int = 0,
         val updateResult: RequestResult? = null,
         val initialProductStockStatus: ProductStockStatus = ProductStockStatus.InStock,
         val currentStockStatusState: StockStatusState = StockStatusState.Mixed
-    )
+    ) : Parcelable
 
+    @Parcelize
     data class ProductStockStatusInfo(
         val productId: Long,
         val stockStatus: ProductStockStatus,
         val manageStock: Boolean
-    )
+    ) : Parcelable
 
-    sealed class StockStatusState {
+    sealed class StockStatusState : Parcelable {
+        @Parcelize
         data object Mixed : StockStatusState()
+
+        @Parcelize
         data class Common(val status: ProductStockStatus) : StockStatusState()
     }
 }
