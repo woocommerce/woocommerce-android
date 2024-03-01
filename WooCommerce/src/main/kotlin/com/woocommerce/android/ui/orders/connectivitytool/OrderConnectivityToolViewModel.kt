e568671f6ef656aaa30902eb5441c6c8630eb595
ThomazFB <thomazfcortez@gmail.com>
2024-03-01 12:09:02 -0300

    Add basic ViewState type to OrderConnectivityToolViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolViewModel.kt
@@ -1,4 +1,20 @@
 package com.woocommerce.android.ui.orders.connectivitytool
 
-class OrderConnectivityToolViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
+import javax.inject.Inject
+
+class OrderConnectivityToolViewModel @Inject constructor(
+    savedState: SavedStateHandle
+) : ScopedViewModel(savedState) {
+    val viewState = savedState.getStateFlow(
+        scope = viewModelScope,
+        initialValue = ViewState()
+    )
+
+    data class ViewState(
+        val isContactSupportEnabled: Boolean = false
+    )
 }
