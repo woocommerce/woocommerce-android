7689969153271ad18dbdb3be1f06a06838d4ceb2
ThomazFB <thomazfcortez@gmail.com>
2024-03-01 12:12:04 -0300

    Correctly define the viewState as LiveData

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolViewModel.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.orders.connectivitytool
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
@@ -9,10 +10,11 @@ import javax.inject.Inject
 class OrderConnectivityToolViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
-    val viewState = savedState.getStateFlow(
+    private val _viewState = savedState.getStateFlow(
         scope = viewModelScope,
         initialValue = ViewState()
     )
+    val viewState = _viewState.asLiveData()
 
     data class ViewState(
         val isContactSupportEnabled: Boolean = false
