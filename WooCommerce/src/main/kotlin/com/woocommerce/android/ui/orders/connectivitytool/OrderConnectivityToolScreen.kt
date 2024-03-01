d00d73f57bd18b71da8905ea13243c5f71988536
ThomazFB <thomazfcortez@gmail.com>
2024-03-01 12:11:52 -0300

    Set initial structure around OrderConnectivityToolScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolScreen.kt
@@ -1,8 +1,23 @@
 package com.woocommerce.android.ui.orders.connectivitytool
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 
 @Composable
-fun OrderConnectivityToolScreen() {
+fun OrderConnectivityToolScreen(viewModel: OrderConnectivityToolViewModel) {
+    val viewState by viewModel.viewState.observeAsState()
+    OrderConnectivityToolScreen(
+        isContactSupportButtonEnabled = viewState?.isContactSupportEnabled ?: false
+    )
+}
 
+@Composable
+fun OrderConnectivityToolScreen(
+    isContactSupportButtonEnabled: Boolean
+) {
+    Column {
+
+    }
 }
