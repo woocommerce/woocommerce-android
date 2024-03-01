e1ea7bbb3597b71a27c600209fcbd651a4fbaaa7
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-03-01 11:54:55 +0100

    Update UI to hide attribution details in a collapsible view

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailAttributionInfoView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailAttributionInfoView.kt
@@ -1,23 +1,35 @@
 package com.woocommerce.android.ui.orders.details.views
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import com.woocommerce.android.R
 import com.woocommerce.android.model.OrderAttributionOrigin
 import com.woocommerce.android.model.origin
+import com.woocommerce.android.ui.compose.component.WCTextButton
 import org.wordpress.android.fluxc.model.OrderAttributionInfo
 
 @Composable
@@ -45,54 +57,92 @@ private fun OrderAttributionContent(
     Column(
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_100)),
         modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(id = R.dimen.major_100))
     ) {
+        var detailsExpanded by rememberSaveable { mutableStateOf(false) }
+
         OrderAttributionInfoRow(
             title = stringResource(id = R.string.order_detail_attribution_origin),
             value = attributionInfo.origin.label
         )
 
-        attributionInfo.sourceType?.let { sourceType ->
-            OrderAttributionInfoRow(
-                title = stringResource(id = R.string.order_detail_attribution_source_type),
-                value = sourceType
-            )
+        AnimatedVisibility(
+            enter = expandVertically(),
+            exit = shrinkVertically(),
+            visible = detailsExpanded,
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_100)),
+            ) {
+                attributionInfo.sourceType?.let { sourceType ->
+                    OrderAttributionInfoRow(
+                        title = stringResource(id = R.string.order_detail_attribution_source_type),
+                        value = sourceType
+                    )
+                }
+
+                attributionInfo.campaign?.let { campaign ->
+                    OrderAttributionInfoRow(
+                        title = stringResource(id = R.string.order_detail_attribution_campaign),
+                        value = campaign
+                    )
+                }
+
+                attributionInfo.source?.let { source ->
+                    OrderAttributionInfoRow(
+                        title = stringResource(id = R.string.order_detail_attribution_source),
+                        value = source
+                    )
+                }
+
+                attributionInfo.medium?.let { medium ->
+                    OrderAttributionInfoRow(
+                        title = stringResource(id = R.string.order_detail_attribution_medium),
+                        value = medium
+                    )
+                }
+
+                attributionInfo.deviceType?.let { deviceType ->
+                    OrderAttributionInfoRow(
+                        title = stringResource(id = R.string.order_detail_attribution_device_type),
+                        value = deviceType
+                    )
+                }
+
+                attributionInfo.sessionPageViews?.let { sessionPageViews ->
+                    OrderAttributionInfoRow(
+                        title = stringResource(id = R.string.order_detail_attribution_session_page_views),
+                        value = sessionPageViews
+                    )
+                }
+            }
         }
 
-        attributionInfo.campaign?.let { campaign ->
-            OrderAttributionInfoRow(
-                title = stringResource(id = R.string.order_detail_attribution_campaign),
-                value = campaign
-            )
-        }
-
-        attributionInfo.source?.let { source ->
-            OrderAttributionInfoRow(
-                title = stringResource(id = R.string.order_detail_attribution_source),
-                value = source
-            )
-        }
-
-        attributionInfo.medium?.let { medium ->
-            OrderAttributionInfoRow(
-                title = stringResource(id = R.string.order_detail_attribution_medium),
-                value = medium
-            )
-        }
-
-        attributionInfo.deviceType?.let { deviceType ->
-            OrderAttributionInfoRow(
-                title = stringResource(id = R.string.order_detail_attribution_device_type),
-                value = deviceType
-            )
-        }
-
-        attributionInfo.sessionPageViews?.let { sessionPageViews ->
-            OrderAttributionInfoRow(
-                title = stringResource(id = R.string.order_detail_attribution_session_page_views),
-                value = sessionPageViews
-            )
+        if (attributionInfo.hasAdditionalDetails) {
+            WCTextButton(
+                onClick = { detailsExpanded = !detailsExpanded },
+                contentPadding = PaddingValues(
+                    horizontal = dimensionResource(id = R.dimen.major_100),
+                    vertical = ButtonDefaults.TextButtonContentPadding.calculateTopPadding()
+                ),
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(
+                            id = if (detailsExpanded) R.string.hide_details else R.string.show_details
+                        ).uppercase()
+                    )
+                    Icon(
+                        painter = painterResource(
+                            id = if (detailsExpanded) R.drawable.ic_arrow_up
+                            else R.drawable.ic_arrow_down
+                        ),
+                        contentDescription = null
+                    )
+                }
+            }
         }
     }
 }
@@ -107,7 +157,10 @@ private fun OrderAttributionInfoRow(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = dimensionResource(id = R.dimen.minor_100))
+            .padding(
+                horizontal = dimensionResource(id = R.dimen.major_100),
+                vertical = dimensionResource(id = R.dimen.minor_100)
+            )
     ) {
         Text(
             text = title,
@@ -146,3 +199,11 @@ private val OrderAttributionOrigin.label
         OrderAttributionOrigin.Mobile -> stringResource(id = R.string.order_detail_attribution_mobile_origin)
         OrderAttributionOrigin.Unknown -> stringResource(id = R.string.order_detail_attribution_unknown_origin)
     }
+
+private val OrderAttributionInfo.hasAdditionalDetails
+    get() = campaign != null ||
+        source != null ||
+        sourceType != null ||
+        medium != null ||
+        deviceType != null ||
+        sessionPageViews != null
