bf839379774c5bcec0b076eab34695d458998ae3
ThomazFB <thomazfcortez@gmail.com>
2024-03-01 12:13:53 -0300

    Connect Fragment with respective ViewModel and Compose View

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolFragment.kt
@@ -1,7 +1,23 @@
 package com.woocommerce.android.ui.orders.connectivitytool
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
 import com.woocommerce.android.ui.base.BaseFragment
 
 class OrderConnectivityToolFragment : BaseFragment() {
+    val viewModel: OrderConnectivityToolViewModel by viewModels()
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                OrderConnectivityToolScreen(viewModel = viewModel)
+            }
+        }
+    }
 }
