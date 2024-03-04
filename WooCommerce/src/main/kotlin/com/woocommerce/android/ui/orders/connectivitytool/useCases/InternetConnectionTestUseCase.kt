10c762b297d845649be0e56b72340739cec6414b
ThomazFB <thomazfcortez@gmail.com>
2024-03-04 15:42:42 -0300

    Add InternetConnectionTestUseCase implementation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/InternetConnectionTestUseCase.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/InternetConnectionTestUseCase.kt
@@ -1,12 +1,20 @@
 package com.woocommerce.android.ui.orders.connectivitytool.useCases
 
+import com.woocommerce.android.tools.NetworkStatus
 import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus
+import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class InternetConnectionTestUseCase {
+class InternetConnectionTestUseCase @Inject constructor(
+    private val networkStatus: NetworkStatus
+) {
     operator fun invoke(): Flow<ConnectivityTestStatus> = flow {
         emit(ConnectivityTestStatus.InProgress)
-        emit(ConnectivityTestStatus.Success)
+        if (networkStatus.isConnected()) {
+            emit(ConnectivityTestStatus.Success)
+        } else {
+            emit(ConnectivityTestStatus.Failure)
+        }
     }
 }
