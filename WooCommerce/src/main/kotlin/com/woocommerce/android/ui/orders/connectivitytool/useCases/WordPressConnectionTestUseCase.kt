983ac5db18c215430036ce72e50e403d8c0e369a
ThomazFB <thomazfcortez@gmail.com>
2024-03-04 16:43:05 -0300

    Add WordPressConnectionTestUseCase implementation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/WordPressConnectionTestUseCase.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/WordPressConnectionTestUseCase.kt
@@ -1,12 +1,23 @@
 package com.woocommerce.android.ui.orders.connectivitytool.useCases
 
 import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus
+import com.woocommerce.android.util.BuildConfigWrapper
+import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import org.wordpress.android.fluxc.store.WhatsNewStore
 
-class WordPressConnectionTestUseCase {
+class WordPressConnectionTestUseCase @Inject constructor(
+    private val whatsNewStore: WhatsNewStore,
+    private val buildConfigWrapper: BuildConfigWrapper
+) {
     operator fun invoke(): Flow<ConnectivityTestStatus> = flow {
         emit(ConnectivityTestStatus.InProgress)
-        emit(ConnectivityTestStatus.Success)
+        whatsNewStore.fetchRemoteAnnouncements(
+            versionName = buildConfigWrapper.versionName,
+            appId = WhatsNewStore.WhatsNewAppId.WOO_ANDROID
+        ).fetchError?.let {
+            emit(ConnectivityTestStatus.Failure)
+        } ?: emit(ConnectivityTestStatus.Success)
     }
 }
