5cda5d5ce7db548f9241382b62ecf4994ea706b0
ThomazFB <thomazfcortez@gmail.com>
2024-03-05 16:44:32 -0300

    Introduce InternetConnectionCheckUseCaseTest initial structure

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/InternetConnectionCheckUseCaseTest.kt
@@ -0,0 +1,44 @@
+package com.woocommerce.android.ui.orders.connectivitytool.useCases
+
+import com.woocommerce.android.tools.NetworkStatus
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus.InProgress
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus.Success
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class InternetConnectionCheckUseCaseTest : BaseUnitTest() {
+    private lateinit var sut: InternetConnectionCheckUseCase
+    private lateinit var networkStatus: NetworkStatus
+
+    @Before
+    fun setUp() {
+        networkStatus = mock()
+        sut = InternetConnectionCheckUseCase(networkStatus)
+    }
+
+    @Test
+    fun `when network is connected then emit Success`() = testBlocking {
+        // Given
+        val stateEvents = mutableListOf<ConnectivityTestStatus>()
+        whenever(networkStatus.isConnected()).thenReturn(true)
+
+        // When
+        sut.invoke().onEach {
+            stateEvents.add(it)
+        }.launchIn(this)
+
+        // Then
+        assertThat(stateEvents).isEqualTo(listOf(InProgress, Success))
+    }
+}
