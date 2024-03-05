b8d4f5cf622cf26b269c5de263450727b3328b49
ThomazFB <thomazfcortez@gmail.com>
2024-03-05 14:08:50 -0300

    Implement StoreConnectionTestUseCase

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/StoreConnectionTestUseCase.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/StoreConnectionTestUseCase.kt
@@ -1,13 +1,25 @@
 package com.woocommerce.android.ui.orders.connectivitytool.useCases
 
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus.Failure
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus.InProgress
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityTestStatus.Success
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
+import org.wordpress.android.fluxc.store.WooCommerceStore
 
-class StoreConnectionTestUseCase @Inject constructor() {
+class StoreConnectionTestUseCase @Inject constructor(
+    private val wooCommerceStore: WooCommerceStore,
+    private val selectedSite: SelectedSite
+
+    ) {
     operator fun invoke(): Flow<ConnectivityTestStatus> = flow {
-        emit(ConnectivityTestStatus.InProgress)
-        emit(ConnectivityTestStatus.Success)
+        emit(InProgress)
+        wooCommerceStore.fetchSSR(selectedSite.get())
+            .takeIf { it.isError }
+            ?.let { emit(Failure) }
+            ?: emit(Success)
     }
 }
