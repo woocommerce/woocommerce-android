404774035e410f4e827d3136aacdf188ffcc0a3d
ThomazFB <thomazfcortez@gmail.com>
2024-03-06 17:05:44 -0300

    Adjust ConnectivityTestRow to contain expected components

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolScreen.kt
@@ -1,20 +1,33 @@
 package com.woocommerce.android.ui.orders.connectivitytool
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityCheckStatus
 import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityCheckStatus.Failure
 import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.ConnectivityCheckStatus.InProgress
@@ -50,25 +63,25 @@ fun OrderConnectivityToolScreen(
             .padding(dimensionResource(id = R.dimen.major_100))
     ) {
         ConnectivityTestRow(
-            testTitle = "Internet Connection",
+            checkTitle = "Internet Connection",
             errorMessage = "No internet connection",
             testStatus = internetConnectionTestStatus
         )
         Spacer(modifier = modifier.weight(1f))
         ConnectivityTestRow(
-            testTitle = "WordPress Connection",
+            checkTitle = "WordPress Connection",
             errorMessage = "WordPress connection failed",
             testStatus = wordpressConnectionTestStatus
         )
         Spacer(modifier = modifier.weight(1f))
         ConnectivityTestRow(
-            testTitle = "Store Connection",
+            checkTitle = "Store Connection",
             errorMessage = "Store connection failed",
             testStatus = storeConnectionTestStatus
         )
         Spacer(modifier = modifier.weight(1f))
         ConnectivityTestRow(
-            testTitle = "Store Orders",
+            checkTitle = "Store Orders",
             errorMessage = "Store orders failed",
             testStatus = storeOrdersTestStatus
         )
@@ -85,21 +98,41 @@ fun OrderConnectivityToolScreen(
 
 @Composable
 fun ConnectivityTestRow(
-    testTitle: String,
+    @StringRes checkTitle: Int,
+    @DrawableRes iconDrawable: Int,
     errorMessage: String,
     testStatus: ConnectivityCheckStatus
 ) {
     Column {
         Row {
-            Text(testTitle)
-            Spacer(modifier = Modifier.weight(1f))
-            val status = when (testStatus) {
-                NotStarted -> "Not Started"
-                InProgress -> "In Progress"
-                Failure -> "Failed"
-                else -> "Success"
+            Box(
+                modifier = Modifier
+                    .size(dimensionResource(id = R.dimen.major_250))
+                    .clip(CircleShape)
+                    .background(colorResource(id = R.color.more_menu_button_icon_background))
+            ) {
+                Image(
+                    painter = painterResource(id = iconDrawable),
+                    contentDescription = stringResource(id = checkTitle),
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.major_125))
+                        .align(Alignment.Center)
+                )
             }
-            Text(status)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(stringResource(id = checkTitle))
+            Spacer(modifier = Modifier.weight(1f))
+            when (testStatus) {
+                NotStarted -> null
+                InProgress -> null
+                Failure -> R.drawable.ic_rounded_chcekbox_checked
+                Success -> R.drawable.ic_rounded_chcekbox_partially_checked
+            }?.let {
+                Image(
+                    painter = painterResource(id = it),
+                    contentDescription = null
+                )
+            }
         }
 
         if (testStatus == Failure) {
@@ -113,12 +146,14 @@ fun ConnectivityTestRow(
 @Preview
 @Composable
 fun OrderConnectivityToolScreenPreview() {
-    OrderConnectivityToolScreen(
-        isContactSupportButtonEnabled = true,
-        internetConnectionTestStatus = NotStarted,
-        wordpressConnectionTestStatus = InProgress,
-        storeConnectionTestStatus = Failure,
-        storeOrdersTestStatus = Success,
-        onContactSupportClicked = {}
-    )
+    WooThemeWithBackground {
+        OrderConnectivityToolScreen(
+            isContactSupportButtonEnabled = true,
+            internetConnectionTestStatus = NotStarted,
+            wordpressConnectionTestStatus = InProgress,
+            storeConnectionTestStatus = Failure,
+            storeOrdersTestStatus = Success,
+            onContactSupportClicked = {}
+        )
+    }
 }
