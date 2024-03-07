6eddb453f00099b8d9a2a8afdd609dd5781213d4
ThomazFB <thomazfcortez@gmail.com>
2024-03-07 15:01:55 -0300

    Define ConnectivityCheckCardData

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/ConnectivityCheckCardData.kt
@@ -0,0 +1,38 @@
+package com.woocommerce.android.ui.orders.connectivitytool
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.woocommerce.android.R
+
+typealias OnReadMoreClicked = (url: String) -> Unit
+
+sealed class ConnectivityCheckCardData(
+    @StringRes val title: Int,
+    @StringRes val suggestion: Int,
+    @DrawableRes val icon: Int,
+    val readMoreAction: OnReadMoreClicked? = null
+) {
+    data object InternetConnectivityCheckData : ConnectivityCheckCardData(
+        title = R.string.orderlist_connectivity_tool_internet_check_title,
+        suggestion = R.string.orderlist_connectivity_tool_internet_check_suggestion,
+        icon = R.drawable.ic_wifi
+    )
+
+    data object WordPressConnectivityCheckData : ConnectivityCheckCardData(
+        title = R.string.orderlist_connectivity_tool_wordpress_check_title,
+        suggestion = R.string.orderlist_connectivity_tool_wordpress_check_suggestion,
+        icon = R.drawable.ic_storage
+    )
+
+    data object StoreConnectivityCheckData : ConnectivityCheckCardData(
+        title = R.string.orderlist_connectivity_tool_store_check_title,
+        suggestion = R.string.orderlist_connectivity_tool_store_check_suggestion,
+        icon = R.drawable.ic_more_menu_store
+    )
+
+    data object StoreOrdersConnectivityCheckData : ConnectivityCheckCardData(
+        title = R.string.orderlist_connectivity_tool_store_orders_check_title,
+        suggestion = R.string.orderlist_connectivity_tool_store_orders_check_suggestion,
+        icon = R.drawable.ic_clipboard
+    )
+}
