6cdbb98b9a130b76b240ec522a763693c6795583
ThomazFB <thomazfcortez@gmail.com>
2024-03-07 18:33:25 -0300

    Adjust StoreOrdersCheckUseCase to contain error mapping

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/StoreOrdersCheckUseCase.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/useCases/StoreOrdersCheckUseCase.kt
@@ -5,11 +5,15 @@ import com.woocommerce.android.ui.orders.connectivitytool.ConnectivityCheckStatu
 import com.woocommerce.android.ui.orders.connectivitytool.ConnectivityCheckStatus.Failure
 import com.woocommerce.android.ui.orders.connectivitytool.ConnectivityCheckStatus.InProgress
 import com.woocommerce.android.ui.orders.connectivitytool.ConnectivityCheckStatus.Success
+import com.woocommerce.android.ui.orders.connectivitytool.FailureType
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.wordpress.android.fluxc.store.WCOrderStore
 import org.wordpress.android.fluxc.store.WCOrderStore.HasOrdersResult
 import javax.inject.Inject
+import org.wordpress.android.fluxc.store.WCOrderStore.OrderErrorType.PARSE_ERROR
+import org.wordpress.android.fluxc.store.WCOrderStore.OrderErrorType.PLUGIN_NOT_ACTIVE
+import org.wordpress.android.fluxc.store.WCOrderStore.OrderErrorType.TIMEOUT_ERROR
 
 class StoreOrdersCheckUseCase @Inject constructor(
     private val orderStore: WCOrderStore,
@@ -20,8 +24,13 @@ class StoreOrdersCheckUseCase @Inject constructor(
         orderStore.fetchHasOrders(
             site = selectedSite.get(),
             status = null
-        ).takeIf { it is HasOrdersResult.Success }?.let {
-            emit(Success)
-        } ?: emit(Failure())
+        ).run { this as? HasOrdersResult.Failure }?.let { result ->
+             when (result.error.type) {
+                TIMEOUT_ERROR -> Failure(FailureType.TIMEOUT)
+                PARSE_ERROR -> Failure(FailureType.PARSE)
+                PLUGIN_NOT_ACTIVE -> Failure(FailureType.JETPACK)
+                else -> Failure(FailureType.GENERIC)
+            }.let { emit(it) }
+        } ?: emit(Success)
     }
 }
