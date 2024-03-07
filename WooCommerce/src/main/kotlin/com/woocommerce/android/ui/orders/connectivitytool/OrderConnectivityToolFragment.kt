f03def226f62711422f887b353b950248fb1576c
ThomazFB <thomazfcortez@gmail.com>
2024-03-07 19:48:25 -0300

    Intercept WebView events in OrderConnectivityToolFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/OrderConnectivityToolFragment.kt
@@ -7,12 +7,15 @@ import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
+import com.woocommerce.android.AppUrls
 import com.woocommerce.android.R
 import com.woocommerce.android.support.help.HelpOrigin
 import com.woocommerce.android.support.requests.SupportRequestFormActivity
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.OpenSupportRequest
+import com.woocommerce.android.ui.orders.connectivitytool.OrderConnectivityToolViewModel.OpenWebView
+import com.woocommerce.android.util.ChromeCustomTabUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,6 +38,7 @@ class OrderConnectivityToolFragment : BaseFragment() {
         viewModel.event.observe(viewLifecycleOwner) {
             when (it) {
                 is OpenSupportRequest -> openSupportRequestScreen()
+                is OpenWebView -> openWebView(it.url)
             }
         }
         viewModel.startConnectionChecks()
@@ -49,4 +53,6 @@ class OrderConnectivityToolFragment : BaseFragment() {
             extraTags = ArrayList()
         ).let { activity?.startActivity(it) }
     }
+
+    private fun openWebView(url: String) { ChromeCustomTabUtils.launchUrl(requireContext(), url) }
 }
