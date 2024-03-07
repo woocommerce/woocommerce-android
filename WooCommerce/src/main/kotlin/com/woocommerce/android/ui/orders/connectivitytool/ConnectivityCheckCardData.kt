5b5772df1dcafe3bc39afb2437e12afd1fed667b
ThomazFB <thomazfcortez@gmail.com>
2024-03-07 15:07:47 -0300

    Move ConnectivityCheckStatus to ConnectivityCheckCardData

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/ConnectivityCheckCardData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/connectivitytool/ConnectivityCheckCardData.kt
@@ -10,35 +10,55 @@ sealed class ConnectivityCheckCardData(
     @StringRes val title: Int,
     @StringRes val suggestion: Int,
     @DrawableRes val icon: Int,
+    val connectivityCheckStatus: ConnectivityCheckStatus,
     val readMoreAction: OnReadMoreClicked? = null
 ) {
-    data object InternetConnectivityCheckData : ConnectivityCheckCardData(
+    class InternetConnectivityCheckData(
+        checkStatus: ConnectivityCheckStatus
+    ) : ConnectivityCheckCardData(
         title = R.string.orderlist_connectivity_tool_internet_check_title,
         suggestion = R.string.orderlist_connectivity_tool_internet_check_suggestion,
-        icon = R.drawable.ic_wifi
+        icon = R.drawable.ic_wifi,
+        connectivityCheckStatus = checkStatus
     )
 
-    data object WordPressConnectivityCheckData : ConnectivityCheckCardData(
+    class WordPressConnectivityCheckData(
+        checkStatus: ConnectivityCheckStatus
+    ) : ConnectivityCheckCardData(
         title = R.string.orderlist_connectivity_tool_wordpress_check_title,
         suggestion = R.string.orderlist_connectivity_tool_wordpress_check_suggestion,
-        icon = R.drawable.ic_storage
+        icon = R.drawable.ic_storage,
+        connectivityCheckStatus = checkStatus
     )
 
     class StoreConnectivityCheckData(
+        checkStatus: ConnectivityCheckStatus,
         readMoreAction: OnReadMoreClicked
     ) : ConnectivityCheckCardData(
         title = R.string.orderlist_connectivity_tool_store_check_title,
         suggestion = R.string.orderlist_connectivity_tool_store_check_suggestion,
         icon = R.drawable.ic_more_menu_store,
+        connectivityCheckStatus = checkStatus,
         readMoreAction = readMoreAction
     )
 
     class StoreOrdersConnectivityCheckData(
+        checkStatus: ConnectivityCheckStatus,
         readMoreAction: OnReadMoreClicked
     ) : ConnectivityCheckCardData(
         title = R.string.orderlist_connectivity_tool_store_orders_check_title,
         suggestion = R.string.orderlist_connectivity_tool_store_orders_check_suggestion,
         icon = R.drawable.ic_clipboard,
+        connectivityCheckStatus = checkStatus,
         readMoreAction = readMoreAction
     )
+}
+
+enum class ConnectivityCheckStatus {
+    NotStarted,
+    InProgress,
+    Success,
+    Failure;
+
+    fun isFinished() = this == Success || this == Failure
 }
