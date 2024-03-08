42e80356475c616a54ef6f87d14142a305e268d6
ThomazFB <thomazfcortez@gmail.com>
2024-03-08 14:10:46 -0300

    Add the Connectivity Tool analytics event fields

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsTracker.kt
@@ -220,6 +220,7 @@ class AnalyticsTracker private constructor(
         const val KEY_CATEGORY = "category"
         const val KEY_START_PAYMENT_FLOW = "start_payment_flow"
         const val KEY_HORIZONTAL_SIZE_CLASS = "horizontal_size_class"
+        const val KEY_SUCCESS = "success"
 
         const val KEY_SORT_ORDER = "order"
         const val VALUE_DEVICE_TYPE_REGULAR = "regular"
