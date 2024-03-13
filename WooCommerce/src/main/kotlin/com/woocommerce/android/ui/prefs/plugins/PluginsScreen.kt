0a98a7684e0d6c94561e4109e5d2ac0e13b9e1d9
Ondrej Ruttkay <ruttkay@gmail.com>
2024-03-13 22:39:31 +0100

    Display the plugin version status in the plugin list

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/plugins/PluginsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/plugins/PluginsScreen.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.prefs.plugins
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,14 +17,21 @@ import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.woocommerce.android.R
+import com.woocommerce.android.extensions.isNotNullOrEmpty
 import com.woocommerce.android.ui.compose.component.ProgressIndicator
 import com.woocommerce.android.ui.compose.component.Toolbar
 import com.woocommerce.android.ui.compose.preview.LightDarkThemePreviews
 import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState
+import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loaded.Plugin.PluginStatus.INACTIVE
+import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loaded.Plugin.PluginStatus.UPDATE_AVAILABLE
+import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loaded.Plugin.PluginStatus.UP_TO_DATE
 
 @Composable
 fun PluginsScreen(viewModel: PluginsViewModel) {
@@ -68,16 +76,40 @@ private fun PluginsScreen(state: ViewState) {
 private fun Plugins(plugins: List<ViewState.Loaded.Plugin>) {
     LazyColumn {
         items(plugins) { plugin ->
-            Column(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(dimensionResource(R.dimen.major_100))
             ) {
-                Text(
-                    text = plugin.name,
-                    style = MaterialTheme.typography.subtitle1
-                )
-                Text(text = plugin.version)
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    Text(
+                        text = plugin.name,
+                        style = MaterialTheme.typography.subtitle1,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    if (plugin.authorName.isNotNullOrEmpty()) {
+                        Text(text = plugin.authorName!!, color = colorResource(id = R.color.color_on_surface_medium))
+                    }
+                }
+
+                Column {
+                    Row (modifier = Modifier.align(Alignment.End)) {
+                        Text(
+                            style = MaterialTheme.typography.subtitle1,
+                            text = plugin.version,
+                        )
+                    }
+
+                    Text(
+                        text = stringResource(id = plugin.status.title),
+                        color = colorResource(id = plugin.status.color),
+                        style = MaterialTheme.typography.subtitle2
+                    )
+                }
             }
 
             if (plugins.last() != plugin) {
@@ -98,9 +130,9 @@ private fun PreviewPlugins() {
     PluginsScreen(
         ViewState.Loaded(
             plugins = listOf(
-                ViewState.Loaded.Plugin("Plugin 1", "1.0"),
-                ViewState.Loaded.Plugin("Plugin 2", "2.0"),
-                ViewState.Loaded.Plugin("Plugin 3", "3.0")
+                ViewState.Loaded.Plugin("Plugin 1", "Automattic", "1.0", UP_TO_DATE),
+                ViewState.Loaded.Plugin("Plugin 2", null,"2.0", UPDATE_AVAILABLE),
+                ViewState.Loaded.Plugin("Plugin 3", "Gutenberg", "3.0", INACTIVE),
             )
         )
     )
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/plugins/PluginsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/plugins/PluginsViewModel.kt
@@ -1,41 +1,92 @@
 package com.woocommerce.android.ui.prefs.plugins
 
+import android.text.TextUtils
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.R
 import com.woocommerce.android.extensions.isNotNullOrEmpty
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.common.PluginRepository
 import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loaded.Plugin
+import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loaded.Plugin.PluginStatus.INACTIVE
+import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loaded.Plugin.PluginStatus.UPDATE_AVAILABLE
+import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loaded.Plugin.PluginStatus.UP_TO_DATE
 import com.woocommerce.android.ui.prefs.plugins.PluginsViewModel.ViewState.Loading
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
-import org.wordpress.android.fluxc.store.WooCommerceStore
+import org.wordpress.android.fluxc.model.plugin.ImmutablePluginModel
+import org.wordpress.android.util.AppLog
+import org.wordpress.android.util.AppLog.T.PLUGINS
+import org.wordpress.android.util.helpers.Version
 import javax.inject.Inject
 
+@OptIn(FlowPreview::class)
 @HiltViewModel
 class PluginsViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    wooStore: WooCommerceStore,
+    pluginRepository: PluginRepository,
     site: SelectedSite
 ) : ScopedViewModel(savedStateHandle) {
     private val _viewState = MutableStateFlow<ViewState>(Loading)
-    val viewState = _viewState.asLiveData()
+    val viewState = merge(
+        _viewState.take(1),
+        _viewState.drop(1).debounce(500L)
+    ).asLiveData()
 
     init {
         viewModelScope.launch {
-            val result = wooStore.fetchSitePlugins(site.get())
-            if (result.isError || result.model == null) {
-                _viewState.value = ViewState.Error
-            } else {
-                _viewState.value = ViewState.Loaded(
-                    plugins = result.model!!
-                        .filter { it.version.isNotNullOrEmpty() }
-                        .map { Plugin(it.displayName, it.version) }
-                )
-            }
+            pluginRepository.fetchInstalledPlugins(site.get()).fold(
+                onSuccess = { plugins ->
+                    _viewState.value = ViewState.Loaded(
+                        plugins = plugins
+                            .filter { it.installedVersion.isNotNullOrEmpty() && it.displayName.isNotNullOrEmpty() }
+                            .map { Plugin(it.displayName!!, it.authorName, it.installedVersion!!, it.getState()) }
+                    )
+                },
+                onFailure = { _viewState.value = ViewState.Error }
+            )
+        }
+    }
+
+    private fun ImmutablePluginModel.getState(): Plugin.PluginStatus {
+        return when {
+            !isActive -> INACTIVE
+            isUpdateAvailable() -> UPDATE_AVAILABLE
+            else -> UP_TO_DATE
+        }
+    }
+
+    private fun ImmutablePluginModel.isUpdateAvailable(): Boolean {
+        if (TextUtils.isEmpty(installedVersion)
+            || TextUtils.isEmpty(wpOrgPluginVersion)) {
+            return false
+        }
+        val installedVersionStr = installedVersion
+        val availableVersionStr = wpOrgPluginVersion
+        try {
+            val currentVersion = Version(installedVersionStr)
+            val availableVersion = Version(availableVersionStr)
+            return currentVersion < availableVersion
+        } catch (e: IllegalArgumentException) {
+            val errorStr = String.format(
+                "An IllegalArgumentException occurred while trying to compare site plugin version: %s"
+                    + " with wporg plugin version: %s", installedVersionStr, availableVersionStr
+            )
+            AppLog.e(PLUGINS, errorStr, e)
+            // If the versions are not in the expected format, we can assume that an update is available if the version
+            // values for the site plugin and wporg plugin are not the same
+            return !installedVersionStr.equals(availableVersionStr, ignoreCase = true)
         }
     }
 
@@ -51,8 +102,16 @@ class PluginsViewModel @Inject constructor(
         ) : ViewState {
             data class Plugin(
                 val name: String,
-                val version: String
-            )
+                val authorName: String?,
+                val version: String,
+                val status: PluginStatus
+            ) {
+                enum class PluginStatus(@StringRes val title: Int, @ColorRes val color: Int) {
+                    UP_TO_DATE(R.string.plugin_state_up_to_date, R.color.color_info),
+                    UPDATE_AVAILABLE(R.string.plugin_state_update_available, R.color.color_alert),
+                    INACTIVE(R.string.plugin_state_inactive, R.color.color_on_surface_disabled)
+                }
+            }
         }
     }
 }
