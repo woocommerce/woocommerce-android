f5d8082e5930fef0a3f64a258d78f97b64b80398
samiuelson <samiuelson@gmail.com>
2024-03-15 14:24:02 +0100

    Fix detekt issues

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/account/CloseAccountDialogFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/account/CloseAccountDialogFragment.kt
@@ -178,8 +178,11 @@ class CloseAccountDialogFragment : DialogFragment() {
             // In the currently used version of kotlin and compose compiler, using multiline if else breaks the build
             @Suppress("MultiLineIfElse")
             WCTextButton(
-                onClick = if (state.isAccountDeletionError) viewModel::onContactSupportClicked
-                else viewModel::onConfirmCloseAccount,
+                onClick = if (state.isAccountDeletionError) {
+                    viewModel::onContactSupportClicked
+                } else {
+                    viewModel::onConfirmCloseAccount
+                },
                 enabled = state.enteredUserName == state.currentUserName
             ) {
                 Box(
