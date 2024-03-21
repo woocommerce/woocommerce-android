4fcf0b8c045cd0cff9e5eadf14b8f71a52716306
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-03-21 17:57:30 +0000

    Remove more unused classes related to store creation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/storecreation/installation/InstallationConst.kt
+++
@@ -1,7 +0,0 @@
-package com.woocommerce.android.ui.login.storecreation.installation
-
-object InstallationConst {
-    const val STORE_LOAD_RETRIES_LIMIT = 15
-    const val INITIAL_STORE_CREATION_DELAY = 15000L
-    const val SITE_CHECK_DEBOUNCE = 5000L
-}
