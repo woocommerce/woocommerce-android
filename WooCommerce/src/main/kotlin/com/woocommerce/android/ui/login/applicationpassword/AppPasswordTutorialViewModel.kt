2dda84d723a07da30a9ce2779391ea25a1accbda
ThomazFB <thomazfcortez@gmail.com>
2024-03-25 16:48:32 -0300

    Define UI event functions in AppPasswordTutorialViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialViewModel.kt
@@ -1,4 +1,9 @@
 package com.woocommerce.android.ui.login.applicationpassword
 
 class AppPasswordTutorialViewModel {
+    fun onContinueClicked() {
+    }
+
+    fun onContactSupportClicked() {
+    }
 }
