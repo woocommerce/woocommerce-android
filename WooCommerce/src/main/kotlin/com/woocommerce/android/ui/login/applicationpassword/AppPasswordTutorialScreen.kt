a04ab66c07a2b8d992a3653e2c13b70e573f3851
ThomazFB <thomazfcortez@gmail.com>
2024-03-25 16:48:43 -0300

    Define basic UI structure in AppPasswordTutorialScreen.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialScreen.kt
@@ -1,8 +1,39 @@
 package com.woocommerce.android.ui.login.applicationpassword
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ApplicationPasswordTutorialScreen() {
+fun ApplicationPasswordTutorialScreen(viewModel: AppPasswordTutorialViewModel) {
+    ApplicationPasswordTutorialScreen(
+        onContinueClicked = viewModel::onContinueClicked,
+        onContactSupportClicked = viewModel::onContactSupportClicked
+    )
+}
 
+@Composable
+fun ApplicationPasswordTutorialScreen(
+    onContinueClicked: () -> Unit,
+    onContactSupportClicked: () -> Unit
+) {
+    Text("We couldn't log in into your store")
+
+    Button(onClick = onContinueClicked) {
+        Text("Continue")
+    }
+
+    Button(onClick = onContactSupportClicked) {
+        Text("Contact support")
+    }
+}
+
+@Preview
+@Composable
+fun ApplicationPasswordTutorialScreenPreview() {
+    ApplicationPasswordTutorialScreen(
+        onContinueClicked = { },
+        onContactSupportClicked = { }
+    )
 }
