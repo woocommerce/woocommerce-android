fdf4d32e6d843d8b6863b53b86f74b2073d339a5
ThomazFB <thomazfcortez@gmail.com>
2024-03-25 15:55:39 -0300

    Create AppPasswordTutorial types

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialFragment.kt
@@ -0,0 +1,4 @@
+package com.woocommerce.android.ui.login.applicationpassword
+
+class AppPasswordTutorialFragment {
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialScreen.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.login.applicationpassword
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ApplicationPasswordTutorialScreen() {
+
+}
