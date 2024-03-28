c21bed36579ed78a2f85b74e6907b595452a709e
Alejo <alejandro.torres.veiga@automattic.com>
2024-03-28 12:15:33 -0500

    fix unit tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/ObserveAnalyticsCardsConfigurationTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/analytics/ObserveAnalyticsCardsConfigurationTest.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.analytics
 
 import com.woocommerce.android.model.AnalyticCardConfiguration
 import com.woocommerce.android.model.AnalyticsCards
+import com.woocommerce.android.ui.analytics.hub.GetAnalyticPluginsCardActive
 import com.woocommerce.android.ui.analytics.hub.ObserveAnalyticsCardsConfiguration
 import com.woocommerce.android.ui.analytics.hub.sync.AnalyticsSettingsDataStore
 import com.woocommerce.android.ui.analytics.hub.sync.AnalyticsSettingsResourcesRepository
@@ -12,17 +13,20 @@ import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
 class ObserveAnalyticsCardsConfigurationTest : BaseUnitTest() {
     private val settingsDataStore: AnalyticsSettingsDataStore = mock()
     private val resourcesRepository: AnalyticsSettingsResourcesRepository = mock()
+    private val getAnalyticPluginsCardActive: GetAnalyticPluginsCardActive = mock()
 
     private lateinit var sut: ObserveAnalyticsCardsConfiguration
 
     private fun initializeViewModel() {
-        sut = ObserveAnalyticsCardsConfiguration(settingsDataStore, resourcesRepository)
+        sut = ObserveAnalyticsCardsConfiguration(settingsDataStore, resourcesRepository, getAnalyticPluginsCardActive)
     }
 
     @Test
@@ -33,8 +37,10 @@ class ObserveAnalyticsCardsConfigurationTest : BaseUnitTest() {
             AnalyticCardConfiguration(AnalyticsCards.Session, "Visitors", true),
             AnalyticCardConfiguration(AnalyticsCards.Products, "Products", true)
         )
+        val defaultPluginCardsActive = setOf(AnalyticsCards.Bundles)
         whenever(resourcesRepository.getDefaultAnalyticsCardsConfiguration()).thenReturn(defaultConfiguration)
         whenever(settingsDataStore.observeCardsConfiguration()).thenReturn(flowOf(null))
+        whenever(getAnalyticPluginsCardActive.invoke()).thenReturn(defaultPluginCardsActive)
 
         initializeViewModel()
         val result = sut.invoke().first()
@@ -43,25 +49,19 @@ class ObserveAnalyticsCardsConfigurationTest : BaseUnitTest() {
 
     @Test
     fun `when there is a configuration saved, the saved configuration is retrieved`() = testBlocking {
-        val defaultConfiguration = listOf(
-            AnalyticCardConfiguration(AnalyticsCards.Revenue, "Revenue", true),
-            AnalyticCardConfiguration(AnalyticsCards.Orders, "Orders", true),
-            AnalyticCardConfiguration(AnalyticsCards.Session, "Visitors", true),
-            AnalyticCardConfiguration(AnalyticsCards.Products, "Products", true)
-        )
-
+        val defaultPluginCardsActive = setOf(AnalyticsCards.Bundles)
         val savedConfiguration = listOf(
             AnalyticCardConfiguration(AnalyticsCards.Session, "Visitors", true),
             AnalyticCardConfiguration(AnalyticsCards.Orders, "Orders", true),
             AnalyticCardConfiguration(AnalyticsCards.Products, "Products", true),
             AnalyticCardConfiguration(AnalyticsCards.Revenue, "Revenue", false),
         )
-
-        whenever(resourcesRepository.getDefaultAnalyticsCardsConfiguration()).thenReturn(defaultConfiguration)
         whenever(settingsDataStore.observeCardsConfiguration()).thenReturn(flowOf(savedConfiguration))
+        whenever(getAnalyticPluginsCardActive.invoke()).thenReturn(defaultPluginCardsActive)
 
         initializeViewModel()
         val result = sut.invoke().first()
         assertThat(result).isEqualTo(savedConfiguration)
+        verify(resourcesRepository, never()).getDefaultAnalyticsCardsConfiguration()
     }
 }
