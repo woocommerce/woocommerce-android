7eae5bf15552cfd946d1c81273861a91282cb9c1
Alejo <alejandro.torres.veiga@automattic.com>
2024-03-29 07:25:59 -0500

    when cached configuration is outdated, merge it with default configuration

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/ObserveAnalyticsCardsConfiguration.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/ObserveAnalyticsCardsConfiguration.kt
@@ -1,6 +1,8 @@
 package com.woocommerce.android.ui.analytics.hub
 
 import com.woocommerce.android.model.AnalyticCardConfiguration
+import com.woocommerce.android.model.AnalyticsCards
+import com.woocommerce.android.ui.analytics.hub.settings.SaveAnalyticsCardsConfiguration
 import com.woocommerce.android.ui.analytics.hub.sync.AnalyticsSettingsDataStore
 import com.woocommerce.android.ui.analytics.hub.sync.AnalyticsSettingsResourcesRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,18 +12,37 @@ import javax.inject.Inject
 class ObserveAnalyticsCardsConfiguration @Inject constructor(
     private val settingsDataStore: AnalyticsSettingsDataStore,
     private val resourcesRepository: AnalyticsSettingsResourcesRepository,
-    private val getAnalyticPluginsCardActive: GetAnalyticPluginsCardActive
+    private val getAnalyticPluginsCardActive: GetAnalyticPluginsCardActive,
+    private val saveAnalyticsCardsConfiguration: SaveAnalyticsCardsConfiguration
 ) {
     suspend operator fun invoke(): Flow<List<AnalyticCardConfiguration>> {
         val activePluginCards = getAnalyticPluginsCardActive()
         return settingsDataStore.observeCardsConfiguration().map { currentConfiguration ->
-            currentConfiguration?.map { cardConfiguration ->
+            var configuration = currentConfiguration?.map { cardConfiguration ->
                 if (cardConfiguration.card.isPlugin && (cardConfiguration.card in activePluginCards).not()) {
                     cardConfiguration.copy(isVisible = false)
                 } else {
                     cardConfiguration
                 }
             } ?: resourcesRepository.getDefaultAnalyticsCardsConfiguration()
+
+            if (currentConfiguration != null && shouldUpgradeTheConfiguration(configuration)) {
+                configuration = upgradeConfiguration(configuration)
+                saveAnalyticsCardsConfiguration(configuration)
+            }
+
+            configuration
         }
+    }
+
+    private fun shouldUpgradeTheConfiguration(currentConfiguration: List<AnalyticCardConfiguration>): Boolean {
+        return currentConfiguration.size != AnalyticsCards.entries.size
+    }
+
+    private suspend fun upgradeConfiguration(
+        currentConfiguration: List<AnalyticCardConfiguration>
+    ): List<AnalyticCardConfiguration> {
+        val defaultConfiguration = resourcesRepository.getDefaultAnalyticsCardsConfiguration()
+        return (currentConfiguration + defaultConfiguration).distinctBy { it.card }
     }
 }
