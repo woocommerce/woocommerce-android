0f3aa2403085e46e76493ce7da11ba4b5b47c57d
ThomazFB <thomazfcortez@gmail.com>
2024-03-29 18:00:42 -0300

    Configure AppPasswordTutorialFragment basic code

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/AppPasswordTutorialFragment.kt
@@ -1,3 +1,28 @@
 package com.woocommerce.android.ui.login.applicationpassword
 
-class AppPasswordTutorialFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AppPasswordTutorialFragment : BaseFragment() {
+    val viewModel: AppPasswordTutorialViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WooThemeWithBackground {
+                    ApplicationPasswordTutorialScreen(viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
