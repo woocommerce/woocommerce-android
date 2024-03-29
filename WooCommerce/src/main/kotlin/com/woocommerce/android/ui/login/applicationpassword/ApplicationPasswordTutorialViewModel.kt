61a6f40056268c57ee8d8da945391667277034a9
ThomazFB <thomazfcortez@gmail.com>
2024-03-29 18:17:56 -0300

    Fix Hilt build issues

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/ApplicationPasswordTutorialViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/applicationpassword/ApplicationPasswordTutorialViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class ApplicationPasswordTutorialViewModel(
+class ApplicationPasswordTutorialViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
     fun onContinueClicked() {
