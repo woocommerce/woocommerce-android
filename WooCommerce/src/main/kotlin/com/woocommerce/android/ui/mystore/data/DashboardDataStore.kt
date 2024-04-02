e922bcef28801b1e388e19387f5e90ecb846af10
Ondrej Ruttkay <ruttkay@gmail.com>
2024-04-02 14:12:04 +0200

    Add data store and serializer

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/data/DashboardDataStore.kt
@@ -0,0 +1,42 @@
+package com.woocommerce.android.ui.mystore.data
+
+import androidx.datastore.core.DataStore
+import com.woocommerce.android.util.WooLog
+import com.woocommerce.android.util.WooLog.T
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+import javax.inject.Inject
+
+class DashboardDataStore @Inject constructor(
+    private val dataStore: DataStore<DashboardDataModel>
+) {
+    val dashboard: Flow<DashboardDataModel?> = dataStore.data
+        .catch { exception ->
+            // dataStore.data throws an IOException when an error is encountered when reading data
+            if (exception is IOException) {
+                WooLog.e(T.DASHBOARD, "Error reading dashboard data.", exception)
+                emit(DashboardDataModel.getDefaultInstance())
+            } else {
+                throw exception
+            }
+        }
+        .map {
+            if (it == DashboardDataModel.getDefaultInstance()) {
+                null
+            } else {
+                it
+            }
+        }
+
+    suspend fun updateDashboard(dashboard: DashboardDataModel) {
+        runCatching {
+            dataStore.updateData {
+                dashboard
+            }
+        }.onFailure {
+            WooLog.e(T.DASHBOARD, "Failed to update dashboard data")
+        }
+    }
+}
---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/mystore/data/DashboardSerializer.kt
@@ -0,0 +1,20 @@
+package com.woocommerce.android.ui.mystore.data
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+internal object DashboardSerializer : Serializer<DashboardDataModel> {
+    override val defaultValue: DashboardDataModel = DashboardDataModel.getDefaultInstance()
+    override suspend fun readFrom(input: InputStream): DashboardDataModel {
+        try {
+            return DashboardDataModel.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: DashboardDataModel, output: OutputStream) = t.writeTo(output)
+}
