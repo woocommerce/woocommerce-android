e870714f5ea03d6a41761e0262895a620a2bcbdb
Alejo <alejandro.torres.veiga@automattic.com>
2024-04-04 15:06:12 -0600

    refactor plugin url as object

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/PluginUrls.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/PluginUrls.kt
@@ -1,9 +1,7 @@
 package com.woocommerce.android.model
 
-class PluginUrls {
-    companion object{
-        const val SUBSCRIPTIONS_URL = "https://woo.com/products/woocommerce-subscriptions/"
-        const val BUNDLES_URL = "https://woo.com/products/product-bundles/"
-        const val COMPOSITE_URL = "https://woo.com/products/composite-products/"
-    }
+object PluginUrls {
+    const val SUBSCRIPTIONS_URL = "https://woo.com/products/woocommerce-subscriptions/"
+    const val BUNDLES_URL = "https://woo.com/products/product-bundles/"
+    const val COMPOSITE_URL = "https://woo.com/products/composite-products/"
 }
