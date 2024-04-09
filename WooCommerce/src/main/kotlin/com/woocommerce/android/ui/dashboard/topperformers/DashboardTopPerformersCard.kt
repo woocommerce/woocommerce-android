9fd34dcf7426e283728d904309f5466f9e7ac6a9
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-04-09 13:46:34 +0200

    Add loading state for top performers

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/topperformers/DashboardTopPerformersCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/topperformers/DashboardTopPerformersCard.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -17,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -25,6 +29,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.animations.SkeletonView
 import com.woocommerce.android.ui.compose.component.ProductThumbnail
 import com.woocommerce.android.ui.compose.preview.LightDarkThemePreviews
 import com.woocommerce.android.ui.compose.viewModelWithFactory
@@ -59,47 +64,58 @@ private fun TopPerformersCard(
             .wrapContentHeight(),
         shape = RoundedCornerShape(0.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(
-                text = stringResource(id = R.string.dashboard_top_performers_title),
-                style = MaterialTheme.typography.subtitle1,
-                fontWeight = FontWeight.Bold,
-            )
-            Row(modifier = Modifier.padding(top = 8.dp)) {
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = stringResource(id = R.string.product),
-                    style = MaterialTheme.typography.body2,
-                    color = colorResource(id = R.color.color_on_surface_medium_selector)
-                )
-                Text(
-                    text = stringResource(id = R.string.dashboard_top_performers_items_sold),
-                    style = MaterialTheme.typography.body2,
-                    color = colorResource(id = R.color.color_on_surface_medium_selector)
-                )
-            }
-            when {
-                topPerformersState?.isError == true -> TopPerformersErrorView()
-                topPerformersState?.isLoading == true -> TopPerformersLoading()
-                topPerformersState?.topPerformers.isNullOrEmpty() -> TopPerformersEmptyView()
-                else -> TopPerformerProductList(
-                    topPerformers = topPerformersState?.topPerformers!!,
-                )
-            }
+        if (topPerformersState?.isLoading == true) {
+            TopPerformersLoading(modifier = Modifier.padding(16.dp))
+        } else {
+            TopPerformersContent(topPerformersState, lastUpdateState)
+        }
+    }
+}
 
-            if (!lastUpdateState.isNullOrEmpty()) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.CenterHorizontally),
-                    text = lastUpdateState,
-                    style = MaterialTheme.typography.body2,
-                    color = colorResource(id = R.color.color_on_surface_medium_selector),
-                    textAlign = TextAlign.Center
-                )
-            }
+@Composable
+private fun TopPerformersContent(
+    topPerformersState: TopPerformersState?,
+    lastUpdateState: String?
+) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.dashboard_top_performers_title),
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold,
+        )
+        Row(modifier = Modifier.padding(top = 8.dp)) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = stringResource(id = R.string.product),
+                style = MaterialTheme.typography.body2,
+                color = colorResource(id = R.color.color_on_surface_medium_selector)
+            )
+            Text(
+                text = stringResource(id = R.string.dashboard_top_performers_items_sold),
+                style = MaterialTheme.typography.body2,
+                color = colorResource(id = R.color.color_on_surface_medium_selector)
+            )
+        }
+        when {
+            topPerformersState?.isError == true -> TopPerformersErrorView()
+            topPerformersState?.topPerformers.isNullOrEmpty() -> TopPerformersEmptyView()
+            else -> TopPerformerProductList(
+                topPerformers = topPerformersState?.topPerformers!!,
+            )
+        }
+
+        if (!lastUpdateState.isNullOrEmpty()) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                text = lastUpdateState,
+                style = MaterialTheme.typography.body2,
+                color = colorResource(id = R.color.color_on_surface_medium_selector),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -125,9 +141,74 @@ private fun TopPerformerProductList(
 }
 
 @Composable
-private fun TopPerformersLoading() {
-    Column {
-        Text("Text")
+private fun TopPerformersLoading(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        SkeletonView(
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .height(18.dp)
+                .width(200.dp)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 16.dp)
+        ) {
+            SkeletonView(
+                modifier = Modifier
+                    .height(16.dp)
+                    .width(80.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            SkeletonView(
+                modifier = Modifier
+                    .height(16.dp)
+                    .width(50.dp)
+            )
+        }
+        repeat(5) {
+            TopPerformerSkeletonItem()
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun TopPerformerSkeletonItem() {
+    Row(
+        modifier = Modifier
+            .padding(top = 16.dp, bottom = 16.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        SkeletonView(
+            modifier = Modifier
+                .height(42.dp)
+                .width(42.dp)
+                .clip(RoundedCornerShape(6.dp))
+        )
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .weight(1f)
+        ) {
+            SkeletonView(
+                modifier = Modifier
+                    .height(14.dp)
+                    .width(180.dp)
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            SkeletonView(
+                modifier = Modifier
+                    .height(12.dp)
+                    .width(150.dp)
+            )
+        }
+        SkeletonView(
+            modifier = Modifier
+                .height(14.dp)
+                .width(30.dp)
+        )
     }
 }
 
@@ -177,7 +258,7 @@ private fun TopPerformersEmptyView(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(top = 24.dp, bottom = 24.dp),
+            .padding(top = 32.dp, bottom = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Image(
