6a126fad899af81613573291828c39b55db0ccd7
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-04-10 10:18:23 +0200

    Replace woo.com domain usages in other code url definitions

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/PluginUrls.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/PluginUrls.kt
@@ -1,7 +1,7 @@
 package com.woocommerce.android.model
 
 object PluginUrls {
-    const val SUBSCRIPTIONS_URL = "https://woo.com/products/woocommerce-subscriptions/"
-    const val BUNDLES_URL = "https://woo.com/products/product-bundles/"
-    const val COMPOSITE_URL = "https://woo.com/products/composite-products/"
+    const val SUBSCRIPTIONS_URL = "https://woocommerce.com/products/woocommerce-subscriptions/"
+    const val BUNDLES_URL = "https://woocommerce.com/products/product-bundles/"
+    const val COMPOSITE_URL = "https://woocommerce.com/products/composite-products/"
 }
