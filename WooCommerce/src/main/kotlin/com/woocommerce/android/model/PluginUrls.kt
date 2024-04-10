fac10de207534cf50d7641451fd972642a4fdc31
Alejo <alejandro.torres.veiga@automattic.com>
2024-04-10 12:28:23 -0600

    add gift card plugin url

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/PluginUrls.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/PluginUrls.kt
@@ -4,4 +4,5 @@ object PluginUrls {
     const val SUBSCRIPTIONS_URL = "https://woo.com/products/woocommerce-subscriptions/"
     const val BUNDLES_URL = "https://woo.com/products/product-bundles/"
     const val COMPOSITE_URL = "https://woo.com/products/composite-products/"
+    const val GIFT_CARDS_URL = "https://woocommerce.com/products/gift-cards/"
 }
