9609a46f945507a2162af31c7d06e60082ce65f6
ThomazFB <thomazfcortez@gmail.com>
2024-04-17 17:57:07 -0300

    Remove Dark mode support from WooTheme

--- WooCommerce-Wear/src/main/java/com/woocommerce/wear/presentation/theme/Theme.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/wear/presentation/theme/Theme.kt
@@ -1,17 +1,25 @@
 package com.woocommerce.wear.presentation.theme
 
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.wear.compose.material.MaterialTheme
 
 @Composable
-fun WCAndroidTheme(
+fun WooTheme(
     content: @Composable () -> Unit
 ) {
-    /**
-     * Empty theme to customize for your app.
-     * See: https://developer.android.com/jetpack/compose/designsystems/custom
-     */
     MaterialTheme(
+        colors = WooWearColors,
+        typography = WooTypography,
         content = content
     )
+}
+
+@Composable
+private fun SurfacedContent(
+    content: @Composable () -> Unit
+) {
+    Surface(color = MaterialTheme.colors.background) {
+        content()
+    }
 }
