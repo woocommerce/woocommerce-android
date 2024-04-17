50933958c39cdfd43421eaf6c2b7be5fd3838ea9
ThomazFB <thomazfcortez@gmail.com>
2024-04-17 19:10:04 -0300

    Introduce the LoginScreen

---
+++ WooCommerce-Wear/src/main/java/com/woocommerce/wear/ui/login/LoginScreen.kt
@@ -0,0 +1,47 @@
+package com.woocommerce.wear.ui.login
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import androidx.wear.tooling.preview.devices.WearDevices
+import com.woocommerce.wear.presentation.theme.WooTheme
+
+@Composable
+fun LoginScreen() {
+    WooTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            TimeText()
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Continue on phone")
+                }
+            }
+        }
+
+    }
+}
+
+@Preview(device = WearDevices.LARGE_ROUND, showSystemUi = true)
+@Preview(device = WearDevices.SQUARE, showSystemUi = true)
+@Composable
+fun Preview() {
+    LoginScreen()
+}
