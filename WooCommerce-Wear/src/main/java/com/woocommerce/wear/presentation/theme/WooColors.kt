20221444a3b6178b9213eb5939ec78ef4958a42d
ThomazFB <thomazfcortez@gmail.com>
2024-04-17 17:56:54 -0300

    Configure WooColors with Wear types

--- WooCommerce-Wear/src/main/java/com/woocommerce/wear/presentation/theme/WooColors.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/wear/presentation/theme/WooColors.kt
@@ -1,23 +1,9 @@
 package com.woocommerce.wear.presentation.theme
 
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
+import androidx.wear.compose.material.Colors
 
 object WooColors {
-    val md_theme_light_primary = Color(0xFF674399)
-    val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-    val md_theme_light_primary_variant = Color(0xFF3C2861)
-    val md_theme_light_secondary = Color(0xFFC9356E)
-    val md_theme_light_onSecondary = Color(0xFF000000)
-    val md_theme_light_secondary_variant = Color(0xFF880E4F)
-    val md_theme_light_error = Color(0xFFD63638)
-    val md_theme_light_onError = Color(0xFFFFFFFF)
-    val md_theme_light_background = Color(0xFFF2F2F2)
-    val md_theme_light_onBackground = Color(0xFF000000)
-    val md_theme_light_surface = Color(0xFFFFFFFF)
-    val md_theme_light_onSurface = Color(0xFF000000)
-
     val md_theme_dark_primary = Color(0xFFB17FD4)
     val md_theme_dark_onPrimary = Color(0xFF000000)
     val md_theme_dark_primary_variant = Color(0xFF7F54B3)
@@ -32,22 +18,7 @@ object WooColors {
     val md_theme_dark_onSurface = Color(0xFFFFFFFF)
 }
 
-val LightColors = lightColors(
-    primary = WooColors.md_theme_light_primary,
-    primaryVariant = WooColors.md_theme_light_primary_variant,
-    secondary = WooColors.md_theme_light_secondary,
-    secondaryVariant = WooColors.md_theme_light_secondary_variant,
-    background = WooColors.md_theme_light_background,
-    surface = WooColors.md_theme_light_surface,
-    error = WooColors.md_theme_light_error,
-    onPrimary = WooColors.md_theme_light_onPrimary,
-    onSecondary = WooColors.md_theme_light_onSecondary,
-    onBackground = WooColors.md_theme_light_onBackground,
-    onSurface = WooColors.md_theme_light_onSurface,
-    onError = WooColors.md_theme_light_onError,
-)
-
-val DarkColors = darkColors(
+val WooWearColors = Colors(
     primary = WooColors.md_theme_dark_primary,
     primaryVariant = WooColors.md_theme_dark_primary_variant,
     secondary = WooColors.md_theme_dark_secondary,
