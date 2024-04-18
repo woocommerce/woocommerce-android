0c64f88c47df6990750616e9c421182a96527d36
ThomazFB <thomazfcortez@gmail.com>
2024-04-18 19:37:25 -0300

    Finish assisted injection configuration

--- WooCommerce-Wear/src/main/java/com/woocommerce/wear/ui/login/LoginViewModel.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/wear/ui/login/LoginViewModel.kt
@@ -5,7 +5,9 @@ import androidx.navigation.NavHostController
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel(assistedFactory = LoginViewModel.Factory::class)
 class LoginViewModel @AssistedInject constructor (
     private val loginRepository: LoginRepository,
     @Assisted private val navController: NavHostController
