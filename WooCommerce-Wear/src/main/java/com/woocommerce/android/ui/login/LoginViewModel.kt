9175b03f7bcdd8a6b33f393f411fb7af3a78c5ca
ThomazFB <thomazfcortez@gmail.com>
2024-04-23 17:54:25 -0300

    Implement initial structure for LoginViewModel

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginViewModel.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginViewModel.kt
@@ -1,21 +1,52 @@
 package com.woocommerce.android.ui.login
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import androidx.navigation.NavHostController
+import com.woocommerce.android.ui.NavRoutes.MY_STORE
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 @HiltViewModel(assistedFactory = LoginViewModel.Factory::class)
 class LoginViewModel @AssistedInject constructor(
     private val loginRepository: LoginRepository,
-    @Assisted private val navController: NavHostController
-) : ViewModel() {
+    @Assisted private val navController: NavHostController,
+    savedState: SavedStateHandle
+) : ScopedViewModel(savedState) {
+    private val _viewState = savedState.getStateFlow(
+        scope = this,
+        initialValue = ViewState()
+    )
+    val viewState = _viewState.asLiveData()
+
+    init {
+        launch { observeLoginChanges() }
+    }
+
+    private suspend fun observeLoginChanges() {
+        loginRepository.isUserLoggedIn().collect { isLoggedIn ->
+            if (isLoggedIn) {
+                navController.navigate(MY_STORE.route)
+            } else {
+                _viewState.update { it.copy(isLoading = false) }
+            }
+        }
+    }
+
     fun onLoginButtonClicked() {
-        loginRepository.apply { }
         navController.navigate("myStore")
     }
+
+    data class ViewState(
+        val isLoading: Boolean = true,
+        val isSyncButtonVisible: Boolean = false
+    )
 
     @AssistedFactory
     interface Factory {
