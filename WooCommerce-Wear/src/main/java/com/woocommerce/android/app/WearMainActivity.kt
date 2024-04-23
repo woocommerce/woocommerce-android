47096fab2e2c65105025e483caa3638948ce052f
ThomazFB <thomazfcortez@gmail.com>
2024-04-23 17:35:42 -0300

    Move Navigation and WearMainActivity to app package

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/app/WearMainActivity.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/app/WearMainActivity.kt
@@ -1,9 +1,10 @@
-package com.woocommerce.android.ui
+package com.woocommerce.android.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.woocommerce.android.app.WooWearNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
