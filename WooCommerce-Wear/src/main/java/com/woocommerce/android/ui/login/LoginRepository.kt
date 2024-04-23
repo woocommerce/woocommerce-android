70c76a06d1aa103492070a856519b979045ab125
ThomazFB <thomazfcortez@gmail.com>
2024-04-23 17:29:13 -0300

    Create initial dataStore usage for login

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginRepository.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginRepository.kt
@@ -1,10 +1,20 @@
 package com.woocommerce.android.ui.login
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.woocommerce.android.datastore.DataStoreQualifier
+import com.woocommerce.android.datastore.DataStoreType
 import javax.inject.Inject
+import kotlinx.coroutines.flow.map
 
-class LoginRepository @Inject constructor() {
-    @Suppress("FunctionOnlyReturningConstant")
-    fun isUserLoggedIn(): Boolean {
-        return false
+class LoginRepository @Inject constructor(
+    @DataStoreQualifier(DataStoreType.LOGIN) private val loginDataStore: DataStore<Preferences>
+) {
+    fun isUserLoggedIn() = loginDataStore.data
+        .map { it[stringPreferencesKey(generateStoreConfigKey())].isNullOrEmpty().not() }
+
+    private fun generateStoreConfigKey(): String {
+        return "store_config_key"
     }
 }
