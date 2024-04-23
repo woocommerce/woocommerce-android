b2a0566a7dbc36caeb8342dc1bd8ae5ad1290cb8
ThomazFB <thomazfcortez@gmail.com>
2024-04-23 17:28:54 -0300

    Introduce navigation route names constants

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/Navigation.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/Navigation.kt
@@ -6,6 +6,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.woocommerce.android.ui.NavRoutes.LOGIN
+import com.woocommerce.android.ui.NavRoutes.MY_STORE
 import com.woocommerce.android.ui.login.LoginScreen
 import com.woocommerce.android.ui.login.LoginViewModel
 import com.woocommerce.android.ui.mystore.MyStoreScreen
@@ -19,14 +21,20 @@ fun WooWearNavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable("login") {
+        composable(LOGIN.route) {
             val viewModel: LoginViewModel = hiltViewModel<LoginViewModel, LoginViewModel.Factory> {
                 it.create(navController)
             }
             LoginScreen(viewModel)
         }
-        composable("myStore") {
+        composable(MY_STORE.route) {
             MyStoreScreen()
         }
     }
+}
+
+enum class NavRoutes(val route: String) {
+    LOGIN("login"),
+    MY_STORE("myStore")
+
 }
