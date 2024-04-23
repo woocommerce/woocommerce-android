ad8ac2d3e56702869e9bb867e81b9c0130b7ff0f
ThomazFB <thomazfcortez@gmail.com>
2024-04-23 15:24:51 -0300

    Remove test code

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/MainActivity.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.woocommerce.android.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.google.android.gms.wearable.Wearable
-import com.woocommerce.android.sync.ConnectWearableUseCase
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject lateinit var connectWearable: ConnectWearableUseCase
-
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
@@ -22,21 +16,5 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent { WooWearNavHost() }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val dataClient = Wearable.getDataClient(this)
-        connectWearable(
-            dataClient,
-            Wearable.getMessageClient(this),
-            Wearable.getCapabilityClient(this)
-        )
-
-        dataClient.addListener {
-            it.forEach {
-                Log.d("DATA LAYER", "Data item received: $it")
-            }
-        }
     }
 }
