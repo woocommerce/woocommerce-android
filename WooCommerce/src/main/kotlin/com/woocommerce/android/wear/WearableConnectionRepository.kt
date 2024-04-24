8ff23c0fae1493c513956f6b440e9bfefd12c59c
ThomazFB <thomazfcortez@gmail.com>
2024-04-24 18:45:02 -0300

    Adjust Data client parameters to use the ones in the commons library

--- WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionRepository.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.wear
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataMap
 import com.google.android.gms.wearable.PutDataMapRequest
+import com.woocommerce.commons.wear.DataParameters.TIMESTAMP
 import com.woocommerce.commons.wear.DataPath
 import java.time.Instant
 import javax.inject.Inject
@@ -23,6 +24,6 @@ class WearableConnectionRepository @Inject constructor(
 
     private fun PutDataMapRequest.addData(data: DataMap) = apply {
         dataMap.putAll(data)
-        dataMap.putLong("timestamp", Instant.now().epochSecond)
+        dataMap.putLong(TIMESTAMP.value, Instant.now().epochSecond)
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionService.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionService.kt
@@ -5,6 +5,8 @@ import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.DataMap
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
+import com.woocommerce.commons.wear.DataParameters
+import com.woocommerce.commons.wear.DataParameters.TOKEN
 import com.woocommerce.commons.wear.DataPath.AUTH_DATA
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -25,7 +27,7 @@ class WearableConnectionService : WearableListenerService() {
         Log.d(TAG, "onMessageReceived: ${message.path}")
         connRepository.sendDataToAllNodes(
             path = AUTH_DATA,
-            data = DataMap().apply { putString("token", "a-test-token") }
+            data = DataMap().apply { putString(TOKEN.value, "a-test-token") }
         )
         Log.d(TAG, "${message.path} replied")
     }
