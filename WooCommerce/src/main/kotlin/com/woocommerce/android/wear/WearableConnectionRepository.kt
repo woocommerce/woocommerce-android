2281085d9f822a66f01d6539866174988271bfab
ThomazFB <thomazfcortez@gmail.com>
2024-04-24 14:21:08 -0300

    Add initial auth request message handling

--- WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionRepository.kt
@@ -3,20 +3,26 @@ package com.woocommerce.android.wear
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataMap
 import com.google.android.gms.wearable.PutDataMapRequest
+import java.time.Instant
 import javax.inject.Inject
 
 class WearableConnectionRepository @Inject constructor(
     private val dataClient: DataClient
 ) {
-    private fun sendDataToAllNodes(
+    fun sendDataToAllNodes(
         path: MessagePath,
         data: DataMap = DataMap()
     ) {
         PutDataMapRequest
             .create(path.value)
-            .apply { dataMap.putAll(data) }
+            .addData(data)
             .asPutDataRequest().setUrgent()
             .let { dataClient.putDataItem(it) }
+    }
+
+    private fun PutDataMapRequest.addData(data: DataMap) = apply {
+        dataMap.putAll(data)
+        dataMap.putLong("timestamp", Instant.now().epochSecond)
     }
 
     enum class MessagePath(val value: String) {
--- WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionService.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionService.kt
@@ -2,18 +2,35 @@ package com.woocommerce.android.wear
 
 import android.util.Log
 import com.google.android.gms.wearable.DataEventBuffer
+import com.google.android.gms.wearable.DataMap
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
+import com.woocommerce.android.wear.WearableConnectionRepository.MessagePath.AUTH
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class WearableConnectionService : WearableListenerService() {
+
+    @Inject
+    lateinit var connRepository: WearableConnectionRepository
 
     override fun onDataChanged(dataEvents: DataEventBuffer) {
         super.onDataChanged(dataEvents)
-        Log.d("DATA RECEIVED", "onDataChanged")
+        Log.d(TAG, "onDataChanged")
     }
 
     override fun onMessageReceived(message: MessageEvent) {
         super.onMessageReceived(message)
-        Log.d("MESSAGE RECEIVED", "onMessageReceived")
+        Log.d(TAG, "onMessageReceived: ${message.path}")
+        connRepository.sendDataToAllNodes(
+            path = AUTH,
+            data = DataMap().apply { putString("token", "a-test-token") }
+        )
+        Log.d(TAG, "${message.path} replied")
+    }
+
+    companion object {
+        private const val TAG = "WearableConnectionService"
     }
 }
