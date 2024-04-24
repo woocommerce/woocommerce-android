f85e5a38dca6ced825715558456666ca29266b18
ThomazFB <thomazfcortez@gmail.com>
2024-04-24 12:53:33 -0300

    Introduce initial message trigger to phone

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginViewModel.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginViewModel.kt
@@ -1,8 +1,11 @@
 package com.woocommerce.android.ui.login
 
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import androidx.navigation.NavHostController
+import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.MessageClient
 import com.woocommerce.android.ui.NavRoutes.MY_STORE
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
@@ -10,12 +13,18 @@ import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.parcelize.Parcelize
 
 @HiltViewModel(assistedFactory = LoginViewModel.Factory::class)
 class LoginViewModel @AssistedInject constructor(
     private val loginRepository: LoginRepository,
+    private val messageClient: MessageClient,
+    private val capabilityClient: CapabilityClient,
     @Assisted private val navController: NavHostController,
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
@@ -40,13 +49,36 @@ class LoginViewModel @AssistedInject constructor(
     }
 
     fun onLoginButtonClicked() {
-        navController.navigate("myStore")
+        _viewState.update { it.copy(isLoading = true) }
+        launch {
+            fetchReachableNodes()
+                .map { (node, _) ->
+                    async {
+                        messageClient.sendMessage(node.id, "/request_auth", byteArrayOf())
+                    }
+                }.awaitAll()
+
+            _viewState.update { it.copy(isLoading = false) }
+        }
     }
 
+    private suspend fun fetchReachableNodes() = capabilityClient
+        .getAllCapabilities(CapabilityClient.FILTER_REACHABLE)
+        .await()
+        .flatMap { (capability, capabilityInfo) ->
+            capabilityInfo.nodes.map { it to capability }
+        }
+        .groupBy(
+            keySelector = { it.first },
+            valueTransform = { it.second }
+        )
+        .mapValues { it.value.toSet() }
+
+    @Parcelize
     data class ViewState(
         val isLoading: Boolean = true,
         val isSyncButtonVisible: Boolean = false
-    )
+    ) : Parcelable
 
     @AssistedFactory
     interface Factory {
