8f8250ec34de262e88ca3ba5be767cf150a71e6a
ThomazFB <thomazfcortez@gmail.com>
2024-04-24 19:41:04 -0300

    Introduce site data handling

--- WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionRepository.kt
@@ -3,10 +3,13 @@ package com.woocommerce.android.wear
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataMap
 import com.google.android.gms.wearable.PutDataMapRequest
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.commons.wear.DataParameters
+import com.woocommerce.commons.wear.DataParameters.SITE_ID
 import com.woocommerce.commons.wear.DataParameters.TIMESTAMP
 import com.woocommerce.commons.wear.DataParameters.TOKEN
 import com.woocommerce.commons.wear.DataPath
+import com.woocommerce.commons.wear.DataPath.SITE_DATA
 import com.woocommerce.commons.wear.DataPath.TOKEN_DATA
 import java.time.Instant
 import javax.inject.Inject
@@ -14,15 +17,37 @@ import org.wordpress.android.fluxc.store.AccountStore
 
 class WearableConnectionRepository @Inject constructor(
     private val dataClient: DataClient,
-    private val accountStore: AccountStore
+    private val accountStore: AccountStore,
+    private val selectedSite: SelectedSite
 ) {
     fun sendTokenData() {
+        sendData(
+            TOKEN_DATA,
+            DataMap().apply {
+                putString(TOKEN.value, accountStore.accessToken.orEmpty())
+                putLong(TIMESTAMP.value, Instant.now().epochSecond)
+            }
+        )
+    }
+
+    fun sendSiteData() {
+        sendData(
+            SITE_DATA,
+            DataMap().apply {
+                putString(SITE_ID.value, selectedSite.get().id.toString())
+                putLong(TIMESTAMP.value, Instant.now().epochSecond)
+            }
+        )
+    }
+
+    private fun sendData(
+        dataPath: DataPath,
+        dataMap: DataMap
+    ) {
         PutDataMapRequest
-            .create(TOKEN_DATA.value)
-            .apply {
-                dataMap.putString(TOKEN.value, accountStore.accessToken.orEmpty())
-                dataMap.putLong(TIMESTAMP.value, Instant.now().epochSecond)
-            }.asPutDataRequest().setUrgent()
+            .create(dataPath.value)
+            .apply { dataMap.putAll(dataMap) }
+            .asPutDataRequest().setUrgent()
             .let { dataClient.putDataItem(it) }
     }
 }
