6deba9c780875ae76ee3f543821b541389cd3c91
ThomazFB <thomazfcortez@gmail.com>
2024-04-25 18:38:01 -0300

    Observe selectedSite changes in the WearableConnectionService

--- WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionService.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/wear/WearableConnectionService.kt
@@ -4,16 +4,34 @@ import android.util.Log
 import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.commons.wear.MessagePath.REQUEST_SITE
 import com.woocommerce.commons.wear.MessagePath.REQUEST_TOKEN
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class WearableConnectionService : WearableListenerService() {
 
     @Inject
     lateinit var connRepository: WearableConnectionRepository
+
+    @Inject
+    lateinit var coroutineScope: CoroutineScope
+
+    @Inject
+    lateinit var selectedSite: SelectedSite
+
+    override fun onCreate() {
+        super.onCreate()
+        coroutineScope.launch {
+            selectedSite.observe().collect {
+                connRepository.sendTokenData()
+            }
+        }
+    }
 
     override fun onDataChanged(dataEvents: DataEventBuffer) {
         super.onDataChanged(dataEvents)
