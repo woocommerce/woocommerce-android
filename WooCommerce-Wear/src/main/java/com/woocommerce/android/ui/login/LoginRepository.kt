76aef7ae4c092ac42e61f166f3b9034605c04f12
ThomazFB <thomazfcortez@gmail.com>
2024-04-25 18:37:41 -0300

    Refactor how token is retrieved from data store

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginRepository.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginRepository.kt
@@ -12,7 +12,7 @@ import com.woocommerce.commons.wear.DataParameters.SITE_JSON
 import com.woocommerce.commons.wear.DataParameters.TOKEN
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import org.wordpress.android.fluxc.model.SiteModel
 
 class LoginRepository @Inject constructor(
@@ -23,15 +23,15 @@ class LoginRepository @Inject constructor(
     val currentSite
         get() = loginDataStore.data
             .map { it[stringPreferencesKey(CURRENT_SITE_KEY)] }
+            .distinctUntilChanged()
             .map { it?.let { gson.fromJson(it, SiteModel::class.java) } }
 
     val isUserLoggedIn
-        get() = combine(loginDataStore.data, currentSite) { prefs, site ->
-            site?.takeIf { it.siteId > 0 }
-                ?.let { prefs[stringPreferencesKey(generateTokenKey(it.siteId))] }
-                ?.isNotEmpty()
-                ?: false
-        }
+        get() = loginDataStore.data
+            .map { prefs ->
+                searchToken(prefs)?.isNotEmpty()
+                    ?: false
+            }
 
     suspend fun receiveStoreData(data: DataMap) {
         val siteJSON = data.getString(SITE_JSON.value)
@@ -46,6 +46,12 @@ class LoginRepository @Inject constructor(
             }
         }
     }
+
+    private fun searchToken(prefs: Preferences) =
+        prefs[stringPreferencesKey(CURRENT_SITE_KEY)]
+            ?.let { gson.fromJson(it, SiteModel::class.java) }
+            ?.takeIf { it.siteId > 0 }
+            ?.let { prefs[stringPreferencesKey(generateTokenKey(it.siteId))] }
 
     private fun generateTokenKey(siteId: Long) = "$TOKEN_KEY:$siteId"
 
