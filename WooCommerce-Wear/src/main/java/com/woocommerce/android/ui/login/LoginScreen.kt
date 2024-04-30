7c22212c0d8c624b22eec3bf0e7b59fe79b70fdf
ThomazFB <thomazfcortez@gmail.com>
2024-04-29 23:48:21 -0300

    Properly define the LoginScreen error UI structure

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.woocommerce.android.ui.login
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
@@ -17,6 +20,7 @@ import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.woocommerce.android.presentation.theme.WooTheme
+import com.woocommerce.android.presentation.theme.WooTypography
 
 @Composable
 fun LoginScreen(viewModel: LoginViewModel) {
@@ -37,8 +41,8 @@ fun LoginScreen(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            TimeText()
             if (isLoading) {
+                TimeText()
                 Text("Loading...")
             } else {
                 SyncScreen(onLoginButtonClicked)
@@ -49,16 +53,34 @@ fun LoginScreen(
 
 @Composable
 fun SyncScreen(
-    onLoginButtonClicked: () -> Unit
+    onLoginButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Log in",
+            style = WooTypography.body1
+        )
+        Text(
+            text = "Something went wrong",
+            style = WooTypography.body2
+        )
+        Text(
+            text = "Make sure your phone is nearby with the Woo app installed and Bluetooth is on.",
+            textAlign = TextAlign.Center,
+            style = WooTypography.caption2
+        )
         Button(
             onClick = onLoginButtonClicked,
-            modifier = Modifier.fillMaxWidth()
+            modifier = modifier.fillMaxWidth()
         ) {
-            Text("Continue on phone")
+            Text("Try Again")
         }
     }
 }
