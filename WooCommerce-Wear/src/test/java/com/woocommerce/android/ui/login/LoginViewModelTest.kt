37286102fad578382ed20b76e46fa90c5d6978f7
ThomazFB <thomazfcortez@gmail.com>
2024-04-30 16:35:52 -0300

    Introduce new test during LoginViewModel startup

--- WooCommerce-Wear/src/test/java/com/woocommerce/android/ui/login/LoginViewModelTest.kt
+++ WooCommerce-Wear/src/test/java/com/woocommerce/android/ui/login/LoginViewModelTest.kt
@@ -5,11 +5,13 @@ import androidx.navigation.NavHostController
 import com.woocommerce.android.BaseUnitTest
 import com.woocommerce.android.phone.PhoneConnectionRepository
 import com.woocommerce.android.ui.NavRoutes
-import com.woocommerce.commons.wear.MessagePath
+import com.woocommerce.commons.wear.MessagePath.REQUEST_SITE
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.mockito.Mockito.never
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -57,7 +59,7 @@ class LoginViewModelTest : BaseUnitTest() {
         sut.onTryAgainClicked()
 
         // Then
-        verify(phoneConnectionRepository).sendMessage(MessagePath.REQUEST_SITE)
+        verify(phoneConnectionRepository).sendMessage(REQUEST_SITE)
     }
 
     @Test
@@ -74,6 +76,35 @@ class LoginViewModelTest : BaseUnitTest() {
         // Then
         assertThat(isLoading).isNotNull()
         assertThat(isLoading).isTrue
+    }
+
+    @Test
+    fun `on viewModel init, then send REQUEST_SITE message and observe the logged in status`() = testBlocking {
+        // Given
+        whenever(loginRepository.isUserLoggedIn).thenReturn(flowOf(false))
+        whenever(phoneConnectionRepository.sendMessage(REQUEST_SITE))
+            .doReturn(Result.success(Unit))
+
+        // When
+        createSut()
+
+        // Then
+        verify(phoneConnectionRepository).sendMessage(REQUEST_SITE)
+        verify(loginRepository).isUserLoggedIn
+    }
+
+    @Test
+    fun `on viewModel init, then fail REQUEST_SITE message and cancel loading`() = testBlocking {
+        // Given
+        whenever(phoneConnectionRepository.sendMessage(REQUEST_SITE))
+            .doReturn(Result.failure(Exception("")))
+
+        // When
+        createSut()
+
+        // Then
+        verify(phoneConnectionRepository).sendMessage(REQUEST_SITE)
+        verify(loginRepository, never()).isUserLoggedIn
     }
 
     private fun createSut() {
