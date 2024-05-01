449a6a340cbd4b51a205a4b3c28cdf1e6d76bbb2
ThomazFB <thomazfcortez@gmail.com>
2024-05-01 03:40:59 -0300

    Simplify how the ObserveLoginRequest timeout logic works

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/ObserveLoginRequest.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/ObserveLoginRequest.kt
@@ -1,32 +1,47 @@
 package com.woocommerce.android.ui.login
 
-import com.woocommerce.android.phone.PhoneConnectionRepository
-import com.woocommerce.android.phone.PhoneConnectionRepository.RequestState
 import com.woocommerce.android.ui.login.ObserveLoginRequest.LoginRequestState.Failed
 import com.woocommerce.android.ui.login.ObserveLoginRequest.LoginRequestState.Logged
 import com.woocommerce.android.ui.login.ObserveLoginRequest.LoginRequestState.Waiting
-import com.woocommerce.commons.wear.MessagePath.REQUEST_SITE
 import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class ObserveLoginRequest @Inject constructor(
-    private val loginRepository: LoginRepository,
-    private val phoneRepository: PhoneConnectionRepository,
+    private val loginRepository: LoginRepository
 ) {
     operator fun invoke() = combine(
         loginRepository.isUserLoggedIn,
-        phoneRepository.requestState
-    ) { isUserLoggedIn, requestState ->
+        timeoutFlow
+    ) { isUserLoggedIn, timeoutState ->
         when {
             isUserLoggedIn -> Logged
-            requestState == RequestState.Waiting(REQUEST_SITE) -> Waiting
+            timeoutState == LoginTimeoutState.Waiting -> Waiting
             else -> Failed
         }
     }
+
+    private val timeoutFlow: Flow<LoginTimeoutState>
+        get() = flow {
+            emit(LoginTimeoutState.Waiting)
+            delay(TIMEOUT_MILLIS)
+            emit(LoginTimeoutState.Timeout)
+        }
 
     enum class LoginRequestState {
         Logged,
         Waiting,
         Failed
+    }
+
+    enum class LoginTimeoutState {
+        Waiting,
+        Timeout
+    }
+
+    companion object {
+        const val TIMEOUT_MILLIS = 20000L
     }
 }
