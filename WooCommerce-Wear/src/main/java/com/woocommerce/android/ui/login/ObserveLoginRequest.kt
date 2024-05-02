e2e565631978a96d5585f6050bf7a5c70e012135
ThomazFB <thomazfcortez@gmail.com>
2024-05-02 17:17:02 -0300

    Fix lint issues

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/ObserveLoginRequest.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/ObserveLoginRequest.kt
@@ -3,11 +3,11 @@ package com.woocommerce.android.ui.login
 import com.woocommerce.android.ui.login.ObserveLoginRequest.LoginRequestState.Failed
 import com.woocommerce.android.ui.login.ObserveLoginRequest.LoginRequestState.Logged
 import com.woocommerce.android.ui.login.ObserveLoginRequest.LoginRequestState.Waiting
-import kotlinx.coroutines.flow.combine
-import javax.inject.Inject
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
 class ObserveLoginRequest @Inject constructor(
     private val loginRepository: LoginRepository
