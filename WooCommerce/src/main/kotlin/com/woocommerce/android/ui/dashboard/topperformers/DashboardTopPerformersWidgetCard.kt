7c677c0dc684b8d5fe2a96c36b0ede48d1a04a20
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-05-02 22:19:48 +0100

    Introduce a common header for stats cards

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/topperformers/DashboardTopPerformersWidgetCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/topperformers/DashboardTopPerformersWidgetCard.kt
@@ -2,23 +2,16 @@ package com.woocommerce.android.ui.dashboard.topperformers
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -27,21 +20,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -52,7 +40,6 @@ import com.woocommerce.android.model.DashboardWidget
 import com.woocommerce.android.ui.analytics.ranges.StatsTimeRange
 import com.woocommerce.android.ui.analytics.ranges.StatsTimeRangeSelection
 import com.woocommerce.android.ui.analytics.ranges.StatsTimeRangeSelection.SelectionType
-import com.woocommerce.android.ui.analytics.ranges.toDashboardLocalizedGranularityStringId
 import com.woocommerce.android.ui.compose.animations.SkeletonView
 import com.woocommerce.android.ui.compose.component.ProductThumbnail
 import com.woocommerce.android.ui.compose.preview.LightDarkThemePreviews
@@ -60,13 +47,13 @@ import com.woocommerce.android.ui.compose.rememberNavController
 import com.woocommerce.android.ui.compose.viewModelWithFactory
 import com.woocommerce.android.ui.dashboard.DashboardFragmentDirections
 import com.woocommerce.android.ui.dashboard.DashboardViewModel
-import com.woocommerce.android.ui.dashboard.DashboardViewModel.Companion.SUPPORTED_RANGES_ON_MY_STORE_TAB
 import com.woocommerce.android.ui.dashboard.DashboardViewModel.DashboardEvent.OpenRangePicker
 import com.woocommerce.android.ui.dashboard.DashboardViewModel.DashboardWidgetAction
 import com.woocommerce.android.ui.dashboard.DashboardViewModel.DashboardWidgetMenu
 import com.woocommerce.android.ui.dashboard.TopPerformerProductUiModel
 import com.woocommerce.android.ui.dashboard.WidgetCard
 import com.woocommerce.android.ui.dashboard.WidgetError
+import com.woocommerce.android.ui.dashboard.stats.DashboardStatsHeader
 import com.woocommerce.android.ui.dashboard.topperformers.DashboardTopPerformersViewModel.OpenAnalytics
 import com.woocommerce.android.ui.dashboard.topperformers.DashboardTopPerformersViewModel.OpenDatePicker
 import com.woocommerce.android.ui.dashboard.topperformers.DashboardTopPerformersViewModel.OpenTopPerformer
@@ -192,11 +179,14 @@ private fun TopPerformersContent(
     onEditCustomRangeTapped: () -> Unit,
 ) {
     Column {
-        TopPerformersDatePicker(
-            selectedDateRange,
-            onGranularityChanged,
-            onEditCustomRangeTapped,
-        )
+        selectedDateRange?.let {
+            DashboardStatsHeader(
+                rangeSelection = it,
+                dateFormatted = it.currentRangeDescription,
+                onCustomRangeClick = onEditCustomRangeTapped,
+                onTabSelected = onGranularityChanged
+            )
+        }
         Divider(modifier = Modifier.padding(bottom = 16.dp))
         Row(
             modifier = Modifier.padding(horizontal = 16.dp)
@@ -234,108 +224,6 @@ private fun TopPerformersContent(
                 color = colorResource(id = R.color.color_on_surface_medium_selector),
                 textAlign = TextAlign.Center
             )
-        }
-    }
-}
-
-@Composable
-private fun TopPerformersDatePicker(
-    selectedDateRange: StatsTimeRangeSelection?,
-    onGranularityChanged: (SelectionType) -> Unit,
-    onCustomRangeClicked: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val isCustomRange = selectedDateRange?.selectionType == StatsTimeRangeSelection.SelectionType.CUSTOM
-    Row(
-        modifier = modifier.padding(start = 16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Text(
-            text = stringResource(
-                id = selectedDateRange?.selectionType?.toDashboardLocalizedGranularityStringId() ?: 0
-            ),
-            style = MaterialTheme.typography.body2,
-        )
-        Row(
-            modifier = Modifier
-                .clickable(enabled = isCustomRange) { onCustomRangeClicked() }
-                .weight(1f)
-        ) {
-            Text(
-                text = selectedDateRange?.currentRangeDescription ?: "",
-                style = MaterialTheme.typography.body2,
-                color = if (isCustomRange) {
-                    colorResource(id = R.color.color_primary)
-                } else {
-                    colorResource(id = R.color.color_on_surface_medium_selector)
-                }
-            )
-            if (isCustomRange) {
-                Icon(
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                        .size(16.dp),
-                    painter = painterResource(id = R.drawable.ic_edit_pencil),
-                    contentDescription = "",
-                    tint = colorResource(id = R.color.color_primary)
-                )
-            }
-        }
-        TopPerformersDateGranularityDropDown(
-            selectedDateRange = selectedDateRange,
-            dateGranularityOptions = SUPPORTED_RANGES_ON_MY_STORE_TAB,
-            onGranularityChanged = onGranularityChanged
-        )
-    }
-}
-
-@Composable
-private fun TopPerformersDateGranularityDropDown(
-    selectedDateRange: StatsTimeRangeSelection?,
-    dateGranularityOptions: List<SelectionType>,
-    onGranularityChanged: (SelectionType) -> Unit
-) {
-    var showDropDown by remember { mutableStateOf(false) }
-    Box {
-        IconButton(onClick = { showDropDown = !showDropDown }) {
-            Icon(
-                imageVector = Icons.Default.DateRange,
-                tint = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium),
-                contentDescription = stringResource(R.string.dashboard_stats_edit_granularity_content_description),
-            )
-        }
-        DropdownMenu(
-            modifier = Modifier.width(250.dp),
-            offset = DpOffset(
-                x = dimensionResource(id = R.dimen.major_100),
-                y = 0.dp
-            ),
-            expanded = showDropDown,
-            onDismissRequest = { showDropDown = false }
-        ) {
-            dateGranularityOptions.forEach { granularity ->
-                DropdownMenuItem(
-                    modifier = Modifier.height(40.dp),
-                    onClick = {
-                        onGranularityChanged(granularity)
-                        showDropDown = false
-                    }
-                ) {
-                    Text(
-                        modifier = Modifier.weight(1f),
-                        text = stringResource(id = granularity.toDashboardLocalizedGranularityStringId())
-                    )
-                    if (selectedDateRange?.selectionType == granularity) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_menu_check),
-                            contentDescription = stringResource(
-                                id = R.string.dashboard_stats_edit_granularity_selected_content_description
-                            )
-                        )
-                    }
-                }
-            }
         }
     }
 }
