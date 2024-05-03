07ba6ec37383d399c510ec85c4469bcc519ca743
ThomazFB <thomazfcortez@gmail.com>
2024-05-02 22:01:27 -0300

    Implement initial Revenue fetching

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/mystore/StatsRepository.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/mystore/StatsRepository.kt
@@ -1,21 +1,46 @@
 package com.woocommerce.android.ui.mystore
 
-import com.woocommerce.android.ui.login.LoginRepository
+import com.woocommerce.commons.extensions.formatToYYYYmmDDhhmmss
 import org.wordpress.android.fluxc.store.WCStatsStore
 import java.util.Calendar
 import java.util.Locale
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.WCRevenueStatsModel
+import org.wordpress.android.fluxc.store.WCStatsStore.FetchRevenueStatsPayload
+import org.wordpress.android.fluxc.store.WCStatsStore.StatsGranularity
 
 class StatsRepository(
-    private val loginRepository: LoginRepository,
     private val wcStatsStore: WCStatsStore,
 ) {
-    fun fetchStoreStats() {
-        val locale = Locale.getDefault()
-        val selectedSite = loginRepository.currentSite ?: return
+    suspend fun fetchStoreStats(
+        selectedSite: SiteModel
+    ): Result<WCRevenueStatsModel?> {
         val todayRange = TodayRangeData(
             selectedSite = selectedSite,
-            locale = locale,
+            locale = Locale.getDefault(),
             referenceCalendar = Calendar.getInstance()
+        ).currentRange
+
+        val result = wcStatsStore.fetchRevenueStats(
+            FetchRevenueStatsPayload(
+                site = selectedSite,
+                granularity = StatsGranularity.DAYS,
+                startDate = todayRange.start.formatToYYYYmmDDhhmmss(),
+                endDate = todayRange.end.formatToYYYYmmDDhhmmss()
+            )
         )
+
+        return when {
+            result.isError -> Result.failure(Exception())
+            else -> {
+                val revenueStatsModel = wcStatsStore.getRawRevenueStats(
+                    selectedSite,
+                    result.granularity,
+                    result.startDate!!,
+                    result.endDate!!
+                )
+                Result.success(revenueStatsModel)
+            }
+        }
     }
 }
