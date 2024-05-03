5113944be1b20c29f1c254a4d1d3e0bf2c3a00cc
ThomazFB <thomazfcortez@gmail.com>
2024-05-03 03:35:23 -0300

    Configure the AppSecrets injection

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/di/AppConfigModule.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/di/AppConfigModule.kt
@@ -1,12 +1,14 @@
 package com.woocommerce.android.di
 
 import android.content.Context
+import com.woocommerce.android.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.wordpress.android.fluxc.network.UserAgent
 import java.util.Locale
+import org.wordpress.android.fluxc.network.rest.wpcom.auth.AppSecrets
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -14,6 +16,8 @@ class AppConfigModule {
     companion object {
         private const val USER_AGENT_APPNAME = "wc-android-wear"
     }
+    @Provides
+    fun provideAppSecrets() = AppSecrets(BuildConfig.OAUTH_APP_ID, BuildConfig.OAUTH_APP_SECRET)
 
     @Provides
     fun provideUserAgent(appContext: Context) = UserAgent(appContext, USER_AGENT_APPNAME)
