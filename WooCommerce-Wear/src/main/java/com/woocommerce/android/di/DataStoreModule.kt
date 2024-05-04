ba5b3af01298f39dc816b019ab7be97e74269e64
ThomazFB <thomazfcortez@gmail.com>
2024-05-04 01:53:40 -0300

    Configure Stats DataStore

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/di/DataStoreModule.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/di/DataStoreModule.kt
@@ -6,7 +6,9 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
 import com.woocommerce.android.datastore.DataStoreQualifier
+import com.woocommerce.android.datastore.DataStoreType
 import com.woocommerce.android.datastore.DataStoreType.LOGIN
+import com.woocommerce.android.datastore.DataStoreType.STATS
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +29,19 @@ class DataStoreModule {
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile(LOGIN.typeName)
+        },
+        scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
+    )
+
+    @Provides
+    @Singleton
+    @DataStoreQualifier(STATS)
+    fun provideStatsDataStore(
+        appContext: Context,
+        @AppCoroutineScope appCoroutineScope: CoroutineScope
+    ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
+        produceFile = {
+            appContext.preferencesDataStoreFile(STATS.typeName)
         },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
