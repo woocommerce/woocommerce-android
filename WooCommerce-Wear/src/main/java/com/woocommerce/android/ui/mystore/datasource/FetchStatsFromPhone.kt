7e236cddd46b3a326ce434e3f7d25cd98257160c
ThomazFB <thomazfcortez@gmail.com>
2024-05-06 20:58:22 -0300

    Configure Stats observation from DataStore

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/mystore/datasource/FetchStatsFromPhone.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/mystore/datasource/FetchStatsFromPhone.kt
@@ -5,16 +5,35 @@ import androidx.datastore.preferences.core.Preferences
 import com.woocommerce.android.datastore.DataStoreQualifier
 import com.woocommerce.android.datastore.DataStoreType
 import com.woocommerce.android.phone.PhoneConnectionRepository
+import com.woocommerce.android.ui.login.ObserveLoginRequest.Companion.TIMEOUT_MILLIS
+import com.woocommerce.android.ui.mystore.stats.StatsRepository
 import com.woocommerce.commons.wear.MessagePath.REQUEST_STATS
 import javax.inject.Inject
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
 
 class FetchStatsFromPhone @Inject constructor(
-    private val phoneRepository: PhoneConnectionRepository
+    @DataStoreQualifier(DataStoreType.STATS) private val statsDataStore: DataStore<Preferences>,
+    private val phoneRepository: PhoneConnectionRepository,
+    private val statsRepository: StatsRepository
 ) {
-    suspend operator fun invoke(): Flow<MyStoreStatsData> {
+    suspend operator fun invoke(): Flow<MyStoreStatsRequest?> {
         phoneRepository.sendMessage(REQUEST_STATS)
-        return flowOf()
+        return combine(statsRepository.observeStatsDataChanges(), timeoutFlow) { statsData, isTimeout ->
+            when {
+                statsData?.isFinished == true -> statsData
+                isTimeout -> MyStoreStatsRequest.Error
+                else -> null
+            }
+        }
     }
+
+    private val timeoutFlow: Flow<Boolean>
+        get() = flow {
+            emit(true)
+            delay(TIMEOUT_MILLIS)
+            emit(false)
+        }
 }
