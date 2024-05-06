e180455181492cb524f9f981d369f87c49d09cc7
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-05-06 15:20:35 +0100

    Remove the my store fragment files

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/DashboardDestination.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/DashboardDestination.kt
@@ -1,8 +1,7 @@
 package com.woocommerce.android.ui.main
 
 import com.woocommerce.android.R
-import com.woocommerce.android.util.FeatureFlag
 
 object DashboardDestination {
-    val id = if (FeatureFlag.DYNAMIC_DASHBOARD.isEnabled()) R.id.dashboard else R.id.my_store
+    val id = R.id.dashboard
 }
