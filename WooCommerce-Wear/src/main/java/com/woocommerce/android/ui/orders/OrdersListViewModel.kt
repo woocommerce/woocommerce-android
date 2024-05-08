64d6213f452e133ba806e857eb4ff2246917649d
ThomazFB <thomazfcortez@gmail.com>
2024-05-07 22:54:30 -0300

    Introduce OrdersListViewModel basic structure

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/OrdersListViewModel.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/OrdersListViewModel.kt
@@ -1,4 +1,21 @@
 package com.woocommerce.android.ui.orders
 
-class OrdersListViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.NavHostController
+import com.woocommerce.commons.viewmodel.ScopedViewModel
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@Suppress("UnusedPrivateProperty")
+@HiltViewModel(assistedFactory = OrdersListViewModel.Factory::class)
+class OrdersListViewModel @AssistedInject constructor(
+    @Assisted private val navController: NavHostController,
+    savedState: SavedStateHandle
+) : ScopedViewModel(savedState) {
+    @AssistedFactory
+    interface Factory {
+        fun create(navController: NavHostController): OrdersListViewModel
+    }
 }
