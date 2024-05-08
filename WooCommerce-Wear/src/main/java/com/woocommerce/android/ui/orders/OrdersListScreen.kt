7ea5a8248f025c4264911085d1b0d11105044f68
ThomazFB <thomazfcortez@gmail.com>
2024-05-07 22:57:29 -0300

    Introduce basic scaffold for OrdersListScreen

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/OrdersListScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/OrdersListScreen.kt
@@ -1,4 +1,25 @@
 package com.woocommerce.android.ui.orders
 
-class OrdersListScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun OrdersListScreen(viewModel: OrdersListViewModel) {
+    viewModel.apply {  }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Orders List",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
