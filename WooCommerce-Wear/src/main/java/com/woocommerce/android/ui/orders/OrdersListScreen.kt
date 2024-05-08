0d6d67328b4dad53fdcfc8adfe8e7b7e2cf58e04
ThomazFB <thomazfcortez@gmail.com>
2024-05-08 01:33:07 -0300

    Wire OrdersListScreen with respective ViewModel

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/OrdersListScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/OrdersListScreen.kt
@@ -6,23 +6,47 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.tooling.preview.devices.WearDevices
 import com.woocommerce.android.presentation.theme.WooTheme
 
 @Composable
 fun OrdersListScreen(viewModel: OrdersListViewModel) {
-    viewModel.apply { }
+    val viewState by viewModel.viewState.observeAsState()
+    OrdersListScreen(
+        orders = viewState?.orders.orEmpty()
+    )
+}
+
+@Composable
+fun OrdersListScreen(
+    orders: List<String>
+) {
     WooTheme {
         Column(
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Text(
-                text = "Orders List",
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
+            orders.forEach {
+                Text(
+                    text = it,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
+}
+
+@Preview(device = WearDevices.LARGE_ROUND, showSystemUi = true)
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Preview(device = WearDevices.SQUARE, showSystemUi = true)
+@Preview(device = WearDevices.RECT, showSystemUi = true)
+@Composable
+fun Preview() {
+    OrdersListScreen(orders = listOf("Order 1", "Order 2", "Order 3"))
 }
