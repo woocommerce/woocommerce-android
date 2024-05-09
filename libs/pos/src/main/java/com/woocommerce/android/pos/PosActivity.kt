d4e627dbf83a1aa466a5811e9c7fb2ba03d9ef11
Andrey <akidinov@gmail.com>
2024-05-09 09:46:01 +0200

    Renamed activity

--- libs/pos/src/main/java/com/woocommerce/android/pos/PosActivity.kt
+++ libs/pos/src/main/java/com/woocommerce/android/pos/PosActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.woocommerce.android.pos.ui.theme.WCAndroidTheme
 
-class MainActivity : ComponentActivity() {
+class PosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
