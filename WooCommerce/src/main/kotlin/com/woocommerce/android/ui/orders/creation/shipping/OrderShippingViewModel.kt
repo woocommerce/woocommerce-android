577c929770bf56da2f94df6cdb48ed14633959f1
Alejo <alejandro.torres.veiga@automattic.com>
2024-05-09 16:13:54 -0600

    use real data

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderShippingViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderShippingViewModel.kt
@@ -3,7 +3,6 @@ package com.woocommerce.android.ui.orders.creation.shipping
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.R
-import com.woocommerce.android.extensions.capitalize
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.ShippingMethod
 import com.woocommerce.android.viewmodel.MultiLiveEvent
@@ -11,7 +10,6 @@ import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
@@ -21,7 +19,8 @@ import javax.inject.Inject
 @HiltViewModel
 class OrderShippingViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val resourceProvider: ResourceProvider
+    private val resourceProvider: ResourceProvider,
+    private val getShippingMethodById: GetShippingMethodById
 ) : ScopedViewModel(savedStateHandle) {
 
     private val navArgs: OrderShippingFragmentArgs by savedState.navArgs()
@@ -43,26 +42,13 @@ class OrderShippingViewModel @Inject constructor(
         navArgs.currentShippingLine?.let { shippingLine: Order.ShippingLine ->
             launch {
                 viewState.value = ViewState.ShippingState(
-                    method = getShippingMethod(shippingLine),
+                    method = getShippingMethodById(shippingLine.methodId),
                     name = shippingLine.methodTitle,
                     amount = shippingLine.total,
                     isEditFlow = true,
                     isSaveChangesEnabled = false
                 )
             }
-        }
-    }
-
-    @Suppress("MagicNumber")
-    private suspend fun getShippingMethod(shippingLine: Order.ShippingLine): ShippingMethod? {
-        return if (shippingLine.methodId == null) {
-            null
-        } else {
-            delay(1000)
-            ShippingMethod(
-                id = shippingLine.methodId,
-                title = shippingLine.methodId.capitalize()
-            )
         }
     }
 
