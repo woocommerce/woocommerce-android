689489f23fc0b2f86ad05051fb4aca11e838c8ae
Alejo <alejandro.torres.veiga@automattic.com>
2024-05-09 19:55:48 -0600

    track shipping method selection

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderShippingViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/OrderShippingViewModel.kt
@@ -3,6 +3,9 @@ package com.woocommerce.android.ui.orders.creation.shipping
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsEvent
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.extensions.capitalize
 import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.ShippingMethod
@@ -21,7 +24,8 @@ import javax.inject.Inject
 @HiltViewModel
 class OrderShippingViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val resourceProvider: ResourceProvider
+    private val resourceProvider: ResourceProvider,
+    private val tracker: AnalyticsTrackerWrapper
 ) : ScopedViewModel(savedStateHandle) {
 
     private val navArgs: OrderShippingFragmentArgs by savedState.navArgs()
@@ -107,6 +111,10 @@ class OrderShippingViewModel @Inject constructor(
 
     fun onMethodSelected(selected: ShippingMethod) {
         (viewState.value as? ViewState.ShippingState)?.let {
+            tracker.track(
+                AnalyticsEvent.ORDER_SHIPPING_METHOD_SELECTED,
+                mapOf(AnalyticsTracker.KEY_SHIPPING_METHOD to selected.id)
+            )
             viewState.value = it.copy(
                 method = selected,
                 isSaveChangesEnabled = isSaveChangesEnabled(newMethodId = selected.id)
