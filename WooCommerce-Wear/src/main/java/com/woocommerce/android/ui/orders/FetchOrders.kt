93c559c63ea8479ae1f38aac16cdac86ff2bcd01
ThomazFB <thomazfcortez@gmail.com>
2024-05-10 17:01:33 -0300

    Introduce Order list handling from Phone

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/FetchOrders.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/FetchOrders.kt
@@ -2,12 +2,16 @@ package com.woocommerce.android.ui.orders
 
 import com.woocommerce.android.phone.PhoneConnectionRepository
 import com.woocommerce.android.system.NetworkStatus
+import com.woocommerce.android.ui.login.ObserveLoginRequest.Companion.TIMEOUT_MILLIS
 import com.woocommerce.android.ui.orders.OrdersListViewModel.OrderItem
 import com.woocommerce.android.util.DateUtils
 import com.woocommerce.commons.wear.MessagePath.REQUEST_ORDERS
 import java.util.Locale
 import javax.inject.Inject
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flow
 import org.wordpress.android.fluxc.model.OrderEntity
 import org.wordpress.android.fluxc.model.SiteModel
@@ -24,20 +28,33 @@ class FetchOrders @Inject constructor(
 ) {
     suspend operator fun invoke(
         selectedSite: SiteModel
-    ): Flow<List<OrderItem>> = flow {
-        if (networkStatus.isConnected()) {
-            emit(fetchOrdersFromStore(selectedSite))
+    ): Flow<List<OrderItem>> = combine(
+        selectOrdersDataSource(selectedSite),
+        timeoutFlow
+    ) { orders, isTimeout ->
+        when {
+            orders.isNotEmpty() -> orders.map { it.toOrderItem(selectedSite) }
+            isTimeout -> emptyList()
+            else -> null
+        }
+    }.filterNotNull()
+
+    private suspend fun selectOrdersDataSource(
+        selectedSite: SiteModel
+    ): Flow<List<OrderEntity>> {
+        return if (networkStatus.isConnected()) {
+            flow {
+                val result = when (val result = ordersRepository.fetchOrders(selectedSite)) {
+                    is Success -> result.orders
+                    else -> emptyList()
+                }
+                emit(result)
+            }
         } else {
             phoneRepository.sendMessage(REQUEST_ORDERS)
-
+            return ordersRepository.observeOrdersDataChanges()
         }
     }
-
-    private suspend fun fetchOrdersFromStore(selectedSite: SiteModel) =
-        when (val result = ordersRepository.fetchOrders(selectedSite)) {
-            is Success -> result.orders.map { it.toOrderItem(selectedSite) }
-            else -> emptyList()
-        }
 
     private fun OrderEntity.toOrderItem(
         selectedSite: SiteModel
@@ -69,4 +86,11 @@ class FetchOrders @Inject constructor(
             status = formattedStatus
         )
     }
+
+    private val timeoutFlow: Flow<Boolean>
+        get() = flow {
+            emit(true)
+            delay(TIMEOUT_MILLIS)
+            emit(false)
+        }
 }
