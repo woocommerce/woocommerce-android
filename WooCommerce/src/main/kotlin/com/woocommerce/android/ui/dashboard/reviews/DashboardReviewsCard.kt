729cc95400b6ea867e71927c2a46ba309918ef1b
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-05-10 18:34:03 +0100

    Add UI of the review items

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/reviews/DashboardReviewsCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/reviews/DashboardReviewsCard.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.dashboard.reviews
 
+import android.widget.RatingBar
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,7 +9,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,13 +19,19 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.woocommerce.android.R
 import com.woocommerce.android.extensions.fastStripHtml
 import com.woocommerce.android.model.DashboardWidget
+import com.woocommerce.android.model.ProductReview
 import com.woocommerce.android.ui.compose.animations.SkeletonView
 import com.woocommerce.android.ui.compose.viewModelWithFactory
 import com.woocommerce.android.ui.dashboard.DashboardFilterableCardHeader
@@ -32,6 +41,7 @@ import com.woocommerce.android.ui.dashboard.WidgetCard
 import com.woocommerce.android.ui.dashboard.WidgetError
 import com.woocommerce.android.ui.dashboard.defaultHideMenuEntry
 import com.woocommerce.android.ui.reviews.ProductReviewStatus
+import com.woocommerce.android.util.StringUtils
 
 @Composable
 fun DashboardReviewsCard(
@@ -105,8 +115,89 @@ private fun ProductReviewsCardContent(
         if (viewState.reviews.isEmpty()) {
             EmptyView(selectedFilter = viewState.selectedFilter)
         } else {
-            viewState.reviews.forEach { review ->
-                Text(text = review.review.fastStripHtml())
+            viewState.reviews.forEachIndexed { index, productReview ->
+                ReviewListItem(
+                    review = productReview,
+                    showDivider = index < viewState.reviews.size - 1
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ReviewListItem(
+    review: ProductReview,
+    showDivider: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_comment),
+            contentDescription = null,
+            tint = if (review.read == false) {
+                MaterialTheme.colors.primary
+            } else {
+                MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+            }
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(
+                text = if (review.product == null) {
+                    stringResource(
+                        R.string.product_review_list_item_title, review.reviewerName
+                    )
+                } else {
+                    stringResource(
+                        R.string.review_list_item_title,
+                        review.reviewerName,
+                        review.product?.name?.fastStripHtml().orEmpty()
+                    )
+                },
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.onSurface
+            )
+
+            val reviewText = buildAnnotatedString {
+                if (review.status == ProductReviewStatus.HOLD.toString()) {
+                    withStyle(SpanStyle(color = colorResource(id = R.color.woo_orange_50))) {
+                        append(stringResource(id = R.string.pending_review_label))
+                    }
+
+                    append(" • ")
+                }
+
+                append(StringUtils.getRawTextFromHtml(review.review))
+            }
+
+            Text(
+                text = reviewText,
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+            )
+
+            if (review.rating > 0) {
+                AndroidView(
+                    factory = { context ->
+                        RatingBar(context, null, androidx.appcompat.R.attr.ratingBarStyleSmall)
+                    },
+                    update = { ratingBar ->
+                        ratingBar.rating = 100F
+                        ratingBar.numStars = review.rating
+                    }
+                )
+            }
+
+            Spacer(modifier = Modifier)
+
+            if (showDivider) {
+                Divider()
             }
         }
     }
@@ -128,8 +219,7 @@ private fun ReviewsLoading(
                 SkeletonView(width = 24.dp, height = 24.dp)
 
                 Column(
-                    verticalArrangement = Arrangement.spacedBy(4.dp),
-                    modifier = Modifier
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
                 ) {
                     SkeletonView(width = 260.dp, height = 16.dp)
                     SkeletonView(width = 120.dp, height = 16.dp)
