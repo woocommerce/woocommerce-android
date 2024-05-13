c3a4000ea22b3ee384f73efa2a2438a588b8a9ca
ThomazFB <thomazfcortez@gmail.com>
2024-05-13 19:25:30 -0300

    Configure OrderDetailsViewModel order parsing and handling

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/OrderDetailsViewModel.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/OrderDetailsViewModel.kt
@@ -4,17 +4,23 @@ import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ui.NavArgs.ORDER_ID
+import com.woocommerce.android.ui.login.LoginRepository
 import com.woocommerce.android.ui.orders.OrdersRepository
+import com.woocommerce.android.ui.orders.ParseOrderData
+import com.woocommerce.android.ui.orders.ParseOrderData.OrderItem
 import com.woocommerce.commons.viewmodel.ScopedViewModel
 import com.woocommerce.commons.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class OrderDetailsViewModel @Inject constructor(
-    private val ordersRepository: OrdersRepository,
+    ordersRepository: OrdersRepository,
+    parseOrder: ParseOrderData,
+    loginRepository: LoginRepository,
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
     private val _viewState = savedState.getStateFlow(
@@ -24,16 +30,27 @@ class OrderDetailsViewModel @Inject constructor(
     val viewState = _viewState.asLiveData()
 
     init {
+        _viewState.update { it.copy(isLoading = true) }
         launch {
             savedState.get<Long>(ORDER_ID.key)
                 ?.let { ordersRepository.getOrderFromId(it) }
-                ?.let { /* map OrderEntity to UI model */ }
+                ?.let {
+                    val site = loginRepository.selectedSite ?: return@let null
+                    parseOrder(site, it)
+                }.let { presentOrderData(it) }
         }
 
     }
 
+    private fun presentOrderData(order: OrderItem?) {
+        _viewState.update {
+            it.copy(isLoading = false, orderItem = order)
+        }
+    }
+
     @Parcelize
     data class ViewState(
-        val isLoading: Boolean = false
+        val isLoading: Boolean = false,
+        val orderItem: OrderItem? = null
     ) : Parcelable
 }
