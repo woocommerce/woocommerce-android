3d06d083b63d501de5cc7f2c348b268b103e6e1d
ThomazFB <thomazfcortez@gmail.com>
2024-05-13 19:34:28 -0300

    Adjust OrderDetailsScreen to listen to Order data changes

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/OrderDetailsScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/OrderDetailsScreen.kt
@@ -5,29 +5,38 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import com.woocommerce.android.presentation.component.LoadingScreen
+import com.woocommerce.android.ui.orders.ParseOrderData
+import com.woocommerce.android.ui.orders.ParseOrderData.OrderItem
 
 @Composable
 fun OrderDetailsScreen(viewModel: OrderDetailsViewModel) {
     val viewState = viewModel.viewState.observeAsState()
     OrderDetailsScreen(
-        isLoading = viewState.value?.isLoading ?: false
+        isLoading = viewState.value?.isLoading ?: false,
+        order = viewState.value?.orderItem
     )
 }
 
 @Composable
 fun OrderDetailsScreen(
-    isLoading: Boolean
+    isLoading: Boolean,
+    order: OrderItem?
 ) {
     Column {
-        if (isLoading) {
-            LoadingScreen()
-        } else {
-            OrderDetailsContent()
+        when {
+            isLoading -> LoadingScreen()
+            order == null -> OrderLoadingFailed()
+            else -> OrderDetailsContent(order)
         }
     }
 }
 
 @Composable
-fun OrderDetailsContent() {
-    Text("Order Details Content")
+fun OrderDetailsContent(order: OrderItem) {
+    Text(order.number)
+}
+
+@Composable
+fun OrderLoadingFailed() {
+    Text("Failed to load Order data")
 }
