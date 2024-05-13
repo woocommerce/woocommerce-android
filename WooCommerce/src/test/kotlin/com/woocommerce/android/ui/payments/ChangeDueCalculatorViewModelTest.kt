020cb3b43211681637e98c051aafb92022498208
Rooney <backwardstruck@users.noreply.github.com>
2024-05-13 15:58:39 -0400

    Update test class

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/ChangeDueCalculatorViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/ChangeDueCalculatorViewModelTest.kt
@@ -4,13 +4,14 @@ import com.woocommerce.android.ui.orders.details.OrderDetailRepository
 import com.woocommerce.android.ui.payments.methodselection.ChangeDueCalculatorViewModel
 import com.woocommerce.android.ui.payments.methodselection.ChangeDueCalculatorViewModel.UiState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.whenever
 import kotlin.test.Test
+import java.math.BigDecimal
 
 @ExperimentalCoroutinesApi
 class ChangeDueCalculatorViewModelTest {
@@ -34,12 +35,17 @@ class ChangeDueCalculatorViewModelTest {
     }
 
     @Test
-    fun `order details load successfully emits success state`() = runBlockingTest {
-        // TODO: Fix this test
+    fun `order details load successfully emits success state`() = runTest {
+//        val order = Order(orderId = 1L, total = BigDecimal("100.00"))  // Assuming Order is a data class
+//        whenever(orderDetailRepository.getOrderById(1L)).thenReturn(order)
+//
+//        viewModel.loadOrderDetails()
+//
+//        assert(viewModel.uiState.value == UiState.Success(order.total, BigDecimal.ZERO))
     }
 
     @Test
-    fun `order details load failure emits error state`() = runBlockingTest {
+    fun `order details load failure emits error state`() = runTest {
         whenever(orderDetailRepository.getOrderById(1L)).thenReturn(null)
 
         viewModel.loadOrderDetails()
