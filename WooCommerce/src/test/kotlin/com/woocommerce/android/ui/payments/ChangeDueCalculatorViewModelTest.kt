c21d8a1c35105709278bdf929eb903906ac13273
Rooney <backwardstruck@users.noreply.github.com>
2024-05-13 16:08:07 -0400

    Update test class

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/ChangeDueCalculatorViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/ChangeDueCalculatorViewModelTest.kt
@@ -1,23 +1,18 @@
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
 import com.woocommerce.android.ui.payments.methodselection.ChangeDueCalculatorViewModel
 import com.woocommerce.android.ui.payments.methodselection.ChangeDueCalculatorViewModel.UiState
+import com.woocommerce.android.viewmodel.BaseUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
 import org.junit.Before
-import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.whenever
-import kotlin.test.Test
 import java.math.BigDecimal
 
 @ExperimentalCoroutinesApi
-class ChangeDueCalculatorViewModelTest {
-
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
+class ChangeDueCalculatorViewModelTest : BaseUnitTest() {
 
     @Mock
     private lateinit var orderDetailRepository: OrderDetailRepository
@@ -35,17 +30,17 @@ class ChangeDueCalculatorViewModelTest {
     }
 
     @Test
-    fun `order details load successfully emits success state`() = runTest {
-//        val order = Order(orderId = 1L, total = BigDecimal("100.00"))  // Assuming Order is a data class
-//        whenever(orderDetailRepository.getOrderById(1L)).thenReturn(order)
-//
-//        viewModel.loadOrderDetails()
-//
-//        assert(viewModel.uiState.value == UiState.Success(order.total, BigDecimal.ZERO))
+    fun `order details load successfully emits success state`() = testBlocking {
+        //TODO fix this test
+        whenever(orderDetailRepository.getOrderById(1L)).thenReturn(null)
+
+        viewModel.loadOrderDetails()
+
+        assert(viewModel.uiState.value == UiState.Success(BigDecimal.ZERO, BigDecimal.ZERO))
     }
 
     @Test
-    fun `order details load failure emits error state`() = runTest {
+    fun `order details load failure emits error state`() = testBlocking {
         whenever(orderDetailRepository.getOrderById(1L)).thenReturn(null)
 
         viewModel.loadOrderDetails()
