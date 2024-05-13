a5d1eed3b8ed51295b6066f75723add5afff3bbf
ThomazFB <thomazfcortez@gmail.com>
2024-05-13 15:29:00 -0300

    Define Order Details scaffolds

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/OrderDetailsScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/OrderDetailsScreen.kt
@@ -1,2 +1,33 @@
 package com.woocommerce.android.ui.orders.details
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import com.woocommerce.android.presentation.component.LoadingScreen
+
+@Composable
+fun OrderDetailsScreen(viewModel: OrderDetailsViewModel) {
+    val viewState = viewModel.viewState.observeAsState()
+    OrderDetailsScreen(
+        isLoading = viewState.value?.isLoading ?: false
+    )
+}
+
+@Composable
+fun OrderDetailsScreen(
+    isLoading: Boolean
+) {
+    Column {
+        if (isLoading) {
+            LoadingScreen()
+        } else {
+            OrderDetailsContent()
+        }
+    }
+}
+
+@Composable
+fun OrderDetailsContent() {
+    Text("Order Details Content")
+}
---
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/OrderDetailsViewModel.kt
@@ -0,0 +1,26 @@
+package com.woocommerce.android.ui.orders.details
+
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import com.woocommerce.commons.viewmodel.ScopedViewModel
+import com.woocommerce.commons.viewmodel.getStateFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.parcelize.Parcelize
+
+@HiltViewModel
+class OrderDetailsViewModel @Inject constructor(
+    savedState: SavedStateHandle
+): ScopedViewModel(savedState) {
+    private val _viewState = savedState.getStateFlow(
+        scope = this,
+        initialValue = ViewState()
+    )
+    val viewState = _viewState.asLiveData()
+
+    @Parcelize
+    data class ViewState(
+        val isLoading: Boolean = false
+    ) : Parcelable
+}
