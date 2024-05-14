ff19eca157c51d7c9819a1b53dfec4996185bf1d
Rooney <backwardstruck@users.noreply.github.com>
2024-05-14 16:46:51 -0400

    Don't try/catch

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/ChangeDueCalculatorViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/ChangeDueCalculatorViewModel.kt
@@ -3,13 +3,11 @@ package com.woocommerce.android.ui.payments.methodselection
 import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
-import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.io.IOException
 import java.math.BigDecimal
 import javax.inject.Inject
 
@@ -38,18 +36,10 @@ class ChangeDueCalculatorViewModel @Inject constructor(
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     internal fun loadOrderDetails() {
         launch {
-            try {
-                val order = orderDetailRepository.getOrderById(orderId)
-                if (order != null) {
-                    _uiState.value = UiState.Success(amountDue = order.total, 0.00.toBigDecimal())
-                } else {
-                    _uiState.value = UiState.Error
-                }
-            } catch (e: IOException) {
-                WooLog.e(
-                    tag = WooLog.T.ORDERS,
-                    message = "Error loading order details"
-                )
+            val order = orderDetailRepository.getOrderById(orderId)
+            if (order != null) {
+                _uiState.value = UiState.Success(amountDue = order.total, 0.00.toBigDecimal())
+            } else {
                 _uiState.value = UiState.Error
             }
         }
