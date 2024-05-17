7ffb17bc05a2f4db4ac987c69ab5fed89e4d7b27
Andrey <akidinov@gmail.com>
2024-05-17 15:29:56 +0200

    Implemented track method

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosAnalyticsTracker.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosAnalyticsTracker.kt
@@ -1,12 +1,32 @@
 package com.woocommerce.android.ui.woopos.util.analytics
 
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class WooPosAnalyticsTracker @Inject constructor() {
-    fun trackEvent(analytics: WooPosAnalytics) {
-        when (analytics) {
-            is WooPosAnalytics.Error -> TODO()
-            is WooPosAnalytics.Event -> TODO()
+class WooPosAnalyticsTracker @Inject constructor(
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
+) {
+    suspend fun track(analytics: WooPosAnalytics) {
+        withContext(Dispatchers.IO) {
+            when (analytics) {
+                is WooPosAnalytics.Event -> {
+                    analyticsTrackerWrapper.track(
+                        analytics,
+                        analytics.properties
+                    )
+                }
+
+                is WooPosAnalytics.Error -> {
+                    analyticsTrackerWrapper.track(
+                        analytics,
+                        analytics.errorContext.simpleName,
+                        analytics.errorType,
+                        analytics.errorDescription
+                    )
+                }
+            }
         }
     }
 }
