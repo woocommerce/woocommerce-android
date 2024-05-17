3275df8c64318d78cf6b35a2bb9646cdecc7920e
Rooney <backwardstruck@users.noreply.github.com>
2024-05-17 15:24:59 -0400

    Create new compose class

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/ChangeDueCalculatorScreen.kt
@@ -0,0 +1,76 @@
+package com.woocommerce.android.ui.payments.methodselection
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
+
+@Composable
+fun ChangeDueCalculatorScreen(
+    uiState: ChangeDueCalculatorViewModel.UiState,
+    onNavigateUp: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = getTitleText(uiState)) },
+                navigationIcon = {
+                    IconButton(onClick = onNavigateUp) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back)
+                        )
+                    }
+                },
+                backgroundColor = colorResource(id = R.color.color_toolbar),
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when (uiState) {
+                is ChangeDueCalculatorViewModel.UiState.Loading -> Text(
+                    "Loading...",
+                    style = MaterialTheme.typography.h6
+                )
+                is ChangeDueCalculatorViewModel.UiState.Success -> Text(
+                    "Amount Due: ${uiState.amountDue}",
+                    style = MaterialTheme.typography.body1
+                )
+                is ChangeDueCalculatorViewModel.UiState.Error -> Text("Error", style = MaterialTheme.typography.h6)
+            }
+        }
+    }
+}
+
+@Composable
+private fun getTitleText(uiState: ChangeDueCalculatorViewModel.UiState): String {
+    return when (uiState) {
+        is ChangeDueCalculatorViewModel.UiState.Success -> stringResource(
+            R.string.cash_payments_take_payment_title,
+            uiState.amountDue
+        )
+        else -> stringResource(id = R.string.cash_payments_take_payment_title)
+    }
+}
