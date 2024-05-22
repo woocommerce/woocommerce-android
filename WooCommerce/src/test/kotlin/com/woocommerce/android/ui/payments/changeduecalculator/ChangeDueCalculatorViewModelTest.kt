f291856c521131cd9b854a7fe46806594628b915
Rooney <backwardstruck@users.noreply.github.com>
2024-05-21 23:09:04 -0400

    Clean up test

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/ChangeDueCalculatorViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/ChangeDueCalculatorViewModelTest.kt
@@ -9,6 +9,7 @@ import com.woocommerce.android.viewmodel.BaseUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
@@ -24,7 +25,7 @@ class ChangeDueCalculatorViewModelTest : BaseUnitTest() {
 
     private val site: SiteModel = mock()
     private val order: Order = mock {
-        on { total }.thenReturn(BigDecimal(1L))
+        on { total }.thenReturn(BigDecimal(ORDER_TOTAL))
     }
 
     private val selectedSite: SelectedSite = mock {
@@ -39,20 +40,14 @@ class ChangeDueCalculatorViewModelTest : BaseUnitTest() {
 
     private val orderDetailRepository: OrderDetailRepository = mock()
 
-    private val savedStateHandle: SavedStateHandle = mock()
-
+    private var savedStateHandle: SavedStateHandle = mock {
+        SavedStateHandle(mapOf("orderId" to 1L))
+    }
     private lateinit var viewModel: ChangeDueCalculatorViewModel
 
-    @Test
-    fun `when ViewModel is initialized, then initial state is loading`() {
-        // GIVEN
-        whenever(savedStateHandle.get<Long>("orderId")).thenReturn(1L)
-
-        // WHEN
-        viewModel = initViewModel()
-
-        // THEN
-        assertThat(viewModel.uiState.value).isInstanceOf(ChangeDueCalculatorViewModel.UiState.Loading::class.java)
+    @Before
+    fun setup() {
+        savedStateHandle = SavedStateHandle(mapOf("orderId" to 1L))
     }
 
     @Test
@@ -60,8 +55,7 @@ class ChangeDueCalculatorViewModelTest : BaseUnitTest() {
         // GIVEN
         val expectedAmountDue = "100.00"
         val expectedChange = BigDecimal("0.0")
-        whenever(orderDetailRepository.getOrderById(1L)).thenReturn(order)
-        whenever(savedStateHandle.get<Long>("orderId")).thenReturn(1L)
+        whenever(orderDetailRepository.getOrderById(any())).thenReturn(order)
 
         // WHEN
         viewModel = initViewModel()
@@ -72,18 +66,6 @@ class ChangeDueCalculatorViewModelTest : BaseUnitTest() {
         uiState as ChangeDueCalculatorViewModel.UiState.Success
         assertThat(uiState.change).isEqualTo(expectedChange)
         assertThat(uiState.amountDue).isEqualTo(expectedAmountDue)
-    }
-
-    @Test
-    fun `given order details retrieval failure, when order details are loaded, then error state is emitted`() = runTest {
-        // GIVEN
-        // TODO
-
-        // WHEN
-        // TODO
-
-        // THEN
-        // TODO
     }
 
     private fun initViewModel(): ChangeDueCalculatorViewModel {
