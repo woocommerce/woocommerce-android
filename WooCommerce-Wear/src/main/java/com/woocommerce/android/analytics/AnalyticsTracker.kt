46efd35a577865536c1b1e9a97abd5cdee6e4a43
ThomazFB <thomazfcortez@gmail.com>
2024-05-22 17:21:08 -0300

    Create AnalyticsTracker initial track function

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/analytics/AnalyticsTracker.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/analytics/AnalyticsTracker.kt
@@ -1,15 +1,27 @@
 package com.woocommerce.android.analytics
 
 import com.google.android.gms.wearable.DataMap
+import com.google.gson.Gson
 import com.woocommerce.android.phone.PhoneConnectionRepository
+import com.woocommerce.commons.DataParameters.ANALYTICS_PARAMETERS
+import com.woocommerce.commons.DataParameters.ANALYTICS_TRACK
 import com.woocommerce.commons.DataPath
 import javax.inject.Inject
 
 class AnalyticsTracker @Inject constructor(
     private val phoneRepository: PhoneConnectionRepository
 ) {
-    fun track(stat: AnalyticsParameter, properties: Map<String, *> = emptyMap<String, Any>()) {
+    private val gson by lazy { Gson() }
 
+    fun track(stat: AnalyticsParameter, properties: Map<String, String> = emptyMap()) {
+        val parameters = gson.toJson(properties)
+        phoneRepository.sendData(
+            dataPath = DataPath.ANALYTICS_DATA,
+            data = DataMap().apply {
+                putString(ANALYTICS_TRACK.value, stat.name)
+                putString(ANALYTICS_PARAMETERS.value, parameters)
+            }
+        )
     }
 }
 
