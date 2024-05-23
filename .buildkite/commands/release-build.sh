cdec317fbe0ddf0ad3191fea373a4ffc565488c5
Olivier Halligon <olivier.halligon@automattic.com>
2024-05-23 21:05:47 +0200

    Make `trigger_release_build` accept `app:` param
    And also rename `release-builds.yml` pipeline to `release-build.yml` (singular)
    
    Then:
     - Pass that param as `APP_TO_BUILD` env var to the triggered pipeline
     - Make use of that `$APP_TO_BUILD` var in the `release-build.yml` pipeline (`label` and `command`) so it passes the value to the `release-build.sh` script it calls
     - Make the `release-build.sh` script pass `app:"$1"` when it calls the `build_and_upload_google_play` lane
     - Make the `build_and_upload_google_play` lane propagate that `app:` parameter to the `build_and_upload_{beta,release}` lanes it calls

--- .buildkite/commands/release-build.sh
+++ .buildkite/commands/release-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
 
+APP_TO_BUILD="${1:-WooCommerce}" # WooCommerce or WooCommerce-Wear
+
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
@@ -7,4 +9,4 @@ echo "--- :closed_lock_with_key: Installing Secrets"
 bundle exec fastlane run configure_apply
 
 echo "--- :hammer_and_wrench: Building"
-bundle exec fastlane build_and_upload_google_play
+bundle exec fastlane build_and_upload_google_play app:"${APP_TO_BUILD}"
