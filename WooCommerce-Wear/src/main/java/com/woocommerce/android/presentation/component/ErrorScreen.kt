490e45cdff8a6c7f47839dd718ff13b8cb54e349
ThomazFB <thomazfcortez@gmail.com>
2024-05-23 21:07:55 -0300

    Add Retry button to the ErrorScreen component

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/presentation/component/ErrorScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/presentation/component/ErrorScreen.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Text
+import androidx.wear.tooling.preview.devices.WearDevices
+import com.woocommerce.android.R
 
 @Composable
 fun ErrorScreen(
     errorText: String,
+    onRetryClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -28,5 +34,21 @@ fun ErrorScreen(
             textAlign = TextAlign.Center,
             modifier = modifier.padding(8.dp)
         )
+        Button(
+            onClick = onRetryClicked,
+            modifier = modifier.padding(8.dp)
+        ) {
+            Text(stringResource(id = R.string.error_screen_retry_button))
+        }
     }
+}
+
+@Preview(device = WearDevices.LARGE_ROUND, showSystemUi = true)
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Composable
+fun ErrorScreenPreview() {
+    ErrorScreen(
+        errorText = "An error occurred",
+        onRetryClicked = { }
+    )
 }
