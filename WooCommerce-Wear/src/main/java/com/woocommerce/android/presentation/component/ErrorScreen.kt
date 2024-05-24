ded875f1cb50d23a5ff7d0c59502c3ec4ee2cd47
ThomazFB <thomazfcortez@gmail.com>
2024-05-24 12:15:44 -0300

    Improve ErrorScreen with retry button

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/presentation/component/ErrorScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/presentation/component/ErrorScreen.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.presentation.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,9 +13,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Text
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.woocommerce.android.R
+import com.woocommerce.android.presentation.theme.WooColors
+import com.woocommerce.android.presentation.theme.WooTheme
+import com.woocommerce.android.presentation.theme.WooTypography
 
 @Composable
 fun ErrorScreen(
@@ -31,12 +36,18 @@ fun ErrorScreen(
     ) {
         Text(
             text = errorText,
+            style = WooTypography.caption1,
             textAlign = TextAlign.Center,
             modifier = modifier.padding(8.dp)
         )
         Button(
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = WooColors.woo_gray_alpha
+            ),
             onClick = onRetryClicked,
-            modifier = modifier.padding(8.dp)
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         ) {
             Text(stringResource(id = R.string.error_screen_retry_button))
         }
@@ -45,10 +56,14 @@ fun ErrorScreen(
 
 @Preview(device = WearDevices.LARGE_ROUND, showSystemUi = true)
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Preview(device = WearDevices.SQUARE, showSystemUi = true)
+@Preview(device = WearDevices.RECT, showSystemUi = true)
 @Composable
 fun ErrorScreenPreview() {
-    ErrorScreen(
-        errorText = "An error occurred",
-        onRetryClicked = { }
-    )
+    WooTheme {
+        ErrorScreen(
+            errorText = "Failed to load order details, make sure your Watch is connected to the Internet or your phone is nearby.",
+            onRetryClicked = { }
+        )
+    }
 }
