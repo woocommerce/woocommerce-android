8c0015a2e51bbd55e380307836f24f08c2424f80
ThomazFB <thomazfcortez@gmail.com>
2024-05-24 22:04:18 -0300

    Adjust the LoginScreen loading

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/login/LoginScreen.kt
@@ -1,13 +1,12 @@
 package com.woocommerce.android.ui.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
@@ -16,6 +15,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -28,9 +28,9 @@ import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.woocommerce.android.R
-import com.woocommerce.android.compose.component.LoadingScreen
 import com.woocommerce.android.compose.theme.WooColors
 import com.woocommerce.android.compose.theme.WooTheme
+import com.woocommerce.android.compose.theme.WooTypography
 
 @Composable
 fun LoginScreen(viewModel: LoginViewModel) {
@@ -44,7 +44,8 @@ fun LoginScreen(viewModel: LoginViewModel) {
 @Composable
 fun LoginScreen(
     isLoading: Boolean,
-    onTryAgainClicked: () -> Unit
+    onTryAgainClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     WooTheme {
         Box(
@@ -53,9 +54,9 @@ fun LoginScreen(
         ) {
             if (isLoading) {
                 TimeText()
-                LoadingScreen()
+                LoginLoadingScreen(modifier)
             } else {
-                LoginInstructionsScreen(onTryAgainClicked)
+                LoginInstructionsScreen(onTryAgainClicked, modifier)
             }
         }
     }
@@ -64,7 +65,7 @@ fun LoginScreen(
 @Composable
 private fun LoginInstructionsScreen(
     onTryAgainClicked: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier
 ) {
     Box(
         contentAlignment = Alignment.Center,
@@ -105,6 +106,29 @@ private fun LoginInstructionsScreen(
                 Text(stringResource(id = R.string.login_screen_action_button))
             }
         }
+    }
+}
+
+@Composable
+private fun LoginLoadingScreen(
+    modifier: Modifier
+) {
+    TimeText()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_woo_bubble_white_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = stringResource(id = R.string.login_screen_loading_text),
+            textAlign = TextAlign.Center,
+            style = WooTypography.title3,
+            modifier = modifier
+                .padding(top = 8.dp)
+        )
     }
 }
 
