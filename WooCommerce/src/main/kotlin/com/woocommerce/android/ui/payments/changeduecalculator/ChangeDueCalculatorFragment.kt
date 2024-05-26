eb4cabaccdbfca6b5b5c7eb7afe24ca1e057634e
Rooney <backwardstruck@users.noreply.github.com>
2024-05-26 15:23:54 -0400

    Navigate back with the action

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/ChangeDueCalculatorFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/ChangeDueCalculatorFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.main.AppBarStatus
+import com.woocommerce.android.ui.payments.cardreader.onboarding.CardReaderFlowParam
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,14 +31,25 @@ class ChangeDueCalculatorFragment : BaseFragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return ComposeView(requireContext()).apply {
             setContent {
                 val uiState by viewModel.uiState.collectAsState()
                 ChangeDueCalculatorScreen(
                     uiState = uiState,
                     onNavigateUp = { viewModel.onBackPressed() },
-                    onCompleteOrderClick = {},
+                    onCompleteOrderClick = {
+                        val action = ChangeDueCalculatorFragmentDirections
+                            .actionChangeDueCalculatorFragmentToSelectPaymentMethodFragment(
+                                cardReaderFlowParam = CardReaderFlowParam.CardReadersHub(),
+                                isOrderPaid = true
+                            )
+                        findNavController().navigate(action)
+                    },
                     onAmountReceivedChanged = { viewModel.updateAmountReceived(it) }
                 )
             }
