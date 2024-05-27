a9f776b05dfe2c4316cd03444c820b7e4cd6500e
samiuelson <samiuelson@gmail.com>
2024-05-27 16:30:16 +0200

    Add redirect to card reader tutorial

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderActivity.kt
@@ -1,28 +1,102 @@
 package com.woocommerce.android.ui.woopos.cardreader
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.snackbar.Snackbar
 import com.woocommerce.android.R
-import com.woocommerce.android.ui.moremenu.MoreMenuFragmentDirections
+import com.woocommerce.android.cardreader.config.CardReaderConfigForSupportedCountry
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.orders.creation.OrderCreateEditRepository
+import com.woocommerce.android.ui.payments.cardreader.CardReaderCountryConfigProvider
 import com.woocommerce.android.ui.payments.cardreader.onboarding.CardReaderFlowParam
+import com.woocommerce.android.ui.payments.cardreader.onboarding.CardReaderType
+import com.woocommerce.android.viewmodel.ResourceProvider
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.wordpress.android.fluxc.store.WooCommerceStore
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class WooPosCardReaderActivity : AppCompatActivity() {
+class WooPosCardReaderActivity : AppCompatActivity(R.layout.activity_woo_pos_card_reader) {
+
+    @Inject
+    lateinit var orderCreateEditRepository: OrderCreateEditRepository
+
+    @Inject
+    lateinit var resourceProvider: ResourceProvider
+
+    @Inject
+    lateinit var cardReaderCountryConfigProvider: CardReaderCountryConfigProvider
+
+    @Inject
+    lateinit var wooStore: WooCommerceStore
+
+    @Inject
+    lateinit var selectedSite: SelectedSite
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_woo_pos_card_reader)
 
-        val navController = findNavController(R.id.woopos_card_reader_nav_host_fragment)
-        navController.setGraph(R.navigation.nav_graph_payment_flow, intent.extras)
-        navController.navigate(
-            MoreMenuFragmentDirections.actionMoreMenuToPaymentFlow(
-                CardReaderFlowParam.PaymentOrRefund.Payment(
-                    orderId = 1,
-                    paymentType = CardReaderFlowParam.PaymentOrRefund.Payment.PaymentType.SIMPLE,
-                )
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.woopos_card_reader_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val countryConfig = cardReaderCountryConfigProvider.provideCountryConfigFor(
+            wooStore.getStoreCountryCode(selectedSite.get())
+        ) as CardReaderConfigForSupportedCountry
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            Log.d("WOOPOS", "current destination: $destination")
+        }
+        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph_payment_flow).apply {
+            setStartDestination(R.id.cardReaderTutorialDialogFragment)
+        }
+
+        lifecycleScope.launch {
+            val result = orderCreateEditRepository.createSimplePaymentOrder(
+                countryConfig.minimumAllowedChargeAmount,
+                customerNote = resourceProvider.getString(R.string.card_reader_tap_to_pay_test_payment_note),
+                isTaxable = false,
             )
+            result.fold(
+                onSuccess = {
+                    withContext(Dispatchers.Main) {
+                            val args = collectPaymentArgs(it.id)
+                            navController.setGraph(
+                                navGraph,
+                                args
+                            )
+
+                            navController.navigate(R.id.cardReaderTutorialDialogFragment, args)
+                    }
+                },
+                onFailure = {
+                    Snackbar.make(
+                        findViewById(android.R.id.content),
+                        "Failed to create order",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            )
+        }
+    }
+
+    private fun collectPaymentArgs(orderId: Long) = Bundle().apply {
+        putParcelable(
+            "cardReaderFlowParam",
+            CardReaderFlowParam.PaymentOrRefund.Payment(
+                orderId = orderId,
+                paymentType = CardReaderFlowParam.PaymentOrRefund.Payment.PaymentType.SIMPLE,
+            )
+        )
+        putParcelable(
+            "cardReaderType",
+            CardReaderType.EXTERNAL
         )
     }
 }
