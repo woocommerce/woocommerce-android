2893bdfe95e53d205ed75e7f8b52314d5f1f7a2b
Rooney <backwardstruck@users.noreply.github.com>
2024-05-28 14:07:50 -0400

    WooThemeWithBackground

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/ChangeDueCalculatorFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/ChangeDueCalculatorFragment.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.main.AppBarStatus
 import com.woocommerce.android.ui.payments.cardreader.onboarding.CardReaderFlowParam
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,23 +39,25 @@ class ChangeDueCalculatorFragment : BaseFragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                val uiState by viewModel.uiState.collectAsState()
-                ChangeDueCalculatorScreen(
-                    uiState = uiState,
-                    onNavigateUp = { viewModel.onBackPressed() },
-                    onCompleteOrderClick = {
-                        val action = ChangeDueCalculatorFragmentDirections
-                            .actionChangeDueCalculatorFragmentToSelectPaymentMethodFragment(
-                                cardReaderFlowParam = CardReaderFlowParam.PaymentOrRefund.Payment(
-                                    viewModel.navArgs.orderId,
-                                    CardReaderFlowParam.PaymentOrRefund.Payment.PaymentType.ORDER
-                                ),
-                                isOrderPaid = true
-                            )
-                        findNavController().navigate(action)
-                    },
-                    onAmountReceivedChanged = { viewModel.updateAmountReceived(it) }
-                )
+                WooThemeWithBackground {
+                    val uiState by viewModel.uiState.collectAsState()
+                    ChangeDueCalculatorScreen(
+                        uiState = uiState,
+                        onNavigateUp = { viewModel.onBackPressed() },
+                        onCompleteOrderClick = {
+                            val action = ChangeDueCalculatorFragmentDirections
+                                .actionChangeDueCalculatorFragmentToSelectPaymentMethodFragment(
+                                    cardReaderFlowParam = CardReaderFlowParam.PaymentOrRefund.Payment(
+                                        viewModel.navArgs.orderId,
+                                        CardReaderFlowParam.PaymentOrRefund.Payment.PaymentType.ORDER
+                                    ),
+                                    isOrderPaid = true
+                                )
+                            findNavController().navigate(action)
+                        },
+                        onAmountReceivedChanged = { viewModel.updateAmountReceived(it) }
+                    )
+                }
             }
         }
     }
