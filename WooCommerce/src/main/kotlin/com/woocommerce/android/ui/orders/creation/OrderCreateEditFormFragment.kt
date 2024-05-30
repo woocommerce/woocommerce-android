2cc1684240f99ee036be783fe132e4cbc719822e
Alejo <alejandro.torres.veiga@automattic.com>
2024-05-30 10:01:53 -0600

    hide survey behind a feature flag

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditFormFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditFormFragment.kt
@@ -93,6 +93,7 @@ import com.woocommerce.android.ui.products.selector.ProductSelectorSharedViewMod
 import com.woocommerce.android.ui.products.selector.ProductSelectorViewModel
 import com.woocommerce.android.ui.products.selector.ProductSelectorViewModel.SelectedItem
 import com.woocommerce.android.util.CurrencyFormatter
+import com.woocommerce.android.util.FeatureFlag
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowDialog
@@ -410,7 +411,9 @@ class OrderCreateEditFormFragment :
 
         bindShippingLinesSection(binding)
 
-        bindFeedbackSection(binding)
+        if (FeatureFlag.EOSL_M3.isEnabled()) {
+            bindFeedbackSection(binding)
+        }
 
         observeViewStateChanges(binding)
 
