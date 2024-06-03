6f5ef63b4965bcf7c29c92375fa3827e2eaafd47
Andrey <akidinov@gmail.com>
2024-06-03 10:56:08 +0200

    Removed usage of ScopedViewModel from some VMs

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cart/WooPosCartViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cart/WooPosCartViewModel.kt
@@ -1,11 +1,8 @@
 package com.woocommerce.android.ui.woopos.cart
 
-import androidx.lifecycle.SavedStateHandle
-import com.woocommerce.android.viewmodel.ScopedViewModel
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class WooPosCartViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-) : ScopedViewModel(savedStateHandle)
+class WooPosCartViewModel @Inject constructor() : ViewModel()
