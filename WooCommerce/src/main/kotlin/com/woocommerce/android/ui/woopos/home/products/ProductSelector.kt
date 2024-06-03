c5dbaf20250c21ed10b2e4ba0c87df2d18a75e94
samiuelson <samiuelson@gmail.com>
2024-06-03 21:41:01 +0200

    Update grid UI

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/ProductSelector.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/ProductSelector.kt
@@ -1,12 +1,18 @@
 package com.woocommerce.android.ui.woopos.home.products
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import com.woocommerce.android.ui.woopos.util.WooPosPreview
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -36,20 +42,19 @@ fun ProductSelector(
         val state = productsState.collectAsState()
         val gridState = rememberLazyGridState()
         LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 128.dp),
-            horizontalArrangement = spacedBy(4.dp),
-            verticalArrangement = spacedBy(4.dp),
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = spacedBy(34.dp),
+            verticalArrangement = spacedBy(34.dp),
+            contentPadding = PaddingValues(16.dp),
             state = gridState
         ) {
-            items(
-                count = state.value.products.size,
-                key = { index -> state.value.products[index].productId }
-            ) { index ->
-                ProductItem(product = state.value.products[index])
+            itemsIndexed(state.value.products) { _, product ->
+                ProductItem(product = product)
             }
         }
         InfiniteGridHandler(gridState) {
             onEndOfProductsGridReached()
+            Log.d("ProductSelector", "End of products grid reached")
         }
     }
 }
@@ -57,7 +62,10 @@ fun ProductSelector(
 @Composable
 fun ProductItem(product: ListItem) {
     ConstraintLayout(
-        modifier = Modifier.background(Color.Yellow)
+        modifier = Modifier
+            .border(1.dp, Color.Gray, shape = RoundedCornerShape(4.dp))
+            .padding(16.dp)
+            .fillMaxWidth(0.5f)
     ) {
         Text(
             text = product.title,
@@ -68,7 +76,11 @@ fun ProductItem(product: ListItem) {
 }
 
 @Composable
-fun InfiniteGridHandler(gridState: LazyGridState, buffer: Int = 1, onEndOfProductsGridReached: () -> Unit) {
+fun InfiniteGridHandler(
+    gridState: LazyGridState,
+    buffer: Int = 1,
+    onEndOfProductsGridReached: () -> Unit
+) {
     val loadMore = remember {
         derivedStateOf {
             val layoutInfo = gridState.layoutInfo
