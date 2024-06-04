b2d3a1c8a95f9260e484e8f0346f79a0b825a1f2
Rooney <backwardstruck@users.noreply.github.com>
2024-06-04 02:29:05 -0400

    Handle the space and update test

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/CurrencyVisualTransformation.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/CurrencyVisualTransformation.kt
@@ -1,9 +1,9 @@
 package com.woocommerce.android.ui.payments.changeduecalculator
 
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import org.wordpress.android.fluxc.model.WCSettingsModel
 
 class CurrencyVisualTransformation(
@@ -36,7 +36,8 @@ class CurrencyVisualTransformation(
         val offsetMapping = object : OffsetMapping {
             override fun originalToTransformed(offset: Int): Int {
                 return if (currencyPosition == WCSettingsModel.CurrencyPosition.LEFT ||
-                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE) {
+                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE
+                ) {
                     offset + adjustedPrefixLength
                 } else {
                     offset
@@ -45,7 +46,8 @@ class CurrencyVisualTransformation(
 
             override fun transformedToOriginal(offset: Int): Int {
                 return if (currencyPosition == WCSettingsModel.CurrencyPosition.LEFT ||
-                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE) {
+                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE
+                ) {
                     (offset - adjustedPrefixLength).coerceAtLeast(0)
                 } else {
                     offset.coerceAtMost(text.length)
