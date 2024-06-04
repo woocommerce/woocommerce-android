785a91ca00df7645478d21ba8c8bc7e145b1bdd4
Rooney <backwardstruck@users.noreply.github.com>
2024-06-04 02:26:33 -0400

    Handle the space

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/CurrencyVisualTransformation.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/changeduecalculator/CurrencyVisualTransformation.kt
@@ -1,9 +1,9 @@
 package com.woocommerce.android.ui.payments.changeduecalculator
 
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.input.VisualTransformation
 import org.wordpress.android.fluxc.model.WCSettingsModel
 
 class CurrencyVisualTransformation(
@@ -12,21 +12,32 @@ class CurrencyVisualTransformation(
 ) : VisualTransformation {
     override fun filter(text: AnnotatedString): TransformedText {
         val transformedText = when (currencyPosition) {
-            WCSettingsModel.CurrencyPosition.RIGHT, WCSettingsModel.CurrencyPosition.RIGHT_SPACE -> {
+            WCSettingsModel.CurrencyPosition.RIGHT -> {
                 text.text + currencySymbol
             }
-
+            WCSettingsModel.CurrencyPosition.RIGHT_SPACE -> {
+                text.text + " " + currencySymbol
+            }
+            WCSettingsModel.CurrencyPosition.LEFT_SPACE -> {
+                currencySymbol + " " + text.text
+            }
             else -> {
                 currencySymbol + text.text
             }
         }
 
+        val adjustedPrefixLength = when (currencyPosition) {
+            WCSettingsModel.CurrencyPosition.LEFT_SPACE -> currencySymbol.length + 1
+            WCSettingsModel.CurrencyPosition.RIGHT_SPACE -> currencySymbol.length + 1
+            WCSettingsModel.CurrencyPosition.LEFT -> currencySymbol.length
+            else -> 0
+        }
+
         val offsetMapping = object : OffsetMapping {
             override fun originalToTransformed(offset: Int): Int {
                 return if (currencyPosition == WCSettingsModel.CurrencyPosition.LEFT ||
-                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE
-                ) {
-                    offset + currencySymbol.length
+                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE) {
+                    offset + adjustedPrefixLength
                 } else {
                     offset
                 }
@@ -34,9 +45,8 @@ class CurrencyVisualTransformation(
 
             override fun transformedToOriginal(offset: Int): Int {
                 return if (currencyPosition == WCSettingsModel.CurrencyPosition.LEFT ||
-                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE
-                ) {
-                    (offset - currencySymbol.length).coerceAtLeast(0)
+                    currencyPosition == WCSettingsModel.CurrencyPosition.LEFT_SPACE) {
+                    (offset - adjustedPrefixLength).coerceAtLeast(0)
                 } else {
                     offset.coerceAtMost(text.length)
                 }
