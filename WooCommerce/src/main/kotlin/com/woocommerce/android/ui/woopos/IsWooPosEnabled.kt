7e8635a0f54bb1e2abbb090a301bd2329bb85290
Andrey <akidinov@gmail.com>
2024-06-05 16:08:49 +0200

    Added check on WC Core version

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/IsWooPosEnabled.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/IsWooPosEnabled.kt
@@ -1,7 +1,9 @@
 package com.woocommerce.android.ui.woopos
 
+import com.woocommerce.android.extensions.semverCompareTo
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.payments.GetActivePaymentsPlugin
+import com.woocommerce.android.util.GetWooCorePluginCachedVersion
 import com.woocommerce.android.util.IsWindowClassExpandedAndBigger
 import org.wordpress.android.fluxc.model.payments.inperson.WCPaymentAccountResult
 import org.wordpress.android.fluxc.store.WCInPersonPaymentsStore
@@ -16,6 +18,7 @@ class IsWooPosEnabled @Inject constructor(
     private val getActivePaymentsPlugin: GetActivePaymentsPlugin,
     private val isWindowSizeExpandedAndBigger: IsWindowClassExpandedAndBigger,
     private val isWooPosFFEnabled: IsWooPosFFEnabled,
+    private val getWooCoreVersion: GetWooCorePluginCachedVersion,
 ) {
     private var cachedResult: Boolean? = null
 
@@ -35,11 +38,22 @@ class IsWooPosEnabled @Inject constructor(
                 ippPlugin == WOOCOMMERCE_PAYMENTS &&
                 paymentAccount.storeCurrencies.default.lowercase() == "usd" &&
                 isWindowSizeExpandedAndBigger() &&
-                isPluginSetupEnabled(paymentAccount)
+                isPluginSetupEnabled(paymentAccount) &&
+                isWooCoreSupportsOrderAutoDraftsAndExtraPaymentsProps()
             ).also { cachedResult = it }
     }
 
     private fun isPluginSetupEnabled(paymentAccount: WCPaymentAccountResult): Boolean =
         paymentAccount.status == WCPaymentAccountResult.WCPaymentAccountStatus.COMPLETE ||
             paymentAccount.status == WCPaymentAccountResult.WCPaymentAccountStatus.ENABLED
+
+    private fun isWooCoreSupportsOrderAutoDraftsAndExtraPaymentsProps(): Boolean {
+        val wooCoreVersion = getWooCoreVersion() ?: return false
+        return wooCoreVersion.semverCompareTo(WC_VERSION_SUPPORTS_ORDER_AUTO_DRAFTS_AND_EXTRA_PAYMENTS_PROPS) >= 0
+    }
+
+
+    private companion object {
+        const val WC_VERSION_SUPPORTS_ORDER_AUTO_DRAFTS_AND_EXTRA_PAYMENTS_PROPS = "6.6.0"
+    }
 }
