9483431080f104c3103d633e74ff7ad9d8cc8e8b
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-06-05 17:16:26 +0200

    Track card interacted for orders card

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/orders/DashboardOrdersViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/orders/DashboardOrdersViewModel.kt
@@ -159,6 +159,7 @@ class DashboardOrdersViewModel @AssistedInject constructor(
         }
 
     private fun onNavigateToOrders() {
+        parentViewModel.trackCardInteracted(DashboardWidget.Type.ORDERS.trackingIdentifier)
         triggerEvent(NavigateToOrders)
     }
 
@@ -173,10 +174,12 @@ class DashboardOrdersViewModel @AssistedInject constructor(
     }
 
     fun onFilterSelected(filter: OrderStatusOption) {
+        parentViewModel.trackCardInteracted(DashboardWidget.Type.ORDERS.trackingIdentifier)
         selectedFilter.value = filter.key
     }
 
     fun onOrderClicked(orderId: Long) {
+        parentViewModel.trackCardInteracted(DashboardWidget.Type.ORDERS.trackingIdentifier)
         triggerEvent(NavigateToOrderDetails(orderId))
     }
 
