d74f88bdc91ededb214c233cd7c326f0510e20e4
Andrey <akidinov@gmail.com>
2024-06-06 15:59:12 +0200

    UI model for exit pos confirmation dialog

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeState.kt
@@ -1,6 +1,18 @@
 package com.woocommerce.android.ui.woopos.home
 
+import androidx.annotation.StringRes
+
 sealed class WooPosHomeState {
-    data object Cart : WooPosHomeState()
+    data class Cart(
+        val exitConfirmationDialog: WooPosExitConfirmationDialog?
+    ) : WooPosHomeState()
+
     data object Checkout : WooPosHomeState()
 }
+
+data class WooPosExitConfirmationDialog(
+    @StringRes val title: Int,
+    @StringRes val message: Int,
+    @StringRes val positiveButton: Int,
+    @StringRes val negativeButton: Int,
+)
