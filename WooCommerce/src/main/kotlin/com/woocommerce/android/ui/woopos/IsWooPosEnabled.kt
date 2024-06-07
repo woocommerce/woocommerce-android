a50bda1122df5691727ca121c450fc36b507e29c
malinajirka <malinajirka@gmail.com>
2024-06-07 11:13:21 +0200

    Rearrange statements to reduce remote requests

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/IsWooPosEnabled.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/IsWooPosEnabled.kt
@@ -2,9 +2,9 @@ package com.woocommerce.android.ui.woopos
 
 import com.woocommerce.android.extensions.semverCompareTo
 import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.ui.payments.GetActivePaymentsPlugin
 import com.woocommerce.android.ui.payments.cardreader.onboarding.CardReaderOnboardingChecker
 import com.woocommerce.android.ui.payments.cardreader.onboarding.CardReaderOnboardingState
+import com.woocommerce.android.ui.payments.cardreader.onboarding.PluginType
 import com.woocommerce.android.util.GetWooCorePluginCachedVersion
 import com.woocommerce.android.util.IsWindowClassExpandedAndBigger
 import org.wordpress.android.fluxc.model.SiteModel
@@ -20,7 +20,6 @@ private typealias LocalSiteId = Int
 class IsWooPosEnabled @Inject constructor(
     private val selectedSite: SelectedSite,
     private val ippStore: WCInPersonPaymentsStore,
-    private val getActivePaymentsPlugin: GetActivePaymentsPlugin,
     private val isWindowSizeExpandedAndBigger: IsWindowClassExpandedAndBigger,
     private val isWooPosFFEnabled: IsWooPosFFEnabled,
     private val getWooCoreVersion: GetWooCorePluginCachedVersion,
@@ -33,20 +32,18 @@ class IsWooPosEnabled @Inject constructor(
         val selectedSite = selectedSite.getOrNull() ?: return false
 
         if (!isWooPosFFEnabled()) return false
+        if (!isWindowSizeExpandedAndBigger()) return false
+        if (!isWooCoreSupportsOrderAutoDraftsAndExtraPaymentsProps()) return false
 
-        val ippPlugin = getActivePaymentsPlugin() ?: return false
         val onboardingStatus = cardReaderOnboardingChecker.getOnboardingState()
-        val paymentAccount = getOrFetchPaymentAccount(selectedSite, ippPlugin) ?: return false
-        val countryCode = paymentAccount.country
 
-        return (
-            countryCode.lowercase() == "us" &&
-                ippPlugin == WOOCOMMERCE_PAYMENTS &&
-                paymentAccount.storeCurrencies.default.lowercase() == "usd" &&
-                isWindowSizeExpandedAndBigger() &&
-                isIPPOnboardingCompleted(onboardingStatus) &&
-                isWooCoreSupportsOrderAutoDraftsAndExtraPaymentsProps()
-            )
+        if (onboardingStatus.preferredPlugin != PluginType.WOOCOMMERCE_PAYMENTS) return false
+        if (!isIPPOnboardingCompleted(onboardingStatus)) return false
+
+
+        val paymentAccount = getOrFetchPaymentAccount(selectedSite, WOOCOMMERCE_PAYMENTS) ?: return false
+        if (paymentAccount.country.lowercase() != "us") return false
+        return paymentAccount.storeCurrencies.default.lowercase() == "usd"
     }
 
     private suspend fun getOrFetchPaymentAccount(
