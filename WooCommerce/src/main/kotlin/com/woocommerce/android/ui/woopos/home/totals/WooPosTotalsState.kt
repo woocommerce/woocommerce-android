49af108df2ef01a45607c4b700e622ce0bf0fd89
Rooney <backwardstruck@users.noreply.github.com>
2024-06-13 13:14:35 -0400

    Remove TODO

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsState.kt
@@ -5,7 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class WooPosTotalsState(
-    val orderId: Long?,// TODO: remove. Ultimately we don't need to display order id anywhere
+    val orderId: Long?,
     val isCollectPaymentButtonEnabled: Boolean,
     var orderTotals: java.math.BigDecimal,
     // taxAmount = updatedOrder.tax,
