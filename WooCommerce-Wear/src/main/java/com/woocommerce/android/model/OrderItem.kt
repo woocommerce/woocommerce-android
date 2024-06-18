aba6f8c3250dbb889199cf79924d9f058dd022d0
ThomazFB <thomazfcortez@gmail.com>
2024-06-18 14:58:36 -0300

    Add Order Item name mapping to the parsing functions

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/model/OrderItem.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/model/OrderItem.kt
@@ -5,6 +5,7 @@ import org.wordpress.android.fluxc.model.order.LineItem
 
 data class OrderItem(
     val itemId: Long,
+    val name: String,
     val quantity: Float,
     val totalTax: BigDecimal,
     val price: BigDecimal,
@@ -13,6 +14,7 @@ data class OrderItem(
 
 fun LineItem.toAppModel() = OrderItem(
     itemId = id ?: 0L,
+    name = name.orEmpty(),
     quantity = quantity ?: 0F,
     totalTax = totalTax?.toBigDecimalOrNull() ?: BigDecimal.ZERO,
     price = price?.toBigDecimalOrNull() ?: BigDecimal.ZERO,
