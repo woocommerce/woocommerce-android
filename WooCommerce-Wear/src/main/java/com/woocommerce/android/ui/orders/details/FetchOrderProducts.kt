b6711de64f52204329d93162190e8b75fca73bff
ThomazFB <thomazfcortez@gmail.com>
2024-06-18 14:58:55 -0300

    Adjust FetchOrderProducts use case to request the Orders Refunds when network is available

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/FetchOrderProducts.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/ui/orders/details/FetchOrderProducts.kt
@@ -1,7 +1,9 @@
 package com.woocommerce.android.ui.orders.details
 
 import com.woocommerce.android.extensions.combineWithTimeout
+import com.woocommerce.android.model.getNonRefundedProducts
 import com.woocommerce.android.phone.PhoneConnectionRepository
+import com.woocommerce.android.system.NetworkStatus
 import com.woocommerce.android.ui.orders.OrdersRepository
 import com.woocommerce.android.ui.orders.details.FetchOrderProducts.OrderProductsRequest.Error
 import com.woocommerce.android.ui.orders.details.FetchOrderProducts.OrderProductsRequest.Finished
@@ -13,25 +15,18 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flowOf
 import org.wordpress.android.fluxc.model.SiteModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.flow
 
 class FetchOrderProducts @Inject constructor(
     private val phoneRepository: PhoneConnectionRepository,
-    private val ordersRepository: OrdersRepository
+    private val ordersRepository: OrdersRepository,
+    private val networkStatus: NetworkStatus
 ) {
     suspend operator fun invoke(
         selectedSite: SiteModel,
         orderId: Long
     ): Flow<OrderProductsRequest> {
-        if (phoneRepository.isPhoneConnectionAvailable().not()) {
-            return flowOf(Error)
-        }
-
-        phoneRepository.sendMessage(
-            REQUEST_ORDER_PRODUCTS,
-            orderId.toString().toByteArray()
-        )
-
-        return ordersRepository.observeOrderProductsDataChanges(orderId, selectedSite.siteId)
+        return selectDataSource(selectedSite, orderId)
             .combineWithTimeout(TIMEOUT_FOR_ORDER_PRODUCTS) { orderProducts, isTimeout ->
                 when {
                     orderProducts.isNotEmpty() -> Finished(orderProducts)
@@ -39,6 +34,33 @@ class FetchOrderProducts @Inject constructor(
                     else -> Error
                 }
             }.filterNotNull()
+    }
+
+    private suspend fun selectDataSource(
+        selectedSite: SiteModel,
+        orderId: Long
+    ): Flow<List<WearOrderedProduct>> {
+        return when {
+            networkStatus.isConnected() -> flow {
+                ordersRepository.fetchOrderRefunds(selectedSite, orderId)
+                    .getNonRefundedProducts(emptyList())
+                    .map {
+                        WearOrderedProduct(
+                            amount = it.quantity.toString(),
+                            total = it.total.toString(),
+                            name = it.name
+                        )
+                    }
+            }
+            phoneRepository.isPhoneConnectionAvailable() -> {
+                phoneRepository.sendMessage(
+                    REQUEST_ORDER_PRODUCTS,
+                    orderId.toString().toByteArray()
+                )
+                ordersRepository.observeOrderProductsDataChanges(orderId, selectedSite.siteId)
+            }
+            else -> flowOf(emptyList()) // Retrieve stored data instead
+        }
     }
 
     sealed class OrderProductsRequest {
