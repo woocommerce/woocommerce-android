6f72c5dab5e5b05ed0a49d4c6b7a00325fa1a2bb
Alejo <alejandro.torres.veiga@automattic.com>
2024-06-20 16:31:26 -0600

    add menu customer list

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/customer/MenuCustomerListFragment.kt
@@ -0,0 +1,59 @@
+package com.woocommerce.android.ui.customer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.main.AppBarStatus
+import com.woocommerce.android.viewmodel.MultiLiveEvent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MenuCustomerListFragment : BaseFragment() {
+
+    private val viewModel by viewModels<CustomerListDetailsViewModel>()
+
+    override val activityAppBarStatus: AppBarStatus = AppBarStatus.Hidden
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            WooThemeWithBackground {
+                MenuCustomerListScreen(viewModel)
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.event.observe(
+            viewLifecycleOwner
+        ) { event ->
+            when (event) {
+                is CustomerSelected -> {
+                    Toast.makeText(requireContext(), "Customer ${event.remoteCustomerId}", Toast.LENGTH_LONG).show()
+                }
+
+                is MultiLiveEvent.Event.Exit -> {
+                    findNavController().navigateUp()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        AnalyticsTracker.trackViewShown(this)
+    }
+}
