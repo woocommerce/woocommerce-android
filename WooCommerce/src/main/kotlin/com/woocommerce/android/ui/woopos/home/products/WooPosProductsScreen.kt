41a40878eb474c554a77cfed181c9d43e3590883
Andrey <akidinov@gmail.com>
2024-06-21 11:10:33 +0200

    Small adjustments to the products screen to move it towards correct UI

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsScreen.kt
@@ -1,7 +1,5 @@
 package com.woocommerce.android.ui.woopos.home.products
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,12 +7,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyGridState
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -27,11 +25,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosTheme
 import com.woocommerce.android.ui.woopos.home.products.WooPosProductsUIEvent.EndOfProductsGridReached
 import com.woocommerce.android.ui.woopos.home.products.WooPosProductsUIEvent.ItemClicked
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -55,18 +53,11 @@ private fun WooPosProductsScreen(
     onItemClicked: (item: WooPosProductsListItem) -> Unit,
     onEndOfProductsGridReached: () -> Unit,
 ) {
-    Card(
-        shape = RoundedCornerShape(16.dp),
-        backgroundColor = MaterialTheme.colors.surface,
-        elevation = 4.dp,
-        modifier = Modifier.padding(16.dp)
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
     ) {
-        Box(
-            Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
-        ) {
-            ProductSelector(productsState, onItemClicked, onEndOfProductsGridReached)
-        }
+        ProductSelector(productsState, onItemClicked, onEndOfProductsGridReached)
     }
 }
 
@@ -79,23 +70,20 @@ fun ProductSelector(
     ConstraintLayout(
         modifier = Modifier
             .fillMaxHeight()
-            .background(MaterialTheme.colors.surface, RoundedCornerShape(8.dp))
             .padding(16.dp)
     ) {
         val state = productsState.collectAsState()
-        val gridState = rememberLazyGridState()
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(34.dp),
-            verticalArrangement = Arrangement.spacedBy(34.dp),
-            contentPadding = PaddingValues(16.dp),
-            state = gridState
+        val listState = rememberLazyListState()
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(2.dp),
+            state = listState
         ) {
             itemsIndexed(state.value.products) { _, product ->
                 ProductItem(item = product, onItemClicked = onItemClicked)
             }
         }
-        InfiniteGridHandler(gridState) {
+        InfiniteGridHandler(listState) {
             onEndOfProductsGridReached()
         }
     }
@@ -106,30 +94,43 @@ private fun ProductItem(
     item: WooPosProductsListItem,
     onItemClicked: (item: WooPosProductsListItem) -> Unit
 ) {
-    ConstraintLayout(
-        modifier = Modifier
-            .border(2.dp, Color.Gray, shape = RoundedCornerShape(4.dp))
-            .clickable { onItemClicked(item) }
-            .padding(16.dp)
-            .fillMaxWidth(0.5f)
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = MaterialTheme.colors.surface,
+        elevation = 2.dp,
     ) {
-        Text(
-            text = item.title,
-            style = MaterialTheme.typography.body1,
-            color = MaterialTheme.colors.onSurface,
-        )
+        ConstraintLayout(
+            modifier = Modifier
+                .clickable { onItemClicked(item) }
+                .height(112.dp)
+                .fillMaxWidth()
+        ) {
+            val (textRef) = createRefs()
+
+            Text(
+                text = item.title,
+                style = MaterialTheme.typography.h4,
+                color = MaterialTheme.colors.onSurface,
+                modifier = Modifier.constrainAs(textRef) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+        }
     }
 }
 
 @Composable
 private fun InfiniteGridHandler(
-    gridState: LazyGridState,
+    listState: LazyListState,
     buffer: Int = 1,
     onEndOfProductsGridReached: () -> Unit
 ) {
     val loadMore = remember {
         derivedStateOf {
-            val layoutInfo = gridState.layoutInfo
+            val layoutInfo = listState.layoutInfo
             val totalItemsNumber = layoutInfo.totalItemsCount
             val lastVisibleItemIndex = (layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: 0) + 1
 
@@ -159,9 +160,11 @@ fun WooPosHomeScreenPreview() {
             )
         )
     )
-    WooPosProductsScreen(
-        productsState = productState,
-        onItemClicked = {},
-        onEndOfProductsGridReached = {}
-    )
+    WooPosTheme {
+        WooPosProductsScreen(
+            productsState = productState,
+            onItemClicked = {},
+            onEndOfProductsGridReached = {}
+        )
+    }
 }
