7766445a06ac767fc41538282385f054a0b78cd9
ThomazFB <thomazfcortez@gmail.com>
2024-06-21 11:49:26 -0300

    Adjust OrdersComplicationService to better describe the Order's preview data

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/OrdersComplicationService.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/OrdersComplicationService.kt
@@ -4,15 +4,27 @@ import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationType
 import androidx.wear.watchface.complications.data.PlainComplicationText
 import androidx.wear.watchface.complications.data.ShortTextComplicationData
+import androidx.wear.watchface.complications.data.SmallImage
+import androidx.wear.watchface.complications.data.SmallImageComplicationData
 import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import com.woocommerce.android.R
 
 class OrdersComplicationService : SuspendingComplicationDataSourceService() {
     override fun getPreviewData(type: ComplicationType): ComplicationData? {
-        if (type != ComplicationType.SHORT_TEXT) {
-            return null
+
+        return when (type) {
+            ComplicationType.SHORT_TEXT -> {
+                createComplicationData(
+                    text = getString(R.string.orders_complication_preview_data),
+                    contentDescription = getString(R.string.orders_complication_preview_description)
+                )
+            }
+            ComplicationType.SMALL_IMAGE -> {
+                null
+            }
+            else -> null
         }
-        return createComplicationData("Mon", "Monday")
     }
 
     override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData {
