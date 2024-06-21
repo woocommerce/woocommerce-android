b51b7f8b64a495176ecc48e18efa0a9d4ea5e84f
ThomazFB <thomazfcortez@gmail.com>
2024-06-21 12:00:51 -0300

    Introduce proper tap action into the Complications

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/OrdersComplicationService.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/OrdersComplicationService.kt
@@ -1,5 +1,8 @@
 package com.woocommerce.android.wear.complications
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationType
 import androidx.wear.watchface.complications.data.PlainComplicationText
@@ -9,6 +12,7 @@ import androidx.wear.watchface.complications.data.SmallImageComplicationData
 import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
 import com.woocommerce.android.R
+import com.woocommerce.android.app.WearMainActivity
 
 class OrdersComplicationService : SuspendingComplicationDataSourceService() {
     override fun getPreviewData(type: ComplicationType): ComplicationData? {
@@ -20,20 +24,48 @@ class OrdersComplicationService : SuspendingComplicationDataSourceService() {
                     contentDescription = getString(R.string.orders_complication_preview_description)
                 )
             }
-            ComplicationType.SMALL_IMAGE -> {
-                null
-            }
+
+            ComplicationType.SMALL_IMAGE -> createImageComplicationData(
+                contentDescription = getString(R.string.icon_complication_preview_description)
+            )
+
             else -> null
         }
     }
 
     override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData {
-        return createComplicationData("Test", "Description")
+        return when (request.complicationType) {
+            ComplicationType.SHORT_TEXT -> createComplicationData(
+                text = "Test",
+                contentDescription = getString(R.string.orders_complication_preview_description)
+            )
+
+            else -> createImageComplicationData(
+                contentDescription = getString(R.string.icon_complication_preview_description)
+            )
+        }
     }
 
     private fun createComplicationData(text: String, contentDescription: String) =
         ShortTextComplicationData.Builder(
             text = PlainComplicationText.Builder(text).build(),
             contentDescription = PlainComplicationText.Builder(contentDescription).build()
-        ).build()
+        ).setTapAction(createAppPendingIntent()).build()
+
+    private fun createImageComplicationData(
+        contentDescription: String
+    ) = SmallImageComplicationData.Builder(
+        smallImage = SmallImage.PLACEHOLDER,
+        contentDescription = PlainComplicationText.Builder(contentDescription).build()
+    ).setTapAction(createAppPendingIntent()).build()
+
+    private fun createAppPendingIntent(): PendingIntent {
+        val intent = Intent(applicationContext, WearMainActivity::class.java)
+        val flags = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S) {
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+        return PendingIntent.getActivity(applicationContext, 0, intent, flags)
+    }
 }
