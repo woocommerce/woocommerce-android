6ac5820fb82269691f14d83551c94b93bc951455
ThomazFB <thomazfcortez@gmail.com>
2024-06-24 07:43:12 -0300

    Revert "Remove Order Count complication"
    This reverts commit dc4156a69d2c5904bf2deddd778d8947f8836de4.

---
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/OrderCountComplicationService.kt
@@ -0,0 +1,35 @@
+package com.woocommerce.android.wear.complications
+
+import androidx.wear.watchface.complications.data.ComplicationData
+import androidx.wear.watchface.complications.data.ComplicationType
+import androidx.wear.watchface.complications.datasource.ComplicationRequest
+import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import com.woocommerce.android.R
+
+class OrderCountComplicationService : SuspendingComplicationDataSourceService() {
+    override fun getPreviewData(type: ComplicationType): ComplicationData? {
+        return when (type) {
+            ComplicationType.SHORT_TEXT -> {
+                createTextComplicationData(
+                    context = applicationContext,
+                    content = "5",
+                    description = getString(R.string.orders_complication_preview_description)
+                )
+            }
+
+            else -> null
+        }
+    }
+
+    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
+        return when (request.complicationType) {
+            ComplicationType.SHORT_TEXT -> createTextComplicationData(
+                context = applicationContext,
+                content = "10", // get Today's Orders count
+                description = getString(R.string.orders_complication_preview_description)
+            )
+
+            else -> null
+        }
+    }
+}
