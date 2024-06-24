d76b5da0fb7fc9384a4ff64417c9d035253c7fda
Andrey <akidinov@gmail.com>
2024-06-24 17:05:16 +0200

    Use int as identifier to launch

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,7 +24,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -80,13 +84,16 @@ private fun WooPosCartScreen(
 
                 Spacer(modifier = Modifier.height(16.dp))
 
+                val listState = rememberLazyListState()
+                ScrollToBottomHandler(state, listState)
+
                 LazyColumn(
                     modifier = Modifier
                         .weight(1f)
                         .fillMaxWidth(),
+                    state = listState,
                     verticalArrangement = Arrangement.spacedBy(8.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    state = rememberLazyListState()
                 ) {
                     items(state.itemsInCart) { item ->
                         ProductItem(
@@ -113,6 +120,21 @@ private fun WooPosCartScreen(
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun ScrollToBottomHandler(
+    state: WooPosCartState,
+    listState: LazyListState
+) {
+    val previousItemsCount = remember { mutableIntStateOf(0) }
+    val itemsInCartSize = state.itemsInCart.size
+    LaunchedEffect(itemsInCartSize) {
+        if (itemsInCartSize > previousItemsCount.intValue) {
+            listState.animateScrollToItem(itemsInCartSize - 1)
+        }
+        previousItemsCount.intValue = itemsInCartSize
     }
 }
 
