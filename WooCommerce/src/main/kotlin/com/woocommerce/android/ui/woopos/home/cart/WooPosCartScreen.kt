a4f7d2d4847556fcf47ffb65241d940af94b1521
Andrey <akidinov@gmail.com>
2024-06-25 16:05:03 +0200

    Extracted content description to a string

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
@@ -114,7 +114,7 @@ private fun CartToolbar(
         IconButton(onClick = { onBackClicked() }) {
             Icon(
                 imageVector = ImageVector.vectorResource(toolbar.icon),
-                contentDescription = "cart icon",
+                contentDescription = stringResource(R.string.woo_pos_cart_back_content_description),
                 tint = MaterialTheme.colors.onBackground,
                 modifier = Modifier.size(28.dp)
             )
