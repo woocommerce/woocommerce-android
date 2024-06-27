a35273bc485a0919977f540bde60c96683bc4927
Andrey <akidinov@gmail.com>
2024-06-27 11:00:01 +0200

    Fixed formatting

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsViewModel.kt
@@ -52,7 +52,6 @@ class WooPosProductsViewModel @Inject constructor(
                 onEndOfProductsGridReached()
             }
 
-
             is WooPosProductsUIEvent.ItemClicked -> {
                 onItemClicked(event.item)
             }
