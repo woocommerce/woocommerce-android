6fb29c15968a9288131846bf5ae04071af0046b5
ThomazFB <thomazfcortez@gmail.com>
2024-06-28 11:24:46 -0300

    Fix lint issues

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/ui/login/LoginScreen.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/ui/login/LoginScreen.kt
@@ -2,18 +2,14 @@ package com.woocommerce.android.wear.ui.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
