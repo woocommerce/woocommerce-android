4186d26db06d1714d89fe7a789743fd5dda080cc
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-07-01 12:47:28 +0200

    Move code to new package

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/productinfo/AddProductWithAIFragmentV2.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/productinfo/AddProductWithAIFragmentV2.kt
@@ -1,11 +1,16 @@
-package com.woocommerce.android.ui.products.ai
+package com.woocommerce.android.ui.products.ai.productinfo
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import com.facebook.flipper.plugins.uidebugger.model.InspectableValue.Text
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
@@ -23,7 +28,13 @@ class AddProductWithAIFragmentV2 : BaseFragment() {
 
             setContent {
                 WooThemeWithBackground {
-                    Text("Product creation with AI V2 Fragment")
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        Text("Product creation with AI V2 Fragment")
+                    }
                 }
             }
         }
