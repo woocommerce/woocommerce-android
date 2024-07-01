e377cca2e2e0d22d468aaf086b19077dccd81415
Andrey <akidinov@gmail.com>
2024-07-01 16:02:33 +0200

    Fixed formatting

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeState.kt
@@ -2,8 +2,8 @@ package com.woocommerce.android.ui.woopos.home
 
 sealed class WooPosHomeState {
     sealed class Cart : WooPosHomeState() {
-        data object Empty: Cart()
-        data object NotEmpty: Cart()
+        data object Empty : Cart()
+        data object NotEmpty : Cart()
     }
 
     data object Checkout : WooPosHomeState()
