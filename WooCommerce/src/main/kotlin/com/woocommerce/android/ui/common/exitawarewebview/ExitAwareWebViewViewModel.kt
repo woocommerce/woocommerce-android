546dd8f52dba16f1823ffb3491418aa779051a34
Hafiz Rahman <hfzrhmn@gmail.com>
2024-07-03 18:13:39 +0700

    add exit aware webviewmodel and navigation

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/common/exitawarewebview/ExitAwareWebViewViewModel.kt
@@ -0,0 +1,65 @@
+package com.woocommerce.android.ui.common.exitawarewebview
+
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.ui.common.exitawarewebview.ExitAwareWebViewViewModel.UrlComparisonMode.EQUALITY
+import com.woocommerce.android.ui.common.exitawarewebview.ExitAwareWebViewViewModel.UrlComparisonMode.PARTIAL
+import com.woocommerce.android.ui.common.exitawarewebview.ExitAwareWebViewViewModel.UrlComparisonMode.STARTS_WITH
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ExitWithResult
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.navArgs
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.wordpress.android.fluxc.network.UserAgent
+import javax.inject.Inject
+
+@HiltViewModel
+class ExitAwareWebViewViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    val userAgent: UserAgent
+) : ScopedViewModel(savedStateHandle) {
+    private val navArgs: ExitAwareWebviewFragmentArgs by savedStateHandle.navArgs()
+    private var isExiting = false
+
+    val viewState = navArgs.let {
+        ViewState(
+            urlToLoad = it.urlToLoad,
+            title = it.title,
+            displayMode = it.displayMode,
+            captureBackButton = it.captureBackButton,
+            clearCache = it.clearCache
+        )
+    }
+
+    fun onUrlLoaded(url: String) {
+        fun String.matchesUrl(url: String) = when (navArgs.urlComparisonMode) {
+            PARTIAL -> url.contains(this, ignoreCase = true)
+            EQUALITY -> equals(url, ignoreCase = true)
+            STARTS_WITH -> url.startsWith(this, ignoreCase = true)
+        }
+
+        if (navArgs.urlsToTriggerExit?.any { it.matchesUrl(url) } == true && !isExiting) {
+            isExiting = true
+            triggerEvent(ExitWithResult(Unit))
+        }
+    }
+
+    fun onClose() {
+        triggerEvent(Exit)
+    }
+
+    data class ViewState(
+        val urlToLoad: String,
+        val title: String?,
+        val displayMode: DisplayMode,
+        val captureBackButton: Boolean,
+        val clearCache: Boolean = false
+    )
+
+    enum class UrlComparisonMode {
+        PARTIAL, EQUALITY, STARTS_WITH
+    }
+
+    enum class DisplayMode {
+        REGULAR, MODAL
+    }
+}
