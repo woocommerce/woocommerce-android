99919f21c96898ded6491cff14727c981d961a84
ThomazFB <thomazfcortez@gmail.com>
2024-07-08 19:40:27 -0300

    Add EMPTY constant to GoogleAdsStat

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/GoogleAdsStat.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/GoogleAdsStat.kt
@@ -2,7 +2,13 @@ package com.woocommerce.android.model
 
 data class GoogleAdsStat(
     val campaigns: List<Campaign>
-)
+) {
+    companion object {
+        val EMPTY = GoogleAdsStat(
+            campaigns = emptyList()
+        )
+    }
+}
 
 data class Campaign(
     val id: Long
