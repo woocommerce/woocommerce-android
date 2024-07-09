312ade3703e6f1345f19e2737bf32c76bd02531e
Rooney <backwardstruck@users.noreply.github.com>
2024-07-09 16:18:28 -0400

    Add a few tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsViewModelTest.kt
@@ -3,14 +3,21 @@ package com.woocommerce.android.ui.woopos.home.totals
 import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
 import com.woocommerce.android.ui.woopos.cardreader.WooPosCardReaderFacade
+import com.woocommerce.android.ui.woopos.home.ChildToParentEvent
+import com.woocommerce.android.ui.woopos.home.ParentToChildrenEvent
 import com.woocommerce.android.ui.woopos.home.WooPosChildrenToParentEventSender
 import com.woocommerce.android.ui.woopos.home.WooPosParentToChildrenEventReceiver
 import com.woocommerce.android.ui.woopos.util.format.WooPosFormatPrice
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class WooPosTotalsViewModelTest : BaseUnitTest() {
@@ -19,22 +26,59 @@ class WooPosTotalsViewModelTest : BaseUnitTest() {
     private val cardReaderFacade: WooPosCardReaderFacade = mock()
     private val orderDetailRepository: OrderDetailRepository = mock()
     private val priceFormat: WooPosFormatPrice = mock()
-    private val savedState: SavedStateHandle = mock()
+
+    private fun createMockSavedStateHandle(): SavedStateHandle {
+        val handle = SavedStateHandle()
+        handle["orderId"] = MutableStateFlow(EMPTY_ORDER_ID)
+        handle["totalsViewState"] = MutableStateFlow(WooPosTotalsState.Loading)
+        return handle
+    }
+
+    private companion object {
+        private const val EMPTY_ORDER_ID = -1L
+    }
 
     @Test
-    fun `given state checkout, when SystemBackClicked passed, then BackFromCheckoutToCartClicked event should be sent`() =
-        runTest {
-            // GIVEN
+    fun `initial state is loading`() = runTest {
+        // WHEN
+        val savedState = createMockSavedStateHandle()
+        val viewModel = createViewModel(savedState)
 
+        // THEN
+        assertEquals(WooPosTotalsState.Loading, viewModel.state.value)
+    }
 
-            val viewModel = createViewModel()
+    @Test
+    fun `given OnNewTransactionClicked, should send NewTransactionClicked event and reset state to initial`() = runTest {
+        // GIVEN
+        val savedState = createMockSavedStateHandle()
+        val viewModel = createViewModel(savedState)
 
-            // WHEN
+        // WHEN
+        viewModel.onUIEvent(WooPosTotalsUIEvent.OnNewTransactionClicked)
 
-            // THEN
-        }
+        // THEN
+        assertEquals(WooPosTotalsState.Loading, viewModel.state.value)
+        verify(childrenToParentEventSender).sendToParent(ChildToParentEvent.NewTransactionClicked)
+    }
 
-    private fun createViewModel() = WooPosTotalsViewModel(
+    @Test
+    fun `given OrderCreationStarted event, should reset state to initial`() = runTest {
+        // GIVEN
+        val savedState = createMockSavedStateHandle()
+        val parentToChildrenEventsFlow = MutableSharedFlow<ParentToChildrenEvent>()
+        whenever(parentToChildrenEventReceiver.events).thenReturn(parentToChildrenEventsFlow)
+
+        val viewModel = createViewModel(savedState)
+
+        // WHEN
+        parentToChildrenEventsFlow.emit(ParentToChildrenEvent.OrderCreation.OrderCreationStarted)
+
+        // THEN
+        assertEquals(WooPosTotalsState.Loading, viewModel.state.value)
+    }
+
+    private fun createViewModel(savedState: SavedStateHandle) = WooPosTotalsViewModel(
         parentToChildrenEventReceiver,
         childrenToParentEventSender,
         cardReaderFacade,
