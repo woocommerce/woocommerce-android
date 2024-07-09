ca31ec57b98fb31a00d5fa6913ffcda034b1e661
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-07-09 13:22:05 +0100

    Update test data to show some product rows

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/preview/AiProductPreviewViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/preview/AiProductPreviewViewModel.kt
@@ -6,25 +6,37 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.ui.products.ai.AIProductModel
+import com.woocommerce.android.ui.products.ai.BuildProductPreviewProperties
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 @HiltViewModel
 class AiProductPreviewViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val buildProductPreviewProperties: BuildProductPreviewProperties
 ) : ScopedViewModel(savedStateHandle) {
-    val state: LiveData<State> = flowOf(
-        State.Success(
-            AIProductModel.buildDefault(
-                name = "Product Name",
-                description = "Product Description"
-            ),
-            emptyList()
+    val state: LiveData<State> = flow {
+        // TODO: Replace with actual AI generation
+        val productModel = AIProductModel.buildDefault(
+            name = "Product Name",
+            description = "Product Description"
         )
-    ).asLiveData()
+
+        val propertyGroups = buildProductPreviewProperties(productModel.toProduct(emptyList(), emptyList()))
+
+        emit(State.Success(productModel, propertyGroups.map { group ->
+            group.map { property ->
+                ProductPropertyCard(
+                    icon = property.icon,
+                    title = property.title,
+                    content = property.content
+                )
+            }
+        }))
+    }.asLiveData()
 
     @Suppress("UNUSED_PARAMETER")
     fun onFeedbackReceived(positive: Boolean) {
