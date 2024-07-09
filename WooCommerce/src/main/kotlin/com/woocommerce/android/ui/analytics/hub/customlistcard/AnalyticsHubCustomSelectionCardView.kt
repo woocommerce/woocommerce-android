95e7471c8bbcd6e074ab82c99832af30f3d4a706
ThomazFB <thomazfcortez@gmail.com>
2024-07-09 15:13:10 -0300

    Create AnalyticsHubCustomSelectionCardView layout XML file

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/customlistcard/AnalyticsHubCustomSelectionCardView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/customlistcard/AnalyticsHubCustomSelectionCardView.kt
@@ -2,11 +2,14 @@ package com.woocommerce.android.ui.analytics.hub.customlistcard
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import com.google.android.material.card.MaterialCardView
+import com.woocommerce.android.databinding.AnalyticsCustomSelectionCardViewBinding
 
 class AnalyticsHubCustomSelectionCardView @JvmOverloads constructor(
     ctx: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : MaterialCardView(ctx, attrs, defStyleAttr) {
+    val binding = AnalyticsCustomSelectionCardViewBinding.inflate(LayoutInflater.from(ctx), this)
 }
