9cd08d64bc557f6a9c58305e5b3eedc7cbd787e5
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-07-09 13:08:04 +0100

    Introduce AIProductModel

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/AIProductModel.kt
@@ -0,0 +1,93 @@
+package com.woocommerce.android.ui.products.ai
+
+import com.woocommerce.android.model.Product
+import com.woocommerce.android.model.ProductCategory
+import com.woocommerce.android.model.ProductTag
+import com.woocommerce.android.ui.products.ProductHelper
+import com.woocommerce.android.ui.products.ProductStatus.DRAFT
+import com.woocommerce.android.ui.products.ProductType.SIMPLE
+import java.math.BigDecimal
+
+data class AIProductModel(
+    val selectedVersion: Int = 0,
+    val names: List<String>,
+    val descriptions: List<String>,
+    val shortDescriptions: List<String>,
+    val isVirtual: Boolean,
+    val price: BigDecimal,
+    val shipping: Shipping,
+    val categories: List<String>? = null,
+    val tags: List<String>? = null
+) {
+    val name: String
+        get() = names[selectedVersion]
+
+    val description: String
+        get() = descriptions[selectedVersion]
+
+    val shortDescription: String
+        get() = shortDescriptions[selectedVersion]
+
+    @Suppress("unused")
+    fun toProduct(
+        existingCategories: List<ProductCategory>,
+        existingTags: List<ProductTag>
+    ): Product = ProductHelper.getDefaultNewProduct(SIMPLE, isVirtual).copy(
+        name = name,
+        description = description,
+        shortDescription = shortDescription,
+        regularPrice = price,
+        categories = getCategories(existingCategories),
+        tags = getTags(existingTags),
+        weight = shipping.weight,
+        height = shipping.height,
+        length = shipping.length,
+        width = shipping.width,
+        status = DRAFT
+    )
+
+    private fun getCategories(
+        existingCategories: List<ProductCategory>
+    ): List<ProductCategory> = categories.orEmpty().map { name ->
+        existingCategories.find { category ->
+            category.name == name
+        } ?: ProductCategory(name = name)
+    }
+
+    private fun getTags(
+        existingTags: List<ProductTag>
+    ): List<ProductTag> = tags.orEmpty().map { name ->
+        existingTags.find { tag ->
+            tag.name == name
+        } ?: ProductTag(name = name)
+    }
+
+    data class Shipping(
+        val weight: Float,
+        val height: Float,
+        val length: Float,
+        val width: Float
+    )
+
+    companion object {
+        @Suppress("UNUSED_PARAMETER")
+        fun fromJson(json: String): AIProductModel {
+            TODO()
+        }
+
+        fun buildDefault(
+            name: String,
+            description: String,
+        ): AIProductModel {
+            return AIProductModel(
+                names = listOf(name),
+                descriptions = listOf(description),
+                shortDescriptions = listOf(""),
+                isVirtual = false,
+                price = BigDecimal.ZERO,
+                shipping = Shipping(0f, 0f, 0f, 0f)
+            )
+        }
+    }
+}
+
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/preview/AiProductPreviewViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/preview/AiProductPreviewViewModel.kt
@@ -1,13 +1,59 @@
 package com.woocommerce.android.ui.products.ai.preview
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import com.woocommerce.android.ui.products.ai.AIProductModel
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 @HiltViewModel
 class AiProductPreviewViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ScopedViewModel(savedStateHandle) {
+    val state: LiveData<State> = flowOf(
+        State.Success(
+            AIProductModel.buildDefault(
+                name = "Product Name",
+                description = "Product Description"
+            ),
+            emptyList()
+        )
+    ).asLiveData()
 
+    @Suppress("UNUSED_PARAMETER")
+    fun onFeedbackReceived(positive: Boolean) {
+        TODO()
+    }
+
+    sealed interface State {
+        data object Loading : State
+        data class Success(
+            private val product: AIProductModel,
+            val propertyGroups: List<List<ProductPropertyCard>>,
+            val shouldShowFeedbackView: Boolean = true
+        ) : State {
+            val title: String
+                get() = product.name
+            val description: String
+                get() = product.description
+            val shortDescription: String
+                get() = product.shortDescription
+        }
+
+        data class Error(
+            val onRetryClick: () -> Unit,
+            val onDismissClick: () -> Unit
+        ) : State
+    }
+
+    data class ProductPropertyCard(
+        @DrawableRes val icon: Int,
+        @StringRes val title: Int,
+        val content: String
+    )
 }
