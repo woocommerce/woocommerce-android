99b1c0d98590b9831e5081234d6d76773e1535ee
Hafiz Rahman <hfzrhmn@gmail.com>
2024-07-10 19:22:03 +0700

    Add logic to choose which webview to use based on Jetpack connection

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuFragment.kt
@@ -30,7 +30,6 @@ import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.Start
 import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.ViewAdminEvent
 import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.ViewCouponsEvent
 import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.ViewCustomersEvent
-import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.ViewGoogleEvent
 import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.ViewGoogleForWooEvent
 import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.ViewInboxEvent
 import com.woocommerce.android.ui.moremenu.MoreMenuViewModel.MoreMenuEvent.ViewPayments
@@ -100,9 +99,8 @@ class MoreMenuFragment : TopLevelFragment() {
                 is NavigateToSettingsEvent -> navigateToSettings()
                 is NavigateToSubscriptionsEvent -> navigateToSubscriptions()
                 is StartSitePickerEvent -> startSitePicker()
-                is ViewGoogleForWooEvent -> openInExitAwareWebview(event.url)
+                is ViewGoogleForWooEvent -> openGoogleForWooWebview(event.url, event.canAutoLogin)
                 is ViewAdminEvent -> openInBrowser(event.url)
-                is ViewGoogleEvent -> openInAuthBrowser(event.url)
                 is ViewStoreEvent -> openInBrowser(event.url)
                 is ViewReviewsEvent -> navigateToReviews()
                 is ViewInboxEvent -> navigateToInbox()
@@ -158,18 +156,6 @@ class MoreMenuFragment : TopLevelFragment() {
         ChromeCustomTabUtils.launchUrl(requireContext(), url)
     }
 
-    private fun openInExitAwareWebview(url: String) {
-        // todo-11917: The following is a test case where the webview exits automatically when the
-        //  GLA Report tab is opened. It needs to be replaced with the right success URL.
-        findNavController().navigateSafely(
-            NavGraphMainDirections.actionGlobalExitAwareWebViewFragment(
-                urlToLoad = url,
-                urlsToTriggerExit = arrayOf("wp-admin/admin.php?page=wc-admin&path=%2Fgoogle%2Freports"),
-                urlComparisonMode = ExitAwareWebViewViewModel.UrlComparisonMode.PARTIAL
-            )
-        )
-    }
-
     private fun navigateToReviews() {
         findNavController().navigateSafely(
             MoreMenuFragmentDirections.actionMoreMenuToReviewList()
@@ -194,13 +180,32 @@ class MoreMenuFragment : TopLevelFragment() {
         )
     }
 
+    private fun openGoogleForWooWebview(url: String, canAutoLogin: Boolean) {
+        if (canAutoLogin) {
+            openInAuthBrowser(url)
+        } else {
+            openInExitAwareWebview(url)
+        }
+    }
+
+    private fun openInExitAwareWebview(url: String) {
+        findNavController().navigateSafely(
+            NavGraphMainDirections.actionGlobalExitAwareWebViewFragment(
+                urlToLoad = url,
+                urlsToTriggerExit = arrayOf(), // todo-11917: Replace with the right success URL
+                title = getString(R.string.more_menu_button_google),
+                urlComparisonMode = ExitAwareWebViewViewModel.UrlComparisonMode.PARTIAL
+            )
+        )
+    }
+
     private fun openInAuthBrowser(url: String) {
         findNavController().navigateSafely(
             NavGraphMainDirections.actionGlobalWPComWebViewFragment(
                 urlToLoad = url,
-                urlsToTriggerExit = arrayOf(),
+                urlsToTriggerExit = arrayOf(), // todo-11917: Replace with the right success URL
                 title = getString(R.string.more_menu_button_google),
-                clearCache = true
+                urlComparisonMode = ExitAwareWebViewViewModel.UrlComparisonMode.PARTIAL
             )
         )
     }
