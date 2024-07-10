f35b4658be904867c0fbdea4fee46ee4890a62e4
Andrey <andrey.kidinov@a8c.com>
2024-07-10 16:37:26 +0200

    Update WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/WooPosisFeatureFlagEnabled.kt
    Co-authored-by: Samuel Urbanowicz <samiuelson@gmail.com>

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/WooPosisFeatureFlagEnabled.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/WooPosisFeatureFlagEnabled.kt
@@ -3,7 +3,7 @@ package com.woocommerce.android.ui.woopos
 import com.woocommerce.android.util.FeatureFlag
 import javax.inject.Inject
 
-class WooPosisFeatureFlagEnabled @Inject constructor() {
+class WooPosIsFeatureFlagEnabled @Inject constructor() {
     operator fun invoke(): Boolean {
         return FeatureFlag.WOO_POS.isEnabled()
     }
