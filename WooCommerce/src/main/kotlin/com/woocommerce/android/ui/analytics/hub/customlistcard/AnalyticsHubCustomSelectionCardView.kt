52dbc025922a703960540197a6bcd950219ad638
ThomazFB <thomazfcortez@gmail.com>
2024-07-11 22:05:01 -0300

    Add SkeletonView handling to the AnalyticsHubCustomSelectionCardView

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/customlistcard/AnalyticsHubCustomSelectionCardView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/customlistcard/AnalyticsHubCustomSelectionCardView.kt
@@ -3,12 +3,15 @@ package com.woocommerce.android.ui.analytics.hub.customlistcard
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import com.google.android.material.card.MaterialCardView
+import com.woocommerce.android.R
 import com.woocommerce.android.databinding.AnalyticsCustomSelectionCardViewBinding
 import com.woocommerce.android.ui.analytics.hub.AnalyticsHubCustomSelectionListViewState
 import com.woocommerce.android.ui.analytics.hub.AnalyticsHubCustomSelectionListViewState.DataViewState
 import com.woocommerce.android.ui.analytics.hub.AnalyticsHubCustomSelectionListViewState.LoadingAdsViewState
 import com.woocommerce.android.ui.analytics.hub.AnalyticsHubCustomSelectionListViewState.NoAdsState
+import com.woocommerce.android.widgets.SkeletonView
 
 class AnalyticsHubCustomSelectionCardView @JvmOverloads constructor(
     ctx: Context,
@@ -16,6 +19,7 @@ class AnalyticsHubCustomSelectionCardView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : MaterialCardView(ctx, attrs, defStyleAttr) {
     val binding = AnalyticsCustomSelectionCardViewBinding.inflate(LayoutInflater.from(ctx), this)
+    private var skeletonView = SkeletonView()
 
     fun updateInformation(viewState: AnalyticsHubCustomSelectionListViewState) {
         when (viewState) {
@@ -26,14 +30,25 @@ class AnalyticsHubCustomSelectionCardView @JvmOverloads constructor(
     }
 
     private fun setDataViewState(viewState: DataViewState) {
-        TODO("Not yet implemented")
+        viewState.apply {  }
     }
 
     private fun setNoAdsViewState(viewState: NoAdsState) {
-        TODO("Not yet implemented")
+        viewState.apply {  }
     }
 
     private fun setSkeleton() {
-        TODO("Not yet implemented")
+        skeletonView.show(
+            binding.analyticsCardListContainer,
+            R.layout.skeleton_analytics_list_card,
+            delayed = true
+        )
+        binding.analyticsCardTitle.visibility = GONE
+        binding.analyticsItemsTitle.visibility = GONE
+        binding.analyticsItemsValue.visibility = GONE
+        binding.analyticsListLeftHeader.visibility = GONE
+        binding.analyticsListRightHeader.visibility = GONE
+        binding.analyticsItemsTag.visibility = View.GONE
+        binding.noDataText.visibility = GONE
     }
 }
