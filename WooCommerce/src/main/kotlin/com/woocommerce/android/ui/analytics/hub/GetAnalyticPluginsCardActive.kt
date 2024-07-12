b2387af5891c4d94db01b444f58bd240a7c446ee
ThomazFB <thomazfcortez@gmail.com>
2024-07-11 21:35:14 -0300

    Adjust Google Ads card to be conditionally set by its respective Feature Flag

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/GetAnalyticPluginsCardActive.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/GetAnalyticPluginsCardActive.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.analytics.hub
 
 import com.woocommerce.android.model.AnalyticsCards
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.util.FeatureFlag
 import org.wordpress.android.fluxc.store.WooCommerceStore
 import javax.inject.Inject
 
@@ -9,12 +10,15 @@ class GetAnalyticPluginsCardActive @Inject constructor(
     private val selectedSite: SelectedSite,
     private val wooCommerceStore: WooCommerceStore
 ) {
-    private val analyticPlugins = listOf(
+    private val analyticPlugins = mutableListOf(
         WooCommerceStore.WooPlugin.WOO_PRODUCT_BUNDLES,
         WooCommerceStore.WooPlugin.WOO_GIFT_CARDS,
-        WooCommerceStore.WooPlugin.JETPACK,
-        WooCommerceStore.WooPlugin.GOOGLE_ADS
-    )
+        WooCommerceStore.WooPlugin.JETPACK
+    ).apply {
+        if (FeatureFlag.GOOGLE_ADS_ANALYTICS_HUB_M1.isEnabled()) {
+            add(WooCommerceStore.WooPlugin.GOOGLE_ADS)
+        }
+    }.toList()
 
     suspend operator fun invoke(): Set<AnalyticsCards> {
         val selectedSite = selectedSite.getOrNull() ?: return emptySet()
