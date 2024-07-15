3a8f3ea4037cc12bfe70c1a1e584f8594c43813d
Hafiz Rahman <hfzrhmn@gmail.com>
2024-07-15 20:54:44 +0700

    Update logic as the card does not need to display latest campaign.
    To be replaced with impressions and clicks information instead.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/google/DashboardGoogleAdsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/google/DashboardGoogleAdsViewModel.kt
@@ -3,30 +3,32 @@ package com.woocommerce.android.ui.dashboard.google
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.R
+import com.woocommerce.android.model.DashboardWidget
 import com.woocommerce.android.ui.dashboard.DashboardViewModel
 import com.woocommerce.android.ui.dashboard.DashboardViewModel.DashboardWidgetAction
 import com.woocommerce.android.ui.dashboard.DashboardViewModel.DashboardWidgetMenu
 import com.woocommerce.android.ui.dashboard.DashboardViewModel.RefreshEvent
-import com.woocommerce.android.ui.google.ObserveMostRecentGoogleAdsCampaign
+import com.woocommerce.android.ui.dashboard.defaultHideMenuEntry
+import com.woocommerce.android.ui.google.HasGoogleAdsCampaigns
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.transformLatest
+import kotlinx.coroutines.withContext
 
 @HiltViewModel(assistedFactory = DashboardGoogleAdsViewModel.Factory::class)
 class DashboardGoogleAdsViewModel @AssistedInject constructor(
     savedStateHandle: SavedStateHandle,
     @Assisted private val parentViewModel: DashboardViewModel,
-    private val observeMostRecentGoogleAdsCampaign: ObserveMostRecentGoogleAdsCampaign
+    private val hasGoogleAdsCampaigns: HasGoogleAdsCampaigns
 ) : ScopedViewModel(savedStateHandle) {
     private val _refreshTrigger = MutableSharedFlow<RefreshEvent>(extraBufferCapacity = 1)
     private val refreshTrigger = merge(_refreshTrigger, (parentViewModel.refreshTrigger))
@@ -37,39 +39,36 @@ class DashboardGoogleAdsViewModel @AssistedInject constructor(
         .transformLatest {
             emit(DashboardGoogleAdsState.Loading)
 
-            emitAll(
-                observeMostRecentGoogleAdsCampaign()
-                    .map { result ->
-                        result.fold(
-                            onSuccess = { campaign ->
-                                if (campaign == null) {
-                                    DashboardGoogleAdsState.NoCampaigns(
-                                        onCreateCampaignClicked = { },
-                                        menu = DashboardWidgetMenu(emptyList())
-                                    )
-                                } else {
-                                    DashboardGoogleAdsState.HasCampaigns(
-                                        onCreateCampaignClicked = { },
-                                        showAllCampaignsButton = DashboardWidgetAction(
-                                            R.string.dashboard_google_ads_card_view_all_campaigns_button
-                                        ) { },
-                                        menu = DashboardWidgetMenu(emptyList())
-                                    )
-                                }
-                            },
-                            onFailure = {
-                                // When unable to fetch campaigns, instead of showing error,
-                                // assume that there are no campaigns so that merchants can still create campaign.
-                                DashboardGoogleAdsState.NoCampaigns(
-                                    onCreateCampaignClicked = { },
-                                    menu = DashboardWidgetMenu(emptyList())
-                                )
-                            }
-                        )
-                    }
+            val hasCampaigns = withContext(Dispatchers.IO) {
+                hasGoogleAdsCampaigns()
+            }
+
+            emit(
+                if (hasCampaigns) {
+                    DashboardGoogleAdsState.HasCampaigns(
+                        onCreateCampaignClicked = { },
+                        showAllCampaignsButton = DashboardWidgetAction(
+                            R.string.dashboard_google_ads_card_view_all_campaigns_button
+                        ) { },
+                        menu = widgetMenu
+                    )
+                } else {
+                    DashboardGoogleAdsState.NoCampaigns(
+                        onCreateCampaignClicked = { },
+                        menu = widgetMenu
+                    )
+                }
             )
         }
         .asLiveData()
+
+    private val widgetMenu = DashboardWidgetMenu(
+        items = listOf(
+            DashboardWidget.Type.GOOGLE_ADS.defaultHideMenuEntry {
+                // todo
+            }
+        )
+    )
 
     sealed class DashboardGoogleAdsState(
         open val menu: DashboardWidgetMenu,
