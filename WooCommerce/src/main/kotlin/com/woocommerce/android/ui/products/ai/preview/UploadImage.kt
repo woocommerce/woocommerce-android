e249380ff23d1e8ee114a38f714e1e3322cba6cf
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-07-16 16:04:38 +0200

    Return Product.Image after uploading an image successfully

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/preview/UploadImage.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/preview/UploadImage.kt
@@ -2,17 +2,17 @@ package com.woocommerce.android.ui.products.ai.preview
 
 import com.woocommerce.android.media.MediaFilesRepository
 import com.woocommerce.android.model.Image
+import com.woocommerce.android.model.Product
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.retry
 import kotlinx.coroutines.flow.transform
-import org.wordpress.android.fluxc.model.MediaModel
 import javax.inject.Inject
 
 class UploadImage @Inject constructor(
     private val mediaFilesRepository: MediaFilesRepository
 ) {
-    suspend operator fun invoke(selectedImage: Image): Result<MediaModel> =
+    suspend operator fun invoke(selectedImage: Image): Result<Product.Image> =
         when (selectedImage) {
             is Image.LocalImage -> mediaFilesRepository.uploadFile(selectedImage.uri)
                 .transform {
@@ -27,7 +27,15 @@ class UploadImage @Inject constructor(
                 .retry(1)
                 .catch { emit(Result.failure(it)) }
                 .first()
+                .map {
+                    Product.Image(
+                        id = it.mediaId,
+                        source = it.url,
+                        name = it.fileName,
+                        dateCreated = null,
+                    )
+                }
 
-            is Image.WPMediaLibraryImage -> mediaFilesRepository.fetchWordPressMedia(selectedImage.content.id)
+            is Image.WPMediaLibraryImage -> Result.success(selectedImage.content)
         }
 }
