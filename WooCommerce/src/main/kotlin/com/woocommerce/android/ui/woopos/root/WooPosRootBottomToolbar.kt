9f15bcbed2a1eacef195fc74d458a2eb3744979a
Andrey <akidinov@gmail.com>
2024-07-17 11:28:03 +0200

    Menu card button with use of the actual button for ripple effect

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/root/WooPosRootBottomToolbar.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/root/WooPosRootBottomToolbar.kt
@@ -7,9 +7,11 @@ import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -17,7 +19,6 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -30,10 +31,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosTheme
@@ -46,48 +48,85 @@ fun WooPosBottomToolbar(
     state: State<WooPosRootScreenState>,
     onUIEvent: (WooPosRootUIEvent) -> Unit,
 ) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.padding(8.dp.toAdaptivePadding())
-    ) {
-        MenuCard { onUIEvent(WooPosRootUIEvent.ExitPOSClicked) }
-        Spacer(modifier = Modifier.width(8.dp.toAdaptivePadding()))
-        CardReaderStatusCard(state) { onUIEvent(WooPosRootUIEvent.ConnectToAReaderClicked) }
+    ConstraintLayout(modifier = modifier) {
+        val (menuCard, cardReaderStatusCard) = createRefs()
+        val marginBetweenCards = 8.dp.toAdaptivePadding()
+
+        CardReaderStatusCard(
+            modifier = Modifier
+                .constrainAs(cardReaderStatusCard) {
+                    start.linkTo(menuCard.end, margin = marginBetweenCards)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                },
+            state = state,
+        ) { onUIEvent(WooPosRootUIEvent.ConnectToAReaderClicked) }
+
+        MenuCard(
+            modifier = Modifier
+                .constrainAs(menuCard) {
+                    start.linkTo(parent.start)
+                    top.linkTo(cardReaderStatusCard.top)
+                    bottom.linkTo(cardReaderStatusCard.bottom)
+                    height = Dimension.fillToConstraints
+                }
+        ) { onUIEvent(WooPosRootUIEvent.ExitPOSClicked) }
     }
 }
 
 @Composable
-private fun MenuCard(onClick: () -> Unit) {
+private fun MenuCard(
+    modifier: Modifier,
+    onClick: () -> Unit
+) {
     Card(
-        modifier = Modifier
-            .wrapContentSize(Alignment.Center),
+        modifier = modifier,
         backgroundColor = MaterialTheme.colors.surface,
         elevation = 8.dp,
         shape = RoundedCornerShape(8.dp),
     ) {
-        TextButton(onClick = onClick) {
-            Spacer(modifier = Modifier.width(16.dp.toAdaptivePadding()))
-            Icon(
-                imageVector = ImageVector.vectorResource(id = R.drawable.woopos_ic_exit_pos),
-                contentDescription = null,
-                tint = MaterialTheme.colors.secondaryVariant,
-                modifier = Modifier.size(14.dp)
-            )
-            Spacer(modifier = Modifier.width(4.dp.toAdaptivePadding()))
-            Text(
-                modifier = Modifier.padding(8.dp.toAdaptivePadding()),
-                text = stringResource(id = R.string.woopos_exit_pos),
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.button
-            )
-            Spacer(modifier = Modifier.width(16.dp.toAdaptivePadding()))
+        TextButton(
+            onClick = onClick,
+            contentPadding = PaddingValues(0.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                repeat(3) {
+                    Circle(size = 4.dp, color = MaterialTheme.colors.primary)
+                    if (it < 2) {
+                        Spacer(modifier = Modifier.height(4.dp.toAdaptivePadding()))
+                    }
+                }
+            }
         }
     }
 }
 
+//@Composable
+//private fun PopUpMenuButton(onClick: () -> Unit) {
+//    Spacer(modifier = Modifier.width(20.dp.toAdaptivePadding()))
+//    Icon(
+//        imageVector = ImageVector.vectorResource(id = R.drawable.woopos_ic_exit_pos),
+//        contentDescription = null,
+//        tint = MaterialTheme.colors.secondaryVariant,
+//        modifier = Modifier.size(14.dp)
+//    )
+//    Spacer(modifier = Modifier.width(4.dp.toAdaptivePadding()))
+//    Text(
+//        modifier = Modifier.padding(8.dp.toAdaptivePadding()),
+//        text = stringResource(id = R.string.woopos_exit_pos),
+//        color = MaterialTheme.colors.secondaryVariant,
+//        style = MaterialTheme.typography.button
+//    )
+//    Spacer(modifier = Modifier.width(20.dp.toAdaptivePadding()))
+//}
+
 @Composable
 private fun CardReaderStatusCard(
+    modifier: Modifier,
     state: State<WooPosRootScreenState>,
     onClick: () -> Unit
 ) {
@@ -127,7 +166,7 @@ private fun CardReaderStatusCard(
     )
 
     Card(
-        modifier = Modifier
+        modifier = modifier
             .wrapContentSize(Alignment.Center),
         backgroundColor = MaterialTheme.colors.surface,
         elevation = 8.dp,
@@ -135,7 +174,7 @@ private fun CardReaderStatusCard(
     ) {
         TextButton(onClick = onClick) {
             Spacer(modifier = Modifier.width(16.dp.toAdaptivePadding()))
-            ReaderStatusIllustration(illustrationColor)
+            Circle(size = 12.dp, color = illustrationColor)
             Spacer(modifier = Modifier.width(4.dp.toAdaptivePadding()))
             ReaderStatusText(
                 modifier = Modifier.animateContentSize(),
@@ -162,10 +201,13 @@ private fun ReaderStatusText(
 }
 
 @Composable
-private fun ReaderStatusIllustration(color: Color) {
+private fun Circle(
+    size: Dp,
+    color: Color
+) {
     Box(
         modifier = Modifier
-            .size(12.dp)
+            .size(size)
             .background(color = color, shape = CircleShape)
     )
 }
@@ -177,6 +219,7 @@ fun PreviewWooPosBottomToolbarStatusNotConnected() {
         mutableStateOf(
             WooPosRootScreenState(
                 WooPosCardReaderStatus.NotConnected,
+                menu = WooPosRootScreenState.Menu.Hidden,
                 null
             )
         )
@@ -191,6 +234,20 @@ fun PreviewWooPosBottomToolbarStatusConnected() {
         mutableStateOf(
             WooPosRootScreenState(
                 WooPosCardReaderStatus.Connected,
+                menu = WooPosRootScreenState.Menu.Visible(
+                    listOf(
+                        WooPosRootScreenState.Menu.MenuItem(
+                            id = 0,
+                            title = R.string.woopos_exit_confirmation_title,
+                            icon = R.drawable.woopos_ic_exit_pos,
+                        ),
+                        WooPosRootScreenState.Menu.MenuItem(
+                            id = 1,
+                            title = R.string.woopos_exit_confirmation_title,
+                            icon = R.drawable.woopos_ic_exit_pos,
+                        )
+                    )
+                ),
                 null
             )
         )
