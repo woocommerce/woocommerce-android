dff85caf2de79cd00c904fc69b4d3f4822f71f2a
Andrey <akidinov@gmail.com>
2024-07-18 15:58:02 +0200

    Simulate delay for testing. Dummy loading state

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashScreen.kt
@@ -1,10 +1,31 @@
 package com.woocommerce.android.ui.woopos.splash
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.woocommerce.android.ui.woopos.root.navigation.WooPosNavigationEvent
 
 @Composable
 fun WooPosSplashScreen(onNavigationEvent: (WooPosNavigationEvent) -> Unit) {
     val viewModel: WooPosHomeViewModel = hiltViewModel()
+    val state = viewModel.state.collectAsState()
+    when (state.value) {
+        is WooPosSplashState.Loading -> Loading()
+        is WooPosSplashState.Loaded -> onNavigationEvent(WooPosNavigationEvent.OpenHomeFromSplash)
+    }
+}
+
+@Composable
+private fun Loading() {
+    Text(
+        text = "Loading...",
+        style = MaterialTheme.typography.h1,
+        modifier = Modifier.fillMaxSize(),
+        textAlign = TextAlign.Center
+    )
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashViewModel.kt
@@ -3,13 +3,21 @@ package com.woocommerce.android.ui.woopos.splash
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WooPosHomeViewModel @Inject constructor() : ViewModel() {
+    private val _state = MutableStateFlow<WooPosSplashState>(WooPosSplashState.Loading)
+    val state: StateFlow<WooPosSplashState> = _state
+
     init {
         viewModelScope.launch {
+            delay(2000)
+            _state.value = WooPosSplashState.Loaded
         }
     }
 }
