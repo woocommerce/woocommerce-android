b420894fabb94c76ca44c8adcf0075e548bd3ca8
AnirudhBhat <abhat38@gmail.com>
2024-07-19 10:51:35 +0530

    Add Banner closed event

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsUIEvent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsUIEvent.kt
@@ -4,4 +4,5 @@ sealed class WooPosProductsUIEvent {
     data class ItemClicked(val item: WooPosProductsListItem) : WooPosProductsUIEvent()
     data object EndOfProductListReached : WooPosProductsUIEvent()
     data object PullToRefreshTriggered : WooPosProductsUIEvent()
+    data object SimpleProductsOnlyBannerClosed : WooPosProductsUIEvent()
 }
