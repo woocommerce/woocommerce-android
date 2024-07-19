daae3b49b83a9b85dc5adaa9293997dba9bf8f0f
ThomazFB <thomazfcortez@gmail.com>
2024-07-18 22:40:21 -0300

    Add report card support to the GoogleAds feature

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/AnalyticsHubViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/analytics/hub/AnalyticsHubViewModel.kt
@@ -693,7 +693,10 @@ class AnalyticsHubViewModel @Inject constructor(
             itemTitleValue = googleAdsStats.totals.formatSales(currencyFormatter).orEmpty(),
             delta = googleAdsStats.deltaPercentage
                 .run { this as? DeltaPercentage.Value }?.value,
-            reportUrl = null,
+            reportUrl = getReportUrl(
+                selection = ranges,
+                card = ReportCard.GoogleAds
+            ),
             items = googleAdsStats.googleAdsCampaigns.map {
                 AnalyticsHubListCardItemViewState(
                     title = it.name.orEmpty(),
@@ -773,7 +776,7 @@ class AnalyticsHubViewModel @Inject constructor(
     }
 }
 
-enum class ReportCard { Revenue, Orders, Products, Bundles, GiftCard }
+enum class ReportCard { Revenue, Orders, Products, Bundles, GiftCard, GoogleAds }
 
 fun AnalyticsCards.toReportCard(): ReportCard? {
     return when (this) {
@@ -782,6 +785,7 @@ fun AnalyticsCards.toReportCard(): ReportCard? {
         AnalyticsCards.Products -> ReportCard.Products
         AnalyticsCards.Bundles -> ReportCard.Bundles
         AnalyticsCards.GiftCards -> ReportCard.GiftCard
+        AnalyticsCards.GoogleAds -> ReportCard.GoogleAds
         else -> null
     }
 }
