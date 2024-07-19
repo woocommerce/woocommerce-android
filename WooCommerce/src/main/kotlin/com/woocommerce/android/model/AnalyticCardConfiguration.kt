d6fbd91ffa727d7e58ef8ab0162537d6e047c1bf
ThomazFB <thomazfcortez@gmail.com>
2024-07-19 02:03:47 -0300

    Rename ReportCard.GoogleAds to ReportCard.GoogleCampaigns

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/AnalyticCardConfiguration.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/AnalyticCardConfiguration.kt
@@ -8,12 +8,26 @@ data class AnalyticCardConfiguration(
     val isVisible: Boolean = true,
 )
 
-enum class AnalyticsCards(val resId: Int, val isPlugin: Boolean = false) {
+enum class AnalyticsCards(
+    val resId: Int,
+    val isPlugin: Boolean = false
+) {
     Revenue(R.string.analytics_revenue_card_title),
     Orders(R.string.analytics_orders_card_title),
     Products(R.string.analytics_products_card_title),
     Session(R.string.analytics_session_card_title, isPlugin = true),
     Bundles(R.string.analytics_bundles_card_title, isPlugin = true),
     GiftCards(R.string.analytics_gift_cards_card_title, isPlugin = true),
-    GoogleAds(R.string.analytics_google_ads_card_title, isPlugin = true),
+    GoogleAds(R.string.analytics_google_ads_card_title, isPlugin = true);
+
+    val trackName: String
+        get() = when (this) {
+            Revenue -> "revenue"
+            Orders -> "orders"
+            Products -> "products"
+            Session -> "session"
+            Bundles -> "bundles"
+            GiftCards -> "giftCards"
+            GoogleAds -> "googleCampaigns"
+        }
 }
