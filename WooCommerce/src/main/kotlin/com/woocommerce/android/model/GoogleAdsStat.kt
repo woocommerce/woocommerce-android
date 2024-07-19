f6afb94e6aac08adfe05b6c88483af7aea2cc939
ThomazFB <thomazfcortez@gmail.com>
2024-07-18 21:51:59 -0300

    Add format support to GoogleAdsTotals

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/GoogleAdsStat.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/GoogleAdsStat.kt
@@ -1,5 +1,7 @@
 package com.woocommerce.android.model
 
+import com.woocommerce.android.util.CurrencyFormatter
+
 data class GoogleAdsStat(
     val googleAdsCampaigns: List<GoogleAdsCampaign>,
     val totals: GoogleAdsTotals = GoogleAdsTotals(
@@ -27,4 +29,14 @@ data class GoogleAdsCampaign(
 data class GoogleAdsTotals(
     val sales: Double?,
     val spend: Double?
-)
+) {
+    fun formatSales(currencyFormatter: CurrencyFormatter) =
+        sales?.let {
+            currencyFormatter.formatCurrency(it.toString())
+        }.orEmpty()
+
+    fun formatSpend(currencyFormatter: CurrencyFormatter) =
+        spend?.let {
+            currencyFormatter.formatCurrency(it.toString())
+        }.orEmpty()
+}
