a1e567a013b6f11f19cb9f99a90add73a4d76a10
Andrey <akidinov@gmail.com>
2024-07-22 11:07:29 +0200

    Some missing tests to WooPosHomeViewModelTest

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeViewModelTest.kt
@@ -5,12 +5,12 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import kotlin.test.Test
-import org.assertj.core.api.Assertions.assertThat
 
 @ExperimentalCoroutinesApi
 class WooPosHomeViewModelTest {
@@ -68,6 +68,52 @@ class WooPosHomeViewModelTest {
         verify(parentToChildrenEventSender).sendToChildren(ParentToChildrenEvent.OrderSuccessfullyPaid)
         assertThat(viewModel.state.value.screenPositionState)
             .isEqualTo(WooPosHomeState.ScreenPositionState.Cart.Empty)
+    }
+
+    @Test
+    fun `given state is Checkout NotPaid, when ExitConfirmationDialogDismissed passed, then exit confirmation dialog should be dismissed`() = runTest {
+        // GIVEN
+        whenever(childrenToParentEventReceiver.events).thenReturn(
+            flowOf(ChildToParentEvent.CheckoutClicked(emptyList()))
+        )
+        val viewModel = createViewModel()
+
+        // WHEN
+        viewModel.onUIEvent(WooPosHomeUIEvent.ExitConfirmationDialogDismissed)
+
+        // THEN
+        assertThat(viewModel.state.value.exitConfirmationDialog).isNull()
+    }
+
+    @Test
+    fun `given state is Cart Empty, when ExitPosClicked passed, then exit confirmation dialog should be shown`() = runTest {
+        // GIVEN
+        val eventsFlow = MutableSharedFlow<ChildToParentEvent>()
+        whenever(childrenToParentEventReceiver.events).thenReturn(eventsFlow)
+        val viewModel = createViewModel()
+
+        // WHEN
+        eventsFlow.emit(ChildToParentEvent.ExitPosClicked)
+
+        // THEN
+        assertThat(viewModel.state.value.exitConfirmationDialog)
+            .isEqualTo(WooPosExitConfirmationDialog)
+    }
+
+    @Test
+    fun `given state is Cart NotEmpty, when ExitPosClicked passed, then exit confirmation dialog should be shown`() = runTest {
+        // GIVEN
+        val eventsFlow = MutableSharedFlow<ChildToParentEvent>()
+        whenever(childrenToParentEventReceiver.events).thenReturn(eventsFlow)
+        val viewModel = createViewModel()
+
+        // WHEN
+        eventsFlow.emit(ChildToParentEvent.CartStatusChanged.NotEmpty)
+        eventsFlow.emit(ChildToParentEvent.ExitPosClicked)
+
+        // THEN
+        assertThat(viewModel.state.value.exitConfirmationDialog)
+            .isEqualTo(WooPosExitConfirmationDialog)
     }
 
     private fun createViewModel() = WooPosHomeViewModel(
