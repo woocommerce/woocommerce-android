05842c1fbc09fb783d105b9e148b716e9c7c4ace
Hafiz Rahman <hfzrhmn@gmail.com>
2024-07-23 19:11:29 +0700

    Update GoogleAdsWebView on include various callbacks that will be needed for tracking later.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/google/webview/GoogleAdsWebViewScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/google/webview/GoogleAdsWebViewScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.common.wpcomwebview.WPComWebViewAuthenticator
 import com.woocommerce.android.ui.compose.component.Toolbar
 import com.woocommerce.android.ui.compose.component.WCWebView
 import com.woocommerce.android.ui.google.webview.GoogleAdsWebViewViewModel.DisplayMode.MODAL
@@ -19,10 +20,17 @@ import org.wordpress.android.fluxc.network.UserAgent
 
 @Composable
 fun GoogleAdsWebViewScreen(viewViewModel: GoogleAdsWebViewViewModel) {
+    val authenticator = viewViewModel.wpComWebViewAuthenticator.takeIf {
+        viewViewModel.viewState.canUseAutoLoginWebview
+    }
+
     GoogleAdsWebViewScreen(
         viewState = viewViewModel.viewState,
+        wpcomWebViewAuthenticator = authenticator,
         userAgent = viewViewModel.userAgent,
         onUrlLoaded = viewViewModel::onUrlLoaded,
+        onPageFinished = viewViewModel::onPageFinished,
+        onUrlFailed = viewViewModel::onUrlFailed,
         onClose = viewViewModel::onClose
     )
 }
@@ -30,8 +38,11 @@ fun GoogleAdsWebViewScreen(viewViewModel: GoogleAdsWebViewViewModel) {
 @Composable
 fun GoogleAdsWebViewScreen(
     viewState: GoogleAdsWebViewViewModel.ViewState,
+    wpcomWebViewAuthenticator: WPComWebViewAuthenticator?,
     userAgent: UserAgent,
     onUrlLoaded: (String) -> Unit,
+    onPageFinished: (String) -> Unit,
+    onUrlFailed: (String, Int?) -> Unit,
     onClose: () -> Unit,
     clearCache: Boolean = false
 ) {
@@ -51,7 +62,10 @@ fun GoogleAdsWebViewScreen(
         WCWebView(
             url = viewState.urlToLoad,
             userAgent = userAgent,
+            wpComAuthenticator = wpcomWebViewAuthenticator,
             onUrlLoaded = onUrlLoaded,
+            onPageFinished = onPageFinished,
+            onUrlFailed = onUrlFailed,
             captureBackPresses = viewState.captureBackButton,
             clearCache = clearCache,
             modifier = Modifier
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/google/webview/GoogleAdsWebViewViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/google/webview/GoogleAdsWebViewViewModel.kt
@@ -1,9 +1,12 @@
 package com.woocommerce.android.ui.google.webview
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.ui.common.wpcomwebview.WPComWebViewAuthenticator
+import com.woocommerce.android.ui.google.CanUseAutoLoginWebview
 import com.woocommerce.android.ui.google.webview.GoogleAdsWebViewViewModel.UrlComparisonMode.EQUALITY
 import com.woocommerce.android.ui.google.webview.GoogleAdsWebViewViewModel.UrlComparisonMode.PARTIAL
 import com.woocommerce.android.ui.google.webview.GoogleAdsWebViewViewModel.UrlComparisonMode.STARTS_WITH
+import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ExitWithResult
 import com.woocommerce.android.viewmodel.ScopedViewModel
@@ -15,10 +18,20 @@ import javax.inject.Inject
 @HiltViewModel
 class GoogleAdsWebViewViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    val wpComWebViewAuthenticator: WPComWebViewAuthenticator,
+    val canUseAutoLoginWebview: CanUseAutoLoginWebview,
     val userAgent: UserAgent
 ) : ScopedViewModel(savedStateHandle) {
     private val navArgs: GoogleAdsWebViewFragmentArgs by savedStateHandle.navArgs()
     private var isExiting = false
+
+    // `WebView`'s onPageFinished callback is called multiple times when loading the same URL once,
+    // so this flag is needed for callback to be called exactly one time.
+    private var isUrlToLoadFinishedOnce = false
+
+    // `WebView`s onPageFinished callback is still called even if the url failed to load.
+    // This flag is used to ensure the callback can return early.
+    private var isUrlLoadingFailed = false
 
     val viewState = navArgs.let {
         ViewState(
@@ -26,7 +39,8 @@ class GoogleAdsWebViewViewModel @Inject constructor(
             title = it.title,
             displayMode = it.displayMode,
             captureBackButton = it.captureBackButton,
-            clearCache = it.clearCache
+            clearCache = it.clearCache,
+            canUseAutoLoginWebview = canUseAutoLoginWebview()
         )
     }
 
@@ -43,8 +57,24 @@ class GoogleAdsWebViewViewModel @Inject constructor(
         }
     }
 
+    fun onPageFinished(url: String) {
+        if (isUrlLoadingFailed) {
+            isUrlLoadingFailed = false
+            return
+        }
+
+        if (url == viewState.urlToLoad && !isUrlToLoadFinishedOnce) {
+            isUrlToLoadFinishedOnce = true
+        }
+    }
+
     fun onClose() {
         triggerEvent(Exit)
+    }
+
+    fun onUrlFailed(url: String, errorCode: Int?) {
+        WooLog.d(WooLog.T.GOOGLE_ADS, "Failed to load URL: $url, errorCode: $errorCode")
+        isUrlLoadingFailed = true
     }
 
     data class ViewState(
@@ -52,7 +82,8 @@ class GoogleAdsWebViewViewModel @Inject constructor(
         val title: String?,
         val displayMode: DisplayMode,
         val captureBackButton: Boolean,
-        val clearCache: Boolean = false
+        val clearCache: Boolean = false,
+        val canUseAutoLoginWebview: Boolean
     )
 
     enum class UrlComparisonMode {
