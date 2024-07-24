86fc5286bdbaf866a57d291d8dbb01c7e8677ef0
Rooney <backwardstruck@users.noreply.github.com>
2024-07-24 17:32:46 -0400

    Adjust icon color

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosErrorComponent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosErrorComponent.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
@@ -32,6 +33,8 @@ fun WooPosErrorComponent(
     primaryButton: Button? = null,
     secondaryButton: Button? = null
 ) {
+    val amberColor = colorResource(id = R.color.woo_amber_60)
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -42,8 +45,8 @@ fun WooPosErrorComponent(
         Icon(
             imageVector = icon,
             contentDescription = null,
-            tint = MaterialTheme.colors.error,
-            modifier = Modifier.size(56.dp)
+            tint = amberColor,
+            modifier = Modifier.size(64.dp)
         )
 
         Text(
