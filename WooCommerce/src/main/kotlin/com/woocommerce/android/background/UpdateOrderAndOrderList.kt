a6c5ffa19c429a3314dc6256889a51650740aad3
Alejo <alejandro.torres.veiga@automattic.com>
2024-07-24 09:11:48 -0600

    use case to update an order by id and the first page of orders

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/background/UpdateOrderAndOrderList.kt
@@ -0,0 +1,20 @@
+package com.woocommerce.android.background
+
+import com.woocommerce.android.tools.SelectedSite
+import org.wordpress.android.fluxc.store.WCOrderStore
+import javax.inject.Inject
+
+class UpdateOrderAndOrderList @Inject constructor(
+    private val updateOrdersList: UpdateOrdersList,
+    private val orderStore: WCOrderStore,
+    private val selectedSite: SelectedSite
+) {
+    suspend operator fun invoke(remoteOrderId: Long): Boolean {
+        val orderFetchedSuccess = selectedSite.getOrNull()?.let { site ->
+            val result = orderStore.fetchSingleOrder(site, remoteOrderId)
+            result.isError.not()
+        } ?: false
+        val listFetchedSuccess = updateOrdersList()
+        return orderFetchedSuccess && listFetchedSuccess
+    }
+}
