6172a91aba01bb641aa4609a07fbe05749b88fc2
AnirudhBhat <abhat38@gmail.com>
2024-07-24 16:07:33 +0530

    Add state that handles all the data of product info

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeState.kt
@@ -1,5 +1,7 @@
 package com.woocommerce.android.ui.woopos.home
 
+import androidx.annotation.StringRes
+
 sealed class WooPosHomeState {
     sealed class Cart : WooPosHomeState() {
         data object Empty : Cart()
@@ -9,5 +11,18 @@ sealed class WooPosHomeState {
     sealed class Checkout : WooPosHomeState() {
         data object NotPaid : Checkout()
         data object Paid : Checkout()
+    }
+
+    data class ProductsInfoDialog(
+        val shouldDisplayDialog: Boolean = false,
+        @StringRes val header: Int,
+        @StringRes val primaryMessage: Int,
+        @StringRes val secondaryMessage: Int,
+        @StringRes val secondaryMessageActionLabel: Int,
+        val primaryButton: PrimaryButton,
+    ) : WooPosHomeState() {
+        data class PrimaryButton(
+            @StringRes val label: Int,
+        )
     }
 }
