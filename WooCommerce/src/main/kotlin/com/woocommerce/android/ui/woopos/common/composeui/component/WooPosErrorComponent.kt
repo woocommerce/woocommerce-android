d0a3580caed5d142f6478b329a5a9c89fb350d3e
Rooney <backwardstruck@users.noreply.github.com>
2024-07-25 17:16:25 -0400

    Merge in trunk

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosErrorComponent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosErrorComponent.kt
@@ -1,18 +1,12 @@
 package com.woocommerce.android.ui.woopos.common.composeui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -21,88 +15,72 @@ import androidx.compose.material.icons.filled.Error
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosTheme
+import com.woocommerce.android.ui.woopos.common.composeui.toAdaptivePadding
 
 @Composable
 fun WooPosErrorComponent(
     modifier: Modifier = Modifier,
-    icon: ImageVector,
+    icon: ImageVector = Icons.Default.Error,
     message: String,
     reason: String,
     primaryButton: Button? = null,
     secondaryButton: Button? = null
 ) {
-    val amberColor = colorResource(id = R.color.woo_amber_60)
-
-    Box(
+    Column(
         modifier = modifier
-            .clip(RoundedCornerShape(16.dp))
-            .background(MaterialTheme.colors.surface)
-            .padding(32.dp)
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 32.dp, vertical = 48.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
-        ) {
-            Icon(
-                imageVector = icon,
-                contentDescription = null,
-                tint = amberColor,
-                modifier = Modifier.size(64.dp)
+        Spacer(modifier = Modifier.height(16.dp.toAdaptivePadding()))
+
+        Icon(
+            modifier = Modifier.size(64.dp),
+            imageVector = icon,
+            contentDescription = stringResource(id = R.string.woopos_error_icon_content_description),
+            tint = WooPosTheme.colors.error,
+        )
+
+        Spacer(modifier = Modifier.height(40.dp.toAdaptivePadding()))
+
+        Text(
+            text = message,
+            style = MaterialTheme.typography.h4,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Spacer(modifier = Modifier.height(8.dp.toAdaptivePadding()))
+
+        Text(
+            text = reason,
+            style = MaterialTheme.typography.h5
+        )
+
+        Spacer(modifier = Modifier.height(40.dp.toAdaptivePadding()))
+
+        primaryButton?.let {
+            WooPosButton(
+                text = it.text,
+                onClick = it.click,
+                modifier = Modifier.padding(top = 16.dp)
             )
+            Spacer(modifier = Modifier.height(24.dp.toAdaptivePadding()))
+        }
 
-            Text(
-                text = message,
-                style = MaterialTheme.typography.h6,
-                color = MaterialTheme.colors.onSurface,
-                textAlign = androidx.compose.ui.text.style.TextAlign.Center
+        secondaryButton?.let {
+            WooPosButton(
+                text = it.text,
+                onClick = it.click,
             )
-
-            Divider(
-                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
-                thickness = 0.5.dp,
-                modifier = Modifier
-                    .padding(vertical = 8.dp)
-                    .widthIn(min = 150.dp)
-            )
-
-            Text(
-                text = reason,
-                style = MaterialTheme.typography.subtitle1,
-                color = MaterialTheme.colors.onSurface,
-                textAlign = androidx.compose.ui.text.style.TextAlign.Center
-            )
-
-            primaryButton?.let {
-                Spacer(modifier = Modifier.height(32.dp))
-                WooPosButton(
-                    text = it.text,
-                    onClick = it.click,
-                    modifier = Modifier
-                        .fillMaxWidth(0.7f)
-                        .height(56.dp)
-                )
-            }
-
-            secondaryButton?.let {
-                Spacer(modifier = Modifier.height(16.dp))
-                WooPosButton(
-                    text = it.text,
-                    onClick = it.click,
-                    modifier = Modifier
-                        .fillMaxWidth(0.7f)
-                        .height(56.dp)
-                )
-            }
+            Spacer(modifier = Modifier.height(16.dp.toAdaptivePadding()))
         }
     }
 }
@@ -115,17 +93,19 @@ data class Button(
 @Composable
 @WooPosPreview
 fun WooPosErrorStatePreview() {
-    WooPosErrorComponent(
-        icon = Icons.Default.Error,
-        message = stringResource(R.string.woopos_totals_main_error_label),
-        reason = "Reason",
-        primaryButton = Button(
-            text = stringResource(R.string.retry),
-            click = { /* Handle click */ }
-        ),
-        secondaryButton = Button(
-            text = stringResource(R.string.cancel),
-            click = { /* Handle click */ }
+    WooPosTheme {
+        WooPosErrorComponent(
+            icon = Icons.Default.Error,
+            message = stringResource(R.string.woopos_totals_main_error_label),
+            reason = "Reason",
+            primaryButton = Button(
+                text = stringResource(R.string.retry),
+                click = { }
+            ),
+            secondaryButton = Button(
+                text = stringResource(R.string.cancel),
+                click = { }
+            )
         )
-    )
+    }
 }
