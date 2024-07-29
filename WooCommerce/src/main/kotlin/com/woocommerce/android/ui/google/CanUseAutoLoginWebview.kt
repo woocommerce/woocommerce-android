35f35d1431f9583e6b285bdd992c00f8acf16bf8
Hafiz Rahman <hfzrhmn@gmail.com>
2024-07-29 22:16:23 +0700

    Update CanUseAutoLoginWebview only to support WordPress.com sites.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/google/CanUseAutoLoginWebview.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/google/CanUseAutoLoginWebview.kt
@@ -1,18 +1,12 @@
 package com.woocommerce.android.ui.google
 
 import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.tools.SiteConnectionType
-import com.woocommerce.android.tools.connectionType
 import javax.inject.Inject
 
-// The Google Ads feature (available for Jetpack and Jetpack Connection Package sites) needs to open non-WordPress.com
-// wp-admin URLs. To do so, it has to pick whether to use `WPComWebView` (which includes automatic login) or
-// `ExitAwareWebView` (which doesn't).
-// While technically Jetpack Connection Package sites can automatically login with `WPComWebView` too, there's a
-// behavior where the login redirect does not work properly (it redirects to WordPress.com instead), so it has to use
-// `ExitAwareWebView` instead.
-class CanUseAutoLoginWebview @Inject constructor(
-    private val selectedSite: SelectedSite
-) {
-    operator fun invoke(): Boolean = selectedSite.get().connectionType == SiteConnectionType.Jetpack
+// The Google for WooCommerce feature uses a webview to display campaign creation or dashboard. When first opened, the
+// webview can automatically logs user in (thus saving them some steps) if the site is a WordPress.com site.
+// For .org Jetpack or Jetpack Connection Package sites, the redirection does not work correctly
+// (it logs in then redirects to WordPress.com instead), so we should not use automatic login for these sites.
+class CanUseAutoLoginWebview @Inject constructor(private val selectedSite: SelectedSite) {
+    operator fun invoke(): Boolean = selectedSite.get().isWPCom
 }
