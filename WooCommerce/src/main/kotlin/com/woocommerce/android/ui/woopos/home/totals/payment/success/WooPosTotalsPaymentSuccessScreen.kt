fd9ebd7e5c20b493f13cc0524dce52c1758f1d83
Andrey <akidinov@gmail.com>
2024-08-05 16:06:15 +0200

    CheckMarkIcon

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/payment/success/WooPosTotalsPaymentSuccessScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/payment/success/WooPosTotalsPaymentSuccessScreen.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.woopos.home.totals.payment.success
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,15 +11,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,12 +52,10 @@ fun WooPosPaymentSuccessScreen(
     ) {
         Spacer(modifier = Modifier.weight(1f))
 
-        Icon(
-            painter = painterResource(id = R.drawable.woo_pos_ic_payment_success),
-            tint = Color.Unspecified,
-            contentDescription = null,
-        )
+        CheckMarkIcon()
+
         Spacer(modifier = Modifier.height(56.dp.toAdaptivePadding()))
+
         Text(
             text = stringResource(R.string.woopos_payment_successful_label),
             style = MaterialTheme.typography.h4,
@@ -98,6 +101,26 @@ fun WooPosPaymentSuccessScreen(
         }
 
         Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+private fun CheckMarkIcon() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(156.dp)
+            .shadow(8.dp, CircleShape)
+            .background(
+                MaterialTheme.colors.background, CircleShape
+            )
+    ) {
+        Icon(
+            imageVector = Icons.Default.Check,
+            tint = WooPosTheme.colors.success,
+            contentDescription = null,
+            modifier = Modifier.size(72.dp)
+        )
     }
 }
 
