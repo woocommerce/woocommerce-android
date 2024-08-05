da57c62c9e17895d9221ef758e05a59578f33704
Andrey <akidinov@gmail.com>
2024-08-05 16:08:34 +0200

    paymentSuccessBackground color

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/payment/success/WooPosTotalsPaymentSuccessScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/payment/success/WooPosTotalsPaymentSuccessScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -46,7 +45,7 @@ fun WooPosPaymentSuccessScreen(
             .padding(24.dp.toAdaptivePadding())
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .background(Color(0xFF98F179)),
+            .background(WooPosTheme.colors.paymentSuccessBackground),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
