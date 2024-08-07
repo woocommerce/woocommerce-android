54b3dccc1ba8e57bd0bd67f2dabf723c551ae24c
malinajirka <malinajirka@gmail.com>
2024-08-07 10:06:54 +0200

    Update TTP process name on debug builds

--- WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/WooCommerce.kt
@@ -30,7 +30,7 @@ open class WooCommerce : Application(), HasAndroidInjector, Configuration.Provid
         //  > Caused by: java.lang.IllegalStateException: Default FirebaseApp is not initialized in this process
         //  > com.stripe.cots.aidlservice. Make sure to call FirebaseApp.initializeApp(Context) first.
         // In this case we don't want to initialize any Firebase (or any at all) features of the app in their process.
-        if (getCurrentProcessName() == TAP_TO_PAY_STRIPE_PROCESS_NAME) return
+        if (getCurrentProcessName() == "$packageName:$TAP_TO_PAY_STRIPE_PROCESS_NAME_SUFFIX") return
 
         // Disables Volley debug logging on release build and prevents the "Marker added to finished log" crash
         // https://github.com/woocommerce/woocommerce-android/issues/817
@@ -55,6 +55,6 @@ open class WooCommerce : Application(), HasAndroidInjector, Configuration.Provid
     override fun androidInjector(): AndroidInjector<Any> = androidInjector
 
     companion object {
-        private const val TAP_TO_PAY_STRIPE_PROCESS_NAME = "com.woocommerce.android:stripelocalmobile"
+        private const val TAP_TO_PAY_STRIPE_PROCESS_NAME_SUFFIX = "stripelocalmobile"
     }
 }
