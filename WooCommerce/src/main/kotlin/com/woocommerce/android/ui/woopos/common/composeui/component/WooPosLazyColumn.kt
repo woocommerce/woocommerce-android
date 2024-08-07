110f745997f806624ef783fac6eaa6f1ac8aa573
Andrey <akidinov@gmail.com>
2024-08-07 15:17:28 +0200

    Use card for better shadow

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosLazyColumn.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosLazyColumn.kt
@@ -1,11 +1,11 @@
 package com.woocommerce.android.ui.woopos.common.composeui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
@@ -18,8 +18,6 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosTheme
@@ -49,48 +47,15 @@ fun WooPosLazyColumn(
         }
 
         if (showShadow.value) {
-            val height = 16.dp.toAdaptivePadding()
-            Box(
+            Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(height)
-                    .align(Alignment.TopCenter)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(height)
-                        .background(
-                            brush = Brush.horizontalGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    MaterialTheme.colors.onSurface.copy(alpha = .03f),
-                                    MaterialTheme.colors.onSurface.copy(alpha = .03f),
-                                    MaterialTheme.colors.onSurface.copy(alpha = .03f),
-                                    MaterialTheme.colors.onSurface.copy(alpha = .03f),
-                                    Color.Transparent,
-                                ),
-                                startX = 0f,
-                                endX = Float.POSITIVE_INFINITY
-                            )
-                        )
-                )
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(height)
-                        .background(
-                            brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    MaterialTheme.colors.onSurface.copy(alpha = .1f),
-                                    Color.Transparent,
-                                ),
-                                startY = 0f,
-                                endY = height.value
-                            )
-                        )
-                )
-            }
+                    .height(0.5.dp)
+                    .padding(horizontal = .5.dp)
+                    .align(Alignment.TopCenter),
+                elevation = 4.dp.toAdaptivePadding(),
+                backgroundColor = MaterialTheme.colors.onBackground.copy(alpha = 0.1f)
+            ) {}
         }
     }
 }
