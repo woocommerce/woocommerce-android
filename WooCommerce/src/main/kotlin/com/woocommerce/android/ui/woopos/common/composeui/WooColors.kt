05259cbed173cefc0268225d99c80d79b44b0102
Rooney <backwardstruck@users.noreply.github.com>
2024-08-09 15:18:28 -0400

    Create helper file

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/WooColors.kt
@@ -0,0 +1,70 @@
+package com.woocommerce.android.ui.woopos.common.composeui
+
+import androidx.compose.ui.graphics.Color
+
+object WooColors {
+    val Purple0 = Color(0xFFF2EDFF)
+    val Purple10 = Color(0xFFF7EDF7)
+    val Purple15 = Color(0xFFE5CFE8)
+    val Purple20 = Color(0xFFC792E0)
+    val Purple30 = Color(0xFFB17FD4)
+    val Purple40 = Color(0xFFAF7DD1)
+    val Purple50 = Color(0xFF7F54B3)
+    val Purple60 = Color(0xFF674399)
+    val Purple80 = Color(0xFF3C2861)
+    val Purple90 = Color(0xFF271B3D)
+
+    val Pink10 = Color(0xFFED9BB8)
+    val Pink30 = Color(0xFFEB6594)
+    val Pink50 = Color(0xFFC9356E)
+    val Pink70 = Color(0xFF880E4F)
+    val Pink90 = Color(0xFF5C0935)
+
+    val Red5 = Color(0xFFFACFD2)
+    val Red10 = Color(0xFFFFA6AB)
+    val Red20 = Color(0xFFFF8085)
+    val Red30 = Color(0xFFF86368)
+    val Red50 = Color(0xFFD63638)
+    val Red60 = Color(0xFFB32D2E)
+    val Red70 = Color(0xFF8A2424)
+
+    val Blue5 = Color(0xFFBBE0FA)
+    val Blue30 = Color(0xFF5198D9)
+    val Blue40 = Color(0xFF1689DB)
+    val Blue50 = Color(0xFF2271B1)
+
+    val Orange5 = Color(0xFFF7DCC6)
+    val Orange10 = Color(0xFFFFBF86)
+    val Orange30 = Color(0xFFE68B28)
+    val Orange50 = Color(0xFFB26200)
+    val Orange70 = Color(0xFF351F04)
+
+    val Yellow10 = Color(0xFFF2CF75)
+    val Yellow20 = Color(0xFFF0C443)
+    val Yellow30 = Color(0xFFDBAE17)
+    val Yellow50 = Color(0xFF907300)
+    val Yellow70 = Color(0xFF5C4B00)
+
+    val Celadon0 = Color(0xFFECF7F4)
+    val Celadon5 = Color(0xFFA7E8D4)
+    val Celadon10 = Color(0xFF65D9B9)
+    val Celadon20 = Color(0xFF2FC39E)
+    val Celadon40 = Color(0xFF009172)
+
+    val Green0 = Color(0xFFEBF7F1)
+    val Green5 = Color(0xFFA4F5C8)
+    val Green10 = Color(0xFF59E38F)
+    val Green20 = Color(0xFF1ED15A)
+    val Green50 = Color(0xFF008A20)
+
+    val White = Color(0xFFFFFFFF)
+    val Gray0 = Color(0xFFF6F7F7)
+    val Gray5 = Color(0xFFDCDCDE)
+    val Gray6 = Color(0xFFF2F2F7)
+    val Gray20 = Color(0xFFB4B1B8)
+    val Gray40 = Color(0xFF787C82)
+    val Gray60 = Color(0xFF51565F)
+    val Gray70 = Color(0xFF3D444B)
+    val Gray80 = Color(0xFF2C3338)
+    val Black = Color(0xFF000000)
+}
