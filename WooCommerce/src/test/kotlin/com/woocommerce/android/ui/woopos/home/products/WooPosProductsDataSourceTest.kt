c14afcb86673cea5a944946bc32bccb02d0bd430
AnirudhBhat <abhat38@gmail.com>
2024-08-12 11:09:43 +0530

    Add test to verify filter logic allows only non virtual products for cached products

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsDataSourceTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsDataSourceTest.kt
@@ -260,4 +260,43 @@ class WooPosProductsDataSourceTest {
             val remoteResult = flow[1] as WooPosProductsDataSource.ProductsResult.Remote
             assertThat(remoteResult.productsResult.getOrNull()?.any { it.remoteId == 1L }).isFalse()
         }
+
+    @Test
+    fun `given cached products, when loadSimpleProducts called, then filter out virtual products`() =
+        runTest {
+            // GIVEN
+            whenever(handler.canLoadMore).thenReturn(AtomicBoolean(true))
+            whenever(handler.productsFlow).thenReturn(
+                flowOf(
+                    listOf(
+                        ProductTestUtils.generateProduct(
+                            productId = 1,
+                            productName = "Product 1",
+                            amount = "0",
+                            productType = "simple",
+                            isVirtual = true,
+                            isDownloadable = false,
+                        ),
+                        ProductTestUtils.generateProduct(
+                            productId = 2,
+                            productName = "Product 2",
+                            amount = "20.0",
+                            productType = "simple",
+                            isVirtual = false,
+                            isDownloadable = false
+                        ).copy(firstImageUrl = "https://test.com")
+                    )
+                )
+            )
+            whenever(handler.loadFromCacheAndFetch(any(), any(), any())).thenReturn(Result.success(Unit))
+            val sut = WooPosProductsDataSource(handler, productStore, site)
+
+            // WHEN
+            val flow = sut.loadSimpleProducts(forceRefreshProducts = false).toList()
+
+            // THEN
+            val cachedResult = flow[0] as WooPosProductsDataSource.ProductsResult.Cached
+
+            assertFalse(cachedResult.products.any { it.remoteId == 1L })
+        }
 }
