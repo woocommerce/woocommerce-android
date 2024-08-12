f6139e809badf6f6aea4bf7627aaf2e1653e1f48
Rooney <backwardstruck@users.noreply.github.com>
2024-08-12 16:48:18 -0400

    Make this private

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/WooPosTheme.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/WooPosTheme.kt
@@ -106,3 +106,33 @@ object WooPosTheme {
         @Composable
         get() = LocalCustomColors.current
 }
+
+private object WooPOSColors {
+    val Gray0 = Color(0xFFF6F7F7)
+
+    val Purple50 = Color(0xFF7F54B3)
+    val Purple90 = Color(0xFF271B3D)
+
+    val PrimaryDarkPurple = Color(0xFF9C70D3)
+    val PrimaryVariantDarkBlue = Color(0xFF3700B3)
+    val SecondaryDarkGreen = Color(0xFF0A9400)
+    val SecondaryVariantDarkGray = Color(0xFF8D8D8D)
+    val SurfaceDarkBlack = Color(0xFF121212)
+
+    val SecondaryLightGreen = Color(0xFF004B3E)
+    val SecondaryVariantLightGray = Color(0xFF50575E)
+
+    val DarkLoadingSkeleton = Color(0xFF616161)
+    val DarkBorder = Color(0xFF8D8D8D)
+    val DarkSuccess = Color(0xFF06B166)
+    val DarkError = Color(0xFFBE4400)
+    val DarkSuccessBackground = Color(0xFF74C758)
+    val DarkSuccessText = Color(0xFFF2EBFF)
+
+    val LightLoadingSkeleton = Color(0xFFE1E1E1)
+    val LightBorder = Color(0xFFC6C6C8)
+    val LightSuccess = Color(0xFF03D479)
+    val LightError = Color(0xFFF16618)
+    val LightSuccessBackground = Color(0xFF98F179)
+}
+
