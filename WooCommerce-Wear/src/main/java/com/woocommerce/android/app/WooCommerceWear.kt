0e1ea0f6649168c8c7b0ed08b685bcaaab6fcfa7
ThomazFB <thomazfcortez@gmail.com>
2024-08-12 19:30:54 -0300

    Initialize CrashLogging in Wear

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/app/WooCommerceWear.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/app/WooCommerceWear.kt
@@ -1,15 +1,22 @@
 package com.woocommerce.android.app
 
 import android.app.Application
+import com.automattic.android.tracks.crashlogging.CrashLogging
 import com.yarolegovich.wellsql.WellSql
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
 import org.wordpress.android.fluxc.persistence.WellSqlConfig.Companion.ADDON_WOOCOMMERCE
 
 @HiltAndroidApp
 open class WooCommerceWear : Application() {
+
+    @Inject
+    lateinit var crashLogging: CrashLogging
+
     override fun onCreate() {
         super.onCreate()
         WellSql.init(WellSqlConfig(applicationContext, ADDON_WOOCOMMERCE))
+        crashLogging.initialize()
     }
 }
