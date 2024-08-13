6fe3c8125595cf0a28bdddf44884359607b6cc5a
ThomazFB <thomazfcortez@gmail.com>
2024-08-13 18:13:14 -0300

    Fix injection issue with crashLogging

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/app/WooCommerceWear.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/app/WooCommerceWear.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.app
 import android.app.Application
 import com.automattic.android.tracks.crashlogging.CrashLogging
 import com.yarolegovich.wellsql.WellSql
+import dagger.Lazy
 import dagger.hilt.android.HiltAndroidApp
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -15,19 +16,19 @@ import javax.inject.Inject
 open class WooCommerceWear : Application() {
 
     @Inject
-    lateinit var crashLogging: CrashLogging
+    lateinit var crashLogging: Lazy<CrashLogging>
 
     override fun onCreate() {
         super.onCreate()
         WellSql.init(WellSqlConfig(applicationContext, ADDON_WOOCOMMERCE))
-        crashLogging.initialize()
+        crashLogging.get().initialize()
     }
 
     @Suppress("unused")
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun onUnexpectedError(event: OnUnexpectedError) {
         with(event) {
-            crashLogging.sendReport(exception = exception, message = "FluxC: ${exception.message}: $description")
+            crashLogging.get().sendReport(exception = exception, message = "FluxC: ${exception.message}: $description")
         }
     }
 }
