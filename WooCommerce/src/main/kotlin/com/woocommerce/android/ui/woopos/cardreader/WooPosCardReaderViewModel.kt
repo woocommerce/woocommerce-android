fbf876db0eef741848bbd3dcb0f52049067ec492
samiuelson <samiuelson@gmail.com>
2024-08-19 20:15:40 +0200

    Convert prop's initializer to getter

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderViewModel.kt
@@ -10,29 +10,28 @@ import javax.inject.Inject
 @HiltViewModel
 class WooPosCardReaderViewModel @Inject constructor(val savedStateHandle: SavedStateHandle) :
     ViewModel() {
-    val cardReaderMode: WooPosCardReaderMode = savedStateHandle.get<WooPosCardReaderMode>(
-        WOO_POS_CARD_READER_MODE_KEY
-    ).run {
-        when (this) {
-            is WooPosCardReaderMode.Connection -> {
-                WooPosCardReaderMode.Connection
-            }
+    val cardReaderMode: WooPosCardReaderMode
+        get() = savedStateHandle.get<WooPosCardReaderMode>(WOO_POS_CARD_READER_MODE_KEY).run {
+            when (this) {
+                is WooPosCardReaderMode.Connection -> {
+                    WooPosCardReaderMode.Connection
+                }
 
-            is WooPosCardReaderMode.Payment -> {
-                if (orderId != -1L) {
-                    WooPosCardReaderMode.Payment(orderId)
-                } else {
-                    val errorMessage = "Tried collecting payment with invalid orderId"
+                is WooPosCardReaderMode.Payment -> {
+                    if (orderId != -1L) {
+                        WooPosCardReaderMode.Payment(orderId)
+                    } else {
+                        val errorMessage = "Tried collecting payment with invalid orderId"
+                        WooLog.e(WooLog.T.POS, "Error in WooPosCardReaderViewModel - $errorMessage")
+                        error(errorMessage)
+                    }
+                }
+
+                null -> {
+                    val errorMessage = "WooPosCardReaderMode not found in savedStateHandle"
                     WooLog.e(WooLog.T.POS, "Error in WooPosCardReaderViewModel - $errorMessage")
                     error(errorMessage)
                 }
             }
-
-            null -> {
-                val errorMessage = "WooPosCardReaderMode not found in savedStateHandle"
-                WooLog.e(WooLog.T.POS, "Error in WooPosCardReaderViewModel - $errorMessage")
-                error(errorMessage)
-            }
         }
-    }
 }
