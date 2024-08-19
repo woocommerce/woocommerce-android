255b583e09e46a308f63083e88ebf4c1ba127ccd
samiuelson <samiuelson@gmail.com>
2024-08-19 20:12:14 +0200

    Remove ScopedViewModel supertype

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderViewModel.kt
@@ -1,16 +1,15 @@
 package com.woocommerce.android.ui.woopos.cardreader
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.woocommerce.android.ui.woopos.cardreader.WooPosCardReaderActivity.Companion.WOO_POS_CARD_READER_MODE_KEY
 import com.woocommerce.android.util.WooLog
-import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class WooPosCardReaderViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-) : ScopedViewModel(savedStateHandle) {
+class WooPosCardReaderViewModel @Inject constructor(val savedStateHandle: SavedStateHandle) :
+    ViewModel() {
     val cardReaderMode: WooPosCardReaderMode = savedStateHandle.get<WooPosCardReaderMode>(
         WOO_POS_CARD_READER_MODE_KEY
     ).run {
@@ -18,6 +17,7 @@ class WooPosCardReaderViewModel @Inject constructor(
             is WooPosCardReaderMode.Connection -> {
                 WooPosCardReaderMode.Connection
             }
+
             is WooPosCardReaderMode.Payment -> {
                 if (orderId != -1L) {
                     WooPosCardReaderMode.Payment(orderId)
@@ -27,6 +27,7 @@ class WooPosCardReaderViewModel @Inject constructor(
                     error(errorMessage)
                 }
             }
+
             null -> {
                 val errorMessage = "WooPosCardReaderMode not found in savedStateHandle"
                 WooLog.e(WooLog.T.POS, "Error in WooPosCardReaderViewModel - $errorMessage")
