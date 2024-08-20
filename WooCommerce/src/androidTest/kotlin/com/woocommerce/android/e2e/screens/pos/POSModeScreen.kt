0433badbe5256c6a552a2520589cf5caccb8eec9
AnirudhBhat <abhat38@gmail.com>
2024-08-20 14:56:56 +0530

    Add POSModeScreen

---
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/e2e/screens/pos/POSModeScreen.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.e2e.screens.pos
+
+import com.woocommerce.android.R
+import com.woocommerce.android.e2e.helpers.util.Screen
+
+class POSModeScreen : Screen(R.id.pos_mode_compose_view) {
+
+}
