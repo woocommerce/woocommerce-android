1a7a0b7f61d47eaea1a09598225c4b0266643c96
AnirudhBhat <abhat38@gmail.com>
2024-08-20 15:01:22 +0530

    Add test tag for WooPosBanner so that we can reference this in UI test

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosBanner.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosBanner.kt
@@ -17,8 +17,11 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -48,6 +51,8 @@ fun WooPosBanner(
                 end = 2.dp.toAdaptivePadding(),
                 bottom = 16.dp.toAdaptivePadding()
             )
+            .testTag("woo_pos_simple_products_banner")
+
     ) {
         Card(
             shape = RoundedCornerShape(8.dp),
