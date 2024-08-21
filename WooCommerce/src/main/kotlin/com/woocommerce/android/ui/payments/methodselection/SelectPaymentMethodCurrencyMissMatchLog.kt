8782ef864fba2a09ac44230d5d6e85c6452d8c69
Andrey <akidinov@gmail.com>
2024-08-21 10:47:12 +0200

    Fixed the condition

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/SelectPaymentMethodCurrencyMissMatchLog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/SelectPaymentMethodCurrencyMissMatchLog.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 
 class SelectPaymentMethodCurrencyMissMatchLog @Inject constructor() {
     operator fun invoke(storeCurrency: String, orderCurrency: String) {
-        if (storeCurrency.equals(orderCurrency, ignoreCase = true)) {
+        if (!storeCurrency.equals(orderCurrency, ignoreCase = true)) {
             val message = "⚠️ Order currency: $orderCurrency differs from store's currency: " +
                 "$storeCurrency which can lead to payment methods being unavailable"
             WooLog.w(WooLog.T.ORDERS, message)
