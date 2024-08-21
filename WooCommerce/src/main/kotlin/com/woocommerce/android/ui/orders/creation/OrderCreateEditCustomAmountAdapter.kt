b04b9e4a003f3c9ccb7e1a5c7cc2ef8cd2474e4e
Alejo <alejandro.torres.veiga@automattic.com>
2024-08-20 22:06:17 -0600

    add / remove listener from the custom amount items

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditCustomAmountAdapter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditCustomAmountAdapter.kt
@@ -1,7 +1,9 @@
 package com.woocommerce.android.ui.orders.creation
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -37,25 +39,22 @@ class OrderCreateEditCustomAmountAdapter(
 
     inner class CustomAmountViewHolder(private val binding: OrderCreationCustomAmountItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        private val safePosition: Int?
-            get() = bindingAdapterPosition.takeIf { it != RecyclerView.NO_POSITION }
-
-        init {
-            binding.root.setOnClickListener {
-                safePosition?.let {
-                    val customAmountUIModel = getItem(it)
-                    if (!customAmountUIModel.isLocked) {
-                        onCustomAmountClick(customAmountUIModel)
-                    }
-                }
-            }
-        }
 
         fun bind(customAmountUIModel: CustomAmountUIModel) {
             binding.customAmountLayout.customAmountName.text = customAmountUIModel.name
             binding.customAmountLayout.customAmountAmount.text = currencyFormatter.formatCurrency(
                 customAmountUIModel.amount.toString()
             )
+            binding.customAmountLayout.customAmountEdit.isVisible = isLocked.not()
+            if (isLocked.not()) {
+                binding.root.setOnClickListener {
+                    onCustomAmountClick(customAmountUIModel)
+                }
+                binding.root.isClickable = true
+            } else if (binding.root.hasOnClickListeners()) {
+                binding.root.setOnClickListener(null)
+                binding.root.isClickable = false
+            }
         }
     }
 
