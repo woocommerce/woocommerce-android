fb30634034576af4efed8201e6d400486911c4e6
Andrey <akidinov@gmail.com>
2024-08-22 11:19:21 +0200

    Changed animation to look better with the changed placement

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -334,15 +333,8 @@ private fun ProductItem(
 
     val elevation by animateDpAsState(
         targetValue = if (hasAnimationStarted) 4.dp else 0.dp,
-        animationSpec = tween(durationMillis = 150, delayMillis = 100),
+        animationSpec = tween(durationMillis = 200, delayMillis = 100),
         label = "elevation"
-    )
-
-    val itemHeight = 64.dp
-    val offsetY by animateDpAsState(
-        targetValue = if (hasAnimationStarted) 0.dp else -itemHeight,
-        animationSpec = tween(durationMillis = 70),
-        label = "offsetY"
     )
 
     val alpha by animateFloatAsState(
@@ -368,8 +360,7 @@ private fun ProductItem(
 
     Card(
         modifier = modifier
-            .offset(y = offsetY)
-            .height(itemHeight)
+            .height(64.dp)
             .semantics { contentDescription = itemContentDescription }
             .graphicsLayer(alpha = alpha),
         elevation = elevation,
