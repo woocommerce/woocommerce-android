152625c8c4dc340fa9cb4e1ab9c3a9c5203216d7
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-08-22 13:21:44 +0100

    Remove unused AI repository function

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ai/AIRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ai/AIRepository.kt
@@ -80,36 +80,6 @@ class AIRepository @Inject constructor(
         )
     }
 
-    // TODO remove this when cleaning up legacy code for product creation
-    @Suppress("LongParameterList")
-    suspend fun generateProductLegacy(
-        productName: String,
-        productKeyWords: String,
-        tone: String,
-        weightUnit: String,
-        dimensionUnit: String,
-        currency: String,
-        existingCategories: List<ProductCategory>,
-        existingTags: List<ProductTag>,
-        languageISOCode: String
-    ): Result<String> {
-        return fetchJetpackAIQuery(
-            prompt = AIPrompts.generateProductCreationPromptLegacy(
-                name = productName,
-                keywords = productKeyWords,
-                tone = tone,
-                weightUnit = weightUnit,
-                dimensionUnit = dimensionUnit,
-                currency = currency,
-                existingCategories = existingCategories.map { it.name },
-                existingTags = existingTags.map { it.name },
-                languageISOCode = languageISOCode
-            ),
-            feature = PRODUCT_CREATION_FEATURE,
-            format = ResponseFormat.JSON
-        )
-    }
-
     suspend fun generateOrderThankYouNote(
         customerName: String,
         productName: String,
