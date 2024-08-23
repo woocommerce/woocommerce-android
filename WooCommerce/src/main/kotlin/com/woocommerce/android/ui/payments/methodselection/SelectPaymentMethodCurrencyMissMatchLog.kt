9d13ceb1f82c8fce2b9bf514d37f8a9eca2f55a6
Andrey <akidinov@gmail.com>
2024-08-23 09:55:26 +0200

    Log via wrapper

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/SelectPaymentMethodCurrencyMissMatchLog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/SelectPaymentMethodCurrencyMissMatchLog.kt
@@ -1,14 +1,17 @@
 package com.woocommerce.android.ui.payments.methodselection
 
 import com.woocommerce.android.util.WooLog
+import com.woocommerce.android.util.WooLogWrapper
 import javax.inject.Inject
 
-class SelectPaymentMethodCurrencyMissMatchLog @Inject constructor() {
+class SelectPaymentMethodCurrencyMissMatchLog @Inject constructor(
+    private val wooLogWrapper: WooLogWrapper,
+) {
     operator fun invoke(storeCurrency: String, orderCurrency: String) {
         if (!storeCurrency.equals(orderCurrency, ignoreCase = true)) {
             val message = "⚠️ Order currency: $orderCurrency differs from store's currency: " +
                 "$storeCurrency which can lead to payment methods being unavailable"
-            WooLog.w(WooLog.T.ORDERS, message)
+            wooLogWrapper.w(WooLog.T.ORDERS, message)
         }
     }
 }
