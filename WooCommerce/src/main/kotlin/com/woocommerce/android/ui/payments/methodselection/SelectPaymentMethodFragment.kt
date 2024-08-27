af0ea401a9f8e16bdcfa5be12d9f406b06693c87
Andrey <akidinov@gmail.com>
2024-08-27 16:39:29 +0200

    Fixed formatting

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/SelectPaymentMethodFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/methodselection/SelectPaymentMethodFragment.kt
@@ -215,12 +215,13 @@ class SelectPaymentMethodFragment : BaseFragment(R.layout.fragment_select_paymen
 
                 is SkipScreenInPosAndNavigateToCardReaderPaymentFlow -> {
                     if (findNavController().currentDestination?.id == R.id.selectPaymentMethodFragment) {
-                        val action =
-                            SelectPaymentMethodFragmentDirections.actionSelectPaymentMethodFragmentToCardReaderPaymentFlow(
-                                event.cardReaderFlowParam,
-                                event.cardReaderType
-                            )
-                        findNavController().navigate(action)
+                        findNavController().navigate(
+                            SelectPaymentMethodFragmentDirections
+                                .actionSelectPaymentMethodFragmentToCardReaderPaymentFlow(
+                                    event.cardReaderFlowParam,
+                                    event.cardReaderType
+                                )
+                        )
                     }
                 }
 
