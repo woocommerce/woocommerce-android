958d11daf9ec6bff7ffdb50784ec29f0c35b96ac
ThomazFB <thomazfcortez@gmail.com>
2024-08-26 21:55:20 -0300

    Implement IsDeviceBatterySaverActive basic check

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/IsDeviceBatterySaverActive.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/appwidgets/IsDeviceBatterySaverActive.kt
@@ -1,9 +1,18 @@
 package com.woocommerce.android.ui.appwidgets
 
+import android.content.Context
+import android.os.Build
+import android.os.PowerManager
+
 class IsDeviceBatterySaverActive(
 
 ) {
-    operator fun invoke() {
-
+    operator fun invoke(
+        context: Context
+    ): Boolean {
+        return context.getSystemService(Context.POWER_SERVICE)
+            .run { this as? PowerManager }
+            ?.isPowerSaveMode
+            ?: false
     }
 }
