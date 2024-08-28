55f55f0aace55042efd1da2901536d51b43b7c8c
AnirudhBhat <abhat38@gmail.com>
2024-08-28 13:16:58 +0530

    Add UI test to verify that Product info dialog icon is shown on the products screen toolbar when Simple products only banner is dismissed

---
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooPosHomeProductInfoDialogTest.kt
@@ -0,0 +1,87 @@
+package com.woocommerce.android
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.woocommerce.android.e2e.helpers.InitializationRule
+import com.woocommerce.android.e2e.helpers.TestBase
+import com.woocommerce.android.e2e.rules.RetryTestRule
+import com.woocommerce.android.e2e.screens.TabNavComponent
+import com.woocommerce.android.e2e.screens.login.WelcomeScreen
+import com.woocommerce.android.ui.login.LoginActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class WooPosHomeProductInfoDialogTest : TestBase() {
+    @get:Rule(order = 1)
+    val rule = HiltAndroidRule(this)
+
+    @get:Rule(order = 2)
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @get:Rule(order = 3)
+    val initRule = InitializationRule()
+
+    @get:Rule(order = 4)
+    var activityRule = ActivityTestRule(LoginActivity::class.java)
+
+    @get:Rule(order = 5)
+    var retryTestRule = RetryTestRule()
+
+    @Inject
+    lateinit var dataStore: DataStore<Preferences>
+
+    @Before
+    fun setUp() = runTest {
+        rule.inject()
+        WelcomeScreen
+            .skipCarouselIfNeeded()
+            .selectLogin()
+            .proceedWith(BuildConfig.SCREENSHOTS_URL)
+            .proceedWith(BuildConfig.SCREENSHOTS_USERNAME)
+            .proceedWith(BuildConfig.SCREENSHOTS_PASSWORD)
+
+        TabNavComponent()
+            .gotoMoreMenuScreen()
+            .openPOSScreen(composeTestRule)
+        dataStore.edit { it.clear() }
+    }
+
+    @Test
+    fun testProductInfoDialogIconIsDisplayedOnProductsScreenIfSimpleProductsBannerIsNotVisible() = runTest {
+        composeTestRule.waitUntil(5000) {
+            try {
+                composeTestRule.onNodeWithTag("product_list")
+                    .assertExists()
+                    .assertIsDisplayed()
+                true
+            } catch (e: AssertionError) {
+                false
+            }
+        }
+
+        composeTestRule.onNodeWithContentDescription(
+            composeTestRule.activity.getString(R.string.woopos_banner_simple_products_close_content_description)
+        ).performClick()
+
+        composeTestRule.onNodeWithContentDescription(
+            composeTestRule.activity.getString(R.string.woopos_banner_simple_products_info_content_description)
+        ).assertIsDisplayed()
+    }
+}
