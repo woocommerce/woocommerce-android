42e2d59dfcec8e3de6bf2bc0b6f121c23bbc54ef
Andrey <akidinov@gmail.com>
2024-08-29 15:03:31 +0200

    Removed loading text from the splash screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.woocommerce.android.R
@@ -57,14 +56,6 @@ private fun Loading() {
         Text(
             text = stringResource(id = R.string.woopos_splash_title),
             style = MaterialTheme.typography.h5,
-        )
-
-        Spacer(modifier = Modifier.height(16.dp.toAdaptivePadding()))
-
-        Text(
-            text = stringResource(id = R.string.woopos_splash_message),
-            style = MaterialTheme.typography.h4,
-            fontWeight = FontWeight.Bold
         )
     }
 }
