d78b8cb9be96b494b6575debf9c8769920d7fccd
ThomazFB <thomazfcortez@gmail.com>
2024-08-29 21:31:20 -0300

    Fix import lint issues

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/picker/VariationPickerViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/variations/picker/VariationPickerViewModel.kt
@@ -14,16 +14,16 @@ import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.withIndex
 import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 @OptIn(FlowPreview::class)
 @HiltViewModel
