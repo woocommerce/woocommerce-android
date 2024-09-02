220e979b98509a0d066d0aff8b86cc9dd76409d5
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-09-02 17:36:18 +0200

    Fix typo on string id

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/campaigs/BlazeCampaignItem.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/campaigs/BlazeCampaignItem.kt
@@ -74,7 +74,7 @@ fun BlazeCampaignItem(
                     CampaignStat(
                         statName = stringResource(R.string.blaze_campaign_status_ctr_label),
                         statValue = stringResource(
-                            id = R.string.blaze_campaign_status_ctr_valur,
+                            id = R.string.blaze_campaign_status_ctr_value,
                             campaign.impressions,
                             campaign.clicks
                         )
