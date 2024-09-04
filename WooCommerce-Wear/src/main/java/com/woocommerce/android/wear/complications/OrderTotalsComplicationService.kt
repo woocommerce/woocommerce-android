8052dd0cbe836a988551ad3c89a6fefaa8e5a84b
ThomazFB <thomazfcortez@gmail.com>
2024-09-04 02:02:03 -0300

    Update Order Totals preview data with the expected string resources

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/OrderTotalsComplicationService.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/OrderTotalsComplicationService.kt
@@ -4,14 +4,15 @@ import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationType
 import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import com.woocommerce.android.R
 
 class OrderTotalsComplicationService : SuspendingComplicationDataSourceService() {
     override fun getPreviewData(type: ComplicationType): ComplicationData? {
         return when (type) {
             ComplicationType.SHORT_TEXT -> createTextComplicationData(
                 context = applicationContext,
-                content = "$42k",
-                description = "Displays the total value of the current order"
+                content = getString(R.string.order_totals_complication_preview_value),
+                description = getString(R.string.order_totals_complication_preview_description)
             )
 
             else -> null
