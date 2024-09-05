7e16161fe905fd171df31b91e0d972f3ce7c11cd
AnirudhBhat <abhat38@gmail.com>
2024-09-05 11:56:28 +0530

    Add proper tint color for product info icon color

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -202,7 +203,7 @@ private fun ProductsToolbar(
                             contentDescription = stringResource(
                                 id = R.string.woopos_banner_simple_products_info_content_description
                             ),
-                            tint = MaterialTheme.colors.primary,
+                            tint = MaterialTheme.colors.onSurface.copy(ContentAlpha.high),
                         )
                     }
                 }
