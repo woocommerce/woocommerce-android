e190c2e05f4e1b618dc9be446ad83f5e12b5a54b
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-09-06 17:26:44 +0100

    Fix the recursion issue that causes a StackOverflow crash

--- WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/NumberExtensionsWrapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/extensions/NumberExtensionsWrapper.kt
@@ -2,10 +2,11 @@ package com.woocommerce.android.extensions
 
 import java.util.Locale
 import javax.inject.Inject
+import com.woocommerce.android.extensions.compactNumberCompat as compactNumberCompatExt
 
 class NumberExtensionsWrapper @Inject constructor() {
     fun compactNumberCompat(
         number: Long,
         locale: Locale = Locale.getDefault()
-    ): String = compactNumberCompat(number, locale)
+    ): String = compactNumberCompatExt(number, locale)
 }
