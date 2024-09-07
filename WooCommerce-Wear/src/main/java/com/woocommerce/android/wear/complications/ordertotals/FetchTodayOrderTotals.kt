a408aa185db49af1f8e3f67d9468d086e58fc302
ThomazFB <thomazfcortez@gmail.com>
2024-09-07 12:36:53 -0300

    Add constants for the FetchTodayOrderTotals internal logic

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/ordertotals/FetchTodayOrderTotals.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/ordertotals/FetchTodayOrderTotals.kt
@@ -12,19 +12,26 @@ class FetchTodayOrderTotals @Inject constructor(
     private val loginRepository: LoginRepository,
     private val locale: Locale
 ) {
-    suspend operator fun invoke(): String? {
+    suspend operator fun invoke(): String {
         return loginRepository.selectedSite
             ?.let { statsRepository.fetchRevenueStats(it) }
             ?.getOrNull()
             ?.parseTotal()
             ?.totalSales
             ?.format()
-            ?: "N/A"
+            ?: DEFAULT_EMPTY_VALUE
     }
 
     private fun Double.format(): String {
         if (this < 1000) return this.toString()
         val exp = (ln(this) / ln(1000.0)).toInt()
-        return String.format(locale, "%.1f %c", this / 1000.0.pow(exp.toDouble()), "kMGTPE"[exp - 1])
+        val formattingValue = this / 1000.0.pow(exp.toDouble())
+        return String.format(locale, NUMBER_FORMAT, formattingValue, UNIT_SUFFIX[exp - 1])
+    }
+
+    companion object {
+        const val NUMBER_FORMAT = "%.1f %c"
+        const val UNIT_SUFFIX = "kMGTPE"
+        const val DEFAULT_EMPTY_VALUE = "N/A"
     }
 }
