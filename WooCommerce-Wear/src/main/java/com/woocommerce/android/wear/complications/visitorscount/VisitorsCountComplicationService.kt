2ef64ca10f526990078c5ed6ff1a5679d6fce7de
ThomazFB <thomazfcortez@gmail.com>
2024-09-09 01:10:08 -0300

    Wire VisitorsCountComplicationService with Visitors data from FetchStatsForComplications use case

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/visitorscount/VisitorsCountComplicationService.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/visitorscount/VisitorsCountComplicationService.kt
@@ -5,9 +5,17 @@ import androidx.wear.watchface.complications.data.ComplicationType
 import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
 import com.woocommerce.android.R
+import com.woocommerce.android.wear.complications.FetchStatsForComplications
+import com.woocommerce.android.wear.complications.FetchStatsForComplications.StatType.VISITORS
 import com.woocommerce.android.wear.complications.createTextComplicationData
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class VisitorsCountComplicationService : SuspendingComplicationDataSourceService() {
+
+    @Inject lateinit var fetchStatsForComplications: FetchStatsForComplications
+
     override fun getPreviewData(type: ComplicationType): ComplicationData? {
         return when (type) {
             ComplicationType.SHORT_TEXT -> createTextComplicationData(
@@ -21,6 +29,14 @@ class VisitorsCountComplicationService : SuspendingComplicationDataSourceService
     }
 
     override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
-        TODO("Not yet implemented")
+        return when (request.complicationType) {
+            ComplicationType.SHORT_TEXT -> createTextComplicationData(
+                context = applicationContext,
+                content = fetchStatsForComplications(VISITORS),
+                description = getString(R.string.visitors_count_complication_preview_description)
+            )
+
+            else -> null
+        }
     }
 }
