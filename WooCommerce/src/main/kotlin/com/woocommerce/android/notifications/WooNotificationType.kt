2fe3177f0d44ae0ce94a797fcc30c80cb7b9f4fc
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-09-09 17:25:49 +0200

    Add missing mapping for Blaze push notification type

--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationType.kt
@@ -13,6 +13,10 @@ fun NotificationModel.getWooType(): WooNotificationType {
     return when (this.type) {
         NotificationModel.Kind.STORE_ORDER -> WooNotificationType.NEW_ORDER
         NotificationModel.Kind.COMMENT -> WooNotificationType.PRODUCT_REVIEW
+        NotificationModel.Kind.BLAZE_APPROVED_NOTE,
+        NotificationModel.Kind.BLAZE_REJECTED_NOTE,
+        NotificationModel.Kind.BLAZE_CANCELLED_NOTE,
+        NotificationModel.Kind.BLAZE_PERFORMED_NOTE -> WooNotificationType.BLAZE
         else -> WooNotificationType.LOCAL_REMINDER
     }
 }
