ee30f0eb2e3e73f9686d8cb987911bb92a38b941
ThomazFB <thomazfcortez@gmail.com>
2024-09-09 00:49:25 -0300

    Wire OrderCountComplicationService data acquisition with respective use case call

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/ordercount/OrderCountComplicationService.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/ordercount/OrderCountComplicationService.kt
@@ -6,8 +6,16 @@ import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
 import com.woocommerce.android.R
 import com.woocommerce.android.wear.complications.createTextComplicationData
+import com.woocommerce.android.wear.complications.ordertotals.FetchStatsForComplications
+import com.woocommerce.android.wear.complications.ordertotals.FetchStatsForComplications.StatType.ORDER_COUNT
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class OrderCountComplicationService : SuspendingComplicationDataSourceService() {
+
+    @Inject lateinit var fetchStatsForComplications: FetchStatsForComplications
+
     override fun getPreviewData(type: ComplicationType): ComplicationData? {
         return when (type) {
             ComplicationType.SHORT_TEXT -> createTextComplicationData(
@@ -21,6 +29,14 @@ class OrderCountComplicationService : SuspendingComplicationDataSourceService() 
     }
 
     override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
-        TODO("Not yet implemented")
+        return when (request.complicationType) {
+            ComplicationType.SHORT_TEXT -> createTextComplicationData(
+                context = applicationContext,
+                content = fetchStatsForComplications(ORDER_COUNT),
+                description = getString(R.string.order_count_complication_preview_description)
+            )
+
+            else -> null
+        }
     }
 }
