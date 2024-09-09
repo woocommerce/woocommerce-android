8a42f1e322006a352f9686c59cac408011c044ba
ThomazFB <thomazfcortez@gmail.com>
2024-09-09 00:48:33 -0300

    Introduce OrderCountComplicationService with Preview code

---
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/complications/ordercount/OrderCountComplicationService.kt
@@ -0,0 +1,26 @@
+package com.woocommerce.android.wear.complications.ordercount
+
+import androidx.wear.watchface.complications.data.ComplicationData
+import androidx.wear.watchface.complications.data.ComplicationType
+import androidx.wear.watchface.complications.datasource.ComplicationRequest
+import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import com.woocommerce.android.R
+import com.woocommerce.android.wear.complications.createTextComplicationData
+
+class OrderCountComplicationService : SuspendingComplicationDataSourceService() {
+    override fun getPreviewData(type: ComplicationType): ComplicationData? {
+        return when (type) {
+            ComplicationType.SHORT_TEXT -> createTextComplicationData(
+                context = applicationContext,
+                content = getString(R.string.order_count_complication_preview_value),
+                description = getString(R.string.order_count_complication_preview_description)
+            )
+
+            else -> null
+        }
+    }
+
+    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
+        TODO("Not yet implemented")
+    }
+}
