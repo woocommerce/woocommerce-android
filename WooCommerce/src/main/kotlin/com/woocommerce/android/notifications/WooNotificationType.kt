6c02efd6387f72afce7fe88b56fe2ccc6d8ddcb6
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-09-11 17:32:20 +0200

    Fix tracking issue in push notifications

--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationType.kt
@@ -1,32 +1,49 @@
 package com.woocommerce.android.notifications
 
 import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import org.wordpress.android.fluxc.model.notification.NotificationModel
 
 sealed interface WooNotificationType : Parcelable {
-    @Parcelize
-    data object NewOrder : WooNotificationType
+    val trackingValue: String
 
     @Parcelize
-    data object ProductReview : WooNotificationType
+    data object NewOrder : WooNotificationType {
+        @IgnoredOnParcel override val trackingValue: String = "NEW_ORDER"
+    }
 
     @Parcelize
-    data object LocalReminder : WooNotificationType
+    data object ProductReview : WooNotificationType {
+        @IgnoredOnParcel override val trackingValue: String = "PRODUCT_REVIEW"
+    }
+
+    @Parcelize
+    data object LocalReminder : WooNotificationType {
+        @IgnoredOnParcel override val trackingValue: String = "LOCAL_REMINDER"
+    }
 
     @Parcelize
     sealed interface BlazeStatusUpdate : WooNotificationType, Parcelable {
         @Parcelize
-        data object BlazeApprovedNote : BlazeStatusUpdate
+        data object BlazeApprovedNote : BlazeStatusUpdate {
+            @IgnoredOnParcel override val trackingValue: String = "blaze_approved_note"
+        }
 
         @Parcelize
-        data object BlazeRejectedNote : BlazeStatusUpdate
+        data object BlazeRejectedNote : BlazeStatusUpdate {
+            @IgnoredOnParcel override val trackingValue: String = "blaze_rejected_note"
+        }
 
         @Parcelize
-        data object BlazeCancelledNote : BlazeStatusUpdate
+        data object BlazeCancelledNote : BlazeStatusUpdate {
+            @IgnoredOnParcel override val trackingValue: String = "blaze_cancelled_note"
+        }
 
         @Parcelize
-        data object BlazePerformedNote : BlazeStatusUpdate
+        data object BlazePerformedNote : BlazeStatusUpdate {
+            @IgnoredOnParcel override val trackingValue: String = "blaze_performed_note"
+        }
     }
 }
 
