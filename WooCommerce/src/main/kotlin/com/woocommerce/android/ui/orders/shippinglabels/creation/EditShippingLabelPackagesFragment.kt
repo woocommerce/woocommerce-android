dc57afc28a8c574fba380af7973924d2f3142559
AnirudhBhat <abhat38@gmail.com>
2024-09-11 09:16:35 +0530

    Revert using fragment to BaseFragment as the former was causing duplicate toolbar issue.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creation/EditShippingLabelPackagesFragment.kt
@@ -17,7 +17,9 @@ import com.woocommerce.android.extensions.navigateBackWithNotice
 import com.woocommerce.android.extensions.navigateBackWithResult
 import com.woocommerce.android.extensions.navigateSafely
 import com.woocommerce.android.extensions.takeIfNotEqualTo
+import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.base.UIMessageResolver
+import com.woocommerce.android.ui.main.AppBarStatus
 import com.woocommerce.android.ui.main.MainActivity.Companion.BackPressListener
 import com.woocommerce.android.ui.orders.shippinglabels.creation.EditShippingLabelPackagesViewModel.OpenHazmatCategorySelector
 import com.woocommerce.android.ui.orders.shippinglabels.creation.EditShippingLabelPackagesViewModel.OpenPackageCreatorEvent
@@ -37,7 +39,7 @@ typealias OnHazmatCategorySelected = (ShippingLabelHazmatCategory) -> Unit
 
 @AndroidEntryPoint
 class EditShippingLabelPackagesFragment :
-    Fragment(R.layout.fragment_edit_shipping_label_packages),
+    BaseFragment(R.layout.fragment_edit_shipping_label_packages),
     BackPressListener {
     companion object {
         const val EDIT_PACKAGES_CLOSED = "edit_packages_closed"
@@ -62,6 +64,9 @@ class EditShippingLabelPackagesFragment :
             viewModel::onURLClicked
         )
     }
+
+    override val activityAppBarStatus: AppBarStatus
+        get() = AppBarStatus.Hidden
 
     private val skeletonView: SkeletonView = SkeletonView()
 
