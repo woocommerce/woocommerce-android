03fa7e79ce37c0f66bb68085da470e5b253e0017
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-09-11 16:56:54 +0200

    Improve notification type API

--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationType.kt
@@ -1,25 +1,44 @@
 package com.woocommerce.android.notifications
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import org.wordpress.android.fluxc.model.notification.NotificationModel
 
-enum class WooNotificationType {
-    NEW_ORDER,
-    PRODUCT_REVIEW,
-    LOCAL_REMINDER,
-    BLAZE_APPROVED_NOTE,
-    BLAZE_REJECTED_NOTE,
-    BLAZE_CANCELLED_NOTE,
-    BLAZE_PERFORMED_NOTE,
+sealed interface WooNotificationType : Parcelable {
+    @Parcelize
+    data object NewOrder : WooNotificationType
+
+    @Parcelize
+    data object ProductReview : WooNotificationType
+
+    @Parcelize
+    data object LocalReminder : WooNotificationType
+
+    @Parcelize
+    sealed interface BlazeStatusUpdate : WooNotificationType, Parcelable {
+        @Parcelize
+        data object BlazeApprovedNote : BlazeStatusUpdate
+
+        @Parcelize
+        data object BlazeRejectedNote : BlazeStatusUpdate
+
+        @Parcelize
+        data object BlazeCancelledNote : BlazeStatusUpdate
+
+        @Parcelize
+        data object BlazePerformedNote : BlazeStatusUpdate
+    }
 }
+
 
 fun NotificationModel.getWooType(): WooNotificationType {
     return when (this.type) {
-        NotificationModel.Kind.STORE_ORDER -> WooNotificationType.NEW_ORDER
-        NotificationModel.Kind.COMMENT -> WooNotificationType.PRODUCT_REVIEW
-        NotificationModel.Kind.BLAZE_APPROVED_NOTE -> WooNotificationType.BLAZE_APPROVED_NOTE
-        NotificationModel.Kind.BLAZE_REJECTED_NOTE -> WooNotificationType.BLAZE_REJECTED_NOTE
-        NotificationModel.Kind.BLAZE_CANCELLED_NOTE -> WooNotificationType.BLAZE_CANCELLED_NOTE
-        NotificationModel.Kind.BLAZE_PERFORMED_NOTE -> WooNotificationType.BLAZE_PERFORMED_NOTE
-        else -> WooNotificationType.LOCAL_REMINDER
+        NotificationModel.Kind.STORE_ORDER -> WooNotificationType.NewOrder
+        NotificationModel.Kind.COMMENT -> WooNotificationType.ProductReview
+        NotificationModel.Kind.BLAZE_APPROVED_NOTE -> WooNotificationType.BlazeStatusUpdate.BlazeApprovedNote
+        NotificationModel.Kind.BLAZE_REJECTED_NOTE -> WooNotificationType.BlazeStatusUpdate.BlazeRejectedNote
+        NotificationModel.Kind.BLAZE_CANCELLED_NOTE -> WooNotificationType.BlazeStatusUpdate.BlazeCancelledNote
+        NotificationModel.Kind.BLAZE_PERFORMED_NOTE -> WooNotificationType.BlazeStatusUpdate.BlazePerformedNote
+        else -> WooNotificationType.LocalReminder
     }
 }
