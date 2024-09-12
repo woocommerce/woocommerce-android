ac57e5e0ecfc26a7fb15d00ae1d8df227e13d0c4
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-12 17:11:00 +0200

    Use `checkDependencies` in lint configuration of app module
    This way, we combine all lint issues of app and library modules into one lint report (wear remains separate). We do this to send sarif report file to GitHub Code Scanning API with data from all modules.

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -2,8 +2,17 @@
 
 echo "--- 🧹 Linting"
 cp gradle.properties-example gradle.properties
-./gradlew lintJalapenoDebug
-lint_exit_code=$?
+./gradlew :WooCommerce:lintJalapenoDebug
+app_lint_exit_code=$?
+
+./gradlew :WooCommerce-Wear:lintJalapenoDebug
+wear_lint_exit_code=$?
+
+if [ $app_lint_exit_code -ne 0 ] || [ $wear_lint_exit_code -ne 0 ]; then
+  lint_exit_code=1
+else
+  lint_exit_code=0
+fi
 
 upload_sarif_to_github 'WooCommerce/build/reports/lint-results-jalapenoDebug.sarif' 'woocommerce' 'woocommerce-android'
 
