28b322dc8f6ec77ec1361b3f7af0aec4b2657747
Wojciech Zięba <wojtek.zieba@automattic.com>
2024-09-13 18:56:14 +0200

    Update .buildkite/commands/lint.sh
    Co-authored-by: Ian Guedes Maia <iangmaia@users.noreply.github.com>

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -8,10 +8,9 @@ app_lint_exit_code=$?
 ./gradlew :WooCommerce-Wear:lintJalapenoDebug
 wear_lint_exit_code=$?
 
+lint_exit_code=0
 if [ $app_lint_exit_code -ne 0 ] || [ $wear_lint_exit_code -ne 0 ]; then
   lint_exit_code=1
-else
-  lint_exit_code=0
 fi
 
 upload_sarif_to_github 'WooCommerce/build/reports/lint-results-jalapenoDebug.sarif' 'woocommerce' 'woocommerce-android'
