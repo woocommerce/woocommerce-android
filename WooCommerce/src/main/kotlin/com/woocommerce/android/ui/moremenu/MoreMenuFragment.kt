769b21b5d7eb3a86ec5af66a78cbcc89343b9f36
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-13 14:12:35 +0200

    temp: add lint violation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/moremenu/MoreMenuFragment.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.moremenu
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -63,6 +64,10 @@ class MoreMenuFragment : TopLevelFragment() {
 
     override fun scrollToTop() {
         return
+    }
+
+    override fun onAttach(activity: Activity) {
+
     }
 
     override fun onCreateView(
