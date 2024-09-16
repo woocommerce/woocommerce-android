3aa2c8f359b785f0610a44050e63910eb1e33107
Rooney <backwardstruck@users.noreply.github.com>
2024-09-16 16:01:58 -0400

    Add mock to test

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsRepositoryTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsRepositoryTest.kt
@@ -1,7 +1,11 @@
 package com.woocommerce.android.ui.woopos.home.totals
 
 import com.woocommerce.android.model.Order
+import com.woocommerce.android.model.Product
 import com.woocommerce.android.ui.orders.creation.OrderCreateEditRepository
+import com.woocommerce.android.ui.products.ProductBackorderStatus
+import com.woocommerce.android.ui.products.ProductStockStatus
+import com.woocommerce.android.ui.products.ProductTaxStatus
 import com.woocommerce.android.ui.woopos.common.data.WooPosGetProductById
 import com.woocommerce.android.util.DateUtils
 import kotlinx.coroutines.test.runTest
@@ -14,6 +18,8 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import java.math.BigDecimal
+import java.util.Date
 
 class WooPosTotalsRepositoryTest {
     private val orderCreateEditRepository: OrderCreateEditRepository = mock()
@@ -49,6 +55,33 @@ class WooPosTotalsRepositoryTest {
             getProductById
         )
         val productIds = listOf(1L, 2L, 3L)
+
+        val product1 = Product(
+            remoteId = 1L, name = "Product 1", price = BigDecimal(10),
+            sku = "SKU1", attributes = emptyList(),
+            parentId = 0L, description = "", shortDescription = "", slug = "", type = "",
+            status = null, catalogVisibility = null, isFeatured = false,
+            stockStatus = ProductStockStatus.InsufficientStock, backorderStatus = ProductBackorderStatus.No,
+            dateCreated = Date(), firstImageUrl = null, totalSales = 0L, reviewsAllowed = false,
+            isVirtual = false, ratingCount = 0, averageRating = 0.0f, permalink = "", externalUrl = "",
+            buttonText = "", salePrice = BigDecimal.ZERO, regularPrice = BigDecimal.ZERO,
+            taxClass = "", isStockManaged = false, stockQuantity = 0.0, shippingClass = "",
+            shippingClassId = 0L, isDownloadable = false, downloads = emptyList(),
+            downloadLimit = 0L, downloadExpiry = 0, purchaseNote = "", numVariations = 0,
+            images = emptyList(), saleEndDateGmt = null, saleStartDateGmt = null,
+            isSoldIndividually = false, taxStatus = ProductTaxStatus.Taxable,
+            isSaleScheduled = false, isPurchasable = false, menuOrder = 0, categories = emptyList(),
+            tags = emptyList(), groupedProductIds = emptyList(), crossSellProductIds = emptyList(),
+            upsellProductIds = emptyList(), variationIds = emptyList(), length = 0f, width = 0f,
+            height = 0f, weight = 0f, subscription = null, isSampleProduct = false, specialStockStatus = null,
+            isConfigurable = false, minAllowedQuantity = null, maxAllowedQuantity = null,
+            bundleMinSize = null, bundleMaxSize = null, groupOfQuantity = null,
+            combineVariationQuantities = null
+        )
+
+        whenever(getProductById(1L)).thenReturn(product1)
+        whenever(getProductById(2L)).thenReturn(product1)
+        whenever(getProductById(3L)).thenReturn(product1)
 
         // WHEN
         repository.createOrderWithProducts(productIds = productIds)
