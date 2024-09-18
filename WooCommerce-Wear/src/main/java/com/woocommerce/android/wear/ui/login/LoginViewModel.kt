14563a5457e50b40c7bdad29ce172ae58b894acf
ThomazFB <thomazfcortez@gmail.com>
2024-09-18 09:39:40 -0300

    Update LoginViewModel to move away from using a internal NavController reference

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/ui/login/LoginViewModel.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/ui/login/LoginViewModel.kt
@@ -3,11 +3,9 @@ package com.woocommerce.android.wear.ui.login
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
-import androidx.navigation.NavHostController
 import com.woocommerce.android.wear.analytics.AnalyticsTracker
 import com.woocommerce.android.wear.extensions.getStateFlow
-import com.woocommerce.android.wear.ui.NavRoutes
-import com.woocommerce.android.wear.ui.NavRoutes.MY_STORE
+import com.woocommerce.android.wear.ui.login.FetchSiteData.LoginRequestState
 import com.woocommerce.android.wear.ui.login.FetchSiteData.LoginRequestState.Logged
 import com.woocommerce.android.wear.ui.login.FetchSiteData.LoginRequestState.Timeout
 import com.woocommerce.android.wear.ui.login.FetchSiteData.LoginRequestState.Waiting
@@ -15,19 +13,16 @@ import com.woocommerce.android.wear.viewmodel.WearViewModel
 import com.woocommerce.commons.WearAnalyticsEvent.WATCH_STORE_DATA_FAILED
 import com.woocommerce.commons.WearAnalyticsEvent.WATCH_STORE_DATA_REQUESTED
 import com.woocommerce.commons.WearAnalyticsEvent.WATCH_STORE_DATA_SUCCEEDED
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedFactory
-import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 
-@HiltViewModel(assistedFactory = LoginViewModel.Factory::class)
-class LoginViewModel @AssistedInject constructor(
+@HiltViewModel
+class LoginViewModel @Inject constructor(
     private val fetchSiteData: FetchSiteData,
     private val analyticsTracker: AnalyticsTracker,
-    @Assisted private val navController: NavHostController,
     savedState: SavedStateHandle
 ) : WearViewModel() {
     @Suppress("ForbiddenComment")
@@ -45,21 +40,13 @@ class LoginViewModel @AssistedInject constructor(
 
     private fun requestSiteData() {
         analyticsTracker.track(WATCH_STORE_DATA_REQUESTED)
-        _viewState.update { it.copy(isLoading = true) }
         launch {
             fetchSiteData().collect { loginState ->
+                _viewState.update { it.copy(loginState = loginState) }
                 when (loginState) {
-                    Logged -> {
-                        analyticsTracker.track(WATCH_STORE_DATA_SUCCEEDED)
-                        navController.navigate(MY_STORE.route) {
-                            popUpTo(NavRoutes.LOGIN.route) { inclusive = true }
-                        }
-                    }
-                    Timeout -> {
-                        analyticsTracker.track(WATCH_STORE_DATA_FAILED)
-                        _viewState.update { it.copy(isLoading = false) }
-                    }
-                    Waiting -> _viewState.update { it.copy(isLoading = true) }
+                    Logged -> analyticsTracker.track(WATCH_STORE_DATA_SUCCEEDED)
+                    Timeout -> analyticsTracker.track(WATCH_STORE_DATA_FAILED)
+                    else -> { /* Do nothing */ }
                 }
             }
         }
@@ -67,11 +54,6 @@ class LoginViewModel @AssistedInject constructor(
 
     @Parcelize
     data class ViewState(
-        val isLoading: Boolean = true
+        val loginState: LoginRequestState = Waiting,
     ) : Parcelable
-
-    @AssistedFactory
-    interface Factory {
-        fun create(navController: NavHostController): LoginViewModel
-    }
 }
