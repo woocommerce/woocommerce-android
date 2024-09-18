3fd1d72268f5a9d4e9cf49eed72c97143592ed8a
samiuelson <samiuelson@gmail.com>
2024-09-18 10:45:48 +0200

    Satisfy detekt's complaints

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/requests/SupportRequestFormActivity.kt
@@ -3,7 +3,6 @@ package com.woocommerce.android.support.requests
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.viewModels
