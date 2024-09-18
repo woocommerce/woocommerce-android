91e6636dfc7b0703482fd7df0e9781313d291170
ThomazFB <thomazfcortez@gmail.com>
2024-09-18 09:39:47 -0300

    Update LoginViewModelTest

--- WooCommerce-Wear/src/test/java/com/woocommerce/android/wear/ui/login/LoginViewModelTest.kt
+++ WooCommerce-Wear/src/test/java/com/woocommerce/android/wear/ui/login/LoginViewModelTest.kt
@@ -1,19 +1,15 @@
 package com.woocommerce.android.wear.ui.login
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.navigation.NavHostController
 import com.woocommerce.android.BaseUnitTest
-import com.woocommerce.android.wear.ui.NavRoutes
 import com.woocommerce.android.wear.ui.login.FetchSiteData.LoginRequestState.Logged
 import com.woocommerce.android.wear.ui.login.FetchSiteData.LoginRequestState.Timeout
+import com.woocommerce.android.wear.ui.login.FetchSiteData.LoginRequestState.Waiting
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
@@ -21,54 +17,52 @@ class LoginViewModelTest : BaseUnitTest() {
 
     private lateinit var sut: LoginViewModel
     private val fetchSiteData: FetchSiteData = mock()
-    private val navController: NavHostController = mock()
 
     @Test
-    fun `when user is logged in, navigate to MY_STORE`() = testBlocking {
+    fun `when login Timeouts, then view state is Timeout`() = testBlocking {
         // Given
+        var loginState: FetchSiteData.LoginRequestState? = null
+        whenever(fetchSiteData.invoke()).thenReturn(flowOf(Timeout))
+        createSut()
+        sut.viewState.observeForever { loginState = it.loginState }
+
+        // Then
+        assertThat(loginState).isNotNull()
+        assertThat(loginState).isEqualTo(Timeout)
+    }
+
+    @Test
+    fun `when login is Logged, then view state is Logged`() = testBlocking {
+        // Given
+        var loginState: FetchSiteData.LoginRequestState? = null
         whenever(fetchSiteData.invoke()).thenReturn(flowOf(Logged))
-
-        // When
         createSut()
+        sut.viewState.observeForever { loginState = it.loginState }
 
         // Then
-        verify(navController).navigate(
-            eq(NavRoutes.MY_STORE.route),
-            builder = any()
-        )
+        assertThat(loginState).isNotNull()
+        assertThat(loginState).isEqualTo(Logged)
     }
 
     @Test
-    fun `when user is not logged in, loading is stopped`() = testBlocking {
+    fun `when login is Waiting, then view state is Waiting`() = testBlocking {
         // Given
-        var isLoading: Boolean? = null
-        whenever(fetchSiteData.invoke()).thenReturn(flowOf(Timeout))
+        var loginState: FetchSiteData.LoginRequestState? = null
+        whenever(fetchSiteData.invoke()).thenReturn(flowOf(Waiting))
         createSut()
-        sut.viewState.observeForever { isLoading = it.isLoading }
+        sut.viewState.observeForever { loginState = it.loginState }
 
         // Then
-        assertThat(isLoading).isNotNull()
-        assertThat(isLoading).isFalse
+        assertThat(loginState).isNotNull()
+        assertThat(loginState).isEqualTo(Waiting)
     }
 
-    @Test
-    fun `when login is waiting, then view state is loading`() = testBlocking {
-        // Given
-        var isLoading: Boolean? = null
-        whenever(fetchSiteData.invoke()).thenReturn(flowOf(Timeout))
-        createSut()
-        sut.viewState.observeForever { isLoading = it.isLoading }
 
-        // Then
-        assertThat(isLoading).isNotNull()
-        assertThat(isLoading).isFalse
-    }
 
     private fun createSut() {
         sut = LoginViewModel(
             fetchSiteData,
             mock(),
-            navController,
             SavedStateHandle()
         )
     }
