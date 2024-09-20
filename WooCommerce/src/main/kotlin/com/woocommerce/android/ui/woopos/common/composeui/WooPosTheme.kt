df96ac63d31a5d3e6949e5d42417e95d1f6dd953
Rooney <backwardstruck@users.noreply.github.com>
2024-09-20 15:04:24 -0400

    Space these colors out a bit

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/WooPosTheme.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/WooPosTheme.kt
@@ -70,11 +70,13 @@ private object WooPosColors {
     val Purple80 = Color(0xFF3C2861)
     val Purple90 = Color(0xFF271B3D)
     val PurpleDarkSecondary = Color(0x99EBEBF5)
+
     val Pink10 = Color(0xFFED9BB8)
     val Pink30 = Color(0xFFEB6594)
     val Pink50 = Color(0xFFC9356E)
     val Pink70 = Color(0xFF880E4F)
     val Pink90 = Color(0xFF5C0935)
+
     val Red5 = Color(0xFFFACFD2)
     val Red10 = Color(0xFFFFA6AB)
     val Red20 = Color(0xFFFF8085)
@@ -82,30 +84,36 @@ private object WooPosColors {
     val Red50 = Color(0xFFD63638)
     val Red60 = Color(0xFFB32D2E)
     val Red70 = Color(0xFF8A2424)
+
     val Blue5 = Color(0xFFBBE0FA)
     val Blue30 = Color(0xFF5198D9)
     val Blue40 = Color(0xFF1689DB)
     val Blue50 = Color(0xFF2271B1)
+
     val Orange5 = Color(0xFFF7DCC6)
     val Orange10 = Color(0xFFFFBF86)
     val Orange30 = Color(0xFFE68B28)
     val Orange50 = Color(0xFFB26200)
     val Orange70 = Color(0xFF351F04)
+
     val Yellow10 = Color(0xFFF2CF75)
     val Yellow20 = Color(0xFFF0C443)
     val Yellow30 = Color(0xFFDBAE17)
     val Yellow50 = Color(0xFF907300)
     val Yellow70 = Color(0xFF5C4B00)
     val Celadon0 = Color(0xFFECF7F4)
+
     val Celadon5 = Color(0xFFA7E8D4)
     val Celadon10 = Color(0xFF65D9B9)
     val Celadon20 = Color(0xFF2FC39E)
     val Celadon40 = Color(0xFF009172)
+
     val Green0 = Color(0xFFEBF7F1)
     val Green5 = Color(0xFFA4F5C8)
     val Green10 = Color(0xFF59E38F)
     val Green20 = Color(0xFF1ED15A)
     val Green50 = Color(0xFF008A20)
+
     val White = Color(0xFFFFFFFF)
     val WhiteAlpha005 = Color(0x0DFFFFFF)
     val WhiteAlpha008 = Color(0x14FFFFFF)
@@ -114,6 +122,7 @@ private object WooPosColors {
     val WhiteAlpha038 = Color(0x61FFFFFF)
     val WhiteAlpha060 = Color(0x99FFFFFF)
     val WhiteAlpha087 = Color(0xDEFFFFFF)
+
     val Gray0 = Color(0xFFF6F7F7)
     val Gray5 = Color(0xFFDCDCDE)
     val Gray6 = Color(0xFFF2F2F7)
@@ -125,6 +134,7 @@ private object WooPosColors {
     val Gray80Alpha012 = Color(0x1F2C3338)
     val Gray80Alpha030 = Color(0x4D3C3C43)
     val Gray900 = Color(0xFFF7F7F7)
+
     val Black = Color(0xFF000000)
     val Black90 = Color(0xFF121212)
     val Black90Alpha004 = Color(0x0A000000)
