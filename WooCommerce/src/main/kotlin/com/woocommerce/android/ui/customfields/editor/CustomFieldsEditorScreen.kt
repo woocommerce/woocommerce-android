cd3d5e0ae5fce05fec8f25641a74e4ada2236b9b
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-09-23 11:47:37 +0100

    Handle accessibility

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/customfields/editor/CustomFieldsEditorScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/customfields/editor/CustomFieldsEditorScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Colors
 import androidx.compose.material.LocalContentColor
@@ -40,6 +41,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
@@ -194,8 +198,23 @@ private fun Toggle(
     onToggle: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val contentDescription = stringResource(R.string.custom_fields_editor_toggle_accessibility_description)
+    val state = if (useHtmlEditor) {
+        stringResource(R.string.custom_fields_editor_html_toggle)
+    } else {
+        stringResource(R.string.custom_fields_editor_text_toggle)
+    }
+
     Box(
         modifier = modifier
+            .toggleable(
+                value = useHtmlEditor,
+                onValueChange = onToggle
+            )
+            .clearAndSetSemantics {
+                this.contentDescription = contentDescription
+                this.stateDescription = state
+            }
             .background(
                 MaterialTheme.colors.toggleBackgroundColor,
                 MaterialTheme.shapes.medium
