7cb6e3b6cc1e394f751726316eccd816480ffffd
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-09-25 12:02:32 +0100

    Show a pencil icon on the icon

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/OrderDetailFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/OrderDetailFragment.kt
@@ -426,6 +426,13 @@ class OrderDetailFragment :
             }
             new.isCustomFieldsButtonShown?.takeIfNotEqualTo(old?.isCustomFieldsButtonShown) {
                 binding.customFieldsCard.root.isVisible = it
+                if (it && !FeatureFlag.CUSTOM_FIELDS.isEnabled()) {
+                    // When the feature flag is disabled, keep the original text and hide the icon
+                    with(binding.customFieldsCard.customFieldsButton) {
+                        icon = null
+                        text = getString(R.string.orderdetail_view_custom_fields)
+                    }
+                }
             }
             new.isAIThankYouNoteButtonShown.takeIfNotEqualTo(old?.isAIThankYouNoteButtonShown) {
                 binding.orderDetailsAICard.isVisible = it
