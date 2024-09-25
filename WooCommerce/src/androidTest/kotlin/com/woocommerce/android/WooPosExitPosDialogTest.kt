5d3281fc66a6c06db1430f6cab9a96f39f1a479e
AnirudhBhat <abhat38@gmail.com>
2024-09-25 14:49:21 +0530

    Add test to verify exit confirmation dialog is displayed properly.

---
+++ WooCommerce/src/androidTest/kotlin/com/woocommerce/android/WooPosExitPosDialogTest.kt
@@ -0,0 +1,102 @@
+@file:Suppress("DEPRECATION")
+
+package com.woocommerce.android
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.woocommerce.android.e2e.helpers.InitializationRule
+import com.woocommerce.android.e2e.helpers.TestBase
+import com.woocommerce.android.e2e.rules.RetryTestRule
+import com.woocommerce.android.e2e.screens.TabNavComponent
+import com.woocommerce.android.e2e.screens.login.WelcomeScreen
+import com.woocommerce.android.ui.login.LoginActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class WooPosExitPosDialogTest : TestBase() {
+    @get:Rule(order = 1)
+    val rule = HiltAndroidRule(this)
+
+    @get:Rule(order = 2)
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @get:Rule(order = 3)
+    val initRule = InitializationRule()
+
+    @get:Rule(order = 4)
+    var activityRule = ActivityTestRule(LoginActivity::class.java)
+
+    @get:Rule(order = 5)
+    var retryTestRule = RetryTestRule()
+
+    @Inject
+    lateinit var dataStore: DataStore<Preferences>
+
+    @Before
+    fun setUp() = runTest {
+        rule.inject()
+        dataStore.edit { it.clear() }
+        WelcomeScreen
+            .skipCarouselIfNeeded()
+            .selectLogin()
+            .proceedWith(BuildConfig.SCREENSHOTS_URL)
+            .proceedWith(BuildConfig.SCREENSHOTS_USERNAME)
+            .proceedWith(BuildConfig.SCREENSHOTS_PASSWORD)
+
+        TabNavComponent()
+            .gotoMoreMenuScreen()
+            .openPOSScreen(composeTestRule)
+    }
+
+    @Test
+    fun testExitPOSDialogIsDisplayedWhenExitButtonClicked() = runTest {
+        composeTestRule.waitUntil(5000) {
+            try {
+                composeTestRule.onNodeWithTag("product_list")
+                    .assertExists()
+                    .assertIsDisplayed()
+                true
+            } catch (e: AssertionError) {
+                false
+            }
+        }
+
+        composeTestRule.waitUntil(5000) {
+            composeTestRule.onNodeWithTag(
+                "woo_pos_menu_button"
+            ).performClick()
+            true
+        }
+
+        composeTestRule.waitUntil(5000) {
+            composeTestRule.onNodeWithTag(
+                "woo_pos_popup_menu_${R.string.woopos_exit_confirmation_title}",
+                useUnmergedTree = true
+            ).performClick()
+            true
+        }
+
+        composeTestRule.waitUntil(5000) {
+            composeTestRule.onNodeWithTag("woo_pos_exit_confirmation_dialog")
+                .assertExists()
+                .assertIsDisplayed()
+            true
+        }
+    }
+}
