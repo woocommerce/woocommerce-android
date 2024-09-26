4c81f4079a03063bbe3ebb46ca1c41f6d9cb61a8
Andrey <akidinov@gmail.com>
2024-09-26 15:47:35 +0200

    Removed showing the AI banner on the dashboard

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/banner/AIProductBannerDialogViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ai/banner/AIProductBannerDialogViewModel.kt
@@ -1,16 +1,25 @@
 package com.woocommerce.android.ui.products.ai.banner
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.AppPrefsWrapper
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AIProductBannerDialogViewModel @Inject constructor(
-    savedState: SavedStateHandle
+    savedState: SavedStateHandle,
+    appPrefsWrapper: AppPrefsWrapper
 ) : ScopedViewModel(savedState) {
+    init {
+        launch {
+            appPrefsWrapper.wasAIProductDescriptionPromoDialogShown = true
+        }
+    }
+
     fun onTryNowButtonClicked() {
         triggerEvent(TryAIProductDescriptionGeneration)
     }
