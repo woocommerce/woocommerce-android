4f3a92e5f54b06b7fb4cc26d68c1f4a145d812cc
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-09-30 17:14:17 +0200

    Add empty BlazeCampaignObjectiveViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/objective/BlazeCampaignObjectiveFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/objective/BlazeCampaignObjectiveFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.Text
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.composeView
 import com.woocommerce.android.ui.main.AppBarStatus
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BlazeCampaignObjectiveFragment : BaseFragment() {
     override val activityAppBarStatus: AppBarStatus
         get() = AppBarStatus.Hidden
+
+    val viewModel: BlazeCampaignObjectiveViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return composeView {
@@ -26,10 +31,10 @@ class BlazeCampaignObjectiveFragment : BaseFragment() {
     }
 
     private fun handleEvents() {
-//        viewModel.event.observe(viewLifecycleOwner) { event ->
-//            when (event) {
-//                is MultiLiveEvent.Event.Exit -> findNavController().navigateUp()
-//            }
-//        }
+        viewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is MultiLiveEvent.Event.Exit -> findNavController().navigateUp()
+            }
+        }
     }
 }
