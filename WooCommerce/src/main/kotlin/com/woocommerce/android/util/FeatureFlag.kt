87995219f692d0aaf64a1cfd82b4f4000e862b52
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-10-01 18:47:19 +0200

    Add feature flag for objective section

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -16,7 +16,8 @@ enum class FeatureFlag {
     SHOW_INBOX_CTA,
     ENDLESS_CAMPAIGNS_SUPPORT,
     CUSTOM_FIELDS,
-    REVAMP_WOO_SHIPPING;
+    REVAMP_WOO_SHIPPING,
+    OBJECTIVE_SECTION;
 
     fun isEnabled(context: Context? = null): Boolean {
         return when (this) {
@@ -28,7 +29,8 @@ enum class FeatureFlag {
             BETTER_CUSTOMER_SEARCH_M2,
             ORDER_CREATION_AUTO_TAX_RATE,
             CUSTOM_FIELDS,
-            REVAMP_WOO_SHIPPING -> PackageUtils.isDebugBuild()
+            REVAMP_WOO_SHIPPING,
+            OBJECTIVE_SECTION -> PackageUtils.isDebugBuild()
 
             NEW_SHIPPING_SUPPORT,
             INBOX,
