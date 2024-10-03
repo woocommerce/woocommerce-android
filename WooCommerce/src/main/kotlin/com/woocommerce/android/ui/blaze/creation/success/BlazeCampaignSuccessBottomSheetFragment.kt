bbcd326b20a7c244d6560a06ccea114253801b3a
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-10-03 14:48:36 +0200

    Track tapping on positive or negative feedback

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/success/BlazeCampaignSuccessBottomSheetFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/success/BlazeCampaignSuccessBottomSheetFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.woocommerce.android.AppUrls.BLAZE_CAMPAIGN_CREATION_SURVEY_URL_I1
+import com.woocommerce.android.analytics.AnalyticsEvent
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.ui.compose.composeView
 import com.woocommerce.android.util.ChromeCustomTabUtils
 import com.woocommerce.android.widgets.WCBottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class BlazeCampaignSuccessBottomSheetFragment : WCBottomSheetDialogFragment() {
+    @Inject
+    lateinit var analyticsTracker: AnalyticsTrackerWrapper
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return composeView {
             BlazeCampaignSuccessBottomSheet(
@@ -26,6 +33,13 @@ class BlazeCampaignSuccessBottomSheetFragment : WCBottomSheetDialogFragment() {
     }
 
     private fun onFeedbackRequestTapped(isPositive: Boolean) {
+        analyticsTracker.track(
+            stat = AnalyticsEvent.BLAZE_CAMPAIGN_CREATION_FEEDBACK,
+            properties = mapOf(
+                AnalyticsTracker.KEY_SOURCE to "satisfied",
+                AnalyticsTracker.KEY_IS_USEFUL to isPositive
+            )
+        )
         ChromeCustomTabUtils.launchUrl(requireContext(), BLAZE_CAMPAIGN_CREATION_SURVEY_URL_I1)
     }
 }
