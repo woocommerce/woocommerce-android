809789e957e4c8d10de8ae2be5b7d89be7a1df06
ThomazFB <thomazfcortez@gmail.com>
2024-10-03 06:13:00 -0300

    Add ViewState and PageTab types into the WooShippingLabelsPackageCreationViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creationV2/packages/WooShippingLabelsPackageCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creationV2/packages/WooShippingLabelsPackageCreationViewModel.kt
@@ -1,11 +1,42 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creationV2.packages
 
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.R
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.parcelize.Parcelize
 
 @HiltViewModel
 class WooShippingLabelsPackageCreationViewModel @Inject constructor(
     savedState: SavedStateHandle
-) : ScopedViewModel(savedState)
+) : ScopedViewModel(savedState) {
+
+    private val _viewState = savedState.getStateFlow(
+        scope = viewModelScope,
+        initialValue = ViewState(createPageTabs())
+    )
+    val viewState = _viewState.asLiveData()
+
+    private fun createPageTabs(): List<PageTab> {
+        return listOf(
+            PageTab(R.string.woo_shipping_labels_package_creation_tab_custom),
+            PageTab(R.string.woo_shipping_labels_package_creation_tab_carrier),
+            PageTab(R.string.woo_shipping_labels_package_creation_tab_saved),
+        )
+    }
+
+    @Parcelize
+    data class ViewState(
+        val pageTabs: List<PageTab> = emptyList()
+    ) : Parcelable
+
+    @Parcelize
+    data class PageTab(
+        val titleResource: Int
+    ) : Parcelable
+}
