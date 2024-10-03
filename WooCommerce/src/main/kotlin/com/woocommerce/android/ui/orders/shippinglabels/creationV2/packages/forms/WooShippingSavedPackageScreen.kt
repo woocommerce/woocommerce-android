c3308a195ee1fe002ad74282fd0d03d67566ce57
ThomazFB <thomazfcortez@gmail.com>
2024-10-03 08:59:22 -0300

    Introduce all three package creation screens

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creationV2/packages/forms/WooShippingSavedPackageScreen.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.orders.shippinglabels.creationV2.packages.forms
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WooShippingSavedPackageScreen() {
+
+}
