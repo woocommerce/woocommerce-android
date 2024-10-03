475d3ad527dc672be4fd5380edccfa6000fa39b7
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-10-03 11:55:24 +0200

    Adds new feedback module at the bottom of Blaze success sheet

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/success/BlazeCampaignSuccessBottomSheet.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/success/BlazeCampaignSuccessBottomSheet.kt
@@ -1,23 +1,32 @@
 package com.woocommerce.android.ui.blaze.creation.success
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
 import com.woocommerce.android.R.dimen
 import com.woocommerce.android.R.drawable
 import com.woocommerce.android.R.string
@@ -28,6 +37,7 @@ import com.woocommerce.android.ui.compose.preview.LightDarkThemePreviews
 @Composable
 fun BlazeCampaignSuccessBottomSheet(
     onDoneTapped: () -> Unit,
+    onRequestFeedback: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Surface(
@@ -71,6 +81,57 @@ fun BlazeCampaignSuccessBottomSheet(
                 Text(text = stringResource(id = string.blaze_campaign_created_success_done_button))
             }
             Spacer(modifier = Modifier.height(16.dp))
+            RequestFeedback(
+                onFeedbackReceived = onRequestFeedback,
+                feedbackRequestText = R.string.blaze_campaign_created_success_feedback_request,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun RequestFeedback(
+    onFeedbackReceived: (Boolean) -> Unit,
+    @StringRes feedbackRequestText: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .background(
+                color = colorResource(id = R.color.ai_product_suggestion_box_background),
+                shape = RoundedCornerShape(dimensionResource(id = R.dimen.minor_100))
+            )
+            .padding(dimensionResource(id = R.dimen.major_100))
+    ) {
+        Text(
+            text = stringResource(id = feedbackRequestText),
+            color = colorResource(id = R.color.color_on_surface_medium),
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(
+            onClick = { onFeedbackReceived(true) },
+            modifier = Modifier.size(dimensionResource(id = R.dimen.major_200))
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_thumb_up),
+                contentDescription = stringResource(id = R.string.ai_feedback_form_positive_button),
+                tint = colorResource(id = R.color.color_on_surface_medium)
+            )
+        }
+
+        Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.minor_100)))
+
+        IconButton(
+            onClick = { onFeedbackReceived(false) },
+            modifier = Modifier.size(dimensionResource(id = R.dimen.major_200))
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_thumb_down),
+                contentDescription = stringResource(id = R.string.ai_feedback_form_negative_button),
+                tint = colorResource(id = R.color.color_on_surface_medium)
+            )
         }
     }
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/success/BlazeCampaignSuccessBottomSheetFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/blaze/creation/success/BlazeCampaignSuccessBottomSheetFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.woocommerce.android.AppUrls.BLAZE_CAMPAIGN_CREATION_SURVEY_URL_I1
 import com.woocommerce.android.ui.compose.composeView
+import com.woocommerce.android.util.ChromeCustomTabUtils
 import com.woocommerce.android.widgets.WCBottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,11 +14,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class BlazeCampaignSuccessBottomSheetFragment : WCBottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return composeView {
-            BlazeCampaignSuccessBottomSheet(::onDoneClicked)
+            BlazeCampaignSuccessBottomSheet(
+                ::onDoneClicked,
+                ::onFeedbackRequestTapped
+            )
         }
     }
 
     private fun onDoneClicked() {
         dismiss()
+    }
+
+    private fun onFeedbackRequestTapped(isPositive: Boolean) {
+        ChromeCustomTabUtils.launchUrl(requireContext(), BLAZE_CAMPAIGN_CREATION_SURVEY_URL_I1)
     }
 }
