1ebc893a81bdf155ae179b20b3e17085c1cde15d
ThomazFB <thomazfcortez@gmail.com>
2024-10-03 11:28:54 -0300

    Add a Pager bound to the TabRow

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creationV2/packages/WooShippingLabelsPackageCreationScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creationV2/packages/WooShippingLabelsPackageCreationScreen.kt
@@ -1,9 +1,11 @@
 package com.woocommerce.android.ui.orders.shippinglabels.creationV2.packages
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
@@ -12,6 +14,7 @@ import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
@@ -38,12 +41,21 @@ fun WooShippingLabelsPackageCreationScreen(
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun WooShippingLabelsPackageCreationScreen(
     modifier: Modifier = Modifier,
     tabs: List<PageTab>
 ) {
     var tabIndex by remember { mutableIntStateOf(0) }
+    val pagerState = rememberPagerState { tabs.size }
+    LaunchedEffect(key1 = tabIndex) {
+        pagerState.animateScrollToPage(tabIndex)
+    }
+    LaunchedEffect(key1 = pagerState.currentPage, pagerState.isScrollInProgress) {
+        if (!pagerState.isScrollInProgress)
+            tabIndex = pagerState.currentPage
+    }
 
     Scaffold(
         topBar = {
@@ -58,14 +70,15 @@ fun WooShippingLabelsPackageCreationScreen(
             }
         },
         content = { paddingValues ->
-            Column(
+            HorizontalPager(
+                state = pagerState,
                 modifier = modifier
                     .background(MaterialTheme.colors.surface)
                     .padding(paddingValues)
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
-            ) {
-                when (tabs[tabIndex].type) {
+            ) { currentPageIndex ->
+                when (tabs[currentPageIndex].type) {
                     CUSTOM -> WooShippingCustomPackageCreationScreen()
                     CARRIER -> WooShippingCarrierPackageScreen()
                     SAVED -> WooShippingSavedPackageScreen()
