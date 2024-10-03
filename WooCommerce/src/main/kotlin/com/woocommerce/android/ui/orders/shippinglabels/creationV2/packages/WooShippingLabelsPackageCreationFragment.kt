9bd36dc6a3c6238e0b50e4c2d709b40b4f9ce46b
ThomazFB <thomazfcortez@gmail.com>
2024-10-03 05:55:20 -0300

    Define WooShippingLabelsPackageCreationFragment

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/shippinglabels/creationV2/packages/WooShippingLabelsPackageCreationFragment.kt
@@ -0,0 +1,28 @@
+package com.woocommerce.android.ui.orders.shippinglabels.creationV2.packages
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+import androidx.fragment.app.viewModels
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WooShippingLabelsPackageCreationFragment : BaseFragment() {
+    val viewModel: WooShippingLabelsPackageCreationViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WooThemeWithBackground {
+                    WooShippingLabelsPackageCreationScreen(viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
