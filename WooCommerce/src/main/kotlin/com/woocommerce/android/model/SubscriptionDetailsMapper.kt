9fa672a21d03945f68c6faddaf5bb7b032a57100
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-10-08 11:31:05 +0100

    Introduce an overload to the subscription details mapper

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/SubscriptionDetailsMapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/SubscriptionDetailsMapper.kt
@@ -1,77 +1,73 @@
 package com.woocommerce.android.model
 
 import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonElement
-import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import org.wordpress.android.fluxc.model.WCProductModel.SubscriptionMetadataKeys
 import org.wordpress.android.fluxc.model.metadata.WCMetaData
+import org.wordpress.android.fluxc.model.metadata.get
 
 object SubscriptionDetailsMapper {
     private val gson by lazy { Gson() }
-    fun toAppModel(metadata: String): SubscriptionDetails? {
-        val jsonArray = gson.fromJson(metadata, JsonArray::class.java) ?: return null
-
-        val subscriptionInformation = jsonArray
-            .mapNotNull { it as? JsonObject }
-            .filter { jsonObject -> jsonObject[WCMetaData.KEY].asString in SubscriptionMetadataKeys.ALL_KEYS }
-            .associate { jsonObject ->
-                jsonObject[WCMetaData.KEY].asString to jsonObject[WCMetaData.VALUE]
-            }
-
-        return if (subscriptionInformation.isNotEmpty()) {
-            val price = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_PRICE]?.asString
-                ?.toBigDecimalOrNull()
-
-            val periodString = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_PERIOD]?.asString ?: ""
-            val period = SubscriptionPeriod.fromValue(periodString)
-
-            val periodIntervalString = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_PERIOD_INTERVAL]
-                ?.asString ?: ""
-            val periodInterval = periodIntervalString.toIntOrNull() ?: 0
-
-            val lengthInt = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_LENGTH]?.asString
-                ?.toIntOrNull()
-            val length = if (lengthInt != null && lengthInt > 0) lengthInt else null
-
-            val signUpFee = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_SIGN_UP_FEE]?.asString
-                ?.toBigDecimalOrNull()
-
-            val trialPeriodString = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_TRIAL_PERIOD]
-                ?.asString
-            val trialPeriod = trialPeriodString?.let { SubscriptionPeriod.fromValue(trialPeriodString) }
-
-            val trialLengthInt = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_TRIAL_LENGTH]
-                ?.asString?.toIntOrNull()
-            val trialLength = if (trialLengthInt != null && trialLengthInt > 0) trialLengthInt else null
-
-            val oneTimeShipping = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_ONE_TIME_SHIPPING]
-                ?.asString == "yes"
-
-            val paymentsSyncDate = subscriptionInformation[SubscriptionMetadataKeys.SUBSCRIPTION_PAYMENT_SYNC_DATE]
-                ?.extractPaymentsSyncDate()
-
-            SubscriptionDetails(
-                price = price,
-                period = period,
-                periodInterval = periodInterval,
-                length = length,
-                signUpFee = signUpFee,
-                trialPeriod = trialPeriod,
-                trialLength = trialLength,
-                oneTimeShipping = oneTimeShipping,
-                paymentsSyncDate = paymentsSyncDate
-            )
-        } else {
-            null
+    fun toAppModel(metadata: List<WCMetaData>): SubscriptionDetails? {
+        if (metadata.none { it.key in SubscriptionMetadataKeys.ALL_KEYS }) {
+            return null
         }
+
+        val price = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_PRICE]?.valueAsString?.toBigDecimalOrNull()
+
+        val periodString = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_PERIOD]?.valueAsString ?: ""
+        val period = SubscriptionPeriod.fromValue(periodString)
+
+        val periodIntervalString = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_PERIOD_INTERVAL]
+            ?.valueAsString ?: ""
+        val periodInterval = periodIntervalString.toIntOrNull() ?: 0
+
+        val lengthInt = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_LENGTH]?.valueAsString
+            ?.toIntOrNull()
+        val length = if (lengthInt != null && lengthInt > 0) lengthInt else null
+
+        val signUpFee = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_SIGN_UP_FEE]?.valueAsString
+            ?.toBigDecimalOrNull()
+
+        val trialPeriodString = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_TRIAL_PERIOD]
+            ?.valueAsString
+        val trialPeriod = trialPeriodString?.let { SubscriptionPeriod.fromValue(trialPeriodString) }
+
+        val trialLengthInt = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_TRIAL_LENGTH]
+            ?.valueAsString?.toIntOrNull()
+        val trialLength = if (trialLengthInt != null && trialLengthInt > 0) trialLengthInt else null
+
+        val oneTimeShipping = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_ONE_TIME_SHIPPING]
+            ?.valueAsString == "yes"
+
+        val paymentsSyncDate = metadata[SubscriptionMetadataKeys.SUBSCRIPTION_PAYMENT_SYNC_DATE]
+            ?.extractPaymentsSyncDate()
+
+        return SubscriptionDetails(
+            price = price,
+            period = period,
+            periodInterval = periodInterval,
+            length = length,
+            signUpFee = signUpFee,
+            trialPeriod = trialPeriod,
+            trialLength = trialLength,
+            oneTimeShipping = oneTimeShipping,
+            paymentsSyncDate = paymentsSyncDate
+        )
     }
 
-    private fun JsonElement.extractPaymentsSyncDate(): SubscriptionPaymentSyncDate? {
-        return when {
-            isJsonObject -> asJsonObject.let {
-                val day = it["day"].asInt
-                val month = it["month"].asInt
+    fun toAppModel(metadata: String): SubscriptionDetails? {
+        val metadataList = gson.fromJson(metadata, Array<WCMetaData>::class.java)?.toList() ?: return null
+
+        return toAppModel(metadataList)
+    }
+
+    private fun WCMetaData.extractPaymentsSyncDate(): SubscriptionPaymentSyncDate? {
+        return when(isJson) {
+            true -> value.stringValue.let {
+                val jsonObject = JsonParser.parseString(it).asJsonObject
+                val day = jsonObject["day"].asInt
+                val month = jsonObject["month"].asInt
                 if (day == 0) {
                     SubscriptionPaymentSyncDate.None
                 } else {
@@ -79,7 +75,7 @@ object SubscriptionDetailsMapper {
                 }
             }
 
-            else -> asString?.toIntOrNull()?.let { day ->
+            false -> valueAsString.toIntOrNull()?.let { day ->
                 if (day == 0) {
                     SubscriptionPaymentSyncDate.None
                 } else {
