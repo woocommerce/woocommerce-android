a2b550b20895ad36966cd77e24a58cb1cedf66fb
Alejo <alejandro.torres.veiga@automattic.com>
2024-11-11 22:40:35 -0600

    add toShippingLineDetails

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/ShippingLineDetails.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/shipping/ShippingLineDetails.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.orders.creation.shipping
 
+import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.ShippingMethod
 import java.math.BigDecimal
 
@@ -9,3 +10,29 @@ data class ShippingLineDetails(
     val amount: BigDecimal,
     val name: String
 )
+
+fun List<Order.ShippingLine>.toShippingLineDetails(
+    shippingMethods: List<ShippingMethod>,
+): List<ShippingLineDetails>? {
+    if (this.isEmpty()) {
+        return null
+    }
+    val filteredShippingLines = this.filter { line -> line.methodId != null }
+    val shippingMethodsMap = shippingMethods.associateBy { it.id }
+    return filteredShippingLines.map { shippingLine ->
+        val method = shippingLine.methodId?.let {
+            if (it == " ") {
+                shippingMethodsMap[ShippingMethodsRepository.NA_ID]
+            } else {
+                shippingMethodsMap[it]
+            }
+        }
+
+        ShippingLineDetails(
+            id = shippingLine.itemId,
+            name = shippingLine.methodTitle,
+            shippingMethod = method,
+            amount = shippingLine.total
+        )
+    }
+}
