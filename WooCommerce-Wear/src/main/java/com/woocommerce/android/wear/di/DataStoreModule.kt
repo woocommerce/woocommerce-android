f2f7797103a963739c855b90dd12b5514a602b9d
ThomazFB <thomazfcortez@gmail.com>
2024-12-03 12:04:26 -0300

    Add Event recordings for the Wear DataStores whenever the File Corruption Handler is called

--- WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/di/DataStoreModule.kt
+++ WooCommerce-Wear/src/main/java/com/woocommerce/android/wear/di/DataStoreModule.kt
@@ -7,6 +7,7 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import com.automattic.android.tracks.crashlogging.CrashLogging
 import com.woocommerce.android.wear.datastore.DataStoreQualifier
 import com.woocommerce.android.wear.datastore.DataStoreType.LOGIN
 import com.woocommerce.android.wear.datastore.DataStoreType.ORDERS
@@ -27,10 +28,14 @@ class DataStoreModule {
     @DataStoreQualifier(LOGIN)
     fun provideLoginDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = { appContext.preferencesDataStoreFile(LOGIN.typeName) },
-        corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+        corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store: Wear ${LOGIN.typeName}")
+            emptyPreferences()
+        },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
 
@@ -39,10 +44,14 @@ class DataStoreModule {
     @DataStoreQualifier(STATS)
     fun provideStatsDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = { appContext.preferencesDataStoreFile(STATS.typeName) },
-        corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+        corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store: Wear ${STATS.typeName}")
+            emptyPreferences()
+        },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
 
@@ -51,10 +60,14 @@ class DataStoreModule {
     @DataStoreQualifier(ORDERS)
     fun provideOrdersDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = { appContext.preferencesDataStoreFile(ORDERS.typeName) },
-        corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+        corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store: Wear ${ORDERS.typeName}")
+            emptyPreferences()
+        },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
 }
