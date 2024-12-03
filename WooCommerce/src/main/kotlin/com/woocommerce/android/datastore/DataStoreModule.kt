7b09b41af764e5cf9f02f0d9b9f8b2465435991c
ThomazFB <thomazfcortez@gmail.com>
2024-12-03 12:03:02 -0300

    Add Event recordings for All DataStores whenever the File Corruption Handler is called

--- WooCommerce/src/main/kotlin/com/woocommerce/android/datastore/DataStoreModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/datastore/DataStoreModule.kt
@@ -8,9 +8,12 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import com.automattic.android.tracks.crashlogging.CrashLogging
 import com.woocommerce.android.datastore.DataStoreType.ANALYTICS_CONFIGURATION
 import com.woocommerce.android.datastore.DataStoreType.ANALYTICS_UI_CACHE
+import com.woocommerce.android.datastore.DataStoreType.COUPONS
 import com.woocommerce.android.datastore.DataStoreType.DASHBOARD_STATS
+import com.woocommerce.android.datastore.DataStoreType.LAST_UPDATE
 import com.woocommerce.android.datastore.DataStoreType.TOP_PERFORMER_PRODUCTS
 import com.woocommerce.android.datastore.DataStoreType.TRACKER
 import com.woocommerce.android.di.AppCoroutineScope
@@ -32,12 +35,16 @@ class DataStoreModule {
     @DataStoreQualifier(TRACKER)
     fun provideTrackerDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile("tracker")
         },
-        corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+        corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store. DataStore Type: ${TRACKER.name}")
+            emptyPreferences()
+        },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
 
@@ -46,12 +53,16 @@ class DataStoreModule {
     @DataStoreQualifier(ANALYTICS_UI_CACHE)
     fun provideAnalyticsDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile("analytics")
         },
-        corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+        corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store. DataStore Type: ${ANALYTICS_UI_CACHE.name}")
+            emptyPreferences()
+        },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
 
@@ -60,12 +71,16 @@ class DataStoreModule {
     @DataStoreQualifier(ANALYTICS_CONFIGURATION)
     fun provideAnalyticsConfigurationDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile("analytics_configuration")
         },
-        corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+        corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store. DataStore Type: ${ANALYTICS_CONFIGURATION.name}")
+            emptyPreferences()
+        },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
 
@@ -74,12 +89,14 @@ class DataStoreModule {
     @DataStoreQualifier(DASHBOARD_STATS)
     fun provideCustomDateRangeDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<CustomDateRange> = DataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile("custom_date_range_configuration")
         },
         corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store. DataStore Type: ${DASHBOARD_STATS.name}")
             CustomDateRange.getDefaultInstance()
         },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO),
@@ -91,12 +108,14 @@ class DataStoreModule {
     @DataStoreQualifier(TOP_PERFORMER_PRODUCTS)
     fun provideTopPerformersCustomDateRangeDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<CustomDateRange> = DataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile("top_performers_custom_date_range_configuration")
         },
         corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store. DataStore Type: ${TOP_PERFORMER_PRODUCTS.name}")
             CustomDateRange.getDefaultInstance()
         },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO),
@@ -105,15 +124,17 @@ class DataStoreModule {
 
     @Provides
     @Singleton
-    @DataStoreQualifier(DataStoreType.COUPONS)
+    @DataStoreQualifier(COUPONS)
     fun provideCouponsCustomDateRangeDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ): DataStore<CustomDateRange> = DataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile("dashboard_coupons_custom_date_range_configuration")
         },
         corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store. DataStore Type: ${COUPONS.name}")
             CustomDateRange.getDefaultInstance()
         },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO),
@@ -122,15 +143,19 @@ class DataStoreModule {
 
     @Provides
     @Singleton
-    @DataStoreQualifier(DataStoreType.LAST_UPDATE)
+    @DataStoreQualifier(LAST_UPDATE)
     fun provideLastUpdateDataStore(
         appContext: Context,
+        crashLogging: CrashLogging,
         @AppCoroutineScope appCoroutineScope: CoroutineScope
     ) = PreferenceDataStoreFactory.create(
         produceFile = {
             appContext.preferencesDataStoreFile("update")
         },
-        corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+        corruptionHandler = ReplaceFileCorruptionHandler {
+            crashLogging.recordEvent("Corrupted data store. DataStore Type: ${LAST_UPDATE.name}")
+            emptyPreferences()
+        },
         scope = CoroutineScope(appCoroutineScope.coroutineContext + Dispatchers.IO)
     )
 }
