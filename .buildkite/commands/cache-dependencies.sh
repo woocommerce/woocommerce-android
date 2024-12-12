6a5af1ec0655011193e8512cb4adeae0c8cfb909
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-12-12 20:58:39 +0200

    CI: Add a scheduled dependency cache job
    FYI: This job will be then used by 'buildkite-ci' and configured as a
    'buildkite_pipeline_schedule' with a weekly frequency.

---
+++ .buildkite/commands/cache-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :closed_lock_with_key: Installing Secrets"
+bundle exec fastlane run configure_apply
+
+echo "--- :hammer_and_wrench: Building"
+./gradlew assembleJalapenoDebug
+
+save_gradle_dependency_cache
