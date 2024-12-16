4ba57b7e2b81e47f96305b9b2483fb631582e968
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-12-16 19:18:43 +0200

    CI: Cache all dependencies as part of the scheduled dependency cache job

--- .buildkite/commands/cache-dependencies.sh
+++ .buildkite/commands/cache-dependencies.sh
@@ -8,7 +8,22 @@ install_gems
 echo "--- :closed_lock_with_key: Installing Secrets"
 bundle exec fastlane run configure_apply
 
-echo "--- :hammer_and_wrench: Building"
-./gradlew assembleJalapenoDebug
+echo "--- 🛠 Download Mobile App Dependencies [Assemble Mobile App]"
+./gradlew :WooCommerce:assembleJalapenoDebug
+
+echo "--- 🛠 Download Wear App Dependencies [Assemble Wear App]"
+./gradlew :WooCommerce-Wear:assembleJalapenoDebug
+
+echo "--- 🧹 Download Lint Dependencies [Lint Mobile App]"
+./gradlew :WooCommerce:lintJalapenoDebug
+
+echo "--- 🧹 Download Detekt Dependencies [Run Detekt]"
+./gradlew detektAll
+
+echo "--- 🧪 Download Unit Test Dependencies [Assemble Unit Tests]"
+./gradlew assembleJalapenoDebugUnitTest lib:cardreader:assembleDebugUnitTest lib:iap:assembleDebugUnitTest
+
+echo "--- 🧪 Download Android Test Dependencies [Assemble Android Tests]"
+./gradlew assembleJalapenoDebugAndroidTest
 
 save_gradle_dependency_cache
