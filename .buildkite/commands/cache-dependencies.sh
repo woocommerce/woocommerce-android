cec496cc558b15fe3aef1fc93c96b722f1b82b78
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-12-17 18:24:18 +0200

    CI: Add extra echo to force buildkite ci show the expandable sections

--- .buildkite/commands/cache-dependencies.sh
+++ .buildkite/commands/cache-dependencies.sh
@@ -10,20 +10,26 @@ bundle exec fastlane run configure_apply
 
 echo "--- 🛠 Download Mobile App Dependencies [Assemble Mobile App]"
 ./gradlew :WooCommerce:assembleJalapenoDebug
+echo ""
 
 echo "--- 🛠 Download Wear App Dependencies [Assemble Wear App]"
 ./gradlew :WooCommerce-Wear:assembleJalapenoDebug
+echo ""
 
 echo "--- 🧹 Download Lint Dependencies [Lint Mobile App]"
 ./gradlew :WooCommerce:lintJalapenoDebug
+echo ""
 
 echo "--- 🧹 Download Detekt Dependencies [Run Detekt]"
 ./gradlew detektAll
+echo ""
 
 echo "--- 🧪 Download Unit Test Dependencies [Assemble Unit Tests]"
 ./gradlew assembleJalapenoDebugUnitTest lib:cardreader:assembleDebugUnitTest lib:iap:assembleDebugUnitTest
+echo ""
 
 echo "--- 🧪 Download Android Test Dependencies [Assemble Android Tests]"
 ./gradlew assembleJalapenoDebugAndroidTest
+echo ""
 
 save_gradle_dependency_cache
