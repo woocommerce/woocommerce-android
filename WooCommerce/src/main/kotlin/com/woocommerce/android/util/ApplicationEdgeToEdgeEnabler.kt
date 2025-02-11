fbb6f8e80a6847586ed962746850d9830fa7b5fd
Alejo <alejandro.torres.veiga@automattic.com>
2025-02-11 09:05:14 -0600

    Enable edge-to-edge only for supported activities
    Add a check to prevent enabling edge-to-edge on unsupported activities. Report a crash when an activity is not a `ComponentActivity` and is edge-to-edge enabled.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/ApplicationEdgeToEdgeEnabler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/ApplicationEdgeToEdgeEnabler.kt
@@ -5,11 +5,36 @@ import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.automattic.android.tracks.crashlogging.CrashLogging
 import javax.inject.Inject
 
-class ApplicationEdgeToEdgeEnabler @Inject constructor() : Application.ActivityLifecycleCallbacks {
+class ApplicationEdgeToEdgeEnabler @Inject constructor(
+    private val crashLogger: CrashLogging
+) : Application.ActivityLifecycleCallbacks {
+
+    private val unsupportedEdgeToEdgeActivities = setOf(
+        "leakcanary.internal.RequestPermissionActivity"
+    )
+
     override fun onActivityCreated(activity: Activity, bindle: Bundle?) {
-        (activity as? ComponentActivity)?.enableEdgeToEdge()
+        val isEdgeToEdgeSupported = isEdgeToEdgeSupported(activity)
+        if (PackageUtils.isDebugBuild() && isEdgeToEdgeSupported) {
+            (activity as ComponentActivity).enableEdgeToEdge()
+        } else {
+            (activity as? ComponentActivity)?.enableEdgeToEdge() ?: run {
+                if (isEdgeToEdgeSupported) {
+                    val message = "Activity $activity is not a ComponentActivity"
+                    crashLogger.sendReport(
+                        exception = ClassCastException(message),
+                        message = message
+                    )
+                }
+            }
+        }
+    }
+
+    private fun isEdgeToEdgeSupported(activity: Activity): Boolean {
+        return (activity::class.java.name in unsupportedEdgeToEdgeActivities).not()
     }
 
     override fun onActivityStarted(activity: Activity) {
