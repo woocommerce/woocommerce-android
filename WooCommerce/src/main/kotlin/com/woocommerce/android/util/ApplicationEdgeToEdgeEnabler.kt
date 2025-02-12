8df5e6b7b14575a287c4c1d6c31d63642553d974
Alejo <alejandro.torres.veiga@automattic.com>
2025-02-12 09:13:56 -0600

    add documentation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/ApplicationEdgeToEdgeEnabler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/ApplicationEdgeToEdgeEnabler.kt
@@ -12,11 +12,43 @@ class ApplicationEdgeToEdgeEnabler @Inject constructor(
     private val crashLogger: CrashLogging
 ) : Application.ActivityLifecycleCallbacks {
 
+    /**
+     * Set of activity class names that should not be edge-to-edge.
+     */
     private val unsupportedEdgeToEdgeActivities = setOf(
         "leakcanary.internal.RequestPermissionActivity"
     )
 
-    override fun onActivityCreated(activity: Activity, bindle: Bundle?) {
+    /**
+     * Called when an activity has been created.
+     *
+     * This function checks if edge-to-edge display is supported and, if so, enables it.
+     * It handles different scenarios based on the debug/release build type and whether the
+     * activity is a ComponentActivity.
+     *
+     * @param activity The activity that has been created.
+     * @param bundle  A Bundle containing the activity's previously frozen state, if there was one.
+     *                This parameter is not used within this method but is part of the lifecycle callback.
+     *
+     * @throws ClassCastException if `isEdgeToEdgeSupported` is true and the activity is not an instance of `ComponentActivity`,
+     *                            a `ClassCastException` is thrown. The exception is caught and logged by the crashLogger.
+     *
+     * Behavior:
+     *   - Checks if edge-to-edge is supported using `isEdgeToEdgeSupported(activity)`.
+     *   - **Debug Build:**
+     *     - If edge-to-edge is supported, it calls `enableEdgeToEdge()` directly on the activity,
+     *       assuming it's a `ComponentActivity`.
+     *   - **Release Build or Edge-to-Edge Not Supported in Debug build:**
+     *     - If the activity is a `ComponentActivity`, it calls `enableEdgeToEdge()`.
+     *     - If the activity is not a `ComponentActivity` but edge-to-edge is supported, it logs a
+     *       `ClassCastException` using `crashLogger.sendReport()`.
+     *     - If the activity is not a `ComponentActivity` and edge-to-edge isn't supported, nothing happens.
+     *
+     * Edge Case Handling:
+     *  - In release builds, the code gracefully handles non-ComponentActivities when edge-to-edge is supported by logging the issue instead of crashing.
+     *  - In debug builds, the code will attempt to cast the activity to a `ComponentActivity`. If the cast fails, then a `ClassCastException` will be thrown by the application.
+     */
+    override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
         val isEdgeToEdgeSupported = isEdgeToEdgeSupported(activity)
         if (PackageUtils.isDebugBuild() && isEdgeToEdgeSupported) {
             (activity as ComponentActivity).enableEdgeToEdge()
