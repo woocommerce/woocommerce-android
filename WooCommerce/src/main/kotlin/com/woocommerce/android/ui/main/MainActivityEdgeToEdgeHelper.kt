5e78422179741b8ead33f20d617c52a801d93285
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-02-19 13:35:18 +0100

    Fix an issue with bottom navigation insets on API < 30

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainActivityEdgeToEdgeHelper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainActivityEdgeToEdgeHelper.kt
@@ -1,9 +1,10 @@
 package com.woocommerce.android.ui.main
 
-import android.view.View
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.woocommerce.android.databinding.ActivityMainBinding
+import com.woocommerce.android.util.SystemVersionUtils
 import javax.inject.Inject
 
 class MainActivityEdgeToEdgeHelper @Inject constructor() {
@@ -13,22 +14,17 @@ class MainActivityEdgeToEdgeHelper @Inject constructor() {
                 WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
             )
 
-            v.setPadding(systemInsets.left, systemInsets.top, systemInsets.right, 0)
-            insets
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.snackRoot) { v, insets ->
-            val systemInsets = insets.getInsets(
-                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
-            )
-
-            val isBottomNavVisible = binding.bottomNav.visibility == View.VISIBLE
-
+            val isBottomNavVisible = binding.bottomNav.isVisible
             // Apply bottom padding only if bottom nav is hidden
             // because bottom nav is already handling the padding
-            val bottomPadding = if (isBottomNavVisible) 0 else systemInsets.bottom
+            // Note: this doesn't work well on Android Q and below, so we apply it only on Android R and above
+            // TODO: Remove this when Androidx Core v1.16 is released by using
+            //  ViewGroupCompat#installCompatInsetsDispatch
+            //  see: https://developer.android.com/develop/ui/views/layout/edge-to-edge#backward-compatible-dispatching
+            val bottomPadding = if (SystemVersionUtils.isAtLeastR() && isBottomNavVisible) 0 else systemInsets.bottom
 
-            v.setPadding(0, 0, 0, bottomPadding)
+            v.setPadding(systemInsets.left, systemInsets.top, systemInsets.right, bottomPadding)
+
             insets
         }
     }
