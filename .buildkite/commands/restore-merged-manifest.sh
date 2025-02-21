2a17b40f316f2bc0118dee23bee3fd8ddb365b09
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-02-21 13:37:21 +0200

    CI: Restore merged manifest with build variant (restore merged manifest)

--- .buildkite/commands/restore-merged-manifest.sh
+++ .buildkite/commands/restore-merged-manifest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 MODULE=$1
+BUILD_VARIANT=$2
 
 echo "--- 💾 Restore Debug Manifest (Jalapeno)"
-restore_android_merged_manifest ${MODULE} || true
+restore_android_merged_manifest ${MODULE} ${BUILD_VARIANT} || true
