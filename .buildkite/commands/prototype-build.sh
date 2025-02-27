f7f049b3d9ff80645d76aefd7fac1ea92bea109e
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-02-27 13:38:24 +0100

    Add jdkImage modules to artifacts

--- .buildkite/commands/prototype-build.sh
+++ .buildkite/commands/prototype-build.sh
@@ -16,4 +16,5 @@ bundle exec fastlane build_and_upload_prototype_build app:"${APP_TO_BUILD}"
 find ~/.gradle/caches/8.12.1/transforms/ -type f -name "R_jar-snapshot.bin" -exec cp {} . \;
 find ~/.gradle/caches/8.12.1/transforms/ -type f -name "fluxc-annotations_jar-snapshot.bin" -exec cp {} . \;
 find ~/.gradle/caches/8.12.1/transforms/ -type f -name "jrt-fs.jar" -exec cp {} . \;
+find ~/.gradle/caches/8.12.1/transforms/ -path "*/jdkImage/*" -type f -name "modules" -exec cp {} . \;
 ls
