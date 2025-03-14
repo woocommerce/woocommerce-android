a74ae26dd800b2009e6f84bd915c2ed950e01f6b
AnirudhBhat <abhat38@gmail.com>
2025-03-14 13:34:53 +0530

    Make necessary changes in WooPosVariationsDataSource to integrate with BaseDataSource

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsDataSource.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsDataSource.kt
@@ -1,14 +1,13 @@
 package com.woocommerce.android.ui.woopos.home.items.variations
 
 import com.woocommerce.android.model.ProductVariation
+import com.woocommerce.android.ui.products.ProductStatus
 import com.woocommerce.android.ui.products.variations.selector.VariationListHandler
+import com.woocommerce.android.ui.woopos.home.items.common.BaseDataSource
 import com.woocommerce.android.util.WooLog
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Inject
@@ -18,12 +17,12 @@ import javax.inject.Singleton
 class WooPosVariationsDataSource @Inject constructor(
     private val handler: VariationListHandler,
     private val variationCache: VariationsLRUCache<Long, List<ProductVariation>>
-) {
+) : BaseDataSource<ProductVariation>() {
     private suspend fun getCachedVariations(productId: Long): List<ProductVariation> {
         return variationCache.get(productId) ?: emptyList()
     }
 
-    private suspend fun updateCache(productId: Long, variations: List<ProductVariation>) {
+    private suspend fun updateVariationCache(productId: Long, variations: List<ProductVariation>) {
         variationCache.put(productId, variations)
     }
 
@@ -34,42 +33,6 @@ class WooPosVariationsDataSource @Inject constructor(
     fun canLoadMore(numOfVariations: Int): Boolean {
         return handler.canLoadMore(numOfVariations)
     }
-
-    fun fetchFirstPage(
-        productId: Long,
-        forceRefresh: Boolean = true
-    ): Flow<FetchResult> = flow {
-        if (forceRefresh) {
-            updateCache(productId, emptyList())
-        }
-
-        val cachedVariations = getCachedVariations(productId)
-        if (cachedVariations.isNotEmpty()) {
-            emit(FetchResult.Cached(cachedVariations))
-        }
-
-        val result = handler.fetchVariations(
-            productId,
-            forceRefresh = true,
-            filterOptions = mapOf(
-                WCProductStore.VariationFilterOption.STATUS to VARIATION_STATUS_PUBLISH,
-                WCProductStore.VariationFilterOption.DOWNLOADABLE to VARIATION_DOWNLOADABLE_FALSE
-            )
-        )
-        if (result.isSuccess) {
-            val remoteVariations = handler.getVariationsFlow(productId).firstOrNull()?.applyFilter() ?: emptyList()
-            updateCache(productId, remoteVariations)
-            emit(FetchResult.Remote(Result.success(remoteVariations)))
-        } else {
-            emit(
-                FetchResult.Remote(
-                    Result.failure(
-                        result.exceptionOrNull() ?: Exception("Unknown error while fetching variations")
-                    )
-                )
-            )
-        }
-    }.flowOn(Dispatchers.IO)
 
     suspend fun loadMore(productId: Long): Result<List<ProductVariation>> = withContext(Dispatchers.IO) {
         val result = handler.loadMore(
@@ -94,17 +57,33 @@ class WooPosVariationsDataSource @Inject constructor(
         private const val VARIATION_STATUS_PUBLISH = "publish"
         private const val VARIATION_DOWNLOADABLE_FALSE = "false"
     }
+
+    override suspend fun fetchFromCache(productId: Long?): List<ProductVariation> = getCachedVariations(productId!!)
+
+    override suspend fun fetchFromRemote(
+        productId: Long?
+    ): Result<List<ProductVariation>> {
+        return handler.fetchVariations(
+            productId!!,
+            forceRefresh = true,
+            filterOptions = mapOf(
+                WCProductStore.VariationFilterOption.STATUS to ProductStatus.PUBLISH.value,
+                WCProductStore.VariationFilterOption.DOWNLOADABLE to
+                    WCProductStore.DownloadableOptions.FALSE.toString(),
+            )
+        )
+            .mapCatching { handler.getVariationsFlow(productId).firstOrNull()?.applyFilter() ?: emptyList() }
+    }
+
+    override suspend fun updateCache(productId: Long?, data: List<ProductVariation>) {
+        updateVariationCache(productId = productId!!, variations = data)
+    }
 }
 
 private fun Result<Unit>.logFailure() {
     val error = exceptionOrNull()
     val errorMessage = error?.message ?: "Unknown error"
     WooLog.e(WooLog.T.POS, "Loading variations failed - $errorMessage", error)
-}
-
-sealed class FetchResult {
-    data class Cached(val data: List<ProductVariation>) : FetchResult()
-    data class Remote(val result: Result<List<ProductVariation>>) : FetchResult()
 }
 
 private fun List<ProductVariation>.applyFilter(): List<ProductVariation> {
