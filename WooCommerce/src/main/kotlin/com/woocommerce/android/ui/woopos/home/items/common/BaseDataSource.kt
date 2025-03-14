24b0e37c7c9f42104cd01dc08bb56f1c420f60cf
AnirudhBhat <abhat38@gmail.com>
2025-03-14 10:48:58 +0530

    Add a new BaseDataSource class that defines how the data must be fetched from the data source. All other specific data sources must extend from this class.

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/common/BaseDataSource.kt
@@ -0,0 +1,39 @@
+package com.woocommerce.android.ui.woopos.home.items.common
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+abstract class BaseDataSource<T> : FetchDataSource<T> {
+
+    protected abstract suspend fun fetchFromCache(productId: Long? = null): List<T>
+    protected abstract suspend fun fetchFromRemote(
+        productId: Long? = null,
+    ): Result<List<T>>
+    protected abstract suspend fun updateCache(productId: Long? = null, data: List<T>)
+
+    override fun fetchData(
+        fetchOptions: FetchOptions
+    ): Flow<FetchResult<T>> = flow {
+        if (fetchOptions.forceRefresh) {
+            updateCache(fetchOptions.productId, data = emptyList())
+        }
+
+        val cachedData = fetchFromCache(fetchOptions.productId)
+        emit(FetchResult.Cached(cachedData))
+
+        val remoteResult = fetchFromRemote(fetchOptions.productId)
+        if (remoteResult.isSuccess) {
+            val remoteData = remoteResult.getOrThrow()
+            updateCache(productId = fetchOptions.productId, data = remoteData)
+            emit(FetchResult.Remote(Result.success(remoteData)))
+        } else {
+            emit(
+                FetchResult.Remote(
+                    Result.failure(remoteResult.exceptionOrNull() ?: Exception("Unknown error"))
+                )
+            )
+        }
+    }.flowOn(Dispatchers.IO)
+}
