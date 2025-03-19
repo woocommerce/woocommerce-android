b1bf3158d05a6a29608b4a28bc1a5bfd87f4789f
AnirudhBhat <abhat38@gmail.com>
2025-03-19 08:31:26 +0530

    Fix detekt errors

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosToolbar.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosToolbar.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -340,7 +339,6 @@ fun ToggleSwitch(
         )
     }
 }
-
 
 @Composable
 private fun CardReaderStatusButton(
