dd0d543fbb19f79346684f979a9a1fbb7b9ae30e
Andrey <akidinov@gmail.com>
2025-04-04 15:03:25 +0200

    Update JetpackBenefitsScreen to use safeDrawing for window insets

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/jetpack/benefits/JetpackBenefitsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/jetpack/benefits/JetpackBenefitsScreen.kt
@@ -9,12 +9,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
@@ -62,8 +61,7 @@ fun JetpackBenefitsScreen(
             .background(MaterialTheme.colors.surface)
             .fillMaxSize()
             .padding(dimensionResource(id = R.dimen.major_100))
-            .windowInsetsPadding(WindowInsets.navigationBars)
-            .windowInsetsPadding(WindowInsets.displayCutout)
+            .windowInsetsPadding(WindowInsets.safeDrawing)
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
