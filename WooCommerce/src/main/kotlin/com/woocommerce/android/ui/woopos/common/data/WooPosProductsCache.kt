64c02f3303e0b0cd91eaa7da53168011af6d524f
samiuelson <samiuelson@gmail.com>
2025-04-08 14:09:00 +0200

    Update WooPosProductsCache interface

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosProductsCache.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosProductsCache.kt
@@ -3,13 +3,11 @@ package com.woocommerce.android.ui.woopos.common.data
 import com.woocommerce.android.model.Product
 
 interface WooPosProductsCache {
-    suspend fun addProducts(products: List<Product>)
+    suspend fun addAll(products: List<Product>)
 
-    suspend fun getProducts(): List<Product>
+    suspend fun getAll(): List<Product>
 
     suspend fun getProductById(productId: Long): Product?
 
-    suspend fun clearCache()
-
-    suspend fun searchLocally(query: String): List<Product>
+    suspend fun clear()
 }
