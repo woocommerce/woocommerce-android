11cbd4daddecabbe0202a55ee5ceab22bc4e5e10
samiuelson <samiuelson@gmail.com>
2025-04-08 19:56:01 +0200

    Fix getPopularItems after merge

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchEmptyStateRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchEmptyStateRepository.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.woopos.home.items.search
 
 import com.woocommerce.android.model.Product
+import com.woocommerce.android.ui.woopos.common.data.WooPosMockedPopularProductsProvider
 import com.woocommerce.android.ui.woopos.util.datastore.WooPosPreferencesRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
@@ -8,12 +9,12 @@ import javax.inject.Inject
 
 @Suppress("MagicNumber")
 class WooPosItemsSearchEmptyStateRepository @Inject constructor(
-    private val productsDataSource: WooPosSearchProductsDataSource,
-    private val preferencesRepository: WooPosPreferencesRepository
+    private val preferencesRepository: WooPosPreferencesRepository,
+    private val popularProductsProvider: WooPosMockedPopularProductsProvider,
 ) {
     suspend fun getPopularItems(): List<Product> {
         delay(50)
-        return productsDataSource.getPopularProducts()
+        return popularProductsProvider.getPopularProducts()
     }
 
     suspend fun getLastSearches(): List<String> = preferencesRepository.recentProductSearches.first()
