f67a52e35bef0d809073b27183ef34a59ff128dd
Andrey <akidinov@gmail.com>
2025-04-09 15:36:24 +0200

    Implement WooPosCartProductUpdater to handle product updates in the cart

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartProductUpdater.kt
@@ -0,0 +1,104 @@
+package com.woocommerce.android.ui.woopos.home.cart
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.woopos.home.ChildToParentEvent
+import com.woocommerce.android.ui.woopos.home.ParentToChildrenEvent
+import com.woocommerce.android.ui.woopos.home.WooPosChildrenToParentEventSender
+import com.woocommerce.android.ui.woopos.util.format.WooPosFormatPrice
+import com.woocommerce.android.viewmodel.ResourceProvider
+import javax.inject.Inject
+
+class WooPosCartProductUpdater @Inject constructor(
+    private val childrenToParentEventSender: WooPosChildrenToParentEventSender,
+    private val resourceProvider: ResourceProvider,
+    private val formatPrice: WooPosFormatPrice
+) {
+    suspend operator fun invoke(
+        currentState: WooPosCartState,
+        updatedProducts: List<ParentToChildrenEvent.OrderCreated.ProductInfo>,
+    ): WooPosCartState {
+        val currentBodyState = currentState.body as? WooPosCartState.Body.WithItems ?: return currentState
+        val mutableCurrentBodyList = currentBodyState.itemsInCart.toMutableList()
+        var changesDone = false
+
+        currentBodyState.itemsInCart.forEachIndexed { index, item ->
+            when (item) {
+                is WooPosCartItemViewState.Product -> {
+                    updatedProducts.forEach { updatedProduct ->
+                        val productExists = updatedProduct.id == item.id && updatedProduct.id != 0L
+                        val productDoesNotExist = updatedProduct.id == 0L && updatedProduct.name == item.name
+
+                        mutableCurrentBodyList[index] = when (item) {
+                            is WooPosCartItemViewState.Product.Simple -> {
+                                when {
+                                    productExists -> {
+                                        val newItem = item.copy(
+                                            name = updatedProduct.name,
+                                            price = formatPrice(updatedProduct.price)
+                                        )
+                                        val itemChanged = newItem.name != item.name || newItem.price != item.price
+                                        changesDone = changesDone || itemChanged
+                                        newItem
+                                    }
+
+                                    productDoesNotExist -> {
+                                        val newItem = item.copy(
+                                            price = formatPrice(updatedProduct.price),
+                                            productDoesNotExist = false
+                                        )
+                                        changesDone = true
+                                        newItem
+                                    }
+
+                                    else -> item
+                                }
+                            }
+
+                            is WooPosCartItemViewState.Product.Variation -> {
+                                when {
+                                    productExists -> {
+                                        val newItem = item.copy(
+                                            name = updatedProduct.name,
+                                            price = formatPrice(updatedProduct.price)
+                                        )
+                                        val itemChanged = newItem.name != item.name || newItem.price != item.price
+                                        changesDone = changesDone || itemChanged
+                                        newItem
+                                    }
+
+                                    productDoesNotExist -> {
+                                        val newItem = item.copy(
+                                            price = formatPrice(updatedProduct.price),
+                                            productDoesNotExist = false
+                                        )
+                                        changesDone = true
+                                        newItem
+                                    }
+
+                                    else -> item
+                                }
+                            }
+                        }
+                    }
+                }
+
+                is WooPosCartItemViewState.Coupon -> {
+                }
+            }
+        }
+
+        if (changesDone) {
+            childrenToParentEventSender.sendToParent(
+                ChildToParentEvent.ToastMessageDisplayed(
+                    message = resourceProvider.getString(R.string.woopos_cart_changes_in_the_cart)
+                )
+            )
+        }
+
+        return currentState.copy(
+            body = currentBodyState.copy(
+                itemsInCart = mutableCurrentBodyList
+            )
+        )
+    }
+}
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartViewModel.kt
@@ -12,7 +12,6 @@ import com.woocommerce.android.model.ProductVariation
 import com.woocommerce.android.ui.woopos.common.data.WooPosGetProductById
 import com.woocommerce.android.ui.woopos.common.data.WooPosGetVariationById
 import com.woocommerce.android.ui.woopos.home.ChildToParentEvent
-import com.woocommerce.android.ui.woopos.home.ChildToParentEvent.ToastMessageDisplayed
 import com.woocommerce.android.ui.woopos.home.ParentToChildrenEvent
 import com.woocommerce.android.ui.woopos.home.WooPosChildrenToParentEventSender
 import com.woocommerce.android.ui.woopos.home.WooPosParentToChildrenEventReceiver
@@ -49,6 +48,7 @@ class WooPosCartViewModel @Inject constructor(
     private val formatPrice: WooPosFormatPrice,
     private val analyticsTracker: WooPosAnalyticsTracker,
     private val analyticsTrackingDataKeeper: WooPosAnalyticsTrackingDataKeeper,
+    private val cartProductUpdater: WooPosCartProductUpdater,
     savedState: SavedStateHandle,
 ) : ViewModel() {
     private val _state = savedState.getStateFlow(
@@ -122,6 +122,7 @@ class WooPosCartViewModel @Inject constructor(
                     productId = it.id,
                     id = it.variationId
                 )
+
                 is WooPosCartItemViewState.Coupon -> WooPosItemsViewModel.ItemClickedData.Coupon(it.id, it.name)
             }
         }
@@ -146,7 +147,12 @@ class WooPosCartViewModel @Inject constructor(
 
                     is ParentToChildrenEvent.OrderSuccessfullyPaid -> clearCart()
 
-                    is ParentToChildrenEvent.OrderCreated -> updateProductsWithUpdateProducts(event.updatedProducts)
+                    is ParentToChildrenEvent.OrderCreated -> {
+                        _state.value = cartProductUpdater(
+                            currentState = _state.value,
+                            updatedProducts = event.updatedProducts
+                        )
+                    }
 
                     is ParentToChildrenEvent.SearchEvent.RecentSearchSelected,
                     is ParentToChildrenEvent.CheckoutClicked,
@@ -156,84 +162,6 @@ class WooPosCartViewModel @Inject constructor(
                 }
             }
         }
-    }
-
-    private suspend fun updateProductsWithUpdateProducts(
-        updatedProducts: List<ParentToChildrenEvent.OrderCreated.ProductInfo>
-    ) {
-        val currentBodyState = _state.value.body as? WooPosCartState.Body.WithItems ?: return
-        val mutableCurrentBodyList = currentBodyState.itemsInCart.toMutableList()
-
-        for (updatedProduct in updatedProducts) {
-            for (i in 0 until updatedProduct.quantity.toInt()) {
-                mutableCurrentBodyList.forEachIndexed { index, item ->
-                    when (item) {
-                        is WooPosCartItemViewState.Product.Simple -> {
-                            if (updatedProduct.id == item.id && updatedProduct.id != 0L) {
-                                mutableCurrentBodyList[index] = item.copy(
-                                    name = updatedProduct.name,
-                                    price = formatPrice(updatedProduct.price),
-
-                                )
-                            }
-                        }
-
-                        is WooPosCartItemViewState.Product.Variation -> {
-                            if (updatedProduct.id == item.id && updatedProduct.id != 0L) {
-                                mutableCurrentBodyList[index] = item.copy(
-                                    name = updatedProduct.name,
-                                    price = formatPrice(updatedProduct.price),
-                                )
-                            }
-                        }
-
-                        is WooPosCartItemViewState.Coupon -> {
-                        }
-                    }
-                }
-
-                mutableCurrentBodyList.forEachIndexed { index, item ->
-                    when (item) {
-                        is WooPosCartItemViewState.Product.Simple -> {
-                            if (updatedProduct.id == 0L && updatedProduct.name == item.name) {
-                                mutableCurrentBodyList[index] = item.copy(
-                                    price = formatPrice(updatedProduct.price),
-                                    productDoesNotExist = true,
-                                )
-                            }
-                        }
-
-                        is WooPosCartItemViewState.Product.Variation -> {
-                            if (updatedProduct.id == 0L && updatedProduct.name == item.name) {
-                                mutableCurrentBodyList[index] = item.copy(
-                                    price = formatPrice(updatedProduct.price),
-                                    productDoesNotExist = true,
-                                )
-                            }
-                        }
-
-                        is WooPosCartItemViewState.Coupon -> {
-                        }
-                    }
-                }
-            }
-        }
-
-        if (mutableCurrentBodyList.any { it is WooPosCartItemViewState.Product && it.productDoesNotExist }) {
-            viewModelScope.launch {
-                childrenToParentEventSender.sendToParent(
-                    ToastMessageDisplayed(
-                        message = resourceProvider.getString(R.string.woopos_cart_changes_in_the_cart)
-                    )
-                )
-            }
-        }
-
-        _state.value = _state.value.copy(
-            body = currentBodyState.copy(
-                itemsInCart = mutableCurrentBodyList
-            )
-        )
     }
 
     private fun handleBackFromCheckoutToCartClicked() {
@@ -246,8 +174,10 @@ class WooPosCartViewModel @Inject constructor(
                 when (event.itemData) {
                     is WooPosItemsViewModel.ItemClickedData.Product.Simple ->
                         handleSimpleProductClicked(event.itemData.id)
+
                     is WooPosItemsViewModel.ItemClickedData.Product.Variation ->
                         handleVariationClicked(event.itemData.productId, event.itemData.id)
+
                     is WooPosItemsViewModel.ItemClickedData.Coupon ->
                         handleCouponClicked(event.itemData.id, event.itemData.couponCode)
                 }
