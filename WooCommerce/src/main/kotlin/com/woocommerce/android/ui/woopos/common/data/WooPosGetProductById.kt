33272512313f3b0a6ad16c8127d86b473bc00933
Andrey <akidinov@gmail.com>
2025-04-11 10:30:26 +0200

    Use cache as source of products

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetProductById.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetProductById.kt
@@ -1,18 +1,14 @@
 package com.woocommerce.android.ui.woopos.common.data
 
 import com.woocommerce.android.model.Product
-import com.woocommerce.android.model.toAppModel
-import com.woocommerce.android.tools.SelectedSite
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
-import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Inject
 
 class WooPosGetProductById @Inject constructor(
-    private val store: WCProductStore,
-    private val site: SelectedSite,
+    private val cache: WooPosProductsCache,
 ) {
     suspend operator fun invoke(productId: Long): Product? = withContext(IO) {
-        return@withContext store.getProductByRemoteId(site.get(), productId)?.toAppModel()
+        cache.getProductById(productId)
     }
 }
