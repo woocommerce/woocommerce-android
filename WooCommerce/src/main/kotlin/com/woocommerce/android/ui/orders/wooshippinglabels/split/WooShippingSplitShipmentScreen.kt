d75b10e39eb5dd7be2c2c45384d3a1c401c23d47
Irfan Ömür <omur.irfan@gmail.com>
2025-04-14 20:28:32 +0300

    Add preview for split shipment screen compose

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/split/WooShippingSplitShipmentScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/split/WooShippingSplitShipmentScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
@@ -485,4 +486,32 @@ fun SelectableProductsSection(
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun WooShippingSplitShipmentScreenPreview() = WooThemeWithBackground {
+    WooShippingSplitShipmentScreen(
+        viewState = SplitShipmentViewState(
+            shipmentSelected = 0,
+            selectableItems = mapOf(
+                0 to SelectableShippableItemsUI(
+                    shippableItems = emptyList(),
+                    formattedTotalWeight = "",
+                    formattedTotalPrice = ""
+                ),
+                1 to SelectableShippableItemsUI(
+                    shippableItems = emptyList(),
+                    formattedTotalWeight = "",
+                    formattedTotalPrice = ""
+                )
+            )
+        ),
+        onBack = {},
+        onDismissInstructions = {},
+        onUpdateSelection = { _, _, _ -> },
+        onUpdateShipment = {},
+        onUpdateSelectedShipment = {},
+        onRemoveShipment = {}
+    )
 }
