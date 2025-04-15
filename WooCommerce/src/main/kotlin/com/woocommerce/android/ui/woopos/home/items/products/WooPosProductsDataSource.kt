611d4ab18e97434df095e7d6820d7f39507a543e
Andrey <akidinov@gmail.com>
2025-04-15 16:55:51 +0200

    storeProductList in prepopulation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/products/WooPosProductsDataSource.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/products/WooPosProductsDataSource.kt
@@ -57,6 +57,7 @@ class WooPosProductsDataSource @Inject constructor(
                 val productsList = result.model ?: emptyList()
                 val products = productsList.map { it.toAppModel() }
 
+                productsIndex.storeProductList(products.map { it.remoteId })
                 productsToFetch.addAll(products)
 
                 hasMoreToFetch = products.size == PRE_POPULATION_PAGE_SIZE
