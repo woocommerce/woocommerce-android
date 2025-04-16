27ea8f551559035eb4a54a1c50b1d2d7e8ed0597
Andrey <akidinov@gmail.com>
2025-04-16 13:54:52 +0200

    Rename WooPosProductsTypesFilter to WooPosProductsTypesFilterConfig and update references

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosMockedPopularProductsProvider.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosMockedPopularProductsProvider.kt
@@ -1,11 +1,8 @@
 package com.woocommerce.android.ui.woopos.common.data
 
 import com.woocommerce.android.model.Product
+import com.woocommerce.android.model.toAppModel
 import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.ui.woopos.home.items.products.WooPosProductsDataSource.Companion.NORMAL_PAGE_SIZE
-import com.woocommerce.android.ui.woopos.home.items.products.WooPosProductsIndex
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.ProductRestClient
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.reports.ReportsRestClient
 import org.wordpress.android.fluxc.store.WCProductStore
 import org.wordpress.android.fluxc.store.WCProductStore.ProductSorting
 import javax.inject.Inject
@@ -15,21 +12,33 @@ import javax.inject.Singleton
 class WooPosMockedPopularProductsProvider @Inject constructor(
     private val selectedSite: SelectedSite,
     private val productStore: WCProductStore,
+    private val productsTypesFilterConfig: WooPosProductsTypesFilterConfig,
 ) {
     companion object {
         private const val MAX_POPULAR_PRODUCTS = 3
     }
 
-    suspend fun getPopularProducts(): List<Product> {
+    private val popularProductsCache = mutableListOf<Product>()
+
+    fun getPopularProducts(): List<Product> = popularProductsCache
+
+    suspend fun fetchPopularProducts(): Result<Unit> {
         val result = productStore.fetchProducts(
             site = selectedSite.get(),
             offset = 0,
             pageSize = MAX_POPULAR_PRODUCTS,
-            filterOptions = createProductFilters(),
-            includeTypes = createIncludedTypes(),
+            filterOptions = productsTypesFilterConfig.filters,
+            includeTypes = productsTypesFilterConfig.includeTypes,
             sortType = ProductSorting.POPULARITY_DESC,
         )
 
-
+        if (result.isError) {
+            return Result.failure(Exception(result.error.message))
+        } else {
+            val products = result.model ?: emptyList()
+            popularProductsCache.clear()
+            popularProductsCache.addAll(products.map { it.toAppModel() })
+            return Result.success(Unit)
+        }
     }
 }
