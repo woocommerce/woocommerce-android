c75a7b9ecac33ef5937db9f98261b665e8d33fc3
Andrey <akidinov@gmail.com>
2025-04-16 08:25:54 +0200

    Fallback to remote when fetching product

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetProductById.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetProductById.kt
@@ -1,14 +1,25 @@
 package com.woocommerce.android.ui.woopos.common.data
 
 import com.woocommerce.android.model.Product
+import com.woocommerce.android.model.toAppModel
+import com.woocommerce.android.tools.SelectedSite
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
+import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Inject
 
 class WooPosGetProductById @Inject constructor(
+    private val selectedSite: SelectedSite,
     private val cache: WooPosProductsCache,
+    private val productsStore: WCProductStore,
 ) {
     suspend operator fun invoke(productId: Long): Product? = withContext(IO) {
-        cache.getProductById(productId)
+        cache.getProductById(productId) ?: productsStore.getProductByRemoteId(
+            site = selectedSite.get(),
+            remoteProductId = productId,
+        )?.toAppModel()?.let {
+            cache.addAll(listOf(it))
+            it
+        }
     }
 }
