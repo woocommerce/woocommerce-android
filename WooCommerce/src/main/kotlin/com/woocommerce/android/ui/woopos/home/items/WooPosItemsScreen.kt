0062f845df3bed1e1e29d0137c7cb9dc52741ac0
Andrey <akidinov@gmail.com>
2025-04-21 14:53:10 +0200

    Extracted WooPosItemsToolbar

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemsScreen.kt
@@ -1,57 +1,37 @@
 package com.woocommerce.android.ui.woopos.home.items
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.LocalOffer
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
 import com.woocommerce.android.ui.woopos.common.composeui.component.Button
 import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosErrorScreen
 import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosPaginationErrorIndicator
-import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosSearchInput
 import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosSearchInputState
 import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosSearchUIEvent
-import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosText
 import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosSpacing
 import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosTheme
-import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosTypography
 import com.woocommerce.android.ui.woopos.common.composeui.designsystem.toAdaptivePadding
 import com.woocommerce.android.ui.woopos.home.items.WooPosItemSelectionViewState.Product
 import com.woocommerce.android.ui.woopos.home.items.WooPosItemsUIEvent.EndOfItemsListReached
@@ -171,7 +151,7 @@ private fun MainItemsList(
 
                 is WooPosItemsViewState.Content -> MaterialTheme.colorScheme.onSurface
             }
-            ItemsToolbar(
+            WooPosItemsToolbar(
                 state = state.value,
                 titleColor = titleColor,
                 onToolbarInfoIconClicked = onToolbarInfoIconClicked,
@@ -246,117 +226,6 @@ private fun MainItemsList(
             refreshing = state.value.pullToRefreshState == WooPosPullToRefreshState.Refreshing,
             state = pullToRefreshState
         )
-    }
-}
-
-@Composable
-private fun ItemsToolbar(
-    state: WooPosItemsViewState,
-    titleColor: Color,
-    onToolbarInfoIconClicked: () -> Unit,
-    onCouponsButtonClicked: () -> Unit,
-    onSearchEvent: (WooPosSearchUIEvent) -> Unit,
-) {
-    val isSearchExpanded = state is WooPosItemsViewState.Content &&
-        state.search is WooPosItemsViewState.Content.SearchState.Visible &&
-        state.search.state is WooPosSearchInputState.Open
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(56.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-    ) {
-        AnimatedVisibility(
-            visible = !isSearchExpanded,
-            enter = fadeIn(animationSpec = tween(200)),
-            exit = fadeOut(animationSpec = tween(200)),
-        ) {
-            WooPosText(
-                text = stringResource(id = R.string.woopos_products_screen_title),
-                style = WooPosTypography.Heading,
-                fontWeight = FontWeight.Bold,
-                color = titleColor,
-            )
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            when (state) {
-                is WooPosItemsViewState.Content -> {
-                    when (val searchState = state.search) {
-                        WooPosItemsViewState.Content.SearchState.Hidden -> Unit
-                        is WooPosItemsViewState.Content.SearchState.Visible -> {
-                            WooPosSearchInput(
-                                state = searchState.state,
-                                onEvent = onSearchEvent,
-                                modifier = Modifier.weight(1f)
-                            )
-
-                            Spacer(modifier = Modifier.width(WooPosSpacing.Small.value))
-
-                            VerticalDivider(
-                                modifier = Modifier.padding(vertical = WooPosSpacing.Small.value),
-                                thickness = 1.dp,
-                            )
-
-                            Spacer(modifier = Modifier.width(WooPosSpacing.Small.value))
-                        }
-                    }
-
-                    if (state.couponsEnabled) {
-                        IconButton(
-                            modifier = Modifier.size(40.dp),
-                            onClick = {
-                                onCouponsButtonClicked()
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Outlined.LocalOffer,
-                                contentDescription = stringResource(
-                                    id = R.string.coupons
-                                ),
-                                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.87f),
-                                modifier = Modifier.size(32.dp)
-                            )
-                        }
-                        Spacer(modifier = Modifier.width(WooPosSpacing.Small.value))
-
-                        VerticalDivider(
-                            modifier = Modifier.padding(vertical = WooPosSpacing.Small.value),
-                            thickness = 1.dp,
-                        )
-
-                        Spacer(modifier = Modifier.width(WooPosSpacing.Small.value))
-                    }
-
-                    if (state.bannerState.isBannerHiddenByUser) {
-                        IconButton(
-                            modifier = Modifier.size(40.dp),
-                            onClick = {
-                                onToolbarInfoIconClicked()
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Outlined.Info,
-                                contentDescription = stringResource(
-                                    id = R.string.woopos_banner_simple_products_info_content_description
-                                ),
-                                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.87f),
-                                modifier = Modifier.size(32.dp)
-                            )
-                        }
-                    }
-                }
-
-                is WooPosItemsViewState.Empty,
-                is WooPosItemsViewState.Error,
-                is WooPosItemsViewState.Loading -> Unit
-            }
-        }
     }
 }
 
