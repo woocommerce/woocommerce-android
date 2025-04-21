74cd8cc26f3aa33ca4c9fb6670a3275db2dcd525
malinajirka <malinajirka@gmail.com>
2025-04-21 09:02:08 +0200

    Initialize tests

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/WooPosCouponsViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.woocommerce.android.ui.woopos.home.items.coupons
+
+import com.woocommerce.android.ui.woopos.util.WooPosCoroutineTestRule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+
+class WooPosCouponsViewModelTest {
+    @Rule
+    @JvmField
+    val coroutineTestRule = WooPosCoroutineTestRule()
+
+    @Test
+    fun dumymTest() {
+        assertThat(true).isTrue()
+    }
+}
