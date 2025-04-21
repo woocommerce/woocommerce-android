574fd0a75087eac06fbe53d43e309d5593990906
malinajirka <malinajirka@gmail.com>
2025-04-21 12:52:06 +0200

    Fix unit tests

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/WooPosCouponsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/WooPosCouponsViewModelTest.kt
@@ -1,17 +1,19 @@
 package com.woocommerce.android.ui.woopos.home.items.coupons
 
 import com.woocommerce.android.ui.woopos.util.WooPosCoroutineTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class WooPosCouponsViewModelTest {
     @Rule
     @JvmField
     val coroutineTestRule = WooPosCoroutineTestRule()
 
     @Test
-    fun dumymTest() {
+    fun dummyTest() {
         assertThat(true).isTrue()
     }
 }
