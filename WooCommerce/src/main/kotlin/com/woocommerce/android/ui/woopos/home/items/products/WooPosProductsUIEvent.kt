87f28cee98341a9908bbf9687a1973faa48dd479
malinajirka <malinajirka@gmail.com>
2025-04-22 13:33:34 +0200

    Create ProductsUIEvent class

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/products/WooPosProductsUIEvent.kt
@@ -0,0 +1,11 @@
+package com.woocommerce.android.ui.woopos.home.items.products
+
+import com.woocommerce.android.ui.woopos.home.items.WooPosItemSelectionViewState
+
+sealed class WooPosProductsUIEvent {
+    data class ItemClicked(val item: WooPosItemSelectionViewState) : WooPosProductsUIEvent()
+    data object EndOfItemsListReached : WooPosProductsUIEvent()
+    data object PullToRefreshTriggered : WooPosProductsUIEvent()
+    data object ProductsLoadingErrorRetryButtonClicked : WooPosProductsUIEvent()
+    data object BackButtonClicked : WooPosProductsUIEvent()
+}
