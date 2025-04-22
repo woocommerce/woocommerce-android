de3fb5fe9a47bb45dcdfd53bea784e3bec39367b
Andrey <akidinov@gmail.com>
2025-04-22 09:45:49 +0200

    Fixed the naming of the test

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemsViewModelTest.kt
@@ -30,7 +30,7 @@ import java.math.BigDecimal
 import kotlin.test.assertTrue
 
 @ExperimentalCoroutinesApi
-class WooPosItemsViewModelTestSelectionViewState {
+class WooPosItemsViewModelTest {
     @Rule
     @JvmField
     val coroutinesTestRule = WooPosCoroutineTestRule()
