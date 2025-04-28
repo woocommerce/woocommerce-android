3da2761a0f90c111ee0d9fa151ccbe93ab422ae2
Andrey <akidinov@gmail.com>
2025-04-28 09:53:58 +0200

    Use resource instead of the URI to play cha ching

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/util/WooPosSoundHelper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/util/WooPosSoundHelper.kt
@@ -2,7 +2,6 @@ package com.woocommerce.android.ui.woopos.common.util
 
 import android.content.Context
 import android.media.MediaPlayer
-import androidx.core.net.toUri
 import com.woocommerce.android.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
@@ -13,8 +12,7 @@ class WooPosSoundHelper @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     suspend fun playChaChing() = withContext(Dispatchers.IO) {
-        val chaChingUri = "android.resource://${context.packageName}/${R.raw.cha_ching}".toUri()
-        val mp = MediaPlayer.create(context, chaChingUri)
+        val mp = MediaPlayer.create(context, R.raw.cha_ching)
         mp.setOnCompletionListener { it.release() }
         mp.start()
     }
