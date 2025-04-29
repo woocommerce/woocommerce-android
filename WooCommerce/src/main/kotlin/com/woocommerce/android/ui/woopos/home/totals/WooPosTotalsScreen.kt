b9950a941f1a3cf73176919e1f3ea00fc80d9d8e
malinajirka <malinajirka@gmail.com>
2025-04-29 15:40:43 +0200

    Update color of text in totals section

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -328,6 +329,7 @@ private fun TotalsGrid(totals: Totals.Visible) {
             TotalsGridRow(
                 textOne = stringResource(R.string.woopos_payment_discount_label),
                 textTwo = totals.orderDiscountText,
+                colorOne = WooPosTheme.colors.onSurfaceVariantHighest,
             )
 
             Spacer(modifier = Modifier.height(WooPosSpacing.Medium.value.toAdaptivePadding()))
@@ -336,6 +338,7 @@ private fun TotalsGrid(totals: Totals.Visible) {
         TotalsGridRow(
             textOne = stringResource(R.string.woopos_payment_subtotal_label),
             textTwo = totals.orderSubtotalText,
+            colorOne = WooPosTheme.colors.onSurfaceVariantHighest
         )
 
         Spacer(modifier = Modifier.height(WooPosSpacing.Medium.value.toAdaptivePadding()))
@@ -343,6 +346,7 @@ private fun TotalsGrid(totals: Totals.Visible) {
         TotalsGridRow(
             textOne = stringResource(R.string.woopos_payment_tax_label),
             textTwo = totals.orderTaxText,
+            colorOne = WooPosTheme.colors.onSurfaceVariantHighest
         )
 
         Spacer(modifier = Modifier.height(WooPosSpacing.Medium.value.toAdaptivePadding()))
@@ -368,8 +372,10 @@ private fun TotalsGridRow(
     textTwo: String,
     styleOne: WooPosTypography = WooPosTypography.BodyLarge,
     fontWeightOne: FontWeight = FontWeight.Normal,
+    colorOne: Color = Color.Unspecified,
     styleTwo: WooPosTypography = WooPosTypography.BodyLarge,
     fontWeightTwo: FontWeight = FontWeight.Normal,
+    colorTwo: Color = Color.Unspecified,
 ) {
     Row(
         modifier = Modifier.fillMaxWidth(),
@@ -379,11 +385,13 @@ private fun TotalsGridRow(
             text = textOne,
             style = styleOne,
             fontWeight = fontWeightOne,
+            color = colorOne,
         )
         WooPosText(
             text = textTwo,
             style = styleTwo,
             fontWeight = fontWeightTwo,
+            color = colorTwo,
         )
     }
 }
