580a05fe011c134e3dc0e1a6a8a3d5763163f5cf
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-05-02 13:52:35 +0200

    Migrate remaining ProductTagsDao tests.
    `:libs:fluxc-plugin:testDebugUnitTest` passes

--- libs/fluxc-plugin/src/test/java/org/wordpress/android/fluxc/persistence/dao/ProductTagsDaoTest.kt
+++ libs/fluxc-plugin/src/test/java/org/wordpress/android/fluxc/persistence/dao/ProductTagsDaoTest.kt
@@ -4,17 +4,20 @@ import android.app.Application
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import java.io.IOException
+import junit.framework.TestCase.assertTrue
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.persistence.ProductSqlUtils
+import org.wordpress.android.fluxc.persistence.SiteSqlUtils
 import org.wordpress.android.fluxc.persistence.WCAndroidDatabase
 import org.wordpress.android.fluxc.wc.product.ProductTestUtils
 
@@ -55,6 +58,98 @@ class ProductTagsDaoTest {
 
         savedTagList = sut.getProductTags(site.id)
         assertThat(savedTagList).isEqualTo(listOf(updated))
+    }
+
+    @Test
+    fun testGetProductTagsForSite() = runTest {
+        val tagList = ProductTestUtils.generateProductTags(site.id)
+        assertTrue(tagList.isNotEmpty())
+
+        // Insert product tag list
+        sut.upsertProductTags(tagList)
+
+        // Get tag list for site and verify
+        val savedTagListExists = sut.getProductTags(site.id)
+        assertEquals(tagList.size, savedTagListExists.size)
+
+        // Get tag list for a site that does not exist
+        val nonExistingSiteId = 400
+        val savedTagList = sut.getProductTags(nonExistingSiteId)
+        assertEquals(0, savedTagList.size)
+    }
+
+    @Test
+    fun testGetProductTagByName() = runTest {
+        val tagList = ProductTestUtils.generateProductTags(site.id)
+        assertTrue(tagList.isNotEmpty())
+
+        // Insert product tag list
+        sut.upsertProductTags(tagList)
+
+        // Get tag by name and verify
+        val firstTag = tagList.first()
+        val savedTagExists = sut.getProductTag(site.id, name = firstTag.name)
+        assertThat(savedTagExists).isEqualTo(firstTag)
+
+        // Get tag for a name that does not exist
+        val nonExistingTagName = "test"
+        val savedTag = sut.getProductTag(site.id, name = nonExistingTagName)
+        assertThat(savedTag).isNull()
+    }
+
+    @Test
+    fun testGetProductTagsByNames() = runTest {
+        val tagList = ProductTestUtils.generateProductTags(site.id)
+        assertTrue(tagList.isNotEmpty())
+
+        // Insert product tag list
+        sut.upsertProductTags(tagList)
+
+        // Get tags by list of name and verify
+        val tagNames = tagList.map { it.name }.toList()
+        val savedTagListExists = sut.getProductTags(site.id, tagsNames = tagNames)
+        assertThat(savedTagListExists).containsExactlyInAnyOrderElementsOf(tagList)
+
+        // Get tags for a name that does not exist
+        val monExistingTagList = sut.getProductTags(site.id, tagsNames = listOf("test", "test1", "test2"))
+        assertEquals(0, monExistingTagList.size)
+
+        val savedTagList = sut.getProductTags(site.id, tagsNames = listOf(tagNames[0], tagNames[1], "test"))
+        assertEquals(2, savedTagList.size)
+    }
+
+    @Test
+    fun testDeleteProductTagsForSite() = runTest {
+        val tags = ProductTestUtils.generateProductTags(site.id)
+
+        sut.upsertProductTags(tags)
+
+        // Verify product tags inserted
+        var savedTags = sut.getProductTags(site.id)
+        assertEquals(tags.size, savedTags.size)
+
+        // Delete tags for site and verify
+        sut.deleteProductTagsForSite(site.id)
+        savedTags = sut.getProductTags(site.id)
+        assertEquals(0, savedTags.size)
+    }
+
+    @Test
+    @Ignore("This test is ignored until SiteModel is moved to Room and foreign key constraints are added")
+    fun testDeleteSiteDeletesProductTags() = runTest{
+        val tags = ProductTestUtils.generateProductTags(site.id)
+        assertTrue(tags.isNotEmpty())
+
+        sut.upsertProductTags(tags)
+
+        // Verify tags inserted
+        var savedTags = sut.getProductTags(site.id)
+        assertEquals(tags.size, savedTags.size)
+
+        // Delete site and verify tags are deleted via foreign key constraint
+        SiteSqlUtils().deleteSite(site)
+        savedTags = sut.getProductTags(site.id)
+        assertEquals(0, savedTags.size)
     }
 
     @After
