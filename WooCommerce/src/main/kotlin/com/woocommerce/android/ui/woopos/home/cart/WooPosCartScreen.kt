7f7c38f4ba22fac782d2da4ba3a6feb4b75e73f2
malinajirka <malinajirka@gmail.com>
2025-05-07 13:33:04 +0200

    Fix bottom padding for cart list on checkout screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/cart/WooPosCartScreen.kt
@@ -223,7 +223,7 @@ private fun CartBodyWithItems(
     ScrollToTopHandler(items, listState)
 
     val spacerHeight by animateDpAsState(
-        targetValue = if (!isCheckoutButtonVisible) 182.dp else 0.dp,
+        targetValue = if (!isCheckoutButtonVisible) 100.dp else 0.dp,
         label = "cart list height animation"
     )
 
