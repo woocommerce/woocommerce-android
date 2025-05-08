f2cfb4c8077b77ea31dfb670e6f95ecab4ace361
samiuelson <samiuelson@gmail.com>
2025-05-08 14:23:42 +0200

    Update test

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosSearchProductsDataSourceTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosSearchProductsDataSourceTest.kt
@@ -103,7 +103,7 @@ class WooPosSearchProductsDataSourceTest {
                     filterOptions = anyOrNull(),
                     includeTypes = anyOrNull(),
                 )
-            ).thenReturn(WooResult(ProductSearchResult(emptyList(), true)))
+            ).thenReturn(WooResult(ProductSearchResult(emptyList(), true, 23)))
 
             // WHEN
             val result = sut.searchRemoteProducts(query)
