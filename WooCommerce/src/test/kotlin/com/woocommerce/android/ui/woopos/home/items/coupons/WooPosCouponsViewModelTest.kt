82ce016fa76f40a0029f58163937f2b9a09bd1e7
malinajirka <malinajirka@gmail.com>
2025-05-09 15:44:36 +0200

    Add test for WooPosCouponsViewModelTest

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/WooPosCouponsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/WooPosCouponsViewModelTest.kt
@@ -1,10 +1,22 @@
 package com.woocommerce.android.ui.woopos.home.items.coupons
 
+import com.woocommerce.android.ui.woopos.home.ChildToParentEvent
+import com.woocommerce.android.ui.woopos.home.WooPosChildrenToParentEventSender
+import com.woocommerce.android.ui.woopos.home.items.WooPosCouponsViewState
+import com.woocommerce.android.ui.woopos.home.items.WooPosItemsViewModel.ItemClickedData
+import com.woocommerce.android.ui.woopos.home.items.coupons.creation.WooPosCouponCreationFacade
+import com.woocommerce.android.ui.woopos.home.items.navigation.WooPosItemsNavigator
 import com.woocommerce.android.ui.woopos.util.WooPosCoroutineTestRule
+import com.woocommerce.android.ui.woopos.util.analytics.WooPosAnalyticsEvent.Event.ItemAddedToCart.WooPosItemSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.assertj.core.api.Assertions.assertThat
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
 class WooPosCouponsViewModelTest {
@@ -12,8 +24,39 @@ class WooPosCouponsViewModelTest {
     @JvmField
     val coroutineTestRule = WooPosCoroutineTestRule()
 
-    @Test
-    fun dummyTest() {
-        assertThat(true).isTrue()
+    private val listViewStateManager: WooPosCouponsListViewStateManager = mock()
+    private val fromChildToParentEventSender: WooPosChildrenToParentEventSender = mock()
+    private val couponCreationFacade: WooPosCouponCreationFacade = mock()
+    private val navigator: WooPosItemsNavigator = mock()
+
+    @Before
+    fun setUp() {
+        whenever(listViewStateManager.viewState).thenReturn(flowOf(WooPosCouponsViewState.Loading()))
     }
+
+    @Test
+    fun `when add coupon icon is tapped, then newly created coupon added to cart`() = runTest {
+        // GIVEN
+        whenever(couponCreationFacade.createCoupon()).thenReturn(1L)
+        val viewModel = createViewModel()
+
+        // WHEN
+        viewModel.onUIEvent(WooPosCouponsUIEvent.CreateCouponClicked)
+
+        // THEN
+        verify(fromChildToParentEventSender).sendToParent(
+            ChildToParentEvent.ItemClickedInProductSelector(
+                itemData = ItemClickedData.Coupon(1L),
+                source = WooPosItemSource.COUPON_LIST
+            )
+        )
+    }
+
+    private fun createViewModel() =
+        WooPosCouponsViewModel(
+            listViewStateManager,
+            fromChildToParentEventSender,
+            couponCreationFacade,
+            navigator,
+        )
 }
