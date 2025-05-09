b4857092b40cf5975e99ff98226ccfe4b1d65877
malinajirka <malinajirka@gmail.com>
2025-05-09 15:48:22 +0200

    Init vm

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/WooPosCouponsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/WooPosCouponsViewModelTest.kt
@@ -1,10 +1,15 @@
 package com.woocommerce.android.ui.woopos.home.items.coupons
 
+import com.woocommerce.android.ui.woopos.home.WooPosChildrenToParentEventSender
+import com.woocommerce.android.ui.woopos.home.items.WooPosCouponsViewState
+import com.woocommerce.android.ui.woopos.home.items.navigation.WooPosItemsNavigator
 import com.woocommerce.android.ui.woopos.util.WooPosCoroutineTestRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.assertj.core.api.Assertions.assertThat
+import kotlinx.coroutines.flow.flowOf
+import org.junit.Before
 import org.junit.Rule
-import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
 class WooPosCouponsViewModelTest {
@@ -12,8 +17,19 @@ class WooPosCouponsViewModelTest {
     @JvmField
     val coroutineTestRule = WooPosCoroutineTestRule()
 
-    @Test
-    fun dummyTest() {
-        assertThat(true).isTrue()
+    private val listViewStateManager: WooPosCouponsListViewStateManager = mock()
+    private val fromChildToParentEventSender: WooPosChildrenToParentEventSender = mock()
+    private val navigator: WooPosItemsNavigator = mock()
+
+    @Before
+    fun setUp() {
+        whenever(listViewStateManager.viewState).thenReturn(flowOf(WooPosCouponsViewState.Loading()))
     }
+
+    private fun createViewModel() =
+        WooPosCouponsViewModel(
+            listViewStateManager,
+            fromChildToParentEventSender,
+            navigator,
+        )
 }
