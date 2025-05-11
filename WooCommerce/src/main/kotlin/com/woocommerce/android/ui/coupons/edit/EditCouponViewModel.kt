4bdfba1e15c8f7ab33d2808c79359203c2637ee0
malinajirka <malinajirka@gmail.com>
2025-05-11 19:55:38 +0200

    Replace activity toolbar with screen toolbar on EditCouponFragment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/edit/EditCouponViewModel.kt
@@ -258,6 +258,10 @@ class EditCouponViewModel @Inject constructor(
         isSaving.value = false
     }
 
+    fun onBackPressed() {
+        exitFlow()
+    }
+
     private fun getSaveButtonText(): Int = when (mode.value) {
         is Mode.Edit -> R.string.coupon_edit_save_button
         is Mode.Create -> R.string.coupon_create_save_button
