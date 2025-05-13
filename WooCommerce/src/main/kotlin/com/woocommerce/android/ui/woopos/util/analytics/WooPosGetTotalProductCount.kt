bf52ab67f89fffcad93cfa652362f73ffbc9a886
samiuelson <samiuelson@gmail.com>
2025-05-13 10:58:37 +0200

    Move fetchProductsCount operation to IO

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosGetTotalProductCount.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosGetTotalProductCount.kt
@@ -1,8 +1,10 @@
 package com.woocommerce.android.ui.woopos.util.analytics
 
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.util.CoroutineDispatchers
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Inject
@@ -11,21 +13,20 @@ import javax.inject.Singleton
 @Singleton
 class WooPosGetTotalProductCount @Inject constructor(
     private val productStore: WCProductStore,
-    private val selectedSite: SelectedSite
+    private val selectedSite: SelectedSite,
+    private val dispatchers: CoroutineDispatchers
 ) {
     private val mutex = Mutex()
-    private var totalProductCount: Int? = null
+    @Volatile private var totalProductCount: Int? = null
 
-    suspend operator fun invoke(): Int? {
-        if (totalProductCount == null) {
-            fetchProductCount()
-        }
-        return totalProductCount
+    suspend operator fun invoke(): Int? = withContext(dispatchers.io) {
+        totalProductCount?.let { return@withContext it }
+        fetchProductCount()
+        return@withContext totalProductCount
     }
 
     private suspend fun fetchProductCount() {
         mutex.withLock {
-            if (totalProductCount != null) return
             val site = selectedSite.get()
             val result = fetchTotalProductCount(site)
             if (result.isSuccess) {
