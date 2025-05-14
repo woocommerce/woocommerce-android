6a31c7fc1220b93c0e8d3da75509810d20a6bb70
samiuelson <samiuelson@gmail.com>
2025-05-14 20:16:52 +0200

    Remove product list animation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/products/WooPosProductsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/products/WooPosProductsScreen.kt
@@ -12,8 +12,13 @@ import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -144,6 +149,7 @@ private fun Content(
         listState = listState,
         onItemClicked = onItemClicked,
         onEndOfProductsListReached = onEndOfItemListReached,
+        animateItems = false,
     ) {
         ProductsPaginationError(
             onRetryClicked = {
