384e7b273ded8e4628ecbae92e47235c0fe06508
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-05-16 16:29:41 +0300

    Db: Remove unused 'StatsXyz' tables/code from 'wp-fluxc'
    List of tables:
    - StatsBlock
    - StatsRequest

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/InsightsMostPopularModel.kt
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android.fluxc.model.stats
-
-data class InsightsMostPopularModel(
-    val siteId: Long,
-    val highestDayOfWeek: Int,
-    val highestHour: Int,
-    val highestDayPercent: Double,
-    val highestHourPercent: Double
-)
