2b764e19f6a8cbd73fbf28663c7b5077e2c4321d
malinajirka <malinajirka@gmail.com>
2025-05-19 14:05:55 +0200

    Suppress detekt warning

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/tracking/CouponCreationFlowTrackerEventProvider.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/tracking/CouponCreationFlowTrackerEventProvider.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.coupons.tracking
 
 import com.woocommerce.android.analytics.IAnalyticsEvent
 
+@Suppress("VariableNaming")
 interface CouponCreationFlowTrackerEventProvider {
     val COUPON_CREATION_SUCCESS: IAnalyticsEvent
     val COUPON_CREATION_FAILED: IAnalyticsEvent
