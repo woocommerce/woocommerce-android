b06398a7f6ed3a55b130bc584ee14928e30d7011
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-05-20 12:48:42 +0200

    Migrate `WCProductSettingsModel` to be a Room entity

--- libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/model/WCProductSettingsModel.kt
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/model/WCProductSettingsModel.kt
@@ -1,21 +1,14 @@
 package org.wordpress.android.fluxc.model
 
-import com.yarolegovich.wellsql.core.Identifiable
-import com.yarolegovich.wellsql.core.annotation.Column
-import com.yarolegovich.wellsql.core.annotation.PrimaryKey
-import com.yarolegovich.wellsql.core.annotation.Table
-import org.wordpress.android.fluxc.persistence.WellSqlConfig
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE)
-data class WCProductSettingsModel(@PrimaryKey @Column private var id: Int = 0) : Identifiable {
+@Entity(
+    tableName = "WCProductSettingsModel",
+)
+data class WCProductSettingsModel(
     // note that there are many more product settings than this, but for now these are all we need
-    @Column var localSiteId = 0
-    @Column var weightUnit = ""
-    @Column var dimensionUnit = ""
-
-    override fun getId() = id
-
-    override fun setId(id: Int) {
-        this.id = id
-    }
-}
+    @PrimaryKey val localSiteId: Int = 0,
+    val weightUnit: String = "",
+    val dimensionUnit: String = "",
+)
---
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/ProductSettingsDao.kt
@@ -0,0 +1,22 @@
+package org.wordpress.android.fluxc.persistence.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
+import org.wordpress.android.fluxc.model.WCProductSettingsModel
+
+@Dao
+abstract class ProductSettingsDao {
+
+    @Upsert
+    abstract suspend fun upsertProductSettings(productSettings: WCProductSettingsModel)
+
+    @Query(
+        """
+            SELECT * FROM WCProductSettingsModel
+            WHERE localSiteId = :siteId
+        """
+    )
+    abstract suspend fun getProductSettings(siteId: LocalId): WCProductSettingsModel?
+}
