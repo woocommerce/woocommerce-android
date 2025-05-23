011091daa8c8cf9c601df20f8ea003c08812e36a
samiuelson <samiuelson@gmail.com>
2025-05-23 17:17:52 +0200

    Implement recent searches repository

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/search/WooPosCouponsSearchEmptyStateRepository.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.woopos.home.items.coupons.search
+
+import com.woocommerce.android.ui.woopos.util.datastore.WooPosPreferencesRepository
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class WooPosCouponsSearchEmptyStateRepository @Inject constructor(
+    private val preferencesRepository: WooPosPreferencesRepository,
+) {
+    suspend fun getLastSearches(): List<String> = preferencesRepository.recentCouponSearches.first()
+
+    suspend fun addRecentSearch(search: String) {
+        preferencesRepository.addRecentCouponSearch(search)
+    }
+} 
