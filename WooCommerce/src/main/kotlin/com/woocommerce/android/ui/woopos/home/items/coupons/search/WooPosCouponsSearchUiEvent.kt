5e30b173fec78451f82e64e3e620772c0de0d3bd
samiuelson <samiuelson@gmail.com>
2025-05-23 12:19:10 +0200

    Implement WooPosCouponsSearchUiEvent

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/search/WooPosCouponsSearchUiEvent.kt
@@ -0,0 +1,9 @@
+package com.woocommerce.android.ui.woopos.home.items.coupons.search
+
+import com.woocommerce.android.ui.woopos.home.items.WooPosItemSelectionViewState
+
+sealed class WooPosCouponsSearchUiEvent {
+    data class OnCouponClicked(val coupon: WooPosItemSelectionViewState.Coupon) : WooPosCouponsSearchUiEvent()
+    data object OnNextPageRequested : WooPosCouponsSearchUiEvent()
+    data object LoadingErrorRetryButtonClicked : WooPosCouponsSearchUiEvent()
+}
