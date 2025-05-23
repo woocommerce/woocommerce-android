c71462a90fd99a18f1b6d366e7054353e20971f2
samiuelson <samiuelson@gmail.com>
2025-05-23 12:11:15 +0200

    Create WooPosCouponsSearchViewState model

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/coupons/search/WooPosCouponsSearchViewState.kt
@@ -0,0 +1,21 @@
+package com.woocommerce.android.ui.woopos.home.items.coupons.search
+
+import com.woocommerce.android.ui.woopos.home.items.WooPosContentViewState
+import com.woocommerce.android.ui.woopos.home.items.WooPosItemSelectionViewState
+import com.woocommerce.android.ui.woopos.home.items.WooPosPaginationState
+import com.woocommerce.android.ui.woopos.home.items.WooPosPullToRefreshState
+
+sealed class WooPosCouponsSearchViewState {
+    data object EmptySearchQuery : WooPosCouponsSearchViewState()
+
+    data class Content(
+        val searchQuery: String,
+        override val items: List<WooPosItemSelectionViewState.Coupon>,
+        override val pullToRefreshState: WooPosPullToRefreshState = WooPosPullToRefreshState.Enabled,
+        override val paginationState: WooPosPaginationState = WooPosPaginationState.None,
+    ) : WooPosCouponsSearchViewState(), WooPosContentViewState
+
+    data object Empty : WooPosCouponsSearchViewState()
+    data class Error(val searchQuery: String) : WooPosCouponsSearchViewState()
+    data object Loading : WooPosCouponsSearchViewState()
+}
