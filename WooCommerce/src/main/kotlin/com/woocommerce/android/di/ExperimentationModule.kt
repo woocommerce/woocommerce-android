06a7ea287d76d4151161a570bd6c73b4636c37bf
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-05-30 13:41:48 +0200

    Update `automattic-tracks` to `6.0.3`
    Address breaking changes on `experimentation` module. It no longer depends on FluxC.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/di/ExperimentationModule.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/di/ExperimentationModule.kt
@@ -1,14 +1,18 @@
 package com.woocommerce.android.di
 
-import com.automattic.android.experimentation.ExPlat
+import android.content.Context
+import com.automattic.android.experimentation.ExperimentLogger
+import com.automattic.android.experimentation.VariationsRepository
 import com.woocommerce.android.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
-import org.wordpress.android.fluxc.store.ExperimentStore
 import org.wordpress.android.fluxc.utils.AppLogWrapper
+import org.wordpress.android.util.AppLog
+import java.io.File
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -16,16 +20,19 @@ import javax.inject.Singleton
 class ExperimentationModule {
     @Provides
     @Singleton
-    fun provideExPlat(
-        experimentStore: ExperimentStore,
+    fun provideExperimentationRepository(
+        @ApplicationContext appContext: Context,
         appLogWrapper: AppLogWrapper,
         @AppCoroutineScope appCoroutineScope: CoroutineScope,
-    ) = ExPlat(
-        platform = ExperimentStore.Platform.WOOCOMMERCE_ANDROID,
+    ) = VariationsRepository.create(
+        platform = "woocommerceandroid",
         experiments = emptySet(),
-        experimentStore = experimentStore,
-        appLogWrapper = appLogWrapper,
-        coroutineScope = appCoroutineScope,
-        isDebug = BuildConfig.DEBUG
+        failFast = BuildConfig.DEBUG,
+        logger = object : ExperimentLogger {
+            override fun d(message: String) = appLogWrapper.d(AppLog.T.API, message)
+            override fun e(message: String, throwable: Throwable?) = appLogWrapper.e(AppLog.T.API, message, throwable)
+        },
+        cacheDir = File(appContext.filesDir, "experiments"),
+        coroutineScope = appCoroutineScope
     )
 }
