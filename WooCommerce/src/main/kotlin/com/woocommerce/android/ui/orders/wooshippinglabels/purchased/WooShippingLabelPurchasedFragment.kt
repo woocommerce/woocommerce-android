2ae408785cc74ac5105c3bec6e6b5e12d9f6a9ed
Irfan Ömür <irfan.omur@automattic.com>
2025-06-03 21:56:57 +0300

    Remove unused classes related to shipping label purchased screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/purchased/WooShippingLabelPurchasedFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.woocommerce.android.ui.orders.wooshippinglabels.purchased
-
-import com.woocommerce.android.ui.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class WooShippingLabelPurchasedFragment : BaseFragment()
