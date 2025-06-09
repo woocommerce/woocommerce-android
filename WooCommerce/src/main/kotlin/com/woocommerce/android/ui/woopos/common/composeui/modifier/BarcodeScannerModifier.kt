960c7a4e913864ed1a35fd31e523180899e14e12
malinajirka <malinajirka@gmail.com>
2025-06-09 17:15:47 +0200

    Add heuristic for detecting hw keyboard from barcode scanner

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/modifier/BarcodeScannerModifier.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/modifier/BarcodeScannerModifier.kt
@@ -1,8 +1,11 @@
 package com.woocommerce.android.ui.woopos.common.composeui.modifier
 
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.focusable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.focus.FocusRequester
@@ -11,21 +14,29 @@ import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.input.key.utf16CodePoint
-
-@Suppress("SpellCheckingInspection")
-private const val ALLOWED_BARCODE_CHARS =
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._~:/?#[]@!$&'()*+,;="
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.wordpress.android.fluxc.utils.CurrentTimeProvider
 
 fun Modifier.listenForBarcodes(
     onBarcodeScanned: (String) -> Unit,
     enabled: Boolean = true
 ): Modifier = composed {
+    val scope = rememberCoroutineScope()
     val focusRequester = remember { FocusRequester() }
-    val barcodeBuffer = remember { StringBuilder() }
+    val detector = remember { BarcodeInputDetector(onBarcodeScanned, scope, CurrentTimeProvider()) }
 
     LaunchedEffect(enabled) {
         if (enabled) {
             focusRequester.requestFocus()
+        }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            detector.clear()
         }
     }
 
@@ -37,18 +48,123 @@ fun Modifier.listenForBarcodes(
 
             if (keyEvent.type == KeyEventType.KeyDown) {
                 val pressedKey = keyEvent.utf16CodePoint.toChar()
-                if (pressedKey == '\n' || pressedKey == '\r') {
-                    val scannedBarcode = barcodeBuffer.toString()
-                    if (scannedBarcode.isNotEmpty()) {
-                        onBarcodeScanned(scannedBarcode)
-                        barcodeBuffer.clear()
-                    }
-                    return@onKeyEvent true
-                } else if (pressedKey in ALLOWED_BARCODE_CHARS) {
-                    barcodeBuffer.append(pressedKey)
-                    return@onKeyEvent true
-                }
+                return@onKeyEvent detector.handleKeyInput(pressedKey)
             }
-            return@onKeyEvent false
+
+            false
         }
 }
+
+@VisibleForTesting
+class BarcodeInputDetector(
+    private val onBarcodeScanned: (String) -> Unit,
+    private val coroutineScope: CoroutineScope,
+    private val currentTimeProvider: CurrentTimeProvider,
+) {
+    companion object {
+        @Suppress("SpellCheckingInspection")
+        const val ALLOWED_BARCODE_CHARS =
+            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._~:/?#[]@!$&'()*+,;="
+        const val MAX_SCANNER_TOTAL_SCAN_TIMEOUT_MS = 1500L
+        const val MAX_SCANNER_INTER_CHAR_DELAY_MS = 100L
+        const val MIN_BARCODE_LENGTH = 4
+    }
+
+    private val barcodeBuffer = StringBuilder()
+    private var scanStartTime: Long? = null
+    private var lastCharTime: Long? = null
+    private var timeoutJob: Job? = null
+
+    fun handleKeyInput(char: Char): Boolean {
+        val currentTime = currentTimeProvider.currentDate().time
+
+        when (char) {
+            '\n', '\r' -> {
+                cancelTimeout()
+                val isLikelyScanner = scanStartTime != null &&
+                    (currentTime - scanStartTime!! <= MAX_SCANNER_TOTAL_SCAN_TIMEOUT_MS) &&
+                    lastCharTime != null &&
+                    (currentTime - lastCharTime!! <= MAX_SCANNER_INTER_CHAR_DELAY_MS)
+                if (isLikelyScanner) processBarcodeBuffer()
+                clear()
+                return true
+            }
+
+            in ALLOWED_BARCODE_CHARS -> {
+                cancelTimeout()
+
+                if (scanStartTime == null) {
+                    startNewScan(char, currentTime)
+                } else {
+                    handleContinuedScan(char, currentTime)
+                }
+                return true
+            }
+
+            else -> {
+                clear()
+                return false
+            }
+        }
+    }
+
+    private fun startNewScan(char: Char, currentTime: Long) {
+        scanStartTime = currentTime
+        lastCharTime = currentTime
+        barcodeBuffer.append(char)
+
+        timeoutJob = coroutineScope.launch {
+            delay(MAX_SCANNER_TOTAL_SCAN_TIMEOUT_MS)
+            processBarcodeBuffer()
+        }
+    }
+
+    private fun handleContinuedScan(char: Char, currentTime: Long) {
+        val totalElapsedTime = currentTime - scanStartTime!!
+        val timeSinceLastChar = currentTime - lastCharTime!!
+        val humanInputDetected = totalElapsedTime > MAX_SCANNER_TOTAL_SCAN_TIMEOUT_MS ||
+            timeSinceLastChar > MAX_SCANNER_INTER_CHAR_DELAY_MS
+
+        if (humanInputDetected) {
+            clear()
+            startNewScan(char, currentTime)
+        } else {
+            lastCharTime = currentTime
+            barcodeBuffer.append(char)
+
+            val remainingTime = MAX_SCANNER_TOTAL_SCAN_TIMEOUT_MS - totalElapsedTime
+            if (remainingTime > 0) {
+                timeoutJob = coroutineScope.launch {
+                    delay(remainingTime)
+                    processBarcodeBuffer()
+                }
+            } else {
+                clear()
+            }
+        }
+    }
+
+    private fun processBarcodeBuffer() {
+        val scannedBarcode = barcodeBuffer.toString()
+
+        if (scannedBarcode.length >= MIN_BARCODE_LENGTH) {
+            onBarcodeScanned(scannedBarcode)
+        }
+
+        clear()
+    }
+
+    fun clear() {
+        cancelTimeout()
+        barcodeBuffer.clear()
+        scanStartTime = null
+        lastCharTime = null
+    }
+
+    private fun cancelTimeout() {
+        timeoutJob?.cancel()
+        timeoutJob = null
+    }
+}
+
+
