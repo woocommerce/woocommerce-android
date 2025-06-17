fb2fe24ce21e6c13d5024d89a64e1807f7504ece
malinajirka <malinajirka@gmail.com>
2025-06-17 15:55:43 +0200

    Show unclickable overlay in order creation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditFormFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/creation/OrderCreateEditFormFragment.kt
@@ -561,6 +561,25 @@ class OrderCreateEditFormFragment :
                 binding.taxRateSelectorSection.isVisible = it.isShown
                 binding.setTaxRateButton.text = it.label
             }
+            new.isUpdatingOrderDraft.takeIfNotEqualTo(old?.isUpdatingOrderDraft) { isUpdatingOrderDraft ->
+                if (isUpdatingOrderDraft) {
+                    // Show overlay with fade in animation
+                    binding.orderUpdateOverlay.isVisible = true
+                    binding.orderUpdateOverlay.animate()
+                        .alpha(1f)
+                        .setDuration(200)
+                        .start()
+                } else {
+                    // Hide overlay with fade out animation
+                    binding.orderUpdateOverlay.animate()
+                        .alpha(0f)
+                        .setDuration(200)
+                        .withEndAction {
+                            binding.orderUpdateOverlay.isVisible = false
+                        }
+                        .start()
+                }
+            }
         }
     }
 
