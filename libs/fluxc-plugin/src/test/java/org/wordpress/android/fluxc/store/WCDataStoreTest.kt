50cd9a8551ff34333478b2c1bda89d7db5c2383c
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-06-19 15:59:04 +0200

    Improve readability of `WCDataStoreTest`
    Use plain assertions (works now as `WCLocationModel` is immutable data class now). Update names of tests

--- libs/fluxc-plugin/src/test/java/org/wordpress/android/fluxc/store/WCDataStoreTest.kt
+++ libs/fluxc-plugin/src/test/java/org/wordpress/android/fluxc/store/WCDataStoreTest.kt
@@ -2,7 +2,7 @@ package org.wordpress.android.fluxc.store
 
 import androidx.room.Room
 import com.yarolegovich.wellsql.WellSql
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,7 +14,6 @@ import org.robolectric.annotation.Config
 import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
 import org.wordpress.android.fluxc.TestSiteSqlUtils
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.model.data.WCCountryMapper
 import org.wordpress.android.fluxc.model.data.WCLocationModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
@@ -58,65 +57,45 @@ class WCDataStoreTest {
     }
 
     @Test
-    fun `fetch countries`() = test {
-        val result = fetchCountries()
-
-        Assertions.assertThat(result.model?.size).isEqualTo(sampleData.size)
-        val first = WCCountryMapper.map(sampleResponse.first()).first()
-        Assertions.assertThat(result.model?.first()?.name).isEqualTo(first.name)
-        Assertions.assertThat(result.model?.first()?.code).isEqualTo(first.code)
-        Assertions.assertThat(result.model?.first()?.parentCode).isEqualTo(first.parentCode)
-    }
-
-    @Test
-    fun `get countries`() = test {
+    fun `given fetched countries, requesting countries returns a list of all countries`() = test {
         fetchCountries()
-
         val sampleCountries = sampleData.filter { it.parentCode == "" }
+
         val countries = store.getCountries().sortedBy { it.code }
 
-        Assertions.assertThat(countries.size).isEqualTo(sampleCountries.size)
+        assertThat(countries).isEqualTo(sampleCountries)
+    }
 
-        countries.forEachIndexed { i, country ->
-            Assertions.assertThat(country.code).isEqualTo(sampleCountries[i].code)
-            Assertions.assertThat(country.name).isEqualTo(sampleCountries[i].name)
-            Assertions.assertThat(country.parentCode).isEqualTo(sampleCountries[i].parentCode)
+    @Test
+    fun `given fetched countries, requesting states for a country with states returns the expected list of states`() =
+        test {
+            fetchCountries()
+            val sampleStates = sampleData.filter { it.parentCode == "CA" }.sortedBy { it.code }
+
+            val states = store.getStates("CA").sortedBy { it.code }
+
+            assertThat(states).isEqualTo(sampleStates)
         }
-    }
 
     @Test
-    fun `get non-empty states`() = test {
-        fetchCountries()
+    fun `given fetched countries, requesting states for a country without states returns empty list`() =
+        test {
+            fetchCountries()
 
-        val sampleStates = sampleData.filter { it.parentCode == "CA" }.sortedBy { it.code }
-        val states = store.getStates("CA").sortedBy { it.code }
+            val states = store.getStates("CZ")
 
-        Assertions.assertThat(states.size).isEqualTo(sampleStates.size)
-
-        states.forEachIndexed { i, state ->
-            Assertions.assertThat(state.code).isEqualTo(sampleStates[i].code)
-            Assertions.assertThat(state.name).isEqualTo(sampleStates[i].name)
-            Assertions.assertThat(state.parentCode).isEqualTo(sampleStates[i].parentCode)
+            assertThat(states).isEqualTo(emptyList<WCLocationModel>())
         }
-    }
 
     @Test
-    fun `get empty states`() = test {
-        fetchCountries()
+    fun `given fetched countries, requesting states with empty country code returns empty list`() =
+        test {
+            fetchCountries()
 
-        val states = store.getStates("CZ")
+            val states = store.getStates("")
 
-        Assertions.assertThat(states).isEqualTo(emptyList<WCLocationModel>())
-    }
-
-    @Test
-    fun `when empty country code is passed, then empty list is returned when getting states`() = test {
-        fetchCountries()
-
-        val states = store.getStates("")
-
-        Assertions.assertThat(states).isEqualTo(emptyList<WCLocationModel>())
-    }
+            assertThat(states).isEqualTo(emptyList<WCLocationModel>())
+        }
 
     private suspend fun fetchCountries(): WooResult<List<WCLocationModel>> {
         val payload = WooPayload(sampleResponse.toTypedArray())
