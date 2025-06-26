f45b789d1ca510daf60ebc0723ca9c083dfc564f
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-06-26 11:14:11 +0100

    Handle accepting UPS DAP terms

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/upsdap/UPSDAPTermsOfServiceViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/upsdap/UPSDAPTermsOfServiceViewModel.kt
@@ -3,45 +3,50 @@ package com.woocommerce.android.ui.orders.wooshippinglabels.upsdap
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.orders.wooshippinglabels.models.OriginShippingAddress
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UPSDAPTermsOfServiceViewModel @Inject constructor(
-    savedState: SavedStateHandle
+    savedState: SavedStateHandle,
+    private val acceptUPSDAPTerms: AcceptUPSDAPTerms
 ) : ScopedViewModel(savedState) {
     companion object {
         private const val TERMS_URL = "https://www.ups.com/assets/resources/webcontent/" +
-            "en_US/ups_dap_supplemental_tc.pdf"
+                "en_US/ups_dap_supplemental_tc.pdf"
         private const val PROHIBITED_ITEMS_URL = "https://www.ups.com/us/en/support/shipping-support/" +
-            "shipping-special-care-regulated-items/prohibited-items.page"
+                "shipping-special-care-regulated-items/prohibited-items.page"
         private const val TECHNOLOGY_AGREEMENT_URL = "https://www.ups.com/assets/resources/webcontent/en_US/UTA.pdf"
     }
 
     private val args: UPSDAPTermsOfServiceBottomSheetFragmentArgs by savedState.navArgs()
 
-    private var isTermsOfServiceAccepted = savedState.getStateFlow(
+    private val isTermsOfServiceAccepted = savedState.getStateFlow(
         scope = viewModelScope,
         key = "terms_of_service_accepted",
         initialValue = false
     )
-    private var isProhibitedItemsChecked = savedState.getStateFlow(
+    private val isProhibitedItemsChecked = savedState.getStateFlow(
         scope = viewModelScope,
         key = "prohibited_items_checked",
         initialValue = false
     )
-    private var isTechnologyAgreementChecked = savedState.getStateFlow(
+    private val isTechnologyAgreementChecked = savedState.getStateFlow(
         scope = viewModelScope,
         key = "technology_agreement_checked",
         initialValue = false
     )
+
+    private val isLoading = MutableStateFlow(false)
 
     private val conditionsState = combine(
         isTermsOfServiceAccepted,
@@ -64,8 +69,9 @@ class UPSDAPTermsOfServiceViewModel @Inject constructor(
         )
     }
 
-    val viewState = conditionsState.map { conditionsState ->
+    val viewState = combine(isLoading, conditionsState) { isLoading, conditionsState ->
         ViewState(
+            isLoading = isLoading,
             originShippingAddress = args.originAddress,
             conditionsState = conditionsState,
             onUrlClicked = ::onUrlClicked,
@@ -84,8 +90,18 @@ class UPSDAPTermsOfServiceViewModel @Inject constructor(
     }
 
     private fun onContinueClicked() {
-        // TODO handle accepting the terms and conditions
-        triggerEvent(MultiLiveEvent.Event.ExitWithResult(Unit))
+        launch {
+            isLoading.value = true
+            acceptUPSDAPTerms(args.originAddress).fold(
+                onSuccess = {
+                    triggerEvent(MultiLiveEvent.Event.ExitWithResult(Unit))
+                },
+                onFailure = {
+                    triggerEvent(MultiLiveEvent.Event.ShowSnackbar(R.string.error_generic))
+                }
+            )
+            isLoading.value = false
+        }
     }
 
     data class ConditionsState(
@@ -98,6 +114,7 @@ class UPSDAPTermsOfServiceViewModel @Inject constructor(
     )
 
     data class ViewState(
+        val isLoading: Boolean,
         val originShippingAddress: OriginShippingAddress,
         val conditionsState: ConditionsState,
         val onUrlClicked: (String) -> Unit,
@@ -105,7 +122,7 @@ class UPSDAPTermsOfServiceViewModel @Inject constructor(
     ) {
         val areAllConditionsAccepted: Boolean
             get() = conditionsState.isTermsOfServiceChecked &&
-                conditionsState.isProhibitedItemsChecked &&
-                conditionsState.isTechnologyAgreementChecked
+                    conditionsState.isProhibitedItemsChecked &&
+                    conditionsState.isTechnologyAgreementChecked
     }
 }
