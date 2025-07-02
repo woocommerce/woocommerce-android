c426fd2ae937d06a2c053c2a27e204bd7e646300
Andrey <akidinov@gmail.com>
2025-07-02 14:09:40 +0200

    Refactor event handling in WooPosItemsSearchHelper to close search on checkout click

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemsSearchHelper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemsSearchHelper.kt
@@ -47,10 +47,6 @@ class WooPosItemsSearchHelper @Inject constructor(
                         updateLoadingState(isLoading = false)
                     }
 
-                    is ParentToChildrenEvent.OrderSuccessfullyPaid -> {
-                        onCloseSearchClicked()
-                    }
-
                     is ParentToChildrenEvent.SearchEvent.RecentSearchSelected -> {
                         onSearchChanged(event.query, event.query.length)
                     }
@@ -59,16 +55,19 @@ class WooPosItemsSearchHelper @Inject constructor(
                             onSearchChanged("", 0)
                         }
                     }
+                    is ParentToChildrenEvent.CheckoutClicked -> {
+                        onCloseSearchClicked()
+                    }
 
                     is ParentToChildrenEvent.BackFromCheckoutToCartClicked -> Unit
                     is ParentToChildrenEvent.BarcodeEvent -> Unit
                     is ParentToChildrenEvent.ItemClickedInItemsList -> Unit
-                    is ParentToChildrenEvent.CheckoutClicked -> Unit
                     is ParentToChildrenEvent.SearchEvent.ChangedQuery -> Unit
                     is ParentToChildrenEvent.OrderCreated -> Unit
                     is ParentToChildrenEvent.CouponsRemoved -> Unit
                     is ParentToChildrenEvent.RemoveCouponsClicked -> Unit
                     is ParentToChildrenEvent.CouponsValidationFailed -> Unit
+                    is ParentToChildrenEvent.OrderSuccessfullyPaid  -> Unit
                 }
             }
         }
