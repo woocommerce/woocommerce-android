a0374be7b27dac999ba69d55c61fb94e0c554926
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-07-03 12:56:32 +0100

    Remove the "async" argument
    The plugin ignores this argument.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/networking/WooShippingLabelRestClient.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/networking/WooShippingLabelRestClient.kt
@@ -150,7 +150,6 @@ class WooShippingLabelRestClient @Inject constructor(
             site = site,
             path = url,
             body = mapOf(
-                "async" to true,
                 "origin" to origin,
                 "destination" to destination,
                 // For this purchase endpoint, `id` represents the shipment ID instead of the package ID
