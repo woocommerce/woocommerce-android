0c746f9d42f94ae40b09e9ae22b7d81c34785477
Irfan Ömür <irfan.omur@automattic.com>
2025-07-05 02:19:34 +0300

    Add printFile function to open PDF reader for customs form

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationFragment.kt
@@ -142,8 +142,17 @@ class WooShippingLabelCreationFragment : BaseFragment(), BackPressListener {
                 is WooShippingLabelCreationViewModel.NavigateToUPSDAPTermsOfService -> navigateToUPSDAPTermsOfService(
                     event.originAddress
                 )
+
+                is WooShippingLabelCreationViewModel.PrintCustomsForm -> printFile(event.file)
             }
         }
+    }
+
+    /**
+     * This just opens the default PDF reader of the device
+     */
+    private fun printFile(file: File) {
+        ActivityUtils.previewPDFFile(requireActivity(), file)
     }
 
     private fun setupResultHandlers() {
