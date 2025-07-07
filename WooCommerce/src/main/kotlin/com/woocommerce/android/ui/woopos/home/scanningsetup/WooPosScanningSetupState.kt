1cc154071b50082533f0c2d617a3d9f07d8899e7
Andrey <akidinov@gmail.com>
2025-07-07 11:39:03 +0200

    Add scanning setup dialog and state management for Bluetooth barcode scanner configuration

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/scanningsetup/WooPosScanningSetupState.kt
@@ -0,0 +1,75 @@
+package com.woocommerce.android.ui.woopos.home.scanningsetup
+
+import android.os.Parcelable
+import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class WooPosScanningSetupState(
+    val isVisible: Boolean = false,
+    val currentStep: ScanningSetupStep? = null,
+) : Parcelable {
+    sealed class ScanningSetupStep : Parcelable {
+        @Parcelize
+        data class Welcome(
+            val title: String,
+            val message: String,
+            val bluetoothOptionTitle: String,
+            val bluetoothOptionDescription: String,
+            val skipButtonText: String,
+        ) : ScanningSetupStep()
+
+        @Parcelize
+        data class Introduction(
+            val title: String,
+            val message: String,
+            val primaryButtonText: String,
+            val secondaryButtonText: String,
+        ) : ScanningSetupStep()
+
+        @Parcelize
+        data class BluetoothWarning(
+            val title: String,
+            val message: String,
+            val primaryButtonText: String,
+            val secondaryButtonText: String,
+        ) : ScanningSetupStep()
+
+        @Parcelize
+        data class BluetoothPairing(
+            val title: String,
+            val message: String,
+            @DrawableRes val barcodeImageRes: Int,
+            val instructionText: String,
+            val primaryButtonText: String,
+            val secondaryButtonText: String,
+        ) : ScanningSetupStep()
+
+        @Parcelize
+        data class PairOnYourDevice(
+            val title: String,
+            val message: String,
+            @DrawableRes val barcodeImageRes: Int,
+            val instructionText: String,
+            val primaryButtonText: String,
+            val secondaryButtonText: String,
+        ) : ScanningSetupStep()
+
+        @Parcelize
+        data class TestYourScanner(
+            val title: String,
+            val message: String,
+            @DrawableRes val barcodeImageRes: Int,
+            val instructionText: String,
+            val primaryButtonText: String,
+            val secondaryButtonText: String,
+        ) : ScanningSetupStep()
+
+        @Parcelize
+        data class ScannerSetupComplete(
+            val title: String,
+            val message: String,
+            val primaryButtonText: String,
+        ) : ScanningSetupStep()
+    }
+}
