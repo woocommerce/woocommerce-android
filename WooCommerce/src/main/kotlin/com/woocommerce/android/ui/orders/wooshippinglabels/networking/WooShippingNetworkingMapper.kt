c62981a7ba21df834158a319abda5acbeffed288
Irfan Ömür <irfan.omur@automattic.com>
2025-07-10 20:15:14 +0300

    Improve parsing of full names in WooShippingNetworkingMapper
    This commit introduces a new `parseFullName` private method to handle the splitting of full names into first and last names.
    
    The new method improves upon the previous logic by:
    - Correctly handling names with multiple spaces.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/networking/WooShippingNetworkingMapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/networking/WooShippingNetworkingMapper.kt
@@ -93,11 +93,11 @@ class WooShippingNetworkingMapper @Inject constructor(
     }
 
     operator fun invoke(originAddressDTO: OriginAddressDTO): Address {
-        val name = originAddressDTO.name?.split(" ") ?: listOf("", "")
+        val (firstName, lastName) = parseFullName(originAddressDTO.name)
         return Address(
             company = originAddressDTO.company.orEmpty(),
-            firstName = name.getOrElse(0) { "" },
-            lastName = name.getOrElse(1) { "" },
+            firstName = firstName,
+            lastName = lastName,
             phone = originAddressDTO.phone.orEmpty(),
             country = Location(
                 name = originAddressDTO.country.orEmpty(),
@@ -113,7 +113,7 @@ class WooShippingNetworkingMapper @Inject constructor(
     }
 
     operator fun invoke(destinationAddressDTO: DestinationAddressDTO): Address {
-        val name = destinationAddressDTO.name?.split(" ") ?: listOf("", "")
+        val (firstName, lastName) = parseFullName(destinationAddressDTO.name)
         return Address(
             company = destinationAddressDTO.company.orEmpty(),
             address1 = destinationAddressDTO.address.orEmpty(),
@@ -124,16 +124,16 @@ class WooShippingNetworkingMapper @Inject constructor(
                 name = destinationAddressDTO.country.orEmpty(),
                 code = destinationAddressDTO.country.orEmpty()
             ),
-            firstName = name.getOrElse(0) { "" },
+            firstName = firstName,
             phone = destinationAddressDTO.phone.orEmpty(),
             address2 = destinationAddressDTO.address2.orEmpty(),
             email = destinationAddressDTO.email.orEmpty(),
-            lastName = name.getOrElse(1) { "" }
+            lastName = lastName
         )
     }
 
     operator fun invoke(originAddressPurchaseDTO: OriginAddressPurchaseDTO): OriginShippingAddress {
-        val name = originAddressPurchaseDTO.name?.split(" ") ?: listOf("", "")
+        val (firstName, lastName) = parseFullName(originAddressPurchaseDTO.name)
         return OriginShippingAddress(
             id = originAddressPurchaseDTO.id.orEmpty(),
             address1 = originAddressPurchaseDTO.address,
@@ -142,13 +142,13 @@ class WooShippingNetworkingMapper @Inject constructor(
             state = originAddressPurchaseDTO.state,
             postcode = originAddressPurchaseDTO.postcode.orEmpty(),
             country = originAddressPurchaseDTO.country.orEmpty(),
-            firstName = name.getOrElse(0) { "" },
+            firstName = firstName,
             company = originAddressPurchaseDTO.company,
             phone = originAddressPurchaseDTO.phone.orEmpty(),
             email = originAddressPurchaseDTO.email.orEmpty(),
             isDefault = false,
             isVerified = originAddressPurchaseDTO.isVerified,
-            lastName = name.getOrElse(1) { "" }
+            lastName = lastName
         )
     }
 
@@ -389,4 +389,14 @@ class WooShippingNetworkingMapper @Inject constructor(
                 category = hazmatSelection.requestFieldValue
             )
         } ?: HazmatDTO()
+
+    private fun parseFullName(name: String?): Pair<String, String> {
+        val safeName = name.orEmpty()
+        val lastSpaceIndex = safeName.indexOfLast { it == ' ' }
+        return if (lastSpaceIndex == -1) {
+            "" to ""
+        } else {
+            safeName.substring(0, lastSpaceIndex) to safeName.substring(lastSpaceIndex + 1)
+        }
+    }
 }
