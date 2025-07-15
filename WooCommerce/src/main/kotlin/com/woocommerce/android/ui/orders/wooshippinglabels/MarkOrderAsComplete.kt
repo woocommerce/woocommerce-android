c33d1f7e669ee6c54e86151042f8f8871930ec41
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-07-15 22:30:48 +0300

    Use if instead of when

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/MarkOrderAsComplete.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/MarkOrderAsComplete.kt
@@ -2,7 +2,6 @@ package com.woocommerce.android.ui.orders.wooshippinglabels
 
 import com.woocommerce.android.OnChangedException
 import com.woocommerce.android.ui.orders.details.OrderDetailRepository
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.CoreOrderStatus
 import org.wordpress.android.fluxc.store.WCOrderStore.UpdateOrderResult
@@ -18,9 +17,10 @@ class MarkOrderAsComplete @Inject constructor(
         )
             .first { it is UpdateOrderResult.RemoteUpdateResult }
 
-        return when {
-            result.event.isError -> Result.failure(OnChangedException(result.event.error))
-            else -> Result.success(Unit)
+        return if (result.event.isError) {
+            Result.failure(OnChangedException(result.event.error))
+        } else {
+            Result.success(Unit)
         }
     }
 }
