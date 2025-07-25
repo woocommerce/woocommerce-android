0de29002100f152dc88207313c8330039fed28ec
Andrey <akidinov@gmail.com>
2025-07-25 15:48:36 +0200

    Fixed formatting

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeScreen.kt
@@ -136,9 +136,10 @@ private fun WooPosHomeScreen(
                 onBarcodeEvent = { result ->
                     onHomeUIEvent(WooPosHomeUIEvent.OnBarcodeEvent(result))
                 },
-                enabled = (state.screenPositionState is WooPosHomeState.ScreenPositionState.Cart ||
-                        state.screenPositionState is WooPosHomeState.ScreenPositionState.Checkout.FullScreenTotals) &&
-                    state.dialogState !is WooPosHomeState.DialogState.ScanningSetupDialog
+                enabled = (
+                    state.screenPositionState is WooPosHomeState.ScreenPositionState.Cart ||
+                        state.screenPositionState is WooPosHomeState.ScreenPositionState.Checkout.FullScreenTotals
+                    ) && state.dialogState !is WooPosHomeState.DialogState.ScanningSetupDialog
             )
     ) {
         Row(
