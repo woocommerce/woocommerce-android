d39bc9a3c10a96453e2b966cc3be9d90221e6337
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-07-28 13:16:52 +0300

    Introduce Material3 theme definition

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/theme/Theme.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/theme/Theme.kt
@@ -1,9 +1,11 @@
 package com.woocommerce.android.ui.compose.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme as Material2Theme
+import androidx.compose.material.Surface as Material2Surface
 
 /**
  * This theme should be used to support light/dark colors if the composable root of the view tree
@@ -25,24 +27,42 @@ fun WooTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
+    val material2Colors = if (!useDarkTheme) {
         Material2LightColors
     } else {
         Material2DarkColors
     }
 
+    val material3Colors = if (!useDarkTheme) {
+        Material3LightColorScheme
+    } else {
+        Material3DarkColorScheme
+    }
+
     MaterialTheme(
-        colors = colors,
-        typography = Material2Typography,
-        content = content
-    )
+        colorScheme = material3Colors,
+        shapes = MaterialTheme.shapes.copy(
+            small = Material2Theme.shapes.small,
+            medium = Material2Theme.shapes.medium,
+            large = Material2Theme.shapes.large
+        ),
+        typography = Material3Typography
+    ) {
+        Material2Theme(
+            colors = material2Colors,
+            typography = Material2Typography,
+            content = content
+        )
+    }
 }
 
 @Composable
 private fun SurfacedContent(
     content: @Composable () -> Unit
 ) {
-    Surface(color = MaterialTheme.colors.background) {
-        content()
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Material2Surface(color = Material2Theme.colors.background) {
+            content()
+        }
     }
 }
