a276a108863422979c37b8d174a450b7f431b67a
samiuelson <samiuelson@gmail.com>
2025-07-30 15:16:09 +0200

    Update to material3

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ai/ProductImageRemoveBackgroundFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ai/ProductImageRemoveBackgroundFragment.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -78,13 +80,16 @@ class ProductImageRemoveBackgroundFragment : BaseFragment() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductImageRemoveBackgroundScreen(productImage: Product.Image) {
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(stringResource(R.string.remove_background)) },
-                backgroundColor = MaterialTheme.colors.surface
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                )
             )
         }
     ) { paddingValues ->
