fce90bdf745023c92b08b3e37559ecdc41631f51
Andrey <akidinov@gmail.com>
2025-07-30 16:14:56 +0200

    Remove unused WindowInsets imports and simplify padding in WooLogViewerScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/WooLogViewerScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/WooLogViewerScreen.kt
@@ -4,15 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -147,8 +141,7 @@ fun WooLogViewerScreen(
         },
         modifier = Modifier
             .fillMaxSize()
-            .background(color = colorResource(id = R.color.color_toolbar))
-            .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Top)),
+            .background(color = colorResource(id = R.color.color_toolbar)),
     ) { padding ->
         Column(modifier = Modifier.padding(padding)) {
             SearchLayoutWithParams(
@@ -230,7 +223,6 @@ private fun LogViewerEntries(
     LazyColumn(
         state = lazyListState,
         modifier = modifier,
-        contentPadding = WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom).asPaddingValues(),
     ) {
         itemsIndexed(entries) { index, entry ->
             LogViewerEntry(
