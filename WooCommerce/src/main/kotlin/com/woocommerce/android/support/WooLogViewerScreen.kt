18fe651e370f58cb59dd3d3cc418ec3995272b8d
Andrey <akidinov@gmail.com>
2025-07-31 08:54:00 +0200

    Add contentWindowInsets to fillMaxSize modifier in WooLogViewerScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/WooLogViewerScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/WooLogViewerScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -142,7 +143,8 @@ fun WooLogViewerScreen(
             )
         },
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        contentWindowInsets = WindowInsets(0.dp),
     ) { padding ->
         Column(modifier = Modifier.padding(padding)) {
             SearchLayoutWithParams(
