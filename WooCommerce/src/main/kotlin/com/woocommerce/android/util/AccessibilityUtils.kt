dd4d810f3f10610473746cdd825f45c33d90ac79
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-08-01 13:18:04 +0200

    Improve accessibility utility method

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/AccessibilityUtils.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/AccessibilityUtils.kt
@@ -2,7 +2,7 @@ package com.woocommerce.android.util
 
 import android.view.View
 
-fun View.announceAccessibilityChange(message: String) {
-    this.accessibilityLiveRegion = View.ACCESSIBILITY_LIVE_REGION_POLITE
+fun View.announceAccessibilityChange(message: String, liveRegionMode: Int = View.ACCESSIBILITY_LIVE_REGION_POLITE) {
+    this.accessibilityLiveRegion = liveRegionMode
     this.contentDescription = message
 }
