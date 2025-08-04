c77e0c12fd25a1f9b1d82ab2332cf439813530fa
samiuelson <samiuelson@gmail.com>
2025-08-04 16:32:54 +0200

    Rename fun

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ai/ProductImageRemoveBackgroundViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ai/ProductImageRemoveBackgroundViewModel.kt
@@ -9,7 +9,6 @@ import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -51,13 +50,13 @@ class ProductImageRemoveBackgroundViewModel @Inject constructor(
 
                     triggerEvent(MultiLiveEvent.Event.ShowSnackbar(errorMessage))
                     triggerEvent(MultiLiveEvent.Event.Exit)
-                    trackError(error)
+                    trackBackgroundRemovalError(error)
                 }
             )
         }
     }
 
-    private fun trackError(error: Throwable) {
+    private fun trackBackgroundRemovalError(error: Throwable) {
         analyticsTrackerWrapper.track(
             stat = AnalyticsEvent.PRODUCT_IMAGE_REMOVE_BACKGROUND_ERROR,
             errorContext = this.javaClass.simpleName,
