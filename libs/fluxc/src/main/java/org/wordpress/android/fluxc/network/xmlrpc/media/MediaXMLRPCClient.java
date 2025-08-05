080d5fd1d172e670b1157d2bfd49e9ec85ca861b
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-08-05 15:27:14 +0300

    Use the explicit `userAgent` property instead of `toString()`

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/media/MediaXMLRPCClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/media/MediaXMLRPCClient.java
@@ -191,7 +191,7 @@ public class MediaXMLRPCClient extends BaseXMLRPCClient implements ProgressListe
         Builder builder = new Request.Builder()
                 .url(url)
                 .post(requestBody)
-                .addHeader("User-Agent", mUserAgent.toString())
+                .addHeader("User-Agent", mUserAgent.getUserAgent())
                 .addHeader("Accept", "*/*");
 
         if (authString != null) {
