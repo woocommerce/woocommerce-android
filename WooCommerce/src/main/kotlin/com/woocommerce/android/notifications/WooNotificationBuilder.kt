1e57ffeea0d992f4fab1bffa5e19180673440001
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-08-05 13:04:39 +0200

    Add extra notification check at the time of displaying the notification
    We currently handle runtime permissions for push notifications in the app. However this permission could be revoked and we are not re checking it again when the time of displaying the notification comes. This commit adds an extra check right before displaying the notification to avoid any potential crashes.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationBuilder.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/WooNotificationBuilder.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.notifications
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
@@ -183,7 +184,10 @@ class WooNotificationBuilder @Inject constructor(
                 flags
             )
             builder.setContentIntent(pendingIntent)
-            NotificationManagerCompat.from(context).notify(pushId, builder.build())
+            @SuppressLint("MissingPermission") // Lint won't detect we are checking the permission right below
+            if(isNotificationsEnabled()) {
+                NotificationManagerCompat.from(context).notify(pushId, builder.build())
+            }
         } catch (e: RemoteException) {
             // see https://github.com/woocommerce/woocommerce-android/issues/920
             WooLog.e(WooLog.T.NOTIFS, e)
