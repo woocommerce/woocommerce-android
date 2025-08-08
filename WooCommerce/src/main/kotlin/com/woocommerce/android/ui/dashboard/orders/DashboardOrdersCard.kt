e37cd113287641142d9efadb98032917da727eaf
Andrey <akidinov@gmail.com>
2025-08-08 16:02:39 +0200

    Remove unused import statement in DashboardOrdersCard.kt

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/orders/DashboardOrdersCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/orders/DashboardOrdersCard.kt
@@ -1,6 +1,5 @@
 package com.woocommerce.android.ui.dashboard.orders
 
-import android.os.Bundle
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
