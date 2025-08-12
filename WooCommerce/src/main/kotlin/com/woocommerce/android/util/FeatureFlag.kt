3861c13d4505f64aa4f025d5ef60be701a30274e
Andrey <akidinov@gmail.com>
2025-08-12 10:16:35 +0200

    Fixed merge conflict wrong resolution

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -26,7 +26,8 @@ enum class FeatureFlag {
             WOO_POS_SETTINGS,
             WC_SHIPPING_BANNER,
             BETTER_CUSTOMER_SEARCH_M2,
-            ORDER_CREATION_AUTO_TAX_RATE -> PackageUtils.isDebugBuild()
+            ORDER_CREATION_AUTO_TAX_RATE,
+            AI_PRODUCT_IMAGE_BACKGROUND_REMOVAL -> PackageUtils.isDebugBuild()
 
             NEW_SHIPPING_SUPPORT,
             BULK_UPDATE_ORDERS_STATUS,
