12f268984a9e8f3dc6b5c8047e3adfc234158126
Andrey <akidinov@gmail.com>
2025-08-12 10:35:09 +0200

    Add toolbar with back navigation to WooPosSettingsScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/WooPosSettingsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/WooPosSettingsScreen.kt
@@ -1,28 +1,55 @@
 package com.woocommerce.android.ui.woopos.settings
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
 import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosText
+import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosToolbar
+import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosTheme
 import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosTypography
 import com.woocommerce.android.ui.woopos.root.navigation.WooPosNavigationEvent
 
 @Composable
-@Suppress("UnusedParameter")
 fun WooPosSettingsScreen(
     onNavigationEvent: (WooPosNavigationEvent) -> Unit,
-    viewModel: WooPosSettingsViewModel = hiltViewModel()
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val viewModel: WooPosSettingsViewModel = hiltViewModel()
+    BackHandler { onNavigationEvent(WooPosNavigationEvent.GoBack) }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        WooPosText(
-            text = "Settings Screen",
-            style = WooPosTypography.Heading,
+        WooPosToolbar(
+            titleText = stringResource(R.string.woopos_settings_title),
+            onBackClicked = { onNavigationEvent(WooPosNavigationEvent.GoBack) }
+        )
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            WooPosText(
+                text = "Settings Screen",
+                style = WooPosTypography.Heading,
+            )
+        }
+    }
+}
+
+@WooPosPreview
+@Composable
+fun WooPosSettingsScreenPreview() {
+    WooPosTheme {
+        WooPosSettingsScreen(
+            onNavigationEvent = {}
         )
     }
 }
