30a077322766c2e1b7fe1f2472364c32657d03f8
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-08-13 14:34:17 +0300

    Test: Refactor wc product attribute test fixtures

--- libs/fluxc-tests/src/test/java/org/wordpress/android/fluxc/wc/attributes/WCProductAttributesTestFixtures.kt
+++ libs/fluxc-tests/src/test/java/org/wordpress/android/fluxc/wc/attributes/WCProductAttributesTestFixtures.kt
@@ -6,38 +6,38 @@ import org.wordpress.android.fluxc.model.attribute.WCGlobalAttributeModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.attributes.AttributeApiResponse
 
 object WCProductAttributesTestFixtures {
-    val stubSite = SiteModel().apply { id = 321 }
+    val stubSite = SiteModel().apply { id = 1 }
 
     val attributeCreateResponse by lazy {
         "wc/product-attribute-create.json"
-                .jsonFileAs(AttributeApiResponse::class.java)
+            .jsonFileAs(AttributeApiResponse::class.java)
     }
 
     val attributesFullListResponse by lazy {
         "wc/product-attributes-all.json"
-                .jsonFileAs(Array<AttributeApiResponse>::class.java)
+            .jsonFileAs(Array<AttributeApiResponse>::class.java)
     }
 
     val parsedAttributesList by lazy {
         listOf(
-                WCGlobalAttributeModel(
-                        1,
-                        321,
-                        "Color",
-                        "pa_color",
-                        "select",
-                        "menu_order",
-                        true
-                ),
-                WCGlobalAttributeModel(
-                        2,
-                        321,
-                        "Size",
-                        "pa_size",
-                        "select",
-                        "menu_order",
-                        false
-                )
+            WCGlobalAttributeModel(
+                1,
+                stubSite.id,
+                "Color",
+                "pa_color",
+                "select",
+                "menu_order",
+                true
+            ),
+            WCGlobalAttributeModel(
+                2,
+                stubSite.id,
+                "Size",
+                "pa_size",
+                "select",
+                "menu_order",
+                false
+            )
         )
     }
 }
