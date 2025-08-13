60c66f9da307d0d74364733322fcda2e46d91b57
Andrey <akidinov@gmail.com>
2025-08-13 15:49:41 +0200

    Simplify clickable modifier by removing unnecessary interaction source and ripple indication

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/details/hardware/WooPosHardwareSettingsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/details/hardware/WooPosHardwareSettingsScreen.kt
@@ -1,7 +1,6 @@
 package com.woocommerce.android.ui.woopos.settings.details.hardware
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,11 +12,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -66,10 +63,7 @@ private fun HardwareSettingsMenuItem(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = ripple()
-            ) { onClick() }
+            .clickable(onClick = onClick)
             .padding(WooPosSpacing.Medium.value),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start
