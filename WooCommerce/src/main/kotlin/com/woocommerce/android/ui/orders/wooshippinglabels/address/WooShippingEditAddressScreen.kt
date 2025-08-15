ed9ccd457593e3a9f890b00eb9aadd6fe82605c7
Irfan Ömür <irfan.omur@automattic.com>
2025-08-15 19:43:19 +0300

    Update bottom section design on edit address screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/WooShippingEditAddressScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/WooShippingEditAddressScreen.kt
@@ -39,6 +39,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -355,7 +356,8 @@ fun WooShippingEditAddressScreen(
                     onTextChange = onPhoneChange,
                 )
             }
-            Surface(shadowElevation = 8.dp) {
+            HorizontalDivider()
+            Surface {
                 AddressStatusSection(
                     editableAddress = editableAddress,
                     addressStatus = addressStatus,
