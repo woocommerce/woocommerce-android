2cda46547f489b04f2e5e53a2cf28a78cb56e72c
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-08-19 18:38:37 +0100

    Update background of the shipments list to match other order details sections

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailWooShippingShipmentListView.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/details/views/OrderDetailWooShippingShipmentListView.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -65,7 +66,8 @@ fun OrderDetailWooShippingShipmentListView(
             modifier = Modifier.padding(16.dp)
         )
         Card(
-            shape = RectangleShape
+            shape = RectangleShape,
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerLow),
         ) {
             ShipmentList(
                 shipments = shipments,
