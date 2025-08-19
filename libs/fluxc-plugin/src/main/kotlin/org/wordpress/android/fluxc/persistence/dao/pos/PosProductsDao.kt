2a511e74838fb98ae624f0b84ab92c19b8627f98
malinajirka <malinajirka@gmail.com>
2025-08-19 16:23:31 +0200

    Replace primitive types with LocalId and RemoteID

--- libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/pos/PosProductsDao.kt
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/pos/PosProductsDao.kt
@@ -4,19 +4,21 @@ import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
 import org.wordpress.android.fluxc.persistence.entity.pos.WCPosProductModel
 
 @Dao
 abstract class PosProductsDao {
     @Query("SELECT * FROM PosProductEntity WHERE localSiteId = :localSiteId")
-    abstract fun observeAllProducts(localSiteId: Int): Flow<List<WCPosProductModel>>
+    abstract fun observeAllProducts(localSiteId: LocalId): Flow<List<WCPosProductModel>>
 
     @Query("SELECT * FROM PosProductEntity WHERE localSiteId = :localSiteId AND remoteId = :remoteId")
-    abstract suspend fun getProduct(localSiteId: Int, remoteId: Long): WCPosProductModel?
+    abstract suspend fun getProduct(localSiteId: LocalId, remoteId: RemoteId): WCPosProductModel?
 
     @Upsert
     abstract suspend fun upsertProducts(products: List<WCPosProductModel>)
 
     @Query("DELETE FROM PosProductEntity WHERE localSiteId = :localSiteId AND remoteId = :remoteId")
-    abstract suspend fun deleteProduct(localSiteId: Int, remoteId: Long)
+    abstract suspend fun deleteProduct(localSiteId: LocalId, remoteId: RemoteId)
 }
