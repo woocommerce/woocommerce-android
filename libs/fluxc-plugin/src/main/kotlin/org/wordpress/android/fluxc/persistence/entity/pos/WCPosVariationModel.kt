b9b917b45e102b1881fc32d78cf86aaef0aeca1c
samiuelson <samiuelson@gmail.com>
2025-08-20 12:13:12 +0200

    Add WCPosVariationModel

---
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/persistence/entity/pos/WCPosVariationModel.kt
@@ -0,0 +1,40 @@
+package org.wordpress.android.fluxc.persistence.entity.pos
+
+import androidx.room.Entity
+import androidx.room.Index
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
+
+/**
+ * POS-specific product variation table with limited columns for better performance in POS context.
+ */
+@Entity(
+    tableName = "PosVariationEntity",
+    primaryKeys = ["localSiteId", "remoteProductId", "remoteVariationId"],
+    indices = [
+        Index(value = ["localSiteId", "sku"]),
+        Index(value = ["localSiteId", "remoteProductId"])
+    ]
+)
+data class WCPosVariationModel(
+    val localSiteId: LocalId = LocalId(0),
+    val remoteProductId: RemoteId = RemoteId(0),
+    val remoteVariationId: RemoteId = RemoteId(0),
+    val dateModified: String = "",
+    val sku: String = "",
+    val globalUniqueId: String = "",
+    val variationName: String = "",
+    val price: String = "",
+    val regularPrice: String = "",
+    val salePrice: String = "",
+    val description: String = "",
+    val stockQuantity: Double = 0.0,
+    val stockStatus: String = "",
+    val manageStock: Boolean = false,
+    val backordered: Boolean = false,
+    val attributesJson: String = "",
+    val imageUrl: String = "",
+    val status: String = "",
+    val lastUpdated: String = "",
+    val downloadable: Boolean = false,
+)
