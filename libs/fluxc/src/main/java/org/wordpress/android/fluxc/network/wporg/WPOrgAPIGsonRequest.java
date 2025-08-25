f75b1dd9d639e92bd761c56cf970c1f2b2007c47
malinajirka <malinajirka@gmail.com>
2025-08-25 08:53:19 +0200

    Add headers to WPOrg response

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/wporg/WPOrgAPIGsonRequest.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/wporg/WPOrgAPIGsonRequest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class WPOrgAPIGsonRequest<T> extends GsonRequest<T> {
     public WPOrgAPIGsonRequest(int method, String url, Map<String, String> params, Map<String, Object> body,
-                               Class<T> clazz, Listener<T> listener, BaseErrorListener errorListener) {
+                               Class<T> clazz, MyListener<T> listener, BaseErrorListener errorListener) {
         super(method, params, body, url, clazz, null, listener, errorListener);
         // If it's a GET request, add the parameters to the URL
         if (method == Method.GET) {
