395ca007c090d7ccc0dae56329a0231abd6577a1
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-08-25 17:51:24 +0300

    Test: Move gateway test fixtures and store test to fluxc-plugin
    FYI: This is done in preparation to the subsequent WellSQL to Room
    table migration.

--- libs/fluxc-plugin/src/test/java/org/wordpress/android/fluxc/store/WCGatewayStoreTest.kt
+++ libs/fluxc-plugin/src/test/java/org/wordpress/android/fluxc/store/WCGatewayStoreTest.kt
@@ -1,4 +1,4 @@
-package org.wordpress.android.fluxc.wc.gateways
+package org.wordpress.android.fluxc.store
 
 import com.google.gson.Gson
 import com.yarolegovich.wellsql.WellSql
@@ -15,29 +15,28 @@ import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.gateways.GatewayMapper
 import org.wordpress.android.fluxc.model.gateways.WCGatewayModel
-import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType.NOT_FOUND
+import org.wordpress.android.fluxc.network.BaseRequest
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType.INVALID_ID
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.gateways.GatewayRestClient
 import org.wordpress.android.fluxc.persistence.WCGatewaySqlUtils.GatewaysTable
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
-import org.wordpress.android.fluxc.store.WCGatewayStore
 import org.wordpress.android.fluxc.test
 import org.wordpress.android.fluxc.tools.initCoroutineEngine
 import org.wordpress.android.fluxc.wc.gateways.GatewayTestFixtures.gatewaysResponse
+import org.wordpress.android.fluxc.wc.gateways.GatewayTestFixtures.stubSite
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner::class)
 class WCGatewayStoreTest {
     private val restClient = mock<GatewayRestClient>()
-    private val site = SiteModel().apply { id = 321 }
     private val errorSite = SiteModel().apply { id = 123 }
     private val mapper = GatewayMapper(Gson())
     private lateinit var store: WCGatewayStore
     private val gatewayId = gatewaysResponse.first().gatewayId
-    private val error = WooError(INVALID_ID, NOT_FOUND, "Invalid gateway ID")
+    private val error = WooError(WooErrorType.INVALID_ID, BaseRequest.GenericErrorType.NOT_FOUND, "Invalid gateway ID")
 
     @Before
     fun setUp() {
@@ -45,7 +44,7 @@ class WCGatewayStoreTest {
         val config = SingleStoreWellSqlConfigForTests(
             appContext,
             listOf(GatewaysTable::class.java),
-            WellSqlConfig.ADDON_WOOCOMMERCE
+            WellSqlConfig.Companion.ADDON_WOOCOMMERCE
         )
         WellSql.init(config)
         config.reset()
@@ -73,27 +72,27 @@ class WCGatewayStoreTest {
     @Test
     fun `update gateway`() = test {
         fetchAllTestGateways()
-        val gateway = store.getGateway(site, gatewayId)
+        val gateway = store.getGateway(stubSite, gatewayId)
         assertThat(gateway).isEqualTo(mapper.toModel(gatewaysResponse.first()))
         val gatewayIdCod = GatewayRestClient.GatewayId.CASH_ON_DELIVERY
         val updatedGateway = gatewaysResponse.first().copy(enabled = true)
-        whenever(restClient.updateGateway(site, gatewayIdCod, true))
+        whenever(restClient.updateGateway(stubSite, gatewayIdCod, true))
             .thenReturn(WooPayload(updatedGateway))
 
         store.updateGateway(
-            site = site,
+            site = stubSite,
             gatewayId = gatewayIdCod,
             enabled = true
         )
 
-        assertThat(store.getGateway(site, gatewayId)).isEqualTo(mapper.toModel(updatedGateway))
+        assertThat(store.getGateway(stubSite, gatewayId)).isEqualTo(mapper.toModel(updatedGateway))
     }
 
     @Test
     fun `get gateway`() = test {
         fetchAllTestGateways()
 
-        val gateway = store.getGateway(site, gatewayId)
+        val gateway = store.getGateway(stubSite, gatewayId)
 
         assertThat(gateway).isEqualTo(mapper.toModel(gatewaysResponse.first()))
     }
@@ -102,7 +101,7 @@ class WCGatewayStoreTest {
     fun `get all gateways`() = test {
         fetchAllTestGateways()
 
-        val gateways = store.getAllGateways(site)
+        val gateways = store.getAllGateways(stubSite)
 
         assertThat(gateways.size).isEqualTo(2)
         assertThat(gateways.first()).isEqualTo(mapper.toModel(gatewaysResponse.first()))
@@ -113,7 +112,7 @@ class WCGatewayStoreTest {
 
     private suspend fun fetchAllTestGateways(): WooResult<List<WCGatewayModel>> {
         val fetchGatewaysPayload = WooPayload(gatewaysResponse.toTypedArray())
-        whenever(restClient.fetchAllGateways(site)).thenReturn(fetchGatewaysPayload)
-        return store.fetchAllGateways(site)
+        whenever(restClient.fetchAllGateways(stubSite)).thenReturn(fetchGatewaysPayload)
+        return store.fetchAllGateways(stubSite)
     }
 }
