72f535892b8579274779ccc62274aefef5726ca8
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2025-08-25 17:04:14 +0200

    Add entry point for orders in the POS floating menu

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosToolbarViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosToolbarViewModel.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.woopos.home.toolbar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.automirrored.filled.Help
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.DocumentScanner
 import androidx.compose.material.icons.filled.SearchOff
@@ -182,6 +183,10 @@ class WooPosToolbarViewModel @Inject constructor(
 
             addAll(
                 listOf(
+                    WooPosToolbarState.Menu.MenuItem(
+                        title = R.string.woopos_orders_title,
+                        icon = Icons.AutoMirrored.Filled.List,
+                    ),
                     WooPosToolbarState.Menu.MenuItem(
                         title = R.string.woopos_barcode_scanning_title,
                         icon = Icons.Default.DocumentScanner,
