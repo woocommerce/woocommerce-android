e9efb3fbe7bce800c2ac611aa4c445b20d8b18f8
Olivier Halligon <olivier.halligon@automattic.com>
2025-08-25 13:25:47 +0200

    Install secrets before running Instrumented Tests

--- .buildkite/commands/run-instrumented-tests-emulator.sh
+++ .buildkite/commands/run-instrumented-tests-emulator.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -u
 
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :closed_lock_with_key: Installing Secrets"
+bundle exec fastlane run configure_apply
+
 echo "--- 📋 Checking Android SDK"
 echo "adb --version"
 adb --version
