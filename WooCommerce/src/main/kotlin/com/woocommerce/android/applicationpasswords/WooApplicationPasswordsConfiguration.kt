90d18fb63294c62cd5bc65a5dd1c5ae01791c406
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-08-30 19:31:48 +0100

    Add a feature flag for the feature

--- WooCommerce/src/main/kotlin/com/woocommerce/android/applicationpasswords/WooApplicationPasswordsConfiguration.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/applicationpasswords/WooApplicationPasswordsConfiguration.kt
@@ -2,12 +2,14 @@ package com.woocommerce.android.applicationpasswords
 
 import com.woocommerce.android.BuildConfig
 import com.woocommerce.android.util.DeviceInfo
+import com.woocommerce.android.util.FeatureFlag.APP_PASSWORDS_FOR_JETPACK_SITES
 import jakarta.inject.Inject
 import org.wordpress.android.fluxc.network.rest.wpapi.applicationpasswords.ApplicationPasswordsConfiguration
 
 class WooApplicationPasswordsConfiguration @Inject constructor() : ApplicationPasswordsConfiguration {
     override val isEnabledForSiteCredentials: Boolean = true
-    override val isEnabledForJetpack: Boolean = false
+    override val isEnabledForJetpack: Boolean
+        get() = APP_PASSWORDS_FOR_JETPACK_SITES.isEnabled()
     override val applicationName: String =
         "${BuildConfig.APPLICATION_ID}.app-client.${DeviceInfo.name.replace(' ', '-')}"
 }
