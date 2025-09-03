983752911b8eeb775dadfb97b4c5f8234c1c75d3
Irfan Ömür <irfan.omur@automattic.com>
2025-09-03 13:04:42 +0300

    Fix detekt error
    Added commas in test method names for better readability.

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/ListOrObjectDeserializerTest.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/ListOrObjectDeserializerTest.kt
@@ -16,7 +16,7 @@ class ListOrObjectDeserializerTest {
     data class Item(val a: Int, val b: String)
 
     @Test
-    fun `when array of primitives is deserialized then returns list of ints`() {
+    fun `when array of primitives is deserialized, then returns list of ints`() {
         val json = "[1, 2, 3]"
         val type = object : TypeToken<List<Int>>() {}.type
         val result: List<Int> = gson().fromJson(json, type)
@@ -24,7 +24,7 @@ class ListOrObjectDeserializerTest {
     }
 
     @Test
-    fun `when array of objects is deserialized then returns list of data class`() {
+    fun `when array of objects is deserialized, then returns list of data class`() {
         val json = """[{"a":1,"b":"x"},{"a":2,"b":"y"}]"""
         val type = object : TypeToken<List<Item>>() {}.type
         val result: List<Item> = gson().fromJson(json, type)
@@ -32,7 +32,7 @@ class ListOrObjectDeserializerTest {
     }
 
     @Test
-    fun `when object has numeric string keys then converted to ordered list`() {
+    fun `when object has numeric string keys, then converted to ordered list`() {
         val json = """{"2":"a","1":"b","10":"z"}"""
         val type = object : TypeToken<List<String>>() {}.type
         val result: List<String> = gson().fromJson(json, type)
@@ -41,14 +41,14 @@ class ListOrObjectDeserializerTest {
     }
 
     @Test(expected = JsonParseException::class)
-    fun `when object has non numeric key then throws json parse exception`() {
+    fun `when object has non numeric key, then throws json parse exception`() {
         val json = """{"a":1,"2":2}"""
         val type = object : TypeToken<List<Int>>() {}.type
         gson().fromJson<List<Int>>(json, type)
     }
 
     @Test
-    fun `when json is null then returns null list`() {
+    fun `when json is null, then returns null list`() {
         val json = "null"
         val type = object : TypeToken<List<Int>?>() {}.type
         val result: List<Int>? = gson().fromJson(json, type)
@@ -56,14 +56,14 @@ class ListOrObjectDeserializerTest {
     }
 
     @Test(expected = JsonParseException::class)
-    fun `when json type is string then throws json parse exception`() {
+    fun `when json type is string, then throws json parse exception`() {
         val json = "\"not an array or object\""
         val type = object : TypeToken<List<Int>>() {}.type
         gson().fromJson<List<Int>>(json, type)
     }
 
     @Test
-    fun `when object has numeric keys and objects then converted to ordered list of objects`() {
+    fun `when object has numeric keys and objects, then converted to ordered list of objects`() {
         val json = """{"2":{"a":2,"b":"y"},"1":{"a":1,"b":"x"}}"""
         val type = object : TypeToken<List<Item>>() {}.type
         val result: List<Item> = gson().fromJson(json, type)
