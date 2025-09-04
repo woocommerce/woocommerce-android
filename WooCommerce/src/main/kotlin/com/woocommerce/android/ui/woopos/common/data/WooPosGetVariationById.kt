5095fb6b6da38f3c81c7b4bdf69b232b8e023f35
samiuelson <samiuelson@gmail.com>
2025-09-04 21:24:25 +0200

    Revert WooPosGetVariationById

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetVariationById.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetVariationById.kt
@@ -1,25 +1,18 @@
 package com.woocommerce.android.ui.woopos.common.data
 
+import com.woocommerce.android.model.toAppModel
 import com.woocommerce.android.tools.SelectedSite
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
-import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
-import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
-import org.wordpress.android.fluxc.store.pos.localcatalog.WooPosLocalCatalogStore
+import org.wordpress.android.fluxc.store.WCProductStore
 import javax.inject.Inject
 
 class WooPosGetVariationById @Inject constructor(
-    private val posStore: WooPosLocalCatalogStore,
+    private val store: WCProductStore,
     private val site: SelectedSite,
 ) {
     suspend operator fun invoke(productId: Long, variationId: Long): WooPosVariation? = withContext(IO) {
         val siteModel = site.getOrNull() ?: return@withContext null
-        val result = posStore.getVariation(
-            siteId = LocalId(siteModel.id),
-            productId = RemoteId(productId),
-            variationId = RemoteId(variationId)
-        )
-
-        result.getOrNull()?.toWooPosVariation()
+        store.getVariationByRemoteId(siteModel, productId, variationId)?.toAppModel()?.toWooPosVariation()
     }
 }
