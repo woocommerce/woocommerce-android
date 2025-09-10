f5884ec487f2902f352ebac3ea40f1fa5b7b85d7
malinajirka <malinajirka@gmail.com>
2025-09-10 14:47:26 +0200

    Fix variation comparison

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/searchbyidentifier/WooPosSearchByIdentifierResultConverter.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/searchbyidentifier/WooPosSearchByIdentifierResultConverter.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.woopos.common.data.searchbyidentifier
 
 import com.woocommerce.android.ui.woopos.common.data.WooPosProductsCache
+import com.woocommerce.android.ui.woopos.common.data.models.WooPosProductModelVersion2
 import javax.inject.Inject
 
 class WooPosSearchByIdentifierResultConverter @Inject constructor(
@@ -15,7 +16,7 @@ class WooPosSearchByIdentifierResultConverter @Inject constructor(
         return when (result) {
             is WooPosSearchByIdentifierResult.Success -> {
                 val product = result.product
-                if (product.type.value.equals("variation", ignoreCase = true)) {
+                if (product.type == WooPosProductModelVersion2.WooPosProductType.VARIATION) {
                     variationProcess(product)
                 } else {
                     productsCache.addAll(listOf(product))
