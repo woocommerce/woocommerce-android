b963275a1616c1759bce742dd573c78df7d72500
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-09-15 16:29:37 +0100

    Add skeleton for CIAB eligibility checking

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ciab/CIABSiteGateKeeper.kt
@@ -0,0 +1,29 @@
+package com.woocommerce.android.ciab
+
+import androidx.annotation.VisibleForTesting
+import com.woocommerce.android.tools.SelectedSite
+import javax.inject.Inject
+
+class CIABSiteGateKeeper @Inject constructor(private val selectedSite: SelectedSite) {
+    fun isFeatureSupported(
+        @Suppress("unused")
+        feature: CIABAffectedFeature
+    ): Boolean {
+        // For now, all affected features are unsupported in CIAB.
+        // If there are exceptions in the future, we can handle them here.
+        return !isCurrentSiteCIAB()
+    }
+
+    fun isFeatureUnsupported(feature: CIABAffectedFeature): Boolean {
+        return !isFeatureSupported(feature)
+    }
+
+    private fun isCurrentSiteCIAB(): Boolean {
+        TODO()
+    }
+
+    companion object Companion {
+        @VisibleForTesting
+        const val CIAB_GARDEN_NAME = "commerce"
+    }
+}
