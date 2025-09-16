206e20d2f00f4dce27b24f9cb114b96391d5166e
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-09-16 20:29:26 +0200

    Fix warning

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/BookingListFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/BookingListFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.woocommerce.android.R
