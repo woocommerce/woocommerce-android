cbb8166e38c0f4cc0516a17a26fe064fa2eb926a
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-09-17 11:02:53 +0200

    Remove TODO to fix detekt failure

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/ShowBookingsTab.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/ShowBookingsTab.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 
 class ShowBookingsTab @Inject constructor() {
     suspend operator fun invoke(): Result<Boolean> = withContext(Dispatchers.IO) {
-        // TODO: Fetch if site has any published bookable product AND if site is CIAB
+        // Add here: Fetch if site has any published bookable product AND if site is CIAB
         return@withContext Result.success(FeatureFlag.BOOKINGS_MVP.isEnabled())
     }
 }
