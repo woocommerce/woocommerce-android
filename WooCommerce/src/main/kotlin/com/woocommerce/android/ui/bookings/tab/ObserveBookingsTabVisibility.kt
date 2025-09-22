2ad102ad397840ef4c7ee4fcafe17d800c832ccf
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-09-22 18:26:07 +0200

    Avoid fetching bookings over the network if site is not CIAB

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/ObserveBookingsTabVisibility.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/ObserveBookingsTabVisibility.kt
@@ -8,6 +8,8 @@ import com.woocommerce.android.util.FeatureFlag
 import com.woocommerce.android.util.WooLog
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import org.wordpress.android.fluxc.model.SiteModel
@@ -18,23 +20,30 @@ class ObserveBookingsTabVisibility @Inject constructor(
     private val productListRepository: ProductListRepository
 ) {
 
-    operator fun invoke(siteModel: SiteModel): Flow<Boolean> = productListRepository.observeProductsCount(
-        filterOptions = mapOf(
-            ProductFilterOption.STATUS to ProductStatus.PUBLISH.value,
-            ProductFilterOption.TYPE to BOOKING_PRODUCT_TYPE
-        ),
-        excludeSampleProducts = true
-    ).onStart {
-        productListRepository.fetchProductList(
-            productFilterOptions = mapOf(ProductFilterOption.TYPE to BOOKING_PRODUCT_TYPE)
-        ).onFailure {
-            WooLog.w(WooLog.T.BOOKINGS, "Failed to fetch bookable products")
+    operator fun invoke(siteModel: SiteModel): Flow<Boolean> = flow {
+        val isCIABSite = FeatureFlag.BOOKINGS_MVP.isEnabled() && siteModel.isCIABSite()
+        if (!isCIABSite) {
+            emit(false)
+        } else {
+            emitAll(
+                productListRepository.observeProductsCount(
+                    filterOptions = mapOf(
+                        ProductFilterOption.STATUS to ProductStatus.PUBLISH.value,
+                        ProductFilterOption.TYPE to BOOKING_PRODUCT_TYPE
+                    ),
+                    excludeSampleProducts = true
+                )
+                    .map { count -> count > 0 }
+                    .onStart {
+                        productListRepository.fetchProductList(
+                            productFilterOptions = mapOf(ProductFilterOption.TYPE to BOOKING_PRODUCT_TYPE)
+                        ).onFailure {
+                            WooLog.w(WooLog.T.BOOKINGS, "Failed to fetch bookable products")
+                        }
+                    }.distinctUntilChanged()
+            )
         }
     }
-        .map { productsCount ->
-            productsCount > 0 && siteModel.isCIABSite() && FeatureFlag.BOOKINGS_MVP.isEnabled()
-        }
-        .distinctUntilChanged()
 
     companion object {
         @VisibleForTesting
