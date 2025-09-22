ed7dfe09b64eb7dfac309a7fe749b0cdb36b9616
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-09-22 14:44:56 +0100

    Minor refactoring of ObserveStockWidgetStatus

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/data/ObserveStockWidgetStatus.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/data/ObserveStockWidgetStatus.kt
@@ -6,35 +6,33 @@ import com.woocommerce.android.ciab.CIABSiteGateKeeper
 import com.woocommerce.android.model.DashboardWidget
 import com.woocommerce.android.tools.SelectedSite
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.transformLatest
 import javax.inject.Inject
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class ObserveStockWidgetStatus @Inject constructor(
     private val selectedSite: SelectedSite,
     private val observePublishedProductsCount: ObservePublishedProductsCount,
     private val ciabSiteGateKeeper: CIABSiteGateKeeper
 ) {
+    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke() = selectedSite.observe()
         .filterNotNull()
-        .transformLatest { site ->
+        .flatMapLatest { site ->
             if (ciabSiteGateKeeper.isFeatureUnsupported(CIABAffectedFeature.ProductsStockDashboardCard)) {
-                emit(DashboardWidget.Status.Hidden)
+                flowOf(DashboardWidget.Status.Hidden)
             } else {
-                emitAll(
-                    observePublishedProductsCount().map { hasPublishedProducts ->
-                        if (hasPublishedProducts) {
-                            DashboardWidget.Status.Available
-                        } else {
-                            DashboardWidget.Status.Unavailable(
-                                badgeText = R.string.my_store_widget_unavailable
-                            )
-                        }
+                observePublishedProductsCount().map { hasPublishedProducts ->
+                    if (hasPublishedProducts) {
+                        DashboardWidget.Status.Available
+                    } else {
+                        DashboardWidget.Status.Unavailable(
+                            badgeText = R.string.my_store_widget_unavailable
+                        )
                     }
-                )
+                }
             }
         }
 }
