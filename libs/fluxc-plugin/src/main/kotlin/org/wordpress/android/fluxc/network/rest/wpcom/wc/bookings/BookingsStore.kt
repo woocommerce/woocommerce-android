b838aa7bdeca58c10968d690c859231983f58b62
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-09-23 17:37:09 +0100

    Update fetchBookings to return whether we can load more bookings or not

--- libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/bookings/BookingsStore.kt
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/bookings/BookingsStore.kt
@@ -11,6 +11,7 @@ import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
 import org.wordpress.android.fluxc.persistence.dao.BookingsDao
 import org.wordpress.android.fluxc.persistence.entity.BookingEntity
 import org.wordpress.android.fluxc.tools.CoroutineEngine
+import org.wordpress.android.fluxc.utils.HeadersParser
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,13 +20,14 @@ import javax.inject.Singleton
 class BookingsStore @Inject constructor(
     private val bookingsRestClient: BookingsRestClient,
     private val bookingsDao: BookingsDao,
+    private val headersParser: HeadersParser,
     private val coroutineEngine: CoroutineEngine,
 ) {
     suspend fun fetchBookings(
         site: SiteModel,
         perPage: Int = BookingsRestClient.DEFAULT_PER_PAGE,
         page: Int = 1
-    ): WooResult<List<BookingEntity>> {
+    ): WooResult<Boolean> {
         return coroutineEngine.withDefaultContext(AppLog.T.API, this, "fetchBookings") {
             val response = bookingsRestClient.fetchBookings(site, perPage, page)
             when {
@@ -33,7 +35,11 @@ class BookingsStore @Inject constructor(
                 response.result != null -> {
                     val entities = response.result.map { it.toEntity(site.localId()) }
                     bookingsDao.insertOrReplace(entities)
-                    WooResult(entities)
+                    val totalPages = headersParser.getTotalPages(response.headers)
+                    // Determine if we can load more from the total pages header if available, otherwise
+                    // infer it from the number of items returned
+                    val canLoadMore = (totalPages?.let { it > page }) ?: (entities.size == perPage)
+                    WooResult(canLoadMore)
                 }
 
                 else -> WooResult(WooError(GENERIC_ERROR, UNKNOWN))
--- libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/utils/HeadersParser.kt
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/utils/HeadersParser.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.fluxc.utils
 
+import org.wordpress.android.fluxc.network.rest.Header
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
@@ -12,15 +13,19 @@ class HeadersParser @Inject constructor(
         private const val SERVER_DATE_HEADER = "date"
     }
 
-    fun <T> getTotalPages(result: WooResult<T>): Int? {
+    fun getTotalPages(headers: List<Header>): Int? {
         return try {
-            result.headers
+            headers
                 .findLast { TOTAL_PAGES_HEADER.equals(it.key, ignoreCase = true) }
                 ?.value?.toInt()
         } catch (e: NumberFormatException) {
             logger.e(AppLog.T.API, "Failed to parse total pages from headers", e)
             null
         }
+    }
+
+    fun <T> getTotalPages(result: WooResult<T>): Int? {
+        return getTotalPages(result.headers)
     }
 
     fun <T> getServerDate(response: WooResult<T>): String? = response.headers
