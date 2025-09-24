e1227bc4bf22e4b663fc5cd395c21eee9b803bfa
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-09-24 16:23:42 +0200

    Moves logic to observe site changes to ObserveBookingsTabVisibility

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/BookingsTabController.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/BookingsTabController.kt
@@ -3,17 +3,12 @@ package com.woocommerce.android.ui.bookings.tab
 import androidx.lifecycle.lifecycleScope
 import com.woocommerce.android.R
 import com.woocommerce.android.databinding.ActivityMainBinding
-import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.main.MainActivity
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class BookingsTabController @Inject constructor(
-    private val observeBookingsTabVisibility: ObserveBookingsTabVisibility,
-    private val selectedSite: SelectedSite
+    private val observeBookingsTabVisibility: ObserveBookingsTabVisibility
 ) {
     private lateinit var activity: MainActivity
     private lateinit var binding: ActivityMainBinding
@@ -27,14 +22,9 @@ class BookingsTabController @Inject constructor(
         checkBookingsTabVisibility()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     private fun checkBookingsTabVisibility() {
         activity.lifecycleScope.launch {
-            selectedSite.observe()
-                .filterNotNull()
-                .flatMapLatest { siteModel ->
-                    observeBookingsTabVisibility(siteModel)
-                }
+            observeBookingsTabVisibility()
                 .collect { isVisible ->
                     binding.bottomNav.menu.findItem(R.id.bookings)?.isVisible = isVisible
                 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/ObserveBookingsTabVisibility.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/ObserveBookingsTabVisibility.kt
@@ -3,14 +3,18 @@ package com.woocommerce.android.ui.bookings.tab
 import androidx.annotation.VisibleForTesting
 import com.woocommerce.android.di.AppCoroutineScope
 import com.woocommerce.android.extensions.isCIABSite
+import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.products.ProductStatus
 import com.woocommerce.android.ui.products.list.ProductListRepository
 import com.woocommerce.android.util.FeatureFlag
 import com.woocommerce.android.util.WooLog
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
@@ -21,10 +25,20 @@ import javax.inject.Inject
 
 class ObserveBookingsTabVisibility @Inject constructor(
     private val productListRepository: ProductListRepository,
+    private val selectedSite: SelectedSite,
     @AppCoroutineScope private val appCoroutineScope: CoroutineScope,
 ) {
 
-    operator fun invoke(siteModel: SiteModel): Flow<Boolean> = flow {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): Flow<Boolean> =
+        selectedSite.observe()
+            .filterNotNull()
+            .flatMapLatest { siteModel ->
+                observeBookingTabVisibility(siteModel)
+            }
+
+
+    private fun observeBookingTabVisibility(siteModel: SiteModel): Flow<Boolean> = flow {
         val isCIABSite = FeatureFlag.BOOKINGS_MVP.isEnabled() && siteModel.isCIABSite()
         if (!isCIABSite) {
             emit(false)
