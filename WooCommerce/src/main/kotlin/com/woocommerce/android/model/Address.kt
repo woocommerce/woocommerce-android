46baeacf707101fd4359417a48fdf113e02fb949
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-09-30 18:56:20 +0200

    temp: introduce regression

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/Address.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/Address.kt
@@ -81,7 +81,7 @@ data class Address(
     fun toShippingLabelModel(): ShippingLabelAddress {
         return ShippingLabelAddress(
             company = company,
-            name = "$firstName $lastName".trim().takeIf { it.isNotBlank() },
+            name = "$firstName $lastName".trim().takeIf { it.isBlank() },
             phone = phone,
             address = address1,
             address2 = address2,
