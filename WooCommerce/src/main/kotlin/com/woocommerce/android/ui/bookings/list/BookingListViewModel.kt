cf6bd9c93e12de1dca37790e8495c408942b2d1f
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-10-01 13:04:38 +0100

    Handle fetching bookings when search query changes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/list/BookingListViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/list/BookingListViewModel.kt
@@ -3,17 +3,22 @@ package com.woocommerce.android.ui.bookings.list
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.AppConstants
 import com.woocommerce.android.R
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getNullableStateFlow
 import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.bookings.BookingsFilterOption
 import javax.inject.Inject
@@ -73,24 +78,33 @@ class BookingListViewModel @Inject constructor(
     }.asLiveData()
 
     init {
-        monitorFilterChanges()
+        monitorSearchAndFilterChanges()
     }
 
-    private fun monitorFilterChanges() {
+    @OptIn(FlowPreview::class)
+    private fun monitorSearchAndFilterChanges() {
         launch {
-            selectedTab.collectLatest {
-                // Cancel any ongoing fetch or load more operations
-                bookingsFetchJob?.cancel()
-                bookingsLoadMoreJob?.cancel()
+            val queryFlow = searchQuery
+                .drop(1) // Skip the initial value to avoid double fetch on init
+                .debounce {
+                    if (it.isNullOrEmpty()) 0L else AppConstants.SEARCH_TYPING_DELAY_MS
+                }
 
-                bookingsFetchJob = fetchBookings(BookingListLoadingState.Loading)
-            }
+            merge(selectedTab, queryFlow)
+                .collectLatest {
+                    // Cancel any ongoing fetch or load more operations
+                    bookingsFetchJob?.cancel()
+                    bookingsLoadMoreJob?.cancel()
+
+                    bookingsFetchJob = fetchBookings(BookingListLoadingState.Loading)
+                }
         }
     }
 
     private fun fetchBookings(initialLoadingState: BookingListLoadingState) = launch {
         loadingState.value = initialLoadingState
         bookingListHandler.loadBookings(
+            searchQuery = searchQuery.value,
             forceRefresh = true,
             filters = prepareFilters()
         ).onFailure {
