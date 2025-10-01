b40de0b89630fa617e5f0ff4846347a1a0543de2
samiuelson <samiuelson@gmail.com>
2025-10-01 16:50:52 +0200

    Run incremental sync on splash screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/splash/WooPosSplashViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.ui.woopos.common.data.WooPosPopularProductsProvider
 import com.woocommerce.android.ui.woopos.home.items.products.WooPosProductsDataSource
+import com.woocommerce.android.ui.woopos.localcatalog.WooPosPerformLocalCatalogIncrementalSync
 import com.woocommerce.android.ui.woopos.orders.WooPosOrdersInMemoryCache
 import com.woocommerce.android.ui.woopos.tab.WooPosCanBeLaunchedInTab
 import com.woocommerce.android.ui.woopos.tab.WooPosLaunchability
@@ -23,13 +24,17 @@ class WooPosSplashViewModel @Inject constructor(
     private val popularProductsProvider: WooPosPopularProductsProvider,
     private val analyticsTracker: WooPosAnalyticsTracker,
     private val posCanBeLaunchedInTab: WooPosCanBeLaunchedInTab,
-    private val ordersCache: WooPosOrdersInMemoryCache
+    private val ordersCache: WooPosOrdersInMemoryCache,
+    performIncrementalSyncUseCase: WooPosPerformLocalCatalogIncrementalSync
 ) : ViewModel() {
     private val _state = MutableStateFlow<WooPosSplashState>(WooPosSplashState.Loading)
     val state: StateFlow<WooPosSplashState> = _state
 
     init {
         val splashScreenStartTime = System.currentTimeMillis()
+
+        performIncrementalSyncUseCase.execute("on splash screen")
+
         viewModelScope.launch {
             val launchability = posCanBeLaunchedInTab()
 
