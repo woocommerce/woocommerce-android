16f633610b9d3bf337077f65a09d87c4fcbbb931
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-10-02 15:48:03 +0200

    Observe booking in the ViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/details/BookingDetailsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/details/BookingDetailsViewModel.kt
@@ -4,12 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.bookings.BookingsRepository
 import com.woocommerce.android.ui.bookings.compose.BookingAttendanceStatus
 import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
@@ -17,6 +20,7 @@ import javax.inject.Inject
 class BookingDetailsViewModel @Inject constructor(
     savedState: SavedStateHandle,
     resourceProvider: ResourceProvider,
+    private val bookingsRepository: BookingsRepository,
 ) : ScopedViewModel(savedState) {
 
     private val navArgs: BookingDetailsFragmentArgs by savedState.navArgs()
@@ -35,6 +39,7 @@ class BookingDetailsViewModel @Inject constructor(
                 toolbarTitle = resourceProvider.getString(R.string.booking_details_title, navArgs.bookingId),
             )
         }
+        observeBooking(navArgs.bookingId)
     }
 
     private fun onAttendanceStatusSelected(status: BookingAttendanceStatus) {
@@ -47,5 +52,13 @@ class BookingDetailsViewModel @Inject constructor(
 
     private fun onCancelBooking() {
         // TODO Add logic to Cancel booking
+    }
+
+    private fun observeBooking(bookingId: Long) {
+        bookingsRepository.observeBooking(bookingId)
+            .onEach { booking ->
+
+            }
+            .launchIn(this)
     }
 }
