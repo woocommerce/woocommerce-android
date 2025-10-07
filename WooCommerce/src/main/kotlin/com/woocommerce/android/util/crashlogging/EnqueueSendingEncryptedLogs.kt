9892c31cde40d49684101550cf60194aa49871d4
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-10-07 14:21:15 +0300

    Fix: Revert enqueue sending encrypted logs back to blocking
    FYI: This is done because we've identified a regression where the actual
    encrypted log, although seen as sent, is not actually ending-up being
    uploaded into our systems. Thus, when someone then tries to browse it,
    based on it UUID, they will get: "Log File Not Found.Invalid Log File"
    
    For more info see: p1759832867149029-slack-CGPNUU63E
    
    PS: The PR that introduced this regression is #14407, which brought-in
    some app logs improvements.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/EnqueueSendingEncryptedLogs.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/EnqueueSendingEncryptedLogs.kt
@@ -3,23 +3,20 @@ package com.woocommerce.android.util.crashlogging
 import com.automattic.android.tracks.crashlogging.EventLevel
 import com.automattic.android.tracks.crashlogging.EventLevel.FATAL
 import com.automattic.encryptedlogging.EncryptedLogging
-import com.woocommerce.android.di.AppCoroutineScope
 import com.woocommerce.android.tools.NetworkStatus
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 class EnqueueSendingEncryptedLogs @Inject constructor(
     private val encryptedLogging: EncryptedLogging,
     private val encryptedLogsFileProvider: EncryptedLogsFileProvider,
     private val networkStatus: NetworkStatus,
-    @AppCoroutineScope private val appCoroutineScope: CoroutineScope
 ) {
     operator fun invoke(
         uuid: String,
         eventLevel: EventLevel
     ) {
-        appCoroutineScope.launch {
+        runBlocking {
             encryptedLogging.enqueueSendingEncryptedLogs(
                 uuid = uuid,
                 file = encryptedLogsFileProvider.provide(),
