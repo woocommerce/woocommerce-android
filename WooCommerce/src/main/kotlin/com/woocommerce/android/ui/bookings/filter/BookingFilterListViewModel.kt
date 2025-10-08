c17ecea93b8e4ca03d5a41383ad1f835a52a25a1
Irfan Ömür <irfan.omur@automattic.com>
2025-10-08 03:13:12 +0300

    Add BookingsFilterListViewModel

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/filter/BookingFilterListViewModel.kt
@@ -0,0 +1,58 @@
+package com.woocommerce.android.ui.bookings.filter
+
+import androidx.annotation.StringRes
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.R
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.bookings.BookingsFilterOption
+import javax.inject.Inject
+
+@HiltViewModel
+class BookingFilterListViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+) : ScopedViewModel(savedStateHandle) {
+
+    private val _options = MutableStateFlow(
+        defaultBookingFilters().map { BookingFilterListItem(title = it.titleRes()) }
+    )
+    val options = _options.asLiveData(viewModelScope.coroutineContext)
+
+    @StringRes
+    private fun BookingsFilterOption.titleRes(): Int = when (this) {
+        BookingsFilterOption.TeamMember -> R.string.bookings_filter_title_team_member
+        BookingsFilterOption.AttendanceStatus -> R.string.bookings_filter_title_attendance_status
+        BookingsFilterOption.PaymentStatus -> R.string.bookings_filter_title_payment_status
+        BookingsFilterOption.BookingType -> R.string.bookings_filter_title_type
+        is BookingsFilterOption.Customer -> R.string.bookings_filter_customer_name
+        BookingsFilterOption.Category -> R.string.bookings_filter_category
+        is BookingsFilterOption.DateRange -> R.string.bookings_filter_title_date
+        BookingsFilterOption.ServiceEvent -> R.string.bookings_filter_title_service_event
+    }
+
+    private fun defaultBookingFilters(): List<BookingsFilterOption> = listOf(
+        BookingsFilterOption.TeamMember,
+        BookingsFilterOption.AttendanceStatus,
+        BookingsFilterOption.PaymentStatus,
+        BookingsFilterOption.BookingType,
+        BookingsFilterOption.Customer(customerId = null),
+        BookingsFilterOption.Category,
+        BookingsFilterOption.DateRange(before = null, after = null),
+        BookingsFilterOption.ServiceEvent,
+    )
+
+    data class BookingFilterListItem(
+        @StringRes val title: Int,
+        val value: String? = null,
+        val onClick: () -> Unit = {}
+    )
+
+    data class BookingFilterListUiState(
+        val items: List<BookingFilterListItem>,
+        val onClose: () -> Unit = {},
+        val onShowBookings: () -> Unit = {},
+    )
+}
