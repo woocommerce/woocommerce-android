19cd00051b280c41ad4ebfbba4c573356a035bda
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-10-14 10:23:18 +0200

    Make the UI of the skeleton closer to the real implementation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/details/BookingDetailsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/details/BookingDetailsScreen.kt
@@ -1,13 +1,16 @@
 package com.woocommerce.android.ui.bookings.details
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,11 +23,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.bookings.compose.BookingAppointmentDetails
 import com.woocommerce.android.ui.bookings.compose.BookingAppointmentDetailsModel
@@ -167,14 +170,23 @@ private fun BookingDetailsLoading() {
                 .background(MaterialTheme.colorScheme.surfaceContainer)
         ) {
             repeat(6) {
-                SkeletonView(
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
-                        .padding(start = 16.dp)
-                        .padding(vertical = 10.dp)
+                        .padding(vertical = 10.dp, horizontal = 16.dp)
                         .fillMaxWidth()
-                        .height(20.dp)
-                        .align(Alignment.End)
-                )
+                ) {
+                    SkeletonView(
+                        modifier = Modifier
+                            .height(20.dp)
+                            .width(50.dp + 10 * (it % 3).dp)
+                    )
+                    SkeletonView(
+                        modifier = Modifier
+                            .height(20.dp)
+                            .width(100.dp + 10 * (it % 5).dp)
+                    )
+                }
                 HorizontalDivider(
                     Modifier.padding(start = 16.dp)
                 )
