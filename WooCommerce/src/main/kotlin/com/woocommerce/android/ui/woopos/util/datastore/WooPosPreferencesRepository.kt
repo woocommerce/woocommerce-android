d8fa5d736c919eb055021ea7d518f93ec2439cdb
malinajirka <malinajirka@gmail.com>
2025-10-15 18:54:26 +0200

    Add periodicLocalCatalogSyncEnabled methods

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/datastore/WooPosPreferencesRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/datastore/WooPosPreferencesRepository.kt
@@ -7,6 +7,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.woocommerce.android.tools.SelectedSite
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -78,6 +79,19 @@ class WooPosPreferencesRepository @Inject constructor(
     suspend fun setAllowCellularDataUpdate(allow: Boolean) {
         dataStore.edit { preferences ->
             preferences[allowCellularDataUpdateKey] = allow
+        }
+    }
+
+    suspend fun isPeriodicSyncEnabledForSite(siteId: Long): Boolean {
+        val key = booleanPreferencesKey("pos_periodic_sync_enabled_$siteId")
+        val preferences = dataStore.data.map { it[key] ?: true }.first()
+        return preferences
+    }
+
+    suspend fun disablePeriodicSyncForSite(siteId: Long) {
+        val key = booleanPreferencesKey("pos_periodic_sync_enabled_$siteId")
+        dataStore.edit { preferences ->
+            preferences[key] = false
         }
     }
 
