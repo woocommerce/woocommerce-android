5f13732e89ca0d35fa54ef51892caa4275144da8
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-10-16 16:22:20 +0100

    Update the staff member endpoint path

--- libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/bookings/BookingsRestClient.kt
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/bookings/BookingsRestClient.kt
@@ -69,7 +69,7 @@ class BookingsRestClient @Inject constructor(
         site: SiteModel,
         resourceId: Long
     ): WooPayload<BookingResourceDto> {
-        val endpoint = WOOCOMMERCE.resources.id(resourceId).pathV2Bookings
+        val endpoint = WOOCOMMERCE.resources.team_members.id(resourceId).pathV2Bookings
 
         val response = wooNetwork.executeGetGsonRequest(
             site = site,
