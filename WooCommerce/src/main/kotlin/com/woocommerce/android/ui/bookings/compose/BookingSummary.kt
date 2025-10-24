36f180cccefb7d084410c4a2842e3c1e331fce77
Irfan Ömür <irfan.omur@automattic.com>
2025-10-24 16:04:18 +0300

    Update BookingSummary component design

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/compose/BookingSummary.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/compose/BookingSummary.kt
@@ -4,20 +4,17 @@ import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.bookings.details.AttendanceUpdateStatus
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
@@ -28,34 +25,29 @@ fun BookingSummary(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        horizontalAlignment = Alignment.Start,
         modifier = modifier
             .background(color = MaterialTheme.colorScheme.surfaceContainer)
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
     ) {
         Text(
             text = model.date,
             color = MaterialTheme.colorScheme.onSurface,
             style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Medium),
         )
-        FlowRow(
-            modifier = Modifier.padding(top = 2.dp),
-        ) {
-            Text(
-                text = "${model.name} • ",
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                style = MaterialTheme.typography.labelMedium.copy(fontSize = 13.sp),
-            )
-            Text(
-                text = model.customerName ?: stringResource(R.string.orderdetail_customer_name_default),
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                style = MaterialTheme.typography.labelMedium.copy(fontSize = 13.sp),
-            )
-        }
+        Text(
+            text = model.name,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            style = MaterialTheme.typography.bodyLarge,
+        )
+        Text(
+            text = model.customerName ?: stringResource(R.string.orderdetail_customer_name_default),
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            style = MaterialTheme.typography.bodyLarge,
+        )
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier
-                .padding(top = 8.dp)
+            modifier = Modifier.padding(top = 6.dp)
         ) {
             model.attendanceStatus?.let {
                 BookingAttendanceStatusTag(
