9441247ca3861fddeeda237207e956bcc4742a67
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-10-28 14:13:27 +0100

    Bump the DB version

--- libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCAndroidDatabase.kt
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCAndroidDatabase.kt
@@ -126,7 +126,7 @@ import org.wordpress.android.fluxc.persistence.migrations.MIGRATION_7_8
 import org.wordpress.android.fluxc.persistence.migrations.MIGRATION_8_9
 import org.wordpress.android.fluxc.persistence.migrations.MIGRATION_9_10
 
-const val WC_DATABASE_VERSION = 71
+const val WC_DATABASE_VERSION = 72
 
 @Database(
     version = WC_DATABASE_VERSION,
