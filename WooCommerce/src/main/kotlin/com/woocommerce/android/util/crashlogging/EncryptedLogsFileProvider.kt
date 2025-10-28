b6e83aba0cbc5321d6931f482449875d901606a7
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-10-28 17:12:04 +0100

    Avoid calling createTempFile function outside of runCatching

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/EncryptedLogsFileProvider.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/crashlogging/EncryptedLogsFileProvider.kt
@@ -10,17 +10,16 @@ class EncryptedLogsFileProvider @Inject constructor(
     private val wooLog: WooLog,
     private val dispatchers: CoroutineDispatchers
 ) {
-    suspend fun provide(): File {
+    suspend fun provide(): File? {
         return withContext(dispatchers.io) {
             runCatching {
                 val logs = wooLog.getCurrentLogEntries().take(LOG_ENTRIES_LIMIT)
                 File.createTempFile("log", "").apply {
                     appendText(logs.joinToString("\n"))
                 }
-            }.getOrElse { exception ->
+            }.onFailure { exception ->
                 WooLog.e(WooLog.T.UTILS, "Could not create log file: ${exception.message}")
-                File.createTempFile("log_empty_file_due_to_error", "")
-            }
+            }.getOrNull()
         }
     }
 
