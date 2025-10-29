dc47dfee2c77d169a33dd688d3fc9aa9180c3bab
Hicham Boushaba <hicham.boushaba@gmail.com>
2025-10-29 18:41:18 +0100

    Improve communication between viewmodels

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/filter/BookingCustomerFilterPage.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/filter/BookingCustomerFilterPage.kt
@@ -1,50 +1,62 @@
 package com.woocommerce.android.ui.bookings.filter
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
 import com.woocommerce.android.model.Order
+import com.woocommerce.android.ui.orders.creation.customerlist.CustomerListGetSupportedSearchModes
+import com.woocommerce.android.ui.orders.creation.customerlist.CustomerListIsAdvancedSearchSupported
+import com.woocommerce.android.ui.orders.creation.customerlist.CustomerListRepository
 import com.woocommerce.android.ui.orders.creation.customerlist.CustomerListSelectionScreen
 import com.woocommerce.android.ui.orders.creation.customerlist.CustomerListSelectionViewModel
-import com.woocommerce.android.ui.orders.creation.customerlist.CustomerSelected
-import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
+import com.woocommerce.android.ui.orders.creation.customerlist.CustomerListViewModelMapper
+import com.woocommerce.android.util.StringUtils
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel(assistedFactory = BookingCustomerFilterViewModel.Factory::class)
+class BookingCustomerFilterViewModel @AssistedInject constructor(
+    savedStateHandle: SavedStateHandle,
+    repository: CustomerListRepository,
+    mapper: CustomerListViewModelMapper,
+    isAdvancedSearchSupported: CustomerListIsAdvancedSearchSupported,
+    getSupportedSearchModes: CustomerListGetSupportedSearchModes,
+    analyticsTracker: AnalyticsTrackerWrapper,
+    stringUtils: StringUtils,
+    @Assisted private val onCustomerSelected: (Order.Customer) -> Unit
+) : CustomerListSelectionViewModel(
+    savedStateHandle,
+    repository,
+    mapper,
+    isAdvancedSearchSupported,
+    getSupportedSearchModes,
+    analyticsTracker,
+    stringUtils
+) {
+    override fun exitWithCustomer(customer: Order.Customer) {
+        this.onCustomerSelected.invoke(customer)
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(onCustomerSelected: (Order.Customer) -> Unit): BookingCustomerFilterViewModel
+    }
+}
 
 @Composable
 fun BookingCustomerFilterPage(
     onCustomerSelected: (Order.Customer) -> Unit,
 ) {
-    val viewModel = hiltViewModel<CustomerListSelectionViewModel>()
+    val viewModel = hiltViewModel { factory: BookingCustomerFilterViewModel.Factory ->
+        factory.create(onCustomerSelected)
+    }
 
     CustomerListSelectionScreen(
         viewModel = viewModel,
         handleInsets = false,
         showToolbar = false
     )
-
-    HandleEvents(viewModel.event, onCustomerSelected)
-}
-
-@Composable
-private fun HandleEvents(
-    event: LiveData<Event>,
-    onCustomerSelected: (Order.Customer) -> Unit
-) {
-    val lifecycleOwner = LocalLifecycleOwner.current
-
-    DisposableEffect(event, lifecycleOwner) {
-        val observer = Observer { event: Event ->
-            when (event) {
-                is CustomerSelected -> onCustomerSelected(event.customer)
-            }
-        }
-
-        event.observe(lifecycleOwner, observer)
-
-        onDispose {
-            event.removeObserver(observer)
-        }
-    }
 }
