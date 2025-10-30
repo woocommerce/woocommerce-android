60c9f47c4c717846098cb5626422834e342fedd8
Irfan Ömür <irfan.omur@automattic.com>
2025-10-30 18:16:49 +0300

    Add BookingTypeFilterRoute composable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/filter/type/BookingTypeFilterPage.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/filter/type/BookingTypeFilterPage.kt
@@ -1,7 +1,24 @@
 package com.woocommerce.android.ui.bookings.filter.type
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.woocommerce.android.ui.bookings.filter.SingleChoiceFilterPage
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.bookings.BookingsFilterOption
+
+@Composable
+fun BookingTypeFilterRoute(
+    initialType: BookingsFilterOption.BookingType,
+    onTypeFilterChanged: (BookingsFilterOption.BookingType) -> Unit,
+) {
+    val viewModel = hiltViewModel<BookingTypeFilterViewModel, BookingTypeFilterViewModel.Factory> { factory ->
+        factory.create(initialType, onTypeFilterChanged)
+    }
+
+    val uiState by viewModel.uiState.collectAsState()
+    BookingTypeFilterPage(uiState)
+}
 
 @Composable
 fun BookingTypeFilterPage(state: BookingTypeFilterUiState) {
