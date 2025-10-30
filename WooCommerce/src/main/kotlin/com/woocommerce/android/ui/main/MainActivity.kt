1551b9bf3c03aaa1110aadbbd274175354f6db4e
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-10-30 10:21:34 +0100

    Refine comment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainActivity.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/main/MainActivity.kt
@@ -113,6 +113,7 @@ import com.woocommerce.android.util.ChromeCustomTabUtils
 import com.woocommerce.android.util.PackageUtils
 import com.woocommerce.android.util.WooAnimUtils.Duration
 import com.woocommerce.android.util.WooAnimUtils.animateBottomBar
+import com.woocommerce.android.util.WooLog
 import com.woocommerce.android.util.WooPermissionUtils
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.widgets.AppRatingDialog
@@ -391,7 +392,7 @@ class MainActivity :
                         updateAppBarVisibility(fragment)
                     }
                     if (isAtNavigationRoot()) {
-                        remove() //Remove callback to avoid infinite recursion
+                        remove() //Remove callback to avoid infinite recursion from onBackPressed() call below
                         onBackPressedDispatcher.onBackPressed()
                     }
                 }
