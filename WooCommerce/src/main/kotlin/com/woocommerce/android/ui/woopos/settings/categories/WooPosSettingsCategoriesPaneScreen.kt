b0a301603233caf6fa414931cb07cd59678ca1ac
Andrey <akidinov@gmail.com>
2025-10-30 11:09:54 +0100

    WooPosSettingsCategoriesPaneScreen title bold

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/categories/WooPosSettingsCategoriesPaneScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/categories/WooPosSettingsCategoriesPaneScreen.kt
@@ -148,6 +148,7 @@ private fun FixedCategoryItem(
         WooPosText(
             text = stringResource(item.titleRes),
             style = WooPosTypography.BodyMedium,
+            fontWeight = FontWeight.Bold,
         )
     }
 }
