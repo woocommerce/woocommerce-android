2b19f02f2b44fb2fdc51eb11a29df5ad9a3f38c6
samiuelson <samiuelson@gmail.com>
2025-11-06 13:11:23 +0100

    Add tests

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/settings/categories/WooPosSettingsCategoriesViewModelTest.kt
@@ -0,0 +1,113 @@
+package com.woocommerce.android.ui.woopos.settings.categories
+
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.woopos.localcatalog.WooPosIsLocalCatalogSupported
+import com.woocommerce.android.ui.woopos.util.WooPosCoroutineTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import org.wordpress.android.fluxc.model.SiteModel
+
+@ExperimentalCoroutinesApi
+class WooPosSettingsCategoriesViewModelTest {
+
+    @Rule
+    @JvmField
+    val coroutineTestRule = WooPosCoroutineTestRule()
+
+    private val isLocalCatalogSupported: WooPosIsLocalCatalogSupported = mock()
+    private val selectedSite: SelectedSite = mock()
+
+    private val mockSite = SiteModel().apply { id = 123 }
+
+    private lateinit var sut: WooPosSettingsCategoriesViewModel
+
+    @Before
+    fun setUp() {
+        whenever(selectedSite.get()).thenReturn(mockSite)
+    }
+
+    @Test
+    fun `given local catalog not supported, when viewmodel is initialized, then LOCAL_CATALOG is hidden`() = runTest {
+        // GIVEN
+        whenever(isLocalCatalogSupported(mockSite.localId())).thenReturn(false)
+
+        // WHEN
+        sut = createViewModel()
+        advanceUntilIdle()
+
+        // THEN
+        assertThat(sut.state.value.categories).doesNotContain(WooPosSettingsCategory.LOCAL_CATALOG)
+    }
+
+    @Test
+    fun `given local catalog supported, when viewmodel is initialized, then LOCAL_CATALOG is visible`() = runTest {
+        // GIVEN
+        whenever(isLocalCatalogSupported(mockSite.localId())).thenReturn(true)
+
+        // WHEN
+        sut = createViewModel()
+        advanceUntilIdle()
+
+        // THEN
+        assertThat(sut.state.value.categories).contains(WooPosSettingsCategory.LOCAL_CATALOG)
+    }
+
+    @Test
+    fun `given local catalog not supported, when viewmodel is initialized, then other categories are visible`() = runTest {
+        // GIVEN
+        whenever(isLocalCatalogSupported(mockSite.localId())).thenReturn(false)
+
+        // WHEN
+        sut = createViewModel()
+        advanceUntilIdle()
+
+        // THEN
+        assertThat(sut.state.value.categories).contains(
+            WooPosSettingsCategory.STORE,
+            WooPosSettingsCategory.HARDWARE,
+            WooPosSettingsCategory.HELP
+        )
+    }
+
+    @Test
+    fun `given local catalog supported, when viewmodel is initialized, then all categories are visible`() = runTest {
+        // GIVEN
+        whenever(isLocalCatalogSupported(mockSite.localId())).thenReturn(true)
+
+        // WHEN
+        sut = createViewModel()
+        advanceUntilIdle()
+
+        // THEN
+        assertThat(sut.state.value.categories).containsExactlyInAnyOrder(
+            WooPosSettingsCategory.STORE,
+            WooPosSettingsCategory.HARDWARE,
+            WooPosSettingsCategory.LOCAL_CATALOG,
+            WooPosSettingsCategory.HELP
+        )
+    }
+
+    @Test
+    fun `when default state is created, then LOCAL_CATALOG is initially hidden`() {
+        // GIVEN & WHEN
+        val initialState = WooPosSettingsCategoriesState(
+            categories = WooPosSettingsCategory.entries.filter { it != WooPosSettingsCategory.LOCAL_CATALOG }
+        )
+
+        // THEN
+        assertThat(initialState.categories).doesNotContain(WooPosSettingsCategory.LOCAL_CATALOG)
+    }
+
+    private fun createViewModel() = WooPosSettingsCategoriesViewModel(
+        context = mock(),
+        selectedSite = selectedSite,
+        isLocalCatalogSupported = isLocalCatalogSupported,
+    )
+}
