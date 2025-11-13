0fe24b1a85d24a515ea33e7d02aa7322ced7b23a
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-11-13 13:57:49 +0100

    Use WCTextButton in TimePickerDialog

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/component/TimePickerDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/compose/component/TimePickerDialog.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerDefaults
 import androidx.compose.material3.TimePickerDialog
@@ -42,18 +41,18 @@ fun TimePickerDialog(
             )
         },
         confirmButton = {
-            TextButton(
+            WCTextButton(
                 onClick = {
                     onTimeSelected(Time(timeState.hour, timeState.minute))
                 },
-            ) {
-                Text(text = stringResource(id = android.R.string.ok))
-            }
+                text = stringResource(id = android.R.string.ok)
+            )
         },
         dismissButton = {
-            TextButton(onClick = { onDismissRequest() }) {
-                Text(text = stringResource(id = android.R.string.cancel))
-            }
+            WCTextButton(
+                onClick = { onDismissRequest() },
+                text = stringResource(id = android.R.string.cancel)
+            )
         }
     ) {
         // WooTheme sets very big (compared to defaults) font sizes. Those are not handled well in the TimePicker,
