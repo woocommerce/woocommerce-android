8b25b679fbbb5f5603af78e04a74a1b124d3e4df
malinajirka <malinajirka@gmail.com>
2025-11-13 10:18:37 +0100

    Make settings loading time instant

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/categories/WooPosSettingsCategoriesViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/settings/categories/WooPosSettingsCategoriesViewModel.kt
@@ -1,40 +1,27 @@
 package com.woocommerce.android.ui.woopos.settings.categories
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.woocommerce.android.tools.SelectedSite
-import com.woocommerce.android.ui.woopos.localcatalog.WooPosIsLocalCatalogSupported
+import com.woocommerce.android.ui.woopos.home.items.products.WooPosProductsDataSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WooPosSettingsCategoriesViewModel @Inject constructor(
-    selectedSite: SelectedSite,
-    private val isLocalCatalogSupported: WooPosIsLocalCatalogSupported,
+    productsDataSource: WooPosProductsDataSource,
 ) : ViewModel() {
-    private val _state = MutableStateFlow(createInitialState())
+    private val _state = MutableStateFlow(WooPosSettingsCategoriesState(emptyList()))
     val state: StateFlow<WooPosSettingsCategoriesState> = _state.asStateFlow()
 
     init {
-        viewModelScope.launch {
-            val categories = WooPosSettingsCategory.entries.filter {
-                if (!isLocalCatalogSupported(selectedSite.get().localId())) {
-                    it != WooPosSettingsCategory.LOCAL_CATALOG
-                } else {
-                    true
-                }
+        val categories = WooPosSettingsCategory.entries.filter {
+            when (productsDataSource.getCurrentSyncStrategy()) {
+                WooPosProductsDataSource.SyncStrategy.REMOTE -> it != WooPosSettingsCategory.LOCAL_CATALOG
+                WooPosProductsDataSource.SyncStrategy.LOCAL_CATALOG -> true
             }
-            _state.value = WooPosSettingsCategoriesState(categories)
         }
-    }
-
-    private fun createInitialState(): WooPosSettingsCategoriesState {
-        return WooPosSettingsCategoriesState(
-            categories = WooPosSettingsCategory.entries.filter { it != WooPosSettingsCategory.LOCAL_CATALOG }
-        )
+        _state.value = WooPosSettingsCategoriesState(categories)
     }
 }
