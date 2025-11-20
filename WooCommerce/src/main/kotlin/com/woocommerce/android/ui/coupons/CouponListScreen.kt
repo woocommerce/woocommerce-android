45979934c1aaeb352a95757aaca55762f867c551
Andrey <akidinov@gmail.com>
2025-11-20 16:38:31 +0100

    Restore coupon drawable files and revert non-WooPos icon changes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/CouponListScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/coupons/CouponListScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -45,7 +44,6 @@ import com.woocommerce.android.ui.coupons.CouponListViewModel.LoadingState
 import com.woocommerce.android.ui.coupons.CouponListViewModel.LoadingState.Appending
 import com.woocommerce.android.ui.coupons.CouponListViewModel.LoadingState.Refreshing
 import com.woocommerce.android.ui.coupons.components.CouponExpirationLabel
-import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosIcons
 import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosTheme
 
 @Composable
@@ -101,7 +99,7 @@ private fun EmptyCouponList() {
         )
         Spacer(Modifier.size(dimensionResource(id = R.dimen.major_325)))
         Image(
-            painter = rememberVectorPainter(image = WooPosIcons.CouponsEmpty),
+            painter = painterResource(id = R.drawable.img_empty_coupon_list),
             contentDescription = null,
         )
     }
