60590a0c400d1b3c45a148dae3adebe39baa63c9
Irfan Ömür <irfan.omur@automattic.com>
2025-12-09 20:53:12 +0300

    Replace Icons.Filled.Share with a vector drawable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/support/WooLogViewerScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/support/WooLogViewerScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -45,10 +44,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
@@ -228,7 +229,7 @@ private fun LogFileContent(
                         }
                         IconButton(onClick = state.onShareClicked) {
                             Icon(
-                                Icons.Filled.Share,
+                                ImageVector.vectorResource(R.drawable.ic_share_24dp),
                                 contentDescription = stringResource(id = R.string.share),
                                 tint = colorResource(id = R.color.color_icon_menu)
                             )
