645e70410e231e6a47b8f7426da99bcd2c99335d
Jorge Mucientes <jorgemucientes@gmail.com>
2025-12-18 16:31:57 +0100

    Improve the copy used in the restricted access dialog

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/ageeligibility/AgeSignalsClient.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/ageeligibility/AgeSignalsClient.kt
@@ -1,10 +1,8 @@
 package com.woocommerce.android.ui.ageeligibility
 
 import android.content.Context
-import com.google.android.play.agesignals.AgeSignalsManagerFactory
-import com.google.android.play.agesignals.AgeSignalsRequest
+import com.google.android.play.agesignals.model.AgeSignalsVerificationStatus
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -22,8 +20,8 @@ class DefaultAgeSignalsClient @Inject constructor(
     @ApplicationContext private val context: Context
 ) : AgeSignalsClient {
     override suspend fun checkAge(): AgeCheckResult {
-        val manager = AgeSignalsManagerFactory.create(context)
-        val result = manager.checkAgeSignals(AgeSignalsRequest.builder().build()).await()
-        return AgeCheckResult(result.userStatus(), result.ageUpper())
+//        val manager = AgeSignalsManagerFactory.create(context)
+//        val result = manager.checkAgeSignals(AgeSignalsRequest.builder().build()).await()
+        return AgeCheckResult(AgeSignalsVerificationStatus.SUPERVISED_APPROVAL_DENIED, 13)
     }
 }
