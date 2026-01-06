d30a1eea4b75085829584019d3fc0abd70bcdbc5
Andrey <akidinov@gmail.com>
2026-01-06 14:09:43 +0100

    Add tests for WooPosPromo ViewModel and deep link

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopospromo/WooPosPromoViewModelTest.kt
@@ -0,0 +1,45 @@
+package com.woocommerce.android.ui.woopospromo
+
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class WooPosPromoViewModelTest : BaseUnitTest() {
+
+    private lateinit var viewModel: WooPosPromoViewModel
+
+    private fun createViewModel() {
+        viewModel = WooPosPromoViewModel(savedStateHandle = mock())
+    }
+
+    @Test
+    fun `given viewModel created, when state observed, then currentPage is 0`() = testBlocking {
+        createViewModel()
+
+        assertThat(viewModel.state.value.currentPage).isEqualTo(0)
+    }
+
+    @Test
+    fun `given viewModel on first page, when onNextClick called, then currentPage increments`() = testBlocking {
+        createViewModel()
+
+        viewModel.onNextClick()
+
+        assertThat(viewModel.state.value.currentPage).isEqualTo(1)
+    }
+
+    @Test
+    fun `given viewModel on last page, when onNextClick called, then currentPage stays at max`() = testBlocking {
+        createViewModel()
+        val maxPage = viewModel.state.value.pages.size - 1
+
+        repeat(maxPage + 5) {
+            viewModel.onNextClick()
+        }
+
+        assertThat(viewModel.state.value.currentPage).isEqualTo(maxPage)
+    }
+}
