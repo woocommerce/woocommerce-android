cfcdb66ff1c1293b7e8fe54d1c7a1a1c480583ce
Irfan Ömür <irfan.omur@automattic.com>
2026-01-09 16:12:06 +0300

    Add push notifications card to dashboard

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/pushnotifications/DashboardPushNotificationsCard.kt
@@ -0,0 +1,88 @@
+package com.woocommerce.android.ui.dashboard.pushnotifications
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
+import com.woocommerce.android.model.DashboardWidget
+import com.woocommerce.android.ui.compose.component.WCOverflowMenu
+import com.woocommerce.android.ui.compose.component.getText
+import com.woocommerce.android.ui.compose.preview.LightDarkThemePreviews
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import com.woocommerce.android.ui.dashboard.DashboardViewModel.DashboardWidgetMenu
+import com.woocommerce.android.ui.dashboard.defaultHideMenuEntry
+
+@Composable
+fun DashboardPushNotificationsCard(
+    modifier: Modifier = Modifier,
+    onHideClicked: () -> Unit,
+) {
+    val menu = DashboardWidgetMenu(
+        items = listOf(
+            DashboardWidget.Type.PUSH_NOTIFICATIONS.defaultHideMenuEntry(onHideClicked)
+        )
+    )
+
+    Row(
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = colorResource(id = R.color.woo_gray_5),
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(start = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.wp_woo),
+            contentDescription = null,
+            modifier = Modifier
+                .width(59.dp)
+                .padding(top = 16.dp)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.my_store_widget_push_notifications_title),
+                style = MaterialTheme.typography.h6,
+            )
+            Text(
+                text = stringResource(id = R.string.my_store_widget_push_notifications_description),
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(top = dimensionResource(id = R.dimen.minor_50))
+            )
+        }
+        WCOverflowMenu(
+            items = menu.items,
+            onSelected = { item -> item.action() },
+            mapper = { it.title.getText() },
+            tint = colorResource(id = R.color.color_on_surface_high)
+        )
+    }
+}
+
+@LightDarkThemePreviews
+@Composable
+fun DashboardPushNotificationsCardPreview() {
+    WooThemeWithBackground {
+        DashboardPushNotificationsCard(onHideClicked = {})
+    }
+}
