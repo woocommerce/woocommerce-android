e9d8d75c84c9c423d133f1dacb02fa9fad177273
Jorge Mucientes <jorgemucientes@gmail.com>
2026-01-21 16:38:48 +0100

    Remove Woo PN token only when successfully removed from server

--- WooCommerce/src/test/kotlin/com/woocommerce/android/notifications/push/PushNotificationRepositoryTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/notifications/push/PushNotificationRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.notifications.push
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.woocommerce.android.AppPrefsWrapper
@@ -66,7 +67,7 @@ class PushNotificationRepositoryTest : BaseUnitTest() {
     }
 
     @Test
-    fun `given selected site and stored uuid, when registering push token succeeds, then disables wpcom notifications for site`() =
+    fun `given selected site and stored uuid, when registering push token succeeds, then saves token and disables wpcom notifications`() =
         testBlocking {
             whenever(selectedSite.getIfExists()).thenReturn(siteModel)
             whenever(siteModel.siteId).thenReturn(SITE_ID)
@@ -74,9 +75,19 @@ class PushNotificationRepositoryTest : BaseUnitTest() {
             whenever(pushNotificationsStore.registerPushToken(siteModel, "token", "stored-uuid"))
                 .thenReturn(WooResult(RETURNED_TOKEN))
 
+            val mutablePreferences: MutablePreferences = mock()
+            whenever(preferences.toMutablePreferences()).thenReturn(mutablePreferences)
+            whenever(pushNotificationsDataStore.updateData(any())).thenAnswer { invocation ->
+                val transform = invocation.getArgument<suspend (Preferences) -> Preferences>(0)
+                testBlocking { transform(preferences) }
+                preferences
+            }
+
             sut.registerPushTokenInWooCoreSystem("token")
 
             verify(pushNotificationsStore).registerPushToken(siteModel, "token", "stored-uuid")
+            val expectedTokenKey = stringPreferencesKey("push_token_$SITE_ID")
+            verify(mutablePreferences)[expectedTokenKey] = RETURNED_TOKEN
             verify(notificationStore).updateNotificationSettingsFor(
                 listOf(
                     SiteNotificationSetting(
