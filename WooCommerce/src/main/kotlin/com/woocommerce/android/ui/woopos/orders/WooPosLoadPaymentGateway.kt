f84fe892d5df13b14367f3174b2f5105e12f983a
samiuelson <samiuelson@gmail.com>
2026-01-26 17:51:15 +0100

    Refactor payment gateway loading to use coroutine context for improved performance

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/WooPosLoadPaymentGateway.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/WooPosLoadPaymentGateway.kt
@@ -4,16 +4,19 @@ import com.woocommerce.android.model.Order
 import com.woocommerce.android.model.PaymentGateway
 import com.woocommerce.android.model.toAppModel
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.util.CoroutineDispatchers
+import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.store.WCGatewayStore
 import javax.inject.Inject
 
 class WooPosLoadPaymentGateway @Inject constructor(
     private val gatewayStore: WCGatewayStore,
-    private val selectedSite: SelectedSite
+    private val selectedSite: SelectedSite,
+    private val coroutineDispatchers: CoroutineDispatchers
 ) {
-    suspend operator fun invoke(order: Order): PaymentGateway {
+    suspend operator fun invoke(order: Order): PaymentGateway = withContext(coroutineDispatchers.io) {
         val paymentGateway = gatewayStore.getGateway(selectedSite.get(), order.paymentMethod)?.toAppModel()
-        return if (paymentGateway != null && paymentGateway.isEnabled) {
+        return@withContext if (paymentGateway != null && paymentGateway.isEnabled) {
             paymentGateway
         } else {
             PaymentGateway(methodTitle = REFUND_METHOD_MANUAL)
