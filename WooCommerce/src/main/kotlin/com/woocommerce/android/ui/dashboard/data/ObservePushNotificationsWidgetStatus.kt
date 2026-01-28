b5517e637d0eb4cf182ec5078424ccb511e09cc7
Irfan Ömür <irfan.omur@automattic.com>
2026-01-28 08:52:48 +0300

    Add visibility logic for push notifications dashboard widget

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/data/ObservePushNotificationsWidgetStatus.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/dashboard/data/ObservePushNotificationsWidgetStatus.kt
@@ -1,18 +1,36 @@
 package com.woocommerce.android.ui.dashboard.data
 
 import com.woocommerce.android.model.DashboardWidget
+import com.woocommerce.android.notifications.push.PushNotificationRegistrationStatus
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.tools.SiteConnectionType
+import com.woocommerce.android.tools.connectionType
 import com.woocommerce.android.util.FeatureFlag
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class ObservePushNotificationsWidgetStatus @Inject constructor() {
-    operator fun invoke(): Flow<DashboardWidget.Status> = flowOf(
-        // TODO Add logic to check token registration status
-        if (FeatureFlag.WOO_PUSH_NOTIFICATIONS_SYSTEM_M2.isEnabled()) {
-            DashboardWidget.Status.Available
-        } else {
-            DashboardWidget.Status.Hidden
+class ObservePushNotificationsWidgetStatus @Inject constructor(
+    private val selectedSite: SelectedSite,
+    private val pushNotificationRegistrationStatus: PushNotificationRegistrationStatus
+) {
+    operator fun invoke(): Flow<DashboardWidget.Status> = flow {
+        if (!FeatureFlag.WOO_PUSH_NOTIFICATIONS_SYSTEM_M2.isEnabled()) {
+            emit(DashboardWidget.Status.Hidden)
+            return@flow
         }
-    )
+
+        val site = selectedSite.getIfExists()
+        if (site == null || site.connectionType != SiteConnectionType.ApplicationPasswords) {
+            emit(DashboardWidget.Status.Hidden)
+            return@flow
+        }
+
+        if (pushNotificationRegistrationStatus() == PushNotificationRegistrationStatus.Status.REGISTERED) {
+            emit(DashboardWidget.Status.Hidden)
+            return@flow
+        }
+
+        emit(DashboardWidget.Status.Available)
+    }
 }
