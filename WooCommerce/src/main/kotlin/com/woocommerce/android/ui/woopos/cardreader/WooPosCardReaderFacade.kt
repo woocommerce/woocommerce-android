4620a9b7b09f18d0e3eebec04daae3637118b91e
Andrey <akidinov@gmail.com>
2026-01-29 11:26:01 +0100

    Expose battery status flow in WooPosCardReaderFacade

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderFacade.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardreader/WooPosCardReaderFacade.kt
@@ -2,6 +2,7 @@ package com.woocommerce.android.ui.woopos.cardreader
 
 import com.woocommerce.android.cardreader.CardReaderManager
 import com.woocommerce.android.cardreader.connection.CardReaderStatus
+import com.woocommerce.android.cardreader.connection.event.CardReaderBatteryStatus
 import com.woocommerce.android.cardreader.connection.event.SoftwareUpdateAvailability
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,6 +15,7 @@ class WooPosCardReaderFacade @Inject constructor(
 ) {
     val readerStatus: StateFlow<CardReaderStatus> = cardReaderManager.readerStatus
     val softwareUpdateAvailability: Flow<SoftwareUpdateAvailability> = cardReaderManager.softwareUpdateAvailability
+    val batteryStatus: Flow<CardReaderBatteryStatus> = cardReaderManager.batteryStatus
 
     fun cancelReconnection() {
         cardReaderManager.cancelReconnection()
