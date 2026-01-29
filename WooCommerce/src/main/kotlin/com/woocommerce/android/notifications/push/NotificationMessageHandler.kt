0f667f42a1b5c260058a833971919f48d2697e67
Irfan Ömür <irfan.omur@automattic.com>
2026-01-29 21:14:41 +0300

    Update registration status check for push notifications

--- WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/push/NotificationMessageHandler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/notifications/push/NotificationMessageHandler.kt
@@ -90,7 +90,7 @@ class NotificationMessageHandler @Inject constructor(
 
         val notification = notificationModel.toAppModel(resourceProvider)
         val isRegisteredForWooPush = runBlocking {
-            registrationStatus(notification.remoteSiteId) == PushNotificationRegistrationStatus.Status.REGISTERED_WOO_ONLY
+            registrationStatus(notification.remoteSiteId) == PushNotificationRegistrationStatus.Status.REGISTERED_BOTH
         }
         val pushUserId = messageData[PUSH_ARG_USER]
 
