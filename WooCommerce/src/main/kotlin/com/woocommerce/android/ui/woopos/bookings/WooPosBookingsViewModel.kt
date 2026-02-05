466d9870350158df86b3b55ab28f8a5526e8b53e
malinajirka <malinajirka@gmail.com>
2026-02-05 16:23:43 +0100

    Introduce initial methods and state management in `WooPosBookingsViewModel`

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingsViewModel.kt
@@ -1,11 +1,53 @@
 package com.woocommerce.android.ui.woopos.bookings
 
 import androidx.lifecycle.ViewModel
+import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosSearchUIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @Suppress("LargeClass")
 @HiltViewModel
-class WooPosBookingsViewModel @Inject constructor(
-) : ViewModel() {
+class WooPosBookingsViewModel @Inject constructor() : ViewModel() {
+
+    private val _state = MutableStateFlow<WooPosBookingsState>(WooPosBookingsState.Loading)
+    val state: StateFlow<WooPosBookingsState> = _state.asStateFlow()
+
+    private val _scrollToTopEvent = MutableSharedFlow<Unit>()
+    val scrollToTopEvent: SharedFlow<Unit> = _scrollToTopEvent.asSharedFlow()
+
+    fun onRefresh() {
+    }
+
+    fun onBookingSelected(bookingId: Long) {
+    }
+
+    fun onEndOfBookingsListReached() {
+    }
+
+    fun onPaginationErrorTryAgain() {
+    }
+
+    fun onSearchEvent(event: WooPosSearchUIEvent) {
+    }
+
+    fun onSearchErrorRetry() {
+    }
+
+    fun onBookingsEmptyActionClicked() {
+    }
+
+    fun onBookingsLoadingErrorRetryButtonClicked() {
+    }
+
+    fun onUIEvent(event: WooPosBookingsUIEvent) {
+    }
+
+    fun onIssueRefundDialogDismissed() {
+    }
 }
