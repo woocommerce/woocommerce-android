76e760283137e552ca0894df5b44fdfaf899f515
Andrey <akidinov@gmail.com>
2026-02-06 16:02:13 +0100

    Add pay by card button to booking details screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/details/WooPosBookingDetails.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/details/WooPosBookingDetails.kt
@@ -1,13 +1,20 @@
 package com.woocommerce.android.ui.woopos.bookings.details
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.woopos.bookings.WooPosBookingsState
 import com.woocommerce.android.ui.woopos.bookings.WooPosBookingsUIEvent
+import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosButton
 import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosText
+import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosSpacing
 import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosTypography
 
 @Composable
@@ -17,13 +24,19 @@ fun WooPosBookingDetails(
     details: WooPosBookingsState.BookingDetailsViewState.Computed.Details,
     onUIEvent: (WooPosBookingsUIEvent) -> Unit
 ) {
-    Box(
+    Column(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         WooPosText(
             text = "Booking Detail",
             style = WooPosTypography.Heading
+        )
+        Spacer(modifier = Modifier.size(WooPosSpacing.Medium.value))
+        WooPosButton(
+            text = stringResource(R.string.woopos_bookings_pay_by_card),
+            onClick = { onUIEvent(WooPosBookingsUIEvent.PayByCardClicked) }
         )
     }
 }
