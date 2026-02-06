83ef1e4572ced4894566b90f2f56e606b5313a98
Andrey <akidinov@gmail.com>
2026-02-06 16:01:55 +0100

    Add PayByCardClicked event to bookings UI events

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingsUIEvent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingsUIEvent.kt
@@ -2,4 +2,5 @@ package com.woocommerce.android.ui.woopos.bookings
 
 sealed interface WooPosBookingsUIEvent {
     data class BookingActionClicked(val action: WooPosBookingsState.BookingAction) : WooPosBookingsUIEvent
+    data object PayByCardClicked : WooPosBookingsUIEvent
 }
