4d7ee2efb4f8af371e91ec43177c1008813f402e
samiuelson <samiuelson@gmail.com>
2026-02-10 18:48:39 +0100

    Add card payment functionality to WooPos bookings

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.ui.bookings.list.BookingListHandler
 import com.woocommerce.android.ui.bookings.list.BookingListSortOption
+import com.woocommerce.android.ui.woopos.cardpayment.CardPaymentSource
 import com.woocommerce.android.ui.woopos.cashpayment.CashPaymentSource
 import com.woocommerce.android.ui.woopos.home.items.WooPosPaginationState
 import com.woocommerce.android.ui.woopos.home.items.WooPosPullToRefreshState
@@ -215,7 +216,7 @@ class WooPosBookingsViewModel @Inject constructor(
         when (event) {
             is WooPosBookingsUIEvent.BookingActionClicked -> handleBookingAction(event.action)
             is WooPosBookingsUIEvent.AttendanceToggled -> { }
-            is WooPosBookingsUIEvent.PayByCardClicked -> { }
+            is WooPosBookingsUIEvent.PayByCardClicked -> handlePayByCard()
             is WooPosBookingsUIEvent.PayByCashClicked -> handlePayByCash()
             is WooPosBookingsUIEvent.AddBookingNoteClicked -> { }
             is WooPosBookingsUIEvent.CopyEmailClicked -> { }
@@ -236,6 +237,18 @@ class WooPosBookingsViewModel @Inject constructor(
                 WooPosNavigationEvent.OpenCashPayment(
                     orderId = details.orderId,
                     source = CashPaymentSource.BOOKINGS,
+                )
+            )
+        }
+    }
+
+    private fun handlePayByCard() {
+        val details = (_state.value as? WooPosBookingsState.Content)?.selectedDetails ?: return
+        viewModelScope.launch {
+            _navigationEvent.emit(
+                WooPosNavigationEvent.OpenCardPayment(
+                    orderId = details.orderId,
+                    source = CardPaymentSource.BOOKINGS,
                 )
             )
         }
