89743b257d02c106e3a6501f0ea0c33e453fa743
samiuelson <samiuelson@gmail.com>
2026-02-10 20:06:39 +0100

    Add card payment screen navigation wiring and stub screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardpayment/WooPosCardPaymentNavigation.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardpayment/WooPosCardPaymentNavigation.kt
@@ -1,7 +1,14 @@
 package com.woocommerce.android.ui.woopos.cardpayment
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.woocommerce.android.ui.woopos.home.HOME_ROUTE
+import com.woocommerce.android.ui.woopos.root.navigation.WooPosNavigationEvent
 import com.woocommerce.android.ui.woopos.root.navigation.navigateOnce
 
 const val CARD_PAYMENT_ROUTE_ORDER_ID_KEY = "orderId"
@@ -26,4 +33,43 @@ fun NavController.navigateToCardPaymentScreen(
             .replace("{$CARD_PAYMENT_ROUTE_ORDER_ID_KEY}", orderId.toString())
             .replace("{$CARD_PAYMENT_ROUTE_SOURCE_KEY}", source.name)
     )
+}
+
+fun NavGraphBuilder.cardPaymentScreen(
+    onNavigationEvent: (WooPosNavigationEvent) -> Unit
+) {
+    composable(
+        route = CARD_PAYMENT_ROUTE,
+        arguments = listOf(
+            navArgument(CARD_PAYMENT_ROUTE_ORDER_ID_KEY) { type = NavType.LongType },
+            navArgument(CARD_PAYMENT_ROUTE_SOURCE_KEY) {
+                type = NavType.StringType
+                defaultValue = CardPaymentSource.CHECKOUT.name
+            }
+        ),
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { fullWidth -> fullWidth },
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { fullWidth -> -fullWidth },
+            )
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { fullWidth -> -fullWidth },
+            )
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { fullWidth -> fullWidth },
+            )
+        },
+    ) {
+        WooPosCardPaymentScreen(
+            onNavigationEvent = onNavigationEvent,
+        )
+    }
 }
