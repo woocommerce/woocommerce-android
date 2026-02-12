a223a47ded99365723a7b6b39d0b98c292e83129
Irfan Ömür <irfan.omur@automattic.com>
2026-02-12 20:14:15 +0300

    Update bookings tab visibility logic

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/BookingsTabController.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/tab/BookingsTabController.kt
@@ -2,14 +2,15 @@ package com.woocommerce.android.ui.bookings.tab
 
 import androidx.lifecycle.lifecycleScope
 import com.woocommerce.android.databinding.ActivityMainBinding
-import com.woocommerce.android.extensions.isTwoPanesShouldBeUsed
 import com.woocommerce.android.ui.main.BottomNavigationPosition
 import com.woocommerce.android.ui.main.MainActivity
+import com.woocommerce.android.ui.woopos.tab.WooPosTabShouldBeVisible
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class BookingsTabController @Inject constructor(
-    private val observeBookingsVisibility: ObserveBookingsVisibility
+    private val observeBookingsVisibility: ObserveBookingsVisibility,
+    private val shouldPosTabBeVisible: WooPosTabShouldBeVisible
 ) {
     private lateinit var activity: MainActivity
     private lateinit var binding: ActivityMainBinding
@@ -27,15 +28,12 @@ class BookingsTabController @Inject constructor(
     }
 
     private fun checkBookingsTabVisibility() {
-        // On large screens, Bookings is accessible from the More Menu instead of a bottom tab.
-        if (activity.isTwoPanesShouldBeUsed) {
-            return
-        }
-
         activity.lifecycleScope.launch {
+            val isPosVisible = shouldPosTabBeVisible().getOrDefault(false)
             observeBookingsVisibility()
                 .collect { isVisible ->
-                    binding.bottomNav.menu.findItem(BottomNavigationPosition.BOOKINGS.id)?.isVisible = isVisible
+                    binding.bottomNav.menu.findItem(BottomNavigationPosition.BOOKINGS.id)?.isVisible =
+                        isVisible && !isPosVisible
                 }
         }
     }
