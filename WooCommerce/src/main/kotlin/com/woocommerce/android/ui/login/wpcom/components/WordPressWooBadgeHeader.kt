34f8aa59e5bb571f4e84ff93feedd2ff755b2409
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-02-13 12:29:47 +0100

    Update WordPressWooBadgeHeader with proper branding and icon fixes

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/wpcom/components/WordPressWooBadgeHeader.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/wpcom/components/WordPressWooBadgeHeader.kt
@@ -1,35 +1,55 @@
 package com.woocommerce.android.ui.login.wpcom.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 
 @Composable
 fun WordPressWooBadgeHeader(modifier: Modifier = Modifier) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.minor_100)),
-        verticalAlignment = Alignment.CenterVertically,
+    Box(
         modifier = modifier
     ) {
-        val logoModifier = Modifier.size(dimensionResource(id = R.dimen.image_major_50))
-        Image(
+        Icon(
             painter = painterResource(id = R.drawable.ic_wordpress),
             contentDescription = null,
-            modifier = logoModifier.padding(dimensionResource(id = R.dimen.minor_50))
+            tint = colorResource(R.color.wp_blue_50),
+            modifier = Modifier.size(48.dp)
+                .align(Alignment.CenterStart)
         )
-        Image(painter = painterResource(id = R.drawable.ic_connecting), contentDescription = null)
         Image(
             painter = painterResource(id = R.drawable.ic_woo),
             contentDescription = null,
-            modifier = logoModifier.padding(dimensionResource(id = R.dimen.minor_50))
+            modifier = Modifier
+                .padding(start = 38.dp)
+                .border(
+                    width = 2.dp,
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = CircleShape
+                )
+                .size(52.dp)
+                .align(Alignment.CenterEnd)
         )
+    }
+}
+
+@Preview
+@Composable
+private fun WordPressWooBadgeHeaderPreview() {
+    WooThemeWithBackground {
+        WordPressWooBadgeHeader()
     }
 }
