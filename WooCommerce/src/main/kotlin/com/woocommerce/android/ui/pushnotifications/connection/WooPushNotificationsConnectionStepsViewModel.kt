4f3f93c6b15f02dc53cffbe5c536097cf32d24e1
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-02-17 13:42:55 +0000

    Update ViewModel to support multi-step installation pattern

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/pushnotifications/connection/WooPushNotificationsConnectionStepsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/pushnotifications/connection/WooPushNotificationsConnectionStepsViewModel.kt
@@ -1,13 +1,24 @@
 package com.woocommerce.android.ui.pushnotifications.connection
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
-import com.woocommerce.android.R
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.woocommerce.android.model.UiString
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.util.StringUtils
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,26 +27,61 @@ class WooPushNotificationsConnectionStepsViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ScopedViewModel(savedStateHandle) {
 
-    private val _viewState = MutableLiveData(
-        ViewState(siteAddress = getSiteAddress())
+    private val siteAddress = getSiteAddress()
+
+    private val currentStep = savedStateHandle.getStateFlow(
+        scope = viewModelScope,
+        initialValue = Step(StepType.ConnectStore),
     )
-    val viewState: LiveData<ViewState> = _viewState
 
-    init {
-        startConnectStoreStep()
-    }
-
-    private fun startConnectStoreStep() {
-        val currentState = _viewState.value ?: return
-        _viewState.value = currentState.copy(
-            steps = currentState.steps.mapIndexed { index, step ->
-                if (index == 0) {
-                    step.copy(status = ConnectionStepStatus.IN_PROGRESS)
-                } else {
-                    step
-                }
+    val viewState = combine(
+        currentStep,
+        flowOf(StepType.entries.toList())
+    ) { currentStep, stepTypes ->
+        ViewState(
+            siteAddress = siteAddress,
+            steps = stepTypes.map { stepType ->
+                Step(
+                    type = stepType,
+                    state = when {
+                        currentStep.type == stepType -> currentStep.state
+                        currentStep.type > stepType -> StepState.Success
+                        else -> StepState.Idle
+                    }
+                )
             }
         )
+    }.asLiveData()
+
+    init {
+        monitorCurrentStep()
+        startNextStep()
+    }
+
+    fun onCloseClick() {
+        triggerEvent(Exit)
+    }
+
+    fun onRetryClick() {
+        startNextStep()
+    }
+
+    private fun startNextStep() {
+        currentStep.update { it.copy(state = StepState.Ongoing) }
+    }
+
+    private fun monitorCurrentStep() = launch {
+        currentStep
+            .collectLatest { step ->
+                if (step.state != StepState.Ongoing) return@collectLatest
+
+                @Suppress("NoOp")
+                when (step.type) {
+                    StepType.ConnectStore -> Unit
+                    StepType.CheckPluginCompatibility -> Unit
+                    StepType.EnablePushNotifications -> Unit
+                }
+            }
     }
 
     private fun getSiteAddress(): String {
@@ -43,21 +89,37 @@ class WooPushNotificationsConnectionStepsViewModel @Inject constructor(
         return StringUtils.getSiteDomainAndPath(site).ifBlank { site.name.orEmpty() }
     }
 
+
     data class ViewState(
         val siteAddress: String,
-        val steps: List<ConnectionStepUiModel> = listOf(
-            ConnectionStepUiModel(
-                title = R.string.woo_push_notifications_connection_steps_step_connect_store,
-                status = ConnectionStepStatus.NOT_STARTED
-            ),
-            ConnectionStepUiModel(
-                title = R.string.woo_push_notifications_connection_steps_step_check_plugin_compatibility,
-                status = ConnectionStepStatus.NOT_STARTED,
-            ),
-            ConnectionStepUiModel(
-                title = R.string.woo_push_notifications_connection_steps_step_enable_push_notifications,
-                status = ConnectionStepStatus.NOT_STARTED
-            )
-        )
-    )
+        val steps: List<Step>
+    ) {
+        val isDone = steps.all { it.state == StepState.Success }
+    }
+
+    @Parcelize
+    data class Step(
+        val type: StepType,
+        val state: StepState = StepState.Idle
+    ) : Parcelable
+
+    enum class StepType {
+        ConnectStore,
+        CheckPluginCompatibility,
+        EnablePushNotifications
+    }
+
+    sealed interface StepState : Parcelable {
+        @Parcelize
+        data object Idle : StepState
+
+        @Parcelize
+        data object Ongoing : StepState
+
+        @Parcelize
+        data object Success : StepState
+
+        @Parcelize
+        data class Error(val errorMessage: UiString) : StepState
+    }
 }
