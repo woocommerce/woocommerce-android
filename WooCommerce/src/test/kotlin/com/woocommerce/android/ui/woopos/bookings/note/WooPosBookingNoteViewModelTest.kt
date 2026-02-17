00657fd4c462a0da5c8503cfb1e80881d63e5328
samiuelson <samiuelson@gmail.com>
2026-02-17 14:46:33 +0100

    Fix WooPosBookingNoteViewModelTest using removed AttendanceStatus.Booked

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/bookings/note/WooPosBookingNoteViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/bookings/note/WooPosBookingNoteViewModelTest.kt
@@ -56,7 +56,7 @@ class WooPosBookingNoteViewModelTest {
         parentId = 0L,
         personCounts = listOf(1L),
         localTimezone = "",
-        attendanceStatus = BookingEntity.AttendanceStatus.Booked,
+        attendanceStatus = BookingEntity.AttendanceStatus.Unattended,
         note = "Existing note",
         order = BookingOrderInfo(),
         customerNote = ""
