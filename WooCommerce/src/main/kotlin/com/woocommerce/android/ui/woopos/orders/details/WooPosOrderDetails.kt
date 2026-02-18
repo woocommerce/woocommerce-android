350d98ff41b934b68a4e38c68eb63f955ebe17f8
malinajirka <malinajirka@gmail.com>
2026-02-18 15:12:59 +0100

    Always show qtyAndUnitPrice for bookable line items

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/details/WooPosOrderDetails.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/details/WooPosOrderDetails.kt
@@ -291,22 +291,23 @@ private fun OrderProductItem(row: WooPosOrdersState.OrderDetailsViewState.Comput
             null -> Unit
         }
 
-        if (bookingInfo == null) {
-            WooPosText(
-                text = row.qtyAndUnitPrice,
-                style = WooPosTypography.BodyMedium,
-                color = WooPosTheme.colors.onSurfaceVariantHighest,
-                modifier = Modifier.constrainAs(qtyText) {
-                    top.linkTo(
-                        if (hasAttributes) attributesText.bottom else nameText.bottom,
-                        margin = marginXSmall
-                    )
-                    start.linkTo(nameText.start)
-                    end.linkTo(totalText.start, margin = marginSmall)
-                    width = Dimension.fillToConstraints
-                }
-            )
+        val qtyBottomAnchor = when {
+            bookingInfo != null -> bookingInfoText
+            hasAttributes -> attributesText
+            else -> nameText
         }
+
+        WooPosText(
+            text = row.qtyAndUnitPrice,
+            style = WooPosTypography.BodyMedium,
+            color = WooPosTheme.colors.onSurfaceVariantHighest,
+            modifier = Modifier.constrainAs(qtyText) {
+                top.linkTo(qtyBottomAnchor.bottom, margin = marginXSmall)
+                start.linkTo(nameText.start)
+                end.linkTo(totalText.start, margin = marginSmall)
+                width = Dimension.fillToConstraints
+            }
+        )
 
         WooPosText(
             text = row.lineTotal,
