0429074c301003be9116ed0a8b8bef07066f9392
malinajirka <malinajirka@gmail.com>
2026-02-19 16:20:02 +0100

    Add test for resolving payment status in WooPosBookingViewStateMapper

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingViewStateMapperTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/bookings/WooPosBookingViewStateMapperTest.kt
@@ -285,6 +285,7 @@ class WooPosBookingViewStateMapperTest {
         runTest {
             // GIVEN
             val booking = sampleBooking(id = 42L)
+            whenever(paymentStatusResolver.resolve(any(), any())).thenReturn(PaymentStatus.PAID)
 
             // WHEN
             val result = mapper.mapToDetailsViewState(booking, resourceName = null)
