a0230ae7caa1adcf620a1ed1a0457e9e7f02e157
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-02-20 14:52:10 +0000

    Make isSiteConnected a property

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/pushnotifications/introduction/WooPushNotificationsIntroductionViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/pushnotifications/introduction/WooPushNotificationsIntroductionViewModel.kt
@@ -4,9 +4,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.woocommerce.android.AppUrls
 import com.woocommerce.android.extensions.isVersionAtLeast
-import com.woocommerce.android.model.JetpackConnectionStatus
-import com.woocommerce.android.model.JetpackSiteRegistrationStatus
-import com.woocommerce.android.model.JetpackStatus
 import com.woocommerce.android.support.help.HelpOrigin
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.jetpack.FetchJetpackStatus
@@ -27,16 +24,8 @@ class WooPushNotificationsIntroductionViewModel @Inject constructor(
     private val fetchActiveWCPluginVersion: FetchActiveWCPluginVersion,
     private val selectedSite: SelectedSite
 ) : ScopedViewModel(savedStateHandle) {
-
     companion object {
         const val PUSH_NOTIFICATIONS_MIN_WC_VERSION = "10.6.0" // TODO CHECK CORRECT VERSION LATER
-
-        private val JetpackStatus.isSiteConnected: Boolean
-            get() = when (jetpackConnectionStatus) {
-                is JetpackConnectionStatus.AccountConnected -> true
-                is JetpackConnectionStatus.AccountNotConnected ->
-                    jetpackConnectionStatus.siteRegistrationStatus == JetpackSiteRegistrationStatus.REGISTERED
-            }
     }
 
     private val _viewState = MutableStateFlow<ViewState>(ViewState.Loading)
