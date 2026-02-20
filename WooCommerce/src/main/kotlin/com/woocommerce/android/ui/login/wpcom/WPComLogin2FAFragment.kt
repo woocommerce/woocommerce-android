9317dcf376614139540c37f8d729422df27d692d
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-02-20 17:29:20 +0000

    Remove unused navigation logic from login to push notifications setup

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/wpcom/WPComLogin2FAFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/login/wpcom/WPComLogin2FAFragment.kt
@@ -11,7 +11,6 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import com.woocommerce.android.NavGraphJetpackActivationDirections
 import com.woocommerce.android.NavGraphJetpackInstallDirections
-import com.woocommerce.android.NavGraphPushNotificationsDirections
 import com.woocommerce.android.R
 import com.woocommerce.android.extensions.navigateSafely
 import com.woocommerce.android.ui.base.BaseFragment
@@ -19,7 +18,6 @@ import com.woocommerce.android.ui.base.UIMessageResolver
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.login.wpcom.WPComLoginPostLoginViewModel.ShowJetpackActivationScreen
 import com.woocommerce.android.ui.login.wpcom.WPComLoginPostLoginViewModel.ShowJetpackCPInstallationScreen
-import com.woocommerce.android.ui.login.wpcom.WPComLoginPostLoginViewModel.ShowPushNotificationsConnectionSteps
 import com.woocommerce.android.ui.main.AppBarStatus
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.Exit
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowSnackbar
@@ -57,17 +55,10 @@ class WPComLogin2FAFragment : BaseFragment() {
             when (event) {
                 is ShowJetpackActivationScreen -> navigateToJetpackActivationScreen(event)
                 is ShowJetpackCPInstallationScreen -> navigateToJetpackCPInstallationScreen()
-                is ShowPushNotificationsConnectionSteps -> navigateToPushNotificationsConnectionSteps()
                 is ShowSnackbar -> uiMessageResolver.showSnack(event.message)
                 is Exit -> findNavController().navigateUp()
             }
         }
-    }
-
-    private fun navigateToPushNotificationsConnectionSteps() {
-        findNavController().navigateSafely(
-            NavGraphPushNotificationsDirections.actionGlobalToWooPushNotificationsConnectionStepsFragment()
-        )
     }
 
     private fun navigateToJetpackActivationScreen(event: ShowJetpackActivationScreen) {
