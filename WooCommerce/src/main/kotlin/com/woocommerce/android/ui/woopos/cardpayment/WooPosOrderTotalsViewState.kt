8823de81554126c666c55b18668d0af08f810872
malinajirka <malinajirka@gmail.com>
2026-03-03 09:32:56 +0100

    Merge WooPosOrderTotalsViewState into WooPosCardPaymentState

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/cardpayment/WooPosOrderTotalsViewState.kt
+++
@@ -1,8 +0,0 @@
-package com.woocommerce.android.ui.woopos.cardpayment
-
-data class WooPosOrderTotalsViewState(
-    val subtotal: String,
-    val discount: String?,
-    val taxes: String,
-    val total: String,
-)
