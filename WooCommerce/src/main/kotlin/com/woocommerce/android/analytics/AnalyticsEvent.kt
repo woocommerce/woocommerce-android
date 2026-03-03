81c93caa5aed299d246898e139b396c598902a0f
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-03-03 14:06:38 +0000

    Rename main tab booking events to present tense

--- WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsEvent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/analytics/AnalyticsEvent.kt
@@ -505,8 +505,8 @@ enum class AnalyticsEvent(override val siteless: Boolean = false) : IAnalyticsEv
 
     MAIN_TAB_HUB_MENU_SELECTED,
     MAIN_TAB_HUB_MENU_RESELECTED,
-    MAIN_TAB_BOOKINGS_SELECTED,
-    MAIN_TAB_BOOKINGS_RESELECTED,
+    MAIN_TAB_BOOKINGS_SELECT,
+    MAIN_TAB_BOOKINGS_RESELECT,
 
     // -- Settings
     SETTING_CHANGE,
