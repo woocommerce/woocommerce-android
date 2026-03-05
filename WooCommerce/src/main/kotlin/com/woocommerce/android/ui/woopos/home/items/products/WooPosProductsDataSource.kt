9a6f49d78b0a9cd9369cd4727404ba646d8ca73b
Andrey <akidinov@gmail.com>
2026-03-05 13:49:51 +0100

    Use 'cache' as search method for in-memory filter path

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/products/WooPosProductsDataSource.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/products/WooPosProductsDataSource.kt
@@ -624,7 +624,7 @@ class WooPosProductsRemoteDataSource @Inject constructor(
                 SearchProductsResult.Local(
                     products = localProducts,
                     searchTimeMillis = 0L,
-                    searchMethod = "like",
+                    searchMethod = "cache",
                 )
             )
         }
