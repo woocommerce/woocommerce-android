c13367a77471a95a836546c96d7f010e6f164a3b
Andrey <akidinov@gmail.com>
2026-03-05 09:51:31 +0100

    Change WooPosFtsSearchResult from sealed class to sealed interface

--- libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/store/pos/localcatalog/WooPosFtsSearchResult.kt
+++ libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/store/pos/localcatalog/WooPosFtsSearchResult.kt
@@ -3,7 +3,7 @@ package org.wordpress.android.fluxc.store.pos.localcatalog
 import org.wordpress.android.fluxc.persistence.entity.pos.WooPosProductEntity
 import org.wordpress.android.fluxc.persistence.entity.pos.WooPosVariationEntity
 
-sealed class WooPosFtsSearchResult {
-    data class Product(val entity: WooPosProductEntity) : WooPosFtsSearchResult()
-    data class Variation(val entity: WooPosVariationEntity) : WooPosFtsSearchResult()
+sealed interface WooPosFtsSearchResult {
+    data class Product(val entity: WooPosProductEntity) : WooPosFtsSearchResult
+    data class Variation(val entity: WooPosVariationEntity) : WooPosFtsSearchResult
 }
