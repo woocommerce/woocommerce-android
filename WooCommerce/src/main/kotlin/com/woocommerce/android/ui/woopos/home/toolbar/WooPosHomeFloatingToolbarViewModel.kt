f4e8c9224f12547227c8d4db282f718f8c0922c2
Andrey <akidinov@gmail.com>
2026-03-10 15:21:20 +0100

    Fix detekt ArgumentListWrapping in WooPosHomeFloatingToolbarViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosHomeFloatingToolbarViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosHomeFloatingToolbarViewModel.kt
@@ -66,7 +66,9 @@ class WooPosHomeFloatingToolbarViewModel @Inject constructor(
                             batteryState = mapBatteryState(batteryStatus)
                         )
                     }
-                    is NotConnected, Connecting -> flowOf(WooPosHomeFloatingToolbarState.WooPosCardReaderStatus.NotConnected)
+                    is NotConnected, Connecting -> flowOf(
+                        WooPosHomeFloatingToolbarState.WooPosCardReaderStatus.NotConnected
+                    )
                     Reconnecting -> flowOf(WooPosHomeFloatingToolbarState.WooPosCardReaderStatus.Reconnecting)
                 }
             }.collect { cardReaderStatus ->
