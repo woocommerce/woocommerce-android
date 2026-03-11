5675e62a3e846c0139b6a0582945735c9928f52a
Andrey <akidinov@gmail.com>
2026-03-11 11:23:16 +0100

    Save recent search when tapping a variable product in search results
    The search was saved for simple products and variation search results
    but not for variable products. By the time a variation is selected,
    the toolbar switches to VariationList state so isSearchOpen() returns
    false and the search term is lost.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchViewModel.kt
@@ -314,6 +314,8 @@ class WooPosItemsSearchViewModel @Inject constructor(
                         )
                     )
                 }
+
+                storeRecentSearch()
             }
 
             is WooPosItemSelectionViewState.Product.VariationSearchResult -> {
