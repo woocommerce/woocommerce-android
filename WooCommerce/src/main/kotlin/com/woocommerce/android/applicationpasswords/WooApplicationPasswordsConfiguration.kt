e94e954b60d3a149a823271714694064aaa6df4b
Irfan Ömür <irfan.omur@automattic.com>
2026-03-12 18:32:13 +0300

    Wait for feature flags in app passwords

--- WooCommerce/src/main/kotlin/com/woocommerce/android/applicationpasswords/WooApplicationPasswordsConfiguration.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/applicationpasswords/WooApplicationPasswordsConfiguration.kt
@@ -2,34 +2,23 @@ package com.woocommerce.android.applicationpasswords
 
 import com.woocommerce.android.AppPrefsWrapper
 import com.woocommerce.android.BuildConfig
-import com.woocommerce.android.di.AppCoroutineScope
 import com.woocommerce.android.util.DeviceInfo
 import com.woocommerce.android.util.FeatureFlag
 import com.woocommerce.android.util.FeatureFlagRepository
 import jakarta.inject.Inject
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import org.wordpress.android.fluxc.network.rest.wpapi.applicationpasswords.ApplicationPasswordsConfiguration
 
 class WooApplicationPasswordsConfiguration @Inject constructor(
     private val appPrefs: AppPrefsWrapper,
     private val featureFlagRepository: FeatureFlagRepository,
-    @AppCoroutineScope private val appCoroutineScope: CoroutineScope
 ) : ApplicationPasswordsConfiguration {
-    private val remoteFeatureFlagDeferred = appCoroutineScope.async(
-        start = CoroutineStart.LAZY,
-        context = Dispatchers.IO
-    ) {
-        featureFlagRepository.isEnabled(FeatureFlag.APP_PASSWORDS_FOR_JETPACK_SITES)
-    }
-
     override val applicationName: String =
         "${BuildConfig.APPLICATION_ID}.app-client.${DeviceInfo.name.replace(' ', '-')}"
 
     override suspend fun isEnabledForJetpackAccess(): Boolean {
-        return appPrefs.jetpackAppPasswordsEnabled &&
-            remoteFeatureFlagDeferred.await()
+        if (!appPrefs.jetpackAppPasswordsEnabled) return false
+
+        featureFlagRepository.awaitRemoteFlagsLoaded()
+        return featureFlagRepository.isEnabled(FeatureFlag.APP_PASSWORDS_FOR_JETPACK_SITES)
     }
 }
