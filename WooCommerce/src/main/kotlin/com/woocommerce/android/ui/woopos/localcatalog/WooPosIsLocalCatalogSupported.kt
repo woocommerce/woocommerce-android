f1a0f564fe33685e9a56b81813bce98b7977af99
malinajirka <malinajirka@gmail.com>
2026-03-13 11:14:51 +0100

    Simplify WooPosIsLocalCatalogSupported to only check file-based sync
    Remove constructor params for deleted classes (fileApproachEnabled,
    isVariationsEndpointAvailable, prefsRepo). The sync approach check
    now only validates WooCommerce version for file-based sync support.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/localcatalog/WooPosIsLocalCatalogSupported.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/localcatalog/WooPosIsLocalCatalogSupported.kt
@@ -2,12 +2,10 @@ package com.woocommerce.android.ui.woopos.localcatalog
 
 import com.woocommerce.android.extensions.semverCompareTo
 import com.woocommerce.android.ui.woopos.common.util.WooPosLogWrapper
-import com.woocommerce.android.ui.woopos.featureflags.WooPosLocalCatalogFileApproachEnabled
 import com.woocommerce.android.ui.woopos.featureflags.WooPosLocalCatalogM1Enabled
 import com.woocommerce.android.ui.woopos.tab.WooPosCanBeLaunchedInTab
 import com.woocommerce.android.ui.woopos.tab.WooPosLaunchability
 import com.woocommerce.android.ui.woopos.tab.WooPosTabShouldBeVisible
-import com.woocommerce.android.ui.woopos.util.datastore.WooPosPreferencesRepository
 import com.woocommerce.android.util.FetchActiveWCPluginVersion
 import com.woocommerce.android.util.GetWooCorePluginCachedVersion
 import org.wordpress.android.fluxc.model.LocalOrRemoteId
@@ -15,9 +13,6 @@ import javax.inject.Inject
 
 class WooPosIsLocalCatalogSupported @Inject constructor(
     private val wooPosLocalCatalogM1Enabled: WooPosLocalCatalogM1Enabled,
-    private val fileApproachEnabled: WooPosLocalCatalogFileApproachEnabled,
-    private val prefsRepo: WooPosPreferencesRepository,
-    private val isVariationsEndpointAvailable: WooPosIsLocalCatalogVariationsEndpointAvailable,
     private val getWooVersion: GetWooCorePluginCachedVersion,
     private val fetchWooVersion: FetchActiveWCPluginVersion,
     private val posTabShouldBeVisible: WooPosTabShouldBeVisible,
@@ -32,7 +27,7 @@ class WooPosIsLocalCatalogSupported @Inject constructor(
             }
         }
 
-        if (!isSyncApproachSupported(localSiteId)) {
+        if (!isSyncApproachSupported()) {
             return false
         }
 
@@ -53,27 +48,13 @@ class WooPosIsLocalCatalogSupported @Inject constructor(
         return true
     }
 
-    private suspend fun isSyncApproachSupported(
-        localSiteId: LocalOrRemoteId.LocalId
-    ): Boolean {
-        if (fileApproachEnabled()) {
-            if (!isFileBasedSyncSupported()) {
-                wooPosLogWrapper.d(
-                    "Local Catalog not supported: WooCommerce version does not support" +
-                        " file-based sync (requires $WC_FILE_BASED_SYNC_MIN_VERSION)."
-                )
-                return false
-            }
-        } else {
-            if (!isVariationsEndpointAvailable()) {
-                wooPosLogWrapper.d("Local Catalog not supported: Variations endpoint not available.")
-                return false
-            }
-
-            if (!prefsRepo.isPeriodicSyncEnabledForSite(localSiteId)) {
-                wooPosLogWrapper.d("Local Catalog not supported: Periodic sync disabled.")
-                return false
-            }
+    private suspend fun isSyncApproachSupported(): Boolean {
+        if (!isFileBasedSyncSupported()) {
+            wooPosLogWrapper.d(
+                "Local Catalog not supported: WooCommerce version does not support" +
+                    " file-based sync (requires $WC_FILE_BASED_SYNC_MIN_VERSION)."
+            )
+            return false
         }
         return true
     }
--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/localcatalog/WooPosIsLocalCatalogSupportedTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/localcatalog/WooPosIsLocalCatalogSupportedTest.kt
@@ -1,12 +1,10 @@
 package com.woocommerce.android.ui.woopos.localcatalog
 
 import com.woocommerce.android.ui.woopos.common.util.WooPosLogWrapper
-import com.woocommerce.android.ui.woopos.featureflags.WooPosLocalCatalogFileApproachEnabled
 import com.woocommerce.android.ui.woopos.featureflags.WooPosLocalCatalogM1Enabled
 import com.woocommerce.android.ui.woopos.tab.WooPosCanBeLaunchedInTab
 import com.woocommerce.android.ui.woopos.tab.WooPosLaunchability
 import com.woocommerce.android.ui.woopos.tab.WooPosTabShouldBeVisible
-import com.woocommerce.android.ui.woopos.util.datastore.WooPosPreferencesRepository
 import com.woocommerce.android.util.FetchActiveWCPluginVersion
 import com.woocommerce.android.util.GetWooCorePluginCachedVersion
 import com.woocommerce.android.viewmodel.BaseUnitTest
@@ -22,8 +20,6 @@ import org.wordpress.android.fluxc.model.LocalOrRemoteId
 class WooPosIsLocalCatalogSupportedTest : BaseUnitTest() {
 
     private var featureFlagM1Enabled: WooPosLocalCatalogM1Enabled = mock()
-    private var fileApproachEnabled: WooPosLocalCatalogFileApproachEnabled = mock()
-    private var preferencesRepository: WooPosPreferencesRepository = mock()
     private var getWooVersion: GetWooCorePluginCachedVersion = mock()
     private var fetchWooVersion: FetchActiveWCPluginVersion = mock()
     private var logger: WooPosLogWrapper = mock()
@@ -39,23 +35,12 @@ class WooPosIsLocalCatalogSupportedTest : BaseUnitTest() {
     @Before
     fun setup() = testBlocking {
         whenever(featureFlagM1Enabled.invoke()).thenReturn(true)
-        whenever(fileApproachEnabled.invoke()).thenReturn(true)
         whenever(getWooVersion()).thenReturn("10.5.0")
         whenever(posTabShouldBeVisible.invoke(false)).thenReturn(Result.success(true))
         whenever(posCanBeLaunchedInTab.invoke(false)).thenReturn(WooPosLaunchability.Launchable)
 
-        val variationsEndpointChecker = WooPosIsLocalCatalogVariationsEndpointAvailable(
-            getWooVersion = getWooVersion,
-            fetchWooVersion = fetchWooVersion,
-            logger = logger,
-            dispatchers = coroutinesTestRule.testDispatchers
-        )
-
         isLocalCatalogSupported = WooPosIsLocalCatalogSupported(
             wooPosLocalCatalogM1Enabled = featureFlagM1Enabled,
-            fileApproachEnabled = fileApproachEnabled,
-            prefsRepo = preferencesRepository,
-            isVariationsEndpointAvailable = variationsEndpointChecker,
             getWooVersion = getWooVersion,
             fetchWooVersion = fetchWooVersion,
             posTabShouldBeVisible = posTabShouldBeVisible,
@@ -86,47 +71,7 @@ class WooPosIsLocalCatalogSupportedTest : BaseUnitTest() {
     }
 
     @Test
-    fun `given file approach disabled and variations endpoint not available, when check invoked, then returns false`() = testBlocking {
-        // GIVEN
-        whenever(fileApproachEnabled.invoke()).thenReturn(false)
-        whenever(getWooVersion()).thenReturn("10.2.9")
-
-        // WHEN
-        val result = isLocalCatalogSupported(SITE_ID)
-
-        // THEN
-        assertThat(result).isFalse()
-    }
-
-    @Test
-    fun `given file approach disabled and all paginated sync checks pass, when check invoked, then returns true`() = testBlocking {
-        // GIVEN
-        whenever(fileApproachEnabled.invoke()).thenReturn(false)
-        whenever(getWooVersion()).thenReturn("10.3.0")
-        whenever(preferencesRepository.isPeriodicSyncEnabledForSite(SITE_ID)).thenReturn(true)
-
-        // WHEN
-        val result = isLocalCatalogSupported(SITE_ID)
-
-        // THEN
-        assertThat(result).isTrue()
-    }
-
-    @Test
-    fun `given file approach disabled and periodic sync disabled, when check invoked, then returns false`() = testBlocking {
-        // GIVEN
-        whenever(fileApproachEnabled.invoke()).thenReturn(false)
-        whenever(preferencesRepository.isPeriodicSyncEnabledForSite(SITE_ID)).thenReturn(false)
-
-        // WHEN
-        val result = isLocalCatalogSupported(SITE_ID)
-
-        // THEN
-        assertThat(result).isFalse()
-    }
-
-    @Test
-    fun `given file approach enabled and WC version below minimum, when check invoked, then returns false`() = testBlocking {
+    fun `given WC version below minimum, when check invoked, then returns false`() = testBlocking {
         // GIVEN
         whenever(getWooVersion()).thenReturn("10.4.9")
 
@@ -138,7 +83,7 @@ class WooPosIsLocalCatalogSupportedTest : BaseUnitTest() {
     }
 
     @Test
-    fun `given file approach enabled and WC version unknown, when check invoked, then returns false`() = testBlocking {
+    fun `given WC version unknown, when check invoked, then returns false`() = testBlocking {
         // GIVEN
         whenever(getWooVersion()).thenReturn(null)
         whenever(fetchWooVersion()).thenReturn(null)
@@ -151,7 +96,7 @@ class WooPosIsLocalCatalogSupportedTest : BaseUnitTest() {
     }
 
     @Test
-    fun `given file approach enabled and cached version null but fetched version sufficient, when check invoked, then returns true`() = testBlocking {
+    fun `given cached version null but fetched version sufficient, when check invoked, then returns true`() = testBlocking {
         // GIVEN
         whenever(getWooVersion()).thenReturn(null)
         whenever(fetchWooVersion()).thenReturn("10.5.0")
