0b91badf43590403721ca82e5269452ff5495a4e
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-04-07 17:47:48 +0100

    Fix icon color in product type picker
    Use a dedicated ic_calendar_add_outline drawable with color_icon fill
    instead of applying a blanket tint on the ImageView, which broke the
    stateful color selectors of other icons.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/typesbottomsheet/ProductTypeBottomSheetBuilder.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/typesbottomsheet/ProductTypeBottomSheetBuilder.kt
@@ -68,7 +68,7 @@ class ProductTypeBottomSheetBuilder @Inject constructor(
                 type = ProductType.BOOKABLE_SERVICE,
                 titleResource = R.string.product_type_bookable_service_title,
                 descResource = R.string.product_type_bookable_service_desc,
-                iconResource = R.drawable.ic_calendar_add,
+                iconResource = R.drawable.ic_calendar_add_outline,
                 isVisible = ciabSiteGateKeeper.isFeatureSupported(CIABAffectedFeature.BookableServiceCreation)
             )
         )
