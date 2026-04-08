27671645ef0b979cb250b16899e3c28c46975920
Andrey <akidinov@gmail.com>
2026-04-08 10:26:41 +0200

    Add reusable WooPosListDetailLayout component for phone/tablet

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosListDetailLayout.kt
@@ -0,0 +1,70 @@
+package com.woocommerce.android.ui.woopos.common.composeui.component
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+
+@Composable
+fun WooPosListDetailLayout(
+    isDetailVisible: Boolean,
+    onBackFromDetail: () -> Unit,
+    listPane: @Composable () -> Unit,
+    detailPane: @Composable () -> Unit,
+    emptyDetailPane: @Composable () -> Unit = {},
+    modifier: Modifier = Modifier,
+    listWeight: Float = 0.3f,
+    detailWeight: Float = 0.7f,
+) {
+    val configuration = LocalConfiguration.current
+    val isPhone = minOf(configuration.screenWidthDp, configuration.screenHeightDp) < 674
+
+    if (isPhone) {
+        BackHandler(enabled = isDetailVisible) {
+            onBackFromDetail()
+        }
+
+        AnimatedContent(
+            targetState = isDetailVisible,
+            transitionSpec = {
+                if (targetState) {
+                    (slideInHorizontally(initialOffsetX = { it }) + fadeIn()) togetherWith
+                        (slideOutHorizontally(targetOffsetX = { -it }) + fadeOut())
+                } else {
+                    (slideInHorizontally(initialOffsetX = { -it }) + fadeIn()) togetherWith
+                        (slideOutHorizontally(targetOffsetX = { it }) + fadeOut())
+                }
+            },
+            label = "list_detail_transition",
+            modifier = modifier.fillMaxSize(),
+        ) { showDetail ->
+            if (showDetail) {
+                detailPane()
+            } else {
+                listPane()
+            }
+        }
+    } else {
+        Row(modifier = modifier.fillMaxSize()) {
+            Box(modifier = Modifier.weight(listWeight)) {
+                listPane()
+            }
+            Box(modifier = Modifier.weight(detailWeight)) {
+                if (isDetailVisible) {
+                    detailPane()
+                } else {
+                    emptyDetailPane()
+                }
+            }
+        }
+    }
+}
