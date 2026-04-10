a07e33fcfa9eb0a9344fb52ba769b95d8fe2e70d
Irfan Ömür <irfan.omur@automattic.com>
2026-04-10 22:43:05 +0300

    Add carrier terms provider enum

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/carriertos/CarrierTermsProvider.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.carriertos
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+enum class CarrierTermsProvider : Parcelable {
+    UPSDAP,
+    FEDEX,
+}
