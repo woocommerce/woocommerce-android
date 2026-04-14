fa7b677691e10fcd2e1be59011061335f0a676cc
Andrey <akidinov@gmail.com>
2026-04-14 09:09:46 +0200

    Simplify phone-flag check to !isTabletSize path

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/WooPosIsScreenSizeAllowed.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/WooPosIsScreenSizeAllowed.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import com.woocommerce.android.ui.woopos.common.util.WooPosLogWrapper
 import com.woocommerce.android.ui.woopos.util.ext.getScreenHeightDp
 import com.woocommerce.android.ui.woopos.util.ext.getScreenWidthDp
-import com.woocommerce.android.ui.woopos.util.ext.isWooPosPhoneLayout
 import com.woocommerce.android.util.FeatureFlag
 import com.woocommerce.android.util.FeatureFlagRepository
 import javax.inject.Inject
@@ -26,9 +25,7 @@ class WooPosIsScreenSizeAllowed @Inject constructor(
         val isTabletSize = shortSize >= MIN_TABLET_SHORT_SIZE_DP && longSize >= MIN_TABLET_LONG_SIZE_DP
         if (isTabletSize) return true
 
-        val isPhoneWithFlagEnabled = context.isWooPosPhoneLayout() &&
-            featureFlagRepository.isEnabled(FeatureFlag.WOO_POS_PHONE)
-        if (isPhoneWithFlagEnabled) return true
+        if (featureFlagRepository.isEnabled(FeatureFlag.WOO_POS_PHONE)) return true
 
         wooPosLog.i(
             "POS Not allowed reason: Screen size is not allowed. " +
