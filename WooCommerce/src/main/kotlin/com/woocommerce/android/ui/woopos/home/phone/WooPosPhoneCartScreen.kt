be0640a9c397681e550f2074165f13b529817c5d
Andrey <akidinov@gmail.com>
2026-04-17 09:05:26 +0200

    WOOMOB-2709 add WooPosPhoneCartScreen wrapper

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/phone/WooPosPhoneCartScreen.kt
@@ -0,0 +1,35 @@
+package com.woocommerce.android.ui.woopos.home.phone
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
+import com.woocommerce.android.ui.woopos.common.composeui.designsystem.WooPosTheme
+import com.woocommerce.android.ui.woopos.common.composeui.isPreviewMode
+import com.woocommerce.android.ui.woopos.home.cart.WooPosCartScreen
+import com.woocommerce.android.ui.woopos.home.cart.WooPosCartScreenProductsPreview
+import com.woocommerce.android.ui.woopos.home.cart.WooPosCartViewModel
+
+@Composable
+fun WooPosPhoneCartScreen(
+    modifier: Modifier = Modifier,
+    viewModel: WooPosCartViewModel = hiltViewModel(),
+) {
+    if (isPreviewMode()) {
+        WooPosCartScreenProductsPreview(modifier.fillMaxSize())
+    } else {
+        WooPosCartScreen(
+            modifier = modifier.fillMaxSize(),
+            viewModel = viewModel,
+        )
+    }
+}
+
+@Composable
+@WooPosPreview
+fun WooPosPhoneCartScreenPreview() {
+    WooPosTheme {
+        WooPosPhoneCartScreen()
+    }
+}
