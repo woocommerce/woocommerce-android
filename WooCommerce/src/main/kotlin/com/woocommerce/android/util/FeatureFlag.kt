e239ff4ebe58ed393ef5ec6cc4e862a55d4a47f7
Andrey <akidinov@gmail.com>
2026-04-20 16:40:19 +0200

    Add REMOTE_TAP_TO_PAY feature flag

--- WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/util/FeatureFlag.kt
@@ -33,4 +33,5 @@ enum class FeatureFlag(
     WOO_SELF_DRIVEN_PUSH_NOTIFICATIONS_M1("woo_self_driven_push_notifications_m1", localValue = false),
     WOO_SELF_DRIVEN_PUSH_NOTIFICATIONS_M2("woo_self_driven_push_notifications_m2", localValue = false),
     LOGGED_OUT_FF_PANEL("logged_out_ff_panel", localValue = PackageUtils.isDebugBuild()),
+    REMOTE_TAP_TO_PAY("remote_tap_to_pay", localValue = PackageUtils.isDebugBuild()),
 }
