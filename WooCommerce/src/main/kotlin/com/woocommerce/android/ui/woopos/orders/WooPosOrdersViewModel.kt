9c2c3661af6139416642382fefc6d1e48b5da484
malinajirka <malinajirka@gmail.com>
2026-04-20 13:22:59 +0200

    Detekt auto-correct

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/WooPosOrdersViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/WooPosOrdersViewModel.kt
@@ -1,11 +1,8 @@
 package com.woocommerce.android.ui.woopos.orders
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class WooPosOrdersViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-) : ViewModel()
+class WooPosOrdersViewModel @Inject constructor() : ViewModel()
