906ece0654b65abcd4e2353149bea1a011b69e30
Andrey <akidinov@gmail.com>
2026-04-22 16:02:51 +0200

    Add CardReaderRemoteSession with NSD, TLS and protocol loop

---
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/remote/CardReaderRemoteSessionState.kt
@@ -0,0 +1,16 @@
+package com.woocommerce.android.cardreader.remote
+
+sealed class CardReaderRemoteSessionState {
+    object Idle : CardReaderRemoteSessionState()
+
+    object Starting : CardReaderRemoteSessionState()
+
+    data class ReadyToPair(
+        val deviceName: String,
+        val fingerprintSuffix: String,
+    ) : CardReaderRemoteSessionState()
+
+    data class WaitingForPayment(
+        val tabletName: String?,
+    ) : CardReaderRemoteSessionState()
+}
