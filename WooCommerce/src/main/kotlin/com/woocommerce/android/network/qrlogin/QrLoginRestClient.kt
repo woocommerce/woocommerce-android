44618f5ef4a0b09ea05e2f79c72883d21d03380b
malinajirka <malinajirka@gmail.com>
2026-04-25 23:57:43 +0200

    Add QrLoginExchangeException sealed hierarchy

--- WooCommerce/src/main/kotlin/com/woocommerce/android/network/qrlogin/QrLoginRestClient.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/network/qrlogin/QrLoginRestClient.kt
@@ -7,3 +7,14 @@ data class QrLoginCredentials(
     val applicationPassword: Secret,
     val uuid: String?
 )
+
+sealed class QrLoginExchangeException(message: String) : Exception(message) {
+    data object TokenRejected : QrLoginExchangeException("Token was rejected by the site")
+    data object EndpointMissing : QrLoginExchangeException("Exchange endpoint not available on the site")
+    data object RateLimited : QrLoginExchangeException("Rate limit hit on the exchange endpoint")
+    data object Network : QrLoginExchangeException("Network failure during exchange")
+    data object MalformedResponse : QrLoginExchangeException("Exchange response was malformed")
+    data class HttpError(val code: Int) : QrLoginExchangeException("HTTP $code from exchange endpoint")
+    data class Unknown(val original: Throwable) :
+        QrLoginExchangeException("Unknown exchange failure: ${original.javaClass.simpleName}")
+}
