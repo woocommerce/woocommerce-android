8a1f36d99516605889eef84411bb406de924d86e
malinajirka <malinajirka@gmail.com>
2026-04-26 00:06:04 +0200

    Add QrLoginAvailability tests

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/login/qrlogin/QrLoginAvailabilityTest.kt
@@ -0,0 +1,46 @@
+package com.woocommerce.android.ui.login.qrlogin
+
+import com.woocommerce.android.util.DeviceFeatures
+import com.woocommerce.android.util.FeatureFlag
+import com.woocommerce.android.util.FeatureFlagRepository
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class QrLoginAvailabilityTest : BaseUnitTest() {
+
+    private val featureFlagRepository: FeatureFlagRepository = mock()
+    private val deviceFeatures: DeviceFeatures = mock()
+
+    private val availability = QrLoginAvailability(featureFlagRepository, deviceFeatures)
+
+    @Before
+    fun setUp() {
+        whenever(featureFlagRepository.isEnabled(FeatureFlag.QR_LOGIN)).thenReturn(true)
+        whenever(deviceFeatures.hasCamera()).thenReturn(true)
+    }
+
+    @Test
+    fun `given flag enabled and camera present, when isAvailable, then true`() {
+        assertThat(availability.isAvailable()).isTrue()
+    }
+
+    @Test
+    fun `given feature flag disabled, when isAvailable, then false`() {
+        whenever(featureFlagRepository.isEnabled(FeatureFlag.QR_LOGIN)).thenReturn(false)
+
+        assertThat(availability.isAvailable()).isFalse()
+    }
+
+    @Test
+    fun `given device has no camera, when isAvailable, then false`() {
+        whenever(deviceFeatures.hasCamera()).thenReturn(false)
+
+        assertThat(availability.isAvailable()).isFalse()
+    }
+}
