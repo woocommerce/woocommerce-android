2afdd307e315db8c64a8a04f4b7a2d52cc332ec4
samiuelson <samiuelson@gmail.com>
2026-04-28 17:07:26 +0200

    Revert navigationBarsPadding from shared WooPosFullScreenInputLayout
    This change affected all screens using the layout and is unrelated to
    the refund flow rewrite. Should be handled separately if needed.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosFullScreenInputLayout.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosFullScreenInputLayout.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -38,7 +37,6 @@ fun WooPosFullScreenInputLayout(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .navigationBarsPadding()
     ) {
         WooPosToolbar(
             titleText = titleText,
