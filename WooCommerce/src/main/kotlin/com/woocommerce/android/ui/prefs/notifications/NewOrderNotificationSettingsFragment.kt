c485ed79cd30ca98da2410f64d874826d277bed8
Irfan Ömür <irfan.omur@automattic.com>
2026-04-30 02:20:42 +0300

    Register new order settings destination

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/notifications/NewOrderNotificationSettingsFragment.kt
@@ -0,0 +1,30 @@
+package com.woocommerce.android.ui.prefs.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.woocommerce.android.R
+import com.woocommerce.android.analytics.AnalyticsTracker
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.composeView
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NewOrderNotificationSettingsFragment : BaseFragment() {
+    private val viewModel: NewOrderNotificationSettingsViewModel by viewModels()
+
+    override fun getFragmentTitle() = getString(R.string.settings_notifs_new_orders)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return composeView {
+            NewOrderNotificationSettingsScreen(viewModel = viewModel)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        AnalyticsTracker.trackViewShown(this)
+    }
+}
