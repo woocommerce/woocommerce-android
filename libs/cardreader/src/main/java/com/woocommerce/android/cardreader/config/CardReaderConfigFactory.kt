a9668534554c8adc4bdd45f8add8e55cf96d6573
Josh Heald <josh.heald@automattic.com>
2026-04-30 17:26:50 +0100

    Refactor CardReaderConfigFactory to map lookup to fix complexity

--- libs/cardreader/src/main/java/com/woocommerce/android/cardreader/config/CardReaderConfigFactory.kt
+++ libs/cardreader/src/main/java/com/woocommerce/android/cardreader/config/CardReaderConfigFactory.kt
@@ -1,26 +1,29 @@
 package com.woocommerce.android.cardreader.config
 
 class CardReaderConfigFactory {
-    fun getCardReaderConfigFor(countryCode: String?): CardReaderConfig {
-        return when (countryCode) {
+    fun getCardReaderConfigFor(countryCode: String?): CardReaderConfig =
+        COUNTRY_CONFIGS[countryCode] ?: CardReaderConfigForUnsupportedCountry
+
+    private companion object {
+        val COUNTRY_CONFIGS: Map<String, CardReaderConfigForSupportedCountry> = mapOf(
             // PR is Puerto Rico and it's a US territory
-            "US", "PR" -> CardReaderConfigForUSA
-            "CA" -> CardReaderConfigForCanada
-            "GB" -> CardReaderConfigForGB
-            "FR" -> CardReaderConfigForFR
-            "DE" -> CardReaderConfigForDE
-            "IE" -> CardReaderConfigForIE
-            "NL" -> CardReaderConfigForNL
-            "SG" -> CardReaderConfigForSG
-            "NZ" -> CardReaderConfigForNZ
-            "AT" -> CardReaderConfigForAT
-            "BE" -> CardReaderConfigForBE
-            "FI" -> CardReaderConfigForFI
-            "IT" -> CardReaderConfigForIT
-            "LU" -> CardReaderConfigForLU
-            "PT" -> CardReaderConfigForPT
-            "ES" -> CardReaderConfigForES
-            else -> CardReaderConfigForUnsupportedCountry
-        }
+            "US" to CardReaderConfigForUSA,
+            "PR" to CardReaderConfigForUSA,
+            "CA" to CardReaderConfigForCanada,
+            "GB" to CardReaderConfigForGB,
+            "FR" to CardReaderConfigForFR,
+            "DE" to CardReaderConfigForDE,
+            "IE" to CardReaderConfigForIE,
+            "NL" to CardReaderConfigForNL,
+            "SG" to CardReaderConfigForSG,
+            "NZ" to CardReaderConfigForNZ,
+            "AT" to CardReaderConfigForAT,
+            "BE" to CardReaderConfigForBE,
+            "FI" to CardReaderConfigForFI,
+            "IT" to CardReaderConfigForIT,
+            "LU" to CardReaderConfigForLU,
+            "PT" to CardReaderConfigForPT,
+            "ES" to CardReaderConfigForES,
+        )
     }
 }
