1107fc9c93075c8d11ceabf90ee1761cd9a6fd38
Irfan Ömür <irfan.omur@automattic.com>
2026-05-08 22:05:16 +0300

    Save new order notification preferences

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/notifications/NewOrderNotificationSettingsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/notifications/NewOrderNotificationSettingsViewModel.kt
@@ -6,6 +6,7 @@ import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsEvent
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.analytics.AnalyticsTrackerWrapper
+import com.woocommerce.android.di.AppCoroutineScope
 import com.woocommerce.android.notifications.NotificationChannelType
 import com.woocommerce.android.notifications.NotificationChannelsHandler
 import com.woocommerce.android.notifications.NotificationChannelsHandler.NewOrderNotificationSoundStatus
@@ -13,19 +14,29 @@ import com.woocommerce.android.notifications.ShowTestNotification
 import com.woocommerce.android.notifications.push.PushNotificationRepository
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.products.ParameterRepository
+import com.woocommerce.android.util.CoroutineDispatchers
 import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.pushnotifications.WooPushNotificationPreferences
 import org.wordpress.android.fluxc.model.pushnotifications.WooPushNotificationPreferences.StoreOrderPreferences
 import java.math.BigDecimal
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.seconds
 
+@OptIn(FlowPreview::class)
 @HiltViewModel
 class NewOrderNotificationSettingsViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
@@ -35,7 +46,9 @@ class NewOrderNotificationSettingsViewModel @Inject constructor(
     private val showTestNotification: ShowTestNotification,
     private val analyticsTracker: AnalyticsTrackerWrapper,
     selectedSite: SelectedSite,
-    private val pushNotificationRepository: PushNotificationRepository
+    private val pushNotificationRepository: PushNotificationRepository,
+    @AppCoroutineScope private val appCoroutineScope: CoroutineScope,
+    private val coroutineDispatchers: CoroutineDispatchers
 ) : ScopedViewModel(savedStateHandle) {
     private val site: SiteModel = selectedSite.get()
 
@@ -45,25 +58,30 @@ class NewOrderNotificationSettingsViewModel @Inject constructor(
             newOrderNotificationSoundStatus = notificationChannelsHandler.checkNewOrderNotificationSound()
         )
     )
+    private val saveOrderPreferencesTrigger = MutableSharedFlow<Unit>(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
     private var savedOrderPreferences = _viewState.value.toStoreOrderPreferences()
 
     val viewState = _viewState.asLiveData()
 
     init {
         observeCachedNotificationPreferences()
+        observeOrderPreferencesChanges()
         fetchNotificationPreferences()
     }
 
     fun onNotificationsEnabledChanged(isEnabled: Boolean) {
-        _viewState.update { it.copy(notificationsEnabled = isEnabled) }
+        updateOrderPreferences(_viewState.value.copy(notificationsEnabled = isEnabled))
     }
 
     fun onNotificationPreferenceChanged(preference: NotificationPreference) {
-        _viewState.update { it.copy(notificationPreference = preference) }
+        updateOrderPreferences(_viewState.value.copy(notificationPreference = preference))
     }
 
     fun onThresholdAmountChanged(amount: BigDecimal) {
-        _viewState.update { it.copy(thresholdAmount = amount.coerceAtLeast(MIN_THRESHOLD_AMOUNT)) }
+        updateOrderPreferences(_viewState.value.copy(thresholdAmount = amount.coerceAtLeast(MIN_THRESHOLD_AMOUNT)))
     }
 
     fun refreshNotificationSettings() {
@@ -113,6 +131,15 @@ class NewOrderNotificationSettingsViewModel @Inject constructor(
         }
     }
 
+    private fun observeOrderPreferencesChanges() {
+        launch {
+            saveOrderPreferencesTrigger
+                .debounce(ORDER_PREFERENCES_SAVE_DEBOUNCE_MS)
+                .conflate()
+                .collect { saveOrderPreferences(_viewState.value.toStoreOrderPreferences()) }
+        }
+    }
+
     private fun fetchNotificationPreferences() {
         launch {
             pushNotificationRepository.fetchWooNotificationPreferences(site)
@@ -127,6 +154,45 @@ class NewOrderNotificationSettingsViewModel @Inject constructor(
                     )
                 }
         }
+    }
+
+    private fun updateOrderPreferences(updatedViewState: ViewState) {
+        if (updatedViewState == _viewState.value) return
+
+        _viewState.value = updatedViewState
+        saveOrderPreferencesTrigger.tryEmit(Unit)
+    }
+
+    private suspend fun saveOrderPreferences(orderPreferences: StoreOrderPreferences) {
+        if (!hasUnsavedOrderPreferences(orderPreferences)) return
+
+        val result = appCoroutineScope.async(coroutineDispatchers.main) {
+            pushNotificationRepository.updateWooNotificationPreferences(
+                site = site,
+                preferences = WooPushNotificationPreferences(storeOrder = orderPreferences)
+            )
+        }.await()
+        val hasNewerOrderPreferences = _viewState.value.toStoreOrderPreferences() != orderPreferences
+
+        result.onSuccess { savedOrderPreferences = orderPreferences }
+            .onFailure {
+                if (!hasNewerOrderPreferences) {
+                    _viewState.update { savedOrderPreferences.toViewState(it) }
+                    showUpdateError(orderPreferences)
+                }
+            }
+    }
+
+    private fun showUpdateError(orderPreferences: StoreOrderPreferences) {
+        triggerEvent(
+            MultiLiveEvent.Event.ShowActionStringSnackbar(
+                message = resourceProvider.getString(R.string.settings_notifs_error_update),
+                actionText = resourceProvider.getString(R.string.retry),
+            ) {
+                _viewState.update { orderPreferences.toViewState(it) }
+                saveOrderPreferencesTrigger.tryEmit(Unit)
+            }
+        )
     }
 
     private fun applyOrderPreferences(orderPreferences: StoreOrderPreferences) {
@@ -173,5 +239,6 @@ class NewOrderNotificationSettingsViewModel @Inject constructor(
     companion object {
         private const val DEFAULT_THRESHOLD_AMOUNT = 100
         private val MIN_THRESHOLD_AMOUNT = BigDecimal.ONE
+        private const val ORDER_PREFERENCES_SAVE_DEBOUNCE_MS = 500L
     }
 }
