5519a31abda6535866183c78eef691b7ebe8187e
samiuelson <samiuelson@gmail.com>
2026-05-08 14:13:23 +0200

    Suppress CyclomaticComplexMethod on onUIEvent dispatcher

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsViewModel.kt
@@ -190,6 +190,7 @@ class WooPosTotalsViewModel @Inject constructor(
         createDraftOrderJob?.cancel()
     }
 
+    @Suppress("CyclomaticComplexMethod")
     fun onUIEvent(event: WooPosTotalsUIEvent) {
         when (event) {
             is WooPosTotalsUIEvent.OnNewTransactionClicked -> viewModelScope.launch {
