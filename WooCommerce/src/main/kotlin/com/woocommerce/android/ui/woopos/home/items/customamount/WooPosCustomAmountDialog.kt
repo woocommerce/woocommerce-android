25e18dfb99a1c98437a9b9014f5933a325bc5982
samiuelson <samiuelson@gmail.com>
2026-05-13 17:06:35 +0200

    POS: add nav-bar padding to custom amount form submit button

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/customamount/WooPosCustomAmountDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/customamount/WooPosCustomAmountDialog.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.toggleable
@@ -75,6 +76,7 @@ fun WooPosCustomAmountFormScreen(
         Box(
             modifier = Modifier
                 .fillMaxWidth()
+                .navigationBarsPadding()
                 .padding(WooPosSpacing.Medium.value),
         ) {
             FormSubmitButton(state = state, onSubmit = viewModel::onSubmit)
