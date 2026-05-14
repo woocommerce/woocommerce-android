cfb58d5fb14bc490781e1537b2176375a9e36235
Irfan Ömür <irfan.omur@automattic.com>
2026-05-14 22:29:48 +0300

    Wire new review detail screen to shared notification state

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/notifications/NewReviewNotificationSettingsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/notifications/NewReviewNotificationSettingsFragment.kt
@@ -4,27 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import com.woocommerce.android.R
 import com.woocommerce.android.analytics.AnalyticsTracker
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.base.UIMessageResolver
 import com.woocommerce.android.ui.compose.composeView
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class NewReviewNotificationSettingsFragment : BaseFragment() {
-    private val viewModel: NewReviewNotificationSettingsViewModel by viewModels()
+    private val sharedViewModel: NotificationSettingsSharedViewModel by hiltNavGraphViewModels(
+        R.id.nav_graph_notification_settings
+    )
+
+    @Inject
+    lateinit var uiMessageResolver: UIMessageResolver
 
     override fun getFragmentTitle() = getString(R.string.settings_notifs_new_reviews)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return composeView {
-            NewReviewNotificationSettingsScreen(viewModel = viewModel)
+            NewReviewNotificationSettingsScreen(sharedViewModel = sharedViewModel)
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        observeEvents()
     }
 
     override fun onResume() {
         super.onResume()
         AnalyticsTracker.trackViewShown(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sharedViewModel.savePendingNotificationPreferences()
+    }
+
+    private fun observeEvents() {
+        sharedViewModel.event.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is MultiLiveEvent.Event.ShowActionStringSnackbar -> uiMessageResolver.showActionSnack(
+                    event.message,
+                    event.actionText,
+                    event.action
+                )
+            }
+        }
     }
 }
