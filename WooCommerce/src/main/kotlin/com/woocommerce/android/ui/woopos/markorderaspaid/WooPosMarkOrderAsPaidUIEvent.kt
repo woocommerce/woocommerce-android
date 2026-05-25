458b4e516019c44da5ea5a6a9ee4fcf1442e45b8
samiuelson <samiuelson@gmail.com>
2026-05-25 10:51:53 +0200

    POS: drop stale mark-as-paid domain duplicates before trunk merge

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/markorderaspaid/WooPosMarkOrderAsPaidUIEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.woocommerce.android.ui.woopos.markorderaspaid
-
-sealed class WooPosMarkOrderAsPaidUIEvent {
-    data class NoteChanged(val newNote: String) : WooPosMarkOrderAsPaidUIEvent()
-    data object ConfirmClicked : WooPosMarkOrderAsPaidUIEvent()
-}
