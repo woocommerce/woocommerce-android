537fe4c4022e71195528c60247c4fce851588d17
Josh Heald <josh.heald@automattic.com>
2026-05-26 18:36:28 +0100

    Persist handled POS refund dismiss token

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/details/refund/WooPosIssueRefundScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/orders/details/refund/WooPosIssueRefundScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -200,12 +201,12 @@ fun WooPosIssueRefundScreen(
         }
     }
 
-    var lastDismissRequestToken by remember(viewModelKey) { mutableIntStateOf(dismissRequestToken) }
+    var lastDismissRequestToken by rememberSaveable(viewModelKey) { mutableIntStateOf(0) }
     LaunchedEffect(dismissRequestToken) {
         if (dismissRequestToken != lastDismissRequestToken && dismissRequestToken > 0) {
             handleDismiss()
+            lastDismissRequestToken = dismissRequestToken
         }
-        lastDismissRequestToken = dismissRequestToken
     }
 
     val state by viewModel.state.collectAsStateWithLifecycle()
