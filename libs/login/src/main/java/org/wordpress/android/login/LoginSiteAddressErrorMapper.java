52e922e02a5d2c62c8fe1a39eeb273b207ccabaa
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-06-03 19:58:00 +0100

    Fix login mapper nullability lint

--- libs/login/src/main/java/org/wordpress/android/login/LoginSiteAddressErrorMapper.java
+++ libs/login/src/main/java/org/wordpress/android/login/LoginSiteAddressErrorMapper.java
@@ -1,5 +1,6 @@
 package org.wordpress.android.login;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 
 import org.wordpress.android.fluxc.store.SiteStore.SiteError;
@@ -7,7 +8,7 @@ import org.wordpress.android.fluxc.store.SiteStore.SiteErrorType;
 
 class LoginSiteAddressErrorMapper {
     @StringRes
-    int getSiteInfoErrorResId(SiteError error, boolean networkAvailable) {
+    int getSiteInfoErrorResId(@NonNull SiteError error, boolean networkAvailable) {
         if (error.type == SiteErrorType.TLS_CERTIFICATE_VALIDITY_ERROR) {
             return R.string.error_site_url_certificate_validity;
         } else if (error.type == SiteErrorType.REMOTE_SITE_CERTIFICATE_ERROR) {
