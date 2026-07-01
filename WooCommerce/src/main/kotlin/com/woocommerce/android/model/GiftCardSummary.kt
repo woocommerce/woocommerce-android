97a488c7310fb4ff456e2e7a8c249bd3c3ef415f
AdamGrzybkowski <agrzybkowski@outlook.com>
2026-07-01 11:14:19 +0200

    Map order gift cards onto the Order domain model

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
@@ -1,13 +1,16 @@
 package com.woocommerce.android.model
 
+import android.os.Parcelable
 import com.woocommerce.android.network.giftcard.GiftCardRestClient
+import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
+@Parcelize
 data class GiftCardSummary(
     val id: Long,
     val code: String,
     val used: BigDecimal
-)
+) : Parcelable
 
 fun GiftCardRestClient.GiftCardSummaryDto.toAppModel(): GiftCardSummary {
     return GiftCardSummary(
