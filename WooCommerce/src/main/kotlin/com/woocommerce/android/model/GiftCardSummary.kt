ee3673669e5630916d30a8f97b457e3c2d99e027
AdamGrzybkowski <agrzybkowski@outlook.com>
2026-07-01 11:15:03 +0200

    Remove the now-unused gift-card summary fetch

--- WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/model/GiftCardSummary.kt
@@ -1,7 +1,6 @@
 package com.woocommerce.android.model
 
 import android.os.Parcelable
-import com.woocommerce.android.network.giftcard.GiftCardRestClient
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
@@ -11,11 +10,3 @@ data class GiftCardSummary(
     val code: String,
     val used: BigDecimal
 ) : Parcelable
-
-fun GiftCardRestClient.GiftCardSummaryDto.toAppModel(): GiftCardSummary {
-    return GiftCardSummary(
-        id = this.id ?: 0,
-        code = this.code.orEmpty(),
-        used = this.amount?.toBigDecimalOrNull() ?: BigDecimal.ZERO
-    )
-}
