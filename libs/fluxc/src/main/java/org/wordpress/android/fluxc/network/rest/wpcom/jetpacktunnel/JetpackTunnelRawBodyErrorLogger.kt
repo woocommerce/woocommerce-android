1e6e0678967b76d53a82e3b49740f9ced9424328
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-07-07 20:27:09 +0100

    Mark raw body formatter visible for tests

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/jetpacktunnel/JetpackTunnelRawBodyErrorLogger.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/jetpacktunnel/JetpackTunnelRawBodyErrorLogger.kt
@@ -1,9 +1,11 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel
 
+import androidx.annotation.VisibleForTesting
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequest.WPComGsonNetworkError
 import org.wordpress.android.util.AppLog
 
 object JetpackTunnelRawBodyErrorLogger {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     fun buildMessage(method: String, path: String, error: WPComGsonNetworkError): String? {
         val rawBody = error.errorData?.optString("raw_body")?.takeIf { it.isNotBlank() } ?: return null
         val sanitizedRawBody = sanitize(rawBody)
