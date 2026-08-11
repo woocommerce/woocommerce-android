0ff1f93fb05fa4c64bf6461c5d73d8554d08528e
malinajirka <malinajirka@gmail.com>
2026-08-11 15:54:59 +0200

    Restore default padding on JITM banner CTA button
    The custom contentPadding zeroed out all sides (not just start),
    leaving the pressed/ripple background flush with the button text.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/banner/Banner.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/banner/Banner.kt
@@ -3,11 +3,11 @@ package com.woocommerce.android.ui.payments.banner
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -108,7 +108,7 @@ fun Banner(bannerState: JitmState.Banner) {
 
             TextButton(
                 onClick = bannerState.onPrimaryActionClicked,
-                contentPadding = PaddingValues(start = dimensionResource(id = R.dimen.minor_00)),
+                contentPadding = ButtonDefaults.TextButtonContentPadding,
                 modifier = Modifier.constrainAs(ctaButton) {
                     top.linkTo(description.bottom)
                     start.linkTo(parent.start, margin = majorMargin)
