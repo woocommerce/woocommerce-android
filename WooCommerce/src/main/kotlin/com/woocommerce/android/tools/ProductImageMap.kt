b92a722fcf616f78e11a9a77f72ac5ef42077a86
Irfan Ömür <irfan.omur@automattic.com>
2026-08-30 14:44:50 +0300

    Make WCProductStore#getProductByRemoteId suspendable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/tools/ProductImageMap.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/tools/ProductImageMap.kt
@@ -50,7 +50,7 @@ class ProductImageMap @Inject constructor(
 
         // product isn't in our map so get it from the database
         selectedSite.getIfExists()?.let { site ->
-            productStore.getProductByRemoteId(site, remoteProductId)?.getFirstImageUrl()?.let { imageUrl ->
+            productStore.getProductByRemoteIdBlocking(site, remoteProductId)?.getFirstImageUrl()?.let { imageUrl ->
                 map[remoteProductId] = imageUrl
                 pendingRequestIds.remove(remoteProductId)
                 return imageUrl
