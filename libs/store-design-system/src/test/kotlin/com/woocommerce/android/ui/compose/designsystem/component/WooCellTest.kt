6792eada16986c031c0d95eb58a760b0603e7757
Hicham Boushaba <hicham.boushaba@gmail.com>
2026-09-04 15:00:20 +0100

    Use muted descriptions in More

--- libs/store-design-system/src/test/kotlin/com/woocommerce/android/ui/compose/designsystem/component/WooCellTest.kt
+++ libs/store-design-system/src/test/kotlin/com/woocommerce/android/ui/compose/designsystem/component/WooCellTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.testTag
@@ -53,6 +54,17 @@ class WooCellTest {
 
         assertThat(style.containerColor).isEqualTo(colors.surface.bright)
         assertThat(style.slotContentColor).isEqualTo(colors.surface.onVariantLowest)
+    }
+
+    @Test
+    fun `given disabled cell with description color override, when color resolves, then lowest variant wins`() {
+        val descriptionColor = wooCellDescriptionColor(
+            enabled = false,
+            descriptionColor = Color.Magenta,
+            colors = colors,
+        )
+
+        assertThat(descriptionColor).isEqualTo(colors.surface.onVariantLowest)
     }
 
     @Test
