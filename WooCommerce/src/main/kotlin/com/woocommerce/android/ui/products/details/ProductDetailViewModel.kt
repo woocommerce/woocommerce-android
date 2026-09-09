68bcf56874ead609f3133a1de6430c864a60d2e6
Andrey <andrey.kidinov@a8c.com>
2026-09-09 10:04:45 +0200

    Update WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/details/ProductDetailViewModel.kt
    Co-authored-by: Irfan Ömür <irfan.omur@automattic.com>

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/details/ProductDetailViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/details/ProductDetailViewModel.kt
@@ -2077,7 +2077,8 @@ class ProductDetailViewModel @Inject constructor(
 
     /**
      * Show the upsell/cross-sell promo if it hasn't already been shown and the product
-     * doesn't already have linked products
+     * doesn't already have linked products and the store has at least
+     * [MIN_PUBLISHED_PRODUCTS_FOR_LINKED_PRODUCTS_PROMO] published products
      */
     private fun checkLinkedProductPromo() {
         if (appPrefsWrapper.isPromoBannerShown(PromoBannerType.LINKED_PRODUCTS) ||
