da82f17e364a2112650907d7ab7f53283b3a9307
Wojtek Zieba <wojtek.zieba@automattic.com>
2026-09-25 12:03:49 +0200

    Use merged JUnirReport.xml file for annotation results
    Previously we got the 'first xml file' which could be a result of just one matrix

--- .buildkite/commands/run-instrumented-tests.sh
+++ .buildkite/commands/run-instrumented-tests.sh
@@ -27,7 +27,7 @@ if [[ "$TESTS_EXIT_STATUS" -ne 0 ]]; then
 fi
 
 echo "--- 🚦 Report Tests Status"
-results_file=$(find "build/instrumented-tests" -type f -name "*.xml" -print -quit)
+results_file="build/instrumented-tests/JUnitReport.xml"
 
 if [[ $BUILDKITE_BRANCH == trunk ]] || [[ $BUILDKITE_BRANCH == release/* ]]; then
     annotate_test_failures "$results_file" --slack "build-and-ship"
